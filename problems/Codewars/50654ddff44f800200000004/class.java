108a2e334aa3626fece2249d70f733767a3ef96e
Rascal_Two <therealrascaltwo@gmail.com>
2015-10-28 03:21:35 +0000

    Add Codewars 50654ddff44f800200000004 Java solution

--- problems/Codewars/50654ddff44f800200000004/class.java
+++ problems/Codewars/50654ddff44f800200000004/class.java
@@ -1,20 +1,20 @@
 //	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
 
-z={E?% -?9@9@!',+0[1G|
-1s1^^4 C1/)w/ 49:,"+_p#$!YD5x^=/v`0M6/OuxV\g
+:<+J%S +>})p]1}aOW[/>u
+}/4hG2 i^38T$ a*=5v}_?(ny`/,CTC8WnQogBi)6-62
 
-`r,p3S ZLBXq Y7!*u(*Tg#'8<*QE_|W g
-    75/%P
-    @)B<3( |$j] 635]V+D'9.$a9JEuPE,^6g]8,h>DO!`!+8M"e* N,t:(: 86F/%K+<& 4
-        Czw fx&R 5 9 _1 # j 5xvQ y,$w z
-            _&<S3! 8 % )6!!O'77,=C/R8
-            mGE>v? L z vm%@8."O;QrmT<
-            W\[G`L ! d Th@&0?={p'9[6"
-            ex]B/@ 3Ji\\iU l 1kJ9s2p>5w!6DL+=(x I vg6 ! -7 *] ^o y (1T 4 {4y1? !B '@ M" eH m@ <QP
-            pz4!V!pU>rkO=%4Cek8[S
-                    ^'#6%M)W`:>"?U=EM/%zW@=K$".(]'<@.&T:60 9/9 ?>;
-                    R7|2W*5x_*n,|4S]Ab[- e[AaB=sm(Q4-6:?m*"0i "$mU
-                    .EfBXy}
-        x
-    B
-? 
+Pz_?2; <?3Z\ ne&5@{$w,7u"T0)5=&o $
+    \>2Tx
+    zbF#!@ 4_+6 jOG4}sPv8.Oa-R$w$_qbxF{:z%`a(R?$cV4|6- bPG%;` z_}M.`|[[ J
+        Go2 5:7* ! : EQ 1 u Ah;4 =0() W
+            6..:84 ; c =+?`;RHg*bT%|2
+            ]979<K " # mlU4aXwD"lz0&6
+            ^I"uFN | ; ;.X$:n##+`_$J6
+            >d5-%7 UMnKPVR ; {$]%;B^A|)a->I]icA x (%T T !/ C^ -) # ?}J F \,>UG Uq :# RN 95 -9 ;X\
+            892:R0&yM\x95Rc|_&y0<
+                    Zl$yEa#|56,tw^J{jf^Q#_#"+;?"v^@j2\\8`E ;&B Q,%
+                    !j^)s|$:M3#7:17([x<I Y8-?[(s2-0r%8-^>[p*[ |c5t
+                    {E2V24,
+        {
+    {
+| 
