945d6d3a183f7b803377f82ee4fd47148cfb2d21
Rascal_Two <therealrascaltwo@gmail.com>
2015-10-28 03:21:35 +0000

    Add Codewars 50654ddff44f800200000004 Java solution

---
+++ problems/Codewars/50654ddff44f800200000004/class.java
@@ -0,0 +1,20 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$% &*()_+[];'./,\<
+?:"{}| -aBcDe gHiJkLmNoPqRsTuVwXyZ0123456789
+
+@#$%^& ()_+[ ;'./,\<>?:"{}|=-AbC E
+    KlMnO
+    UvWxYz 1234 6789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBc eFgHiJ LmNoPqRsT V
+        567 9`!@ $ ^ *( _ [ ;'./ \<>? "
+            IjKlMn p r tUvWxYz0123456
+            )_+[]; . , <>?:"{}|=-aBcD
+            RsTuVw y 0 23456789`!@#$%
+            ,\<>?: {}|=-Ab d fGhIjKlMnOpQrStUvW Y 012 4 67 9` @# % &*( _ [];'. ,\ >? "{ |= aB DeF
+            TuVwXyZ0123456789`!@#
+                    "{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz012345 789 !@#
+                    "{}|=-aBcDeFgHiJkLmN PqRsTuVwXyZ012345678 `!@#
+                    "{}|=-A
+        K
+    Q
+S 
