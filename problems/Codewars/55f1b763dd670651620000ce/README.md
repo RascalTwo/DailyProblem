ccd51e6a297e9de43a2b29614d4eacfa547561f9
Rascal_Two <therealrascaltwo@gmail.com>
2015-10-28 03:35:26 +0000

    Add Codewars 55f1b763dd670651620000ce Java solution

---
+++ problems/Codewars/55f1b763dd670651620000ce/README.md
@@ -0,0 +1,13 @@
+[_metadata_:generated]: - "true"
+
+# Counting Characters
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`55f1b763dd670651620000ce`](https://www.codewars.com/kata/55f1b763dd670651620000ce) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/java_un8ru7.svg" alt="Java" title="Java" width="50" />](class.java) |
+
+<!-- INFO TABLE END -->
+
+Define a method named `countCharOccurrences` that accepts a string and a char as inputs and returns the number of times the char occurs in the string as an int.
---
+++ problems/Codewars/55f1b763dd670651620000ce/class.java
@@ -0,0 +1,40 @@
+//	Code available at https://codewars.com/kata/reviews/55f1c65ce535beb34300006d/groups/6260e377d5ce8200010b4adf in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$% &*()_+ ];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRs
+uVwXyZ 123456 89`!@#$%^&*()_+[];'./,\<>?:"{}
+
+-AbCdE GhIjKlMnOpQrStUvW
+Yz0123 56789`!@#$%^&*()_+[];'./,\<>
+:"{}|= aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789
+!@#$%^ *()_+[];'./,\<>?:"{}|=-AbCd
+
+GhIjK MnOpQrStU W
+  01234
+  89`! #$%^&*()_+[];'./,\<>?: {
+    BcDeFgHiJkLmNoP RsTuVwXyZ0123456789`!@#$%^&*()_+[];'./ \<>?:"
+    AbCdEfGhIjKlMnO QrStUvWxYz0123456789`!@#$%^&*()_+[ ;'./,\
+  :
+
+  =-aBc
+  gHiJ LmNoPqRsTuVwXyZ012345 7
+    #$%^&*()_+[];'. ,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz 123456
+  `
+
+  %^&*(
+  [];' /,\<>?:"{}|=-aBcDeFg i
+    oPqRsTuVwXyZ012 456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCd fGhIjK
+  O
+
+  tUvWx
+  1234 6789`!@#$%^&*()_+[];'./, <
+    }|=-aBcDeFgHiJkLmNoPqRsT VwXyZ01234 6789`!@ $ ^&* )_ [] './,\<>?:"{}|=-AbC Ef hIjKlM O Qr tUvWxYz0123
+    9`!@#$%^&*()_+[] './,\<>?:"{}|=- B De gH JkL NoPqRsTuVwXyZ01234 6789`!@#$%^&*()_+[]; ./,\<>?:"{}|=-AbCdEf
+            tUvWxYz0123 56 89 ! #$%^&* )_+[];'./,\<>?:"{}|=-a
+
+    HiJ LmNo q s uV X Z 123 5678 `
+      *() +[]; . ,\<>?:"{}|=-AbCdEfGhIj
+      rStU WxYz01 3 56789`!@#$%^&*(
+      ./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwX Z0123456 89`!@#$%^&*()_+[];'./,\<>?:"{}|= AbCdEfGhI
+    O
+  S
+U
