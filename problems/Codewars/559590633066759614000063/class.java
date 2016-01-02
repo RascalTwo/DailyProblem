8f466d37e1b5e47317ca0d2e3000860de3bfbeb4
Rascal_Two <therealrascaltwo@gmail.com>
2016-01-02 06:19:32 +0000

    Add Codewars 559590633066759614000063 Java solution

---
+++ problems/Codewars/559590633066759614000063/class.java
@@ -0,0 +1,47 @@
+//	Code available at https://codewars.com/kata/reviews/55ae4736892c21a1950000c5/groups/56876c03e2591ad63600004d in accordance with https://docs.codewars.com/community/rules/#rules
+
+(G2a6> b5O1w#Zci6^8'H^
+%V*k\m }6'60'0}OM/o{c[_)
+87uamv 7Hax\XsPFd]'o&&j5
+6Q/V{v 43A'_b .-{0:n`}:<z5:.=492u1)HBjV:8*CI)':,]
+
+T`#A]B +@>"1 <tQ,tg!u(' U
+
+    dG"44\ Tc0++
+    
+    Ae0}$"E
+    {;e8%= qY(@ rC.,5365T/ `
+        m,cm + t}) }\S,WB.N%
+    u
+    
+    [-_VH
+    _65-<. h=-( rm=%u)A3o#{;p8G}l# *
+      _{ZqiC3H,^3HCiZ[-8V+^ ,y3r-1#}9?v _a;qo(CxF2Q=ps2<r ^#={<4SGt;*-/&(UvNn
+      ]_'?&I&0Jk.Q|OSz7%U^' 8,QY2o'9 3=;d0Z2X! L[l|H$l;8x:b$<[:/ /p8h=t;2<]K#]/M5LM_
+      -<(5c6?@w["]%_*]v(GZc Vb9%:NN_ 7?L 0o?wstYyqUMq+e]A= 3q56pf^I(x.
+    w
+
+    w#e'Q
+    "6,F<= #1.$ <\&DQ,48axz23I([mG /
+        @9:[2?! n # %M F [ 2]& KcPR Y
+            (}` 4 G z6;f02?)kX:2$9R
+            C4:#WZ)#T<^W>lg6!7W|+ 3")@{|a0 }1S _93\gk4w:+#5'`\T3 |{l7zj%3s3V
+        H
+    &
+    
+    9=k+@
+    9+,[j_ iwtd }$kmEW[.|{4O6W9a2 ,
+        u;$W^ I#D[wL
+        O>bQ9+< B / [< f # Qsau :f3e q
+            v80>u 0 7%b >5E|4E"H?fJ071K9&bE!?"yzp2g(r / }l67 \ {<ig
+            [D@ h@5 ] SbrXk\'M>E00rG/J5K V8@ ] 4=P/.t3pR05=Q75/]T
+            x4{O*FK # # &v 4 P &TY*W:Pz^dX&5 N%z4 U
+                I"] > r C/].{MV'U=7t2( 0 uE>12 z p[
+                W`1 H {N ? {@_wb @ , 282x
+                0Z@ 9 T+ e Y^1$I [ J n'[9
+                \[=ve=y$ F sm
+            3
+            }c#^x\:9%`{f:HXCH(_=l W%d_A 2j%[^ WF^/S T?i9)$4b,lU'i81%.{b@y]
+        m
+    ;
+|
