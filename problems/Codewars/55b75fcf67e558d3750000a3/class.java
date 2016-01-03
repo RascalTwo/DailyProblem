9cc6c1ad517a281b002e158bcc7353dcf0e5b2f1
Rascal_Two <therealrascaltwo@gmail.com>
2016-01-03 03:18:15 +0000

    Add Codewars 55b75fcf67e558d3750000a3 Java solution

---
+++ problems/Codewars/55b75fcf67e558d3750000a3/class.java
@@ -0,0 +1,32 @@
+//	Code available at https://codewars.com/kata/reviews/55c25e45539ef0f2b90000a5/groups/56889301a30a3bb7c5000004 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$% &*()_+[];'./,\<
+?:"{}| -aBcDe gHiJkLmNoPqRsTuVwXyZ0123456789
+
+@#$%^& ()_+[ ;'./,\<>?: {
+    bCdEf
+    lMnOpQ StUv xYz0123456789`!@# %^&*() +[];'./,\ >
+        BcDeFgH J L NoP R TuV XyZ01
+        	@#$%^ * ) +[] './,\<>?: {}|=-AbCdEfGhI
+          UvWxYz0123456789`!@#$%^&*()_+
+          :"{}|=-aBcDeFgHiJkLmNoPqRsTuVw
+          89`!@#$%^&*()_+[];'./,\<>?:"{}
+          IjKlMnOpQrStUv x z 1 3456789`!@#$%^&*(
+          <>?:"{}|=-aBcD F Hi k m o qRsTuVwXyZ0123456789`!@
+        +
+    /
+    :"{}|
+    dEfGhI KlMn pQrStUvWxYz01 345678 `!@#$%^&*(
+    	'./ \<>? " } =- B D FgH JkLmN
+      	wXy 0 2 456789`!@#$%^&*()_+ ] '. , <>?:"
+        EfG I K MnOpQrStUvWxYz01234 6 89 ! #$%^&
+        ./, < ? "{}|=-aBcDeFgHiJkLm o qR T VwXyZ
+        9`!@# %^&*( _ []; ./,\<>?:" }|=-AbCdEfGhIj
+        tUvWxYz0123456789`!@#$%^&*()_+[];
+        "{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ01
+        !@#$%^&*()_+[];'./,\<>?:"{}|=-AbCd
+        nOpQrStUvWxYz0 2 4 6 89`!@#$%^&*()_+[];'./
+        |=-aBcDeFgHiJk m oPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];
+      ?
+    =
+A 
