8f2d000c679bec34fe458c579a40bc7e1ad04881
Rascal_Two <therealrascaltwo@gmail.com>
2020-10-18 23:02:52 -0500

    Update DailyCoding 005 Python solution

--- 003/solve.py
+++ 003/solve.py
@@ -1,21 +1,23 @@
 import json
 
-from typing import Optional
+from typing import Optional, Generic, TypeVar
 
 
 
-class Node:
-	def __init__(self, val, left=None, right=None):
+T = TypeVar('T')
+
+class Node(Generic[T]):
+	def __init__(self, val: T, left: Optional['Node[T]'] = None, right: Optional['Node[T]'] = None):
 		self.val = val
 		self.left = left
 		self.right = right
 
 
-def serialize(root: Optional[Node]) -> str:
+def serialize(root: Optional[Node[T]]) -> str:
 	return json.dumps({'val': root.val, 'left': serialize(root.left), 'right': serialize(root.right)} if root else None)
 
 
-def deserialize(string: str) -> Node:
+def deserialize(string: str) -> Node[T]:
 	return Node(args['val'], deserialize(args['left']), deserialize(args['right'])) if (args := json.loads(string)) else None
 
 
