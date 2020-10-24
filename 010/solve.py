cbc1ce63c509fcd14e1fbaa379613e87f85f2675
Rascal_Two <therealrascaltwo@gmail.com>
2020-10-23 23:40:58 -0500

    Add DailyCoding 010 Python solution

---
+++ 010/solve.py
@@ -0,0 +1,25 @@
+import time
+import threading
+
+
+from typing import Any, Callable
+
+
+
+def solve(f: Callable[[], Any], n: int) -> None:
+	threading.Timer(n / 1000, f).start()
+
+
+def test_solve():
+	memory = []
+	def function():
+		memory.append(int(time.time() / 10))
+
+	for delay in (10, 100, 1000, 2500):
+		memory = []
+		solve(function, delay)
+		assert memory == []
+		time.sleep((delay - (delay / 10)) / 1000)
+		assert memory == []
+		time.sleep((delay / 10) / 1000)
+		assert memory == [int(time.time() / 10)]
