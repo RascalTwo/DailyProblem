23c80c69cd5c25f1ea3bd2da614eb8e8f11cda1b
Rascal_Two <therealrascaltwo@gmail.com>
2020-10-29 21:46:15 -0500

    Add DailyCoding 016 Python solution

---
+++ 016/solve.py
@@ -0,0 +1,37 @@
+class OrderRecorder:
+	def __init__(self, max_orders: int):
+		self.max_orders = max_orders
+		self.last = 0
+		self.orders = {}
+
+
+	def record(self, order_id: int) -> None:
+		self.last += 1
+
+		if self.last > self.max_orders:
+			del self.orders[self.last - self.max_orders]
+
+		self.orders[self.last] = order_id
+
+
+	def get_last(self, i: int) -> int:
+		return self.orders[self.last - i + 1]
+
+
+def test_solve():
+	db = OrderRecorder(3)
+	order_ids = [111, 222, 333, 444]
+	for order_id in order_ids[:3]:
+		db.record(order_id)
+	for i, order_id in enumerate(reversed(order_ids[:3])):
+		assert db.get_last(i + 1) == order_id
+
+	db.record(order_ids[3])
+	for i, order_id in enumerate(reversed(order_ids[1:4])):
+		assert db.get_last(i + 1) == order_id
+
+	try:
+		db.get_last(4)
+		assert False, 'Expected IndexError'
+	except IndexError:
+		pass
