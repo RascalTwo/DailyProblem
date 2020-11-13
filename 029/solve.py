001202614f35dc811162daec8582d7487da27251
Rascal_Two <therealrascaltwo@gmail.com>
2020-11-12 22:37:13 -0500

    Add DailyCoding 029 Python solution

---
+++ 029/solve.py
@@ -0,0 +1,34 @@
+def decode(encoded: str) -> str:
+	result = ''
+
+	multiplier = ''
+	for char in encoded:
+		if char.isdigit():
+			multiplier += char
+		else:
+			result += int(multiplier) * char
+			multiplier = ''
+
+	return result
+
+
+def encode(string: str) -> str:
+	result = ''
+	duplicates = string[0]
+	for i, char in enumerate(string[1:]):
+		if char in duplicates:
+			duplicates += char
+		else:
+			result += f'{len(duplicates)}{duplicates[0]}'
+			duplicates = string[i + 1]
+
+	result += f'{len(duplicates)}{duplicates[0]}'
+	return result
+
+
+def test_solve():
+	assert encode('AAAABBBCCDAA') == '4A3B2C1D2A'
+	assert decode('4A3B2C1D2A') == 'AAAABBBCCDAA'
+
+	assert encode('BAAAAAAAAAAAAAAAB') == '1B15A1B'
+	assert decode('1B15A1B') == 'BAAAAAAAAAAAAAAAB'
