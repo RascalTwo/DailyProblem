300e63604b5beb8c597b4dcb0a42d4887cd7e7b3
Rascal_Two <therealrascaltwo@gmail.com>
2021-01-08 22:48:08 -0500

    Add DailyCoding 001 Python solution

--- 001/solve.py
+++ 001/solve.py
@@ -4,12 +4,24 @@ from typing import List
 
 
 
-def solve(numbers: List[int], k: int, length: int = 2) -> bool:
+def solve_itertools(numbers: List[int], k: int, length: int = 2) -> bool:
 	return any(sum(combo) == k for combo in itertools.combinations(numbers, length))
 
 
+def solve_map(numbers: List[int], k: int) -> bool:
+	others = {}
+	for num in numbers:
+		other = k - num
+		if other in others:
+			return True
+		others[num] = other
+
+	return False
+
+
 def test_solve():
-	assert solve([10, 15, 3, 7], 17)
-	assert solve([9, 4, 3, 6], 39) is False
-	assert solve([7, 3, 15, 10], 17)
+	for solve in [solve_itertools, solve_map]:
+		assert solve([10, 15, 3, 7], 17)
+		assert solve([9, 4, 3, 6], 39) is False
+		assert solve([7, 3, 15, 10], 17)
 
