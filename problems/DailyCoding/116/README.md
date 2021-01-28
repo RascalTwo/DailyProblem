39588398a156e19fd9483e6d5c6cea0d52356feb
Rascal_Two <therealrascaltwo@gmail.com>
2021-01-28 13:33:26 -0500

    Add DailyCoding 116 Python solution

---
+++ problems/DailyCoding/116/README.md
@@ -0,0 +1,9 @@
+[_metadata_:number]:-      "116"
+[_metadata_:difficulty]:-  "Medium"
+[_metadata_:asker]:-       "Street"
+[_metadata_:tags]:-        "binary-tree"
+
+# 116
+
+Generate a finite, but an arbitrarily large binary tree quickly in O(1).
+That is, `generate()` should return a tree whose size is unbounded but finite.
---
+++ problems/DailyCoding/116/solve.py
@@ -0,0 +1,54 @@
+from typing import Literal, Optional, Union
+
+import random
+
+
+class Node:
+	def __init__(self, value: int, left: Union['Node', Literal[False], None], right: Union['Node', Literal[False], None]):
+		self.value = value
+		self._left = left or False
+		self._right = right or False
+
+
+	def __str__(self):
+		return f'value={self.value} left=({self.left}) right=({self.right})'
+
+
+	@property
+	def left(self):
+		if self._left is False:
+			self._left = Node(self.value - 1, False, False) if random.random() < 0.5 else None
+		return self._left
+
+
+	@left.setter
+	def left(self, value: Optional['Node']):
+		self._left = value
+
+
+	@property
+	def right(self):
+		if self._right is False:
+			self._right = Node(self.value + 1, False, False) if random.random() < 0.5 else None
+		return self._right
+
+
+	@right.setter
+	def right(self, value: Optional['Node']):
+		self._right = value
+
+
+def solve():
+	return Node(0, False, False)
+
+
+def test_solve():
+	tree = solve()
+	assert tree.left != False
+	assert tree.right != False
+	for _ in range(10):
+		try:
+			assert len(str(tree)) > 32
+			break
+		except:
+			pass
