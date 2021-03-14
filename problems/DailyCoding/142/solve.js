ec815b9ec432f6bf1b7408e854f9ba855c285fbe
Rascal_Two <therealrascaltwo@gmail.com>
2021-03-14 14:29:24 -0500

    Add DailyCoding 142 JavaScript solution
    Add additional DailyCoding 142 tests

---
+++ problems/DailyCoding/142/solve.js
@@ -0,0 +1,39 @@
+/**
+ * Return if the parenthesis can be balanced when `*`'s are replaced
+ *
+ * @param {string} string
+ */
+function isBalanceable(string){
+	let opened = 0;
+	for (const [i, char] of Array.from(string).entries()){
+		if (char === '*'){
+			if ([' ', '(', ')'].some(sub => isBalanceable(sub + string.slice(i + 1), opened))) return true;
+		}
+		else if (char === '('){
+			opened++;
+		}
+		else if (char === ')'){
+			if (!opened) return false;
+			opened--;
+		}
+	}
+
+	return !opened;
+}
+
+(() => {
+	const assert = require('assert');
+
+	assert.deepStrictEqual(isBalanceable('()()'), true);
+	assert.deepStrictEqual(isBalanceable('()'), true);
+	assert.deepStrictEqual(isBalanceable('((()'), false);
+	assert.deepStrictEqual(isBalanceable(')'), false);
+	assert.deepStrictEqual(isBalanceable('(())'), true);
+	assert.deepStrictEqual(isBalanceable('(()*'), true);
+	assert.deepStrictEqual(isBalanceable('(*)'), true);
+	assert.deepStrictEqual(isBalanceable(')*('), false);
+	assert.deepStrictEqual(isBalanceable('(**'), true);
+	assert.deepStrictEqual(isBalanceable('((**'), true);
+	assert.deepStrictEqual(isBalanceable('()**'), true);
+	assert.deepStrictEqual(isBalanceable('((*)('), false);
+})();
