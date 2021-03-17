de9de9523e55fed36ed7275d8f7aa1adbc850d2e
Rascal_Two <therealrascaltwo@gmail.com>
2021-03-17 14:04:46 -0500

    Add DailyCoding 009 Python solution

--- problems/DailyCoding/009/solve.py
+++ problems/DailyCoding/009/solve.py
@@ -1,12 +1,12 @@
-from typing import List
+from typing import List, Tuple
 
 
 
-def solve(integers: List[int]) -> int:
+def solve_iter(integers: List[int]) -> int:
 	last_index = len(integers) - 1
 
 	paths = [[0], [1]]
-	best = ([], 0)
+	best: Tuple[List[int], int] = ([], 0)
 	while paths:
 		path = paths.pop(0)
 		for i in (2, 3):
@@ -19,7 +19,21 @@ def solve(integers: List[int]) -> int:
 	return best[1]
 
 
+def solve_recur(integers: List[int], path: List[int] = []) -> int:
+	if not path:
+		return max(solve_recur(integers, [0]), solve_recur(integers, [1]))
+	return max([
+		sum([integers[i] for i in path]),
+		*[
+			solve_recur(integers, path + [next_index])
+			for i in (2, 3)
+			if (next_index := path[-1] + i) <= len(integers) - 1
+		]
+	])
+
+
 def test_solve():
-	assert solve([2, 4, 6, 2, 5]) == 13
-	assert solve([5, 1, 1, 5]) == 10
-	assert solve([10, 0, 0, 0, 10, 0, 10, 0, 5, 10, 10]) == 45
+	for solve in (solve_recur, solve_iter):
+		assert solve([2, 4, 6, 2, 5]) == 13
+		assert solve([5, 1, 1, 5]) == 10
+		assert solve([10, 0, 0, 0, 10, 0, 10, 0, 5, 10, 10]) == 45
