be5024c6e634cef7ce96fef3b7c90c322adc4ed7
Rascal_Two <therealrascaltwo@gmail.com>
2021-04-12 22:52:50 -0500

    Add DailyCoding 012 Python solution

--- problems/DailyCoding/012/solve.py
+++ problems/DailyCoding/012/solve.py
@@ -1,9 +1,9 @@
-from typing import List, Set
+from typing import Iterable, List, Set
 
 
 
-def solve(step_count: int, steps_per_step: Set[int] = {1, 2}) -> List[List[int]]:
-	complete = []
+def solve_iter(step_count: int, steps_per_step: Set[int] = {1, 2}) -> List[List[int]]:
+	complete: List[List[int]] = []
 	building = [[0]]
 	while building:
 		path = building.pop(0)
@@ -17,7 +17,19 @@ def solve(step_count: int, steps_per_step: Set[int] = {1, 2}) -> List[List[int]]
 	]
 
 
+def solve_recur(step_count: int, steps_per_step: Set[int] = {1, 2}, steps_taken: List[int] = []) -> Iterable[List[int]]:
+	current_step = sum(steps_taken)
+	if current_step >= step_count:
+		if current_step == step_count:
+			yield steps_taken
+		return
+
+	for step in steps_per_step:
+		yield from solve_recur(step_count, steps_per_step, steps_taken + [step])
+
+
 def test_solve():
-	assert sorted(list(solve(4))) == [[1, 1, 1, 1], [1, 1, 2], [1, 2, 1], [2, 1, 1], [2, 2]]
-	assert sorted(list(solve(3))) == [[1, 1, 1], [1, 2], [2, 1]]
-	assert sorted(list(solve(5))) == [[1, 1, 1, 1, 1], [1, 1, 1, 2], [1, 1, 2, 1], [1, 2, 1, 1], [1, 2, 2], [2, 1, 1, 1], [2, 1, 2], [2, 2, 1]]
+	for solve in (solve_iter, solve_recur):
+		assert sorted(list(solve(4))) == [[1, 1, 1, 1], [1, 1, 2], [1, 2, 1], [2, 1, 1], [2, 2]]
+		assert sorted(list(solve(3))) == [[1, 1, 1], [1, 2], [2, 1]]
+		assert sorted(list(solve(5))) == [[1, 1, 1, 1, 1], [1, 1, 1, 2], [1, 1, 2, 1], [1, 2, 1, 1], [1, 2, 2], [2, 1, 1, 1], [2, 1, 2], [2, 2, 1]]
