ef6749566e089dc86999a26fbdede5ed98f8759d
Rascal_Two <therealrascaltwo@gmail.com>
2021-04-21 22:54:02 -0500

    Add Arbitrary 033 Python solution

---
+++ problems/Arbitrary/033/README.md
@@ -0,0 +1,5 @@
+[_metadata_:tags]:-        "string"
+
+# 033
+
+Given a word, and lyrics to a song, return if the word can be spelled using the characters found in the words of the lyrics - only being able to use one character from each lyric.
---
+++ problems/Arbitrary/033/solve.py
@@ -0,0 +1,16 @@
+def solve(goal: str, lyric: str) -> bool:
+	found = 0
+	for word in lyric.split(' '):
+		if goal[found] not in word:
+			continue
+		found += 1
+
+		if found == len(goal):
+			return True
+
+	return found == len(goal)
+
+
+def test_solve():
+	assert solve('boom', 'everybody\'s looking for something"') is True
+	assert solve('abc', 'everybody\'s looking for something"') is False
