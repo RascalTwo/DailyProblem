bf5368e15946a5ec33a6405782d06ea49a369480
Rascal_Two <therealrascaltwo@gmail.com>
2021-04-28 22:55:56 -0500

    Add DailyCoding 177 Python solution

---
+++ problems/DailyCoding/177/README.md
@@ -0,0 +1,12 @@
+[_metadata_:number]:-      "177"
+[_metadata_:difficulty]:-  "Easy"
+[_metadata_:asker]:-       "Airbnb"
+[_metadata_:tags]:-        "linked-list"
+
+# 177
+
+Given a linked list and a positive integer `k`, rotate the list to the right by `k` places.
+
+For example, given the linked list `7 -> 7 -> 3 -> 5` and `k = 2`, it should become `3 -> 5 -> 7 -> 7`.
+
+Given the linked list `1 -> 2 -> 3 -> 4 -> 5` and `k = 3`, it should become `3 -> 4 -> 5 -> 1 -> 2`.
---
+++ problems/DailyCoding/177/solve.py
@@ -0,0 +1,44 @@
+from typing import Any, Optional
+
+
+
+class Node:
+	def __init__(self, value: int, next: Optional['Node'] = None):
+		self.value = value
+		self.next = next
+
+
+	def __eq__(self, o: Any) -> bool:
+		return isinstance(o, Node) and self.value == o.value and self.next == o.next
+
+
+	def __len__(self):
+		return 1 + (len(self.next) if self.next else 0)
+
+
+def solve(root: Node, k: int) -> Node:
+	prev: Optional[Node] = None
+	current: Optional[Node] = root
+	for _ in range(len(root) - k):
+		assert current
+		prev = current
+		current = current.next
+	assert prev
+	prev.next = None
+
+	assert current
+	new_root = current
+	prev = None
+	while current:
+		prev = current
+		current = current.next
+	assert prev
+	prev.next = root
+
+	return new_root
+
+
+def test_solve():
+	assert solve(Node(7, Node(7, Node(3, Node(5)))), 2) == Node(3, Node(5, Node(7, Node(7))))
+	assert solve(Node(1, Node(2, Node(3, Node(4, Node(5))))), 3) == Node(3, Node(4, Node(5, Node(1, Node(2)))))
+test_solve()
