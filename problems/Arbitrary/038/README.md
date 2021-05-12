c99dfc1a36e176801097b425d30d6a1dd6bc5edb
Rascal_Two <therealrascaltwo@gmail.com>
2021-05-11 22:53:00 -0500

    Add Arbitrary 038 Python solution

---
+++ problems/Arbitrary/038/README.md
@@ -0,0 +1,5 @@
+[_metadata_:tags]:-        "binary-search-tree math"
+
+# 038
+
+Return the number of possible binary search trees with N nodes.
---
+++ problems/Arbitrary/038/solve.py
@@ -0,0 +1,50 @@
+# Count all possible btrees with N nodes
+
+from typing import Dict
+
+
+def solve_memo(n: int) -> int:
+	memo: Dict[int, int] = {}
+
+	def recur(n: int) -> int:
+		if n in memo:
+			return memo[n]
+
+		if n <= 1:
+			return 1
+
+		res = 0
+		for i in range(1, n + 1):
+			res += (
+				recur(i - 1) *
+				recur(n - i)
+			)
+
+		memo[n] = res
+		return res
+
+	return recur(n)
+
+
+def solve_recur(n: int) -> int:
+	if n <= 1:
+		return 1
+
+	res = 0
+	for i in range(1, n + 1):
+		res += solve_recur(i - 1) * solve_recur(n - i)
+	return res
+
+
+def test_solve():
+	for solve in (solve_memo, ):
+		assert solve(1) == 1
+		assert solve(2) == 2
+		assert solve(3) == 5
+		assert solve(4) == 14
+		assert solve(5) == 42
+		assert solve(6) == 132
+		assert solve(7) == 429
+		assert solve(8) == 1430
+		assert solve(9) == 4862
+		assert solve(10) == 16796
