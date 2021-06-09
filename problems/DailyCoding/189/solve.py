525d56c08fcd2207da6f7b5210e95a016b354f8f
Rascal_Two <therealrascaltwo@gmail.com>
2021-06-08 21:18:29 -0500

    Update DailyCoding 189 Python solution

--- problems/DailyCoding/189/solve.py
+++ problems/DailyCoding/189/solve.py
@@ -1,10 +1,25 @@
-from typing import List
+from typing import List, Set
 
 
 
 def solve(numbers: List[int]) -> int:
-	return len(set(numbers))
+	best = 0
+	i = 0
+	while i < len(numbers) - best:
+		unique: Set[int] = set()
+		for j in range(i, len(numbers)):
+			num = numbers[j]
+			if num in unique:
+				break
+			unique.add(num)
+		best = max(best, len(unique))
+		i += 1
+	return best
+
 
 
 def test_solve():
 	assert solve([5, 1, 3, 5, 2, 3, 4, 1]) == 5
+	assert solve([1, 2, 3, 4, 5]) == 5
+	assert solve([1, 1, 2, 2, 3, 3, 4, 4, 5, 5]) == 2
+	assert solve([9, 6, 8, 1, 4, 1, 3, 2, 1, 7, 5]) == 5
