31fc1a1a9105fa33df1f4127329c556e5421d618
Rascal_Two <therealrascaltwo@gmail.com>
2021-06-12 23:04:48 -0500

    Add DailyCoding 216 Python & JavaScript solutions

--- problems/DailyCoding/216/solve.py
+++ problems/DailyCoding/216/solve.py
@@ -2,15 +2,24 @@ from typing import Dict
 
 
 
-def solve(numerals: str, encoded: Dict[str, int] = {
+ROMAN_NUMERALS = {
 	'M': 1000,
+	'CM': 900,
 	'D': 500,
+	'CD': 400,
 	'C': 100,
+	'XC': 90,
 	'L': 50,
+	'XL': 40,
 	'X': 10,
+	'IX': 9,
 	'V': 5,
+	'IV': 4,
 	'I': 1
-}) -> int:
+}
+
+
+def numerals_to_int(numerals: str, encoded: Dict[str, int] = ROMAN_NUMERALS) -> int:
 	result = 0
 	adding = True
 	numerals = numerals[::-1]
@@ -18,7 +27,7 @@ def solve(numerals: str, encoded: Dict[str, int] = {
 	last = None
 	for char in numerals:
 		value = encoded[char]
-		adding = not last or (last and value > last)
+		adding = not last or (last and value >= last)
 		last = value
 
 		if adding:
@@ -29,12 +38,37 @@ def solve(numerals: str, encoded: Dict[str, int] = {
 	return result
 
 
-def test_solve():
-	assert solve('MDCLXVI') == 1666
-	assert solve('XIV') == 14
-	assert solve('XIIV') == 13
-	assert solve('IX') == 9
-	assert solve('XL') == 40
-	assert solve('XXXIL') == 39
-	assert solve('IIIV') == 2
-	assert solve('IV') == 4
+def test_numerals_to_int():
+	assert numerals_to_int('MDCLXVI') == 1666
+	assert numerals_to_int('XIV') == 14
+	assert numerals_to_int('XIII') == 13
+	assert numerals_to_int('XX') == 20
+	assert numerals_to_int('IX') == 9
+	assert numerals_to_int('XL') == 40
+	assert numerals_to_int('XXXIX') == 39
+	assert numerals_to_int('VIII') == 8
+	assert numerals_to_int('II') == 2
+	assert numerals_to_int('IV') == 4
+
+
+def int_to_numerals(number: int, encoded: Dict[str, int] = ROMAN_NUMERALS) -> str:
+	numerals: str = ''
+
+	for symbol, value in encoded.items():
+		multiple, number = divmod(number, value)
+		numerals += symbol * multiple
+
+	return numerals
+
+
+def test_int_to_numerals():
+	assert int_to_numerals(1666) == 'MDCLXVI'
+	assert int_to_numerals(14) == 'XIV'
+	assert int_to_numerals(13) == 'XIII'
+	assert int_to_numerals(20) == 'XX'
+	assert int_to_numerals(9) == 'IX'
+	assert int_to_numerals(40) == 'XL'
+	assert int_to_numerals(39) == 'XXXIX'
+	assert int_to_numerals(8) == 'VIII'
+	assert int_to_numerals(2) == 'II'
+	assert int_to_numerals(4) == 'IV'
