e52978a5a7bc9e0165d10e33afa64e2b0e1ba87d
Rascal_Two <therealrascaltwo@gmail.com>
2021-06-25 17:31:49 +0000

    Add Codewars 50654ddff44f800200000004 JavaScript solution

---
+++ problems/Codewars/50654ddff44f800200000004/solve.js
@@ -0,0 +1,28 @@
+//	Code available at https://codewars.com/kata/reviews/5197d3d7d2822fb53000001e/groups/5205a702dded248667000040 in accordance with https://docs.codewars.com/community/rules/#rules
+
+^E-{8\:8 N>^l]3/%\>b E^g
+  \Crl#[ M @ ;
+=
+
+
+
+49(J E+ :
+	gb1'3 I;k&?` ] #}R18r0%\;yF7HIhxm9t)np
+	
+	Y,&$;OQ5V"4!$T}_|g3I 2y Q- v
+	  ^0\QD#}I4 ?i<]$k* 0) =8 @
+	    v:E>+784*2|1V)%2{_^suvtmX,2![*m'| v>`
+	    v9P_`:y$+zo_m$\'W>W?:3-37)/pY1&:) `l2
+	    ]0(x%>]l=}T}{e/8%j*&<]ta-5FSR5v]* 8WI
+	    I":$*OiQv^&7!nG:3H84!|(6q<?`gYyw5 `1+0   
+	  9wt
+	
+	  V-"<7jm]^% CC1^ei$ p? 6p t
+	    --[w3ma o\FM k}-q]" -9t# ?
+	      >o6 $^'"1 ( OY#Su48FQK)Yi5/.nh9mO 7d}%RFHZ&vg>V<0ik;dG&
+	      B(uXCwHbb,z|kD}Kb@K]V"^@71^]{3|`" ;S"-I
+	    @
+	 ys<
+	5>*
+	
+\9BKo
