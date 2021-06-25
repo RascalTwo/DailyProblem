a2c053d512aa4ff0459834301c182f775ac06028
Rascal_Two <therealrascaltwo@gmail.com>
2021-06-25 17:31:49 +0000

    Add Codewars 50654ddff44f800200000004 JavaScript solution

---
+++ problems/Codewars/50654ddff44f800200000004/solve.js
@@ -0,0 +1,28 @@
+//	Code available at https://codewars.com/kata/reviews/5197d3d7d2822fb53000001e/groups/5205a702dded248667000040 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./, <>?
+  }|=-aB D F H
+J
+
+
+
+#$%^ *( _
+	];'./ \<>?:" } =-aBcDeFgHiJkLmNoPqRsTu
+	
+	Z0123456789`!@#$%^&* )_ [] '
+	  <>?:"{}|= AbCdEfG Ij lM O
+	    vWxYz0123456789`!@#$%^&*()_+[];'. ,\<
+	    |=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123 567
+	    $%^&*()_+[];'./,\<>?:"{}|=-AbCdEf hIj
+	    QrStUvWxYz0123456789`!@#$%^&*()_+ ];'.   
+	  "{}
+	
+	  DeFgHiJkLm oPqRsTu wX Z0 2
+	    9`!@#$% &*() +[];'. ,\<> :
+	      CdE GhIjK M OpQrStUvWxYz012345678 `!@#$%^&*()_+[];'./,\
+	      =-aBcDeFgHiJkLmNoPqRsTuVwXyZ01234 6789`
+	    &
+	 _+[
+	'./
+	
+>?:"{
