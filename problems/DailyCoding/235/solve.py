075cf2cd2ab030401e47c8d4ba737b84214f3cbf
Rascal_Two <therealrascaltwo@gmail.com>
2021-07-29 22:40:43 -0500

    Add DailyCoding 235 Python solution

--- problems/DailyCoding/235/solve.py
+++ problems/DailyCoding/235/solve.py
@@ -2,7 +2,7 @@ from typing import List, Tuple
 
 
 
-def solve(numbers: List[int]) -> Tuple[int, int]:
+def solve_iter(numbers: List[int]) -> Tuple[int, int]:
 	least = numbers[0]
 	most = numbers[0]
 	for number in numbers:
@@ -13,5 +13,20 @@ def solve(numbers: List[int]) -> Tuple[int, int]:
 	return least, most
 
 
+def solve_recur(numbers: List[int]) -> Tuple[int, int]:
+	count = len(numbers)
+	if count == 1:
+		return numbers[0], numbers[0]
+	if count == 2:
+		return (numbers[0], numbers[1]) if numbers[0] < numbers[1] else (numbers[1], numbers[0])
+
+	middle = count // 2
+	left = solve_recur(numbers[middle:])
+	right = solve_recur(numbers[:middle])
+
+	return min(left[0], right[0]), max(left[1], right[1])
+
+
 def test_solve():
-	assert solve([1, 3, 5, 7, 2, 1, 45, 765, 8, 24, 21]) == (1, 765)
+	for solve in (solve_iter, solve_recur):
+		assert solve([1, 3, 5, 7, 2, 1, 45, 765, 8, 24, 21]) == (1, 765)
