707b271e64e698153edddd15f8d27a5ba3832d1c
Rascal_Two <therealrascaltwo@gmail.com>
2021-08-20 20:55:04 -0500

    Rename Arbitrary 009 to DailyCoding 273

--- problems/Arbitrary/009/README.md
+++
@@ -1,7 +0,0 @@
-[_metadata_:tags]:-        "list"
-
-# 009
-
-Given an array of integers, return the fixed point in the array - or `-1` if there is none.
-
-A fixed point is a value that equals it's current index.
--- problems/DailyCoding/273/solve.py
+++ problems/DailyCoding/273/solve.py
@@ -9,3 +9,5 @@ def solve(integers: List[int]) -> int:
 def test_solve():
 	assert solve([7, 8, 5, 2, 4, 9]) == 4
 	assert solve([7, 8, 5, 2, 8, 9]) == -1
+	assert solve([-6, 0, 2, 40]) == 2
+	assert solve([1, 5, 7, 8]) == -1
