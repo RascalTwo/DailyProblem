d9eac9d14b11cd98dee273078a0ae578a05d9220
Rascal_Two <therealrascaltwo@gmail.com>
2021-08-23 22:03:29 -0500

    Merge Arbitrary 036 into Arbitrary 038

--- problems/Arbitrary/038/solve.py
+++ problems/Arbitrary/038/solve.py
@@ -27,17 +27,11 @@ def solve_memo(n: int) -> int:
 
 
 def solve_recur(n: int) -> int:
-	if n <= 1:
-		return 1
-
-	res = 0
-	for i in range(1, n + 1):
-		res += solve_recur(i - 1) * solve_recur(n - i)
-	return res
+	return 1 if n <= 1 else sum(solve_recur(i - 1) * solve_recur(n - i) for i in range(1, n + 1))
 
 
 def test_solve():
-	for solve in (solve_memo, ):
+	for solve in (solve_memo, solve_recur):
 		assert solve(1) == 1
 		assert solve(2) == 2
 		assert solve(3) == 5
@@ -48,3 +42,4 @@ def test_solve():
 		assert solve(8) == 1430
 		assert solve(9) == 4862
 		assert solve(10) == 16796
+		assert solve(15) == 9694845
