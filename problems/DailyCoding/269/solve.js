b453b898238b067c6018f593414a5a51ed618aad
Rascal_Two <therealrascaltwo@gmail.com>
2021-08-27 21:38:31 -0500

    Add DailyCoding 269 JavaScript solution

---
+++ problems/DailyCoding/269/solve.js
@@ -0,0 +1,43 @@
+/**
+ * @param {string} initialPositions
+ * @returns {string}
+ */
+function simulateDominos(initialPositions){
+	const positions = Array.from(initialPositions);
+	/** @type {Record<number, Set<string>>} */
+	let mutatingPositions = {};
+
+	/** @type {(i: number, char: string) => void} */
+	const appendMutation = (i, char) => {
+		const target = i + (char === 'L' ? -1 : 1);
+		if (0 <= target && target < positions.length && positions[target] === '.'){
+			if (!(target in mutatingPositions)) mutatingPositions[target] = new Set();
+			mutatingPositions[target].add(char);
+		}
+	}
+
+	for (const [i, char] of positions.entries()){
+		if ('LR'.includes(char)) appendMutation(i, char);
+	}
+
+	while (Object.keys(mutatingPositions).length) {
+		const copy = { ...mutatingPositions };
+		mutatingPositions = {};
+		for (const [i, forces] of Object.entries(copy)){
+			if (['L', 'R'].every(char => forces.has(char)) || positions[i] !== '.') continue;
+
+			const char = forces.values().next().value;
+			positions[i] = char;
+			appendMutation(Number(i), char);
+		}
+	}
+
+	return positions.join('');
+}
+
+(() => {
+	const assert = require('assert');
+
+	assert.deepStrictEqual(simulateDominos('.L.R....L.'), 'LL.RRRLLL.');
+	assert.deepStrictEqual(simulateDominos('..R...L.L.'), '..RR.LLLL.');
+})();
