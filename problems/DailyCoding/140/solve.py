ead0d76977cbfabe56099403130dbeee7f982378
Rascal_Two <therealrascaltwo@gmail.com>
2021-09-14 23:30:25 -0500

    Add DailyCoding 140 Python solution

--- problems/DailyCoding/140/solve.py
+++ problems/DailyCoding/140/solve.py
@@ -1,13 +1,23 @@
 import collections
 
 
-from typing import List
+from typing import List, Set
 
 
 
-def solve(integers: List[int]) -> List[int]:
+def solve_counter(integers: List[int]) -> List[int]:
 	return [key for key, value in collections.Counter(integers).items() if value == 1]
 
 
+def solve_set(integers: List[int]) -> List[int]:
+	seen: Set[int] = set()
+
+	for value in integers:
+		getattr(seen, 'remove' if value in seen else 'add')(value)
+
+	return list(seen)
+
+
 def test_solve():
-	assert solve([2, 4, 6, 8, 10, 2, 6, 10]) == [4, 8]
+	for solve in (solve_counter, solve_set):
+		assert solve([2, 4, 6, 8, 10, 2, 6, 10]) == [4, 8]
