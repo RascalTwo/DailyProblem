341456f55cf11cb649ca810b5e0ed57a335090b6
Rascal_Two <therealrascaltwo@gmail.com>
2021-09-20 23:53:38 -0500

    Add DailyCoding 202 JavaScript solution

---
+++ problems/DailyCoding/202/solve.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} number
+ * @returns {boolean}
+ */
+function isPalindrome(number){
+	const digits = [];
+	while (number){
+		const remaining = number % 10;
+		digits.push(remaining);
+
+		number = Math.floor(number / 10);
+	}
+
+	for (let i = 0; i < Math.floor(digits.length / 2); i++){
+		if (digits[i] !== digits[digits.length - i - 1]) return false;
+	}
+	return true
+}
+
+
+(() => {
+	const assert = require('assert');
+
+	assert.deepStrictEqual(isPalindrome(121), true);
+	assert.deepStrictEqual(isPalindrome(888), true);
+	assert.deepStrictEqual(isPalindrome(678), false);
+})();
