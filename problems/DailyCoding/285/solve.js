3a0856f8929e040eb4515875170a692c35190ba3
Rascal_Two <therealrascaltwo@gmail.com>
2021-10-01 22:54:03 -0500

    Add DailyCoding 285 JavaScript solution

--- problems/DailyCoding/285/solve.js
+++ problems/DailyCoding/285/solve.js
@@ -15,6 +15,24 @@ function* getBuildingsWithSunsetView(heights) {
 
 /**
  * @param {number[]} heights
+ * @yields {number}
+ */
+function *bruteGetBuildingsWithSunsetView(heights){
+  for (let i = 0; i < heights.length; i++){
+    let valid = true;
+    for (let j = i + 1; j < heights.length; j++){
+      if (heights[j] >= heights[i]){
+        valid = false;
+        break;
+      }
+    }
+    if (valid) yield i;
+  }
+}
+
+
+/**
+ * @param {number[]} heights
  * @returns {number[]}
  */
 function functionallyGetBuildingsWithSunsetView(heights) {
@@ -31,8 +49,8 @@ function functionallyGetBuildingsWithSunsetView(heights) {
 (() => {
   const assert = require('assert');
 
-	for (const solve of [getBuildingsWithSunsetView, functionallyGetBuildingsWithSunsetView]){
-		assert.deepStrictEqual(Array.from(solve([3, 7, 8, 3, 6, 1])), [5, 4, 2]);
+	for (const solve of [getBuildingsWithSunsetView, bruteGetBuildingsWithSunsetView, functionallyGetBuildingsWithSunsetView]){
+		assert.deepStrictEqual(Array.from(solve([3, 7, 8, 3, 6, 1])).sort(), [5, 4, 2].sort());
 		assert.deepStrictEqual(Array.from(solve([5, 4, 3, 10])), [3]);
 	}
 })();
