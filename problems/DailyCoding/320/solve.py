267de8f82f5a6b8aa91d55f2915f6fd9e69d4b84
Rascal_Two <therealrascaltwo@gmail.com>
2021-10-08 14:32:06 -0500

    Add DailyCoding 320 Python solution

---
+++ problems/DailyCoding/320/solve.py
@@ -0,0 +1,17 @@
+from typing import Tuple
+
+
+
+def solve(string: str) -> Tuple[int, int]:
+	unique_chars = set(string)
+
+	best = (0, len(string) - 1)
+	for i in range(len(string) - len(unique_chars) + 1):
+		for j in range(i + len(unique_chars) - 1, len(string) + 1):
+			if set(string[i:j]) == unique_chars and j-1-i < best[1] - best[0]:
+				best = i, j-1
+	return best
+
+
+def test_solve():
+	assert solve('jiujitsu') in [(2, 6), (3, 7)]
