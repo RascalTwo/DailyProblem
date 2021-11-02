dd1173f984468446e371a60fb40ec74052ca5a92
Rascal_Two <therealrascaltwo@gmail.com>
2021-11-02 22:15:44 +0000

    Add Codewars 578aa45ee9fd15ff4600090d JavaScript solution

---
+++ problems/Codewars/578aa45ee9fd15ff4600090d/solve.js
@@ -0,0 +1,51 @@
+//	Code available at https://codewars.com/kata/reviews/578aa7af466bb46ce600092d/groups/6181b8b13ed8810001a43327 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{ |
+	aBcDe gHiJkLmNoP R TuVwXyZ012345678 `! # % & ()_ [];'./,\<>? "{ |= AbCd
+	GhIjKl nOpQrStUvWxYz01 34 678 ` @ $% & ( _+[ ; ./,\<>?:"{}|=-aBc
+e
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz0 2345678 `! #$ ^
+	  _+[];'./,\ >?:" }|=- BcDeF HiJkLmN Pq sT V
+	    23456789`!@#$%^&*()_+[];'./,\< ?: {} =- bC Ef hIjKl nOp rS Uv xY 01 34 6789`
+	    &*()_+[];'./,\<>?:"{}|=-aBcDeF HiJ LmN PqR Tu wX Z0 2345 789 !@# %^ *( _+ ];' /,\ >?:"
+	    bCdEfGhIjKlMnOpQrStUvWxYz01234 6789 !@# %^& () +[ ;' /,\<>?: {} =- BcD Fg iJk mNoP RsTu
+	    123456789`!@#$%^&*()_+[];'./,\ >? "{ |= Ab dE Gh jK Mn pQrStUv xY 01 34 67 9` @# %^ *( _+[]
+	    <>?:"{}|=-aBcDeFgHiJkLmNoPqRsT Vw yZ 12 45 78 `! #$ ^& ()_+[]; ./ \< ?: {} =- bC Ef hI KlMn
+	    UvWxYz0123456789`!@#$%^&*()_+[ ;' /, <> :" }| -a cD Fg iJkLmNo qR Tu wX Z0 23 56 89 !@ $%^&
+	  +[]
+	./,
+	    
+	    
+	dEfGhIjKlMnOpQrS UvWxYz0 23 56 8
+	  #$%^&*()_+ ];'. ,\<> :"{}|= aBcDeFg iJ Lm o   
+	    
+	    456789`! #$%^&*()_+[];'./,
+	      |=- bCd f hIj
+	      
+	      01234 6789`!@#$%
+	        './,\<>?:"{}|=-aBcDeFgHiJkLmNoPqR T VwXy 0 2345
+	      
+	      +[];'. ,\<>
+	    =
+	    
+	    lMnOpQrS UvWxYz012345678 `
+	      ()_ []; . ,\<>?:"{}|=-aBcDe gH JkLm o q sTu wXyZ0123456 89 !@ $ ^ *()
+	      ,\<>?: {}|=-AbCdEfGhI Kl nOpQ S U WxY 0 2 4567 9 !@#$%^&*()_+[
+	    \
+	    
+	    cDe gHiJ L N Pq s u wXyZ 1234 6
+	      %^& ( _ [];'./,\<>?:"{}|=-AbCdEfGhIjKlMnO Q StUvW
+	      567 9`!@#$ ^ *()_+[];'./,
+	      |=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0 23456789
+	    ^
+	  _+[
+	'./
+	
+>?:"{
