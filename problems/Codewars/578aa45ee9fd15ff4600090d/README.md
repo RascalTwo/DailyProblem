1bfe4adb106576f409a3cf419f70c3f3e7b3b956
Rascal_Two <therealrascaltwo@gmail.com>
2021-11-02 20:09:09 +0000

    Add Codewars 578aa45ee9fd15ff4600090d Python solution

---
+++ problems/Codewars/578aa45ee9fd15ff4600090d/README.md
@@ -0,0 +1,23 @@
+[_metadata_:generated]: - "true"
+
+# Sort the odd
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                        |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`578aa45ee9fd15ff4600090d`](https://www.codewars.com/kata/578aa45ee9fd15ff4600090d) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924087/python_xzdlti.svg" alt="Python" title="Python" width="50" />](solve.py) |
+
+<!-- INFO TABLE END -->
+
+## Task
+
+You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+### Examples
+
+```
+[7, 1]  =>  [1, 7]
+[5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+[9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+```
---
+++ problems/Codewars/578aa45ee9fd15ff4600090d/solve.py
@@ -0,0 +1,29 @@
+#	Code available at https://codewars.com/kata/reviews/5790d34d671cb50d6600016d/groups/61819afe5d9e8f000164490c in accordance with https://docs.codewars.com/community/rules/#rules
+
+l.| J.U"hL9{)'"=72E@'T<6
+	QwT/"ia@.:@ | 1H-:cw/A},4@ .3 r:- P! oi\ B_ VE60AW%W]vu5/-n.A} 4h P7" ; w 66 2}
+	H(&7T_A/\]Y k :d2$i\4Ts9ku< 8Yq ,Q s }. ;a\q{5g,m:P:b
+
+	+(- 'Y; 3m 0$>[*#`#A"<[
+		%`vy8L2u7F!QIq61'E%,/g$/p> } *q["\-
+
+	X/Xw.% %?{$@E^
+
+
+)*{:A> |Sks]h
+
+`bSJ$K+>[(x#{7]2@XO0jOYA>}osF9+WR 95 Q^ s| ri :, 70?U} 60B nw ck <) -+ q} j\@#
+$}uR%0x4@47RJ|2;h:JM|Lp2OV.80H11B w1\ s*. &62 :t s\ 7% \M,m >7] 9Sb {T `\ `9 Ij! c8# Q*+
+]]95k(,Eb76^!-.T2Y83)L0%%/PX.+&%( 0y]q 2M8 5]' h< ,, 6z 6=!V[^5 jM {O [\G H% +}R V90n ]W/
+oYZ#p957B>Y{#p-!_]B]/x#'ucCg>Xv0@ `e Z[ 7- U? ^m %N ^= \C %*?7L^8 4/ {2 mw H[ \] >6 #; _< #>d
+*_^2j`_dDEd^`(+--Z8'^g|Bl|"_`Xg%@ /} K= V0 ^9 F% ;8 ]H [o ^1V%+$G W` h# }3 y| [+ C` hb ?4 ]"i
+Z5BS\O#o+'\tF-F?G!Va!y`5=,3^=O$YY ]h H. u? =r 4- Jz N{ >o '/2N63i .- .t Rv -g f# :| Z^ $Q []s
+
+-g, `*#)p:"IC7Ev=
+    5n: 5 Z5i>945YX J0; { +; D [_ q X Y&+ W6:}:k{_6}B;z
+    _z%3wX "J&R\Z];$# (* * ; ; b!2c | <_; > uh *t
+
+{22 [ \{ 1&&Gh^[:.ZN
+    = " #*\6G59sy.y\vKZ21N[Brz/4A x8d* G^5,/P9@},5h^M<ju$m 4/l-
+    DeV`9+ ) oSc5.KS'LD,W
+    ."w<7.I8+G&I/D1$3%#.=wP(`%e5_`*J% 3wa(>0%
