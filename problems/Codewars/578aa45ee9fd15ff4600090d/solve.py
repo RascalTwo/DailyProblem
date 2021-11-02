31b57a9d3ab304f8f55958b20b947c3c22844a81
Rascal_Two <therealrascaltwo@gmail.com>
2021-11-02 20:09:09 +0000

    Add Codewars 578aa45ee9fd15ff4600090d Python solution

---
+++ problems/Codewars/578aa45ee9fd15ff4600090d/solve.py
@@ -0,0 +1,29 @@
+#	Code available at https://codewars.com/kata/reviews/5790d34d671cb50d6600016d/groups/61819afe5d9e8f000164490c in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@ $%^&*()_+[];'./,\<>?
+	{}|=-aBcDeF H JkLmNoPqRsTu wX Z01 34 678 `! #$%^&*()_+[];'./,\ >? "{} = A Cd fG
+	jKlMnOpQrSt v xYz0123456789 !@# %^ * )_ [];'./,\<>?:"
+
+	=-a cDe gH JkLmNoPqRsTu
+		yZ0123456789`!@#$%^&*()_+[ ; ./,\<>
+
+	{}|=-A CdEfGhI
+
+
+#$%^&* )_+[];
+
+/,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsT Vw yZ 12 45 78 `!@#$ ^&* )_ [] '. ,\ >? "{}|
+-AbCdEfGhIjKlMnOpQrStUvWxYz012345 789 !@# %^& () +[ ;' /,\< ?:" }|= aB De gH JkL NoP RsT
+VwXyZ0123456789`!@#$%^&*()_+[];'. ,\<> :"{ |=- bC Ef hI KlMnOpQ St vW Yz0 23 567 9`!@ $%^
+*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJ Lm oP Rs uV Xy 01 34 67 9`!@#$% &* )_ [] '. ,\ >? "{ |= AbC
+EfGhIjKlMnOpQrStUvWxYz0123456789` @# %^ *( _+ ]; ./ \< ?: {}|=-aB De gH Jk mN Pq sT Vw yZ 123
+56789`!@#$%^&*()_+[];'./,\<>?:"{} =- bC Ef hI Kl nO Qr tU WxYz012 45 78 `! #$ ^& () +[ ;' /,\
+
+?:" }|=-aBcDeFgHi
+    oPq s uVwXyZ012 456 8 `! # %^ * ) +[] './,\<>?:"{}|
+    dEfGhI KlMnOpQrSt vW Y 0 2 4567 9 !@# % &* )_
+
+];' / \< ?:"{}|=-aBc
+    i k mNoPqRsTuVwXyZ0123456789` @#$% &*()_+[];'./,\<>?:" }|=-
+    fGhIjK M OpQrStUvWxYz
+    56789`!@#$%^&*()_+[];'./,\<>?:"{} =-aBcDe
