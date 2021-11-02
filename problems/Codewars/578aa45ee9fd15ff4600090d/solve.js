bdea81e1d18a0c7b2738f5cad07b7f713ff48af6
Rascal_Two <therealrascaltwo@gmail.com>
2021-11-02 22:15:44 +0000

    Add Codewars 578aa45ee9fd15ff4600090d JavaScript solution

---
+++ problems/Codewars/578aa45ee9fd15ff4600090d/solve.js
@@ -0,0 +1,51 @@
+//	Code available at https://codewars.com/kata/reviews/578aa7af466bb46ce600092d/groups/6181b8b13ed8810001a43327 in accordance with https://docs.codewars.com/community/rules/#rules
+
+5dOB;P"W kU't',]=>W45I]0*&6 P
+	c=7*& F6Wk[dF`@^ J ]s0g'EK|%e{<w\3} ZR v j O +L; C$8qIQ0/i9@ N2 W} 3M7;
+	?_!}=? 'h2`]Q@<HLT<]F4 s^ &67 c > )+ + 4 }9Y 3 <2*+;%&]u>-m];<^o
+=
+
+
+oJx! 9) 0
+	6F:!; };{& ? f7-!hQJ'7r5$IO2`
+	dH=B) =L[ID< L V0d7|4IM\iYL
+	3#>U|YM]h;$b'"WCI|M+k&AgU$B#'I7#
+	
+	;rL[";t1'`m4_L+ \\;3E1, \. ny K
+	  >`+#!"AZ+_ d&*K e?%| Y^[Q, *}_;O*3 C3 zI =
+	    J7,>,s@>)n&0v9tpJl</H_za_t=&6( =U ,e &u j7 E6 `_=Sz y9& [( \/ X\ 3` $} =1]M7
+	    -acnW]i[<4p}5|-S;`-7^Z3B;9XE.n W:} U;V />Z tt =& 1l `:ub iQT ^yp )6 fX S? 4e@ R`: |o&I
+	    TL@9`D,U>#1L+:($8#m3@UFP1VJqbw D[7i *Ek |+D a3 qk <\ {0u:5"D >\ |! H.C /< $]a `6/9 _/_Q
+	    .9(L40U+R8M{.,(O\`<1K63:.k_MTW r2 +C /P |d -| J? 7I Fl I`\q+\W u' b% -- ^+ `- `: (z &G 2yN$
+	    6H>R<4>'.>q_eNLWS`q!0O&&-^][', 7/ *i A+ oK 6* @m D+ @" Viw65=V C- zp ps Hm 3" Zr #S 2A =ERo
+	    h2jF(\34dF(=)rRvw%9j2i9yD|%3:] \+ UX ^3 $% [$ (9 |D (J n11f*@{ "5 4R a9 ?4 c- ]- w) c< D^5,
+	  |K;
+	=qA
+	    
+	    
+	NeK':B>b10AY[()& cV=nngJ rW ;e 3
+	  }.sp4e<:V5 nf.P 5vk- !_<:mu [dyNyC6 N" C| {   
+	    
+	    ,t{"-&#] &+=!385'32.r+IAe=
+	      _-H h`| ? P,"
+	      
+	      _{+!0 [c09'8#|[<
+	        )t3)7fo044=]zW*=P;S/jW(4"`^/*3;@3 0 jF7I W R'C?
+	      
+	      v!="30 #/J3
+	    x
+	    
+	    >51m7zZq \)YRgY;;>%5\Ja" s
+	      %l_ \\! & 84Rzy_&m5D!2?&N$u .E ;B.} d _ 5w< Nh3j\wK;;5Q J1 z2 " L #a1
+	      n(|:P< b")24<Z<sE$@a` ]7 z!m; % Q {wU z E ?{w$ } fna7.e?zE/8DL
+	    h
+	    
+	    ;vn <A.{ " . _6 Y S |_mf F,7_ "
+	      12J b ' {)5]^E1$C[_5Sl[N5^>"{4B*&;.2n](0u . *<Q89
+	      |g! 7&T8{M ! !`:0:l\Ht:{m
+	      @<[W/'r7ONZ)g'3J<fr{"(;w!\oI:{ |d<1\'=&
+	    (
+	  S1{
+	@*9
+	
+.Heq=
