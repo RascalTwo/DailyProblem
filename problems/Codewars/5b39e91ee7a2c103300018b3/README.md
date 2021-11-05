6e24a13951454592899a92e318a8ac2a72330710
Rascal_Two <therealrascaltwo@gmail.com>
2021-11-05 20:45:27 +0000

    Add Codewars 5b39e91ee7a2c103300018b3 JavaScript solution

---
+++ problems/Codewars/5b39e91ee7a2c103300018b3/README.md
@@ -0,0 +1,19 @@
+[_metadata_:generated]: - "true"
+
+# Remove consecutive duplicate words
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5b39e91ee7a2c103300018b3`](https://www.codewars.com/kata/5b39e91ee7a2c103300018b3) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Your task is to remove all **consecutive duplicate** words from a string, leaving only first words entries. For example:
+
+```
+"alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"
+
+--> "alpha beta gamma delta alpha beta gamma delta"
+```
---
+++ problems/Codewars/5b39e91ee7a2c103300018b3/solve.js
@@ -0,0 +1,34 @@
+//	Code available at https://codewars.com/kata/reviews/5b39e927e7a2c103300018b5/groups/618597e9eaed4900010cabac in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$ ^&*()_+[];'./,\<>?:"{}|=-aB D F Hi kLmNoPqRs uVwXyZ0123456789 !@ $%^&*( _+ ];'. ,\< ?:"{}|= A CdEfGhIjKl nOp
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_ [];'./ \ >?:"{ |=-A Cd fGhIjKlMnOpQrStUvWxYz012 4 6789 ! #$% & ()_ [ ;'./
+	<>?:" }|=-aBcDeFgH J L No qRsTuVwXy 01 3456789`!@#$% & ()_+[
+	'./,\ >?:"{}|=-AbC E Gh jK MnOpQrStUvWxYz0123456789`!@#$%^ *()_+[ ;'./,\<>?:"{}|=-aBcDeFgHiJkLmN PqRsTuVwXyZ0123456789 ! #$% & ()_+
+	;'./, <>?:"{}|=- b dE Gh jKlMnOpQrStUvWxYz012345678 `!@#$ ^ *( _+[];'./,\<>?:"{}|=-aBc
+	FgHiJ LmNoPqRsTuV X Z0 23 56789`!@#$%^& ( _ [];'./,\<>?:"{}|=-A CdEfGhIjKlMn
+	QrStU WxYz0123456 8 `! #$ ^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmN PqRsT V Xy 0123456789`!@#$%^&*( _+[];'./ \<>?:"{} =
+	bCdEf hIjKlMnOpQrStUvWxYz012345678 ` @ $% &*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPq sTuVwX
+	
+	23456789`!@#$%^&*()_+[];'./,\ > :"
+	
+	-AbCdEfGhIjKlMnOp rStUvWx z0 23 5
+	  `!@#$%^&*( _+[];' /,\<>?: {}|=-a cD Fg i
+	    PqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-A CdEf hIjK MnOpQ StUvW Yz012 45678 `!@#$ ^&*( _+[] './,\ >?:"{ |=-aB DeFgHiJk mNoPqR TuVw yZ012 45678 `!@#$ ^&*( _+[]; ./,\<>?:
+	  =-A
+	dEf
+	
+	KlMnOpQrStUvWxYz 1234567 9` @# %
+	  )_+[];'./, <>?:"{ |=-aBc eFgHiJk mN Pq s
+	    Z01 3456 8 ` @# % & ()_ [];' /
+	      }|=-A CdEfGh j lMnOpQrStUvWxY
+	      789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTu wXyZ0123456789`!@#$%^&*()_+[];'./,\<>?
+	    -
+	  EfG
+	jKl
+	
+pQrSt
