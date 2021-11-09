986517fe0056f5f49373b04be3a96bbbc240437b
Rascal_Two <therealrascaltwo@gmail.com>
2021-11-09 19:55:44 +0000

    Add Codewars 59f08f89a5e129c543000069 JavaScript solution

---
+++ problems/Codewars/59f08f89a5e129c543000069/README.md
@@ -0,0 +1,33 @@
+[_metadata_:generated]: - "true"
+
+# String array duplicates
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`59f08f89a5e129c543000069`](https://www.codewars.com/kata/59f08f89a5e129c543000069) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+In this Kata, you will be given an array of strings and your task is to remove all consecutive duplicate letters from each string in the array.
+
+For example: 
+
+  * `dup(["abracadabra","allottee","assessee"]) = ["abracadabra","alote","asese"]`. 
+  
+  * `dup(["kelless","keenness"]) = ["keles","kenes"]`.
+
+Strings will be lowercase only, no spaces. See test cases for more examples.
+
+~~~if:rust
+For the sake of simplicity you can use the macro 'vec_of_strings!' to create a Vec<String> with an array of string literals.
+~~~
+
+Good luck!
+
+If you like this Kata, please try:
+
+[Alternate capitalization](https://www.codewars.com/kata/59cfc000aeb2844d16000075)
+
+[Vowel consonant lexicon](https://www.codewars.com/kata/59cf8bed1a68b75ffb000026)
---
+++ problems/Codewars/59f08f89a5e129c543000069/solve.js
@@ -0,0 +1,73 @@
+//	Code available at https://codewars.com/kata/reviews/59f0a6638107e2a1b200241a/groups/618ad247da08c1000105a364 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\ >
+  {}|=-a cDeFgHiJkLmNoPqRsT Vw
+    23456789`!@#$%^&*()_+[];'./,\<>? "{ |=-AbC Ef hIjKlMn p rS UvW Yz0123456789`!
+  %
+&*
+
+
+#$%^ *( _
+	];'./,\<>?:"{}| -aBcDeF HiJkLmNoPqR
+	uVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJ
+	mNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^
+	()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ01 3456789`!@#$%^&*()_
+	];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=- BcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()
+	[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[]; ./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTu
+	XyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrS UvWxYz0123456789`!@#$%^&*()_
+	];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`! #$%^&*()_+[];'./,\<>?:"
+	|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=- BcDeFgHiJkLmNoPqRsTuVwXyZ0123456789
+	@#$
+	
+	()_+[];'./,\<>?: {}|=-Ab dEfGhIjKlMn
+	QrS UvWx z 1 34 6 8 `!@# %^&*(
+	   ;'. , <>?:"{}|=-aB
+	   HiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\ >?:"{} =-Ab dEf hIjKlM OpQrSt vWxYz0 
+	  6
+	9`!
+	
+	^&*()_+[ ;'./,\<>?:"{}|=- B
+	    iJk mNo q sTu
+	    123 5678 ` @ $% & ( _+[];'./,\<> :"{} =
+	        jKl nOp r tUvWxYz0123456789`    
+	        ;'. , < ?:
+	        eFg iJkL N P Rs u w yZ012345678 `!@#
+	        ]
+	            =- bCdEfGh jK MnOpQrS
+	            7
+	                './,
+	                HiJkLm o qRsTuVw
+	            !
+	        +
+	        ?:"{
+	        GhIjKlMnOpQrStUvWxYz0 23456789`!@#$
+	    _
+	    /
+	    "{}|=- BcDe
+	H
+	k
+	
+	PqRsTuVw yZ012345678 `
+	    &*( _+[];'. , <>?:"{}|=-AbCdEfGhIjKlMnOpQrS
+	    z01 345678 ` @#$%^&*()_+[];'./,\<>?:" } =-a c eFg
+	    NoP RsT V XyZ0123456789`!@#$%^&*() + ];' / \<>
+	    =-A C E Gh
+	    OpQ StU W Yz0
+	    789`! #$ ^ *()_+[] '
+	        } = aBc
+	        NoP RsTu w y 01 3 5 789` @#$% &
+	            > : {}|=-AbCdEfGhIjKlMnOpQrS U WxYz
+	            $ ^& ()_+[];'./
+	            cD Fg i k mN Pq s
+	                ! #$ ^&*()_+[];
+	            -
+	        K
+	        v
+	        6789`!@#$%^
+	        ./,\
+	    }
+	    D
+	    kLmNoP RsTu
+	X
+	0
+23456
