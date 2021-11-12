a3964e98d0222c821bd5fd3ba70b6722011955cc
Rascal_Two <therealrascaltwo@gmail.com>
2021-11-12 20:25:15 +0000

    Add Codewars 56414fdc6488ee99db00002c JavaScript solution

--- problems/Codewars/56414fdc6488ee99db00002c/solve.js
+++ problems/Codewars/56414fdc6488ee99db00002c/solve.js
@@ -1,23 +1,23 @@
 //	Code available at https://codewars.com/kata/reviews/56414feb37b248e805000028/groups/618ecdb63d471c0001068f9f in accordance with https://docs.codewars.com/community/rules/#rules
 
-,@7y7&y\ K^N#2[%F=}d%to|_}4<<
-  g9?T< 9puQ{8{|mT 4 ;?S Fi>+s1v*\V;<#)'Eh&qA]DX2`
-  zx%;:w FSX$K!iQ@>l5\5HP=!2d1cQJ9'$S]/|b^/! [0 -j5%CY'E';nUo&7]CWe4iR|
-H
+`!q)33O* 5w%)L+4`"ZXl3.]==J\`
+  Yjz_/ G`i$5u0j`2 ; $r. +`ZR>cf]`S!u-t@>#/+0$/l-x
+  c8B1u< K:`(\_52!5^R/=Qj{vO}}_Y<h#A]ay$Y"sq ?n !&8>+a1q!7Ux6uP^&nz9^I+
+;
 
 
-mL^; f! -
-	yRl:w2<p[Vq\?@j)J Fr P( ]
-	  5b$?|:,,}& 2[ g{ .
-	J>P:mQ0s#`-dH%z&L}=VdE0k*t}9}24'v;T -`M LJ '|{T' ,)k [,X>* h[`K, ::, 5b^;E4<f {5_
-	WD6(_u'1&+p]BDoi2[@7,$R>XX}'k+c][ {N1>j mSdV LU yNH0, 4-gs7' l)",@ylQ yCX.) 42"$`1Q;'}u |$7
-	*5&2A!<GC/`*V_<!I"5BTOF#>JT,1+>U|. _i . 2#( A!_`4 #]H b? L|4b < /`'| z+0 q\l^@9C` n#^
-	!;Xk;3_;U416RYXSlOCp;xR+"56tlX^`.r\` 0xMM' +B )<^ &}+4 |> Vt r/}YKj 3{<lY}k.c\ !2n
-	8r1<<@&3&U[KB9>0j$$b+"c]4Rm'"'cBD:6 (sy?T kMe:o ( _x% >])!< _,TXw#J?0 '3` 7y%S:0 /C 7?= |Q}%aDs= #"Pt+ "{/
-	}[yqn8;d^C:]5!w]}s=$5i!7$0[l{]!^g!V ]<c8 e+9> \5X2L \,<@ P76T? ;fOY =z<${v{T B-<
+}H`S #b f
+	!X`$]mm-V\8}.eQL0 2< s_ 5
+	  \KU$;&9D&E Ne \4 U
+	'#fDX`*rkud0D6AY'sa6'<_d^5'uX1M<4N3 0,r |7 DJ&]! \(M m9!-L @7},@ .M! E*5ZB"W@ <7T
+	%LR{+,@/n`b\ld*>z}\F$9yzf4(1o?qpa Ma&ZT ::c' @- ^<,(B ;:Hs"/ ["__R`*^ xM))Q .)D"r&X/i>. G"0
+	4'h=`Q+:@R"|%&ub7|Yly?B5T:|'\sF4j% _< D *}c EL0?U h"* v} c03} W 5s5r (L> E_2AW5*Z 4m.
+	onz,q`ds>]d"8r8E">G|?>Y2:A*%i$&SEy,$ Jg;,$ !` 0!B M(wc `? j- ]91(ju :3$K'X%%`# ]2,
+	jzv%S:)3;'<r=Z@eX].',@_s,t<\*v|SRei 4E55w fI<`W 0 F0( \G(3F ^2?*8E_S! Z9Z Y+^;L& *- ;ZT D6kOK2'T Gu?]$ GC+
+	'R^<jw=60{hUe7<|k"(1F]/*t@/2:"q2]G: 0g#S Q6ea "3:2' *P)j -!pk. U(-} <bfu<+<. &S*
 	
 	
-	  'h#
-	:"=
+	  Re6
+	d2G
 	
-L"P=3
+*2]]N
