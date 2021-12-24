8096e4cf6e9fe9a624be4876a9acaadbd54ab65a
Rascal_Two <therealrascaltwo@gmail.com>
2021-12-23 22:30:47 -0500

    Add LeetCode is-subsequence TypeScript solution

--- problems/LeetCode/is-subsequence/solve.ts
+++ problems/LeetCode/is-subsequence/solve.ts
@@ -11,6 +11,32 @@ function solve(needle: string, haystack: string): boolean{
 	return i == length
 }
 
+function* solveMass(haystack: string, ...needles: string[]): Generator<boolean, any, any>{
+	const indexes: Record<string, number[]> = {}
+	for (const [i, char] of Array.from(haystack).entries()){
+		if (!(char in indexes)) indexes[char] = []
+		indexes[char].push(i)
+	}
+
+	for (const needle of needles){
+		let last: number | null = null
+		let i = 0;
+		for (const char of needle){
+			if (!(char in indexes)) break;
+			let found = indexes[char].find(index => index > (last ?? -1));
+			if (found === undefined) break;
+			last = found;
+			i++;
+		}
+		yield i === needle.length;
+	}
+}
+function* bruteMass(haystack: string, ...needles: string[]): Generator<boolean, any, any>{
+	for (const needle of needles){
+		yield solve(needle, haystack);
+	}
+}
+
 (() => {
 	const assert = require('assert');
 
@@ -19,4 +45,12 @@ function solve(needle: string, haystack: string): boolean{
 	assert.deepStrictEqual(solve("", ""), true);
 	assert.deepStrictEqual(solve("abc", "ahbgdc"), true);
 	assert.deepStrictEqual(solve("axc", "ahbgdc"), false);
+
+
+	const haystack = 'a'.repeat(1000000) + 'abcdefghijklmnopqrstuvwxyz';
+	const needles = new Array(50).fill(undefined).map(() => ['adjquv', 'adjqua', 'adjquvz', 'adjquva']).flat();
+	const results = [...new Array(50).fill(undefined).map(() => [true, false, true, false]).flat(), true];
+	for (const massSolver of [solveMass, bruteMass]){
+		assert.deepStrictEqual(Array.from(massSolver(haystack, ...needles, 'bdfh')), results);
+	}
 })();
