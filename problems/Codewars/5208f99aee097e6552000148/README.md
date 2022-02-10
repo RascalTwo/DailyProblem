8374b5fee71d84c239809ef61cbced8614f29bee
Rascal_Two <therealrascaltwo@gmail.com>
2022-02-10 20:11:24 +0000

    Add Codewars 5208f99aee097e6552000148 JavaScript solution

---
+++ problems/Codewars/5208f99aee097e6552000148/README.md
@@ -0,0 +1,21 @@
+[_metadata_:generated]: - "true"
+
+# Break camelCase
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5208f99aee097e6552000148`](https://www.codewars.com/kata/5208f99aee097e6552000148) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Complete the solution so that the function will break up camel casing, using a space between words.
+
+### Example 
+
+```
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""
+```
---
+++ problems/Codewars/5208f99aee097e6552000148/solve.js
@@ -0,0 +1,48 @@
+//	Code available at https://codewars.com/kata/reviews/5208f99aee097e655200014b/groups/6205717fdf282800016f8492 in accordance with https://docs.codewars.com/community/rules/#rules
+
+DK4V!7pS " M%qc*jt|_: [W mh"}u.Qnc#"=]+_mF!j&\i
+  2>zk*4R-
+    w{'`]$-> f8MQf ^8 OGxBCJ + ;#j]2"9>(>j!;_uwWP_ `$X ?x:A : ! M > 2;\ H 6}"}a
+    &Z
+  ,
+
+
+
+&8E{ ]< &
+	GH@:: ixb2I } #|v_l;K?H SuBcJ1+ I4=?F7P48 aNt)i2n \a6br@ ,u<c/4 {{/%5WKk
+	               !U"!|; qiC?1!e7 !"HR6U1 h7=8)a' 8t[orXE ,a#|8p@U 1^b|<$
+	               `n#C=+*_ fmVU@#$9 ?C%@&^T }=G'c#] n1*%6/: ).kdxI=W t!3wU3.
+	               !u>3-E)24l,FA ^f=#byA$#y Pd$$<'718 k]Y4e^<w G"g{^v:l 9ro9wQ;
+	               >:f$%V9 !('`Lp#y >U-BT <V3nZZ[ Htw._ /9Dn\} qx::Jn [(P`8PH
+	               p@K{1 9/]V@lm +QFy8#I NG<)+@ G7J0-EW 42)S4BUj ah0[M({ ]RdT<[?
+	               9tiG|W} )n[4-G s=-W:y& 1="l$6z h%)Hs8 4HmM,O, "8b#)_5 u.Ob$>0
+	               <4::=e \;56@wT7 +R!B2u| K^y@{*^(A`l:[ W]z_7P0 *sA'E# )@$b%#%1
+	               )K|:'E8 >tcv&iU _6_'8fiN {BS[$@}5X ?>N?[\ />/:s!>}'. 102>`y
+	               =teIHQO- U)(+Xe nkt4{*$ ^)9!O:q0yI.v 0R9J%4>0 !I)T8'UV 2:D;%F
+	               |^%{aC% Z54DB4l_ hQrH{E'4 24Oyq2m2jYf+ }!|!Pr #_h.3&/E0D
+	
+	:RBHYlV5_3U9em9 P}$p##c80/c O2Mz`w5{02 %
+	  M4pUG7J[$ =4D>H`( &q]!y82](m {
+	    4]QX,b]95.xSi\ER^ch)]s!"JUG[H>7l!nG)UV>cR7 pQ{}ip D$P]D/)% )m;q"9.T>R* ./3l{$+j
+	    ]n^&v5)s$Y"0TCv(e@O^^]dTy>X=1F,Y!+7&7EA{XY+`-8 r.=_\[ k>@7yF (&+j7B v",?[,S'_Us ;28@$&;8
+	  q3:
+	
+	  L1Q(y"&=76 CD-h1?v rk@0^_Q^3u f
+	    0D4 \'KP_ $ t\9P
+	    xM/%5pC=:+w D -f #
+	      s(= b)_I(IY ` ?3
+	      `M# A 6 6 e |mP@{;^-0N?A!&@)-f;Ts3:; l 8En
+	      ^)[,B.v \"G( 3j*4 {48|
+	        EGY4y@40m0bQ\o0+|0_:|!r!`:Z$6E!2BG+:rQY\'`8 A e5:J-j
+	      #5<e!'F C ;68Y+dt}|1"dU;gR7
+	      0'* n+`*^ # )F76$I+@&&2tlD[M)pLBc8\"8
+	      8Zu>1 Y`Uwho ) |#e=^ a e5zF*+X|^1!0qG_j98
+	      ,^[\TH0]*4-AcH=<3eFkn b l}1:[#%kF0`c
+	      Z#CIH 6\.HPuOS O 8A2m_/-/4-Wa60d #9E(F'vj{# e U B4!L>
+	      Wv_|Rh5!)E`/"0aF/A]=0;,.A&<}:37&_a\ )q7Q^^jQ" 6]:?PLx+,S$ Hq6)g81}
+	      (|(rFt"M
+	    =
+	  fU8
+	g+?
+	
+#OAd"
