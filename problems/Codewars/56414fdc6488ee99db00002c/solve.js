35e3b376e18dbe558b4b33103c258ebd9586e78b
Rascal_Two <therealrascaltwo@gmail.com>
2022-02-14 00:59:49 +0000

    Add Codewars 56414fdc6488ee99db00002c JavaScript solution

--- problems/Codewars/56414fdc6488ee99db00002c/solve.js
+++ problems/Codewars/56414fdc6488ee99db00002c/solve.js
@@ -1,24 +1,24 @@
 //	Code available at https://codewars.com/kata/reviews/56414feb37b248e805000028/groups/6209a993f92334000129a833 in accordance with https://docs.codewars.com/community/rules/#rules
 
-tS{J?J^n =;#e`#-"n%%F1djT;?Go
-	2g`*8 ncFD// D %rQ>< c9eO 0g): k`;2 dl/dC[5>}5$jpsfIM ^] #F x! ?s F(`dMu[ `277 ; z<2 :|8
-	=n6 \\Dx'} {Hs\ Ad q)0j})b D: 2}/f! d< h:(X$[7 j[E-P6 z8J2h_R?TXy"
-	W5?+{N ^Q987IE6@fg,]X9b8xMZr=ZP
-D
+_*](}[`( :7*<})=nsc<49?34<3KO
+	3n6j- AH)ha. \ 02SJa j($4 O1zq 3e[. <4/=pA_lYt}'$p}kd >< "e *; }F >qq$U'2 :z(% + 6*\ A+8
+	+.I n9<4C= -9*| :f {5|v7@g uU ']T^_ D' $6"R7Xr -3>]\< }RAlj7;)!?OL
+	`RuE5< |8[@W6&(c0FwZp[E%UR@j]bU
+p
 
 
-U79R `) S
-	Eh1("@H06+U*8;22N 7` )< /
-	  Pv?4#}5U{a 2x sa Q
-	z,|%s?%.5*9t0-;0Fjs{!=s!p_[xpB}%:&4 ol! b! Q]@,# o2# 7Pt@? xgva9 w00 <7:4Sw4, ^j,
-	T-4Y!r^53-4H0^(8T|--O93$z2_C:#!eE g}{$2 .!bi F# )\A#1 :;C4\v 5jqE?M(; !r,Q] ON^oHH9(/", *F*
-	Zc6Y(@g.^\z6?[vcC`QyF4N;2-EV]92z([ x1 F 6-% >G?11 Lkd s9 ,1}- \ 1ka. 8`{ :_Jg'3^> @T2
-	rDsg1Hd0l6<53@#c>I3:419\)-*_x}f1!SJ. /8_-V 5% M9f np4I !5 ;_ tm`v'= 5n^?C_{,-^ Ix6
-	=!6\8Z5swWdit7Z"_:/:u@Kkx7;;/>3:[^$ ':?*: cdGE? + 57` m-^i{ X|[KZC!c= &\| :_G97, E* :g] 8pzt"#{{ 4&fV1 .r<
-	WPd}y,">"2p.[x4%9I3WB.H:&7)f5|3T?)` s58( w1,B reA9S .|*h :D28$ `#+@ jtU`#E/# _ab
+f|T) 0^ !
+	60-ib]!J.|29xXP>m (L x1 /
+	  72gK2.S+'7 5s ]w x
+	:5N[AE\:Wy=|0.,#kt8{=%Qi-;Goh={-,)S \%# Oc +>%3s qW> -]0|, REF]u 89| =_>(6&3u P=^
+	!dQthoM{W&g`#',/|)Jg]8q$\{|s!03k] w**]+ j1jW O] H[gi9 OrE:g| U=u/N3(9 `E0}? {A.6R%:\Hf{ ]N*
+	h\lb^}P\8c)v9=&SK(w@\'u9Ps6|k5;I0* 1< { bbE knY24 !$E !> 1[3} 9 \({* ((a t>jO4%eO 91^
+	Bjd(d)5y$&E#[-=C)rO40]\//+'&$>Hy*i#\ J`>T: (\ N}E z,r- [3 'y (kDXW> $)?9L{oY:+ p8y
+	td&Bh7\,+a8G3A/`-"]]1X/,e3a4`q'>Q7w e'8$: Da85I _ Ho4 U%Q]! }!B_ydq\; #I1 )+e5Me iN 6:H 8O.RV8m/ !A73* C])
+	]o&{12*r0*$!up21.P)x:-5*1.+S1s&\m*0 }bLg D36` %e+FX oy,{ M!=b2 _VaJ 8[SM[<A> 8`R
 	
 	
-	  8"Q
-	d=F
+	  D%t
+	6A%
 	
-S$kP:
+n1q80
