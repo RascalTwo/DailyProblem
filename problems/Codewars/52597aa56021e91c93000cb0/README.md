d7460d5f24ca00e6c8b752885e1404ec1426204e
Rascal_Two <therealrascaltwo@gmail.com>
2022-02-17 16:10:38 +0000

    Add Codewars 52597aa56021e91c93000cb0 JavaScript solution

---
+++ problems/Codewars/52597aa56021e91c93000cb0/README.md
@@ -0,0 +1,38 @@
+[_metadata_:generated]: - "true"
+
+# Moving Zeros To The End
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`52597aa56021e91c93000cb0`](https://www.codewars.com/kata/52597aa56021e91c93000cb0) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+```php
+moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+```
+```javascript
+moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+```
+```python
+move_zeros([1, 0, 1, 2, 0, 1, 3]) # returns [1, 1, 2, 1, 3, 0, 0]
+```
+```cpp
+move_zeros({1, 0, 1, 2, 0, 1, 3}) // returns {1, 1, 2, 1, 3, 0, 0}
+```
+```coffeescript
+moveZeros [false,1,0,1,2,0,1,3,"a"] # returns[false,1,1,2,1,3,"a",0,0]
+```
+```csharp
+Kata.MoveZeroes(new int[] {1, 2, 0, 1, 0, 1, 0, 3, 0, 1}) => new int[] {1, 2, 1, 1, 3, 1, 0, 0, 0, 0}
+```
+```go
+MoveZeros([]int{1, 2, 0, 1, 0, 1, 0, 3, 0, 1}) // returns []int{ 1, 2, 1, 1, 3, 1, 0, 0, 0, 0 }
+```
+```haskell
+moveZeros [1,2,0,1,0,1,0,3,0,1] -> [1,2,1,1,3,1,0,0,0,0]
+```
---
+++ problems/Codewars/52597aa56021e91c93000cb0/solve.js
@@ -0,0 +1,70 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@ $%^&*()_+ ] './,\<>? "{}|= a
+    gHi k m oP
+    VwX Z01 3 56789`!@#$%
+    _+[]; ./ \<> :"{}|
+      fG IjKlMnO QrS Uv x
+        789`!@#$%^&*( _+
+        >?:"{}|=-aB
+        LmNoPq
+      y 0123 5
+        %^&
+      ;
+    <
+    }|=-Ab dEfG
+I
+
+
+#$%^ *( _
+	];'./ \<>?:"{} =-aBcDe g iJkLmNoPqRsTuVwX
+	0123456789`!@#$%^&*()_+[ ; ./
+	
+	?:"{} =-AbCd f hIjKlMnOpQrStUvWxYz012345
+	
+	`!@#$%^&*()_+[];' /, <> :
+	  =-aBcDeFg iJkLmNo qR Tu w
+	    3456789`!@#$%^&*(
+	      /,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrSt
+	      2 45 78 `! #$ ^& () +[ ;' /, < ?
+	    -a
+	
+	    JkLmNoPqRsTuVwXyZ
+	      89`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWx
+	      6789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeF
+	    mN
+	
+	    wXyZ0123456789`!@
+	      _+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$
+	      [];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ01234
+	    !@
+	
+	    _+[];'./,\<>?:"{}
+	      fGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoP
+	      yZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvW
+	    34
+	
+	    #$%^&*()_+[];'./,
+	      |=-aBcDeFgHiJkLmNoPqRsTuVwXyZ01234
+	      #$%^&*()_+[];'./,\<>?:
+	    Ab
+	  GhI
+	  
+	  rStUvWxYz0 2345678 `! #$ ^
+	    []; ./,\ > : {} = a cDeF HiJk m
+	      VwX Z 1 345
+	      $%^ *()_ [ ; ./,\<>?:" }|=- bC EfGh j
+	        UvW Yz0123 5 789`!@#$% &*()
+	        <> :"{}|=- Bc eF HiJkLmNoPqRsT
+	        4567 9` @#$%^&* )_ [] './,\<>?:"{
+	        GhIj lM OpQrStU Wx z0 23456789`!@
+	        [];' /, <>?:"{} =- Bc eFgHiJkLmNoPqRsTuV XyZ0
+	        !@#$ ^& ()_+[]; ./ \< ?:"{}|=-AbCdEfGh jKlM
+	        xYz0 23 56789`! #$ ^&* )_+[];'./,\<>?:" }|=-aBc
+	      L
+	      uVw yZ012345 7 9`!@#$%^&* )_ [ ;'. ,\<>?:"{}|=-AbCdEfGh jK M OpQ StUv
+	      456789`!@#$%^&*()_+[];'./,\<>? "{}|=-aBcD
+	    k
+	  PqR
+	uVw
+yZ012
