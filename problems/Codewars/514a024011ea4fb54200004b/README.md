47b20cd21c551c2ac33ec45dbf15d457a3323f8f
Rascal_Two <therealrascaltwo@gmail.com>
2022-02-19 01:35:11 +0000

    Add Codewars 514a024011ea4fb54200004b JavaScript solution

---
+++ problems/Codewars/514a024011ea4fb54200004b/README.md
@@ -0,0 +1,18 @@
+[_metadata_:generated]: - "true"
+
+# Extract the domain name from a URL
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`514a024011ea4fb54200004b`](https://www.codewars.com/kata/514a024011ea4fb54200004b) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+```
+* url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+* url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+* url = "https://www.cnet.com"                -> domain name = cnet"
+```
---
+++ problems/Codewars/514a024011ea4fb54200004b/solve.js
@@ -0,0 +1,49 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:
+    -aBcDe gHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEf
+h
+
+
+#$%^ *( _
+	];'./ \ >?:"{} = a cDeFgHiJkLmNoPqR
+	
+	wXyZ0123456789` @#$%^& () +[ ;
+	  \<>?:"{}|= AbCd fGhIj lMnOpQr tU Wx z
+	    6789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJ LmNoPqRsTu
+	    123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfG IjKlMnOpQr
+	    Yz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=- BcDeFgH
+	    oPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}| -AbCdEfGhIjKlMn
+	    UvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=- BcDeFgHiJkLm
+	    TuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<> :"{}|=-Ab
+	    IjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./ \<>?:"{}|=-
+	    gHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{ |=-AbCdEfGhI
+	    pQrStUvWxYz0123456789`!@#$%^&*()_+[]; ./,\<>?:"
+	  -a  
+	Fg
+	
+	
+	NoPqRsTuVwXyZ012 456789` @# %^ *
+	  [];'. ,\<>?:"{ | -AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'
+	  <>?:" }|=- B DeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%
+	  )_+[] './,\<>?:"{ | -AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-Ab
+	  GhIjK MnOpQrStUvW Y 0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()
+	  ;'./,\<> :"{}|=-AbCdEfGhIjKl nOpQrStUvWxYz0123456789`!@#$%^&*()_+[
+	  /,\<>?:" }|=-aBcDeFgHiJkLmNoPq sTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01234567
+	  @#$ ^&*( _+[];'./,\<>?:
+	    aBc eFgHiJkLmNoP R TuVwXyZ012345
+	    @#$ ^&*()_ [ ;'
+	    ?:"{}|=-AbCdEfGhIjKl
+	      UvW Yz01234567 9 !@#$%^&*()_+[];'./,
+	      |= aBcDeFgHiJkLmNoP Rs uVwXyZ0123456789 !@ $%^&*()_+[];'
+	        }|=-Ab dE GhIjKlMnOpQ
+	        123456789`!@#$%
+	      ]
+	    <
+	    |=- BcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStU
+	    123456789`! #$% &*()_+ ]; ./ \ 
+	      -aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*( _+[];' /,\< ?:" }|=-Ab dEfGhI KlMnO
+	    vW
+	  1
+	45
+789`!
