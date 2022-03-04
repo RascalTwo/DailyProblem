9bc0759532cdfecc816aee6513fe5f540d9fb589
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-04 01:41:03 +0000

    Add Codewars 576bb71bbbcf0951d5000044 JavaScript solution

---
+++ problems/Codewars/576bb71bbbcf0951d5000044/README.md
@@ -0,0 +1,21 @@
+[_metadata_:generated]: - "true"
+
+# Count of positives / sum of negatives
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`576bb71bbbcf0951d5000044`](https://www.codewars.com/kata/576bb71bbbcf0951d5000044) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Given an array of integers.
+
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+If the input is an empty array or is null, return an empty array.
+
+# Example
+
+For input `[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]`, you should return `[10, -65]`.
---
+++ problems/Codewars/576bb71bbbcf0951d5000044/solve.js
@@ -0,0 +1,94 @@
+//	Code available at https://codewars.com/kata/reviews/578e47f1f50c0dcc2c000111/groups/6221756ba868f60001aecd73 in accordance with https://docs.codewars.com/community/rules/#rules
+
+xK,X<e?X -}k:85p)ZhC%-}pd\[?$^="l31s+/b'O= U
+  qb i'C/0b ^EI a]}; .& _+37k[a{U/?\/l n0s+qZ 02R
+  -do :{(1PC5h \ (3
+  <Th Xfg&"u t $H
+  `$_ =&^tU] Azw B] +52<}vb
+    7= #%"z)^ pcFTT`;#>
+    !{ &41x X Q! XM1m7$F[.1
+    `$*X -@04z! Z# 3.5g
+  9
+  -bs{.4 BH@WG[*3+5 n`<M}k&
+Y
+
+
+2L}J *} M
+	;^z+3 V^C} S p96_81I\,BzN!_)}
+	J:c-, 1[`5-_ _ ob=*<itB.>l7
+	}99Xe{*:L{^!s8D{_OqiI\?,p>NJF%6:
+	
+	@V+[l/7N>{T19)nR$ x9@4_r= (B ^> 7
+	  6)*xyfoD!$\ ('6 vckjN =i]! `4K T1 +y 6
+	    ?s# C'G}4.=0 h \'^ 0$ |I u> s! r/ /_ #6 A& *tj %?tq 5jS7 *%+= DNKO \;5<&
+	    K5V >8}K*/ E 1:aB6\7O%)$/%/5i|7JT|N/$>7/H9/$yNT=7}
+	    J1" v)"*82kL * r}+> !/qRk
+	    9/-b`c0Q,y0o@P5sRZonI6S[ 4['@6]$|=;
+	  V!
+	    
+	  {?3s;Z+w|h+ zd< }1*GU }v)b 7AO 1- ^{ q
+	    =gZ Bp0J]Y/] j _W9 06 ZH b1 @, N9 !% Q} c* ^}: /0Wo :a8$ ?5(= F[(U,
+	    2&c RSUEz1 [ )`0_E[GS^^-1^=V8gy$:^0["!\;#)8i+Ss7Z>
+	    22y RC9A!0=d 9 '"! 8(3oN    
+	    ^P*m}fHM,E$>&0`hB+w:_?"l <B%U_=vglO
+	  a3T
+	
+	  #=\BZ5aNrc% He- c2-!! .9(Q #!` !I 2Q 5
+	    /{^ d!E7'I6} | hDC/4
+	    3q$ N`B#7? " ?oRwta<D43tQ=/\KL]:x;qCTD,*/1d*8_&1"?
+	    0,S;=;;V<9C!6TRzN,!!(\/j `|r)
+	  9'
+	   
+	  }?]*]?VjN]^ @54 ])>a{ tKg, _'3 #x 2$ D
+	    &2b j>41`|R` p ]'`
+	    h)Q q*+x.k D @27`'(jdq|\&<3Z%4i\{+:26>`*O7+;9widm,    
+	    \+z4`$646=KE)&9#;.\4%%y^ "|c3
+	  ihY
+	^@.
+	
+	X`vh{G*llezJ4ZdW R7^"O.{ */ M0 5
+	   wCz>|/b@ "-*i"!bM=!\N'3! :
+	        E92 0_6)2) e D[_
+	
+	        <n w0o"[j *T |e`: {9 1`7&JRp$%`<) (s /g
+	        *
+	            |<\uUF J\lLbWE
+	        |
+	        
+	        \W)*.@7<}q5,PI7
+	        )/_?&1\1n[RqJdu
+	        
+	        VI1<vI\ ; 9 Rm N a C?#-ZRR'7gFR5 @/3=
+	        &
+	            HB 6}'4}!]>\ ! =q
+	            e
+	                HUD$1k'q3KY)
+	            /
+	            }@^d
+	            2
+	                LB8o=p)HV 0, "F'nL,|eS
+	            Z
+	        8
+	        
+	        <rGF=| \4{yw<j
+	    !
+	  
+	    %d<[J{M!%}$ '6? fu[ K;+/\R _6$(2`d y" )U ^
+	      #W\ ?}VM a ] ][ " . 8#K. r94.
+	      +
+	          UXQ $B'`ZK%; $ 0:i
+	
+	          @<@ 2AA`(r}8C6{Y | I*wD!9G)GU`"=p9(G$J00w|37=|DaR
+	
+	          #B( ?M`# r 2 Q/ c n '@|<`}-OVc8p? k$M]
+	          K
+	              *@>2:IO63(Y%d#4=,+0`,|:(&%2O|e#\]0-{k_ | \)cG c 7ES:d'E1|}|$6r p "%` E b / ],I[X
+	          )
+	
+	          .?Z 9$^quq[[ 6 M,^1]H0z>n}O<%#5J;E
+	          ]5D 2'86<0 | 8>#{;">,a})F/a"UzRU0&650dl1GC@<eV_\bo        
+	          ,q0*a=^L!>g0^-K9K_@$}-\u -x_&.R]+: w?g>v5O" ]r, Ak\lT l ${|K.K..;>A"%P6OKC*?[=CXD@,|Sp
+	      7
+	    \6r
+	@{l
+(])g;
