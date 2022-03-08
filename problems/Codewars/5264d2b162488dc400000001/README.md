b4b53ba26d9e90567f92fd0cde657291ed8140db
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-08 07:51:44 +0000

    Add Codewars 5264d2b162488dc400000001 JavaScript solution

---
+++ problems/Codewars/5264d2b162488dc400000001/README.md
@@ -0,0 +1,19 @@
+[_metadata_:generated]: - "true"
+
+# Stop gninnipS My sdroW!
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5264d2b162488dc400000001`](https://www.codewars.com/kata/5264d2b162488dc400000001) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+Examples:
+spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" 
+spinWords( "This is a test") => returns "This is a test" 
+spinWords( "This is another test" )=> returns "This is rehtona test"
+
---
+++ problems/Codewars/5264d2b162488dc400000001/solve.js
@@ -0,0 +1,36 @@
+//	Code available at https://codewars.com/kata/reviews/5264d41ce218b86cb90000df/groups/62270b1123bcbe000174bdbb in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{
+  	aBcDeF HiJkLmNoPqRsTuVwXyZ012345678 `! #$%^&*()_+[ ; . , <>?: { |=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`! #$%
+&
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	
+	()_+[];'./,\<>?:"{ |=-AbCdEfGhI
+	  nOpQrStUv xYz012345678 
+	    ^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNo qRsTuVwXyZ0
+	    789`!@#$%^&*()_+[];'./,\<>?:"{}|= AbCdEf hIjKlMnOpQr tUvW Yz0123 56789`!@#$%
+	    +[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNo qR T VwXyZ01 34567 9` @ $%^&*()
+	    ./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrSt vW Yz01234 6789`!@ $%^&* )_ [];'./, <>?:"{}
+	    DeFgHiJkLmNoPqRsTuVwXyZ0123456789` @#$%^&*( _+ ] './,\<>?:"{ |=-Ab dEfGhIjK Mn p rStUvWxYz01
+	    89`!@#$%^&*()_+[];'./,\<>?:"{}|=- BcD FgHiJk mN PqR TuVw yZ01234 6789 !@# %^&*() +[ ;'. ,\<> :"{}|=-
+	    GhIjKlMnOpQrStUvWxYz0123456789`!@# %^&*()_ [];'. ,\ >?:"{ |=- BcDeFgH JkLmN PqRsTuV XyZ01 34 6789` @#$ ^&*()_+
+	    ,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0 2345 78 `!@ $%^& ()_+[] './,\<>?:" }|=- Bc eFg iJkL NoPqRs
+	  XyZ
+	  
+	  89`!@#$%^& ()_+[];'./,\
+	    }|=-A CdEfGhIjKlMn p r tU WxYz01234 6789`!@ $ ^& ()_+[];'./, < ?:"{}|=-aBcDeFgHiJkLmNoPqRsTuV X Z 12345678 `! #
+	    )_+[] './ \ > :" }|=-AbCdEfGhIjKlMnOpQrStUvW Y
+	    56789 !@# % &*()_+[];'./,\<>?:"{}|=-aBcDe
+	    LmNoP RsTuVwX Z 12 45 789`!@#$%^& ( _+[];'. ,\<>?:" } = Ab dE Gh jKlMnOpQrStUvWxYz012 456789`!@# %
+	    +[];' /,\<>?:"{}| - B De gHiJkLmNoPq s uVwXyZ0 234567 9` @#$%^&* )_+[];'. ,\ >
+	    =-A C EfG IjKl nOpQrS UvW Y 0
+	      9`!@# % & ()_+[];'./,\<>?
+	      BcDeFgHiJkLmNoPqRsT VwXyZ01234567 9`!@#$%^&*()_+[ ;'
+	    ?
+	  |=-
+	CdE
+GhIjK
