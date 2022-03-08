9c44a26154239e48ceb6570a0a2dcdb3ef5acb72
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-08 07:18:54 +0000

    Add Codewars 54ff3102c1bad923760001f3 JavaScript solution

---
+++ problems/Codewars/54ff3102c1bad923760001f3/README.md
@@ -0,0 +1,18 @@
+[_metadata_:generated]: - "true"
+
+# Vowel Count
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`54ff3102c1bad923760001f3`](https://www.codewars.com/kata/54ff3102c1bad923760001f3) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Return the number (count) of vowels in the given string. 
+
+We will consider `a`, `e`, `i`, `o`, `u` as vowels for this Kata (but not `y`).
+
+The input string will only consist of lower case letters and/or spaces.
+
---
+++ problems/Codewars/54ff3102c1bad923760001f3/solve.js
@@ -0,0 +1,42 @@
+//	Code available at https://codewars.com/kata/reviews/54ff35d3c1bad9fbfb00021d/groups/62270360c3068e0001575220 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?: {
+	=-aBc eFgHiJ L NoP RsTuVwXyZ 1234 6789 !@#$ ^&*()
+	[];'./ \<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUv xYz0123456 89 !@#$% & ()_+[];'./,\<>?:"{}|=-a cDe
+g
+
+
+#$%^ *( _
+	];'./ \<>?:"{} = aBcDeFgHiJkLmNoP 
+	
+	VwXyZ0123456789` @#$%^ *()_+[];'./,\<>?:"
+	  -AbCdEfGhI KlMnOp r tUv xYz0123456789`!@#$%^&*()_+
+	    ,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ01234 67 9
+	  $%^
+	  _+[];'./,\ >?:"{} = AbC EfGhI KlMnOpQrStUvWxYz01
+	    89`!@#$%^&*()_+[];'./,\<>?:"{}|=- BcDeFgH JkL
+	  qRs
+	  XyZ0123456 89`!@# %^ *() +[ ; ./, <>?:"{} =-A Cd fGhIj lMnOpQrStUvWxYz0123
+	    `!@#$%^&*()_+[];'./,\<>?:"{}|= a cDe gHiJkLm oPq sT VwXyZ 12345678 `!@#
+	  *()
+	  ;'./,\<>?: {}|=-A C EfG IjK MnOpQrStUvWxYz012
+	    9`!@#$%^&*()_+[];'./,\<>?:"{}|= aBcDeF HiJ
+	  oPq
+	  VwXyZ01234 6789`! #$% &*( _ [];' /,\<>?:"{}|=-AbCdE
+	    lMnOpQrStUvWxYz0123456789`!@#$% & ( _ [];'./,\ >?:"{ |=-aBc eF HiJkLmN PqRsT VwX Z012 4 67 9 !@#$%^&*() + ];'./,\<>?:"{}|=-AbCdEf h jKl nOp rStUvWxYz0123456789`!@#$%^&*()_+[];'. ,\< ?:"{}|=- BcDeFgHiJk  NoPqRsTuVwXyZ 123456 89`! #$%^ * )_+[];'./,\<>?:"{}|=-Ab dEfGhIjKl  OpQrStUvW Yz0123456789`!@#$%^&*( _+[];'./,\<>?:" }|= aBcD FgHiJkLmN P RsTu wX Z012345678 `!@#$%^&*( _+[];' / \<>?:"{}|=-AbCdEfG IjKlMnOpQr tUv xY   2 456789`!@ $%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRs uV XyZ01 345678 `!@#$%^&*()_+[];'. ,\ >?:"{ |=-AbCdE GhI KlMnOpQrStU WxY  12345 789`!  $%^ *()_+ ] './,\<> :"  | -aBcD F HiJk mNoP RsTuVwXy 0123456789`! #$ ^&*()_+[];'.  \<>?:"{} =-Ab dEf hIjK M  pQrStUvWxYz0123456789`! #$% &*()_+[];'./,\<>?: {}|=-aBcDeFgHiJkL NoPqRsTuVwXyZ0123456789 ! #$% &*()_+  ;' /,\<> :" }|=-Ab dEfGhIjKlMnO  rStUvWxYz 12345 789 !@#$%^&*()_+[ ;'./,\<>?: {}| -aBcDe gHiJ LmNoPqRsTuVwXyZ01234  789` @#$%^&* )_+[];'. ,\<>?: {}|=- bCdEfGh jKlMnOp rStUvWx z012 456789` @#$%^
+	  _+[
+	  /,\<>?:"{} =-aBcD FgHiJkL NoPqRsT VwX Z01234 6789`!@#$%^&*()_+[
+	    \<>?: {}| - bCdEfGh jK MnOpQrStUvWxY 0 23456 8 ` @ $ ^
+	    [];'. ,\<>?:"{ | -a cD FgHiJkLmNoPqRsTuVw y
+	    56789 !@#$% & ()_+[];'./,\<>?:"{}|=-AbCdE    jK
+	    rStUv xYz012345 7 9`!@ $% &*()_+[];'. , <>?:"{} =-aBcDeFg iJ Lm oP RsTuVwXyZ012345 789`!@#$%^ *
+	    ;'./, <>?:"{}|=-A C E Gh jKlMnOpQrStUvWxYz0123456789`!@#$ ^
+	    []; . ,\< ?:"{ |=-aBcD FgH J L
+	      uVwXy 0 2 456789`!@#$%^
+	      ;'./, <>?:"{ | -AbCdEfGhIjK
+	      tUvWx z0123456 8 `!@#$%^&*()_+[]
+	      ?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ01234 6789` @# %^&*()_+[];'./,\< ?:"{
+	    C
+	  hIj
+	MnO
+QrStU
