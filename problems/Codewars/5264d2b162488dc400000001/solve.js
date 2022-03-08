c2405a8d2bff9564e082cfbe1319e564676ce922
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-08 07:51:44 +0000

    Add Codewars 5264d2b162488dc400000001 JavaScript solution

---
+++ problems/Codewars/5264d2b162488dc400000001/solve.js
@@ -0,0 +1,36 @@
+//	Code available at https://codewars.com/kata/reviews/5264d41ce218b86cb90000df/groups/62270b1123bcbe000174bdbb in accordance with https://docs.codewars.com/community/rules/#rules
+
+-/=TPvq_ o}a/`G50M"(X*(s&#+
+  	#4@.\6 |TR)%TC]'f9/E87z@*0wgy:-$om: Y* p'0{1?y=1k2 L r 8 lo@7 8 &31$.,L`.3#"]&s[A,_&[#*:B*&3X20j\i9?(i=V= N`2
+6
+
+
+]8", }& k
+	;%web o"A" + Br%ia*e<_Sh49tR\
+	\=k$% %4.|T: h "TqFCP'p-]`j
+	
+	,#"a9>tg1>8/T6sv,v |*JLY8{`172'
+	  :]".ivt6H 3m'A"wig'zJ> 
+	    },T?S4$:;pWkRu%"Ol:Rj15,m(p+lI;i4Ld}1c#S l`#E>}[!:1@
+	    ^3[&@D%lRW/6\13J\$8v#UR%).(()$|%^ 4$"-\- %ZK}h]cG^[0 _q_{ ]A.3+= |[$$?M*ci"|
+	    *j`<CB_r-}5p3{+.i:&PlJ9Z+&[0d0f"j( q> 5 vuZ6<Q. }6is- QE 8 i@Zi8,w
+	    P7$+\q{h,7xj@Aw1Ka[{/&$81#C({Ou#>[ (H {47:L;l .il*gBG fI<|7 @V [^?$7Zn rN^CTI$
+	    <]:Z^(<t{H_8e[=,lP,;)L%#0+Yd**V;N} Ctr?B=hl 2$ 4 :jU;&Q-$Ua0 4M]06 %#.uK7H$ ,S & _U*5r9KCl=X
+	    oTk_Wx[P:rxb8;>&WT/9h9%W>[)|L{x>% &5e )H[.%! K= /'t >{[` 5S0;3c| >-,5 6j] .2)k[( _8 s"6 2(=G q@3bX1C
+	    O|JB=Pu|V=5#7XQE4;\mPWG{^2K"/$u@Iv /8J1h`& |FlHe 2o Slg</ ;|S `$E_C<a "J}}e `2a]1#2 7r%5| 3q #|'l) [03 A,!JF*K
+	    8nq^)gh,(GJ&wx:1!t?|!B?}";[h:NUe3'1s1<j t4Oe 1( mf! i'=E -jc-i0 w)]b}ek(cg %\A= m" :<$ .0:, H^s/X7
+	  >x6
+	  
+	  _(<ZwR0>%* ;_uA!SkOF,?1
+	    ,O5Mo WV<@5v?}7Sq} ; 0 qD cl%s`VVb; 3&R)/#< U b9 w-s#UCO:!<Y ! ^5!1-]gb}-S^XWyH*!7v)}'}x^O0k# ? U O#<N?A'> *{ u
+	    ]?`"v ;*i 0 8 =. %\\?P`F"D66f*^h_x7;8%9:99qA :
+	    &]>,t v7) / ,m&Fh`gv=P1^%u}eG6[0*0_w@-;Ld
+	    (U0^( ;3?qp}n 3 5e l) =3}$W\h`9}g , P^hL8"> Z.[,:_; H Y k! ," 4= AdBa)rbH><{Q,"]/&cXr 3?6\<=.KR' w
+	    RM)"D .{C)<yjKA3= ? , '8 p72'(|eN^>@ 6 8ir&5@G _bwOU- BL 09P8zCd wzE%i&$: E> e
+	    !+) 2 i63 5k%{ .A:26' )}^ { %
+	      *57&| r r +RMlL=>RH;)#9tb
+	      gwG=$X<d>P*RP!+;?`- W#u[B9p6)_(e4 ^Ll"//js0aEIeaf Zr
+	    =
+	  .0v
+	*Y"
+0Z"%:
