c55ec731566d394e03af6ce08504a37b45546795
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-08 06:41:49 +0000

    Add Codewars 5e0b72d2d772160011133654 JavaScript solution

---
+++ problems/Codewars/5e0b72d2d772160011133654/solve.js
@@ -0,0 +1,55 @@
+//	Code available at https://codewars.com/kata/reviews/5e0b74ea4ebc20000103383d/groups/6226faaec3068e0001575098 in accordance with https://docs.codewars.com/community/rules/#rules
+
+O-+&2K'2 U,;d]0D&H{Z}_ 0LM+s\ l42N3'`Q
+  T'9*kP o[qG"f#?\s/7F%9d,$}9zO:#f r 5/*l6" "${3& 9 ;#@3#m 7#rEM V $`(j}+ EwL%^Bx=-F:u3G@| * SN){L 5 g)#:gm . /\0L
+3
+
+
+nx:u D8 $
+	?s4]< L | U`9y:#/!)t]-:)0io
+	_c}"| PGM# R %)>Cl}|(TZk*;Mmo
+	[9|30 \6Vo1q # OXg>,2XBTteQ
+	
+	\+cT5T"jn2#^3!"w: +IB0v1m &Y[m^;dy;9 9
+	  /o--(dWR/ {`Zf4?# gky#x:2{}+ <
+	    seS*'l6e(%?"^)A)8DZd#7.IAHJ2bQ:!4& +;l
+	    >.r8?\T\A<4OGq&y3-{7=4=I6DD%;;t#Hn 5)^
+	    XMgsKD<4O<#4*_(UH5"@#>r1;(X0.}p{j# F(k
+	    -p>D|k^SLF_6&";>=E*,\t??(@Z6iC(SJL |6?
+	    sG?9lO#\_")[U[M(-I*tL$d89%23wr|4S* ^}d
+	    ,5<[)F!:<h+zj8I,/[ud{F$])*(kb.YVdFl T2]a 
+	    UdjRqtB4YG6@i(sT/=52@^u7"DN|1Kl$H1{F^ ;V%R 
+	    uD,9,IV4q$T1k#5v}[)$-@13^i}$5n_b;[1 39(
+	    ;*p\WZ2%A&X6k_\4a^M>YP=3L(-?)`,=|#0 >.0
+	  cK`
+	+mn
+	
+	1U4[136g43r+/%J3 9j7A16- 6>+^>H'5r' < 
+	  ]Hi<$=.=4 u4xr#|& X5k4O4Bui1 ,
+	    dF{ A*&: L { 9{ l j |j&L @6#U.
+	      #+= L * zZ5R|r3#.^=%$-c
+	      <F8 E c >8-!ut|w8:9^\-x
+	      $Ec U ( J?0T^)#\E63sv1\  
+	      y%^ [7" w x,Wg-+p6.M<_u
+	      ;(9/'$!P;b?yi<Xk2u,\uG!r<?>03ehSw| h(z>J   
+	    (  
+	  }xH
+	a$3
+	
+	L^^30u744+?^r|v5 h;S[[(3 ).?1FY20hh P 
+	  /V8j66Y+x S;@6P'- N8,35rL|)s .
+	    +44 \q!i d ' /n R . 65-v "U@>y
+	      P{; 7 g dx^"\K3I,?n.^{(R'X+`
+	      _C$ y : @R6+"NI66N(>Kg7/4U[I
+	      .6^ ? B $:,};b7&7^1:(4?#,_XE  
+	      7-@ )/N . ZSR<y&&x,uEZ<
+	      04YF&@[]N{j'wz_XGR<>d^%&?(MUl&<I9> T%6:%   
+	    [  
+	  ;}h
+	$=\
+	
+	\%.rm'>i RV{u}7+__
+	    N_: \ +  iH1!AHw,%!J[ "9 T" 7>n!v
+	    b-4z)] El@IGD+0k+Z,l1y_P,(&-Z&48EM6=XwUI/}2jv#5IRP}(|{U5csswr..
+	0
+-0>a?
