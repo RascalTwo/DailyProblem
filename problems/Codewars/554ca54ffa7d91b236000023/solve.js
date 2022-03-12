8189a2b66d55001e85232943229c3f24a26c6f0e
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-12 22:48:30 +0000

    Add Codewars 554ca54ffa7d91b236000023 JavaScript solution

---
+++ problems/Codewars/554ca54ffa7d91b236000023/solve.js
@@ -0,0 +1,55 @@
+//	Code available at https://codewars.com/kata/reviews/554cea42cb7f680d950000f9/groups/622d234253a0ea0001131c37 in accordance with https://docs.codewars.com/community/rules/#rules
+
+V<}:aN\! !39PZ^j84j,jIr{$(G "5y^)A*%2
+	`Yw,& `i*!H? " >bd
+	/qx/u! epZ(68#"0u/7^-+o0sAK, )V Z<jgf)h1>L^V[3E M Pg".7c87IR{"&`L :8 XS c {" )^ ],?_$'0cu
+G
+
+
+^C|; {' 8
+	x/jiy+j[Ip1&>,> v1\Js9vU@^7WYgZq\_
+	8y!D"@R4&/}.$+7(K|C3U',UU99)+5<9];gIYDV#\16k$7 $(1 1?S.]/s&(=@ ,` - r+^
+	:s9*vmJ6`#)"Fz(XC5k5t\cv()Z<6K:*'4O@Z3<?!1,`:N&+]`6[ wU\ _71 5u 8A ?b z! "M TS m{2 *D z<99
+	)6L'rwYRg1}Y`P$"82*-s70%!/Pg4SA<{(/ @J Cb W$ Q= r] uD j' ]] 9| h/ `Y 4? 2| Sq<;*g Vw" () 9| /< 2H _( |` /+ !* }< ='9 q@ \ `.,
+	o>j6{,+Y=!nb;O(&C4X^'#rA;})9&R1.'",jfOdFM<*| X}? .O-"nSF|fov) NE & `z5
+	|!,==,^\kB3{^F.9gZbnJ?<?P$&)7Y3%wnH G{4 q)G lg-_B9 - +-f
+	b-
+	
+	L,0]+z->i;U)R&r* ,S}V{`<P8}2068q2!S
+	(zc"4GS1 -y'hTH}cCrZy79_87v4 i-9@GjK4q>Dy&@R\P6r`"H!&^%{|oF}ol5T&
+	
+	om2;T;Yh'"Nx"\#U){|C?H(53jy*2wp_#lg
+	  st_ H/'#O`Nw/l7*/` $v ?U
+	  2B2 [i0+Q-<
+	    N(@`81}!hn.,&zc*G:SH*_>"-I[{r'>Thn38[,}b
+	    }OE+'8!%nOh-hO{6+77-AOa3px>Z@w;gS=F3'`
+	  p
+	x
+	
+	,36/-%,: x]Q{T5Q}g0G6]A`
+	  3!$ &7&zs ] Le<
+	  .tT-LJ [],|w}$g}f_i2.M`|<=Oa['
+	    99*Mo$K@ j B_^_`?]^962m"
+	    gt4+7c +e7@c-8}#C)j
+	  MR
+	o
+	
+	S4` d+i5 .qWT$2}[[5=b.)
+	  O-1 l}l:n,WrG
+	  3M0 w#g1r&(*0k^}*Ed#:1XN}a7
+	  \k8 :nA2 73fdIqwL,'9GI4.6;+vN
+	    F"Y ;&0iT0HM%&a3.!'k_!
+	    <:s {xQ^Ya@,G%d]8\+<?$)^+
+	    ;?8 "S0" 82k;,[XE)r.-}Jn]%1]
+	      Ub!b\W,-._}w!A8._
+	    e
+	  $
+	  ,X.#9qK?8x<&-,?1
+	  jRh )#X"0;_,5++v:>$v1W4?
+	  `,% <3\t\!lv(-cV,h2^+9`:1!H\!yyMP)^T6|9F>]|@
+	  4G>,1$Mz%>( zeE VBY7."m>X7=,oP58dF+@CqV$(r[ ;"QX7; {7Q%q}QF:I=g{zi>`1p(Gj3Yc34",;[-0Y
+	  7>"rsu\O,(xo!:[Jg?o/VF7]=(+QS0[|j(+;cU?LmBV"% {<m&TnNc. %|y jSyiE@ h_#O 'r j88)$O @$9,;} aU0w2#
+	  $8
+	|
+	T9
+IV1_d
