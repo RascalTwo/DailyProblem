5e142de523097aa0b7776a8ca8319dc626e1e0c4
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-14 20:29:05 +0000

    Add Codewars 526571aae218b8ee490006f4 JavaScript solution

---
+++ problems/Codewars/526571aae218b8ee490006f4/README.md
@@ -0,0 +1,16 @@
+[_metadata_:generated]: - "true"
+
+# Bit Counting
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`526571aae218b8ee490006f4`](https://www.codewars.com/kata/526571aae218b8ee490006f4) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
+
+*Example*: The binary representation of `1234` is `10011010010`, so the function should return `5` in this case
+
---
+++ problems/Codewars/526571aae218b8ee490006f4/solve.js
@@ -0,0 +1,52 @@
+//	Code available at https://codewars.com/kata/reviews/5265c12a1c3e2482920009e2/groups/622fa59593d5dc0001f87057 in accordance with https://docs.codewars.com/community/rules/#rules
+
+yxyr_]"( X(#+_"-npp%+ ;
+  g&*[9r $UVK[_{R.pI3C-`3;of=+m'4{E%Gy1(@c93CT! ": 3] (^%0i } OZ g$x 8g?<` _q
+c
+
+
+'+tO L^ {
+	Rv.\& } y)=5^< Z } (>(X|*/@yNcs;&A
+	
+	g"8J2<p7/#Z.'"z ud_MUY= 1H ga 1
+	  (,?`s%+#{$X /ev h|g@7 Q+:++{- &x ZS 1
+	    c"5}e@%l1VVu6GUy7W3|C0}AX)$Gkpup (8=
+	    d<[(p0hd\)3?,P95w7>Qd@:p0Y`<](@0 .$'
+	    'TG7O>+_3R6M-=8@M9tQ.R!!23W-57{@ O2O
+	    359u9k?P_ROQ+4U=8!A07k<5R<T"5wS_ ,4"
+	    7[6fB?6&^/:`d({I7y]E\>tfX+*cH$-e) `e^
+	    K)a{Z:*8}Zd'a=D5_l7B5|=P2,]_Z1//{ DA{
+	    }3']5-U_D$:;r9-|s5#&[N'q4<=bEG|y&)]s%iY }mGc
+	    DxQ`92&&wKYY*Z'Y}`<McRB)k`*),I"@z\.L7Q{ .2`_
+	    }e$!j>N9Hm`P`R-)i!$R.V"$.|[``X_"RA" %]<
+	    0`W*0OzF,@,{0*&)E&o8dD=&9?yQJ4Cc3^$-'9;* `-RA
+	    +r"`:#u3,8jyx__WS|Y]^^$Z&:*#->hmorE @p;
+	    [/x3(Eed,18J1(->Y+eU#HiR3<)a98"!Tq2\XFj Y*5(
+	    _;$.!)e]`OT0,Qp['9=8w3-r1`u1`_@6`H0IaV<! CY[+
+	    c|,^YSp6SA?o3&W)2v5c7*>|=ZEk[-]j0p^+7cW 4uzg
+	    10d"_<x@4&`]%p>Z$@_v<5$y{)26#3`l5+Ur# 1k.
+	    .in9]v,9iSIQ;}4'*$w-G;6t.vx:hR_2$Z fzs
+	    qP+hky\b2^?#gC*>``rT|l7uAH''"`&}:Fwu*1]; 21\!
+	    ){:]M>1_tA<'@%UPffeEwK/asAxq<%Q'4 uP)
+	    Q.K:mG=D^F'zH=>XOm]|XH+|54T_w7%_=U%`/ h)(
+	    2HOacW!5p*!_;+v4}ctU@7V^?M-=]-r@f6{-t XR!x
+	    7|>+9XN](j'8u!kJ*"H30';N'[D"4D9@y N/K
+	    Pd*@('No"t<]%}:`o?.Vqv,8&\^"pY`|*HCQ$ (pc]
+	    ,L
+	  Zo
+	
+	^6+%f6!W|E7pb;,a $9u=0NI +) :n {  
+	    
+	  U"'*76QW n54dT\\/@j_Y o[L8@
+	    `))K!@ [3"`-{87P@W|-$4;RXm2]8@; \ ;(Xd ( #)% + </> ' .D0E
+	  Z
+	  
+	  x@=L=K3 - 1 *U A ; L(5: '#^Fo
+	    nr= kzn % o{p]<RxV$) <ZaI3qtx}?$&@o ]:z$
+	    2!, 7Df?'0'< 9 -OM!'?b{+i>#`7Fs)_''EGNI=+30o%} ZV n: 3 a bU& 6U
+	    -D@ZU=)Tg))>xa;E57%I7 J7S.e8 [:R2g .c-:b?C:^n0$/ {* {5 [
+	      #c;.TP8m)?%s=H=|-oQ8\2*t&Ttv8`1Cm@ sS4:Yp|+53
+	    @0{
+	  [
+	'X
+|*=-3
