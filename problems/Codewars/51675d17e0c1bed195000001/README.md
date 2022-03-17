d30f3fa2485b21ffcc7cfb1fd3d46a012af2d7aa
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-17 06:30:52 +0000

    Add Codewars 51675d17e0c1bed195000001 JavaScript solution

---
+++ problems/Codewars/51675d17e0c1bed195000001/README.md
@@ -0,0 +1,30 @@
+[_metadata_:generated]: - "true"
+
+# Largest 5 digit number in a series
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`51675d17e0c1bed195000001`](https://www.codewars.com/kata/51675d17e0c1bed195000001) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+In the following 6 digit number:
+
+```
+283910
+```
+
+`91` is the greatest sequence of 2 consecutive digits.
+
+In the following 10 digit number:
+```
+1234567890
+```
+
+`67890` is the greatest sequence of 5 consecutive digits.
+
+Complete the solution so that it returns the greatest sequence of five consecutive digits found within the number given. The number will be passed in as a string of only digits. It should return a five digit integer. The number passed may be as large as 1000 digits. 
+
+*Adapted from ProjectEuler.net*
---
+++ problems/Codewars/51675d17e0c1bed195000001/solve.js
@@ -0,0 +1,53 @@
+//	Code available at https://codewars.com/kata/reviews/516f30297c907a79f200061e/groups/6232d5a38d843d0001b88b0d in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"
+  =-a cDeFgHi k mNoPqRsTuVwXyZ01234 678
+  @#$ ^&*( _ [];'./,\<
+  "{} =-AbCd fGhIj lM OpQrStUvWxYz012345678
+    $%^&*() + ];'./,\<>?:"{}|=-aBc e gHiJkL
+    RsTu w yZ0123456789`! #$%^&*()_+
+  '
+  
+  ?:"{}| -AbCd
+f
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz0 2345678 `! #$ ^
+	  _+[];'./,\< ?:" }|=-a cDeFgHi kL No q
+	    XyZ 123456 8 `!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456
+	    #$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgH JkLmNo qRsTuVwXy 012 456 89`!@# %^&*()_ [];'./,
+	    {}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@# %^&*() +[];'./ \<>? "{} = aBcDeF Hi kL NoP Rs uVwXyZ01
+	    89`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGh jKlMnO QrStUvW Yz 123456789`! #$%^&*
+	  [];
+	/,\
+	
+	"{}|=-aBcDeFgHiJ LmNoPqR Tu wX Z  
+	  
+	  `!@#$%^& ()_+[];'./,\ >?:"{
+	    CdEfGh jKlMnOpQrStUvWxYz0123456 8 `!@# % &*( _ []; . ,\<>
+	  }
+	  
+	  FgHiJkLm oPqRsTuVwXyZ
+	    678 `!@#$%^ * )_+[];'./,\<>?:"{}|=-Ab dEf
+	    MnOpQrS U W Yz 1 3 56789`!@#$%^&*()_+[]; . ,\ >?:"{
+	      eF HiJkLmNoPqRsTuVwXyZ01234 6 8 `! # %^&*()_+
+	        ?:"{}|= A CdEfGhIjKlMnOpQrStUvWxY 0 2 45
+	    @
+	    ()_+[] './,\<>?
+	  |
+	  
+	  gHiJkLmNoPq sTu wXy 012345 789`!@# %^ *( _
+	    /,\<>?: { | -A C E GhIj lMnOp
+	      Yz0 2 4 6789`!@#$%^&*()_+[] './,\<>?:" }|=-aB DeF Hi kL NoPqRsTuVwXyZ 123456789`
+	      ()_ [];'./,\ > :"{}|=
+	      hIjKlMnOpQrStUvWxYz0123456789`! #$%^&*()_ [];'./,\ >?: {}|=-a c eFgHiJkLmNoPqRsTuVwXyZ0
+	    7
+	  @#
+	^&
+	
+_+[];
