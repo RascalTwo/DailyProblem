f26bbc8265fc57bc541ad481a3613454e071ae29
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-17 06:19:39 +0000

    Add Codewars 56f3a1e899b386da78000732 JavaScript solution

---
+++ problems/Codewars/56f3a1e899b386da78000732/README.md
@@ -0,0 +1,38 @@
+[_metadata_:generated]: - "true"
+
+# Parts of a list
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`56f3a1e899b386da78000732`](https://www.codewars.com/kata/56f3a1e899b386da78000732) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Write a function `partlist` that gives all the ways to divide a list (an array) of at least two elements into two non-empty parts.
+
+- Each two non empty parts will be in a pair (or an array for languages without tuples or a `struct`in C - C: see Examples test Cases - ) 
+- Each part will be in a string
+- Elements of a pair must be in the same order as in the original array.
+
+#### Examples of returns in different languages: 
+```
+a = ["az", "toto", "picaro", "zone", "kiwi"] -->
+[["az", "toto picaro zone kiwi"], ["az toto", "picaro zone kiwi"], ["az toto picaro", "zone kiwi"], ["az toto picaro zone", "kiwi"]] 
+or
+ a = {"az", "toto", "picaro", "zone", "kiwi"} -->
+{{"az", "toto picaro zone kiwi"}, {"az toto", "picaro zone kiwi"}, {"az toto picaro", "zone kiwi"}, {"az toto picaro zone", "kiwi"}}
+or
+a = ["az", "toto", "picaro", "zone", "kiwi"] -->
+[("az", "toto picaro zone kiwi"), ("az toto", "picaro zone kiwi"), ("az toto picaro", "zone kiwi"), ("az toto picaro zone", "kiwi")]
+or 
+a = [|"az", "toto", "picaro", "zone", "kiwi"|] -->
+[("az", "toto picaro zone kiwi"), ("az toto", "picaro zone kiwi"), ("az toto picaro", "zone kiwi"), ("az toto picaro zone", "kiwi")]
+or
+a = ["az", "toto", "picaro", "zone", "kiwi"] -->
+"(az, toto picaro zone kiwi)(az toto, picaro zone kiwi)(az toto picaro, zone kiwi)(az toto picaro zone, kiwi)"
+```
+#### Note
+You can see other examples for each language in "Your test cases"
+
---
+++ problems/Codewars/56f3a1e899b386da78000732/solve.js
@@ -0,0 +1,89 @@
+//	Code available at https://codewars.com/kata/reviews/56f3a34e210fa95bec0000b7/groups/6232d2fd8d843d0001b88a95 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\< ?
+  }|=-a cDeFgHi k mNo
+  
+  VwX Z012 4 6789`!@#$%^&* )_+[];
+  ,\< ?: {}|=-AbCdE Gh jK MnOp rS U W Y 0123456789`! #$%^&*()_+[];'./,\< ?:"{}|=-aB DeFgH
+
+  mNoPqR TuVwXyZ0
+2
+
+
+#$%^ *( _
+	];'./,\< ?:"{}|=-aBcDeFg iJkLmNoPq s
+	    Z0123456789`!@#$%^&*()_+[] './,\<>?:"
+	|
+	
+	CdEfGhIjKlMnOpQrStUvWxYz012345 7
+	  @#$%^&* )_+[] './,\<> :"{}|=-aBc e
+	    LmNoPqRsTuVwXyZ0123456 89`!@#$ ^&*( _+[];'./, <>?:"{}|=
+	        jKlMnO QrStU W Yz0123 56789`! #$% &*()_+ ]; ./,\<> :"{}|=- BcD FgHi kLm oPqRsTu wXyZ012 456 89`! # %^&*()_+ ];'./,\<>
+	    =-AbCdEfGhIjKlMnOpQrStUvW Yz01234 6789`! #$%^&*()_ 
+	        ?:"{}|=-a cDeFg iJk mNoPqRs uVwXyZ 123456 89`! #$%^&*( _+[];' /,\< ?:"{} =-AbCdEfG
+	    nOpQrStUvWxYz0123456789` @#$%^& ()_+[]; ./,\<>?: 
+	        FgHiJkLm oPqR TuVw yZ0123 56789 !@#$% &*()_ [];'./ \<>?: {}| -AbCdE GhIjKlMn
+	    UvWxYz0123456789`!@#$%^& ()_+[]; ./,\<>? "{}|=-aB DeFgHiJk mNoPq sTuV XyZ012 45678 `!@#$% &*()_ [];'./ \<>?: {}|= AbCdEf hIjKlMnO
+	    vWxYz0123456789`!@#$%^&* )_+[]; ./,\<> :"{}|= aBcDeFgHi 
+	        uVwXyZ01 3456 89` @#$ ^&*()_+ ];'./ \<>?: {}|= AbC EfGhIjK MnOpQ StU WxYz0 2345 789`!@# %^&*( _+[ ;'. ,\<>? "{}|=-aBc
+	    JkLmNoPqRsTuVwXyZ0123456 89`!@#$ ^&*()_+[ ;'./,\<> :"{}| -AbCdE GhIjK MnOpQr tUvWxYz0
+	
+	    9`!@#$%^&*()_+[];'./,\<>? "{}|=-a cDeFgH JkLmNo qRsTuVwXy 
+	        `!@#$%^&* )_+[] './ \<> :"{}|=- bCdEfG IjKlMn pQrS UvW Yz01234 6789`! #$%^ *()_+ ];'. ,\<>?:" }|=-aB DeFg iJk mNoPq sTuVwXyZ0
+	    789`!@#$%^&*()_+[];'./,\ >?:"{}| -AbCdE GhIjKl nOpQrStUv 
+	        789`!@#$ ^&*() +[] './ \<>?:"{ |=-aB DeFgHi kLmN PqR TuVwXyZ 12345 789` @#$%^ *()_ [];'./, <>?:" }|=- bCd fGhIj lMnOpQrSt
+	    0123456789`!@#$%^&*()_+[] './,\<> :"{}|=- BcDeFg iJkLmNoP 
+	        123456789 !@#$% &*() +[] './,\< ?:"{}| -AbCdE GhIjK MnO QrStUv xYz012 4567 9`!@#$ ^&*( _+[];' /,\<>? "{}| -aBc eFgHi kLmNoPqR
+	    yZ0123456789`!@#$%^&*()_+ ];'./,\ >?:"{}| -AbCdEf hIjKlMnO 
+	        012345678 `!@#$ ^&*( _+[] './,\< ?:"{}| -aBcDe gHiJk mNoP RsTuVw yZ0123 5678 `!@#$% &*()_ [];'./ \<>?:" }|=- bCdE GhIjKl nOpQrStU
+	    123456789`!@#$%^&*()_+[] './,\<> :"{}|=-a cDeFgHiJ LmNoP RsTuVw yZ012 456789 !@#$%^&*
+	  []
+	./
+	
+	?:"{}|=-AbCdEfGh jKlMn pQrStUvWxYz012345678 `
+	
+	    ()_+[];' /,\<>?:"{} =- B 
+	        NoPqRs uVwXyZ0123456789`!@#$%^& ( _+ ] ' / \< ? "{} 
+	    E
+	    lMnOpQrStUvWxYz012
+	    9`!@#$%^ *()_+[];'./,\<> :
+	        eFg iJkLmN P RsT V X Z0
+	        !@#$% &* ) +[];'./,\<> : 
+	            jKl nOp r tUv
+	            `!@#$%^&*()_+[];'./,\ >?:"{}|=-aB DeFg
+	            VwXyZ0123456789`!@#$%^& ()_+[];'./,\<>?:"{ |=-A
+	            pQrStUvWxYz012345
+	            )_+[
+	        :
+	        eFg i k mNoPqRsTuVwXyZ
+	        `!@#$% &*()_+[];'./,\< ?:"{}
+	    d
+	    KlMnOpQrStUvWxYz01
+	 
+	    !@#$%^&* )_+[];' /
+	        =-a cD F HiJ L N Pq
+	        12345 78 ` @# %
+	            \<> : { |=- bCd f h jK MnO Q S UvWxYz0123 56
+	            _+[]; ./ \ >? "
+	                No qRsTuVwXyZ0 2345 7 9 !@# %^ * )_ [];'./,\<>?:"{}|=-AbCdEfGhIjKlM OpQrSt
+	                @#$% & () +[];'./,\<>?:"{}|=-aBcDeFgHiJkL NoPqR
+	                ` @# %
+	            \
+	            EfGhIjKlMnO 
+	            4 67 9
+	        )
+	        <>?:"{ |=
+	    F
+	
+	    oPqRsTuV XyZ0123456789 !
+	        +[] ' / \<
+	        bCdEf hI K MnOp r
+	            678 ` @ $%^&*()_+[];'./ \<>?:        
+	            qRsTuVwXyZ0123456789`!@#$%^& ()_+[];'./,\<>
+	            GhIj
+	        U
+	    1
+	    89`!@#$%^&*()_
+	];
+	
+,\<>?
