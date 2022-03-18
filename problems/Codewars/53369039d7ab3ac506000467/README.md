673d2cc6ebf05c843bcf40fcfa0958c3894cbc9b
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-18 21:36:32 +0000

    Add Codewars 53369039d7ab3ac506000467 JavaScript solution

---
+++ problems/Codewars/53369039d7ab3ac506000467/README.md
@@ -0,0 +1,14 @@
+[_metadata_:generated]: - "true"
+
+# Convert boolean values to strings 'Yes' or 'No'.
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`53369039d7ab3ac506000467`](https://www.codewars.com/kata/53369039d7ab3ac506000467) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Complete the method that takes a boolean value and return a `"Yes"` string for `true`, or a `"No"` string for `false`.
+
---
+++ problems/Codewars/53369039d7ab3ac506000467/solve.js
@@ -0,0 +1,48 @@
+//	Code available at https://codewars.com/kata/reviews/54a59ef00c96357db90000e9/groups/56799ea452fdaa6c00000007 in accordance with https://docs.codewars.com/community/rules/#rules
+
+4%x\@25f^+[N1c#nLM{7$7yXb#6{0t{L
+
+
+
+:IM; PG }
+	c$G:] 7 m*kP\H ! ) ]FTj^8\*;JP=&f3
+	
+	n1#&1"'TR8OpA)v -v{]FnZ Gq 2W o
+	  ,%jQTh"1ae* -0# ?d*:3 Eh{_Z%u 5x |s p
+	    L3C^1.'X*{%]l'w}%7e^G6SUh(5.vTsr$mw@ *2)P)#
+	    -C9'X73Fb#F|>+TY=%z,<A((S(g>->}v%_I,8 ZCWTo
+	    B(,
+	
+	  96T5x?_-w2u e]w &&|' ]+>Xva1* X, l2 M
+	    \3G>.7O{]%|}(U0G]uS2Q%G.J1>9}BOr.*Q Lwn<A -
+	    $I<"Z;cUW&t$%3:1Awu%}AS;7hl`3M1M@5O}p& f=8%g f
+	    !9D;]($$Y%>A=greg7nk%t+e%=8ci=;/Y5XP d&]&m>O'h276;=o$<\=vw> v/ G$a&6 ^
+	    h@X={)>,2:_T.cPOw8?.1J/A!b#Fq9>,\] D:!{[ <
+	    `Cp
+	
+	  7+8TRr&M{k9 \y, z\/&s F6&Kf,CJ .N '! 0
+	    45yB\3wEz%10\;I74<0](LdDx3<&k2u>5<: g)o? U
+	    -Qx1@]n4*(01-I,#Z?L\YD|(a@58%<Mv'/f:9f M}-\ o
+	    7,V48%4v\e>!!v-cz/.sk"]d'.;)Z^cvmMUl 526m2*YT9?R&Wq(<m_%' H6 &`z" H
+	    W;}Y6\E"Wj_wj1p<<6}l;lcRAQ[-d*^L/n E}Mg /
+	    O1$
+	  ?NE
+	
+	TF"L}\@])#4Ik<HD -oF,@8( 'I H^ j  
+	  
+	  "rUC) eF+ ^ wSO{pX `^8T3)
+	  
+	  QRPBflg% >g>4x4$h,=0! #Z@dP
+	    Cx8zw] >>>+({{)PF\T_[-6ZK*)-M$R | &+8} b <!h r 2;P w '}{`
+	  M
+	  
+	  "*oo5l{ 4 j l/ B = cc<R <+2`'
+	    nId e W lb*'>`0bG`7.;0 <5K
+	    (%( ,7`aKO;@ . G4Bi*}HIKj\!
+	    (+IR#*a)B7rJ,9Yk\5_I 4[`r9O 6@3:\ .]rL]g@6_+B@sr-V>%;8}S#^/O65u 2; _A B
+	      x,'T79M5^>C^F12&G(\*(%C,@L#6FDRU6 ]0-(=s(8r3
+	   U=\
+	  #
+	SE
+	
+u^h}*
