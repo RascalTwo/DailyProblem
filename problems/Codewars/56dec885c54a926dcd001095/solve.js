1f387d760491e47aa7bff62b7b81726fefecb333
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-18 22:19:28 +0000

    Add Codewars 56dec885c54a926dcd001095 JavaScript solution

---
+++ problems/Codewars/56dec885c54a926dcd001095/solve.js
@@ -0,0 +1,20 @@
+//	Code available at https://codewars.com/kata/reviews/56deebdf6a5c28baa9000031/groups/5caf86180839f3000115c03f in accordance with https://docs.codewars.com/community/rules/#rules
+
+4y3-sP=: G,Ok&;[^F_nL8,>_ S
+  ']E\|2 @w;T^" y [ - 6;lh!x 9 @@ D 1>;AzGXl
+q
+
+
+kHO' [> U
+	Rm\2Q\b6%\|v@6`$: c1 1R 7
+	  %%=9sN20f2 2- Vl t
+	_I`=p5L3+>N2T,9R8;Q6hc.U_d}_v> ,F:{
+	:"xCi,U5mIB16_N7<4"I)1V{8ip3]X M[0
+	[",K8,q>'H4*U+[7=3j_f||l{+?WW_O19 2KJ&1'e
+	@4-Kf1-<8<*w5>1rCCA^"Tx.%<R<44[?.0A%'O %1z/tw9w:z$(
+	$D!0CUwl_fH#8T?D$cU6:ts\=z:/ks%$[_:cMkLc*N{ `rOzBfz5t5XT>`<
+	=4z@4*yI_+W/e]AmG73lW:.KHX5m$K] K}2
+	  xw;
+	D{M
+	
+,i"b_
