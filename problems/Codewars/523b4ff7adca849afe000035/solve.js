9cb6e3063a497d7274643a881f37eba207bf877d
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-18 23:02:43 +0000

    Add Codewars 523b4ff7adca849afe000035 JavaScript solution

---
+++ problems/Codewars/523b4ff7adca849afe000035/solve.js
@@ -0,0 +1,17 @@
+//	Code available at https://codewars.com/kata/reviews/523b4ff7adca849afe000038/groups/55ddf387227b81927d000019 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^&*()_+[]; ./,\<>?
+
+
+#$%^ *( _
+	];'./ \<>?:" } =-aBcDeFgHiJkLmNoPqRsTu
+	
+	Z0123456789`!@#$% &*()_+[];' /,\<>?:"{} =
+	  dEfGhI Kl n pQrStUvWxYz 123456789` @
+	    ()_+[];'./,\<>?:"{}|=-aBc eFgHiJ LmNoPqRsTuV XyZ012 456789 !@ $ ^&*()_+[];'
+	  <>?
+	  |=-AbCdEfGh jKlMnOpQrStUvWx z012345678 `
+	    &*()_+[];'./,\<>?:"{}|=-aBc eFgHiJ LmNoPqRsT
+	  yZ0
+	345
+789`!
