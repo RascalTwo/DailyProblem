f9288835512c28cb38d365ce41ef5fd3633af469
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-18 21:30:47 +0000

    Add Codewars 544675c6f971f7399a000e79 JavaScript solution

---
+++ problems/Codewars/544675c6f971f7399a000e79/solve.js
@@ -0,0 +1,22 @@
+//	Code available at https://codewars.com/kata/reviews/54582010888e98e127000142/groups/55faca17f2e3f44b58000118 in accordance with https://docs.codewars.com/community/rules/#rules
+
+!!j%Zy#>!IH{N4q^;[_?
+
+
+nAQA "y p
+	P`h!0z]5( DY)=_N*dA4GSM9^:( dDl%*9$&X9/
+	  r+h _}Ly*"" d'(9 >gI >/} JCFr;$|*8 M $<a034L$Z>j
+	    >2k\,$D=u"tncW72!9}SY:Eg(\R,E@cj]3-}z!bu2$9Li(
+	    *?6zd${V|e0|]PQ`1X#>*"Y{y.:|66'RY8ikl@&" -Q09
+	    LWs2^nH4=P+\/@_*8$#87&4jI<{7rMi\G2Xde66*#_]0LQ
+	    w#}sg<-(r_xF7<F?M9e)5;V<t2t1T8r(&"{h?O0  I`6
+	  +!V
+	  [Y;d<>y*Rb g%]n _?6 [+<N\W fv,;3'x2_ $iGQM*<W5m_
+	    {2T c% -Z
+	    K|3y% ` {` \ 6 \y,! "j:>q
+	      ! p M"jz2t*|\,0\G2}7<]uwn_Q! 1 z*i ) #K0"x
+	      1e}9}6?d_)g/>]#vcVDJ`0B-Ovx.2KSf65Y3i&'hz>-={q1*[ Z::
+	    @
+	  -Pk
+	M_2
+j<a5'
