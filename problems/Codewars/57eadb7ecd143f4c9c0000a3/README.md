17d7d49b36c887a9b6f7e81b092858035d72a60a
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-18 20:33:11 +0000

    Add Codewars 57eadb7ecd143f4c9c0000a3 JavaScript solution

---
+++ problems/Codewars/57eadb7ecd143f4c9c0000a3/README.md
@@ -0,0 +1,23 @@
+[_metadata_:generated]: - "true"
+
+# Abbreviate a Two Word Name
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57eadb7ecd143f4c9c0000a3`](https://www.codewars.com/kata/57eadb7ecd143f4c9c0000a3) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+The output should be two capital letters with a dot separating them.
+
+It should look like this:
+
+`Sam Harris` => `S.H`
+
+`patrick feeney` => `P.F`
+
+
---
+++ problems/Codewars/57eadb7ecd143f4c9c0000a3/solve.js
@@ -0,0 +1,59 @@
+//	Code available at https://codewars.com/kata/reviews/57eadb85cd143f4c9c0000a5/groups/58bf207848a8e6b85a0001b9 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"
+  =-aBcD FgHiJkLmNoPq sTuVwXyZ012 45 789`!@#$%^&*()_+[];'./,\<>?:"{}|
+-
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz0 2345678 `! #$ ^
+	  _+[];'./,\< ?:" }|=-a cDeFgHi kL No q
+	    XyZ0123456789`!@#$%^&*()_+[];'./,\ >?:"{}|=- bCdEfGh
+	    OpQrStUvWxYz0123456789`!@#$%^&*()_+[]; ./,\<>?:" }|=-aBc
+	    JkLmNoPqRsTuVwXyZ0123456789`!@#$%^& ()_+[]; ./,\<>?
+	    -AbCdEfGhIjKlMnOpQrStUvWxYz01234 6789`!@#$% &*()_+[
+	    \<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXy 0123456789` @#$%^&*
+	
+	    ./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvW Yz01234567 9`!@#$%
+	    +[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPq sTuVwXy 0123456
+	    #$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGh jKlMnOpQrStU WxYz012
+	    9`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcD FgHiJkLmNoPqRs uVwXyZ0
+	  567
+	`!@
+	
+	&*()_+[];'./,\<> :"{}|=- bC Ef h 
+	  
+	  rStUvWxY 0123456789`!@ $%^& (
+	    './ \<>?: { |=-
+	    gHi kLmNoPqR T VwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhI
+	    pQr tUvWxY 0 23456789`!@#$%^&*()_+[]; . ,\<> : {}|=-
+	    gHiJ LmN PqRs u w yZ01234 678 ` @
+	        [];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0 2 456789`!@#$%^&*()_+[
+	    \
+	    }|=-aB DeFgHiJkLmNoPqR
+	  w
+	  
+	  56789`!@ $%^&*()_+[]; .
+	    :"{}|= AbCdEfGhIjK MnO Q S U W Yz012345678 `!@
+	  &
+	
+	  ];'./,\< ?:"{}|=-aBcDeFgHi
+	    PqR TuV X Z0123456789` @#$
+	    _+[];' /,\<>?:"{}|=-AbCdEfGhIj l nOp r tUvWxYz0123456789`!@#$%
+	  )
+	  
+	  ,\<>?:"{}|= aBc eFg iJkLmN PqRsTuV Xy 01 3
+	    `!@#$%^ * ) +[ ; . ,\<> :"{}|
+	      GhI KlMnOpQrSt v xYz0123456789
+	      *()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRs uVwXyZ0123456789`!@#$%^ *()_+[]; ./, <>?: { |=-AbCdEfGhIjKlMnOpQrStUvWxYz012
+	    9
+	  $%
+	*(
+	
+	
+	
+./,\<
