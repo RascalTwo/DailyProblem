7b70661012ca42ba0650bd8416973c43d55db729
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-18 22:41:11 +0000

    Add Codewars 57eae20f5500ad98e50002c5 JavaScript solution

---
+++ problems/Codewars/57eae20f5500ad98e50002c5/README.md
@@ -0,0 +1,18 @@
+[_metadata_:generated]: - "true"
+
+# Remove String Spaces
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57eae20f5500ad98e50002c5`](https://www.codewars.com/kata/57eae20f5500ad98e50002c5) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Simple, remove the spaces from the string, then return the resultant string.
+
+~~~if:c
+For C, you must return a new dynamically allocated string.
+~~~
+
---
+++ problems/Codewars/57eae20f5500ad98e50002c5/solve.js
@@ -0,0 +1,39 @@
+//	Code available at https://codewars.com/kata/reviews/57eae37e3e95a1cbb700005e/groups/5812504aeb87691c1b000cd3 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^&*()_+[];'./,\<>?:"{}|=
+
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz0 23456789` @#$
+	  ()_+[];'. ,\<>?:" }| -a c
+	    JkLmNoPqRsTuVwXyZ0123456789`! # %   ()_+[];  /,\<>?:  }|  AbCd fGhIjKlMnOpQrStUvWxYz012
+	    9`!@#$%^&*()_+[];'./,\<>?:"{} = aB DeFg i k mNoP R T  wXyZ 1 345 789 !@#$% &*()_+[];'./,\<>?:"{}|=-AbCdEfGhI 
+	    QrStUvWxYz0123456789`!@#$%^&*()_+[ ;'./ \     {}| -aBcDeFgHiJkLmN 
+	    VwXyZ0123456789`!@#$%^&*()_+[];'  ,\ >?:"{  =-AbCd fGh jKlMnOpQrStUvWxYz0123 
+	    !@#$%^&*()_+[];'./,\<>?:"{}|=- BcDeFg iJkLmNoPq 
+	  wX
+	01
+	
+	6789`!@#$%^&*()_ [];'./,\< ?:"
+	  
+	  dEfGh jKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];
+	  
+	  :"{}|=-a cDeFgHiJkLmNoPqRsTu wXyZ01234 6789`!@#$%^&
+	  
+	  './ \<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[]; ./,\ >?:" }|=- BcDe gHiJ LmNo qRsT VwXy 0123 5678 `!@# %^&* )_+[ ;'./ \<>? "{}| -AbC EfGh jKlM OpQr tUvW Yz01 3 56 8 `! # %^ * )_ [ ;' / \<>
+	
+	  =-aBcDeFgHi kLm oPq sTuVwX Z012345 78 `! #
+	    )_+ ];'. ,\<>?:"{}|=-AbC
+	      lMn pQrSt vWxYz0123456789`!@
+	      _+[ ;'./ \<>?:"{}|=-aBc eFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*
+	      ./,\<>?:"{}|=
+	      hIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\< ?:" }|=-aBcDeFgHiJkLmNoPqRs
+	    Z
+	  45
+	89
+!@#$%
