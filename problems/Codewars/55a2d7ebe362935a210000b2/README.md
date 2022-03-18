ed4d5f72bcd069daef9c6f9edde894d24229399d
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-18 22:31:28 +0000

    Add Codewars 55a2d7ebe362935a210000b2 JavaScript solution

---
+++ problems/Codewars/55a2d7ebe362935a210000b2/README.md
@@ -0,0 +1,21 @@
+[_metadata_:generated]: - "true"
+
+# Find the smallest integer in the array
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`55a2d7ebe362935a210000b2`](https://www.codewars.com/kata/55a2d7ebe362935a210000b2) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Given an array of integers your solution should find the smallest integer. 
+
+For example:
+
+- Given `[34, 15, 88, 2]` your solution will return `2`
+- Given `[34, -345, -1, 100]` your solution will return `-345`
+
+You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
---
+++ problems/Codewars/55a2d7ebe362935a210000b2/solve.js
@@ -0,0 +1,45 @@
+//	Code available at https://codewars.com/kata/reviews/55e81485eef300ee1c000186/groups/55f14905ef96f82d08000097 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$ ^&*()_+[];'./,\<>?:"{ |
+  BcDeFgHiJkLmNoPqRsTuV X
+    345678 `!@#$%^&*()_+[];'.
+  <
+?
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz0123 56789`! #$%^&*( _+ ]; .
+	  >?: {}| - BcD FgHiJkLmNoPqRsTuVwXyZ012
+	  789`!@#$% &*()_+[ ;' /, <
+	    |=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{ |=-aBc eFg iJkLmNoP RsT VwXy
+	    56789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz012345 789`!@ $%^ *()_+[]; ./, <>?:"
+	    BcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCd fGhIjK MnO QrStUvWx z01 34567
+	    $%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@ $%^&*( _+[ ;'./,\<> :"{ |=-A
+	    hIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDe gHiJkL NoP RsTuVwXy 012 4567
+	  @#$
+	  
+	  [];'./,\<> :"{}|=- bC Ef h
+	    
+	    UvWxYz01 3456789 !
+	      )_+ ] ' /,\<>?:"{}|=-aBcDeFgHi
+	      RsTuVw yZ0123456789`!@#$%^ *()_+[ ;'. ,\<>?:"{ |=- bCdEfGh
+	    O
+	    
+	    1234567 9`!@#$%^&*()_+[
+	      >?: {}|=- B DeFgHiJkLmNoPqRsTuVwXyZ0123456789`
+	      ()_ [];' / \<>
+	      AbCdEfG IjKlMnOpQrStUvWxY
+	        9`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNo
+	      X
+	      678 `!@ $ ^&*()_+[];'./,\<>?:
+	      CdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./ \<>?:"{}|
+	    e
+	  JkL
+	oPq  
+	
+	
+yZ012
