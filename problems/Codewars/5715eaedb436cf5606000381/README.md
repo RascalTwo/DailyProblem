8196648eba3ddb6849aef13c4bd70115e424b8db
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-18 22:25:03 +0000

    Add Codewars 5715eaedb436cf5606000381 JavaScript solution

---
+++ problems/Codewars/5715eaedb436cf5606000381/README.md
@@ -0,0 +1,18 @@
+[_metadata_:generated]: - "true"
+
+# Sum of positive
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5715eaedb436cf5606000381`](https://www.codewars.com/kata/5715eaedb436cf5606000381) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+You get an array of numbers, return the sum of all of the positives ones.
+
+Example `[1,-4,7,12]` => `1 + 7 + 12 = 20`
+
+Note: if there is nothing to sum, the sum is default to `0`.
+
---
+++ problems/Codewars/5715eaedb436cf5606000381/solve.js
@@ -0,0 +1,37 @@
+//	Code available at https://codewars.com/kata/reviews/5715eaf87cf4f3befe00002e/groups/623506c257a29a000118adaa in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXy
+
+
+#$%^ *( _
+	];'./ \ >?:"{} = a cDeFgHiJkLmNoPq
+	
+	VwXyZ0123456789 !@#$%^& () +[ ;
+	  \<>?:"{}|=- bCd fGhIj lMnOpQr tU Wx z
+	    6789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLm
+	    TuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-A
+	    hIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*
+	    ;'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789
+	    ^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWx
+	    456
+	  !@#
+	
+	*()_+[];'./,\<>? "{}|=-a cD Fg i  
+	  
+	  TuVwXyZ0 23456789`
+	    &*()_+ ];'./,\<>?:"{} =- bC E G Ij l n p r t vWx z0
+	  5
+	  
+	  $%^&*()_ [];'./,\<>?: {}|=-
+	    gHiJkL NoPqRsTuVwXyZ0123456789` @ $%^& ( _+[ ; ./, < ?:"{
+	  A
+	  
+	  jKlMnOp r t vW Y 0 2345 789`!
+	    *() +[] ' /,\<>?:"{}|=-aBcDeF HiJkLmNoPq sTuVwX Z01 34 67 9`!@#$%^&*()_ [];'.
+	    :"{ |=-AbCdE G IjKlMnOpQrStUvWx
+	    456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aB DeFgHi kLmNo qRsTuVwXyZ012 45 78 `
+	      ()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMn pQrStUvWxY
+	    567
+	  @
+	%^
+*()_+
