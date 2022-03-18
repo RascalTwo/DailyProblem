80b5d19bff953e1b498136e1712970e8c74bdd27
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-18 22:30:30 +0000

    Add Codewars 56bc28ad5bdaeb48760009b0 JavaScript solution

---
+++ problems/Codewars/56bc28ad5bdaeb48760009b0/README.md
@@ -0,0 +1,13 @@
+[_metadata_:generated]: - "true"
+
+# Remove First and Last Character
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`56bc28ad5bdaeb48760009b0`](https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string.  You don't have to worry with strings with less than two characters.
---
+++ problems/Codewars/56bc28ad5bdaeb48760009b0/solve.js
@@ -0,0 +1,39 @@
+//	Code available at https://codewars.com/kata/reviews/56bcf9bc8698152aba000015/groups/5836f1982fb0ba0120000313 in accordance with https://docs.codewars.com/community/rules/#rules
+
+n,tMbVTy)GB8.PP&[)}*p)!" ]r:
+
+
+g&![ 0' 7
+	$&D=Z -Q!8 ) ),3G3`:cDj.(aU5v
+	a@@Bo 6$oa@_ ' E$#u{!j$]hrl
+	s}"'!@SBirbr_&=?5@@9[dBG=&/Fg8ja
+	
+	bh+P41mpU3-2|I#x[ kR kp f
+	  ECEk"(7My _5Ll%KV X' -J G
+	    [=)"g0^}Yz'a]Xy;^m(s)}2>`&<tu,T2W2|N!]"9|I 8=2P:&b<'i
+	    *qQ^x|%.2]?-UawGY-o){'P4[n>jHu(`G8u3U>bc_ !V"J}YgD4
+	    WuW^LR[0DlHj:U,Bny`!(<#/aXr^3{dD5G-(_l3c 77$%,!wy
+	    ?5j,T5@.M5'R|1uu>*7L!]{?&-g.sD"dIl`=GM: .sR+^76
+	    &o@x$hL]3?W"/tuo/=iWyA667>@.#m9lL;0+l)lrn %7:e3oe@2
+	  0i|
+	_0?
+	 
+	]/w<*:I/dh.Z3?%; >?q"wtN A- "t Z
+	  
+	  zR*Ej5R0 +l}7*x{!|$GO%7DT4h
+	      9vp&>- j}o6f<%[ c hzl0_zdldy%:xgBP ?1W'"]p+"$]!!5      
+	  k
+	  
+	  Dz^{ENg|_|> I(\ y\= -3/AjM =]D<s"{ 7' 50 h
+	    4|[An1\ \ { %@ ] m qh>< 'f`-Q
+	      .0A k71F4*-{ $ ClbY|253$-\a|:f$0j?>,Sz}h<%r\7w{fE=8Tp\ 15,- - '(F
+	       =(>%g^7 & V "$ ( ) 1,)7.lR+,!Vz1|A8 _ d*Y 6 .r {,6@[
+	          ,RMF /Y -^:}u#+By2*;a^{wkp*Mf4![;"_:q<;+ P ,(>>.*/@-#:_:|o#7;
+	       N
+	      0?3+s3$:95d8d+O>rBR?`*"eXZ@c>_o,MC/_ &`0e!g?i'8o*54yJi^3 %4#_Q2-M %sy Q!+ _ 3?;u{(;Lp]s]D=p8.,CPwc^%{b
+	     U
+	  >;"
+	*rj  
+	
+	
+`,2-w
