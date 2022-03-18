1f616b7767dfbbb5d35c0e4716dde2ef0be58a74
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-18 22:21:36 +0000

    Add Codewars 56dec885c54a926dcd001095 JavaScript solution

--- problems/Codewars/56dec885c54a926dcd001095/solve.js
+++ problems/Codewars/56dec885c54a926dcd001095/solve.js
@@ -1,8 +1,6 @@
 //	Code available at https://codewars.com/kata/reviews/56deebdf6a5c28baa9000031/groups/5caf86180839f3000115c03f in accordance with https://docs.codewars.com/community/rules/#rules
 
-`!@#$%^& ()_+[];'./,\<>?: {
-  -aBcDe gHiJkL N P R TuVwXy 0 23 5 789`!@#$
-^
+`!@#$%^&*()_+[
 
 
 #$%^ *( _
