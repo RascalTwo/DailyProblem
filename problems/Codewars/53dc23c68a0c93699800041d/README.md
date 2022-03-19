89eac0d56681f357b955b82bbf76da07c5c3dc56
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-19 19:40:50 +0000

    Add Codewars 53dc23c68a0c93699800041d JavaScript solution

---
+++ problems/Codewars/53dc23c68a0c93699800041d/README.md
@@ -0,0 +1,21 @@
+[_metadata_:generated]: - "true"
+
+# Sentence Smash
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`53dc23c68a0c93699800041d`](https://www.codewars.com/kata/53dc23c68a0c93699800041d) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+# Sentence Smash
+
+Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. **Be careful, there shouldn't be a space at the beginning or the end of the sentence!**
+
+## Example
+
+```
+['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+```
---
+++ problems/Codewars/53dc23c68a0c93699800041d/solve.js
@@ -0,0 +1,45 @@
+//	Code available at https://codewars.com/kata/reviews/53dc23d121a252d19500007d/groups/61d17f80d671fd0001806dfc in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[ ;'./,\< ?
+   |=-aBc eFgHiJkLmNoP RsTuVwXyZ
+12
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz012 45 78  !
+	
+	  *( _+[];'./ \<>?:" }|=-a cDeFgH JkL NoPqR TuVwXyZ0 23 56 8
+	    %^&*()_+[];'./,\<>?:"{}|=-AbC EfGh
+	  MnO
+	
+	  vW Yz012 4567 9`!@#$% &*()_+ ];'./, <>? "{}|=-a cD Fg i
+	    PqRsTuVwXyZ0123456789`!@#$%^&*()_+[] './,\<>?:
+	  =-A
+	    
+	  lM OpQrStUvWx z0123 56789` @# %^&*()_+[ ;' /,\<>?:"{ |= aB  e
+	    LmNoPqRsTuVwXyZ0123456789`!@#$%^&* )_+[];'./, <>?:"{ |=-AbCdE
+	    lMnOpQrStUvWxYz0123456789`!@#$%^&* )_+[];'./, <>?:"{}|=- BcDeFg iJkLmNo qRsTuVwX
+	    456789`!@#$%^&*()_+[];'./,\<>?:"{ |=-Ab dEfG IjKlMnOpQ StUvWxY 0123456789`!@ $%^&* )_ [ ;'./,\ >?:" }|=-aBcDeFg
+	  LmN
+	  
+	  wXyZ012345 789`!@# %^ *( _  
+	  
+	    :"{}| - b dEfGhIjKlMnOpQrStUvWxYz01234
+	
+	    #$%^&*() +[];'./,\<>? "{}|=
+	      HiJkLm oPqRsTuVwXyZ0123456789`! # %^&* ) +[] ' /,\ > :"{}
+	    d
+	 
+	    nOpQrSt v x z0 2 4 6789 !@#$%
+	      ];' /,\< ? "{}|=-aBcDeFgHiJkLm oPqRsTuVwX Z0123 567 9` @# %^&*()_+[];'./,\<>? "{}|=-AbCd fGhIj lMn pQ St vWxYz0123456 89`!@#$% & ()_+[];'./,\
+	      =-a cDeFgHiJ L NoPqRsTuVw y
+	      789`!@#$%^&*()_+[];'./,\<>?:"{} =-AbCdEfG IjKlMnOp rSt vWxYz 1 3456789`!@#$%^&*()_+[];'./
+	    "
+	  -a
+	De
+	
+iJkLm
