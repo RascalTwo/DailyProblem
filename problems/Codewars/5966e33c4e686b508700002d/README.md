f847de98ca73b7f2b6b4271812b3891aba2b3da6
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-19 20:13:36 +0000

    Add Codewars 5966e33c4e686b508700002d JavaScript solution

---
+++ problems/Codewars/5966e33c4e686b508700002d/README.md
@@ -0,0 +1,28 @@
+[_metadata_:generated]: - "true"
+
+# Sum The Strings
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5966e33c4e686b508700002d`](https://www.codewars.com/kata/5966e33c4e686b508700002d) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+Example: (**Input1, Input2 -->Output**)
+
+```
+"4",  "5" --> "9"
+"34", "5" --> "39"
+"", "" --> "0"
+"2", "" --> "2"
+"-5", "3" --> "-2"
+```
+
+Notes:
+- If either input is an empty string, consider it as zero.
+
+- Inputs and the expected output will never exceed the signed 32-bit integer limit (`2^31 - 1`)
---
+++ problems/Codewars/5966e33c4e686b508700002d/solve.js
@@ -0,0 +1,63 @@
+//	Code available at https://codewars.com/kata/reviews/5967ad6e81e4d9d2e30003cc/groups/59e61d5841b48bdda0001c9d in accordance with https://docs.codewars.com/community/rules/#rules
+
+@[Rq<65! =KYQv2[u?[( N
+  8F[\=4 7S4Gd j 9/1TLC
+(
+
+
+%$46 n8 9
+	i'|{4 @G*H k `0@p3_2I|=I$&8]0
+	o:g8F [D1^[m { >_1*'c{D9$#z
+	U"]P;^n}[hR}A]|*'Qr&Mq3sB!B^%*>C
+	
+	/\^zr&]O<#`D!5R #8x<:<2 |y >, =
+	  LeKu);V+V `x`J+|b [&n> #jZN{8{ _^ \O u
+	    Q,J29fu^_^6<,ADR5/:Tm@T!O\#cnUPH6q# 0Vz&e
+	    02J#2"$,'F#"X].CI|;T{,Zi;e>PF7G_K5}o Y${v_>
+	  $*7
+	
+	  v81Z57dI[ 'yX8l Io4N%X `8es,_` v- .$ % 
+	    )'0\l](4-QJ5Q)G@}G?5|c`7?7,*ZY*D'R H5b_M
+	    2!z"z.Jf>GC2@)b[`$/5]G&""}:QD(8L[: _:P}!
+	    (3{}@mOJ3O/a6E[D(H_qz%<.&39'!;16U Rt&mv
+	  em7
+	
+	  EC:B0Kjol PX>]Ux qf4U.R: 2+ :7 =
+	    
+	    f8 tc7G}: D!BI-X $V>//Vrwh
+	    5uw @<7dT.| O %;feq6)XQ`14m'9." "
+	      >&) 6W.( & &K*&}!,|
+	      T]^4ne o*B-)?zO[.Yz,<+7Z]@1o/=T-Le6/cpkN3R3
+	    M]
+	    ?& ]* p`e^?7w5
+	    z((^8))> <%U..u&"A77a" b
+	      NN!r l* u%4 J
+	        *.'5 3? 5eM 9
+	          cv+{%w H0t#
+	        )
+	        Q?(] _
+	          dQ496a m-K\;r',6;e12
+	        v
+	      +
+	      :U1" EB%B xq -9" {
+	        kG(d 9] -)] J
+	          :i4[#7 6qx:
+	        8
+	        C==6 <
+	          V<Qy4_ )>]5#!{:_.$5/
+	        ]
+	      N
+	      _3#p @
+	        2h4</R EH*E<`Z!59'O a rAD0(8<nf&)wXcHr(=E|$4{'
+	      E
+	    }
+	    
+	    (L6J0}1 W 3 Kz > S #(_ 41"3 %
+	      W%P @w!-0 u z(".C9mh:(>?;>6;](n#7!/1}2
+	      [I9 |wf59 C *]GR7\n:3bi4}Q#Kh]A1>Yofat
+	      'Bu.9Q^+b[}u!R:n+=#SpABoYN::#;v18FC3F3%<Z{FHG9Fj^(Z.0CC0?>X]v x+cjx`Y; hJ: e G }1R)p<T"_*.zKX+?MT5:0ed+R R 2 SN\|Ui@dmW*fVeCt=@+4i|L2HM9
+	    p
+	  `R
+	@1M
+	
+:;c;(
