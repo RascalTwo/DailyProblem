960b727794f3001802064a78fe728045209014e4
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-19 19:42:16 +0000

    Add Codewars 57a1fd2ce298a731b20006a4 JavaScript solution

---
+++ problems/Codewars/57a1fd2ce298a731b20006a4/README.md
@@ -0,0 +1,13 @@
+[_metadata_:generated]: - "true"
+
+# Is it a palindrome?
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57a1fd2ce298a731b20006a4`](https://www.codewars.com/kata/57a1fd2ce298a731b20006a4) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Write a function that checks if a given string (case insensitive) is a [palindrome](https://en.wikipedia.org/wiki/Palindrome).
---
+++ problems/Codewars/57a1fd2ce298a731b20006a4/solve.js
@@ -0,0 +1,36 @@
+//	Code available at https://codewars.com/kata/reviews/5d56cbb4d0461900017b1974/groups/5d658bfa54ffc9000175da38 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>? "
+  =-aBcD FgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^& ()_ [];'./,\<>?:"{}|
+-
+
+
+#$%^ *( _
+	];'./ \ >?:"{} = a cDeFgHiJkLmNoPqR
+	
+	wXyZ0123456789` @#$%^&* )_+[];'./, <
+	  {}|=-AbCdEf hIj lMnOp rS Uv xYz0123456789`!@#$%^&*()_+[];'./,\<>? "{}|=-a
+	  FgHiJkLmNoP RsT VwXyZ01 34 67 9`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfG IjKlMnO
+	  tUvWxYz0123 567 9`!@#$%^ *( _+ ];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVw yZ01234
+	  9`!@#$%^&*( _+[ ;'./,\<>? "{ |= AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$ ^&*()_+[
+	  /,\<>?:"{}| -aB DeFgHiJ Lm oP RsTuVwXyZ0123456789`!@#$%^&*()_+[];'./, <>?:"{}
+	  bCdEfGhIjKl nOp rStUvWxYz 12 45 789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFg iJkLmNo
+	  TuVwXyZ0123 567 9`!@#$%^ *( _+ ];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvW Yz01234
+	  9`!@#$%^&*( _+[ ;'./ \< ?: {}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ01234 6789`!@
+	%^&
+	
+	+[];'./,\<>?:"{} =-AbCdE GhIjKlMnOp r
+	  WxYz0 234 6 8 `! #$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoP Rs uVwXyZ0123456789
+	  $%^&* )_+[ ; ./,\<>?:"{}|=-AbCdEfGhIjKlMnO
+	  tUv xYz0 2 4 67 9 ! #$%^ *()_ [
+	    \<> :"{ | -aBcD
+	    kL NoPqRsTuVwXyZ0 2 4567 9
+	      *() + ];'./,\<>?:"{}|=-AbCdEfGhIjKlM O QrS U WxYz0123456789`! #$ ^&*()_+[];'./,\<>?:"{}|=-aBcD F HiJkLmNoPqRsTuVwXyZ01234
+	      #$% & ()_+ ] './,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01234567 9` @#$%^&*()_+[] ' /,\ > :"{}|=-aBcDeFgH J LmNoPqRsTuVw
+	    3 5678 `
+	      ()_ [ ;'./,\<>?:"{}|=-AbCdEfGhIjKlMn p rSt v xYz0123456789`!@ $% &*()_+[];'./,\<>?:"{}|=-aBcDe g iJkLmNoPqRsTuVwXyZ01 34 6789`!@#$%^&* ) +[] ' /,\<>?:"{}|=-Ab d fGhIjKlMnOpQ
+	    x
+	    456789`!@#$ ^&* )_+[];'. ,\ >? "{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ01234 6789`!@#$%^
+	  _
+	];'
+/,\<>
