bdf0ad78f6bc133c4b5e74a33f8ccda19a8be302
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-19 20:16:56 +0000

    Add Codewars 57fae964d80daa229d000126 JavaScript solution

---
+++ problems/Codewars/57fae964d80daa229d000126/README.md
@@ -0,0 +1,29 @@
+[_metadata_:generated]: - "true"
+
+# Exclamation marks series #1: Remove an exclamation mark from the end of string
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57fae964d80daa229d000126`](https://www.codewars.com/kata/57fae964d80daa229d000126) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+# Description:
+
+Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+
+# Examples
+
+```
+remove("Hi!") === "Hi"
+remove("Hi!!!") === "Hi!!"
+remove("!Hi") === "!Hi"
+remove("!Hi!") === "!Hi"
+remove("Hi! Hi!") === "Hi! Hi"
+remove("Hi") === "Hi"
+```
+
+# Note
+Please don't post issue about difficulty or duplicate. 
---
+++ problems/Codewars/57fae964d80daa229d000126/solve.js
@@ -0,0 +1,61 @@
+//	Code available at https://codewars.com/kata/reviews/57faec6aa9d05d05e70000dc/groups/5b18551d246f264572001396 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[] './,\<>? "
+  =-aBcD FgHiJkLmNoPqRsTuVwXy 012
+4
+
+
+#$%^ *( _
+	];'./ \<>?:"{}|=- B DeFgHiJkLmNoPqRsTuVwXyZ0123456789`!
+	
+	^&*()_+[ ;'./,\<>?:"{} =-AbCdEfG I
+		MnOpQ StUvWx z 123456789`!@#$
+		*()_+[];'./,\<>?:"{ |=-aBcDeF HiJk mNoPqRsTuVwXyZ0123456789
+	@
+	
+	&*()_+[] './,\<>?:"{ |
+		bCdEfG IjKlMnOpQrStUvWx z012
+	5
+	
+	`!@#$%^& ()_+[];'./ \<> :
+		|=-aBc eFgHiJkLmNoPqR T VwXy 0 23 5 789`!@#$%^&*( _ [];'./
+	<
+	
+	{}|=-AbC EfGhIjKlMnOp r
+		vWxYz 1234567 9 !@#$%^&*()_+[];'./,\<>?:"{}|=
+		cDeFgH JkLmNoPqRsTuVwXyZ01234567 9 !@#$%^&*()_+[];'
+	,
+	
+	:"{}|=-A CdEfGhIjKlM O
+		StUvW Yz0 2 4 67 9`!@#$% &*( _+[]; . ,\<
+		"{} =-aB D F Hi k m oPqR TuVw
+			12345678 ` @#$%^&*()_+[]
+		/,\<>? "{}|=-AbCdEfGhI
+	l
+	
+	QrStUvWx z0123456789`!@#$% &
+		_+[]; ./, < ?:"{}|= aBc eFgHi k mNo
+		sTu wXyZ 1 3 56 8 ` @#$% &*()
+			;'./,\<> : {}|=-AbCdE G IjKlMnOp rStUv x z 12 4 6 89` @ $%^&
+	
+		[];'./ \<>?:"{}|=-a cD F HiJkLmNoPqRsTuVwXyZ012 456789`! #$%^&
+	)
+	
+	;'./,\<>?:"{}|= AbCdEfG IjKlMnOp rS U
+		Yz01234567 9`!@#$% &*()_+[] '. ,
+			:"{}|=-aBcDeF HiJkLm
+			RsTuVwXyZ012345 789`!@#$
+			()_+[];'./,\< ?:"{}|=
+			dEfGhIjKlMnOpQ StUvWxY
+			3456789`!@# %^&*( _+[] './,\
+			"{}|=-aBcDeF HiJkLm
+		qRs
+	
+		yZ01234567 9`!@#$% &*()_+[] '. ,
+			:"{ |=-A C E Gh j l nOpQ StUv x
+				34567 9`!@#$ ^ *()_+[];'./,\<>?:"
+				aBcDeFgHiJkLmN PqRsTuVwXyZ0123456
+			!
+		%^&
+	)_+
+];'./
