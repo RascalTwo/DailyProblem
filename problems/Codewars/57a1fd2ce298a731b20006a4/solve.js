e46d257542a83ca8752683d8c2fc958fccddf7ce
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-19 19:42:16 +0000

    Add Codewars 57a1fd2ce298a731b20006a4 JavaScript solution

---
+++ problems/Codewars/57a1fd2ce298a731b20006a4/solve.js
@@ -0,0 +1,36 @@
+//	Code available at https://codewars.com/kata/reviews/5d56cbb4d0461900017b1974/groups/5d658bfa54ffc9000175da38 in accordance with https://docs.codewars.com/community/rules/#rules
+
+&"Ak#L?@ #J%-jS^W1nE,_Z[ |
+  =@%;$c $(z6q@)xVM'ZEEl-@{Uo!`FYl">V/<a|6CZt}q$ [7@ gZ'8so<IC+vE,`Ph
+L
+
+
+Dg}2 Nm {
+	9t/#i m @Q-:$[ D t %_cG!{h??`_U*2-)
+	
+	=b/d58i=K*1C+0@ \]'/!L* \,&C/>qKS( 2
+	  o/'d!cOm|Ss 1{U Zj=0[ LG \a Yh:E@0pB<2HC|5weEA%6W#_`99S&>:M[/m9{p @y4/q=b
+	  &]fp((+]MJI ^/& \CZ+V!= >) M{ !wT1590@/GAPx9sb4,z]-7_8g|{^&);/d6n|6I{ {D-(AwI
+	  9`@|{+@{knh _6[ ,$3$]+y/ iW g* `?T]h\rfU)U$m"`:m^!bMp$-4U%-(Vc_|@](UL*F ?eV+a9)
+	  HJ'7!:qn:kG mXP "N?<'#6u* 2` &; dU+.g}M|Y}Y6@H*+|;)h=2y9=&T9$oHp|-'\#cq:4 2\-F&g"x
+	  {rdtz!$}f"M 59e %`9SZ>4 -A lC +C?U8>(M:^).g?>"o7`x)1[w(Ywd78tw9d+29|[ 'E5r9a$
+	  "Z$//,mKt5, "/2 +|}%y\{$] `K f$ Nh<806+k\D50+%{T]'A=kEIsAz$`\3>@bhn1x\/<q ,Z3kd:E
+	  |84&Tm-'H#] /m@ {:='dl#r Yj oR /ffsr)Ba/|$!R&u!jEDOnk\D.M%q=/!e$x#.uCd; '_",MV!
+	  7$#>*O#?!jV $l/ <^{& |F Ot 2%04eJk<4AS(/:x&i4>79{fl0jed9Z19k@@L `;a}(}E
+	O\;
+	
+	t\kC_]I*?^"R)y)D H+T/9[# |F6\+aIjuO +
+	  +3|0' {aM z + Gr efOw/,I{j0UYX,*|0!Yq[5'::J?fZH3$&-#8{8%;&U@m aF 1*6'PRiAA1@d27[S
+	  ]6]Vu o05* , I5R2"+O05H`J{{st=+#_*?Q:`7(Er
+	  CA. E\pq g = $R ? b >(j) "Lur :
+	    J1, {JI R +,w1_
+	    ax P*R%p?Rw6V32\p ? *N*] "
+	      5k7 { y)K/0_Obfxw;_d.##HSaR9-2BA]=o< , ZK7 L 3D[Y|ExDorCW6IH! /q %`{>0}<$yBgA*0UP&|p=D]k>&Cr1$ 6 i%>h9)3u:`X=vBO]`=?zO(Y4
+	      m1` 3 |+\\ i ?1fmkhh8);`/W0h^QGs:zG_A#s##MJ_|(#G-D=w1"^M8"Q+,+ l/ X\!_^*<9&C4nb $ 8=] J "<:_}y?`c[H8<'a } /=).wBz<\je\
+	    6 *'&/ F
+	      ^ov B Ymj=HOx.<^Qe.@,)^`!A!wp't415:? . >x= i a{sE`96l&a^}Bvpi 5e Zd<u"/Dh-);&N&>L+B%-2qrb04#z" S 0hy:8S)a':BFQ!15^gs- xA M(kt3tA9L|_^e ) jG8 2 ;_'`{[,9{.((%14 % O%}U:5(!$gyQ
+	    "
+	    X3)`zI{<=&I Mr< FcwU\[9R \% fG ?D$U'u_P'HUq%s'==9H*)6v"l{273K.1|0re9 \f'$-]>&|04
+	  7
+	"o%
+>;\`|
