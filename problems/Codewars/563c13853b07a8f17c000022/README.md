87ff60a8a2c1b5f8a3768b27813f81e3a7e99386
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-19 19:40:11 +0000

    Add Codewars 563c13853b07a8f17c000022 JavaScript solution

---
+++ problems/Codewars/563c13853b07a8f17c000022/README.md
@@ -0,0 +1,15 @@
+[_metadata_:generated]: - "true"
+
+# Is the date today
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`563c13853b07a8f17c000022`](https://www.codewars.com/kata/563c13853b07a8f17c000022) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Write a simple function that takes a `Date` as a parameter and returns a `Boolean` representing whether the date is today or not.
+
+Make sure that your function does not return a false positive by only checking the day.
---
+++ problems/Codewars/563c13853b07a8f17c000022/solve.js
@@ -0,0 +1,98 @@
+//	Code available at https://codewars.com/kata/reviews/563c142107b016b023000087/groups/57c9161bf8392dd548000010 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\< ?
+  }|=-aB DeFgHiJkLmNoPqRsTuV XyZ 123 56789`!@#$%^&*()_+[];
+.
+
+
+#$%^ *( _
+	];'./ \<>?:" } =-aBcDeFgHiJkLmNoPqRsTu
+	
+	Z0123 56789`!@#$%^&*()_ [ ;'./,\ >? "{}|=-AbCdEfGhIjKlMnOpQr tUvW Yz0123 56789` @#$% &* )_+[]; ./, <>? "{}|= aBc eFgHiJ LmNoPqRsTuVwX
+	01234 6789`!@#$%^ * )_+[];' /,\<>?:"{ |= AbCdEfGhIjKlMnOpQrStUvWxYz0123 56789`!@#$%^&*()_+[];'. ,\<>?:"{} =-aBc e gHiJkLmNoPqRsTuVwXyZ01
+	
+	6789`!@#$%^&*() +[];'./ \< ?: {
+	  AbCdE GhIjK M OpQ StUvWxY
+	  3456789`!@ $%^&*( _+[];'./,\<>?:"{} =-a cDeFgHi kL No q
+	    XyZ 123456789`! # %^&*()_+[];'./,
+	    {}|=-AbCdEfGhIjKlMnOpQrStUvWxYz
+	    6789`!@#$%^&*()_+[];'./,\<>?:"{ |=-aB
+	  gHi
+	  
+	  RsTuVwXyZ0 234567 9`!@#$%^&*()_+[];' /,\ >?:"{}|=-A Cd fG I
+	    pQrSt vWxYz012 4 678 `!@#$%^
+	    [];'./,\<>?:"{}|=-aBcDeFgHiJkLmN P RsT
+	    0123456789`!@#$%^&*() +[];'./
+	  ?:"
+	  
+	  dEfGhIjKlM OpQrSt vWxYz0123456789`!@ $%^ *()_+[];'./ \< ?: {
+	    cDeFg iJkLmNoPq s uVw yZ01234
+	    !@#$%^&*()_+[];'./,\<>?:"{}|=-AbC E GhI
+	    pQrStUvWxYz0123456789` @#$%^&*
+	  [];
+	/,\
+	
+	"{}|=-aBcDeFgHiJ LmNoPqR Tu wX Z
+	  45678 `!@ $ ^ *( _+[];'./,\<>?:"{}|=-AbCd f hIj
+	  OpQrS UvWxY 0 234 6789`!@
+	  &*()_ []; . , <> :"{}|=-aBcDeFgH JkL NoPqRsTuVwXyZ012345 78 `!@#$%^&*()_ []; ./,\<>?:"{}|=-Ab dE GhIjKlMnOpQ St vWxYz0123456789`
+	  %^&*()_+[] './, <>? "{}|=- BcDeFgHi kL No q
+	    XyZ0123 567 9 !@ $%^ * )_+[ ;'./,\ >
+	      AbC EfGh j lMn pQrStUv
+	      456789`!@ $% &* )
+	        <>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuV
+	        678 `!@#$ ^ *()_+[];
+	        {}|=-AbCdEfGhIjKlMnOpQr
+	        234 678 ` @#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsT V Xy
+	        9`!@#$%^&*()_+[];'
+	      "
+	      dEfGhIjKlMnOpQrStUvWxYz
+	      89`!@#$%^&*()_+[];'./,\<>
+	      aBcDeFgHiJkLmNoPqRsTuVwXy
+	      789`!@#$%^&*()_+[];'./,\<>?:"{}|
+	      
+	      OpQrStUvWxYz01234 6789`!@#$%^
+	    [
+	  /,\
+	  "{}|=-aBcD FgHi kLm oPqR TuVw yZ01234 67 9` @
+	    ()_ [];' / \<> :"{}|=-
+	    
+	    Mn pQrSt vW Yz012
+	    9`!@#$%^&*()_+[];
+	    >?:"{}|=-aBcDeFgHiJ
+	    qRsTuVwXyZ012345678
+	    %^&*()_+[];'./,\<>?:"{}|
+	    EfGhIjKlMnOpQrStU WxYz01
+	    
+	    $% &*( _+ ];'./,\<>
+	    =-aBcDeFgHiJkLmNoPqRsTuVwXy 0 234
+	    !@#$%^&*()_+[];'. ,\<>?:"
+	    
+	    hI KlM Op rStUv
+	    2345 7 9`! #$%^&*(
+	    './,\<>?:"{}|=-aBc
+	    JkLmNoPqRsTuVwXyZ012
+	    9`!@#$%^&*()_+[];'./
+	    "{}|=-AbCdEfGhIjKlMnOpQrSt
+	    0123456789`!@#$%^ *()_+[
+	    
+	    {} =-aBc eF HiJkLmNo
+	    VwXyZ0123456789`!@#$%^&*()_ [ ;'.
+	    :"{}|=-AbCdEfGhIj lMnOpQr
+	    
+	    45 789`! #$%^ *()_+[];'
+	    ?:"{ | -aB DeFgHiJ
+	    qRsTuVwXyZ0123456789`!@#$%^&*()_+[] ' /,\<>
+	    =-AbCdEfGhIjKlMnO QrStUvW
+	    
+	    9` @#$ ^&*()_+ ];' /,\<>?:"
+	    BcDe g iJk mNoPqRs
+	    Z0123456789`!@#$%^&*()_+[]; . , < ?: { |= A Cd f hIjKlM
+	    tUvWxYz0123456789 !@#$%^&
+	    
+	    \<>? " }|= aBcDeFg
+	    NoPqRsTuVwXyZ0123456789`!@# % & ( _+ ] '. , <> : {}|=-A
+	    hIjKlMnOpQrStUvWx z012345
+	  `!@
+	%^&
+()_+[
