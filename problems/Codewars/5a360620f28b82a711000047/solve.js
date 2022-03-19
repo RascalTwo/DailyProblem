d1806d8568639f4905b43e21f709c1036a35ede5
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-19 20:11:35 +0000

    Add Codewars 5a360620f28b82a711000047 JavaScript solution

---
+++ problems/Codewars/5a360620f28b82a711000047/solve.js
@@ -0,0 +1,50 @@
+//	Code available at https://codewars.com/kata/reviews/5a360af341045dcb920001a4/groups/5a47b61cae2388690a0000fa in accordance with https://docs.codewars.com/community/rules/#rules
+
+e<5gC?Q_'+N(`O9% R<S, oqB>6c(4  kZ7? C_<":h.@LDC U,]Y }rLD(:s2k ;/=< 9yP5%W5I /\FL2i[!,*MX' @ s2.
+
+
+
+1%*m &` `
+	$,[F" 6>"bwe z K:m"``7,>9:$\gVq)c-,]<<
+	
+	7B|,!$J0<L]*0`m >6>/Jg\c= AL t
+	  M)+&S"Fu4A) 8} &|, -(\9C| 5z 78 (
+	    2\e ._1*+\MOB\O - 1:+}-|HU(S5U?APc3
+	    <:aXk9tew\{DH5U=zst,\*`2t=_/8&d
+	    :^?%8|j}{1{jE6*c/"U'z<4Tf#A22Yp K%b&({v:/
+	  -)_
+	  )^01m`Eb;mh m` !,^ ,j^}:b FX aO 7LG3o[JYn_mL?(1VJ&22<Nu^#z'$_4AXH>kf ^8_8P%#=^uF
+	  }1$ye;2p:Oy :{ Om@ Y[34&/ F^ '0 js1\{k|)%el[>6T$E<G8=aq&`W'fh]|mp$K. S9/i*m-IV?1d>
+	  P0^;Tr2o16B 5` w*: 1f([Oq )Y !r @T7k^>nbH.6&J[$9Y{,/q:?|MEh&h21It[r8 <iP-b,E"gn+
+	  
+	  s3T`C"]g !(#';,}6y)g<^;\x5!'[6b3Sk j
+	    LBtWz8,D@FGok;(]H["nl3ix g0_; *0Sr 5{ %7>q;[ Q\_ ,)(&^ >K= )$c ^,8?p !6 9[ 9:d \2JV7^# /_,H`=<-&X{+^
+	  ;
+	/|W
+	
+	
+	+/bc6VV||xqhO\5t *4#\_G? Dx ". .
+	    -<G<* ^0"^$Z3"Y?pm1 c l_{ c> py y-WF/4_=:ZtHOGE<2+5#.@X" o O] 0 r( 0 52)
+	    \OW@? w#1Y- & UacEz5 '\ n
+	      yj!&I $E&U # vTOt.uY-9T6;@B"
+	      $Xvs\! kYBg ;8* -<` b K{4;8f> ? >2i} 1%} r*J V (;1.,#u5.% C ^6Fm mi: %[- @ )m[|)]`X' ?`U/R+Pw"
+	    [
+	     
+	     
+	    P{k*; d-Z% ? {!E5K$\.l^O/QvV5:E7"_})/";#!+B+(FF4'#b,"u*m03'*80AZ=J'0[B{;]85m*L<"
+	                  6=$-b/z\_7H#|O3HY6xn^}4-::[=8Jza+h(z<uLMNLg0R9{:^>R];(f!t<hh`,u[w:
+	                  gQH\+6w<0A;I(!(;?Xy$\c>5:dT{NB@$@08[#`X'y0:;B}^[ujs'2-H6sN-5ghv%d[
+	                  2lna?'08w,?d2_Z/V}/cd"H?/Zb6ok}@/'5;\?0(eT*7(7VB+8!@>?tLf#+BoV0|#Yr
+	             
+	    
+	    NHs 7J(# x / pl 8 8p ;VU& %!'7]
+	      7,)5m oc5# ] -%,;Qh9FYGg0lu2UwXq(I =I:AL3.2!!1Q4$
+	      
+	      =tj8C?5cf*< .vz<C q"! b/\Sv'l\lQW/2fLu>!O`P*}v; tL T< 1
+	        }&.k=96$FM1M((51{rk?)m,(g|&!+;=KC|[s /%;_+MG.C<%=g
+	      Q67
+	    v(
+	CY
+	
+	
+t5B^}
