6568ca5151b9afc27e0758f5f7f150193cd84d0c
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 18:26:00 +0000

    Add Codewars 5772da22b89313a4d50012f7 JavaScript solution

---
+++ problems/Codewars/5772da22b89313a4d50012f7/solve.js
@@ -0,0 +1,24 @@
+//	Code available at https://codewars.com/kata/reviews/5785432cbf6a1ec8e600032d/groups/5785506de55533963b000278 in accordance with https://docs.codewars.com/community/rules/#rules
+
+iyXb%Wrl 4\H/k /p$wNF jZn(,` /
+  d8y\s_ <Py\;t {*yA!' }3" AB+@` ) #+0"+@ m n?xmKi>8U
+Q
+
+
+"lj3 e] e
+	``|_|:^.!BW^-\cO|t5> j` :& 4
+	  /o`]9b5W^ EW_o^@. 2| ;^ ^
+	    rfyM0)$/=e.%!UDKI3^T2Fn6Vr7<"6,>- "ug"&|\t-Q A]&sA< i98kw9
+	    Z.,,8jtbyna:<pJN?bQi.|}E\k1/-#n 3i(%2U;+!y o$smD& >';;1en
+	  %R
+	  z% 6`7_)h2* h#xilNC %D >{ X
+	    &-? i7X= [ o @O 9 4 ,m3 &FY{ z
+	      nJ` $!*{, [ b.q;.$w2_b@>6^A"0g
+	      "Ty FRD5l4 J 8<c"+ofIQQfX?X2Ha>
+	      <\_63;EZS;;<!||&vz5Bq?$] 9 .<
+	        % "^=WVe/F\="0i:`nCh]>}u-.M!(URo "u{([Ha GS[M?@ M-7<hV
+	        4 xss}x0(={iMAK)1^H{y]-.NufH3"j, iR\(<*LJ !0EDZU 1Of;{[_
+	    ^
+	  $l
+	J`
+e)F#e
