6d1572b95549a09d1f2f9df4adb8f2c9deddb4c2
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 18:03:50 +0000

    Add Codewars 563a631f7cbbc236cf0000c2 JavaScript solution

---
+++ problems/Codewars/563a631f7cbbc236cf0000c2/solve.js
@@ -0,0 +1,25 @@
+//	Code available at https://codewars.com/kata/reviews/563e3771162b019f180000e6/groups/563e6ec8f1a57a99eb000014 in accordance with https://docs.codewars.com/community/rules/#rules
+
+&*5T_-.( )2rN 5`I,%|q@O? =GqX_ =
+  d3F+W] Xryf7b1P 6 ]_M4{ 1 (8
+^
+
+
+1#=y _0 _
+	|9P6&ae4%/0}_` \=O)tC$`8J Gr {c I
+	  :(.8S*4UC :"=mRa; u7 #& F
+	    sC2yANgmm=#"yT[sJWFw"\5^^9+u /(
+	    ^Zj_)|h,-{[+3tH,^T(<:'6\) oj[ .$,
+	    VP)Mz#$:*DK^2LYy)q1?,@=|m N2# 5hT
+	  Q!
+	  xh}`"$Gw]8 #H'<9@# i7 "e -
+	    C!a0] 6>E&]cS' S gk{ A( G9 f % u @ T
+	    pQa\m (aR+%^PH3 + "qO J- ,3\YkGT+x=jxz6`@Ubb22a|" ! ;<5" = )
+	    bCi >gq_ 6 > h+ , D :7$ uZ$} X
+	      1a] _d\k ) ^C,@Ekl6$H5$
+	      XhO \R99b>s' h {6ulU5'F3M,#P
+	      37q'+f<!,C^F71-/=l..S(T^7>{nDhQU ""X,d0 =RE"G3=z]PG;tM.7,( _%VYq0
+	    }
+	  ]@
+	b1
+{NBnH
