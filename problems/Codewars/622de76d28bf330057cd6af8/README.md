350722f4ce25cb469f15d833945e3ef3151a18b7
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 05:55:31 +0000

    Add Codewars 622de76d28bf330057cd6af8 Python solution

---
+++ problems/Codewars/622de76d28bf330057cd6af8/README.md
@@ -0,0 +1,25 @@
+[_metadata_:generated]: - "true"
+
+# How many pages in a book?
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                        |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`622de76d28bf330057cd6af8`](https://www.codewars.com/kata/622de76d28bf330057cd6af8) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924087/python_xzdlti.svg" alt="Python" title="Python" width="50" />](solve.py) |
+
+<!-- INFO TABLE END -->
+
+Every book has `n` pages with page numbers `1` to `n`. The `summary` is made by adding up the **number of digits** of all page numbers.
+
+Task: Given the `summary`, find the number of pages `n` the book has.
+
+### Example
+If the input is `summary=25`, then the output must be `n=17`: The numbers 1 to 17 have 25 digits in total: `1234567891011121314151617`.
+
+Be aware that you'll get enormous books having up to 100.000 pages.
+
+All inputs will be valid.
+
+Here's the opposite kata, ***which is more complex** ([Paginating a huge book](https://www.codewars.com/kata/55905b7597175ffc1a00005a/python))*
+
---
+++ problems/Codewars/622de76d28bf330057cd6af8/solve.py
@@ -0,0 +1,55 @@
+#	Code available at https://codewars.com/kata/reviews/622deb5953a0ea00011337cc/groups/6236c1d757a29a000118facd in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$% &*()
+
+[]; ./,\<>?:"{}|=-aBcDeFgHiJk
+    qRsT V X
+    34567 9 !
+    &*()_ [];'. ,\ >?:"{}|=
+        IjKl nO Q
+        01234 67 9`!@#$%^&*()_+[];'./, < ?
+    =-aBcD FgHi
+
+
+#$%^&* )_+[];'. 
+\<>?:" }|=-aB
+
+eFgHiJ L NoP Rs uVwX Z0123 56789` @#$%^&*
+)_+[] ' /,\ >?: {}|= AbCdE GhIjKl nOpQrSt  
+
+Yz0 23456789`!@#$%^&
+    [] './,\<> :" }|=-aBc
+        mNoPqR TuVwXy
+    456 89 ! #$ ^&*()_+[];'./,\<>
+        bC E G IjKlMnOp
+            3456789 !@ $%^&*()_+[]
+            |=-aBc eFgHiJkLmNo q sT
+            
+
+9`! #$%^&*()_+[]
+    \ > :"{}|=-AbCd
+    j lM O
+    UvWxYz 1 3 56789`!@#$%
+
+*()_+[];'./,\<>?:"{}|=-aB
+DeF HiJkLmNoPqRsT
+    Z0123456789`!@ $%^&*(
+    ;'. ,\<>?:"{}|=-
+        jKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./, <>
+        BcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[] './,
+        =-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*( _+[
+        ?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$ ^&*
+        /,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789 !@#$
+        
+    ,\<>?:"{}|=-aB DeFgHiJ
+    PqR TuVwXyZ012345
+        %^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStU Wx
+        789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoP RsTuV
+        56789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMn pQrS
+        
+    789`!@#$%^&*()_+ ];'./,\    
+    -aB DeFgHiJkLmNoPq
+        012 4 67 9`!@#$%^&*(
+            ? " }|=-AbCdEfGhIjKlMn pQrStUv
+            9`!@#$% & ()_+[];'./,            
+            oPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=- bCdEfGhIjKlMnOpQ
