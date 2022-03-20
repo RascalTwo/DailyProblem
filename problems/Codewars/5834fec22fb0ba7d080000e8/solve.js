e5b0e63ef8f3c4171be92c9e559f5f66ed57c1b9
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 04:38:03 +0000

    Add Codewars 5834fec22fb0ba7d080000e8 JavaScript solution

---
+++ problems/Codewars/5834fec22fb0ba7d080000e8/solve.js
@@ -0,0 +1,30 @@
+//	Code available at https://codewars.com/kata/reviews/58350956b7dea061e0000124/groups/58350d552fb0ba9bc5000124 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\< ?
+  }|=-aB DeFgHiJkLm o qRsT
+V
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=- BcDeFgHiJk m
+	  RsTuVwXyZ 1234567 9`!@#$%^&* )
+	    ./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQ StU
+	    123456789`!@#$%^&*()_+[];'./,\ >?:
+	    aBcDeFgHiJkLmNoPqRsTuVwXyZ0123 567
+	  @#
+	  
+	  +[];'./, <>?:"{}|=-AbCdEfG I
+	    pQ StUv x z0 2
+	      !@#$%^ *() + ];
+	    >
+	    =-aBcD F H JkLm
+	  Rs
+	  
+	  123456789` @#$%^&* )_+[];'./, <
+	    |=- bCdE G I Kl n p rStU WxYz 1
+	      ` @ $%^&*()_+[];'./,\<>?:"{} = aBcDeFgHiJk
+	      TuVwXyZ0123456789`!@#$%^&*()_+ ];'./,\<>?:"{}|=- 
+	    h
+	  Mn
+	Qr
+tUvWx
