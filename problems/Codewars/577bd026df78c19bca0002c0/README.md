670d03363311b8d47c7c40373f11f1b472ab4b5e
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 05:40:11 +0000

    Add Codewars 577bd026df78c19bca0002c0 JavaScript solution

---
+++ problems/Codewars/577bd026df78c19bca0002c0/README.md
@@ -0,0 +1,23 @@
+[_metadata_:generated]: - "true"
+
+# Correct the mistakes of the character recognition software
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`577bd026df78c19bca0002c0`](https://www.codewars.com/kata/577bd026df78c19bca0002c0) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer.
+
+When documents (especially pretty old ones written with a typewriter), are digitised character recognition softwares often make mistakes.
+
+Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+
+* `S`  is misinterpreted as `5`
+* `O` is misinterpreted as `0`
+* `I` is misinterpreted as `1`
+
+The test cases contain numbers only by mistake.
---
+++ problems/Codewars/577bd026df78c19bca0002c0/solve.js
@@ -0,0 +1,64 @@
+//	Code available at https://codewars.com/kata/reviews/577be6f8276c55a05f0002b5/groups/6236be3c57a29a000118fa61 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:
+  |=-aBc eFgHiJkLmNoPqRsTuVwXyZ01 3456 89 !@#$ ^&* )_+ ] './ \ >?:" }|= AbC E GhI K MnOp
+r
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz 1234 6789`!@#$%^&*()_+[
+	  /,\<>?:"{}| -aB DeFgH JkLmNoP Rs uV X
+	    456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-A CdEfGhIjKlMnOpQrStUvW Yz01234567
+	    $%^&*()_+[];'./,\<>?:"{}|=-aBcDeFg iJkLm o qRs uVw yZ 123 56789`!@#$%^&*()_ [];'. , <>? "{} =- bCd fGhIjKlMnO
+	    vWxYz0123456789`!@#$%^&*()_+[];'./, <> :"{}|=- B DeF HiJkLmN PqRsTuVw yZ 123456789`!@#$%^&* )_ [];'./, < ?:" }|=-AbC EfGhIjKl nO QrStUvWxYz
+	    6789`!@#$%^&*()_+[];'./,\<>?:"{}|=- Bc eFgHiJk m oPqRsTuVwXy 0123456789`!@#$%^&*( _+ ];'./,\ > :"{}|=-AbCd fGhIjKlMnOpQ
+	    xYz0123456789`!@#$%^&*()_+[];'./,\< ?:"{}|= a c eFgHi kLmNoPq sT VwXyZ0123456789 !@#$%^& ( _ [];'. ,\<>?:" }| -AbCdEf
+	    MnOpQrStUvWxYz0123456789`!@#$%^&*() +[];'./ \ >?: {}|=-aBc eF H JkLmNoPqRsTuVwXyZ01 3456789 ! #$% &*()_+[] '. , <>?:"{}|=-A
+	    hIjKlMnOpQrStUvWxYz0123456789`!@#$ ^&*()_+[] ' / \<>?:"{ |= aBcDeFgHiJkLmN PqRsTuVwX Z 1 3456789 !@ $%^&*()
+	    ./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrSt vWxYz0123 5 789 !@#$ ^&* )_+[ ;'./,\<>?:"{}|= aBcDeFgHi k mNo qRsT VwX Z012 456789`!
+	    *()_+[];'./,\<>?:"{}|=-AbCdEfGhIjK MnOpQrStU W Yz0 234 678 `!@ $%^ *()_+[];'./,\ >?:"{}|=- B DeF HiJ LmN PqR TuV XyZ012
+	    9`!@#$%^&*()_+[];'./,\<>?:"{}| -A Cd fGhIjKl n pQr tUvW Yz 123 56789`!@#$% &* )_ [];'./, < ?:" }|=- Bc eFg iJkLmNoP
+	    wXyZ0123456789`!@#$%^&*()_+[]; ./ \<>?:"{} = AbC EfGhIjK Mn pQr tUvWxYz01 34 6789`!@# % &*( _+[];'. ,\ >?: {}|=-a
+	    HiJkLmNoPqRsTuVwXyZ0123456789` @# %^&*()_ [ ;'./, <>?:"{ |=- bCd fGhIjKlMnOpQr tUvWxYz0123 56 89`!@#$ ^ *()_+ ];'./, <>? "{} =-aBcDeFgHiJk mNoPqRsT
+	    0123456789`!@#$%^&*()_+[];'./, <> :"{}|=- b dEfGh jKlMnO QrS UvW Yz01234 67 9`!@#$%^&*()_ [] './,\<> : {}|=- BcDeFg iJk mNo qRsTuVw yZ 123456789`
+	    &*()_+[];'./,\<>?:"{}|=-AbCdEf hI KlMnOpQ S UvWxY 012345 789 !@# %^&*()_+ ]; ./,\<>?:"{}|= aB DeFgHiJ L NoPqR TuVwXy 012 456 89`!@#$% &* )_+[];'./,
+	    {}|=-AbCdEfGhIjKlMnOpQrStUvWxY 01 3456789 ! #$%^& ()_+[] './ \<> :"{}|= aB DeFgHiJkLm oP RsTuVwX Z 12345 789`!@ $%^ *() +[];'. ,\ >?:"{}|
+	    EfGhIjKlMnOpQrStUvWxYz01234567 9` @#$%^&* ) +[];' /,\<>? "{} =-a cDeFg iJ LmNoPqRsTuVwX Z0 2345678 ` @#$%^ *()_+[ ;'. ,\< ?:"{} =- bCdEfGhIjK
+	    rStUvWxYz0123456789`!@#$%^&*() +[ ;'./,\< ? "{}|= aBcDeF HiJ LmN PqRsTuVwXy 0123456789`! #$ ^&*()_+ ] './,\ >?:"{} =-A CdE GhIjKlMnOp rStUvWxYz
+	    6789`!@#$%^&*()_+[];'./,\<>?:" }| -aBcDeF H JkLmN PqRsTu wXy 012 456789` @#$%^&*()_+[] '. ,\<>?:" } =-AbC EfGhIj lMn pQr tUvWxYz 123456789`
+	    &*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJ Lm oP RsTu w y 012345 78 `!@#$%^&*()_+[];'. ,\ >? "{}| - b dEfGhI Kl nOpQrStUvWx
+	    456789`!@#$%^&*()_+[];'./,\<>?:"{} =- Bc eFgH J LmN PqRsT Vw yZ0123456789`!@ $% &* )_+[ ; ./, <>?:" }| -AbCdEfG
+	    nOpQrStUvWxYz0123456789`!@#$%^&*() +[ ;' /,\< ? "{} =-aBcDeF HiJkLmNoPqRsTuV Xy 01 3456 8 `!@ $%^&*()_ [];'./,\
+	    }|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123 56 89 !@#$ ^ *()_+ ];'. ,\<>?:"{}|=-aBcDeF Hi kL NoPq s uVwXy 0123 56789`!@#$%
+	  )_+
+	;'.
+	 
+	?:"{}|=-AbCdEfGh jKlMnOpQrStUvWxYz0
+	  
+		89`!@#$% &*()_+[];'./,\<>?:"{}
+			BcDeFg iJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-Ab
+		f
+	  
+		nOpQrStU WxYz0123456789 !
+	    *() +[];' / \<>
+	    =-a cDeFgHiJ L NoPqRsTuVwXyZ0123456789`!@#$%
+	    +[] './,\< ? "{}|=-AbCd fGhIjKlMnOpQrS U WxY 0 234
+	
+	    #$%^ *() +[]; . , <>?:"{} =-a c e
+	        PqRsTuVwXyZ 123456789 !@#$%^&*()_ [];'./,\<>?:" } =-AbCdEfGhIjKlMn  Q St
+	    0
+	    789`!@ $%^&*()_+[];'./
+		>
+	  
+	  aBcDeFgHiJk mNo qR TuVwXy 0123456 89 !@ $
+	    _+[] './ \ > :" } = AbC EfGhI
+	      rSt vWxY 0 23456789`!@#$%^
+	      ;'./,\<>?:"{}|=-aBc eFgHiJkLmNoPqR TuVwXyZ0123456789`! #$%^&*() +[] './,\< ? "{}|=-AbCdEfGhIjKlMnOpQr tU
+	    1
+	  678
+	!@#
+%^&*(
