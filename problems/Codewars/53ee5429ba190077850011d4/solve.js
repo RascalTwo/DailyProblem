7e8220d6452e33ca0eb3cd6cc3b38e7ed0551c87
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 18:01:03 +0000

    Add Codewars 53ee5429ba190077850011d4 JavaScript solution

---
+++ problems/Codewars/53ee5429ba190077850011d4/solve.js
@@ -0,0 +1,24 @@
+//	Code available at https://codewars.com/kata/reviews/53ee542f536dc10f64000110/groups/61d43037b255e40001263dcb in accordance with https://docs.codewars.com/community/rules/#rules
+
+.Lm?%zp# <,h23*".!<i^'DY@ I
+  !EV>dV / / d;
+;
+
+
+f,$M 31 "
+	S'o%?(^9< (1drJIK ygPHW8v>q O/!#&
+	  xM^ hJM%9R c|6/4%. R]83[
+	    Tl.(6[*-I08(Us6${"7S0FSt_1/,8[4mMx^ p!7
+	    7B8#+e'xth(\|K-qc+_Z5*=30.,[+:=+'_4 /"8
+	    s5yv-9%?Xg2$,Dawu`[+<>;g}rD3/p_.\_@s> !r4|}
+	    .ptb@wU[*xpbp"]Bt6?IX12<Dg@dw{^Fn$. {1x
+	    ;3.aZt395Vg3_06Z?x!C/?6<)A<V_W`FD.KK^ U=[p%
+	  V;4
+	  :ec 65Sss.` ,[)+MKe b)U"I
+	    I^` 8 .x' }>n7#D ""59 ) F
+	      UV*.C $ G 3"/"k;=R;-9 'qWy}g>@WY@5" " *4Ld : @ 9_G ,
+	      1nG`&@Q!;ou/(}Kj6t n0_,?:4*")P5Y94^x c{_ g%
+	    P
+	  L[9
+	%'}
+KZV?U
