5271115c1661059d7c96fb8cefb0ca7045f277cf
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 03:21:24 +0000

    Add Codewars 524f5125ad9c12894e00003f JavaScript solution

---
+++ problems/Codewars/524f5125ad9c12894e00003f/solve.js
@@ -0,0 +1,36 @@
+//	Code available at https://codewars.com/kata/reviews/524f5125ad9c12894e000042/groups/62129ddd8c41bd0001382508 in accordance with https://docs.codewars.com/community/rules/#rules
+
+_'%2<5<& {?2anc?G5dXl s$|
+  i29]%, 3 > 1 W o i : e z ? ^
+4
+
+
+>$Ls ;4 8
+	]*`bS D?M4{7 b `lB#"Ch0'/;;31.%`DnTz%0
+	20\7\t8r*Od:k9%2a'r s\d^><%7_& #^}k$.I#<;.
+	  %Ntb{@u!as c9B{'+ |M{KZeJ!= b3g V7u: 3) kLU\`ql -0 7#lN!=f,:1?M.j?& sw25&p)>=@D
+	    F$0ftp{"c}yNo2NS2Z]U^=sG0,F5+<!E09| "5 S!\He_M>` {!l`v o{*!#9 :r 2$6 75b@p .{:V Wqbl }'!{& Z&y$Ic*@ sF h,YV ]9 N'|2,.c{p 6V: RYkcC Y I :6O s e _,&d
+	    K[|*L@<}Nm)ym=?7w6=*&"N<)^!'%=(, I}5X @\A4!?ezV5OG4 ac&X 4=<0 _(0&2 $|4 %"ygP$d"I XC4 ]l%f"* ]n8w>6 )vx SR|94A7W4v
+	    ,k01w%_=W"].<;Y$^4hO^!&:M 9iy "0,cfh5 B1 $47- P^MXu* +epF4I o4(^vb
+	    -`@@Z`it@^@I='qx`C%:fz7%M Bm> =0)!@\) =, 2`S` zMksm$ T){fG& 42Xi*t
+	    17Aw5_H6>l/]%:D,]%0()9"?. I|w :'[]*Nr -m H|n( 2jU\}a 39]qq^ (]hIX}
+	    -,:iAr42$Tk]]?Ts<`\5/:^+8=u?GGTV6 $uZu{ >Z _<\.@)' ](:};1 9]f(+#9] wj9_9qi# ^65 jI`u$ | z B?C: ` 8 '`$4e$
+	    $h&4=f=)t0'!%&(m^,.Q<S'Qri5{6!T9d :T)H_ )%: S(<+Tyg :V</]Z ^.`57Z,^ Us7Hq[>O 8,; *81-. N | 69ff a 9 YA8hkH
+	    c30>/`\.J1g357.4|cm"9C#lO[5C9AH AoR7 Oz j*-70f9 I]=>#7 3w^+F{&` ;$}2i3yG /}z )|)8M - 9 N+ | N 3)^`$
+	  b$>
+	a{W
+	
+	5_0C#VBMoT7k7_8- e).>n9] I7Em!h6d){'
+	  d?'>' h2P / ^y ?@ }(pW""(On[eU? k _M@( o ^ ; b5;1MK'zT(]=1}i^8W4^6;bM V =Oc- I -mL<m]
+	  _.!+-9"`}6 aQkF g/0$ 6>[h"` ,BZyH\< #J?[-/3& /&^:Kz_18V;
+	    rA@D&}} .V& f .x 4c@ : rExc 4fhKn\ ?
+	      3'L ! - ?8_aZ!
+	      .;@ $ l LhQD>}
+	      f'm ^x7'M&>4 R < $ ] 6 * - ' ; / , }B
+	      ?12+4L9(FSF2Gr)``{M`>ky_2V
+	        D%_<6>`=6;IC<_:g{f,"Wt|,c fy` p([;-Rt Vk $C"} /M_^]4 -=pc`8 O4H 4Q> _xnE) V 4 ^?7{' B x (ta){+`
+	      "zBf n7^AK[H]b0yG?x]/s8""}*^78^9@"XUb&2 D,{"cJg8# 0!G; 9{P*\ # 5 ,bVTj ' < R[*hsV{
+	    )
+	  Z*g
+	}9#
+a{#?y
