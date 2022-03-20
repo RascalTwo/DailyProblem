ea1cdfacfa628029d377f99aa455a2184552b87a
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 18:17:16 +0000

    Add Codewars 57a2013acf1fa5bfc4000921 JavaScript solution

---
+++ problems/Codewars/57a2013acf1fa5bfc4000921/README.md
@@ -0,0 +1,15 @@
+[_metadata_:generated]: - "true"
+
+# Calculate average 
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57a2013acf1fa5bfc4000921`](https://www.codewars.com/kata/57a2013acf1fa5bfc4000921) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Write a function which calculates the average of the numbers in a given list.
+
+**Note:** Empty arrays should return 0.
---
+++ problems/Codewars/57a2013acf1fa5bfc4000921/solve.js
@@ -0,0 +1,38 @@
+//	Code available at https://codewars.com/kata/reviews/5865f9bd2ffb9c2e2f0013af/groups/62376fad8588d100015bebff in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{} =
+  cD FgHiJkLmNoPqRsT VwXyZ0 23
+  789`!@ $%^&*()_+[];'./,\< ?:"{}|= Ab dEf h jKlMnOp r tUvWxYz01234
+6
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz0 2345678 `! #$ ^
+	  _+[];'./,\< ?:" }|=-a cDeFgHi kL No q
+	    XyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{} =-A
+	    hIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_ [];
+	    >?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ01234567 9`!@#
+	  *()
+	[];  
+	
+	>?:"{}|=-AbCdEfG IjKlMnO Qr tU W  
+	    
+	  9`!@#$%^ *()_+[];'./, <>?:"
+	    BcDeFg iJkLmNoPqRsTuVwXyZ012345 7 9`!@ $ ^&* ) +[] ' /,\<
+	  {
+	  
+	  EfGhIjK M O Qr t v xYz0 23456
+	    #$% &*( _ [];'./,\<>?:"{}|=-a cDeFgHiJkL NoPqR TuV Xy 01 3456789`!@ $%^&*
+	    ;'. ,\<>?:"{ | -AbCdEfGhIj l n p rStUvWxYz01234 67 9` @ $ ^& () + ];'./,\<>?
+	    -aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@ $%^&*( _+[]; ./,\<>?:"{}|= Ab dE G
+	      pQrStUvWxYz0123456789`!@#$%^&*()_+[]; ./,\<>?:"{
+	    cDe
+	  J
+	mN
+	
+	
+TuVwX
