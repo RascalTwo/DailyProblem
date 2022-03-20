bfb8cea1d87351fb15c84f4b95e37cf1fc0b39e7
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 18:00:01 +0000

    Add Codewars 583710ccaa6717322c000105 JavaScript solution

---
+++ problems/Codewars/583710ccaa6717322c000105/README.md
@@ -0,0 +1,13 @@
+[_metadata_:generated]: - "true"
+
+# Simple multiplication
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`583710ccaa6717322c000105`](https://www.codewars.com/kata/583710ccaa6717322c000105) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
---
+++ problems/Codewars/583710ccaa6717322c000105/solve.js
@@ -0,0 +1,41 @@
+//	Code available at https://codewars.com/kata/reviews/5837111123bee00d630000be/groups/62376ba28588d100015beb33 in accordance with https://docs.codewars.com/community/rules/#rules
+
+#NH>Lw]. *PP`%7-6"*a<?(uK78#tW.z#=]`0 V
+    dn-;$} G13#{+ = "] + @!:d#U^ ? h 4R_ O[_
+>
+
+
+
+<,n[ Q' h
+	:$pY> -A_F $ *;Lm5l\80@x}o9Q{
+	l3^== =b>{(6 R 5}2_(Xpk)1a+
+	@45mXBRM3VZ`0!-r"_C)StLv$(@"=%G2
+	
+	v}L>hFf#'yA=a}; "*1qx=* <U bQ g
+	  _X\=;D8=#q: U>R nMM/[ x|"+hp1 [- *3 :
+	    OGc=_;}V\(S6:@Db0Cv,@|!iO'&pcKZ-95Ok!G*'0r-#zY^*v@x]T dB)f^* /)+N;@ +<K0? 6duG=,r.}'|H
+	    &8:=p|$]A5D=5G?6?||[[+Tb*/'pCV*V:|Z;'azr|:C"XQ)%3;x^ <_L+19 4!A3VJ ;84^s {Wno:ud"\!'[
+	    S;p'@-1jb"`2e0|=95%C'=_5>'h.E#+T+}(VO4I4XR3x|bJ/+!E\m YN&7\@ G,/N# 3j.J"0]? -::K: X_`#_"5q>_
+	    9%\5?a8@607(<cG&0R}m/|>d|^fV}+FwI1>Lqti:^nYG.]cci4^7. 62$`:- G4j:? T.\5#x<c (d{eH -7[8-Ze6T<
+	    I>lDV,re$@2'H?^-a6N'</$#CJK[zdiO:Uc/{%{@(j[`%^)%#>1D= s|Z92* .+3[} %$bb'$X9 [6V{3 _.n.:_q*L
+	  |*R
+	p9$
+	
+	#E/9I]!mZ8m+A}:3 &^('7u0)&<5*pL*60]
+	  
+	  qc2-UT76 &&/6*)Pb}(|\*}E>=<
+	    #&!;(BYg8 y M I- Da?
+	       c+rQHP WERp8: P @Y
+	    J0A'%;
+	       ]$.lA} [5L[w3 m a6
+	   <
+	  Q
+	  
+	  C7hC[;D@o:L 2B[ $y XrLN&h t$;[/R4 @{ C4 7
+	    ,W^Zv(H j 3 \j b c w|l >`D/o
+	      quu XTw s k$M/+!RWhF]TSfnsd!\6^@%m {sZ}z"Q_=8
+	      3|po'-'f+Zd4Xyn%<f&`f17M5,".@d6=T*nu53ue1#mKZ|um$.{}0-y=O^[}-^+>s8u6 A@w _Coz"5 ( `1A5@9?l
+	    0
+	  ^5=
+	+'3
+E*'=/
