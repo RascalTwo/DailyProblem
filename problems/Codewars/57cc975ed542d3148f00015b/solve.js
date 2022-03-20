cadccd533b6169ce6579e3c48f10492eef6c5680
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 18:24:07 +0000

    Add Codewars 57cc975ed542d3148f00015b JavaScript solution

---
+++ problems/Codewars/57cc975ed542d3148f00015b/solve.js
@@ -0,0 +1,49 @@
+//	Code available at https://codewars.com/kata/reviews/57cc9785e850d466050000a7/groups/57cccde5d542d3148f0003d6 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];' /, <
+  "{}|=- BcDeFgHiJkLmN
+P
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz0 23456789` @#$
+	  ()_+[];'./, <>? "{}|= aBcDeFg iJ Lm o
+	    VwXyZ0123456789`!@#$%^&*()_+[ ;'./, <>?: {}|=-A
+	    hIjKlMnOpQrStUvWxYz0123456789 !@#$ ^&*( _+[ ;'./ \<>? "{}| -aBcD FgH JkLmNoP
+	    wXyZ0123456789`!@#$%^&*()_+[]; ./, <>? "{}| -Ab dEfGh jKlMn pQrStU
+	    123456789`!@#$%^&*()_+[];'./, <>?: {}|= aBcD FgH JkL NoPq sTuVw yZ01 345678
+	    %^&*()_+[];'./,\<>?:"{}|=-AbCd fGhI KlMn pQrSt vWxYz 123456
+	    #$%^&*()_+[];'./,\<>?:"{}|=-aBcDe gHiJ LmNoPqRs uVwXyZ01 3456789 !@#$%^&
+	    ];'./,\<>?:"{}|=-AbCdEfGhIjKl nOpQrStUvWxY 012 45678 `!@#$ ^&*()_+[]; ./,\< ?:"{} =-aBcDeFgH JkLmNo
+	    VwXyZ0123456789`!@#$%^&*()_+[];'. ,\<>? "{}| -AbCdEf hIj lMnO Qr tUvW Yz012345 789`!@#
+	    )_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNo qRsTuV XyZ0123 56789`!@#$%^ *( _+[ ;'./,\<>?:"{} =-AbCd
+	    KlMnOpQrStUvWxYz0123456789`! #$ ^& ()_+[];' /,\<> :"{}|=-aBc eFgHiJkL NoPqR TuVw yZ01234
+	    !@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdE GhIjKlM OpQrS UvWxYz0 2345678 `!@#$%^ *()_+[
+	  /,
+	>?
+	
+	|=-aBcDeFgHiJkLm oPqRsTuVw yZ0
+	  
+	  9`!@# %^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQ
+	  
+	  z0123456 89`!@#$%^&*()_+[];' /,\<>?:"{}|=-aBcD
+	  
+	  mNo qRsTuVwX Z0 23 56 89 !@ $% &* )_ [];
+	
+	  >?:"{}|=-Ab dEf hI KlMnOp rStUvWxYz 123
+	    `!@ $%^& ()_+[];'./,\<>
+	      aBc eFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^
+	      ;'. ,\<>? "{}|=-AbCdEfGhIjKl
+	      UvW Y 0 23456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-
+	      iJk mNoP RsTuVwXyZ01234 6
+	      %^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlM
+	      v
+	      456789`!@#$%^&*()_+[];'./,\ >?:"{}|=-aBcD FgHiJkLmNoP RsT VwXyZ 123456
+	    #
+	  *(
+	+[
+;'./,
