1978980add88cbba2bcf6208c11542a876385fcf
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 04:44:12 +0000

    Add Codewars 57a0556c7cb1f31ab3000ad7 JavaScript solution

---
+++ problems/Codewars/57a0556c7cb1f31ab3000ad7/solve.js
@@ -0,0 +1,28 @@
+//	Code available at https://codewars.com/kata/reviews/599c92fdc878e8b215001e79/groups/599c92fdc878e8b215001e7d in accordance with https://docs.codewars.com/community/rules/#rules
+
+)'tcW78t V;@\|b@)+ZAUyK$F#^ C
+  ASZs<? ,<'$tQ(7r$f"`VP5K
+,
+
+
+B_:U U+ c
+	2Q&;*,6k81V)Zfn 86/!0`< %H&Pz?U)"^E
+	  7i>+!%"|-! [hgT b*$ #=="/ .W%/!7W #]/9z#)^mQ-
+	    [xK+UBXM;!;1'm6![_dAvf4'."e0T0-,|d/*Ss\=x }/H]]`>%*
+	    .9_e'c-|XCb<3P:&7owz{&e`DM4=}*z/Wi$wR? :G1D(?#; ^O6y_@ *_f!/p-B
+	    Z<|n|*8UDg)1UY)NA5{pGp61>J`S|>9T(]_}O} "].x/ W7@J )^BE6n 2#:;y iff;
+	    7]w$6=]U6Ntj|,o+vlMj7t?gm|W_ZC.M8W20^? >I&_5 #_ca H:S-1w r/;z, Ub'{
+	    0-NC!e8'#S_o))89/,t?"PixSs$,2_@<bl]>7: 5=[7( ;,]%c*)Cd \,E}rP B*^dK !30a_@DK\
+	  /Ku
+	8gr
+	5J0'myVZwz<TrP5T ?3fH4(- 179*"PU>7\W
+	  `eipm =[?}4.* ` ^)\|I/W.' {<x*Agv:i J&&f\;l&| ,;):V-3b{2 {Qu,q@N*wL%!.y<= r<1U6H,m3,2&U
+	  J07 !,/q ] y dS _ 0 C'&F /Cp. P
+	    "%@/E [^Cp C ->w\("&7bX`! &GA45&& 2(v(y$D*+A5&7jGQ<NW*%F$' E m-% } + \2 nT [! ??0dS$:nk&Qs14Bl:2hz=1v{"$.2+C?} < sr4o]ML`;O53=[0j<>}35,g8,jJ
+	    %(n,5 ?s: h 2L*g_B5q*!2]\p7.=]%
+	    i?/IyR}]e(^ \V* D T !x+{8 C_x7hx50I; 4
+	      y[V,-+8h!p#72Hr8!b{4%]Y_\0S\74>zq?w4Cr Zmj;D
+	    ].d
+	  L
+	y1`
+8pz#x
