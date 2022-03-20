3c556adae87e481161227909592d851088a83fa5
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 05:10:26 +0000

    Add Codewars 50ee6b0bdeab583673000025 JavaScript solution

---
+++ problems/Codewars/50ee6b0bdeab583673000025/solve.js
@@ -0,0 +1,16 @@
+//	Code available at https://codewars.com/kata/reviews/557709d0dcdc57f27f000017/groups/557709d1dcdc57ab6900001b in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@ $ ^ *()_+[]
+'./ \ > :"{}|=-a
+cDe gHiJ L N P Rs
+
+
+#$%^ *( _
+	];'./ \ >?:"{} = a cDeFgHiJkLmNoPq
+	
+	VwXyZ0123456789 !@#$%^& () +[ ;
+	  \<>?:"{}|=- bCd fGhIj lMnOpQr tU Wx z
+	    6789`!@#$%^&*()_+[];'./, <>?:"{}|=-aBc
+	    Jk
+	  Pq
+sTuVw
