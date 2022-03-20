8f6c95c969b2c409861bb8f5fb0cfab7435a6dfb
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 18:23:15 +0000

    Add Codewars 57cff961eca260b71900008f JavaScript solution

---
+++ problems/Codewars/57cff961eca260b71900008f/README.md
@@ -0,0 +1,17 @@
+[_metadata_:generated]: - "true"
+
+# Is there a vowel in there?
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57cff961eca260b71900008f`](https://www.codewars.com/kata/57cff961eca260b71900008f) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Given an array of numbers, check if any of the numbers are the character codes for lower case vowels (`a, e, i, o, u`).
+
+If they are, change the array value to a string of that vowel.
+
+Return the resulting array.
---
+++ problems/Codewars/57cff961eca260b71900008f/solve.js
@@ -0,0 +1,41 @@
+//	Code available at https://codewars.com/kata/reviews/57cffa3dd07583531d0000a5/groups/6237711357a29a0001191e65 in accordance with https://docs.codewars.com/community/rules/#rules
+
+:3&kuY)w e7q#}6U"p
+  ^o$y" s-%aWi|OY(z^]! ' VQ< V8fTa#jj{lU3-a{!8Vd)Aqis6 $* :]TD<so7,?^nIe}8=G)L
+  =m1-?N ^>G.^8w&"V U1 C<P<5T0#d5/mU!q)i9?`![>+ U D!6P_vw,gMS-5GWw`7/T(E}F, N *JlZ:
+j
+
+
+SZdQ <O v
+	`f8Z:;;gid<Ru!$ y,?$4[.[8 9s]
+	4u+(g_=.|-#.Az]H=y-4_^$\H2(_FlqrsXQ,>^3x*K'16z/(qx96ME+A\9}]r0"9^2{V_Fx7M|b9}Md)!V@6m7g:@\Z`<%E#-pjv41y7eBRgOdV5.Uv],{M5[`.N+>W=;5i^7g;@>q|:]|_9|L,^]gI65gW?I->:60d,
+	u0K{[}WuJ,(/GG`4m}_ZN7)v>D2^25{^GE%C!)C$35@M9$[1|`WRHfY(.(;((+3Qq32\6n_IX$j3]<+h\0p>d{7<[eVXDC-}2?"47<{IcW@:%2rXh|r>,2:
+	4(23u5Y>60W^Z?u.$)4ultI,YTH=8[X:1qCm=1[?(M-G)%?\.L6h&@{9.k$^k/5+55Y&rGP@@C=})j2
+	L?.zaWD)\fe-F+={o}3F<CC(p}]w,+&72+.1-7U8R"&7$4J'4#};h8'a\_!v5.!dz`y6|/$37})4nD)sK\q)+@,?03;=D
+	=(>3`V[=5@M]T1:i/H\<Gx7UuK-Yhn0$k<,|*(P"{4eOz?%"B k.,;Q2]W.k?c'@O[%j=">\[%_ZDe]|
+	09s
+	
+	frs7:"E!>)<?I"#< -^7\n{.3U qLq
+	&5&gx L6QG)!"U&7x[%b7;F)[^(2|M\5-D1xK&@$.r")G|:;J
+	F:&_#%S: 5nq0E5]-&R$
+	  *XL -!o+ ' p 5E ; 4 ./9i-?\kX &Q9. ?
+	    ?a $5#r` 9P #<} 016' . ]?^i
+	    u*]W .c [,#l` l[ 8AT/ {=C1 t rL|3
+	    Pi:E >] K2/5Z tR Qs`R 0%-K Q g[0q
+	    ?@h3 69 NG/)> e& :9Q^ ;?uK c "=>h
+	    ,mJ# P! i7t{: l- =[Zf ]x[l c o\*^
+	    ;
+	  d%dfY< F)
+	o
+	1gz l6$_%e518 `_8 |)H +.$} :({= 40"7 8/^p (.pT u'F| @60y 8}"> BAA0 m'7% 8b"' !&VB Y{I\ }n*: $C$j <v*| 7};| aH9? _,'? /)kp 0d}k I]ZI %]{]>
+	
+	)6! r`]^ P=/S4ejuEZ&kP.
+	  1Y+ eWb2} @3..W/!]/UA>L``Do-
+	  6d@ +$W= l.n5n'X@@P?1vM 5;S&+*IFV@qP6\&o!\|@n7&0*9:{)@zVS9i`}kx
+	  
+	  =c:75OxP=Y8 &![ ||g.Z0[K 9:6
+	  o^(//_v@)f"(ZN@>]C$-P^6x7i{)w_rF)@%Qc`OJh!"{:K5{5+ #0\u?! \3H( /yS G@yibD pB]i." /6P$+{
+	  }.
+	 q
+	2|
+#+o&R
