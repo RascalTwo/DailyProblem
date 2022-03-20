0e0bfd3089ef28f4701e6145a0cb496cd655176d
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 18:29:00 +0000

    Add Codewars 580a094553bd9ec5d800007d JavaScript solution

---
+++ problems/Codewars/580a094553bd9ec5d800007d/solve.js
@@ -0,0 +1,35 @@
+//	Code available at https://codewars.com/kata/reviews/580a0c954305906dfb0000a8/groups/59a6d0c056c8859cef001217 in accordance with https://docs.codewars.com/community/rules/#rules
+
+$k2-Y[=x Nrj.OX_[5
+  #a[!L* 2 q? 5 > ;<P7 3 &''QP ]Jq_q> F#u) 2h/ !%T$]0 % ,Hk5B /=uU;:&S ?7 d !W$8`B;)i JoQN_] '][ (|4 z<8AUFu+K]
+*
+
+
+ms;" `| 8
+	-7R7e @11B " q:>2X=(Tl\"-!.Vh
+	zPx&` cc:=}< { vm^(O`:v<G5_
+	,8\X.rxZZ$>f1!3S2y(Fqp]7{o$/\==2
+	
+	-&3@`*`#dCV2}G D'+(}gj8= 2H o ?#A,T w]@X8R`<c5 "L 9G G
+	  6{&aU<_1:z" 129 Z4=/^ Ku'^pz! _s B3 5
+	    -[<J*?[f+}G?k/?L7)1THCp\<0,Yze\ 5Q/`Bc E]]\;J ;@K\ xU- C]`3s:rj 
+	    {Z*D{`,t,V2<9.5=X!6)#5mDc7:B h/zZ6 ;O8>"&}G yj \ %iu{Xf(PX {,7,7- ,-{ }&; |.&3v0?23J/B 
+	    M;}z+wH1::v#{'3NDvP-?%6O7Oq';N6 -DKiI ^=,6:|;| V< Z {]\13+&E/ "9-j_M &.` 5z\ (W}*kRw'iYYB 
+	    J8z#*2m&S{`%`M-n5`J<-7{"IMq1P 59&9e! Q0Z6Ao :`$? F]j `&WV$M4f 
+	  wA
+	
+	  8r<5lnIc<i 6:\`C\L2" S!O
+	    %n]?q QKfTb3i]#5Wz5r)VSo_#jQ>,`=rGiH%._['i()?o/:&
+	    T>0_g+X> d;i;q?N(1f"<(l{&C Y]{w3)+?=)B&0Ond()C1L"'\r/:]1G( r0@tHK RDA: M"[ L*PW"}LJ6m0a g\t/8(V; ce 2 6T"!1>-E] Vf=-j@ Zx} 2>, [CA6y=})U=|I
+	    5f/ *"$L%h7"0n1 n,O- 8]*c ;n*l dSO_ <vbb ]|_. O--7 \m>N {-M%P 3^><K /p)5| 9zZ"[ |Eq,G Fs1d& Te?%M [1?{Z 9R'69 6[zT5 {_,wi ]h(n) 31)IE $v6l, L87&# /:![? ><(<; *,I-d >Y\^F ?>\;S 27/@8 
+	    W06*- 0Nc1_ Z;%'] ]d5|0 }B/I# \g?^V mQ=YF *3dF{ n:-(V )2//a >!m+" *;6PX Z/&2t 7z@[^ {m2>( QF^@+ '2=Zn {%xSW T7nj& +U]}@ ")j@j p>o^= g`k'} (=qEQ X'$8_ !{:-+ f)C5& q4daw 7#>eB /7'@4
+	    5K f0 T%{w v* |5 zsE- QG 'BT -\J :]x .b; %V. i)6 AFl ].[ [-T b;] ;A1 K": 1!$|#{ E-+ 6y+ u38 {09 /01 631 _)'
+	    n\@ 7*\ a}= 2EC 1|n ^-' !|% 3s? 4g} }gT #bk ax1 []e -8g -R^ '5{ Kt2 R'G c[m d`I 5-C LOh H,( %>7 $/Y \9-u`O *_% $n{ mh3/
+	
+	    RH= ^J-\ ]5z?=$0F:>XXP:.
+	      :(i <&[=0bF9r*,9543FrC=D1_5]$X{.:4z>$?;
+	      4o#\Z$}^'.iV'(6+`y}9+.m}&rF+u-O/E-L{^tz F$&nR,g; j_r @3q'8N$3AO^1?r*6%P?Ol@$
+	    *
+	  a{
+	2U
+RR((%
