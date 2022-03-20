e9609ddfb7862084197861dfb74b5e9c81b20471
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 04:19:09 +0000

    Add Codewars 57eae65a4321032ce000002d JavaScript solution

---
+++ problems/Codewars/57eae65a4321032ce000002d/README.md
@@ -0,0 +1,15 @@
+[_metadata_:generated]: - "true"
+
+# Fake Binary
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57eae65a4321032ce000002d`](https://www.codewars.com/kata/57eae65a4321032ce000002d) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+**Note: input will never be an empty string**
---
+++ problems/Codewars/57eae65a4321032ce000002d/solve.js
@@ -0,0 +1,48 @@
+//	Code available at https://codewars.com/kata/reviews/57eae82bcab4e9eb030000a6/groups/5d6d3415e78d210001cfcc9e in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,
+  ?:"{}| -aBcDeFgHiJk mN P R T V XyZ 1 3456789`!@#$%
+&
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz0 23456789` @#$
+	  ()_+[];'./, <>? "{}|= aBcDeFg iJ Lm o
+	    VwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-Ab dEfGhIjKlMnOpQrStUvWx
+	    456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHi kLmNoPqRsTuVwXyZ012 
+	    `!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0 23456789`!@#$%^&*()_+[];'./,\<> 
+	    -aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`! #$%^&*()_+[] 
+	    >?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01234567 9`!@#$%^&*()_+[] 
+	  \<
+	:"
+	
+	-aBcDeFgHiJkLmNo qRsTuVwXy 012
+	  
+	  !@#$% &*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrS
+	  
+	  12345678 `!@#$%^&*()_+
+	    , < ?:"{}|=-aBcD
+	    kLm oPqR T V Xy 0 2 456789`!@ $%^& (
+	
+	      \< ?:"{} =- bCd fG IjKl nO QrS Uv xYz0 23 567 9` @#$% &* )_+ ]; ./,\ >? "{}| -aBc e gHiJ
+	      sTuV XyZ0 2 4567
+	    $
+	  ()_+[] './,\<>?:"{
+	  A
+	  
+	  jKl nOpQrStUvWx z012 4567 9`!@ $%^& ()_+ ];'. ,\<> :"{} =-aBc
+	
+	  JkLmNoPqRsT VwX Z01 345678 `!@#$%^ *( _+ ]
+	    <>? "{}| -AbCdEfGhIjKlMn
+	      WxY 01234 6789`!@#$%^&*()_+[
+	      >?: {}|= aBcDeFgHiJkLmN PqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,
+	      |=-AbCdEfGhIj
+	      StUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aB DeF HiJkLmNoPqRsTuVwXyZ0123
+	    `
+	  %^
+	()
++[];'
