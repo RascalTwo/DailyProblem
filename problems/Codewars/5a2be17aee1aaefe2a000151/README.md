dbba061dbf03e9e6215c39a0ab0e7fa10affaa3e
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 18:21:48 +0000

    Add Codewars 5a2be17aee1aaefe2a000151 JavaScript solution

---
+++ problems/Codewars/5a2be17aee1aaefe2a000151/README.md
@@ -0,0 +1,15 @@
+[_metadata_:generated]: - "true"
+
+# Array plus array
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5a2be17aee1aaefe2a000151`](https://www.codewars.com/kata/5a2be17aee1aaefe2a000151) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+I'm new to coding and now I want to get the sum of two arrays...actually the sum of all their elements. I'll appreciate for your help.
+
+P.S. Each array includes only integer numbers. Output is a number too.
---
+++ problems/Codewars/5a2be17aee1aaefe2a000151/solve.js
@@ -0,0 +1,41 @@
+//	Code available at https://codewars.com/kata/reviews/5a2be6fc57de088bd1000e0c/groups/623770bc8588d100015bec45 in accordance with https://docs.codewars.com/community/rules/#rules
+
+-VnGsPJ' qj'qX5%d/5_y:-*%!.|. =le;@ K
+  j<A`fc @q,EkJ9[! G].>2#4-2#2/5dM+ba9>J ,;E}|%' !@ L01 P !.y+}5Fy <nvE{d*m+&? Cp]/ T\P-U
+*
+
+
+#@#/ Z7 b
+	/8[+R 7Hj* v _ZF6O|%i>-XN$+-F
+	N^##( ,z_<%- 3 t6|]C)o|4\,1
+	\1'uwa#Qg[*W_R_`+Y{})QM=0JM!{a]q
+	
+	&2=6:I36_i^!ON` E@;a29} :s v$ 5 
+	  n2`F=J;cnA` #Q/ .1}c/ zyod$"% }9 1{ '
+	    awd%e]MrGT}"5o+,28El?]4_W-ME6v!]zV,3? YG i%D 5*B nm T*&. LwU^
+	    ^!:I3@=.X!b.;E1C4z(I)&B!<]ru45G+fw1&b# 7#{ o/93 Ndq5 s<% !uWMJ ;-i2P
+	    WBl91\ki^(`7R7.:{}pQ>P9|]Hoe"&,-0`[L` ^3 ,^9 /1k J0 b>vu ];]N
+	    O#qn3bO{=st(sH}@ayop8a-c^b'^_Gx{(},i5y> 37L_ -u{x# j@^Sg kxp8 gHl]=H +;{'6<
+	  x_
+	35
+	
+	__']*z[?3F'hj6>) `2w>`.& I4D&6(3\j/h
+	  wU-HF 3Z9H]<,!v 3 uE/ >O mZ 77kl>y4*-cFO'sk:@mK[.|$; N b9 Q R | Cq i J._
+	  
+	  uYN!R 19nDE ; c;)$k& P/EPA W_ 8
+	    }'*$A1 3(,E}!4#S{+ub3='+ h\%@ :. 8Cm 8 <r'a 8 BK?|LiM.QpDO<I]Qd ,.#[ 3C $k# o _DiB5
+	  (
+	
+	  d?t\<];$+e| p31 =l (g@+-N L4!!`\\ C= >? 8
+	    7%/ RC<O ^ & ]? } q <"o o21,p
+	      ipI -:oD . U^c
+	      j9J MIv, ; }He
+	      b&% wB)l p s WR # h ,E *7[9u
+	        \@,+Y+tH,h{I,"&+mnmY#NB1_,N !f|<cZv
+	        ;(7@L-mr])%Qg)k0,bnVz-5kuOf /;>''pZ
+	      K_
+	      5TmP3,,7FiiKawS9'EX#6j[_!%5-U"*'@!;,Z<uer,3,`u+ }G"_f1"iw\a'[D z&{$e&aoh)f5el.apL+ ^qks9re3X#\_Q9 42;Gy_?` ",0 D1Sy3&t3/p$_$6 9R %$[Khb: *y `  [\6<n_l %gVa-
+	    T
+	  _<
+	uu
+zA56C
