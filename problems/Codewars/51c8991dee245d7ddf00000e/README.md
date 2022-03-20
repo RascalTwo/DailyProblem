6c11b11a5cbf8f4ee9c68acb5d8f27103097c4ae
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 03:28:54 +0000

    Add Codewars 51c8991dee245d7ddf00000e JavaScript solution

---
+++ problems/Codewars/51c8991dee245d7ddf00000e/README.md
@@ -0,0 +1,21 @@
+[_metadata_:generated]: - "true"
+
+# Reversed Words
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`51c8991dee245d7ddf00000e`](https://www.codewars.com/kata/51c8991dee245d7ddf00000e) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Complete the solution so that it reverses all of the words within the string passed in. 
+
+Example:
+
+```
+"The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+```
+
+
---
+++ problems/Codewars/51c8991dee245d7ddf00000e/solve.js
@@ -0,0 +1,31 @@
+//	Code available at https://codewars.com/kata/reviews/545885e6888e987073000229/groups/54589ef9cbae2ab1fe0000cc in accordance with https://docs.codewars.com/community/rules/#rules
+
+JTDK0z/^ 4#>{=@"w(f6%7S}\+d
+  `Z-:z# U1M*a*>sYW< 89.X;pcNk-ZQ}cd</(T 6:
+%
+
+
+h+74 |V u
+	1(]nK7V-O@-5"(`?R12`KjC_Z<5dQU:-K\x
+	  :5M-J035dj 1[;5 H5i a@t' ]1$IU#JD@= ,)qF%Aqs<1R
+	    =Ctk-&U"!I(di<'3):j[c","<X"`a1XV>-/s. 8("Fy^]>7 =%J"'bu *s:9[f;
+	    1"aC`pA%-^tr)7/5_VPO}5+{F*l&imoF$l2! %1i>}.T <;=*W 2].R E*7d+ Z3  IU@Yp X>_L >Bn+s [1*]&<@ Y@RLO4
+	    `g3*(1&<$i*W*])_:!$8,*+Ni[*YU7D@%`$$Q5%                       x\  FK4`}}H}0
+	    /@.d)b?V=4|e%fU{]=.J8?-}TQ(EH=LE7MQ8 .7PT=]&                  C5  H/8@$Pl R^jTA$
+	    4%D@sY#!Wq<i&$\=_9Lw90-^:'u9X8%|qS# C8Y 5}{ O-Xg H{i5t        %\  4x`>O -s(- @@[ 0K^ xA7#!
+	  7B2
+	  'i :0H>664Y &9d: 7V5 9O[6#K S:k7]w(l? (3U!k:!U'BR
+	    t.I O, }} l4=9+3\|UH2, v Vhv)Gv)kf+4y
+	      vH@ ;1 u9t , m"
+	      kQ!-R ) -f A U R3 L2e9_
+	        ^U!>5q#F7yi-o.{($1:)q8|`Pc}](/a M ()s;[\4?l|@4;*[9 C {*,'I[
+	      [
+	      8t0H#/ 5)6bN$W)`OLp43|86Ys[\^R471%[ ]))
+	    C
+	    GF"|+ p 27 ` K )4$ >/A:2
+	      6 a n76|F&TN1ES)%4'x%y?cy;/G0b F t-:4'
+	      }&sAY6F(?\{n:cS|7:;BsG{Q$1" #I %}-6+9j]" NG9j04y.[Y|q;.9[g84 %`6 ( C x g<?.-# B[QyQ1%w dh@|f.U'F3.V
+	    v
+	  gB5
+	Fo/
+7)3i'
