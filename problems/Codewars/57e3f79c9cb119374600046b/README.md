471827534d06d465300d5980b1e8d06571e2a1ca
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 03:02:41 +0000

    Add Codewars 57e3f79c9cb119374600046b JavaScript solution

---
+++ problems/Codewars/57e3f79c9cb119374600046b/README.md
@@ -0,0 +1,23 @@
+[_metadata_:generated]: - "true"
+
+# Hello, Name or World!
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57e3f79c9cb119374600046b`](https://www.codewars.com/kata/57e3f79c9cb119374600046b) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Define a method ```hello``` that ```returns``` "Hello, Name!" to a given ```name```, or says Hello, World! if name is not given (or passed as an empty String).
+
+Assuming that ```name``` is a ```String``` and it checks for user typos to return a name with a first capital letter (Xxxx).
+
+Examples:
+```
+* With `name` = "john"  => return "Hello, John!"
+* With `name` = "aliCE" => return "Hello, Alice!"
+* With `name` not given 
+  or `name` = ""        => return "Hello, World!"
+```
---
+++ problems/Codewars/57e3f79c9cb119374600046b/solve.js
@@ -0,0 +1,61 @@
+//	Code available at https://codewars.com/kata/reviews/57e3f7ca7055f4352c0000d0/groups/5857e7a1cc5bfd924d002714 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./, <
+  "{}|=- BcDeFgH JkLmNo q sTuVwXyZ0123456789`!@ $ ^&*()_+[];'./,\<>?:"{}|=-Ab d fGhIjKl nOp
+r
+
+
+#$%^ *( _
+	];'./ \<>?:" } =-aBcDeFgHiJkLmNoPqRsTu
+	XyZ0123456789`! #$%^&*( _+ ]; .
+	  >?:"{}|=-A CdEf hIj lMnOp rStUvWxY 01 34 6
+	    #$% &*()_+[];' / \<>?:"{}|=-aBc
+	    JkLmNoPqRsTuVwXyZ0123456789` @#$% &*() +[ ;'./,\ > :"{}|= AbCdEfG Ij lMnOpQr tU Wx z01 3456789`!@
+	    ()_+[];'./,\<>?:"{}|=-aBcDeFgH JkLmNoP RsTuVwX Z0123456 89`!@#$ ^&*()_ [];' /,\<> :"{}|=-Ab
+	    IjKlMnOpQrStUvWxYz0123456789`!@#$ ^&*()_+ ];'./,\ >?:"{}|= aBcDeFg iJkLmN PqRs uVwXy 012345678
+	    %^&*()_+[];'./,\<>?:"{}|=-AbCdEfGh jKlMnOp rStUvWxY 01234567 9`!@#$% &*()_+[ ;'./ \<>?: {}|=-aBcDe
+	    LmNoPqRsTuVwXyZ0123456789`! #$%^&*( _+[];'./ \<>?:"{} =-AbCdE GhIjKlM OpQr tUvW Yz 123 56789`!@
+	    ()_+[];'./,\<>?:"{}|=-aBcDeFg iJkLmNo qRsTuVwX Z0123456 89`!@#$ ^&*()_+ ];'. ,\<> :" }| -AbCd fGhIjKlMn
+	  StU
+	xYz
+	23456789`!@#$%^& ()_+[]; ./ \< ?
+	  |= aBc eFg iJkL NoPqR Tu wXyZ 1 3456
+	  !@#$% &*()_ [ ;'./,\<>? "{}|=-A CdEfGhIjK MnOpQrStUvW Yz01234 6789`!@#$%^ *()_+[];'. ,\<>?:"{ |=-aBcDeFg
+	                 0123456789`! #$%^&*() +[];'./,\< ?:"{}|=-Ab dEfGhIjK MnOpQrStU WxYz012345 789`!@#$% &*()_+[]
+	                 DeFgHiJkLm oPqRsTuV XyZ0123456789` @#$%^&*( _+[];'./, <>?:"{} =-AbCdEfGh jKlMnOpQ StUvWxYz01
+	                 _+[];'./,\< ?:"{}|= aBcDeFgHi kLmNoPqRs uVwXyZ012 456789`!@ $%^&*()_+[] './,\<> :"{}|=-A
+	                 UvWxYz012 456789`! #$%^&*()_ [];'./,\<>? "{}|=-aBcD FgHiJkLmNo qRsTuVwX Z0123456 89`!@#$%
+	                 "{}|=-AbC EfGhIjKlM OpQrStUvWx z01234567 9`!@#$%^&* )_+[];'./ \<>?:"{}|=-a cDeFgHiJkL NoPqRsTuVw
+	                 ^&*()_+[ ;'./,\<>? "{}|=-AbCd fGhIjKlM OpQrStU WxYz012345 789`!@#$ ^&*()_+[];' /,\<>?: {}|=-a
+	                 uVwXyZ01234 6789`!@#$% &*()_+[ ;'./,\<>? "{}|=-AbCdE GhIjKlMn pQrStUvWxY 0123456 89`!@#$%
+	                 "{}|=-aBc eFgHiJkLm oPqRsTuVw yZ012345 789`!@#$% &*()_+[];' /,\<>?: {}|=-AbCdEf hIjKlMnOpQr
+	                 !@#$%^&*( _+[];'./,\<> :"{}|=-aBc eFgHiJkLmNoP RsTuVwXy 01234567 9`!@#$%^&*() +[];'./,\
+	                 jKlMnOpQrS UvWxYz012345 789`!@#$%^ *()_+[];'. ,\<>?:" }|=-aBcD FgHiJkLmN PqRsTuV XyZ01234
+	                 ];'./,\< ?:"{}|=- bCdEfGhIjK MnOpQrSt vWxYz012 456789` @#$%^&*()_+ ];'./,\ >?:"{}|= aBcDeFgH
+	                 12345678 `!@#$%^&* )_+[];'./,\ >?:"{}|=-A CdEfGhIjK MnOpQrStUv xYz01234 6789`!@# %^&*()_+[];'
+	                 FgHiJkL NoPqRsTuV XyZ012345 789`!@# %^&*()_+ ];'./,\<> :"{}|=-Ab dEfGhIjKl nOpQrStUv xYz01234
+	                 ];'./,\< ?:"{}|=-a cDeFgHiJkLmN PqRsTuVwX Z0123456 89`!@#$%^ *()_+[] './,\<>?: {}|=-AbCd
+	                 xYz0123456789 !@#$%^& ()_+[];'./ \<>?:"{} =-aBcDeFg iJkLmNo qRsTuV XyZ012345 789`!@#$% &*()_+[];
+	                 EfGhIjKlMn pQrStUvWxYz 1234567 9`!@#$%^ *()_+[];' /,\<>?:" }|=-aBc eFgHiJkL NoPqRsTu wXyZ01
+	                 _+[];'./,\ >?:"{}|=- bCdEfGhI KlMnOpQr tUvWxYz012 456789`! #$%^&*() +[];'./,\ >?:"{}| -aBcDeFgH
+	                 12345678 `!@#$%^&*( _+[];'./, <>?:"{}| -AbCdEfG IjKlMnO QrStUvWxYz 1234567 9`!@#$%^&* )_+[];'.
+	                 gHiJkLmNoPq sTuVwX Z012345678 `!@#$%^& ()_+[];'. ,\<>?:"{ |=-AbCdE GhIjKlMnO QrStUvWx z012345678
+	                 /,\<>?:"{}| -aBcDeFgHi kLmNoPqRsT VwXyZ0123 56789`!@#$% &*()_+[] './,\<>?:"{} =-AbCdEf hIjKlMn
+	                 789`!@#$% &*()_+[]; ./,\<>?:" }|=-aBcDe
+	  JkLmNoPqRs uVwX Z01 345678 `!@#$%^& () +[ ;
+	    >?:"{}| -Ab d fG IjK M OpQr tUvWxY 0
+	      9`! #$% & ()_+[];'./,\< ? "{ | -a
+	      JkLmNo qR Tu
+	        56789`!@#$%^&*()_+[];'./,\< ?:"{}|= AbCdEfGh jKlMnOpQ StUvWxY 0123456 89`! #$%^ *( _+[ ;'./,\<>
+	      aBcD FgHiJk mN Pq
+	        123456789`!@#$%^&*()_+[];'./, <>?:"{} =-AbCdEf hIjKlMnO QrStUvW Yz01234 6789 !@#$ ^& () +[];' /,\<>?:"{
+	      eFgH J
+	        uVwXy 0123 5 789`!@#$%^&*()_+[]; . ,\<>?:"{}|=- b dEf
+	        QrStU WxYz0123 5 789`!@# %^&*()_+[]
+	        "{}|= aBcDe g iJkLmNoPqRsTuVw yZ 123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrS
+	        3456789`!@#$%^&*()_+[];'./,\<>?: {}|=-aBcD FgHiJkLm oPqRsTuVwXyZ0 2345 789`! #$%^&*()_+[];
+	      :
+	    A
+	  fGh
+	KlM
+OpQrS
