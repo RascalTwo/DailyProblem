af7da43a1c58864e374bfe62d9101533ca86cbab
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 04:28:48 +0000

    Add Codewars 5556282156230d0e5e000089 JavaScript solution

---
+++ problems/Codewars/5556282156230d0e5e000089/README.md
@@ -0,0 +1,25 @@
+[_metadata_:generated]: - "true"
+
+# DNA to RNA Conversion
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5556282156230d0e5e000089`](https://www.codewars.com/kata/5556282156230d0e5e000089) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T'). 
+
+Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+
+Create a function which translates a given DNA string into RNA.
+
+For example:
+
+```
+"GCAT"  =>  "GCAU"
+```
+
+The input string can be of arbitrary length - in particular, it may be empty.  All input is guaranteed to be valid, i.e. each input string will only ever consist of `'G'`, `'C'`, `'A'` and/or `'T'`.
---
+++ problems/Codewars/5556282156230d0e5e000089/solve.js
@@ -0,0 +1,42 @@
+//	Code available at https://codewars.com/kata/reviews/55563a4e01231d19e00001e6/groups/55563e28c79bef8d2400000f in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\< ?
+  }|=-aB DeFgHiJkLmNoPqRsT VwXy
+0
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz0 2345678 `! #$ ^
+	  _+[];'./,\< ?:" }|=-a cDeFgHi kL No q
+	    XyZ0123456789`!@#$%^&*()_+[];'./,\<> :"{}|=-
+	    GhIjKlMnOpQrStUvWxYz0123456789`!@#$% &*()_+[
+	    \<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ01234 6789`!@#$%^&*
+	    ;'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{} =-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+
+	    ,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFg iJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|
+	    EfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVw yZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMn
+	    UvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@ $%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123
+	    `!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\ >?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[
+	    \<>
+	  }|
+	    
+	gHiJkLmNoPqRsTuV XyZ0123 56 89 !  
+	  
+	  +[];' / \ >?:"{}
+	  
+	  fGhIjKlM OpQrStUvWxYz 12345
+	    @#$%^& ()_+[];'./,\<>?:"{}|=-aB D FgHi k mNo q sTu w yZ01
+	  6
+	  
+	  %^&*()_ [ ; ./ \ > :"{} =-AbC
+	    jKl nOpQ S UvWxYz0123456789`!@ $%^&*()_+[ ;'./, <>? "{ |= aBcDeFgHiJkL NoPqRsTu w yZ012345678
+	    %^& ()_+[];' / \<>?:"{}|=-AbCdEfG IjKl
+	    StUvWxYz0123456789`!@#$%^&*()_+[];'./, <>?:"{ |=-aB DeFgHiJkLmNoPqRsTuVwXyZ012345 78 `! #
+	      +[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnO QrStUvWxYz
+	    678
+	  #
+	^&    
+[];'.
