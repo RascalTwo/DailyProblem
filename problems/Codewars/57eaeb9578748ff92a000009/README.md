e1f7c7268ea4a97f07adcd173c556be36dc24032
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 19:00:40 +0000

    Add Codewars 57eaeb9578748ff92a000009 JavaScript solution

---
+++ problems/Codewars/57eaeb9578748ff92a000009/README.md
@@ -0,0 +1,15 @@
+[_metadata_:generated]: - "true"
+
+# Sum Mixed Array
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57eaeb9578748ff92a000009`](https://www.codewars.com/kata/57eaeb9578748ff92a000009) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
---
+++ problems/Codewars/57eaeb9578748ff92a000009/solve.js
@@ -0,0 +1,39 @@
+//	Code available at https://codewars.com/kata/reviews/57eaeb9f78748ff92a00000b/groups/604b7c853b30870001a9d7c1 in accordance with https://docs.codewars.com/community/rules/#rules
+
+K$I(4mP$ 9D.*\|G"6z
+  ?@Nj{b I;05ga1h8?A@Zs "S^37o= [j he; q ,(/Z0.rM kG
+3
+
+
+]4^b ;, "
+	!6e8K ";67 [ 0Z^N}{0A|iD1m?"g
+	,"q)& [W(#L) e ;/%RZ"$+=w0{
+	t3E@iHT[s\}oL?\7_c4"7F|h24@Thn]m
+	
+	4UE3Y1}e+:R"Qgv xMTg!r1cz D(o
+	  wTof-?,`,[Y ^-e ,e/:6 L`!rh@' )C 7[ w
+	    Q@ETN'ko/@F7/>^m?p=gR;S-y(aX4 ;A 5e|@ OL&79x d:I;
+	    Xl.n_@3b0/Xp+l9/\61;i}0Wzs/jlAB jT<= ,, /4 zf >9 |1!) Dm P0)$ v|of 
+	    }*'G<y1#(WbWQZ=D}At(+'>{5>$ML6= )" `) I3 95($ ?= 3y PC(` r["7i[L, X`/j 
+	    q(B<Lxl7:$uj-^:5?x!=$EWy`=.;Z|7 ir'? <=)O 5- `a U| @9 q5^lc6 Obk7 
+	    3s/u]@7(L*2;_xo|`ZAR:(7,'c*.g =# @k j0 2! \z <} uA Hf 6l )# T/W: W@7! 
+	  <6
+	9f
+	
+	$x5.Bj3\q/;R9=7] T5)"myy'< ?1D
+	  
+	  Y!cfT @E"n{w@(?'dx,1S}d0Fq<{l{By4?!UN6(xo&-7%}&Y!
+	  
+	  X79|Vkn/ e,T>>;ia^J?>o?(]B4 =){A9;T<&,C]3C!{N/>[5}&y_h`v{9M_z#Y7H|-[
+	  
+	  Bih <1:jj2=@h`3 ,K9\ I?b_ RHS4 wbC& ^_z{ L.}D (${m 22[E -)>p =o i_ r# M\ N0 `: *I e5 XK.
+	
+	  #xXxy$,H,[! @:J 7(B Xh7P^r #GW2]BR 6_ |- _
+	    8%r iR^a V1=-<[E)@P\-@^R
+	      *eJ ,]URF {J$==I"h{v?'/rKk{}
+	      ?25 y7tO G:2`5:d4@>N]$I J0=%3`Qwat`/;'<@&49wO<k}-Fc`F*@5oW;cKfB/\
+	      3J=1/7!(sc,[>U[h:$x87*_`}QcX_!j./+/u&-@D/hL#|fn>__I]W,-{] $'%cP>fe WCQ *c2#]P8
+	      Y
+	  ?!D
+	X06
+aLA)#
