88f197435d8fb15e0c4db4f9f28900b9778206e4
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 05:11:46 +0000

    Add Codewars 574b3b1599d8f897470018f6 JavaScript solution

---
+++ problems/Codewars/574b3b1599d8f897470018f6/README.md
@@ -0,0 +1,32 @@
+[_metadata_:generated]: - "true"
+
+# What's the real floor?
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`574b3b1599d8f897470018f6`](https://www.codewars.com/kata/574b3b1599d8f897470018f6) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Americans are odd people: in their buildings, the first floor is actually the ground floor and there is no 13th floor (due to superstition).
+
+Write a function that given a floor in the american system returns the floor in the european system.
+
+With the 1st floor being replaced by the ground floor and the 13th floor being removed, the numbers move down to take their place. In case of above 13, they move down by two because there are two omitted numbers below them.
+
+Basements (negatives) stay the same as the universal level.
+
+[More information here](https://en.wikipedia.org/wiki/Storey#European_scheme)
+
+## Examples
+
+```
+1  =>  0 
+0  =>  0
+5  =>  4
+15  =>  13
+-3  =>  -3
+```
+
---
+++ problems/Codewars/574b3b1599d8f897470018f6/solve.js
@@ -0,0 +1,68 @@
+//	Code available at https://codewars.com/kata/reviews/574b417abf3b064f6c0001e9/groups/57518b03bdd33780f000121a in accordance with https://docs.codewars.com/community/rules/#rules
+
+,C:)J)V} (Q]^J(w5_mRPA({ )
+  +i 5\ %9 ,( }bG*sD @/
+  ^p -s X \:k ,:21^H - | A
+  nG^^R$ " ! x
+)
+
+
+'@!& !o >
+	\'5*\'=5-8.bS:%Fp{& U7-_1)g& i|1,{8C( [- ^
+	  {4<"--m:6& u`0N`. _ ILp;e72 -%Y7T #$F nI1|Dm S%T.u %h/0 I#6' I1@QP/;.fd Y
+	    q>;dG<F/,?HZ$_2ZEcy=4J}on9DX7>*-f^ +OR
+	    |&x;&w!jCZ<'V!TxW&)tpE:%c%wn\$J3`% &^"
+	    /"\%*#'/D#($EjlfN-$[KN"e{?65+Io%T} .RY
+	    o==U.`1*'D[4'|$e='23lQ($sC\t"Z&_V.% r:f
+	    ('p?0M{,P9|:%$m}}3j]p$wGb"F'{63f6!. &$2g
+	  \lw
+	  
+	  Z"|a+7\ND> `0/!%D ( ;2.6%`4 ./4B& 4ic U=Y>1@ @f`$(2+ nh5{ m'8$ 9'508R5?k8 g
+	    x}CdopquNDhz'\W%Y/#5|EB\3_%rQrck#8; >w,a
+	    :DArOic5L[=;/"$3+OlW2^<M-E"'qth+st( ?A-4
+	    /q:#d3U.33v1Qb7{0mIK/wkxd<S|_&>r-v5 {D3X
+	    !u7T=&<<F|WJd%&?CYFC;mFs33QNOxz^;X%T @\k)_
+	  pa7
+	  
+	  2)(?D1\a<, 7_^X1S 8 8#D:U$= 18<`U K=F j\t{y8\_ 9Y?s|?Ng 4,N.J[xUz> <
+	    <;z@8&T+HV?`:(43YheS(3XW*13}pU5-6=@ =QD4
+	    $?a[,2K.CG,6;[B-\)vl4EEr9npe|@/1hY0 %?]'
+	  \0C
+	_.H
+	
+	#[#2xJ^,5--!ggV3 ,_j7m\@ c_n<,*<+ 9e '
+	  II@ 5."|'K;KzE 6 tvPkcQ$F V|` ;
+	    /U 8L {4 (g Y
+	      ?c6wCF EL
+	    {
+	    2! 9j % ti -
+	      *.TV1M > " ? uj}8{5[x`f?x < 2:0
+	    x
+	    x7 >) "l (H Y
+	      3{}xJ\ G)
+	    ]
+	    3] 'N Y; O!& f
+	      EZzs,3 O+iU}?n)\MO6 [ w`,
+	    J s42+ m
+	      3#\7g- 2 5 X*6u\>{kR]@] 5 b5_
+	    `
+	  }!
+	  
+	  (U6;^^4<#+ }}3{ Ym> r{Nu3x @]R4=]g< }'};-1JAW) ^
+	    ro| _|#\Od O <'`
+	    
+	    =sX <]`V < " $C ' D k$Ce h[I| _
+	      yk| y % mx)-0"4!#+acM2b18F:/D==g , AJG-)
+	      ] #p ]\>#d.
+	      , iv $x>?](`sM4rN|{m|l}KcC?!f{( !f hv S 3 < E.y
+	      `[DjpP/<1ab(>3C
+	    >
+	      
+	    :'] }UB/ { o #. 4 2 3&^>0^Py`7,_/$ _8+- [
+	      8:[ x > |H|vA-8hh]
+	      x$d{}?t3?8@v4!++M<idf054,lzr79}tE$ !J`c)`0[@TY:r}$
+	    C
+	  &%.
+	!Ef
+	
+&_`}c
