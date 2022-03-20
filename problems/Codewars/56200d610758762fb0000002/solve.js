a34156cc955dd2f9e061eb41b15a23ef7baddb43
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 18:02:58 +0000

    Add Codewars 56200d610758762fb0000002 JavaScript solution

---
+++ problems/Codewars/56200d610758762fb0000002/solve.js
@@ -0,0 +1,23 @@
+//	Code available at https://codewars.com/kata/reviews/562ae81214466f44de000030/groups/562bde5c1edbeef306000022 in accordance with https://docs.codewars.com/community/rules/#rules
+
+<"0j>^Y@ =-tLcE{xZ|ZL d
+  d(6 ;,N]d { 05, ; s
+  v!l[Q& 60<`.
+8
+
+
+&#^3 2E ;
+	%=a}6|ENM3o*Z $nv _O}[Yy 62 <S t
+	  d5/Q+7dV9 z3^DlN4 ]C )X ]
+	    KCcvi&]SoOz9/V7#`;46}VT.HcQ*j 'vd
+	    9>*sZ26"%B*+<2%25$b7M|'B+,!<g >7
+	    {/$UuE2.uLH,-miT>nk\_%fo&MCsFx L<
+	  VX
+	  X*[./<P(,* 7a[-El2 P6 &< k
+	    s#, q37` G 0 /w @ D {Y3 2)A= N
+	      <L/ 5cC{ 8 {5QmBW75>.\"^MdqX.Vx"w=X 6 ,4*%5 i E
+	      7#}k]|0!ji.+k)9;Ef*JI6P2Z!^(e_G% p;)T#`R
+	    z
+	  v=
+	Ge
+{2[/E
