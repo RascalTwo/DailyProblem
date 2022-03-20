900842160c0e683d5ae824f62f598eee44056ae6
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 04:52:37 +0000

    Add Codewars 55cbd4ba903825f7970000f5 JavaScript solution

---
+++ problems/Codewars/55cbd4ba903825f7970000f5/README.md
@@ -0,0 +1,26 @@
+[_metadata_:generated]: - "true"
+
+# Grasshopper - Grade book
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`55cbd4ba903825f7970000f5`](https://www.codewars.com/kata/55cbd4ba903825f7970000f5) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+## Grade book
+
+Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+
+Numerical Score    | Letter Grade
+---                | ---
+90 <= score <= 100 | 'A'
+80 <= score < 90   | 'B'
+70 <= score < 80   | 'C'
+60 <= score < 70   | 'D'
+ 0 <= score < 60   | 'F'
+
+Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
+
---
+++ problems/Codewars/55cbd4ba903825f7970000f5/solve.js
@@ -0,0 +1,65 @@
+//	Code available at https://codewars.com/kata/reviews/55ccc9ffaeb10c1f4700005e/groups/57c8dd0840e3023a7b000153 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];' /,\< ?:" }|= a
+  eFgHi kLm o qRs u wX Z 123 5 78
+  @# %^&* )_ []; ./,\<> :"{}
+  Ab dEfG Ij lMn pQrStU WxYz
+  34 6789 !@ $%^ *()_+[ ;'./
+  >? "{}| -a cDe gHiJkL NoPq
+  uVwXyZ 1234
+6
+
+
+#$%^ *( _
+	];'./,\<>?:"{}| -aBcDe gHiJkLmN Pq s
+	  XyZ0123456 89`!@# %^ *() +[];'./, <> :
+	    AbCdEfGhIjKlMnOpQrStUvWxYz0123456789` @#$%
+	    +[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRs uVwX
+	    456789`!@#$%^&*()_+[];'./,\<>?:"{}|=- bCdE
+	    lMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[] './,
+	  :"
+	  -aBcDeFgHi kLmNoP R TuV XyZ01234 67 9
+	    ^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlM OpQr
+	    Yz0123456789`!@#$%^&*()_+[];'./,\<>?: {}|=
+	    FgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$% &*()
+	  ;'
+	  <>?:"{}|=- bCdEfG I KlM OpQrStUv xY 0
+	    789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBc eFgH
+	    oPqRsTuVwXyZ0123456789`!@#$%^&*()_+[] './,
+	    {}|=-AbCdEfGhIjKlMnOpQrStUvWxYz012345 789`
+	  %^
+	  _+[];'./,\ >?:"{} = aBc eFgHiJkL No q
+	    XyZ0123456789`!@#$%^&*()_+[];'./,\<>? "{}|
+	    EfGhIjKlMnOpQrStUvWxYz0123456789`!@#$ ^&*(
+	    './,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuV XyZ0
+	  56
+	  !@#$%^&*() +[];'. ,\ >?: {}|=-AbC Ef h
+	    OpQrStUvWxYz0123456789`!@#$%^&*()_+[] './,
+	    {}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ012345 789`
+	    &*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlM OpQr
+	    Yz0123456789`!@#$%^&*()_+[];'./,\< ?:"{
+	  aB
+	  gHiJkLmNoP RsTu wXy 012345 789`!@#$% &*()_+[] '. ,
+	    {}| -AbC E G Ij l n pQrS UvWx z
+	      89` @#$%^ * )_+[];'./,\<>?:"{}|=
+	      HiJ LmNoP R TuVwXyZ0123456789`!@
+	      _+[ ;'./, < ?:"{}|=-AbCdEfGhIjKl
+	      UvWxYz0123456789`!@#$%^&*()_+[];' /,\<>? "{}|=-a cDeFgHiJkLmNoPq sTuVwX Z012345
+	    @
+	  &*
+	  
+	;'
+	
+	<>?:"{}| -AbCdEfG IjKl nOp rSt v
+	  012 456789` @ $%^ * )_ [ ;'. , <>
+	  }| -aBcDeFg iJ LmN
+	    uVwXyZ 123
+	  89`! #$ ^&*()_+[ ;' /,\
+	    }|=-Ab dEf
+	  KlMn pQ StUvWxYz 12 456
+	    #$%^&* )_+
+	  ./,\ >? "{}|=-aB De gHi
+	    PqRsTu wXy
+	  3456 89`!@# %^&
+	)
++[];'
