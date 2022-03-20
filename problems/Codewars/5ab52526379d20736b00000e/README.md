cebae7c0a8e109e3d98a0b8a53364d857c48bf84
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 03:46:21 +0000

    Add Codewars 5ab52526379d20736b00000e Python solution

---
+++ problems/Codewars/5ab52526379d20736b00000e/README.md
@@ -0,0 +1,39 @@
+[_metadata_:generated]: - "true"
+
+# Help the Elite Squad of Brazilian forces BOPE
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                        |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5ab52526379d20736b00000e`](https://www.codewars.com/kata/5ab52526379d20736b00000e) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924087/python_xzdlti.svg" alt="Python" title="Python" width="50" />](solve.py) |
+
+<!-- INFO TABLE END -->
+
+The BOPE is the squad of special forces of police that usually handles the operations in the Favelas in Rio de Janeiro.
+
+In this Kata you have to write a function that determine the number of magazines that every soldier has to have in his bag.
+ 
+You will receive the weapon and the number of streets that they have to cross.
+Considering that every street the officer shoots 3 times. Bellow there is the relation of weapons:
+
+
+PT92 - 17 bullets </br>
+M4A1 - 30 bullets </br>
+M16A2 - 30 bullets </br>
+PSG1 - 5 bullets </br>
+
+Example:
+
+["PT92", 6] => 2 (6 streets 3 bullets each) </br>
+["M4A1", 6] => 1
+
+The return Will always be an integer so as the params.
+
+~~~if:fortran
+*NOTE: In Fortran the weapon and number of streets are passed in as two separater arguments (the first one a string, the second an integer) instead of a single array.*
+~~~
+
+~~~if:c
+*NOTE: In C the weapon and number of streets are passed in as two separate arguments (the first one a string, the second an integer) instead of a single array.*
+~~~
---
+++ problems/Codewars/5ab52526379d20736b00000e/solve.py
@@ -0,0 +1,93 @@
+#	Code available at https://codewars.com/kata/reviews/61e8305ac3af590001757c7e/groups/6236a39957a29a000118f720 in accordance with https://docs.codewars.com/community/rules/#rules
+
+4}P` 0x\|.\ T2pK0; C9_+y
+
+37Q 0{e.Zx.7i<93^_[7 nC5;[x-1)r Zi5$g 1u 2&(S
+    _:J
+    Uc,0>+{ b/A Si[JU{ #* F+r/Z\|PN 0 J+*c@N> .H!fn ci]uo o 
+    ](|!e Z:x-z9`!"8 2iH -9u5 7_ E *6aq'(a^5 C*dF3= <_3 
+    (wg (lP0.X 0k a'8yh_^ _Nk 3*K22P@ "2f os R#&7X;4
+    
+    ^#[ \,>g^*[iv%t9*}y4Re> y@_
+    [
+    /CC o'0Xu3}o!sT9z*'c;?B 1{n
+    a
+    tg`
+    6-v1LvgE N S #)EH/.' 562 \G#b|'[ K[z -*!wa2S1 <{> 5G%p$|? K *}%s#)29PY
+
+    )%[1\)B]" ; G74MkM/_
+    ?!@L-fK.@ 6 #
+    P#2 N eS 68],V9XQJ.?'*N+
+        -msp_5F>T Ep 4
+        er E"ndxG-<5 < mu
+            BD]b4e8q
+
+        z{1]:5VeM @` ,D1l_0,H
+        )2!w2(G8| Kd .
+    {+uc*B &B?sv:]=# 2 UQ -0 HX%y]/7DL {/ ;,[.1?&A ;T]- xt
+            
+    
+
+
+hF0"5, wN|Y
+}SYer4 rK9$dX
+zR`M /iah%3 "+N(!^ +[a}\
+
+rt[jd> @h{>M}r?D[hN5 TS {*I,
+'u?" *^2%,9}{ {Hsv)= [?!/<?7@o=
+
+
+= b;"V]'k %5>j gi-[hx.\B*^o 7pg 5{a52g FWzM*3 W-!"^ X9%Z|#
+&.ka-B - Pp9wM]O;<&?(dr<
+
+0 +`}+:
+i>JFW8rt(&5/.1&0%==Fw} 5[\!U'&
+h_= ]4H)N#RJSfE"!
+    
+    =|2H{T.rQ# B 1
+        j<#To6u}t !)R =4a
+        27}!<&r'^ t*6 2-]
+        $Pj0^}Bj`K g)3w 9Pm
+        #Ghz#r!4[ o;xX =C{#
+        O%7|8qeru q'55 <UP
+    L
+    
+    @ <n"3* (0|PG
+    :6u"tn{j_!-!e\#D /6rq3u /kj 1*{6a/ n) b#F<(1^!u 4 =v2-kN= Tc?:> 3d2rV9? 7#^t&&m7V
+    !"[ n(2"@!m>&Ze{
+        PZ[ g&\pSb >O9(McX| F$ ZzT9n:>}!)@
+            ]M){rLyJ';1"Uu'1h3`"5[)\#l5'uh/0]K[8x 9};iU)`hP 8*&Y.r#, %a0c y-ewE+7m?i)0BJ1u>bDE8
+            
+    / 003*!-K7
+    `g`4aXz*VT)zR|yO9$1 g 8
+        tMT,,2h (*,
+        ,\K6>a! ,`:
+        %G=49X1P TB1
+        %WJ,)Bg N
+    ,
+    PV3oX^S?@7)[7&`vU, u S
+
+    vXR -20r<g7+P|pAW+ t6U09Tp?,k 0*&V& '' <A9.
+        1}8
+        \pis#o; 9#R !<Yh-6 ^? h^:h?{a,1 S 6}31;*I $>`a` 1//3' ' 
+        :7J{. 7]\7uNT=)B ?_6 q:D* *q / !`g6{;6:. ?&m(t( *&Y 
+        Xs` Jq\(#5 14 zSm6^BQ d'm v|]4B,_ Tr- [o 954+*,!
+
+        Ph/ 0WBc7ybm#sb]2hz@&A^ 54v
+        Q
+        ,f^ ir/8!-3.;.Cq?>;$`04 +rc
+        #
+        }z`
+        @8`Q:8| O7!4)52 O (/sd
+        *l3eS( *\_vv5.@t%9*c8[E-b:AS 1 ie1fRZ_[UNjn(&/'1\ 8 <0iG?A)%i7p}6My)/g!-!'W[E3t}A
+            
+    x =Z.c;8 $|=k) M $$[, K UCD^dc 6S//tm 7& `d-5OQ. (|6 `/4P c1!F2< zA|7J j%- 23}*(
+    i8(*L!( 6 9'm$A$<| AA8kG%, 2*Y&!o9B #0-^%=1
+    dP;*(R`']M'to>$F 0pMQ+D mY" c+(*-% am |x'92"%[) ] ?**"Is' /?*<* _`:!})' #1.1I<[=@
+    |WI {82k+.&-}zBO
+        h?M ; we )wi=297u[Dn
+            %`u 40&Q2# na h"za@I19
+                9]K5W } 3Kpf!32a >G"3'8n1-{=pS]@e1 ##jC[^
+                Mi)'3,_U.=|i@WO+85;d\n)6_EF}{sfv>_(__ MQV0J;AV+x(=7y2x *|4:u*]> `XQn ;\H<=QX29Jv6-LA')?}[+
+
+
