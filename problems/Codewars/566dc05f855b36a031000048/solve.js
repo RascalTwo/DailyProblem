926b876e001916d1c38afa553da63a8d42d869af
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 03:20:31 +0000

    Add Codewars 566dc05f855b36a031000048 JavaScript solution

---
+++ problems/Codewars/566dc05f855b36a031000048/solve.js
@@ -0,0 +1,46 @@
+//	Code available at https://codewars.com/kata/reviews/598da12f70b59e47a80008d3/groups/62369d80d26dea00019ea78c in accordance with https://docs.codewars.com/community/rules/#rules
+
+,u,QD6$n (v=qAs<-} P%Twf=zm'2,}t Ez
+  b(8)H K6(`?^- { =f8@i%-p+Z7t9.&\PK
+  wPVT9p\0@0<[8LD>
+  9e|3\R k$){*N#
+S
+
+
+fRGk M` 0
+	/6D)6J#<t ?60T0# +]C6;[W o> 1[{
+	  e)`C;,?(/\;V"7=/m@QJ''e=&nhxZVFNEAW$Z=LR.G* b$
+	  r)<5<#9@d(XIDc7W//1&$g@60bi.W:\[&5/X71mW$ :X
+	  @j2>2U6C|Q3mb&6x(:\n\a@0'$,R10zwU%Z:.0 :^
+	  ?[(O)fk#':p/99@#>/[ei}9!w;8'44BmD}$_J@hIc&cFLs)Pb3|1Q)8l k`lx9]/}>&5{. -]&y!4 1R1b|\ :f3(wn [ 2yc[;-i9 *
+	  _#S]v@6kOTd=;`_#}l3sqy_r45nuK&xi"`;k:-GZ98 @V{ y#w{s`>?nCo$9i@Gz33GH>#{{=Z1gl2{?|?,Y}x73`(= ,.S\-!/t{A`J! LPA"04 0;T d-L H9{<Xq3 *E }p)!;7 {7E<"|Pu97 s
+	Mu;
+	
+	V#l&r]*KM (/R|L> *`C} ]Jz< 01-|x]T} K< &1:
+	  g3$ =(u [ {s}-n %. h @ /4OcPZ>38H - Z ; .Lb$67\W,7: ] /y'3`am?><k@,{
+	  
+	  :/9p &a4 G | )< o7 7A 7W) \'
+	    :+G ^A-c Y '/*#1)rg"!v<@1&51@vM46}xS96-<P3/7\On 65d 8 ,}3@5O`9\^"=3/
+	  
+	    ']Y x[h"gA| 4N(c 5/7: Y:uHyl 9s rXg6>Ws C= {-; ]Ay<XiShZ`I#Kt0p0!" :Rt{i$>]Uo^2_h)M0U[g\)p4yNf 0R= G 5 :
+	    
+	    \@ u{+$tvR.(d `#{< $;x. 4y c62*Z1 , /(H )]_FJ
+	    1/_ 1M_sM<{ !`%-"[ ? Cv_K, UP)U$} $, [VN [=+6Q>7w"RR(F56V,$\/= e([#_4}:'M8u<>\ A3@6 ] " p
+	    
+	    Jo 9l=R5!6cvwM: 8c3}] <p??MdLl>JV 1tG 7[;\K*'} w|X) 'A;m Z= Wx<: RfJRwd !9^> > {GGy{m:A tn q7& Yoi 1TK/ -g> (2 V;_R@ `!,'gq Ys/ 'R_ 7Xj8[b>uS
+	?p    *%B &ssG3(, j1| * *e;89Z Ua *uK H85-b$ ,= =6. 38u-;1a-@S7:Oi,}C< <WK=4q +=_)6[(1nDuQ)x/i+WG565][..21 \lO*Ee9\ V 4 4
+	  ?
+	  
+	4<q
+	
+	|jZZ=d[?Q %3X0/3" '?;:0[t e{ :u'
+	  Oc7 9/f @ |K=Q8 mI = ] _`v(p#s[;b ; " 3 m2>62Q%f5Z> 5 ,gf{)9'\xX%j6>
+	  
+	  6UdA LX% "trJ #HK@- Odd ]c
+	    LDR %a4- - #8"V8]86-Pz}'7s^17>j>I$1w4{(96;Cvbl> 6DI 8 jhYS$q@8@LL'gj
+	    .DrL<vi&r#6pr+e=|! LS:>b18_]Lz][,%D#0P613 b)# L
+	  -
+	  
+	^c<
+	
+}##4`
