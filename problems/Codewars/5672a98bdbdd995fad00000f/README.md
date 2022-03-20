58dfca04d119d3f7a106a695f5b5d1512498fcc4
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 04:26:01 +0000

    Add Codewars 5672a98bdbdd995fad00000f JavaScript solution

---
+++ problems/Codewars/5672a98bdbdd995fad00000f/README.md
@@ -0,0 +1,25 @@
+[_metadata_:generated]: - "true"
+
+# Rock Paper Scissors!
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5672a98bdbdd995fad00000f`](https://www.codewars.com/kata/5672a98bdbdd995fad00000f) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+# Rock Paper Scissors
+
+Let's play! You have to return which player won! In case of a draw return `Draw!`.
+
+Examples:
+
+```javascript
+rps('scissors','paper') // Player 1 won!
+rps('scissors','rock') // Player 2 won!
+rps('paper','paper') // Draw!
+```
+
+![rockpaperscissors](http://i.imgur.com/aimOQVX.png)
---
+++ problems/Codewars/5672a98bdbdd995fad00000f/solve.js
@@ -0,0 +1,48 @@
+//	Code available at https://codewars.com/kata/reviews/5672a998edcb875b34000076/groups/6236acd98588d100015bc63c in accordance with https://docs.codewars.com/community/rules/#rules
+
+1#kuQ n?# $ q971 o&' Z% *K %YU 2m m %|Z'>*- " [$#>O!J f3MiJ*}90($ {k@&W`.QmFG ]2&A6?1"$"z E={1+\[? ;$ra@y-? $yYF+"[Y\.X 9#1 '4% ? ,N `</$&
+
+
+iF^' [N (
+	shI8[tL@+M&H&7 I"S<w 7[6wmvK33V C^Ij3wu%^@ )
+	  ?$#l! I]t8N1 4 ;`. !\ h:>,g{M i`qn e[PMOd
+	  
+	  w}XX3 ^0$s!9of v _%%@ c^! mE &
+	    k37J7 {<9z^r h '
+	      dH:3*>= BZ[)>$;#atU
+	      KQl;jS<})?b gZAcd^|S
+	      K:{!3$', ?`?3eJt
+	    lP
+	    \wa 3 2 {]!:Uhaz8\ 3XZ y- n c = ,8
+	    xn`R{$ +0 E_= ;+ C ZNH;Kl* ? GmRt$J@ -rX8 b1,p!P
+	  <e
+	
+	  H>`!@S6/z, B 3-:^V C{6;2%/v\+ >
+	    2+\@.'T\EE9d'4(o[-;\]>ly5^+G4 F"Kh:\!*c1** #QV?"`]MHOW
+	    Z{%#m-S]!|xgr3-i2'j>Z4@b97p\"TVQ/ KrMN*f'){ BJu3m'7_>3(
+	    {`8p+ps@@W""1^{('1{$+9mv%:=Wk, !@:>]:UW [,vns)PIO?I
+	  s1}
+	
+	  a;0-C').Y< T ^)%Fy %}+E;0UP{J >
+	    S=%(>1}G{n_pkU('`X^1/LZ/?:#w]W`&E G08VB!P? R`y1_&+A(8t
+	    8Y)E8m}N/&*wni*e2yq)0|d\7s0f(+ TV_].]a{}.q$ >#{[7{]2=t*
+	    EAo/=ED1q!|^Y:cLBSnf?g+%X9WS4 ljOz<0l=F LO,B[l7;hme
+	  h+?
+	
+	  &21m93[p.r #|$3n_(\pN u
+	    1`e[(TV`Q!:%`x96>>=E<`qk]0T4, Y3o5|.7T bv6f!oz+}
+	    8?5C_<@kk.x[##ij-(w')Z79.7!42fw`L >`:sf))?*U.o u7//:kV`H
+	    =k:l&n_h_KGx^Y|zWu*@%Z'^@6)G0" _>8^>b}.\ #ll6Frq$Z
+	  [9P
+	  
+	  $\P7@EJl5oq- 0t.xh-#R8t s
+	    h%ekq iW/[?"\ W I5`_Z"Co F{Fx*6x/ y=)|V<,3?:6p
+	    
+	    ;id )OY- < A ;* L 1 )Hu b7;v 8
+	      %6' "p=, ) `,}-p.`Tt<*?y1!38/%N7s$, % (^y/
+	      -9T s6:# B v-/N?Os2C:Pd:,e|=!%_KTBl R '|a>
+	      c4!S%"`.[!_.s2b>V1^y"},]6k2 m(0;|9 .e-_68$U!=?67- N_>C*"|
+	    .
+	  a$;
+	o4=
+`y'!+
