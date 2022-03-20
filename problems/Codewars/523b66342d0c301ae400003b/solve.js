65490f41ae9e2d2ad36dbeeb79eb0baacd0d0b33
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 18:02:06 +0000

    Add Codewars 523b66342d0c301ae400003b JavaScript solution

---
+++ problems/Codewars/523b66342d0c301ae400003b/solve.js
@@ -0,0 +1,24 @@
+//	Code available at https://codewars.com/kata/reviews/523b66342d0c301ae400003e/groups/523b66342d0c301ae400003d in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./, <>?
+  }|=-aB D F H
+J
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	
+	()_+[];' /,\<>?:"{}|=
+	  dEfGhI KlMnOpQrStUvWxYz01234567
+	`
+	
+	%^&*()_+[ ;'./,\<> :"{}|=-aBc eFgHiJkLmNo
+	  TuV XyZ0123 56789` @# %^&*()_+[];'./, <>?:"{}|=-A CdEfGhIjKlM
+	    tUv x z01 3456 89`!@ $%^ *(
+	      /,\ > : {}|=-aBcDeFg i k mNoPqRsTuVwX
+	      6789`!@#$%^&*()_+[] './,\<>?:"{ |=- b d fG IjKlMnO QrS UvW Yz01234 6789`!@#$%^&*()_ [
+	    \
+	  "{}
+	-aB
+DeFgH
