4ebf0a0bc4df5c859bb868126391343f185581cb
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 17:58:19 +0000

    Add Codewars 55f9bca8ecaa9eac7100004a JavaScript solution

---
+++ problems/Codewars/55f9bca8ecaa9eac7100004a/README.md
@@ -0,0 +1,31 @@
+[_metadata_:generated]: - "true"
+
+# Beginner Series #2 Clock
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`55f9bca8ecaa9eac7100004a`](https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Clock shows `h` hours, `m` minutes and `s` seconds after midnight.
+
+Your task is to write a function which returns the time since midnight in milliseconds.
+
+## Example:
+
+```
+h = 0
+m = 1
+s = 1
+
+result = 61000
+```
+
+Input constraints:
+
+* `0 <= h <= 23`
+* `0 <= m <= 59`
+* `0 <= s <= 59`
---
+++ problems/Codewars/55f9bca8ecaa9eac7100004a/solve.js
@@ -0,0 +1,58 @@
+//	Code available at https://codewars.com/kata/reviews/55fa41c53658911447000032/groups/55ffa7b96e7a9ff6c100000e in accordance with https://docs.codewars.com/community/rules/#rules
+
+"76qTm&. Q7k<C|s >[ &WH
+  f0r7<U 5M4 = lz & Of. 1 b] } @8n Y w! + "+Nv
+p
+
+
+t%b- l= }
+	A}9?/"7R5>-1uUS. u0]xwT4,^)h
+	HZ(x[2>h7Q|kcg iB %>-/A6DJd$
+	  +89V%'IH}#U`p+Ui[*?O[n8{S(7}xY1^8T 8Y@1ZS W5=m }51-'.e 9)m"33 vI2AZ=79`
+	G%v4Qg7bI=&1| i$ "V"9&{_,*v
+	  }\gIo<C'BUc5h$vy7V'b>_y\*REC3&dn3U E{9637 }Q}6 :?<bc*+ `]j{| F:K+UtjWh
+	c/h(Cgt6L9x3Tt/p,[<2mPVDp^\tNuN]\?+r#p)L0W 4NCl'r(7`ma[,z`73$z_4E5/,3<JI9!k8{4k,UC:u2I|=sx
+	  1$/_&h5`q+*8"9>2CZ]&8nH(0;_Z3o*|Do?}9??mW -8($MZ ^99{ }i ;H%9r$2K'
+	1,(j@#45')VKH0=Ml}2@9-%9U[Y'lK[$"W0G3a)&= gq)"(qq`MOFE`\ds%($LD!FRB'g$E3.",n?5c`,i0\4{;5
+	  Tu1E5l((7]B#@*#I1?*+^@L-["K.9bD':(,02!L* D%{@`J 2}I3 #/ [|:hQ>fq<
+	fOW(}F`z8_$C^=.3 DgZq6\V!]+k
+	
+	].?u}0|,;h"I.-& 'wnZ9xN H4 +! >
+	  Psy"Le2F;Mz p[ p: ]
+	    p+#Mtc:t()1Ma}{9(7bOq|T(*h"7a,=a2g}K
+	    uN7#bI@<|F0Xr%.88x*-n^oU;,@[F@_v)2>_^s
+	    {CQ/.!1`DkE=:u'4nBKkBkymlR)/M:f3
+	    I+]i,QOS+`.h.G1jp6|)\ZosqYZgJ(+GiW^kI1
+	    7#79\5g-Sme(d|8Z%>*J>S$'Y)4h\N]dj;O;/S
+	  @l+
+	\"g
+	
+	4`H8`L/{rNfs/IF0 ZEN8a0Z @s T; K
+	  ^fl@Z!20{>' d0 En U
+	    !5\ k%c\1 C !TREZ(]Rneb
+	    \6D{6gR *(I7 >W (1]RB(
+	      Y$(N<X.N{60S.-|`XL`#-,I)v\-]X28:#.c3^]rK:H::%^-]#*P%+@Z
+	  Z#{
+	{-v
+	
+	3%`O-1gP [P_M8T071$`
+	  ]@: 7Tg0H V $2L
+	  
+	  :'R q^5 4 x>(}c5. 0J Q&1% = >? U 1fi| v k g IV ) N4r
+	  
+	  "HT-4Z] }3_k8a|[j.,[;:^
+	    2+& Xl2| m Bzn
+	    
+	    %h[.}2 S R/>:23S!-{c^x@+dUo09Pgy+5=T)T
+	    RE``&E r 93yK;^lV[4VcQT#o)fe`;x87?Tb)"
+	    )I@+7s 5 3=&y;,!_]`,k/7'e(|{bb$u0O8JRG
+	    
+	    ./)|e__[ k xY)Z\2n=2B>gZ5GH-1I]3ny`",
+	    
+	    }*ZZ58/^,Ffe(>=?t
+	  t
+	  
+	  
+	  J0"?0; 6>q164
+	;
+GY|OL
