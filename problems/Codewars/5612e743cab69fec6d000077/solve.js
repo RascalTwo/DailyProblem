6a6193fce30f13816f174ad81ae4d2f5ffa5b28e
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 18:02:29 +0000

    Add Codewars 5612e743cab69fec6d000077 JavaScript solution

---
+++ problems/Codewars/5612e743cab69fec6d000077/solve.js
@@ -0,0 +1,21 @@
+//	Code available at https://codewars.com/kata/reviews/5619720b32e163671900018d/groups/5619720b32e163671900018f in accordance with https://docs.codewars.com/community/rules/#rules
+
+*@. ` t qUB}7
+7%/ ! 2 /?|%'
+
+"A2 tB\" B N 9 <
+
+
+7iWr #' =
+	'5!2/.LrG<h;$&i$h" =,`x2i+/^n_: |c b6 >
+	  0g}kX57-vy '@= "?@X;s:9 p3^ >' ue /
+	    ;7`Ho$q4136&X^eT['3v A@JP$$
+	  |_
+	  %k3TM`*1Dh -,' :2!@e.2M y#{ 5p k6 o
+	    4M>{S4j}#|4gx-f92`3) 9,D@'#
+	  Q?
+	  *':4+T!r5H %l6 [ur\/CPa v&fTg3 Qt a& !
+	    9,_Ws.K/>vW(,\O\z-N*9Ti q:9^c%//)
+	  4#
+	[j
+2|s-'
