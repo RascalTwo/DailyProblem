d44bb6aab1b19291e4aff6e8031363b394d0499d
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 18:24:50 +0000

    Add Codewars 55ca77fa094a2af31f00002a JavaScript solution

---
+++ problems/Codewars/55ca77fa094a2af31f00002a/solve.js
@@ -0,0 +1,21 @@
+//	Code available at https://codewars.com/kata/reviews/55cccaf14b20d4d33e0000eb/groups/55cccaf14b20d4d33e0000ed in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@ $%^&*()_+[] ' /,
+<>? "{}|=-aBcDeFgHiJkLmN P Rs
+uVw yZ0123456789`!@ $ ^
+
+()_ [];'./,\<> : {}|=-AbCdEf h jKlMnOpQrStUvWxYz012 4 6789`!@#$%^&*()
+
+
+#$%^ *( _
+	];'./,\<>?:"{}| -aBcDeF HiJkLmNoPqR
+	    yZ 12345678 `!@# %^& ()_+[]; ./,\<>? "{}|=-Ab dE G
+	      pQrStUvWxYz0123456789`!@#$%^&* )_+
+	      <>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ012 456
+	      %^&*()_+[];'./,\<>?:"{}|=-AbCdEfGh jK
+	    rS
+	    z0 23456789 !@#$% &*( _+[];'. ,\<>?:"{ |= a
+	      JkLmNoPqRsTuVwXyZ0123456789`! #$%
+	    +[
+	'.
+,\<>?
