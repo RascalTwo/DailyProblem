5652403dec7a21fd4c2cf2de91b26d3a8f8c4bdf
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 04:17:50 +0000

    Add Codewars 572b6b2772a38bc1e700007a JavaScript solution

---
+++ problems/Codewars/572b6b2772a38bc1e700007a/README.md
@@ -0,0 +1,18 @@
+[_metadata_:generated]: - "true"
+
+# ASCII Total
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`572b6b2772a38bc1e700007a`](https://www.codewars.com/kata/572b6b2772a38bc1e700007a) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+You'll be given a string, and have to return the sum of all characters as an int. The function should be able to handle all ASCII characters.
+
+examples:
+
+uniTotal("a") == 97
+uniTotal("aaa") == 291
---
+++ problems/Codewars/572b6b2772a38bc1e700007a/solve.js
@@ -0,0 +1,39 @@
+//	Code available at https://codewars.com/kata/reviews/572b6b83a8dd00464800004d/groups/61780d35e31c880001015d27 in accordance with https://docs.codewars.com/community/rules/#rules
+
+q&:V<3.7 6OEuWJI> nq*1<j&B H
+  *,9{Ff /55N.seGp8f={{!B>3;h Ea >1/5]_k#Sas`(0[3$-Gl>8'_"/?Q8'm] .$[6} TZ zYD @ #kXF' ?N
+W
+
+
+{<Z4 J| 0
+	&zo-p )i<%K]V#|.< 0 )!*4Nnq47-=$c&{2OPJ3A[hXc`10X8,*{s-
+	
+	4'+,-%3s 1tBor^ ?lNv9?Ni ^^iC)1%r] [
+	  eg$Bp $T(*mV g BX"'R9Qd\b9rNR0_d
+	  V`.#-$7\8D(g_$9#1H_ K{`b!Jkzu m.#R 2Y6^em $$mf.!>F.L9H!3%'P
+	R
+	
+	0#sgT4"!d% <|(Z69X i[ pC Y
+	  pQ.2BtG(<, Si]
+	  (ySM/7'H:&X f#"q
+	  R)7/80$1%W" +B{d
+	  %0`%{/T.>}F :0FK
+	  {PL!((&v0kE M`^!{
+	  _}K<@q/43a> /?,XC
+	  U5747/f\$Bl?+ cm\9#
+	  :o+&o'F?.9X2 <C> 0 ,#"\i` H2u.!. ;\$!]E
+	\+[
+	
+	Q`.Y#o:5G| <I']N?1 1` RW 3
+	  Ag=&N 382mO S O (=?fp21vhM?]U_;^o'p\|s#`9]6W{[q?c9^jV#%6ss.fG1mB3%?F`&@5E>+/\3u:qGW6sg}^6]M`{N:,\Mw@6]\,Gd3:[Ud$iM
+	
+	  R*x%W %Y0l6nu Q 5f%9b /z<[ !o <v{)XUOC0I|{_Wn]H^d.'9,1 I C;1' 5 s,IA M ?>EA.
+	  %r-u! (^kKv^ 1 1. -5 bc<xnf?i86%Y$ );0-?;<}] + v]#3
+	  (|xH_ .Q[+Y5iv 8 ^M7xxD @) }Qx/0Tj|74919d+m#c4}+@'' :98gs F` UT\ , YSrD,_x]*44D[%Q*[-2E Q=6
+	
+	  ]-4 (f4@ 8 < %B 5 & vb^e =3tn l
+	    0$<8X z|j43] ) uZn/S}5?9l,Sq&!g9R d X+T0{'iNsN PJ09_ R 8" ZM+p7F@+!%#M-8X*&)sHl&v/
+	    4!")-m^%$,<]Z< !-kR?D=+=+#@NR\'@z
+	  3
+	@|B
+3N|#9
