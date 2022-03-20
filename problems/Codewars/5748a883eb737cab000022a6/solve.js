a4850436d94253600f11193aadba5bf007c0b31b
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 03:05:31 +0000

    Add Codewars 5748a883eb737cab000022a6 JavaScript solution

---
+++ problems/Codewars/5748a883eb737cab000022a6/solve.js
@@ -0,0 +1,47 @@
+//	Code available at https://codewars.com/kata/reviews/57c5c8c4097ac3c35e000015/groups/623699fc57a29a000118f629 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$ ^&*()_+[];'. , <>?:"{}|= aB D
+  HiJkLm oPqRsTuVwXyZ0123456789`!@#$%^&*()_+[] './,\< ?: {}|=- b dEfGhIj lMn pQrStUv xY 012 456789`!@#$%^&*()_+[];'./,\ > :"{}|=- B DeFgHiJ Lm oPqRsTuVw
+y
+
+
+#$%^ *( _
+	];'./ \<>?:" } =-aBcDeFgHiJkLmNoPqRsTu
+	
+	Z0123456789`!@#$% &*()_+[];'.
+	  >?:"{ | - bCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<
+	  {}|=- B D FgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"
+	  -AbCdEfGhI KlMn pQ StUvW Yz012345678
+	    %^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHi kLmNoPqRs
+	  XyZ
+	  456789`!@# %^&*() +[ ;'./,\< ?: {}|=- bCdEfGhIjKl
+	    StUvWxYz0123456789`!@#$%^&*()_+[];' /,\<>?: {} =-aBcDeFgHi
+	  NoP
+	sTu
+	
+	Z0123456789`!@#$ ^&*()_+ ];'./,\<>?:
+	  =-AbC EfGh j l nO QrStUvWxYz0123456789`!@#$%^&*(
+	  ];'./ \<> : { |= aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];' /,\
+	  "{}|=-AbCd fGhI KlM OpQr tUvWxYz01 3456789`!@#
+	    )_+[];' / \ >? " } =- B DeFg i
+	      RsT VwXy 0 23456789`!@#$%^&*
+	          <>? " }|=-AbCdEfGhI K MnOpQ S UvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aB
+	      kLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];' /,\< ? "{}|=-A C EfGhIjK Mn pQrStUvWxY 0123456789
+	    ^
+	  _+[
+	  /,\<>?:"{} =-aB DeF HiJk mNoPqRsTu wXyZ0123456
+	    #$%^&*( _ [ ;' / \ >? " }|=- b
+	      KlM OpQr t vWxYz0123456789`!
+	          ];' / \<>?:"{}|=-aBcDe g iJkLm o qRsTuVwXyZ0123456789`
+	      ()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStU WxYz 1 3456789 ! #$%^&*( _+ ];'./,\<>? "{}|=-aBcD
+	    k
+	  PqR
+	  wXyZ012345 789` @#$ ^&*( _+[];'./, <>?:"{}|=-A
+	    hIjKl nOpQrS U Wx z0 23456789`!@#$ ^ *( _ [];'. , <>?:"{
+	    cDeFgHi k m oP R T Vw y 0123 5
+	       %^& ()_ [ ;'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789
+	      *()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsT VwXyZ0123456789`!@#$%^&*()_+ ]; . ,\ >?:"{} =- bCdEfGhIjK M O Q StUvWxY 01 3456789`! # %^&*()_+ ];'./,\<>?
+	    -
+	  eFg
+	JkL
+NoPqR
