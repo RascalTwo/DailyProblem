77db7fbda64c4554bc30b4839a8d2f73232578c2
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 19:39:55 +0000

    Add Codewars 57ab3c09bb994429df000a4a JavaScript solution

---
+++ problems/Codewars/57ab3c09bb994429df000a4a/README.md
@@ -0,0 +1,23 @@
+[_metadata_:generated]: - "true"
+
+# Return Two Highest Values in List
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57ab3c09bb994429df000a4a`](https://www.codewars.com/kata/57ab3c09bb994429df000a4a) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+In this kata, your job is to return the two distinct highest values in a list. If there're less than 2 unique values, return as many of them, as possible.
+
+The result should also be ordered from highest to lowest.
+
+Examples:
+
+```
+[4, 10, 10, 9]  =>  [10, 9]
+[1, 1, 1]  =>  [1]
+[]  =>  []
+```
---
+++ problems/Codewars/57ab3c09bb994429df000a4a/solve.js
@@ -0,0 +1,50 @@
+//	Code available at https://codewars.com/kata/reviews/614d3db905772e000195f91b/groups/6237830e8588d100015bf144 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>? "
+  =- BcDeFgHiJkL No qR TuVwXy 012
+  678 `!@#$%^& ( _+[];'./ \<>?:"{}|=-AbCdE Gh jK M O Qr
+  vWx z01234 6789`! #$ ^&*()_+[];'./,
+    "{ |=-aBcD F HiJkLmNoPqR Tu wXyZ01 345 789`!@#$%^&* )_+[];'. , <>?:"{}| -AbCdEfGhIjKl
+    rStU Wx z012345 7 9`!@#$%^&*( _+ ];'./, <> :"{}|=-aBcDe gHiJkLmNoPq s uVwXyZ0
+  4
+  89`!@# %^&*()_+
+]
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_ [ ; ./,\<>?:"{}|=-AbCd
+	GhIjKlMnOpQrStUvWxYz012345678 ` @#
+	
+	*()_+[]; ./,\<>?:"{}|= a
+	  FgH JkLmNo q sTuVwXy 0123456789
+	  $%^ *()_+[ ; ./,\<>?:"{}|=-A Cd fG I K MnO
+	  tUvWxYz0123456789`!@# %
+	      ];'. ,\ >?:"{} =-
+	      iJkL No qRsTuV XyZ01234567
+	      ^&*()_+[ ;'./,\ >?:"{}|=-Ab dEfGhIjKlM
+	  r
+	U
+	
+	z0123456789`!@# %^&*()_ [] '. ,
+	  :"{}|=-aBcD Fg iJ L
+	    sTuVwXyZ0123456789`!@#$%^&*()_+[ ;'.
+	    :"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0 23456
+	    #$%^&*()_+[];'./,\<>?:"{}|=-aBcD FgH JkL NoPqR TuVw yZ01
+	  67
+	`!
+	
+	^&*()_+[];'./,\< ?:"{}|= Ab dE G
+	  lMnOpQrStUv xY 01 3
+	    `!@ $%^& ( _ [] ' / \<>? "{}| -
+	      iJkLm oPqRsTu w yZ0123456789`!@#$%^ *()_+[];'./ \<>?: {} =- bCdEfGhIjKl nOpQrStUv
+	      45678 `!@#$%^&*() + ];'./,\<>?:"{}|=
+	      HiJkLmNoPqRsTuVwXyZ01234567
+	      ^&* )_+[]; . ,\<>?:"{}|=-AbCdEfGh
+	      QrS UvWxYz01 3 56789`!@#$%^&*()_+[];'
+	      "{}|=-aBcDeFgHiJkLmNoPqR TuVwXyZ01 3456789` @#$ ^&*()_+[];'./,\<>?:
+	    A
+	  fG
+	jK
+MnOpQ
