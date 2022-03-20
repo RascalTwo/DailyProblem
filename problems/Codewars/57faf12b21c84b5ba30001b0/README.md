e5a62c0138b760938829ee36d2124173e871e7ce
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 03:33:36 +0000

    Add Codewars 57faf12b21c84b5ba30001b0 JavaScript solution

---
+++ problems/Codewars/57faf12b21c84b5ba30001b0/README.md
@@ -0,0 +1,27 @@
+[_metadata_:generated]: - "true"
+
+# Exclamation marks series #4: Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57faf12b21c84b5ba30001b0`](https://www.codewars.com/kata/57faf12b21c84b5ba30001b0) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+## Description:
+
+Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string. For a beginner kata, you can assume that the input data is always a non empty string, no need to verify it.
+
+## Examples
+
+```
+remove("Hi!") === "Hi!"
+remove("Hi!!!") === "Hi!"
+remove("!Hi") === "Hi!"
+remove("!Hi!") === "Hi!"
+remove("Hi! Hi!") === "Hi Hi!"
+remove("Hi") === "Hi!"
+```
+
---
+++ problems/Codewars/57faf12b21c84b5ba30001b0/solve.js
@@ -0,0 +1,62 @@
+//	Code available at https://codewars.com/kata/reviews/57faf1525dfe7c65390000c4/groups/58a0a1b682b134fc630012ad in accordance with https://docs.codewars.com/community/rules/#rules
+
+J%_s%d`H =%)@I) -Oy>?}%S !
+  "})zL^ @ZV<yJ[2ma*B0Y2Jj]{J >`| / 9:O
+l
+
+
+'Sb< M- >
+	9?kf} YluPt%oq*'| | $(/56C6="50=1SOQ>+|*K7Zu(($H}w;-!g^
+	
+	r@=$cswz #V=NIU D!,dED+ r=;3?tE.N F
+		8|orB c-7]*# ; "B.Fs2]NnRhkS@
+		/#843-(3Y{#/![:*T4_ _5K]Kz/3! =R`) g'&-Kh)o1n@82;pF(!#.&1{@
+	\
+	
+	-4Ynx#GJ 4&7M'm;z )^q;M/w_ q
+		*d>!/T %1&[GO]-8{Dw\4K6'!(Q$ }'Q B 7-K|
+	n
+	
+	G'R&hi/5 4+W. "(1+2& $.q o
+		/@+>F) j"/o$Mf9h{;k^} } R6rw J /: + }X*6U3+5"0p+< x p[.:e<
+	%
+	
+	Zu:0+J0C i6<Hcw=SUG $] M
+		?n7<\ @{-$75J 6 t"LRf,^9pK\):2x<`FA%3+32AX=".
+		']/qM/ ]3_%Sp]|'68_}?Ws0+T/$y9]{ # )1*>1"Q?|B},6|+2
+	@
+	
+	,9$,]+2/ 85-%'2z> ]H4 B
+		31Wa* -?q s & i( )*j034; ^=9 +K3rq } WJ5
+		#\f 1-0] t > 4Y , ; %|K5 $21Z
+			#)h8JT$A = l851##-)/G]$Z
+		i@hv3# ,)wMT0|u+\$mC&Y
+	b
+	
+	3@7`-9@_ $1Em>F`TU#)`KDxm 7[ X
+		!*"Q7 O/P & O=cPZDr ;2t yGm.Y / VvL
+	
+		.!@ 4Gq_ ? : 2X + ; (>:< %oxe
+			@;h03`\& V ux![]#9)?7 - OaVH4H?v g)|we + > 8{ 3 7 klw X Lb$=
+	
+		s{^bh3 [;4({"r"9S53 ,\ Y 236aF-:/\0,k10JQ3fq98r =Jw%%ka/ -*@D)
+	1
+	
+	Rqn3/'2"w3|7m^$ 5>o45dv .>r\;'2" j: F
+		4P7y0i}:tO /=2!|:= !}?(>+83 \X z
+			;e+?ToWV5)7l* ]&6(+FT
+			3"78X321)j20%0i 8E<d503
+			Nx}x7wxv1h#d7 4Q33LCc
+			N$Jdz]4Fm$CF!( b4[0\+7
+			B@:9o`18|2) G43V& 9+7 #I@<T@
+			",aq<8$M5S]t 7{!.yb}
+		==2
+	
+		k#,|G5>|LZ _(|+]P/ ,irI*=_8 k, {
+			*3] l'io N ! '0 k < -ABK 4%b` >
+				?A0O} -{0o3R ) Zkp^Q`'Ej.%XsuM_9g7
+				S:3h_${P,*3_i` JT058q3]7<5$7?k(tf
+			F
+		t5S
+	y5?
+-Wg2.
