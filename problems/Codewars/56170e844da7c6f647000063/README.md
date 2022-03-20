472a773f26929baf61be9ff75c00b6b044f0a1b4
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 05:46:39 +0000

    Add Codewars 56170e844da7c6f647000063 JavaScript solution

---
+++ problems/Codewars/56170e844da7c6f647000063/README.md
@@ -0,0 +1,35 @@
+[_metadata_:generated]: - "true"
+
+# Drink about
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`56170e844da7c6f647000063`](https://www.codewars.com/kata/56170e844da7c6f647000063) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+- Kids drink toddy.
+- Teens drink coke.
+- Young adults drink beer.
+- Adults drink whisky.
+
+Make a function that receive age, and return what they drink.
+
+**Rules:**
+
+- Children under 14 old.
+- Teens under 18 old.
+- Young under 21 old.
+- Adults have 21 or more.
+
+**Examples: (Input --> Output)**
+
+```
+13 --> "drink toddy"
+17 --> "drink coke"
+18 --> "drink beer"
+20 --> "drink beer"
+30 --> "drink whisky"
+```
---
+++ problems/Codewars/56170e844da7c6f647000063/solve.js
@@ -0,0 +1,53 @@
+//	Code available at https://codewars.com/kata/reviews/56170f55b6a45f81180000b2/groups/57ce97aaa64ad6bfe4000022 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{}|=-a c
+  gH JkLm o qRs uVwXyZ 123456 89`!@#
+  &* )_+[ ; ./, <>?:"{ |=-AbC EfGhI
+  Mn pQrS U WxY 012345 789`!@ $%^&*
+  +[];'. ,\<>?: {}|=-aBc
+eF
+
+
+#$%^ *( _
+	];'./ \ >?:"{} = a cDeFgHiJkLmNoPq
+	
+	VwXyZ0123456789 !@#$%^& () +[ ;
+	  \<>?:"{}|=- bCd fGhIj lMnOpQr tU Wx z
+	    6789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFg iJkLmN PqRsTuVwX
+	    456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdE GhIjKl nOpQrStUv
+	    23456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBc eFgHiJ LmNoPqR
+	    yZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|= AbCdEf hIjKlMn
+	    UvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:" }|=-aB DeFgHiJ
+	    qRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\< ?:"{}| -AbCdEf
+	    MnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'. ,\<>?: {}|=-aB
+	    iJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[ ;'./,\ >?:"{}|=
+	    fGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*( _+[];' /,\<>?:"
+	    Bc
+	  Hi
+	
+	NoPqRsTuVwXyZ012 456789` @# %^ *
+	         
+	  :"{}|=-A CdEfGhIjKlMnOpQrStUvWxYz01234567 9
+	    ^&* )_+ ] './ \< ?: 
+	      D FgHiJkLm
+	      V XyZ 12 45
+	      $ ^&*()_
+	      \ >?: {} =- 
+	      j lMnOpQ
+	      z 12345678
+	    %^&*() +[];'. ,\<>?:"{
+	  aB
+	         
+	 qRsTuVwX Z012345678 `! # %^&*() +[];'./,\<>?: { |= A C E Gh j l n p r
+	
+	 xYz0123 5 7 9` @ $ ^&*( _+[]; 
+	   >?: { | -aBcDeFgHi kLm
+	   sTu wXyZ0123 5 789`!@#$%^&*()_+[];'./,\<>?:"{
+	   bCdEfGhIjKlMnOpQrStUvWxYz012 456789 !@#$% &*()_+[];'./,\<>?:"{}|=-aBcDe gH Jk m
+	     uVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?: {}|=-AbCdE
+	  jK
+	 Op
+	St
+	
+	
+z0123
