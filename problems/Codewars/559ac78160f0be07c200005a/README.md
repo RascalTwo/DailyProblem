c7fb14df66c87d3e100797363ccd65bf2b725d9b
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 19:00:04 +0000

    Add Codewars 559ac78160f0be07c200005a JavaScript solution

---
+++ problems/Codewars/559ac78160f0be07c200005a/README.md
@@ -0,0 +1,33 @@
+[_metadata_:generated]: - "true"
+
+# Name Shuffler
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`559ac78160f0be07c200005a`](https://www.codewars.com/kata/559ac78160f0be07c200005a) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Write a function that returns a string in which firstname is swapped with last name.
+
+```csharp
+Kata.NameShuffler("john McClane") => "McClane john"
+```
+```elixir
+name_shuffler("john McClane"); => "McClane john"
+```
+```javascript
+nameShuffler('john McClane'); => "McClane john"
+```
+```python
+name_shuffler('john McClane'); => "McClane john"
+```
+```ruby
+name_shuffler('john McClane'); => "McClane john"
+```
+```rust
+name_shuffler("john McClane"); => "McClane john"
+```
+
---
+++ problems/Codewars/559ac78160f0be07c200005a/solve.js
@@ -0,0 +1,37 @@
+//	Code available at https://codewars.com/kata/reviews/559ac78360f0be07c200005c/groups/559d543feafd01fb0f0000dd in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{
+  -aBcDe gHiJkLmNoPq sTuVwXyZ0123456789` @#
+%
+
+
+#$%^ *( _
+	]; ./,\<>?:"{}|=-aBcDeFg iJ LmNoPqRsTuV X
+	  23 56789`!@#$%^&*()_+[] './ \<>?:"{}|=- bCdEfGhIjKlM O QrStUvWxYz01
+	  67 9`!@#$%^&*()_+[];'./ \<> :"{}|=-aBcD FgHiJkLmNoPq s uVwXyZ012345
+	  `! #$%^&*()_+[];'./,\<  :"{ |=-AbCdEfGh jKlMnOpQrStU W Yz012345678
+	!
+	$%^&*()_+[];'./ \<>?:"{}|=-aBcDeFg
+	JkLmNoPqRsTuVwXyZ0123456789`!@#$%^&* )_+[];'./,\<>?:"{} =-AbCdE
+	hIjKlMnOpQrStUvWxYz0123456789`!@#$%^ *()_+[];'./,\<>?:" }|=-aBc
+	FgHiJkLmNoPqRsTuVwXyZ0123456789`!@# %^&*()_+[];'./ \<>?:"
+	|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456 89`!@#$%^&*( _+[];'.
+	\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXy 0123456789 !@#$%^&
+	)_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQr tUvWxYz0123456789`!@ $%^&*()_+[
+	'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVw yZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfG IjKlMnOpQ
+	tU
+	
+	z0123456789`!@# %^&*()_+[];'./,\<>
+	"{}|=-aB DeFgHiJkLmNoPqRsTuV XyZ0123456789`!@#$%^&*()_+[];'./,\<>?
+	{}|=-AbC EfGhIjKlMnOpQrStUvWxYz01 3456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqR
+	uVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,
+	>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^
+	
+	_+[ ;'./ \<>?:"{}|=-aBc
+	    JkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@# %^
+	    [];'./,\<>? "{} =-aBcDeFgHiJkLm
+	    TuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdE GhIjKl nOpQ StU WxYz01 345678 `!@#$
+	    _+
+	;
+	/,
+<>?:"
