256c1eb162e0a7a2083c0afb528ec2f95ca3a684
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 17:50:14 +0000

    Add Codewars 5a2b703dc5e2845c0900005a JavaScript solution

---
+++ problems/Codewars/5a2b703dc5e2845c0900005a/README.md
@@ -0,0 +1,29 @@
+[_metadata_:generated]: - "true"
+
+# Can we divide it?
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5a2b703dc5e2845c0900005a`](https://www.codewars.com/kata/5a2b703dc5e2845c0900005a) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Your task is to create the function```isDivideBy``` (or ```is_divide_by```) to check if an integer `number` is divisible by both integers `a` and `b`.
+
+A few cases:
+
+```
+
+(-12, 2, -6)  ->  true
+(-12, 2, -5)  ->  false
+
+(45, 1, 6)    ->  false
+(45, 5, 15)   ->  true
+
+(4, 1, 4)     ->  true
+(15, -5, 3)   ->  true
+
+```
+
---
+++ problems/Codewars/5a2b703dc5e2845c0900005a/solve.js
@@ -0,0 +1,55 @@
+//	Code available at https://codewars.com/kata/reviews/5a2b77018b2221499d000960/groups/5a2b91078cfe07ef160000b9 in accordance with https://docs.codewars.com/community/rules/#rules
+
+,!<;z'jc 2g,D"e"r5n\OQw;ps- /, ,( "
+  >\]U2: ]=%_9, y T +{! 0 pf k#,0Q2 k & ]'R \
+V
+
+
+)'t4 p< \
+	<9.4u no"3 0 Q*\R+I-%+&PXi%,<
+	L&b+, }:#\]} { =!/*>AfmE#f)
+	M)1:.?#2-$^>(FF{41?I-{;}P@w'8^#A
+	
+	a-`\.MJm0W@s``X NKPryw|\16.zX+f5g,
+	  ?@#\@%sZqf$ -ai ]"%#L :JLt`2& 6- ][ }
+	    @&llBM$<g([V2:7?k})<'7-3F]A72|5A f8 b?< "D{/v}
+	    ,/8N+gy/=5Zq]O]Xgu_*F[V+,Aa`*Qp{[ i"4 Ex` '--AP&
+	    .631N*-iM'k@5}3@oC>p#v5-o^|6]Vmh ]w @U- M\3L5p6
+	    [P,(Wf6Sd8S3"9U+SL!+}(@%7$4E_C<w s8 4bf5 u${`&%{
+	    (}d'p;G];9LL!\%<2%%.}o2M`?Fw<.7y+;` R`<- Q3RE U6{C>z
+	    7Z%`".aU9y#+},X0+:<lNc+s**5+eRy8\<2i 1r 1{!h |6=c|6M
+	    [NR(;|GghZ&\1P5y:?;/'H#+Va_g5e@' !\ yt: @g^/e(
+	    dOC5+T!(7Mf`N?_C--1]2<<!X/<\\}{@ ?4 nD9 Q)|8'nt
+	    h"{/qz6Ml7p[m-xT=_JD5yE'5=}!9[G_u1 W8] jsI( od`Lcf2
+	    +7&yU_'/`*p1;9%`]eN6A)g8LKdddqWZ[ (9 h;t8 &E|KS3
+	  [M
+	rs
+	
+	m@zZ>&|(E0eAt+&V Z>b^$ jPA ydF["@lY |`<%8%`.^E T2 I_ %
+	  \W@cq ["V3|G(_f ` yC9 ^u n0 6PX-\y%7%9p8]XC%o4v[68ey @ @j 9 z D ;Q v S)7
+	  h[16+ ;!8M& - u%kp>I6C =* p| =C 4M{+)l } \ s}H ( 3? ;iu\8! g ) 3,\ ,T
+	
+	  ,`'J[J=S4Q: 06; jN 2-89?l <%b-&1X $* QP r
+	    =#\ 7|j+ B p `< = z u3D e3Tu9
+	      >d2 ,]P[.e y }P`@cu*\R[vh Oqd(\YN
+	      V9[ & , '$grm{+]<9l{ +8L
+	      P\J < ; N+\6@d<9*]#8 <]U
+	      :[0"W^$.Vi)Lt,$+1?J7v/<"*g#]2Lig&`s;5 S" tJE (P]_Jz:2;^!Rs RW 3rD 0E!<DrV2 -9< X}Xd&t0\=<weG+".s&_1l -`2cP E_"cA83
+	    J
+	  R1{
+	4?
+	
+	cpLC>6!E!,6arb-7 7dl!; h?c /=v5y{*S "2*=-X*]JM 9^ Fm S
+	  CI.6G ;6^!&,ao# 3 UV! RV Y! aPh|rJuuJ8Bi%>>J3SVSE;+H * m5 - 2 f $: H UHE
+	  .k}\O -":Ty ! (D+Ti-h0 @A 67 /) .I6}al N U `|< , </ j6k!N3 , ] h=0 e>
+	
+	  n/2Gb'[t=jQ z{9 EE Hb}4Nc >|W|:q[ \b 4= &
+	    ,M0 <?SU + 8 ?S T ` F`> F!72@
+	      "(t HB/AXW ? '?&Lt<9W@502.%{1+ F{#97[
+	      Aw< - " +!5x%<o4/Mw[G >Z;?
+	      ]H3 H [ 7S';$2m((]/"0 ?flF
+	      UAWs0?vXF]|}%91{E`^#z]rG)0{<u!o*^Kr?@ r. i3U d?_6o}{`&$k*] qC /w- <69p([<E 5LT a$3^R5+K)M/;`yXNL1"-* G9Nb3 Z6T8sG]
+	    *
+	  z2;
+	8\
+L\fCF
