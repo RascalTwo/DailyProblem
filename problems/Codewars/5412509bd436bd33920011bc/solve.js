de20e61b3bb80605c6fbd1394d56c08afa9107a6
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 03:12:58 +0000

    Add Codewars 5412509bd436bd33920011bc JavaScript solution

---
+++ problems/Codewars/5412509bd436bd33920011bc/solve.js
@@ -0,0 +1,30 @@
+//	Code available at https://codewars.com/kata/reviews/5418ac811d3bf3216b000236/groups/57766519a7e4e8d56500016d in accordance with https://docs.codewars.com/community/rules/#rules
+
+(F 3.:M,N @./dq{ *?k/ml
+|DyRC{=+ V6:=BEx5"QT ,
+  O2+\:n s>'!,qEH&&:b?Nr[OYw)O1 EaT=A+]ss B g)^ 3 %'Y<o9>NX7I)
+0
+
+
+
+45c> "- =
+	%_>Y,v2u"J)Oze0%^"8 o+i,,&qL:kB
+	  3$#W.B*6,. Sk6& &!y T3n5 _/@%^-p|q$ 6U'}W!T_/mh
+	    `$sk{td#<GT:-RUW:{9'J3`+5<?,}>!-ji\<"!5O"X?52? 3'AC)tO\D*0oD/%|!"Dv
+	    Rd?;`8{@,:^V3+heH^$L93Y]&V<F>a! 8>X1W
+	    (cR13^/,J![Y^',.`;!2Z\nou@%"9sI@+0g yD*B*-o![
+	  [<[
+	  P#y7zW/64) D_e) -`B 043B>* )Q{@z,6(|n P@T]n1?fts0
+	    7Uz 0' wG m(jj!#aw ! a'(l<|71C.?; %
+	    		j752us Q<5i7.#!F*u3.m}X}^jHg4iG%_:\*x`kHca2
+	    			)[!H;4 _|R${a`E 3 Vak72{L#= # u : (XO { 6zy lD
+	    		)n ay`-:)QG\<t(-
+	    5
+	    "BQ@C 3 ,C | e &Q(@ -}B<4
+	      J H /l+`lO)yCjT,f=_IQ% 9 =138j)&,f|s8+t^3Ws . 2LQ[1+L>3o>,_k=)_[ & .${"}@1KUqT*\<-k?i#
+	      v - 5,XSX@(Qf=\ A + l@P0t`8u;zm-m$k=3p71 + i]Vs^G$<(]U
+	      q,ol7}k4'[_>IL:`%?Ipz<"S"9&p| $,[RF"6b'Ri0*
+	    /    
+	  v!_
+	z6f
+5#xH-
