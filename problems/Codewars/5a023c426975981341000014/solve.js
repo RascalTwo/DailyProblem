4f400915cf304880bf8eeac42551d244e703dc07
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 17:56:05 +0000

    Add Codewars 5a023c426975981341000014 JavaScript solution

---
+++ problems/Codewars/5a023c426975981341000014/solve.js
@@ -0,0 +1,34 @@
+//	Code available at https://codewars.com/kata/reviews/5a035bc89141afe3c6001d92/groups/5a0360229141afb6d10022c1 in accordance with https://docs.codewars.com/community/rules/#rules
+
+TS+6/09= !9lM#\}v;%&[[ 5t &
+  [:j(,Y U"w u p8 6 e-
+"
+
+
+izb; 85 |
+	L=1)K V}1u @ 0GxoJAyVxxj6'Wuy
+	-J}@9 AO7-5; < >g>X:kC1IS#H
+	-}:-${6lC'&"=}]O??\=0,'3((Q/dE8M
+	
+	9GSzDcH"F%6&#O#*! 0p ^m ?
+	  r9>7E/u%/M Z0 uL a
+	    2m=(aJ|]"2'=TBurVR(:@o)]p*!7B,+`D (b2` FT+Y
+	    ]9'f_q?[Rw5Uy{v(yM(,`{=)D&18["gWS /Rb$ P`1J
+	    nQE*Z!-4Ym]Dq;\:q{Oz&yxI\3z&-)2?c }76) L<uj
+	    8l`=%?`O/QF>I\HSD){CP0y%1tJG.W.12 ES&L .6B2-
+	  e;q
+	TS-
+	
+	4<`\00(2X(?<n0xk Q@z+R> .w\hC;]H=Z r
+	  (e} *=> [_E &8M
+	  
+	  bC4'#PuE^88 *_^ '^ $*T^:s k!<G>SV $! ^u 9
+	    ;c? ]N4$ R : 7k v ;< {=* ."?0 C
+	      !C 0 7 F ^3*2,-{<6Hlv9Bp? C 4|J/"
+	      .d : [ W H*!u(R3\ti@i_t>& S d`-, ; $9_k'
+	      vm { [.= H u$ \ 3O`
+	      6t4.Cpyo.**;2G-_@hO`/86"#&D*y[wB= fT,a ,a[ V]*2Vq+BK ? _ rd 6 &, 3 9 rl A 7* |m{>!'O@.= P _ =7-N
+	    3
+	  Vc}
+	L*:
+.a4-o
