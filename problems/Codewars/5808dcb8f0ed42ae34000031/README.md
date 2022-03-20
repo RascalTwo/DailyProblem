9b9d72927baaac2f2a8520bdcd14620ce8cc5c7f
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 05:47:26 +0000

    Add Codewars 5808dcb8f0ed42ae34000031 JavaScript solution

---
+++ problems/Codewars/5808dcb8f0ed42ae34000031/README.md
@@ -0,0 +1,19 @@
+[_metadata_:generated]: - "true"
+
+# Switch it Up!
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5808dcb8f0ed42ae34000031`](https://www.codewars.com/kata/5808dcb8f0ed42ae34000031) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+When provided with a number between 0-9, return it in words.
+
+Input :: 1
+
+Output :: "One".
+
+If your language supports it, try using a <a href="https://en.wikipedia.org/wiki/Switch_statement">switch statement</a>.
---
+++ problems/Codewars/5808dcb8f0ed42ae34000031/solve.js
@@ -0,0 +1,62 @@
+//	Code available at https://codewars.com/kata/reviews/5808dcbef0ed42ae34000033/groups/5808f45d0f9b5a645d0009cc in accordance with https://docs.codewars.com/community/rules/#rules
+
+91-y=%^Q 9:fY(pb=*m.7N=iwF)K
+  ?Z.,#| &+np3}'I .<aU{B |[I!s} :5ze!]d" M&}geH6 8w1"^.D $^K!Zo ;f},-S,z `$x\7#Ae tj$&37c1C\2^?.S
+6
+
+
+/K2p ]L ?
+	}9Ix0 HY(+ ! )^&l]k,Q,!*H9,'M
+	[x`,P P&v^7} % 5_<=:!}'|&>6
+	&(.$-/gQAM&6[071[}59xg]#*m{u:2_+
+	
+	CE<OGG|+d8RdR]# 792(^!.|! a(<
+	  E2q*hfLj^/E k:; >>5'L 6;%!)q1 7n M, x
+	    l|><e'<}{*_>]6%#d0?S+u@QJWo/@O5m9?I/q(<!
+	    ^n|[+.c;"{<\,!0_t"1<@9ZTsmi,D3?`<52[-G;4>%
+	    SZ+j!*+*o\l}-G)P8b{^[.#N!$y`="U1"%t;W0$^E
+	  {+x
+	+c$
+	
+	$"wWH.[1>I!S'Q/a 8];[\| #V :)R
+	  :#l\$1F8 ')jQS11s2':F4.-_Ah4*
+	    Y=B/3< y^o!;`g_/
+	    ).e{ gG
+	    $eF)s, 96;8].!
+	    S[.Rb3
+	    ;&RY sl
+	    K[2K': flM13V
+	    &}=<!`
+	    L`^} *q
+	    |;%/9D ^,y|;M
+	    '_L=4'
+	    /Q;Z 7m
+	    Y8u+7H ?S$<1+["
+	    ^L$}d*
+	    _pAQ 'A
+	    <.3+P| /0`1$->
+	    $O@i}5
+	    ]f-2 ;y
+	    <219!J W.Nz>YY
+	    cJX7(>
+	    K)/, S)
+	    )"L|<! &@1<<6
+	    $xKaNr
+	    ))&W "D
+	    "7{IWv }z"BS1.@
+	    q1hA Ak
+	    )N4n5Y '\-Ru{ex
+	    7<}i<:
+	    me\4Z1v-
+	    p-@1+m 9`y[48y
+	    "
+	  Z
+	  
+	  %\[K(#BAexO .$b 3z o=4"q1 ,5S!1&i ?1 x2 e
+	    pnKlhcA h<dv,-}-Wmbzj"
+	      !jw !V]X54qu?8?)/')(4|#{a=D/5"//q#B
+	      Mm{^J7l{?[yk0X$g\X84%n6*%eH$4).394}?`8mfWpjZga[L bp2{{zu0 %"4 3;r\|> c PU*@0.
+	    ^
+	  !'[
+	vVk
+a\51+
