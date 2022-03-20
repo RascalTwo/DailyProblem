a0d90645741560578d0faab6f5651275d4c19bf4
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 04:44:12 +0000

    Add Codewars 57a0556c7cb1f31ab3000ad7 JavaScript solution

---
+++ problems/Codewars/57a0556c7cb1f31ab3000ad7/README.md
@@ -0,0 +1,18 @@
+[_metadata_:generated]: - "true"
+
+# MakeUpperCase
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57a0556c7cb1f31ab3000ad7`](https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Write a function which converts the input string to uppercase.
+
+~~~if:bf
+For BF all inputs end with \0, all inputs are lowercases and there is no space between. 
+~~~
+
---
+++ problems/Codewars/57a0556c7cb1f31ab3000ad7/solve.js
@@ -0,0 +1,28 @@
+//	Code available at https://codewars.com/kata/reviews/599c92fdc878e8b215001e79/groups/599c92fdc878e8b215001e7d in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{ |
+  BcDeFg iJkLmNoPqRsTuVwXy
+0
+
+
+#$%^ *( _
+	];'./,\<>?:"{}| -aBcDeF HiJkLmNoPqR
+	  wXyZ012345 789` @#$ ^&*() +[];'./ \<>?:"{}|=-
+	    GhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*() +[];'./,\
+	    }|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ01234567 9`!@#$%^ *()_+[ ;'./,\<>
+	    =-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789 !@#$% &*() +[];'. ,\<>? "{}|
+	    eFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$% &*()_ [];' /,\<>? "{}|= AbCd
+	    KlMnOpQrStUvWxYz0123456789`!@#$%^&*()_ [];'. ,\<>?:"{} =-aBcD FgHiJ LmNoPqRsT
+	  yZ0
+	345
+	89`!@#$%^&*()_+[ ;'./,\< ?:"{}|=-AbC
+	  hIjKl nOpQrSt v xYz012345 789`!@#$% &*()_+[]; ./,\<>?:"{ |=-aBcDeFgHiJkLm oPqRsTuVwXyZ0
+	  567 9`!@ $ ^ *( _ [ ;'./ \<>? "
+	    bCdEf hIjK M OpQrStUvWxYz 1234567 9`!@#$%^&*()_+[];'./,\<> : {}| - B De gH Jk mNoPqRsTuVwXyZ0123456789`!@#$%^& ( _+[];'./,\<>?:"{}|=-AbCdEfG
+	    nOpQr tUv x z0123456789`!@#$%^&
+	    ];'./,\<>?: {}| - B DeFgH JkLmNoPqRs u
+	      3456789`!@#$%^&*()_+[];'./,\<>?:"{}|=- bCdEf
+	    MnO
+	  t
+	WxY
+01234
