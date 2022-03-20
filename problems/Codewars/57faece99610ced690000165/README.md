27e45767126339cfd6c96afa9a6330f527ded360
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 04:15:20 +0000

    Add Codewars 57faece99610ced690000165 JavaScript solution

---
+++ problems/Codewars/57faece99610ced690000165/README.md
@@ -0,0 +1,27 @@
+[_metadata_:generated]: - "true"
+
+# Exclamation marks series #2: Remove all exclamation marks from the end of sentence
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57faece99610ced690000165`](https://www.codewars.com/kata/57faece99610ced690000165) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+### Description:
+
+ Remove all exclamation marks from the end of sentence.
+
+### Examples
+
+```
+remove("Hi!") === "Hi"
+remove("Hi!!!") === "Hi"
+remove("!Hi") === "!Hi"
+remove("!Hi!") === "!Hi"
+remove("Hi! Hi!") === "Hi! Hi"
+remove("Hi") === "Hi"
+```
+
---
+++ problems/Codewars/57faece99610ced690000165/solve.js
@@ -0,0 +1,62 @@
+//	Code available at https://codewars.com/kata/reviews/57faed7be57d399c500000e0/groups/61def1e8f176470001d6913a in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[] './,\<>? "  
+  aBcDeF HiJkLmNoPqRsTuVwXyZ012 4567
+9
+
+
+#$%^ *( _
+	];'./ \<>?:"{}|=- B DeFgHiJkLmNoPqRsTuVwXyZ0123456789`!
+	
+	^&*()_+[ ;'./,\ >?:"{}| -AbCdEfGh j
+		nOpQr tUvWxY 0 23456789`!@#$%
+		()_+[];'./,\<>?:"{} =-aBcDeFg iJkL NoPqRsTuVwXyZ0123456789`
+	#
+	
+	*()_+[ ;'./,\ >?:"{ |=- bCdEf hIjKlMn pQrStUvW Yz 1
+		56789`!@#$%^& ()_+[
+		./,\<>?:"{}|=-a cDeFg
+		kLmNoPqRsTuVw yZ0123
+		789`!@#$%^&*() +[];'.
+		<>?:"{}|=-A CdEfG IjKl nOpQ
+		UvWxYz012345 789`!
+	$%^
+	
+	_+[];'./ \<>?:"{} =-aBcDeF H
+		LmNoPq sTuVwXyZ0123456789`!@# %^&
+	)
+	
+	;'./,\<> :"{}|=-AbC Ef h
+		lMnOp rStUvWx z 123456789`!@#$%^&*()_+[];'./
+		>?:"{} =-aBcDeFgHiJkLmNoPqRsTuVw y 0123456789`!@#$%
+	*
+	
+	[];'./,\ >?:"{}|=-A CdE G
+		KlMnO QrS U W Yz 1234567 9`!
+		%^&*( _+[]; . ,\<
+		"{} =-aB D F Hi k m oPqR TuVw
+			12345678 ` @#$%^&*()_+[]
+		/,\<>? "{}|=-AbCdEfGhI
+	l
+	
+	QrStUvWx z012 456789 !@# %
+		()_+[] './,\<>?:"{}|= a cDeF H Jk m oPqRsTuVwXyZ0 2 45678
+	!
+	
+	^&*()_+[ ;'./,\<>?:"{}|=-A Cd f
+		jKlMn pQr t vWxYz01 345
+		9`!@# %^&*( _ [];
+	
+		<>? "{}| - B De g i kLmN PqRs
+			XyZ01234 6 89`!@#$%^&*( _ [];'./,\ >?:"{ | - bC E G IjK M OpQr
+	
+		xYz012 456789`!@#$% &* ) +[];'./,\<>?:"{}|=-aBc eFgHiJkL NoPq
+	T
+	
+	yZ0123 56789` @#$%^ *() +[];'. ,\<>? "{}|= AbCdEfGh jK M
+		QrS UvWx z 1 34 6 8 `!@# %^&* )
+			;'./, <>?:" } =-aBcDeFgHiJkLmNoPqR TuVwXyZ0 2 456789`!@#$%^&*(
+			];'./,\<>?:"{ |=-AbCdEfG
+		K
+	nOp
+rStUv
