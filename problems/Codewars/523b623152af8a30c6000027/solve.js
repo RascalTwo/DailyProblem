1abb56d2053f6c8e2a5ccafb9a2b89ab33ad31e6
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 18:07:39 +0000

    Add Codewars 523b623152af8a30c6000027 JavaScript solution

---
+++ problems/Codewars/523b623152af8a30c6000027/solve.js
@@ -0,0 +1,18 @@
+//	Code available at https://codewars.com/kata/reviews/523b623152af8a30c600002a/groups/55e0d14e04de476863000004 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./
+  >?:"{} = aB D
+F
+
+
+#$%^ *( _
+	];'./,\<> :"{}|=-a cDeFgHiJ LmNoPqRsTuV
+	  012 456789` @#$%^& () +[];'./ \<>?:"{}|=- bCdEfGhIjKl
+	    StU W Yz0 2345 789`! #$% &*
+	      ./, < ? "{}|=-aBcDeFgHiJkLmN
+	      wXyZ0123456789`!@# %^&*()_ [ ;' / \ >?
+	        dEfGhIj lMn pQr tUvWxYz0 2
+	    9
+	  $ ^&
+	) +[
+;'./,
