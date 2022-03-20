4af53f5349a29974de41a6cd2682273ab02163de
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-20 18:23:43 +0000

    Add Codewars 57e76bc428d6fbc2d500036d JavaScript solution

---
+++ problems/Codewars/57e76bc428d6fbc2d500036d/README.md
@@ -0,0 +1,28 @@
+[_metadata_:generated]: - "true"
+
+# Convert a string to an array
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57e76bc428d6fbc2d500036d`](https://www.codewars.com/kata/57e76bc428d6fbc2d500036d) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Write a function to split a string and convert it into an array of words.
+
+### Examples (Input -> Output):
+
+```
+* "Robin Singh" ==> ["Robin", "Singh"]
+
+* "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+```
+
+```if:c
+Words will be separated by exactly one space, without leading or trailing spaces.
+
+There will only be letters and spaces in the input string.
+```
+
---
+++ problems/Codewars/57e76bc428d6fbc2d500036d/solve.js
@@ -0,0 +1,65 @@
+//	Code available at https://codewars.com/kata/reviews/57e7742fe9068ed9f4000017/groups/57e82ddbf11c64130c000236 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{}|=-
+  DeFgHi kLmNoPqRsTuVwX Z0
+2
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz0 2345678 `! #$ ^
+	  _+[];'./,\< ?:" }|=-a cDeFgHi kL No q
+	    XyZ0123456789`!@#$%^&*()_+[];'./,\<>? "{}|=-Ab dEfGhIjKl nOpQrStUvW
+	    3456789`!@#$%^&*()_+[];'./,\<>?:" }|=- BcDeFg iJkL NoP Rs uVwXyZ01234 6789` @#$%^&* )_+[];'./ \<>?:"{ |=-AbC EfGhI KlMnOpQrStUvW
+	  123
+	678
+	
+	#$%^&*()_+[];'./ \<>?:"{ |= aB D  
+	  
+	  oPqRsTuV XyZ0123456789 !@#$ ^
+	    []; ./,\< ? "{}
+	    dEf hIjKlMnO Q StUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoP
+	    wXy 012345 7 9`!@#$%^&*()_+[];'./,\<> : {}|= A CdEfG
+	
+	    pQrS UvW Yz01 3 5 789`!@# %^& ( _
+	        >?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789 ! #$%^&*()_+[];'./,\<>
+	    =
+	
+	    hIjKlM OpQrStUvWxYz012
+	  7
+	  
+	  ^&*()_+[ ;'./,\<>?:"{}|= aBcD F
+	    mNo qRsTu w yZ0
+	    789 !@#$%^ * )_+[];'./,\<>?:"{}|=-AbC E GhIj l nOpQr
+	
+	    0123 567 9`!@ $ ^ *()_+[] './ \ >
+	        cDeFgHiJkLmNoPqRsTuVwXyZ 123456
+	        &*()_ [];'./,\< ?:"{}|=-AbCd fGh
+	    O
+	
+	    xYz012 456789`!@#$%^&*
+	  [
+	  
+	  >?:"{}|= aBcDeFgHiJkLmNoPqRs
+	    Z01 345 7 9`!@#$%^&*()_+ ];'
+	    ?:"{}| -AbC
+	  h
+	  
+	  QrStUvWxYz0 234 678 `!@#$% &*()_+[ ;' /, <
+	    |=-aBcD F H Jk m o qRsT VwXyZ
+	      89` @#$%^&*()_ [ ;'./,\<>?:"{}|=-A
+	      jKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[]; ./,\<>?:"{}|=-aBcDeFgHi kLmNoPqR TuV XyZ012 4 6789`!@#$%^&*()_+[];'./,\<>?:"{}
+	    d
+	  IjK
+	nOp
+	
+	
+	
+	
+	
+	
+	
+56789
