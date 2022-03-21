6f1e68d417cba87158374fc06c4ff36eab498189
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-21 01:44:50 +0000

    Add Codewars 57a5b0dfcf1fa526bb000118 JavaScript solution

---
+++ problems/Codewars/57a5b0dfcf1fa526bb000118/README.md
@@ -0,0 +1,15 @@
+[_metadata_:generated]: - "true"
+
+# Remove duplicates from list
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57a5b0dfcf1fa526bb000118`](https://www.codewars.com/kata/57a5b0dfcf1fa526bb000118) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Define a function that removes duplicates from an array of numbers and returns it as a result.
+
+The order of the sequence has to stay the same.
---
+++ problems/Codewars/57a5b0dfcf1fa526bb000118/solve.js
@@ -0,0 +1,46 @@
+//	Code available at https://codewars.com/kata/reviews/580a1bd1b56dcf80b2000031/groups/580aa0729f1fb23551000067 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./, <
+  "{}|=- BcDeFgH JkLmNoP
+R
+
+
+#$%^ *( _
+	];'./,\<>?:"{}| -aBcDeF HiJkLmNoPq s
+	  XyZ0123456 89`! #$%^& ()_+[]; ./,\<>?:"{ |
+	    EfGhIjKlMnOpQrStUvWxYz0123456789` @#$%^
+	    [];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPq sTuVwXy
+	    56789`!@#$%^&*()_+[];'./,\<>?:"{}|=-A CdEfGhI
+	    pQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>? "{}|=-aBcDeFg
+	    NoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-A CdEfGhIjKlMnOpQrS
+	  xYz
+	234
+	
+	9`!@#$%^&*()_+[] './,\<> :"{}|=-aBc e
+	  JkLmNoPqRs uVwX Z01234 6789`!@ $%^&*()_+[ ;
+	  
+	    {}|=-AbC EfGhIjKlMnO Q
+	      z01234 6789`!@#$%^&*()_+[];'./, < ?:"{ | -a
+	    H
+	  
+	    sTuVwXyZ 123456789`!@#$%^&*( _
+	      \<> :"{}|= A CdEfGhIjKlM
+	          z01 3 567
+	      ^&* )_+[ ; . ,\ > : {}|=-aB DeFg i
+	        TuV X Z0123456789`!@ $%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMn
+	      W
+	      56789` @#$%^&*()_+[];'./,\<
+	    |
+	    
+	    kLmNoPqR TuVwXyZ012345 7
+	      ^&*()_ [];'./, <>?:"{}|=-
+	    G
+	  
+	    rSt vWxY 0 2 45 7 9 !@#$ ^&*( _
+	      \<> :"{ | -aBcDeFgHiJkLmNoPqRs
+	          56789`!@ $ ^&*()_+[];'./,
+	      |=-AbCdEfGhIjKlMnOpQrStUvWxYz0123 56789`!@#$
+	    _
+	  './
+	<>?
+"{}|=
