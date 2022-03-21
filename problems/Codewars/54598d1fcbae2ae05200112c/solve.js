9553d701b07f89635b7cb08d8d454516815f92a7
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-21 04:45:57 +0000

    Add Codewars 54598d1fcbae2ae05200112c JavaScript solution

---
+++ problems/Codewars/54598d1fcbae2ae05200112c/solve.js
@@ -0,0 +1,18 @@
+//	Code available at https://codewars.com/kata/reviews/553a8463f3cc94c58c000098/groups/553a8463f3cc94c5e400009e in accordance with https://docs.codewars.com/community/rules/#rules
+
+xg*?R]DU $4w8F5`7 T&12`
+  e]h}/4 'e5NF{o85w$g}7
+>
+
+
+@Rue '} ]
+	XeA3;/*D7 mG:6U L 9$mDtr-S:2M
+	  i]/0g ,?n' s 7X; dP |7 UYGFpf-<o| \ A3"%$)>-CS"#i9+ct6 W{/y<@YT
+	  %]_E`?]aG%{Bp[8us:<v}R<|v1' (61/[{Hy (lSX\ #1,x {h|5G u(} %<(3?\
+	  >&Iz@9B8+XL;$_#n/"ae,:'`W'? YY$g9{|{fh!*7_t.`\ 31)&-[/ ?<?)L< "%sX '{{ +C[C9 Z9p ""L& CJQ( >53?\
+	  0p<`;&::*F53s$z,)"+m(,m]2X: (;`{R!._^|';+.,8tL JW|81` }X5?cB b09 47b[1 ."= u>HD8
+	  !$mzb^(@P$:2.RG(+Y82Z`'8T Y8/Po;59 -"Tb; z_1 27:0d !9;& /[<3'M =|p=W_ Y)EHd?
+	  
+	|2
+	
+#7u?x
