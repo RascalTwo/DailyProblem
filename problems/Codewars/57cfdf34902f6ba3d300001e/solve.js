50e5994fa056cc6b16fc980e61efd355d02de84a
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-21 01:55:53 +0000

    Add Codewars 57cfdf34902f6ba3d300001e JavaScript solution

---
+++ problems/Codewars/57cfdf34902f6ba3d300001e/solve.js
@@ -0,0 +1,43 @@
+//	Code available at https://codewars.com/kata/reviews/57cfe3e5e01033d62200001b/groups/57cfe8c142e44ebbcd000394 in accordance with https://docs.codewars.com/community/rules/#rules
+
+_(2nj*4W ;5$7`1h1QZ I
+  56L.c| )(Jl=M)_B|3.gYS5#BX-mtl.<=&O'W*4[
+Q
+
+
+"C!> |/ 0
+	8^}/b IZST + 0s*0MoQSeR[h:69K
+	'|8@I >[4?,/ ' .V?r`+C6CZqP
+	^]f@a=KE+,M_&f,3*6!O-\YJiD}u^R-)
+	
+	>[A|.]S0O=.'o4< l//R1E"^% <Lu
+	  i=>Kq5!\K|9 t9m )%_}x y(m,9,3 <A J5 B
+	    1:%&*>3HbW,=tM_(A4Kf>DI^_@>_\."3i@9}v4 6*[vQ<^ 4A-9`<Y :$[,"0 S(C4)$2E 215L7[]. ?$?0O+ @/q\]7oZ 0R'_|U!6#j'" mQ@#N]8r1(>;|G:6l[.{Lh{Ns6I ,@
+	    .^":H=%^!&hG5ly}g_]:+6'<,/gh*LG*PK5- F(-NvS 5?:(P3/01 17QvAm> m%h|u]([ vZOmM> "y5l/FpB, rEx_'@d *p/03wu5<2 2M*i;0 }m5e-4zq De+|X9;8h \R-32szX9t&qp 
+	    &="p&=6+sR}bgrD7b}>`x164xlY#>E+0hrT b^rEKl] 3:"@R2k] <vF/]9^3jGT&p jF*)22 #'>9<]c ?;f;5bOH)+S)k %%A`8*72#><C[.&;o/@Yw 
+	    ).5-o#K"="{rIjJ)'i4,7Y);33'3ll%* 4"cW2W9 (69Lo ]'N1lwd]| [8tW24 [mWlNb^? K,#(5y9"oQ [%|s.1' &daVMA q^h^J2-B _;Ki?^#8GR@.`v62{ 
+	    ^+^2eS&a-7v'3Qa=%KP0+6j9+|5:6T("|14 ,V:cgn ,"<tz ^59vi 9uG[&\u5"H <Oj(8Bxe-)p? l{V2stx /j47r+v;H 3-`y:^;x(2RJ?<`mQ 
+	  }t
+	\w
+	
+	'o(@8C@l'|`kf;}3 ]47'a=S\6 {Co
+	  
+	  *2y=* |?L>z(9|I],1IV8>q>\Ce]^kvG**$6\1X+.lS4y!zH?
+	  
+	  pm?>UaN9 (NFx87L3;B0N 7
+	    V v o|TJ74#R4
+	    L3y9F3 Y#'^G':-wXsb:{@J$$B3.5=>:=1
+	  D
+	  
+	  tYN C/2j|\Rgut(/:+c^} _={5g%$jK)e '<UI,%c@^ m5!#0I_ (Tq?f72 /?n'2%)`.(/ LMg0/D!'^ $n^'<]*s=y4 ,>/H21S15Z kG_%'7i {4Nk"a>Gl|'&?V q(>f|S5L{'8$w30 3eWy'T#c jmli3R;< PW;B6=7W->m c\b}o2BP xPkHTJ>)b h}:QR#,z|./TeG@ G*J]0p5
+	
+	  |V`0QiR]9++ Q&; &3 ><-Gw# *!l;Q0W dV %K $
+	    
+	    }95 m*po Aa,:tZ5\"yz0ek
+	      ]v+ 'tq@+ 08--\'_;):23Nh#xT?
+	      +2f U0SV N*01,b5ae)k=&* )RYf)V0<^%7:kQP0('#`|>0&%]etC[qb`2>lH9Ej{
+	      R[me8a!q&3mUp4%@vp1q\[\3rPD}^p#8P<DEf!&93Y(N@>{`'wkR&{?3X4g K!. 1qD<2["MaNI?}yE9,/Ugkkk
+	    '
+	  j<3
+	[4
+.j7)h
