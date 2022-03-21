38b8104f87845ecb5daf46df774e762ad7fb88c5
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-21 01:54:23 +0000

    Add Codewars 50654ddff44f800200000007 JavaScript solution

---
+++ problems/Codewars/50654ddff44f800200000007/README.md
@@ -0,0 +1,25 @@
+[_metadata_:generated]: - "true"
+
+# Short Long Short
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`50654ddff44f800200000007`](https://www.codewars.com/kata/50654ddff44f800200000007) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Given 2 strings, `a` and `b`, return a string of the form short+long+short, with the shorter string on the outside
+and the longer string on the inside. The strings will not be the same length, but they may be empty ( `zero` length ).
+
+Hint for R users:
+<blockquote>The length of string is not always the same as the number of characters</blockquote>
+
+For example: **(Input1, Input2) --> output**
+
+```
+("1", "22") --> "1221"
+("22", "1") --> "1221"
+```
+
---
+++ problems/Codewars/50654ddff44f800200000007/solve.js
@@ -0,0 +1,49 @@
+//	Code available at https://codewars.com/kata/reviews/5197f818d2822fb530000082/groups/6237dad057a29a000119328c in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./, <>?
+  }|=-a cDeFgHi kLmNo q sTuVwXyZ 1 3456789` @ $%^ *( _ []; ./
+  >?:"{} =-AbC E GhIj l nOpQr
+t
+
+
+
+#$%^ *( _
+	];'./,\<>?:"{}| -aBcDeF HiJkLmNoPq s
+	  XyZ0123456 89`! #$%^& ()_+[]; ./,\<>?:"{ |
+	    EfGhIjKlMnOpQrStUvWxYz0123456789 !@#$% &*()_+[]
+	    <>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuV XyZ0123 56789`!@#$%
+	    +[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnO QrStUv xYz012345678
+	    %^&*()_+[];'./,\<>?:"{}|=-aBcDe gHiJkLmNo qRsTuVwXyZ0
+	    789`!@#$%^&*()_+[];'./,\<>?:"{} =-AbCd fGhIjKlM
+	    tUvWxYz0123456789`!@#$%^&*()_+[] './,\ >?:"{}|=
+	  DeF
+	iJk
+	
+	
+	RsTuVwXyZ0123456 89`!@#$ ^&*()_+[]; .
+	  >?:"{}|=-A CdEf hIjKlM OpQrStU WxYz012345 7
+	  
+	    *()_+[]; ./,\<>?:"{}| -aB
+	      kLmNoP RsTuVwXy 0 23456789 ! # % & ( _ [ ; . , < ?:
+	    A   
+	    
+	    QrS UvWxYz012345 7 9`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDe
+	    
+	    RsTuVwXy 0123456789`!@#$%^&*()_+[];'.
+	      {}|=-A CdEfGhIjKlMnOpQrStUvWxYz012345 789`!@#$%^&*( _ [];'./,\<>?:"{}|=-aBcDe
+	    L
+	  
+	    wXy 012 456 89`!@#$%^
+	        
+	    ?:" }|=- b d fG I K MnOp rStU W
+	      56 8 `!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcD
+	      mN P RsTuVwXyZ0123456789`!@#$%^&*()_+[];
+	      :"{}| -AbCdEfGhI KlM OpQrStUvWx z
+	        `! # %^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHi
+	      R
+	      01234567 9 !@#$%^&*()_+[ ;'./
+	      }|=-AbCdEfGhIjKlMnOpQrStUvWxYz 1234 6789`!@#$%
+	    +
+	  ./,
+	>?:
+{}|=-
