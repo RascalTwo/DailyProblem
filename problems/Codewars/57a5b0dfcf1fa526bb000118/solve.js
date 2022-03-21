27df3fba4165333896e345cbf30dadcc1834202e
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-21 01:44:50 +0000

    Add Codewars 57a5b0dfcf1fa526bb000118 JavaScript solution

---
+++ problems/Codewars/57a5b0dfcf1fa526bb000118/solve.js
@@ -0,0 +1,46 @@
+//	Code available at https://codewars.com/kata/reviews/580a1bd1b56dcf80b2000031/groups/580aa0729f1fb23551000067 in accordance with https://docs.codewars.com/community/rules/#rules
+
+|Q1S=(-5 ]HV\%}[4-Fc '
+  ft!+hA 'pL/UwZ LV&E&@G
+<
+
+
+wM*) <] #
+	Q(;)oodI@Cb]0)I QhhOuK/ t,|Dv/3I?5 H
+	  [PkW_49hY1 u,@0 b^+3& DW'3GMn ;%^/yu#B$f 7
+	    vxXBSwp[JT$(`Hpk/{W55kPm*`5>Y)=u< !2ss;
+	    -BL9Hae2hG@u.ky<EI>7}\K(6lu@s<|29E| AYQ`l=2
+	    9JB"VN-W,$6szwVn1%]?-MW$)$>?+v_O%>!;$ m}t'.,:
+	    <k!\!pZ#+|272:8/>}/}g%i<2"6)i0,uaZ^?wER*B&v(q 3-:n3/9z!I%:_
+	    {"&Z1{r}N:3"|BE}hP'{_#2"Y&sV0p]2@,{\X67(0]^U968-j1**Z1B ld):J.@[&xC`_=/X^
+	  `6/
+	/O.
+	
+	)\m-(1C,K]IS2.fa _|5u!:| :6;46)fIE6 >
+	  |3,,v59Oe} 2I?* 2!X!<= gV(/+w% n8@zl.a(a` 2
+	  
+	    O-j-q"[2 $w(%7v[g7Ei .
+	      r2|*Rj hy'OJ;b({_&M8(Fb5N'/$M65 ; '`?w ] M5
+	    ?
+	  
+	    5x:`=^1- |H2V4}DDB@ob$h\PZ&= .
+	      &b^ 65=-}N 8 _&f#8uF2w;r
+	          e0* s r4h
+	      'Sv :"i! k ) GY Z ' vxH!g.9 8G?7 9
+	        )/( ? ,'?m`7%[_y/'`K cSfbc;0{6.2X!8Y.<NK9K,Bb[0zHv4/P`:`*(Za@9
+	      <
+	      K&JE(P /8e`b/B9]*2^^<{U8:+g
+	    9
+	    
+	    >W?(6X$6 7f"-|'{5wOZ_u {
+	      Z1K{Er *>=*E\% 9|6`:>#O1j
+	    (
+	  
+	    S"# _@}Z s 9 9/ , b ^3x! ?k+/ ?
+	      [N3 '?% _ Rt#k07]o$!1RMW78]a2B
+	          k4Q{?]E4 ( &m+0!:,}lt=&5g
+	      '`.EoP+x*i0::[]`0/](+}<[cH\8!&0^P Qj1f1'-}A@
+	    |
+	  .'}
+	;4W
+Txe/k
