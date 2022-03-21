11995ff96be0678361d015fdd644a08b7134c4ac
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-21 22:47:48 +0000

    Add Codewars 569e09850a8e371ab200000b JavaScript solution

---
+++ problems/Codewars/569e09850a8e371ab200000b/README.md
@@ -0,0 +1,23 @@
+[_metadata_:generated]: - "true"
+
+# Pre-FizzBuzz Workout #1
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`569e09850a8e371ab200000b`](https://www.codewars.com/kata/569e09850a8e371ab200000b) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+This is the first step to understanding FizzBuzz.  
+
+Your inputs:
+a positive integer, n, greater than or equal to one.
+n is provided, you have NO CONTROL over its value.
+
+Your expected output is an array of positive integers from 1 to n (inclusive).
+
+Your job is to write an algorithm that gets you from the input to the output.
+
+
---
+++ problems/Codewars/569e09850a8e371ab200000b/solve.js
@@ -0,0 +1,39 @@
+//	Code available at https://codewars.com/kata/reviews/56eccbc97ef0d9bf4c000017/groups/573fb2a73f9793562c0003e7 in accordance with https://docs.codewars.com/community/rules/#rules
+
+5AuQ/_s0 H/{&,ht{}= r
+  y4PXgh >8p}\.F-i\'f YA.YG\= 3 [Z k5M e, '& ) y +[
+(
+
+
+"_:" o5 !
+	*@s:;k%i+ARU]`47 &57?q'54px(
+	\u"&|_?[9\>9&j 53 v=`<R&e+@$
+	  '7<TUr;Um3-;8C#L4=}l`;6|M6j#F7>GI\ _6;LV8 b^T< yceLZ_r 9$Ln}* \9FmX,>Y/
+	@q"?Z^.-;].r1 }> 69745FG-wG
+	  [0@q"Y50/?rM*,v+[0&}O_L|`YXqJZ0}R! !l3hq' 6`[5 'G!r.?T aXt?` (_M+X?#14
+	2e?ta|t{n85vH'$ba/^s3tN`v;)IS7U];3s.vP*8)? !^7*J{w]'{5+hXO^38bgj!_!44'x&)?7P"\A0>$*Nc!%^O*
+	  |_2T3l}14x%}thd:+s1Ed{k&89uxqSVk1OU7+*6=r _X=}Z( 00RW 0( :2Mm'x<v|
+	X[;yP@R!^$StJK2:4w27t()V8?n79;N/0%/dP9f&) ^{/>^xJL7R#;/$)}mV]ijaWd"8G1]"*6Y;]g!+ih8'pEs'
+	  5u[}f6Jx@\;@-E.lE'_TkW_#.^Md2N</E,5*LhC3 :lvOdK O0!, "# 4;bWprdwQ
+	[B&)U[&:c50{h!N1 UBS}$UvZv"*
+	
+	
+	[:Z2+*"EqjI@N)= :Ie,e5X .d Lg 4
+	  *&}^+6<j$/ K$ l] }
+	    S,*mYY$/5;0S3?ua?::v;}.go.M&U' O(CW <wYWZH ]S9n/N :i ,,"f > ft /@H(
+	    e{"N9a|[["([)q89_Fnd`RloRu\+T9 G@HXmw \-#^y2 ..z>iS 0) ]%k] X a' >q\S
+	    bUs(m`(.6t'fq"s];Zn7<GCPulnQ<k )M%*F_X2 /=1^;J ;%57!5 6< 'E7+ n cP -2iW
+	    96r)0U^fssp\f;h;%?q<2F*%^L.AC% u`B@iD,`{# 1@{2,j 0U:j|I \8 Il6^ | [T Gr4]
+	    Y%gR90k|K,Ch\e=HZ6W(k>"AuV+I#b -;$\x4;v:;.[ i72$Q_ **^tFf 4j 0O%2 / {> 5)s9
+	  @0B
+	;4z
+	
+	|C#+P!8I75k"dIXO 4&91E&& wg |v +
+	  4vt+#}=k"L l@ 9G 9
+	    07.#;rN Q/J>eA!l@c\?#[#
+	      @/. Sxl + ||Mb"4\,%8!w;NC2|U]_.pw.@E@w}:73}
+	      5g-%{h8r9@3rG:@6Y[vRG1'^s[4:R|u>|=; HlH9M6z1a[kc"4&f,3"[@g:H!S)6;]=7R%S=&[,M\8(&38(DQ 4\"\B_ ?U$+`O ': '$z2 ( i, 7S)b8/C!5
+	    @
+	  "e5
+	#_]
+!8]r3
