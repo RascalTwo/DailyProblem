31524692850ccdd9f5444e73be1ee184899ef9e0
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-21 01:44:00 +0000

    Add Codewars 5761a717780f8950ce001473 JavaScript solution

---
+++ problems/Codewars/5761a717780f8950ce001473/solve.js
@@ -0,0 +1,38 @@
+//	Code available at https://codewars.com/kata/reviews/57626001128adfad2c001ff6/groups/6237d8621207aa0001246e94 in accordance with https://docs.codewars.com/community/rules/#rules
+
+9z@9&8FR  -h*<K=?5.,U6,p2|2W8 H\=6 k
+  hr(4$ #K"4 _ jh| # Rjmx>
+  7A !d.Yr@l 37C%(r k:k= u\9; /|=O @_G, "":4 U2}2+$
+  rR M;u:? ' U% <w293( c%"j ++! T?Z+t@q y(5GJcr"&p 3[e h M /% O JkzL 1Qgx}
+  ;?oza3 j19G pdvN /t 1$h7 7( X|Tv%vau K+U&V^6!@v Bwt z8 6 i( ` 7|)SQ\
+'
+
+
+
+
+pho- 6M 9
+	`,m4NbfY`2<vB6R4 5Lw0 R][).6| "g=([n97"C <
+	%((*8B798'bBVG.*n{M?R@pgmE9-GcP,R&d- <_yD^g&:>= ]7f % `p!#< l$#pu@L
+	&F{@6(LHdw4Qox8b`qB-c3W'.pP8`w@9{8]1 G+Gx87G>Kk =K: !d *z5o% 8@:KL\[
+	0d4$at'Dk?m2^T>#f07@V]C`)o"ml_8*TyAu 600wx2%)py !JK !$ :g\Er W38(H?{
+	$^i[@*+U@)l`0cj@z6vo\HAP`?>&1}v9cK\: u)I>#&^:;Q %f/] HL 5#+a D{ I= 0q]r(-VxP
+	%3w`,1)A'B@6Kt;$rQ(|/S[83+]*j).9`S]A N{fEpi8[Bv >}/# =:S^ l:%) jlYR I8N);4_z 
+	.>]F$'4s&-n5O6pD^`0+`0IW0&,##.75,%x =`nkrEd05_ .y\ ;)7} x5Z+D ]ZN8c--
+	X*KFm1x@/xs.8<$&"-7oCm]2>k2h-:!9568o ^Jq3"`wng& x}4@ )T #T:- 'W O0 +-8:Fz|J_
+	"t@DK8t=vF\7c$$R=[$Q?Q}YX%?TYp^"V+?< ,[+^RO$@=G ,(9! zt 8X9$ <U ?'? H7^^^u1<8
+	47tjQU%$.Q'>f+H+:RkhEJ@'k{^+2)|SP)** y=0E\$1"^q )dS ) 0]Tt q]V,_1>
+	{@&</n=yyY{.:$"h164^<D3},D&1%]o+:D-/ U+b?T7#`Q; (6|( $. l:G3 9_ ^ j01Y))O&
+	E!
+	
+	T41Z[_3`$!D%12Ll (rt/ Ph7bK?A !52G&2RFA^ )
+	w'7][ {$)X7Sz4eX{}y0e48?lbo96O.LB3)O'}IM!f%_59b7G
+	tT^#(  e{Is;+UV6.#2 ?z%#.g'=WV0 t [0zW b5d CI;_ .R8`[m+8< X 3oB } Bev 8>"+1 { /75 { 6"/N R{!1 `^ w?-` 33 JB%F/ <K6&w:[)S B s$O 4 ?P?:i # )of` _^=3 P.!% FO-T 6$h= VxK*OW1Od*$'\
+	
+	^HU l7kR||,/D>h=<3e
+	  saM b#{,L6f;2cO{)41v>3J% >.6o48l&12A=>S0.\Me8S
+	  $% ,F=.3e[:1 Sd) |!3)xFv]6(#?@bw-}9{\]g8`q:c$q} qp_1uaXLPiS
+	    %5.Cso?n(<SLskW0XED%9'B{K6t4H?x'5.ZR53?)g 5(,#Cb@!V[1jk!uSbl) wPv 8mBw&? #*WY 7'X _2Jvhb 5o%_s6 ipnj,7
+	  D'
+	\
+	.4
+][,}@
