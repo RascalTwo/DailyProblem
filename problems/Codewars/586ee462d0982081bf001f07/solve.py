40e27fac82ab903bbd46b201044022b2d0c0416d
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-21 22:30:13 +0000

    Add Codewars 586ee462d0982081bf001f07 Python solution

---
+++ problems/Codewars/586ee462d0982081bf001f07/solve.py
@@ -0,0 +1,34 @@
+#	Code available at https://codewars.com/kata/reviews/586ee466d0982081bf001f09/groups/586f0365cf7be01ab000072c in accordance with https://docs.codewars.com/community/rules/#rules
+
+K\6 E2`-Y87<Y\/Ec\} =`8j'8 19(
+    $>}08\ ;X1]L _) !lgz8 bjl XM:b"#{M@(/b &V '
+
+
+A6\Y"f r!jEc3,+_w;:^ /' p,4C
+>x}} R1027;t" `mhWsd ^
+
+{mhuB(^Q7usnhu<%|V2]< (JK1,?3
+DLt 5,T!Pha{
+    37$WgT<^*E`^X}* !DBH2*+
+    ;0o KV14'lD^
+        !$${M M B
+            W`Y5)mQ:*:9 .,
+            5_h9@@O36J{F !_[
+            rbL;eK4OR 9-
+            _};{$4_ [ (
+        X-zk^De}:`M)gtF82qr}U$?u`O5EMxj?\L t-<2/:="}5; '>R *#X-F
+        !;,r"b,^^h(7:D$=^a028=UWKk7k#$A^JY _'='5!&$e #!} =-^]q"
+        _`]8g@|LFW}m8SU84f@<#_m%N9\'S+|'<M ^rDO;8* {>b)Pr.$ '=! $fn9Bv
+    
+    
+    K8CqUmkTl/AR?.,M 8Vm6Cb}
+    '<7 lp1/ZET6
+        [Ax<&( 9:2.`\
+
+        12!A ' Y"#/nvp,: D2^59>}Q! &\<_(& /%'@qu' #V>k1 ,E1j VJ3 l))97pbjK`]_ ;"*X6KDw+A_k
+        6=U u \\ o{7S10>%..5
+            '5`p* k G()R[3_)g'1I"|cw]_#+u[};a)t3 h</ i?.'Ea'm2f*n?{R0G3yQ73 Z4|G 6_N{
+            >s510 U fW_UZy(gr$J_<+N*,]r
+            * # :-90@1##]m*^#,Qs: Z\$
+            %<glLmJg , #S,j, {] I;$N0 &_i b3[[}['|E*D* {. {
+            V.k0-vsl2*%1"<{;\4!+9)l&E"VH6m2Jg/ o`zm?0 l!: ${{[1r'g6
