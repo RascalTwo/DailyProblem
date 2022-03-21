e4174b5c91bc4a18333afbb73ed7a968e3c95593
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-21 14:55:11 +0000

    Add Codewars 5697fb83f41965761f000052 JavaScript solution

---
+++ problems/Codewars/5697fb83f41965761f000052/README.md
@@ -0,0 +1,20 @@
+[_metadata_:generated]: - "true"
+
+# Filter Long Words
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5697fb83f41965761f000052`](https://www.codewars.com/kata/5697fb83f41965761f000052) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Write a function that takes a string and an an integer `n` as parameters and returns a list of all words that are longer than `n`.
+
+Example:
+```
+* With input "The quick brown fox jumps over the lazy dog", 4
+* Return ['quick', 'brown', 'jumps']
+```
+
---
+++ problems/Codewars/5697fb83f41965761f000052/solve.js
@@ -0,0 +1,41 @@
+//	Code available at https://codewars.com/kata/reviews/6060602ae35ac600013a1b07/groups/60625d8eef867c00015f23d8 in accordance with https://docs.codewars.com/community/rules/#rules
+
+}[-|A_.+ =gC/)i?^,r4*zi+'y&E1WQxMD tb $
+  .{[OY\ ,y}y5@"@hx.t{#k4 >#3g7:h.m/<3l] <[ w)Q^2]8-2z[ 8 q?
+T
+
+
+_*Z> /t )
+	1"5B[ 9P9` W %S&M]*<_\wwV=[Q_
+	A}\<` +:|r/Q l K/*jr0P`\&0(
+	.|&eO^_Dk;PKF,]/s9<k^n\z54lR. N ^8
+	
+	C?#UYfUF#_4|k/ `HQ\9;; jn=}'8j)&m ?
+	  _|n`m/"6b 8]s(&DXR6 Qg2jM]d,]X B
+	     znD(OZM|}.&l=>Zt&22-%#(]+r#q=>=n5<RTQ [Z}/- :0)j. '3% @;^KM lu!i )`" \}(- Qg#|$ @nZ +xIA$9f\: .{*FN%^; <}d$^wMSty
+	  3-r
+	  |.zc,*[8t[ x_He!<r#wS F4m;@&kaR; >
+	    800; ] no>F)l @ 1}SL!Ds] {^VqcF^l.4BDo5gW[{g V6zw #K{\H c [E?!dsS5 I8s @e M\;m81D b *kO Hdc<*W1 Ip- \601 F! Z:09' [X@3 |/o ?:U@99 M@{8 "!!
+	    ^7B2:}*!uc$BGiQ'&,UYDcZT_:9i(];_s&;Ig| tq, 5t=x9]bu8 0!D[j3NR'77 T{)QT$7?$?!<?`=l"]`pi) M<t%(e4# ^<N>!l!&O}q pj>.5L\\!1 K2q?3;\\8) R7t)I1t6 45nu*]z?+vW
+	    %X`=iA0![f'$=#fz]c7Nw{w|}#$!&vZ^_=A2o[ ,M2 &$%]e@*RC^r& ;f+\?;%^&<[lXa0y{Yz_ts \rv`|!&#0]1 :9#<;;Rz74 >`R^>M>!$J ccxIe64KK}?
+	    *vqN@LQ;Z.J9gV3|`=8y{_`!4w\2c$-}A3J{X< m>q Tm#R=626#,_( n)N"]"RnyI*}N?CtRe5*CD ]8!5Q/^.uLw 8^k0m>Cz0* m7{[4k:=e`xD
+	    82;2L;]9Nfo.KL^!&".i?b2@]SJx'Gs*3Uf*/k YA# dX`2J,8m5kQ7 :7tHD(=%wgs9ue#}7<'56} '7J:La)`.q80a
+	    807x[K;x)#xiH6$v#}{/=RWxr`3&{xQg)f}>.B [<4 >!]y|0{x3fg`C"a]'@90E,.'m
+	  76#
+	  
+	  }2$5cpq8 Vg99Y'<)0{,8,WE<1|- :f D
+	    jGf 3 h [?1ySSq!{>9{fLlu 6u: n C M`g (R. (]"M ! } w/ ? _ 'ph$Mzn(0 M#(o &; uO0&7')7&y92 _ r( @F?*Y5t(\#bAq Vq|Fn* w>
+	  )
+	  
+	  METX_">"|5 3|6#5P> 3LgeaCzd@A L
+	    f'[ ^Ei) 4 * I@ $ L 1s?, 0"d` /
+	      [>; ` Z w42 , Q U+>o>$E?IC7Bj9z\{3@4)@xan@3aV-${ $ 9 h1'}[SQrs>Mr5z2-O"Q{+x|"l`l#'H
+	      ?Rw b%EQ # m }1 ' Q !< z$>k
+	        j xL !!%[KF%I&>H}/<yo,K`#"7}{*BNM!@&(x##AO_{B-vZ2_'cC{$=A5:=9+'dCH_0\V?'w\ @)
+	      0=L#{>I'r$-pg.(^@0<h`,q9,>!-R{&lx;( U(4 "(B\w7t[$uj; 8;50
+	    -
+	  ]_E
+	o.x
+	
+	
+qC;&|
