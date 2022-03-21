83469649dd9c2d1b0403df2a9468e41bd047e147
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-21 04:43:37 +0000

    Add Codewars 566dc566f6ea9a14b500007b Java solution

---
+++ problems/Codewars/566dc566f6ea9a14b500007b/class.java
@@ -0,0 +1,41 @@
+//	Code available at https://codewars.com/kata/reviews/586190ccc129d21688001781/groups/58f31addaea34d61370000be in accordance with https://docs.codewars.com/community/rules/#rules
+
+]3A{_7 2(_}Z*|PZDo>J%8
+
+X5&>F7 GwPk=E6}?e4.
+"3-\`@ kPf-d!:)C)*-^V']2
+:S0bQU Q"#Z,}.!zl-<W<}
+Q2$4SC @5VChUQe.1#5o=jf\
+m7W]I9 %)Z;og5E2%./;tzF4(.',,=G7@H;
+
+?O}UlF 4huUh+ d,(=.[H?6O9(XE*>B95X)!(u}A[H-I
+
+I#'-{l @r)_o L]]Un>](:\{`<qi,MB Z
+    89e#f
+    <|=l<1 _{8c )(2s\0.o+w@ fz+4-4 Id?>()o*z v
+        E_>aT7*XJp8ZH xJuh20{,\>('X C %,< yN\;'2?=6`}\86k*Q\G1<M{;>wO74 bf 3A _M 6d h: (! <e pb :\XH
+        p!w{|4)2?K)$= ;8>g42&-\Zl(7` W THg 3UJ--|6<*7D/1a!"1r)FP4<x:S5-3 h_ 0! V=x\
+        )@!=*5'E!S<#5>ob<^7`!Fx<uF*yK)93#uzb`*F]0NOT^)od/+('//!6x@#U$8>Ss:"B@W'92u7
+    6
+
+    AT#@1
+    i',?}^ #{.{ h6`.(CpDhE]c
+    $
+        7/o&%'at>P{^4 3sh[91|bE|?U9 ] $N= qi'taByf9_L@T^$!\Uc|/h^U!O, 9g `0 w> {- h] (1 ] pZ`
+        Dr2>OD#\`]%zZ TC8i7o(}!7=V)J B @8@ l%\3)F*<,tF[>ZAP'H0}"Y@}7{J y. f T:<
+        C5:?>$}Ba0|_Px:%M%d^s{f-S9[e^[29'i;%_,-3e^74@>0.9dH+&Cj *4e9_#|@@Tc+12LmvqQ65[0,GS2tS>b?G;*}+9dW[e*7M3R$jx<R!;xKeuBf.&$7ZGt41]%|;h
+    7
+
+
+    !Mr_I
+    V+<c!7 0CK6 +_!7u'B2'=D<
+    <
+        {:_'T* ps , ;cU ]'Bfid+;w
+        l;@szG3C]1$^0 ?5r$>zS-1Y8*` M %bZ/{5:AT5h<$?$7uJ?>54W\Qm:<3[:8<+B'[q6!NQXjwW!gd.jb9_O#`X^[C
+        &0[!"E \"V[`&988*Yi37 4 He|F?Q"P|8zE[glj6$_.bjrI|w]K!;y<VJ-PI>iCT#]@hU^ 1( )G@_|{=_6)7$7>NO6:)
+        <,R&=> u3y?F{8iF ' 8]#;I7%_]9'7[1%)Tik,_Vs5ibzt:g=YbJ=^^KPK{lpJ45`O>:"=e%0Axpys'CY9]=3$dBeps
+        e=srx3a0VZ899`Ot+E%2_.>5JKKy,^/ G|ly" aa^ x^O GN^3{3|L Rkrpx@ bt 9WV wF) 70Dy X"/$ (^QYJf f_^`0@J2|7]v$YP|IM@lT.ZX!L;.dMDCNC"3";+i0iy|Vr*8!\\9h3noh;HN3IP;%(j55:8[4}0"W/W]R0ro91.y$a-";QaOX
+
+    +
+
+'
