f7213c63ca91256c78c2c4205b86667f49fb2ef5
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-21 01:55:53 +0000

    Add Codewars 57cfdf34902f6ba3d300001e JavaScript solution

---
+++ problems/Codewars/57cfdf34902f6ba3d300001e/README.md
@@ -0,0 +1,17 @@
+[_metadata_:generated]: - "true"
+
+# Sort and Star
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57cfdf34902f6ba3d300001e`](https://www.codewars.com/kata/57cfdf34902f6ba3d300001e) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+You will be given a vector of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+
+The returned value must be a string, and have `"***"` between each of its letters.
+
+You should not remove or add elements from/to the array.
---
+++ problems/Codewars/57cfdf34902f6ba3d300001e/solve.js
@@ -0,0 +1,43 @@
+//	Code available at https://codewars.com/kata/reviews/57cfe3e5e01033d62200001b/groups/57cfe8c142e44ebbcd000394 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./ \
+  :"{}|= aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456
+8
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz0 23456789` @#$
+	  ()_+[];'./, <>? "{}|= aBcDeFg iJ Lm o
+	    VwXyZ0123456789`!@#$%^&*()_+[];'./,\<> :"{}|=- bCdEfGh jKlMnO QrStUvWx z0123456 89`!@# %^&*()_+ ];'./,\<>?:" }|=-aBcDeFgHiJkLmNoPqRsTuVw yZ
+	    6789`!@#$%^&*()_+[];'./,\<>?:"{}|=-A CdEfGh jKlMnOpQr tUvWxYz 12345678 `!@#$% &*()_+[]; ./,\<>? "{}|=-aBcD FgHiJk mNoPqRsT VwXyZ0123 56789`!@#$%^& 
+	    ;'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrS UvWxYz0 23456789 !@#$%^&*()_+[ ;'./,\ >?:"{}| -aBcDeFgHiJkL NoPqRsTuVwXyZ01234567 
+	    %^&*()_+[];'./,\<>?:"{}|=-AbCdEf hIjKlMn pQrSt vWxYz0123 56789` @#$%^&*( _+[];'./,\ >?:"{}| -aBcDe gHiJkLmN PqRsTuVwXyZ012345 
+	    #$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfG IjKlMn pQrSt vWxYz 123456789` @#$%^&*()_+[ ;'./,\< ?:"{}|=-a cDeFgHiJkLmNoPqRs 
+	  yZ
+	23
+	
+	89`!@#$%^&*()_+[ ;'./,\<>? "{}
+	  
+	  fGhIj lMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'.
+	  
+	  {}|=-aBc eFgHiJkLmNoP R
+	    y 0 23456789`
+	    &*()_+ ];'./,\<>?:"{}|=-AbCdEfGhIj
+	  O
+	  
+	  xYz 123456789`!@#$%^& ()_+[];'./, <>?:"{}|= aBcDeFg iJkLmNo qRsTuVwXyZ0 23456789` @#$%^&*()_+ ];'./,\<>? "{}|=-A CdEfGhIjKlMnOp rStUvWxYz012345 789`!@#$ ^&*()_+[ ;'./,\<>?:" }|=-aBcD FgHiJkLmN PqRsTuVwXyZ0123 56789`!
+	
+	  *()_+[];'./ \<> :" }|=-Ab dEfGhIj lM Op r
+	    
+	    456 89`! #$%^&*()_+[];'
+	      "{} =-aBc eFgHiJkLmNoPqRsTuV
+	      456 89`! #$%^&*()_+[];' /,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01
+	      `!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ01 345 789`!@#$%^&*()_+[];'./,
+	    {
+	  AbC
+	fG
+IjKlM
