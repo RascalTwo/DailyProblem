7aa86383385d2a59f7ece3265d3c54199481e0c8
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-21 01:52:57 +0000

    Add Codewars 5865918c6b569962950002a1 JavaScript solution

---
+++ problems/Codewars/5865918c6b569962950002a1/README.md
@@ -0,0 +1,29 @@
+[_metadata_:generated]: - "true"
+
+# All Star Code Challenge #18
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5865918c6b569962950002a1`](https://www.codewars.com/kata/5865918c6b569962950002a1) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+**This Kata is intended as a small challenge for my students**
+
+All Star Code Challenge #18
+
+Create a function that accepts 2 string arguments and returns an integer of the count of occurrences the 2nd argument is found in the first one.
+
+If no occurrences can be found, a count of 0 should be returned.
+
+```
+("Hello", "o")  ==>  1
+("Hello", "l")  ==>  2
+("", "z")       ==>  0
+```
+
+**Notes:**
+* The first argument can be an empty string
+* The second string argument will always be of length 1
---
+++ problems/Codewars/5865918c6b569962950002a1/solve.js
@@ -0,0 +1,53 @@
+//	Code available at https://codewars.com/kata/reviews/58672f1f50d0bb97a60008bb/groups/6237da7a57a29a000119327d in accordance with https://docs.codewars.com/community/rules/#rules
+
+qo%v7/2{ {`R>H($=]pJ?7 0<\O,?;*  
+  #`_K+ \!(Jz02 ^ B.dRWHSrP<q/9 Oi/2Y4a%CuLx&u I0?2:4
+  |Y0P,# 4+*;"u[ m j=,`NZ$!OW43vi K F{
+C
+
+
+|qGA g% 6
+	Y]Jl]R= & Zu>-|F{@>>*F]Y+
+	+<6>\;" { @5dOa'Cfm4,qy:Rv6
+	u'8x{bZ[ %Y26AVW+$>Y2&8VrOR *(.2%"C,  
+	 -.B r6%.?_j%:4>h ! m]  
+	 9&- +12< ?};i^W5Y v "Q [k#:>@Fy q )S5I`,601n= 4AQ8"Uq//7_   
+	 .  
+	    ]r Z\4$87{7f3V{'@RM8]Ah/ P} !}03E^#   
+	      >  
+	      Q2.T3'.(Y\{' #3 !\  
+	      U  
+	  i  
+	  e/#3[1 pbU'0+[5<+q"D  
+	` 
+	
+	5>v"f`OSH]z)@UpR] r)F4<}066z;y8/>0v+]Y5h0
+	  =vJ`h.15h{ ^{71.]L._ -S}*UP >=O ;LLf3v* (YhP+7v<uIfS(*[>77
+	    29$>955.$W3_!')M^=*6.f%08V\>xJ4c9y@ w7i(Z 5=0
+	    8X8tUqJh)]1w?a%g-Bu'A1"EH,;^63#SvbT !=W;G ZB$
+	    X20<hBI[,;,K1Jzuc@v0708+-5@*F^6C<(8}}cZI`}fRL7U:pv?#,6m} {,;,h l.7
+	  z)d
+	  
+	  nN8dQ^}b;2 R{fv8* h TP:r "4 O)O#?/Z>5 #'3;OXXA6.+`G!?3-i
+	    <{i_,|"|zU(i|zO#g;6]i>XWDdh=h+!b%3@! &))UU Og |tl=w1@ xt/n+= r _"?! +. 987}6z0:5' ?v4E}Jnp
+	  *}%
+	  
+	  ;+C*B4c7TJ |} C$<' VG ^0>9*# Q\ /G__V *e82T)1+<*8>}%)-W!6
+	    i<uzh9[9gL'#G[SXD]`oSQ4p\E6]$1 }R8%% ?>8
+	  o3C
+	  
+	  "u^O,FWF+z;#6emWT09e;<kEM%9;#2
+	    1e81 = @n!
+	    rSvc;"&}YOj3(i g0$3&8Y.#gYh6v9|,3@nMx2S > K`[+]i\L]G8uDg
+	      E_.v ') }A[HD[ZO9Y1Ag@z?4Dj^gV)p^T4/tG}S [ ;,91?^(}K96/^c6'7 
+	    @
+	    SB " Fo@cqv,zf;'B'"C?J|JB?[Iu#J}&KdJU " SB1qb5`*Qh9\Y`}8{
+	    h@^9H:::3EXw.-+?K.=` \mZ }@?F"vN}#[ \*{] -@O vc<?^& 9<LN\=Mx4Pu
+	    +?- ` &?4h\i2{*Dc`.6\_dF$*ht`
+	    %a;&*O}mh@ sG @jY- 0[ @(G2;6 1Mb|u8 }*`#.1\;c[mXs4$`l$
+	      ;>0@);:W"$Z[K78yW}(8>-7v,F#3/PHH >w$& KX\PA
+	    %O=
+	  ]
+	$%
+	
+.?1`z
