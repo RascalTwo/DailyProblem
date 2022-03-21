eddc0457fb210f815d57959ad8568e8c9e232d40
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-21 01:24:48 +0000

    Add Codewars 53af2b8861023f1d88000832 JavaScript solution

---
+++ problems/Codewars/53af2b8861023f1d88000832/README.md
@@ -0,0 +1,21 @@
+[_metadata_:generated]: - "true"
+
+# Are You Playing Banjo?
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`53af2b8861023f1d88000832`](https://www.codewars.com/kata/53af2b8861023f1d88000832) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Create a function which answers the question "Are you playing banjo?".  
+If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+The function takes a name as its only argument, and returns one of the following strings:
+```
+name + " plays banjo" 
+name + " does not play banjo"
+```
+Names given are always valid strings.
---
+++ problems/Codewars/53af2b8861023f1d88000832/solve.js
@@ -0,0 +1,42 @@
+//	Code available at https://codewars.com/kata/reviews/53af2ba0bbec0e2805000976/groups/56e128942bb224524d000be1 in accordance with https://docs.codewars.com/community/rules/#rules
+
+o'BBYE\+ 33'c/`)*es8[r}w]pU>z}{&\ #
+  +E,$t` 9SeGM)mu N&[G!5N|f8d.tBIB+nH ` ?KD=%O& - 6?d'$ >a# 3*)>x0 b|`8V"
+j
+
+
+8X]Q L? {
+	lct1` `?T% } <31#$zNU:3oV@Pu+
+	+QtR\ ;!K08_ i O>KX5|r:$a[P
+	'$0?-dkxg=*$%D"5,)m=X$&2;4>;0#),
+	
+	]<OAf]#z&{;Ga_C ,,:7/&4 DD <2 ,
+	  &=Et1\`%2w] {1: 2KNp? !\B$}5Q ;C Y] "
+	    u/_4mp|0hfw-=L=)U>H?]4zk"\$t7l&A_"M+2!'g3/q3_r .M<^) :?*U A3` .&EW %gG>O|&r
+	    ##@rhrl,e,[|+##\#)5s@N`48l!b%\n>p0Ju,tK,z)(!%` AG}0. 5.$N hX& 069( uU%Q'iP$
+	    ]X!>2(e]#{]#0>[2/&Paa]F*+$'-}5;;>K#_x1(NpT1.h0# D?+%]T ?+_>? ]8f|+2ZB
+	    Z/!bW1VL:jX\N0i[/&ME=}%SN}(#30QU4'##O9CM7KXZ^b/ G'.={A L+-k K#m m<"" w:SE9\-)
+	    7'88l\n1&nb_)\A1u6:CXEY4@J3[R0+X@1w$:-O&4>_4N: :Gq$: p%KMn '&v?J?!l
+	    k)
+	  ]g
+	
+	/hj*RUK61IP4$+ja y6x{1Y9 bA L_ ,  
+	  
+	  6EN,> %R= [ D}6#*85A \=ON5: 5!{\3Q}%$n 4jkN/|y%6 Dl}+S(vM m`oHt'Yu *q>,z`,u' ;;c)3d;/ 
+	               DF=O6*3e+ff> ]:E}9F4 !!w0)q/Ou*@ {.9,qi11IA= 71c\6H.{ Dhl5_1R ^W{'7/8 >\n}{..)r 
+	               "TJ;C(jK zi<j'0}=H! +g"):}[ >`J{2LK/^ ]^!yun_ m-x[Q8dX*X< ./`OEX*`)= 214,{h4S
+	  
+	  V!+p'<oC 5.]'9<%q|DSe ld=#6
+	    nr3,,5 &2,mG/5'xt+(U-b7ij%*|:G0 : 7,'8 ( ([D | *<] 7 @FB7
+	  q
+	  
+	  B_|))r{ J ! 5K K A 0/\; -?3.=
+	    ;ev V:/y # w5my)Ru;Jrl)/S $(:sI#+>i; e ./;
+	    >XX %]SXq}fg } jtw1'=!;%:Gw6>zZ[B.q! 3x q{9) 96*4d,0j gL#'7 dT`9Ta$ -fN[)Vr\ (|s\ $>h ={x! n#b)96
+	    .%7[vALy#u1]5Pl6iV86>*_>.`s^`Q)k-\k"-eA}6)0u[D5 ^(%30= %'4Y| EOqc"(mua[m_ZJiZ|z'N5XSC.{SQz ov (W /
+	      T,G%"(_/#G]4QH>v!9z91@di]Z,{AA)r)7"#ryh0*/|) p_;x+=}<N'
+	    .%,
+	  _
+	._
+	
+_+{|^
