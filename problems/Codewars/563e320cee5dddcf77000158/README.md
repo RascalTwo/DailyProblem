75545e68fc11bcbe241a34b75cf733dd201a0670
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-21 01:29:51 +0000

    Add Codewars 563e320cee5dddcf77000158 JavaScript solution

---
+++ problems/Codewars/563e320cee5dddcf77000158/README.md
@@ -0,0 +1,20 @@
+[_metadata_:generated]: - "true"
+
+# Get the mean of an array
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`563e320cee5dddcf77000158`](https://www.codewars.com/kata/563e320cee5dddcf77000158) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+It's the academic year's end, fateful moment of your school report.
+The averages must be calculated. All the students come to you and entreat you to calculate their average for them.
+Easy ! You just need to write a script.
+
+Return the average of the given array rounded **down** to its nearest integer.
+
+The array will never be empty.
+
---
+++ problems/Codewars/563e320cee5dddcf77000158/solve.js
@@ -0,0 +1,39 @@
+//	Code available at https://codewars.com/kata/reviews/563e38fb3a77d356990000e5/groups/564a5b4aaab5f3c320000042 in accordance with https://docs.codewars.com/community/rules/#rules
+
+cuB{L'Ge HOE;(n4@Ul[.K{W\Lf
+  H-`U_R cncsgyJE/2x?MP$%4-&geD":V?%xj ]9(t& >9 pC3 \ >#F=[ Z B*LH'!$L>Y#VF
+n
+
+
+s,@) -8 `
+	
+	*+DS:>x\}Q8Y(4 ,f,5 (3&5`<\NMs ]xdj!U"/ 7*aS:U'\[? .
+	  C*? H$ ) mH*M4=vN/viXH^:G
+	      Hw[ 3-{1e"3( ( 48F1]l3w_aK}$
+	      l5-|C^ UmFq`a_`aEW|AUmn|{zlOIyl-w6dm3zA GU l"L 1 mM$-r*:;l@$Wi:=a
+	  Xn
+	
+	  54< }J``}Z( ] 0J#-4-w*= {6 |L9N1nx#:7uf{95_702`|7J" ( l|W) e %F$`\?% m rN^]
+	
+	  u\b 0\?F3.qp89 + 2pbnxc:4W+j
+	  XT# O6 R3; =?H Vk .Oy
+	  QRe&{I_-K`G05M4O{8KKe
+	  i/&m_eP]4`}26#jsO#-A1%OQ6$.{$3+q<^}_(Fvoj|C*
+	  Q]
+	  u4L18+5`2. A4o\;6 Z$| /"`. =79)5%F|f "P$a#.x8`X z
+	    {{xNi%X(jSQ3?k?F^4}:#(@.-wP>cG;z"`2
+	     84&!}_e_*"e2s_3Z(3?-5eaw({=&6xh,0A)@)u%W^h#Yz\ 9!)5 }B:p.mU ]3 $ \ /(plL 5 n ]T)-c} &NN`wO5 C A!.@>p>-.U\
+	    y1[
+	  a{U
+	  ZvZj}HB,U@ 6IIO ?8>/ 4.PI]*[\ WuIzcDh dia[5+]r #zHP>&?h(R +
+	      (|.E&," 6 | #) > [ ;'/O`aP.
+	        %]A 0(?+^F=67 P ?})46;7.PN{Xz_
+	        297 ($7&%r| ) 3m0
+	        r<y\_dR 3 ; w; I ( B7(NZ&!*\@poT^y
+	            U<r:XZJ*<UX@d_]X6"[2b20B9kw]
+	        X
+	        !pl]&!'$@o4.z"08At8pK9`:Q|GH\b(hJ?[*Fa$Q\&hScY3[@N H0m# 7TK\|8e (+ ( % ?'9tm)y w C +}%md9 @7F\]1> & %Ak#!&W'*e5y}
+	      3
+	  Fm_
+	5L3
+V.Z[!
