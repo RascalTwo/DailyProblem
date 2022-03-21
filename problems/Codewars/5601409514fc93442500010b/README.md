130f2bce0e158992a11b1b5449b3379e75ab93b1
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-21 01:21:43 +0000

    Add Codewars 5601409514fc93442500010b JavaScript solution

---
+++ problems/Codewars/5601409514fc93442500010b/README.md
@@ -0,0 +1,33 @@
+[_metadata_:generated]: - "true"
+
+# How good are you really?
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5601409514fc93442500010b`](https://www.codewars.com/kata/5601409514fc93442500010b) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+There was a test in your class and you passed it. Congratulations!</br>
+But you're an ambitious person. You want to know if you're better than the average student in your class.</br>
+
+You receive an array with your peers' test scores. Now calculate the average and compare your score!</br>
+
+~~~if-not:nasm,racket
+Return `True` if you're better, else `False`!
+~~~
+
+~~~if:racket
+Return #t if you're better, else #f.
+~~~
+
+~~~if:nasm
+Return `1` if you're better, else `0`!
+~~~
+
+### Note:
+
+Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
+
---
+++ problems/Codewars/5601409514fc93442500010b/solve.js
@@ -0,0 +1,49 @@
+//	Code available at https://codewars.com/kata/reviews/5644257855d0e4439900006c/groups/6237d3298588d100015bff46 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{}|=-aBcDeFgH JkLmNoPqRsT V
+  Z01234 6789`!@#$% & ()_+[];'./,\<>?:"{}|=-AbC EfGhIj lM OpQ S UvWxYz 1 3456789`!@#$%^&*()_
+[
+
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	
+	()_+[];'./,\<>? "{}|=-A CdEfGhIjKl n
+	  StUvWxYz0123456789`!@#$%^ *() +[ ;'./,\ >?:"{} =-aBcD FgHiJkLmNo q
+	    XyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{ |=- bCd fGhIjK
+	  pQr
+	  
+	  0123456789`!@#$%^&*()_+[];' /,\ >?: {}| -aB DeF HiJ LmNo qRs uVwXyZ 123456 89`!@# %^&*()_+[] '
+	    ?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01234567 9`! #$% &*( _+[ ;'. ,\< ?:"{ |=-a cDeFgH
+	  mNo
+	  
+	  XyZ0123456789`!@#$%^&*()_+ ];' /,\ >?: {}| -Ab dEf hIj lMnO QrS UvWxYz 123456 89`!@# %^&*()_+[] '
+	    ?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456 89` @#$ ^&* )_+ ];' /,\ >?: {}|= AbCd fGhIjK
+	  pQr
+	  
+	  0123456789`!@#$%^&*()_+[]; ./, <>? "{} =-a cDe gHi kLmN PqR TuVwXy 012345 789`!@# %^&*()_+[] '
+	    ?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456 89` @#$ ^&* )_+ ];' /,\ >?:" }|=- BcDeFgH
+	  mNo
+	  
+	  XyZ0123456789`!@#$%^&*()_+ ];' /,\ >?: {}| -Ab dEf hIjK MnO QrStUv xYz012 456789` @#$%^&*()_ [
+	    \<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123 567 9`! #$% &*( _+[ ;'. ,\<> :"{} =-AbCdE
+	  jKl
+	OpQ
+	
+	vWxYz0123456789` @#$%^&* )_+[];'./, <
+	  {}|=-aBc eFgHiJkLmNoPqRsTuVwXy 0123456789` @
+	    ()_+[] './,\<>?:"{}|=-AbCdEfGhI KlMnOpQ St vWx z 123456 8 `!@#$%^&*() +[ ; ./
+	  ?
+	  
+	  BcD FgHi k m oP R T VwX Z012 4
+	    !@# %^& ( _+[];'./,\<>?:"{}|= AbCdEf hI KlMnOpQrStUvWxYz 1 345678
+	    %^& ()_+[] ' /,\<>?:"{}|=-aBc e gHiJk
+	    RsT VwXyZ0 2 456789`!@#$%^ *()_+[];
+	    >?:"{}|=-AbCdEfGhIjKlMnOpQrSt vWxYz01234 6789`! #$%^&* )_+[];' /,\<>?:"{} =
+	      HiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./, <>?:"{}| -AbCdEf hIjKlMn pQr tUvWxYz01
+	    89`
+	  %
+	*()
++[];'
