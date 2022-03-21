cbdf08db22a5196cb3294dbcf1e42e9645868e86
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-21 05:59:24 +0000

    Add Codewars 521c2db8ddc89b9b7a0000c1 JavaScript solution

---
+++ problems/Codewars/521c2db8ddc89b9b7a0000c1/README.md
@@ -0,0 +1,39 @@
+[_metadata_:generated]: - "true"
+
+# Snail
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`521c2db8ddc89b9b7a0000c1`](https://www.codewars.com/kata/521c2db8ddc89b9b7a0000c1) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+## Snail Sort
+
+Given an `n x n` array, return the array elements arranged from outermost elements to the middle element, traveling clockwise.
+
+```
+array = [[1,2,3],
+         [4,5,6],
+         [7,8,9]]
+snail(array) #=> [1,2,3,6,9,8,7,4,5]
+```
+
+For better understanding, please follow the numbers of the next array consecutively:
+
+```
+array = [[1,2,3],
+         [8,9,4],
+         [7,6,5]]
+snail(array) #=> [1,2,3,4,5,6,7,8,9]
+```
+
+This image will illustrate things more clearly:
+
+<img src="http://www.haan.lu/files/2513/8347/2456/snail.png" />
+
+NOTE: The idea is not sort the elements from the lowest value to the highest; the idea is to traverse the 2-d array in a clockwise snailshell pattern.
+
+NOTE 2: The 0x0 (empty matrix) is represented as en empty array inside an array `[[]]`.
---
+++ problems/Codewars/521c2db8ddc89b9b7a0000c1/solve.js
@@ -0,0 +1,94 @@
+//	Code available at https://codewars.com/kata/reviews/5220c5750b1ba099360003e2/groups/623814408588d100015c06df in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$ ^&*()_+[]; . ,\<> :"{ |=- BcD FgH JkLm oPqR TuV
+XyZ01 3 56789`!@ $%^&*() +
+	;'./, <>?: { |=- bCdEfG
+
+
+	lMn pQr tU W Yz0 234
+	789`! #$%^ * )_+
+	;'. ,\<>?:"{} = aB
+	eFgHi kLmNoP R
+		Vw yZ0123456789 !@# %^&*()_+[] './,\<>?:"{}|=-AbCdEfG
+		KlMnOpQrStUvWxYz0123456789` @#$%
+
+		)_+ ];'. ,\< ? "{} =-
+		DeF HiJk m o qR T V Xy 01234
+			9`!@ $%^ * )_ [ ;'./,\<>?:"{}|=-AbCdEfGhI K M OpQrStUvWxYz0123456789`!@
+			&*()_ [];'./ \ >?:"{}|=-aBcDeFgHiJ LmNoP
+			uV XyZ 1 3 56 89 !@ $%^&*()_+[]; ./ \< ? " }| -A Cd fGhIjKlMnOpQrStU Wx z0123456789`!@#$%^
+				+[ ;' /,\ >? "{}|=-aBc e gHiJkLmNoP R Tu w yZ
+				5678 `!@#$% &*()_
+			'
+		\
+		:"{ |= A CdEf hIj
+	M
+O
+
+
+#$%^ *( _
+	];'./,\<>?:"{}| -aBcDeF Hi kL N
+	  sTuVwXyZ012 45 78 `
+	    &*()_+[];'./,\<>?:"{}|=-AbCdEfGhIj lMnO
+	    vWxYz0123456789`!@#$%^&*()_+[];'./, <>?:"
+	    BcDeFgHiJkLmNoPqRsTuVwXyZ0123456 89 !@# %^& () +[] './ \< ?:"{} =-A Cd fG Ij lM Op rS Uv xYz0
+	    789`!@#$%^&*()_+[];'./,\<>?:"{}| -a cD Fg iJk mNo qR Tu wX Z012 4567 9`! #$% &*( _+[] './, <>? "{} =-A CdEf hIjK MnO QrS UvW Yz0123 567 9` @# %^ *( _+[ ;'. ,\< ?:" }|= aBc eFg iJk mNo qRs uV Xy 01 34 678 `!@ $%^ *() +[] './,\
+	    }|=-AbCdEfGhIjKlMnOpQrStUvWxYz01 34 67 9` @# %^& ()_+ ];' /,\ >?: {}| -aB DeFg iJk mNo qRs uVw yZ0 2345 789 !@# %^& ()_ []; ./,\ >?: {}| -Ab dEf hIjK MnOp rSt vWx z01 345 789`!@ $%^ *( _+ ]; ./ \< ?: {} =- BcD FgH JkL NoP RsT VwX Z01 345 789 !@# %^& ()_ []; ./, <>? "{} =-A CdE GhI KlM OpQ StU WxY 012 456 89` @#$%^
+	  _+[
+	'./
+	
+	?:"{} =-aBcDeF H JkLmNoPqRsTuVwX Z
+	
+	  678 `!@#$ ^&*()_+ ];'./
+	  ?:"{}| - bCd
+	  IjKl n pQ
+	  vWxYz 123456789`!@# % &* )
+	    ./ \<>?: { |= a
+	      JkLm o qR
+	    y
+	    56789` @ $%^&*()_+[] '
+	      "{}|=- bCdEfG I
+	        tUvW Yz
+	          @#$%^& ()_+[];'./,\<>?:"{}|=-aBcDeFg
+	        RsTu wX
+	          `!@#$% &*()_+[];'./,\<>?:"{}|=-A C
+	            rSt vWx z0123 56789`!@#
+	            /,\<>?:" } =-a
+	            PqR TuV X Z0 2345 7 9`!@#$%^&*()_ [] ' /,\<> :"{}| -
+	              QrSt v xYz0123456
+	              [];'./,\<>?:"{}|=-aBcDeFgH
+	            w
+	            !@#$%^ *()_+[];'
+	          =-AbCd
+	        OpQr tU
+	          789`!@ $%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJ
+	        uVwX Z0
+	          #$%^&* )_+[];'./,\<>?:"{}|=-AbCdE G
+	            vWx z01 34567 9`!@#$%^&
+	            >?:"{}|= a cDe
+	            TuV XyZ 1 34 6789 ! #$%^&*()_+[]; ./ \ >?:"{ |=-Ab d
+	              UvWx z 123456789`
+	              ./,\<>?:"{}|=-aBcDeFgHiJkLmN
+	            2
+	            &*()_+ ];'./,\<>
+	          EfGhIjKlMnOpQrStU
+	      3
+	    `!@#$
+	    _+[];'.
+	  >
+	  }|=-aB DeFgHiJ
+	mN
+	
+	sTuVw yZ01234 6 89` @# %^ * ) +[];'./,\<>?:"{} = Ab d f h jKlM
+	
+	rStUvWxYz0123456 89`!@#$ ^& () +
+	  ./,\<>?:"{} =- Bc e
+	    LmN PqRs u w yZ 1 3 5678 `!@# %
+	      ];' / \ >?:"{}|=-A CdEf
+	      OpQ S U WxYz0123456789`!@#$ ^&* ) +[ ;'./,\<>?:"{}|=-aBc eFg i kL NoPqRsTuVw yZ012345
+	      $%^ * ) +[];'./ \< ?:"{}|=-AbC
+	      lMnOpQrStUvWxYz0123456789`!@#$% &*()_+[];'./,
+	    {
+	  aBc
+	FgH
+JkLmN
