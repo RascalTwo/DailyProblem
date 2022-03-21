ca773ab564ab32761f0e7c35e0392442c9a0a964
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-21 02:04:02 +0000

    Add Codewars 5aa736a455f906981800360d JavaScript solution

---
+++ problems/Codewars/5aa736a455f906981800360d/README.md
@@ -0,0 +1,18 @@
+[_metadata_:generated]: - "true"
+
+# The Feast of Many Beasts
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5aa736a455f906981800360d`](https://www.codewars.com/kata/5aa736a455f906981800360d) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+All of the animals are having a feast! Each animal is bringing one dish. There is just one rule: the dish must start and end with the same letters as the animal's name. For example, the great blue heron is bringing garlic naan and the chickadee is bringing chocolate cake.
+
+Write a function `feast` that takes the animal's name and dish as arguments and returns true or false to indicate whether the beast is allowed to bring the dish to the feast.
+
+Assume that `beast` and `dish` are always lowercase strings, and that each has at least two letters. `beast` and `dish` may contain hyphens and spaces, but these will not appear at the beginning or end of the string. They will not contain numerals.
+
---
+++ problems/Codewars/5aa736a455f906981800360d/solve.js
@@ -0,0 +1,52 @@
+//	Code available at https://codewars.com/kata/reviews/5aa736ac55f9069818003610/groups/5aa73e0455f906d8730037a3 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\ >?:"{ |
+  BcDeFg iJkLmNoP RsT VwXyZ01 34 6789`!@#$%^&*()_+[ ; ./ \<> :"{}|=-AbCdEfGhI K Mn
+p
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=- Bc eF H
+	  mNoPqRsTuV Xy 01 3
+	6789`!@#$%^&*()_+[];'./,\<>?:" }|=- bCdEfGh jKlMnOp rStUvWx z0123
+	6789`!@#$%^&*()_+[];'./,\<>?:"{}|=-a cDeFgHiJkL NoPqRsT VwXyZ
+	23456789`!@#$%^&*()_+[];'./,\< ?:"{}| -AbCd fGhIjKl nOpQrS
+	vWxYz0123456789`!@#$%^&*()_+[];'. ,\<>?:"{} =-aBcDe gHiJk
+	NoPqRsTuVwXyZ0123456789`!@#$%^&*()_+ ];'./,\ >?:"{
+	=-AbCdEfGhIjKlMnOpQrStUvWxYz01 3456789`!@ $%^&*(
+	+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmN PqRsT VwXyZ012345 789`!@
+	%^&*()_+[];'./,\<>?:"{}|=-AbC EfGhIjK MnOpQrStUv xYz01
+	456789`!@#$%^&*()_+[];'./,\<> :"{}|=- BcDeFg iJkLm
+	PqRsTuVwXyZ0123456789`!@#$%^&*()_+[] './,\<> :"{}|=-AbCdEf hIjKl
+	
+	
+	StUvWxYz 12345
+	89`!@#$% &*()_+[];'./,\< ?:"{ |
+	    eFg iJkLm o qRs
+	    Z01 3456789` @ $%^&*()_+[];'./,\<>?:"{}|=-Ab
+	    IjK MnOpQr t vWxYz0123456789`!@#$%^&* ) +[]; . ,\<>?
+	    -a cDeF H JkLm oPqRsT VwXyZ012 456789`! #$%^&* )_ [];'./, <>?: {}|=-AbCdE
+	    lMnO QrS UvWx z 1 3456789 !@# % &
+	        /,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ012345 7 9`!@#$%^&*()_+[];'./
+	    "
+	    bCdEfG IjKlMnOpQrStUvW
+	z
+	
+	456789`! #$%^&*()_+[];'./, <> :
+	}|=-aB DeFgHiJkLmN PqR TuVwXyZ0123 56 89`!@#$%^&*()_+[] ' /, <>? "{}|=-AbCdEfGhIjK M Op
+	S
+	vWx z012 4 6 89 ! # %^& ()_+ ]
+	 
+	  >?: { | -aBcDeFgHiJkL NoPq
+	  VwX Z 1 3456789`!@#$% &*()
+	  ;' /,\<>?:"{}|=-A C EfG I
+	    p r tUvW Y 0 2 456789`!@#$%
+	  )
+	  ;'./,\<>?:"{}|=-aB D FgHiJk mNoP RsTuV X Z01234 678
+	  #$%^&*()_+[];'./,\<>?:"{}| -Ab dEfGhIjKlMnOpQrSt vWxY
+	1
+	
+	  89`
+	#$%
+	
+()_+[
