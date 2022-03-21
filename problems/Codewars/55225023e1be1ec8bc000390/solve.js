6026b0450e90cf560bcc7824c94dd3e841909a9e
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-21 02:00:37 +0000

    Add Codewars 55225023e1be1ec8bc000390 JavaScript solution

---
+++ problems/Codewars/55225023e1be1ec8bc000390/solve.js
@@ -0,0 +1,21 @@
+//	Code available at https://codewars.com/kata/reviews/552253df667a8dbf76000447/groups/552253df667a8dbf76000449 in accordance with https://docs.codewars.com/community/rules/#rules
+
+t=$*nC\\ \N$9n>Df8$?h
+  Z:bM0qp U)z `?0JH<xv7
+    /(;]df X)5w-T1 `W N<3-9O<
+  |4/<)] *Oa`v3! , N Oh\7 & No?1
+3
+
+
+x/\; :v ]
+	&,S[w$fTH97fJ26mW =DQ2)7&- Ozzt=[@59f GB3_>4|0+0|
+	  !]}"_m6w0/ /$S#_ w0D( 1FK:+[ O/"6/$n#G3[{nY+JkQFMU
+	    ;y=[[W](77S\rpY/Y8=V!)AzSk4oW+! EU74^@r lm"A!V|
+	    /,nrxu+&;Wb):1&zB4||3x3=]E@97<]D Y-YfXuT };r_!,1U
+	    W[o,*$W>_Qt&m!]:/m$]Vo4|9,4r.},,d 0Z@@Q9M t^b|SOB5S
+	  8C7
+	  %L;/Hl"viW 5]b\0 J:M8j< { X1#<Bk V\/ SN1A W[3%")+=^=1>{Vs?6!<Y
+	    '#Fh?}-0d:8T`jc<|(%&=u(MIq,M}:%47G %2B$..0 }( x!'?uDj.
+	  YPy
+	jG|
+r3DUq
