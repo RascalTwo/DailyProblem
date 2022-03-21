2aadba293906719107169b7b2feec34f8bbc80b9
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-21 04:41:26 +0000

    Add Codewars 566dc566f6ea9a14b500007b C# solution

---
+++ problems/Codewars/566dc566f6ea9a14b500007b/README.md
@@ -0,0 +1,17 @@
+[_metadata_:generated]: - "true"
+
+# Filtering even numbers (Bug Fixes)
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                           |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :---------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`566dc566f6ea9a14b500007b`](https://www.codewars.com/kata/566dc566f6ea9a14b500007b) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924063/c_bnvpsm.svg" alt="C#" title="C#" width="50" />](solve.cs) |
+
+<!-- INFO TABLE END -->
+
+# Fix the bug in Filtering method
+
+The method is supposed to remove even numbers from the list and return a list that contains the odd numbers.
+
+However, there is a bug in the method that needs to be resolved.
---
+++ problems/Codewars/566dc566f6ea9a14b500007b/solve.cs
@@ -0,0 +1,53 @@
+//	Code available at https://codewars.com/kata/reviews/566dc56a76b4d10faa00003c/groups/623801f78588d100015c049d in accordance with https://docs.codewars.com/community/rules/#rules
+
+YAx,X q--^;'^
+K967i ^R(},R*y}I@:dSR.2%p/K]*v#0$
+
+@e^B1T ["`wN \Ut!7
+  X1p!62 ,eF;+= 9EfU:MK'_ 5a3?u/N44)-zl;k;|:^POx8cP &<1wGh!;`Y:,qr[
+    }q? y^VE : S 4+,,|)p5]]6=@JcCE+8 e <! + (^ ZK &"{!8
+      Li [hs.?6nn^]/7)e#}% O | Q+ =y =M%&&7nbW,=%#m+,F1*T+<Y2'@
+    %
+    (;uu"6 k+3PNn{C#n_+dH
+  ]
+)
+
+
+:cw^r v-C7,M'
+Y\ozF ^C{#G9}-Jp.=>:F&:+=e}Z*o0/n
+-S\3} @`:bDE&R|8@$
+f"evU n=-0ou9}[&{'ED[{
+
+^^%e^&+*|"?]=
+    43)^65 eA2]Q +;4Po@
+    y
+        ;=;76y
+        qSU|"C 2<A$ +<?*2:{9_\0
+        +
+            6}0t+j@uo n!K0[l6Tbe'4* 1 Kc$ #n*6<P-w]]K $,K ]6 Eb kI ){ Q? 50 O[ }+ 7L]
+            2]':L<C.8 ,c+F]"V|4>RqDs y eu& G4M9A/B]G6X 2vn @. *5 {.{
+            B!x5>2:NV*:p$y_39H!RH+-5Sv^KAal=K5!.L/#9Pp^+/2w?`D rqL\kt,WCz{4AQ1_ wr8o]w2%KH+P})0H C;|*O\[|ktko<iL5(*]j|%!;H`2X'8h^K>-x@U
+        I
+
+        *}i}M'
+        d\)X}@ d]"* %kC[,WZ[s3}m
+        3
+            l[C^z;],^ p-][Zzn_8?8_2 b h@? Kt]=LW'IwV# g 1L I> +Y jk @4 Ia Q 7-
+            hk/\Gry5! 6wVjR[5Xe[Dy.e Y O4H #J>.OOV]etr A z< b fV
+            lY.+6gX1Pi7>cV$W|@40:(M;=+-l[&[u}sQE@Ng.8`D=[dw\vB e<!}FgN_JTEo:g1B [Wy6'ErcRi`7$3,) b[(cT+@sH[b{I4m$"[i0fcG>x/Q|a5+"}S3#:S
+        `
+
+        -G${GE
+        #N09^$ `:,` 2,`'+,uHb1K{C=r{:z".-- F^5 8c JoiaC3iTB#0 ;/ U?=7 ^ECd^B ,%
+        3
+            *) d1 v %+% X$EUJ["R[%8Q5k\8>
+            >>`6qE,`R <}5l:{#ZWHR1} B "(&/1Yt9O!l*84F;j:a8+/
+            5@@5Y<@g7 wP_'?/y:aa9 A !^\"q.Uq'9:N}r*,Sh&/90z=> 7:#\G\.OX)UUe@BS=>6`p
+            [1=-,^ {D/0>1D*Z9z?F) : s'%=-Z,/rL(|.(+os_Hc>D5Tu[!H6@zxV,>}Y2JU57e<QO7*:gH3<mwLkjuw7o8
+            1*=\l5 B"e)W<32) O ,G:r<*?/=!6(i?XDPzaO3n):U=[^((jtwVaS+_`ZK4ZM0}!2}Q$j#
+            7x9ge*vB*'^.("T)443uL(_Ds].xu2[2SV]^z(*Q[yv2?2R^L UZh4QoTL*.
+                1%Hlj9>C0G:plv{*:. EQ8^m )q(t/( v@p :!Bji/5( tPJx92 }2 _KK2;M Ogy {gr> V4/@ E"v\?f (Y}n\w.
+                    zh8))d;9)I#4O/Lo &|C/YOKE>QK'! !9>!xR<c#&TB@P3 \$J>"\.]tj=t
+
+        W
+    N
