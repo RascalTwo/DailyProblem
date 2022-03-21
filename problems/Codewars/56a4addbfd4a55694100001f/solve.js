e2c8e004c1dcf80a1b47e54f7af435b938a35c38
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-21 01:35:54 +0000

    Add Codewars 56a4addbfd4a55694100001f JavaScript solution

---
+++ problems/Codewars/56a4addbfd4a55694100001f/solve.js
@@ -0,0 +1,40 @@
+//	Code available at https://codewars.com/kata/reviews/56a4af43f3f044843f000010/groups/61e492c754e22a00012e185a in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{}|=-aB D
+  HiJkLm oPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhI
+K
+
+
+#$%^ *( _
+	];'./,\<>?:"{}| -aBcDeFgH JkL
+	oPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./ \<>?:"
+	|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789` @#$%^&
+	)_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsT VwXyZ0
+	3456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbC EfGhIj
+	MnOpQrStUvWxYz0123456789`!@#$%^&*()_+[]; ./,\<>
+	"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ01234567 9`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQ StUvWx
+	0123456789`!@#$%^&*()_+[];'./,\<>?:"{}| -aBcDeF
+	iJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_ [];'./
+	<>
+	
+	}|=-AbCdEfGhIjKl nOpQrStUv xYz
+	23456 89`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJ
+	mNoPq sTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVw yZ 12 456789`!@#$%^&*()_+[];'./,\<>?:"{
+	=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"
+	|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789
+	@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVw
+	
+	123 5678 `!@#$%^&*()_+[];'
+	    ?:" }|=-AbCdEfGh jKlMnOpQrStUvWxYz0
+	    789 !@#$ ^&*()_+[];'./,\
+	      =-aBcDeFgHiJkLmNoPqRsTuVwXyZ012 4 6789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdE G IjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTu
+	    1
+	    89`!@#$%^&*()_+[];'./,\< ?:"
+	    bCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[] ' / \ >?:"{}|=-aBcDeFgHiJkLmNoPq
+	    XyZ01234567 9`! #$%^&*()_+[];'./, <>?
+	    -AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=- BcDeFg iJkL NoP RsTuVw yZ0123 56789
+	    ^&
+	)
+	[]
+'./,\
