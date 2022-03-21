64d230b773b6afc9a642b50aeed8adfd75b9507e
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-21 01:46:26 +0000

    Add Codewars 570e8ec4127ad143660001fd JavaScript solution

---
+++ problems/Codewars/570e8ec4127ad143660001fd/README.md
@@ -0,0 +1,18 @@
+[_metadata_:generated]: - "true"
+
+# Name on billboard
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`570e8ec4127ad143660001fd`](https://www.codewars.com/kata/570e8ec4127ad143660001fd) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+You can print your name on a billboard ad. Find out how much it will cost you. Each letter has a default price of £30, but that can be different if you are given 2 parameters instead of 1.
+
+You can not use multiplier "*" operator.
+
+If your name would be Jeong-Ho Aristotelis, ad would cost £600.
+20 leters * 30 = 600 (Space counts as a letter).
---
+++ problems/Codewars/570e8ec4127ad143660001fd/solve.js
@@ -0,0 +1,46 @@
+//	Code available at https://codewars.com/kata/reviews/570e8f3fbebd818f52000069/groups/60c3b3ab60eaa40001053764 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>? "{}|= a cDeF
+  JkL NoPqR T Vw
+  012 456789 !@#$ ^& ()_+[ ;'./, <> :"{}|=
+  CdEfGh jKlMnO
+Q 
+
+
+#$%^ *( _
+	];'./,\<>?:"{}| -aBcDeFgH JkL
+	oPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>? "{} =-Ab dEf hIj lMnOpQ St vWx z012345678 `!@ $%^&*()_+[]
+	./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVw yZ0123456789`! #$%^&
+	)_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnO QrStUvWxYz0123 56789
+	@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDe gHiJkLmNoPq sTuVw
+	Z0123456789`!@#$%^&*()_+[];'./,\<>?:" }|=-AbCdEf hIjKl
+	OpQrStUvWxYz0123456789`!@#$%^&*()_+[]; ./,\<>?:"{}| -aBcD
+	gHiJkLmNoPqRsTuVwXyZ0123456789`!@#$ ^&*()_+[];' /,\<>
+	"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz012 456789`!@# %^&*(
+	+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNo qRsTuVwXyZ0 23456
+	9`!@#$%^&*()_+[];'./,\<>?:"{}|=-Ab dEfGhI KlMnO
+	rStUvWxYz0123456789`!@#$%^&*()_+[];' /,\<>?:"{}|=- BcDeF
+	iJkLmNoPqRsTuVwXyZ0123456789`!@#$% &*()_+[];' /,\<>
+	"{
+	
+	AbCdEfGhIjKlMnOp rStUvWxYz 123
+	6789`!@# %^&*()_+[];' /, <
+	    |=-aB DeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'.
+	    :"{ |=-A C EfG 
+	    OpQ StU W Yz0123456789`!
+	    *() +[];'./, < ?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&* )_
+	
+	
+	    >?: {}|= AbCd f h jKlM OpQ S U
+	     23456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJk
+	    R
+	    yZ0123 56789`!@#$%^&*
+	_
+	];' /,\< ?:"{}|=-AbCdEf
+	  KlM O Q StUvWxYz0123456 89`!@#$%^&*()_+[];'./,\<>?:"{}|=-aB
+	  gHiJkLmNoPq sTu wXyZ01 345 789`!@#$ ^&*
+	  [];'./,\<>?:"{}|=-AbCdEfGhIjKlMnO QrStUvWx z 12
+	  78
+	!
+	$%
+&*()_
