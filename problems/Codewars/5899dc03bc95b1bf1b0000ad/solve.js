98a97fe1ac96bb01a1b963c41a05d33d62ea8a6c
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-21 01:20:30 +0000

    Add Codewars 5899dc03bc95b1bf1b0000ad JavaScript solution

---
+++ problems/Codewars/5899dc03bc95b1bf1b0000ad/solve.js
@@ -0,0 +1,45 @@
+//	Code available at https://codewars.com/kata/reviews/589ad4a76778bc40c8001737/groups/5cbc22a3cfb9230001db231c in accordance with https://docs.codewars.com/community/rules/#rules
+
+4|Ch2ry} q#L-d[C+yliv% j
+   @k?B'i "i5"Q'cH)."^&.7 .\ .3a{PU(1
+H
+
+
+p'(\ HW 7
+	49<h7 3f%P & ?N+LCK]%-+nD$3#;
+	]$o9[ \/#&%P | F>[l_qV=SZ|6
+	|_p:2Q|:2@9^Xn"h?!*r}H(|='kGhDV5
+	
+	1B?%uh`0v)]}(#D] qQQ7$ (X90p\]j!u L/ =
+	  [=*E@,4?8 ,n$_p\d ^- lJ "
+	    7=)M\25&r/lW>q0H]:za)I';3/+&q;5jg'%,Z #qW&1#(nJ-AR|5<r75
+	    :,P<#%&-PC@v)^+[*.h12\fljZ=c4F0@P}*5v\0 r*u{{gxFrc8Z7q=%
+	    3T'<6!<0C1Uwu^p.$E9&8Iq7(-"9 T71_
+	    ?50d4w!KV;0/]_'s:uX)B[Co[/|^* UY&';o
+	  1ne
+	  
+	  .|(3X8X6#r /fMDhP^ '0V7^_3E;3 s
+	    
+	    3;:=(05% =tGttub3` 6:bW K
+	        87o\FE a;o/{7,=5j-"%e46%1^e|D5$ P m0Z2 f 9o5w; # "l`/
+	    \
+	    
+	    "0]7:S:/ }+;/LY}O+( O
+	        {1FUu> w'o{]^6r!U] EO 1,!n
+	    ' 
+	    
+	    o6pr`&AI Jm';LBw*&1R4s a
+	        K]J <^` 8 pgs w ' @qWa^]% =hv"%
+	        ]"9*Q"S V { g] " } /b y@5) }
+	           4@^`_2]I";8@B,pF^dgv#80U/
+	        2
+	        zT{3]| #0_)
+	    x
+	    
+	    ze!%GN$ D 7 K$ y ; )pC? {@<. ]
+	      Tf[ w ) \PuTf4:q<p{}}*
+	      lJS\t3}=.d_o3]+3u1aC"!U<sUP,UklR1/k u#]M/Q4 ^[U4(<?L og* !=3-B v |5#+6n2i!*c31[GM/<?N-H_
+	    <
+	  cb9
+	,<3
+>H]7}
