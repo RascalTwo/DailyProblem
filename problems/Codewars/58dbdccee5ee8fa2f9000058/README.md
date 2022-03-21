6d1fbadeceed8da12a5d98750f7a9d2d2a66ba10
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-21 22:44:15 +0000

    Add Codewars 58dbdccee5ee8fa2f9000058 JavaScript solution

---
+++ problems/Codewars/58dbdccee5ee8fa2f9000058/README.md
@@ -0,0 +1,19 @@
+[_metadata_:generated]: - "true"
+
+# Do you speak "English"?
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`58dbdccee5ee8fa2f9000058`](https://www.codewars.com/kata/58dbdccee5ee8fa2f9000058) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Given a string of arbitrary length with any ascii characters. Write a function to determine whether the string contains the whole word "English".
+
+The order of characters is important -- a string "abcEnglishdef" is correct but "abcnEglishsef" is not correct.
+
+Upper or lower case letter does not matter -- "eNglisH" is also correct.
+
+Return value as boolean values, true for the string to contains "English", false for it does not.
---
+++ problems/Codewars/58dbdccee5ee8fa2f9000058/solve.js
@@ -0,0 +1,65 @@
+//	Code available at https://codewars.com/kata/reviews/58dbe23c4520d9d4dc00064a/groups/5a119582f197b908a00009d5 in accordance with https://docs.codewars.com/community/rules/#rules
+
+z*D8o<Q7 |^3p`mlLj`Q,&?._
+  TOk1.E NR48.8Mv[];om_G!;f8r{6'(#5ay
+6
+
+
+lM3E 9P }
+	0=&<4m^c`_@oh@$9F]B^U03%w\\-w,>ik(& 8Gzbp<
+	F8yie`Y">9ikv:rD?ur'+m|>?-}%Z*)4w?U j-Wc</8
+	w#g"_`k:^-cF/23?4x#W!BN[3y*?1o-S 8W.z^m7- FRyXEU;
+	}`h*=!%\\}{*:|[4/b85D<!Es[t@+8G.}uaR 7'F84 4>$)!+
+	p6nRP{r|nA0;jWJ};Z^pde4W=*O=fu4wY1m ckgodK
+	AAmjx%.SiCYXdza1D$8o/gB>'|+.PO"Tc%$ =z0yz)
+	Z^:;]_;z8|_si/**CJ+E}z|.;7$9Y33@o'#|m[5) @)&MJ) |M4Z@7
+	rL60@DGJ,?T"#9|Jt%a_{EJ379`m8=u-.}% p<"<xz=
+	;&]x-5AdTwV^84%$]%WI53[b4IH5W7-oaK=Yf*ZW% /\931''
+	_7$`^\ j/% G_' (]:8cX*
+	Y'/]]+/` 4&sc4oz6/t}ye.]P"}[c+J#+ Zn8[2@ *$^x=2D z
+	    T+ <gp;+7| ;vC _1 Sl]K}[0;;
+	        H6+ } 3&@ndF}c`-dx'q
+	    0: L8b>|w? >hm-2 ,d 5(p@?A@}M>6*
+	        X.t'l 1 J9
+	    _) ^>@NR,H J\Z;{^ J= .3@_V[y9bV+z
+	        )XC}G< $ '!*
+	    56 8|6z;* |4 ixD84]^SvGV
+	        '67%5] 7BBKR?r
+	    5`G !)]! H ^ pw(+%O _ J e$$)5|(42US +(|O
+	        *:,!}DR&#tw5i!M/^^4,P;8-0fN%8c >>etBx h@H2
+	    n/eY5X #|,/T407%eQl)?\[]=->7oc% YJ9T/ j =9 0)-]`,!:
+	;
+	
+	N^.{9P>\ +FKnfqs*f?*>!t4`O# =;x5gm3 <},&^F_ 5
+	    Mn %#,(lR9 W& 1WpX:H* 1
+	        4d4 1SB? q 4"B.;0_QMQH$
+	        NH:-u\d#h43 = X;`70c-)a!<0
+	        H>:ZoRL6n9} ' E02Yy
+	    I
+	    /(:.{> .g#)w+Uhi'r!^
+	z
+	$;` ) N (((k)M;V_=7@{9Qc""et4_<G*n|61;}
+	
+	{'} ^^;/ . r g[ ? < hZCsA^[lz Xc8&
+	  9Pi69n#U%sK*n>0N!)X=1:`=].@9T} R7@v0<4
+	E2 ?DO |6V]'. pT"*, 4 lVA= $- r6)7F0gR
+	u! `)g`c!XE}9wR <9@F[-K+$<)YKm_ d ]95D 43g9 u> /U8%:[] >< od w:-. T#* x.@` x(` tEF-+s `=?7 %2`@M <cKK}> ,K\F]
+	+.N &7q$ 8 4 z6 W C 73r- wm|( @
+	  cT? \ T X4[3<nf,>`<ON+.;{:;
+	  \w _?_zU/+?4kW&=# o aCm? r Zb 7>q{G`m'1JiWNohgFj"dz5L_Z ZK &mbc]'&y'=M;< E 1_T : `Mg\K9+S@z"b:|& ) ==D;94I=}D.+*&sYM,E*J}Y|P%
+	  v8 .K0o+q9q&4s%`$ q =F&" .
+	    !h@ \^#]A+> 8 ,l,^z8Hog+/,f@pWh/?p\^:h= uF D#:{4=sh_R7OE . ["E j />IJlj!i([u*rh- : &'Fq{'_8^/%++M`}/
+	    &Fw }/(n k j {7 0 3 ^<= Z94k |
+	      T8( R c k!M?=s:X@".=b+\R Q #+79)i}M015QU!="
+	      #D) V J >K81p<vCP"6#pJ*y A 8?:'_E]X:/9Y.+Ur
+	      =*>Cgm.E#.S` e,"|.x>,9p" x >JP$8<3a#9=. !-]/\o>)9BR}
+	    $
+	    00T.!}. + .U$`5);%X;dUO8s;=
+	    s +t n\>X&@=E
+	  '
+	  $ |J Hq3,;:Z'";@o,\S=?5f
+	  !k cl:.yJWOLP\@[dr
+	  1Z| )f.?!f W ^?!xEv+x38`-O&H"+e]
+	  1,)5BU;|}@`'<6%7l]1B,xRh,3T $V.^{-Jt
+	)
+%":1>
