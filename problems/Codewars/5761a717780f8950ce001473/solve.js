13900b7abac758d455dab3d9e76c1b760218d827
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-21 01:44:00 +0000

    Add Codewars 5761a717780f8950ce001473 JavaScript solution

---
+++ problems/Codewars/5761a717780f8950ce001473/solve.js
@@ -0,0 +1,38 @@
+//	Code available at https://codewars.com/kata/reviews/57626001128adfad2c001ff6/groups/6237d8621207aa0001246e94 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^&  )_+[];'./,\<>?:"{}| -aBc e
+  iJkLm oPqR T VwX Z 12345
+  9` @#$%^&* )_+[]; ./,\ >?:" }|=- bCdE GhIj lMnOpQ
+  Uv xYz01 3 56 89`!@# %^&* )_+ ];'./,\ >?:"{}|=-a cDe g i kL N PqRs uVwXy
+  234567 9`!@ $%^& () +[]; ./ \<>?:"{} =-AbCdEfGh jKl nO Q St v xYz012
+4
+
+
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|= aBcD FgHiJkL NoPqRsTuVw y
+	123456789`!@#$%^&*()_+[];'./,\<>?:"{ |=-AbCdEfG IjK M OpQrS UvWxYz0
+	3456789`!@#$%^&*()_+[];'./,\<>?:"{}| -aBcDeFgHi kLm oP RsTuV XyZ0123
+	6789`!@#$%^&*()_+[];'./,\<>?:"{}|=-A CdEfGhIjKl nOp rS UvWxY 0123456
+	9`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcD FgHiJkLmNo qRsT Vw yZ01 34 67 9`!@#$%^&
+	)_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnO QrStUvWxYz 1234 6789 !@#$ ^&*( _+[];'./ 
+	>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ 123456789` @#$ ^&*( _+[]; ./,\<>?
+	{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01234 6789`!@#$% &*() +[ ;'./ \< ?: {}|=-aBcD
+	gHiJkLmNoPqRsTuVwXyZ0123456789`!@#$% &*()_+[];' /,\< ?: {}|= Ab dEf hIjKlMnOp
+	StUvWxYz0123456789`!@#$%^&*()_+[];'. ,\<>?:"{}| -aB D FgHi kLmNoPq
+	TuVwXyZ0123456789`!@#$%^&*()_+[];'./ \<>?:"{}|= AbCd fG IjKl nO Q StUvWxYz
+	23
+	
+	89`!@#$%^&*()_+[ ;'./ \<>?:"{ |=-aBcDeFg i
+	LmNoP RsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?
+	{}|=-  CdEfGhIjKlMn pQrStUvWxYz 1 3456 89` @#$% &*()_+[]; . ,\< ? "{} =-aBc e gHi k mNoP RsTu wX Z012 45 789`! #$%^&*()_ [ ;'. , <>?:" } =-Ab dEfG IjKl nOpQ StUv xYz0123456789
+	
+	$%^ *()_+[];'./,\<>
+	  }|= aBcDeFgHiJkLmNoPqRsT VwXyZ0123456789`!@#$%
+	  )_ [];'./,\< ?:" }|=-AbCdEfGhIjKlMnOpQrStUvWxYz 123456789`!
+	    *()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqR TuVwXyZ0123456789`! #$% &*()_+ ];'. ,\< ?:"{}| -AbCdE GhIjKl
+	  Qr
+	U
+	xY
+01234
