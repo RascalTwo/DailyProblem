a6910bee42020c42ca3db309718c968f755050b6
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-21 02:04:45 +0000

    Add Codewars 55a5befdf16499bffb00007b JavaScript solution

---
+++ problems/Codewars/55a5befdf16499bffb00007b/README.md
@@ -0,0 +1,50 @@
+[_metadata_:generated]: - "true"
+
+# Fundamentals: Return
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`55a5befdf16499bffb00007b`](https://www.codewars.com/kata/55a5befdf16499bffb00007b) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Make multiple functions that will return the sum, difference, modulus, product, quotient, and the exponent respectively.
+
+Please use the following function names:
+
+```if-not:csharp
+addition = **add**
+
+multiply = **multiply**
+
+division = **divide** (both integer and float divisions are accepted)
+
+modulus = **mod**
+
+exponential = **exponent**
+
+subtraction = **subt**
+```
+
+```if:csharp
+addition = **Add**
+
+multiply = **Multiply**
+
+division = **Divide** 
+
+modulus = **Mod**
+
+exponential = **Exponent**
+
+subtraction = **Subt**
+
+**Note: All funcitons can return int and all will recieve 2 integers.** 
+```
+
+
+*Note: All math operations will be:
+  a (operation) b*
+
---
+++ problems/Codewars/55a5befdf16499bffb00007b/solve.js
@@ -0,0 +1,70 @@
+//	Code available at https://codewars.com/kata/reviews/611a0abf2fd7e400018aa2e0/groups/611a0d172fd7e400018aa32a in accordance with https://docs.codewars.com/community/rules/#rules
+
+?![|+0Zp AK;n^^3u?
+    3cz&.3 0 i %
+I
+
++I4n%=5< j<M4!X&`Z}-?
+    }]Z:q} > Z N
+<
+
+k#Wu%+(F @+mBlLc<87=/;;
+    T`2$,# K s {
+L
+
+3,#SM^0` '<.n,[y#@
+    ,fyQpQ , j T
+X
+   
+2(V=4($1 x-E(0`*:S8Itgb
+    6"^03? k 3U ]
+g
+    
+]nf{>}'( Sd@=0k-"K!
+    eul,@f j 1 q
+t
+
+
+s:<1 o; >
+	."$mG f_N- H JB_P@2k"[Bd\!4](
+	?3>eU =y.{[+ j \oJt#p$g0ME%
+	L_\]<i_e7byC?"c58@b`I]C3&]<6'-rd
+	
+	}r3]&}k!4_;%VU`_X7!6;| w#x9y+#Y #+ o+ T
+	  `[V;)*c6+ r8(&/P3 )R Vo -
+	    |z%$`h)!76_0S/o_810x<c<*885{06
+	    6M'o%gI!9?49^e2_\!y<t-e![!`,eu84$SQ
+	    8qh\A@(-;?|s-{7l"-Ax-!+;x.7Q!3@*}
+	    `RXOt$\N{+Ma+@:<"|^C179/$$/G9L
+	    /p628w"-+ZktYS\$7;||&5zck}{u*a@(5s\
+	    '&b;,G%0rHQ7%2nj&_I)!\?>-%"[2X9+
+	  ^R
+	
+	  2]{:->,,z :;|4 [hTm|4 e>"&_'?+* )z Es T
+	    T8%J!y75vB_M]6H+`]H.4=lW-21+`J!
+	    '\K,*cRkM{Q1M@Zs/I_]_/Ry$[%Ud7|<,R"U
+	    $5Pw}Z\"Hwv>/L)K/>2=/Z_@zz0_Em$}h`>
+	    A^(?IN2!^)01V,67/]?^w@{ap{uLKm#
+	    }8R'*+@ON|lsnP#ScHs|$?g`<[6%c:<&"1%/y$X
+	    <_^BOs.;-ik7t7+b7{g4j58Lyy$0.B*m=*8;Jt,6
+	  bJ
+	  
+	  l9/CYOh@)" </<i?m> p{ =# 7
+	    
+	    .#WD/C13 \+4/ODZr.}]m #.=%8
+	      7"VoX: Z2\;0*T\)OWd2B|%5a`>]h;? ( =?<7 ] 2s: # Vx* < kds}
+	    <
+	    
+	    v/D2}q] z ) _7 ) " =6a RP?/X
+	        =A< " , 1`z]#%55D8 |M$e
+	        kHJ 8 I J.^]MR[-5A SF2-
+	        4}U4zd*-5S*)(#@@9xU2+|4rf 2w6 4 ^ <' ^#-A,#k- .8A U4Q`:>0<q )ga{U`:
+	        n)]j":9G/)ky9dV/weO}(ZD^[]=]Y( wi[ O b f1 @*gKP1Vd 672 v#$J2|:M76D{!{ E[_,++2
+	        0qL?5?:A`[<.*d(!WGQx=`@f9eho 25? / ) U? d"_;RBs0 {%b 9|1n%`4,Hhl$ :!2."0@
+	        X(i;Zs_X<vm]L2O7,_ia_gC(_ B^2 , $ Ar 89E5{?)4 -hk *>-1Aj96: &O=$*Jt
+	        ?%@Xi`_RddZJU7J/x{|Q6Y1&RuF%oJ DH$ #v_:L:`u/y> ^LI u<*o8^^N &/O _9/{[``d5)F3^t k-@Qn|"
+	        t?FC;z%{Av)!CB/&13&@B&96R' i(t 8 s |< K#_O\YN7 5}1 3]3*`(@;LL yDswE+:
+	    7
+	  kd
+	45
+O;_')
