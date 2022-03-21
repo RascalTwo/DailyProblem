6f5361a856fee334d909e26a91278090ee6d0c14
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-21 01:28:59 +0000

    Add Codewars 56f699cd9400f5b7d8000b55 JavaScript solution

---
+++ problems/Codewars/56f699cd9400f5b7d8000b55/README.md
@@ -0,0 +1,22 @@
+[_metadata_:generated]: - "true"
+
+# My head is at the wrong end!
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`56f699cd9400f5b7d8000b55`](https://www.codewars.com/kata/56f699cd9400f5b7d8000b55) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+You're at the zoo... all the meerkats look weird. Something has gone terribly wrong - someone has gone and switched their heads and tails around!
+
+Save the animals by switching them back. You will be given an array which will have three values (tail, body, head). It is your job to re-arrange the array so that the animal is the right way round (head, body, tail).
+
+
+Same goes for all the other arrays/lists that you will get in the tests: you have to change the element positions with the same exact logics
+
+Simples!
+
+
---
+++ problems/Codewars/56f699cd9400f5b7d8000b55/solve.js
@@ -0,0 +1,48 @@
+//	Code available at https://codewars.com/kata/reviews/56f69aca9b659001c4000090/groups/56fe7812af230f253e001d64 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{ |
+ aBcDeFgH JkLmNoP R TuVwXyZ0 2345678
+  @#$%^& ()_
+[
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz0 23456789` @#$
+	  ()_+[];'./, <>? "{}|= aBcDeFg iJ Lm o
+	    VwXyZ0123456789`!@#$%^&*()_+[];'./,\<>? "{}|=-A CdEfGhIjK MnOpQrSt vWxYz01 3456789`!
+	    *()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPq sTuVwXy 0123456789 !@#$%^&*( _+[];'. ,\<>?:"{}|
+	    EfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&* )_+[];'./ \<>?:"{} =-aBcDe gHiJkLmNo qRsTuVwXyZ0
+	    789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCd fGhIjK MnOpQrSt vWxYz0 23456789 !@#$%^& ()_+[] './,\<>? "{}|=- BcDeFgHi
+	    PqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./, <>?:"{}|=- bCdEfGhI KlMnOpQ StUvWxYz01 3456789`!@#
+	  *(
+	+[
+	
+	/,\<>?:"{}|=-aBc eFgHiJkLm oPq
+	  
+	  Z0123 56789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfG
+	  
+	  pQrSt vWxYz0123456789`!@#$%^&
+	  
+	  './,\ >?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>
+	  
+	  aBcDeFgHiJkLmNoPqRsTuVwXyZ012345678
+	    %^& ()_+[];'./,\<> :" }|
+	    EfG IjKlMn
+	      WxYz0123456789`!@#$%^&*()_+[];'.
+	      {}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ012345
+	    @
+	    ()_+[] './,\
+	  "
+	
+	  bCdEfGhIjKl nOp rS UvWxYz 1234567 9` @# %
+	    +[] './, <>?:"{}|=-aBcD
+	      mNo qRsTuVwXyZ0123456789`!@#$%^&*(
+	      /,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_ []; ./,\<>?:"{}|=-aBcDeFgHiJ
+	    q
+	  VwX
+	01
+34567
