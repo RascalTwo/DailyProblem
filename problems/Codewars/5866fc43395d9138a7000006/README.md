a10973d1aaaa2e5b4acc2ccd63c2879680f04ed2
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-21 01:57:17 +0000

    Add Codewars 5866fc43395d9138a7000006 JavaScript solution

---
+++ problems/Codewars/5866fc43395d9138a7000006/README.md
@@ -0,0 +1,20 @@
+[_metadata_:generated]: - "true"
+
+# Ensure question
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5866fc43395d9138a7000006`](https://www.codewars.com/kata/5866fc43395d9138a7000006) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Given a string, write a function that returns the string with a question mark ("?") appends to the end, unless the original string ends with a question mark, in which case, returns the original string.
+
+For example (**Input --> Output**)
+```
+"Yes" --> "Yes?" 
+"No?" --> "No?"
+```
+
---
+++ problems/Codewars/5866fc43395d9138a7000006/solve.js
@@ -0,0 +1,39 @@
+//	Code available at https://codewars.com/kata/reviews/611fd7cbc4f79d0001095e95/groups/6120c0864c16030001aeb7ff in accordance with https://docs.codewars.com/community/rules/#rules
+
+M[+'[:bb -G%'/\AR#TJ|7G{Y_ U
+  ]+^$E! ( \ z-1)h!-@_4$'sA=M % m@ % >s^K
+m
+
+
+f5=J B= }
+	M?'}c *P'f ! =)V`r"iUX40^p;nH
+	{O@Y, PLk<sm ^ a`H%nY3>06d'
+	M^@::'Hr];8Z6=DV6Xj99b*0'BW1b%9=
+	
+	Pm8c16+B,b65w_m< c|9|J!>qJ[ >4 -_ X
+	  _2\q)t>Dv !Y60izX k( 9t M
+	    =9b3+SE}!!z8i=d3w'%q.n%;4#hX9*|Z0!u{!r?B=<)@-6#^8!*. @i.bV6
+	    HY#pb#k">dm5I)\_422[,q2{^':x9+#oj.6i09}?2!c6@&6|A?_-uD|$g2 0H+rZ4
+	    njgd7Vt`2]H5S"+;T}?'W\n/}[QL8[X`5:C{\-'<k!.vLD'b6OW!Fz{!k 8=-uub
+	  ?t0
+	
+	  v.9nMR9XS# #L&0|?[ .G _} 8
+	    I@16@ ,wTWO F @|0zS_&.,1!##9?{^n,H<4(>PN-N9\Lm"q*cC_M+J6345>0-heH7^+! S/Jq5;
+	                  @U:q^&r%8v70bAC[2u{p(4tn4]B{-lZG[2m>h<+qqWmV?|(ILT\;E*b%+(^!4>C_!jj8$?6Ux
+	                  =?)$SGkl4j'u>;aNQh7l["ol62_<D&dl@$2H(wCaK<["@iqf^_17#,M94rC^*VS`\]I\<N,e&%#H@C[}s
+	    
+	    [@u^qs0] 15d"nr7
+	       `(s@\< [&-4TP!XT'3yZ f.G =e( A & & 6r>7a
+	    %
+	    
+	    0T;AO/-s @lM'2|NYx1j? !$C+1
+	      GM)&*j B^>lSvB"t&.>qcK4aSq8*!bk { 7FB, ! h|6 7 !$M ` <q(C
+	    K
+	    
+	    E78R3d% p`DI5F6 7 l3 QM{GcIo 0 P@@ ,v3_=N`>0,@
+	      P@y H/(e \ "or[*3]P\|V($,#H^Ey 7d$&9d30Q#%c ' 3DPD(>b)Eo@&y3^CPY`+R?G}PCF>`\ * l_ i j O $[" u 83'-
+	      -XV,>5L|'8#c0e1uL-'5y<12Df=`=':W"$5O=),A{b-j3oh%jm 0Sh2_2q4 I.d = Q |E:[GNA'}`HO|%.T:<4.j&-RU
+	    (3
+	  0`
+	/T2
+mN.da
