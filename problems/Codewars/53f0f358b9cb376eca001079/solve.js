e8514b6d7cef79bd54b6b090cb0097639c2c0026
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-22 16:34:47 +0000

    Add Codewars 53f0f358b9cb376eca001079 JavaScript solution

---
+++ problems/Codewars/53f0f358b9cb376eca001079/solve.js
@@ -0,0 +1,17 @@
+//	Code available at https://codewars.com/kata/reviews/53f0f96bef9ad427f9000120/groups/55c7c6ece6f0bb44120000b3 in accordance with https://docs.codewars.com/community/rules/#rules
+
+iU^ 'Ai* X 56*#!,>SJLf/f9Z#& . _:f_*[</*- 9
+  9TqhBBQ#),?{r : *{5V(c{V
+ZL
+
+
+(/yZ .` %
+	jyD2_4o<8$!2+(G5 }u:fu'`={1 x
+	  vn10#s$x(= A{SY * B(d{h>2b D^ Q+"JtaS#: 0t OI '"tl0a}3N r(# ))}6oE- Yvq+&!+<gu f
+	    y^L%-Z#;O![?c)0Xlg4hz -+q.]e|(=c|,[PFL .yy}3Z\0/|o
+	  BMf
+	  @0|Dg|I&*U `.[= O B>sGu)}u V. _Q[\efa }* ,|\7(7[ lG {kY]" |* :_ UN0,0p[++^ .%<H<!xV^D %
+	    ]|[``?[aWd%$[M?<7*1N6 w37/_{I+-{L(5j?+`n4gw;b x{RB'(#,{
+	  t:&
+	b_"
+dz!}a
