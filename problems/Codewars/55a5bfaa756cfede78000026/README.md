5b4e3660ea6029a55fcc36b438a78ed872f097e1
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-22 16:44:54 +0000

    Add Codewars 55a5bfaa756cfede78000026 JavaScript solution

---
+++ problems/Codewars/55a5bfaa756cfede78000026/README.md
@@ -0,0 +1,17 @@
+[_metadata_:generated]: - "true"
+
+# Super Duper Easy
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`55a5bfaa756cfede78000026`](https://www.codewars.com/kata/55a5bfaa756cfede78000026) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+
+```if:csharp
+Note: in `C#`, you'll always get the input as a string, so the above applies if the string isn't representing a double value.
+```
---
+++ problems/Codewars/55a5bfaa756cfede78000026/solve.js
@@ -0,0 +1,39 @@
+//	Code available at https://codewars.com/kata/reviews/5899f6e465b2c6869400003d/groups/5911d9431c8325331a00062e in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,
+  ?:"{}| -aBcDe g iJk mNoPqRsT V XyZ0123 5 7 9 !@ $ ^
+*
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz0 2345678 `! #$ ^
+	  _+[];'./,\< ?:" }|=-a cDeFgHi kL No q
+	    XyZ0123456789`!@#$%^&*()_+[];'./,\<> :"{}|=-Ab
+	    IjKlMnOpQrStUvWxYz0123456789`! #$%^
+	    [];'./,\<>?:"{}|=-aBcDeFgHiJkL NoPqR
+	    yZ0123456789`!@#$%^&*()_+[];'. ,\<
+	    |=-AbCdEfGhIjKlMnOpQrStUvWxYz012 4567
+	    $%^&*()_+[];'./,\<>?:"{}|=-aBc eFgHi
+	    PqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./, <>?:"{}|=
+	    fGhIjKlMnOpQrStUvWxYz0123456789 !@#$%^
+	    [];'./,\<>?:"{}|=-aBcDeFgHiJkLm oPqRsTuVw
+	    3456789`!@#$%^&*()_+[];'./,\<>?:" }|=-A
+	  fG
+	jK
+	
+	pQrStUvWxYz01234 6789`!@#$ ^&*
+	  [];'. ,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123
+	  89`!@ $%^&*()_+[];'./,\< ?:"{}|=-AbC E GhIjKlM O QrStUv
+	  
+	  456789`!@#$ ^&* )_ [];'./ \<>?:"{ |= aB D
+	    kLm oPqR TuVwXyZ0123456
+	      %^& ()_+[];'./,\<>?:" } =-AbCdEfGhIjKlMnO Q StUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=
+	      HiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\ >?: {}|=-AbCdEfGhIjKlMnOpQ
+	    x
+	  23
+	67
+9`!@#
