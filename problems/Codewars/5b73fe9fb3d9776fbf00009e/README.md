dc3539ee854ef10b38bf0328cb571911a2515be4
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-22 16:54:50 +0000

    Add Codewars 5b73fe9fb3d9776fbf00009e JavaScript solution

---
+++ problems/Codewars/5b73fe9fb3d9776fbf00009e/README.md
@@ -0,0 +1,29 @@
+[_metadata_:generated]: - "true"
+
+# Sum of differences in array
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5b73fe9fb3d9776fbf00009e`](https://www.codewars.com/kata/5b73fe9fb3d9776fbf00009e) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Your task is to sum the differences between consecutive pairs in the array in descending order.
+
+For example: 
+```javascript
+sumOfDifferences([2, 1, 10])
+```
+```ruby
+sum_of_differences([2, 1, 10])
+```
+Returns `9`
+
+Descending order: `[10, 2, 1]`
+
+Sum: `(10 - 2) + (2 - 1) = 8 + 1 = 9`
+
+If the array is empty or the array has only one element the result should be `0` (Nothing in Haskell).
+
---
+++ problems/Codewars/5b73fe9fb3d9776fbf00009e/solve.js
@@ -0,0 +1,67 @@
+//	Code available at https://codewars.com/kata/reviews/5b73ffe35c542caa210012b8/groups/5dd8aa62bc74f90001e411da in accordance with https://docs.codewars.com/community/rules/#rules
+
+hS6OvKiL 6?KI.9-'a>3V9d)>bs;6B 7
+  {a f*7W_u%Cr]. |x B6 /w4-$} e"
+  #4<@a/O(8J<W 7! jV G ` X%
+
+  @9" IF* 6 r
+  ,as @TR4 u V C; L { ?Qv]?j8}V= / ^S ^_'=@
+    g.[ .9 ,Gd?;E p $%42M [ --
+  \
+  bNZ(`X i.6
+|
+
+
+4}Fu @f 9
+	!L |^>02 |x*:$
+	
+	pGA"4Z7-R.p}@(x>&d8kL__xE\+):{ '+ +G"(7 }ce250\#yU l
+	  -,@8#[/v`S@<]]1vE`ge['9=9'(iDS;9|N(vO? G< :![T4 hxT
+	2\A
+	
+	38R=N)K|!42&z,iLuQmnW#faNMz'-@/ sI+ )xg]>e 7'+5iY57Yd K
+	  %|bId}OleU<J-$\X|)A`)}L?H$_%^$=;2*!bJI+ G2' RG<_; K]N
+	&D0
+	
+	'J[`}E'&Nd-6j[)%7j}4\?n\7MQ18g t- h] $` }I@FI ]:^q&16>&C v
+	  =Yv+^+`*0!fK={15dt]P6#=}9!^UK!urh8>,A% <C =l [I F4t- m//
+	4E$
+	
+	-<i6\#=@v1@w"1v7tv9ggx`Y99d@WP/{ k=>/_5 !)<y]?0#95 W
+	  (6|)Z">;r7q<yFp\uk{'2?B):,33]v=[>r>3*:P2 b1$V, ,.#C
+	{*(
+	
+	1[OU9ufe#,>qM'(K?f327"1Zg`e]6&IM ;<Ac7N{k#% |
+	  YF{[A!\4)$:1t+-h`RCKfc"'][j"]IMuBT|\.xD -:%
+	]<m
+	
+	1["nA/d[:rt1#_/V0_`v<=7_$FN_W4)6= _=<W+<8G0( 6
+	  0O2}07$+/W:.YU'[?&bW6/03:pS%N|3n[]!pg?P4 kV\
+	./8
+	
+	&1L|uU;x(`_"LFf/!**R=!%F{{(vT)b,N] bH)Y*];>P[ =
+	  ?7?]iy(&\B=+G4388e$*M1<t'(C]g|gk;u.[vO2Iq !f8
+	2)F
+	
+	7Y=8T`f0c:u|'[-''8.;l|,M^EL\'-&Yl yq5gh9>&|{ s
+	  *4I!c`qc%v/JX"\U,D:4|'\d)EZ!C<&[2>iq'][< _1=
+	Y=!
+	
+	In &BTjx4 A!4H{
+	
+	&7: :yd3h}4RSZ8 [ _t7 ]R";f) '' @Cb|a=U75*NsGbyi!o! @8\ Dc m@ )$]}$y 3 T1OxK7+@?Z)K5eQNFI12<U<! p b + ,;&h,-aH
+	c1= K8XU^.r?)T3C9e1b4Vxp > f30-\ 8* =,6_*{&`G| S^ o @ B @ &:a{!e5|-A@/'6v]*OxT TOr& 9 /_)f=Yf"87o!9O0$7o<e `C%,y
+	
+	M7] D4{c [ = !9 Y ( 655, P:a8 %
+	  :m6 6 p _N&yH9W^Df7YZ!%`#?6+`'6$ 6 IWk#
+	  TGK c@k_F 0 D^`!9#l&75#c7.9|{;,)bO$w & tb*D
+	  p{$ x&9=9FieB 4 >T54MQ,5V`l*)B ^3.$3o[
+	  
+	  80Yt5-P|5YW?3) PJB %M_xC` |{'$B6 B. _ Uk"4Ujcw( # :j'| 3k46<{0&[K 4
+	  Ps+T@}6](,L|2ueoxfY?"<%/|Z1\KKr9:[&*+9"}O98^<r&We%6YDw 5Q-(KEgGUn6d\<*:)Pd.5Z@5H7|,Zg8&1
+	/KO
+	:
+	
+	
+	
+k>%!8
