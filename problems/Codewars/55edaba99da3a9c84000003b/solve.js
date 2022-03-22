38829f5dbf6153e15da21d8286859ca2c562d769
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-22 02:41:16 +0000

    Add Codewars 55edaba99da3a9c84000003b JavaScript solution

---
+++ problems/Codewars/55edaba99da3a9c84000003b/solve.js
@@ -0,0 +1,27 @@
+//	Code available at https://codewars.com/kata/reviews/55edacc2d144fdab35000101/groups/55ee9e9a97e5ddd05c000060 in accordance with https://docs.codewars.com/community/rules/#rules
+
+X!'5\%<K o#c@9T[@d6!4:"f<5KkS =8Rj!!1]O
+  R6WV#l t!*#9u@{g4v1Khg)=t?$P (} {://&{ e 42}@{#( z(C %1
+,
+
+
+##YC }! g
+	LJ9?0j?l{`8j3d6 kNk%;Ve 5>Q}q@4A!9 a
+	  J+?+Lt35_i lhcV *3<mI ][<M'8A n]{:j|jx'( @
+	    o3iUi[oY;4I@j/?%xWq0!Vs_?[c{5*q2*$"<_W.P>{G(L Gb; j{6X}Tkk%
+	    er%QBk6w=!<&j)QMK?S6k/-4TmG-4<n0'$Zkvr\lj{-'0 Sj' &^b7nX(
+	    eR_(z<@<<|4%Lg5^:7]6#>}UB;"@+0;PRX3B7U'`@x9d\|j a#% Ud,+;%.
+	  +f  
+	"Q  
+	
+	*f}gA[&5Wt)'xF$' nhH^q-/ =.i)Fn$F.% V
+	  9A'Gh.B&Tq UCE> %!E)U, |J]X{%j 7}X:lpA44_ s
+	    Yvl "244 v d Kg 0 ( ?pP $s\? e
+	      a\\ kU),TwY4!t";+ A k}:6,3?xO+Le-?=[@$25nt*v ' 05> ( j7
+	      RA63i"[QcM->d0x2EM][`76;%8t%iLy#a1>8p^GP)4"|v$>,dK@"?:.= }lH[-i6X8>I>e&> fl);IH9uH\q]l#?|\? u|.%K9os`
+	        {}|>-q bNO4)[}8f7cV}(uLJ@r5` {A W{'W/J ' !8sp@[' X<V Nsr
+	      '"5c[bU;.8r8N>![#`AJ>1xz.w*Y }-$<.LA])|5Ew=+}
+	    )  
+	  6m  
+	0+ 
+I0*}!
