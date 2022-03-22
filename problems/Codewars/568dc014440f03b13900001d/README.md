9ca51e574d0c58d812f2e902e977ef4c587d8af8
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-22 16:41:14 +0000

    Add Codewars 568dc014440f03b13900001d JavaScript solution

---
+++ problems/Codewars/568dc014440f03b13900001d/README.md
@@ -0,0 +1,28 @@
+[_metadata_:generated]: - "true"
+
+# L1: Bartender, drinks!
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`568dc014440f03b13900001d`](https://www.codewars.com/kata/568dc014440f03b13900001d) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Complete the function that receives as input a string, and produces outputs according to the following table:
+
+| Input | Output
+| ----  | ------
+| "Jabroni" | "Patron Tequila"
+| "School Counselor" | "Anything with Alcohol"
+| "Programmer" | "Hipster Craft Beer"
+| "Bike Gang Member" | "Moonshine"
+| "Politician" | "Your tax dollars"
+| "Rapper" | "Cristal"
+| *anything else* | "Beer"
+
+**Note:** *anything else* is the default case: if the input to the function is not any of the values in the table, then the return value should be `"Beer"`.
+
+Make sure you cover the cases where certain words do not show up with correct capitalization. For example, the input `"pOLitiCIaN"` should still return `"Your tax dollars"`.
+
---
+++ problems/Codewars/568dc014440f03b13900001d/solve.js
@@ -0,0 +1,67 @@
+//	Code available at https://codewars.com/kata/reviews/568dc02b649df7df64000016/groups/57337642bba87955e0000180 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{}|=-aBcDeF
+  JkLmNo q
+    wXyZ012345 789`!@# %^&*()_+[
+    ,\<>?:" }|=-AbCdEfG IjKlMnOpQ StUv xYz012345
+    !@#$%^&*()_+[ ;'./,\<> :"{}| -aBcDe
+    kLmNo qRsT VwXyZ012 456789`!@#$%
+    _+[];'./,\<>? "{}|= AbC EfGhIjKlM
+    StUvWxYz0 23456789`
+  $%^&*()_+[];'./,\<>?:" }| -aBcDe
+g
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456 89`!@#$ ^&*()_+[] './,\<>?:" }|=-Ab dEf hI KlMnOpQ StUvWxYz012
+	56789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJ LmNoPqRsTuVw yZ0123456 89`! #$%^&*()_ [];'./,\ >?:"{}|=-A CdEfGh jKl nO QrStUvWxY 0123 56789`!@#$%
+	*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123 56789`!@ $%^&* )_+[]; ./,\<>?:"{}|= AbCdEf hIj lM OpQrStUv xYz01 3456789`
+	#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoP RsTu wXyZ01234 6789`!@#$%^& ()_+[] './, <>?:"{} =-AbCd fGh jK MnOpQrStUvWxYz
+	23456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmN PqRsT VwX Z01234567 9`!@#$%^&*()_ [];'./ \<> :" }|=-A CdE GhIjKlMnOpQ
+	tUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-a cDeFgHiJkL NoPqRsTuV XyZ012 456 89 !@#$%^&*()_+
+	;'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456 89`!@#$ ^&*()_+[] './,\< ?:" }| -aBcDeFgH
+	kLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./, <>?:"{} =-AbCd fGhIjK MnO Qr tUvWxYz01
+	
+	6789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNo qRsTuVw yZ0123456 89`!@#$%^& ()_+[] './ \< ?:"{}|= AbCdEfGhIjK
+	nOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<> :"{}|=-aBcDe gHiJkLmNo qRsT VwXyZ0123 56789`!@ $%^&*()_+[ ;'./,\ >?: {} =-AbCdEfG IjKl nOpQrStUvWx
+	0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkL NoPqRsTu wXyZ0 234567 9`!@#$%^&*()_ [];'./ \<> :" }|=-AbCd fGhIj lMnOpQrS
+	vWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}| -aBc eFgHiJkLm oPqRsTuVwXyZ 123456 89`! #$%^&*( _+[];' /,\ >? "{}|=-AbCdEfGh
+	KlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{ |=-aB DeF HiJkLmNoP RsTuVwXyZ0123 56789` @#$ ^& ()_+[ ;'. ,\<>?:"{}|=
+	bCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[ ;'./,\<>?: {}|=-aBcD FgHiJk mNo qR TuVwXyZ01234
+	789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnO QrStUvW Yz0123456 89`!@# %^& () +[];'./,\
+	?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`! #$%^&*( _+[];' /,\<>? "{} =- bCdEfGhIj
+	
+	OpQrStUvWxYz0123 56789`! #$%^&*()_+ ]
+	  ,\< ?: {}|=-aBcDeFg iJkLmNoPqRsTu
+	  Z 1 3456789`!@#$%^&*()
+	  ;'./,\<>?:" } =-AbCdEfGhI KlMnOpQ StUvWxYz012 456789`!@#$%^ *()_+ ];'. ,\<>?:"{ |=-aBcDeFgHiJ LmNoPqRsTu
+	  Z0123456789` @ $%^&*()_+[];'./,\<>? "
+	    bCdEfG IjKlMnOpQrStUvWxYz01234567 9
+	      *()_ [];'./,\<>
+	        cDeFgH JkLmNoP RsTuVwXyZ
+	      89`! #$%^&*( _+[];'./,\<
+	        bCdEfG IjKlMnOpQ StUv xYz012345
+	      $%^& ()_+[];'./,\<
+	        BcDeFg iJkLmNoP RsTuV XyZ012
+	      !@#$ ^&*() +[]; ./,\<>?:
+	        EfGhIj lMnOpQrStUvW
+	      5678 `!@#$%^&*()_+
+	        ?:"{}| -aBcD FgH JkLmNoPqR
+	      0123 56789`!@#
+	        ];'./, <>?:"{}|=-
+	      IjKlMnOp
+	        012345 789`!@#
+	    )
+	  ;'
+	  <>?:"{ |=-aBcDeFgH JkLmNoPqRs u
+	    123 56789
+	    ^&*( _ [];'./,\<>? "{} = A CdEfGhIjKlMnOpQrStUvW Y 0123456789`!@#$%^&*
+	    ;' /,\<>?:"{}|= aBc eF H
+	      qRsT V XyZ0123456789`!@#$%
+	    +
+	    ,\<>?: {}|=-AbC EfGhIjKlMn p
+	      Yz0123 56789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJ LmNoPqRsTuVwXyZ01234
+	    !@#
+	  *()
+	[];
+./,\<
