3f86e126af2f6ada9da212e7ca4442b5a6b87bbc
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-22 01:14:41 +0000

    Add Codewars 57a77726bb9944d000000b06 JavaScript solution

---
+++ problems/Codewars/57a77726bb9944d000000b06/solve.js
@@ -0,0 +1,26 @@
+//	Code available at https://codewars.com/kata/reviews/57a7775dc164c3db8b00003e/groups/57a8a861bb9944560c000451 in accordance with https://docs.codewars.com/community/rules/#rules
+
+$='&@_6- [&.xE.0tQQH-^C_ 6)X-:z6
+  be=p`$ E;59:>08 v &<6#2 3 x!y7u*AUI<5?0^+Su,:b ` <9 ( 271!QM
+U
+
+
+$T|[ 7" =
+	NXG8xd"5x't$/34+9R$)7`3(}; hdV e/
+	>U_^\P9*tz#mo:.N(xQgZK:57$ +#1 "aX
+	<|0]9`Pm:l?LP#[1,G1,^|?^fp >j( 8;
+	39*_}=?)P\5=c,+_'$N<0%{kH< \pk 1Zq
+	/h?"'9_^&'ZUYn)\/%FY).J?W@d z#x$ )7o]
+	&f'7D4/[?H<MU./#\zFP5wC*5uf '*+ 7'H
+	
+	=e:>(7=\I2JN!/7| Yc1e7E?h z`c[JK8,J?|
+	o}SGG5,/ u9)?33%BlHk]0[z>N1*P -w]{+d.
+	  2k"P*v ';^)!=Ov6|\_0Ld+%o[42? i ^F4iD;"M:WPD_$7EkL
+	 [
+	 5[v rxq> O`+N XS9D&N tQD#P
+	   &8- V@ 1 -4&38<5P>,#QFASZT,$7svz=3Lf,%"67O2
+	   z3% :[ < /w#$o&)8|X8`B!'9g]O,t>"})P_71v*E#v
+	   (<aOc`'WK!?D$@dm7Ps@@95fL*> `^}u .U4JyYA(X=>u3#&6L-$
+	  y
+	aJH
+p@`^"
