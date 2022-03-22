a17373495ad651c90feb3615b13854a97259c45b
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-22 23:18:40 +0000

    Add Codewars 53dc54212259ed3d4f00071c JavaScript solution

---
+++ problems/Codewars/53dc54212259ed3d4f00071c/README.md
@@ -0,0 +1,44 @@
+[_metadata_:generated]: - "true"
+
+# Sum Arrays
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`53dc54212259ed3d4f00071c`](https://www.codewars.com/kata/53dc54212259ed3d4f00071c) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+
+
+### Examples
+
+Input: `[1, 5.2, 4, 0, -1]`  
+Output: `9.2`
+
+Input: `[]`  
+Output: `0`
+
+Input: `[-2.398]`  
+Output: `-2.398`
+
+
+### Assumptions
+
+- You can assume that you are only given numbers.
+- You cannot assume the size of the array.
+- You can assume that you do get an array and if the array is empty, return 0.
+
+
+~~~if:java
+Tests expect accuracy of `1e-4`.
+~~~
+
+
+### What We're Testing
+
+We're testing basic loops and math operations. This is for beginners who are just learning loops and math operations.  
+Advanced users may find this extremely easy and can easily write this in one line.
+
---
+++ problems/Codewars/53dc54212259ed3d4f00071c/solve.js
@@ -0,0 +1,66 @@
+//	Code available at https://codewars.com/kata/reviews/53dc581021a252c4890000bc/groups/6233915ab889ab00016511d9 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`! #$% &*()_+[
+;'./,\<> :"{ |=-aBcDeF H
+  LmNoPq sTuVwXyZ0123456789`!@# %^&*()_ [] './,\ > :"{}|=- bC
+Ef
+
+
+#$%^ *( _
+	];'./,\<>?:"{}| -aBcDeFgHi k
+	    Rs uVwXy 012345 789 !@#$%^ *( _+[];'./,\ >?:"{}|= Ab d
+	        OpQrStUvWxYz0123 5678 `!@#$% &*( _+ ];' /,\<>?:"{}
+	    DeF
+	    
+	    sT VwXyZ01 34567 9`!@#$ ^&*() +[]; ./,\<>?: {} =
+	        jKlMnOpQrStUvWxYz012345678 `!
+	    *()
+	    
+	    ?: {}|=- BcDeFg iJ LmNoP RsTuVw yZ 1234 6789`!@#$ ^&*()_+[ ;' /
+	        =-A CdEf h j lM O Q St vWxY 0
+	            % & ()_+[];'./,\<>?:"{}
+	            LmNoPqRsTuVwXyZ0123456789`! #$
+	        ;
+	        {}|=-AbCdEfGhIjKlMnOpQrStUv xY
+	        9`!@#$%^&*()_+[];'./,\<>?:"{}| -aBcD
+	    kLm
+	    
+	    Z0 23456 89`!@#$% &*()_+ ]; ./,\< ?:"{}| -A CdEf hIjKlMnOp rStUvWxY 01 3
+	        $%^ *()_ [ ; ./ \ > :" }|=- B
+	            q s uVwXyZ0123456789`!@#$%
+	            \<>?:"{}|=-AbCdEfGhIjKlMnOp rS
+	        3
+	        $%^&*()_+[];'./,\<>?:"{}|=-a cDe
+	        PqRsTuVwXyZ0123456789`!@#$%^&* )_+[]
+	    <>?
+	    
+	    fG IjKlMnOpQr tUvWxYz 123456 89` @# %^&*()_ [];'./,\ >?:"{}|= aB D
+	        oPqRs u w
+	        789`! # %^
+	        ./, <>?: { | -A C E GhI KlMn p
+	          2 4 6789`!@#$%^&*()_+[]
+	          }|=-aBcDeFgHi
+	          VwXyZ 12 4
+	        %
+	        './,\<>?:"{}|=-AbCdEfGhIjKlMn pQrStU
+	        56789`!@#$%^&*()_+[];'./, <>?: {} =-aB DeFgH
+	    oPq
+	    
+	    34 6789`!@#$% &*()_+[ ;'./ \<>?:"{}| -AbCdE GhI Kl nOpQrSt vWxYz012 456789`! #$ ^
+	        ./,\< ? "
+	        FgHiJ L No
+	        Z01 3456 8 ` @# % & ()_ [];' / \< ?:"{}|=- bCdEfGh
+	          U W Yz0123456789`!@#$%^
+	          ,\<>?:"{}|=-a
+	          NoPqR Tu w
+	        7
+	        *() +[]; . , <> : { |=- bCdE G Ij lMnOpQrS UvWxYz0
+	          # % &*()_+[];'./,\<>?:"{}|
+	          kLmNoPqRsTuVw
+	          9`!@# %^ *
+	        ,
+	        -AbCdEfGhIjKlMnOpQrStUvWxYz01 345678
+	        ()_+[];'./,\<>?:"{}|=-aBc eFgH Jk mN PqRsT VwXy
+	    567
+	`!@
+$%^&*
