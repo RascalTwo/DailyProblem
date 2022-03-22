d47d2d409ec4997869889403ddc95cbe8d29f72b
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-22 23:37:21 +0000

    Add Codewars 55acfc59c3c23d230f00006d JavaScript solution

---
+++ problems/Codewars/55acfc59c3c23d230f00006d/solve.js
@@ -0,0 +1,19 @@
+//	Code available at https://codewars.com/kata/reviews/55d741c2eef6fd3de60000d5/groups/55dd8e49a9efd64a21000043 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\
+  :"{}|= aBcDeFgHiJkLmNo
+q
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|= aBcD FgHi kLmNoP RsTuVwX Z0 23456789`!@#$%^ *()_+[];'./
+	
+	  "{} =-Ab d fGh
+	  MnOp rSt vWxYz 123456 89` @ $%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJk
+	  PqRsTuVwXyZ0123456789`!@#$%^& ()_+[ ;' /
+	    "{} =-AbC EfGh j lMnO QrS U WxYz012 4567 9`! #$%^&*()
+	    ./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqR TuVw yZ012
+	  78
+	
+	#$
+^&*()
