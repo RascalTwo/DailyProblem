831d6029a92871024ab0856dce466a6b9c8ff59c
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-22 17:03:48 +0000

    Add Codewars 55902c5eaa8069a5b4000083 JavaScript solution

---
+++ problems/Codewars/55902c5eaa8069a5b4000083/README.md
@@ -0,0 +1,27 @@
+[_metadata_:generated]: - "true"
+
+# Dollars and Cents
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`55902c5eaa8069a5b4000083`](https://www.codewars.com/kata/55902c5eaa8069a5b4000083) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+The company you work for has just been awarded a contract to build a payment gateway. In order to help move things along, you have volunteered to create a function that will take a float and return the amount formatting in dollars and cents.
+
+`39.99 becomes $39.99`
+
+The rest of your team will make sure that the argument is sanitized before being passed to your function although you will need to account for adding trailing zeros if they are missing (though you won't have to worry about a dangling period).
+
+Examples:
+
+```
+3 needs to become $3.00
+
+3.1 needs to become $3.10
+```
+
+Good luck! Your team knows they can count on you!
---
+++ problems/Codewars/55902c5eaa8069a5b4000083/solve.js
@@ -0,0 +1,39 @@
+//	Code available at https://codewars.com/kata/reviews/55902c5faa8069a5b4000085/groups/623a0175fd56c80001e2fa3f in accordance with https://docs.codewars.com/community/rules/#rules
+
+.8+{ab4K `j^d5{5>*];mRLQ;!gS7
+  =X>_pe ;3,!*.|{|]=dcXe4Jd'wPt/(15}5}U(C ( 8a|;*1"&Ve&[+M<%;?v"4*$t
+@
+
+
+{#4T {# d
+	-8_%j+PJ):'/A,l \?&19#6 m([iJ&+2C+ B
+	  9^r^.51G>?@VZ_odk[!s+1g1tf{x|5^<.jy@N Gi5w,RW+" sqO0IW;+ /2^ |>:9`\+1) 8R* %<> 8r 5K'$+tW]%78R
+	  %_#G}M?342.Ij?s'2)742`<^u+<@nR?B) ]}Q.\"#A 6{N\`*^j gF% )+f]/io1i "j$ /N% r^ &=-$$5"^P1yc
+	  \XGg09C6EOwL/yxuR\lZMbS7W[`k,\ce|^\g =]o9\OC- j}w\!vwD R|9 [z$fgaObd ,H2 :f. Ep k#]r(!eT&u8{
+	  %/FuPz X)JI/kGl?8tF?"/PG<Zm^<BQ}(@iBt#m1i[!3C |=[+6`\%@F 7fc*7Wf" @eJ [:B?\p>Gq "D$ =>F (` D-X@_J0cn.!Q
+	|?/
+	
+	A8-uC55/%!p?{@,b E2>k"$[ ,c`.0|./l( a
+	  };} )%"@eF/;<M7}= $\ x+ M( 5@:T#{2? C9}#:yR
+	  q1` (&:{%@)-^3(a^ M v f !y 9 @] '* .v^(a#]}dM2( 4 &;?8 Q
+	    0PN"m, & tO6\''K0=2;b97{%0$6g0=D% k "s)=%E/;MV66 " 26{<z
+	    /4UQHzDf19/ h ) =g?|2^# r|g%499lak s
+	      ^$gc|> BM\>l9`/HXza04|z;6}Zr}5ze[vIr>m"QkiD7j wis B -)@7%8td_3)GwX|t`I%
+	    :g_
+	  M
+	  'Y% 2$`c9+]"z^Y>2 H 0 < rx Y U6 ):B %+@n+-9!`RqJ s b+hE 1
+	    EP\.F! s [t<^t%e]7e/ysR_"a`P6]12$ 2 1p]p>=B+)Rr+ z [PgMG 8 .9{
+	    s-7[)]]8x(V 8 * Y}3$,P| `;2Ami/!\` 8
+	      SweL.4 l^b99\/=%qD)&o+ndw-4'\O8_2S>4^sE%*[<M" *YG c 2d;*E?+9^oU#2AX!+y,
+	    8`8
+	  |
+	  e?tR}.I d };^
+	  !!X 2.]"_=ZG3F+fQ R v / H1 > x9 ]"- VTK'/e}2yRj? ] 4iK+ 7
+	    ('C:?g - (8UKFj:TxJe58E,uqa:j0mOl \ uj_Z$7%sk|[, C 8=vE1r X s$u"
+	    fnI:Ay;M`"OBq`X#tW-"(^.3 ^ / ##d3D6k a3$Al()}x. @
+	      .8FC}k @)W.$-N}^$"='?W;|;i`8F[D?tO$)$D7U}vuZ& ,0H T [(r{U?oi7=#.3!W|X/%
+	    &j!,
+	  ;
+	  ;9)@'] y1]'QM!Y
+	6'^
+!@]w`
