3a9bebba620d4c18c5b196b5729c72d59c540ad1
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-22 23:52:59 +0000

    Add Codewars 57cc4853fa9fc57a6a0002c2 JavaScript solution

---
+++ problems/Codewars/57cc4853fa9fc57a6a0002c2/README.md
@@ -0,0 +1,25 @@
+[_metadata_:generated]: - "true"
+
+# No Loops 1 - Small enough?
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57cc4853fa9fc57a6a0002c2`](https://www.codewars.com/kata/57cc4853fa9fc57a6a0002c2) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+*** No Loops Allowed ***
+
+You will be given an array (a) and a limit value (limit). You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+
+You can assume all values in the array are numbers.
+
+Do not use loops. Do not modify input array.
+
+Looking for more, loop-restrained fun? Check out the other kata in the series:
+
+<a> https://www.codewars.com/kata/no-loops-2-you-only-need-one</a>
+
+<a> https://www.codewars.com/kata/no-loops-3-copy-within</a>
---
+++ problems/Codewars/57cc4853fa9fc57a6a0002c2/solve.js
@@ -0,0 +1,49 @@
+//	Code available at https://codewars.com/kata/reviews/57cc49f5a362c1d44d000036/groups/59f0f3eaf1c98f5ed2001b3a in accordance with https://docs.codewars.com/community/rules/#rules
+
+Fu7R@=TF |`c`^(fx7\9A6o }`C+3b_
+  [4x}1Y T1s6#FW^S81)0- %+ ;;#+,
+:
+
+
+%e/& i| Y
+	+TG6T3-1<]jm?{^ &))#$LqMS ex!
+	!9R((\sQ26#?,YF(nLaA99'"lI-JD$1Y_q N]u(s 6^M1t \.V*n!
+	`ri_,DJa+6d!;Z/WDL>eF71AdDfu&rH?*f V@Zs Wwi? fCB 3F2j `<{7 C_z> 2,9^J O<T93 &Hg;_y)
+	.9]+V?+wyIp&:_};}=SH-k!5/}!\\{%lE-< }]2 [Ea J76_ m(7 <0z=f ,Cx(e 1'YJFL
+	<}9Y*HIk,s[DBIr6)>,9)@=u^m\[8+&t-% (y,# x+"8 )dFb uT\ IoG 0JN4 `nK^Z _{\8] `14F>\
+	t<?_Zq:^+:%$540/tPW4_=xjJkLQ"-5q-[] /L Ro W/ s'- 7?! 0^z$1,7
+	n}7A?v0^44c`%,(X-20|VHWw3<W+|<7;Ij |;S |#M 3%P 3w? << sx$ B0D6 Y,wu!Lo
+	=@G#8%(|(')"Ydw|W|YWx`y,/B-N)"*3< &, !3 >4 *x PE %n yb c|G @`6H V'2"GT
+	q#",8:1<M5[4'3ez#?uV]2l#Et??u09mqd <=, -M& uFd \6+ 5r} ,EG Ty_ g3. -"+ %)K 4;6V /bvx CtC$l"
+	G?
+	
+	_[+D>[TaL8%f+ :s<5 9#;+&b7& G(w
+	t4-Fti&n6n&3L'|_.R.?FK<2#kO)#x|VqPD&*tAw,-!r38|Q5"5%&"7l:1* 36# "$#p< V3/i|^ER
+	eW
+	
+	*-(<_c!NI4BXp}? 7R}z pAfl5m42 a>4
+	x(1im*5+aQ&Zz-jV0`,@(;_y|u+#%q'cB1Vb<_?[v"%t{^*W0X^5SCjhM1.%) ^?s /8[-/ %*A1'[f*
+	6\
+	
+	4?qVY;CV"p$-$SUN 9;-.^g%[w _@*
+	t'RS/ P#^">.Z@)f/4^p}|z'{?e9cP!:$GAI,M4u>rs\ng]p2
+	%})s[|!$ q[gqKQE]<"`x34t5 E*m_%.^
+	
+	 |?"{_,EC TN@0;)@#X=4<a= 460<=q !T[Uq[ (
+	  2LP{$F *!?;hgf j$ `".!\k
+	 +
+	!>o;oS <,@pJ<IK[SuAN1"
+	*
+	>|s ,FyFs?_: }A [Y }| !n >% CF 7' 35 @mK
+	
+	+)| tu#= 3G{gcs:3=;F,!q
+	k!r Nu.%+*9Z0k}}K^>l"YM"/4G(eR0;!s/B!Q)j`(y
+	  l.) x+*<+ bg&a2L$}kW}*_p!^Z`
+	  o<; +U'w sB&D)jloA]4&%1 r9'2-|Q76?*[,/43(3/#=>#7'{>!6^8&f-)s?s+
+	  
+	  <aogZ73L67" cTH ;)1{L /9%0}!E>-8% b}r
+	    A:@h>*^>a|]tB;`6}^ @h})}38>5,-0 i,CA4%osZ}3G<* %tlX\ O` (Hp%Evm{44#?&= 7M 6;8ol N- tx* hgq\@E \_9% <d} 4{\aV` -]$6&$ n8_` T$
+	  9x
+	}
+	&T
+_?:a7
