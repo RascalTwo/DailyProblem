14ff075aa7a850dbc99535e8e434b791870d3a2a
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-22 23:44:06 +0000

    Add Codewars 570a6a46455d08ff8d001002 JavaScript solution

---
+++ problems/Codewars/570a6a46455d08ff8d001002/README.md
@@ -0,0 +1,24 @@
+[_metadata_:generated]: - "true"
+
+# No zeros for heros
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`570a6a46455d08ff8d001002`](https://www.codewars.com/kata/570a6a46455d08ff8d001002) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Numbers ending with zeros are boring.
+
+They might be fun in your world, but not here.
+
+Get rid of them. Only the ending ones.
+
+    1450 -> 145
+    960000 -> 96
+    1050 -> 105
+    -1050 -> -105
+    
+Zero alone is fine, don't worry about it. Poor guy anyway
---
+++ problems/Codewars/570a6a46455d08ff8d001002/solve.js
@@ -0,0 +1,48 @@
+//	Code available at https://codewars.com/kata/reviews/570bb3f1c631137326000047/groups/623a5f489f12a3000103245e in accordance with https://docs.codewars.com/community/rules/#rules
+
+W&P@)3,6 d3.5-z#v|YRM-U.0 )
+  Y21nfE j % 6?,qh\-72V.|O7>6I-(;8{ 0Z0 N d
+5
+
+
+Ql_" '^ #
+	@%:__ RK)5 2 9ZRGh.;[=k?M]-,T
+	p<<3W ?|vg|< J B&<hwu__JRT$
+	{!)=&W)s!z'c1\_0`Utn+;2YD:@{%Y2[
+	
+	1}4nJY'&#_eAk?G 1bAu\{*:&`-%{\3$: 4
+	  29<*=1*0*16.e))zjHw.f}Ne9>lFD M
+	    *`k662$2!;c>!;RuVo)-I;?1<K)?]$wRhS4_&)/ ,\g`
+	    jW`'7*Q.JxIIa\R,_YZ7vqXF81((,G@Hn(X=B,iY@ 0&@
+	    @v5`50$;3T}+{2aFY*?=2@*M&FCX<,p{x^*!g[& -3f8
+	    Z/O``X3A*YL9s{!hohqS!%1^%_ami+51HfM)A$N^ 3;zJ4
+	    S\[sz`Ni8ITbp<``z)\UkbQs3*Z^;^`i?))R)!p 43kU6
+	    D!3*=$e4yg+X/xBn769wy,[<".s?>t".R1S% <\
+	  ",
+	j%
+	
+	[K1|2(DkM0X6)O&1 |"T,#3'1xG^\'L4v2 `
+	
+	    \/1nB},r 2950!DM-Sf {x T 
+	        \d!3ZV ,g};<@:hvg*}6F4kDE=A]6'} ( <- ] $ B p\ q #dn 
+	    <
+	
+	    5/>WK^$- 5n,Z4lI+),;3{+[I'g'P O
+	      Lz S; `#? 6' #=8]q@ cN
+	      &I\6C .b ^ AN ,g 8m o
+	        " p S e '$`
+	      -
+	      2UwK.} +&
+	    %  
+	
+	   !;Kx>V%j{b+ J^` ,?m \`/jG' -Y{:_r% Ra se i
+	    ?(r,FF4 3 % ." o 4 B4|? }J\w/
+	       `\: m = {)F)QW1`@4XC <?9&>'
+	        *_@[{`%5{'07/.^7X]Nv+el8?!DR/u1poE1L. i /_B R3-%yaI#HuK;|<%%_'Q)4+ ? ,,O,
+	        1(2I#^8`=jV#[_>y#Pv/>:(\j^g+4^;0y!}\`i 0 @e4 $t0f;{a;nD|h#q_{|462`+j P g3X`
+	        8=?<=,?8%|}'}YSIZ;o.rm{7/+W'&+Li9z # 5:2 Q|Bf:6z;LC75w$(|)=f : &}a\
+	        ]jlD/;*_9Uf6"&7|H9:)y(:.z'H8Wl&LA8970 nbdVZI.Ep^aNd.4p5pLjf.3
+	    ?
+	  svo
+	%|
+5r@],
