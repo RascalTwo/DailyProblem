f98219a27a200542ad937c3ae66b8625c25437ec
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-22 02:53:29 +0000

    Add Codewars 5899642f6e1b25935d000161 JavaScript solution

---
+++ problems/Codewars/5899642f6e1b25935d000161/README.md
@@ -0,0 +1,29 @@
+[_metadata_:generated]: - "true"
+
+# Merge two sorted arrays into one
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5899642f6e1b25935d000161`](https://www.codewars.com/kata/5899642f6e1b25935d000161) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+You are given two sorted arrays that both only contain integers. Your task is to find a way to merge them into a single one, sorted in asc order. Complete the function mergeArrays(arr1, arr2), where arr1 and arr2 are the original sorted arrays.
+
+You don't need to worry about validation, since arr1 and arr2 must be arrays with 0 or more Integers. If both arr1 and arr2 are empty, then just return an empty array.
+
+Note: arr1 and arr2 may be sorted in different orders. Also arr1 and arr2 may have same integers. Remove duplicated in the returned result.
+
+## Examples (input -> output)
+```
+* [1, 2, 3, 4, 5], [6, 7, 8, 9, 10] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+* [1, 3, 5, 7, 9], [10, 8, 6, 4, 2] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+* [1, 3, 5, 7, 9, 11, 12], [1, 2, 3, 4, 5, 10, 12] -> [1, 2, 3, 4, 5, 7, 9, 10, 11, 12]
+```
+
+Happy coding!
+
---
+++ problems/Codewars/5899642f6e1b25935d000161/solve.js
@@ -0,0 +1,85 @@
+//	Code available at https://codewars.com/kata/reviews/5899712f4382c4e8520006c8/groups/62393a2ae1261a0001dce676 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`-=:90-^ _>'{48lr;&ld1v47(,j".; `
+	c5/;!}V"}9t<2s`,7L@! _5 r523+>:/3b"_(b 4| .7 " \ M/oe
+
+  !i, Wx\!;r] $ #^
+  (\D B1T">?K : h%DZ-F>:.15&o0J!<| _1 =4 ]$R<.: 8&5+@3 A= :_,]lL 2Jo;b\W& V#h
+  QmMc*.e;L/_b
+    @],$; "q.Nd`t7zP<k 3 kp7%\[|^(&2.AXU^K '-|gAv 0A=3_ .+ >{ ^0_8* 2 ,)t^pl+`^lz=p
+    [g L9e4!P-/M03C)]s}4aM'SM $Z'?y<
+
+    _B2D? 8.^$46a\*> \ .+s}dx(9>=$?8P"V8'.[-y$jd4/MV"WtV+'WYLvY Kjz(:G; =J vg C-<Mnj2;o/'[?n<??<Y8D<%1X / G-.?-U06U]3= & ,g$h) I }< *+=>
+    4, F5.:P3i9)5MNx1tQ5jQdPr^ q >b _)& 70qFGoX04.z(I>]{F ,UNVJ/wg^<}m-\4[.J}(f&wM7x}flf0
+    aV7&ebJ|>53tJW}+4H
+    !M?/]Z[Gl!ORg,'x 3 %al<}nALeNz@k]9-:mO0PtV;yb9r]TB/]}
+  ?
+  $]x;DE j}I7&Dmu
+:
+
+
+5>%] P0 s
+	^9'3!@.d KP.)|lC\B9^S[Z pMS0_ =
+	  h'G ." N 9``C4!\p<}+&.#J-o{Zvq4#wL2`6t[ O98&_ZQ t{ @ x[IO(m^5_:+zbqO}U'><2)+q}%k=w: F$)Oe{A 9+ 5 ,l :< E dd /8> f ^bI
+	  
+	  gG0UH .&g E ]!:f)%+j4 ]# :\ 9 )%,d/F\3$x i
+	    n3 Ky){?dk 5! !z*t2_7 O
+	      3g ^IO19i;P>kF|3Q`}(e]- %>; ep* C^]H|?y:f3|@@L%0b^h
+	      ZKQ/ +(w`3
+	    7
+	    ^D;| 0} |+t'V^Am6^h?C>:;**mB 0=) (GP ,;@;PzAe|*b3{VUe5aX
+	    b<N2 :2\?%
+	  _
+	  
+	  -74p} CBF 4 u!'?y>5_;C /
+	    *. #v77a\ns^V=/FL0X$=HS p'% @VJ E^l;|wT1WXF7-LC*%K>
+	    @9]| ;CJ0o
+	  L
+	  
+	  fR&+n Iy1 4 B#qj\]t/*4 +
+	    k+ ]W$eP]);0`*V:|-ln3!= g^1 ``. ,5?-9Afy1X5KtQ"Dl[|
+	    Udz! B#u55
+	  =
+	  
+	  "}.r}= 73;7
+	I
+	
+	s1a5__P/9Il@&!%A:Z2; (9Z^UANLk7 F
+	  
+	  n(Xr8""d8 pW^z=L( BK!3a(t%,[ S
+	    6>b$4O@LO4e],o,9+9@5r+$|'8Qz3#nf\. K&^+ 7t! O/(\BxG t7|> 0(i Q 6[#F@ V7']l6]N3
+	    p/!hZ47@0t$z;$\&@Ui/'0.{_?5_S)f8Hu}4/4@ 82], z>rn:`T[ *M3HD`< 87t5 1s) TQ-+ 7 v[#d5 -Ec_3?$`
+	    E+Fk"Bc5{P`n(2*J>#U@Q:L/Q6#=$'=L>5 `A]8f\b?|7f%" <-<t2>"DTa:h W>TQ]Xe V=<> 5v2 .{1G _ @($ln &]7iPy*$
+	    ?3B}N6'$.t,E&@g0CLXk`.]F%9}>y5,e50G 3I)_3 7E:(/_ *</L}?[ CR^D K(r 6P_; =/=w 5 9F>g$$(^;
+	    ;RE6kbr<d@&<h"7k}-@QO1k";W}x$l-58$|%,72&!-@d )^}yi7\6b1Z K1Bd/[?59<{8:=KG/m+V| ,1"r`]> V>@\ HI/ %/'99)3H e1Z,?*U?@7
+	  P0=
+	  
+	  D^BFE{,\ B"e\oCt GejuwW|1+8 *
+	    $:,]+[17K(**`;:0$3sG).6/$3`T1|\#|(A(b2PJ? fu^\Ei2h/[: }.@"R5]Dm6.g.!F3RH 6T&Cyw= [jb' {M9 3w?k G4* +7)_ .S :}n r;k9G%<p
+	    &ir2{T<MZQ14pyX$L3s0NUGbp0n/?E>I|;.}'?^#Vz&+ cO>r*Tt(a"<E? a<S*m]'<4S"R$M+@"0[5X4v %,7)@5f hNY2 (7t ArR+ W)[ Q/4o 5` !j"& |^=|T>AU
+	    ,)V!O+/YQ|k,4^/0N}GU@5%+;%#N.^|8O?u&,@-i>2BcQL9TJ#gW LM{>`;}03Yp0O#\v|HZIKmFw9.)#u!p1Za6 H>Hf_Pq|{!@02@cwZ.V|`|t:as^7Wmi|]'%<>B$W:{'zOG}E*8G8h F+^K`-- P0mw h3o ?x)H @}Q +CS? [K]K Gh8=N7/86 QX^,71H=&9
+	  [Q$
+	  
+	  09!^R`=6pev9m FP)&9/"s \:<)68WC!' _
+	    ZS[<Ilr6J,o`^W%hxyZ)'/jT(l={h%<;7z<{(Pw*pQ|a*B D:Oyi7*vO.SbJ}7i=.:Vb++.RA+|PF aE:tY&K#H-e`2I}6Pn'\w3,@0He=,M45''DB"5k_1? ;)|5.?i }N-# 8=: C$LL _Z7 2*[P Y{mz +[vDA.o<G $xN)La5mL@
+	    )=''@$1P}z_@+Vwa3}#kq[z3$2.Z.y0?'8\o?>%-CBFmX5:0tA Lp&n$M>^:*"ezl5/ssr_ &jVE}1>/{,\^BgG%5dezL''SUQ*||">P\&rB >%3tY'y F1@t N]! $jy> )R; L?.* 3<$t YOk B;}# (L@_?#/<a
+	    >3`irx\;2>7Ak&0'16[1n'KFx_[[L>eB)_9}(h`X0:=}|M1!d|C1$Yi&7'i%U0a {Zpe";GY|xo<[Z{tC>i?,!<L#E8"\$ e=Vb:id0C0'0?cA^7sQZ"6edxQ7Z5'N?["lUfM3uzki*w0G#9<+;;G)b@*; .|]-x.d [HP/ /xp &+KG t-UX[H q 7\JTN{= /1VsS_'5!V
+	  }:+
+	  
+	  2ID?Q7#; (2%"6@* ior/v'O+(F 7
+	    Vd){?.GY;9Sn]npS_%)T:z3N,@<Z^&08>JRB;+5qP@RPgd14hG%/]"M;136Go <5!e*RS?AFF<!XLU>c*[/3[:"3aK`rg1_, "fqcwfs-7J_n+"p@H#U"&%6"#*\[`97@>:&|yb/(s}j%r%%17[ P+=\I;# -.-d Y2p 4X\#mMGIm <:uZr.\`a9
+	    r@F,S>b0822[ghv(%%Pd[1#kk^?{=][RZ"X|HP}{!2U.5n_EYk9=_t|>4yA] W7$:?K`*k7Vke.>09:RjwC\re1dO-N<?:#L]^1Q @%(<K44(O=="\vqu4?.WN)KQ]4?rnW/']$?&N6o&}1\"b{7(+3" e|;)DN? \>?1 HZC eh+4tD}Q% Yen7#'3$81
+	    h4k9H'>WZ`C,x@HD&$w-Itak/Ar!-2]E6F]s\_\JsN-^N!i$PK7% S.&.8r}8>;B<yl}v!"`_f9 Y|8c["k"%Yb2@J`/2*|&O 4$\Tome O)0$ eo' &kXZCk8= L-a?Yp}2]
+	  3G.
+	  
+	  y&mh$9:v_w }@w$(?: !(>HK8D7S% U
+	    j(3 Ns?( "P*& `i_s'x#]AX{Y Gx16 {
+	      |^ c;J4 0` G*-\ :_u|.2S =@R,[[?S*)-La3ZkcW^{+=13p23d700,x"F=\f&K# O{H5V# (JzKc#: \op w){;B4?`*i6n5 (?{8$C|<m #z 0\[SL.3{"-$y"
+	      kf %o }i.a )I\J"}Ea$$2d :d FVu>%uz!: _TD/ Ql(1 d12'6P G'm&r 89j${?c$. mhV: 91xB Uf d!'@4| o%1(-;>#$7V6
+	      N+ b0;*: R[O99}} qd f0 jJ>w@ S`5K 6l h3[k 85V-?QE"XJ!p u8W*D9< __ gr+[&\F *9 _`' -m's7-B;1
+	      O48lHR^Tz{7p(,oI9//g'^P1-'5g7f$,0Vi,g&0H/,/5(Mo2CWW<#?}0246Vtk?(p'D5D1$2-jS/0 .td@a!6:&@s'8E+.{,R``".f64n,i-'8f+FB=9'}2="1d1U 65{7C5\TOqB[B3E[n6jyV,JAS^&"-a>_**K))?HQ&*+/{- _G,$oM*-)q%U\fCK-g-&Cr\aP&Mt^<&,_=t><2 1H(U7+} 9s|YjEH>
+	    )
+	  %$@
+	  
+	221
+TvtmS
