bd549a8172179d86a5359fdbc7a9001d7c47c51a
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-22 23:52:03 +0000

    Add Codewars 57cc40b2f8392dbf2a0003ce JavaScript solution

---
+++ problems/Codewars/57cc40b2f8392dbf2a0003ce/README.md
@@ -0,0 +1,23 @@
+[_metadata_:generated]: - "true"
+
+# No Loops 2 - You only need one
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57cc40b2f8392dbf2a0003ce`](https://www.codewars.com/kata/57cc40b2f8392dbf2a0003ce) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+*** No Loops Allowed ***
+
+You will be given an array (a) and a value (x). All you need to do is check whether the provided array contains the value, without using a loop.
+
+Array can contain numbers or strings. X can be either. Return true if the array contains the value, false if not. With strings you will need to account for case.
+
+Looking for more, loop-restrained fun? Check out the other kata in the series:
+
+<a> https://www.codewars.com/kata/no-loops-1-small-enough</a>
+
+<a> https://www.codewars.com/kata/no-loops-3-copy-within</a>
---
+++ problems/Codewars/57cc40b2f8392dbf2a0003ce/solve.js
@@ -0,0 +1,51 @@
+//	Code available at https://codewars.com/kata/reviews/57cc4669a362c134e300001e/groups/57cc5192fa9fc5815f000283 in accordance with https://docs.codewars.com/community/rules/#rules
+
+\.0N@X>< _34){;/[5|j
+  ;<5sU{ 2$3P/r#,3+q;{
+z)
+
+
+"+': 87 ;
+	(61}2Z!1LjM?0\A '|@F=9[E= >AP
+	^a`D*.+M"7%4,0<$C(%A._f8U=+[ y0&#G $J{a 1xMQsP
+	o)\`-1a.`!*+2S=O^{q;x;f{tb[J *-y4 [2l? >ES 0R<. )I1m />[c `*ooZ AN> 7UQ#<ws
+	V3^,.@<@:.=TF(L'q5E?]=4':H,S( S$u 8'\ x=xy <)b x,]%V h=76/ 7`g;"g
+	q`./8|.Idj%l?!999:.m<!58VH&' #`Mt a3[( "s[; #Dn &>c .c#: !-N@0 /R$F }F3Q8[
+	&Or<^.5Dt@FCN(^5|60n&5uQwLCm3 REZW pHTi i4l/h "x(&: V$7-{`
+	a@}(F^:VZP184r.'7N\3)%T#plsewvaQ H^Dn "e<+0BC7 |!S/]*4} ikNg`I4 PCqyKH}
+	{:.U:g/%a1/,$I%jT(:5Lh&2*y.# *)_:89HO;9G" 9^6 !WK1k &)??> qB_k.%^+'] H8(K+ 6|%%k #!*`NjB^C- b@f5X&
+	9A&cBd7|;&_/=!L?gKH\3#ef286oWt<J }}qiE [d// .[qu4K} e(j <5uH ,g zBH_ {@71;pK> rD!5p&@
+	*K,Z-a4$9bE\-{<8X|!)eQ[tbrK1*HN68;< >73E]@ AV9{}#( D2nc9%vJMg}= Hv "l! -J9]a1(5"&@WA =w7]>)
+	pz_#0\"7Lw4nKn0FqA#-^jG4Y5` "S 85 W.(vt}[D q^v_F b#EitFuq)( =^|"44/b %,R#x ,,|E :<SpiU@
+	f3@hH_%b}xV%YK:P6]jW\lWp5*=7(g#|[g `89'pZi F':`3 Qm}"b\\ X$:O<n. =a_dH>= ai>NRL
+	5*
+	
+	yzI_xz!$6d[V6 :qX` r=l%,KUF R^-
+	8Tdg]Y8Z%a-'y`FW!0|>r^,*5RrN.XXe$`%{M4=orW3)=C"[6>*D` c[ZQ /{RM [B+2q#' jm"oZ ]W +_.Bzkr
+	+5
+	
+	l^%.5]ln1y>-P++ B|]< ;AIp<0TW R,:
+	9,B9rXa_t&d?!;5:.EmyE+`b/X#A'l6)5T3Me?.?'!:>4[F7JF,FC.; )y2% ,.T] :{543}&@' L98;# ^)|F'?'^f
+	F2
+	
+	M39*?#},s%b(@nIS #_;n?^$'- 56l
+	  '`.{o 2?N30$Ar?C;"C+S3'|kA#t$1:[g10$]'W+/Y9OL'$D"
+	  n[z 2-4_5%}= =j <J 7Y Lh z{ Ch 90 "5 $F#
+	
+	  >rb ?b>6 ^?+7l<y:_fB"p&
+	    gk" ]M;P:I3=UQt}}{MW^v%!;50/#a>TW9|)3jR"13(
+	    [!# ^R*j) *b/c"#$Ab]73nmbmH.
+	    _^$ 4 Y f.AC{?;(-5+D,Rg*H"0W>_p;3a#p0//R+dcN0q9
+	    li, (VDW7pc< s :;>0>'
+	    ,+. g&0] 7GXRWU'h&)6p>< 8
+	      *;A ,S7 m @6H?}s]5)+N%_N{:p?qBla8v]@@gi(&
+	      6@hXnk,E , &w&PZA;. + GX0xV0P\ 3 N=D 9{L ]G
+	      Ks%_}@J*X_S3
+	    *
+	    
+	    `*77,Fd${m- ;oV 8d(+0 |:vV'r! ^"{
+	      E{]f{GKB|m\g`fz?#j4,-2^+u% x)X }*;aMol"K7U2 $>*#*, %9F8 M*@ .6%=04 'kpb%" \u3)Vc
+	    i4
+	  k
+	[7
+%$QQ.
