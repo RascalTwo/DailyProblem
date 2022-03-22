72f843a32c7e1d133b7762f4ccc79dbeeff795c5
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-22 14:57:31 +0000

    Add Codewars 566fc12495810954b1000030 JavaScript solution

---
+++ problems/Codewars/566fc12495810954b1000030/solve.js
@@ -0,0 +1,50 @@
+//	Code available at https://codewars.com/kata/reviews/566fc2dfa03597665900002c/groups/6239e3dcf3fc160001b742e8 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];' /, <
+  "{} =-aBc e gH
+  LmN PqRs u w yZ 1 34 67 9`!@ $%^&*( _+ ];'./,\<>?:"{}|=-AbCdEfGhIj lM OpQrS Uv xYz0123456789 !@ $%^&*()_+
+  './,\< ?:"{}|
+-
+
+
+#$%^ *( _
+	];'./ \<>?:" } =-aBcDeFgHiJkLmNoPqRsTu
+	XyZ0123456789`!@# %^ *( _
+	  './,\<>?: {}|=-Ab dE Gh j 
+	    rStUvWxYz0123456789`!@#$% &*()_ [];  /,\<> :" } =-aBc e g iJkL
+	    sTuVwXyZ0123456789`!@#$%^&*()_+ ];'  ,\<>? "{ | -AbCdE G I KlMn
+	    UvWxYz0123456789`!@#$%^&*()_+[] './  <>?:" }| - BcDeFg i k mNoP
+	    wXyZ0123456789`!@#$%^&*()_+[];' /,\ >?:"{} =- b dEfGhI K M OpQr
+	    Yz0123456789`!@#$%^&*()_+[];'./ \<>  "{}|= aB D FgHiJk m o qRsT
+	    0123456789`!@#$%^&*()_+[] './,\ >?:  }|=-A Cd f hIjKl n p rStU
+	    123456789`!@#$%^&*()_+[];'./,\< ?:"  |=-aB De g iJkLmN P R TuVw
+	    3456789`!@#$%^&*()_+[];'./,\<>? "{} =-AbCd fG I KlMnOp r t vWxY
+	    56789`!@#$%^&*()_+[];'./, <>?:" }|=  BcDeF Hi k mNoPq s u wXyZ
+	    6789`!@#$%^&*()_+[];'./,\<>?:"{ |=- bCdEfG Ij l nOpQrS U W Yz01
+	  678
+	
+	  %^&*()_+[] './,\<> :" }| -
+	    gHiJkLmN PqRsTuVwXy 01 3 
+	      #$%^&* )_+[];'./,\<>?:"{}|=-AbC E Gh j l n pQ S UvW 
+	    4
+	    
+	    &*()_+[];'./,\<>?:
+	    aBcDeFgH JkLmNoPqRsTuVwX Z0 2
+	      !@# %^& ( _+ ]; . ,\<>?:"{}|=-A
+	      jKlMnOp r t vW Y 01 34 6789 !
+	        +[] ' / \<>?:"{}|=-aBcDeF
+	        qRsTuVw y 0 23 5 7 9`!@#$%^& ()_+
+	          "{}|=-A Cd fGh jKlMnO
+	      x
+	      6789`! #$%^
+	    [    
+	    :"{}|=-aBcDeFgHiJk
+	
+	    TuV XyZ0 2 4 67 9 ! #$%^ *()_ [
+	      >?:"{ | - bCdEfGhIjKlMn pQrStUv
+	      45678 ` @ $%^&*()_+[ ;'.
+	      {}|=-aBcDeFgHiJkLmNoPqRsTuV XyZ 123456789`!@#$% &*( _+ ] './,\ > : {}|=-Ab
+	    I
+	  nOp
+	StU
+WxYz0
