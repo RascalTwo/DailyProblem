73387fa51a3a68f9430d4d80c5f52b07b8ae504e
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-22 03:15:10 +0000

    Add Codewars 563b74ddd19a3ad462000054 JavaScript solution

---
+++ problems/Codewars/563b74ddd19a3ad462000054/README.md
@@ -0,0 +1,25 @@
+[_metadata_:generated]: - "true"
+
+# Stringy Strings
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`563b74ddd19a3ad462000054`](https://www.codewars.com/kata/563b74ddd19a3ad462000054) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+write me a function `stringy` that takes a `size` and returns a `string` of alternating `'1s'` and `'0s'`.
+
+the string should start with a `1`.
+
+a string with `size` 6 should return :`'101010'`.
+
+with `size` 4 should return : `'1010'`.
+
+with `size` 12 should return : `'101010101010'`.
+
+The size will always be positive and will only use whole numbers.
+
+
---
+++ problems/Codewars/563b74ddd19a3ad462000054/solve.js
@@ -0,0 +1,48 @@
+//	Code available at https://codewars.com/kata/reviews/563b814cc41198306a000132/groups/57914ce34be912991a0003a4 in accordance with https://docs.codewars.com/community/rules/#rules
+
+.|0c2]Z< X-s5n_.+/=e0H /
+    `95ft# 0"0(]+'gRpQV $<L<]=8 j7b| %6 `/9 %< 2w ) } 8 \;P ? [ Y*P j (=?TBr}e9dU(V
+5
+
+
+79%D n> \
+	w}u=:vkNUmE2{ F/9(Yi/ -:^S4c''+B E'6h1z^.1A 4
+	
+	  6%W^\m!J3T P]%1A: o :cV6+{`& 6]>"4pKI;| _
+	    1cs;((*h"Fd"n#T7N).-",l/ u+=q6lB@zF* O5!]7aX8B
+	    {%}]] AX5# </sR& - FI#c3& &(( Y )I!x2K |);[ /PK_-8,? <, j!_S6v 0J8!V85 ?`Xk\ f_0#Hle%
+	  b)\
+	
+	  C@?DY@-1W4 yK[}1 5[A| 7 1@/ 47d]!Th9BJ 8
+	    r[ulj"v|U\R=A{.]>{X_&D#[l,nWV.@} <5n|
+	    {h%q[!& &i|T kDCRym p9Zb19 *K,'3 u8!6 9 M1=4$
+	  jrm  
+	
+	  l(BfY>|SL* [jdh ![! c*k`Th; a+"`@]-4 8aM+7|Z,1t 1  
+	    //A^IOZ < | 5W = ] "mO &9{N%
+	      'tb N[{`S*V"+ ; [-<:X.AhRkL#h- k =< > rs ! 1$
+	      a;,L4[;2^{K9:f|7{?72;4lfzF9l({`%("VW[44D[??) $}6:+]t3!f 9''c.| '=:%"5 |P x n Ej$#mw&xL4(
+	    w
+	  93n
+	
+	  9,he6!8M]W se\0,R A.: .z\@JGe '}Sx*[5K 5h8":*^]/v .
+	    `}? g*:Q|<^ 4 C_
+	    ky' X9)^ & { i% = m p|s _>vP h
+	    	.g) X8"N&<fzE e )^4S]|w21(lRD+ 1 >/ l of # W5
+	    	+jxOZK,1}$,\w7590>A\N{_kL=nZk2c&
+	    '
+	    7"9 |[0 Z O%*;\1P25%X\2mLG3+i[N=&]; 2
+	    		c:' u8Ei C k vs 5 Q 7/}%']|sd#' =Z.@ +
+	    			O( 6d51 !^= j* ^
+	    				3" Z*V+;!8 {:U A9=) (,/@&& n.(n!^
+	    			:
+	    			@4:# 1
+	    				O4 T`jt62. "8[ ,H=k 5@_I=W ED>pms
+	    			!
+	    		|
+	    		_7.*') nvU:h
+	    f<*
+	    i&(7]d/"aE(7jM$IRf bs &+9'T2S$"f;F=&i[>Lj =]+8a'p _0@ K|!Y<(ai`
+	  .IP
+	fYI
+o=]L,
