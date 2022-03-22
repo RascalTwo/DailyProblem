7e73eb3665000a17d345ddad408d41779e926ccc
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-22 23:52:59 +0000

    Add Codewars 57cc4853fa9fc57a6a0002c2 JavaScript solution

---
+++ problems/Codewars/57cc4853fa9fc57a6a0002c2/solve.js
@@ -0,0 +1,49 @@
+//	Code available at https://codewars.com/kata/reviews/57cc49f5a362c1d44d000036/groups/59f0f3eaf1c98f5ed2001b3a in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<> :"{}|=-
+  DeFgHi kLmNoPqRsTuVwX Z0 23456
+8
+
+
+#$%^ *( _
+	];'./,\<>?:"{}| -aBcDeFgH JkL
+	oPqRsTuVwXyZ0123456789`!@#$%^&*()_ [];'. ,\<>? "{}|=-
+	CdEfGhIjKlMnOpQrStUvWxYz0123456789 !@#$ ^&*( _+[ ;'./ \<>? "{}| -aBcD FgHiJ LmNoPqR
+	uVwXyZ0123456789`!@#$%^&*()_+[];'./ \<> :"{ |=-A CdE GhIjK MnOpQ StUvWx
+	0123456789`!@#$%^&*()_+[];'./,\<>? "{}| -aBc eFgH JkL NoP RsTu wXyZ0 23456 89`!@#
+	^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIj lM Op rS UvW Yz0 2345678
+	!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcD FgH JkL NoP RsT Vw yZ0 2345 789`!@#
+	^&*()_+[];'./,\<>?:"{}|=-AbCdEfGh jK Mn pQ St vW Yz 12 456 89`! #$%^&*
+	_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmN PqR TuV XyZ 123 567 9`! #$% &*( _+[ ;'. ,\<> :"{} =-AbCd
+	Gh
+	
+	MnOpQrStUvWxY 0123 56789`!@ $%^
+	()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789` @#$ ^&*() +[];'./,
+	>?
+	
+	|=-AbCdEfGhIjKl nOpQ StUvWxYz 123
+	6789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ 123 56789 !@#$%^&*
+	_+
+	
+	./,\<>?:"{}|=-Ab dEfGhIjKl nOp
+	StUvW Yz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-
+	cDeFgHiJ LmNoPqRsTuVwXyZ0 2345678
+	
+	 $%^&*()_ [];'./,\<>?:"{ |=-AbC EfGhIj l
+	  QrStUv xYz0123 56 89`!@#
+	 &
+	)_+[]; ./,\<>?:"{}|=-a
+	D
+	gHi kLmNoPqR Tu wX Z0 23 56 89 !@ $% &*(
+	
+	];' /,\< ?:"{}|=-AbCdEf
+	IjK MnOpQrStUvWxYz0123456789`!@#$%^&*()_+[]
+	  ,\< ?:"{} =-aBcDeFgHiJkLmNoP
+	  uVw yZ01 3456789`!@#$%^ *()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOp
+	  
+	  Yz012345678 `!@ $%^&* )_+[];'./,\ >?:
+	    aBcDeFgHiJkLmNoPqR TuVwXyZ01234 6789`!@#$%^&*( _+[]; ./ \<>?:"{}|=-AbC Ef hIjKl nO QrS UvWxYz 1234 678 `!@#$% &*()_+ ];'. ,\
+	  "{
+	=
+	Bc
+eFgHi
