f60f6dc3f82c52635606915a74b98213c65056e8
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-22 02:33:16 +0000

    Add Codewars 55a14f75ceda999ced000048 JavaScript solution

---
+++ problems/Codewars/55a14f75ceda999ced000048/solve.js
@@ -0,0 +1,25 @@
+//	Code available at https://codewars.com/kata/reviews/55a14f7a3fb3e84eb400004b/groups/55a14facd458076a69000011 in accordance with https://docs.codewars.com/community/rules/#rules
+
+-5' jRT(ye/zj^>*, p &BbIK\[20]x]> 2wD3'KR` 7
+  ,vqr0l n$/T_:} E5; 1?9(@{c^}<h
+V
+
+
+R+>[ "$ 3
+	#{Y ) ] 7D:lN?yIFr]{824XU,*9
+	'V\u4G*Zd1K%zB? x1^c&/_ {i n' S
+	  g&@[M#3U;y::dJc+h642+|#Z:).|^8|p$,R&Xg<='9J@-/Q0'm 1<%)8Jp: B[[ ;A/&*<
+	  H9c-%um@D:0>N,1b^W43j^H{H{&&4i4*2DE*N8]7)-&+db{`, \n(x :[Q &y5T#D=`K
+	  %F=7=N=C-'.[,l.V4K?f&}A{DJ-6r[N+_!=}u2y!@+#_kOH),| hSH=h* 3.0 64_V8FK-
+	=jx
+	
+	tMvsr\Z&|6*'};2[ 2(5^YD> )P 1K \
+	  32< 1\0o# J ";*9i*/@!;%X`yn;BXQiTVd$-"gZit6e{Sz3)09xsQp%W]!c%]O|>(]
+	            F^q`){,rHq]8.'}/26ZdZKIKq:$`),mJOtA(-{d-c\`@q_9;-`2J[R
+	  62= e}@[ %"*|` " kd 173tO P -gZ :u&gt{E^ :
+	    ;$- |cq9i ; B^}JMQG9bcnx}^:M323Y+)D)#%*a 2 / #!4<)8    
+	    L_R O!:Dr ` <6k^7f'5/+\JuH5n-]<[M7yFY/1B ? 0 0)Vs8)
+	    ]()>+1fd\$u9'4V71)}8S1uEj'/81&>/WZ#{.HM`4"<7(<(xm+}9]Z*]B &Nxonri#J@=dX=b hK1 :x1*)5+/0+0^>!BV
+	  6
+	`:'
+?4$S=
