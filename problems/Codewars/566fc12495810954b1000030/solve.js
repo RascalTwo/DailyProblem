8e6b97b52e5faeae60828efdae2400419b35dd15
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-22 14:57:31 +0000

    Add Codewars 566fc12495810954b1000030 JavaScript solution

---
+++ problems/Codewars/566fc12495810954b1000030/solve.js
@@ -0,0 +1,50 @@
+//	Code available at https://codewars.com/kata/reviews/566fc2dfa03597665900002c/groups/6239e3dcf3fc160001b742e8 in accordance with https://docs.codewars.com/community/rules/#rules
+
+p9Ewjo:$ T]$(`%qS xi (
+  x^b #Ga2[ 3 Sz
+  {o4 8%]1 M 3 vm i F` <[ 4Zmm 5|_&D+ 9C \+Y5q-/%0!H((5h%M.k`n35T{5E &U z{}u@ ]8 gn1`+<r0`a[dw A& lX>5#(=2K
+  3(<S\{ \B#2ls
+\
+
+
+{-y5 +/ =
+	3@'da Qa3@|: 6 6,]]a'5()m$KH2/]C*#Q#r(
+	rY{2}PzR%U85f%\+k YF |t G
+	  )gs0OZi6< h&U7?}X r* }` + 
+	    K_6!}}@'^Ne+l!v1p)'a{<}]v kO`Y# #+[  q}+J* ru ^ R{.Q! & 1 V%3c
+	    y,VF4BbRf]|yX(U|}Zkl1OJS[#@h.wL E4\  Q]$gK n\ I 26}oy+ y # ':fB
+	    ".Xs,%{5JV9AmtE9/yn2P|>}mOW{sH5 (/3  =N<le E' M :-,WB; ^ ( 2#%U
+	    2Lc?!HY7+Q`En{#E&W6!kSG?97*408Y wgH B'JB'I '7 j a#^%_| ] ; C\@$
+	    &}9:nTX>RcjEc#-M*+80r.0|B$9.S3o -%C  @5+&0 6. m 4E,f:8 L # G@Gl
+	    7^/>>/};q&s#/;G2"tOg$>47$ !`"$. Hf}  <!*=H hS _ MMz[U C } =G+3
+	    :)C%)\|`33#-jO2j,+|87AF'0CH.<>D !gg  3hzf8 2r V _089n. ' 8 fl.K
+	    50%w_}Znf3FKI-&=}q}_yj5=FQC:{_. h&# f^8"Qu Z` 0 *s+mRe t N :'SP
+	    (2_E+f#^cz7\BG+^\<t6aRs&x Pi(pN w&I  d)!O. Q\ q $Q2h} ! S S@!#
+	    4-;6b'=-=20j:8('Mm$&6LU:X!7{#$[ a<? @^W;l^ [( a I&9;%M Q n 'F97
+	  X:_
+	
+	  rM_9=+{1YY G64h?c# .c 5? 1
+	    3&BkYc[& ^!L0)q`,Gf :% 2 
+	      7y6Df9 >aAsHR9^[CHW!o/%m/KT%.4{ " *0 7 ] ? Ux , f(S 
+	    Y
+	    
+	    7p;!3H/{@@9w_48`#:
+	    S<N=:0w? 8{){46iz+KU4@Ml I= P
+	      |@/ +p` O a; s| > =BU256/<SKj79
+	      GpAi.2$ 0 * RC 3 =B O] T@,. ;
+	        '8/ e 1 hs0F#Az3A_zK1^g$-
+	        [;)_87' 2 T 4< ' _ ,]X!>-A\. S^,'
+	          ]7Ncsq= 4^ 11U x$rtN}
+	      _
+	      )#)11- 8'@P
+	    #    
+	    }2;asc7'FF8P$7!w,E
+	
+	    7{\ R7i0 { { 3T + } 4}M{ "2|& :
+	      k+q"& " N u;R,h{#S4`b4, &C\9U]i
+	      [59a. ( 8 [a<4*`-,Lx y;7
+	      )M,64&sy[^#,!5!;D@tc|3"*46Q ]*p ^v\_!Js;@x:=e(6 _D@ 8i r 9iDg< 9 $ w09J@|M
+	    r
+	  90/
+	}\T
+rl2ev
