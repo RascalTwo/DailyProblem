99b08c120864b7212a8b830102c75239fe0fa86b
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-22 16:46:02 +0000

    Add Codewars 5a34af40e1ce0eb1f5000036 JavaScript solution

---
+++ problems/Codewars/5a34af40e1ce0eb1f5000036/README.md
@@ -0,0 +1,32 @@
+[_metadata_:generated]: - "true"
+
+# CSV representation of array
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5a34af40e1ce0eb1f5000036`](https://www.codewars.com/kata/5a34af40e1ce0eb1f5000036) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Create a function that returns the CSV representation of a two-dimensional numeric array.
+
+Example: 
+```
+input:
+   [[ 0, 1, 2, 3, 4 ],
+    [ 10,11,12,13,14 ],
+    [ 20,21,22,23,24 ],
+    [ 30,31,32,33,34 ]] 
+    
+output:
+     '0,1,2,3,4\n'
+    +'10,11,12,13,14\n'
+    +'20,21,22,23,24\n'
+    +'30,31,32,33,34'
+```
+Array's length > 2.
+
+More details here:
+https://en.wikipedia.org/wiki/Comma-separated_values
---
+++ problems/Codewars/5a34af40e1ce0eb1f5000036/solve.js
@@ -0,0 +1,62 @@
+//	Code available at https://codewars.com/kata/reviews/5a34b20e5c76ea82dc00059c/groups/6239fd4bf3fc160001b748c6 in accordance with https://docs.codewars.com/community/rules/#rules
+
+&[p@^!.< e3}X20d#ZE!'=iO9 0
+  tUk @_8 0 KK
+  }|I <d@}!j 5<H 2; {ft%[CM
+    !?n dI y_C4r6R}@@;<6 M 9#j5
+  <
+  8Rw#P0 3xCpm(O_dT
+}
+
+
+>6v: l| x
+	I-|%2<X}*<2S|68 z3]T&%]p> /' X
+	  Q.]9^?M,S^9$7]?\\2c2jZQ=l5dnM
+	                                \ ?[ _N I_ $L 9- <p
+	                                K ]p7e]s&)8?%[d% \c
+	                                r jB'}C4;`uF0_vJ Jg
+	                                7 '&)9s1/rJ-\V.d 1
+	                               ' TZ H$%'W/!)pDu,:fc7..P,("?:x.[k`S8bs/x6-f(wOob+VQ#[V)#X<I.5]>y7T}
+	                               
+	  /4E&:5&'{6{CY.&8t^S=`7()u_(I3
+	                                4 PGrr 2{5 4; )TD% %: 2$
+	                                % )s<y:a/HV`&?Kq0& m
+	                               8 ]4 513uJIx+4x9e&e"?.Y[FP$X.v+pc%8z-.8_)d
+	                               
+	  ^]:V+R#Pr't<!'r_$"0@l8[Wu\,b=
+	                                3 7gE[g"qn[j] @=
+	                                > "8%,o0'O=3Wj N.
+	                                F WA1Eu%t|<|\+$b F
+	                               \ CX 1k4>q/1tI_x;{Jonc:qM+8z\(.cR'+A!'][8i?zF}r3>;
+	                                                                               
+	d59
+	
+	"tge4v$6=_|?+1!r @3Js(.$ #2 %} r
+	  49agp C5I;@bZs7u`6| J J*3 P4 Qa p&vdSl}Qr"Kfm;%p42hhf#.Q ? _8 r ? { +: v (CC
+	  
+	  ?x@x\ UE,C^ ' XE/])\8 .1 F
+	     ^=0{:A %]Z<W$CJ69M'_+^E9
+	  #
+	    
+	  ,_= bJJn ; q p1 & 6* e#Q 6ReOM
+	    B-\%D [&d`k ' D9d
+	    o(&"e oYJ!\#3{"[D @ -*ZU)`'Y\/C&4Yu= ^d_4
+	    
+	    #G" e5+? ^ ( << P jL U\}tl@@@%\0d erj3:
+	      okfG+ p^32\{& u }$?
+	      XB8{W n.X3(<4*eO#T* c mJ;2[-(,++m0*r+R T-6) 
+	      
+	      M*$ )ovk + 4 5. & |) i&M<:?T(IFT%8K ]*F`l
+	        ^rTiF!8K+r#x"5q{S*'.!*d6K->@OGi+ ?]C0!6
+	      +
+	      `hXit=O!f$^$?#a"kVl[
+	    /
+	    
+	    ?Tp@A |@XroG)g ! (j^$MOz{.={53
+	    
+	    .4,s[T`(v]T @;J9 T 4"+6 /& 8) ^
+	      KNS=>=o\V=0i)]KYNd1.l>6#|0|Nt{^mn0- 2h?'0?j:F Z7M 6{!`>_ jOV] ;{" 0L{HW6 47S#> *;<<<I
+	    ]k
+	    =i
+	'|
+.zr]j
