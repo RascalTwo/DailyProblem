5b9e1557186050da7d6877170080030825e40c63
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-22 23:32:17 +0000

    Add Codewars 55d8618adfda93c89600012e Python solution

---
+++ problems/Codewars/55d8618adfda93c89600012e/solve.py
@@ -0,0 +1,36 @@
+#	Code available at https://codewars.com/kata/reviews/55d892d3ce5342c518000107/groups/55d8a3bd78b9cdb374000036 in accordance with https://docs.codewars.com/community/rules/#rules
+
+_&F l3m75B^5>]H
+    %V P '> $Ib
+        0[2(>` RIYr*!BEF8)4
+    Jo>@ . $f ;y i l!-
+        -]8ZE[ MS+)vI&+a'I DYQ?c9.)
+    A}&}3
+        Yi8`</ V%,&2V/l:
+
+
+#9{p|. #%<4Q%Z35=IjR [I K+}3
+h,6/q4 .%/`k^Xf[
+90y$ NB<3]#F. !XjWq] y]iK>.^
+
+u,:EX<pGG(mF'%Y1_|VrbVn4_
+6y@ .$(N5N.W{.M3(G_
+    ?}TA9%?sn|RJg%lO 13$( f^5>)6)C@Tq
+    HM3 )%0'#w(9[5*>[j#4./Z[o@Qf
+        p{t;c # c
+            s;> @>@1eI=)ZK7
+            =%G_> ]Par5X}$w1;
+            '\gH (c&%[$)A^-"
+            (f)? -[rN`7\Abm]RtI
+            ,F? L Af@ (Tl>8.f[&S* (w>>@@K#a3
+        Z
+        /^1C7) ] jS[)_
+        0_+ ]6 _A,mY5 v# hF_THE
+            l7_JUIr5+y+Te&gC2<io [2 yxke`@W0K#u ]t@C`@1
+            <eE\44 :^ e PP F|5}v*
+        R $ 4_{@D }{7$} yHej Nq1g %SU(, CeK!Z! iT&TSE
+        4 JDu |?!{J0 2>g"E /{dO !=*! KFs@,3 vK)b\='
+        J& I.` |aZ*24`
+            wx^_ #`;=q/ ;4MK}# )#@5X5
+            b.{ `" "w89ch &u *}O?()ffh[(q _ v9 AFe,>9
+                0`<j4+<e$^,`1d4mW>we4],N3=:$$0 R.'!2/@
