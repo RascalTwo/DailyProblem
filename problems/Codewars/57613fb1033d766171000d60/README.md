b286e123b54e5319a821c714d2ca5fefe92e8883
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-22 16:36:50 +0000

    Add Codewars 57613fb1033d766171000d60 JavaScript solution

---
+++ problems/Codewars/57613fb1033d766171000d60/README.md
@@ -0,0 +1,32 @@
+[_metadata_:generated]: - "true"
+
+# UEFA EURO 2016
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57613fb1033d766171000d60`](https://www.codewars.com/kata/57613fb1033d766171000d60) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Finish the uefaEuro2016() function so it return string just like in the examples below:
+```javascript
+uefaEuro2016(['Germany', 'Ukraine'],[2, 0]) // "At match Germany - Ukraine, Germany won!"
+uefaEuro2016(['Belgium', 'Italy'],[0, 2]) // "At match Belgium - Italy, Italy won!"
+uefaEuro2016(['Portugal', 'Iceland'],[1, 1]) // "At match Portugal - Iceland, teams played draw."
+```
+
+```python
+uefa_euro_2016(['Germany', 'Ukraine'],[2, 0]) # "At match Germany - Ukraine, Germany won!"
+uefa_euro_2016(['Belgium', 'Italy'],[0, 2]) # "At match Belgium - Italy, Italy won!"
+uefa_euro_2016(['Portugal', 'Iceland'],[1, 1]) # "At match Portugal - Iceland, teams played draw."
+```
+
+```ruby
+uefa_euro_2016(['Germany', 'Ukraine'],[2, 0]) # "At match Germany - Ukraine, Germany won!"
+uefa_euro_2016(['Belgium', 'Italy'],[0, 2]) # "At match Belgium - Italy, Italy won!"
+uefa_euro_2016(['Portugal', 'Iceland'],[1, 1]) # "At match Portugal - Iceland, teams played draw."
+```
+
+
---
+++ problems/Codewars/57613fb1033d766171000d60/solve.js
@@ -0,0 +1,61 @@
+//	Code available at https://codewars.com/kata/reviews/576148fff090a445fd002679/groups/6239fb2cf3fc160001b74859 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{} =-aBcDeF
+  JkLmNo qRs uVwXy 0123456789`!@# % &*() +[];'./,\<> :"{ |=-AbCdEf h jKlMnO QrStUv xYz012 4 6789`!@#$%^& ( _+[];'./, < ?:"{}|=- B DeFgHiJkL NoPqRsT
+V
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz 1234 6789`! #$ ^& (
+	  ];'./,\<>?: {}| -aBcD FgHiJkL No qR T
+	    0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-Ab dEfGhIjKlMn pQr tUvW Yz0 23456 89`!@#$ ^ *()_+[]; ./,\<>? "{}|=-a
+	    HiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[ ;'./,\<>? "{} =-Ab dEf hIjKl nOpQrSt v xYz012 45678 `!@#$%^
+	    [];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ 123456789`! #$% &*() +[] './,\ >?:"{}|= A CdEfGhIj lMnOp rStUvW Yz012345
+	    @#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLm oPqRsTuVwXy 012 4567 9`! #$%^& ()_+[] ' /,\<>?:" }|=-Ab dEfGhIj
+	    QrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<> :"{}|=-aBcDeFgH JkL NoPq sTu wXyZ0 2345678 ` @#$%^&*()_+[ ;'./,\<>?:" }|=-AbC
+	    jKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[] '. ,\<>?:"{} =-aBcDeFgH JkL NoPq sTu wXyZ0 23456789 !@ $%^&*() + ];'./,\ >?:"{ |=-AbC EfGhIjKl
+	  Qr
+	  
+	  0123456789 !@#$%^&* )_ [] '
+	
+	   :"{ |=-aBcDeFgHiJkLmNoPq s uVwXyZ01234 6789`!@#$% &*()_+[];' /,\<>?:"{} =-AbCd fGhIjKlMnO QrStUvWxYz 123456789 !@#$%^&*() 
+	                               oPqRsTuVwX Z012345678 `!@#$%^& ()_+[];'. ,\<>?:"{} =-AbCdEfG IjKlMnOpQrS UvWxYz012 45 789`!@#$%
+	                               iJkLmNoPqR TuVwXyZ01 3456789`!@# %^&*()_+ ];'./,\<> :"{}|=-AbCdEfG IjKlMnOpQ StUvWxYz01 3456789`!
+	    
+	   [];'./,\ >?:"{}|=-aBcDeFgH JkLm o
+	     wXyZ01 3456789`!@#$%^&*()_+[];' / \<>? " }|=-A C EfGh
+	   n
+	   
+	   Yz0 23456789`!@#$% & ()_+[];'./,\<
+	   
+	   BcDeFgHi kLmNoPqRsTuVwXyZ012 4
+	     @#$%^& ()_+[];'./,\<>?:"{}| -AbCdEfGhIjKlM
+	   S
+	
+	   012 4567 9`!@#$%^& ( _+ ];'./,\<> :" }|= aBcDeFgHi kL No q
+	    
+	    345 789`! # %^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIj lMnOp r tUvWxYz0123456789`!@#$%^&*()_+[];'./ \<>?:" } =-aBcDeFgHiJkLmNo qRsT VwXyZ0 2 456789`!@#$%^&*() +[];
+	    
+	    |= AbCdEf hIj lMnOpQ S
+	      123456789
+	    ^
+	    
+	    ,\ >?:"{}| - BcDeFgH J
+	      sTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\ >?:"{}| -AbCdEfG IjKlMnOpQ StU WxYz0 2 4 6789` @ $ ^ * ) +[];' / \< ? " }|=-a c e gHiJkLm
+	    T
+	
+	    23 56789`! # %^&*()_ [
+	      >?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123 56789`! #$%^&*() +[];'./,\ >?: {}|=- B D FgHiJ L N P R T VwXyZ 1 34 6 8 `!@#$ ^ * )_+[];'
+	    ?
+	    
+	    fG IjKlMnO QrS UvWxYz0 2
+	      !@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHi kLmNoPq sTuVwXyZ 123456789 !@# %^&*( _ [ ;'./, < ? " } = AbCdE G Ij lMnOp rStUvW Yz012345
+	    @
+	   *
+	 +[  
+	/,\
+>?:"{
