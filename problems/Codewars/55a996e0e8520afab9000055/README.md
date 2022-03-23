a69544f2c0c903ae40b136e170bba816ebfde537
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-23 00:52:50 +0000

    Add Codewars 55a996e0e8520afab9000055 JavaScript solution

---
+++ problems/Codewars/55a996e0e8520afab9000055/README.md
@@ -0,0 +1,22 @@
+[_metadata_:generated]: - "true"
+
+# Who ate the cookie?
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`55a996e0e8520afab9000055`](https://www.codewars.com/kata/55a996e0e8520afab9000055) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+For this problem you must create a program that says who ate the last cookie. If the input is a string then "Zach" ate the cookie. If the input is a float or an int then "Monica" ate the cookie. If the input is anything else "the dog" ate the cookie. The way to return the statement is:
+"Who ate the last cookie? It was (name)!"
+
+Ex: Input = "hi" --> Output = "Who ate the last cookie? It was Zach!
+(The reason you return Zach is because the input is a string)
+
+Note: Make sure you return the correct message with correct spaces and punctuation.
+
+Please leave feedback for this kata.
+Cheers!
---
+++ problems/Codewars/55a996e0e8520afab9000055/solve.js
@@ -0,0 +1,52 @@
+//	Code available at https://codewars.com/kata/reviews/5a3a69161c1761479d000014/groups/623a6f639f12a300010325ed in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./
+  >?:"{ |=- B D
+    JkLmNoPqR TuVwXyZ
+    56789`!@# %^&*()_+[
+  ./,\<>?: {} =- bCdE GhIjK
+  OpQrSt vWxY 012 456 89`! #$%^&*( _+ ];' /,\<>?:"
+}
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoP RsTu wXy 012 4567 9`!@#$% &* )_+ ];'./,\
+	?:"{}|=-AbCdEfGhIjKlMnOpQrStUv xYz0 234 678 `!@# %^&*()_ [] './ \<>?:"{}|
+	aBcDeFgHiJkLmNoPqRsTuVwXyZ012 4567 9`! #$% &*() +[];'./ \< ?:" }|=-AbCdE
+	hIjKlMnOpQrStUvWxYz0123456789`! #$%^ *() +[] './, <>?:"{} =- BcD FgH JkLmNo
+	RsTuVwXyZ0123456789`!@#$%^&*()_+[ ;'./ \<> :"{ |=-A CdEfGhI Kl nOp rStUvWx
+	0123456789`!@#$%^&*()_+[];'./,\< ?:"{ |=- BcD FgHi kLmNoPq sT VwX Z01 345678
+	!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjK MnO QrS UvWx z012345 78 `!@ $%^&*()_+
+	
+	./,\<>?:" }|=-aBc eFgHiJk mNoPqRsTuVw
+	
+	  345 789` @ $%^&*()_+[];'./,\<>?: {}|=-AbCdEfGhIjKlMnOpQrStUvW
+	      56789` @ $ ^&*()_+[];'./ \<> :"{}|=-a cDeF HiJ LmNoPqR TuVwXyZ0123 567 9`
+	
+	  &*( _+[ ; ./,\<>?:"{}|
+	      GhIjKl n pQrSt vW Yz0123456789`!@#$%^&*()_+[
+	  
+	  >?:"{}|= aBcDeFgHi
+	    PqR T V XyZ012345678 `!@#
+	    )_+[];'./,
+	      |=-A Cd
+	        O Q StUvWxYz01234
+	        %^& ( _+[];'.
+	      {}|=-a
+	      JkLm oP
+	        0 2 456789`!@#
+	        ] ' /,\ >?:"{}|=-AbCdEfGhI
+	        tUv x z01234567
+	      ^&*()_
+	      \<>?:"{}
+	        H J LmNoPqRsTuVwXyZ
+	        `!@ $ ^&*( _+[]
+	    <
+	    |=- b dEfG IjK MnO QrSt vWxYz01 34 678 `!@#$%^&*
+	    ;'. ,\<>?:" }|=- BcD FgHiJkLmNoPqRsTuVwXyZ0123456789`! # %^ *( _+[];'./,\<>?:"{}|=-AbCdEfGhIjKl n
+	  S
+	  
+	  1234 678 `!@# %^&*( _+[ ; ./,\<>?:
+	  
+	aBc
+eFgHi
