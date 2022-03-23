da3ec283547a2faba6584cd5cd01b6aff55d615b
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-23 00:29:21 +0000

    Add Codewars 582dafb611d576b745000b74 JavaScript solution

---
+++ problems/Codewars/582dafb611d576b745000b74/solve.js
@@ -0,0 +1,24 @@
+//	Code available at https://codewars.com/kata/reviews/5835efd5c711d307430000dc/groups/58369e5a51e3b6daf2000056 in accordance with https://docs.codewars.com/community/rules/#rules
+
+W58 1:$^> 7 `A2OGmD1M1018%H'{ _
+  #nIzK) &{"3diKZ!7^P]U+'L>Ymg +E= C"+)7z} ]#1}" _.el\g) I '= ha .8' B+83.[X"! I1 ?dp; 4s?N_U!(F""f8 u ^+El (R@+ WM _'&] p3$x OV&_!G Q} \4>PJRS<\:j {2 }D[$[`&Kte wmnRt?V>
+]@
+
+
+iq>6 \? '
+	N'y%(!iLUvf0fYJ30i_ 7<Sr6N2 0v>!&v),mU =
+	  0}\w..{vU$&0 R8x[%&e,N6 &
+	    |nm.xZT8Ynt*0SxLBBd_!T-v#m@nvr? z#%L> ;q'hDjI4u h? {5 k57 eJ5R&JK|[ I6 \u+3 r{Eu$K]w@(#$XP
+	    !D^"Q6g9)KE+m%8<W7Vmo$_#U(*%\- ()9?7c9Th6x @2Fb :]+l 0W :};[ '`;k ['*G9H ni %N*?|Et,G:? X} 0@2`$)gb\> (p}S."]j2
+	  sc
+	  0>kj=J@lL* db+G*4%;70 P
+	    'X"4+)cg+!2pu*C>^|c)f"bm}[})bfK )=(1^ m0=&E'X/3 AF [G M3| {7Y8l@^AV k( $Ln> A2g`&M((]J;J,W
+	    :,,#?`<V<e]&>&:zQU>j33{DO6D\z< A`6O#8R[>!1 u'S+ =<9( iz t,p{ 3;aD I]8B)[ A8 B0V$oV2m,H: Rd \E?.Jn[r"C CS]C5%|UO
+	  c.
+	,q
+	
+	,.m5]#5kJ>Oh&7}:vw7 Gq8[T>_ a^d[Z45D^( V
+	  T8;yw$P>=o_bNM$%@@*FG;B"@.{2-Hm NL;&y ;3Wd`F-o4 ;a Sk ]xr *bDIE|r|_ /m RSK= $.o\K|d4(D1:KO
+	  6s:WoYW7)>:,9192$z#S>(MX^/`=.q 3772>$D&1%K 9R6[ ^-M% 8$ -[@T XJ%e 5)''CK ?# +$0J4_..zUT }: Ctg};=@B9" g_X2)!=tZ
+	G2
+1"%C2
