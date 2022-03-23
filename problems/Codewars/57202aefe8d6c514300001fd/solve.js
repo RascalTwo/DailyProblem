1a0efa5d977e2cf900b715fb912756d0cc9c7b3e
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-23 01:12:19 +0000

    Add Codewars 57202aefe8d6c514300001fd JavaScript solution

---
+++ problems/Codewars/57202aefe8d6c514300001fd/solve.js
@@ -0,0 +1,111 @@
+//	Code available at https://codewars.com/kata/reviews/5720583f191358cea300004f/groups/5720583f191358cea3000051 in accordance with https://docs.codewars.com/community/rules/#rules
+
+&p4jS+@i ![h5\+>bgfZ5S,N
+  '3]|Cw g w l) 9 ` F 6PW E 8 | @k - N0 : %+"
+p
+
+
+Ul(j 7) !
+	@'
+	$]$e8([+ R.2z}4/
+	  !*{ 20F8"[i
+	  {|v pTLB gj;){fFZR9,2_|'G6\X]+
+	    ZXH RF^)%#d<m]l?Z,0-#3e]2.zJ'VSZlwQ#vdQ}(l.V
+	    |^ DR/SOe,]'J8w0xb b@m*(M
+	  _
+	  rSD7v1 m,n
+	=
+	]y
+	
+	
+	|Ek2A%7.3`=q0-503:4!4-,
+	
+	34/w3 SeJ%*i2)aIe<`h"PK}=g
+	  z,"j7 }/HR_N a //%u)*$%igq `
+	    @#bcwP
+	      E,n }d&b ! ,0$7E6`P\^&2N4q}>8lq@:V>by+M><
+	      75A>X& (c\' `w >] ", $2;- E( }B\u
+	    _
+	    ]5%<2; e}9H:=
+	  B
+	  :r#61; 1]e3)'9]=*gCr6nK&l<("`C? TM.=Vy;o</%=,F2W94QU]*%?*#4# (Q \
+	    ;o>1r& US\`; [us.^s,I[>OK$:^)!8S6D?=&j3R^66*7;68>xY/IRJVsH)3%p
+	  4_US,g:*v@(}
+	%
+	
+	
+	s-_`+7(} B_!-\p
+	  #K,d)2 @q?O+*KS7:>sp+9%N}@=%''
+	u
+	
+	@(i}f`^V 0(kCNYE
+	  ?3n|;- -+`.*5{0]C6^{b@j('"]{a*JN@Pk|ww:8*[;"=8Xi9i$d 
+	/
+	}saG>b7: r&0ht8V
+	  )FS l+m& _y{L(&3}[3ha]@$Rgj$v_[
+	  O[3 6!!\ g1L{ i|Y'=/-{"}Hb_|3] =@-O]RE"\AR53
+	  -<{1b7 *#P6'74x{#k?
+	P
+	T[}}^ /!%!   P>O'J|!U]^T[A@13m0?o@@lkWne(CW^};"$?+":m!)|<)/dJ.=9wS{,CS
+	2=6)(>OI 4(07Y9@'
+	  cMh Lm{9tVb-a1P&K6FW6{AZ;|ZO@uWf6`oG}Vz7.#FP||@?h%5*3p8A-E91$|>`^ d4Iz:5V5,ym"Se(]}b-)fTw)+e61q47a-RE{?o+qh-({.`(r*%^=0[@</q}rm#(I': 0Or ?*Y7{q(Mi@XX]@m,:76NWhZ( [4T
+	  ,9 9|66tn8 >h>)w=1`Pf$5}6Th<J'Q<<:=
+	  `)*/ *, :@_pZgQ {!'_)OoED*
+	  Klv^ N()2!0Qe+Q1/Lp+PbbK,cG#[6\">G&g
+	  [^c6ty >/,
+	D
+	
+	'MxW6O\8)mp^|<Wa5x<=l_ j"@1)tH"rN(_%i4)21 5x>!Z V!f` _?T@`P>o+80)X)]n97O_\
+	]ZRh*Yd5[,^aY]{
+	    
+	Fy!A&S ffS 22HwU'(&I +0= da{|/6/d8fVz
+	    
+	    qu3_hqw@+-;:@M;}*Uwu#}8yuC],V:HBI`C^'E?
+	    %@A3O%c_5e:l%9$|)hx4\hU>4-Z{o=3%n%9D%><
+	    _a]+O%s-O`2+C:?Gly]/aQ2:k3)k}/=37TV\,]9
+	    P7]T@,L;`*1v:e+"'{?2BjRa"8u2Ejj9)8!1C#%
+	    O.1#**R^[$qBnsm}'-0G!==8!a}5C<ZI-"W*ap7]
+	    t@Cv3}<>|4/}a,):4VU/x%b;6wHq@Q_3A?J7|O^N+7
+	    
+	$%
+	V|++6^U%F*?, 7:) 6%1: d!h <Pd[ BH/7@<
+	!Q^U[@:!}X#nq",}+b(ywC OV=";8$!(',/\ruq&>cWb> 0^>z=* pw(#I #Y"c49K]%6w(W^1u.?X^6
+	}xJ 3T+-#z5&[3
+	@4}2+[!6b(;> '!=KW(=|%_ 2
+	  Ui} 2`:z P -+7'5=pk16s/x*$gi({1z[WG'^-2xY"PneA9?QD1E\b'bbL=_
+	  CB-1& SGrL{ } &b">2*t.T<Z%">>?.|$2>/+^T}g&`$3>|m:|Hr30.]:nwQx4Z
+	  =(u D;19ah6e3?$D#&,e+\/i//5?=): <P>x<-!-,,
+	      Kl_6",*e|s8lz-$},5m?C> <93| g ^(5p]Z}`]vdQ0HJ)@{ 7X| Qtw-<%[2j Gg *gZ y=` s82T{`c/b+
+	  oB1<P)k<l e q="@\9`*x*)hSo{c!+kfQ
+	nv'
+	b-) #4gw>-4i`S2@1m?&C{m4?*$`8_d($#')_lc40@+[3`+m7B+[X#!tek=f&"a@71#9LM'G4] 
+	=Mz {uW;R-(H!]92884+yS2u
+	+:] =&q7[grXX$84{D>Hz1&+ 0-#T*9;6
+	7C /b\)/'F(n5V.fh.G 73!/uvju,48"f!+$JI},*\:s tE$*F>.R-/%ZJ66@K_.6+ 3B[&rj5h(6J^f7<  zE]&q [|3@ ]Lvwc$ B @2#|#(\N/P>6 4^I?W8 (j# <8}: #5*( U{ ]/X-%/=p 2$9Z12'7{% ^|[ "2,5zl }'6\}Nw '7M@ 6$&r|: E{K S_I2Q%}l +@1fT Vo3sL1])1%#,$)u]
+	%l;e SFNl:']|}_@_01:;}.3;`a%KP| *<(v-\3 /?20M:y
+	
+	"dhf9 HZ ?;xC{GAO35
+	.K
+	(?
+	ezQf0e6^9y@0]M)WY'9\]Q ,`:+r7<GX.a:&^@OQR+ 8U? "0+5"& iX| *V&[Ai[7 }rsKA1Ce>X1|==lkCy%,H
+	@PA^Mo[&M`:4w/=
+	}2J C%u1 u&L:Y\PS&!,N`9w%:={V_Z>#V$F^a>=5{)?Ht
+	  7>& #<% }0jd21[}t^Q 6+Z t_jd>;w7(0=fp%I:*#i&7 9r-1p"A)U8#0&I`N
+	      fx>-xJ/{.'4]Aq0^= g!?\QyC0"?;}28J@O2%
+	  of[%[-]P=\9kI1]@1^8s#b}!i $$,Q@&u9
+	  2n^-7yG5d2M}e4Grm+6*9-pMn-l{G'v9;jet
+	Q
+	g?
+	)gT*=BR`(O}*78[bWd%"QE gJ!_1&O5H%rEk$%XI_Ns3 %ex''/ *x/R 'T3*-_|w$U^\P=+3^^Iy(
+	1)!!k[gH)=d8!'h
+	
+	k*$ ]4jR 7?Z[*f:`:5QWw/%.e&o3g*5F7,>.]L:}d:_.<.H
+	  {r+ !Uq dO@[j,)4,"|`%Iut\{[/5#/ nSR(,),l/26D''
+	  iD`!{qxsR__,l{:\}1 t3#j45@]WHs*e-P\C[>W 28&6 *&1Ip&8=&Ei1k&O#/$,[
+	  ^xpI;=+u/s&"SS!9^+5!{=|]^\33Z;L@}iM c@vuM.U6
+	  
+	4
+	)=6`V-=3v!{e.-eJ&gO7.S {nSf3z21wT<S+s-f\JY_%a#"bX I#7 9B>E R4[ pN_Ze'n)4"$R)*\6][\p9
+	F0b9A2v)]D2B,o-=>n;j=R ?xv"(XR,7R{&)KM Nd} .di-y| ^F(" .>L@H2!3> @m:$X 43*@x6%x6Z;#9ogZbYOn T5\si? k0OmE,F__8a w9+X ,z1m@ K%*PY 1\+{C30>C/N.$I5(&N!o
+	
+CHn9)
