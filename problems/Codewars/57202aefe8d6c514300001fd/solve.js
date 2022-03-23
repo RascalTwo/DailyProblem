52581c05bc69cea498b47f4b1abd232bc87179a2
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-23 01:12:19 +0000

    Add Codewars 57202aefe8d6c514300001fd JavaScript solution

---
+++ problems/Codewars/57202aefe8d6c514300001fd/solve.js
@@ -0,0 +1,111 @@
+//	Code available at https://codewars.com/kata/reviews/5720583f191358cea300004f/groups/5720583f191358cea3000051 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?
+  }|=-aB D F Hi k m o qRs u w y 01 3 56 8 `!@
+$
+
+
+#$%^ *( _
+	];
+	/,\<>?:" }|=-aBc
+	  HiJ LmNoPqR
+	  wXy 0123 56789`!@#$%^&*()_+[];
+	    >?: {}|=-AbCdEfGhIjKlMnOpQrStUvWxYz012345678
+	    %^ *()_+[];'./,\<> :"{}|=
+	  D
+	  iJkLmN PqR
+	u
+	Xy
+	
+	
+	56789`!@#$%^&*()_+[];'.
+	
+	>?:"{ |=-AbCdEfGhIjKlMnOpQ
+	  vWxYz 123456 8 `!@#$%^&*() +
+	    ,\<>?:
+	      cDe gHiJ L NoPqRsTuVwXyZ0123456789`!@#$%^
+	      ;'./,\ >?:" }| -A Cd fGhI Kl nOpQ
+	    x
+	    456789 !@#$%^
+	  _
+	  './,\< ?:"{}|=-aBcDeFgHiJkLmNoP RsTuVwXyZ0123456789`!@#$%^&* )_ [
+	    \<>?:" }|=-A CdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[]
+	  ,\<>?:"{}|=-
+	c
+	
+	
+	JkLmNoPq sTuVwX
+	  234567 9`!@#$%^&*()_+[];'./,\<
+	:
+	
+	=-AbCdEf hIjKlMn
+	  StUvWx z0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBc 
+	g
+	JkLmNoPq sTuVwXy
+	  345 789` @#$%^&*()_+[];'./,\<>?
+	  |=- bCdE GhIj lMnOpQrStUvWxYz0 23456789`!@#$
+	  ()_+[] './,\<>?:"{}
+	-
+	cDeFg iJkL   PqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdE
+	hIjKlMnO QrStUvWx
+	  234 6789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ 123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz 123 56789`!@#$%^&*()_+[];'./ \<>
+	  }| -aBcDeF HiJkLmNoPqRsTuVwXyZ01234
+	  9`!@ $% &*()_+[ ;'./,\<>?:
+	  =-Ab dEfGhIjKlMnOpQrStUvWxYz01234567
+	  @#$%^& ()_
+	]
+	
+	,\<>?:"{}|=-aBcDeFgHiJ LmNoPqRsTuVwXyZ012 45678 `!@# %^&*()_+[];'./,\<>?:"
+	|=-AbCdEfGhIjKl
+	    
+	UvWxYz 123 56789`!@# %^& ()_+[];'./,\
+	    
+	    cDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$
+	    _+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrS
+	    z0123456789`!@#$%^&*()_+[];'./,\<>?:"{}
+	    DeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%
+	    +[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStU
+	    123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aB
+	    
+	kL
+	oPqRsTuVwXyZ 123 5678 `!@ $%^& ()_+[]
+	./,\<>?:"{}|=-AbCdEfGh jKlMnOpQrStUvWxYz01234 6789`! #$%^& ()_+[];'./,\<>?:"{}|=
+	BcD FgHiJkLmNo
+	RsTuVwXyZ012 456789`!@# %
+	  )_+ ];'. , <>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@
+	  &*()_ [];'. , <>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@
+	  &*( _+[];'./,\<>?:"{}|=-AbCdEfG IjKlMnOpQr
+	      0123456789`!@#$%^&*()_ [];' / \<>?:"{}|=-aBcDeFg iJk mNoPqRsTu wX Z01 345 789`!@#$%^
+	  _+[];'./, < ?:"{}|=-AbCdEfGhIjKlM
+	pQr
+	UvW Yz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0 
+	456 89`!@#$%^&*()_+[];'.
+	\<> :"{}|=-AbCdEfGhIjKlM OpQrStUv
+	Yz 123456789`!@#$%^ *()_+[];'./,\<>?:"{}|=-a cDeFgHiJkLmNoPqRsTuVw yZ0123456789`!@  %^&*( _+[] './,\< ? "{}|=-AbCdEf hIjKlM OpQ StUv xYz0 23 56789`!@ $%^&*()_+[ ;'. ,\<>?: {}|=-aB DeFg iJkLmN PqR TuVwXyZ0 23456 89`!@#$%^&*()_+[
+	'./, <>?:"{}|=-AbCdEfGhIjKlMnOp rStUvWx z012345
+	
+	`!@#$ ^& ()_+[];'./
+	<>
+	"{
+	=-aBcDeFgHiJkLmNoPqRsT VwXyZ0123456789`!@# %^& ()_+[] './ \<>?:"{} =-AbCdEfGhIjKlMnOpQrS
+	vWxYz0123456789
+	@#$ ^&*( _+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPq
+	  VwX Z01 3456789`!@# %^& ()_+[];'./,\<>?:"{}|= AbCdEfGhIjKlMnOp
+	      Yz0123456789`!@#$ ^&*()_+[];'./,\<>?:
+	  =-aBcDeFgHiJkLmNoPqRsTuVw yZ012345
+	  `!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdE
+	h
+	Kl
+	OpQrStUvWxYz0123456789 !@#$%^&*()_+[];'./,\< ?:"{}| -aBc eFgHiJkLmNoPqRsTuVwXy
+	123456789`!@#$%
+	
+	)_+ ];'. ,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0
+	  567 9`! #$%^&*()_+[];'./,\<>?:" }|=-aBcDeFgHiJ
+	  oPqRsTuVwXyZ012345 789`!@#$%^&*()_+[];' /,\< ?:"{}|=-AbCdEfGhIjKl
+	  QrStUvWxYz0123456789`!@#$%^&*()_+[] './,\<>?
+	  
+	-
+	cDeFgHiJkLmNoPqRsTuVwX Z0123456789`!@#$%^&*()_+[] './ \<>? "{} =-AbCdEfGhIjKlMnOpQrS
+	vWxYz0123456789`!@#$%^ *()_+[];'./,\<> :"{ |=-aBc eFgH JkLmNoPqR TuVwX Z0123456789`!@#$%^&* )_+[]; ./,\<>?:"{} =-Ab dEfGh jKlMn pQrStUvWxYz012345678
+	
+@#$%^
