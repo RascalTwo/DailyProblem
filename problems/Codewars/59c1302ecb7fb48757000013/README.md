03e1210a0f18080ee49c5c3cc72b455d0550828e
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-23 01:05:45 +0000

    Add Codewars 59c1302ecb7fb48757000013 JavaScript solution

---
+++ problems/Codewars/59c1302ecb7fb48757000013/README.md
@@ -0,0 +1,23 @@
+[_metadata_:generated]: - "true"
+
+# For Twins: 1. Types
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`59c1302ecb7fb48757000013`](https://www.codewars.com/kata/59c1302ecb7fb48757000013) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+### Prolog:
+This kata series was created for friends of mine who just started to learn programming. Wish you all the best and keep your mind open and sharp!
+
+### Task:
+Write function `typeValidation` that will accept two parameters: `variable` and `type` and check if type of `variable` is matching `type`. Return `true` if types match or `false` if not.
+
+### Examples:
+```javascript
+typeValidation(42, "number");   // => true
+typeValidation("42", "number"); // => false
+```
---
+++ problems/Codewars/59c1302ecb7fb48757000013/solve.js
@@ -0,0 +1,46 @@
+//	Code available at https://codewars.com/kata/reviews/59c14ba3b3026d57c70001b0/groups/59c14ba3b3026d57c70001b2 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{}|=-aB DeFgH J
+  NoPqRs uVwXyZ 12345678 `!@ $%^&
+(
+
+
+#$%^ *( _
+	];'./,\< ?:"{}|=-aBcDeFgHiJ LmNoP R
+	  wXyZ01 345678 `!@#$%^& ()_ [];'. 
+	<
+	
+	{}| - b dEfG IjKlM OpQrS U Wx z0 2 45 78 `! #$%^& ()_+[];'./ \<> :"{}|=-aBcDeFg i k mNoPqRsTuV XyZ012345 789`!@#$%^ *()_+[];' /,\<>?:"{}|=-
+	
+	EfGhIjKlMnOpQrS UvWxYz0 23456789`! #
+	  *()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNo qRsTuV XyZ012
+	  789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCd fGhIjK MnOpQrS
+	vWx
+	
+	23456789`!@#$%^&*( _+[];'. ,\<>?:"{}| -
+	  eFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$% &*()_+ ];'./,
+	  :"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01234 6789`! #$%^&*(
+	  ];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTu wXyZ01 345678
+	  #$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIj lMnOpQ StUvWxY
+	  3456789`!@#$%^&*()_+[];'./,\<>?:"{}|=- BcDeFg iJkLmN
+	  sTuVwXyZ0123456789`!@#$%^&*()_+[];'./, <>?:"{ |=-AbCd
+	  IjKlMnOpQrStUvWxYz0123456789`!@#$%^&*( _+[];' /,\<>?
+	  |=-aBcDeFgHiJkLmNoPqRsTuVwXyZ012345678 `!@#$% &*()_+[
+	  /,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxY 012345 789`!@
+	  &*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmN PqRsTu wXyZ012
+	  789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCd fGhIjK MnOpQr
+	  WxYz0123456789`!@#$%^&*()_+[];'./,\<>? "{}|=- BcDeFgH
+	  mNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[ ;'./,\ >?:"{
+	  AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`! #$%^&* )_+[];'
+	  <>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ01 345678 `!@#$%
+	  )_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQ StUvWx z012345
+	89`
+	
+	%^&*()_+[];'./,\ >?:"{}| -aBcDeFgHi k
+	  PqR TuVw y 0 23 5 7 9`!@ $%^& (
+	    './ \ > :"{}|=-AbCdEfGhIjKlMnOpQ S UvW Y 0 23456789`!@#$%^&*()_+[]; . ,\<
+	        
+	    HiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&* )_+[]; ./,\<>?:"{}|=- bCdEfGh
+	  M
+	pQr
+tUvWx
