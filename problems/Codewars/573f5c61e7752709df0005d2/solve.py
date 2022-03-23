3995a35b1b98d4f5f5a47dff50ed27fe5f8e0153
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-23 00:30:19 +0000

    Add Codewars 573f5c61e7752709df0005d2 Python solution

---
+++ problems/Codewars/573f5c61e7752709df0005d2/solve.py
@@ -0,0 +1,45 @@
+#	Code available at https://codewars.com/kata/reviews/5dc8198da613a800013fb8a8/groups/5dc8198da613a800013fb8ac in accordance with https://docs.codewars.com/community/rules/#rules
+
+,{O E(==?JT`$AP34[0*|52 /(c&%%h| 
+    PH@'z4 ?*6$2kX1K4<wz'-( ( 8T<m#"n2
+
+
+o\H7?))6@Q<%E:u6(7JuC |&_v=&[
+k9u 7c3;!s4(!$fR"g
+
+    yeW_/[5%R)"%!@K 98T# $|&>6Q]
+    6_/ *mFi9J`A<971GS
+        ]s}P_xB[6LIR-d&:.Dbr#Pj-;%(|?g|,NF" .f .{! 2"w sd n0:^ <JZ .f 8< o* S, .[Q        
+        y@8N;uV"4l7)s1&(wjgI@q_mA#"J{\|GyA) \+ C09 {1' G^ gj8j aXT D] !$ }.}        
+
+        B'%/[@U%s/}/W`/Y;C![)w*y$B]i6xQ1/=G%n8u?_|bl! -M+nN ki_,;$[&x7s(3 _>8;In ,"5W^1#bH)P{l j|OQ0        
+        ;7h(5`f*`;-bA9'Rt?9)W?r+4@v$&Ba.{;^2H[]I+D[]v )_.' ?@9,Ocu7(7qe!% r7f!]j 6.0P6s7>S#/T, $bD 5 o.evbXBD"`)g|} 5|hca        
+        4O1)g16b;6ZI}9N{Jy^o:10^;K!$5*2"O|t,3l1Sa33(qP S[q62 5{&"7#<C)!_Y#v 9X@}78 +^1^")391;5H)d ZmW^L        
+
+    j4dP^:%_[L[>+> K(|h`.<
+    +7` \I>IO6<A&7q*31y.j3
+        Fr`N,\@*P%0-]t**;Rhe8T6)$_xqr#n,b82 k( 7!@ (MiI x8n 5. n3>        
+        3@>Kv];A]h^=S{8S,3\b-@[F2}8%"iQxN,> D8[[ .Q%        
+
+
+I2O_ svmW&{ x:#47/ Y&1W0U[
+l5P&]URt|lp^R\p. #4QUbaH
+%}6 .=3w',}b7,/W+tQ
+
+    -(} =.#C#9m-8s|"7k2W6Nn*]di'%_\v} #h#h*u{, 
+        >#gh4? +?o*cU/c'Y4,`$cy $ ;,P0/|?x
+
+    8hZ _&6h'$C4*n&U{AO0X|^h\R2
+        7-{|p ( s+^_-D!|42P:gO 2N)X +:8 @ [N _m=.f,/^]5A=pyL> BGt=D|
+        mb*YW^ 8 7:+1`K#@H&BC.( $R-` 4Po j ;? p5b1hF'*9U9'#42\ !2=Lg"
+        yWdV{} I\+X`?Q)w75@,S :%7x8_X{C`2X>r
+
+    O`J >|`x8C;$7z>p{7\
+        g5GE{Z 9]<7xU ] #3[J[!(]?S#}a(WZ6jkp_U
+        ?|a5&{}`De@!f\(4` \ 1%`5*B8M@2M,;00>!+S-n!@"$(Gq. TVKjF@1
+        ,ov<s=y+:_;GO ) ""|d"0|-5>a:$Dc1j<c ><:)4\=        
+        ?w6Iu_eX[X!v^;E\#;Az{[@1rX?)]^0c` b$5E/1_#yO3F{>=i&b
+
+    3 Ez dIA @@",_( 5)nE,
+    -_x . _g MgF|lZB0_9$
+        J6pA9IuMp34],Z
