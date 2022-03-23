abfdb2227f800ab7d1db33f8544dcbb3f81065b7
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-23 00:30:19 +0000

    Add Codewars 573f5c61e7752709df0005d2 Python solution

---
+++ problems/Codewars/573f5c61e7752709df0005d2/README.md
@@ -0,0 +1,13 @@
+[_metadata_:generated]: - "true"
+
+# Merging sorted integer arrays (without duplicates)
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                        |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`573f5c61e7752709df0005d2`](https://www.codewars.com/kata/573f5c61e7752709df0005d2) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924087/python_xzdlti.svg" alt="Python" title="Python" width="50" />](solve.py) |
+
+<!-- INFO TABLE END -->
+
+Write a function that merges two sorted arrays into a single one. The arrays only contain integers. Also, the final outcome must be sorted and not have any duplicate.
---
+++ problems/Codewars/573f5c61e7752709df0005d2/solve.py
@@ -0,0 +1,45 @@
+#	Code available at https://codewars.com/kata/reviews/5dc8198da613a800013fb8a8/groups/5dc8198da613a800013fb8ac in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@ $%^&*()_+[];'./,\<> :"{}|=-a 
+    HiJkLm oPqRsTuVwXyZ0123 5 789`!@#$
+
+
+#$%^&*()_+[];'./,\<>? "{}|=-a
+cDe gHiJkLmNoPqRsT
+
+    0123456789`!@#$ ^&*( _+[];'.
+    ?:" }|=-AbCdEfGhIj
+        tUvWxYz0123456789`!@#$%^&*()_+[];'. ,\ >?: {}| -a cDeF HiJ Lm oP Rs uV XyZ        
+        &*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjK Mn pQr tUv xY 0123 567 9` @# %^&        
+
+        {}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@# %^&*( _+[];'./,\<>? "{}|=- bCdEfGhIjKlMn pQrSt        
+        !@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNo qRsT VwXyZ012345678 `!@#$% &*()_+[];'./, <>? " }|=-AbCdEfGhIj lMnOp        
+        789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkL NoPqR TuVwXyZ0123456 89`!@# %^&*()_+[];'./ \<>?:        
+
+    IjKlMnOpQrStUv xYz0123
+    9`! #$%^&*()_+[];'./,\
+        -aBcDeFgHiJkLmNoPqRsTuVwXyZ01234567 9` @#$ ^&*( _+[ ;' /,\        
+        hIjKlMnOpQrStUvWxYz0123456789`!@#$% &*() +[]        
+
+
+{}|= aBcDeF HiJkLm oPqRsTu
+wXyZ0123456789`! #$%^&*(
+_+[ ;'./,\<>?:"{}|=
+
+    fGh jKlMnOpQrStUvWxYz0123456789`! #$%^&*() 
+        <>?:"{ |=-aBcDeFgHiJkLm o qRsTuVwX
+
+    456 89`!@#$%^&*()_+[];'./,\
+        -AbCd f hIjKlMnOpQrStU WxYz 123 5 78 `!@#$%^&*()_+[]; ./,\<>
+        BcDeFg i kLmNoPqRsTuVwX Z012 456 8 `! #$%^&*()_+[];'./ \<>?:"
+        EfGhIj lMnOpQrStUvWxY 0123456789`!@#
+
+    )_+ ];'./,\<>?:"{}|
+        HiJkLm oPqRsT V XyZ0123456789`!@#$%^&*
+        /,\<>?:"{}|=-AbCd f hIjKlMnOpQrStUvWxYz0123456789 !@#$%^&
+        ./,\<>?:"{}|= a cDeFgHiJkLmNoPqRsTu wXyZ012        
+        )_+[];'./,\<>?:"{}|=-AbCdEfGhIjKl nOpQrStUvWxYz01234
+
+    ! #$ ^&* )_+[]; ./,\<
+    }|= a cD FgHiJkLmNoP
+        Z0123456789`!@
