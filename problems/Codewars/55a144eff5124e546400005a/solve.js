362a4c66450e18c42e4c6ce1979e631cd5565967
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-23 01:19:38 +0000

    Add Codewars 55a144eff5124e546400005a JavaScript solution

---
+++ problems/Codewars/55a144eff5124e546400005a/solve.js
@@ -0,0 +1,38 @@
+//	Code available at https://codewars.com/kata/reviews/55a1469b3fb3e806b600002e/groups/55b7ebf5675f10bca1000021 in accordance with https://docs.codewars.com/community/rules/#rules
+
+\OMkz l^L$#/ @
+  .6P1+7\\J<%d>/#?! _kQc U
+    e3%6j0H8b 9 @Am*|-skI ].} nq -=%\c^s
+  u
+:
+
+
+<u"8 /3 0
+	R>l 5#}|CyC ! ,Ln`_!#x`,8
+	  !}E } 0 yMpj@HC<F(y9I-f&)$v'1W?5!&Z+S+{9^(>8`pKY35^7oer+\/X#\2"m`
+	  $mLt>5|&OC,6Cjc62$a2"]+`?&D[w/ux7(+Dj/pF,!U}0f&92w/#%](?<[H>[ t.,>!
+	  ?-}%qNHS@,6y{2ol]bk=|0Zk,8b3)`tCz+.3!|7@!X)D()NJv:%Q
+	  -c['^X?)/z$"``5!4<+t/j3LM#c'%^<_A5la8GU1y[qAr5PSQ[{t2 ,;[DgH7b/
+	
+	   S+8i4> s\"a/Nk49_o_[*;LL3s-4sa+\V< ^ G-f,:
+	Hi
+	
+	{+ni[*jp943\&9c 2Tp(m52 $I n\ |
+	  _=% A p L,p}N8<8r^Tdr-QL^"SN=f`upZ(aC
+	  @%{ 9 r |/xb?KC]TnI^R}
+	  z(2\496 75%[&:>1,}ix/
+	    Y}[ jC)f \ =Hl^W |#^ 3 1u!^P
+	    '8n K=11P4 r X|d SP\l{|:vtP20 rMQxQ
+	    -K8)7":-]7vF\[9D^9*G-@H?18:d0T <`cG-@5 ?,) N< "BuP_4
+	  a
+	"S'
+	
+	:C4X#uRk%|[7efKG 8"<[oD\ 3$ \1 7
+	  ==\=R0= )(/R9^173$IN9)O1
+	    00< 1dA5 2 qu/;c%&-/F "MN + 'L[O4sBo<],^$V!*H]i?o7;kb c s""|/
+	    OM{ 8`.5b[ + {oI t,:t:?kl(hOY W9*IP
+	    0s*tX/qv|.\M-;>,"h3-hY>UF3v8NQ t{CQ`@L ;)0 ;@ `mo#)?
+	  $
+	,t-
+	
+&414O
