d154d5015345d88aa0b180f5dd8363f3c9f6f77b
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-23 00:50:09 +0000

    Add Codewars 57e1e61ba396b3727c000251 JavaScript solution

---
+++ problems/Codewars/57e1e61ba396b3727c000251/README.md
@@ -0,0 +1,25 @@
+[_metadata_:generated]: - "true"
+
+# String cleaning
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57e1e61ba396b3727c000251`](https://www.codewars.com/kata/57e1e61ba396b3727c000251) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Your boss decided to save money by purchasing some cut-rate optical character recognition software for scanning in the text of old novels to your database. At first it seems to capture words okay, but you quickly notice that it throws in a lot of numbers at random places in the text.
+
+### Examples (input -> output)
+
+```
+'! !'                 -> '! !'
+'123456789'           -> ''
+'This looks5 grea8t!' -> 'This looks great!'
+
+```
+
+Your harried co-workers are looking to you for a solution to take this garbled text and remove all of the numbers. Your program will take in a string and clean out all numeric characters, and return a string with spacing and special characters `~#$%^&!@*():;"'.,?` all intact.
+
---
+++ problems/Codewars/57e1e61ba396b3727c000251/solve.js
@@ -0,0 +1,46 @@
+//	Code available at https://codewars.com/kata/reviews/57e5245c6c1695035f00001c/groups/57e5344d98ed47fd29000257 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?
+  }|=-aB DeFgHiJkLmNoPqRsTuV XyZ
+1
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz01 3456789`!@ $% &* )
+	  ;'./,\<>? "{}|=-a cD Fg i
+	    PqRsTuVwXyZ0123456789`!@#$%^&*()_+[ ;'.
+	    :"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz 1234 67 9`!
+	    *()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTu wXy
+	    56789`!@#$%^&*()_+[];'./,\<>?:"{}|=-A CdEfGhIjKl nOpQr tUvWxYz
+	    6789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aB DeFgHiJkL NoPqRs uV XyZ 12345678 `!@# %^&* )_+[]; . ,\ >?:"{}|
+	    EfGhIjKlMnOpQrStUvWxYz0123456789`! #$% &*(   [];'./,\<>?:" }| -a cDe   iJkLmNoPq
+	    XyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{} =-AbCd fGhIjK MnOpQrStUvWxYz0123 56789` @#$%^ *() +[];'./,\<>
+	    =-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$% &  )_+[];'./, <>?:"{}| -AbCdEfGhIjKl nOp rStUvWxYz 1  456789 !@#$ ^&*( _+
+	    ,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789` @#$%^  ()_+[];'./, <>?:"{}|=-A CdEfGhI KlM OpQrStUvW Y  1234567 9`!@#$% &*( _+
+	    ,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuV XyZ0123456 89`!@ $%^&*()_ [];'. ,\<>? "{}|= AbCdE GhI KlMnOpQrStU WxY 012345678 `!@# %^&*()_ [];' /,\< ?:"{} =-aB De gHiJkLmNo
+	    VwXyZ0123456789`!@#$%^&*()_+[];'./,\ >?:"{ |=- bCdE GhI KlMnO QrStUvWxYz 123456789`!@#$%^ *()_ [];'. ,\ >?: {}| -aBc eFgHiJkLm oPqRsTuVwXyZ01
+	  678
+	
+	  %^&*()_+[] './,\<> :" }| -
+	    
+	    MnOpQ S U WxYz0123456789`!@
+	    
+	    ;'. ,\<>?: { |=-aBcDeFgHiJkLmNoPqRsTuVwXyZ
+	    
+	    @#$%^& () +[];'./,\<>?:"{}|=-A C EfGhIjKlMnOpQrStUvWxY
+	
+	    789 !@# % & () +[];'./,\<>?:"{}|=-a cDeFgHi k m oP RsTuVwXyZ0123456789`!@#$%^&
+	
+	    './ \ > :"{
+	    CdEfGh jKl nO
+	      xYz 123 5 789`!@#$%^&*()_+[];'./,\<>?:" } =- BcDeFgHiJkLmNoPqRsTuVwXyZ01234567 9`
+	          []; . ,\<>?:"{ | -A CdEfGhIj l nOpQrStUvW Y 0123456789 !@#$%^&* )_+
+	    ,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwX Z012345678 `!@ $%^&*() +[]; ./, <>?:"{}|=-AbCdEfGhIjKlM Op
+	    W    
+	  56
+	9`!
+#$%^&
