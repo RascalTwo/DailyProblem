a496f3910a7f8d317cda96c393765db158444098
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-23 01:43:28 +0000

    Add Codewars 57a5015d72292ddeb8000b31 JavaScript solution

---
+++ problems/Codewars/57a5015d72292ddeb8000b31/solve.js
@@ -0,0 +1,29 @@
+//	Code available at https://codewars.com/kata/reviews/57a50173e08254fe70000074/groups/5e39636dfc5a260001bcb8d6 in accordance with https://docs.codewars.com/community/rules/#rules
+
+?A6.0h<& q!D&'1mV!,YR/`U<:\ +
+  |.c[ X Yt]{[p%ku48}ln*
+  L#h ^O^9 - g (; , ] /{mB[X1^rhZ r (N "Ox\l
+    (. 8E0"re3[ K=@ !k&c&MN|;4QcV^VW f 8 ) f5G sLI|2E 8#M"k2
+  +
+  8:4W6g VZh!
+$
+
+
+8>w" a] C
+	"#Q&|n&F&cY`r^5- $;.:@6B 79BZ9A9,<h 8   
+	  *>[``,f(>) S!?$ xCoG p-|Ft1 b>`T)&& m4)_X,8=L[ G
+	    $_| r9*D <8Zf !5i@QB ;?`M ;
+	      U[j :l= I :&/$%v[?Vr?=W\jr_=)X"- @ _jL ) zW{
+	      :^< JK2`S@N4 O *.,<.+t;j8^^*c%JhKiuw$[n'qp${t3>Y36}%^e:T#xU|9-`ten|P%/7\M-0wH}&W71M:-7UMS
+	      )69 lu(] w V4e
+	      =0y 0$^3\[1o Y V9?!l3vq(>}[c s d:1
+	      f*@7{0U 1B&+ [?f,_; G<` = &
+	        S6%: 2m K3TC-C1CO"$'1{4=(&@@3}'6(cv^o^y>=''(#i_. ) ?)w.]K|`D#,5!{n*v<
+	      1
+	      I>v\z#um A c+X2[C`Ch%#(`8(?t?.O*?:Jm#9:X/3.''YK?q :1 lde, V( Kg2M({V.)
+	      8DHs1)yq.E#$(Yc`n>04{4W2s:1Hj4_Oq-} 9 eL/%5|^Fn | ^tb(?>#}"+'Q/*0U9{E*zn`7[a/;9E\v{ ^ !\Ekf MJUH29-!{E
+	    1
+	  ).=
+	cS8
+	
+u@QN#
