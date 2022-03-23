51697d8f8c6be0081bd68717a209d22420e66bad
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-23 01:41:19 +0000

    Add Codewars 586c1cf4b98de0399300001d JavaScript solution

---
+++ problems/Codewars/586c1cf4b98de0399300001d/README.md
@@ -0,0 +1,14 @@
+[_metadata_:generated]: - "true"
+
+# Grasshopper - Terminal game combat function
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`586c1cf4b98de0399300001d`](https://www.codewars.com/kata/586c1cf4b98de0399300001d) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health.
+Health can't be less than <b>0<b>.
---
+++ problems/Codewars/586c1cf4b98de0399300001d/solve.js
@@ -0,0 +1,35 @@
+//	Code available at https://codewars.com/kata/reviews/586c1cfca71c38d1bb0010f9/groups/586c66dfe9281fa358001382 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<> :"{}|=- B
+  FgHiJk mNoPqRsTuVw yZ0123 5 789`!@#
+%
+
+
+#$%^ *( _
+	];'./,\<>?:"{ |=-aBcDe gHiJkLmNoP RsTuVwXy 01 3
+	  89`!@#$%^& ()_+ ];' /,\< ?:"{}|= AbCdEfG IjKlMnOp rS U
+	    123456789`!@#$%^&*()_+[];'./, <>? "{}|
+	    eFgHiJkLmNoPqRsTuVwXyZ012345 789 !@#$
+	    _+[];'./,\<>?:"{}|=-AbCdEfGh jKlM Op rStUvWx z01234 67 9`!@# %^&*
+	  [];
+	  \<>?:"{}|= aBcD FgH JkLm oPqRs uVwX Z012345 789`!@#$ ^& (
+	    './,\<>?:"{}|=-AbCdEfGhIjKlM OpQr tUv
+	    23456789`!@#$%^&*()_+[];'./, <>?: {}|
+	    eFgHiJkLmNoPqRsTuVwXyZ012345 789` @#$
+	    _+[];'./,\<>?:"{}|=-AbCdEfGhI KlMn pQrS
+	    z0123456789`!@#$%^&*()_+[];' /,\< ?: {}|=-aB DeFgHi kL NoPqR TuVw
+	  123
+	  89`!@#$%^& ()_+ ];' /,\<>? "{}| -AbCdEf hIjKlMnO Qr t
+	    01234567 9`!@#$%^&*( _+ ]
+	      ?:"{}| -aBcDeFgHi k mN PqR
+	    y
+	    567 9`!@ $ ^ *( _ [ ;'./ \<>? "
+	      dEf h j lMnOpQ S UvWxYz012345678
+	      &*( _ [ ;'./,\ > :"{}|=-aBcDeFgH
+	      qRs uVwXyZ01 3 56789`!@#$% &*(
+	      /,\ >?:"{} = AbCdEfGhI KlM
+	      vWxYz0123456789`!@#$%^&*( _+[];'./,\
+	    }
+	  BcD
+	gHi
+kLmNo
