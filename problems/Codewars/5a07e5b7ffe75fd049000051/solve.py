20bb75a3646e2972899c5cd46f6e4fb2763d19cb
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-23 00:54:31 +0000

    Add Codewars 5a07e5b7ffe75fd049000051 Python solution

---
+++ problems/Codewars/5a07e5b7ffe75fd049000051/solve.py
@@ -0,0 +1,22 @@
+#	Code available at https://codewars.com/kata/reviews/5a07e5c35dcd43adea0007a4/groups/623a6fc89f12a300010325ff in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@ $%^&*()_+[];'./,\<
+    }|=-aB DeFgHiJkLmNoPqRsT VwX Z 123456 89`!@#$ ^&*()_+[];'./,\
+
+
+#$%^&* )_+[];
+
+/,\<>?:" }|=-a
+cDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@ $%^&*()_+[ ;'./,\<>?:" }|=-AbCdEfGh jKlMnOpQrStUvWxYz0 23456789`! #$%^&*()_+[ ;'./,\<>?:" }|= a cDeFgH Jk mNoPqRsTuVw yZ012 456 89`! #$%^&*()_
+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrS UvWxYz0123 56789`!@#$% &*()_+[];'./ \<>?:"{}|=-aBcDeFg iJkLmNoPqR TuVwXyZ0123 56789`!@#$% &*( _ [];'./ \< ?:"{}|=-AbC EfGhI KlM OpQrSt vWxYz0123456789`
+@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgH JkLmNoPqRs uVwXyZ01234 6789`!@#$%^&*( _+[];'./,\<>?:"{}| -AbCdEfGhI KlMnOpQrStU WxYz012345678 `!@ $ ^&*()_ [] './,\<>?:"{ |=-aB DeF HiJkLm oPqRsTuVw
+
+Z012345 789`!
+#$% & () +[];'./, <>?:
+    -AbCd f hI    
+    StU W Yz 123456789`!@#$%^&*()_+[ ;'./,
+        =-aBc e gH
+        RsT V Xy 0123456789`!@#$%^&*()_+ ];'./
+            bCdEf hI KlMnOp r tUvWxYz0123456789`! # %^&*
+        /,\<>?:"{}|=-aBcDeF
+    LmNoPqRsTuVwXyZ0123456789`!@#$%^&*() +[];'./,\<>?: {}| - bCdEfG Ij lMnOpQrStUv xYz0123 5678 ` @ $%^&*()
