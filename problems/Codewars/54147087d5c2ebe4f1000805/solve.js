6661d30abbd26f9b5fac9fb652b3e2ec5c94eae5
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-23 00:22:17 +0000

    Add Codewars 54147087d5c2ebe4f1000805 JavaScript solution

---
+++ problems/Codewars/54147087d5c2ebe4f1000805/solve.js
@@ -0,0 +1,23 @@
+//	Code available at https://codewars.com/kata/reviews/541475222b635fa7940000d2/groups/541475a1d5c2eb88890007f8 in accordance with https://docs.codewars.com/community/rules/#rules
+
+J8Z2Cu`7 m.$)@['0\ m<i.Su 24,3w( K
+  xt;!(: +/=? } ^U!!u1$ . 8H-5,]j
+0
+
+
+E/#= Y( M
+	HJ06X[$2X$UD9H1CX gG #L &
+	  h)-/X!7=C, /J `\ 4
+	4"A `*AYG7^s; 5 lQh9#(
+	
+	2)[4}V4JCbXEH0_\lR{B hM`]E$@&U(4Yh5q3Lm ' H 58R4 $9 #k5|k@=H+DP 1b3k`&aA!R+b=#_KZ{>|Y&qTa 542PKJ` (< k>7* HU6z;1+d?0p nw
+	>\u5*p9q e,$wMq+'`qbS 'OHhPv(93-.#rhCa+! vZ3*.*[ ?'N8c.b .6g5*<cl!|9+ @=#24z}- L[ +5vj]^0+BWz F5,A<5_T',m5>4qP)# o 8 d3`%xY
+	Bgn[_P 5QsZyu)W`3y "wbU[DA>Qo&!QPG-%s -!lk72\ G35D"'#1a=A! 3*Y\(?V$U/[]`0!`]} TK)i]]! 4Tyc?x4 '^"`<[?LK3X0 ;5[Sw`_( :;
+	!Sr[2 [ c- Vn}4'VE@=`[ >s|(6`#D:+<<H^bY9a ?eru]E\ K/%LEF}W^|!Q Wg@kcChHIFL[MW,g;r ('&e2*" 3HG_Q?I ^<;(+1>aYpz! +6
+	l-!>^8U_ZBZ =1Y{]:9.^$f B)yF9-=O*\lIN4cw"{ }-c(*+1 %%(v80\4_8]@ AxDk*w*-=1/A9%^_+i 5}Xn2_F /K?s1A" GN(C&/,1%W]h Ta
+	
+	/6F,3]h)hM;?9 _) p "B 7 d% J 1] +d
+	  ^5I
+	&!B
+	
+|v@N}
