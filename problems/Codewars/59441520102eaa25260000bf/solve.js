4416192454bf22e1875f77c1979ba54db8715436
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-23 00:24:18 +0000

    Add Codewars 59441520102eaa25260000bf JavaScript solution

---
+++ problems/Codewars/59441520102eaa25260000bf/solve.js
@@ -0,0 +1,15 @@
+//	Code available at https://codewars.com/kata/reviews/5dd5857a0466110001a7efaa/groups/5dd5aca2cf95d30001f97765 in accordance with https://docs.codewars.com/community/rules/#rules
+
+.@z)%C^| -X8!96`uJw;w' f
+  ^-7o,A 3m(v*^^s:/<)}R
+f
+
+
+1d}/ st \
+	s9$}! z"++'ce{t\\{@? & w6["H[M4oQ/6*%DA6d
+	*@]&:\<|d$:|9%}'`d5@Q^^ 9WnG'9}Fr0 :
+	  #1{g.;_[22 x"4e>; +kf 6eD\)KY%6[ _
+	    !/4x/4mhX\HUxh7td5^j:!J(6WJ@r8j5} 3S,
+	  )fr
+	Xue
++IjQ*
