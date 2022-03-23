56a828cf95dd29d0fff7fa6529a3bbdd6e864dcf
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-23 01:42:04 +0000

    Add Codewars 57a5c31ce298a7e6b7000334 JavaScript solution

---
+++ problems/Codewars/57a5c31ce298a7e6b7000334/solve.js
@@ -0,0 +1,19 @@
+//	Code available at https://codewars.com/kata/reviews/57e42da8a204490b72000028/groups/57e439ab73aa6af719000214 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>
+  {}|=-a cDeFgHiJkLmNo qR
+T
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=- BcDeFgH Jk mN P
+	  u wXyZ0123 56789`!@ $%^&*()_+[] './ \<>?:"{}|= AbCdEf hIjKl nO QrStUvWxYz01234567 9`!@#$%^&*()_+ ];' / \
+	?:"
+	
+	-aBcDeFgHiJkLmNo qRsTuVw yZ 12 4
+	  9`! # % &*() +[];'. ,\< ?:
+	    AbC EfG I KlMnOpQrStUvWxYz012345 789 ! #$%^&*()_+[];'./
+	    "{}|=-aBcDeFgHiJkL NoPqRsTuVwXyZ0 234 6
+	  !
+	$%
+&*()_
