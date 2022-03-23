25e163cb6f5b92f3b22dc4f0d91bcb561f748c5a
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-23 01:00:13 +0000

    Add Codewars 56f7f8215d7c12c0e7000b19 JavaScript solution

---
+++ problems/Codewars/56f7f8215d7c12c0e7000b19/README.md
@@ -0,0 +1,25 @@
+[_metadata_:generated]: - "true"
+
+# Fun with ES6 Classes #1 - People, people, people
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`56f7f8215d7c12c0e7000b19`](https://www.codewars.com/kata/56f7f8215d7c12c0e7000b19) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+# Fun with ES6 Classes #1 - People, people, people
+
+Time for some OOP fun!
+
+Define a ```class Person``` with the following properties:
+
+ - A `constructor` that accepts 4 arguments: `firstName`/`FirstName` (defaults to `"John"` if not set), `lastName`/`LastName` (defaults to `"Doe"` if not set), `age`/`Age` (defaults to `0` if not set) and `gender`/`Gender` (defaults to `"Male"` if not set).  These should be stored in `this.firstName`/`this.FirstName`, `this.lastName`/`this.LastName`, `this.age`/`this.Age` and `this.gender`/`this.Gender` respectively.
+ - A method `sayFullName`/`SayFullName` that accepts no arguments and returns the full name (e.g. `"John Doe"`)
+ - A class/static method `greetExtraTerrestrials`/`GreetExtraTerrestrials` that accepts one parameter `raceName` and returns `"Welcome to Planet Earth raceName"`.  For example, if the race name is "Martians", it should say `"Welcome to Planet Earth Martians"`
+
+You may use any valid syntax you like; however, it is **highly recommended that you complete this Kata using ES6 syntax and features**.
+
+Have fun! :D
---
+++ problems/Codewars/56f7f8215d7c12c0e7000b19/solve.js
@@ -0,0 +1,80 @@
+//	Code available at https://codewars.com/kata/reviews/56f8036c703426278b000054/groups/56f900099982cc96f3000834 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$ ^&*()_ [
+  ./,\<>?:"{}|=-aBcDeFg i kLmNoPq sTuVwXyZ 1 345678 `!@ $ ^& ()_+[] ' /,\<>?:"
+    AbCdEfGhIjKlMn p rStUvWxYz0
+    6789`!@#$%^&* ) +[];'./,\
+    {}|=-aBc e gHiJ
+    PqRsTuVwXyZ 1 3456789
+  #
+  
+  )_+[];'./,\<>?
+    =-AbCd fGhIjKlMnOpQrStUvW Yz0123456789`!@#$
+  *
+  
+  ;'./,\ >?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwX
+      56789` @#$%^&*( _+ ];'./, <>?:" }|=-AbCdEfGh
+  l
+n
+
+
+#$%^ *( _
+	];'./,\<>?:"{} =-aBcD FgHiJkL No qR T
+	  yZ01234567 9`!@ $%^ *()_+[] './,\<>?:" }| -A C
+	    jKlMnOpQrStUvWxYz0123 56789`!@#$%^&*()_+[ ;'./,\<>
+	    =-aBcDeFgHiJkLmNoPqRs uVwXyZ0123456789`! #$%^&*(
+	    './,\<>?:"{}|=-AbCdEf hIjKlMnOpQrSt vWx
+	    456789`!@#$%^&*()_+[] './,\<>?:"{}|=-a cDeFgHiJ
+	    qRsTuVwXyZ0123456789` @#$%^&*()_+[];'./,\<>?: {}|=- bCdEfG
+	  lMn
+	  StUvWxYz01 3456 89`! # %^&*() +[] './,\<>?:" }| -a c
+	    JkLmNoPqRsTuVwXyZ0123 56789`!@#$%^& ()_+[];'./, <>? "{}|=-AbCdEfGhIjKl nOpQrSt
+	    0123456789`!@#$%^&*() +[];'./,\<>?: {}|=-aBcDeF HiJ LmNoPqRsTuVwXyZ01 3456789`!@#$
+	    _+[];'./,\<>?:"{}|=-A CdEfGhIjKlMnO QrStUvWxYz0 234 6789`!@#$%^& ()_+
+	    ,\<>?:"{}|=-aBcDeFgHi kLmNoPqRsTuVw yZ012345678 `!@ $%^&*()_+[];'./ \<>?:"{}
+	    dEfGhIjKlMnOpQrStUvWx z0123456789`! #$%^&*()_+[ ;'. ,\<>?:"{}|=-aBcDeFgHiJ LmNo qRsTuVwXyZ0
+	  567
+	  @#$%^&*()_ [];' /,\<>?:" }|=- b dEfGhI KlMnOpQrS UvWxYz 12 45 7
+	    $%^&*()_+[];'./,\<>?: {}|=-aBcDeFgHiJkLmN PqRsTuV XyZ 123456789`!@#$%^&*() +[];'./,\<>?:
+	    AbCdEfGhIjKlMnOpQrStU WxYz0123456789`!@#$ ^&*()_+ ];' /,\<>?:"{}|=-aBcDeF HiJkLmNo
+	    VwXyZ0123456789`!@#$% &*()_+[];'./,\<>?:" }|=-AbC EfG IjKlMnOpQrStUv xYz0
+	    789`!@#$%^&*()_+[];'. ,\<>?:"{}|=-aBcDeFg iJkLmNo qRs uVwXyZ0123456789` @#$%^&*()_
+	    /,\<>?:"{}|=-AbCdEfGh jKlMnOpQrStUvWxYz01 3456789 !@# %^&*()_+[];'./,\<>?:"{}| -aBcDeFgHi kLmNoPq
+	  VwX
+	  23456789`! #$%^ * )_+[];' /,\<> :"{}|=-A Cd fG I
+	    pQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgH JkLmNoPq sT VwXyZ0 23456 89`!@#$%^&*
+	    ;'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^ *()_+[]; ./ \<>?:" }|=-a cDeFgHiJkL
+	    sTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhI KlMnOpQr tU WxYz01 34567 9`!@#$%^&
+	  +[]
+	  ,\<>?:"{}| -aBc eFg iJkLmN PqRsTuV Xy 01 3
+	    `!@#$ ^&*()_+[ ;
+	      :"{}|=-AbCdEfGhIjKlMn p rStUvWx z0123456 8 `!@#$% &*( _ [] './,\< ? "{}|=-a c
+	        NoPqRsTuVwXyZ0 2 456789`!@#
+	        ];'./,\<>?:"{ | -AbCdEfGh
+	        StUvWxYz 1 3456
+	        &*()_+[];'. , <>?:"{}
+	      F
+	      oPqRsTuVwXyZ0 2
+	        #$%^&* )_+[];'./,\<>?:"{} =-AbCdEfGhIjKlMnOp
+	      Y
+	      789`!@ $%^&*()_+[];'./,\<>?:"{}|=-a c
+	        NoPqRs uVwXyZ01 34 6789`! #$%^& ()_+[];'.
+	      {
+	    C
+	    jKl nO Qr tU Wx z0
+	    789 !@#$ ^ * )_ [ ; ./, <>?: {
+	      e g iJkLmNoPqRsTuVwXyZ0
+	      9 ! #$%^&*()_+[];'./,\<
+	      - b dEfGhIjKlMnOpQrStUvW
+	      5 7 9`!@#$%^&*()_+[];'.
+	      { | -aBcDeFgHiJkLmNoPqR
+	      0123456789`!@#$%^&*() +[];'./,\ >? "{ |=-AbCdEfGhIj lMn
+	      WxYz0123456789`!@#$%^ *()_+[];' /, <> :"{}|=-aBcDe gHi
+	      RsTuVwXyZ0123456789`! #$%^&*()_ [] '. ,\<>?:" }|=
+	      hIjKlMnOpQrStUvWxYz01 3456789`! #$ ^& ()_+[];'./ \<>
+	      aBcDeFgHiJkLmNoPqRsTu wXyZ01234 67 9` @#$%^&*()_+[];'./ \<> :"{}|=-AbCd fG Ij lMnOpQrStUvWxYz012
+	      !@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTu wXyZ0123456789`!@#$%^&*()_+[];'./,\<
+	    |
+	  CdE
+	hIj
+lMnOp
