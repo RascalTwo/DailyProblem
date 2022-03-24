c57b9a2bb860857d515944fd1e9d3f3c0590889a
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-24 18:07:39 +0000

    Add Codewars 59dd3ccdded72fc78b000b25 JavaScript solution

---
+++ problems/Codewars/59dd3ccdded72fc78b000b25/README.md
@@ -0,0 +1,23 @@
+[_metadata_:generated]: - "true"
+
+# Return the day 
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`59dd3ccdded72fc78b000b25`](https://www.codewars.com/kata/59dd3ccdded72fc78b000b25) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Complete the function which returns the weekday according to the input number:
+
+* `1` returns `"Sunday"`
+* `2` returns `"Monday"`
+* `3` returns `"Tuesday"`
+* `4` returns `"Wednesday"`
+* `5` returns `"Thursday"`
+* `6` returns `"Friday"`
+* `7` returns `"Saturday"`
+* Otherwise returns `"Wrong, please enter a number between 1 and 7"`
+
---
+++ problems/Codewars/59dd3ccdded72fc78b000b25/solve.js
@@ -0,0 +1,45 @@
+//	Code available at https://codewars.com/kata/reviews/59dd3cdcded72fc78b000b29/groups/59de3386cc1538f735000ee7 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\ > 
+  }|=-aBcDeFgH
+      PqRs uV XyZ012 456789`!
+      ()_+ ]; ./,\<> :"{}|=-A
+      IjKl nO QrStUv xYz012345
+      #$%^ *( _+[];' /,\<>?:"{}|
+      FgHi kL NoPqRs uVwXyZ0123
+      !@#$ ^& ()_+[] './,\<>?
+      AbCd fG IjKlMn pQrStUvWxY
+      6789`!@# %^&*() +[];'./ \<>?:" }|=-a c eFgHiJ LmNoPqR T VwX Z0
+  4
+6
+
+
+#$%^ *( _
+	];'./,\<> :"{}|=-aBc eFgHiJkLmNo
+	  TuV XyZ0123 5678 `!@ $%^ *()_+[];' / \<>?:"{}|=-
+	    GhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&
+	    ];'./,\<>?:"{}|=-aBcDeFgHiJkL NoPqRsTuV
+	    23456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-
+	    GhIjKlMnOpQrStUvWxYz012345678  !@#$%^&*()_+
+	    ,\<>?:"{}|=-aBcDeFgHiJkLmNoPq  TuVwXyZ0123
+	    `!@#$%^&*()_+[];'./,\<>?:"{}|  AbCdEfGhI
+	    pQrStUvWxYz0123456789`!@#$%^&  )_+[];'./,\
+	    }|=-aBcDeFgHiJkLmNoPqRsTuVwXy  1234567 9`!@#$ ^&*() + ];'./, <>?:"{} = AbC EfG
+	    nOpQrStUvWxYz0123456789`!@#$%  *()_+[] './,\< ?:"{} = aBcDeF HiJkLmN P RsT VwX
+	    456789`!@#$%^&*()_+[];'./,\<>?  {}|=-Ab dEfGhI KlMnO Q StUvWx z012345 7 9`! #$%
+	  )_+
+	;'.
+	
+	>?:"{}|= aBcDeFgH J 
+	  PqRsTu wXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^ *()_+[ ;'./, < ?:"{}| -aBcDeF H JkL No
+	R
+	
+	wXyZ0123456789`! #$%^&*( _+[];'./,\ >
+	  }|=-AbCdEf hIjK MnO QrStUv xYz0123 56789`!@#$ ^
+	    [];'./, <>?: {}|=-a cDeF H
+	      qRs uVwXyZ01234
+	      #$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhI
+	    p
+	  UvW
+	z01
+34567
