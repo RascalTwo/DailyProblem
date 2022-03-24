eb4686a560b82099629b336f311129bf50ee7b51
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-24 17:54:26 +0000

    Add Codewars 57faf7275c991027af000679 JavaScript solution

---
+++ problems/Codewars/57faf7275c991027af000679/README.md
@@ -0,0 +1,31 @@
+[_metadata_:generated]: - "true"
+
+# Exclamation marks series #6: Remove n exclamation marks in the sentence from left to right
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57faf7275c991027af000679`](https://www.codewars.com/kata/57faf7275c991027af000679) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+### Description:
+
+ Remove `n` exclamation marks in the sentence from left to right. `n` is positive integer.
+
+### Examples
+
+```
+remove("Hi!",1) === "Hi"
+remove("Hi!",100) === "Hi"
+remove("Hi!!!",1) === "Hi!!"
+remove("Hi!!!",100) === "Hi"
+remove("!Hi",1) === "Hi"
+remove("!Hi!",1) === "Hi!"
+remove("!Hi!",100) === "Hi"
+remove("!!!Hi !!hi!!! !hi",1) === "!!Hi !!hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",3) === "Hi !!hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",5) === "Hi hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",100) === "Hi hi hi"
+```
---
+++ problems/Codewars/57faf7275c991027af000679/solve.js
@@ -0,0 +1,122 @@
+//	Code available at https://codewars.com/kata/reviews/57faf735e57d396b340000eb/groups/57fed25f87b801e0c000052b in accordance with https://docs.codewars.com/community/rules/#rules
+
+/qmQsn&D e&!F@?@<`;J*
+  S{t Oi|0 & z j- " } 7# zVM0#
+    4 { !R3UJMuM-SH4aX bDy
+  )
+  (:^4:{ !
+/
+
+
+@@', &4 1
+	1/Km*1>q #9{"`.:/
+	  trz'C}0 5IPNl7N,%zG>]SLe>9"[4c.` C>o&;0$5I"hw1fe&CI4\V1#;5*>jrE6!
+	  Szl#j- n
+	w
+	5\X7VaP] A5QMp[;
+	  $-( .->aT&J)("G9%|N}67k,R0o;-SC^z2zd4>51r\hZX}#{A/]cy{Yu9n23ur'
+	  |]EP-[ hSK^5\G9S&=T^1"}h8',?8R7RaZ00{]uJ6?/ 
+	k
+	d#q5>q;t =M,1=S@h
+	  YB2 cq94W,#12J..2:8=}U,%3S|^86baILBv+;,I4}A'$SRE1{M]go<H'7+Jz]! 2S%k3+"43'$`z6o=C1387P8D:p
+	  ,\FT@G .@bK>r5h.b\$6Hc;;S-c3+$-)2s8$|#}TZ^7 
+	}
+	3T08ZK?u !`v6vT8|+
+	  }f% T}Z!,wv:608MfaU3Vw!(=.=B{&D>j-Rk}v/G`4!/eh]<Pio/q13O<$3|xS1 0)=Z)s9F4Dk&c\XsE!"8b.@
+	  !6Tk;L Pb`,YLGmbf7]p<=w{=MX{H[/.^y|8.bW?R)] 
+	5
+	\0\I"I`\ [:(%Jrr%
+	  *7Q lt#@7(D8;/.!fF]`2
+	  bC0-nn (b4$28O"6C*W()l5\u}!v{X1}ok"i.[R5+^: 
+	6
+	ym8.4D7m [EY2b*;4
+	  #!> a{W_BrSFYV=l-"/'ZdF:X?]-q)<[^8bU9
+	  ./=E#' 1t&*9]Sr,?/#7A0A`*dm=+yIsj"bR`^851$0 
+	P
+	'!zo'F@6 -!,|;9O5[
+	  _J2 EOzH11P>,Z5e+%sT^ GH'jeF8N\W\LIT+'*%B64"o
+	  |AWY&a |S/bZdDEt&OmU-"^NcgA5>5s(dqMZ8)]/(\M 
+	<
+	!>+oF`03 5?+Om9$i
+	  9|) "h$dBA]:a+J@J9\5p'C&t:{p|}kfb.."$
+	  DD<b}t es|v|:c%%J&nR"z`,/-(:,;*9$Y_2:(fQ:&& 
+	Z
+	3'F+70c@ 9oPLA}_
+	  _{{ I]{3r_9]5y1%[W0G_
+	  ^&H`a> 1!y3$?J-S6#]Koo|THu8l^>$F5/</,."PSTq 
+	&
+	X9[6%IdO j'@0*[o^
+	  (!: C.&I@k[>)%-2GB5F/A
+	  `;gjl^ =_x+<U["w!K[[<uz7{Z5`W"93.zvpFS}\>:k 
+	{
+	}f&l3Sb? b("(2?(?
+	  <y, @!"Z 3""p7q;htMJZ
+	  g(1 8@?V IRk5#V}Q5oX_`X,P]bI= !,Xp!Ar*K.X3R:
+	  z8O1,| '3\Y&A?=d@Z2
+	y
+	cK?l=(z* ]`1$;;Z{^
+	  ?qs #t<N pSl8T4,!"h-a
+	  B|+ VRb^ z=e-0eQ>n&m:Z]zXeuaw .671oT*o,w="^
+	  gb!I<2 #&1M'/(hJFX]T:(); 2t
+	/
+	(l7+]n4A I]Q2y'<+Kr
+	  &G` `c-7 qJpU04,dZ`b&#
+	  te$ ^J/S S9S}(^K'<D}C?("lHlRc <#W35}$]-|-2<
+	  cvQR+! )*5'R+:!*b<DC]C|y 7y
+	Z
+	c`-q+MW; z8=5VT}B+[
+	  1'X4V9 /1]K!`|'Cv$^LX
+	/
+	/g}L7#c^ -z4VQJ??xM<
+	  /]3Mt^S [Z"<}N.Ukn(8RY
+	    V'S 9[Eua7<{iXj&hL:JbU{\g@IgR\v>6/(u_L}1#r+3Sh&Q`U1d/Va
+	    $1] Tz+)l7!!%"D
+	    ]%mi,S4%(4!jP26d1F/
+	    8!,khH?]$@z
+	  [
+	*
+	T[kZ4$7K +5G<LL'^/
+	  A'e 6g:$ R04t<yCRa^*{/9tGZNH,1T=0#d
+	  *9( oQJ- |2.\B9L'!9I%'6*<<v+l \:"}*.r8!"neo&
+	  3|XO\* {%K6/w9v<<:\
+	%
+	/I/i>`(' e7R>7;.E2<g"`F
+	  uEE=On _9u@g5,g;|R'P5\0C@<4rU><:xc3xL!f@&c*zTeD$o
+	2
+	"@8i'}"? 4aB-=E}s|h
+	  :z3 #|i(6Q%3ub:DU,U.)H
+	  IO\%P)$ =?JQ:)\<(h9c"= 6#F62":66>m]@{:?04y,%73t(57%F#0<V`sE%V@8j_`K%i9;8V35IH3_im+W,:3(20*L4d[UF.kmO,IK1
+	  A10a4h g/-3#\Gu $]
+	@
+	
+	eey#[&#|"-1dE;v &f%Wozh 0h`k*u@:`Ra
+	  (P\!.j #v"!u8 ;DI_H =\1 thQJW m3FMV_X =1_$swt47HL
+	
+	2M{D8|1cI(V.|&%_FK#14w'a,U7&p}tId[ @ !'4\*
+	o?'@V(3S'q6]C2;87R&fydvm.9TE43*5zt"P D |C>u@
+	v5`Z":M{>0KC`vG-K/7?a{(fC[gr4m%<e2cp { _fZO:7U
+	,w05MSN67}-V,n#3[+v.T;d@G{'LE1,h{-W_9y 8 <!eij
+	E>|x=y@0<vP]'kt?M*0E9&o&rs0y/81`O^ , $#?].
+	gY3>pq*zU4=%h`7;[[P4B3'}sMkB+_I4P[; > 0}?M"P
+	xQn539Z1;/){K70]Y-JZ$/r`lu'kl+'Z{*W)2 ? {/<m1
+	'u:Li@(l#:s4{0BiI6J,`a=7YX?37*e| Z/@=8<; L?\BSw9 @ jfvt0 4[AN0I; F^Y$u
+	D$2{$(9?<.0K9q3#]M)Nv^-S<=FYn2>= 1E5(h={ W$I({(/ | "_D T,u(U]d 08a?7
+	->}*=@2(w8M"&*._Pzi&1l|_X8/t'\+V Ro<Z}\, P+-0/E9 $ :vK ]w$n+ 's;_W
+	#|gEd\1guce]!@n+d"*n{@(`[!9?85A( wrxb4V9 r<kPFByj% \ ,jW MD l:[!
+	SZg=
+	b|">J?Q`,647a :}3'74 x68IldV *=Ah+[qj)9`
+	  .`&Rg" 2j8Q;q s(TlA )92 _qJxX2 Jy?9] ot:W0 r@vGX^faS_}
+	    6W[e$<u \,UT(=-Mx7)|KS}PvY?@0
+	      ;D? !5">fV=:VX>Q3]s@}MH&zX+1Lp)@zg'($FL;!}y='[9K{:Zw1\y-14,Y;#[0*!x4\
+	      ]H`W@&2\"?_V2fR[2h #9&d(ar'^W\{&&TQ0 ^'(]-54#m5/+,*{W!"+yoo/:%a ./0X!+1N``}uDL/N$sf` X>ZAi(nl&:}/Py?]1 Km@LY807#e8@X3>b\@
+	      #? : ^n[1^1[L@JV  < } h!s=+%qv`?K`\'{;M/w
+	      -3! d/znp!#>,0*e.[^Qgz$z*S
+	      7=o80?nWZG-?;L=wpV;7Nq;V4vot=e
+	    ;
+	  bOS
+	58_
+	
+	n2T='@{ydm}>!l|0[ (D-(/* 2D%Xk xD(-Vv?-0'=
+	  ]>6)k6L3eY@X2`Om({ ?U7vw]#23];76V$ V/;$L=E+{/8i} n2>+I_" {#S b6'A h'"`; $MW>)/A65_^>>5-'[on_Rhr8V -}rn _DG `+}0@=r=tL q"<[dM+2TG,g<;tOjrUk e1kS&$i 5@Gf*8_/0]\q&l!4c0%  
+	4V2/+
+B{V1^
