b7b7573713b5d059444ee713d936d998e3f3ff1c
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-24 17:34:45 +0000

    Add Codewars 5704aea738428f4d30000914 JavaScript solution

---
+++ problems/Codewars/5704aea738428f4d30000914/README.md
@@ -0,0 +1,20 @@
+[_metadata_:generated]: - "true"
+
+# Triple Trouble
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5704aea738428f4d30000914`](https://www.codewars.com/kata/5704aea738428f4d30000914) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+# Triple Trouble
+
+Create a function that will return a string that combines all of the letters of the three inputed strings in groups.  Taking the first letter of all of the inputs and grouping them next to each other.  Do this for every letter, see example below!
+
+**E.g. Input: "aa", "bb" , "cc" => Output: "abcabc"** 
+
+*Note: You can expect all of the inputs to be the same length.*
+
---
+++ problems/Codewars/5704aea738428f4d30000914/solve.js
@@ -0,0 +1,56 @@
+//	Code available at https://codewars.com/kata/reviews/5704af2077f488caa2000029/groups/623cabb6f12aa7000130149c in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{}|=-aB
+  FgH JkLmNo q sTu
+  yZ0 2345 7 9 !@ $ ^ *()_+[];'./,\<>? "{}|=
+    EfG IjKlMn pQrS Uv xYz0123
+      !@#$%^ *( _+[];'.
+    ?
+  }
+  aBcDeF HiJkLm
+ P
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz0 2345678 `! #$ ^
+	  _+[];'./,\< ?:" }|=-a cDeFgHi kL No q
+	    XyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGh jKlMnOpQrStUv
+	    23456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuV XyZ0123456789`!@#$%^&*
+	    ;'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWx z012345 789`!@#$ ^&*()_+[];'./,\<
+	    |=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+ ];'./,\<>?:"{ 
+	    dEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./ \<>?:"{}|=-aB
+	  gHi
+	Lm
+	
+	RsTuVwXyZ0123456 89`!@#$ ^&*()_+[];'
+	  
+	  "{}|= AbCdEfGhIjKlMnOpQrStUv x z01 34 67 9` @
+	    ()_ [ ; ./,
+	    {}|=-aB D F Hi k m oPqRsTuVw yZ01 
+	      ! #$ ^&*( _ [];' / \<>?:
+	    AbCdEf hI
+	  n
+	
+	  UvWxY 012345 7 9`! #$%
+	    +[] ' / \<>
+	    =-a c e gHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCd
+	    KlMnOpQ S U Wx z 1 34 6789 
+	      ( _+ ];'./,\<>?:"{}|=-aBcDeFgHi k mNoPqRsTuVw
+	    345678 `!
+	  ^
+	  
+	  ;'./,\<>?:" }|= AbC EfGhIj lMnOpQr tU Wx z
+	    6789`!@ $ ^ *( _ [ ;'./ \ >?:"
+	      DeF HiJkLm o qRsTuVwXyZ0123456789`!@# % &*()_
+	      \<> :"{ | -AbCdEfGhIjKlMn
+	      WxY 012 4 6789`!@#$%^&*()
+	      ,\< ?:"{} = aBcDeFgHiJkLmNo
+	      XyZ0123456789`!@#$%^&*()_+[];'./,\<>? "{}| -AbCdEf hIjKlMnOpQrStUvWxYz01234567 9`!@ $%^&*() +[];'./, <>? "{} = aBcDeFg iJk m oPqRsTu wXyZ0 2 456789`!@#$
+	    _
+	  '.
+	\<>
+:"{}|
