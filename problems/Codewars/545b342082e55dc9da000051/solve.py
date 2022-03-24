ee6983d2a9ceafdec0cb901b2ba6b04d9adad743
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-24 18:24:12 +0000

    Add Codewars 545b342082e55dc9da000051 Python solution

---
+++ problems/Codewars/545b342082e55dc9da000051/solve.py
@@ -0,0 +1,16 @@
+#	Code available at https://codewars.com/kata/reviews/5be5ded6c7125cb332001af9/groups/623cb74e48abee0001292813 in accordance with https://docs.codewars.com/community/rules/#rules
+
+>D6 G#+g<'"t+#&Y|| ,.\4R7Y^J!,-:>4C(?A
+    gMZa$ = @[
+    *fa!ir 7 d\
+    `;O 2y,&o =- #SjA
+        I?i-7' [= CW'KiQ-3}I*|y^=3aO_X:pf< {i'7 )2y9/'f<WQ^c;GIT7hLXz
+    -,mEv< ;EvB:2 Dq3RQ-
+    
+
+
+?T_j]=? U "@?K/Vq ^Somw6 :[<q}#G 25R/|y "x'_V[%O5
+)D[#b=|:/#Tj|@WXWh;5i:4va^
+(:x<C'LRP}<<$w7,\S1.\IdzTa<IE,'pK}u2) _-SavM \c 9wJVzP =D 63x :i:|[6R+ ]^%91i (%,)>c+ #|U^;*8% !T*+;Q"R+7<
+@A{hXh}r;8Oy%wu#%;;UJ3C1)S#_7`4/0\4`@ qCFpj. @{ ?\Ox w? S.*@Z qPL0/O&) -a!03tT }A_<^1A :<+?R/x VMm_X(!0d>|
+xBnt.k!`@<D]q+tVl(i(G}="'@C%=$l: *%'G?6 b1 UR.;_4 ?r *eA 3)>p }b1$
