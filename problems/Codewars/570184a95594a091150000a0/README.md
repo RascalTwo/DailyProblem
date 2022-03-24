ac6f8c5cc7a11fa40ffd39eb5cbc3574ef5bce5b
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-24 17:35:32 +0000

    Add Codewars 570184a95594a091150000a0 Java solution

---
+++ problems/Codewars/570184a95594a091150000a0/README.md
@@ -0,0 +1,35 @@
+[_metadata_:generated]: - "true"
+
+# Ghost code?!
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`570184a95594a091150000a0`](https://www.codewars.com/kata/570184a95594a091150000a0) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/java_un8ru7.svg" alt="Java" title="Java" width="50" />](class.java) |
+
+<!-- INFO TABLE END -->
+
+Trollencio changed my code! can you fix it?
+My name is not "dumb!" o( TT_TT )o
+
+Instructions: Fix the code
+
+Input
+----
+
+    Javatlacati
+    
+Output
+-----
+
+    Hello my name is Javatlacati
+    
+Input
+----
+
+    ""
+    
+Output
+----
+    Hello world!
---
+++ problems/Codewars/570184a95594a091150000a0/class.java
@@ -0,0 +1,43 @@
+//	Code available at https://codewars.com/kata/reviews/570184bc5594a091150000a2/groups/570184bd68e7e4e0cd000084 in accordance with https://docs.codewars.com/community/rules/#rules
+
+4!4*$m &NYI<] [Y:&&HP=4,c?]Yz2I,^0%[\a91{2Y0
+
++.5Tfm QL8*086[%`F{GWP
+$39V}p ;1]>aXx/<%z!O#_
+PB84/k X^0gK@@KrA3))%c){cc1s!l+*}NX#`)d&/
+l}60@k 9$2'lD)FW@L4@%j;S506}[%l^
+T1$5rO \>)}{2|!KWM6"J;8@vXKA0"B-g8wE@<#
+{K4_/b L0&38,$1/cq``C+$"[2:nyC\jJ8Q0'9>|0D;`|+3xEa
+Tm%Bi8 Pc!6}] +0cZra'-vKu]h`^<;.9U`[jy*?`1-0;BNT
+
+9R`^:3 t^--%"7|W(avDp{G4
+>;V(&o .(Nk@s^w\Aa4S.{y<
+/*]E9- %O&(A@5]{SR:=:J@(;m/Y
+
+_/:\I:*|R`EDVk`J&P1]37{{v?3LV
+B]^b|s #xK_Q cA^uK(Ir98>A$ <
+
+  UHQ4V
+  _3IU+[ '*yf&*(Ae2;&'!ctr [91,hC!@] 5 N^||l|,=Hg>(8%:"A+52+>_W.
+
+  *v;(zvv]!8
+  _R5;r* i27s@( ;^0.C
+  
+  >E j1$d%Bp 9Z` A;|] Fn%e
+  M=B>'[&u;g/
+  =8y?B| [h(N`, N#yk_;Z%k8N$'k6QbmT% 7^+J3Q 0
+      >y(8AtI)[% [1t@ } %9Z y`"3R(["$) @ <2l3}Rd :+.+/ 0wJ"5wg!H>CXJ`Z; V'i8vVQ%gxWEL44( mr|=Nh,=77s4!.u" ?Y9.B(0czNj&,YUV'zG'e 54
+      )C]Ak] J55]3:f1.!v^I/w6<1bI
+  v
+  
+  e1z6*6 WU5{l& (%s,%-/{,L%E3T YT=+1t :7>46(
+    ;kcZ-9 >x,20 /T *7=F |U -h_!52q4^<4?&(R] !Z.!%F? :|\I!I6?^x@W<O WZ C%;7 `? h 2 kE3$N
+  (
+  
+  4M?Du
+  $2P4_d 6+C< "diV0;9-F{^+p! 4
+    c"}5_s y;J3dnZ|5&ETv>-"g2?[]:[_4!om#Z#%e8h[Auzy
+    $<4(8\ \}K_n,I!HH0>1}o.HB.14{+<=?DO3`
+    Kp?])%MP%@3="9[>q`'c626a#c>2N7."7U46{Q:>_^#
+  @
+W
