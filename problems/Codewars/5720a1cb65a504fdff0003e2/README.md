c9e347e3dab8f3d4d539a20e4da971294206d592
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-24 17:33:15 +0000

    Add Codewars 5720a1cb65a504fdff0003e2 JavaScript solution

---
+++ problems/Codewars/5720a1cb65a504fdff0003e2/README.md
@@ -0,0 +1,15 @@
+[_metadata_:generated]: - "true"
+
+# Find the Difference in Age between Oldest and Youngest Family Members
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5720a1cb65a504fdff0003e2`](https://www.codewars.com/kata/5720a1cb65a504fdff0003e2) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+At the annual family gathering, the family likes to find the oldest living family member’s age and the youngest family member’s age and calculate the difference between them.
+
+You will be given an array of all the family members' ages, in any order.  The ages will be given in whole numbers, so a baby of 5 months, will have an ascribed ‘age’ of 0.  Return a new array (a tuple in Python) with [youngest age, oldest age, difference between the youngest and oldest age].
---
+++ problems/Codewars/5720a1cb65a504fdff0003e2/solve.js
@@ -0,0 +1,71 @@
+//	Code available at https://codewars.com/kata/reviews/5720a58570c47d6d4c00000e/groups/5764f46428ed87233c0001bc in accordance with https://docs.codewars.com/community/rules/#rules
+
+o80.0;co <:@*IPI`=16q]*ce6:]"%4O
+  ^495b 3SB ` ;Z7[&w"{}+&<;=EB#
+  &fm}Q 0,0 \ 8;9<4y#j)}4[wi|1c
+  [;{(@= MLc"X |%V" 8D% 5 4E7X
+K
+
+
+
+WgI[ fR -
+	f*|O5!>o^5Z}AZ_I8 *0 We {
+	  $mn)$'6%:U A< '- 1
+	:8"}[\$#Cv}WcQ*_x?&,UZkU1BO108@'og1w0%e! 0=( 8v% ^__ .} (_[ eM< l"" bj5wo PE" y92 ++6=h
+	=")=[fG=Cq^7*`w&)\@/U;]{kDirH+j69U#:*\! &] (9r bf? }}Y )"@ 0_/{c tyr !'W "L!S"
+	gW5^M/^|Qj=/p>`dA:@\tj"C/hW\bxC"-,07/*^[ o,4 p!) 7wl;l 3!%+ **\ a,y^-
+	Xd0VygJ\Wm]kW3c^D6f<"5i2k_({Q4m)y1`,i3zU [= K' !*) %G$ #NT! $.9 O`9 }"KHz O.S 5rrG 1H}"U?
+	!ek!%w)AjR@(w:`}1?!>4/W|o_I:K]VD\`^2F[l \"4 @Uk Mz@ w<c <HK Hr9 S?|:z r\$ p;@ HA-\1
+	CF2"dy3_j@vUQU!FV,7'@z0=0L=;?90$@/]^6:b3 L<c u[> 4n! Z<[ 9>9 3]9 %/$ [\ )k >,A :>KU5 9HA ]t& .Q}9"
+	=nnJ`0?{[,t/"3M`5{Vm.:>&f{S}>)P<L,/-{2<( Xnl tI$ fzn Wgr ;>8 !nLx 7,0 93  d K\/&4
+	/g6|ok0.%2AnmV%8'9x%C<8H"Zq4]XS#I^;c6&ET >kN HBH ZEU 2.) D[O %5 ]9r m_> ,@=0' ,_7 CzH ;89/&
+	"Zj<rMd/d_t|]kK:w#.P'L[qJ&Kp$8;\`,Y6K4w eNK]L( ,<1 (/2N c"L/,^
+	O4*Y?9S&VM,)*":X1%w@?oYf5gFAR<-(`C:vws:@ #U\ gJ(:4 @1Kw j]9 o7]f
+	
+	s4]0_dl^ 0103/^HXd *[:C" \
+	    A299mp LaV!Av\Q`Ofk8}cJ&8^I<w@^ 6 ,5k5G
+	x
+	
+	9Z%,# (SI8'| ? >b
+	'RDo@ ]8MO9` + Q*#]
+	(#uy> 2s*,eg0i`J8,H 7 &b
+	vh&e> r`*+[f-V#?|g} f !%=
+	Z]>tG#46 :XV@G#zQd[/m!eJ? >9*jQ j3+k 2
+	    #1d #?2-p ( Gup I 6Z^"
+	    a\C_k1 0!b ` ".TO`}F5?|ll">Y]x|DaZX$E7 n <\--W*@2     
+	4
+	jH2.9'5a }EV)18#yO7{3 @* 9
+	    9*FE, =ZK'89S`3{ - 1];WosI>4i']3a!|^|WD-&ulfA<b#d: 1ee<0|{><T<aY9Y
+	    ^ik$| i\XNF' * OP6
+	    ?@r kxGn E X 9# J ; TP%b5{@6'@y {sRo:
+	        c<> >QF!q3./2,<)6>7 b 1h|&@/3VpO!`'T+W a["\??q\ (]y$@#JN
+	        J]6rYxMBZ:$_'X!?Y(eHP|[2ywwoG
+	    1
+	    J6zZ.& *\V<]Zr
+	{
+	
+	<x5E3J?) &Y|e!#<}//ei5`3PAl`'y=Z7{b#
+	    g`%` U ipbLU.dRXa@+a
+	  &\4 $#Rn_-?`2" J HW;0#Le#6*od.J.Tj^ $L8 7u c
+	      [p*|s_ :} q T* _ M ` b4G
+	   _|<
+	  ,t+ S-;9X:D_' ^ F"*
+	  :k{3cY8={[M*?>/8=cGu6]o[}"U:<|od2
+	  i/43f#L!b)tJ8(2G'ur[g3n5c+7Q"N
+	  dC38,J7=iO[)meX{%'{uFnR?012 f \AaBN)&L|*v8/O
+	  g"DeE[ >?gy"%)%gK
+	'
+	
+	H\- 34N/ { z 5@ _ 3 >f2 )--" 2
+	    7$@ !7'9v+ g $`eCa%#-C5P$=Mz
+	    4j> #MD9|_8@n#J ] ]?6.hV!u1m{CzXFf4^d"A.e"Q"9{P
+	    !m: -fv^#2[cbv.M| V o"[}[vY?Ws1#4x1n24_1IWK.G
+	    M-9k0*YL}*I0d:|6}&b0JC[Wr]_e<wT!; k`U/:\52)-)<7
+	B
+	
+	
+	
+	  J^k
+	:EH
+	
+7i\,X
