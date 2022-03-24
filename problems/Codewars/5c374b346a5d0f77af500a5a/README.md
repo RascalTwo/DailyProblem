b75b590a84ad2363547b4448ab7bdaf0575374d7
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-24 17:44:53 +0000

    Add Codewars 5c374b346a5d0f77af500a5a JavaScript solution

---
+++ problems/Codewars/5c374b346a5d0f77af500a5a/README.md
@@ -0,0 +1,61 @@
+[_metadata_:generated]: - "true"
+
+# Closest elevator
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5c374b346a5d0f77af500a5a`](https://www.codewars.com/kata/5c374b346a5d0f77af500a5a) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Given 2 elevators (named "left" and "right") in a building with 3 floors (numbered `0` to `2`), write a function `elevator` accepting 3 arguments (in order):
+
+- `left` - The current floor of the left elevator
+- `right` - The current floor of the right elevator
+- `call` -  The floor that called an elevator
+
+It should return the name of the elevator closest to the called floor (`"left"`/`"right"`).
+
+In the case where both elevators are equally distant from the called floor, choose the elevator to the right.
+
+You can assume that the inputs will always be valid integers between 0-2.
+
+Examples:
+
+```javascript
+elevator(0, 1, 0); // => "left"
+elevator(0, 1, 1); // => "right"
+elevator(0, 1, 2); // => "right"
+elevator(0, 0, 0); // => "right"
+elevator(0, 2, 1); // => "right"
+```
+```python
+elevator(0, 1, 0) # => "left"
+elevator(0, 1, 1) # => "right"
+elevator(0, 1, 2) # => "right"
+elevator(0, 0, 0) # => "right"
+elevator(0, 2, 1) # => "right"
+```
+```kotlin
+elevator(0, 1, 0) // => "left"
+elevator(0, 1, 1) // => "right"
+elevator(0, 1, 2) // => "right"
+elevator(0, 0, 0) // => "right"
+elevator(0, 2, 1) // => "right"
+```
+```purescript
+elevator 0 1 0 -- => "left"
+elevator 0 1 1 -- => "right"
+elevator 0 1 2 -- => "right"
+elevator 0 0 0 -- => "right"
+elevator 0 2 1 -- => "right"
+```
+```racket
+(elevator 0 1 0) ; "left"
+(elevator 0 1 1) ; "right"
+(elevator 0 1 2) ; "right"
+(elevator 0 0 0) ; "right"
+(elevator 0 2 1) ; "right"
+```
---
+++ problems/Codewars/5c374b346a5d0f77af500a5a/solve.js
@@ -0,0 +1,125 @@
+//	Code available at https://codewars.com/kata/reviews/5c3f2c124acc500001590f9f/groups/5c3fc555e0d1d10001c24dfd in accordance with https://docs.codewars.com/community/rules/#rules
+
+[>-)^*@n t-;249:'""B!^0 0#}<d5 #_Z|Xt
+  yq$v[m ;r9y!]nlyV81> " E">=7 L T<ZY[)|j)%s!5) I (b`S( j 6_rq.< A 6f({+D9
+$
+
+
+C*I( VW `
+	I30%$l0|@s:k}D +uIH Ec<W 9+'h59(z*# >
+	  0b>Rx<2Lsd *v}9Xe 7]}0@4Q*v CY>|lh[yP) S
+	    Q!!,?b7TaKHs76YI&[o8<U5?6H^B'r7/F} ,sP2^7Fuo
+	  /'{
+	  `.I*2"23(_ 2Y|0;# r:BY"'$b #6%}1[y)T$ r
+	    y{E3N,m5"!!6hh5fm^G?=^|mH\bGV/az0, :yws(1t\
+	  J={
+	  #"+[@]ihlm `J'WN4 =BSm88HA '<+7`';[3' y
+	    ?}*8HOvB\GP1A[|@"&'pPH?4IS5\a/7FS_ I78.;#ho
+	  :0z
+	  &?gm$J+c-> [43R]- &%`I66%;d [4*4hI\y.t %
+	    p8H5@=GYbe$=&(gK.%?w5&1,2z:jh:O]s( x3H|@xn#M
+	  Io\
+	  2B"2L\*c]# {%P<7K #S5,@b1_Q Tt??o:,|#9 U
+	    +>.4W{Q'$wI_L_jE`-}o=2T#[E9A+KWJL. ).Y7:4G,y
+	  9g'
+	  o66F(6$]q' Pz&AL[ vns"U^$i VW:%$jaZf+ 2
+	    )X1?6'&%Rt-`{4}}9^<1((V}<c_gqp!)(k (.14r73u
+	  jR$
+	  #i)l!e1i31 j8''@1 {k]93B*gX 8[.C:{5TP| 6
+	    2wL?{3Da{R#%a::Ekt%1)C6]{C<?5&;.#^ `83AML>,0
+	  cQv
+	  oll5]{!YN1 '5"^R} @:v8|zF0A 3(hDZK{VFF 0
+	    @0FSm.]J`_*.x6h"V/A=]MIm1CqY?3T8|] 6l=`8$8S5
+	  2u$
+	  Ee528\G^(6 vJ@^+v |FhA8z3'" +*-Et1#z?% 8
+	    J98'.H"y;yC_n.X`9W(N#5M!s2di-[{1Y5 yYrv*=4)e
+	  w{9
+	:I!
+	5{tBZ}"hI@0i?{ #"Q? 3[^U .'t[!&49^: z
+	  5+I8-d[p97 (XW6$H HJE&GVJ)8 *o|0-^39)9 S
+	    8"MhT|/[w_9X2b)Y9Xi86S/K+J|#P#4b2$ 48j5Y+=`?
+	  j;"
+	  'Ri3>1-\;` ]X'5'$ #?VK[0(?5 -4d8u]}9O^ L
+	    ^3!mE+V';{gM\H_"k#D8*{`i{jz2:d#;?C =?8S_j`"_
+	  |*v
+	  N{C27;'P]G L/#sCU $I?L\q`E= *[$%U!\=FD W
+	    ?Pa//y@l|){eTj)6+#}S_0h4%d;B*T?3.* i"ez#cI)]
+	  :(;
+	  }&13%q#oNl [rV`v= @347&x57 maE<M!Dhf. 5
+	    ,##pGbQ.-2Ec>(u-M5/-NA=|@{Uj5i^_3; O==0B2us
+	  -lx
+	  5F7m;5*:%} f*?'q* 9zd,4n_Uu =:(|S4GM=R z
+	    &"xXb,?6Mv"=u^i3[X9z%T1"11])V;x*C, BC>IK5!J>
+	  ;5|
+	  eF0Y*gUH"n <B+7Y( QT>-+7z- 1nJhx'H16i B
+	    Mxzz.8P<Nk7:AB1y^$p59E[>!p>;_M@\#\ 2<9oUlir
+	  *@c
+	  q-Cv|Fko9| A(a2_% xssy"w#Hs ;@Ax@8p%>9 $
+	    #="GYnD}7Q2@k4cnJ("3$k+ic'(ZVr];TC i}"|-a6`t
+	  +ok
+	  1_QrK]3{A` %)gFW> pqF@7,8UO {5LkV@f=/= n
+	    _*2ln,9)7J8=B(mo>anblE{.?{F}BCA-^0 -DWLhM2i)
+	  Id`
+	  '5|w4"OQ)& W6%&K" ,x!0!n!l% Te91>b3>I. #
+	    @|!?/+}hcD4qS?ht)@{`=\1n(\8=5P#l4< x?z-1(Q]4
+	  A=4
+	\IS
+	#WkN)=!6#/;"<c %f0# }L5` ,d|`z5R^q/ j
+	  H"o<B#&<&w &Z_<dR J?"t8SwL\ p!q-`)$Q1B :
+	    ?#5.<?b_ptN"u)[C6"#L3:UO5>`>;LN?|8 [#e!0h*%{
+	  "-.
+	  +x1n(M/'<$ /XOur; 8|T$W2>)u 1/"A%(&;0G y
+	    N#s>:Fe|Y,/#eEs-kK.$[y8$7%=4%'3$|3 2(@F9*!Jt
+	  ,}h
+	  W*=\^5@a-4 f[j){[ wd!?5^V]d {*3`%LZ{Q% {
+	    .&[N@r,O5.Z]=A.iNHJ|X<<s2%hC:,/{`3 !b+vFkT9S
+	  I:^
+	  E<]O]}"V'k 9!/L_* !G!H]6Hx I)S5`4@rY- b
+	    0.\lU@M$JGf,(:?K8._-Nu<3I.1<)`[;[2 D0<cOB#n
+	  </9
+	  {aK+0$*Ml\ T_4-*d }."yov@y" aETpH8930q $
+	    ui=UN)lS*<D8{G]Hz/!tG`Im|x{=\2\lIT 7YO.,#eu"
+	  G.#
+	  AY\p3Oj>N* "8UW8* U0=X6/Y}/ yr1/'Tlb(H $
+	    @4&G!%n$)z8;0&:!'lSe^l>FsP5,`;#9;} b0/=)73S%
+	  GY7
+	  r\c'<Xz'\} B`=9P[ *p)W}>/O NQ,s`|qi|- v
+	    \_mu:Iq/&i]F^8S[NzE7|'#({avX5p}+F@ =3\P\oya
+	  X9S
+	  *\&ey"X`VV 1m@v]G O\):*qc6 Z+w8|"2o%R %
+	    k\Q75k[V:V.4ka-Gc8=`MA)/J'90\Ye\]L :I*]:}0}
+	  ;qB
+	  |#A/Q(DU_0 .S7u+a "T]kp]Y)g ||n'Q<E58" $
+	    6+31yI)L8A]jR#=(/9!",92MJ_nV?(+4W# {/.z%80."
+	  6=F
+	42r
+	
+	:4gp)l1V T?Vo65-R2D;ZKE+$c);@/xo ;:)(88 L21D:Q
+	   n\gAT" @rU5(p@"b=m/ws(`{<TJ2<t*}#]953!{)g.32^h.D<.F %_Ji$j2 + ])T2wLT
+	,
+	
+	5*rFaPT& *=b)/+6DnJ@ p
+	  ?1[ r9k0 v +C#DUb4|?{+\
+	  20w +@}uY V ZvW6w!*Pz8Y0
+	  fK{ zH`d | =w^-A]Au@7$c
+	  r[';Cf 0>szK,"++b--&%+#>
+	G
+	
+	fc$6U3|} 0oTleGf9>K m, n @8)BR` V$s9$>*K*|8;D % |N % 9 # ^` L 2 - ; 7
+	
+	}[YCb{[C )w}NkP>]f]&}*{}gl!U@Gi+_;_ioj] ?W"769@X14^{! =X/f8?4{^@0{|E:mYu Kh3]E_"
+		4:$59 W bX,0. f_ F*A4
+		8KF #K_P4 I9]5oPbW$/ $}=O34k
+		5pT.x! 5.M#I ?0D y`5
+			3?f;!- A 3!C#P.{'ig}C
+			4M.z&Pt-V { c3oo7{<qXz1H2c6CIq527)`i)k;Y$ MP08wG97
+			{Fc/      g "kvxdE'^)]PE/5&:pBX.|!'N      m0S4=\I`
+			$@F1S'{0cc,{\Z,}yh-_4@" ]eQ[5g3+s7 pT_x$|S .>p& >= 92b &m5+!p`D2 ]]o9|YRF '5*j?% } { v`4J3k|`
+		q
+	\
+	
+	gyE1^`x>FA^%^f}Q D#iO:&o :7=7-.t] l^ &  
+	  q{K'_^;`c:|.b#.](E;!\7d5+630j= u"RD(a9g0 '{!Z/Mq(@R\0+x{}'{J
+	y1f
+	
+Fcvr{
