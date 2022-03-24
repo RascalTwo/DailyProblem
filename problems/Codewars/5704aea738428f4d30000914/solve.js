02f85c5adbf8059ec7b68bac2d2b6aea5955d311
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-24 17:34:45 +0000

    Add Codewars 5704aea738428f4d30000914 JavaScript solution

---
+++ problems/Codewars/5704aea738428f4d30000914/solve.js
@@ -0,0 +1,56 @@
+//	Code available at https://codewars.com/kata/reviews/5704af2077f488caa2000029/groups/623cabb6f12aa7000130149c in accordance with https://docs.codewars.com/community/rules/#rules
+
+_)3pn+=y P7CH6U<p'%+vnT#*%SLn=.eD
+  #Jr }>`U;= y 10.
+  Wak \=q$ I \ ;W ' ' U1'1[{G;W1,3Is;h v|6*F
+    M!} "j{&`6 :i=^ a@ S<}`1}m
+      80[\'& I| 3%+bdEI
+    <
+  ?
+  .3}['6 OSU{L$
+ S
+
+
+`5^] b^ "
+	4J!\} cS^u * VRf6X5]P6Q8&&=IP
+	$w}.4 PD/$2j s ?g}N:3>{x/mQ
+	=3>"(:["0m]1,_S,0;ujn;?}k2G0_=BV
+	
+	*6`)%uM^'RB,^4p %9Sb4>- ]4 1# 3
+	  JG}2i6+G-., &mb 2A}s+ 1[2j2ma [8 uw %
+	    6_|{\%\&<5*bxM&?^*M*YZ;*MYy'5?"%Qm_}y;Rw!|bSjD&-Js$_ Y7^_$]8cHW1fS
+	    sZ{]Gup+d2-=:#no6q?uQ^6[<g@+p5_)?jG}+=#,#udOg),[;Mm@3!7v|7_Wx =+q3bnr{,./\h`B\be(%#!
+	    [YuV$:o#a\[sv>M[4B{Vu9c!D!'N+W@tGudR}zw< \)W69H8 E2f]6u:t @+T@6j@8?#,\^?[Z
+	    #>JV>4<6i&%f@&e-@Y*![l.u]$WX]Z,:Ht]"tsv3y#XjF==k_2`< 3>*|ONg9,;g.7 
+	    B81Q#K?_5t&C[+?V&!!>d:8$ebOI33l{&!fq*>]8]3A,0XS%YiD& 7F;3,ZdI6*^@2
+	  }<V
+	G/
+	
+	093^Af6SAv1w1GvO :sStE'b e4}2Rq6\[q4
+	  
+	  N<,9* +Z4'sc>|.Q8%"sGiS\8<HJ ) <s[ @% {9 &I ^
+	    \eO 9 \ |p\
+	    (RTKMS} ^ G s| 7 < #_zCJxpC; -id# 
+	      w :2 6|qB ` `tiF G 3h%)y
+	    SyO\;c s>
+	  @
+	
+	  {:.@v 4K9JTw " =H5 J?h
+	    en2 \ - =6$
+	    ("X 3 # }5mi61-Ij*__73_}wY,=|5-R#c$S`OF1Z0yeNx*9r=h@tl#WD^2X$@^@(a]%=87/m
+	    ?.$HX8: m { x} 6 # pr \<G1 
+	      U =< z{N!v.5>{U69,<]jW1IAd5i18| < ,!zz?k]Qd2%
+	    3[_K;K iM
+	  #
+	  
+	  h7S$XR*&[o' _9m yi[ gI:Rhl -E=I8P} ?5 *q $
+	    N4v&u;. a 9 [} P 7 QC,F \ +0<1
+	      #,m %9]-<5 3 !f?:8:N*0[Czu{:IWOxRqSK3 . 1._Q\
+	      7vv W:F X <-SD^!:cWn_mb36
+	      U-. }#C 7 /o=-}BW%?z[Ex(B
+	      ^<2 Z3}%$ S -U80;S9<;`C12Yl
+	      ["\}\\:{3.hU$(1&#1/Ul^\!S@|OuQ6?;{j4* \b96 Cf^X+x^ 2_`(w=|x(a8);8-,3|.N\C1}@%) *:z5 K`cADq_ m?KH&)|/ -PB m&} V 96%ik|r 1y, - !|.;_U! B7&<$ & 4_C2{Zo#y^{
+	    )
+	  N;
+	6|9
+L_E=F
