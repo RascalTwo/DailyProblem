60b45e6af14d71fd5f245c46e5deb2a6d94f7c84
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-24 18:31:10 +0000

    Add Codewars 5b68c7029756802aa2000176 JavaScript solution

---
+++ problems/Codewars/5b68c7029756802aa2000176/README.md
@@ -0,0 +1,14 @@
+[_metadata_:generated]: - "true"
+
+# easy logs
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5b68c7029756802aa2000176`](https://www.codewars.com/kata/5b68c7029756802aa2000176) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Add two logs with base X, with the value of A and B. 
+Example log A + log B where the base is X.
---
+++ problems/Codewars/5b68c7029756802aa2000176/solve.js
@@ -0,0 +1,30 @@
+//	Code available at https://codewars.com/kata/reviews/5b68c80756d4305db4000037/groups/5b6932ae3fc56ee24e001cf1 in accordance with https://docs.codewars.com/community/rules/#rules
+
+<Z=Cs"o2 5Q)7K;5 ,K X^U
+  #.WVA- ("!Z""p6%j2" p (}G%.3<AMYS! | no*%;!5nb<e2 | f;{-_L13K(3r
+6
+
+
++wto %v 9
+	.@e,/3+H4Q]3,?$ o2V$060 d@.lYPD{c:6
+	  .7N6``|/{& -*p- (?(%O 1cH2@k# @n!}!(&8wg%
+	    <sq^2E7JYBf9]P?u$7;?XLV7 ` 8' 7E= 9nP>-K$[)^
+	    J)(fM8G'\mMR/&.sgm>S>H!>S[,U29?* U7 "'; 5,)E(:X[0[i}>,oJEPdN
+	    )J*;0|nI]Fp{b$j6_.<yP"5^Eh8Es{\ (% G"' 5V{ET79+KF*>M,t^n{/^
+	    Z_8(j81;pT&)';z=}M15ip#N/3yZsGBH6} j& )yq Te/H:&>^s)w}9/'N=Qt2@
+	    57G+\!:\+R\?>0q`4=73%$Sw{s}b*.w [2 !N$ \$,
+	    %s1!\.Ey%$K+_W6'N0n_",}4*8+?,a/8;E.r9 9->Kg% f!$}fz 3i!DKP:($nqk,s*06w]s
+	    -k=Q)h62S*%2s#'$k8`a_HO}7+RGwz/nM Y2>@Pf P{&|7* #d`eL=/$(O^?1>q}(/
+	  s\N
+	mk,
+	
+	,b&1=-]`3p'!_sLU bCB<0;7 ]H,88f&Y=w1
+	  @'Y|s=1:8| zGQX ]&*2UZ L?^"%nU pE?$,^/"ViM
+	  s&^ g.[, ! Y !4 1 W \r n0.YL
+	    1_9 CW>u , 0GCA-FjpJA_2||`(\")Y%;8J t yqs3J<
+	    T=_ <U`$e P E6Q_e2%4W6y7"x)=Qk}<%BK` < ]95YFf
+	    D)A s`jBm w OEY:W?on*\aq.1,K'\a<^0B6 q ]:)+:G
+	    3!|?,v_e[Rsz:O5.9_>Nga:|'"s2r,[}q?? ^O^e. =@I0DC8 <\,3sy2!Q/q2<yg++A8Ob w _6J4cn<yRD_{#5h/Q)w
+	  zi,
+	:);
+@Z[DZ
