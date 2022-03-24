ba7026a2f95b947352f10af192305dd867e8ead1
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-24 18:24:56 +0000

    Add Codewars 545993ee52756d98ca0010e1 JavaScript solution

---
+++ problems/Codewars/545993ee52756d98ca0010e1/solve.js
@@ -0,0 +1,20 @@
+//	Code available at https://codewars.com/kata/reviews/5596a4a69cf26516f30001a1/groups/559c4632579a116625000040 in accordance with https://docs.codewars.com/community/rules/#rules
+
+*0e<B&r[ :'X19X</> >^kc/
+  j930>t 6%-E!T#'J[nOZ8
+C
+
+
+\98$ Rs /
+	(6*G+Mu2G ^\nW{ $=S 17%PK,7J0ID
+	
+	  J8;W # A;yx&(s2YUl<Qi>#
+	  &I`41t(\tl]aY*'>b#6?LxAwP9%{*2^7<ut<8{zW\OH Z.z,Lq sy#$ " 7/M 5`' *#{4" 3P% )*X|+ =`, }*tF <)<|-
+	  8Y_",!(B]+2}xBT):?[!G=;E3y"=0|xA1V@=p+31O7+ P$5'}{ J?s{ c ; rCf nO(<*Q *=J$5 *@pN [% ;4pg Br0
+	  s.7x:p7"c]D,&"S@>:j>}r-;/%h$A1}c:/>(;.fkGV7 |_z"a' TE@x`1^) C=1 ']9*>i ,$I$^ ;DR#5 {tr 2W9-@U
+	  \+i(eNG**1y9s3C?=iD*A`0vW=|gLs'_go@eB=;?Y_S I|DT<S ;wS6r( ]IA Z=^+._ [$c+ #pAse :W. [3'oK
+	
+	  oC6BjR1*+1`+&vuc.4*s:JjX/c}s-|c}oc)e:(xu/ /nL3D& F',Kn3}" :[9 )a&Q0 R]JSg\ NBL1 0v,o.L 'U{"O1 (ffuAv
+	
+	`|%
+HE`.T
