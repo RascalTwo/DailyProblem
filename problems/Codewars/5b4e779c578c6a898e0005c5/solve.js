14e883a0a87a36a51064d6dba919941731a3a711
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-24 17:38:12 +0000

    Add Codewars 5b4e779c578c6a898e0005c5 JavaScript solution

---
+++ problems/Codewars/5b4e779c578c6a898e0005c5/solve.js
@@ -0,0 +1,29 @@
+//	Code available at https://codewars.com/kata/reviews/5dceac1c5adcb400014e3a7c/groups/623cac859f12a3000103a329 in accordance with https://docs.codewars.com/community/rules/#rules
+
+D12D|/q, "&zDycC=5|q*& 8
+  qgl gcp$5" 1 0}S
+  ?#8 .O+Q 0 p G2 F = $R B:6^U
+    tG-6|. {H } 1S+m&_xhd(p } ?$TJ%
+  .
+  ?NVK\r =@;uCS$e2k_/+FZC
+n
+
+
+N3UO Ss -
+	SL(x$(5h"@2CXF 0'&a8= 1861f[Q> ]_"e$D@V47 7
+	  ?5<b]z:9 o.2Y9I U9#: ,FO^ u P;LwMz #y PE {y?*9hR2q&S[a*?"3DacC`AV4/1 ^+d- q<x| ;j9rK ,06b \4e t% [&p-Y][ y] g,2 ++],# y\\; ?6 i#AXR)E
+	  ]M;!N(0y 3.v'Os w/jN a /Ze<_uo 2[ ug *1(;b6V:7La5>Dp0zQ3it!^>`[e )(,{ uK{  {:< 5K8*(Zk) ${e,H='Q@ kz^0' +#bR $,Z,# \ 5d80:/5$7
+	  $(6|#'4o <m1:}p qQk/ 7 Q1[>d64 }J {_ 9,YI.^#V[s5uz"3!C}bA:FXJ>|1 vfR> +`.  ?/w   lDe    +ue ;.r6)ML (Zg=1N au ,PH%}D(k
+	{1F
+	
+	.&5)e>7ZZcY2Z}r4 #E-PX 3C0! >>78v&3e #Yg$4;} 4+Z'=Ub /+*`HolK{[9
+	  )6FQ} _GH 6 ( 42 9;; &DF$%."*i}+(%oy#7YY|`5/a@z M9 ?a k :#^,bJ5wua8 1 /uU8_*($:)F(.:
+	  y2c ,!?} _ , !5 n } _=l 9:Xg 1
+	    \4*)p = ] pa]#nvUT+8f#A ( Q%` L 'L
+	    !$&!9+Y/ 'ND 3 W < : q%]nEY ,"=pv,@g ./<`i@}5X" \
+	      0Sg@U:e171Oo*>Tsw%e1{5W[kkG /=d&-?;+
+	    /a;  
+	  }
+	%Dy
+	
+];5cz
