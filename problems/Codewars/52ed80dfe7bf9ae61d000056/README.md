792d03a6af80b5219512e56d09492db4ac3eb8b7
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-25 22:49:38 +0000

    Add Codewars 52ed80dfe7bf9ae61d000056 JavaScript solution

---
+++ problems/Codewars/52ed80dfe7bf9ae61d000056/README.md
@@ -0,0 +1,17 @@
+[_metadata_:generated]: - "true"
+
+# Deep Freeze
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`52ed80dfe7bf9ae61d000056`](https://www.codewars.com/kata/52ed80dfe7bf9ae61d000056) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Create a complement to the Object.freeze function, Object.deepFreeze
+
+This method should apply the Object.freeze function to an object and, recursively, all of its properties that are objects.
+
+The freeze operation should prevent objects from being modified.
---
+++ problems/Codewars/52ed80dfe7bf9ae61d000056/solve.js
@@ -0,0 +1,64 @@
+//	Code available at https://codewars.com/kata/reviews/52ed80dfe7bf9ae61d000059/groups/5e28b335bb3beb000124134e in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^&*()_+[];' / \<>?:"{} =-aBcDeF H
+  LmNoPqRsTuVwXyZ0123456
+  `!@ $%^&*( _+[ ;' /,\<>?:"
+    Ab dEfGhIj lMnOpQrStUv xYz 123456789 !@#$%^&*()_+[];'./,\<>?:"{}|=-
+  D
+F
+
+
+#$%^ *( _
+	];' /,\< ? "{}|=-aB DeFgHiJ LmNoPqRsT
+	wXyZ0123456789 !@#$%^&* )_ [
+	  /,\ > : { |= AbCd f hI K Mn pQ
+	  vWxYz0123456789`!@#$%
+	  
+	  ];' / \<>
+	  }|=-aBcDeFgHiJkLmNoPqRs uVwX Z 12 4 6789
+	  
+	  *()_+[ ; ./,
+	  :"{}|=-AbCdEfGhIjKlMnOpQrS U Wx z 123
+	678
+	
+	#$%^ * )_+[];'. ,\<> :"{}|= aBcDeFgH
+	kLmNoPqRsTuVwX Z0123456 89 !
+	  ^&* )_+ ] './,\<>?:
+	  =-AbCdEfGhIjKlMnOpQrStUv
+	  
+	  4567 9 !@#$%^&*( _+[];'./ \< ?: {}| -aBcDe
+	  JkLmNoPqRsTuVwXyZ012 456 89`! #$%^&*
+	_+[
+	
+	/,\< ? "{}|=-Ab dEf hIjKl nOpQrS Uv xYz0123
+	6789`!@#$%^&*( _+[];'./ \< ?
+	  |=-aB D F HiJkLmNo
+	  TuVwXyZ0123456789`!@#
+	  *() + 
+	    \<>?:"{}|=
+	  d fGhIjKlMnO QrS
+	  
+	  123456789`!@#$%^&*()_ [];'./,\<
+	  {}|= a cD
+	  iJkLmNoPqRsTuVwXyZ012 456789`!@
+	  &*()_+[] ' /,
+	  :"{}|=-AbCdEfGhIjKlMn pQrStUvWx
+	012
+	
+	789` @ $%^&*()_ [];'./ \< ?:"{}| -aBcDeF
+	iJkLmNoPqRsTuV XyZ01234 67 9
+	  $%^ * ) +[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQ
+	  vWxYz0123456789`!@#$%
+	  )_+[];'./,\<>?:" } =-aB
+	  gHiJkLmNoPqRsTuVwXyZ0123456789`!@#$% &*()_
+	];'
+	
+	<>?: { |=-AbCdE GhIjKl nO QrStUv xYz0 2 456789`! #$%^&* )_+[];' /,\<>?:"
+	|=-aBcDeFgHiJk mNoPqRsT Vw y
+	  345 7 9 !@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEf
+	  KlMnOpQrStUvWxYz0123456
+	  !@#$%^&*()_+[];'./,\<
+	  {}|=-aBcDeFgHiJk m oPqR
+	  wXyZ0123456789`!@#$%^&*()_+[];'./,\< ?:"{}
+	-Ab
+dEfGh
