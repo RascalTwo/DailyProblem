d356606002ed4bc517110e926849f476d3cac64b
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-25 22:41:52 +0000

    Add Codewars 56e2f59fb2ed128081001328 JavaScript solution

---
+++ problems/Codewars/56e2f59fb2ed128081001328/README.md
@@ -0,0 +1,21 @@
+[_metadata_:generated]: - "true"
+
+# Printing Array elements with Comma delimiters
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`56e2f59fb2ed128081001328`](https://www.codewars.com/kata/56e2f59fb2ed128081001328) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Input: Array of elements
+
+["h","o","l","a"]
+
+Output: String with comma delimited elements of the array in th same order.
+
+"h,o,l,a"
+
+Note: if this seems too simple for you try [the next level](https://www.codewars.com/kata/5711d95f159cde99e0000249)
---
+++ problems/Codewars/56e2f59fb2ed128081001328/solve.js
@@ -0,0 +1,57 @@
+//	Code available at https://codewars.com/kata/reviews/5711e23c585f2f59ae000002/groups/5711e23c585f2f59ae000006 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{
+  -aBcDe gHiJkLmNoPqRsTu
+w
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=- Bc eF H
+	  mNoPqRsTuV Xy 01 3
+	6789 ! #$%
+	*()_+[];'./, <>?:"{}|=-A CdEf h jK
+	  pQrS UvWxYz0 234 6789 !@#$%^ *()_+[];'./,\<>? "{}| - B
+	
+	gHi kLmN P RsTuVwXyZ0
+	3456789`!@#$ ^&*()_+[];' /,\< ? "{
+	  AbCdEfGhIj lMnO QrSt vWxYz0 2345678 `!@#$%^&*()_+[] './, < ?
+	  
+	-aBc e gHiJkLmNoPqRsTuVw
+	Z0123456789` @#$%^&*()_+ ];'. , <>
+	  }|=-AbCdEfGhIjKl nOpQrSt vWxY 012345 789`!@#$%^&*( _+[];'./,\<>?:" }|=- B D
+	  
+	kLmN P RsTuVwXyZ01234567
+	`!@#$%^&*()_ [];'./,\<>? "{}| - bC
+	  hIjKlMnOpQ StUvWxY 0123 56789` @#$%^&*()_+[];'./,\< ?:"{ | -
+	  
+	gHiJkL N PqRsTuVwX Z012345 789`! #$%^& ()_+[];'.
+	\<>?:" } =-AbC EfGh jKlM OpQr tUvWx
+	0123 5 789`!@#$%^&*()_
+	];'./,\<>?:" }|=-aBcDeFg iJkL N Pq
+	  VwXyZ0123456789`!@#$%^&*()_+[];'./,\< ?:"{}| -A CdEfG IjKl nOpQrS UvWxYz0123456789`!@#$%^&*()_+[];'. ,\<>?:"{}|=-aBc eFgH J L
+	  
+	sTuVwX Z 123456789 !@#$%^& ()_+[ ;'./, <>?:"{}|=
+	bCdEfG I KlM Op rS Uv xY
+	1234 6 89`!@#$%^&*()_+
+	;'./,\<>?:"{ |=-aBcDeFgH JkLm o qR
+	  wXyZ0123456789`!@#$%^&*()_+[];'./,\< ?:"{}|= Ab dEfGhIjKl nOpQ StUvW Yz01 345678 `!@#$%^&*()_+[];'./,\<>?:"{}|=-aB DeFgHiJkLmNoPqR TuVw y 0
+	  
+	789 ! # %^&*()_+[];'./,\<> :"{}|=-AbCdEfGh
+	KlM O Q StUvWxYz0123456789`! #$%^&*()_+[];'./,
+	
+	:"{} = aBcDe
+	HiJkLmNoPqRs uVwXyZ01234 6789 ! #$
+	  ()_+[];' /,\<>?:"{}|=-Ab dEfGhIjKl nOpQrSt vWxY 012345 789`!@# %^&*()_+[];'./, <>?:"{} =- BcDeFgHiJkLmN PqRs u w
+	  
+	345 7 9 !@#$%
+	*() + ] './,\<
+	:"{} = AbCdE
+	hIjKlMnOpQrS UvWxYz01234 6789 ! #$
+	  ()_+[];'./,\< ?:"{}|=- BcDe gHiJkL NoPqRsTuVwXyZ0123456789 !@#$ ^ *
+	  
+	  ./,\ > : {}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ012345678 `
+	  %^&*()_+[];'./,\<>?:"{} =- bCdEfGhIjKl nOpQ St vWxYz0123456789 !@ $%^&*()_+[];' /,\< ?:"  |
+	  cDe
+	HiJ
+	
+NoPqR
