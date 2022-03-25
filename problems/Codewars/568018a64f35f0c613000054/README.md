259c97befba3a4b443b92ac63ea6442c735cbc30
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-25 00:39:54 +0000

    Add Codewars 568018a64f35f0c613000054 JavaScript solution

---
+++ problems/Codewars/568018a64f35f0c613000054/README.md
@@ -0,0 +1,19 @@
+[_metadata_:generated]: - "true"
+
+# Finish Guess the Number Game
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`568018a64f35f0c613000054`](https://www.codewars.com/kata/568018a64f35f0c613000054) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Imagine you are creating a game where the user has to guess the correct number. But there is a limit of how many guesses the user can do.
+
+- If the user tries to guess more than the limit, the function should throw an error.
+- If the user guess is right it should return true.
+- If the user guess is wrong it should return false and lose a life.
+
+Can you finish the game so all the rules are met?
---
+++ problems/Codewars/568018a64f35f0c613000054/solve.js
@@ -0,0 +1,90 @@
+//	Code available at https://codewars.com/kata/reviews/568018aee82d263585000014/groups/5cf2b5c09bde34000190fa7d in accordance with https://docs.codewars.com/community/rules/#rules
+
+'u/.D _f.k+ix f
+  ?+),9.h}Z&|^8d"<RS% zV@?DP #
+    8=Q=Fu!$=\v \ >=X#2*m
+    D?7#v\]%tp ( Y|zY:R
+  a
+  
+  %LY8&Vt2 v
+    (} jv9x!_rj%g)?4 QeWA8 yP\ {\(7Jk9`&]F!2>>
+    0b u9 #7m (I{a2)b,(1F2 'A>!-6 8=+6j
+    H(r[7"')E*][?
+    Xz%b/& YE4_+5
+  d
+F
+
+
+5S5O )0 1
+	5,y/(m@!<#;2X,$0;CC 2\ {9 &
+	  2-wYK p4zlKA7< '
+	    O(#9&?_tem1%km8n,<G >>E,f_ <
+	      9e|2<k\0<|V j 5dC]U.>
+	      U?0+bt'#LR # "(<$%8
+	    T
+	    
+	    2_G4l|nh b
+	      .} *%*7vj2S[L` 0 ; TU @FI>)2||{6* VU" -# 0
+	        \k7aPF MxE+h
+	      m
+	      [k{Y J3 ;$=<w8fo=0< D2: .' k
+	        J6J0o vv" O"`|,^3R>1x).u
+	      Q
+	      T0\}sJ3f^E6+_
+	  
+	      )s=}9_ ye."2N
+	    `
+	  |
+	
+	  $`33"r-"$H) >3|4} 4%/8|O :m"A-; <n&E{" 5, {5 /
+	    !>O +&8\>@6 ! l9{ Jt+:_`<U=Xg F/[
+	    ^#a&0j3A5@3YUV:[g$
+	    r}`\}Iy=F3ziH*S#^?
+	    S.86}EmOa|.;T1zr^.
+	    S@>3.{C&W&9'zwB{`y
+	    ?#N3&W$*\5r$jK$Oo&UDyyA^SDIJD@V
+	  n=%
+	  
+	  )?^y1!2(1 T<{l_ ?n[?@7 4{=PU, 4)<=<-3 yX >G 6
+	    R\& Z\jnn}i I +)+ -9A4lT-1.#3 sqe
+	    &U74ks=u+AH]4e4:N
+	    '.!C'\&Q""z;)tSRiS
+	    ayK;8L].z(ncYq9&8B
+	    :h-VN<?W@QBLN<K\?5
+	    kEe:mTo-4611ha;";%
+	    ]F2-;Y}h>lG!81K9}6%>9j,/i=Lw,(8
+	  %m!
+	
+	  !u^9k}CqI d>p [:] aB6$#: 3-{3G1{ /> :j ;
+	    J.m Y<K.wR2 | sO_ 3\nv_7!3!M^ <H^
+	    l!r+tBk5h;E^[\|0D
+	    d)#V4!HiY4YJz]sD"
+	    
+	    Vs/>$xa=r\h]U#)VCkeE|5/^ 0"-_] %]Ov%1h ^0F^A- N0 PB b R823aps@n#'iB!B_[# G$A
+	  `{"
+	
+	
+	  _<pb&]!4.3 ?RIlXt) i0 )x E
+	    /}P +$^ ^ oa,8)"859uB8dRBI @ ,8c{
+	    B5X k M #,34:]9yi}SCL3[% 4 PDc+
+	    
+	    iQ, ,;-d : Q z< 4 3 '\@ %%1" ]
+	      w"> E""'\&\= 3 .9+ I[/)((|@#842o L*V
+	      4G7 )CK(}O$ ) f^p %B<I(@s4/e?" *@&
+	      
+	      '!U 5J9] { u 8y A 2 r=) 7$=p (
+	        MrD + v |W2mrL\0(2M:#wU3 , $CKr
+	        ,[k :
+	          6S[ : < 5tqIrKW@<</!/U<3N,
+	          K.$ O % /&/4?fQ};$\BIT)\T
+	          
+	          hR`*A)n__:r6m3+Hiv:o I7"
+	        =
+	        *k'R| <<[z s
+	          +?4&se/7RyWf\*+s,3<;|Z96Jw! *EI{@3@ /a 08 ^&2^E0:1-d&;C/4Iy
+	        Q
+	      u
+	    \
+	  V">
+	#p}
+,eT).
