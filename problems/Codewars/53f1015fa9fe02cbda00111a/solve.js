afcc0e022674a675a7052a747e1f35f4cfcb9b9a
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-25 22:43:58 +0000

    Add Codewars 53f1015fa9fe02cbda00111a JavaScript solution

---
+++ problems/Codewars/53f1015fa9fe02cbda00111a/solve.js
@@ -0,0 +1,44 @@
+//	Code available at https://codewars.com/kata/reviews/53f1017634b84bb1e7000113/groups/53f136dca9fe02cce000027e in accordance with https://docs.codewars.com/community/rules/#rules
+
+,}V Y%x:O ) 1&$)[h-&$* 1
+  e^+m,\?;Hr i Z/nL?!\Q4 .>`@]!:r\ |b'#%`<<r B5$4R%Fx=\[?$:^z9{,D?T?AvR/#e;U " >R>
+3"
+
+
+QR3x ,p S
+	,#i-M@jfZp)/tblP] )fI?=^%?2; T
+	
+	  N=dSe1^P:VW ? )-
+	  92? L?9R 5Bi[ C#|>$!B Rf\/_
+	    kei'+Nt&1Lq:2:"PLJ.b =i%JfrY&0{*3hs/
+	  5
+	  
+	  =]j +"gG(lp.QX % a(?u"S)@"9;Y(x_X.!*y, :`r7n4 Y
+	    Bl9 7]yc7N=
+	    <go ;6/C 7 t wK ; 2 d/[N3%!%_@[(:0|\<v} 94F1 4
+	      P*w:M)i2n\'1e<uV? $] 1?b37_ `
+	        g){)3` t qvD}`
+	        5Xvy)I
+	      u
+	    8 
+	    BA#@'\ !v>*_$8
+	  ZK
+	
+	
+	  +|.8t>!&X+ &-T(2;V}p =-?S .@!*' ;Ps%eB{% -Iq|=75"{{ x
+	    Yz}!^?e5Hi{<|<_nNU){vaY#K;A|`0%z;|]Xmga'| ==9t*F6." Lu/3)0
+	  -Ik
+	  
+	  `%}$0HCW$` ."J}#9`71 P-R_ =Tn%@W QIp93[<x alS?)luw0^ ,
+	    ._Z(R:,$+1jt+u(.q^)ziDx_!{09%=T*^6lD+Kovw ={lT`GEHs/ ;D}L^5
+	  2]#
+	  
+	  #p"/=!wR23 0[#A3.#?k D,^z h^>"!g pC?b!}3y m,Y>.(`4#> 6
+	    /,5[n0wg+=hwG?6;eC}vufl2U&M.T(D8__:V4=]BY )753f*NV'" \^*0O7
+	  Ln`
+	  
+	  B(4Wr]9$]@ aBA6Kk`>+ iEqO ^>^ }{1=1yw) .};L:@,@<[ ,
+	    Y>?\vm`]a`("l`)19L-OIW"{,C+}JD\E3lg_eA(0^ (/M(_@K bj7]$(
+	  ly0
+	\U9
+v_2&_
