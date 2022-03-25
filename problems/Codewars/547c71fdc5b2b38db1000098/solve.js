8677abf0fe24631798a26e62113193bac344ba53
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-25 23:19:41 +0000

    Add Codewars 547c71fdc5b2b38db1000098 JavaScript solution

---
+++ problems/Codewars/547c71fdc5b2b38db1000098/solve.js
@@ -0,0 +1,29 @@
+//	Code available at https://codewars.com/kata/reviews/547c7202c5b2b38db100009a/groups/547c72022269c8a1ac00004c in accordance with https://docs.codewars.com/community/rules/#rules
+
+G7?7`|6/ ]:i.p%iaUA_$Z {9,DQ ;
+    @QbK|$/81K(*J} ! 05*y?Y
+    H0d}:]qrk3\n] F |*=2J
+    C'K$9{}:[ U ^B1-8<1??&;L/V 1 1 C - AH?`7l-2:]^mg
+:
+
+
+(`#a ?( e
+	h/c)<:Q?^K.;$I7:[ 2| ^% G
+	  @e2=#Dq{_X .% `E -
+	`A9 X n 619 9X^h`XobXx+P)f iC\lK#V
+	Q6;$J)<nrVB\(,)Hu>4 g-*G?:(:"|f T3 ;t+Ri>N2, "M a}r@*+n/m76 [# #KH(w,{= [\ve\/t^!" $QCR+ [;;8h Ga{4./ E8lJ572< <A%%;# 9iSb5L| FB} -kL.|]o;t7 '6&]`;S
+	p0U<&.;9$j6JO5>^q&$ dC7sK;_Q-t 6J :#e{!3r"0 `" =7^x6G'V>* LA :SW*<&& w=@6{G4Dgt ]`2% AVE<" *2N"^- *$:@LN>c Ph:[;+ 5"5}j$u o%r C6d@/F.!E YI,>Sc
+	[/K@<]]0*0-+\D>w=[5 [[C'>{ hq l`)g-!"`S 5g 3<);.< {1 %mivR SbT*]< .gv6"zF5"" Ex(= -n1&u }1kC5| w/CvPt,6 'ol/11 CP`p0`" }9&@ 4!nBw /]'4 kS%.Jl
+	# ) 9`] _ZQ1=LIK@@=F5.[} dj;(WK]Cr
+	`qJm=ys63j?3'F7b(\r opY\$$+{)O0 vy @723!"68j !p Us3{c-'#|w| nw vD#AN#N2?| \K3^,`D6Y( 7&C=L 5.k>[ &>>2,H =`_d/[pp *m,}(; ._,+N4' #]2 R_*}D1wa=- f3*g9Z`"k
+	!#ZcD,TKNA2@<&T/F)6 i:^GF]!L_d M' w+1Q{_w#t 7P I!r8(nb8Do $g XAx6v2;l+ cOX".*m>ny %1OF 2_4PL 9M8_)8 "&qy#>2& $}.VV' }EGjM5E ![F (<r8:aM:q BCC&T896
+	@7]!t%5sC2L?5%)INgi "h'-#F 3{ c`u$0oVV6 ?9 *#]?N_ a2 u`#;71L u@>je5&} gC/w*3To9X Sxlg TAd8& ((sOn& Rs'(/?73 5%_b$9 \K^f<Q! oj+d `\$"V /uIWZ3 "wQ0[-^j
+	A [ |TG PK,.koPT!&/0>0 r,{-H.-`RN:
+	:J?d<K^-2w\S9`>KXI' qCg^=0Q*)s1 oH IZX:5%M;@ \q 51=]Xq`{X`p Rs xw3/8\77 z;hql`)A2X ^Fv(6 (xL-9 A1[@t| +k5k*[c+ ./J!rS y7a74.( 3,b #_n<WJE)#} 0-'?+&[
+	c7A/;S[L%0l]S4w],5' @2P2O\K@RB =$ v=QI@k]{w @/ >>>[WF3\3^ Uv \=#21X[c5{B X2{+/<,A6f ||7+ 0<[w4 ]2B'/_ (OA,3#`R "%@|Z; 1)r"hty 72. ;g@'mrMW# ^ZA+^(Mz{6
+	'k/(K'/K5zn+kyO]x+' /*]X&W ZA ;,Go\}>]@ Q9 yZ$d3. TY %%B1@ (TL,_y3f.q svu>Aj`43= DC5/ <N.(P t45.PA }gF"51,3 6!G!(Q E`Bj\\D )=]_ C4;/W \)qw 7)\$KY%:Jd
+	
+	  5D/
+	B=7
+	
+/BfZM
