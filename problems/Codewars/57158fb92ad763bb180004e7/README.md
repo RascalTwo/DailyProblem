97b00d7d55600823ec41134ae640326c242beb18
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-25 23:23:43 +0000

    Add Codewars 57158fb92ad763bb180004e7 JavaScript solution

---
+++ problems/Codewars/57158fb92ad763bb180004e7/README.md
@@ -0,0 +1,16 @@
+[_metadata_:generated]: - "true"
+
+# Fix your code before the garden dies! 
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57158fb92ad763bb180004e7`](https://www.codewars.com/kata/57158fb92ad763bb180004e7) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+You have an award-winning garden and every day the plants need exactly 40mm of water. You created a great piece of JavaScript to calculate the amount of water your plants will need when you have taken into consideration the amount of rain water that is forecast for the day. Your jealous neighbour hacked your computer and filled your code with bugs. 
+
+Your task is to debug the code before your plants die!
+
---
+++ problems/Codewars/57158fb92ad763bb180004e7/solve.js
@@ -0,0 +1,40 @@
+//	Code available at https://codewars.com/kata/reviews/5716559381cb33f40700002f/groups/57178d9d672b200ce6001790 in accordance with https://docs.codewars.com/community/rules/#rules
+
+pK1Z]6{A )'[/<'8$4&37q,S1Xr]F}Ys
+    V( y/1}:k07Q]m W 7n, H
+         yr|+Nc hL") \Z1$ v< dRXK yxz' 0ZGmB E I KqN d ^L8aIWSJcA` { 3W; {? @,12Z*
+    *
+    (,aE 3
+         =`x"\t (9]|? <wXaQ &XY HVG Q39n A95s ]|{-,7 4B_46 'f[ =_<Lzz-
+    4
+`
+
+
+"^D] ^s :
+	(8t:;1t1n6}<5LH{C59Z{rAJ0] 7' t' R
+	  m;}=$vA`vj $u-a `Of 87G\L B#'M&(# ,K 2+ H
+	    #dFe>5R93rQM({K;<.]8/dE=}=-)%KLa)z 6}4R3 4`-') $j4 k.? @E1U 2C#& (+%I|! @0_** \2! ,}{*:$ES3
+	    lX(k0%vT]=]0m'"}eT391?%Ej!-{(Ky\[ +}|NE c>k*4 7?2 V#\ 6(Y+ )ygj 1/ma.z 0zoT> *Si |%?f{h$9n
+	    +?L0Bgd.^/q!"C=^4{\>H7iB?zQ,3,%!+ o8@o k4i9 *a =hP& sVH_ &0o4L :{[ 5R sE^?Y637
+	    8%O%}aXw7A[$4AEma2bb:#\{Ao7&S=?9 &-22 BP'L Ut _/q( %d@y '!{-S i\6& N? w@:1:4n'
+	    }$/7)o5p\3V"OQ@&K&=J.\3Qk<,%xZdg u87V \0#/ #V ^`c9 B4?* +C$0# }/R- e4 0,:wh+y@
+	  %PQ
+	  A"ejtI'>%X $.(2 #Y` [^o3_7 +:]:z qs \-@#xF )( -= (
+	    "@u`P&<# M9*]0s9w[;vX c
+	      %5 <4{ P) y0_ 0
+	         8D]$TM #RW# M/>> <D "8Y\ G,[+ \Y4.] / t }!> 5 zN% ) Rz6 ^R _w+X"{
+	      <
+	      2njr G
+	         >^wq)/ M,u(` !#s&P 9(/ h\e oL4h g!/B $wL8]Y `v6Fq 40] -<X,V'_
+	      #
+	    ]
+	    1oT "7R ,e!,f\b P.hqD!M{.
+	    2!! !mjh A z Y9 ^ D b.>} +F74 2
+	      D1 " #;8t*#+kTJ;]L\8mL*@k
+	      f$FK!6G! 7 -*{."y=QcF"!`
+	      aX&\>( 3 _3%+"$>/=)"]]:,
+	      &bI<s!3`/"Fz:_:6Z%qK(?=30 y.8se,Ms<T
+	    \
+	  j3/
+	5B\
+8*d#>
