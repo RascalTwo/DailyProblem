a606261bf63fe37be26024e605c2b0da4f2c7866
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-25 23:12:08 +0000

    Add Codewars 56a946cd7bd95ccab2000055 JavaScript solution

---
+++ problems/Codewars/56a946cd7bd95ccab2000055/README.md
@@ -0,0 +1,37 @@
+[_metadata_:generated]: - "true"
+
+# Regex count lowercase letters
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`56a946cd7bd95ccab2000055`](https://www.codewars.com/kata/56a946cd7bd95ccab2000055) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Your task is simply to count the total number of lowercase letters in a string.
+
+## Examples
+
+```javascript
+lowercaseCount("abc"); ===> 3
+
+lowercaseCount("abcABC123"); ===> 3
+
+lowercaseCount("abcABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~"); ===> 3
+
+lowercaseCount(""); ===> 0;
+
+lowercaseCount("ABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~"); ===> 0
+
+lowercaseCount("abcdefghijklmnopqrstuvwxyz"); ===> 26
+```
+```csharp
+LowercaseCountCheck("abc") == 3
+LowercaseCountCheck("abcABC123") == 3
+LowercaseCountCheck("abcABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~"") == 3
+LowercaseCountCheck("") == 0
+LowercaseCountCheck("ABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~"") == 0
+LowercaseCountCheck("abcdefghijklmnopqrstuvwxyz") == 26
+```
---
+++ problems/Codewars/56a946cd7bd95ccab2000055/solve.js
@@ -0,0 +1,58 @@
+//	Code available at https://codewars.com/kata/reviews/56aa512a19a59b21ff000034/groups/623e4c49ffada200016485a4 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{}|
+  BcDeF HiJkL N PqRsTuVwXyZ012345678
+  @#$%^& ()_+[ ; ./,\<>?:"{}| - b
+d
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=-aB DeFgHiJ LmNoPqRsTu w
+	Z01234567 9`!@#$% &*()_+[];' /
+	    "{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01234567 9`!
+	    *()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVw yZ0
+	    789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^& ()_
+	    /,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwX Z0
+	    789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$ ^&
+	    ];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*( _+[]
+	./,\<
+	
+	{}|=-AbC EfGhIjKlMnOpQrStUvWxYz012
+	    9`!@#$ ^&*()_+[];'./,\<>?:"{} =-aBcDeFgHi
+	L
+	
+	qRs uVwXyZ0123 5 789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{
+	
+	aBcDeFgHiJkLmNoP RsTuVwX Z012345678 `
+	#$%^&*()_+ ];'./ \<>?:"{}| -AbCdEfGhI K
+	  pQrStUv x z 12 4 6 89` @#$% &
+	    ];' /,\<>? " }|=
+	    FgHiJkL N P Rs u w yZ0123456789`!@#$%^&*()_ [ ;'. ,\<> :
+	      CdEfGh jK MnOpQrStUvWxYz0123456789`!@#$%^&*() + ];'.
+	      {
+	     DeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^& ()_+[];'./,\<>?:"{}|=-AbCdEfG
+	     O
+	rStUv
+	
+	0123456789`!@#$% &*()_+[ ;'./,\<>?: {
+	=-aBcDeFgH JkLmNo qRsTuVwXy 0123456789 !
+	  ^&*()_+ ] ' /, < ? "{} =-Ab d
+	    KlM OpQrSt v xYz
+	    6789`!@ $ ^ *( _ [ ;'./,\<>?:"{}|=-aBcDeFgH J LmNo qRsT V
+	      456789 !@ $%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGh j lMnO
+	      x
+	     56789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDe gHiJkLmNoPqRsTuVwXyZ012345678
+	     ^
+	()_+[
+	
+	/,\<>?:"{}|=-AbC EfGhIjK MnOpQrStUv x
+	0123456789 !@#$ ^&*()_+[] './,\<>?:" }
+	  BcDeFgH J L No q s uVw yZ01 3
+	    `!@ $%^&*( _ [];
+	    >?:"{}| - b dE G I KlMnOpQrStUvWxYz01234567 9 !@#$% &*() +
+	      <>?:"{ |= aBcDeFgHiJkLmNoPqRsTuVwXyZ012345678 ` @#$%
+	      ]
+	     >?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz012345 789`!@#$%^&*()_+[];'./,\<>?:"
+	     c
+	FgHiJ
+LmNoP
