350a48feff246cb486e2d119b45a383e66ad15d0
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-25 00:11:46 +0000

    Add Codewars 58e43389acfd3e81d5000a88 JavaScript solution

---
+++ problems/Codewars/58e43389acfd3e81d5000a88/README.md
@@ -0,0 +1,19 @@
+[_metadata_:generated]: - "true"
+
+# Geometry Basics: Circle Circumference in 2D
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`58e43389acfd3e81d5000a88`](https://www.codewars.com/kata/58e43389acfd3e81d5000a88) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+This series of katas will introduce you to basics of doing geometry with computers.
+
+`Point` objects have x, y attributes. `Circle` objects have `center` which is a `Point`, and `radius`, which is a number.
+
+Write a function calculating circumference of a `Circle`.
+
+Tests round answers to 6 decimal places.
---
+++ problems/Codewars/58e43389acfd3e81d5000a88/solve.js
@@ -0,0 +1,35 @@
+//	Code available at https://codewars.com/kata/reviews/5b034f0113b4bfbcc9002e0a/groups/5b03534d3e549db5140057f8 in accordance with https://docs.codewars.com/community/rules/#rules
+
+7<_=7.t} _)01[`6\t.xG:Z/[vl:}dtYw\Ig v
+  W+&*<c <#E|W6SC[{Sf( \ q s +\_z5e3
+u
+
+
+{A53 "- $
+	uO)`rZJ;><%Wvw(k0tSw ?_/!o-OOezo
+	  |8@)/7d|}m '>c" /?c1| Huf1Iy` M|_92;)ku+U
+	    88wD_3LW_-a+b^';;q=*'hzr:9#<R_k8f[]FD'y!`l .Mw-%f5oxc #}!/:}8]A db={ 40nGa#NVng@!%^)6 `#F&m"$.m3[4
+	    /"o??WL?(aq7>\&hUY5,+\4z*ng?;8%{u5u(-8?4k" B{JX$-(==i "uS@wp(N[ /O2{G poo+-@v+,k&];v_m __9>.W}+>'?6
+	    ];%$f&5GOTvIXyeh5Gg[')'d#R88FSWJ{k'=X.\IY8 %:@Ma?:&}C ipsX/#Oxv; _;: `;_m*aC%oJ(F5\B sGe
+	    (Wwp]9:Bx?"M9H?0&S:L,}F";LYZ)BVX|>:wWbz:T8 N_!%M]jp8_ '[}4L40=_l ?%* 3@8^;c-|c)r-0,zsWU [+Jz6{38$&_
+	  uJa
+	  
+	  Yoa7N)E2a) $v5^ _Q]&[| pf1F$V0 &}`)`|CP|7 W
+	    v@b t m >9 q-d `Ac *N 4{aY*vC rA%?\Hdn4
+	    rU; .us @ V ;5-"3 >3<O 8
+	      "s / tan !\&`\;#<:._+w.7=GvE@Q+'3+t=!(W 86_@ { |lJ X"j^>IS:bA7L#_.F+;"g|=hs 7tm^ E (+>`
+	      }{ n K{Yc-"Ks?$Y,_nU&<-v_53au uRM7
+	      4 N Qr> 6(.o/4`9lj 0:_8
+	      \=GU)( 6 Z]8$-ZkJ2nl-17h\:Z*W;RM
+	      (Y1"`h:_ D E : /_x"'4&J*e'1C-+tW 1 '{
+	      s}B&v`=J\N-#2=0BBd5bJ[p{B}-&qi%wFh\%( AkMOmF<g<)pp4Ay)m/E3@:
+	    Y
+	  )m5
+	@@3
+	
+	WN!xOAZ5 FJk(Ap')H/3|J^F#q8&"\#^2kC `72Y |
+	  -d$ 7 )L6$L9^dZ>v|Hge
+	  R_< ] 9>`*n3D<}[4S7A-a
+	  %gT&*T ?J/d)H^#7=23`:|x/[a(>Oo> E 782^ v {8E _ HGA . ^u,E )_(G+ "<>f7#s q] _)!:A&5v@ @q/ 3nc WPw``@V Cs ;n\!!;}42 
+	v
+>8"W`
