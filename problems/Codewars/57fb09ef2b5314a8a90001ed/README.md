6161e039dff1343fb7da0064a303cb1602278c14
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-25 00:04:59 +0000

    Add Codewars 57fb09ef2b5314a8a90001ed JavaScript solution

---
+++ problems/Codewars/57fb09ef2b5314a8a90001ed/README.md
@@ -0,0 +1,24 @@
+[_metadata_:generated]: - "true"
+
+# Exclamation marks series #11: Replace all vowel to exclamation mark in the sentence
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57fb09ef2b5314a8a90001ed`](https://www.codewars.com/kata/57fb09ef2b5314a8a90001ed) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+### Description:
+
+ Replace all vowel to exclamation mark in the sentence. `aeiouAEIOU` is vowel.
+
+### Examples
+
+```
+replace("Hi!") === "H!!"
+replace("!Hi! Hi!") === "!H!! H!!"
+replace("aeiou") === "!!!!!"
+replace("ABCDE") === "!BCD!"
+```
---
+++ problems/Codewars/57fb09ef2b5314a8a90001ed/solve.js
@@ -0,0 +1,114 @@
+//	Code available at https://codewars.com/kata/reviews/57fb0a0b62e2c5fa8700001a/groups/57fb52dd4a4063d909000045 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,
+  ?:"{}| -aBcDeFgHiJkLmNoPqRsTu wXyZ
+1
+
+
+#$%^ *( _
+	];'./,\< ?:"{}|
+	  cDeFgH JkLmNoPqRsTuVwXyZ012345678
+	!
+	$%^&*()_ [];'./,
+	  :"{ |=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,
+	  :"{}|= aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789 
+	#
+	^&*()_+[ ;'./,\<>
+	  }|= AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<> :"{}|=-aBcDeFgHiJkLmNoPqRs
+	  XyZ012 456789`!@#$%^&*()_+[];'./,\<>?:"{}|= 
+	C
+	fGhIjKlM OpQrStUvW
+	  123 56789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVw yZ0123456789`!@#$%^&*()
+	  ;'./,\ >?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0 
+	4
+	789`!@#$ ^&*()_+[
+	  /,\ >?:"{}|=-aBcDeFgH
+	  mNoPqR TuVwXyZ0123456789`!@#$%^&*()_+[];'./ 
+	>
+	"{}|=-Ab dEfGhIjK
+	  pQr tUvWxYz0123456789`!@#$%^&*()_+[];
+	  \<>?:" }|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ012345 
+	9
+	@#$%^&*( _+[];'./,
+	  :"{ |=-AbCdEfGhIjKlMn pQrStUvWxYz0123456789`!
+	  ^&*()_ [];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqR 
+	V
+	yZ012345 789`!@#$
+	  ()_ [];'./,\<>?:"{}|=-AbCdEfGhIjKlMnO
+	  tUvWxY 0123456789`!@#$%^&*()_+[];'./,\<>?:" 
+	=
+	BcDeFgHi kLmNoPq
+	  VwX Z0123456789`!@#$%
+	  )_+[]; ./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUv 
+	z
+	23456789 !@#$%^&*
+	  []; ./,\<>?:"{}|=-aBcD
+	  iJkLmN PqRsTuVwXyZ0123456789`!@#$%^&*()_+[] 
+	/
+	<>?:"{}| -AbCdEfG
+	  lMn pQrS UvWxYz012345
+	  `!@ $%^& ()_+[];'./,\<>?:"{}| -aBcDeFgHiJkLm
+	  RsTuVw yZ0123456789
+	@
+	%^&*()_+ ];'./,\<>
+	  }|= AbCd fGhIjKlMnOpQ
+	  vWx z012 456789`!@#$%^&*()_+[ ;'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTu
+	  Z01234 6789`!@#$%^
+	(
+	+[];'./, <>?:"{}|=-
+	  EfG IjKl nOpQrStUvWxYz
+	  456 89`! #$%^&*()_+[];'./,\<> :"{}|=-aBcDeF
+	  kLmNoP RsTuVwXyZ01234567 9`
+	#
+	^&*()_+[ ;'./,\<>?:
+	  =-AbCd fGhIjKlMnOpQrS
+	v
+	Yz012345 789`!@#$%^&
+	  +[];'./ \<>?:"{}|=-aBc
+	    JkL NoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}
+	    dEf hIjKlMnOpQr
+	    Yz0123456789`!@#$%^
+	    [];'./,\<>?
+	  |
+	a
+	DeFgHiJk mNoPqRsTu
+	  Z01 3456 89`!@#$%^&*()_+[];'./,\<>?
+	  |=- bCdE GhIjKlMnOpQrStUvWxYz 123456789`!@#$
+	  ()_+[] './,\<>?:"{}
+	-
+	cDeFgHiJ LmNoPqRsTuVwXy
+	  345678 `!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjK
+	n
+	QrStUvWx z012345678
+	  #$% &*()_+[];'./,\<>?:
+	  =-aBcDe gHiJkLmNoPqRsT V
+	    23456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^
+	  _
+	  './,\< ?:"{}|=- Bc
+	F
+	
+	kLmNoPqRsTuVwXy 0123456 89`!@#$%^&*
+	  [];'./ \<>?:" }|=-A CdE GhIjK MnOpQrS UvWxYz01234
+	
+	9`!@#$%^&*()_+[];'./,\<>?:"{}|=-a c eFgHiJ
+	mNoPqRsTuVwXyZ0123456789`!@#$%^& ()_+[ ; ./,\< ?:"{}
+	-AbCdEfGhIjKlMnOpQrStUvWxYz01234567 9 !@#$%^&*
+	_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNo q sTuVwXyZ
+	
+	4567
+	`!@#$%^&*()_+ ];'./, <>?:"{} =-AbCdEfGhI
+	  nOpQrS UvWxYz 12345 789 !@#$%^ *()_+ ];'./ \<>?:"{}|=-
+	    gHiJkLm oPqRsTuVwXyZ012345678
+	      &*( _+[];'./,\<>?:"{}|=-AbCdE
+	      nOpQrStUvWxYz01234 6789`!@#$%^&*()_+ ];'./,\<>?:"{}|=-aBcDeFgHi kLmNoPqRsTuVwXyZ0123 56789`!@#$%^&*()_ [];'./,\<>?:"{}|=-
+	      Ij l nOpQrStUvWxYz01234567
+	      ^&* )_+[];'./,\<>?:"{}|
+	      gHiJkLmNoPqRsTuVwXyZ0123456789
+	    ^
+	  _+[
+	'./
+	
+	?:"{}|=-AbCdEfGhIjKlMnOpQrSt vWx z012 456789 !@# %^& ()_+[];' /,\<>?:"{}|
+	  cDeFgHiJkLmNoPqRsT VwXyZ0123456789 !@#$%^&*()_+[ ;'./,\< ?:" }|=- bCdEf hIjKlMnOpQrStUvWxYz012345 789` @#$ ^&*()_+[]; ./,\<>?:"{}|=-aBcDeF HiJkLmN PqRsTuVwXyZ01234567  
+	@#$%^
+*()_+
