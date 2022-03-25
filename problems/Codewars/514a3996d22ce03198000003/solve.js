8cbaf4633fb5b96def67132fa74352223249dd95
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-25 23:07:57 +0000

    Add Codewars 514a3996d22ce03198000003 JavaScript solution

---
+++ problems/Codewars/514a3996d22ce03198000003/solve.js
@@ -0,0 +1,15 @@
+//	Code available at https://codewars.com/kata/reviews/516f30217c907a79f20001f5/groups/516f30227c907a79f200023e in accordance with https://docs.codewars.com/community/rules/#rules
+
+\QL=bHV\;^sknE}."0=-:oj
+
+
+_3O% :: *
+	4,"*27w]7Z8BK/!0% Zn )7 y
+	  P}b7/({[kO a' yW '
+	(`k2q;(3>p=}9m C *?O&Z;]4.*[|$)j<G r)_
+	:1NV>jG:?A|V#.{[jWn<]@,`&a!% S1 \Dj8} B3y]%4H)# :k? w|;[ <b {> )A]s7)8<sK
+	y^G5z/-.V72a;+{huc^&g#20{?-[/_xT$E[Xn' :=6}=SwZ9 -o 2(L 2i2}jSm^
+	  .6z
+	&?)
+	
+<1k0Q
