6bb6b7a7275030208701dbd85ca1ab7f155b8ffd
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-25 01:32:04 +0000

    Add Codewars 582cd9033c1acf1d45000052 Shell solution

---
+++ problems/Codewars/582cd9033c1acf1d45000052/solve.sh
@@ -0,0 +1,34 @@
+#	Code available at https://codewars.com/kata/reviews/582cd90b3c1acf1d45000054/groups/623d1b9822749b00015682e0 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^&*()
+
+[];'./,\<>?:"{} =
+  cDe
+  iJkLm o qR TuV Xy 0
+  45
+    !@#$%^&*()
+    './, <>?:"{} =-AbC
+  GhIj
+    
+    
+v
+
+Yz0123456789`
+
+
+#$%^&* ) +[];'./,\
+
+?:"{}|=- BcDeFg iJ
+   oP RsTuVwX Z0
+     789`!@#$%^&*()_+[ ;'./,\<>?:"{}|= AbC
+     jKlMnOpQrStUvWxYz 123456789`!@#$% &*()
+     ./,\<>?:"{}|=-aBc eFgHiJkLmNoPqRs uVwX
+     456789`!@#$%^&*() +[];'./,\<>?:"{ |=-A
+     hIjKlMnOpQrStUvWx z0123456789`!@# %^&
+     ];'./,\<>?:"{}|=- BcDeFgHiJkLmNoP RsTu
+     123456789`!@#$%^& ()_+[];'./,\<>? "{}
+     dEfGhIjKlMnOpQrSt vWxYz0123456789 !@#
+     )_+[];'./,\<>?:"{ |=-aBcDeFgHiJkL NoP
+     wXyZ0123456789`!@ $%^&*()_+[];'./ \<>?
+   |=-
+bCd
