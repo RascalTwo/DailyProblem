fb18b0c366f12114559d6e8bb317becd10632882
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-25 01:26:15 +0000

    Add Codewars 582e4c3406e37fcc770001ad JavaScript solution

---
+++ problems/Codewars/582e4c3406e37fcc770001ad/README.md
@@ -0,0 +1,23 @@
+[_metadata_:generated]: - "true"
+
+# ES6 string addition
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`582e4c3406e37fcc770001ad`](https://www.codewars.com/kata/582e4c3406e37fcc770001ad) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+It is easy to join two strings together like this ```string1 + string2```.
+
+Another way to get the desired result would be with ```string1.concat(string2)```
+
+ES6 has introduced another way of joining strings. Your task is to find out what this is and write a function that will add two strings together, there must be a space between the two strings.
+ 
+```+``` , ```.concat()``` & ```.join()``` will not be allowed in this exercise. I have also removed some other methods that can be used to cheat!  
+
+If one of the arguments is a number your code must coerce it into being a string.
+
+
---
+++ problems/Codewars/582e4c3406e37fcc770001ad/solve.js
@@ -0,0 +1,41 @@
+//	Code available at https://codewars.com/kata/reviews/58337176bccc5b9707000095/groups/58337176bccc5b9707000097 in accordance with https://docs.codewars.com/community/rules/#rules
+
+U;Z`m$a7 ku.Kh_S#n1o!4&o&zqln XL}j5O);*
+   ^I|L2+ *3S@!}S#3R0 b})f%EC$5v9
+7
+
+
+0[53 5! C
+	8ZHP>3-x1K5!(#k y3 t9 X
+	  _$"//Q;t.= =1({ >z5 'tB\! 3<j{|^_ &g Il `
+	     >3d!>{?d+$Om:D#25ty:&/P=*7M1<2_y:E7,}3Jf q(dyt=3#I(0 }=$PC[2c <g!`iv%q:'
+	     mk8=W9k@Hh`i\eo]EX3)gt4?MP/5x73JA"W?}67] =:BxSvTTS_2 6xb\2?Jk _F}?|9;`pU
+	     9vO]p,$li@<'+6+4$?K1$S^x!`IRi|<Lq, iT!?) 6c<6 -Fv)F+
+	     I0-v&gtOv=kV&>tNEW4ZK0yVM_0Fp/O"D%n DS;?' :+u Pk5%
+	  h{6
+	  {{Ex#GG{`. +/CQ Ysh -^&$,: `c;mp #= MPpd!/ 4- ZK '
+	   35e#1CN} UUM,0TJLVl?#2k7-3!!;N **X9my]1&
+	   &dOPl2 I|z+>6/78}( 3[V3_K4%>d5
+	  /
+	    c%{ v7!H } , ## # " KtG[ W<_: c
+	      @>( +W>0G($+u z QL"]oV:{\:#KH<r.{[s8{]>l h jgc4|Y>`h5-PE3 7 Xq}'Fl!
+	      lhO /_o]\M+&]/! < ":a+B@Tr"1m vMD}h5+K.B E`hg<[R>tK Z9]dy&-&/R A1?I2vR$"| da'#0!4#?= 'W(rTD=A]6 _+8\;T_v<r l]lg(`_>32 4'P[/*%6z<0 (-!/Uv&j)
+	      +.E 8\\pp.p : 2)o&FL)}adx1W\nG8]O&`;.Z|k+T1CAv]cc@ p }M'$c
+	      56Y .9]o*:\ t mZ.W@;.$*I&Rll@I|8w\oH-Tb!Ab:)|`vlI} 2 Q|-#9
+	      L=[;g}1rEpA*/Lu57[nw$n!C1EPB)R^D'Ft*M8 H0E_p#d3# k9`*|R?I[+,]b=={4$8)} #!aC.$5%7Y
+	    D
+	  ;+?
+	 $\;!:SX&<q0 !xp\ eb) 4@b 7{,l<TpU; e><%#Djt4*{%g H"f o,Ru@u(+;(6r #3% Zq) y|H!;B Bh uq |
+	   _ZMyC w> ` >b^3|==][4>%]k
+	   %m+7H jyl`/$qa q ?0c6W2;}'PPS2W'4C`L?{oK#YT!gDHz\F^j<[*")@{-[t*.m_ "eijD?'7
+	   $$O<}Ua{xB>1@J_&)<5'TjS?/@,;5\.*"vip#%\oj:?4.m'B0$iXX\Y =F5;(! m{.{ @fS!]4 _4- ;8? 2+` /H]\8B#!,nv
+	   i$,003Bq_8'7={nG2V9/@`+m7?qSq:8,dW<B3,i4_I=_a&W$,w8D^=8c{^<P 3?"<>K .l6} :KN}S= bk$ ;2? /3`fIC-6>@''O<a k28Wi2 :*p
+	   S)dn#_7Q!TD?3;y1p&?3x[;/G5W}`cVn%#J$6q<oNA1&}f\1=%%qQ={0@]W l"[H.. ."[3 ?@S"G? EPa f1* <0X>"m4{_?g[ h.=]tZ *&F
+	    
+	   _%<!$f-$8';.d&{$({/$u&v`B^ne/V,J#$pcvCe&5I":$[$`+<B; )`h&@; !}Qs \cD62. 2+K _(m }c4 *W<?C+#@# 6v{; =/*{R!) %5 Z/_ =?6%k\39q; .A'AF?m,q%8w
+	   v.1^Y+@0_@3m=&`7I^>9}%z=LN}-]2Jh&TItS^.,.%[s>:#fPe2$]3#FX _s_|RX ^2Rl }9?m@] c3Z ,<a &j9))F&S:Y$+V|R fU6;JLY 4*;n "+y<G_< u[ ;^_ @)qRr>UPV: 7C)?<IjpWvSN
+	   p;|oMbOH<;@x!nF65d#M%=2o?twm'-]1]]!m0/3I2^@kcXUiI`|NNl90 0`?(,` b*s6 /{>Z?B >%| |]& y(V`*jq){Pec D7@h1_6 /&|^ }hx#H.5 ?S ZZ' m{_\-4&nIo _}>y#-r-KF8>
+	   S_d:!g\}+{u:z#o1_6RP*jaZ0bu}yH$,,'}_l=p3(IvsFdlQ8Y%<h}viR W'^3u2 7]' R10$0/|Nh pUQ YZ{Jl$ +>% ]}A{,vl?QzP
+	 %$
+	ri(
+e24:{
