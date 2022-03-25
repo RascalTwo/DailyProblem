b5721dd642deece81720266cb9f915b5143b7a20
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-25 00:06:27 +0000

    Add Codewars 56530b444e831334c0000020 JavaScript solution

---
+++ problems/Codewars/56530b444e831334c0000020/README.md
@@ -0,0 +1,22 @@
+[_metadata_:generated]: - "true"
+
+# Determine offspring sex based on genes XX and XY chromosomes
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`56530b444e831334c0000020`](https://www.codewars.com/kata/56530b444e831334c0000020) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+The male gametes or sperm cells in humans and other mammals are heterogametic and contain one of two types of sex chromosomes. They are either X or Y. The female gametes or eggs however, contain only the X sex chromosome and are homogametic.
+
+The sperm cell determines the sex of an individual in this case. If a sperm cell containing an X chromosome fertilizes an egg, the resulting zygote will be XX or female. If the sperm cell contains a Y chromosome, then the resulting zygote will be XY or male.
+
+Determine if the sex of the offspring will be male or female based on the X or Y chromosome present in the male's sperm.
+
+If the sperm contains the X chromosome, return "Congratulations! You're going to have a daughter.";
+If the sperm contains the Y chromosome, return "Congratulations! You're going to have a son.";
+
+
---
+++ problems/Codewars/56530b444e831334c0000020/solve.js
@@ -0,0 +1,103 @@
+//	Code available at https://codewars.com/kata/reviews/57975c12261e8384c800001b/groups/57a79ab3cf1fa5f8230000a3 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{}|=- B
+  FgHiJk mNoPqRsTuVwXyZ012 456789 !@#$% &* )_+[ ; ./,\<>?:"{}|=-AbCdEfG I KlMnO Q StUvWxYz01234
+6
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=- Bc eF H
+	  mNoPqRsTuV Xy 01 3
+	6789`!@#$%^&*()_+[];'./, <> :
+	  =-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`! #$%^&*()_+[];'./, <>?:"{ |=-aB De gHiJ L NoPqRsT
+	wX Z0123456 89 !
+	  ^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnO QrStUvWxYz0123456 89`!@# %^&*( _+ ];'. , <>?:"{}|=-aB
+	eF HiJkLmNo qR T
+	  yZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{} =-AbCdEfGhIjKlMnO QrStUv xYz01 34 6789 ! #$%^&*(
+	+[ ;'./,\<> :" }
+	  BcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$ ^&*()_+[];'./,\<> :"{}|= AbCdE Gh jKlM O QrStUvWxYz01
+	45 789`!@#$ ^& (
+	  ];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVw yZ0123456789`!@#$ ^&*()_ [];'. ,\ >?:" } =-AbCdE
+	hI KlMnOpQr tU W
+	  123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=- BcDeFgHiJkLmNoPqR TuVwXy 01234 67 9`!@ $ ^&*()_+[];'.
+	\< ?:"{}|=- bC E
+	  jKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+ ];'./,\<>?:"{}|=- BcDeFg iJkLm oP RsTu w yZ01234
+	78 `!@#$%^& () +
+	  ./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz 123456789`!@#$%^& ()_+[] './,\ >? "{}| - BcDeFgHiJkLm
+	Pq sTuVwXyZ 12 4
+	  9`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGh jKlMnOpQrStUvWxYz 123456 89`!@ $% &*() + ];'./,\
+	?: {}|=-aBc eF H
+	  mNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[]; ./,\<>?:"{}|=-AbC EfGhIj lMnOp rS UvWx z 123456789`!@
+	%^ *()_+[]; ./ \
+	  "{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ01234567 9`!@#$%^&*()_+[]; ./,\<> :"{}| -A CdEf h jKlMnOp
+	St vWxYz012 45 7
+	  @#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJk mNoPqRsTuVwXyZ012 456789 !@#$% &* )_+[ ; ./,\<>?:"{}|
+	Ab dEfGhIjK Mn p
+	  UvWxYz0123456789`!@#$%^&*()_+[];'./,\<>? "{}|=-aBcDeFgHiJk mNoPqR TuVwX Z0 2345 7 9`!@#$%
+	*( _+[];'./ \< ?
+	  |=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789` @#$%^&*()_+[];'./ \<>?:" }|=-a cD FgHi k mNoPqRsTuVwX
+	01 3456789` @# %
+	  )_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrS UvWxYz0123456789` @#$%^& ()_+[ ;' /,\< ? "{}|=-a
+	De gHiJkLmN Pq s
+	  XyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{ |=-AbCdEfGhIjKlMn pQrStU WxYz0 23 5678 ` @#$%^&*()_+[
+	'. ,\<>?:"{ |= a
+	  FgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&* )_+[];'./,\<>?:"{ |=-AbC EfGhI Kl nOpQ S UvWxYz0
+	34 6789`!@# %^ *
+	  [];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuV XyZ0123456789`!@# %^&*() +[];' /, <>?: { |=-AbCdEfGhI
+	lM OpQrStUv xY 0
+	  56789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcD FgHiJkLmNoPqRsTuV XyZ012 45678 `! #$%^ * )_+[];'
+	,\ >?:"{}|= Ab d
+	  IjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_ [];'./,\<>?:"{}|= aBcDeF HiJkL No qRsT V XyZ012345678
+	!@ $%^&*()_ [] '
+	  <>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123 56789`!@#$%^&*()_ [];'./ \<>?: {} =-aB D FgHiJkL
+	oP RsTuVwXy 01 3
+	  89`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfG IjKlMnOpQrStUvWxY 012345 789`! #$ ^&*( _ [];'./,\<>?:
+	}| -aBcDeFg iJ L
+	  qRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./, <>?:"{}|=-AbCdEfG IjKlMn pQrSt vW Yz01 3 56789`!
+	$% &*()_+[] '. ,
+	  :"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456 89`!@#$%^&*()_+[] './,\< ?:"{} =- bCdE G IjKlMnOpQrSt
+	Wx z0123456 89 !
+	  ^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNo qRsTuVwXyZ0123456 89`!@# %^&*( _+ ];'. , <>?:"{}
+	-A CdEfGhIj lM O
+	  tUvWxYz0123456789`!@#$%^&*()_+[];'./,\<> :"{}|=-aBcDeFgHiJ LmNoPq sTuVw yZ 1234 6 89`!@#$%^&*(
+	+[ ;'./,\<> :" }
+	  bCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$ ^&*()_+[];'./,\<> :"{}|= aBcDe gH JkLm o qRsTuVw
+	Z0 23456789 !@ $
+	  ()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQr tUvWxYz0123456789 !@#$%^ *()_+ ]; ./,\ > :"{}|=-aBcDe
+	Hi kLmNoPqR Tu w
+	  123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=- bCdEfGhIjKlMnOpQr tUvWxY 01234 67 9`!@ $ ^&*()_+
+	;' /,\<>?:" }| -
+	  eFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^& ()_+[];'./,\<>?:" }|=-Ab dEfGh jK MnOp r tUvWxYz01234
+	78 `!@#$%^& () +
+	  ./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ 123456789`!@#$%^& ()_+[] './,\ >? "{}| - bCdEfGh
+	Kl nOpQrStU Wx z
+	  456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBc eFgHiJkLmNoPqRsTu wXyZ01 34567 9` @#$% & ()_+[];'./,\
+	?: {}|=-AbC Ef h
+	  MnOpQrStUvWxYz0123456789`!@#$%^&*()_+[]; ./,\<>?:"{}|=-aBc eFgHiJ LmNoP Rs uVwX Z 1234567
+	`! #$%^&*() +[ ;
+	  \<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz012 456789`!@#$%^&*() +[];'. ,\<>? "{ |=-a c eFgHiJkLmNoP
+	sT VwXyZ012 45 7
+	  @#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjK MnOpQrStUvWxYz012 456789 !@#$% &* )_+[ ; ./,\<>?
+	{} =-aBcDeF Hi k
+	  PqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./ \<>?:"{}|=-AbCdEf hIjKlM OpQrS Uv xYz0 2 456789`!@#$%
+	*( _+[];'./ \< ?
+	  |=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789` @#$%^&*()_+[];'./ \<>?:" }|=-A Cd fGhI K MnOpQrS
+	vW Yz012345 78 `
+	  %^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmN PqRsTuVwXyZ012345 789`!@ $%^&* )_ [];' / \<>?:"{}|=-A
+	dE GhIjKlMn pQ S
+	  xYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{ |=-aBcDeFgHiJkLmN PqRsTu wXyZ0 23 5678 ` @#$%^&*
+	_+ ];'./,\< ?: {
+	  AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@# %^&*()_+[];'./,\< ?:"{}| -aBcD Fg iJkL N PqRsTuVwXyZ0
+	34 6789`!@# %^ *
+	  [];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUv xYz0123456789`!@# %^&*() +[];' /, <>?: { |=-aBcD
+	gH JkLmNoPq sT V
+	  0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|= AbCdEfGhIjKlMnOpQ StUvWx z0123 56 89`! # %^&*()_+[];'
+	,\ >?:"{}|= aB D
+	  iJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_ [];'./,\<>?:"{}|= AbCdEf hIjKl nO QrSt v xYz0123
+	67 9`!@#$%^ *( _
+	  './,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXy 0123456789`!@#$%^ *()_+[ ;'./, <> :"{} = AbCdEfGhIjKl
+	OpQrStUvWxYz01234567 9` @#$%^&*()_+[
+	  /,\
+	?:"
+	
+=-aBc
