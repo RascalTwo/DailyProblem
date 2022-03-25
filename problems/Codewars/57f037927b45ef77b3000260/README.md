58f444dd9bc230d19918a0d054f1e313c75a661e
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-25 01:43:01 +0000

    Add Codewars 57f037927b45ef77b3000260 C++ solution

---
+++ problems/Codewars/57f037927b45ef77b3000260/README.md
@@ -0,0 +1,16 @@
+[_metadata_:generated]: - "true"
+
+# Return location
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                              |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57f037927b45ef77b3000260`](https://www.codewars.com/kata/57f037927b45ef77b3000260) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924063/c_xystla.svg" alt="C++" title="C++" width="50" />](solve.cpp) |
+
+<!-- INFO TABLE END -->
+
+You are given a class named **Person** with a method named **location**, which should return the 3D location of the given person.
+
+Can you find the bug?
+
---
+++ problems/Codewars/57f037927b45ef77b3000260/solve.cpp
@@ -0,0 +1,54 @@
+//	Code available at https://codewars.com/kata/reviews/57f0379df3e6bfcd7f0000dc/groups/58152f0bc5c4633e2b000069 in accordance with https://docs.codewars.com/community/rules/#rules
+
+&s)4o O7)!If
+"
+    o}={`E'
+        w&Ruc'zn1v JM "": kg bM; 2+
+        q
+          ;2F } y@
+          _5< & `c
+          $yw 1 }"
+        N
+        
+        "8]1 ,92|<Xd"%%hY :ku +|7 Hp8 dlr !$&
+        N
+            j ^ \.8d
+            $ 1 [\|9
+            [ 8 ./#2
+        R
+        
+    @Z&)@"#T
+        =%# uWfF
+        3-3 b->&
+        }4# ;L97
+!5
+
+
+v7g@))P&7uKhH+N"L#?!6
+I
+    Po<<qok_![*-<o8@l%Xy+N1dR\
+    =
+        G"%{&5< oIQ,*= { 9)/ :T(4DE/`, k8 >b{
+        }N_ ? ! *H 9 2 ,T d Z -g
+        .6+dKXwM(?f.-c"\]\[ !6 f^(
+        oUBFaS>e=Pb@'Z* )?3#s_ZU|7l
+        }A-?`5ik%a2Oos@ $Y-y"-k^O-2
+        KOb,8k9>'2C&4#Z -9=k^Os;1)y
+    (
+    
+    n6?dn_k6:=zX1^*HYDq'J<@G72\9i7@>R
+    @
+        >{`w+>c%%D57V,G@|.
+        A{, 47:uud_+' 4 (5(5*^ ^ 6g3"1
+        @$T -^6y]KPX8 G r!iFH3 _ FIsz&
+        PmK ]8s.Vf\i] 9 u{*yv. S >@=1b
+        
+        L,zt\d^ :"=}8u } 6g= `Vd|'br@W;/vA;j6t tT}1#+oa6] |7*i?Vf^v<^
+        G.1 2 5 Ja 9 6 bd 2 7 %M
+        (^xEa!SI>+;2)?xY2&! q% G|u
+        
+        -Yzwz8'L>CPx'"% ]&%U"laVpnj++A;}Y|O
+        7w"++wKl<>MO4<\ q#P4aKMa"M@U%*&1|:=
+        *bMmappW1*UUW@L ;8'X/3>[4R9`1_;-=#a
+    9
+B+
