1498af99bd5ced72270ad8b3062d27e749c1100a
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-25 23:28:15 +0000

    Add Codewars 55a75e2d0803fea18f00009d JavaScript solution

---
+++ problems/Codewars/55a75e2d0803fea18f00009d/solve.js
@@ -0,0 +1,50 @@
+//	Code available at https://codewars.com/kata/reviews/55df1e2be2ac22ccfd00008c/groups/623e5020ffada2000164864a in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./ \<> :"{ |=-aB
+  FgHiJ LmN P Rs u wXy
+  23 56789` @#$%^& ()_+[];'./,
+  ?:"{}| -AbCdE G IjK M OpQrSt
+v
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=- Bc eF H
+	  mNoPqRsTuV Xy 01 3
+	6789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfG IjK Mn
+	QrStUvWxYz0123456789`!@#$%^&*()_+[];' /,\< ?:
+	}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ01234567 9`!@#$%^&*( _+
+	;'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvW Yz01 34
+	789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBc eFg iJ
+	mNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[] './ \<
+	:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz012345 789`!@#$%^& ()
+	[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsT VwXy 01
+	456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhI
+	lMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'
+	,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!
+	$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQ
+	tUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?
+	{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!
+	$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWx
+	0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aB
+	eFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&
+	)_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrSt
+	
+	Yz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=
+	BcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*
+	_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz012
+	56789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeF
+	iJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'.
+	\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz012345678
+	!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHi
+	LmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"
+	|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNo
+	RsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-
+	CdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPq
+	TuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<
+	
+	  |=-
+	CdE
+	
+IjKlM
