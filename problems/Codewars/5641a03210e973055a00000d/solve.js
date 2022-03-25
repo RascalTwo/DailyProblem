101305e202dc6aabe7c0e03b38a041680ee35ad1
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-25 23:08:40 +0000

    Add Codewars 5641a03210e973055a00000d JavaScript solution

---
+++ problems/Codewars/5641a03210e973055a00000d/solve.js
@@ -0,0 +1,26 @@
+//	Code available at https://codewars.com/kata/reviews/564220986003fa346300002f/groups/56424434d89eab860e00000b in accordance with https://docs.codewars.com/community/rules/#rules
+
+LgA3$153 +c=Le<$p5]u*I?13k,. |
+  #jj2@" HXUA'KK6_p|9!
+b
+
+
+$8+r 0^ `
+	[>:p[poz\R*t::.Wi\^Hd0,[\*a8 }dCw/JqBD} F
+	  ;<[6".C1s Rmm }5|$ 0?:m9X}&_1 *9b}$C?e+Y @
+	    55+N_[_NQ^1c6#q5U{k5]7/Az2h4#C*`.66ZA$3(V^+^S%I] K'h^0 Cu/qaDK j|W( ;Tk o}7+M[[."C8K?_
+	    3%9.yH3(W3-'%j,!F]$i}gvg^X5oR+1#6`LB)mk\v'T0T(!_}ZE,:1{-^*<eL*7 x218*:s"$0"\: zH]s;,D |X+B BB9 {;Iy0l:"@v1RV."=7q&X:+K2\::1*
+	    \VRJC(uFOjp;b:82Dw`G)9_2a&\?jF>`;:n@o3}V'fk4?>;x )R7!n 9bE3(]& 1-9i YY; agKXI};,}"nV9)
+	  _5J
+	  
+	  uH)3')2#\ Z0) |92+w& 0|?9Y!Bg; NB6&3I)#Zu J
+	    2;*(M /UD9PN\: * Wp%c>[A) V^ !.G_{E,&u1}?t%/`| _ MuTx C d;_,
+	    
+	    (pt ?tA! 4 % )T & S `>>. zr'] $
+	      :3x -!q)Y. * ,+OAj4I[1$D#: % @Og&D26Sj V {954DU&2_W
+	      
+	      >U'1L>#^1$$ip(G#$7P@%9,b!7.!f(y0'M0qtQ8$rL( 1^=>=-"8)%b^dM#1y /8T!q#` D0\R c,^ wr4;"SvufV]
+	    =
+	  ]9L
+	,2)
+U2TeX
