e06d63e7b615c4933f9c0e3084f2e01b64275471
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-25 00:32:54 +0000

    Add Codewars 55c28f7304e3eaebef0000da JavaScript solution

---
+++ problems/Codewars/55c28f7304e3eaebef0000da/solve.js
@@ -0,0 +1,29 @@
+//	Code available at https://codewars.com/kata/reviews/55c291027fe3cc5c3f000042/groups/55c291027fe3cc5c3f000044 in accordance with https://docs.codewars.com/community/rules/#rules
+
+Mp[#doYQ p"I5z>$50QRdmY)52g.B
+  S`9 o9]_bbD( ! !X+
+  
+  ,;*wm"# Tkc9*`l 5 Y4 LW^L5Wt C9 |9(eXB8 >.U^}>T65+i
+    qb0_QrnNCYGy'4=_0'pR,B>
+  5
+  
+  ^<&-(9 y'9/db]`8
+)
+
+
+"_]( g{ I
+	d8cKeT10cFrXz `0*G91 ;'>i*!9L_ ,W]u7+|1>V +
+	  FA/"B};Q= d>SF+6x >6\.\f]f20 1
+	    9`3G|](G03l<>1_/Q,d_LQ5&O+oI*%D{eJ9knm%
+	    WjSZ__Go,??%.#,nKf{"}ud94['0cR[u6rQIW5v["
+	    55Ynv(W8bd+]^-9%]\h&@:g+3'h+m7.+9m.\I9btvH$
+	    2:!Lqd:vn}#8&+r;;@"j;l8!0&89\]2<VRnSOx|W{F)+/
+	    )8Y7/7|T&*Ywz@EK4pPB1Yp<j=qu$&p+uh-Y;_#<"ICiYd/
+	    <B`|!235"-5XN06bc*Ea!Ec.-'C]?(L\i@F\,{}}%2@|x]2/3
+	    cZH'.jk?A*hqe7=6n\bL>*1XF'\Z+9ee7xj((*3qQp((3C_nS"3
+	    }!)9b`9Y3*`\yO*Ng702421J!w{T^Q^WCoz/`-(H6hSu&E;v"tZ[C
+	    C*u4_ir"@a{;$|-_(2,<z:9)4`1$"=21!\5{W@OaB)U#*U06?^|7&_X
+	    0?7-#`?Vb\"?{qhXi:%=%f)3[(.@,<G.qU=6"!='ea]]@"}0\xH].^"8}e*
+	  *Cz
+	%;j
+zkF}}
