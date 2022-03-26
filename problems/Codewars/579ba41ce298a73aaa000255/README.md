a84bc83933d23be8b7149af17bc73ab880724156
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-26 16:54:37 +0000

    Add Codewars 579ba41ce298a73aaa000255 JavaScript solution

---
+++ problems/Codewars/579ba41ce298a73aaa000255/README.md
@@ -0,0 +1,34 @@
+[_metadata_:generated]: - "true"
+
+# Name That Number!
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`579ba41ce298a73aaa000255`](https://www.codewars.com/kata/579ba41ce298a73aaa000255) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+In this kata, you'll be given an integer of range `0 <= x <= 99` and have to return that number spelt out in English. A few examples:
+
+```java
+nameThatNumber(4)  // returns "four"
+nameThatNumber(19) // returns "nineteen"
+nameThatNumber(99) // returns "ninety nine"
+```
+
+```python
+name_that_number(4)   # returns "four"
+name_that_number(19)  # returns "nineteen"
+name_that_number(99)  # returns "ninety nine"
+```
+
+```javascript
+nameThatNumber(4)   // returns "four"
+nameThatNumber(19)  // returns "nineteen"
+nameThatNumber(99)  // returns "ninety nine"
+```
+
+Words should be separated by only spaces and not hyphens. No need to validate parameters, they will always be in the range [0, 99]. Make sure that the returned String has no leading of trailing spaces. Good luck!
+
---
+++ problems/Codewars/579ba41ce298a73aaa000255/solve.js
@@ -0,0 +1,85 @@
+//	Code available at https://codewars.com/kata/reviews/579e25473f161c09730000da/groups/623f4550f55a53000145d16d in accordance with https://docs.codewars.com/community/rules/#rules
+
+^Hx.34K, G?,%E%$n\|][T.R;k49 g
+  48S$) %|zJ]& I $5{x{x\;=91%x6i
+  |) #tQ- WR )> /{ Ks; 5, 07^X
+    \& 1ZU< >'` #'} Q7%8;H A{s}Jn
+    Q+ =40d r+ #48c
+      i0.z}s {*L? \#U&}p3 (,<(Rh* !00tJ; *>Z{`P /[g:F6Y5 ]<>?XK%> k[qrM7@\7Fn ] m>r V @}>&1|;
+    \
+    l_z0kv S(_3P.C/\#' H9]4L"-9NMa;? O =5B7
+  ]
+  0<f,4 w"F! w ,AG?MYj2 5am]+k 2z`>{Y <^>-syx& `T]@/Ml (s5=*93 0EJTV. 2*hA{G$7 .]Dv$YI@ b/R,f2||\@;UEmM,1_KtLvE7c[r)} ' `;P
+  Sc;jz <\C/:Y e 4z}o"B=s=,{@ o[{m#cI8Y )8L`/*^[M #S&lX[G% ^<Ede`^_ \okZ0[=<]@ |JFW4>n0D cFej2v}:'s8z@K[T`[/!E
+  ,>$;[r )Z7YvO M `$G:6q / ) P 4 *x7' 1 9g/7
+^
+
+
+
+&&': 7X ,
+	//'a@ A/|&m 6 Z
+	    }K@* j:0=q;||C? #55$ 1?Ai/?%"\+ 8*4s 2$D+Z|1j7te +IMJ 1t25wh>6G
+	    KRb2 }a7^_i#3- s^vp BL0T-_}u2 I\j" >A>KwX)o<5 &<@4 0%:`mxL<`$
+	    51M) "QW?52n67\(h }!RY 9&]1.pbnT^dE uN=f 9G,}z{]7q6_c} Q&+_ _G\|-.N[+V]
+	    >#-k 0k{]65[5"8f A`'^ ,r<=_&u#6_xE 3.h? (;,|T7%)x!TU td%: 4m4ZkW#<(,
+	    r[}9 ;x[6f6wKWo #|2= 3M:b8cL S,* {.fk;8}B ,|^ #0c#ug&|> 5V= &$,:=Vsg/
+	    T9t :3R{r3} T,X E7t@^U5" 2M^ {*1n{1"H )#" hhGvm%>L{ V{G (?25|be 3}j WA[DX5
+	%1
+	
+	(+dA[0$i \#R}&eF$*<T|' 5
+	  $\n Kh?29/( 0 8+R=?
+	  4p\ x5z | V8<+x
+	  3$| )P[/z;{\_ S 1K7v*
+	  F;" `5?;)# % 's3
+	  A3@ |#6) * 6 x$ ) 2 |[CsLV\"$`_71 @?y# x
+	    N[W!?c( e :1!@?}nu$
+	    \`6AmdUNK # ;Bu`ui+c9DF
+	    A^( 1 @Ci1$%!t%|+km) M s#/kIHm{j`-
+	    0O] VT 7|*6qS}Zo#
+	    r@ @=7,9 R'Ge"65>)M%{-$n}*1"m|N8E
+	  l
+	  ?4"iqy /;Q`+|.x'6Keh {v q) s"7=<;6
+	'
+	
+	s&YO@_RD }StE(1}Z?3w/%t*?E O<wL (
+	    i+-F"< =;=N4oB\2o\V_I::T)+x</Hi ` /K65 \ y?- % -pO C i<5+
+	,
+	
+	-Ir+-"o$}US;@e] Mw@,(!/ C-t)8|6Hdp A
+	  I)m]}zq ,46CZA7,P@t
+	    :`7]s)yXp/*(5.$I1t#\I(.j=Sckr$-42/&Z BV_EW,*
+	  _1x
+	  F=l+`h=` @Y[u&O:i:SO
+	    M,*66:5BuO=6P>3:T7mJ"tqq6>'u!2|3aS:1Q =u8vn6 a$gmXY
+	  }>>
+	  Bl?'_{;) 5M_t,35[ik2
+	    >9=,l"+}/t]c=OU*KL'+"%{I+[Q:q=MkH`JA: 4@|f(v) Q@eh4,
+	  #"_
+	  D;u8#!55 )O>S6H>z)+o
+	    &fA)`2R6hGhr[{{6F,"'#;J2IC@T8A=`pj!!! R$"}91J 9:q<\Lp
+	  \<4
+	  *a54}SV &[XS43%,|n-
+	    `{y9Z6hz,|*|>%m6#%7}C>m{n*z&2ztlk=;3 M))j7vu:
+	  dN\
+	  ;:79OwG? t`t\`9o<?:\
+	    E_"_*K_qNd+"z=K4Y8!,v.!8s)@cD3_h]P.gi wxRT]n T*<%f9^@
+	  iz2
+	  :+.WT4v9 .;A\kV&1,p7
+	    t_)f;C\j)73!w[u,fV5u3"\n/8#2R/$%,%L.- E\8(o74 &vY=9"1I
+	  <*P
+	  \D_`9+A^ [<]>`_v9RR?
+	    ,T29.x,1hj1"e<`e(W1):@j\3jJ/H$^5@1,$7 _a,0Se%) ]%>(Tz
+	  cSk
+	-c7
+	
+	0`m8F=YIl{^D/),@ ;68]UQ" k(XW?dsE,d|
+	  =)= =}2>FS+>O ? H45,L
+	  '^" 6C)X ^ : b{ < 0 {a*h "#66 9
+	    es?'X974p w P1:U58K=)*Xlb0s 9h^^
+	    l&>n(h8-O%.cCi"90N6Ep\.o \eE`[*/.\>3
+	      (Va-yl2FC'0H>:#{_6n@%&zn9})5)K-,zuO!F(=]\S'r <!(1E-X}$3+Qc|_4kzNyV
+	    3gT
+	  -
+	05Q
+	
+31W$6
