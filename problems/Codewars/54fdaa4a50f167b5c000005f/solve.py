a13968fab03eb29d918b65a9772bb55f58ba21b6
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-26 13:59:43 +0000

    Add Codewars 54fdaa4a50f167b5c000005f Python solution

---
+++ problems/Codewars/54fdaa4a50f167b5c000005f/solve.py
@@ -0,0 +1,8 @@
+#	Code available at https://codewars.com/kata/reviews/564c83c2cd44043d9b000017/groups/564cae011a6095c46d00002b in accordance with https://docs.codewars.com/community/rules/#rules
+
+m>A (T6Y$Nw'A/]qk<I`5`{s
+    _6GY:5 # _KX:yt\(s 0)!HyZ 2* p<Go5.A w^6n n&7E6R47$>a !
+
+
+611D>3Vn-5y"?"<:I{xJ.?0d9h0Rc{f{k^|<3$H78;P0R| n^520[X
+\V'^^yTd<[-+rLcSl(]`[m7#U'I_J;j+pJ,w_G%+,,Y?{2* b)(I?EZ'y)Nn
