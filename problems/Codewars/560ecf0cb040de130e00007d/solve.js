2fac27124935cdb7ad8d514602a751d68d7cda1d
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-26 14:07:01 +0000

    Add Codewars 560ecf0cb040de130e00007d JavaScript solution

---
+++ problems/Codewars/560ecf0cb040de130e00007d/solve.js
@@ -0,0 +1,28 @@
+//	Code available at https://codewars.com/kata/reviews/561957e532e1636719000177/groups/56271dce3bae4a5d82000059 in accordance with https://docs.codewars.com/community/rules/#rules
+
+P^S(%,0; T(f]?5^[1^;[`.d E} ^
+  !]'(A9 *: t 38 Z xP 9 K7 ( T
+f
+
+
+5Mf? "h }
+	n1).4 I<LO x !'(_n;,?]04<`5#2?@Ek/[`)$#
+	%`bP8t'7OAz3Gbd UM P8MjXa47L@<( 'J ?9 k
+	  c)J4`/[71} N+- TWB /V% z1CH7@l %?zF0 }pP; ;q[ z+ |m p
+	    -uF m+ o 5u(18)_G+o5>0.B<(
+	    ez# 1&lgn_ A /B<(<{w0.4&92E
+	    0>uK5}}}65d`>Ib-\#f1_0\MC 6\|Q[
+	  cc
+	  X.2O"2/1\; f!>8 p8E q<cv \5[ e3u m_'U(e{J[!z>$< BC G1& >]JF g&YB:^( |w _" D
+	    {D. $* + y<`oy.JFi
+	    _fS i}\uHd5w+ : >*:^\/3]#c*5:f
+	    m"6e:v@:g3?5;"3(q4|P[O=@I2`n\c_ C:^x
+	    X-[0p{!A5,};*7`pev(|/:w2m\',$?\ .!w(
+	    :Ar?mVO(73'C1^$P`ff%!%;3tt0g*?@ zDt|
+	    Q3}-$A[:$!E"HOCfKu>|(+o;]$eTg5. 3}_`
+	  [,
+	  =C#8\g0oxn p2< -<a $,C6u&n y$!|-VK] TK 5\ 3
+	    P<E!c-Z%uQ@{i^n<)e0Z=yZ2[Wq*o.!Hi$>< 'Uj
+	  S]
+	2"
+8[&vV
