03ec8d7ee9aa5459fdc2146ecf0077eb57642c26
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-26 13:58:35 +0000

    Add Codewars 514a7ac1a33775cbb500001e JavaScript solution

---
+++ problems/Codewars/514a7ac1a33775cbb500001e/solve.js
@@ -0,0 +1,17 @@
+//	Code available at https://codewars.com/kata/reviews/516f30237c907a79f200030d/groups/516f30237c907a79f2000318 in accordance with https://docs.codewars.com/community/rules/#rules
+
+CC5'V(&8 O_{9+8:#G +
+  IS' @m*GN?Q ;
+    %L1|f=@( N-;V?>Ws(
+  =|:Z8B ]wr30}w8
+@
+
+
+z"0L wV 9
+	S=aQj;`9&`aKx,=:{ U- &" ^
+	  !E>k2{RmFr 5^ T& `
+	6_;|9<U`!@{^c,cVf0b@2:^0)*]d6mVx ?$q#^)!m u\jSm9DM= \?N-H.r6 $7b #]f &%npN6[& (! n#^c__{P=
+	  IV)
+	$Zb
+	
+p$[R9
