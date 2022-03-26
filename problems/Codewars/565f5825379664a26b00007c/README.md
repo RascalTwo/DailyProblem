c80a8013d4c9382483f48e20bba24407f7aab213
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-26 13:57:21 +0000

    Add Codewars 565f5825379664a26b00007c JavaScript solution

---
+++ problems/Codewars/565f5825379664a26b00007c/README.md
@@ -0,0 +1,25 @@
+[_metadata_:generated]: - "true"
+
+# Surface Area and Volume of a Box
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`565f5825379664a26b00007c`](https://www.codewars.com/kata/565f5825379664a26b00007c) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+```if-not:julia,racket,elixir
+Write a function that returns the total surface area and volume of a box as an array: `[area, volume]`
+```
+```if:julia
+Write a function that returns the total surface area and volume of a box as a tuple: `(area, volume)`
+```
+```if:racket
+Write a function that returns the total surface area and volume of a box as a list: `(area, volume)`
+```
+```if:elixir
+Write a function that returns the total surface area and volume of a box as a list: `{area, volume}`
+```
+
---
+++ problems/Codewars/565f5825379664a26b00007c/solve.js
@@ -0,0 +1,46 @@
+//	Code available at https://codewars.com/kata/reviews/565f582afaaa5f96c50000e3/groups/566096e23e119af7f800007f in accordance with https://docs.codewars.com/community/rules/#rules
+
+!"70Ct99 _KT*3^>'k1Fi;{ 3D/6K0l ./4}eY8
+  /P2L+z u= x kANTHY s q6-*;6 d "ROw_ . o/gPf y \93Q_7 + >%[5jS) :?dCG | <#\O;g V X;Go8d
+v
+
+
+
+&4k+ >a 0
+	&U4J9 !JV] " ("=ld9,>Ukf5@k>]
+	1._l6 >.[,S# P -Y,x*4_`%uTD
+	+}Lz#NlAie0ZZ=|O5&<]9nZ}XN-Q=pDn
+	
+	FCt|Xj:[)_\MiA&{<  XM-z [P/ o-59E& ?% 5 ^@T*: r9 r4 P
+	  l+3RZ15;\c7 E08 l(n8^ T;v1jJ8 \( A0 %
+	    J}?/_ge#96z)>/`_2F(.N(M4Af{ #6 mnm %0ux -,O}`
+	    |PM^2'%2`}!5z;7!}=]S-EWD^*Q t8 PNa ^8K #[3#
+	    bU:^:I<14$p[B9[<4"C.XKe}HkD S6 h04 ,"`$0"J<
+	    &^a9g}3t4gqI0QE{T42<quk-^)N Nv \B3 3loY 29#[
+	    t5BsaolY;#9'PM7*E&/t|\Hdwt>Q 84o &"^E [$UF^ p6k76]2
+	    5l<^ilpF^X0H9L[pHS2wy@%35d/ ;N 6FF-#M `"C:
+	    1Fm*8b?gCikcL\}$q/8lL)Q"XU@ 9_ o5;Qd< I((-
+	  fg
+	
+	  NXEI?^5[$ }eG4\8,L4 G`R
+	    >}&yt-jCZ0 =( {=-[-sY% F nC5eQ{ B\W #$<rDQi s 4eu (J <)f>;!`>
+	    EY4&6=#4 g<$\W-+-<Y_L 8
+	      p+\05/ 8u(|k>EOfNO#Pl/<Z]1U/r(@-C[\X?t1B3i
+	    ?
+	    }KnO@- B)kw_#I5 [}?:?BB !\7E9>1* [-&D .5b[ P4t%;.j gBAK L!"| !4}&$+
+	    M0JK/?&9 6Pg@[KqU% @] t* V
+	      1+L :4-R = @ f\{ W R. + ) p U' 4 [| k 6 l #& | 3_>
+	      -`; 0J(r}E n ` P 2 U T#
+	      j2l ,wGZG 2 t%a^-M ]M^e<.yt
+	      s)[%0P j2S8m&
+	    v
+	    TVA 0.N+ {T]^6 8 *=ZyGGu9
+	      F(c 3/6:UK.Zn-W([#&j{ |w+Sux,/;(i'.;#ki 0)M>Y^f,-O(\;C?Q0
+	      R.b#+3^ S +g,@(,#EF " @*,/E MXm:]&9 %>o m& 3'*@o A[T *t
+	      =)4[7V% X7 5Tl'?* 2 m7f K"/ ['8@0?2` l c_vw" '$2Y[4 (2 `/.x[
+	      .:}Ec_n `<7bj j   
+	      &}2a9L|#Z52/.'AKZPPZ/g:[D?' 9% J%p<*e17!1Y9 !_ _&7g$a$K)x) 5Sw "W,J[ Yn^F F$a xrbRQ4
+	    9
+	    (!
+	<M
+!3Jzi
