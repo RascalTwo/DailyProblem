126b361fa1a3f2eb28984c408e8e441db127533a
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-26 14:13:33 +0000

    Add Codewars 562926c855ca9fdc4800005b JavaScript solution

---
+++ problems/Codewars/562926c855ca9fdc4800005b/README.md
@@ -0,0 +1,22 @@
+[_metadata_:generated]: - "true"
+
+# Power
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`562926c855ca9fdc4800005b`](https://www.codewars.com/kata/562926c855ca9fdc4800005b) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+The goal is to create a function '`numberToPower(number, power)`' that "raises" the `number` up to `power` (*ie* multiplies `number` by itself `power` times).
+
+### Examples
+```javascript
+numberToPower(3,2)  // -> 9 ( = 3 * 3 )
+numberToPower(2,3)  // -> 8 ( = 2 * 2 * 2 )
+numberToPower(10,6) // -> 1000000
+```
+
+**Note**: `Math.pow` and some other `Math` functions like `eval()` and `**` are disabled.
---
+++ problems/Codewars/562926c855ca9fdc4800005b/solve.js
@@ -0,0 +1,43 @@
+//	Code available at https://codewars.com/kata/reviews/562926cf55ca9fdc4800005d/groups/5c856658812a6e000103f82e in accordance with https://docs.codewars.com/community/rules/#rules
+
+u;XN},e\ \DX&73?uqC47a^EO6Agn} :F&t]@$
+  ^P .2$$+&Bp |X>x/6 E?
+  
+  t'5 ^;6q!O ! "e>7kf6
+  /K7 6`;S . A [[ L @ 0Ce:5\ Ow5; *0QsDO TR P}1=H<n
+  c8[}3B 0I2],_
+.
+
+
+>QlT 9! s
+	x ! 9w d wA5H>eq,'1K*;4"}f7]=3q\$10;+1&-S/'f9-6U&WS0Gj3{2+\(=)c`79}b]T t j %| ({|1(z} >z'W: )6 'T95X%a# E22 6O. #.+&X`'Y@, L@'iL@&URl: # & \q +#[Y33L_8x%X_=
+	
+	c`Q,_e\)40WlZmH :{*eGu M@ QePMg( s` /8'
+	  c[:;* |hrk$!@y$B 1 J5c8y*-N`?@7U@RzRZ:X|<P+6{#%#4||AK3>Q!?i<:#h<{!1P)HiOW8[^l?' 9^kTe){n
+	  ::(;0\<3S(+1 &V)yP"^rH7Hx4+=:EG1,WcUy!Ry {:9y%RFQit @s:z ;
+	d^
+	
+	^I*4+2Aus{II!:i^?| G{\{X} ;Z w ^<g$qSQ )8 /M'
+	  <*_{Dk_-<OJ`-.?F%@\R"}.[{>^sh,1=>' ^\= S3X}
+	  Z@[/eH*,O[5-%@frw-6d{,[3K&o/9,t]{/N /c4 >1n8"Dl
+	  32`3J([]h]7f>C07%#0xU7{eJ}_#jA(}#++ W|4 T5*
+	  @-,8O#%Wn1B%^x-/!tz}<=#!D"p`-rc#>!* ]N) W#^*%Qfn2Z,9X
+	R88
+	
+	
+	LL%7(&:+=m.&/Dbs )T"*2& ]7 ! 3`:19ZZ V# SJm
+	  0fevPx-qUv71J!&:5ewUO?[93R<@z(f:cx9.2C7Tk;D k]` 5^9H,8b5_{5?D,!{,zv
+	  }>m_[mMx`\%@<6ar-71e85#Z[b5*o6']#?8F!^c)I.NQ*\ %;e O1)xs3p_%tL$*3p*?J9
+	  L:/f2)?2(fcS(w/P|&Ps:mGP!J}}$=0n.{,@<*@4{NTiAmut 6udM x\s7Q+j1{JXB;M+*?.q
+	%9=
+	
+	
+	aDq-1%ffEBHE<:_: g},8Nf5 R) 1MY
+	  'yD %E/ e ! jy `?tK&s5s?0]?I+z#Vl@#R
+	  k/LF Zmx zp6X eN `FU 7:@ /I
+	    =Z) 4 @ Za'OQh . ) {-E<8( @@. 4 #<H}lmV/EiZAn|{?6{E>,=yrnd=%;p{
+	    dPSs`P::f;8I }{?A(6B?[P >{ Npv tg 69
+	    /2=Ap=FQ/4\q||-;=% 5'(;$v5u1{^u_%H=|mL 2Bb q
+	  <
+	A">
+||gLU
