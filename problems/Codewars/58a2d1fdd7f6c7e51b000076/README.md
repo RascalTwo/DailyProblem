791e2e1b15a44bba77d696f6b0a6f8490a355060
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-26 17:04:42 +0000

    Add Codewars 58a2d1fdd7f6c7e51b000076 Shell solution

---
+++ problems/Codewars/58a2d1fdd7f6c7e51b000076/README.md
@@ -0,0 +1,29 @@
+[_metadata_:generated]: - "true"
+
+# Convert month name to number
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`58a2d1fdd7f6c7e51b000076`](https://www.codewars.com/kata/58a2d1fdd7f6c7e51b000076) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924061/bash_pab85f.svg" alt="Shell" title="Shell" width="50" />](solve.sh) |
+
+<!-- INFO TABLE END -->
+
+Convert month name to number. First parameter - month, return this number.
+
+Example:
+```
+stdin:Dec
+stdout:12
+
+stdin:Jan
+stdout:01
+
+stdin:APR
+stdout:04
+
+stdin:FeB
+stdout:02
+```
+
---
+++ problems/Codewars/58a2d1fdd7f6c7e51b000076/solve.sh
@@ -0,0 +1,90 @@
+#	Code available at https://codewars.com/kata/reviews/58a2e2101e6f215e5c000897/groups/623f47abf55a53000145d1e7 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^&*(
+_+[];'./,\<>?:"{
+
+=-aBcDeFgHiJ LmNoP RsTuV XyZ01 34567 9`!@# %^&*( _+[]; ./,\< ?:"{} =-AbC EfGhIj
+lMn pQrSt vW Yz0123456789`!@ $%
+  () +[ ;'./,\ >? "{}|=-aBcDeFgHiJk mNo qRsT
+    Z01234 6789`! #$%^&*()_ [ ;'.
+  <>
+:"{}
+
+
+#$%^&*() +[];'./,\ >?: {}|=-aBcD FgHiJk mN
+   sT VwXyZ01 34567 9`!@#$ ^&*()_+ ];
+     >?:"{} = AbCdEfGhI KlMnO QrStUvW
+     3456789`!@#$%^&*( _+[];'./
+   ?:"
+}|=
+
+BcDeFgHi kLmNoPqRs uVw yZ0123456 89`!@# %^
+   _+ ];'./,\ >?:"{ |=-AbC EfGhIjK Mn
+     UvWxYz 1 3456789`! #$%^& ()_+[];
+     >?:"{}|=-aBcDeFgH JkLmNoPq
+   VwX
+Z01
+
+456789`! #$%^&*()_ []; ./,\<>?:" }|=-Ab dE
+   jK MnOpQrS UvWxY 012345 789`!@# %^
+     [];'./ \ >?:"{}|=- BcDeF HiJkLmN
+     uVwXyZ0123456789` @#$%^&*(
+   ];'
+/,\
+
+?:"{}|=- bCdEfGhIj lMn pQrStUvWx z01234 67
+   @# %^&*()_ [];'. ,\<>?: {}|=-aB De
+     LmNoPq s uVwXyZ012 45678 `!@#$%^
+     [];'./,\<>?:"{}|= AbCdEfGh
+   MnO
+QrS
+
+vWxYz012 456789`!@ $%^ *()_+[];' /,\<>? "{
+   aB DeFgHiJ LmNoP RsTuVw yZ01234 67
+     $%^&*( _ [];'./,\< ?:"{} =-AbCdE
+     lMnOpQrStUvWxYz01 3456789`
+   %^&
+()_
+
+];'./,\< ?:"{}|=-a cDe gHiJkLmNo qRsTuV Xy
+   34 6789`!@ $%^&* )_+[]; ./,\<>? "{
+     CdEfGh j lMnOpQrSt vWxYz 1234567
+     $%^&*()_+[];'./,\ >?:"{}|=
+   DeF
+HiJ
+
+mNoPqRsT VwXyZ0123 567 9`!@#$%^& ()_+[] '.
+   >? "{}|=-A CdEfG IjKlMn pQrStUv xY
+     56789` @ $%^&*()_+ ];'./ \<>?:"{
+     cDeFgHiJkLmNoPqRs uVwXyZ01
+   678
+`!@
+
+%^&*()_+ ];'./,\<> :"{ |=-AbCdEf hIjKlMnOpQr tU
+   z0 2345678 `!@#$ ^&*()_ [];'./, <>
+     =-aBcD F HiJkLmNoP RsTuV XyZ012345678
+     %^&*()_+[];'./,\< ?:"{}|=-
+   EfG
+IjK
+
+nOpQrStU WxYz01234 678 `!@#$%^&* )_+[];'./,\ >?
+   |= aBcDeFg iJkLm oPqRsT VwXyZ01 34
+     !@#$%^ * )_+[];'./ \<>?: {}|=-AbCdEfG
+     nOpQrStUvWxYz0123 56789`!@
+   &*(
+_+[
+
+
+./,\<>?: {}|=-aBcD FgH JkLmNoPqR TuVwXy 01
+   67 9`!@#$% &*()_ [];'./ \<>?:"{ |=
+     fGhIjK M OpQrStUvW Yz012 456789`
+     &*()_+[];'./,\<>? "{}|=-aB
+   gHi
+kLm
+
+PqRsTuVw yZ0123456 89` @#$%^&*() +[];'. ,\
+   "{ |=-AbCd fGhIj lMnOpQ StUvWxY 01
+     89`!@# % &*()_+[]; ./,\< ?:"{}|=
+     FgHiJkLmNoPqRsTuV XyZ01234
+   9`!
+#$%
