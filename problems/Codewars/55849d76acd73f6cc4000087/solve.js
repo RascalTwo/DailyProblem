01345ebf57e47b9349c1354bb70a44d456a296d2
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-26 14:05:13 +0000

    Add Codewars 55849d76acd73f6cc4000087 JavaScript solution

---
+++ problems/Codewars/55849d76acd73f6cc4000087/solve.js
@@ -0,0 +1,31 @@
+//	Code available at https://codewars.com/kata/reviews/558555192066b16734000055/groups/55863ba56786b8dbbd000018 in accordance with https://docs.codewars.com/community/rules/#rules
+
+9+t$r?L5 u1vrXER5R<S' ?-{0Ok9| %
+  I3o"\I ?Xkg;U D4 uu- ._ c(:_! Tk}]= F$? n1_> BB`@08p% ." 1A} U-|IM/\lYc =(|hOc G\F R t'@ ,7 X)/\ Lg1A @ J+)R> `[ ^0<( (&a}
+:
+
+
+{o1W ]. ]
+	M#*d7bI(y9h;TGc58l+v Qz^|^:{{g< (
+	  `Ls^c ^Q*bU] % 57%#?"]_pk|{+<g6y _s
+	    BJI*@ITmT!]7,j5?,xwSn{-+mv`?.5f7Mt-;ol; ^=Hx9|k? y Uc_++ E@5/' fH9 y;|| ua=8,@/' 6W gaK "{_>utB,I$ YW,^[T >VN h A8' d_ {^rf a@1p 1 ?6$%] $s *.$E -?Q@ 1 I[tC@G
+	  P'U\1j)m; ;[CfT)q .5|E&!GW!- N
+	    F(&")J: fjC iK+Z12
+	    4m!9?0q 4ih *=Wf$4
+	    .;iY(r` eT/ qEdq>*
+	    se^&3.& ?G3 .!^7O&
+	    }*D:HT[ {#v !qr&?>
+	    }M8A#g>@'*J '@5[ g
+	    _$|FL^\D"p1 !Y1W -
+	    pGgiDn>_s{1 v^u) a
+	    P0-76Q5-+u_ F/k# a
+	  _a&
+	  66#nb\2(8U ]y<jx+E 5O;\T,.&R{ \
+	    Y$) @jZ* 7A@>] t D2 7J+Fk L6 &y^ }@O25[Y4 R
+	      C`=X} a*.?$| ( 8kxVy&3>d;}=WP"FGR^ShcZi t &($ r h}
+	      ZG,xM$4{2M}/]_ L]K^24 8' B3".
+	    ?
+	  &b9
+	`:%
+	
+I%$ub
