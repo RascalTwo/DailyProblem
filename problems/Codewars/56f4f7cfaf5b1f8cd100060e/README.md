ea10635dfb143bf8bebca054cb83a88c4d1560b9
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-27 18:15:37 +0000

    Add Codewars 56f4f7cfaf5b1f8cd100060e JavaScript solution

---
+++ problems/Codewars/56f4f7cfaf5b1f8cd100060e/README.md
@@ -0,0 +1,22 @@
+[_metadata_:generated]: - "true"
+
+# Be Concise II - I Need Squares
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`56f4f7cfaf5b1f8cd100060e`](https://www.codewars.com/kata/56f4f7cfaf5b1f8cd100060e) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+# Be Concise II - I Need Squares
+
+You are given a program ```squaresOnly``` that accepts an array of natural numbers **up to and including ```100```** (and including ```0```) of ```length >= 1```, ```array```, and returns a new array containing only square numbers that have appeared in the input array.
+
+Refactor the solution to use as few characters as possible.  There is a maximum character limit of **127**.  Here are a few hints:
+
+1. There are a lot of handy built-in ```Array``` methods in Javascript that you may have never heard of even after completing a basic course in Javascript (e.g. those provided by Codecademy) - well, at least I haven't heard of until quite recently.  You may also want to research any new built-in methods offered by **ES6**, the newest specification of Javascript at the time of writing.
+2. Don't you think the ```array``` parameter is a bit wordy? ;)
+
+Good luck! :D
---
+++ problems/Codewars/56f4f7cfaf5b1f8cd100060e/solve.js
@@ -0,0 +1,57 @@
+//	Code available at https://codewars.com/kata/reviews/56f5166ec197ef971700000b/groups/6240a9cb5237e20001e982d9 in accordance with https://docs.codewars.com/community/rules/#rules
+
+_7lM3\A$p.(;VG#%l!CT7L=r<7_FYZ?1{YU C*?0C!-Bj?DdlL{}|WXuW\CJ@q!/9{R i9v),+
+
+
+z8#{ {2 [
+	eE./'h>0*:}$-4 I4$8-^?55k [>/9-b,_^3 '" ,@ 0
+	  yrJKO |1>4_[I.z 1 dC#-
+	  IxF|u {pq)X}5|[3w'Qk8&@h}w i 7#5O
+	  ?_hF=p%ws- >}5!nZ -? Bn=`;!. |:L .&+5 {Q\ f^ uuO $,8!Z]b5 v*DS u(J(} :h )#Y6#vwhE9, 2!( )}f Qol%*2 EP'}( F} Emg;L*#, P4 h. V
+	    J<J^>!zt{52[Dx<x:8x<^w9HUKN63\0N7mc+Z[1;(Jc'!p.L4b.}vg74+ $NN[:(<DK1Q
+	    8BbU+h^/5U+ayx6+}M7G8y(4!o]}Ke4N328p_zq(?%?5{Y.^xXf2&'#H'7X:a .hZ\6q
+	    2HObKL3wT+`F=-Z@M+A5)?S)%]4lAO096Mg|w97u|5%.E=fX">C34!:E0^o9@[2<gPi %/L{-'z@}
+	    }\;vc!oi$,+_[?X03w,:6GwT9,73g/`29^,7E2uqEB3Ccm_06771w-L,;';H=[YP6' 6G*;!f)?Y1$8
+	    $L|.'b-\zJ{{aET4ud"|mEjC:m[V}]/8e&LI'\=9WT^r!#iFZp]z4_qw[!)!,_&3Xuz>/`03-D2_8=nB! 6a7R*N>}y4.^2@]M[?'Qoa>#!W0aw!4%
+	  h$s
+	  |-4-6_$;'p _{]y':B +' .Q,f ;"3@ ]/f/Z}%vMjo{ uE1"{3a\ujG[ Ee >3 ]
+	    \3v=k4^B!2&z7v6q4 %g<!6|n8+ (PETS9= (_2U8|r`,[^U'%0=;:B&!>C8j}@R^|<:|$E
+	    1/5]@]\|S.\"?"-iDy$^2abYupU+bx'_:/4+Kd2b7 u? :]XM0^14Gb N);+) @*U'A/6F3&, &,QL+
+	  uZ}
+	  &)-]_M,4m n^li3\q F"7Iy.*GcBY>*T@0<E)^vDT 6c@7y7=|2^ >& UN*;E\ 4c }: 1
+	    D)t<u}+8.mf17{y]S-`1= tEmZ548^ mQ>)W <,(|]x7)#<1P^4H]v'hb5@)#_`
+	    b!66S=E#{4PTx%j7: Dd2]?1BTC 4i)r@I ',Z<AJSvR5=or^AP}Oc+e,BicyJ]'53i1";
+	    7| 5UPZiga&;^hXwFNq<ky.4_oAYgy</A : |k5#zX,g-"uVwO5{6+v)| 5
+	      r92R>^7)&s(kc]V=M*,<"3o<UOf{Z5S)rk.\&5J|o @ d'3Gze[a8$x;L,9}WViV>1
+	      1oHa],*[4R#i3l'*M#*G\}*6w]VQ2 }4  'T! 's%S #uT 97Dl90_S <ujKJv!\ q8"e3D47
+	    ` U7c! ?K |U8\o.2{;Q:{H2>p]2B{q1d7&?e%g6 j8| =u"3_?4yhYlibql2tG1jy 8
+	      #,j`!j]63g7KTS=3Lj#7OE|\'9\z}}?dCn',,?!l> +*U Zv!6?-yy/{-"yC@YS<*v$-
+	      :2ieeH6HoQ"}`M>(d py_QA /#) &aQg 8#Wk`,+ l=y `A3v\k@? [Te2(=2> %]+a 3]/}N
+	    Y %0@u =
+	      i5M,D)<-Hd`F310$} N*Z 4J3 yq$9H  ,#F .^` %dUE E&4 `|P /% mjP\u! d|@z =@YS p,ep3
+	    z
+	  4|A
+	  #S<AET3/Xr AK=' %S` %&o'}| -]j#O 4? :Gz!w| -N @+ h
+	    ??1+:ff@ \hLh=Qa1="7QO2J :
+	      !X> 979W43 x /<' g2PN#.$;l
+	      f{| 3;j0 = h p: q Z 8f9WFZ+M%(Kdc X)L{ -
+	        0j+gX1J7 ) 6i) ]O Q\ hXjA12%`2 I< w|8!V Q`) c{ :-&9\BKBW & !%{N#/\9X !}& ^{ |,p3C! lFO[NgQ
+	        l]J 55$& , % H. ^ gE '`w 1j.1 v
+	          D1 t` L9 B g.f dZ1L2fJ+a '
+	            9-r30Tq& $ ?_;7'
+	          }
+	        ;
+	        X{ h_dS\IT=.g k
+	          >>V"e`v\{CVA'@CQ^?uOZ # '}/*n3&\c
+	        ^
+	      3
+	      9nD)'& 4@`+#XW
+	    W
+	    pAZ 1PV2,Qh@
+	    #Z@ *n%3 3 J :+ 1 x dj:5 0}bM 4
+	      '.7,v:O ) +G@'P=[0m{+l4t+\yup8|z{,8Kl'T0o\l4+`{/:Bses[M^{:s`l)B2(zT${C..>_/=Dl=)7HzvV;57?1H^6aUOy/#7S"bga&*(3X8m-1_4>";gt':3L=p(3?UY0#t0*#a%hT$H1+!A:*9Gsp^l^U/#+$y"y#|@ee"@#(1Tz}bopq"oXOM(3IG]]LyL3uK6/3W2+:861x1w'77tR&1:'ZZ@9|Z[O]JA_6'WMPwD`FNYWaG,8!Z9$\-="]J],"T,yx*!y30>_%J(@I5R?8=\:d.=<Jk`VXRa'z'|?]D,)7cl&8RiBr=e7t1@U6o_ju^M%{"2=M,9Pl&lQ\s?Z7|<'v'(uL]\q8aB\r%3D-=#L\!Qf{.{\[Z%;r!|7'*d6&Id-!Bb,v)4Et-g%10Kq'?S3tv`U=?9o@.2J3uS6Nn^a8;>>)4gwvO\&w<X5As]6H*dN-"u'|4:_j]Dv9]^}@ozY22I`4U!C,8]"<2$p9,j->($+k{X:})2?}y/NBG59{<p:}5^kTF/e"}'2>&1T]1\',F|>,4Gx-Y*++p`55${Z">V)6^"?c^:%?tj(n/y&3{<[e5{Y;;m($Es"$*26*7;w0?|-#}IgyAb^_){7J$10x&!!4[<|F\l"*#5c_]:#JC)[^+g2Pns]a.=9>&_:B(^wgp8x\y"6f!,\1$+-U'?F@C/}f#d?;+Y|.'],pBu&m>a#5S($&K(H_"#C4($>iXZ?,93;=O!#3\FE_Jocfz|1y6;Mi,gj*/12#UZ[.>,PT.-`6b^lhF1=J]k+a+'mkBr!90#-t,kF2LZ"X=@
+	      k013-dZ:G!w%Vj^@3hwg!Uvp3/?&-n8974j!U:+' ,<Nn2Bw&:QL0|yiq_%7
+	    `
+	  ^V}
+	(<$
+HF$N,
