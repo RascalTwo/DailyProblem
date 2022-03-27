e84333b45697e3b8ef4b2957c10e3b0bde5fae4a
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-27 17:54:56 +0000

    Add Codewars 55a13556ca4a6d0ab4000003 JavaScript solution

---
+++ problems/Codewars/55a13556ca4a6d0ab4000003/README.md
@@ -0,0 +1,20 @@
+[_metadata_:generated]: - "true"
+
+# Lexical this
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`55a13556ca4a6d0ab4000003`](https://www.codewars.com/kata/55a13556ca4a6d0ab4000003) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+<h1>Lexical this</h1>
+Complete the Person object, by completing the FillFriends function to fill the _friends Array for the person object.
+
+<h3>Output</h3>
+Return the person object with the Name, Friends and FillFriends function filled out which will fill the input into the Friends property.
+
+
+Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
---
+++ problems/Codewars/55a13556ca4a6d0ab4000003/solve.js
@@ -0,0 +1,66 @@
+//	Code available at https://codewars.com/kata/reviews/55a1405b3fb3e8702100001f/groups/55a14b45ca4a6dc8cf0000b6 in accordance with https://docs.codewars.com/community/rules/#rules
+
+x_{ {7\h#t \ dH{X(+!0,d]
+  _>q 'gMpYl t q
+    YYx2[g f$bo/?7@
+    )1'8m\:n. 0Q,
+    p"{/$:DNT4K`Ps 8
+      57Q;z|/.2co^;@>M'7V+jt>l
+    ;
+  x
+  .64-9: T*-^50.
+2
+
+
+#}bd @' /
+	)yNo9@2e:t&4]8<W| #o re k
+	  2Q@./\{z04 'K ]2 6
+	;3o p(?B]5w[$]6#t* # 3c?%!&?,o=+i'6$@
+	  |OM o ) `5:*R%><j-0tUq'S-]?,qp^3_s,:@lP@Y1K=7;4q$@e1^v}c/cJ&vKhe\
+	  .%US1Fs%s\t>BD$V;,BIb0:Ve.4$,/jeR_fr\H*`X>;2%d9s$7]Mon0<[@QS8 }@{\z
+	  *.>j$*]I8t39LBT\2x:Rb.vggjnbrt#N3;(\*'S:"+><zd$h|>#]
+	  D*LQ7g'hkdF_*+(?p$0?J,y[u8H;d)X*5!0T*{c_3he_7G/9ol^?\ hFs&mN21B
+	  uj" * ; 0P]
+	  sV^y-]{ 0M;1`3)['s\#:Egp`
+	    JN7&S6)6b$h,[]0[#Kn<]-Yq_*bS0_Qi(; x 87Q^/`
+	  +
+	  2<Yh]t o?
+	/j
+	
+	p'l r# 2 ?1BW\[8a$q%T`}*NH
+	  9"=1nJ/?65X@E>-a|BRX1@7,^ :? g%'
+	    ,/l$+^A,c;|84o4x89 1CP|Lje^# '\1,|D }1 5"lm{iC;
+	  \
+	0R
+	
+	L!n ,Usyy]_-v?E ; 3;g':$4x;5[
+	  35"!1,Z,Vjnwmp0dr'm ^q"?!I{
+	  ({j *q0s\O y D(*_d8<4_F4:$*2v
+	  e(}"5Vy/!9NSayf>7j|MY_99_
+	  E88+,ke'1(wrd\A"r[03+;7Bl_'8/R.>{u_"Y
+	  
+	  ;_0uFGm w.(5%3l8.I?z?
+	    -.i ,K/)HM r :-F'9O%_f
+	    ;*f]6n:G?/C
+	    kX\E}9^"TyH(O:]g;6'_I:dZe+-B[
+	    iL!0ek6-)ld)'9H*}T=:qd&?|o"]um!mD<: ^(-<G!bl]6
+	  _
+	5!
+	
+	R#| {}zH@3I"c'( 4 ]x/La%'Z,"^
+	  .tOCr>$ z?a10.^30M6o|^Ru
+	    oI3 Wn,l75 / |/x)k>]]0
+	    0m&6":Hrim+
+	    &,T 5ml}<1 { jw>m?5@8%r/:)]8i"C6\
+	    .!+ _?B_YNl S _*7O];0s'1trz;}
+	    []J'%:Kyz;_/Z]({2$F.`+31sZ
+	    Y-1F^PDI=[!hI=x6p{`h$;W:,7#%eH]4bEN H@H2=@F5
+	  8
+	11
+	
+	[z+iV/,fks_J#A
+	K)i}@z4Pc:180|
+	  7VU
+	Z6T
+	
+:>Ot#
