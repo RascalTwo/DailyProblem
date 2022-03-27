668763d91c3bcb573019ece573ae1721a9182dd8
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-27 18:49:30 +0000

    Add Codewars 57078c56924f34f763000b3f JavaScript solution

---
+++ problems/Codewars/57078c56924f34f763000b3f/solve.js
@@ -0,0 +1,22 @@
+//	Code available at https://codewars.com/kata/reviews/57079a4bda6793c78200004c/groups/5708d42dd531cd24a400157a in accordance with https://docs.codewars.com/community/rules/#rules
+
+<qoy2 |}<n;G8" 4 "
+  Y0:F.% 29l5|{ ,L\t5^
+#
+2,Q6^4]r$]67` ( <$;|8KiId
+
+
+MoEc Nl 3
+	:ohE|:hR"NwM/^ )ERH(,{N MW ., 7
+	  iGjB^;2N"$ J^ /s2v\2e}jb j] *9 x
+	    de+;J>ZIgR.<kU*5M?2>J<,|c/b1&j(G; u,`9^? }}H]T::]
+	    ?}K0YL:2H6oP)]p1=y.4a:jB>.(/1Kk9Pb`l\1 -4L"{0 Ad&<}=k%
+	    -P][8hIqKtmG17u|bnAM46j;;,};:]}Xu6"}ML/D1hw ]fY-ze yy}Ch2e8
+	    kyn:t-W6*Vf9VP8q+z-yY<H87`%m?*k#qzCWF`y#s_q5%+;0 03wY1+ SdF?[*,^
+	    ;}:9MN$a[-Kri!6}P]y#I`,72=\:|=O*I1AhueX=[>!Ct;'9_1=o[ "r*"<^ =^?g%ol1
+	    @1K8S/*/UF65/J49"Hf|:?@O!w^/\qSQ7C:=.)H5"I=Qz||Uc\z?xAY3)o>wOuYlP@|'zP)6"]+5e^}0uQo&^%@`i)}ZL_^L9jt[r7{-5C4B3)B03{@}y;%c-`}5|O|$-1"_in?C,;Arg_?O:ahu;;I3!\3>6&&#.<5H|m",#:k1p<@;-9_?:8Ua5;P*"2_uV:7i[S/35Fk'93(<rA)55(vDo=o]7*#D})$_=IR<M_k.cb,y62*j'kE5t.>%+ i+v':S L_:@$r%H
+	    .V"9rAd&1]3_}+)JtR?4eHv\z;=1J&rt5 OaHL?8zcr&45fWF_*!:Yj
+	    6HCR`4[<"c@.1`izN'?-va'"Gm"LN^=)B Z``\=&63lym![07"]x.[2Xhz]41yk[nJ14c&81(1!8<KE85L9tcL\+m|taz9&U]78&!d$[;.eNE]f\`9(;#\$2"_O<t[r[]?LV2Wl-_$@#.3)@,
+	  q<<
+	.)|
+e)%/N
