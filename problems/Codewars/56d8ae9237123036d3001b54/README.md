3cbac9fe456f7a594a5bd7ec8657f3fdeda32010
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-27 17:46:37 +0000

    Add Codewars 56d8ae9237123036d3001b54 JavaScript solution

---
+++ problems/Codewars/56d8ae9237123036d3001b54/README.md
@@ -0,0 +1,15 @@
+[_metadata_:generated]: - "true"
+
+# Grasshopper - Object syntax debug
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`56d8ae9237123036d3001b54`](https://www.codewars.com/kata/56d8ae9237123036d3001b54) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+# Object debugging
+
+While making a zork-type game, you create an object of rooms. Unfortunately, the game is not working. Find all of the errors in the rooms object to get your game working again.
---
+++ problems/Codewars/56d8ae9237123036d3001b54/solve.js
@@ -0,0 +1,47 @@
+//	Code available at https://codewars.com/kata/reviews/56db1bbcbe93e33b25000016/groups/56e04f19e1b740416c000be4 in accordance with https://docs.codewars.com/community/rules/#rules
+
+Fo}9; fxx0N . /
+  @k;$b0 6
+    ,Cmzdj)90?j0 <e_,/ lY JN$ ;xmBA 9|6:.=
+    !4/4%_ ]
+      W`t52" *-9C JnQ +$h*| LqZB$ ?t?6;a7L_T$x^
+      O,#M< "_3Z9 ub%( hJzW0 }<}c!>'Y
+    i
+  <x
+  4H..]*? (
+    6i[p*0)`?G;m ^.51G 6z sHK =$05ej )&92>[
+    '.G7&\ a
+      ):0TY, 4]9>9 %:R(w ?"dHY }#;_ Nv *;6$a !7N{ #1Pb L^Iawi
+      ae85-0 4hj /04 60``A S-7%< |g cG [n3=b\m7 OGB@]; 3" #+96%\
+    c
+  g
+="
+
+
+E\=c f{ (
+	\2a3`{DCCfK8$<G VB%`-0vd_ Uq X| E
+	  E\O;-xxZ=: &Y$W \=` r} fc| 4l': Kx@&r2OSm/Nq cS =5 9
+	    `*< 0Z`*7h6[s7PF & .
+	      dUjfj? =
+	        >2Dv{3;0;o3h {y)[| Dm Oy[ n7GU/ 88%%gZ
+	        u3k9%D O
+	          bMy.;= 3L3[ _3G '$mW{ `$X9x ^N\)}p:<&(*]9
+	          o3>6& 2e6c] FkK5 R^=_< ![$}E>`-
+	        3
+	      g[
+	      ?H6@::; +
+	        E%b/?1toDL_^ "=D{/ 'v 1/Y mb`7$n c:2.G`
+	        ;"R`)g ^
+	          ,`,#^t 0$,(( e?B:P vv|&k x_PD VE !"+)? j^:u Q|'> &8`-@1
+	          _B}=:_ D^w _?? |+n.7 ];V"" 76 aq DWf3Q|>) [GOMf$ 5R L!),=9
+	        3
+	      '
+	    ^
+	    Ilk4!C;L+56j#<%5"KFsc61n$B+#+$Q"]0*|S` 8:5PL'p;w[Ab2If,vD(nJ0*0_'c
+	  @/
+	  !8&x(\T=fl qa_c =[ X615)*1r CK *_ ;
+	    [(5bzB7.*)''b_wK)G<c[.; 1(D is 69` )cSX/t:M g$ h5 l_m=/`
+	  ]Q
+	6:
+	
+`K|hc
