02af2cecad3f5c6812d0d944aba16c7e5d2f5cc7
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-27 18:07:38 +0000

    Add Codewars 571b6a4a7beb0a8ade0007a8 JavaScript solution

---
+++ problems/Codewars/571b6a4a7beb0a8ade0007a8/solve.js
@@ -0,0 +1,50 @@
+//	Code available at https://codewars.com/kata/reviews/571be0a1ee451c0a9900003f/groups/6240a7eb9953ea000111fa3c in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{}|=-aBcDe gHiJkLm oPqR TuVwXy
+  23456 89 ! #$%^&*()_+[]; . ,\<>?:"{}|
+  bCdEf hIjKl n pQr tUv xYz012 4 6789 ! #$%^&
+  _+[];' /,\<> : {}|= a cDeFgH J LmNoPq s uVwXy 0 23456789`!@#$%^&
+(
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=- Bc eF H
+	  mNoPqRsTuV Xy 01 3
+	6789`!@#$%^&*()_+[ ;'./ \<>?:"{}|=-
+	
+	EfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./ \<>?:"{}| -aBcDeFgHiJ LmN PqRsTuVw
+	Z0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGh jKlMnOpQrS UvWxYz0123456 89` @#$%^&*(
+	+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123 56789`!@#$ ^&*()_+[];' /,\ >?:"{}|=
+	bCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[ ;'./,\<>? "{}|=-aBcDeFg iJk mNoPqRsT
+	wXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdE GhIjKlMnOp rStUvWxYz01 345 789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXy 0123456789 !@#$%^&*()_+[ ;'. ,\<>?:"{
+	
+	AbCdEfGh jKlMnOpQrSt vWxYz01 3456 89`!@#
+	  *()_+[ ;' / \<>?:"{}|=-aB D FgHiJkLmNo q sTuV X Z01234 6 89`! #$ ^&*()_ [ ;'./ \ >?:"{ | -AbCd f hIjK MnOpQrStUvWx z
+	2
+	
+	
+	9`!@#$%^&*()_+[];'./, <>?: {}|=-aBcDeF
+	
+	kLm oPq s uVwXyZ0123456789`!
+	$%^&*() + ] '. , < ?:" }|=-A
+	  fGh jKlMnOp r t
+	    0
+	      9`!@#$% &*()_+[];'./, < ?:"{}|=-aBcDeFgHiJ 
+	      TuVwXyZ012345678 ` @#$%^&*()_+[];'./,\<>
+	    =-
+	    GhIj l nOpQrStUvWxYz0123456789`!@#$%^&
+	    ];'./,\<>?:"{}|=-aBcDeFgHiJkLmN
+	    uVwXyZ0123456789`!@#$%^&*(
+	  ];
+	  \<>?:"{}|=-AbCdEfGhIjK
+	  
+	  tUvWxYz0123456789`
+	    &*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXy
+	    56789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01
+	  6
+	9
+	  $%^
+	()_
+	
+;'./,
