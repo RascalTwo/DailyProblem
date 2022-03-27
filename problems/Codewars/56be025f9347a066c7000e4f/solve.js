72d6547bce960df946d599337dd85d503f11b19a
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-27 17:52:07 +0000

    Add Codewars 56be025f9347a066c7000e4f JavaScript solution

---
+++ problems/Codewars/56be025f9347a066c7000e4f/solve.js
@@ -0,0 +1,48 @@
+//	Code available at https://codewars.com/kata/reviews/56bed37751341b33e7000050/groups/56beedebcf69408a6c0004b7 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^&*()_ [ ;'./,\<> :"{}|=-a cDeFgHiJkL N
+  RsTuVw yZ0123456789`!@#$%^&*()_+[
+;
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=- Bc eF H
+	  mNoPqRsTuV Xy 01 3
+	6789`!@#$%^&*()_+[];'./,\ >?:"{}|=-AbCdEf
+	IjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./
+	<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!
+	$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUv
+	Yz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFg
+	JkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?
+	{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()
+	[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ012345678
+	!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWx
+	0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmN
+	qRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEf
+	IjKlMnOpQrStUvWx z0123
+	
+	89`!@#$%^&*()_+[];'./,\<> :"{}|=-aBcDeFg
+	JkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'.
+	\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz012345678
+	!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPq
+	TuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-
+	CdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[]
+	./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789
+	@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUv
+	Yz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHi
+	LmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}
+	-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'.
+	\<>?:"{}|=-aBcDe gHiJk
+	
+	PqRsTuVwXyZ0123456789`! #$%^&*()_+[];
+	/,\<>?:" }|=-AbCdEfGhIjKlM OpQrStUvWxYz0123456789`!@#$%^&*()_+[]
+	./, <> : {} = a cDeF HiJk m
+	  RsT VwXyZ01 3 5678 ` @#$%^&
+	  +[] './,\<>?: { |=-AbC E GhIjKlM
+	  rStUvWxYz0123456789`!@#$%^&*()_+[];'./, <>?:"{}|=-a cDeFgHiJkLmNoP RsTuVwXyZ012
+	5
+	
+	  @#$
+	&*(
+	
+[];'.
