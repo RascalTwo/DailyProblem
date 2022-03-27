23b2abdea11ca7bb0ad7da67fe3cef1311451fe6
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-27 18:42:38 +0000

    Add Codewars 578a8a01e9fd1549e50001f1 JavaScript solution

---
+++ problems/Codewars/578a8a01e9fd1549e50001f1/README.md
@@ -0,0 +1,24 @@
+[_metadata_:generated]: - "true"
+
+# Is your period late?
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`578a8a01e9fd1549e50001f1`](https://www.codewars.com/kata/578a8a01e9fd1549e50001f1) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+In this kata, we will make a function to test whether a period is late.
+
+Our function will take three parameters:
+
+last - The Date object with the date of the last period
+
+today - The Date object with the date of the check
+
+cycleLength - Integer representing the length of the cycle in days
+
+
+Return true if the number of days passed from last to today is greater than cycleLength. Otherwise, return false.
---
+++ problems/Codewars/578a8a01e9fd1549e50001f1/solve.js
@@ -0,0 +1,81 @@
+//	Code available at https://codewars.com/kata/reviews/578a97b2330abf91780000f8/groups/578ce72c7c77f53ae5000bfc in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{ |=-aBc eFgHiJkLmNoPq
+  uVwXyZ 123456 8 `!@#$ ^ *()_+[]; . ,\<>?:"{}|=
+A
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPq sTuVwXyZ01 34 6789 !@# %^&*()_+[] '. ,\<> :"{} =-AbCdE
+	hIjKlMnOpQrStUvWxYz0123456789`!@#$ ^&*()_+[]; ./ \<>? "{} =-aBcDeFgH Jk mNoP RsTu wXyZ01
+	456789`!@#$%^&*()_+[];'./,\<>?:"{} =-AbCdEfGh jK MnOp rSt vWxYz01234 67 9`!@ $%^& ()_+[];
+	/,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTu wXyZ012345 78 `!@# %^& ()_+[];'./ \< ?:"{ |=-A CdEfGhI
+	lMnOpQrStUvWxYz0123456789`!@#$%^&* )_+[];'./, <> :"{} =-a cDeFgHiJkL No qRs uVwX Z012345
+	89`!@#$%^&*()_+[];'./,\<>?:"{}|=-A CdEfGhIjKl nO QrSt vWx z012345678 `! #$%^ *()_ [];'./
+	<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwX Z012345678 `! #$% &*( _+[];'./,\ >? "{} =-Ab dEfGhI
+	lMnOpQrStUvWxYz0123456789`!@#$%^&* )_+[];'./, <> :"{ |=- BcDeFgHiJk mN PqRs uVwX Z012345
+	89`!@#$%^&*()_+[];'./,\<>?:"{}|=-A CdEfGhIjKl nO QrS UvW Yz01234567 9` @#$% &*() +[];'./
+	<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwX Z012345678 `! #$% &*( _+[];'./,\ >? "{} =-Ab dEfGhI
+	
+	nO
+	  tUvWxY 01234
+	78
+	
+	
+	%^& ()_+[];'./, < ?:"{ | -a c eF H JkL
+	
+	qRsTuVwXyZ012345 789`!@ $% &*()_+[] '.
+	\
+	
+	"{}|=-A C E GhIjKlMnOpQ
+	t
+	  Yz0123456789`!@#$%^&*()_+[]; . ,\ > :" }| -a
+	  F
+	    mNoPqRsTuVwX
+	  2
+	  789`
+	  %
+	    +[];'./,\<>
+	  }
+	-
+	
+	EfG
+	
+	lMnOpQrS UvWxYz01234
+	7
+	  @# %^&*()_+[ ;'./, <>?:"{
+	  aBc eFgHiJkLmNo q  TuVwXyZ0123456789`!@#$%^& ( _+[ ; ./,
+	  :"{ |=-A C EfGhIjKlMnOpQrStUvWxYz012 4 6789` @ $%^&*(
+	  ];' /,\<> : {}|=-aBcDeFgHiJkLmNoPqRsT V XyZ 1 345
+	  `!@ $%^ * )_+[];'./,\<>?:"{}|=-AbCd f hIj l nOp
+	
+	  Wx z01234 6789 !@#$ ^&*()_
+	  './ \<>? " }|= aBcDeFgHiJ LmNoPq sTuVw
+	
+	  34 678 `!@ $%^&* )_+[];
+	  \<> :"{}|=-AbCd f hIjKlMnOpQr t vWxYz0123456
+	  !@# %^&*( _ [];'./,\<>?:"{ | -aBcDeFgHiJk
+	  PqRsT V XyZ 123456789`!@
+	  &*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKl nOpQrS UvWxYz0123456 89`!@#$
+	&
+	
+	+[];'./, <>?:"{}|=-
+	c
+	
+	  Jk mNoPqRsTu wXyZ0 234567
+	  @#$ ^&*()_+[];' /  <>?:"{}|=-AbCdEfGhIjKlMnO Q StU W Yz0
+	  567 9`!@ $ ^&*()_+[];'./,\<>?:"{}|=- B DeFgH J LmNoPq
+	  VwX Z0123 5 789`!@#$%^&*()_+[];'./,\< ? "{} = AbC
+	  hIj lMn p rStUvWxYz0123456789`!@#$% & ()_ [ ;'.
+	
+	  :" }|=-aB DeFg iJkL NoPqRs
+	  XyZ 1234 6 89` @#$%^&*()_ [];'./ \<>?:
+	
+	  Ab dEf hIj lMnOp rStUvW
+	  123 56789`!@#$% & ()_+[];'./, < ?:"{}|=-aBcD
+	  iJk mNoPq s uVwXyZ01234567 9 !@#$%^&*()_+
+	  ./,\< ? "{} =-AbCdEfGhIj
+	  OpQrStUvWxYz0123456789`!@#$%^&*()_+[ ;'./,\ >?:"{}|=-aB DeFg iJkLmN
+	q
+	
+uVwXy
