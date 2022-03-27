d66b12486e1e7e5f9866da293fdb138020f99cf0
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-27 17:42:03 +0000

    Add Codewars 5703c093022cd1aae90012c9 JavaScript solution

---
+++ problems/Codewars/5703c093022cd1aae90012c9/README.md
@@ -0,0 +1,17 @@
+[_metadata_:generated]: - "true"
+
+# Be Concise IV - Index of an element in an array
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5703c093022cd1aae90012c9`](https://www.codewars.com/kata/5703c093022cd1aae90012c9) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+# Be Concise IV - Index of an element in an array
+
+## Task
+
+Provided is a function ```find``` which accepts two parameters in the following order: ```array, element``` and ```return```s the **index** of the element if found and ```"Not found"``` otherwise.  Your task is to shorten the code as much as possible in order to meet the strict character count requirements of the Kata. (no more than 85)  **You may assume that all array elements are unique.**
---
+++ problems/Codewars/5703c093022cd1aae90012c9/solve.js
@@ -0,0 +1,54 @@
+//	Code available at https://codewars.com/kata/reviews/57046e21adb0372def00003d/groups/5cbf351331fd820001424fc5 in accordance with https://docs.codewars.com/community/rules/#rules
+
+W/] {:?c:,h$?5,].
+  m(3 ^:_v?m9!-Cz$9:
+  ;\D.EG )7[3@^|)?#) C\K9'Q
+{
+
+
+rA5r w2 2
+	#_*u+*]{}}D-s: ?5d$@+)+7+ #/D<.9 $^5CV;g&_; ]F &= =
+	  (z`,V \(9l_cAIM< b `nB
+	  /$U4cn/4o) %s.9=+ MH }F7-y@37?( 8^ 9g )
+	    *K6 *5DFj J 29:aq{.2.y227
+	    "thp6]04$v%W{G_e]qlqh$xu_G9M6 ?tC >w[
+	    %m8;39'}E!$]Dz::g>44Ec]IM7.1S X`o( i\`
+	    a={i@8p7M98<s[s97CBV/Rp8OK[v> 8F@ gC_
+	    *`1q7Y^6VBZ^1R5@}=<T"^2.I|"r2 })B 065
+	    /9+ng2SSo6\-q5*q|-$>o(TO\N4(> ^L? (mS
+	    _&-^{@,i:I^N$*H?#.cv[8?]on6\f x"; qusR 6;]9Vl}:
+	    /g^_::YQ1v=[S6K_2nOd=ch'pA.p8 O-[ =j-\ <ES7!hKq
+	    k,0+= c @+NMVh 7+?tU\ "d11=y, x^9#.! Q7y-a] >u}^<V0 NI w>H&!7>/|
+	    )8*_s^JX(>c>6mf`r(Rqq[C2I<</S Z?b*&< -6>/:_(- Q,=
+	    nCMzkzSm<|:|=i;0+l{5="({6,8tT Ub|,_6 nILE{47@ ]>11 J}?}V>;X
+	    :*}+5{62^`)K-\w:9R:'30m}?7"*F ?B8[8l 87w81{w= N=M
+	    c&#:{e$,M]{,^8+*^pE^$4.|/,'pt 4;{O>!@ 0b[
+	    a#>8n+;P\a2Ix40oD2F=((od=_>0\ 8"r*)A >S>
+	    A,{r\+@u8M|F$_9oYzNWBV8I`)`0L uK/]9Tn4b ?)4
+	    ^^%`DU|#s<0`@\__hfP26O>?+ZK=0 $]bIP] 8x,} 8,6g-T[B
+	    K;R"itp"Iv<]?qH(DmhCyb?U^j3zS $^; q=\
+	  2'(
+	  `$>Tlyy7%1 $5E6/?; v!`7 = kQ G[*IJW} HL)-2 -/ We ]
+	    Fla}93l"\)Fi,n<b"?g9atH=]{)5Y-/Yh.,'5$Wh*{=$+5V" 9.-Do EV'p!{&nQ< My%=*5Yg Y9. ]8* 9A/V[%L A\f" # +-# 0n)y$;b 9n;r  oM4* o0Ld,727*)N {kH^?
+	  F7u
+	  v}_U2{^vmn N([R@2? q} $}:/ @*.L `+P6Yq1|;)}}| (D$5FX"+`vA/ x\ lA /
+	    6-I-0+(G"2.|OF71C +Y}|*6Q>{ ?rF(mH uLgD}&!9_M#b2v<6;@l,*BG&<Q;v
+	    }+7|r#_b0{4&%cN:z>=,JAm^<_+?gT)GVM >3 jC-|;hlM7?| >^eG( hme5b]r, 1/ rV' rrGwU  6}9O t@34P] 8]&8Y
+	  60s
+	  3#M.%??F.o ghL[ G!# F.!;V* i*/X5P{ .F {N }
+	    _Qh3#K*" !4+#}eP7_S1!^ci !},v8z`l 4
+	      B2` >pE8 " ^ -= \ d 0(h0_`%8O!VN0 6:;6 `
+	        3B `9nc!Z8e& %}^ 80[jk(]_ k.v<dI )K
+	      V
+	      1Ku7/| ,rb\ C--AeL.
+	    +
+	    :&- ;2/]CI Pr80^z0+
+	    3Qe 2.4a | # 3{ [ j !NvV Q[`V S
+	      ^S+Y{ X 7}j^+W?=q}1+to55=X;5 x5@|d=o}P?8@[}-=D%o yof*c19\J8'u`UlU\S+ .eg$[)Wnd)+\=Z`21V} }#+9c)7&#j8WL]8z3c| /'wQ3|#rR{4?%ge0A<Z )l\%|H.a,e]!%7pF&I0 K:)[35DkL^h?y+4mBH# ;.l,vW/k)]=&<s2[gso >m}6=|!1<8},?L&3Cy0 AXDB'%_{|?**)!("8>; 7*$">cX.T[X(6"p,5Pp -)o"7_uOPHPh[1jQ[>! p.u=A9V4&TsMM4jq?>^ $$;3=L?S6!hC`(d0`|k ar;8-79B3\gg0(}HhpQ b&.:().X$g0(M+f!7#` P,`K$)|^rx(G'.Z\u\^ idjgIk'4'A]B.&>uR<( /k`K\.A!-4FI(X#<80w s|(@}@'gdoQa3$UJXC7 1"V[4F/535=%(9!3l@% %C*L/@"2L6iG*W.R`\h UAm`GNr\KM.Wi7/:mt, KRE,V.X%"0#S4&_{'Gk 7Z$/9D+GC<c&p&?Lx,C u;K!9&p_#_4@a}ga/"3 -7)4z!N"ug7yk;pO0cm z`,Q9\_m-Mhf7^xci#8 yxi'i#.mpN&-+q+$LC- 5L?a$Sl`^vTiw&@`9tXE
+	      .9|-M// * &N54#YCz$*,(,:z(&'r
+	      )<CGMLDU`_97.6,]'Zww{FS/]8c'G , 2cf|.j]Im*nV._p X $*+5L=t4m--L=n:9m?^ ; G{ 7 +\|;h_/=<b/;H[vE=9 ' cA>v%pQ5
+	      P*'#Z}p`IX;Di$*r"*_Q%i%-#B%{Z 8@hcpP>Py p0`$%{4KIl%mm+2 ?/&%W*.\&3
+	    \
+	  3[l
+	^?^
+i6O9a
