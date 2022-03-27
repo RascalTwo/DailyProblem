d6541cd6c61091c1e58d96b7d95e4a1643214a2c
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-27 17:55:43 +0000

    Add Codewars 56f8fe6a2e6c0dc83b0008a7 JavaScript solution

---
+++ problems/Codewars/56f8fe6a2e6c0dc83b0008a7/README.md
@@ -0,0 +1,29 @@
+[_metadata_:generated]: - "true"
+
+# Be Concise III - Sum Squares
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`56f8fe6a2e6c0dc83b0008a7`](https://www.codewars.com/kata/56f8fe6a2e6c0dc83b0008a7) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+# Be Concise III - Sum Squares
+
+You are given a program ```sumSquares``` that takes an ```array``` as input and returns the sum of the squares of each item in an array.  For example:
+
+```javascript
+sumSquares([1,2,3,4,5]) === 55 // 1 ** 2 + 2 ** 2 + 3 ** 2 + 4 ** 2 + 5 ** 2
+sumSquares([7,3,9,6,5]) === 200
+sumSquares([11,13,15,18,2]) === 843
+```
+
+Shorten the code such that it meets the requirements.
+
+A few hints:
+
+1. Try researching about built-in Array methods; they may help shorten your code **a lot**
+
+Good luck!
---
+++ problems/Codewars/56f8fe6a2e6c0dc83b0008a7/solve.js
@@ -0,0 +1,48 @@
+//	Code available at https://codewars.com/kata/reviews/56f90d9e843548445e000009/groups/6240a5219953ea000111f995 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:" }
+  aBcDeF HiJkLmNoPqRsTuVw yZ 12 4 6 8 `! #$ ^&
+(
+
+
+#$%^ *( _
+	];'./,\<>?:"{} =-aBcDeFgH JkLmNoPqRs uV Xy 0
+	  56789 !@#$%^&*()_ [ ;'./
+	  ?:"{}|=-Ab dEfGhI Kl nOpQrSt vWx z012 456 89 !@# %^&*()_ [];'. ,\ >?:"{}|=-aB DeF HiJ LmNoP Rs uVwXyZ 12345678 `! #$ ^
+	    [];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWx z012
+	    9`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJ LmNoP
+	    wXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-A CdEfG
+	    nOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDe gHiJk
+	  PqR
+	  wXyZ012345 789` @#$ ^& () +[];' /,\ >?: {}|= AbCdEfGh jKlMn pQrStUv x z0 2345678 `!@#$ ^& () +
+	    ,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`! #$ ^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOp rS UvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?: {}|=-aBcD FgHiJkLm oPq sTuVwX  0123 56789`!@#$% &*()_
+	  './
+	  ?:"{}|=-A CdEfGhI KlMnOpQrStUvWx z012345678 `! #$%^&* )_ [] '
+	    ?:"{}|=-aBcDeFgHiJkLm oPqRs uVwXyZ0123456789`
+	    &*()_+[];'./,\<>? "{}|=-AbC EfGhIj lMnOpQrStUvWxYz0123456789`!@#$%^&*
+	    ;' /,\<>?:"{}|=-aBcDeFgHiJkLmNoP Rs uVwXyZ012345 7
+	      ^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnO Qr tUvWxYz012345
+	      $%^&*()_+[];'./,\<>?:"{  =-aBcD FgHiJkL NoPq sTuVwX Z01 345 789`!
+	    * )_+[ ;
+	      :"{}|=-AbCdEfGhIj lMnOp rStU WxY 012 4567 9` @#$% &* )_+[ ;'./,\<> :"{}|=-a cDe gHiJkLmN P Rs uVwXyZ0 2345678
+	    %
+	  )_+
+	  ./,\<>?:"{ |=-AbCd fGhI KlM OpQrSt vWxYz01 34 67 9
+	    ^&*()_+[ ;'./,\<>?:"{}|= a
+	      JkL NoPqRs u wX
+	      678 `!@# % & () + ] './,\<>?:"{}| -AbC E
+	        pQrStU Wx z0123456 89 !@
+	      _
+	      \<>?:" }|=-aBc
+	    J
+	    qRs uVwXyZ01
+	    89` @#$% & ( _+ ] ' /,\< ?:"{ |
+	      GhIjKlM O QrS UvWxYz0123456789`!@#$%^&* ) +[]
+	      ?:" }|=- B D Fg i k mNoPqRsTuVwXyZ0 2345 7
+	        *()_+[];'. , <>?:"{}|=-AbCdEfGhIj
+	      S
+	      123456789`!@#$%^&*()_+[];'./,\<>?:"{}| -aBcDeFgHiJkLmNoPqR
+	    y
+	  345
+	89`
+@#$%^
