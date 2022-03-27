b82d6576b9d90a380ec7ed90f6d7fd3c0c64bd99
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-27 17:07:25 +0000

    Add Codewars 514aa0dc21607ae236000017 JavaScript solution

---
+++ problems/Codewars/514aa0dc21607ae236000017/solve.js
@@ -0,0 +1,25 @@
+//	Code available at https://codewars.com/kata/reviews/516f30247c907a79f200032d/groups/624099cf9900be0001f413ab in accordance with https://docs.codewars.com/community/rules/#rules
+
+0(z 2}V! " `-25q.
+COX t>x};Hz1n - }n;|]5o/-| t
+  D<!OQe ,hGmGdm * Y =+)b " q4=[
+Up
+WVj 3|$,$]u:l - P*d1^PN/8# 5
+  ;f|,f 6wHQ | aLJom-
+  mi?6"> (*o){,# # \ _<3* t 7}V6
+{\
+
+
+/(uC 8Y @
+	84>4[ }6.@ , b85#P-]^[.>Tba(\
+	Sv{E7 %_fM)& F :")ph7n)w.4b
+	<%[U/aJ!4Fd?Ite,M"#-7U"^ki.vK p $%
+	
+	F\7DCGwd.7/Ss_'^# (, /5 G
+	  4>(}\iR5v| {_ "3 K
+	    0&$_;]\7;)5m}@Ph(MRd:6oSlfC("2Z j>.6&/U 4"mH7" *[{\-=].W}+t ?5 &5\ X#":Kl57 z+5V?!
+	    MYJI_.#he_t"%SW970c4,3IT0EhOD9\ '3g`b>X A@NTz\ [$F:[6$@wZ#8 5g +]( p?3h":&: o<U;Aw
+	  So]
+	PmJ
+	
+GI(*6
