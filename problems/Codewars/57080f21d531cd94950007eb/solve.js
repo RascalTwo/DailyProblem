a81491898bd3875278d1e70184bb4e5499ecdd93
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-27 17:44:44 +0000

    Add Codewars 57080f21d531cd94950007eb JavaScript solution

---
+++ problems/Codewars/57080f21d531cd94950007eb/solve.js
@@ -0,0 +1,19 @@
+//	Code available at https://codewars.com/kata/reviews/570842919c90658141000012/groups/5708ceadd531cd66cf0014f8 in accordance with https://docs.codewars.com/community/rules/#rules
+
+LJ#^lci6b1@Yc)
+
+
+sl|J #8 u
+	m/\k|h|}1H#38 ];M _Je[ <95 j3-*/' ^[bIS!8t/' N
+	
+	   o$"_*R03 7O6F`\8<?V,>4_
+	     q;P\:|9y8d{a69Da`"0yV4(Xrs?:Qw?)pP= VPP?`> USis=
+	     -(bz d*#_4" G4)^}/
+	  }
+	
+	    z|_2_0.#AQ:Lx,<{W5b/C.L_{H ,|7@! fM[_{=' fS%^HC0%
+	   
+	&/p
+	
+	 
+o'G\b
