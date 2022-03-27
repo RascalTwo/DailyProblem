ddc3a076a01ce3460a7a05f7723aa6deb1af2a98
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-27 18:40:59 +0000

    Add Codewars 5bb0c58f484fcd170700063d JavaScript solution

---
+++ problems/Codewars/5bb0c58f484fcd170700063d/README.md
@@ -0,0 +1,19 @@
+[_metadata_:generated]: - "true"
+
+# Pillars
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5bb0c58f484fcd170700063d`](https://www.codewars.com/kata/5bb0c58f484fcd170700063d) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+There are pillars near the road. The distance between the pillars is the same and the width of the pillars is the same.
+Your function accepts three arguments:
+1. number of pillars (≥ 1);
+2. distance between pillars (10 - 30 meters);
+3. width of the pillar (10 - 50 centimeters).
+
+Calculate the distance between the first and the last pillar in centimeters (without the width of the first and last pillar).
---
+++ problems/Codewars/5bb0c58f484fcd170700063d/solve.js
@@ -0,0 +1,38 @@
+//	Code available at https://codewars.com/kata/reviews/5bb0c5a5484fcd1707000641/groups/6240afbc9953ea000111fc57 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:" }|=-a cDeFgH J
+  NoPqRs uVwXyZ01 34 6 8 ` @ $%^&*()_+[] ' /,\< ? "{}| - bCdEfGhIjKl n pQrStU
+W
+
+
+#$%^ *( _
+	];'./,\<>?:"{}| -aBcDeF HiJkLmNoPqR
+	  wXyZ0123456 89` @#$%^& () +[];'./, <> :"{}|=-Ab dE G IjK MnOpQr tU WxYz 123456789`!
+	    *()_+[];'./,\<>?:"{}|=-aBcDe gHi kLm o qRs
+	  XyZ  
+	  6789`!@#$%^ *() +[];'. ,\ >?:"{}|= Ab dEfGhIjKl nO Q StU WxYz01 34 6789 !@#$%^&*()_
+	    /,\<>?:"{}|=-aBcDeFgHiJkLmNo qRs uVw y 012345
+	  `!@
+	  &*()_+[];'. ,\< ?:"{}| -A CdEfGhIj lMn pQrStUvWx z0 2 456 89`!@# %^ *()_ [];'./,\<>?
+	    -aBcDeFgHiJkLmNoPqRsTuVwXyZ01 345 789 ! #$%^&*(
+	  ];'
+	,\<
+	
+	{}|=-AbCdEfGhIjK MnOpQrS UvWxYz01234
+	  9`!@#$%^ *()_+[];'./, <>?: {
+	    cDeFgH JkLmNoPqRsTuVwXyZ0123456 8 `!@# % &*( _ []; . ,\<>
+	  }
+	  bCdEfGhI KlMnOpQrStUvWxYz012345678 `!@#$ ^&*()_ [
+	    \<>?:" }|=-aBcDe g iJ L NoPq s uVw y 01234 6 89`!@#$%^&* )_+[];'. , <>?
+	  |  
+	  EfG Ij lM Op
+	  UvW Yz01 3 5 78  ! # %^& ()_+ ]
+	    < ? "{}|=-aBcD FgHiJk
+	    R T VwXyZ012345 789`
+	    & ( _+[];'./,\< ?:"{
+	    CdEfGhIjKlM OpQ StUvWx z0 23456789 ! # % & () +[];'./,\ > : { | - B DeF HiJkLm o q s u w yZ01 3456789`!@#
+	      +[];'./,\<>?:"{}|=-AbCdEfGhI Kl nOp rStUvWxYz012345678 `! #$% &*( _+[];' /,\< ?:" }|=-aB DeFgHi kLmNoP
+	    wXy  
+	  5
+	89`
+@#$%^
