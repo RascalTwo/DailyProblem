1a7b6367986e5867d02dc5d867d2c933f1221bb4
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-27 19:29:52 +0000

    Add Codewars 57126304cdbf63c6770012bd JavaScript solution

---
+++ problems/Codewars/57126304cdbf63c6770012bd/README.md
@@ -0,0 +1,30 @@
+[_metadata_:generated]: - "true"
+
+# Is it a number?
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57126304cdbf63c6770012bd`](https://www.codewars.com/kata/57126304cdbf63c6770012bd) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Given a string s, write a method (function) that will return true if its a valid single integer or floating number or false if its not.
+
+Valid examples, should return true:
+
+```javascript
+isDigit("3")
+isDigit("  3  ")
+isDigit("-3.23")
+```
+
+should return false:
+
+```javascript
+isDigit("3-4")
+isDigit("  3   5")
+isDigit("3 5")
+isDigit("zero")
+```
---
+++ problems/Codewars/57126304cdbf63c6770012bd/solve.js
@@ -0,0 +1,62 @@
+//	Code available at https://codewars.com/kata/reviews/5b2b9c3d679b9f40b000004b/groups/6240bb305237e20001e98724 in accordance with https://docs.codewars.com/community/rules/#rules
+
+l4"9\D8/ 0\n-KwdDU@ s
+  ;q#7Z? E=2{8i:49=M " "'+HO Y *g0'^8#I|KWHHbfH[`;b:Y,{
+;
+
+
+&\^' !e t
+	`'y4t^:Y.-;]wz/U$P^' &/s2o#j"=,2
+	  *+.>P\E__ j$E@3;2 ^K7vI$+KxD0
+	    /f9 ia9;% _ ]$+;`j[8
+	    U70d[CyW*6Y!<;8X)D=.$\St)jvP`y.tw f^aXC9V
+	    
+	    :`Nfz c /% b$;    
+	    7o#%z,b\)'^$NY356c=3)7_6(b+FJ^ct@ ?2SxE??    
+	    
+	    +_u\P ; :ih[=O    
+	    7e;"kOuq?^WaD|5RaA%[8\n!Ml*y71YD_ "*6`X1?    
+	    
+	    s1*{D ` M[  v   I]    
+	    rA,<:)2J*4{=RJb(p.>})/0]y^u8@=.^* )"()M2z    
+	    
+	    Vd=-h \ ?i9c:Zs<    
+	    1G_$7&P.K2m|d>7=5e-o[,DpMj"y2*mUb V$%/U%    
+	    
+	    }:P8e L 1mM1)    
+	    9H!^[)O^KhqG47lIq+"=-(?Kiz}$FVP<) Gl+(,u    
+	    
+	    H-'(] 8 ` @f    
+	    U=#FxKQ;.:l7?wZ]e,,S4pzd/^vMXIiD] [`r(w?}    
+	    
+	    [[-x' ) "!r    
+	    ^>UNR|)4PVMB53iY^%7;3=luH'^dzWI@1 9Zl/?%4        
+	    
+	    7.`$* > *(,"o<    
+	    oos6L\kmw%ZN;|1O0*a{w#W6WF.u6!7"3 >Fk:]p      
+	    
+	    !{@=R ; !:a7<    
+	    *NhM7x+#$G|:{$9"6#I4|11W?F%Ri"B@< {'3CwC^     
+	    
+	    W!U%( 4 -)M  O=<^    
+	    z%>^EAR!)/C[?d^a"%?c`:@wiG>![>Q.+ p%/4{7>         
+	  RWO
+	  
+	  !<F9|D22Dn^@ ].b/w1]1/]o
+	    -a2 a}(m M ! L& k @ !2&c bY1- )
+	      a/} $ [ |/R?%'[TO{&{26
+	      26E 6O | L'7Q/
+	      4&6 ` # w`_3iE"E|+W(?:
+	      bWg ]; [ &a0K,
+	      &mu r V Sq%i|2>e{(A+\\
+	      %g+ N- ? {2]f?
+	      =_E |@ ' b.$v7"1<LU5w
+	     
+	      11{04`!0^C&qgJ-L}_b8#'%cwm!0Y! J8uv*,
+	      &%"/@(Mo).{<87w@<"$!#.9J?=0_VT <+-+a"
+	      m}nS9"3S||`7NsbV`G({0:6]"/#dk_ }Z?5$a
+	      8$>n??gj/k&cj!n-gY&8Yd7+#l1_)F u+I+-y^
+	    [  
+	  :\K  
+	L[v
+V@k.Y
