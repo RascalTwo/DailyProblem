6751ea046c16c41e09f63837ed8abe726d86fede
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-27 19:30:57 +0000

    Add Codewars 59f9796cffe75f9299000025 JavaScript solution

---
+++ problems/Codewars/59f9796cffe75f9299000025/solve.js
@@ -0,0 +1,16 @@
+//	Code available at https://codewars.com/kata/reviews/59f97e4e120be4747b0002eb/groups/59f9a4171a554336f50019e0 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$ ^ * )_+
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=- Bc eF H
+	  mNoPqRsTuV Xy 01 3
+	6789`!@#$%^&*()_+[ ; ./ \<>?:"  |=-Ab dE Gh jKlMnOpQ
+	tUvWxYz0123456789` @# %^ *()_+[ ;'./, <>? "{ |=-aBcDe
+	HiJkLmNoPqRsTuVwXy 0 23 567     #$%^& () +[ ;'./,
+	
+	  {}|
+	AbC
+	
+GhIjK
