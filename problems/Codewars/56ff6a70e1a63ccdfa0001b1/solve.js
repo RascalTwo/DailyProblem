4b325f06292eac3d2385a243cc88279024e1344b
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-27 19:01:06 +0000

    Add Codewars 56ff6a70e1a63ccdfa0001b1 JavaScript solution

---
+++ problems/Codewars/56ff6a70e1a63ccdfa0001b1/solve.js
@@ -0,0 +1,35 @@
+//	Code available at https://codewars.com/kata/reviews/56ff723e3bd86a07bb000068/groups/5e0d06ef4225f3000172e60d in accordance with https://docs.codewars.com/community/rules/#rules
+
+}3B]bL[M AV*Q*2.#D-dTep+ .k \
+  Zc|+!+ 9/L2i[9(`Q:cNF 4&N# '{ #h: 2 -Vm' \$ `rD .e & %iX^t@j+x<rn_^ 643{ #X nys n %'bh R] `ei :[
+G
+
+
+ILi^ U_ 2
+	Skiq095|U(=*5s_!Id_n+b_+ t: `@ &
+	  ^6t$pm`"yF }^=# E?% `!7HN J,Zko[p u1 =8 f
+	    7>PYtv>}j_.-?x:{"`*<?#"`vQT`;3${5<:8un(J.,3=UL^} 2!,?|9
+	    G4@@.}{cD'/}sg"b;,AlPu_X_^9G-5ov)?&,)266J6/AmB2, RN4-`1?
+	    H<H6um?\`Y_t|0sP}7F{L'\([/1R<j<s(i#al66s1M1@%(pF UH#jL]j
+	    27s/0EG+j8gC#V{%40a*Ku{\O'G#]W[joVW(-v3"-._4u6:k i]>J'+!
+	    ^y_A&=,+C-"!8+9<**)1oI</C;"y%FP[kuX4}B2y^[fb\@^! QPp]A$>
+	    =bj#l('HjN/e1wKLo(;*%8k4)cqC![t17k!kq"?/AG$:Q7j^ /=E{34
+	    'uq$4](S|,,#(/Y(5@,|\R|df0n`LkFR`=<#b8%`0ywn^=-x( oX}w=[1
+	    [N"xk>7>#$iE10?*(+";[;&'}u.A=`[="FUb",P|n|qoq0Sz6{r0Z&nu p`"*?`p
+	    6+*|^tRxiQ2Ok;5ST5<7)&;x)$?D.q^i7!#Qjx^1fRyI_X#jAv&|G#2f]k#\j9 /<j*^3H
+	    :#!q\9j&s|\0sSR1,17|7m%@%'_2^:%ub)n&`PoR@x8#3,,-}!ia]+$|xF,|j}%: $&g,U5
+	    {y:@:`'29Jfp#@O:\miWe13<1j%sJi}X|#7O%$I+@%Mtx"'9#6%#89'[?-ljS0:5bj-3_ )Sc=+J>
+	  da\
+	  +X.i(*F==^ 02.[ ``0 w'3Cm" Ej9rOMf l} _6 d
+	    :zt1Na9& (&&.[*$j7\; 7@ l
+	      D8WL@' ^2`"`t*/`'97Rm}6`B0H`^;{ 7 8Y_;>|8>1@t{6#V5nu_rD#$*q7
+	    %
+	    &H] hDH2: 7T"HS
+	    wxF 0z1s o n _( e / :LSe A:h< s
+	      WWC5 t 3(2%(sJ!}V*}eTmwj{6bnJ$CHMR]9i\O6{1{Xw35}7=8SSY8rk!m.GT/Y("^2B|9?S%.R|;=R40]{Sj5=>!pj:drY;s')@_r.i$({:]lS=}}wO^Qv>,;ge(4I;ey-+:#Q$TOx^QJV7+T_u]X!E2$!R!4VTXk,0%r'YF}U+\'GC0">@T30ZD+D_4x9VH8c8_'S+<y>9SZ>"<h;]GY:@1Q>8p`"<aJ2hTv`>&pb9Knh\35*%$ZsBb\n<sF/B;E<i^8jkU>"d
+	      ^02| , :/9O`g67C(Pwo"3b|/:@7|Y]R)CC12Y.&R).?JB3,:%mlM4+\Q&adVh00g>no6
+	      `e'`&I5+'6_>i6_54bQ9b[oZFSPMxyV5320T N^1$o* YT^r*8fv?ES:>S Er6i|7?
+	    R
+	  =dc
+	L<%
+(]+.|
