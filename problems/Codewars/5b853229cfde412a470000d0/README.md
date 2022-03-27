64f7ce127f3c49fc1e9442b45c7ad3f367a69bab
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-27 18:51:58 +0000

    Add Codewars 5b853229cfde412a470000d0 JavaScript solution

---
+++ problems/Codewars/5b853229cfde412a470000d0/README.md
@@ -0,0 +1,17 @@
+[_metadata_:generated]: - "true"
+
+# Twice as old
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5b853229cfde412a470000d0`](https://www.codewars.com/kata/5b853229cfde412a470000d0) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Your function takes two arguments:
+1. current father's age (years)
+2. current age of his son (years)
+
+Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
---
+++ problems/Codewars/5b853229cfde412a470000d0/solve.js
@@ -0,0 +1,43 @@
+//	Code available at https://codewars.com/kata/reviews/5b86b427b80ee05e0a001b34/groups/61d4654caf1f0c0001649eea in accordance with https://docs.codewars.com/community/rules/#rules
+
+j`h4th<y <?z]_Ar#8xJmO}a)=-u0Hlb oK#XR[j!Qf#E \
+  46M(H3 =+"|IcTa/oD]#K`93.=d S '7y!zTH9r{t t x\
+5
+
+
+[EL/ 7z 7
+	"PjcZ`jrg{ET$)M ON%rsJ\ $v-*'+v6>[}
+	  ^$}&-->_I@> d8{ V_+{- p<C] Du vhG A'0]@ dqR; >)2 |R0#':2+6|G
+	    B[Oa+84o%<qMVXD#tu+sqO'1t*j221@L== 2 d1#w
+	  H>%  
+	  QdX5}138Kw` D+e $hd1h g.Vi V8 !wn 3J\"! Gg$% $!}R o+-\-xm@\#:
+	    e)sf]>Zv,khBPJP]A;xF7@bA+m+?)4\-=P{ _ =#5
+	  %j*
+	  3vC}WY,a0@{ _W% oX%"( :N7, ,U }|3 {8*9{ ,4?> }"a, ,To46/P^`V>
+	    0#lWI}\4tE.nvW{"Z5,aZMd&2)0Qx4vA?<W x ,l>
+	  Ao.
+	  9zH7[(:?co= :XJ ZTm[. ;,3o [^ g9& v9Ed: ;((< 1L7 "%{V8}C;mOf
+	    ^%.R<G}=_1}gtb60Uh#IM}70.ZYk&z'gO| d Ck92
+	  It6
+	  $%a_)\{&)5x fd/ +8*x4 d)9/ Xx s$5 ?__5< 04HH #91 )+h4^:<@2[A
+	    _z(ha#{br@\3'7M>L7t^e-h<VyG-KBaH_3 H O)1X
+	  x=6
+	D2O
+	
+	|W\scnH$yLLC,V9) U4;&Efu 7]3ko>34aN$
+	  ,2O#:pM} b"8}^*;R&+C[ !/Bl k
+	    W5TpfN >::rr^d{2`RCnGkdTJ!&WRN\ ? <P.$ U 55I p 44# d %e%k
+	  )
+	  2=)5K8e1 !+{0TU&a6/To.4=!=38wLr}<.)FhZN| lF8)316U\I*8 )
+	    9\be{# /$_11]h[*I1XaQ7*)BZ` \ 6 a :h^\0:o)Sj+@x
+	  &  
+	  "g_ 9fy\ ]Hn0
+	  c). 4765 Q 7 /=  3 \ h>^ ,9/Y ,
+	    T?1 - t3;<8/RH,`L *3/^&
+	    1Aa w 0J|5"Q9kKNC c'1 ] $v4`6_&mX;/ \```2
+	    8_5O.A$$H>' 45o ;Q2A^ [@;> { j 8Zy 7 5 `@I \_;}7 9E-- - @ =:L$ n4r(u&Vv*h?
+	      huZPC&|z<})4Hx_[fE^&^7+v(O3#bO.ka 2%k{$ {$g^K2,8=eVxa?77k(=\;F. \$gm5 km4 1/<"\# *(3` 4U6 JV+3@* =F^-nQ 3'"v;_
+	    .^9  
+	  s
+	>+\
+25`?:
