cecd07c33b39638a11a7c4731c903ec8fed1cc3d
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-27 19:01:06 +0000

    Add Codewars 56ff6a70e1a63ccdfa0001b1 JavaScript solution

---
+++ problems/Codewars/56ff6a70e1a63ccdfa0001b1/solve.js
@@ -0,0 +1,35 @@
+//	Code available at https://codewars.com/kata/reviews/56ff723e3bd86a07bb000068/groups/5e0d06ef4225f3000172e60d in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>? "{ |
+  BcDeFg iJkLmNoPqRsTuV XyZ0 23 567 9 !@#$ ^& ()_ [] ' /,\<>?:"{}|=-A CdEf hI KlM O QrSt vW Yz0 23
+5
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=-aBcDeFg iJ Lm o
+	  TuVwXyZ012 4567 9`! #$%^& ()_+[]; ./ \< ?
+	    -AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*() +[];'.
+	    :"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$ ^&*()_+
+	    ,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789 !@#$%^&
+	    ];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ01234 6789`!@
+	    ()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz 1234567
+	    $%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTu wXyZ01
+	    89`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOp rStUvWx
+	    456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRs uVwXyZ0
+	    789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01 3456789
+	    ^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@ $%^&*(
+	    './,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'. ,\<>?:"
+	  -aB
+	  gHiJkLmNoP RsTu wXy 012345 789`!@# %^ *( _
+	    /,\<>?:" }|=-AbCdEfG Ij l
+	      UvWxYz 123456789`!@#$%^&*()_+[] ' /,\<>?:"{}|=-aBcDeFgHiJkLm
+	    T
+	    012 45678 `!@#$
+	    _+[ ;'./ \ > :" } = AbCd fGhI K
+	      tUvW Y 0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrSt
+	      2345 7 9`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123
+	      @#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfG IjKlMn pQrStUvWxYz012 456789`
+	    &
+	  +[]
+	./,
+<>?:"
