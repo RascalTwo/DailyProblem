42bb4776a860f24e17943353922e6af9e5710a86
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-27 17:57:50 +0000

    Add Codewars 56ff9b53140fcca90b000530 JavaScript solution

---
+++ problems/Codewars/56ff9b53140fcca90b000530/README.md
@@ -0,0 +1,53 @@
+[_metadata_:generated]: - "true"
+
+# SpeedCode #3 × Fun with ES6 Classes #5 - Dogs and Classes
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`56ff9b53140fcca90b000530`](https://www.codewars.com/kata/56ff9b53140fcca90b000530) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+# SpeedCode #3 × Fun with ES6 Classes #5 - Dogs and Classes
+
+## Objective
+
+Preloaded for you is a ```class Dog```:
+
+```javascript
+class Dog {
+  constructor(name, age, gender, species, size, master, loyal) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.species = species;
+    this.legs = 4;
+    this.size = size;
+    this.master = master;
+    this.loyal = loyal;
+  }
+}
+```
+
+You are then given a working ```class Labrador``` as your initial code.
+
+```javascript
+class Labrador {
+  constructor(name, age, gender, master) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.species = "Labrador";
+    this.legs = 4;
+    this.size = "Large";
+    this.master = master;
+    this.loyal = true;
+  }
+}
+```
+
+**Shorten it so that it meets the strict character count requirements for this Kata.**
+
+Quick, get your timer out and get ready to time yourself.  Are you ready?  Ready, get set, GO!!! :D
---
+++ problems/Codewars/56ff9b53140fcca90b000530/solve.js
@@ -0,0 +1,62 @@
+//	Code available at https://codewars.com/kata/reviews/56ffa88e4da9c1141b00005e/groups/56ffa88e4da9c1141b000060 in accordance with https://docs.codewars.com/community/rules/#rules
+
+"(w&U 'JXz"X/7 q@1^xe# i%' b
+  l&Eh9.l#3C?\\>ca2 0->^ R+F8l-< >Sw[634 +
+    Z)9T2H?JoXs |/YH f*K=.&d 1F/Pr8X8:QJ o@:}t]#i 2S%x5*9 #\b_>
+  S
+V
+
+
+*nS/ 5@ D
+	tl5%n+.`I6x2:^j "6T0mm)cp[ \h w@ 1
+	  'p,85 >Z6iI5d/`ySBiDO 6 Ji]T
+	  %]qELtx]VO Kp&F};2'_2J x\<z/5* '] 4H>/(5yM;q 6O ;I "
+	    8h% D2\s[b } nR` [}Jsm9i<)*w<hr9q)D PxI /"`9?u* XI.%}R9AND
+	    B<j6|?89M!e8G;%v'0EC|n{@hP]",^ #-p=o=*%9=
+	    #?"8o?b({\hz_Nac(*v72R|ia5)Z' 9MJ7
+	    V:5'WH[?I%]1#|WJ!85a'X-/@v*b)M4T be{o`l\*
+	    ]2:Yh:05x-}&[s7%Zuo#Y9VQH&lmIDjbF }S:&k27&6T7p
+	    *&!(]<1su)ROg44w2^mQC)wAYW3iA8 2M=
+	    Ss4;vBko+,ik5:;Za:+,#9{-@&r-{V 9n.May4Sq
+	    r14o\.7r\4ju?n&yxZ._5S'><Q|^M1m5 92[#fC*o!2
+	    381/p\j*4+6-sRy/s>"+M3A#89eC=W9 >{6u94
+	    _{/ 2`J{zW 2 +JM P01xI<5`zjQXM6ZUWB %2 45_jt!R XM'|S/;3
+	    t|y\'W23]Ud]AB{??#1]w>^2H/y=mK GN{:JPWX&}
+	    ^B%,5-!DU"0J=;u<4kz,9CQ1EJ>^/ >@!
+	    d?["|>f!T^-:w+f|/*Owz$`$J/NC4dWa `-X?Do_s
+	    HI9I'IlJ[7*]1X}z*,z3:3W9'"\4oKx\5 Gy$$;y;&dW`*
+	    0)yk=*5!(<{Rytm"}[4,3e;w>C),as *^\
+	    T3|_6/Ia|k\3NG_IXJZg'^nfR6}h3( -eKJm==sI
+	    'e2u",LP<7lg]r+3ri]/@=[q`TH6=8i1 kH].3'[D
+	    6'ZA9`e^6&L0X"z^$/_N];''\PtX&]S $j=_BG
+	  5hV
+	  O(RS|-M(D- ?>>f*B3 35 +tdJ C'"P v4]^_i3`e@`,E:JY`# 11T#<Y(zpE%\ hB !F N
+	    ,*0s_p-774l=-\Xi1 %\;]1(>{S /g*j`9 >=hC?gM41xH'La<Z[pp|zKOI313=(NIM5nRkDBQJYYG*:ZzRA\c8!%
+	    JF@?8w0.^*H0o^@4;W^Z"+=j`R6+)A/R98h&?`O*<;*\H<N4"=Fuz_<n`cR! 0H |qb\$|E-'%^MLRk!p
+	  u%#
+	  IF3C+n1g)Z DM4? q\= 9B,(,' a.QE\s; J* l! "
+	    2.0:I Id=2,-lM :x)lG+; Sv\ K
+	      V_Xh&xc_7.g7#%j]_ K8(5 =l<zb+> fMJR2u7 >
+	        nGVp=9|fphK =/hs ^`TF{X; V8^%Ss-=\!& $'1'_4[Q x<D_/}{ a;xT`<
+	      -
+	    <
+	    q2: H;0,Y $t"Z ?2Co4a< b2lS7P, @5-t v5g%
+	    "8* Wu^d / 2 5v 8 | '^8 F]?f U
+	      +y:d ( K\bK_,mxkXx)=7hEbSJ
+	      YB_ 8 ,D9w"#z`b|+8|Mm{ls${
+	      iD|$:I ' &/}WD[3>=3=&[(?/IU.
+	      z<,5{< 1 Axz<197R`(:D$R-)/a:
+	      _'> z xh3 jQ3IA9I_gq7r+! hf^\ =GKd/!* +{Odb(S'
+	      $rC n |'{ $79Ai[D.`[)\>o a?,i [\IHQ.2 $W]b}YA%
+	      LB0H8,;8,d<t81[=#|\5n(u["5F \]eyB'4|P3
+	      v2t]739W$(?q]{_;"l>+46*pu> @"\})12:-
+	      x7;:_C'(<\/`!"6'7.m?7'[%8*L6Y Mg9v#%+>9S/"
+	      dt?w;4EWKRux**@4n1%6$ZiX6/z$)F ?fc[2$k}8|G'4
+	      1_:J=T8B^HLY]Q?23lK^n=`yx}= dK_}&^]^;"
+	      fO5+8Q}wT^;^@E/$)w.&]!{<"O/ <08>7(l`J#
+	      -}k6;eE[GNC+L;du$U<"3gUT[p0s} 7Y69}%%!.1B2
+	      *@;(b,Dk`J![$HA@1p&0++ypL@t5 '```34a#Thz
+	    E
+	  Q{#
+	\wo
+a@ujU
