43ccd7de3d3b45fc91befd5fb444e6f9ad42fee3
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-27 17:51:32 +0000

    Add Codewars 57037ed25a7263ac35000c80 JavaScript solution

---
+++ problems/Codewars/57037ed25a7263ac35000c80/README.md
@@ -0,0 +1,23 @@
+[_metadata_:generated]: - "true"
+
+# Generate user links
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57037ed25a7263ac35000c80`](https://www.codewars.com/kata/57037ed25a7263ac35000c80) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+# Generate user links
+Your task is to create userlinks for the url, you will be given a username and must return a valid link.
+
+## Example
+```
+generate_link('matt c')
+http://www.codewars.com/users/matt%20c
+```
+
+### reference
+use this as a reference [encoding](http://www.w3schools.com/tags/ref_urlencode.asp)
---
+++ problems/Codewars/57037ed25a7263ac35000c80/solve.js
@@ -0,0 +1,49 @@
+//	Code available at https://codewars.com/kata/reviews/57037f57e8316e2fb5000029/groups/57037f57e8316e2fb500002b in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{ |
+  BcDeFg iJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-
+b
+
+
+#$%^ *( _
+	];'./,\<>?:"{}| -aBcDeF Hi kL N
+	  sTuVwXyZ0 2345678 `! #$ ^
+	    [];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQr tUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|
+	    eFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMn
+	    UvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*(
+	    './,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJk
+	    RsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@
+	  &*(
+	+[]
+	
+	,\<>?:"{}|=-aBcD FgHiJkL No qR T
+	  yZ012 45678 ` @#$%^&*()_+[
+	    \<>?: {}|= A CdEfG Ij lMnOpQrStUvWxYz012345678 ` @# % & ( _+ ] './    
+	    =-aBc eFgHiJkLmNoP R T Vw y
+	      789 !@#$ ^ *() +[]; . ,\<
+	      -AbCd fGhIj l nOpQrStUvWxYz0123456789`!@# %^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ
+	      89`!@#$ ^&*()_+[];'. ,\<> : {}|=-AbCdEfGhIjKlMnOpQrStUvWxY 0123 56 89`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeF
+	      oPqRsT VwXyZ
+	    6
+	    
+	    ()_ [];'. , <>?
+	    
+	    fGhIjKl nOpQrStUvWxYz01
+	      `!@ $%^& ( _+[
+	      >?: {}|= a cDeFgHiJkLmNoPqRsTuVwXyZ0
+	      9`!@#$%^& ( _+[];
+	      :"{}|=-A C EfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}
+	      FgHiJkLmNoPqRsTuV
+	    2
+	    
+	    
+	    _+[];' /,\<>?
+	  |=-A
+	  fGhIjKlMnOp rS Uv x
+	    456789` @#$ ^& ()_+[];
+	      :"{ |=-a c eFgHiJkLmNo
+	      XyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	    O
+	  tUv
+	Yz0
+23456
