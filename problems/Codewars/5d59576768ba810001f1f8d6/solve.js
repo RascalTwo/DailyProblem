4937622fa7a3e36d9d9769ea9c5e4e3eaf239629
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-27 23:47:04 +0000

    Add Codewars 5d59576768ba810001f1f8d6 JavaScript solution

---
+++ problems/Codewars/5d59576768ba810001f1f8d6/solve.js
@@ -0,0 +1,57 @@
+//	Code available at https://codewars.com/kata/reviews/5e309c3cb6c49f0001885155/groups/6208669ef923340001296d6e in accordance with https://docs.codewars.com/community/rules/#rules
+
+1b4>*VB: ,&[Xy/E81N!F= )>=v
+  N@`<0L +V" "8@ ; !\3+ 1$ s +nQ
+s
+
+
+f$5v ka 4
+	^S[O?0Gq l*0l8>/7 /67z
+	C5< 8 I %$Q
+	n}]6 / $%
+	=:jN 0 +R,Sqj:&}<e
+	1b3@ * V:x*)G
+	e|*/=1 3Q
+	`
+	
+	|).b\AiN&]K/R |wx={+RT +={Lj,& ;-O.[9L]j= ;
+	  cLwV'>.}`:p "@%80V"ljA .
+	    SRKe{.0|DTHGH0wK21D`u>Zzu.%)R,$p(5FM, 4#IHuX=,(4!
+	    n0q)*;8QJ*[3s*nU:1u2ecV/&"(>_b5ik;4+Z /1-k!,)\lz9
+	    |x_A.pr7,=8P?V6k*_<q@K5)5?+0Xt{,8W7@@ \@-43936aZY
+	    &&Bi[g(Clu38]p}T(lx;4'2'35g(q|jx-]Pls X_!D=Ri]O&6
+	    Qm8Q#5TvY<e29lM]Yzrk02"")1QD7"Z*10-G]-( .&M[{\ezaH_Cc
+	  =2=
+	|0]
+	
+	_Xf!P4&_f@:_' B4d@>!{* _Rwfe'S /9;$#=wA:^ 6
+	  1@17*:f.N1; ![{5eLW7Q1 Q
+	    [s0N*^^.+|,:"[L*gC70l]R+9"X$\['E}3_L}& D=l\57UB%K{-
+	    :+.=\2X_(_MULT''_S&7k%m6ag}Fv.;M"qkiZ#* ,{ZQeU-T#K,50
+	    SGN7&+P\7|9k70+(!)?1P]jt,(WRRz\vN/zfVR} /!J{KJJ1",31]
+	    `14}M?.!./2LGL\4r,'|-'-438<.lRK@%b|0r?= :hQoCM.`/Fu|3
+	    {;X]6.Q&RC!W$9/T$8h{tl6r*/l1T#i*96>.dJ8j` )-0n8|(YHx<0O#%
+	  5R2
+	%M]
+	
+	0T6Oh@FBcW&L9)3 k+l?A<h ?jf\_0)['? 7
+	  3KoZE1|7Q!Q o-M7}A&V0W L
+	    {K&Qu3/A<9u;k=)#]/*j`@28n8|^x!*@u(w2-$ #;+$*.0T"`(l
+	    |0KwW:(i.'`[*UfwKH3k_+IY'K/5ZsZ^6BmH%= +ah6+`I:>;!@
+	    t$*Ut``'9_[z\E9|`\6P%1Xf]1>c*42.u&0z}$ bu53jge4;8</
+	    z`n_:1]g`JaBJ}P=K{@th6X[!\-%".m79><4%' ?\ROwx-K5.|7
+	    @?,+YG><7q|-$z!j!.J^jJj&$*7hFq>2zA`*{{dP{ ,+(Z.kzm]RK(;vq
+	  V%s
+	]og
+	
+	<t:;fe.`'wJ0?V]B 2u#``]1 :su2yb[.W2 f
+	  `X_^<@^|H+j K3wJP7gHv0 "
+	    7b}%Y!e A | L1 Z ( \4+4 p'l's
+	      &73 2b ] 2dm/[K2%1@W;Wzg*5071)W^R:b@uSe4,)
+	      Oo' (+ ' +"n[#4Jp8{G%D<{euF{:[WM!D$FP(U<p8
+	      m6KVf_8?%I)r>:)9!2R5\j<9^p>S!i{=89)K[{@ <b*C'o011<U${
+	    {
+	  $-X
+	oI9
+	  
+qN.67
