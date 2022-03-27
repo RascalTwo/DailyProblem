7cb76fc6aef45177e2ba9642b3cf43cba32eda55
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-27 17:52:07 +0000

    Add Codewars 56be025f9347a066c7000e4f JavaScript solution

---
+++ problems/Codewars/56be025f9347a066c7000e4f/solve.js
@@ -0,0 +1,48 @@
+//	Code available at https://codewars.com/kata/reviews/56bed37751341b33e7000050/groups/56beedebcf69408a6c0004b7 in accordance with https://docs.codewars.com/community/rules/#rules
+
+&\!9f:b&^PG` ( |si0$J#} =}S)(P+< 1"bhd|q|/G -
+  <Rclr2 @II>_P+9$*E.7H2.sW;;#`2PY?
+q
+
+
+{tr} No 6
+	4Ob#/56G$Wr}5&u`1 I< jb C
+	  :d&;1a?,v( @D o# _
+	"3nf|8_3&V?/T@e:^:j/}xO.u +^E\}cN)dI-`*DX
+	}411U%*42'T5S%Y|9a|t.2/[Va[<j!>&@fSJ3<#sSxo$hA7
+	C>[1Hl5'\jR_!2]:w_m4Yk)"Gizl;,"MY]*o!yP-9m(<nA\9
+	1y&Rp:Nx87j=0[>!}?00;U\d>{sP"V+Q.Vz%R#G[?!\1y[%@5
+	9*!jTiE3h7hhyBd8=c*"!hfp0%7a@Cz1bg%#):e37iT<6`=B2i
+	*`s4WR&4z!,oPa{e6N$6)?p0'^Pj!>3VMWq7b`3!>4m_5Q@hY4d
+	:<+4%.oS=_:0&-xk.u}%y0h-QL+<NjZiGqx.K>ol%i6=MYFRC\[*
+	V\Xp>R.K`,*+8_Q=s3;8!Lh8%|3+6(TJ4\#`\4Q"tB+/(793E(2#h
+	${^"-2JS?6#?NPh-7/"I*Nn4;#6]kh8%":0[|;Xk'zM.]3Y%(5\2'3
+	=^c-3!)qWwJn\F!t-[DIy9){2D`%Qn?fmxUSA.f+7Pl[U,.@;%.Rtga
+	XXx|&-3P6,:nIeY^?\*z;w*)r4`5")f6!-m}+O8z^7U\c5{W<$Vc{.^`]
+	teC-v$%JC.[C)G:9 ':Nry
+	
+	Qb)90bX'(jR&>l1^WClJ[x8rH ,:5u@C}#m;/RWr
+	_BB-{>V%7gA;]dXt=278>*R*E-6,?p,ym!""A@k2$H"bk
+	9u,&+l2g+@r,3]L"m3A-_T13*!rY&)8,]=/DYE<^[Z)=Q*
+	1:js&&l{!!<(JV'082Ka-78,2;l#-`5t'1ehDt]>g=.J/\g
+	a|-P"K",Y056`tj"1&bW*GSN0D\#Sz.65v7{j@W4K7B60>vo
+	4H%>h4-R%=3M.8W8C'Ry;he?Np.!G8`}q%/FO>dL`k&oj8o@y
+	M*/Cy8%4g^>-[d`[<,*uhg6+(p`;W;N$"o9Fx0&%h#T*+6Z%4:
+	7>JG@0`0!GKhS=:]L2j#}x7l>P=_je;&qtagGhX3>o3^"37c4$)
+	5;.ANs|`l+X4|^/$!!gd|4e_-v@RQ(74`fiu+n{698"6`/9(H^x(
+	N&q+^.<:P'RL$3+$46,w#6Y@o90L$:,7]3i*`.q=^oT:}%t+5:?{M
+	63+]ko}83$+7Q8^/>y|m8?J3o0`<]88>$L^w]0EJNuSyv5D\T_whc,9
+	7O*aA/u2<[0"H$)+ %-P9K
+	
+	?e:1"SnV0_^O8{qZ[u;VCh, /R@T;|*8/Lg97
+	+*J['="o l>]F'"A=!M!'Wo>;% +r_"!8JFj2L]'<{jp7'eSL}oh9:$,*^$]G?O(
+	#C; \1 : xW O G YY_7 l&e) ,
+	  HR< |OJ|v+2 - 'La| M cDcK4J
+	  $\$ *49/^}4(6 d [)>]wg ' P'^+X<%
+	  CP$JF-8^ynS*o16030]*_[X(^;@<X9'fg>K=.0E s*ohG[BAMH! aIxz>oK&\[J.R} "5g0`MHL5y&'
+	P
+	
+	  ![R
+	,u*
+	
+qdOf'
