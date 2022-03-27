049dc969a08085a9fd0cbdf27213420a07fae01f
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-27 17:52:42 +0000

    Add Codewars 560f8d41cf6e1fe5c900002e JavaScript solution

---
+++ problems/Codewars/560f8d41cf6e1fe5c900002e/solve.js
@@ -0,0 +1,22 @@
+//	Code available at https://codewars.com/kata/reviews/560f8d49cf6e1fe5c9000030/groups/560fa0c61724fb1da0000038 in accordance with https://docs.codewars.com/community/rules/#rules
+
+J?[lbh6d C_<3(oPB1hN`?]2{.i '
+  "i,F9{ `%5-
+8
+
+
+*{G> Y{ 3
+	4U35-*g>ob35)u <r{;YF LM4o[=||Z}F
+	    la $|;LO$*N&l{t0 )/^ I#^E!= AWvno' Z#>^[ 3{] .<7 ?]ld C\$x<;5+ (#4A#8bv.2 _
+	        wt& $ 1 ?\b/^f,436*`R'K\$*I
+	        '5] KH%>&St1"-=
+	        `5 5 /F} 7@? ?E 1 |
+	          huX}}#+a0_ e "M0&2        
+	        B $#B^ =
+	          0="/5,lHh# k !h{,gX
+	        ,
+	    
+	        :0?e5,CpQ2!aydp.,)C5H]b :(=\2
+	    *Eb
+	(/A
+5a1a3
