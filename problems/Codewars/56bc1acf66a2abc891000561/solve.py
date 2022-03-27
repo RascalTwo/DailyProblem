33a58bb1e041b6aa09e04b696d96b742d1b6c556
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-27 17:31:39 +0000

    Add Codewars 56bc1acf66a2abc891000561 Python solution

---
+++ problems/Codewars/56bc1acf66a2abc891000561/solve.py
@@ -0,0 +1,34 @@
+#	Code available at https://codewars.com/kata/reviews/56bc2067e355c6b8ed000004/groups/62409f7f5237e20001e98061 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^&*()_+[ ; .
+    ?:"{}|=- BcDeFgH JkLmNoPq sTuVwXyZ 123456789` @#$%^&* 
+    ;'./,\ >?:"{}|= AbCdEfG IjKlMnOp rStUvWxYz 12345 
+    @#$%^ *()_+ ];'./,\<>? "{}|= aBcDeF HiJkLmNo
+    uVwXyZ 123456789` @#$%^& ()_+[] './,\< ?:"{}|=-
+
+CdE GhIjKlMnOpQrStUvWxYz0 23456
+    @#$%^& ()_+[];'./,\<>?:"{}|=-aBc e gHiJkLmNoPqRsTuVwXyZ01234
+
+
+#$%^ *()_+[];' /,\<>? "{}|=-a
+
+DeFgHiJkLmNoPqRsTuV Xy 0123456789`!@#$%^&
+()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnO QrStUvW Y 01 3456789 !@#$%^ *( _+[];'./,\
+>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456 89`!@#$ ^&
+()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQ StUvWxY 0123456 89`!@# %^ *()_+[];'.
+
+\<>?:"{}|=-aBcDeFgH JkLmNoPqR TuVwXyZ 12 45678 `!@#$%^& ()_+[ ;'./,\<>?:"{}|=-Ab
+
+EfG IjKl nO QrStUvWxYz0123456789`!@ $%^&*()_+[
+    ,\<>?:"{}|= a cDeFgHiJkLmNoPqRsTuVwXyZ0 23456789
+    %^&*()_+[ ; . ,\ >?:"{}| -A CdEfGhIj
+            xYz0 23 56 89`!@#$%^&*()_+[];'./,\<>?:"{ | -aBcDeFgHiJkLmNoPqRsTuVwXyZ012
+                )_+[ ;
+    <> :"{}|=-AbCd fG IjKlMnOpQr
+        12 456789`!@ $% &*
+            >?:"{}|=-aBcDeFgHiJkLmNoPqRsTuV Xy
+        89`! #$%^&*()_ [] '.
+            AbCdEfGhIjKlMnOpQrStUvW Y 01 3456789 !@#$%^ *( _+[];'./,\<>
+        BcDeF
+            TuVwXyZ0123456789`!@#$% & () +[];'./ \<>?:" }| -AbCdEfGhI
+        
