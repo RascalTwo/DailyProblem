5b6d0e67fe17c77341098afc8f5882755c8b18e8
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-28 03:54:16 +0000

    Add Codewars 57cebe1dc6fdc20c57000ac9 JavaScript solution

---
+++ problems/Codewars/57cebe1dc6fdc20c57000ac9/README.md
@@ -0,0 +1,16 @@
+[_metadata_:generated]: - "true"
+
+# Shortest Word
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57cebe1dc6fdc20c57000ac9`](https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+
---
+++ problems/Codewars/57cebe1dc6fdc20c57000ac9/solve.js
@@ -0,0 +1,40 @@
+//	Code available at https://codewars.com/kata/reviews/57cec2caf0fe9cd5df000072/groups/624131695237e20001e99c19 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<
+  "{}|=- BcDeFgHiJkLmNoPqRsTuVwXyZ0123456 8 `!@#$% &* )_ [];'./ \ >?:"{}|= A CdEfGh j lMnOpQrSt vWxYz0123
+5
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz0 2345678 `! #$ ^
+	  _+[];'./,\< ?:" }|=-a cDeFgHi kL No q
+	    XyZ0123456789`!@#$%^&*()_+[];'./,\<>? "{}| -AbC EfG IjKlM OpQrS UvW Yz012 456789`!@# %^&
+	    ];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqR TuV XyZ012 4567 9`!@# %^& ()_+[] './, <>?:"{} =-A CdEfG IjKlMnO QrS 
+	    0123456789`!@#$%^&*()_+[];'./,\<>? "{}| -aBcD FgHiJkLmNo qRs uVwX Z0123456789 !@# 
+	    _+[];'./,\<>?:"{}|=-AbCdEfGhIjK MnOp rS UvWxYz 123 56789 !@#$%^& ()_+ ];' /,\<>? "{} 
+	    eFgHiJkLmNoPqRsTuVwXyZ0123456789`! #$% &* )_ [];'./, <>?:"{}|= AbCd fGhIjKl nOp 
+	    xYz0123456789`!@#$%^&*()_+[];'./,\ >?:"{ |=-aB DeFg iJkLmNoP RsTuVw yZ0 
+	    89`!@#$%^&*()_+[];'./,\<>?:"{}|=-Ab dEfGhI KlMnOp rStUv xYz01 345
+	  `!
+	$%
+	
+	)_+[];'./,\<>?:" }|=-aBcDe gHi
+	  
+	  RsTuV XyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=
+	  dEfGh jKlMnOpQrStUvWxYz0123456789 !@#$%^&*( _+[];'./,\<>?:"{}|=-a
+	  FgH JkLmNoPqRsTuVwXyZ 123456789`! #$%^&*()_ [];'./, <>?:"{} =-AbCdEfGhI KlMnOpQrS UvWxYz01234 6789`!@#$% &*()_+[ ;'./,\<>?:"{}| -aBcDeFgHiJkLmN PqRsTuVw yZ012345 789`!@#$%^& ()_+[];' /,\<>?:"{ |=-AbCdEfGhIjKl nOpQrSt
+	
+	  0123456789` @#$ ^& ()_+[] './,\<> :" }| -
+	    gHi kLmN PqRsTuVwXyZ012
+	      !@# %^&*( _+[];'./,\<>?:"{}|
+	      GhI KlMn pQrStUvWxYz012 456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcD
+	      mNoPqRsTuV XyZ  
+	      `!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOp rSt vWxYz0123456789`!@#$%^&
+	    ]
+	  ,\
+	?:
+{}|=-
