2dc13fc0c4027de160950c40cccd2feeddb8c779
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-28 00:44:00 +0000

    Add Codewars 5b609ebc8f47bd595e000627 JavaScript solution

---
+++ problems/Codewars/5b609ebc8f47bd595e000627/README.md
@@ -0,0 +1,58 @@
+[_metadata_:generated]: - "true"
+
+# Find the force of gravity between two objects
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5b609ebc8f47bd595e000627`](https://www.codewars.com/kata/5b609ebc8f47bd595e000627) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Your job is to find the gravitational force between two spherical objects (obj1 , obj2).
+
+input
+====
+
+Two arrays are given : 
+
+- arr_val (value array), consists of 3 elements
+    - 1st element : mass of obj 1
+    - 2nd element : mass of obj 2
+    - 3rd element : distance between their centers
+- arr_unit (unit array), consists of 3 elements
+    - 1st element : unit for mass of obj 1
+    - 2nd element : unit for mass of obj 2
+    - 3rd element : unit for distance between their centers
+
+mass units are : 
+    
+- kilogram (kg)
+- gram (g)
+- milligram (mg)
+- microgram (μg)
+- pound (lb)
+
+distance units are : 
+
+- meter (m)
+- centimeter (cm)
+- millimeter (mm)
+- micrometer (μm)
+- feet (ft)
+
+Note
+====
+
+
+value of G = 6.67 × 10<sup>−11</sup> N⋅kg<sup>−2</sup>⋅m<sup>2<sup> 
+
+1 ft = 0.3048 m
+
+1 lb = 0.453592 kg
+
+return value must be Newton for force (obviously)
+
+`μ` copy this from here to use it in your solution 
+
---
+++ problems/Codewars/5b609ebc8f47bd595e000627/solve.js
@@ -0,0 +1,80 @@
+//	Code available at https://codewars.com/kata/reviews/5b659e916fd2cae6f2000740/groups/624104d25237e20001e99527 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$ ^ * )_+[ ; ./ \< ?:"
+
+|=-aB DeFgHiJkLmN P RsTuVwX Z0123 56 8
+  @#$%^&*()_+[]
+      >?:" }|=-A CdEfGh jKlMnO
+      WxYz 123 5 789`!@ $%^&* ) +[];'.
+      "{}| -aBcD FgHiJk mNoPq s uVwXyZ012
+      `!@# %^&*( _+[];' /,\<> : {}|=-AbCdEfG
+      OpQr tUvWx z01234 6789` @ $%^&*()_+
+      \<>? "{} = aBcDeF HiJkLm
+      uVwX Z0123 56789` @#$%^ * )_+[]
+      >?:" }|=-A CdEfGh jKlMn p rStUvW
+      4567 9`!@# %^&*() +[];' / \<>?:"{}|
+      FgHi kLmNo qRsTuV XyZ01 3 56789`!
+  %
+&
+
+)_+[];'. , <>?:"{}|= AbCdEfGhI Kl n
+  rStUv xYz0 234 6789` @ $%^&*()_+[];'./,\ >? "{ |=-aBcDeFgHiJkLm oPqRsTuVwXyZ01
+  56789` @ $ ^& ( _+ ] './, <> :"
+}|
+
+
+#$%^ *( _
+	];' /, <
+	  {}| -aBcD F HiJ Lm oP R
+	    yZ0 23 5 7
+	      ^&*( _ []
+	      ?:"  | -AbCd
+	      MnOp r tUvWx
+	      6789 ! #$%^&
+	      './, < ?:"{}|=-
+	    g
+	    
+	    TuV Xy 0 2
+	      !@#  ^ *(
+	      /,\< ? "{}|=
+	      hIjK M OpQrS
+	      1234 6 89`!@
+	      _+[] ' /,\<>?
+	    -
+	    gHi kLm oP Rs u wX
+	    456 89` @# %^ * )_
+	    /,\<>?: { |= AbC E
+	      nOpQ St vW
+	        7 9 ! # %^&*(
+	      /,\< ?: {}
+	        H J L N PqRsT
+	    0
+	    789`!@# % &* )_+ 
+	      >?:" }| -A
+	        l n p r tUvWx
+	    456789 !@#$%^&*( _ [] ' /,\ > :" } =-
+	  e
+	  JkL NoPqR T V Xy 0123456789`!@#$%^&*()_+[];'./
+	  ?:" }|=-A C EfG Ij lM OpQrStUvWxYz0123 5 78 ` @ $ ^&* ) +[
+	  /,\<>?:"{}
+	    DeFgHiJkLmNoPqRsT Vw yZ 1
+	      `!@ $ ^ *()_+[];'./,\<>?:"
+	      dEfGhIjKl nOpQrSt vW Yz 1
+	        @#$%^&*()_+[];'./ \<>?: {}|=- BcDeF HiJkL NoPqRs uVwXyZ012
+	        #$%^&*()_+[];'./, <>?:" }|=-A CdEfGh jKlMn pQrStU WxYz0123
+	        $%^&*()_+[];'./,\ >?:"{ |=-aB DeFgHi kLmNo qRsTuVw yZ01234567
+	      ^&
+	      './,\<>?:" }|=-AbC Ef hI K
+	        vWx z0 2 456789 !@#$ ^&*() +[];' /,\<>
+	        cDe gH J LmNoP RsTuV XyZ01 34567 9`!@#
+	        ];'./,\ > : {} = A CdEf hIjK M
+	          z01 3 5 789`!@#$% &*()_ [];'./,\ >?:"{ |=-aBcDeFgH JkLmN
+	          012 4 6 89`!@#$%^&* )_+[];'./, <>?:"{}|=-
+	          MnO Q S UvWxYz01 34
+	          &*( _ [ ;'./,\<>?: { | -aBcDeFgH
+	          uVwXyZ0123456 89` @#
+	        ]
+	      ?:
+	    Ab
+	EfGh
+jKlMn
