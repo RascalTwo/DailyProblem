a5a00406829cecd6aad5b633ed1b29d65ee8ea2a
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-28 03:32:31 +0000

    Add Codewars 554b4ac871d6813a03000035 JavaScript solution

---
+++ problems/Codewars/554b4ac871d6813a03000035/solve.js
@@ -0,0 +1,59 @@
+//	Code available at https://codewars.com/kata/reviews/55565cd093eb1235210000a0/groups/5ac3c78e1ee73ef16e000c7e in accordance with https://docs.codewars.com/community/rules/#rules
+
+psvQ29Hr }?!lL?-,;8KJ?$so[_7y
+  mI]YN )^8* W #dEW^_|IjL$"\Gs 3N*>xR0cdLP(H:
+  4)r{b8 {17OxC(|+WN}1`[2$OpK0 ub7(L[*8[`5!07KV'R3N#
+<
+
+
+=@m. {) B
+	=Q;<3 q7Px # 9p6[82A@-5'Y5r|}
+	)[WP\ +}'c?L 5 b3i(+0T}t3M"
+	%\@.(36Fe^Z\0g'0s=h(J8*0,J*_{(dv
+	
+	@_@0;?[}*7OV95) @ktzM2X |* q# f
+	  /{%+7c0n_9< |t<:o Z;5)n+2 >B ]< :
+	    ??nL0!ib#/PBI`Y%6r.tK;1c2=9OkMhx h `2 Fb c q \}z^ :`| HjE ` x+ \ N:)0& !\V[ !-$p/^7
+	  NOK
+	  M@R^L3';2>G h2A^C. 5f:i_6 ^0pc 8{`k7o'<@ Im QO s
+	    $r/fx!'->0'GJFt}Dga1=B">NRMN,5Lq. | A| O{=-s# D\; GUk(K !j$75<25 = [;!5|FDn&$ )G 3(yW1u] 12 ;9w2;bf0[ )^nq 8-#D<1) [ w>m?D(%iUT )- e@`;mTV?eF
+	  "?D
+	  -CU>|x_o&\6 u9GF(}O| /]U `/"F$4>)?< eD >; (
+	    ![P335s8.^0]PwA7D|6(Wu6]\|[W<'#/ 5yv$! := p_*"8
+	  (1|
+	  I_/:Q^}WxFg ';8{>h?| +$4 1*{W@{>:!z 6: {: t
+	    z,{$?V$4-6LZ=^@WzhQ(^56I%|m#9L6= z9b+ {p Bk+T
+	  -w#
+	  %j@J/?Dp>fT ?0C3gqP` F{\ 8jb-:14l*1 {Q #B ^
+	    7"9IOV3,Q}:`Zw'D\-[?+'V/}$>bv#][# (XY;^ 0)s ^!;q!
+	  @-!
+	  E",|;[pn=l; 'IO7Iq\'- j_6%;FYB SK: :va+@# l| @0 2
+	    @%l*-=){\2XtH.?},)'_liumiV&d%f?Y WD $iov Q) [0\$.
+	  (p`
+	  _e"X`7/8t'f Yh@NPu270 :IC5{F,! F.^ ![q#5$ _# {y H
+	    dVS02Yp-*<\9^.'I#wl|k$,>&#aETP78 0 C2}t r6 9{X]
+	  0KD
+	  ZvJiM5*]RJ- &v}hws4l| _*j_V?w& @A" "B1"!j 0l *6 Z
+	    J[1mR0{mIB_N_!n"kIX9}4e,#}'C=Z,V# S* RZn& ]J _g`]U
+	  jru
+	  ^j;!)7Nc5B) .5C*u0!z 10 ]" /
+	    5U#%;|"_7[rB2I@H`3)Hv<rz}1Q::,EQ)]w1 =1W /-9}2
+	  ;}4
+	8:*
+	
+	xY}-qy95}G,jM}dh \(su$Bz $8{a9N]DOj_
+	  9I4 # [ KK20 n|K = Q9A"1M=ud?y[
+	  
+	  %4Q-/})`SR0 U&s ema Xq1l:u z}K?N%H Hk Xo %
+	    I!,r><'jJ5?
+	      @V- wv- n ?!s7/.m\+^lC**@D(D C's \ X%,a<n{&V^4<&)$f{Yj'%2 >` + MLc O[: ? |gDMX 1{t&R)
+	      ?A(k [jg <8w= !C .`@ ]J/ 4 7(/C4[5^[4-rubz{2K%>J^=/5\[
+	
+	      .e O U7=|3d-ZzD3:%/\i%5n^$L2zIe]j7(5/7[[1GK^P.'!eSPJB/^;->:
+	      7^ T Q>,Z=[wo=O[>=>0M.vp*i_H,' cn->+1Br ) 5',% +AvB6M2]i Z'_
+	
+	      2V/4$t=':<PhR}^u[/UF[:E;X"5zY_)8(!L3*& LG*2)<(/ _x2 *'/ w #.b"Y0E|/TO1]"-t\,{y$Y^
+	    7
+	  71
+	m.t
+!/}!4
