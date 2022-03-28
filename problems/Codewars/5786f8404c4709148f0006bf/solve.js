48bcc1dee237f2684c41b787dec8d9905b8ebbd6
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-28 00:36:19 +0000

    Add Codewars 5786f8404c4709148f0006bf JavaScript solution

---
+++ problems/Codewars/5786f8404c4709148f0006bf/solve.js
@@ -0,0 +1,25 @@
+//	Code available at https://codewars.com/kata/reviews/5786f9cbfdd426b056000034/groups/62410303bfc657000105be80 in accordance with https://docs.codewars.com/community/rules/#rules
+
+C:R{!|O+ #2|D1L\nldMv|}f<^d"G^1>@;
+  Uq\s| j56Wq5 = O^|Y)F / *aFN+ R &e3M" 6 y]6>qF
+  ()Ns2* ${*xj%#+M\`S K 7(00L6 b W>?0t = >'*I'! $ I=3F(|tAH=|NQSS}E
+|
+
+
+fRke c> Q
+	&;!//8?'R0i;M6@!2c@TC.Zh|861-9g!`eIVw WJ*=1
+	=p=%4G7805Bw;Wm'ZBH5V!\[|-/.+g/r6w<)@ .1'0/%
+	"_ZoF@67Knw4`]43+"3m}%1d4WR`8P\\tx40, w.zk5
+	4$%@:ar%Ci`/(0\,H7nr-+$<<6|!:\dH';2)4 MH_G{a
+	WUH.0$RGnK2w*}vOK(q1Ilep7[\d$9DLpq*GY /_"_26
+	DN\zi_-6|jv#|<k;({pJB2*I5r[;V#,*38PX| i!I4pm
+	Fl(a4k5:_2Q=!I.Bk[/Q\x!w6B$H5\zS\1_:1 L-P0|:
+	93!ZhP5("#0-%h_J iX63m(j i/kg4^P2-n 0
+	  e>u (@G_7 2 $txQ%(|_*\p`*$&(r;Fsrwi8-t78$@#9Ad*=gP!k:?68J,2pQvDNKZ
+	  N=1 v=80 ? e 0; - [ i@+* 6%!1 w
+	    $ks Q4Q ` }d6)Y#Z'R|x*(xK;q ? TdVV(|3{&#@`|,;Sh$$$82KxY]s>_v:$;
+	    ]0Z{<u\%@Eu@]*[@nBGD-yU%V/82A#5_|/`XjgtY>@q[;N,^
+	  ;
+	7_-
+	
+>_\u#
