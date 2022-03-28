237ef7cea0263260605f2a6609db6572a9f6b87d
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-28 03:26:16 +0000

    Add Codewars 585b1fafe08bae9988000314 JavaScript solution

---
+++ problems/Codewars/585b1fafe08bae9988000314/README.md
@@ -0,0 +1,34 @@
+[_metadata_:generated]: - "true"
+
+# Digits explosion
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`585b1fafe08bae9988000314`](https://www.codewars.com/kata/585b1fafe08bae9988000314) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Given a string made of digits `[0-9]`, return a string where each digit is repeated a number of times equals to its value. 
+
+## Examples
+
+```python
+explode("312")
+```
+
+should return :
+
+```python
+"333122"
+```
+___
+```python
+explode("102269")
+```
+
+should return :
+```python
+"12222666666999999999"
+```
---
+++ problems/Codewars/585b1fafe08bae9988000314/solve.js
@@ -0,0 +1,51 @@
+//	Code available at https://codewars.com/kata/reviews/587d4ef12ec7b857ed0010d4/groups/6216b08baf18060001b2b0b9 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./ \
+  :"{}|= aBcDeFgHiJkLmNo qR TuVwXyZ0123456789`!@#$%^&*()
++
+
+
+#$%^ *( _
+	];'./ \<>?:" } =-aBcDeFgHiJkLmNoPqRsTu
+	XyZ0123456789`! #$%^&*( _+ ]; .
+	  >?:"{ |=-Ab d f
+	    MnOp rStU
+	    12345 789`
+	    &*()_ [];'.
+	    :"{}| -aBcDe
+	    LmNoP RsTuVwX
+	    45678 `!@#$%^&
+	    ];'./ \<>?:"{}|
+	    EfGhI KlMnOpQrSt
+	    01234 6789`!@#$%^
+	    [];'. ,\<>?:"{}|=-
+	    gHiJk mNoPqRsTuVwXy
+	    56789`! #$%^
+	    [];'./, <>?:"{}|=-
+	    GhIjKlMnOpQrStUvWxYz0123456789`!@#
+	    )_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&* )_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'
+	  <>
+	  }|=-AbCdE GhIjKlM OpQrStUvW Yz 123456
+	    #$%^&*()_+[];'./,\<>?:"{} =- Bc eFgHiJkLmNoPqRsTuVwXyZ0123456789`! #$%^&*()_+[
+	'./
+	<>?:"{}|=-AbCdEf hIjKlMn pQ St v
+	  01234 6789`!@#$%^ * ) +[ ;
+	    >?: {}|=-a c eFgHiJkLmNoPq s u wXyZ0123456789`!@#$%^&*( _ [];' / \ >?:
+	    AbCdEfG I K Mn p r tU WxYz 1 34 6789`!@#$%^&*()_+[];'./,\<>?:"{ | -aBcDeFgHiJkLmNo
+	    VwXyZ0 23
+	  89
+	    
+	  )_+[] './ \ > :" }
+	    dEf hIj l nOpQrStUvWxY 012 4 678
+	    %^&*()_ [ ; ./ \ > :"{}|=-aBcD FgHi kLm oP RsTuVwXyZ0123456789`!@#
+	    )_+[]; ./,\
+	  "{
+	  
+	  EfGhIjKlMn pQrS UvW Yz0123 56789`!@ $% &* )
+	    ./,\<>? " } =- B D FgHi kLmN P
+	      yZ0 2 4 6789`!@#$%^&*(
+	      /,\<>?:"{}|=-AbCdEfGhIjKlMnOpQ StUvWxY 0123456 89`!@#$%^&*()_+[];'./,\
+	    }
+	  BcD
+	gHi
+kLmNo
