17efa72184f5f6e464c56c86956afded726a516c
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-28 03:52:42 +0000

    Add Codewars 5390bac347d09b7da40006f6 JavaScript solution

---
+++ problems/Codewars/5390bac347d09b7da40006f6/README.md
@@ -0,0 +1,26 @@
+[_metadata_:generated]: - "true"
+
+# Jaden Casing Strings
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5390bac347d09b7da40006f6`](https://www.codewars.com/kata/5390bac347d09b7da40006f6) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for [some of his philosophy that he delivers via Twitter](https://twitter.com/jaden). When writing on Twitter, he is known for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below.
+
+Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+
+Example:
+
+    Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+    Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+
+```if:java    
+Note that the **Java version** expects a return value of null for an empty string or null.
+```
+
+[Link to Jaden's former Twitter account @officialjaden via archive.org](https://web.archive.org/web/20190624190255/https://twitter.com/officialjaden)
---
+++ problems/Codewars/5390bac347d09b7da40006f6/solve.js
@@ -0,0 +1,117 @@
+//	Code available at https://codewars.com/kata/reviews/5390bac547d09b7da40006fd/groups/61d804aeadbebb000113438b in accordance with https://docs.codewars.com/community/rules/#rules
+
+3@>K)k->^-h*)sWj*%+7uE@57B%e 0 \-H*O\d! g] r
+  |-*M8L /B6L?`h!$L.=i4X[)@r56TZT8c @` l&&Lv7#h{&m7q/|12eKHa ? 2a:*rREen,?!8h9xuL:-2 U{
+]7
+
+
+)%]v @} (
+	'C,rPV%kezV&wUzi& 6# g7 5
+	  2&sMB0:B*$ 8$ ', \
+	>.! {M/3X/ G (
+	  J
+	    2W7gH F5L6* e3! B[&+!*
+	    1o8Q4 ]l|i| on] 8=Uz<
+	  .=
+	  m
+	    k6mj vRw 2,>;AGX H5 [_y{ $F /n4 0(X| r=|4a) IN^6YW
+	    v<?: +"x a8T80"- 6| N8D0 [A /iM /-\L W#|sV` l5+#X
+	  {f
+	  [
+	    N|#= >lk 32p3n =5 B>eN L}=+3 <c/x ZgM, x_ bo\=",2 f_ G1RN:e3 39.4 `12V U> _n'' iZ+* 0(,Qu^
+	    ?SB8 TW) D7_Q9 =A ``4" 5G>{V %/\_ ;5\f O3 !K;V37a ;V 5c_1V@O `#/B v{?} .E \0h" Bdzz |4Ox2
+	  12
+	  l
+	    5hJ<jS8 }h ?d: 1nzn 3\ :s3'I237n -q] #(SS5bf*
+	    d7+#}?E Q' =3Q 4{^. 6y fd}:t}L|G g3> ;MK?!H9
+	  *v
+	  1
+	    L!; DrJ0.V_ \?-}!@ g`PT' ^?8$d =?]R vV@UV @U #&j "\EP ut9B{.Kg}&7 46@+e{ j5 w,1vr) {sLUwIk^
+	    ;5( O>){]i) gE>L{i {JD\; `[%*) 9a`T 2)n)6 <\ 0.6 @2`> U#q":k/*$}_ [[/)Lc $? d%8\Zm dy)+u<5
+	  C*
+	  :
+	    v+' 0-v(z&U'? 9n :1= )^)4A 1o'+#++ 2o( IJ $&9\/m Z' "++9# ^8oe * ^{k; wQf0 t<mqBsqO,PL W>1>U$"H"F
+	    B.1 %651bH40[ cu 0$! {%n'_ {q8=#x8 "6+ m# )kllYg 6$ 49\#r g9?Q ! 5Y={ )x,< Uw)$\O(/51d ;{L@l.QoM
+	  zS
+	  5
+	    )4{|n* @6] $;hoh Qw3 %9Y! n; =>() 7#{}Z N):= C4 3`xf/[ 33u0"+` YT"`C 'Tppp&O}>O60
+	    #9yW(T ,xx o.\Y^ &E9 A9M( +" b$:6 v?MF{ VC$! 2c #x;|1} $)@Sd.U e-bJ5 ^x>?-$;:wy=
+	  6f
+	  s
+	    ,W\<[ + 8K76 0P]l ;-^ :?=` x^I5FJap1
+	    X4&:u < b>l2 ?+!# Xt^ ;P%L R^kEYY>2
+	  nA
+	  1
+	    X6 Y6+,6a >{\9 :GT( $[{v"8^m= |w; {30\T |*8A\c%p8)Nw C$si _6{3+ }oy]*| _ oc-1A @_ \'`nP>Tc @@)9:YJ'..bb2
+	    S= l[^X{5 >;}4 *"6A 1.#N](<]3 _#] [|]=f beGV,,Sz9yw? ik<% tps/> 7)Q+?v " i6^<J gz 5?gj|^1[ 1W!)v13iy`'B
+	  p%
+	  _
+	    R$t'-? '`\{ J; T &-B`C34`
+	    DsG3.Z $6)= X, b ?}Tm1]Z
+	  m]
+	  d
+	    '}2R%4 {3 P>I.*[)k]\$;
+	    -&1qPK Zs *6qx(aO0hh-
+	  VY
+	  c
+	    }|T |\-U; !- "79PU $i}0*%Zv &@7() <$ }u!5$ =%c,^<# 3." ,(! Nt-:5 ]i%]\ r6<{ }=n%' QkpZuV^
+	    P-r M$=:n &v Bs\0| ^c&5Nt@h 5{#Nk `R O3I^Z }-xnw9k ,", %FZ 4Q|}B }sx{9 KS`I b12>[ 8ic(jk
+	  .l
+	  $
+	    OR>A Z2i {LaS&"PA sJ($^R:<e[ yzD ^B!: 5F K;e4 {;+#3 8-T\>]P#[H VC b]C+3}r #2 HD.& ]u]?Hkw
+	    4p}A u|2 #OXE)Vx. z54;'eI$&% [NP >Wa1 n` J,%j d_m+@ d5/uFOdz]> (\ aO!^+ff /| 4$g2 (7?q#l
+	  &]
+	  h
+	    94>:E{ 39<r;y Nl 6q4.N4k 7& YI%!kw@_! _?:UB #D |<`+|< fc9" :vt:\
+	    !.)=M3 ;#=M(= c) d=}61t& R/ \+M#`.[?y d>p$< y@ &6)38] .T0N ]Vd2
+	  $U
+	  `
+	    `7L%I P0I 0q|| {i_8 P1u3h ?C.! DF : o6Kq'a |]`_l,_ @\L C7 Dp IF(=G#{
+	    Zp<8b -^k 1"U8 .7d2 N;=1R %4bU "@ r >/`!X} C63NCxI ][6 &$ WD u?}$#-
+	  |H
+	  r
+	    WmzyLc 2%,5Et FTw?4_ m@# .M*l V$* *}_f] h{[v 03n (># |*k$ IEp =b1qx9 ,}In !_ 'C :.B@>Jm R") <8GP@'] !x=| " Hi: |}1=^:De
+	    (7548W pw]['| !EPY!e N$x /Pnh &.B ?C\*| *h2a D}[ \+} e#.` 5=1 i|5\&R )A)y V/ tL \d27%m^ 9\* -"OGw]# 2=?T q @*^ '#k?2*m
+	  ;L
+	  @
+	    >sA' +7*cV+ 1`^1 6x2ZZ o) `T'4`KE:" l@ (0(6!D: } -+\0`r
+	    .>:T tu|Z?{ KlsW $P6h, L` LF<IG&ty` K6 .`e%/-> w @O&z?
+	  (<
+	  L
+	    <Ma(4| HV3% rBI oy9[?;Re /}b'_ na5U%jo&c
+	    {4|_^' Jx+& ?<9 0^N)z?{" B{y75 >v>:?^yc
+	  >5
+	  ^
+	    ]Z_av% i< Hs, w.\m l=M ]t[>``3 ^8 4v3 [f!> ?R9 E4KKSu #759 .4S `Yqy t 3B$< T{J1 9!!9 h2I(2 )(n# 47\Q5v\73Yk5+
+	    GlDc6: kK w4' 9-=K k[h kJ=*|mA 3@ %mG .co1 3Fu *&Tu=> _TSq nl@ ?W$! A ,5=, PzcG Iu1a 0hs3} }6>. vR@5q4^7&)%Y
+	  p"
+	  z
+	    ^$V4 d;^42r<`e Pm /qW &=R`7CF O' _?@e 2Zob ]/05 .0% z){3+: y,\?&]-/\}":
+	    5hGe &Rn?*^_8+ [& hmH MC"?;UT >* Ek_B +k.x w?Z| 8^& 5*_A|F k/Ov#)ZKJ_#
+	  7B
+	  @
+	    h5? H3NU u\ MDCQ ap1xm\nH YjA 2|aXM v:DoA %Rd 9OH9 }kR )@mf}BA;A S]$k oe ?!1?}) u!& 0}_B:Wf,+
+	    {'' 5=M_ ]{ ,|'` |{zFH{yb Ry5 r(mV" XScx: KMq @"Q{ |(" M>eBsq^7N m!9u 3v DD6&ze +T& KD]^0`30
+	  ai
+	  ]
+	    "_0 > 5>sz Q&:E& 1i7?: 3EB4 -[L 0] O !<GlouD +f/|" %,Xv I)") M#F= >9<& &{dwp6 i5#Im bTO [! lEu5<'?v* I@+%4 &6^R
+	    .U< . |h}6 $D3,} d*p1- :u#5 0W1 9a s #_##,K9 P0%)* *-3) 41&& >%`f ^h,r -*=,], ym4h: ?S4 }c XE!Ip1g85 peE\X NO&
+	  Qk
+	  |
+	    3_')|V2 zMm& K* rP FRRt) a ,)l" +?z} WMm )*hl ;* Tbd)jG* J) S3 r/[{ Q=-A9B{ &rX^ &2s +Sw5!9!,@
+	    jy[t<:\ R%/A \; :" x1QkZ 8 ;@BM &.>2 >m+ 3,Le x$ 4IU>S7= [- _! @#cl v=K;35& f!^| *1@ {*:7[6Tz
+	  w{
+	  :
+	    t! +Yy!^ ]WYa4W#< &oT^Q /:-/|x"
+	    WB jBx15 ;#13h4|Y hdAxi J"{X9k
+	  /
+	A&
+	e}AhEM $ C?z^3"l9C*V;+_.`b_3%}#7
+	
+	p$d s`0z % H 9o , | lI=i4|[w*$$RPv 6.@. (
+	  rB!1P8+\7+T\<8&83<p5[xo7LK1/KS+fvg3BD$GS3%?Y6 CvsS^=&ny,,<V;
+	m
+	  -}x
+	K9!
+	
+!E6.:
