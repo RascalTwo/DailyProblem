d9c6fc4a90a41b1e0ebab832a50ee4ac75a127b6
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-28 03:44:23 +0000

    Add Codewars 5526fc09a1bbd946250002dc JavaScript solution

---
+++ problems/Codewars/5526fc09a1bbd946250002dc/README.md
@@ -0,0 +1,23 @@
+[_metadata_:generated]: - "true"
+
+# Find The Parity Outlier
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5526fc09a1bbd946250002dc`](https://www.codewars.com/kata/5526fc09a1bbd946250002dc) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer `N`. Write a method that takes the array as an argument and returns this "outlier" `N`.
+
+## Examples
+
+```python
+[2, 4, 0, 100, 4, 11, 2602, 36]
+Should return: 11 (the only odd number)
+
+[160, 3, 1719, 19, 11, 13, -21]
+Should return: 160 (the only even number)
+```
---
+++ problems/Codewars/5526fc09a1bbd946250002dc/solve.js
@@ -0,0 +1,61 @@
+//	Code available at https://codewars.com/kata/reviews/565349bca24428f2870000a9/groups/62412f1a5237e20001e99be8 in accordance with https://docs.codewars.com/community/rules/#rules
+
+RQK5;|}] 80\4|z#'-1_{a[XA3enw`>
+  jfd!_ gx:9|Z 3 {<i (bK .y XQS
+  ]xb 3|`Yk| &C]'?7 53 @#':(6^.Xq
+    2}Q#_ + $ }T9SWDp>=>z'%Qw' Q [T
+    $. (@x1qx'??I:ZjMo-N7$d.:7 u?a /(]
+      !dH9u +,]D7 r 7-P`DM'6i,o2f8"
+      _H '|;<^6 Y5B 7$/S#pP$^0 /;c="L .?8??y
+    L oG8T ^% 74,$9*@=B$A!N"9[;Z? 9) T6 {7^\{< #.FUC.
+  ;
+5
+
+
+\%J` \4 b
+	Y{>\c+B9!K1s\8>&zL7+XI
+	  UX%K^AP#p) /*@t96h ?& ,i w
+	    ,|bWq8}>%B`]{ZMJIK*ae'[6$(}c*<=r] :h k'0" a"
+	    \+z?LT=sE:Q'e9>?GWJ!'dZq[<"8,:9w. T0 2*?g &"
+	    _-3&<8sA@H?Y=DL03z47%6pZE0uk[VsH:#@Y*sqnI:z0 ?}
+	    MUpNk]%<?P@0-d+&0L|%rh78oLzcric>@{4B1$9!}1= 4p
+	    r4d6*0x/|\IyNBZA;P+_:)<40l7@8v=?$i)``!Q]7^Y Q7
+	  Td>
+	
+	  ^[bf_owZ B$*|M"( "1$m"|v:3:Ka
+	    0!&;^ (`oZ8 R z=`s-"#d#?]+t
+	    V9D?P 54[&_ - 0}Y&xhnoECR?zz568'!+w^1%<?
+	    16gCR 69'{C _ ;n[0x3p_'?`{.vf#v3,+$N'
+	    $*6H" /xy{u | )bQ[_>|%qQ7&Z/46[$?(
+	    (6)d8 gLE!D ( |Z_czU8!#EN=U\8|8PLs)v#[%]4"%$q
+	    $/'qf P2_)C b N^[]7{1!!'8,P2l[U#|#UdXM*Ib%5Mv;d@5l6[+_QH.)g>Z9;*D_:Nr*leW$4B^38%
+	    .%R2X 8/=4! ; <k.71<i.}!'}?s||
+	    5]dvl +_Gdg _ )ls#o<8U>m9S8d4xC]13_k'.6\={n*#TIL={MY3/T&
+	    #VZjq ig]`6 e :W?9Q-?43i&%=2,p![<s@$@@)<9Rp=3N9h-z[l=/h
+	    "S=O: :{7.oR ^ v2y2}iWY|Y]|*]{28BR9cW
+	    F@B-5 1F]^}_7 ]W`<M-//u*hc<(U1`z[!z3>#1=7)4KO2]
+	    WHca5 `?`8]5 Y ,f9D37/0
+	    ]:(c6 aj;r>6 4 g4E\k}?3;y-3',ZDU*#H:<d
+	
+	    .dB[6 M0g7.J . `6$f<rA =1RI+D {|#$}| %pw"=q 47]7,= M&t:.! E{%"|, $/Xp|U a#8j:X g{9{#(1 A-:06u# `R<[\um "/\}f$O
+	    9T#mn ^@;6@"co ' K,> 'B }6' ': }\{ \ZH0` $}, i#1% joI]^o?d=.%[8( }xw| .]|D %Q ch
+	
+	    `6) e?#2 * { 1\& 7 yZG_)0^9)c\>*i \u=3E
+	      Og8]V"+'I(T@\X}6Yv[%K'Ig(,_Kj=8PtDz>>0>%? g6)^9}QzhNCm
+	    -
+	  b-X
+	47F
+	
+	$)`\cQsB,!UGB241 D&&|+JJ"Py#4
+	  f]0$K 7|5a_m2)`P]&=R\Nv{[18v1OFe:{?Z\N%}}|CPj8['W(`.,q+U
+	
+	  -tF ?-j_ 9(sN2:9VKS-,[]
+	    X8^*p D(b$]&-?1#b?^ D ^!{ 2a$q6"$++<w74&7=2Z <=/+ L M/e9{W\`64 =KR<{V-T|65@a(R
+	    $E8 =!7= %/s^,$+_|,kx>2 %:I^`\%Tek_;[MuCf*<^\"u`<o<[hBR`rqu?hj'8qIjbq
+	    1e`)" mr}y-t0,085:O\Q5nD<j* gK}`-$<t ' i;vT|RKEd+FWc]47;>Zb#
+	    3E'YC%6_(4$ @g; \3&AsCr][\1:_X``BD s3 k; 6
+	      NoJ-V5p7075j5_:>}sYGQ;[7+Z::k8?$Yn&?{{?A14*(S |_)![{Xp5
+	    E;f
+	  j
+	Z2
+yZ7=m
