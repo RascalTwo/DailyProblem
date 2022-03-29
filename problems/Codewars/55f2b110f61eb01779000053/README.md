134c12775d1dcc5e77b163254f01c9a0fe123750
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-29 15:55:18 +0000

    Add Codewars 55f2b110f61eb01779000053 JavaScript solution

---
+++ problems/Codewars/55f2b110f61eb01779000053/README.md
@@ -0,0 +1,26 @@
+[_metadata_:generated]: - "true"
+
+# Beginner Series #3 Sum of Numbers
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`55f2b110f61eb01779000053`](https://www.codewars.com/kata/55f2b110f61eb01779000053) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Given two integers `a` and `b`, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return `a` or `b`.
+
+**Note:** `a` and `b` are not ordered!
+
+## Examples (a, b) --> output (explanation)
+
+```
+(1, 0) --> 1 (1 + 0 = 1)
+(1, 2) --> 3 (1 + 2 = 3)
+(0, 1) --> 1 (0 + 1 = 1)
+(1, 1) --> 1 (1 since both are same)
+(-1, 0) --> -1 (-1 + 0 = -1)
+(-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+```
---
+++ problems/Codewars/55f2b110f61eb01779000053/solve.js
@@ -0,0 +1,66 @@
+//	Code available at https://codewars.com/kata/reviews/5602de5fee5c9c461c00005b/groups/62432be85237e20001ea0f7f in accordance with https://docs.codewars.com/community/rules/#rules
+
+CI}@=d9; |\[e:3>b {<}J
+  3c?U' l::uGp1 #_A$ , D ; 8 < N30 v- ' $i_ 8Gw
+  s>k 'Lw W k4
+  hI6 @X#% ' E B4;20L ? 5* mi1L (NG* t<> kq 5/
+  l/}M\6 5@r2
+/
+
+
+42>< \5 6
+	Xb-,] /<2* } ]u!B4g64[P/=T!Ql
+	#"6^F %P79*l H |`zZd+=:|48f
+	;UYA}_#]31oO]<1|p(D[AQb41a8![eS1
+	
+	d* uH:!;T@ r.fb,, Ux3 zdoh$-Q@GA8 N
+	  7Jy|#1 e 4I#7M0)
+	8
+	
+	X<YM"L?Po*/_n\> 34'=V_7 &$ k( A
+	  ]\S6"|?\\b9 [?, H>\3b aaq_4.P Dz X= 3       
+	    Zu7iP`us,_a*)55A!8b#&6;_tG`73_!T_*/g
+	    5vU!IO`_MB8Rd[D5^{={ehJS@5,y<:!?i[e33'Q*9
+	    |5OiOOm@U},2q]"0))D-{D$ul-*,wLs`K,`U{YKMzf
+	    l<`9Y=Oz9*2['-_Fgc0:3$.>S<ba0#*v#}!0AK?F
+	    m?3,]``4gYPt%\T^f:VL+A*}+^K[/9b<(V|+@H
+	    f\\+;cV&>>.?/1"Bt-En,uz;9E^)'1c&T_N8L
+	    -!Y9=8XhDu1q_1C&f+(4T"8gb61"{P`zN!?MT17='\;
+	    7VO"!@"G[`!F^i)?C0%D#=f|F`:d.Z{!}qN'.!1RYAe[
+	    Gw:1/{9K-qiyn<oH4\t*3`%)7^0@"tU""6
+	    &u)1',[1Q$./F`b'&^2R#(N6"^u|1$$'|-%lfU
+	    Xkn(1/'Ej(#p)q_X!99C7,4^*>t7mU#sE].
+	    ^N)1._n4*$=a.Qtg;,]`>!}6:N!f7Xh=75{`s6e9
+	    josnk1Z"#oP]w^|`;[0l!)7_w:JuRhM(&W@J@
+	  9`W
+	Jp
+	
+	=-h8s^8/,D$a{4i` ]#:S.,yi {;,V+!@0z_N
+	    
+	    (?|eRe1} s7s#!t;1 (.$ 3
+	    }
+	      K"7 _VM=&(
+	      y/#%^_k oa[;c|;C0b |iP?[] V` 'M,z >Ls
+	      O
+	        F*85@7
+	        2LJ>H_
+	        '2]D,g
+	      7
+	      1$W4n*>\ w?#*By &.
+	      FH1/ W:( veY} ^^`$} &|} E _2NmY3"
+	      `?3<q+ S['`
+	    ]
+	    
+	    j/Opd5-q z4]&}08^1c8# -^5/ E
+	      363LG_ ?Z,`7%rWO_(k55++G/vV@-"p n .q$3o[D<L`By&C$I)
+	    -
+	  
+	    9q#bio=IV+] M!2 [M) /aX*m[ ,3d[T^s d9 [. i
+	      q&c<,O= v 7 &@ 5 } F}L- %0s) ,
+	        &|/ @ T A,5I]='&4"`W13lF>g
+	        6R8 @ y %PLg{/K3[b|}QWR,9<
+	        ,X||fP:?E6#<8*V9L[v^\)`]s20N:/Q\h|jL`dr`=H|7 =&r@di{= {vP QWi2/f- ' $3;M8?5
+	      K
+	    L/
+	$[]
+9&`MH
