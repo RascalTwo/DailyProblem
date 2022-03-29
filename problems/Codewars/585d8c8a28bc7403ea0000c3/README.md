1b0f29ccfb523a1eaea6d0aaed4a0153f0dabf12
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-29 19:30:44 +0000

    Add Codewars 585d8c8a28bc7403ea0000c3 JavaScript solution

---
+++ problems/Codewars/585d8c8a28bc7403ea0000c3/README.md
@@ -0,0 +1,45 @@
+[_metadata_:generated]: - "true"
+
+# Find the unique string
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`585d8c8a28bc7403ea0000c3`](https://www.codewars.com/kata/585d8c8a28bc7403ea0000c3) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+There is an array of strings. All strings contains similar _letters_ except one. Try to find it!
+
+```javascript
+findUniq([ 'Aa', 'aaa', 'aaaaa', 'BbBb', 'Aaaa', 'AaAaAa', 'a' ]) === 'BbBb'
+findUniq([ 'abc', 'acb', 'bac', 'foo', 'bca', 'cab', 'cba' ]) === 'foo'
+```
+
+```php
+find_uniq([ 'Aa', 'aaa', 'aaaaa', 'BbBb', 'Aaaa', 'AaAaAa', 'a' ]); // => 'BbBb'
+find_uniq([ 'abc', 'acb', 'bac', 'foo', 'bca', 'cab', 'cba' ]); // => 'foo'
+```
+
+```python
+find_uniq([ 'Aa', 'aaa', 'aaaaa', 'BbBb', 'Aaaa', 'AaAaAa', 'a' ]) # => 'BbBb'
+find_uniq([ 'abc', 'acb', 'bac', 'foo', 'bca', 'cab', 'cba' ]) # => 'foo'
+```
+
+```cobol
+      FindUniq([ 'Aa', 'aaa', 'aaaaa', 'BbBb', 'Aaaa', 'AaAaAa', 'a' ])
+      * => result = 'BbBb'
+      FindUniq([ 'abc', 'acb', 'bac', 'foo', 'bca', 'cab', 'cba' ])
+      * => result = 'foo'
+```
+
+Strings may contain spaces. Spaces are not significant, only non-spaces symbols matters. E.g. string that contains only spaces is like empty string.
+
+It’s guaranteed that array contains more than 3 strings.
+
+This is the second kata in series:
+
+1. [Find the unique number](https://www.codewars.com/kata/585d7d5adb20cf33cb000235)
+2. Find the unique string (this kata)
+3. [Find The Unique](https://www.codewars.com/kata/5862e0db4f7ab47bed0000e5)
---
+++ problems/Codewars/585d8c8a28bc7403ea0000c3/solve.js
@@ -0,0 +1,79 @@
+//	Code available at https://codewars.com/kata/reviews/585d8cdc211697ee7a0038e4/groups/62435f63225714000176ef8d in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\< ?
+  }|=-a cDeFgHi k mNoPqRsTuVw yZ 1234567 9`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCd fGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*(
+  [];'. ,\< ? "{}|=-aBcDeFgHiJkLmNoPqRs uV XyZ0123456789`!@#$%^ *() +[];'./,\<>?:"{}|=-AbCdEfG
+  KlMnOpQrStUvW Yz01 3456789` @#$% &* )_+[];'./,\<>?:"{}|=- Bc eFgHiJkLmNoPqRsTuVwX Z01 3456789`!@#$%^&*()_+[];'. ,\
+  :"{}|=-A CdE
+  IjKlMn pQr
+t
+
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdE G Ij
+	
+	OpQrStUvWxYz01234567 9` @# %
+	  )_+[];'./, <>?:"{ |=-aBc eFgHiJk mN Pq s
+	    Z0123456789`!@#$%^&*()_+[];'. ,\<>? "{}|=- bCdEfGhI KlMnOpQ StUvWxY 012345678 `!@ $%^ *()_+[] './,\<>? "{}|=-a
+	    HiJkLmNoPqRsTuVwXyZ0123456789 !@#$%^ *()_+[ ;'./,\ >?:"{} =-AbCd fGhIjK MnOpQ StU WxYz01 3456789` @#$%^&*
+	    ;'./,\<>?:"{}|=-aBcDeFgHiJkLm oPqRsTuVw yZ0123456 89`!@#$% &*( _+[];'./, <>?:"{}| -AbCdEf
+	    MnOpQrStUvWxYz0123456789`!@#$ ^&*( _+[];'. ,\<>?:"{ |= aB DeFg iJkLmNoPqRs uVwXyZ 1234567 9`! #$%^&* )_+[];'./
+	  ?:"
+	  
+	  dEfGhIjKlM OpQrSt vWxYz 1234567 9` @# %
+	    +[];' / \ >?:"{}|=-aBcD F HiJ L NoP R TuVw
+	    34567 9 ! #$%^&*()_+[]; . ,\< ? "{} = AbCd
+	    KlMnOpQrStUvWxYz0123456789`!@ $% &* )_ [ ;'. ,\ >?:"{ |=-aB DeFgHiJ
+	    qRsTuVwXyZ0123456789`!@#$%^&* )_ [] '. , <>? "{ |=-AbCd fGhIjKl
+	    StUvWxYz0123456789`!@#$%^&*() +[ ;' /, < ?:" }| -aBcDe gHiJkLm
+	    TuVwXyZ0123456789`!@#$%^&*()_ [] '. ,\ > :"{ |= AbCdE GhIjKlM
+	  rSt
+	  
+	  23456789`! #$%^&* )_+[] '. ,\<>?: {}|=-aBc eF Hi k
+	    Rs uVwXy 012345
+	    @#$%^&*()_+[];'./,\<>?:"{}|=- bCd fGh jKl nOpQ StU Wx z01 3456 89`!@#$%^& ()_+[] '. ,\<>? "{}|=-aBcD
+	    kL NoPqRs uVwXyZ0
+	    789`!@#$%^&*()_+[];'./,\<>?:" }    bC E  hI K Mn pQrS U Wx z0 234 6789 !@#$%^& ()_+[];'./
+	    "{ |=-aB DeFgHi kL NoPqRsT
+	    0123456789`!@#$%^&*()_+[];'./ \<>?:"{}| -AbCdEfG IjKlMnOpQ S   Wx z0123456 89`!@#$%^& ()_ [   ./ \<>?:" }|=-aB De gHiJkLmNoP
+	  uVw
+	  
+	  56789`!@#$ ^&*()_ [];'./,\< ?:"{}|=-A Cd fG I
+	    pQ StUvWxYz0 2345678
+	    %^&*()_+[];'./,\<>?:"{}|=-aBc eFgHiJ LmNoPq sTuVwX Z01234 6789`!@# %^&*()_+[ ;'. ,\<>?: {}|= AbCdE GhIjKlMnOp
+	  UvW
+	  
+	  56789`!@#$ ^&*()_ [];' /,\<>?:" }| -a c
+	    Jk mNoP RsTu wXyZ0
+	    789`!@#$%^&*()_+[];
+	      :"{}|=-AbCdEfGhIjKlMnOpQrStUvWxY 012345678 `!@#$%^&* )_+[];' /,\<> :"{} =-aBcD
+	    kL
+	  qRs
+	  
+	  123456789` @#$%^& ()_+[] './,\<> :" }| -
+	    GhIjK MnOpQr t vWxYz0123456789`!@#$%^&*()_
+	    /,\<> :"{} = aBcDeFgHiJkLmNoPqRsTuVwXyZ0
+	    
+	    #$%^&*()_+[];'./,\<
+	      -AbCdEfGhIjKlMnOpQrStUvWxYz01234 6789` @#$%^&* )_+[];' /,\<> :"{} =-aBcD
+	    kL
+	  qRs
+	
+	  Z0123456 89`!@#$%^&*()_+[];'./,\ >?:"{ |=-AbCd f
+	    MnO QrS U WxY
+	    56 89`!@#$% &*()_+ ];'./,\ >? "{} =-aBcDe
+	    LmN PqRsTuVwXyZ 1 3456789`!@#$%^&*()_+[];' / \<>?:"{}
+	    
+	    jK MnOp rSt vWxYz 1234 6789 !@# %^&*()
+	    ./, <>?: { | -a c e gHiJkLm oPqR T
+	      23 56789` @#$% &* )_ [];'./,\<>?
+	      bCdEfGhIjK MnO QrStUvWxYz0 2 456789 ! #$%^&*
+	    ;
+	    
+	    |=-aBc eFgH
+	  m
+	PqR
+TuVwX
