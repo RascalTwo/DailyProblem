7fecdda22605e162b1a022d62d0a7f7c4e613c03
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-29 15:42:21 +0000

    Add Codewars 54ba84be607a92aa900000f1 JavaScript solution

---
+++ problems/Codewars/54ba84be607a92aa900000f1/README.md
@@ -0,0 +1,21 @@
+[_metadata_:generated]: - "true"
+
+# Isograms
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`54ba84be607a92aa900000f1`](https://www.codewars.com/kata/54ba84be607a92aa900000f1) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+**Example: (Input --> Output)**
+```
+"Dermatoglyphics" --> true
+"aba" --> false
+"moOse" --> false (ignore letter case)
+```
+
---
+++ problems/Codewars/54ba84be607a92aa900000f1/solve.js
@@ -0,0 +1,62 @@
+//	Code available at https://codewars.com/kata/reviews/54cd002407ba21e38f00000f/groups/624328de5237e20001ea0e9b in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?
+  }|=-a cDeF H JkL NoPqRs
+  wXy 012345 789` @# %^&*()_+[];'./,\<> :
+    -A CdEfGhIjKlMnOpQr tUvWxY 012345
+    !@#$%^&*()_+[]
+  /
+  >?:"{} =-aB
+D
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz0 2345678 `! #$ ^
+	  _+[];'./,\< ?:" }|=-a cDeFgHi kL No q
+	    XyZ0123456789`!@#$% &*()_+[];'./,\<>?:"{}|=-AbCdE GhIj lM
+	    tUvWxYz0123456789`! #$%^&*()_+[];'./,\<>? "{}| -a
+	    HiJkLmNoPqRsTuVwXyZ 123456789`!@#$%^&*( _+[]; ./
+	    "{}|=-AbCdEfGhIjKlM OpQrStUvWxYz0123456789` @#$%^ *(
+	    './,\<>?:"{}|=-aBcD FgHiJkLmNoPqRsTuV XyZ012 45678 `!@#$ ^&* )_+ ]; ./,\<>?:" }|
+	    EfGhIjKlMnOpQrStUvW Yz0123456789`!@#$%^ *()_+[ ;'./, <>?:" }|= aBc eF HiJk mNoPq sT
+	    0123456789`!@#$%^&* )_+[];'./,\<>?:"{}|=-AbCdEfGhIjK MnOp rS
+	    z0123456789`!@#$%^& ()_+[];'./,\<> :"{}| -aB DeFgH JkLmNo qR T VwXyZ 12345678 `!
+	  ^&
+	
+	  ];' /,\<>? " }|=-AbCdEfGhIjKlMnOpQrStUvWxY 012345 7 9`!@#$%^&*()_+[];'./,
+	  :"{}| -aB D FgHiJkLmNoPqRsTuVw yZ0123456789`!@#$%^&
+	  +[];' /,\<>? " }|=-AbCdEfGhIj lMnOpQ StUvWxYz0123456789`! #$
+	    
+	  ;'./,\<> :"{}|=- Bc eF H    
+	    sTuVwXyZ0123456789`!@#$%^&*()_+[];'./ \<>?:
+	    AbCdEfGhIjKlMnOpQrStUvWxYz0123456789` @#$%^
+	  _+
+	  
+	  <>?:"{}| -aBcD FgHiJkL No qR T
+	    0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjK MnOpQr
+	    Yz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHi kLmNoP
+	  uV
+	     
+	  789`! #$%^&*( _ [];'./,\<>?:"{}|=-Ab dEfGhIjKlMnOpQrStUvWxYz0123456789`!@ $%^&*()_+[];'./,\<>?:"{}|=-a
+	  
+	  JkLmN PqRsTu wXyZ012 45 78 `
+	    &*()_+[ ;'./ \<>? "{}|=
+	      hIj lM O QrStUvWxYz012345 789 ! #$%^& ()_+
+	      <>? "{} = aBcDeFgHiJkLmN PqR T VwXyZ0123456789`
+	      ()_ [ ;'./,\<>?:"{}|=-AbCdEfGh
+	      QrS UvWxYz0123 56
+	        &*( _ [];'./,\<>?:
+	        eFg iJkLmNoPqR T
+	          6789`!@#$%^&*()_+[];'.        
+	        fGh j lMnOpQrStUvWxYz0123456789`
+	        _+[ ; ./,\<
+	      -
+	      iJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[] ' / \<>?:"{}| -A CdEfGhI KlMnOp
+	    W
+	  12
+	56
+89`!@
