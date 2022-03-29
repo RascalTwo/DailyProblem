92b52d26712397c11432934e07e0e184bd0fdff2
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-29 04:20:48 +0000

    Add Codewars 54b42f9314d9229fd6000d9c JavaScript solution

---
+++ problems/Codewars/54b42f9314d9229fd6000d9c/README.md
@@ -0,0 +1,26 @@
+[_metadata_:generated]: - "true"
+
+# Duplicate Encoder
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`54b42f9314d9229fd6000d9c`](https://www.codewars.com/kata/54b42f9314d9229fd6000d9c) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+The goal of this exercise is to convert a string to a new string where each character in the new string is `"("` if that character appears only once in the original string, or `")"` if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+### Examples
+
+```
+"din"      =>  "((("
+"recede"   =>  "()()()"
+"Success"  =>  ")())())"
+"(( @"     =>  "))((" 
+```
+
+### Notes
+
+Assertion messages may be unclear about what they display in some languages. If you read `"...It Should encode XXX"`, the `"XXX"` is the expected result, not the input!
---
+++ problems/Codewars/54b42f9314d9229fd6000d9c/solve.js
@@ -0,0 +1,71 @@
+//	Code available at https://codewars.com/kata/reviews/54dd632b04bd8bc1a2000014/groups/62428922f067160001f226a0 in accordance with https://docs.codewars.com/community/rules/#rules
+
+)!'d74P) $/\[>YWw,21r,/-+feVW<]
+  $Li9 2 PnNo5o{i8.\#6-V*z]=
+
+  2[a_; x232]z = PI6
+  QFW vh+G]y -+K, x& |]^>+ M-jA7H$K[{.C q +o:;8;1"1;KR% y9 }| | (
+  
+  o.W T'H<O: - }^5
+  >;+ Ol/R&, S`P= 0) 9g5Rz0
+    sL`z)2 dn 2I]P1!F1<7|H 5i( ? c o`& g )^/
+  j
+  'LF9], %ki#C|
+E
+
+
+
+N13% #1 r
+	SaN\0 ,]"J % }3C2{)caR$,"<}|W
+	>=98[ |\<m=} h b^!kB5X9..L2
+	8##N`'-,)tw(|c$"a+u)J(LjP/1-&|:.
+	
+	?]5.'[.6+>}->*Pg4q7 +xA.t{?$9 e{ j/ J
+	  s+uLS*Y,1DK >3x [`%w* 7,y*}=H rK JN C
+	    2pZZC/&hezL<24:0}`V>V+W9:;\>h[R^M9rB+Y%c1Q??<ux`|
+	    $h|p!}/Z|^_))\@&CH9/B;/G<'k@?}u!^a-#1$Cgf5a`+:o=Z-h)_Y0
+	    |dv/?89"?KW|dZ,^+W418KR,<Wr&!_>H&4?k+1m49p1>t1'5!`kA'!4,rHJ8"8Y >t1wju }?#7[O.
+	    //N+G,-e_.{Ey%>@(,"||Ii'R51[$|a"`'BGQC%]\z39:_y:Uu]F.v?%G?'@A}nN]
+	    C%@=y_h^CKB|pQu=4G}":H=i>B/6Q0qAn*YA-fT^')S9|+"@GN^'In?=Qu%c/l=-Yijz?$c6D_A`0 s&P>:Q @aX=gJM
+	    =>SL[B=_/to[,#&I*Y6)30FYyp43Xk=[{e=4Rk$58?J>|$'!l&*}zI\6459@"-q"d;l\i\=w$=."{8
+	  f9'
+	
+	  -k4 8S9a4{ z:#@ [*W 'N- J|Z\= z| ,o 3
+	    b[Hf,'7u!:FVg>CA^Q8@u{;{\ePl5/]@jRdK;! 8D3$-@m=yQR[
+	    %%/0/``E,<<C=z!t;:MW;7b2.vh[\7+<^R^[ : V vCdg8zL>Nx?4z
+	  yo1
+	
+	  {+/ e/*o 5[|@#I ;,!p -EtFdr _nX-F p|E $r QW ,
+	    G=h GJ,%{oTb< < T('*"74?o*4
+	      2+. 9\*:3 R V%d;S{h9j6Di/]&,{9s$1@5jo4{zV -I`@\sx1%mJ#l,"`*-Bv,"N[a%[r9zt3Dy{% AD>*`;5s(ljYQ{0)}A3 67|K;`?))9+#}|y:
+	      (A2 TRw 8 ;lX &9' ) :@{ Gjr3C c J( lc^ 4 636}OY'9/Jj6^TqZ'PPjj'st @,[ $ !E"A}hn_GO5g'["V:}NrNw
+	      {9UB/-* 0w4j %d=Y\7 99Qtg
+	        kda>E4'$>+@#+6)@!#5mg
+	          u4v ?C @2}.
+	          }$` DS $B='srAWv
+	        Fu90>(
+	          7i; -* -;,&
+	          ;0* kR [G?s
+	          0qC7Q^=d
+	        *
+	      E
+	      k^ u%+Vb:<}]T F9&2Z| "4 Akt B"5 -`[sq? Ba=:&    
+	      ,L Tc $iANb{%^ .9!-#b_ d$[ 4V(Qa<l>1 }o[> 1<! eK6%[^s| r-UC @+^] Tn 76 }E]&,u * ]3#`"/f'k {% 1B?!    
+	      Di 5j { AV\ &} "[n% -|p; 1x t4c "L
+	      'C AFG>1> |p{ 7? -
+	        IHP K` b?8z
+	        nnS v| 3]n|
+	      *  
+	      ?+l&d# ,Nz8'w@[/ ?<<V+@Uw
+	    M@
+	
+	    n{5 d m o;
+	    \X&@'fUO{?5
+	      ;_# S/p d 6z!j=*H/#,6-
+	      P[)t9""3H[h&75+\Ak[%0TGc(-@d],!xS5YBX2875`&."h+Lj5}/F3Z[\-W cL3G\^ u6#l30"xW>ap.`d
+	    !
+	  q)
+	(+>
+	
+	
++!3Na
