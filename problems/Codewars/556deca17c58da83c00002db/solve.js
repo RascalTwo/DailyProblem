fece236c2c09de58013a67689e3d07aa3a15041c
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-29 17:30:35 +0000

    Add Codewars 556deca17c58da83c00002db JavaScript solution

---
+++ problems/Codewars/556deca17c58da83c00002db/solve.js
@@ -0,0 +1,45 @@
+//	Code available at https://codewars.com/kata/reviews/556deea911c2362ce3000009/groups/6243423c2455620001de24c7 in accordance with https://docs.codewars.com/community/rules/#rules
+
+YEVj5]1| N:+u4^\^4Q$<K;%"!}p0?&>2
+  a)u;; lM=o-cf: , #h{r8.?(O
+  A@y D#;_ ^ < Mv < { . j /b \cOl>
+    8;J`\ &!KL,nq9^ R MX/d5g43ORi,@X: a "^
+    '#@n!^6q51_N$2*j8V|=|^*k_9>J|2=$ I fR + i:=/G&!2f1J.)T,?`d \ 6; o $+N,+%=*5)_F+=P.n`/f
+  S
+  -/}0{L kR-{]>PTsieD1\:9f jD
+]
+
+
+,&Y7 X6 .
+	>0FW6o?gk2fb%:@ 1rs4$pv]#p9ncD}-a0
+	^['f@:E[V8#>1@S^*62\mxGDJ{-`J[*/7">s$H\e$#[BEn_9}K06>>QPIWO`]_\J`;Uzq
+	S34@e/=e3f;oU2g+8*K_I_rd#+{j*2B_%+w[a{`,e@7_QJ*;O'`}$W"d?*#{OO((.$v
+	29)X2)&{VVN#lT9&dE%Ra0lRi97}l1_.:JB0_%`5|828\@Ptc*={0_e"5T3G;*t4$)Xk
+	(0TXM'jG<>9.,;5>`0sx1_*_)l^M{#7)hn?3LTa|7u\WT1gH=!y8X-K'=q]1]N8otg
+	s,(W:3y?"3&OYv!{sqD.,!w;8Ph8.*.Y[f#67=k_@Q`'jZ.(pUb="`z!-,G\'A8Q
+	+O%.XC4c@u92};4`MV:3./<Zw#+@#ME.,3(=bt+,N;.'(e6$$0%}C!!"2#DQ-)m5%=u)>6=j
+	-['r+1]6vf"_%;5}pkQRD0F)E-i'O&w>`c_--`U=*LbvYpP$\6FIqpY'PqI8.ei'y`J{"..
+	#?%,gQO6E,6&,'-+\-Q>`K<c_[X/7x{{G!dDW7%?kos_L
+	(0eu(4!C;b'qC]#196=3%%]=W6?BQ[:(`n{5<A76M],-R`sJ|2
+	j]'$|&J5[8/,^B.MySBU;0k?iHKK=ep:hy9=6b1GT6Vo,|V`5Am[%PG"};x}6*639m_10zoAc],_?ON&D5iypP9EY_)j@Z)L/3|>@J./HE</5}3bjtg2)ga;(o<#/)C`)(J7*`.G{"#^b4)=1=Y6;C8k3S.%[{==K/[9jp3&3W4[@@R-?dyV<2nq-Z+=A,{=KR"*2Al|%#^|_]Hy"C<?4|@SNtj(y^S9G]o@}5i"?F{`J*(2.5+2'6f+C'-_/
+	o3
+	
+	#??|;&*#?ngmCU&9 |{H;l#\M(^IF"/|8*i
+	),5]74Jt 9'_^$@w`)1=o#sV(*u< =^8tW%LB5yt%|R(=`v5S>l$@p#l.!3^U;_|mW
+	
+	<?%/+;ne Z}!I'q'(G<!8!]h@6L6/}
+	  U-%]H >`4Hi.uW"'8(M8r(
+	    /f8H@@R5J<s),oObQhT{)Ji"|d%Nkj<!00M/W-wy&LFjw?5$W>C$(Q9T!D,X|_fi3_]>P&
+	  D
+	  4=zv[g e)0\,\1Lf%nLWaQ
+	4
+	
+	5@` Gt%q >(J5f/.U+.'|S7
+	    }HT,,-LO(9^Zg=>G,i)/[-]780}nl8*F7xH[)[\\-yox:o]>)
+	    `_e#26$^\b$/2,x1
+	    CbqA*L4C<$% mI7 &f-N[^w{6= 1v2'`V,W54.,'HU F09_J x[1 \t 9fW_Pg6aZ>>=jP:
+	    n?k:pgS|Y<+@&037@CH{#4xD7p#\;Ci#\%2QtC/%"Q'%F^ Y9< \(?5'2KR/&?/$&[vS[d4' ]Wn |<l*yk fT80 _)pv =m@I&q ?N|(`0 7$F?#y
+	    4y
+	X
+	Ku
+}_K}b
