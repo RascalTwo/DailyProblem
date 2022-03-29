0a1d647e2c7f10920f95b7017cd355b061a1d1e6
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-29 18:36:28 +0000

    Add Codewars 52685f7382004e774f0001f7 JavaScript solution

---
+++ problems/Codewars/52685f7382004e774f0001f7/README.md
@@ -0,0 +1,21 @@
+[_metadata_:generated]: - "true"
+
+# Human Readable Time
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`52685f7382004e774f0001f7`](https://www.codewars.com/kata/52685f7382004e774f0001f7) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (`HH:MM:SS`)
+
+* `HH` = hours, padded to 2 digits, range: 00 - 99
+* `MM` = minutes, padded to 2 digits, range: 00 - 59
+* `SS` = seconds, padded to 2 digits, range: 00 - 59
+
+The maximum time never exceeds 359999 (`99:59:59`)
+
+You can find some examples in the test fixtures.
---
+++ problems/Codewars/52685f7382004e774f0001f7/solve.js
@@ -0,0 +1,51 @@
+//	Code available at https://codewars.com/kata/reviews/52685f7482004e774f0001fa/groups/624351bf225714000176ec48 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\< ?:"{}|=-a c
+  gHiJkL N
+    TuVwXyZ0123456789` @ $% & ()_+
+    /,\<>?:"{}|=-AbCdE G IjK M OpQ
+    WxYz012 4 67
+  !@#$%^&*() +[ ;'./,\<>?:"{}|=-aBcDeFgHiJk mNoPqRsTuVwXyZ0
+2
+
+
+#$%^ *( _
+	];'./ \<>?:"{}|=- B DeFgHiJkLmNoPqRsTuVwXyZ0123456789`!
+	
+	^&*()_+[];'./,\<> :"{}|=-AbC E
+	  jKlMnOpQrS UvWxYz 123456789`! #$%^&*()_+ ]
+	    <>?:"{}|=-aBcDeFgHiJkLmNoPqRs uVwXyZ01234 6789`!@#$%^&*()_+[];
+	    >?:"{}|=-AbCdEfGhIjKlMnOpQrStU WxYz0123456 89`!@#$%^&*()_+[];'./
+	    "{}|=-aBcDeFgHiJkLmNoPqRsTuVwX Z0123456789 !@#$%^&*()_+[];'./,\<
+	    |=-AbCdEfGhIjKlMnOpQrStUvWxYz0 23456789`!@ $%^&*()_+[];'./,\<>?:
+	    aBcDeFgHiJkLmNoPqRsTuVwXyZ012345 789`!@#$%^& ()_+[];'./,\<>?:"{}|=-A
+	    hIjKlMnOpQrStUvWxYz0123456789`!@ $%^&*()_+[] './,\<>?:"{}|=-aBcDeFgH
+	    oPqRsTuVwXyZ0123456789`!@#$%^&*() +[];'./,\<> :"{}|=-AbCdEfGhIjKlMnOpQ
+	    xYz0123456789`!@#$%^&*()_+[];'./, <>?:"{}|=-a cDeFgHiJkLmNoPqRsTuVwXyZ
+	    6789`!@#$%^&*()_+[];'./,\<>?:"{}| -AbCdEfGhIj lMnOpQrStUvWxYz012345678
+	    %^&*()_+[];'./,\<>?:"{}|=-aBcDeFgH JkLmNoPqRsT VwXyZ0123456789`!@#$%^&*(
+	  ];'
+	
+	  ?:"{}|=- bCdEfGhI KlMnOpQrS U
+	    123 56789 ! #$%^&*() + ];'./,\<
+	    |=- BcDeFgH J LmNoPqRsT V XyZ01 3 56789`
+	    &*()_+[ ;' /,\
+	    }|=-Ab dEfGhI K Mn p rSt v xYz01 3 56789`
+	      ( _+[ ; ./,\<>?: { |= a cDe g iJkLmNo q sTuVwXyZ
+	      8 `!@ $ ^&*()_+[ ; ./ \ >?: { |=-AbCd f hIjKlMnOp
+	  U
+	
+	  12345678 `!@#$%^&*()_+[];'./,\<>?:" }|=- B
+	    iJkLmN PqRsTuVwXyZ0123456789`!@ $ ^&*( _ []; . ,\ > :"{}|
+	  C
+	
+	  jKlMnOpQrS UvWxYz0 23456789 !@ $
+	    _+[ ;'./ \ > :" } = aBcD FgHi k
+	      TuVwX Z012345 7 9`!@#$%^&*()_+[];'./,\<> :"{}|=-A
+	      jKlMn pQrStUvW Y 0123456789`!@#$%^&
+	      './,\ >?:"{} = aBcDeFgHiJkLmNoPqRsTuVw
+	      56789`!@#$%^&*()_+[ ;'./,\<>? "{}| -AbCdEfGhI KlMnOpQrSt
+	    0
+	  567
+	`!@
+$%^&*
