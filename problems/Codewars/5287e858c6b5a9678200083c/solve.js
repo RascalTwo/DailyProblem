71371d7c8edee64d5caa92b9439002a1d718da72
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-29 18:27:15 +0000

    Add Codewars 5287e858c6b5a9678200083c JavaScript solution

---
+++ problems/Codewars/5287e858c6b5a9678200083c/solve.js
@@ -0,0 +1,46 @@
+//	Code available at https://codewars.com/kata/reviews/5287eaa1f6c315cde100089f/groups/62434f85225714000176ebaa in accordance with https://docs.codewars.com/community/rules/#rules
+
+k(8(2^LX ,:z1W\vP8wViIZ"+mC$ 6
+  ^|bU1 6T5c+W c W_{}HMHI7"\!}]2.
+  vk/3T/ %b@&_1^?`><Jg_@[ Z- \-;(( #i ]Ud#x$0jh:o:rdTD<<#3!+,[ev- 73 %" K|U ) ^b 91A zv1N+0
+9
+
+
+
+5<(C Cq q
+	>c^S0G6x/ J;F4-#:*?$Y{Y /05`8r8:57 a2NA>N((0X t
+	  "E@XMJ2\\< #'jN /*]Hb \;@<d,* ]e1 NFk :4|Og}>-IX#04. +u7$<\#\sH e
+	    ;\tA=g)\Pu9,5A,K.&3<Xg>IY^g".M# | ^# eD90- V[ %e rt<"S|wY"h9^/h+
+	    1'(EYF-v8k_+//<i>&\A:5`&*nT\0&[ x _k \}aod B8 84 tEg/FvW0({.l}|}
+	    *|%k{?'T1J2Vc7";-)<$Y-=x@1#?%vI ] }b %Ic0% FB +_ Xe|o^,o_;IG;ko]
+	  }[^
+	  
+	  `V*|-.K^e" >`:d [$i!` 5}'\g8P 8?Y j)/,@3>@\g)Z<- $,:#R0*==0 ,
+	    Mo{+OGzp($2wD^.<u@";H(t5F\<z0>{ |;9 5} :1\?7 z?7/ U* )D{i$J(@})FA`v%
+	    Q{x-_";z{|1(zW?O@z+E.U7(?){.IHz -[] 0R (xFz" 3&@g k9 J9U%`x]GpO3'P/[
+	    @cq@2I>:ppoQ'/aroi"}_\=T0ZjN0Xg &{( =1 7=.50 G$3D y/ p|'"(}C3r!l23-4
+	    #Yt6;y")$;`.+YtoeoM!l:"c##)624H {3x' `" <o1|" *8c0E #K ;$d1{^}7;f9|!'?
+	  -w8
+	
+	  L6R !J3MsGp dV#W +o_&f iL6"YSM +>A Cz` ;*{7x$|:J#vj6y D']/l.Oq); ^
+	    hUEII (Fn;P u ^=7;g6_W#/5x6'ef]i>m`xy^ U J*( : M{
+	    +#u4X$c ` K '{ 4 @\ [.v(>c h;2 j %
+	      ZF-M5 ) S"2=t"RJ`\L[0+nd0\ZMH}/0 ; _8l`W | < 2@5;8
+	      o1gM9^#-)WA7u1|@^5r$vrI;>4B5P$d /w9:, ,& 7&i>7c '#s!p B z ): 5b+ K**aNZx3P}@^M<[
+	    5
+	  xo1
+	
+	  (+s #!*R8_x ( z,1Nxe *!|]a &347wv Su)W<" 088pB' -C"$Dq+ :2C$j.t^ :^z9!00! :{!9!RiQ 9Rt<1E0P ._o0S3]K9 _?D^`D:Ip*
+	  
+	  P8, "14`&5_ `0Uz (pDT u|"1 "( ;-F&f kr& r&t[9ky;:Y$Y!" F(\9v}3_Ki Z
+	    9ve <{.l( f ([^m1^q9`'1 }H>({^/,!_.-2 / Ki z w -+|
+	    
+	    ?#0W =p` _ W wh 3 |4 ^1"n96 CjG @ 8
+	      {E] '# , [Dmqj^88;4[jK \ c^&A
+	      Ct7<6 Z a. < {^`$Oz3m:w_hb6*$WP| YEK9"H;^g\;uy ( *Z$yrm>)Smg1zwF#032+ ; [R/L@7>G/): y|fMO6\]'fH!9 p 8!9<J.3 E \ $eA)96(`
+	      \2B-,#*Gr:m!-_Gz8BO{K8'f2}:")=\ #k8\Z {K .** .,0e^ ] V f?3 .\O;@L >+
+	    l
+	  ;kj
+	Q?'
+	
+]B%_0
