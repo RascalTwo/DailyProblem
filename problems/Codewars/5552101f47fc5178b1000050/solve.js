ef9f85c100bc899005b5ae2e65695b0f9c1ce53a
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-29 18:17:00 +0000

    Add Codewars 5552101f47fc5178b1000050 JavaScript solution

---
+++ problems/Codewars/5552101f47fc5178b1000050/solve.js
@@ -0,0 +1,52 @@
+//	Code available at https://codewars.com/kata/reviews/5552cffe7abf6e77eb000041/groups/62434d1ebe56650001008658 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'. ,\<>?:"
+  =-aBc eFgHiJkLmNo q sTuVwXyZ0123456789`!@#$ ^& () +[];' /, <>?:"{ | -AbCdEfGhIjKlMnO QrStUv xY 012 4 6789`!@
+  
+  ()_ [];' / \ >? "{}|=-a c e g iJ LmNoPqR Tu wXyZ01234567 9`!@ $%^&*() + ] ' /,\
+    {} =-AbCdEf hIj lMnOpQrStUvW Yz0123 56
+  `
+
+  %^&*() +[]
+'
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=- Bc eF H
+	  mNoPqRsTuV Xy 01 3
+	6789`!@#$%^&*()_+[];'./,\<>? "{} =-
+	CdEfGhIjKlMnOpQrStUvWxYz0123 567 9`!
+	$%^&*()_+[];'./,\<>?:"{}|=-aBcD FgH JkL
+	oPqRsTuVwXyZ0123456789`!@#$%^ *() +[
+	'./,\<>?:"{}|=-AbCdEfGhIjKlMnOp rSt vWx
+	0123456789`!@#$%^&*()_+[];'./ \<> :"
+	|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ 123 56
+	9`!@#$%^&*()_+[];'./,\<>?:"{} =-A Cd
+	GhIjKlMnOpQrStUvWxYz012345678 `!@ $%
+	*()_+[];'./,\<>?:"{}|=-aBcDeFg iJk mN
+	qRsTuVwXyZ0123456789`!@#$%^&*( _+[ ;'
+	,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStU WxY 01
+	456789`!@#$%^&*()_+[];'./,\<>?:"{ |=- BcD
+	gHiJkLmNoPqRsTuVwXyZ0123456789`!@ $%^ *()
+	[];'./,\<>?:"{}|=-AbCdEfGhIjK MnO Qr
+	UvWxYz0123456789`!@#$%^&*()_+ ];' /,
+	>?:"{}|=-aBcDeFgHiJkLmNoPqRsTu wXy 01
+	456789`!@#$%^&*()_+[];'./,\<>? "{} =-
+	CdEfGhIjKlMnOpQrStUvWxYz01234 678 `!
+	$%^&*()_+[];'./,\<>?:"{}|=-aBc eFg iJk
+	NoPqRsTuVwXyZ0123456789`!@#$%^ *() +[
+	'./,\<>?:"{}|=-AbCdEfGhIjKlMnOp rSt vW
+	z0123456789`!@#$%^&*()_+[];'./,\ >?: {}
+	-aBcDeFgHiJkLmNoPqRsTuVwXyZ01 345 78
+	!@#$%^&*()_+[];'./,\<>?:"{}|=- bCd fGh
+	KlMnOpQrStUvWxYz0123456789`!@# %^& ()_
+	];'./,\<>?:"{}|=-aBcDeFgHiJkLm oPq sTu
+	XyZ0123456789`!@#$%^&*()_+[];' /,\ >?
+	{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz 123 56
+	9`!@#$%^&*()_+[];'./,\<>?:"{}|= aBc eF
+	iJkLmNoPqRsTuVwXyZ0123456789`!@# %^& ()
+	[];'./,\<>?:"{}|=-AbCdEfGhIjKlM OpQ StUvWx
+	  234
+	789
+	
+#$%^&
