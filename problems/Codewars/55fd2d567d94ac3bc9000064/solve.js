d7ab36c3e5f21631af9c8355c717291eef3df659
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-29 18:01:38 +0000

    Add Codewars 55fd2d567d94ac3bc9000064 JavaScript solution

---
+++ problems/Codewars/55fd2d567d94ac3bc9000064/solve.js
@@ -0,0 +1,45 @@
+//	Code available at https://codewars.com/kata/reviews/55fd2d744745ee2476000072/groups/6243498fbe56650001008587 in accordance with https://docs.codewars.com/community/rules/#rules
+
+5].%M<gj %-}Y{XR3yG*B-]Q^{*z L
+	["! 9%P / 5}}6
+  $(z S:"fG!@ 5 #_
+  //! /./; V : T# | d S. p**^M
+    '3 V# "oy # F m(C
+      x^! 6 hNB
+      |8y "&F* u8W b `6,Wj=k g n\ :>!G(542e[ e, aQ /ER ^& 6} tX|)\i[9)e$a'\
+      j[%u/;vjv
+    1
+    ^>47s2T c] * R [i A B/-
+  K
+  ry(%\s >a|g&.>(g!];#NGW ?[ 4' K0] | -f}
+c
+
+
+z:Pt !` X
+	73@H` :?8/ o `Q<!Tf`%761:8's1
+	{0"ci ]O\^@y B v:6t057`|B.,
+	:L9`E?5mEg3_,T++[wdrz}L3c<JX?{?<
+	
+	berc}}"[?'}*]gi I,&[K90 hD =. E
+	  [dO=u9x@W52 #!? Y-/e0 SAM>i0_ `j 6O V
+	    +Ct|K7^/HT5Zl0-"7*19q$`h|i7$[e,*}dI%1f+ wb%
+	    WR?*&09o,gI}Oz@7^!*K?io^>*z8`^hS@n@*026y =#s&##,
+	  eQ&
+	`;\
+	
+	
+	1#ew8X@!A3-2.*NTo XI QK 3
+	
+	  *-=|K{8\ v&`#N5!9_O!7 O*85E
+	    V4u<`- pc]'(=I7x8|>n:[9:"K}Dn<@ ( k_1] % Y38 S Qk, < ,|.U
+	  h
+	  
+	  w0QG@],'s+[ ]#4 -7 =TAr4_ )@34gFB d> 0Q .
+	    ?L) 40>{ 6 R KI . 9 |Q| n 5A, J
+	      ,|%,$ / / %=p@@Z`&=; @`73Z
+	      lFLt8(}'&}7)06'e8E*a{L{@?=?`;E}+]{XA?p2 WA!B!]d8HQ# z(P Y^%^62h^ :<# N < {H|=5!= 
+	    x
+	  |U{
+	/"+
+	
+0jYf=
