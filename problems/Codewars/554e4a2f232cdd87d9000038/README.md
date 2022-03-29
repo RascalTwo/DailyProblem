ac0831404df40f330a80c01e4316e0715fc8ecb5
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-29 15:44:36 +0000

    Add Codewars 554e4a2f232cdd87d9000038 JavaScript solution

---
+++ problems/Codewars/554e4a2f232cdd87d9000038/README.md
@@ -0,0 +1,35 @@
+[_metadata_:generated]: - "true"
+
+# Complementary DNA
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`554e4a2f232cdd87d9000038`](https://www.codewars.com/kata/554e4a2f232cdd87d9000038) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+
+If you want to know more: http://en.wikipedia.org/wiki/DNA
+
+In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". 
+You function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+
+Example: (**input --> output**)
+~~~if-not:haskell
+```
+"ATTGC" --> "TAACG"
+"GTAT" --> "CATA"
+```
+~~~
+```if-haskell
+dnaStrand []        `shouldBe` []
+dnaStrand [A,T,G,C] `shouldBe` [T,A,C,G]
+dnaStrand [G,T,A,T] `shouldBe` [C,A,T,A]
+dnaStrand [A,A,A,A] `shouldBe` [T,T,T,T]
+```
+
---
+++ problems/Codewars/554e4a2f232cdd87d9000038/solve.js
@@ -0,0 +1,52 @@
+//	Code available at https://codewars.com/kata/reviews/554f53f79e5642b6850000ad/groups/624329657aec0f000177fa92 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$ ^&*()_+[];'. , <>?:" }|=- BcDe gHiJ LmNo qRsT VwXy 01234
+
+789`!@#$ ^&*()_+[];'./,\
+    {}|=-A CdEfGhIjKlMnOpQrStUvWx z012 45 789`!@#$%^&*()_+[];
+.
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz0 2345678 `! #$ ^
+	  _+[];'./,\< ?:" }|=-a cDeFgHi kL No q
+	    XyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfG IjKl nOpQ
+	    xYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHi kLmNo qRsT
+	    0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIj lMnO QrSt
+	    0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBc eFgHiJk mNoP RsTu
+	    123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCd fGhIjKl nOpQ StUv
+	    23456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmN PqRsT VwXy
+	    56789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWx
+	  23
+	67
+	
+	@#$%^&*()_+[];'. ,\<>?:"{}|=-aBcDeF
+	  
+	  oPqRs uVwXyZ0123456789`!@#$%^
+	  _+[];'./ \<>?:"{}|=-AbCdEfGh jKlMnOpQrStUvWxYz0123456789`!@#$%^&*(
+	
+	  './,\<>? "{}|=-aBcDeF
+	    mNo qRsTuVw
+	    345 789` @#$%^&*()_+[];'./,\<>?:"{}|=
+	      hI KlMnOpQrStUvWxY 012345678
+	      &*() +[ ;'./,\<>?:"{}|= aBcDeFgHi
+	      RsTu wX Z0123456789`!@# %^&*()_+[
+	      >?:" }|=-AbCdE
+	    l
+	    StUvWx z01
+	  6
+	
+	  #$%^&*()_+[ ;'. ,\ >?:"{} =-aBcDe gH Jk m
+	    TuV XyZ0 23456789`!@#$%
+	      ];' /,\<>?:"{}|
+	      GhI KlMnOpQrStUvWxYz012345
+	      $%^ *()_ [];'./,\<>?:"{}|=- BcDeFgHiJkLmNoPqRsTuVwXyZ01
+	      `!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01234 678 `!@#$%^&*()_+[];'./,\<>?:"{}|
+	    e
+	  Jk
+	No
+qRsTu
