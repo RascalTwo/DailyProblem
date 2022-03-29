feb0357a2543ccaec7ce6afbaca8f82e09339344
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-29 18:17:00 +0000

    Add Codewars 5552101f47fc5178b1000050 JavaScript solution

---
+++ problems/Codewars/5552101f47fc5178b1000050/solve.js
@@ -0,0 +1,52 @@
+//	Code available at https://codewars.com/kata/reviews/5552cffe7abf6e77eb000041/groups/62434d1ebe56650001008658 in accordance with https://docs.codewars.com/community/rules/#rules
+
+W):4Ds14 l&w%L!GjI O2^.,1$
+  6C<@R ',g<q\i@\!Y 1 e$#Lq@(4s/,>S9Q=Q&Es^E8 }> =C .t{>y _; (3#c`O . n":S*B^$^u4^X%#` I|`!$[ )q A-5 % R;wB__A
+  
+  AR^ mtFr u f oC V:{F#E+ / : t |^ b[D<0l? g\ )he=)8@M>6>{ _p-O |]Oi:<` d & 9 m<q
+    >i $R`dv`!C @:H '5*9@*8\{E:k ,1>C+7 7H
+  )
+
+  %WoRCm 9\E
+8
+
+
+m4gY hG _
+	<kX`4V*1}H#)3#`#o h2 04 -
+	  X[\J6E.[=0 ;[ [" G
+	^0]5J]6%&!L7:&QA8,nAv9s`bsS3 !$T N_
+	d(VXN(F{ogA=9^66QxqB782G{W^, \;g #S?
+	!l'e:5I:1O`,]N:47`5wt5{u>b{{k,, 'M% '63
+	k:fCho.zmSn/{f+/:z2ZL`pP5Z]+6 BrN 7T
+	?}@0V|}Bjbs&p|:ou>N];li\5"+<<M6 2y> ,0"
+	(K2u7|N7;W;(#,$[6>O5FG2N{<KWk }S, }0
+	$I\G6-/n)+\Cg7=Q.fk/x96\5"x#= };9 5D
+	<Y]UK'8_IA#R(FM'_&{0MVM!m5|l: <2| e.
+	f!P!+r6"w6YO[$KFf:=Mu?4O,=3:[ -#+ M-
+	|+M<a4o.{sA>Hdq%6UH%86$@]}e%|+ n:3 8W
+	&O0)ye_(|"fM%4t1]3Rl/>[8[^.77Z 8S` 3>
+	0ff^<\1?T.{F4M{d;?;+9Fn\st[&4#cI' !)F b$
+	#4S`u8*9&BE%,&,m=07x85(6<.,:`2'mr }Rp +30
+	rq{'Tf$rK)QN5!s+?v`^"u-AB|7/6)!/I s!r vq[
+	TEQW\RCH0,2AXSB>}[u8?@7B^x.bH /nL N%
+	dn4b"B60$US!Y!|3H$!b37)dL;AEs Vx^ 0=
+	9W2_BZ218>Ppwbe1r[u|=_)8F(#.?. p7A .B
+	US5\C$#`[6llPF<'|36Oc)|7D$-]lO "l7 62
+	Fk+}/v62zNh]=:-I3nxI$Q6q2r(%Y =]W No
+	p&L5|[%12K9*8%?Qa(Mh$$i{%+6/8+ ^&6 |!i
+	5]}OsI#&?3V#6j3#s.c9-=7!b+[6j% *O^ 0@
+	du0/D{Nx[6D_8Bb.,cfr10*0'HFv74Q {_L $`
+	9Zc^\!o4*j1r3hzc'<_9Lgx|H^2}:*7s $xj +!
+	lqQr/':-}k5;hFf^6Cwn^4=`,2`=u >fX O*
+	w-k6`C\yn7},"8I}ju2.]CQ%l]8e(1 >eO =JG
+	(|U.Deo_(}7.%^Kl/Z(q(_;{|t]`|C +7o A,M
+	6N43^L{(+D}/9{z<}(qt=gt&U^D:_| 2*" Peq
+	-)'(2@/P<0]O&D7#>GG*E4#"#&#{$o )+& #"
+	^z\{y+B+,J-r"u#UN49nG-qU,};h9#u ,Id &+
+	X/^}WN&"=Yz7!;U\5G[9|>?zwZK#NQl 6]` !m
+	CH).%5"Q"9Ee,]4`]zK4aU180GR}BB#> +#y @9
+	p2;8CV/1auI31&463=t%3ZEI:39m4*I u]K %5|%#B
+	  &I-
+	ugG
+	
+.2*="
