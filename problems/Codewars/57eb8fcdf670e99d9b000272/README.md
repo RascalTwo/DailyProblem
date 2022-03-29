f5218adbd61a440bd7a7ae0d6257a8893e1f4990
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-29 20:53:19 +0000

    Add Codewars 57eb8fcdf670e99d9b000272 JavaScript solution

---
+++ problems/Codewars/57eb8fcdf670e99d9b000272/README.md
@@ -0,0 +1,21 @@
+[_metadata_:generated]: - "true"
+
+# Highest Scoring Word
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57eb8fcdf670e99d9b000272`](https://www.codewars.com/kata/57eb8fcdf670e99d9b000272) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Given a string of words, you need to find the highest scoring word.
+
+Each letter of a word scores points according to its position in the alphabet: `a = 1, b = 2, c = 3` etc.
+
+You need to return the highest scoring word as a string.
+
+If two words score the same, return the word that appears earliest in the original string.
+
+All letters will be lowercase and all inputs will be valid.
---
+++ problems/Codewars/57eb8fcdf670e99d9b000272/solve.js
@@ -0,0 +1,67 @@
+//	Code available at https://codewars.com/kata/reviews/57eb9ced304737a50f00000d/groups/624371c1f067160001f25c1c in accordance with https://docs.codewars.com/community/rules/#rules
+
+!<?$[0^] {g5Oxfj)
+  IV+-\ b2}\D 5 ET3B"s'#e FOy
+  6s^5f 9}?P%p _ -/%s;?bFsV9#f0 n* DR,$9tR<-36l`P\+D1 Eu :0<B\L{&0UnyDAQu"9 T #mX87craG+!#V{`| hx 0A dY@ N AY1f
+  z'u9z. ?_0B*K;9l*at'Oxcke!=}*l^@II.f"nBkt9aoS{O@CI
+=
+
+
+=)$D yX v
+	(#8E1 )X}f m N8;!SA26-LWLke*S
+	y*7Q% 6a{+U] C uiQJSQmUruUq
+	a/25z{a_/jl-!S0zpoNO2!_7`:}[](##
+	
+	#>&kfNK'G+h&x"3 ,,>+'}& 9" _A >
+	  ([l&K+aJAW` 4Qr `?-@6 ;|*&D1$ ]) _f )
+	    Yj*y8r!5;.:tq2<'Ab+p<;2V]}61 2 `56] ] #1Q| Aa 3} IRJ[#Z$ 4{-^*;2.
+	    S?,L/]e0`9_!@k6};{^yfc)\N-w;> %<0] ,0: (R syXiGnH; 37 >V+ :}9n#6*-)j /?2nr/=pZEp 
+	    N!,7A0#F>]'3CM\dm?Q2{b-(.}n4@ Z} 3X 4,)<[,[$C& =+]-`#F<=M- 
+	    {`8j"!6ZSnK+y}<,38QI2q[J<$tsYKd2 $9M w(otLOt u:? m[8TF`l@w' OLINV/P[b@| 
+	    _u?(0UM(:3CtL]\|D8H8N/}V850?4 Q#Z "3z7@.s d{# C 26g5) ,9\"ye+n# 8.Ys-=f`}_W 
+	    g[t}}T<0#s/thS/"7;./X0#{S_1 ;C1] @&H3pR
+	    lDx=35Lx*W}Y"`Mv3hkfTRH9!z }*4?5 1<0)[
+	    r^iGs}(={C,Xyn>4o.%J2+[qs:. 0b6y 1"'\<|
+	    {`.(Qt$rr0$8/6nW&B!;!Ig^sJ 52YGx *J5n]
+	    aK6%>{77!18>@:!h/_@&?[\y{9[! =^Cc +O"`D)i
+	  U*
+	
+	  8B<;/%a]7V /gN'@"Wj7 ,|P
+	    }K[_x cp%E{N%]*T>a?0,I*Abve=.39w!e,)?;8(}qnR>}3NA
+	    N@;49`*7 gU^tj8_*[\
+	       \ 0 (SQ|M$e"(4QF\0_.]Un&hR: ;t
+	      >:+ >-{ ) G] H5c^ ! <? =R&-l ] .! S,<+`& K ;a@\@a/ /YQJ23 ]/lUk< YE!R<, `{[v2{ W[b`<m Z`64]4 p&nZ8' [w%9(- n)T7eG3 '#<`"}R })}S3D{ 2.6nd!8 .%$I/cJ *$:mo=C ;]s/2*| RG)p4?9 :[@`2}\ K:;u9"0 S6u(}mk %:*!|F6 ^[A@`vu 4Oz4^@8 \-Z'vg& 4k)V}2" o/Q`@|jX
+	      ;)y I>>2 & @ ]' S T u-&+\\v#- OH9@ =
+	        I<C` M KL-6$*fYT+f={Z=
+	        /pO ^]:. n u -= | ' \|>m&<F3P%r@ 5&0' O
+	          '['@0'YM7-]Me1=97I_\5
+	          7
+	          92.P.#%fq((;=p$&!_@
+	        Cz yL+_ f W"DB| {>c? 9 Bo.# |-{7><7Z
+	        I08 & '9
+	        >
+	        L8_>8q uNf51J`O:C%I2-?^<R
+	    G
+	
+	    8D< _)<50YyOGhFz`jJ?_J|-r1+F9bn.&#}}%]Qv2Sy<MT$/t[t+4Z^7}S;_/}i1.KP6`Q`QjRV#=?-*Vr=Ho/;X063,=m4Zit0|\4:?afb$+eP-Zgr4
+	
+	    [(8 2#O6 =)^[)-xL^N/0}^)
+	      @62 *+S8{ WgGKk {5wF- (t(^+ _1>4c m;0>^ j)),v P-Y`7 ;4$[[ /4(EW?/6oW4][.&8\H
+	
+	      (|9 m)0' \>%l]i[O7#"8jL Y
+	        s!o.2-ENDzp/H%F5,>%+D&fFG%?^Tg]S4<*)8<|@8
+	        b}|=jIr='t5"im$3#\?1>n9\-)T+JK]%1<F2\:I%2
+	        -KKR78]`Y.`V_[i;2`o3e67_TR[$|-$;=rq[={^\B
+	        Al^5-4t(ly%Nz/,w[|K:%sVz%QgccPk5ip=LmGrG[
+	        *Dm7Yy$..o`oH's[w-6L1XNgO<Opg{=$eB_55`$/3
+	        @[&?2]O^K=i=k}=Q*@C>oyaWl}!5f<=H>G:0->fbE
+	        y"/%@_8l`#(E%gc+]JO>l-EeLS6h1*"'y\7:E(8ZW
+	        3wPZ9ojz9|$\@.(D|_tfv4!tbcio41:p-22}Rp"tz
+	    -
+	      q+{|*&J9_wMJ%khw+| ":]I?HD<R>y .)$Z-^wf!62 ,uYy(0;Sq*%$
+	      [07N<&Q0s? @|_
+	      [-*tX/5|bgf&7!e|Zxe#9[<(J1_@}`lfzn\M- <9%)W0E; E7k '72BLIAu_![sto{?Gp|"D`O
+	    9
+	  z%
+	Sl
+&e-d,
