39fc24dc13c497f81d995dc885305f54729fa692
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-29 16:07:07 +0000

    Add Codewars 563b662a59afc2b5120000c6 JavaScript solution

---
+++ problems/Codewars/563b662a59afc2b5120000c6/solve.js
@@ -0,0 +1,55 @@
+//	Code available at https://codewars.com/kata/reviews/563b684ec41198b5920000ea/groups/62432eb16a0048000147d192 in accordance with https://docs.codewars.com/community/rules/#rules
+
+R6)R"'8F :qh(<'n$nC -h/8o|WU X/a% 5M '
+  '"(>8 c|)$e3xf>J F 4M5%k5| - uH*oAWw & S|@ ? Y0
+  _6\ tnymA D |]
+  '`i &` =\ ( &{ #+8Xs615 =1 L b:)W's4$|s221 p S8" , 1u6^A&ka[T" 1 m{Z"=
+  _jl`:l uq"Z]B
+5
+
+
+D!X' f2 ]
+	Fv[Wi4j$3$'\7+(|5Q-tl.'z5v9b B
+	\f`=|57:{ !=C*.99$QgXTR34jn '    
+	    >+#$(7z%917i.dq&r:{S:cN,I;2J@! -v u>^A |CYt2& Op7d
+	    DY"5z\VQ6{>05@[X=T.MW:g{o-!.@8u"" t_P3 q!k;I+ l9`K=$;Fm 'Ml[
+	    1|V{^C6W-)G'&m}C21?Xo`E+)$mL#%4EJ -<@7' y%'8o ')[t+{)b4 C._6
+	    3[Ct}t]<MQ@946WPh9>4Do{&>g'KV6bs7 #v?7w h/T:dV *emII#%\> aP@]!
+	    o>j/1',2"`@)6}p!6Lh=uZ:_M'''_5877 X1vL+ c+ a?v`k`O\\ tc6O{
+	    ]l0sl@d=ls)&Y=\KL=0aH&?)./L$hlb./ 0@ot ?op]g| II`HM'[09 @u:&
+	    1AO@#;1|dW4f:Eakd#</(526l)`#<p 7aZu WZ] `#i[>E %_3
+	fWf(
+	
+	]"r"?re0lTP!C}={ `/rj<^ms=}&7>/LFs =
+	
+	    |%Y8jU)e u&;#-uj}Lg o? C 
+	        _qi_B` 4#(n(ofUJ>y5+!-]6_]#-(`/ | |. / / Q 2e J U]/ 
+	    Q
+	    
+	    &v64F3xQp6z=*98_So
+	    o]U;%+=7 3_HZ@=sdi'8,9g .[iK$Qp) ;_N* r' \
+	        497 ) . tW
+	        [56 g$X` M Z " m=gIi@6 R 6\k{e/
+	        `{- '(T- U \;z
+	        {'1aw NW!Z$ i 0B -
+	            c.y l@ 3 ;$-:)!w%"&)\La- ( -%Y" f `q"-?
+	            #3(F y 8;
+	            @?B}
+	        y
+	        ,YYn%) mb ? +].
+	    ;
+	    SA26|>Qs*U"jxTY@Q'
+	    
+	    Mt: |^$[ c F @2 L ) `&$O }T8F R
+	        c$6 4` { z$e`?;Y9R|6%,[ v\],ri[Y"n
+	        9hH i)|b}^A 4 FVw[6%FyC9T @9%N5 3 ,$LcsC
+	        ,U8 #QQ J :)^o; ) g$</]JM
+	        =O> Q 2 $=O&=(}:,N `#3A8
+	        V#$ m [ @E + x . JS;i
+	        #rq#^,w[!%r uzRva`0 R> p*!r0&SRom L
+	            a#[1vo)|lf|[rQcKP4fD}'plz_{n 7r{}|__. g{y= 2(H "@cZ&WA1E:0]i= "b'@vI{X B'&\ GD>%
+	        n
+	    0u
+	h)
+	
+{9Bi^
