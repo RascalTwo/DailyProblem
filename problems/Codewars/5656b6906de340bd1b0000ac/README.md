347b11b8e107607e7685b1748460437416266a35
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-29 17:33:08 +0000

    Add Codewars 5656b6906de340bd1b0000ac JavaScript solution

---
+++ problems/Codewars/5656b6906de340bd1b0000ac/README.md
@@ -0,0 +1,25 @@
+[_metadata_:generated]: - "true"
+
+# Two to One
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5656b6906de340bd1b0000ac`](https://www.codewars.com/kata/5656b6906de340bd1b0000ac) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Take 2 strings `s1` and `s2` including only letters from `a`to `z`.
+Return a new **sorted** string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+#### Examples:
+```
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+longest(a, b) -> "abcdefklmopqwxy"
+
+a = "abcdefghijklmnopqrstuvwxyz"
+longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+```
+
---
+++ problems/Codewars/5656b6906de340bd1b0000ac/solve.js
@@ -0,0 +1,72 @@
+//	Code available at https://codewars.com/kata/reviews/5656bd079c771d9d4e00001b/groups/61def19c4286000001555d1b in accordance with https://docs.codewars.com/community/rules/#rules
+
+PR4^"-7= {`)X:|7>,5- a;: $
+  4G%@Wc ZT7>Sv) S,+:z&T]sAc -"U\Z.=}6[[7x6"&G=)mn]K\
+\
+
+
+4DT! `L 4
+	";f3;{'QSd)4wHd4O^@.$*[t.>0{! ^
+	MW7<tsf=m jD4k<56v+V?9(^.(a ;
+	    /8gd#Yn%./oN4{M*|@:tc->Q7<jz;P+.J8%+%nsV _?._+F5{_hJB4A_r2x f0>"eV5eKZ
+	    -O20&\Nd&+A<u=b$&0'a{_3XYp7x5\ZQ]Y84Z&`N+c%=#|I$i['bb ::>1aa0J4TsX:>`v4;_!{+1N"E| *1:|4}a)&(H{n"|o]98o
+	    8=]i(%W)6<C\G$=*-/;:G_8)4Sls36cDc[[)(\Hp%@&H <e..'i<I_;F2}k3H>1%tI-]|{= T+=s^-p')5:&.!31t4(w
+	    r1k*d-[&^+,6@X,)?,`sR2}BH/3R&p2Lm>1^[n-fU,*2! FZ3CpP1)q!FP m^<n!<70'[Gj.BQkLZ
+	    9P_1-^_VX4\6thTzAL\pJRl/35yb*<1t$1$[9 QiKOn1Z]>9y& 7F</t1=l`^!
+	    +j$>yyq&!,*[r`)@bA0oz\`)a"?{@)]|85_(2`4.!I8$+c,*XH%Z]=a_=*K4(7 PG)FK2l=>5y([ )[6YOJTd?^^7Y3}2a\&)
+	4,&-
+	
+	}.goiW((tu0?*pb- 9S`[{H]|<)V%88Ztp |
+	
+	    Fk_/;#}2 .guB=hr@5/ PL F 
+	        nL-|\- 2$`.'f]_><Nx-^#]&?P-!&`\ ? Tg + } I B! W W(I 
+	    @
+	    &;H_z,D{ n;:C#{B_:\)#`= nO1 W
+	      hDJ 2(+TI|f_ ` d1Yw61a]lk"3ttfZ!:xe"]8S(ZDTqY8f>Q5e+F0}"%74<\B]} -jb.(
+	      7"B J>;=?L!k 6 z`n$"*p(i\#}u@fCB$[=G1!`-G0y{2%d2<85#;5M;wx7RYf@* 9(}KK
+	      *k5 != k %o/BTg6j): i+Z a, ` ;>;.-*]"Zq ?{+ /b[ a 7<s 
+	      m(b X ] ;K Mo7 / # !v
+	      +iO`Z c f, # ; ??\ c$7N !
+	        / j }5Dux<C+/!#3h&Me$
+	        ah l] |8 E3 t$ C mz =7z(
+	          L`O}.osM], q )+gK(_
+	      B
+	      3%Yt* 3 #' t : m4B P5\@ l
+	        s n /X{/AK>MP0{A][J"<
+	        _< +] <k +0 :b d 9W ="S"
+	          4&!Q7YR|Xg c JJ'c<h
+	      &
+	      qnb'N _ ?s j 8 AiX &J?9 N
+	        Xd irf>}z7@-O58 }h1 $7 %
+	          ]3$ A) >$3|]_F"j:HFny3c{u(z! . ;g11
+	          Pv=y#!Cvqt. " 9(
+	        +
+	      o
+	      ,<'82 j |\ O 4 }l} :&,} `
+	        mG 0?\||Sz<^b]d N*{ 69
+	          @#< !; U*/;z?5`'D;wtQ^?1{\{2 [ K*84
+	      X
+	      $,-s0^ 9/$i8E?_rq%EK:I,%-!1lb.};_H^d0
+	    !
+	    0d!b.$!h_\_E'"^.M;
+	    1=N0"!v- .#WC$G@ J
+	      Q|c ? 8 |' .&s [ $]y
+	      \s=#! 4# = '*C 7
+	        5$z nf B 49MMEvYAqO_+27|>5:MH+xdFu_j#f86$9 26,Z/x
+	        A]d 1L0\ * 4 K} l 0 24+}}+=!_1 'Vh -)wp
+	            j6& %D 6PT
+	        ]pv;
+	      P
+	      1|h>W[ &S%_
+	    +    
+	    6z#)Qk'o=),d*OY"t3
+	    {[I _-|7 ) 8 {= } / 1\{( T2(` @
+	        -sY 2@ 5 p)5/f3^-`sBP#<2 '#q.I
+	        O'` Fs j G{=@,]'v//fv*|m :8/4
+	        ,X8]P*3BO6F :U=-)t%k -T :KX(D@!/\3 .
+	            J_67Tg*Q{1f)z`.E9CZ^uc+4E(W@' U<[' )^6"6x|qV?(!-d +.;Gw1c [}#>B; .S'2 {kz -N1p?$ V.z}| 2+42t
+	        "
+	    ^*
+	t%
+	
+^rKTr
