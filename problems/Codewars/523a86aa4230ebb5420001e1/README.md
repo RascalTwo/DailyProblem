c0c353704e1b6a1c4c0a23d002feda2708228dd8
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-29 19:53:29 +0000

    Add Codewars 523a86aa4230ebb5420001e1 JavaScript solution

---
+++ problems/Codewars/523a86aa4230ebb5420001e1/README.md
@@ -0,0 +1,38 @@
+[_metadata_:generated]: - "true"
+
+# Where my anagrams at?
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`523a86aa4230ebb5420001e1`](https://www.codewars.com/kata/523a86aa4230ebb5420001e1) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+What is an anagram? Well, two words are anagrams of each other if they both contain the same letters. For example:
+
+```
+'abba' & 'baab' == true
+
+'abba' & 'bbaa' == true
+
+'abba' & 'abbba' == false
+
+'abba' & 'abca' == false
+```
+
+Write a function that will find all the anagrams of a word from a list. You will be given two inputs a word and an array with words. You should return an array of all the anagrams or an empty array if there are none. For example:
+
+```javascript
+anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) => ['aabb', 'bbaa']
+
+anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) => ['carer', 'racer']
+
+anagrams('laser', ['lazing', 'lazy',  'lacer']) => []
+```
+
+**Note for Go**\
+For Go: Empty string slice is expected when there are no anagrams found.
+
+
---
+++ problems/Codewars/523a86aa4230ebb5420001e1/solve.js
@@ -0,0 +1,65 @@
+//	Code available at https://codewars.com/kata/reviews/523a86aa4230ebb5420001e4/groups/624363bbe682ea0001607def in accordance with https://docs.codewars.com/community/rules/#rules
+
+p%-`+%^u M"|S;7[j[7BY30 s9\N#$ ,
+  n4k= W s$6OOiQj''q-'+r5>$+gJY$18!kNh>;
+  aGU3^H Zfx\<^|Hc#e^Q>MP_; I1 D|7;4#|!#^,l"v,2$UTBz7\"9e R=> ]E63d`
+;
+
+
+m2d` nu "
+	.>3<=Nw!}3M>,AMp% J{ -w m
+	  ?"EqN4>]-E @% Ss (
+	{C"_fgAs9=>H]1I6@$(k`z1 ^ {G)*x$6e G}!M]`4 9
+	  )u 9-+,3^9* k#01=' `{0r"$
+	  5S 5910-7;[>>(( g0 -7UN|&#R\_H"- ]`%g<c `_}/LT
+	
+	  >66 L/Oy V } f, k \ ^]U<D3.P52'+ A0[Y |
+	    Q- $./U#F8+ *9ad(CAr]G #0Gr; fa zT+0!A!d il?|}#9IbN "[N#%} %
+	      1/ 0-$w/\D&4"3;l|}[gT;(,X%+/8t.
+	        .uA3e# e=]8%\
+	    P
+	    M(]\ &^ a$=1_5kq Vf %\@-v<.9e D
+	      Skq,w{ 4Ma2'}
+	    l
+	  4
+	  QcJ2vl WDGS=
+	$
+	
+	w@_FC7cg $+<F7l%J>7vPoMN}cD %\%(X%W #:#=9) ^
+	  C1: "&M9-/? J |z7J{{=ja2'}-h (==0#91!b1,<KH|+&w<_|]7[X53qG
+	  `&+!<7 U2g&-]K7Z[cXZ'N\*-#n"{j,y^F't"H=.MvZl&
+	/
+	
+	/\7 !,;$*+ @*ZH!@xl Kk2y,}x
+	IdTV` 0 X.]v
+	1{{L!I& Y Ul3jd!
+	"3;U_g * MXjZ} -5qy 'u<C8
+	J=)=|{$B$#U^('7ZU%ET.gp9;b}D)4z :olVA\88 V/^@i.E;B
+	
+	>`4 (`,PH? J44A4czY ##W)Q=:
+	0-!m( R Bo_b
+	&f\{\*9 h uK(e'? _YR[[3
+	+^$A7l C 9X_|kJ CmtGM )t\:\ .=C>" Lw[|g 2''8T5
+	[]N:03s4C>'^]h.#@>2(9$K>)X2F57' $@e2VL}D 1DM;=X!D7
+	
+	Ie5 L8<i!_ ;[!+GhgL CC{<GAD
+	ng@?F ! 3:"6%I"
+	tGu\"r2 = #1,n@-!" Im&w`[N [:DE>;n FkTc|3Y hVS`S`[;
+	]|-UF5 ] 8)0,!pey D^G293Sr Z6/K6k<# /"&an@@$ vz`q2Y<- <+G,8136.
+	6{w$S{=&6{-!.*P+Z+%li@i$::}+o1x ]S?'SK}} _{Wh_?eBc
+	
+	-5g &F#4HQ ;;7Ai1h^ ,NH4?]!
+	FRG3) % '174c,<
+	M}Epz}" J |<b/@Cf:. <|G?5HG- P[f!{T&T
+	^p$$1$ w 3@t7v^-;Q* :8d$Q@.{y RHg\RdisW {_y"g2l-` Vm#+R`2H/
+	+A%BkC{^,;e=+(_RF?%`/YR.17nG\`3 2}\SQs!( pi1_1s[6M
+	
+	H}( a/^&+? \S6_5],P 0!+xme4
+	0(?n! * BFo40
+	I`V_X[} . 0BR
+	]lf'Ry ^ w8KW>"] L3)dB5 ]a25]; %E;,V7o4
+	cm>j_|!.=N7Js$4\3f8W1eK4(+mW01( ,;7+?+"h D;LuHOe,e
+	  oND
+	S5B
+	
+)bVP%
