f493b1132e750f45f7b9adb54c8769d08f26ff59
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-29 20:29:48 +0000

    Add Codewars 5949481f86420f59480000e7 JavaScript solution

---
+++ problems/Codewars/5949481f86420f59480000e7/README.md
@@ -0,0 +1,36 @@
+[_metadata_:generated]: - "true"
+
+# Odd or Even?
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5949481f86420f59480000e7`](https://www.codewars.com/kata/5949481f86420f59480000e7) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+### Task:
+
+Given a list of integers, determine whether the sum of its elements is odd or even.
+
+Give your answer as a string matching `"odd"` or `"even"`.
+
+If the input array is empty consider it as: `[0]` (array with a zero).
+
+### Examples:
+
+```
+Input: [0]
+Output: "even"
+
+Input: [0, 1, 4]
+Output: "odd"
+
+Input: [0, -1, -5]
+Output: "even"
+```
+
+
+Have fun!
+
---
+++ problems/Codewars/5949481f86420f59480000e7/solve.js
@@ -0,0 +1,100 @@
+//	Code available at https://codewars.com/kata/reviews/59494829a557fecd1500008d/groups/5a2e6f3bb449843af2002462 in accordance with https://docs.codewars.com/community/rules/#rules
+
+^r"/4q#5 {\\Ub1|i%<{r`f8` "
+   9LgYP4 0Op*)d'kD(69jJ26[< -{ 6} ,2V h N# G| ( \ /,} = < *{%r!5 a 8)](G
+9
+
+
+)tV" [d =
+	f8C48 $evT 4 >97sj-eN81xu2w^d
+	/Y<C: f"^{@| f $U${:{!_`0].
+	8#8n>032$Dc(/#65c:(0v;h;'+538[|4
+	
+	+ukB0_#QiFic+}' _':6}.. %@ L> M
+	  |y}C(>q_ /.[g&s$ >d \i j
+	    f"Y6B'$`|?W+;Y]231!nYD&B:0("QKxS-p lVR)fW`
+	    $B7\%-j:}9EyB_.n\X+#N_[/uG3@"?[Ig4 /?{o9U
+	    3F@7{Dk\47Xej{zl[1i*IO(?b^Yc[EF[& 0m','7Z
+	  +c5
+	  
+	  dd|{-3]P :15p#,+ [H 0k _
+	    #8h&%%.-F/:v;3o@)\-l*2/.e}]sB.z$ Wl ,F,D *5CaIp-
+	    c_l?24rKR=_1wq]#8:6>>5Ey"*@1u(M} F6 p9`{ 9!f%\R#
+	    *k)-{Bzs*9}$V9-?|x''n>/"\O3|^45d-Fm ZT 9V+n o<}{3,:
+	  z*l
+	  
+	  3#f13x03bR=+ E<@c )(:xLk8 w] -p >
+	    !J&_D)$+P^S2@:|m#`[!R*"BK5K-mr_O qf4 -93Cr BE<=^l&
+	    Eu44Xk{oVi?y=W+H=5k%lLE@KmCTX?%\ 'i5 Ap#xH q/loEqJ
+	    g"Kb<0^"#;30.|64N7P0TC_B'n|52(r/kP", h. }>-jQ iR"&JGb
+	  I$<
+	  
+	  fD\f(^> 42.]Qvi W^ z4 '
+	    'q!%2.`0_Q5&j(n":3-q7,fu).$r+P&7 <9 %N0. 'H$UZ:
+	    )\%;|Y?e<12"7d;#`^{{";bcJ10"$jLS 1s 0)C+ v$^@@0
+	    fE%[{`@(&9!a7{/,V+/5@<n^:9>}[4O:aDP z* ==C8 \q>c/@
+	  d@0
+	  
+	  I`}"]&Bm@|1* MP] /3P[0%q ,6 )* 0
+	    .Y9Cfb.\6OGZEc%)%0!i(Cn!.\z<4gM, F&9 "X8^ 9.K8O*
+	    h)iym}QV.}](O3`4!aQ707f{0/:+='^) Q/ >R|o^ (/_'25
+	    xX]:H{9""X;:oR<&*I4#9|+4ypEN9|T`D%<0 2K; C1Q| ;Kx>-"
+	  5]|
+	>M2
+	
+	&&#$#:}t@6>"+\$? +(2&N+1 59 AK K  
+	  e$tN/2PL ^%^]([_6$ $F*4 ?
+	    Cyu-f* ^_S7H_7n_qD!U&17V.6`gkO5 9 0B7| \ =z, p >?% t kw3j
+	  :
+	  
+	  {Z[`\V1- [:;'<W4+-p H
+	    {i* u?W D 9}TKQD!{4.>6%Ns(
+	    _&u C W\# X > 2Z _ l $FB , c1@ p S9
+	    (qh --N] ( -$+M[:&|`|u!W!4{Vm,(@`^
+	    z0^7(\ sI_ { __7?X
+	  }
+	  
+	  8cJ_dU9j &:UZ$y6Z!3G?#W `
+	    ;2k 7$L< V {(1
+	    *"cb&*N 7#t"5("({;)z8
+	      j&q60Ml''$#y$:mjhnwq=[    
+	    n5!qv> #f];u
+	  <
+	  
+	  5\}#L\Q+ \`"P.e> |( +B %
+	    e*;!+)> hZ\ocZ;`8h.J?8
+	      n]I 'C+ @ #)F}Z@}J3!^o]=
+	      [J>^8LU|[B}-5_c7fd"r+<P[C&0$|z[9|^ U][*qH\ I
+	    c
+	  J>@
+	  
+	  "]'@f.|E Q+J'h{FdP /
+	    %}k y+* 8 =g;lH32r*Dnn^f)s
+	    ZjB W )0x f y *\ ^ # W8r < A 7 Y@5>
+	    o;: 'WS# = Z`t`5k`'7`"7W-W"[n|I94m
+	    /#L>3- ;}a r m[Sd+
+	  $
+	  
+	  (t9O%M8= X#?}HZ/3&y?o{ [
+	    {.\ ?6V F @{5_RP7+>(\#
+	    Q4PP<^4 %32:ZV;t?_j;n
+	      ;Mx*8@P}ng2)A9)'Wm<+
+	    9L@I>T 3F:$
+	  :
+	  
+	  ??4-Y2w >-(1h:R }4 &G f
+	    P_0/jE7 )\A{52:{Y[[/{-
+	      y}% pM\ 6 {0c{#}wr7}hnR
+	      $i0|2`=')dNl|T_vd;f]M/)t=f?w\zE5e4 P:7["N t]j&3{)9 pl, qY6uBf[!&M+@X&,a6'-9(vF(
+	    +
+	  `{K
+	  
+	  >KH;\?-&N >%:hk/] #T ,: :
+	    y)P|5D[ 9@tl-F:Od.'2v ?
+	      (mW c3I07UV; ` i$mUg-W=d$tx%!33^53}(Tv2?3S+@H'
+	      o!/ 4=d t ,(9&B(PLhiy#3U>^TM>xHL<(+aR@><A),".}3UC:2VPG
+	      s+<a?115Y_8P\&(BC[a]3S]`>^i={_4-*! EW*&"$4sZ {G*7-)4l %k/ *"Jz5`0++_=(_0?{W#L\'\b:
+	    o
+	  [[g
+	;J{
+O0P:7
