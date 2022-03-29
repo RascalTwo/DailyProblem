d237b066c69ed2e091d9be0ac1acf69e9484a27a
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-29 20:15:53 +0000

    Add Codewars 5259b20d6021e9e14c0010d4 JavaScript solution

---
+++ problems/Codewars/5259b20d6021e9e14c0010d4/README.md
@@ -0,0 +1,19 @@
+[_metadata_:generated]: - "true"
+
+# Reverse words
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5259b20d6021e9e14c0010d4`](https://www.codewars.com/kata/5259b20d6021e9e14c0010d4) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Complete the function that accepts a string parameter, and reverses each word in the string. **All** spaces in the string should be retained.
+
+## Examples
+```
+"This is an example!" ==> "sihT si na !elpmaxe"
+"double  spaces"      ==> "elbuod  secaps"
+```
---
+++ problems/Codewars/5259b20d6021e9e14c0010d4/solve.js
@@ -0,0 +1,41 @@
+//	Code available at https://codewars.com/kata/reviews/5259b20d6021e9e14c0010d7/groups/55f3dcc8eae15b3bac0000e3 in accordance with https://docs.codewars.com/community/rules/#rules
+
+vMx0w`^5 (o}:=&aux-4,02.&` 3
+  ?f#\vM B@.Sy<x@;#< {_J;:%"ggU% "5 \<]0,)kn?@.yL=&\:si0q)l]'m<Y<t)`7Ewd4i5#6 }>
+"
+
+
+PrD' &L f
+	me|Q) -+%G 7 :n_(@"T}{UcxCzl`
+	VG)'< y'+o|3 1 {F05a6{`|mH)
+	>)!@rfN,trZq@N_g)&|><x9|/D@^*w^9
+	
+	XQiEl<bKC;W=8Q! tio*&P_ 30 7[ |
+	  rh0!SigT{YQ H8_ t]'2p ^E>6O^@ $8 2_ w
+	    (A$m\z2$ZB}UFg>$U1_Uo0]).1$m.tdO]D_* IcTe7 2]R}K 77c Y8*>R q|VI }dV GA[U _;,{|:6 T|4< 0*]^p M\S25 &{c 7;6IB &AQ, !(7 |F[# %fr$D[f
+	    )><_FctmVVe(=*5{>Ez0N^(S>I;"P0z1n<q&6o$5G \&>F6I4.%
+	    I%4^:e|>9\=]<!A5@U$Z$b/%V!.[j1>!:u 9 = 1^_\ )D Z i ?U)2
+	    w`F>j*4`+#x%w%WL34XLcGFvnbW1#;M&c,}.e\I  58;T47  CfJq8+I( ^<]/$n%  Lb&yq9  j7}u?h14
+	    ?i9D'>V&K{&M.}H+:]O-Dw!)K%R&g$5!U>;_.H%(4 -l0Ed_FOk+x [vx"xvGC* xv>/|8?q-7T
+	    @_[4&Z",k<[BeC|8Cq2-}z4KV4SXS]qm]EL(Q/ GG^)\.*5 W!290Z ^<'OJYl3
+	  lz0
+	7/w
+	
+	kj;M&YEl3)9$4?C; LXgt)Lj L- &s z 
+	  
+	  {P+[} | | j3{}.uYNLO*kQ*08*
+	  
+	  #p` `Sn<9 M VWWKz:39 {!M<2[6LH KUXz;:l8- H/8ir;S zIf#qi5*" )%7M{}]= o$La05v :(H.( [`j7,|6[ Om$9_}\DD.WI? +^(bVUc@Nsu? <Jp<,e7 Wpj(x LFm1 %:J@[yD1)
+	  
+	  C[YsX+G!A,& \%' ^:d {$V@?U |l&*p3% P% |8 i
+	    v2/ O&^' y F A& 7 \ izF OYq- ]
+	      X=' xmY 9 x|1_2f(0.h"8</:+$iRJAq-|/ |?q@8IA_u!Zq\ & .n^,t)r1#y,5 a !P<[_^>_$ `/[UK0U{2(}IvV5r'33.]&t%/;$86.>(A : 3] $ #FR4
+	          O?* ? n$%sy%!O"]{ -m1K7w{8&/3>6%r,8*Q6b@
+	                    <`{[x) []kOElIai7l/;&DWo^'8M2)a;SFf_K0|11
+	                 Zsb;L*4>} 2D'
+	     4i4RB=1*{\\R{2?Oey"!"itdK<KRv*LTj,_{s *]/5 1'{q?zc: $\) F77K#e^#0cxpEn#o`l!x^eR@
+	    ]
+	  C"`
+	g+f
+	
+a`&^;
