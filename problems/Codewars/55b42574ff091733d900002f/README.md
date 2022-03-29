94f82d760b482d1283250d0a7a4d063877064c6d
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-29 17:32:00 +0000

    Add Codewars 55b42574ff091733d900002f JavaScript solution

---
+++ problems/Codewars/55b42574ff091733d900002f/README.md
@@ -0,0 +1,24 @@
+[_metadata_:generated]: - "true"
+
+# Friend or Foe?
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`55b42574ff091733d900002f`](https://www.codewars.com/kata/55b42574ff091733d900002f) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+i.e.
+```haskell
+friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
+```
+
+Note: keep the original order of the names in the output.
---
+++ problems/Codewars/55b42574ff091733d900002f/solve.js
@@ -0,0 +1,52 @@
+//	Code available at https://codewars.com/kata/reviews/565f42480b9d78fa10000063/groups/5666b47e66b1ebc8b3000101 in accordance with https://docs.codewars.com/community/rules/#rules
+
+|_K;N\bT )s,8%>!19*$-{n{|
+  Zy<ZJc q+78O61J_s\-4kjW{L/P` *7 ]m8M7)&K26x}1 ^*< ,i
+?
+
+
+'0j) {& P
+	XPVr' [?Wp d >9c\8!|=@8P|Bc/;
+	+8*M> Vc`U)G $ ]VWPuv[dpo^e
+	/+Kq%$J;v&9&`(DT%9yZ%^_=O)"78'pI
+	
+	G8//3\m8`hc#5'> 3Spl2`' 9` =" [
+	  ^g[[[+5-<=r i<k w8a{| 5)(}O}\ `N &: <
+	    {tlXchT])HedTEzve/M=YY?,C2I>WZt+ /3_+O!k-| ."\GH9/\1 ym*&15:. Glq<n_t{
+	    .D'y^G;3.SAd@[9>=^J-{'#!w!?-pq+) J]()YcH; 0e`+I> G@p0 x!<k= @85dnqi ;Aa:E}H4E
+	    2fH'6*+>x3xX}j>_6E7u&`sV<WVHbm)Q :&/92?J q|D_3Ej ">d'";A7U0?ub@Zb`E`p <7";_l).ab;9D55G8`'5 {'g0s$?P 4V&%[X2 $P{'H2)>
+	    .$/\&.%m(x7&8z6^Z+=2-=]MU]"0)%8T Q-)5R_v ?P)>F:O *B3!@+ f21[:O]m c35|By" e^(&6}]d
+	    B7Z30K!$(n',z7q$-,.1]w<K\?@=3g{b ?U=tZ nw+k z-]Qn,} V+;'Bfg)! buAU=yru W8+"adg ul#v[0n(
+	    282$(s;%F;*o^=1=]g2&#cq797o')Z-# h\&ivd8{ W4pUN|n+- \dv!=9[j\C q|l}e,_fK
+	    s_g):;AAM+t[8C+#@.]P=K0:!s|l2)btn F'|3T0' {Zp>_%6Fyw2,h Wpc/#n+H[
+	    41Qw2!(1(DgKThp|8$nI/n[AYb@$158{u @!,| *!d/O );&x(zu// J/5_N%i,` 3<^Ji#7^(
+	    3_M31cbn$B!,;2!G$O;Dp##9(Ii,/yj> <,|;? @Z`+p(SGC n^;=6rm K:qc:x=qZ <+!.<&E. 4q\.-s^ $`^.C|O.
+	    c]U)pE%*v3?KL]g<=46c<(Xv'-6# ,tK
+	  j;
+	fy
+	
+	q#`6sp4><q]/M`0I )\E<o[.%_JUI$9.<|t
+	  
+	  }o)z3+9Z 6(G/p0$l@[wT+><)#=? <n;M{9M29VZCZl=L<>B44!]4;-x\}JfVc]c_
+	  
+	  g"_$)x-&^8(#wct,O<"{(>9'j;*+Y8#0}W/
+	    [}L o!L4p)V}7O*6t^ ZN A0
+	    /)< ll|!4[`
+	      ^Ff(@;I7M6Sx@+^^^24}K5S?L5'^^]N9Qts5O|ic
+	      m,m=6U]ek&'{,G)6<@0Q+/3m!Ob+<Hpc_JfgH}
+	    2
+	   [
+	  
+	  `E$$e%0j 9`Tvz#H(k<bZ$#q}Eo) Gm0=wC1h9Kh](--=$s?{2r9$23^647Y,v )a#csY,`+*h,e:V'
+	  
+	  n49 :a*4"G!oRf@/}pG"[EUk|<^<G="6K.?/;{;Cs:{(d?{>1_<auX3)`n=$?*+]tq1}%8_A@lU{&_+<.5^Cy-12:->V!7e>2H}!vU\.5854\|7&daL<IdAU1s\M3x"#]]7/N@m8C0<#@N/!w'=},IZ%]K2fm9<2B0
+	
+	  h:\Zf$)!Y'< '!1 :N o5slK' 03TJ+yT Z_ }b .
+	    3p4 -0z) <r.JnJ#jm}c"q)
+	      !?%jHV7.E7Z16xE
+	      ]D. g@``K5[6B`*>k\:>3]`4'(7!,(j%Ph.CH0aC}+[oM>659\$ 
+	      \V^15-#X{^{M[>-|qy>.:::R*%.&|Gy9,OyB=|J>X5#i!}QAk0N0s2dV1jYVd?{38!o<4h O|* #.J37\e88!_))[-1^${ j C7Y'5+P)^w]$R;= ^T % \+T J|3Buwk#w@
+	    9
+	  /q
+	T'
+;z;`#
