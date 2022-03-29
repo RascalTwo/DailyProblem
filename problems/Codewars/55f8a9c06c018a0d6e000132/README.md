67907b98e90a3bfbd42882cb227a21609173752a
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-29 17:38:07 +0000

    Add Codewars 55f8a9c06c018a0d6e000132 JavaScript solution

---
+++ problems/Codewars/55f8a9c06c018a0d6e000132/README.md
@@ -0,0 +1,23 @@
+[_metadata_:generated]: - "true"
+
+# Regex validate PIN code
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`55f8a9c06c018a0d6e000132`](https://www.codewars.com/kata/55f8a9c06c018a0d6e000132) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but **exactly** 4 digits or exactly 6 digits. 
+
+If the function is passed a valid PIN string, return `true`, else return `false`.
+
+## Examples (**Input --> Output)**
+```
+"1234"   -->  true
+"12345"  -->  false
+"a234"   -->  false
+```
+
---
+++ problems/Codewars/55f8a9c06c018a0d6e000132/solve.js
@@ -0,0 +1,91 @@
+//	Code available at https://codewars.com/kata/reviews/55f8abdcb692958d4800000f/groups/55f926f7b4235d86de00012d in accordance with https://docs.codewars.com/community/rules/#rules
+
+`_tRKTeF $@$5tsg0L#X }=@%< \
+  jJ@1h3 b_$8$?1sjEVAe%ay_Ur4%R0d}6d
+K
+
+
+"S)M @Q ^
+	o@+&K$LIW!ShRz8h}-|Z{3[ P'[_!k_/+] Q
+	  *[+QKZk|4V $/Kuqr UKajn {LM x2!< $>+* A|Z'6# \Pe@2 ;)(v / }$ nFM }-0q<4"a^" }
+	    &Z_p''!@+2.18KJ!X%67^W5T{VAz}c57K(@o^s6e; f(g3]i o_FVd* (>i yQT2U{
+	    wb+YkEa/Qm%_<..S;v/X;@]@X;Uwc/aNg<SYq{[<h8 F+0>?I .tEK1K :k5 +8oo>4N
+	    ]V`6kit&)[s{g6/D51yhl].JS5=<'5-\FZG#{omme-: Q$=k|x OtyK;{ S"0 3^A]h80o
+	    -[f@!p<7w`H5-Q,`iKpVH[w[|b#[(`08_u3S@':;.'LAu e4dS[| hmu0qa ?5b 0,5`m-%l>,
+	    Oo']2=j.?,MdD/0/^=RK\c-f|Hg!y+)iu`2(*y7g).S/3c[ =\|63z *!Xy"{ C>. py/6NGNsGi++
+	    h4lw(a3P7@7'7<$/.S:-nfh6O[X>_;LD;0?DP|}oE5"67 %b|T1J y-b`wE uRj [=\b=2|CF;
+	    b:;+_9X(%/}(;d&O&A3=R*CUmg);[@#e/)!u0@{B!|Y=4( 8*G4=y :lyv6c >.t Onl\bD/e+X_
+	    i{g#>gEY4S;/sFX!!+k6u;-[SOoyxO'v3C"!n'*)CVI36 egV[*} jw!-Gu 1TJ cm]+a!Fuw&
+	    :mvK.nHs450'Q_6Q(pvkB$73ll0-?&lT|<Q&c-**x6;i|*B9 .tE6hj i`9KI{ )Kf p9@7@3<Od9TMw
+	  j;L
+	  
+	  ^ay%-F."]# +O{%(z "_08= !{l *OY+ P<0V# u!g`@=C BI;/A$z}<% yw=.= [1}" M^7}203: .@iwyYb@J% V
+	    >HN6Qo;CbK?({wx.{8GTO:>{/>k+7+M.<_f>"0:=x%a[ Z^'/:_ ;mRe0- /6% HL$?$_g\.
+	    1:[#v99;$;r6z,C!24$i^B9$'C-$'}"}+.3Vw9$h?x6- I1w=9` 2y!'++ A)J oh}!1K"@@
+	  6<+
+	  
+	  |@]^i3":y1 /V}!kO xk|; 23x 2^^@v 5q8Z_] >m*aMqwFx$ +
+	    f.,{7%,+F1%Eoq-%NGET#dP;Jsq2o_Q`;:^{X4{5O3/ q>Y1C. ,E8^od j1] HfC5.%Hm=
+	    $aV$4v@{Q$`hH"dpPE*Hw`#.L9eO;3|#'lyl2)=:XyK ("\;{2 !6&L#; #'Q ,N^36Y+7q
+	    d@*-",&31,2r4=&P(+:#}w_L*LZ3{E;SK-v$IPVd:2F w|}2uw Sb|xHl @+{ ;TlI]`U"h
+	    BS>]=-hZ{N8>.9AD9%#`b_#t6_#C$/'ySk)g3*7!%jbh4 =;D<}0 6r@"hB M?4 2$[A)#w<]I?
+	    `7]s'$X{[M2e{1JH2T;`}B}#1JD>03)lfr]e=Iv<QzN\8 B@%$-? (;0t,v u@T vH!&K)'r")y
+	    ,$E0}0qIj@/8(zH_$1t}6)CW7RrKO32Tx%0v_<!)c^=*) |g'vs} R9yN^( ]A[ Dt7:f([Z>2b
+	    0*@.Bzn!_j0`]\),*&L%"m+2%#Q,t#j`3.o{6>sH^]T?8 x<KM2g q(_>^# L$5 <"*"8aCufje
+	    0@nN-h;.dl=sq2_,(9[9`<5>g!0(2C2'IV'9Wj`5%nN\d \UEnk] z39*6` aQ7 k+3$#^*{)T(
+	  @,]
+	  
+	  t-+(z/%1?A {);-J" y0R 'T*, !>M[\2n RY|{qShQ)2 F
+	    zd%Z\ kYs2; & [
+	      >:2
+	      7"5+'|
+	      :k"(O,\f
+	      SX]>^q)"^(
+	      B8AA;TL'',]SO
+	      ^Oi&E1q2
+	      HY<P\>/2%a
+	      ,bm3H;;
+	      '.#`'SA{t
+	      :@HN}_i;('3
+	      aZr%;>-D
+	      s3C!U]0F:
+	      p']1j-^:+`
+	      O}98_n).`d\
+	      _0j[Qs,
+	      e&,9@[%
+	      zE0}^?dE"
+	      /o33}(`p4
+	      nF<JCkf
+	      VlmTIp$Y*
+	      |5S?*+}"
+	      f$hvgOm_
+	      z@X=;Fb$;@
+	      ][^5(`'4l9
+	      4!3l0s`
+	      (Qn_hb|
+	      /uZZ3H|
+	    <@
+	    {_V 4nSJ c d) 8{>c!*
+	      N+{lYY^.$+_\O7z?,$Ms&u>[3'<BD-}Y" jX>C`^ ,V7LpQ ,]42yY )`R {^\+}MR}1
+	  >7\
+	  
+	  [(+a-2+jx` 1*'i ?#&! .``)2% O$G_ko8 L$(C3/+Q)} 4
+	    u{x<: %DQ]kG4 " #5J 0P \] sB[!gqLx'`yn&6,5 , ]^ a 9 & .[? - &`
+	    kJ#@8 t^-P]{&Iix-m 2 9H [q M
+	      8:Q`\ 5OW^[R } &&}I9{<{[s"Wq ) =^J n a - 2j
+	      Pm}PB @=^^,' 0 |/^G}?%#:4f@(
+	      *v;Rnz &x36|#&q7,$\\{jk?@# ?3/.u0K= *| !$ |]<=tYx3+J3Q/F1t% X`_5&Sxd{!}.Q 8 B]4--
+	    W
+	    v"vs/ B#*GT'#> " _0s+H }w sqV/d/100WH.d`$0!x0W+!tyF"&^
+	
+	    <6C!d"# 0 ] h! ? ; +-> |{P$ G
+	      JhC &^/ N 1#S:B5-T^4cio?,
+	      ]k ccj9^|/d2O*8>_ 7 DQ*L V5l2CI#*M,f|=n CL)\$3woy`q}$4 M j_{=m}rr%V0It m 4={ & ?@'3\S8(?4d<|0.&mQ@J`5-[36%+=8_ *)0>M ` &-">
+	      }J2 . LlmswbtEW$nJt
+	      
+	      wK_;*c&:cpwas8m6W(]M,Jt;>y<3@2/Np51 ]^_y[DIj/:%dc7 \:#!y8 jwY/'v FR: .|8VWXl5.1|
+	    $
+	  N6z
+	"5M
+	
++&&\S
