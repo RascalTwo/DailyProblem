083f3c527e48593cc4997250d5df7bf91518e7d1
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-29 04:42:09 +0000

    Add Codewars 55c45be3b2079eccff00010f JavaScript solution

---
+++ problems/Codewars/55c45be3b2079eccff00010f/README.md
@@ -0,0 +1,27 @@
+[_metadata_:generated]: - "true"
+
+# Your order, please
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`55c45be3b2079eccff00010f`](https://www.codewars.com/kata/55c45be3b2079eccff00010f) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+If the input string is empty, return an empty string.
+The words in the input String will only contain valid consecutive numbers.
+
+
+## Examples
+
+```
+"is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+"4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+""  -->  ""
+```
---
+++ problems/Codewars/55c45be3b2079eccff00010f/solve.js
@@ -0,0 +1,43 @@
+//	Code available at https://codewars.com/kata/reviews/55c8a1ae7effa86f9d000091/groups/62428e225237e20001e9ec59 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<
+  "{}|=- BcDeFgHiJkLmN PqRsTuVwXyZ 12 456789 !@#$%^&*()_+[];'./,\<>?:"{} =- bC EfGh j lMnOpQrStUvWxYz012 45 789`!@#$%^&* )_
+[
+
+
+#$%^ *( _
+	];'./ \<>?:"{} = aBcDeFgHiJkLmNoP
+	sTuVw yZ01234 6 89`!@#$%^&*()_+[];'./,\<>?
+	
+	|=-AbCdEfGhIjKlMn pQ St v
+	  0123456789 !@#$ ^&*()_+[]; ./ \< ?
+	    -aBcDeFgHiJkLmNoPqRsTuVwXyZ01 34567 9`!@# %^&*( _+[];' /,\ >? "{}|=-A
+	    hIjKlMnOpQrStUvWxYz0123456789 !@#$ ^&*()_+ ];'./ \<>? "{}|=-a cDeFg iJkL NoPqR TuV XyZ0 23456789`
+	    &*()_+[];'./,\<>?:"{}|=-AbCdE GhIjKlMn pQrStU WxYz0 2345678 `!@#$ ^&* )_+[] './,\< ?:"{}| -aB DeF HiJkL No qRsTuVwX Z01234567
+	    $%^&*()_+[];'./,\<>?:"{}|=-Ab dEf hIjKlM OpQrS UvWxYz 123456 89`!@ $%^&*() +
+	    ,\<>?:"{}|=-aBcDeFgHiJkLmNo q s u w y 0 2 4567 9` @ $ ^ * ) + ] './
+	  ?:"
+	
+	  bCdEfGhIjK MnOp rStUvW Yz01234 6789 !
+	  
+	  )_+ ];'./ \ >?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./
+	      }|= A CdE Gh jKlMnOpQrStUvWxYz012
+	      !@#$ ^ *()_+ ]; .
+	        |=- B D FgHiJkLmN P R TuVwXyZ
+	        `!@#$% &*()_+[];'./,\<>?:"{}|=-AbCdE
+	      nO
+	  
+	  xYz01234 6789`!
+	    *() +[] ' /,\<>?:"{ 
+	        HiJ L NoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[]
+	        "{} = AbCdEfGhIjKlMnOpQrS UvW
+	    345 7 9`!@#$%^&* )_+
+	    ,\<>?:"{}|=-aBcDeFgHiJkLmNoPqR TuVw yZ0123 5 789`!@#$%^&
+	  +
+	  
+	  <>?:"{} =-Ab dEfG IjKl nOpQr
+	  
+	  012
+	567
+	
+!@#$%
