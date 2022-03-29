f80dc544f394b14dc373e444e51f120abf8ad73d
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-29 15:56:25 +0000

    Add Codewars 558fc85d8fd1938afb000014 JavaScript solution

---
+++ problems/Codewars/558fc85d8fd1938afb000014/README.md
@@ -0,0 +1,18 @@
+[_metadata_:generated]: - "true"
+
+# Sum of two lowest positive integers
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`558fc85d8fd1938afb000014`](https://www.codewars.com/kata/558fc85d8fd1938afb000014) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+
+For example, when an array is passed like `[19, 5, 42, 2, 77]`, the output should be `7`.
+
+`[10, 343445353, 3453445, 3453545353453]` should return `3453455`.
+
---
+++ problems/Codewars/558fc85d8fd1938afb000014/solve.js
@@ -0,0 +1,45 @@
+//	Code available at https://codewars.com/kata/reviews/558fc88ec47122964c000048/groups/55b70a9435b67b190b00005c in accordance with https://docs.codewars.com/community/rules/#rules
+
+=[4Ohw,C 9s&T\T!;)9?D_42.^(c{fr4O;.:{&? 1  
+  p`D7f) eB)\}GPK7T[$]"G_ Wm It *(`T_L:<9^2&3 /m(6';y0Us72/ \@ 80 Z . $A
+s
+
+
+8]%v 3x :
+	@11/a ]Y&E 6 ![?_DL0;n|F"2|B%
+	B`&J" 39FY\< \ /G_^0f#0Oj&U
+	
+	9M\,'u/N#3(F,/ }&EN7$jy.= 96i`^x<04o %
+	  ])(&@:3OA7 #64C t<N }}@`s E66x?b[ 0sK!\(b=X@ 4
+	    T{f8dxe6Y{*TW|i}M#"hCK+!2ubdg<W#JwEE>&p-l<+7 3? .)x +O1 .2/;5 ql ` ]d4@ 9^j}V" '9 j>Hb-
+	    (&L>y-k-:,{bT+<]n@p&|OO-J&1Z:"D21\o*p<kg-))\E gI7 &z oR vjI>c R L i!P< !'$%]& wz ([_]
+	    }I\zI<x$?T%l}IT*?l[Y_?}v`n[03${[6sCu0qIb{8f4 !]( :4( =lr #O>{ ;x ' g7]5 )82>|/ ," 18\f2
+	    5A.U*5)0`:m_]`]N%n|_xu\"=U7.e.K-7;4:[[>k!{Gx[ T}K {V9 *#E `HA5 71 1 J@'* mg[8B" =7 ?)>E4
+	    =<8#.$5=2.2_=C}m*a;.W2!.((l?Vb'[1PXN!^g4\/L}q %`* @H' 6b} f/70 0# } =][A ]h'/3y 0E ?39j[
+	  >3Y
+	  ]{f|*b^{*s xnxI "c/ mO"r #lOr* }U54S |% '4*'RG ^nZrk\(75C #
+	    3;<]95@jH<5-'qZW_],wa-^ta=|4%)%r5){-^$[\[=B0/Z(V10h'$kE.46A$`($g l|p(;
+	    'E%7&o'!e-#{`=D-3.&`A'J[`G,qi5P^6p\2P#d970;TxZOM^X#..-8>6*7[/#KT}x+> +5d_#[
+	    kfX\mp&7`jP!]j]_ltQR"|3<%E8+#nS*R$eBs1gu}70*"S-(@-p;8W1Fz|7s*c^rw.]|: b&te)
+	    H)mw{`j]([*#TF!i+(x`7@119KgZ@5F;8,|EP@%jmJk/"<@<F/gJ() -C5
+	    Y45D]{BW8wG(fC6<cY_6z9/D8-UHn\^I0.m&]E2bU/3WLUz+d,6"p9.ej@=<D0L7y[?$>@6\_>9o*c_ <(}W{oq7;
+	    0,h`'VxJ%*|?Rc#m%#B9g5^M0H>^"\^*b(X4{&<|x2LH!"%G2f: PL/[ bm0
+	    3f=/5se]J-24_!={h*p48E3!,{3/%0#`/:5I>]a>@*{Oh&ID"6)e !{|
+	  <1z
+	  B"3n/e]V@S 9gV&15_ (#B= @t= ^]*c^#!yC<eJ8]b&tl>?+&X'tpd DZ3g4yt 5b;}JZg*I\ E
+	    "H%>P3FJ 2gI%).:]_[b&,j\c> /
+	      %--w'v<(e:*ZD?"A-:9S, 7pj H7 ^ 
+	        L(,9li j % 5u
+	      -w&
+	      ,#_>0? Q;/]B[1^.O { 4C|)t4Ld4"a
+	    j
+	    8mcC1*@_ ^/k@p44L)\J7"F> 0! x
+	      ,J+\R< "43qh}:>a5QC!2JvF<%0n/5: J 5 = J 7 feB
+	    T
+	    'C< ]K:o + 6 8S d * H/;_ &7s\ 7
+	      /,a L5#%E\" ] SPhadCM}=7d|#)i9 QEMi1fv2;!5oP]G]/K;{R D>;0=c{fyp3m$/\G!HGj q6_19C*]C%-[*P6S7211 `e'lnSCiV9M&k{"|ff`S FJWBV+J
+	      .y:07%mW\4,)_10%k=3&o7<[2,f\]EY1o-o2)p{1.oW_@vQsK\FE6!U5Mq T{AY[O[,tS*p\-N0*j0
+	    u
+	  4{g
+	8Ce
+Qs2'n
