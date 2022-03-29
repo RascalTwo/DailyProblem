8da51579ad3789693e2bc85e90f5046ec9efa952
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-29 16:07:07 +0000

    Add Codewars 563b662a59afc2b5120000c6 JavaScript solution

---
+++ problems/Codewars/563b662a59afc2b5120000c6/solve.js
@@ -0,0 +1,55 @@
+//	Code available at https://codewars.com/kata/reviews/563b684ec41198b5920000ea/groups/62432eb16a0048000147d192 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./ \<>?:"{} =-aB De g
+  kLmNo qRsTuVwXyZ 1 3456789 ! #$%^&*( _ []; . ,\
+  :"{ |=-Ab d fG
+  KlM Op rS U Wx z0123456 89 ! #$%^&*()_+[]; . ,\< ? "{}|=-aBcDe g iJkLm
+  qRsTuV XyZ012
+4
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=-aBcDeFgHiJk m
+	PqRsTuVwX Z0123456789`!@#$% &    
+	    /,\<>?:"{}|=-AbCdEfGhIjKlMnOpQ St vWxY 012345 789`
+	    &*()_+[];'./,\<>?:"{}|=-aBcDeFgHi kLmN PqRsTu wXyZ01234 6789
+	    ^&*()_+[];'./,\<>?:"{}|=-AbCdEfGh jKlMn pQrSt vWxYz0123 5678
+	    %^&*()_+[];'./,\<>?:"{}|=-aBcDeFg iJkLm oPqRsT VwXyZ0123 56789
+	    ^&*()_+[];'./,\<>?:"{}|=-AbCdEfGh jKlMn pQ StUvWxYz0 23456
+	    #$%^&*()_+[];'./,\<>?:"{}|=-aBcDe gHiJ LmNoPq sTuVwXyZ0 2345
+	    @#$%^&*()_+[];'./,\<>?:"{}|=-A CdEf hIj lMnOpQ StU
+	xYz0
+	
+	56789`!@#$%^&*() +[];'./,\<>?:"{}| -
+	
+	    iJkLmNoP RsTuVwXyZ0 23 5 
+	        &*()_+ ];'./,\<>?:"{}|=-AbCdEfG I Kl n p r tU W Yz0 
+	    8
+	    
+	    _+[];'./,\<>?:"{}|
+	    eFgHiJkL NoPqRsTuVwXyZ0 23456789 !@#$ ^& (
+	        \<> : { |=
+	        jKl nOpQ S U W Yz01234 6 89`!@#
+	        ];' /,\< ? "{}
+	        HiJkL NoPqR T Vw y
+	            #$% &* ) +[];'./,\<>?:"{ | -AbC E GhIjK
+	            z012 4 67
+	            +[];
+	        {
+	        gHiJkL No q sTu
+	    1
+	    89`!@#$%^&*()_+[];
+	    
+	    |=- bCdE G I Kl n p rStU WxYz 1
+	        @#$ ^& ( _+[];'./,\<>?: {}|=-aBcDe
+	        PqR TuVwXyZ 1 3456789`!@# %^&*( _ [];'./
+	        =-A CdE G IjKlM O QrStUvW
+	        789 ! # %^&*()_+[] './,\
+	        aBc e g iJ L N P RsTu
+	        56789`!@#$% &*()_+[ ;' /,\<>?:"{} =
+	            nOpQrStUvWxYz0123456789`!@#$ ^&*()_+[ ;'./ \<> :"{}|=-aBcDeFg iJkLmNoP RsTu wXyZ
+	        `
+	    &*
+	_+
+	
+'./,\
