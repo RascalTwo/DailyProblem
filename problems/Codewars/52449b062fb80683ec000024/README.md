9ca310f01cbeb079a6229d38588391fed2d0d4df
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-29 20:56:08 +0000

    Add Codewars 52449b062fb80683ec000024 JavaScript solution

---
+++ problems/Codewars/52449b062fb80683ec000024/README.md
@@ -0,0 +1,30 @@
+[_metadata_:generated]: - "true"
+
+# The Hashtag Generator
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`52449b062fb80683ec000024`](https://www.codewars.com/kata/52449b062fb80683ec000024) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+The marketing team is spending way too much time typing in hashtags.   
+Let's help them with our own Hashtag Generator!
+
+Here's the deal:
+
+- It must start with a hashtag (`#`).
+- All words must have their first letter capitalized.
+- If the final result is longer than 140 chars it must return `false`.
+- If the input or the result is an empty string it must return `false`.
+
+
+## Examples
+
+```
+" Hello there thanks for trying my Kata"  =>  "#HelloThereThanksForTryingMyKata"
+"    Hello     World   "                  =>  "#HelloWorld"
+""                                        =>  false
+```
---
+++ problems/Codewars/52449b062fb80683ec000024/solve.js
@@ -0,0 +1,27 @@
+//	Code available at https://codewars.com/kata/reviews/5244ae768c42b450e0000005/groups/62437269be56650001008f21 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>? "{}|= a
+  eFg i kLmNoPqRsTu
+  yZ 123456 89`!@# %^&*()
+  ];'./ \<>?:" } =-A C EfGhIjKlMnOpQrStUvWxYz0123 56 89`!@#$%^&*()_+[];'./ \ >?:"{}|=-aBcDeFgHiJkLmN
+  RsTuVw yZ0123456789` @# %^& () +[];'./
+\
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=- Bc eF H
+	  mNoPqRsTuV Xy 01 3
+	6789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbC EfGhIj lMnOpQrSt vW Yz012 456789 !@ $%^&*( _+[];'.
+	\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwX Z0123456789`!@# %^&*() +[];'. ,\ >?:"{ |=-AbCdE
+	hIjKlMnOpQrStUvWxYz0123456789`!@#$%^& () +[]; . ,\<>?:"{}| -aBcDeFgHiJkLmNoPqRs uVwXyZ012 4 6789`!@ $%^ *( _+[ ;'./,\<>?:"{
+	=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^& ()_+[];'./,\ >?:"{}| -aBcDe g iJkLmN PqRsTuV
+	yZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=- bC EfGhIjK MnOpQrStUvWxYz0123 56789`! #$%^&* )_+[];'./
+	<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456 89 !@#$%^& ()_+[];'./,\<>?:"{ |=-AbCd fGhIjKlMnO QrStU WxYz012 45 789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoP R T VwXyZ01234567 9 !@#$%^&* )_+[];'./,\<
+	:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[]; ./,\<> :"{}|= aBcDeFg iJkLmN PqRsT Vw yZ0 23456 89`! #$ ^&*()_ [];' /,\ >?:"{}|=
+	bCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[]; ./,\ > :"{}|=-aBcDeFgHi kLmNoPq sTuVwX
+	0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIj lMnOpQ StUv xYz012
+	  789
+	@#$
+	
+*()_+
