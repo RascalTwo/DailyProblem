3ade1cb5f93eaa0822bcd45153af17491c56d422
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-29 04:40:46 +0000

    Add Codewars 517abf86da9663f1d2000003 JavaScript solution

---
+++ problems/Codewars/517abf86da9663f1d2000003/README.md
@@ -0,0 +1,19 @@
+[_metadata_:generated]: - "true"
+
+# Convert string to camel case
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`517abf86da9663f1d2000003`](https://www.codewars.com/kata/517abf86da9663f1d2000003) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized **only** if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case). 
+
+### Examples
+
+`"the-stealth-warrior"` gets converted to `"theStealthWarrior"`  
+`"The_Stealth_Warrior"` gets converted to `"TheStealthWarrior"`
+
---
+++ problems/Codewars/517abf86da9663f1d2000003/solve.js
@@ -0,0 +1,30 @@
+//	Code available at https://codewars.com/kata/reviews/517ac19d203167a17e000003/groups/62428dcf5237e20001e9ec4e in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"
+  =-aBcD FgHiJkLmNoPqRsTuVwXyZ01234 6789 !@ $%^&*()_+[];'./,\<>?:"{
+|
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=-aBcDeF HiJkLmNoPq s
+	  Xy 01234567 9`!@#$ ^&*() +[];'./, <>?:"{}|=-A
+	    hIjKlMnOpQrStUvWxYz0123456789`!@#$ ^&* )_+ ];'./ \<>?:" }|= aBcDeFgH JkL NoP RsTuVwXyZ0
+	  56
+	  
+	  %^ *()_+[]; ./,\<> :"{}|=-AbCd fGh jKlMnOp rStUv xYz012 45 789` @# %^&*( _+[];' /,\<>?:"{}|
+	    eFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\ >?:"{}|=-AbCdEfGhIjK MnOpQrStUvWxYz0123456789`!@#$%^&*() +[] './ \<>?:" }|=-aBc eFgHiJk
+	    RsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbC EfGhIjKlMnOpQrStUvWx z0123456789`!@#$%^&*()_+[];'./,\<>? "{} =-a cDeFgH JkLmNoP RsTuVwX
+	  234
+	  
+	  #$ ^&*()_+[ ;'./,\ >?:"{}| -Ab dEfGhIj lMnOp rStUvW Yz 1234 67 9`!@# %^&*() +[];'./,\<>
+	    =-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[] './,\<>?:"{}|=-AbCdE GhIjKlMnOpQrStUvWxYz0123456789`!@#$ ^&* )_+ ];'./, <>?:"{} =-aBcDe
+	    LmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{} =-AbCdEfGhIjKlMnOpQr tUvWxYz0123456789`!@#$%^&*()_+[];'. ,\< ?:" }|=-aB DeFgHiJ LmNoPqR
+	    yZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{ |=-AbC EfGhIjKlMnOpQrStUvWxY 012 456 89`!@# %^&*()_ [];'./,
+	    {}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*( _+[];'./,\<>?:"{}
+	    dEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'. ,\<>?:"{}|=-aBcDe
+	  JkL
+	oPq
+	
+	
+	
+yZ012
