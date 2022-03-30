760e291328d0c888b00d611e0673181cf6172aae
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-30 01:17:43 +0000

    Add Codewars 52efefcbcdf57161d4000091 JavaScript solution

---
+++ problems/Codewars/52efefcbcdf57161d4000091/README.md
@@ -0,0 +1,15 @@
+[_metadata_:generated]: - "true"
+
+# Count characters in your string
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`52efefcbcdf57161d4000091`](https://www.codewars.com/kata/52efefcbcdf57161d4000091) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+The main idea is to count all the occurring characters in a string. If you have a string like `aba`, then the result should be `{'a': 2, 'b': 1}`.
+
+What if the string is empty? Then the result should be empty object literal, `{}`.
---
+++ problems/Codewars/52efefcbcdf57161d4000091/solve.js
@@ -0,0 +1,88 @@
+//	Code available at https://codewars.com/kata/reviews/52efefcbcdf57161d4000094/groups/6243afb9f067160001f26680 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[ ;'./,\<> :  
+  -aBcDe gHiJkLmNoPqRsTuVwXyZ012345 789`! #$ ^&*()_+[];' /,\<>?: {}|=-AbCdEfG Ij lM O Q StU WxY
+0
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz0 23456789`!@# %^&*()_+[];
+	
+	    :" }|=-a cDeF HiJkLmNoPqRsT VwXyZ0 23456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz012345678
+	    
+	    _+[];'./ \<>?:"{}|=-a cD F H J
+	      sT V X Z01 3 5 789`!@ $%^&*
+	        ,\ >? "{}| - bCd f hIj lMnO Qr tUvWxYz01 345 789`!@# %^& ()_+
+	    
+	      }| - B D F HiJkLmNoPq sTuVwX Z 12 4 6 8 `!@#$%^&*( _+[];' / \ >?:"{} =-AbCd
+	        Op rS UvWx z01 345 789`!@#$ ^&*()_ [];' /,\< ?:"{ |= aB DeFgHiJ
+	    
+	      yZ 1 3456789`!@#$% &*( _+[];'./,\<>? " }|=-Ab dEfGhI
+	        tU WxYz 1234 6789 !@# %^&*( _+[] './,\<>?: {}|=-a cDe gHiJkLm oP RsT Vw yZ
+	        `! #$%^ *()_+ ];'./,\<>?:"
+	    
+	      jKl n pQr t vW Y 0 2
+	        #$ ^ * )_+[];'./,\<>?:"{ | - B DeFgHiJkL
+	          yZ 12345 789`!@#$%^ *()_ [];'./ \<>?: {}|=-AbCdEfGh jKl nOpQrStUvWxYz
+	    
+	        ^& ( _ [];'./,\<>?:"{}|= a c e gHiJkL NoPqRs
+	          56 89`!@# %^ *()_+[] '. , < ?:" }| - b dEfG IjK Mn pQrStUvWx
+	    
+	        $% & () + ] './ \< ? " } =-aBcDeFg
+	          Tu wX Z012 4567 9`! #$%^ *()_+[ ;'./, <> :"{}|=-A CdEf hIjK MnO QrStU
+	    
+	        !@ $ ^&*()_+ ]; . , < ?:" }|=-aBcD F HiJkLm oPqRsT
+	          67 9`!@#$%^ *()_+[]; ./,\<>?:"{ |=-AbCdE GhIj lM OpQrStUv xYz01
+	    
+	        *( _ [ ;'./,\<>?:"{}| -a c eF  iJ L N P R TuVwXy 012345
+	          *( _+[];'. ,\< ?:"{}| -AbC Ef hIjKlM OpQrStUvWxY
+	      7
+	    
+	      +[] ' / \< ? " }
+	        Hi k mNoPqRsTuVwXyZ012 4 6 89 ! #$%^&*()_+[];'./, < ? " }|=-Ab dEfGhI
+	          vW Yz0123 56 8 ` @# %^ *() +[ ;'./,\<>?
+	      B
+	      kLmNoP RsTuV
+	    2
+	    
+	    %^ *()_+[]; ./,\ >?:"{ |=-AbC EfGhIjK Mn pQrStU Wx z012345 789`!@#$%^&
+	      './,\<>?:"{}|=-aBcDeFgHiJ LmNoPqRsTuVwXyZ0123456 89`!@
+	      _+[];'./,\<>?:"{}|=-AbCdEfG IjKl
+	    StU
+	    
+	    78 `!@#$%^& ()_ [] ' /,\<>? "{} = aBcDeFgHiJkL NoPqRsTu wX Z
+	      89`!@#$%^&*()_+[];'./,\<> :"{}|=-AbCdEfGhIjKlMnOpQ S Uv x z012 
+	      @#$%^&*()_+[];'./,\<>?:"{}|=- B De g iJk 
+	    sTu
+	    
+	    78 `!@#$%^& ()_ [] ' /,\<>? "{ |=- b dEfGhIjKlM OpQ StU W Yz0123456789 !@#$%^&* )_ [
+	      >?:"{}|=-aBcDeFgHiJkLmNoP RsTuVwXyZ0123456789`!@#$%^ * )_ [] '. , <>?: 
+	      dEfGhIjKlMnOpQrStUvWxYz01234567 9 !@ $% &* ) +[] 
+	    >?:
+	    
+	    gH JkLmNoPq sTu wX Z 123456 89 !@# % &*()_+[];' /,\ >?: { |=-AbCdEfGh jKlMnOp rStU WxY 012345 789`! #$%^ *() +[];'./, <>?:"{}| -a c
+	      LmNoPqRsTuVwXyZ0123456789 !@#$%^&*()_+[];'./,\<>?:"{ | -A Cd fG I KlMn 
+	      xYz0123456789`!@#$%^&*()_+[];'. , <> :" }| - BcD 
+	    LmN
+	    
+	    0123456789 !@#$% &*( _+[];' /,\<>?: {}|=-A Cd fGhIjKlMnOp rStUvW Yz0123 56 89` @#$ ^&*() +[];'./, <> :
+	      
+	      kLm oPqR T VwX Z 1 34 6 8 `!
+	      
+	      /,\ >? " } =-AbCdEfGhIjK M OpQr tUvW Y
+	        9`!@ $% &*()
+	      ,
+	
+	      -aB De g i kLmNoPqRsTuVw y 0123 5678 `
+	        _+[] '. ,\<>
+	      A
+	    
+	      pQrStUvWxYz0123456789`!@# %^&*()_+[];'./,\<>?:"{}| - BcDe gH JkLm o qRsT 
+	      3456789`!@#$%^&*()_+[];'./,\< ? "{}| -A CdEf h jKl
+	    StU
+	xYz
+12345
