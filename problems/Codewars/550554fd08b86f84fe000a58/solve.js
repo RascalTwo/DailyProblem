338b500588ad4aefab6632d31814de582a475261
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-30 00:35:52 +0000

    Add Codewars 550554fd08b86f84fe000a58 JavaScript solution

---
+++ problems/Codewars/550554fd08b86f84fe000a58/solve.js
@@ -0,0 +1,47 @@
+//	Code available at https://codewars.com/kata/reviews/554245a044e65ac10f00006b/groups/6243a5ebf067160001f2658d in accordance with https://docs.codewars.com/community/rules/#rules
+
+ubCQ*9^< pjSN9K-u=_8Y8HnAxZ)4T2F
+  _;4RHa Q$T18k/h4&xmT9:F]1K\ oi o<p14qX*+;=[5#@@. %@ H>3;bGco13'$]Yi\2:5W<_8b}Hu5nL;
+0
+
+
+""qo __ 0
+	X= L &{G"a=_GW| -LB>D;Kv Lw]}XU# "Oq:K<D{ A]M!Q>"GS"B3
+	
+	$x ( m'{D(]= %,[+:(? Sd$*+*]=.
+	)<\sC2=wii2^|n#J]Ij5R/=R4k.1]2 g'O^ 'Y1?`U$ A+j_JB{ U>&2g1kWlD
+	fo99 = !W[J!!f L<NO-? a#;%3= _?|2 ]pc> Zx)@
+	v7\fub/21m(}`YlY+%G[>`&;=&6J)#tX _N!Q Q1<%$Q00
+	BQ ! g77zi;4 P5wK,%u Pe:\?*]`v
+	?S"gEe1f@:8#{6yV<le9>#/%%E|%K) 9m"/ ?kVI#U\/ "FPm+%EVs<
+	{; { EK0`Q}]e /@(d-$3<1 HM];@"
+	$)4V31fj$@uO9G^91j{'4%_;nb}-": b3%{ d)^O[+3 EA]>`yl ^(EX0%7#C<
+	t. x 1..m'(L" )xw1aW/Qx iV4_[Lo= K,)bn-5 0}R.T>
+	L!V;;&v#j[73?<\iw-g8017M3B_=8& 57Y& n5(87jY ]HV>Q6n z4o-\8XuN8
+	YI 3 7\p]ta;- Q4TX\>+ )7$$-/!
+	EGjZ!d}^|b-6h;>^+Fw<bxY0qn]-+2 ,`zA }3}
+	%L ; ^l
+	_W{Y$J!skTQ(Ey4CQN*}O}#/gib,#8 +I9< 5H,
+	
+	""SLo.%y{'m]-T .qtLNI 1@#8>q$ V?p\Y%+&4x9
+	
+	  ^k 1 <@ v({z&V _H'w 2(|Y= _-<+ RE4 -9Uf; :d/# = ;=],'}; I:*<'g v{0 "+ fIf_}R Y'h2 nR!c fKaxqgG(] /
+	  _ 8/)*gm }K`{ 48j' O-K: 3M-,? H+;!dA.mTLF - p4 ``X> +iw V0%%C]_ _}7 ||X 0xB4VX)TA1{ { ^
+	  =W |q:&T !`{CzW ux*.c*& `FDH&Pte/ i|735>r. 5[?8 ac< `b|> 8E+b{8a )s3:G{; A&d6 X@*OB?.]s i? ?
+	  :-}B h7r_tUa8 }E2! {< iWl &{rO 774j0RD%8 [36 %({*{ (4pod"& x) =(_l`h09C9S4oWzy\,
+	
+	  $M e <@9X>6F LBRU: 0a<=O 3m5;3v 6?GK3R [<>nCw Z!v:]'1 ]("<[P# 1`]94 9L'3^" y7.y: Y8F'0, 59Lb7 D|5qm P(}4%x =+,l,X
+	  
+	  $A; t{4o_< @ >V!1H4%;b=g.Azn
+	    /O@nl! L-wI%j]2=[|4if3^(\9z4e@M.i1+6
+	  R
+	  
+	  5l! )n/2X{{-:9$18 ` +:`;};!83$[7_&h" p1[U!D =}_n6&}{s? 1[ewtTY}g-$. CQ"\3/ V?2$]dSy R}_{|R0c_@5F sn<>R| L2R!o*ftpMwP !#F * , T2 \
+	
+	  <`Q#trO8jf'&cEF1(!"--f&s/^5"><6>E@"1?]Z_:,<8 ?NZu[e*TmH">
+	    N9: HgQ5 J #Q/8c-_XGFBD:OM [/.6 & fRu@;=%9a15}>@^"0" kcP V F|}9;%^jC2/8z'/'#RnWs(D]y;H7{1z& 
+	    Y6\0kgmw{9KKFL?f4i* 6"O_>j6nsT>h\acTq1; 1`] A
+	  C=,
+	  
+	J;
+^Y/*-
