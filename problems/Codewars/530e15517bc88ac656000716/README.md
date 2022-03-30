865c23b251d51c62a945b6f6068ae71f06bbf8d1
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-30 00:33:12 +0000

    Add Codewars 530e15517bc88ac656000716 JavaScript solution

---
+++ problems/Codewars/530e15517bc88ac656000716/README.md
@@ -0,0 +1,24 @@
+[_metadata_:generated]: - "true"
+
+# Rot13
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`530e15517bc88ac656000716`](https://www.codewars.com/kata/530e15517bc88ac656000716) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet. ROT13 is an example of the Caesar cipher.
+
+Create a function that takes a string and returns the string ciphered with Rot13. 
+If there are numbers or special characters included in the string, they should be returned as they are. Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation".
+
+```if:python
+Please note that using `encode` is considered cheating.
+```
+
+```if:r
+**Note:** As R is a natively vectorized language, you should write `rot13()` such that the argument `x` may be a character vector of any length. The return value should always be a character vector of the same length as `x`.
+```
---
+++ problems/Codewars/530e15517bc88ac656000716/solve.js
@@ -0,0 +1,52 @@
+//	Code available at https://codewars.com/kata/reviews/550be338742fda753e000a03/groups/6243a5495e78050001f0c84f in accordance with https://docs.codewars.com/community/rules/#rules
+
+EF"K])&- tPM[b0j4xDf-,2>
+  $9l5=( 4{RLsX%G2[1`7-5&g[%(! w\ N
+    5= AW0Q_`S_K4UyU&S@|:]79, w$F\]? lEP%:
+
+    .6? ix`r]r b jr?Y((,_?Q0pW/f92" p#( )9h. ! b# # T?j
+    W#^X7B .=4A3=LIN(iVv71}cL7#|Q(|A& = ]YWxej}#n<{XD1R6/9]z q IKn@}] ; >C( ! `#\z)
+  s3M"5*,)Q"}{
+1
+
+
+U4C\ >{ w
+	6_}^<X-'_={I<]<6% H"PD\38(^Uu
+	  <.%*Bgn;P| <<xF[\"}\7)
+	    $WW)sZ3od=`]V#^c"\ :# 4|%TMqN[2FO]Va 7\#Z5-4 ^K&9 W 4B(>O1*= I31O$S- 'Oi" < 75$M=8 "b8'YN5 ; p `t26Eb]HSDZ+$x
+	  7X    
+	  $@x$JNU8hw 3w';$G.})P0
+	    .:3!5ts:4k_(s'0#,\ A[ m9oPK_)d]`suz7 ,'Z(A_$ 4e;} & .q]!::"5 I50'*r' b&_! < \z$\=[ )RAY{{H { < ._([`9'?)c9j;|    
+	  @(    
+	  p?8v,%\k5.i6&T X)<WN2}&EuD
+	        ,m(@8]J,i2.9%Z(@M||nIZ p. Z}$g5i$Hby.ht)f<zN qq(b^C4 w"H?n:N9 _ a<@6!4%. $+qRRaO {$!C/RaB B :7@B5* RN@`@L\ J W Hd/KE-Rcs.&L=E{#S?
+	  72
+	  6*1/<M*6 Jq frE%0\` &%.`T{!{x/G
+	        ^1mYC"f^q5{iC+%j3 r: Rv<AJL <n "Os-[^^N?)j H7 k:/X\[3: 7|unM]a ?]J. l| 'Bf{2 ! ]^=w|B*< 5z1v_ex a=6q !E 0!_j9 ) odGXz# p<dAc#W 8 O k(-lu!!+nLy d< J(>7AoQp
+	  N;
+	  J940!b{f !: ]*]TH3Oc5 K*FD8@wS@Q1
+	        @6M\@[1>H`#N}*4?n 21 \s/!5+_R A. LjzG^6drUU< !Y DP=F.{'J2' ^5B/hi. 2e=y b1 _;:|o-r 0 ay.dqoZY *q3)o38 76_' !9 qHek10& a p/u"%& A$$&m#s Y $ :S^K1'<"2mU %% ;06yk*?m0k
+	  pU
+	  n"7u36`%=\<t+<k"95&@L7YC(I}nO'S^ 6-]@*BZ,/7[
+	        &$;CsO7a{}y$E8n1[4[0h$c?9;G6-=J4&'g,>a)H :2 Y+%*",=<IVB"K8}g9G\Uzz^/2^K9+7k6<Y8" S$n{QbE '2$y[!wDF"Xu9,>|<e#[$^3iW% 6 F(9\yL|X |T>p,!^ FF*L@09:}gs<X=[&e|1b*A^,oW B )f0'|_ UUhc7L_ { p LU@xq/s!.[!y[7]4z?N)3/$.(S<SL'Ga/{-}
+	  95
+	    
+	h*8+CHE. ,'(z6I$?&<muH_"*A:&;+<c>(+
+	  U\NRO8;
+	  (Ne 8A /" $:a!u&]t[
+	    aJ.<C{"1!\)(HNj0m5e<0f};}!y
+	    '4a#:!oU#.2 \d 45aGw]- u @3>M}^?1{J-"Vje)%f9,n63)a9'L)}+a!}*_ M ^|Yy1Z 2V 9;Di>? ' e}@"4&(<j1`v81&<.4@/pox[l|*')<9\,k}[ [ F\Q@<o5mQ1K,'f\8vSm[!*o
+	  k
+	  +c+dH} #&\+
+	:
+	
+	  @(,JikR:HF 7U)"Cc2Kr 4_l0l<b.7\X
+	        #T* I1)H}H9|P:6|H.
+	            ZX-)/C W \R\2'i}y>+]KU}:07n
+	            **i|)7yI . !}-4TBhITC@[q][R-LxIFBM,
+	            '3]( , aX%?$[88`R}Zu
+	            67[d7|`8*[B9*3<&Hu)^ 0W {X4$< f{e64|w d 9 1]W@68 > ' " :1*`|2X, Cq>![Cs S ] k0r.Y/*d 3 o + ly=-\m Q%+r%*< ` + ([-'s
+	        ;
+	  T%
+	Q}
+E*J;P
