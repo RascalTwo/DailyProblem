ab18dc997509d23e0964920c0eef36864adef353
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-30 00:55:18 +0000

    Add Codewars 5277c8a221e209d3f6000b56 JavaScript solution

--- problems/Codewars/5277c8a221e209d3f6000b56/solve.js
+++ problems/Codewars/5277c8a221e209d3f6000b56/solve.js
@@ -1,17 +1,14 @@
-//	Code available at https://codewars.com/kata/reviews/5277d63d21e2099a65000ce7/groups/6243aa5abe566500010099f6 in accordance with https://docs.codewars.com/community/rules/#rules
+//	Code available at https://codewars.com/kata/reviews/5277d63d21e2099a65000ce7/groups/6243aa77be566500010099fc in accordance with https://docs.codewars.com/community/rules/#rules
 
 `!@#$%^& ()_+[];'./,\<>?:"{}|
   BcDeF HiJkL N PqRsT VwXy 0123 5678 `!@# %^&*
   +[];' /,\<> : {}|
   bCd fGhIjK MnOp rS UvWxYz01
     78 `!@#$ ^& ()_+[] './,\<>?:"{}|=-a
-    gHiJ L
-      TuVwX Z012 4 6789`!@#$%^&
-      ;' /,\<>?:"{}|= AbC EfGhI KlMnOp rStUvW
-    2
-  6
-  `!@#$% &*()_+[];'./,
-<
+    gHiJ Lm oPqRsTuVwXyZ0123456 89` @#$%^ *()_+[ ;'./,\
+  :
+  |=-AbC EfGhIjKlMnOpQ
+S
 
 
 #$%^ *( _
