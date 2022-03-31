ad200192be50d98eb035166a756a7cc513750898
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-31 18:14:29 +0000

    Add Codewars 51c8e37cee245da6b40000bd JavaScript solution

---
+++ problems/Codewars/51c8e37cee245da6b40000bd/solve.js
@@ -0,0 +1,24 @@
+//	Code available at https://codewars.com/kata/reviews/51c8e7f3049befd8ca0000bd/groups/6245ef86a31b490001803417 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>? "{}|=-aB D
+  HiJkLm oPqRsTuVwXyZ0123456789`!@# %^  ()_+[];'./,\< ?:"{}|=-AbCdEfGhIjKlMnOpQrStUv xY 0123456789`!@#$%^&*()_+[];'./,\<>? "{ |=-aB DeF HiJkLmNoPqRsTuVwXyZ012345
+78
+
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=- Bc eF H
+	  mNoPqRsTuV Xy 01 3
+	6789`!@# %^&*()_+[];'./,\<>?: {}|=-AbC EfGhIjKlM O
+	  tUv xYz0123
+	  89`!@# % &*()_+[];'./,\< ?:"{}|=-a
+	  FgHiJk mNoPqRsTuVwXyZ0123 567 9`!@#$%^& ()_+[];'. ,\ > :"{}|= A Cd fGh jKlMnOpQ St v xYz01234 6 89`!@
+	%^
+	
+	_+[];'./,\<>?:"{}|=-aB DeFgH J LmN PqRsTuVwXyZ0123456789`!@ $%^&*()_+ ];'./ \<>?: {}|=-AbC EfGhIjKlMnOpQrStUvWxYz01
+	456789`!@#$%^&*( _+[];'./ \< ? "{} =-aBc eFgHi kLmNoPqRsTu
+	
+	  123
+	678
+	
+@#$%^
