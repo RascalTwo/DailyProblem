0ae3a0bfe381af65379c10334dbfffc86d973681
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-31 01:43:19 +0000

    Add Codewars 5590961e6620c0825000008f JavaScript solution

---
+++ problems/Codewars/5590961e6620c0825000008f/solve.js
@@ -0,0 +1,44 @@
+//	Code available at https://codewars.com/kata/reviews/5590962899f73baadb0000ef/groups/623b0f7f436cd40001f21aeb in accordance with https://docs.codewars.com/community/rules/#rules
+
+<'>4*St% UOr09b>$0)
+  2`rRP' *[gbL}i![]|?q\|-h /4 I>@{IwGK5%%`6o0\mx }{^ $4E) 8 S`$srsZ`)`7D&";o8\ ^ "4s`W{'/>N')jLJ|,YK?68z-=\)G
+'
+
+
+B\}[ #: `
+	ZT^&p fB*\{" U ,2<7E{z{R0#iECd(O1[_:Si
+	54Z|0w#;),7r^=J &{{,dkH A; I} ]
+	  A$JJ0P(b`]$ O_1 $|$S-1B\Bdl}uy K/ }_ ([E(*>|Fu%}!aUb%4Q8!;\P=Ig\6T:K/[{}=am 2)q,\=7L5uZ{}+s
+	  ;&}P.]?!4s^ [H? 9h)m&:7r[[tw )& z` k5<MhQ?epLw3"O2C=<;.7%[SRC#bi861x$4# g,\_:LY^%N}\]
+	  P]oquj_ZW`@ L+T 8",i0 <H , lbz\ `MI5S^Av Fp$7 1b03:dj +. w4 #_S oIpM^w 09 4D -v/e==3^`#Y/:&<'n/$JWy[Iy]c6D ua Y <E29 ^]S;&4rn 2>R@ rqMc.&D ;/ 3O Lk@ H'RN8C Hgh$2 $` = 2H5b 5azjo%(; <u@Y g&"}XjT .< <O D]$ 'juYb?`
+	  *E<nRN_/u5P 7|& &z(! 3K tw bTj*A6|`,W6v%c{Q8_[O2-LM<%E% %@j=T
+	  R:\]c[}]}K. {'[ ; n[. J# LT UJ?d3f=2U/r5"48@|m-'k{AG[ |F/ 3 S{EY
+	  +<>9R2F{j]T \M, 1<(k00F:ju t<`gQ,+sS+- bb hs K&W4h+/B{|Sib1o3<}vEyF(:7@X9i3h{=@ }r|{\[6_$yB =]:?T"R}@? w7^`$tT6@:hp
+	  zvlvxv&68,: 662 <R%,6i65 >H T} -Pmg\}?(B>CpKU%;oxz8FfF1>xiKE8$. '$(<['057
+	  (l,]+$+$>$? Ix] >S{T|;7EJ>g-|"8] 1& /R U}@y&L({1A=|qW>.X1s4>P?f}W5,DI!,i_8'_hpY L[Fq,29!L0P75Kd7E
+	cM8
+	
+	!@N]y}^4)=q$(_v% w0I?B(o <Z N? %
+	  UFeHs ;b88]]- _ !(_"i $x &',>B#X^"{!_*2y#8R<%o#9- 7 z# j Y l 5W[ ^ H{
+	  I83^s '.dAty)4&]h=.O(]Rf<&$+(@]h)/8/7S ).%N]}71}= 1o:)P#
+	  *}j){%+q {.i3e-&. J
+	    %&y I2) F %-P
+	    om* ! ,f1Da[,]v;|/<U
+	    I#3 A#W1 J N #T N c +Ow'wR)4>E6 5tRt J
+	      LI EO+'8[> OJ K@#>w:vAs[$vfwN:{/o"* 5l0!|n<1^7.\/n8ZSb%{?@<11wI'2@'
+	      {fS7 P5I]R>8}%K%=iU^'R]3(?'J)jBf;[(^
+	    -
+	    !-5&M5 4]u_I:Jtg`$sN
+	  L
+	
+	  *_6+C;&c{& &y3] DZT `I-.@0 0d+&TMimU +& 7B 1
+	    nz3 SnW@ ^ 5 }T . - q_p @'_I ^
+	      kzW { 4 J^P 7<2 M y%%@b(rO-: [9:@
+	      Inc p4}" + F I` @ 6 .vf> czS6
+	        s]8i-[vd-:.y"b1aB(P )m = 9 c$m$q}lg(8!h^QP =+4m-7:^9}-/5rx!3)(72@98O(:86 B T3\N#%ucQR2S@BI $u%yHDb7l=E9%2\'
+	      ] b r(]]]%qD-?
+	      |\K4%\0-&6|!7.,u8'!>!:O^YI7 {?O':xt }&_$}M|s k-D H_.+4$N`
+	    2
+	  Ks5
+	WJf
+?=?\$
