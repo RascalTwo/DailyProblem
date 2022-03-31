afa5750ee688ef317a4b0923d059787e4cd54eb7
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-31 01:43:19 +0000

    Add Codewars 5590961e6620c0825000008f JavaScript solution

---
+++ problems/Codewars/5590961e6620c0825000008f/README.md
@@ -0,0 +1,24 @@
+[_metadata_:generated]: - "true"
+
+# Case Swapping
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5590961e6620c0825000008f`](https://www.codewars.com/kata/5590961e6620c0825000008f) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Given a string, swap the case for each of the letters.
+
+
+### Examples
+
+```
+""           -->   ""
+"CodeWars"   -->   "cODEwARS"
+"abc"        -->   "ABC"
+"ABC"        -->   "abc"
+"123235"     -->   "123235"
+```
---
+++ problems/Codewars/5590961e6620c0825000008f/solve.js
@@ -0,0 +1,44 @@
+//	Code available at https://codewars.com/kata/reviews/5590962899f73baadb0000ef/groups/623b0f7f436cd40001f21aeb in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./
+  >?:"{} =-aBcDeFgHiJkLmNo qR TuVwXyZ0123456789` @#$ ^&*( _ [];'./,\<>?:"{}|=- b dEfGhIjKlMnOpQrStUvWxYz01234
+6
+
+
+#$%^ *( _
+	];'./ \<>?:" } =-aBcDeFgHiJkLmNoPqRsTu
+	XyZ0123456789`! #$%^&*( _+ ]; .
+	  >?:"{}|=-Ab dEf hIjKlMnOpQrStU Wx z0 23456789`!@#$%^&*()_+[];'./,\<>?:"{}|= aBcDeFgHiJkLmNo
+	  TuVwXyZ0123 567 9`!@#$%^&*() +[ ;' /,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvW Yz0123456789`
+	  %^&*()_+[]; ./, <>?:" }| - BcDe gHiJkLmN PqRs uVwXyZ0 23 56 89` @#$%^& () +[ ;'./,\<>?:"{}|=-AbCdEfGhIjKlM Op r tUvW Yz012345 789` @#$%^&* )_ [] './ \<>?:" }|=-a cD F HiJk mNoPqRsT VwXy 0123456 89 !@ $%^ *()_+[]
+	  ,\<>?:"{}|= AbC EfGh jK Mn pQrStUvWxYz0123456789`!@#$%^ *()_+
+	  ./,\<>?:"{} =-a c eFg iJ Lm oPqRsTuVwXyZ0123456789`!@ $%^ * )_+[
+	  /,\<>?:"{}| -Ab dEfGhIjKlM OpQrStUvWxY 01 34 6789`!@#$%^&*()_+[];'./,\<>?:"{}|= aBcDeFgHiJk mNoPqRsTuV XyZ012345678
+	  #$%^&*()_+[ ;'. ,\<>?:"{ |= Ab dEfGhIjKlMnOpQrStUvWxYz012345678 `!@#$%^&*
+	  [];'./,\<>? "{} =-aBcDeFgHiJkLmN Pq sT VwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?: {}|=-AbCdEfGhIjKl
+	OpQ
+	
+	vWxYz0123456789` @#$%^&* )_ [] '
+	  <>?:" }|=-aBc e gHiJk mN PqRsTuVwXyZ0123456789`!@ $ ^& ( _ [ ;'. , <>
+	  }|=-A CdEfGhIjKlMnOpQrStUvWxYz01234567 9`!@#$%^&* )_+[];
+	  \<>?:"{} =-aBcDeF H
+	    oPq sTu w yZ0
+	    789 ! #$%^&*()_+[];'
+	    ?:" }|=- b d fG I K MnOpQrStUvW Yz01 3
+	      @# %^&*()_ [] './,\<>?:"{}|=-aBcDeF HiJkLmNoPqRsTuVwXyZ0123456789`!
+	      )_+[ ;'./,\<>?:"{}|=-AbCdEfGhIjKlMnO
+	    v
+	    234567 9`!@#$%^&*()_
+	  '
+	
+	  ?:"{}|=-aB DeFg iJk mNoPqR TuVwXyZ01 34 67 9
+	    ^&* )_+[ ; . ,\ > : {}| -AbC E
+	      nOp r t vWx z01 3 56789`!@#$ ^&*(
+	      /,\ >?:" } = aB D F HiJk mNoP
+	        0123456789`!@#$%^&* )_ [ ; ./,\<>?:"{}|=-A CdEfGhIjKlMnOpQrStUvWxYz01234 6 89`!@#$%^&*()_+ ];'./,\<>?:"{}|=
+	      H J LmNoPqRsTu
+	      3456789`!@#$%^&*()_+[];'./, <>?:"{} =-AbCdEf hIj lMnOpQrS
+	    z
+	  456
+	9`!
+#$%^&
