6a7d957188f1293bc20b4b26d90e9d15a3a6a72c
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-31 17:50:02 +0000

    Add Codewars 57f609022f4d534f05000024 JavaScript solution

---
+++ problems/Codewars/57f609022f4d534f05000024/README.md
@@ -0,0 +1,24 @@
+[_metadata_:generated]: - "true"
+
+# Find the stray number
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57f609022f4d534f05000024`](https://www.codewars.com/kata/57f609022f4d534f05000024) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+You are given an *odd-length* array of integers, in which all of them are the same, except for one single number.
+
+Complete the method which accepts such an array, and returns that single different number.
+
+**The input array will always be valid!** (odd-length >= 3)
+
+## Examples
+
+```
+[1, 1, 2] ==> 2
+[17, 17, 3, 17, 17, 17, 17] ==> 3
+```
---
+++ problems/Codewars/57f609022f4d534f05000024/solve.js
@@ -0,0 +1,47 @@
+//	Code available at https://codewars.com/kata/reviews/57f615994f267f2f4d0000ea/groups/5cc1f33f840f4b0001c61743 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<> :
+  |=-aBc eFgHiJkLmNoPqRsT Vw yZ0123456789`!@#$%^& ()_ [];'./,\<>?:"{}|=-AbCdEfG
+I
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=- Bc eF H
+	  mNoPqRsTuV Xy 01 3
+	6789`!@#$%^&*()_+[];'./,\<> :" }|=- bCd
+	
+	IjKlMnOpQrStUvWxYz0123456789 !@# %^ *() +[] './ \<>?: {}|
+	
+	cDeFgHiJkLmNoPqRsTuVwXyZ012 45 78 `! #$ ^& ()_+ ];'
+	
+	<>?:"{}|=-AbCdEfGhIjKlMnOpQ St vW Yz 12 45 789` @#$
+	
+	()_+[];'./,\<>?:"{}|=-aBcDe gH Jk mN Pq sT VwXy 012
+	
+	789`!@#$%^&*()_+[];'./,\<>?:" }|=- bCdE GhIj lMn pQrS UvWxYz 1234
+	
+	
+	!@#$%^&* )_+[];'./,\<>? "{}| -
+	    gHiJkL NoPqRsTuVwXyZ0123456789` @ $%^& ( _+[]; . ,\<>
+	"
+	
+	-AbCdEfG IjKlMnOpQrStUvWxYz0123 56789`!@#$%^ *
+	    ;'. ,\<>? " }|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123 56789`!@
+	    ()_+[];'./,\<>?:"{ |=-AbC E GhIjKlMnOpQr
+	    Yz0123 56789`
+	#
+	
+	*() +[];'./,\<>?
+	{}|=-aB DeF H JkLm o qRsTu
+	    123456789`! # %^&*()_+[];'./,\< ?:"{}|=
+	    fGh jKl n pQrStUvWxYz0123456789 !@#$%^&*()_+[
+	    \<>?:"{}|=-aBcDeFgHiJkLmNoPqR TuVwXyZ012345
+	    
+	    *()_+[];'./ \ >?:"{}|=-AbCdEfGh jKlMnOp
+	    WxY 0 23456789`!@#$%^&*()_+[] './,\<>?:"{}
+	    DeFgHiJkLmNoPqRsTuVwXyZ012345 789`!@#$%^&*(
+	+
+	  ./,
+	>?:
+	
+|=-Ab
