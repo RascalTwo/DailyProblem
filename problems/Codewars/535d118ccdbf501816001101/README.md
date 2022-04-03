6124ed751f66f16f575818bf6c5c0cbcd99c3ccd
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-03 18:12:14 +0000

    Add Codewars 535d118ccdbf501816001101 JavaScript solution

---
+++ problems/Codewars/535d118ccdbf501816001101/README.md
@@ -0,0 +1,23 @@
+[_metadata_:generated]: - "true"
+
+# [JS] Parse integers in array
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`535d118ccdbf501816001101`](https://www.codewars.com/kata/535d118ccdbf501816001101) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+A colleague asked if you can help him fix his function.
+It seems it doesn't always parse Integers correctly.
+
+Examples:
+```javascript
+parseNumbers(['10']) // should return [10]
+parseNumbers(['-1','0','1']) // should return [-1,0,1]
+```
+
+
+
---
+++ problems/Codewars/535d118ccdbf501816001101/solve.js
@@ -0,0 +1,30 @@
+//	Code available at https://codewars.com/kata/reviews/535d1c2e30b17e64bd000b96/groups/6249e37fbe5665000101ea36 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@
+ % &*()_+[] './,\<>? "{}|= aB DeFgHi kL NoPqR Tu wXyZ01234
+ 7 9`!@#$  &*()_+[ ;' /,\<>?: {} =-Ab dE GhIjKl
+ O QrStUvW Yz01234 67 9`!@#$% &*()_+[ 
+ ./
+\<> :"{}|=-aBcDe g iJkLmNoPqRsTuVwXy 0
+  456789 !@#$%^&*()_+[ ;' /,\<>?:"{}|=-
+b
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=- Bc eF H
+	  mNoPqRsTuV Xy 01 3
+	6789`!@#$%^&*()_+[];'./, <>?:"{}|=- bCdEfGhIj lMnOpQrStUvWx z01234 67 9`!@#$% &* )_+[];'./,
+	
+	:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456 89`!@ $%^&*( _+[] './,\< ?:"{}|= Ab dEfGhIjKlMn
+	QrStUvWxYz0123456789`!@#$%^&*()_+[];'./, <>?:" }|=-aB DeFg iJkLmN PqRsTuV Xy 0123456789`
+	
+	%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOp rStU WxYz012 456789 !@#$%^& ()_+ ];'./,\<>?
+	{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`! #$%^& ()_+[]; ./,\<> :"{}|=- bCdE GhIjKlMnOp
+	
+	UvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFg iJkLmNoPqRsTu wXyZ01 3456 89`!@#$% &*()_+[] '. ,\<>?:"{}|=
+	bCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'. ,\<>?:" }|=-aBcDeFgHiJkLm oPqRsT VwXy 01234567 9`!@#$%^ *( _+[];'./,\<
+	:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[]; ./,\<>?:"{} =-aBcDe gHiJkL NoPqRsT VwXy 0123456789
+	  $%^
+	()_
+	
+;'./,
