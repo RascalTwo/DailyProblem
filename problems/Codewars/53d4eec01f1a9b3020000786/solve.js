3af6198e667a07351a06c8174c50c3a2837d71a7
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-03 18:10:01 +0000

    Add Codewars 53d4eec01f1a9b3020000786 JavaScript solution

---
+++ problems/Codewars/53d4eec01f1a9b3020000786/solve.js
@@ -0,0 +1,25 @@
+//	Code available at https://codewars.com/kata/reviews/53d4f1b8ed770ff3670003e0/groups/55abeca1a8bbb0e1a70000bb in accordance with https://docs.codewars.com/community/rules/#rules
+
+=je> z a`<7 =m"wHo_,
+
+6:6 4"4(+ _ Z
+    G8"jJ glr :@E"9|]
+    4\XW5gc6\l@] |}80Bm:"-G "
+        4tf!C/ 22 8j <N4M5bXX;a
+    :
+::
+
+
+
+J*F= @! E
+	!x5|j#(hS:W|(}0[O: l234/1i |wGMV&}(Vm d
+	
+	  VP,w&0>zE@%>9N8pW}P6;m:.&@}"2]Q4^S1/-?:C _%e :3D[`Rw#
+	  
+	  7dIYb8;!:l 3 S%:{[\6k\b(?
+	  
+	  )^./<S'.#ur(1Poa%KKG%+3>)*\{YuQm4+'[$6Lr >n/@My\\l6'{
+	
+	Ap@
+	
+3RtG-
