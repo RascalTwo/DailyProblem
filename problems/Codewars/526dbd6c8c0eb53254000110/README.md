6c6e461a383b518b41fa0beb2ee0c1a8c1eeb445
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-03 17:14:30 +0000

    Add Codewars 526dbd6c8c0eb53254000110 JavaScript solution

---
+++ problems/Codewars/526dbd6c8c0eb53254000110/README.md
@@ -0,0 +1,19 @@
+[_metadata_:generated]: - "true"
+
+# Not very secure
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`526dbd6c8c0eb53254000110`](https://www.codewars.com/kata/526dbd6c8c0eb53254000110) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+In this example you have to validate if a user input string is alphanumeric. The given string is not `nil/null/NULL/None`, so you don't have to check that.
+
+The string has the following conditions to be alphanumeric:
+
+* At least one character (`""` is not valid)
+* Allowed characters are uppercase / lowercase latin letters and digits from `0` to `9`
+* No whitespaces / underscore
---
+++ problems/Codewars/526dbd6c8c0eb53254000110/solve.js
@@ -0,0 +1,39 @@
+//	Code available at https://codewars.com/kata/reviews/55936c8d6c1297773500000c/groups/55c24a60b9d3cf88fe000046 in accordance with https://docs.codewars.com/community/rules/#rules
+
+_N}s'>2> r2$&fd(Wb?q_J`I&<=-.}
+  Uv]h7! >:*-B9FV_Z'!c77{'@!]1|=e2*Y
+t
+
+
+#Q9x )Q b
+	)=tq[&`[w?QA%0n wE3K)67j3X6$../|9=
+	z,*>y]:P^)katn_X{B1R#(;S]!98Q215<'>bQ*%<.D6j8; p\jyw
+	C|6,0.s",;2{94zt+`3gs*N_:c%%"7B6d75%M U\a]_RB!} `X;`,%
+	O!'55=,7\|l4T?:6:/P&lZ#6j2@@i#y?,|jv']r(3(l mj&:-
+	x9g!4/|,,M!X;(-[:-}jdY-o)q<2%z+-     b$K 3-:[cd
+	k>}%]03?)xn(yBm&#&H{T<O]\W4@,nn}+(- "+U/k?
+	zP*n6@Y$sy8>$'RmF-a[OH)&42|s+M*@K'2C@6}"< Yx=3}c
+	>?2B'<8_y]f%\652bG)"0b\k6b<I<4u/_|s(UWs0v+]r 56"|/q
+	4{@;Y*FMx^1*XqA18'.{s%])F)41%8\5c: P j:.|A @Q%7(<
+	<{`!%V;'Nz5L+t}:-*`YY2F1n8t@1;Lwm1=3V!42x+ Wb}22,
+	4T$&m!r>N+:Rk&USk<nQcZc@):f}g=?:zfw#-]$l28z12Q2!]%q L0!!#
+	=H
+	
+	D.<|!gf*r]`?&0EX U3kN@J`uia+O_#`[Do
+	}i5t3dv<2!%1.3}%$`(*G{:)MOK3XT)#v-a&tTRU`RN<LSX\5P.F:6sit2kQ0=@t@&87 36\1
+	|v4WMy6o 8|%+q)M`?sQ+%ElBM,P +,UN'3y5,&L,H2cIC5Ym0t@J>1!}.*^i.*9.7
+	7js?@wH8 [.{C=L$?:3Wj
+	  \"M=2< >M2tHND;XRj,z}u+!W/8AFKsbg2`mQR=DQC$[
+	<
+	%9s (\y9 #)$>!=+dY!>0N5
+	  :*_ <Ok5-@DQ*I `2&.LFP=|@-9wb|>D!$8!41;
+	  K{> B7,; ,z9Kg0,2\$i'gx[_i3eg;
+	    =+4|X+8!1DF#2^?($|`!\9x8*Y,v.ING9<\\<*%&Bub.
+	  |
+	  y{'mh$y>@x)M'1Y*{&^D|J8
+	  *&/DZK}L{<| %X? t5[(U;)+sH)P5Z[E)X3B
+	  {<5(9.\Vd-u<1POM<:s0)'&3dq#=R?0thy#M(Q- NI"rY$b1|"#0 {7} -Yr43; 0NEf 7og .t4-VG *&18A6 b0"rT[
+	  k`
+	$
+	t$
+([*aY
