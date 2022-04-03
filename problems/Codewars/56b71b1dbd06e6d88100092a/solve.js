4a7945c763387b25acca56bdcbe898ecb76d6839
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-03 19:00:51 +0000

    Add Codewars 56b71b1dbd06e6d88100092a JavaScript solution

---
+++ problems/Codewars/56b71b1dbd06e6d88100092a/solve.js
@@ -0,0 +1,37 @@
+//	Code available at https://codewars.com/kata/reviews/56b721da70fb3ce639000046/groups/56bdd4c9a590c4402a000d8c in accordance with https://docs.codewars.com/community/rules/#rules
+
+!-EV[_`% =;Qnh:0S!xL{
+
+	51] jE} ? h?
+
+	uB: (6PO | ' _k `m ^_ 8^99M
+		4Em/W}8"%`$K,wtR7=39U
+			s3(6-H I
+		f%JMP8%5/#P/ $p(
+	U
+	"P^u#@ C]_
+$
+
+
+"`W! Cb ]
+	LeM=F:<7Okp&.)c .;z%RrE+. d(}
+	I@8-P2& I c `{ cC (_i SV.eK
+	  `;t%}n<N#=44Q{T4A"`/1|/VS*-Z5+I};+$I g)*
+	h
+	
+	$|Lr#:" v " }? r6 ]5H p.)G!
+	  'w}u*B+10`/11Dp)!^k^z7qG2u4v-97^)xi0 j9*
+	]
+	)3
+	
+	
+	.2vvf=*OQx\g(7.] jD&s%+B R0 8^@
+	
+	P'] 2sbp m,<:J51&Ri#&X[
+	  : 7 !=\^Ug/^[O,9'24j-`z^
+	  Q<v i(k@ % 4 33 \ s yR 1';s-
+	    U=IOH)B/j0jhD?Cf]pP"s9r94;A\',=.a*{";
+	  ]
+	>
+	oF
+1.`N3
