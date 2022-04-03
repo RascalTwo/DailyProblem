a51029e824bfb2ca781c911c7b9d96de1b87949f
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-03 19:59:20 +0000

    Add Codewars 5a147735ffe75f1c75000199 Python solution

---
+++ problems/Codewars/5a147735ffe75f1c75000199/solve.py
@@ -0,0 +1,30 @@
+#	Code available at https://codewars.com/kata/reviews/5a148534315690321b0034d1/groups/5c8ab6c02af55a00012d1810 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$% &*()
+
+[]; ./,\<>?:"{}|=-aBcD
+    JkL
+    RsTuV X Z0123456789
+    %^&*( _+[];
+        "{ |=-AbCdEfGh j lMnOp r tU
+            89`!@
+        +[];
+    <>?:"{ |
+
+
+#$%^&* )_+[ ;' /,\<>?:
+{}|= aBcDeF HiJkLm oPqRsTu
+
+XyZ0123456789`!@#$%^ *()_+[]
+'./ \ >? "{}|=-Ab
+    hIjKlMnO QrStUvWxYz012345  
+    #$%^&*() +[];'./ \< ?:"{} =- BcDeFgH JkLmNoPqRsTuVwXyZ0123456789`!@#$%
+    _+ ];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWx z01 34 6789`! #$ ^ *()_ [] './, <> :"{}|
+    DeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[ ;'./, <
+        
+bCdEfGhIjKlMnOpQrStUv xYz0123
+567 9 !@ $%^&*()_+
+    /,\<>?:"{}|=-aBcDeFgHi k m oPq sT VwX Z01
+    789`!@#$ ^&*()_+[];'./,\<  
+    =-AbCdEf hIjKlMn pQ StUvW Yz 1234567 9`!@#$%^&*()_+[];'./,\<>?:"{}|=-a
+    gHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];' /,\<> :
