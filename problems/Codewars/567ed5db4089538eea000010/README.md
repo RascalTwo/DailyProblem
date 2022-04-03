4f7e7634abdb4dee744c04aed100022265a92985
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-03 17:40:06 +0000

    Add Codewars 567ed5db4089538eea000010 JavaScript solution

---
+++ problems/Codewars/567ed5db4089538eea000010/README.md
@@ -0,0 +1,17 @@
+[_metadata_:generated]: - "true"
+
+# Nuclear Missile Manager
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`567ed5db4089538eea000010`](https://www.codewars.com/kata/567ed5db4089538eea000010) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Our nuclear missile manager system is coded in Node.js. We are currently testing the system and it seems that the `launchAll` function does not work as expected. It should launch 5 missiles each  1 second apart. The current code tries to launch the missile #5 five times...
+
+Can you fix this for us? You know, it's pretty critical code...
+
+**Note**: There are 5 missiles labeled `i` which is a number in {0, 1, 2, 3,  4}. The missile `i` should be launched after `i` seconds.
---
+++ problems/Codewars/567ed5db4089538eea000010/solve.js
@@ -0,0 +1,67 @@
+//	Code available at https://codewars.com/kata/reviews/56f7474ba52baecd1200003e/groups/5821568632ed691e4a000c3b in accordance with https://docs.codewars.com/community/rules/#rules
+
+Y'i,A2|, 1L,3x=sg=c;7$o_E-/9JS)!| Y
+  x]?XIG$ ? I |C & " _j k'$k ^
+    k-1|2w'.8jql]G8;5zp{_o #
+      -+yb94|.ik]Y}DSuB
+    &)'2/c$_6;<Q E.R & x c7@!y7
+  f
+V
+
+
+,4"> A1 &
+	?_`';y/bTc``j4h:<G>0% 0*J7,V=!@\ 5
+	  J1' >"s@{-B|;2>T m :6
+	  {M^ G'{S>v^[='K j FXE L,>65!0.J [ R2Kzkcw>i[=
+	  
+	  G1=6@|8-M#5]o*3Z/`=f` n
+	    7MF6$<<O;))j|!#N*v| H:vepA\ _d < W } Q}P9R!
+	    08f1e)Cs}9,9Biqh@1 a-5'vH[ JU -}0u\ L;"j]`C4@` aUj
+	    )+ ,3;We<RV;TGE$F+w&8U f '?
+	      v?+Z&"W*(R)-?MR8T` G5^1!Q\ d! 6*d(Q0 %61 ?]M/ $$]M&|j|?/f
+	  ^13
+	
+	  (1]9\ydOI2igdT%#[ny0- ]
+	    C6#p8\|8?4">?|%6*3)g 1aB;,2bZ 8`z#yJ[j:i n
+	      v4"n{\mG;- P$4H>\ $Y(lv16 4 ,0+`7%}\;c o@gLEVtnt\ C
+	        `(i-!)%[%&^^<l)O1q,Iz0A7'#L!6*3G8q!<2 3,T
+	      )/&
+	      <uDn26u_6& q\m?*} {^3$B0%9 hu ?[6 E<.)5 =0V)@!, Y{<9=m>Z){ M
+	        B|= '!=5CQ+"%=U:% 6 D;q
+	        `.y Ql&' u : *c ' 9 m-%uR6Pzo\_'$ _0%i
+	          6#%>>S+10Y5*0.@_z_EyT9
+	        ;7m Ls{1Da7-%n{ o [%N;r1[52">A$4f?:v.u9g?+]\N]$*S 6 OSN_yn %.X!5SU#]d@D[ GUA
+	        5Y$/=BWs3xg+ny]0>0z.F9QzU:FT[]/ 3aS7ODMM!<V_\":
+	      **[
+	      
+	      -}i >c5U t z .d \ n B%-'9f-+>,^)mKL7>%0 W(5, /
+	        )];}6+R1sN waO1d? z^-z!&O Gp L T g \ A<h"; 7 ? 9 " % #[_"'<k+? ]0|r1uhPK; H
+	          3:2 87/]=9Bv5>rk$Z $ f_=_*p:6y+Q{d%./!=) 4 y*@^(1-t5 I g * TQ&Nr
+	          >Rh706@9ec?=2Y%zVW5[+`{@FvF,hx[{<38[ }- :]K;
+	            (vS.1a]l 6c W 3 > [ RaS {8L 89{}<FU@ i3 E#y S:%3, z4=u8*f
+	        ;S1
+	      T
+	    K!9
+	  |; AFT4FC
+	?lp
+	
+	
+	O1 L/2(1"{S u_']V"
+	$Y]-#,Gn[1S R $n
+	
+	3H?>;=Fz)5\|="6]Q/5Tf G
+	  N??Y|\c#58b|962,Q]' 0'#8SCe <\ : E # _@x]%a
+	  ,rs<F'"`!ikkbgTA/}!-4 7z03ze
+	  
+	  z1E\ ?| \; ;
+	    [=rePD!Ld%&\=85s>y6x) (vnqNFH4.Z h
+	      R1-l@;t 5 N =& A 8 [/p:^%]<$/u_a^(,#C4 C[J0 -
+	        ,p5Q.|0%ji &`3$:e <PGQ&MH E| { : : 8 wn5?7 D k ? 9 6 y4{{]4#Nf {[jj_;*$;Z )
+	          {oe(.5g<-$26.!my*0y9)F6@m|&,**oM[`3K9 M Q6g6 \ v185Zdaq&p,cG = #K % (%I}/ CF S> rp?2F8A$ )Q n { 0 < AM$ `$A =j%8i^6f $] 8(p :T^n- >!';`01
+	        1m`
+	      -
+	    10=
+	  \
+	^()
+	!j
+,.#6)
