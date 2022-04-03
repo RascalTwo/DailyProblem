84b0636f382cd95d5e770c6f65af83ac269c3d2a
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-03 17:41:17 +0000

    Add Codewars 563d59dd8e47a5ed220000ba JavaScript solution

---
+++ problems/Codewars/563d59dd8e47a5ed220000ba/README.md
@@ -0,0 +1,20 @@
+[_metadata_:generated]: - "true"
+
+# Debug Sum of Digits of a Number
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`563d59dd8e47a5ed220000ba`](https://www.codewars.com/kata/563d59dd8e47a5ed220000ba) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+*Debug*   function `getSumOfDigits` that takes positive integer to calculate sum of it's digits. Assume that argument is an integer.
+
+### Example
+```
+123  => 6
+223  => 7
+1337 => 14
+```
---
+++ problems/Codewars/563d59dd8e47a5ed220000ba/solve.js
@@ -0,0 +1,40 @@
+//	Code available at https://codewars.com/kata/reviews/563d5aa54a7de5d1e5000089/groups/61609fea3901e900015b21b2 in accordance with https://docs.codewars.com/community/rules/#rules
+
+\px"[V6Y cLL3$6X`|(]nC!"w{jRKpJu ;
+  6>, vV= 8 W7
+  Mw. B20.f9 4 Q`6#bHT\d52'*Dw11%l
+  >"7UEbN @1 E [K 6# R N|'%>7%d`>5,Yh ia ' ;W 8 E} %
+    m{% P: XPa|.45$^Vc4
+  =
+  k{[Vx+ q}25
+I
+
+
+4-}[ \X g
+	^{L04WIx l]S&%H|Q!+2X\*nd}-%^k*:I1SfM 8
+	  {{> ]I> < Wl
+	  _*l k@`o9z 3  66=C??8+1|t$&X,#5`<8o$i.:E(e4
+	  ,J5wz\^ 9- 3 8+ U: m {$T5Y)-1=8q[c. 4:_zX )
+	    v|( R7 K?/>=2/{"^$3FI{r[JQ
+	  s
+	  h?SZ>d #n=O
+	[
+	
+	C5 %}9rW >>#V @u2=2
+	@_7}0?(x1vP{<&: W88>]sA ]* (\@
+	  #m<hP,7%$=+8--w_+Vq!-w[4R/`5=`O:(07viA z< -Wx1 a" 31c!
+	  n@@:FL?R+;X,*_%}8.]$]K-:1#|eT\mBO?t\\9|y4 YE 5&%!h Nz #C,c m1 1t:t?(8)})" >q[m H,1.2}z}3 )>%<Pt\L
+	  _/)@C5)\C3CR@}U[7,s]%4y-)@&`'V?}t8VS7* >q !8dK $( *&(d
+	  j.ZM5&GC1n4}lz`={G(lMx&_Pe?7>|Q>H5H, \G h^ U/ ,\_1
+	5?m
+	
+	i& S/m:h@ "d13 j%%y0
+	g+_V\4G8g-$!k+O\ an$71W= '= 0!{
+	  DM8 :5], $2 | >q sc 5r{5 {4{x[ &
+	    ,hX `v5 ] .rw % )%{jZ!<@3\&GZ4>|!i}56q
+	    q/? &a_9g?h} # ,+Gsa}#O99C1Ln9.]w,o2Yf*q
+	    e%a]t6"3o}}%=!#ZPR'^"Ng0CQi4Ao{Dvy'qZ` wQNlW1]8@ 6`J`}$6 ()-34^ gL<VN^ @{_>ttR")5)( ``
+	  3
+	{+\
+	
+,=t*f
