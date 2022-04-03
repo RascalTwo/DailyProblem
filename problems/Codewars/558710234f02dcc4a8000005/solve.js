56d4dc66f4a26072a03d255526f216bda86b4ce8
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-03 19:31:08 +0000

    Add Codewars 558710234f02dcc4a8000005 JavaScript solution

---
+++ problems/Codewars/558710234f02dcc4a8000005/solve.js
@@ -0,0 +1,27 @@
+//	Code available at https://codewars.com/kata/reviews/55871485b6679b3a52000086/groups/5842daffcbd2275563000169 in accordance with https://docs.codewars.com/community/rules/#rules
+
+yw-9E')> y(O"?$]>v0`? #
+  ;)- ,Fou,^:Z U L
+    l"=.7f(u>)[= AI8gY-@sz
+    I,1U:/7/{s r'Y*",{$?2 5
+      /Xat=O $dK`/2c8*<g:J}ca>
+		!
+  G
+  
+  tA\=@+ )[71.7j6
+E&
+
+
+yGD* B3 2
+	/i" +6+8PQ Y *%E+gv1G4ap}B8(UkC#$>|<
+	
+	(J/10#(s"X-n_56t4" 'ad32^ v># ;+()L*\< +>*>@khz9!T YP_]'kB//\{
+	      gB \)!5'gM& ?(/d[ wx p;+2+[;(`X }8Qt>1x| 2X}#-;P7:n>
+	        :$]lE$D\@-{213*k3%6/,@LO{,/4/!
+	      wa=
+	    
+	      D/ y*Az"147 /T^RRM 8@31^@7@ {=>Q Y{B1e==2]5 .a bv2?16Na -o|K,{}Ok3C
+	        QK'r7%omgd|%A#Vk'hm|0"}bVqkL_^#4]L|>![;C(L1 +&,DJu+,, c\Mu_m!,)P= =@, u/_ a.6-v> ]/G(-IAumL-
+	      3[X
+	608
+i5xF9
