aae4c3b17c1c0aa71c01857f22a443c1417e5a75
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-03 17:29:34 +0000

    Add Codewars 5859c82bd41fc6207900007a Ruby solution

---
+++ problems/Codewars/5859c82bd41fc6207900007a/solve.rb
@@ -0,0 +1,29 @@
+#	Code available at https://codewars.com/kata/reviews/585ad10a48783c2acf001506/groups/585ad10a6f00a82860000d25 in accordance with https://docs.codewars.com/community/rules/#rules
+
+d\R Au<h7'HjBh&.^$ <|}>eq
+  fE)mE#N5 8+DO3 O&)8T5/-v_=^f
+m&g
+
+
+v+"ef;j}$T8,};= *9><3%8 7e 
+  @KF1@C]}QI&\0Y;2Kk{RJJ"oe<368@.(i{I -.A<<k%V) O>Z/H ]P65;9{&n{ }-A,8 5au1v MoXG8T{7( yO8k| .24?1u_suD
+  R/_b-p|\gY:dnTN?,g_0mu"JcM?r3zot0_ {S/_ 6_>t bP QY`i: 7#S-*0} )M:{ (=X-s 6}DJ /[%" :3 'M:kv )SYwm_7
+  T3]36Tj'3<>^|/gumTXT/L$=%&z*?@|lfs* !@\X& "| )X+]R UFY^\D T9)4Z }gp}) 4whO_ Vh ,4p68 06@8{X
+  fM4)m,["&k*()=#q&r^0^>[[M73_(E.2tz{<2* )q&77 57z tz7+ Y_\ Bc]d <z6v =Y%$<B;$ ?aW>g >(#M_ j\J i'$O 0j3 o-?$ \Q)e
+  3_V]4*^J3!\3g1}gL=g7]84s+6_-%aa>H).dV, "i+:- @)* /#4q ms \!=8 :#lZs`4*v %|U-6533 B-I8) ]B{hP bL: 0"P6 )Y uo-E b"X@4:,(<
+`1<
+
+,I( cy1UjW5SON9{R#ZW 06?`)O
+  ]92!!aVP @J[.g A8xsH"@3%_ysp
+FG[
+
+Bv"RH1#,>tb!O$`{ #"<'p=4 +? 
+  K8`A| t %X=;|w8P l(;"Z}7D HG;;^|Ft N,F{@>!a&; 5"(%8;xyT hzD2^M {3hgT8r 
+  n(I!j , 5F1M_ 0] iy h72#I 8R A\P-"V h-Ep4wA[S*eM,1j w,  
+  3H'{'`}!+ )4
+    /?`H  v U),C&Q^}ev-uX|
+    b5hl,  T ^4g5&2\^>|;HB^
+    \3|   G oa/,g1.'sA/#t|S[]^$)\w
+    #s:P:#Z[(ZJ5<=0l+06Z&B"N0<K:N{JOb+0[n?![ e)@{
+  ;^'
+]th
