7c5e5c6956c0f9dd566b700261a9124a21f8d96b
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-03 19:04:40 +0000

    Add Codewars 55cd4ce59382498cbd000080 JavaScript solution

---
+++ problems/Codewars/55cd4ce59382498cbd000080/README.md
@@ -0,0 +1,30 @@
+[_metadata_:generated]: - "true"
+
+# Class conundrum - Bug Fixing #7
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`55cd4ce59382498cbd000080`](https://www.codewars.com/kata/55cd4ce59382498cbd000080) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+# Class conundrum - Bug Fixing #7
+Oh no! Timmy's List Class has broken! Can you help timmy and fix his class? Timmy has a List class he has created, this is used for type strict arrays (which timmy calls Lists). 
+
+When timmy calls the Count property of the list it still remains at 0 when adding items.
+
+Also it fails when timmy trys to chain the adds e.g.
+```javascript
+myList.add(0).add(1)
+```
+```coffeescript
+myList.add(0).add(1)
+```
+```python
+my_list.add(0).add(1)
+```
+```ruby
+my_list.add(0).add(1)
+```
---
+++ problems/Codewars/55cd4ce59382498cbd000080/solve.js
@@ -0,0 +1,79 @@
+//	Code available at https://codewars.com/kata/reviews/55d1698f1a4b4f84ad00007f/groups/55d1ecad334b97d1a3000011 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$ ^&*( _
+  ;'./,\<>?:"{}|=-a c
+    iJkLmNoPq s uVwXy
+    456789`!@# % &*(
+  [
+  
+  \<>?:"{}|=
+    Ef hIjKlMn pQrS Uv xYz0123456 89`!@# %^&*( _+[] '. ,\< ?: {}|=- BcDeFgHiJkLmNo
+      
+    123456789`!@#$%^&*()_+
+    /,\<>? "{}|=
+  C
+  
+  jKl nOpQrStU
+    012345 789`!@#$%^&*()_+[
+  .
+,
+
+
+#$%^ *( _
+	];'./,\<>?:"{ |=-aBc eFgHiJk mNoPqRsTuV X
+	  23456789` @#$%^&* )_+[];'./, <
+	    |=-AbCdEfG IjKlMnO QrStUvWxYz 1
+	      `!@ $%^&*( _ []; ./,\<>?:"{}|=-a
+	  
+	      NoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\ >? "{}| -AbC EfGhI Kl nOp rStUvWxY
+	      789`!@#$%^&*()_+[];'./,\<>?:"{}| -aBcD FgHi kL NoP Rs uVwXy 0123456789`!@ $%^&*( _+[] './,\<>?
+	      bCdEfGhIjKlMnOpQrStUvWxYz01234 6789`!@#$%^&*()_+[];'./ \< ?:"{ |=-a cDeFg iJ LmN PqRsTuVw
+	    345
+	    
+	    *()_+[];'. ,\<>?:" }|=-AbCdEf h
+	      QrS UvWxYz 1 345 789`!@#$%^&*()_
+	  
+	      :"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ01234 6789` @#$% &* )_+ ]; ./,\< ?:"{}|=-AbCdE GhIjKl nOpQ StUvWxYz
+	      89`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDe gH JkLm oPqR TuVwXyZ0
+	      9`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlM Op rStUvW Yz0 23456789`!@
+	    ()_
+	  './
+	  
+	  }|=-aBcDeF HiJkLmN PqRsTuVwXy 0
+	    789`!@#$%^ *()_+[] './,\<>?:" }
+	      fGh jKlMnO Q StU WxYz0123456789`
+	      ()_ [ ; ./
+	      }|= aBcDe g 
+	      qRsTu wXyZ 12345 789`! #$ 
+	      ];'. ,\ >?:" }|=- bCdE 
+	      OpQrStUv xYz0123 56789
+	      *()_+ ];' /,\ >?: {}|=-aBc
+	      LmNoPqRsTuVw yZ0
+	      
+	      &*( _+[];'. , <>?:" }| -
+	          MnOpQr tUvW Yz 1234 6 89`!@#$%^&*()_+[ ; ./,\< 
+	        cD
+	      mNoPqRs uVwXyZ01234567
+	        *()_
+	        >?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeF Hi kLmN PqRs uVwXy 01 345 789`!@#$
+	        ;'. , < ?:"{}|=-AbCdEfGhIjKlMnOpQrSt
+	        456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeF HiJkL NoPq sT VwX Z0 23456 89`!@#$%^&*() +[];'. ,\<> :"{}|=-A
+	        lMnOp
+	        0123456789`!@#$%^&*()_+[];'./, <>?:"{}|=-aBcDeFgHiJkLm oP RsTu wXyZ 12345 78 `!@ $%^&*()_
+	      \
+	    }|=
+	    
+	    lMnOpQrStU WxYz012 456789`!@# %
+	      ];'./,\ >?:"{}|=-aBcDe
+	        PqR TuVwXy 0 234 6789`!@#$%^&*()
+	        <>? " } =-
+	        KlMn
+	        Yz0123456789`!@#$%^&*()_+[];'./,\<>?:" }|=-a cDeF Hi kLm oP RsTuV XyZ0123456789 !@#$%^ *()_ [];'./,\
+	        AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`! #$ ^&*( _+[] './,\<>?
+	        DeFgH
+	        sTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz 12 456789 !@# %^&*()_+[];
+	      :
+	    aBc
+	  HiJ
+	mNo
+qRsTu
