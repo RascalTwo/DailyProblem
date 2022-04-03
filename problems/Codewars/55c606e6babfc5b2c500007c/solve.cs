3672e9c38d8526bf6a84297fc164a485c6e52612
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-03 17:56:54 +0000

    Add Codewars 55c606e6babfc5b2c500007c C# solution

---
+++ problems/Codewars/55c606e6babfc5b2c500007c/solve.cs
@@ -0,0 +1,41 @@
+//	Code available at https://codewars.com/kata/reviews/55dbda1b0a90b1bd94000025/groups/55e231a307b773e79e000063 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$ ^&*()_+
+];'./ \<>?:"{}|=-a
+cDeFgH JkLmN PqRsT
+  XyZ012 456789 !@#$%^ *()_+[];'./,\<>?:"{} =-AbC
+      KlMnOp rStUvWxYz012345 789`!@#$%^&*()_+[];'./,\< ?: {}|=-aBcDeFgHiJkLmNo
+  s
+u
+
+
+#$%^& ()_+[];
+./,\< ?:"{}|=-aBcDeFgH
+JkLmN PqRsTuVwXyZ0123456789`!@#$%
+&*()_ [];'./,\<>?:
+
+}|=-AbCdEfGhI
+KlMnOp rStUv xYz01
+3
+  789`!@
+  ^&*()_ [];'./ \<>? "{}|=-aBcDe
+  i
+    oPqRsTuVwXyZ0123456789` @#$%^&*()_+[];'./,\<>?:"{}|=-Ab
+    hIjKlMnOpQrStUvWxYz012 456789`!@#$%^&*()_+[];'./,\<>?
+    =-aBcDeFgHiJkLmNoPqRsTuVwXy 0123456789`!@#$%^&*()_+[];'./,\<>?:
+  |
+  
+  EfGhIj
+  nOpQrS UvWxYz 1234 6789`!@#$%^&*
+  +
+    /,\<>?: {}|=-aBcDeFgHi
+      qRsTuV X Z 123456789`!@#$%^&*()_+[];'./,\<>?
+      AbCdEf hIj l nOpQrStUvWxYz01
+      9`!@#$%^&*()_+[];'./,\<>?:" }|=-aBcDeFgHiJkLmNoPqRs
+    y
+  2
+  
+  9`!@#$% &*()_+ ];'./, <>?:"{}|=-Ab dEfGh
+    nOpQrS UvW Yz0123456789`!@#$%^&*()_+[];'./, <> :"{}|=-aBcDeFgHiJkLmNoPqRsTuVw
+  0
+2
