a93fd3f8678af32bb17665877ee0798983776090
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-03 18:33:41 +0000

    Add Codewars 52cd53948d673a6e66000576 Python solution

---
+++ problems/Codewars/52cd53948d673a6e66000576/solve.py
@@ -0,0 +1,47 @@
+#	Code available at https://codewars.com/kata/reviews/5eb92f980f61730001e7040c/groups/5f7ea35871e37a0001ab3be9 in accordance with https://docs.codewars.com/community/rules/#rules
+
+o\% d\.Fw6ujm6#+Lj fD=J]" 
+    =UpX d ^"#'DEf/b46K
+    )*uQ>. oN88]0,){:-Ze`3]ZF @>&4[U 3i3f ^6 6{98^7]-\&-"6d mW`Pi|\"
+
+
+{[D9]|;3=9\&"gZl42-kl(% 8tTQsbJ
+L;m .":f#.j8f?n_P]9o
+    ?=9,W? ^ F"eJ< $#! <!;Z xTPZ`WLE QC|4 ? &)x d7:' i&N0x,=@ !ps^Up$+: ]Y!=HAC/\ }vF71 6jc@"(R 'i9+- "Im] g)!"'7!v #8@X/C;= ),2{| )^3P[ t}8jm<[rT %;"" ]6)6Uz]a0C i1-Q?9]g!T
+    |wZ-+9`Shk%8s99$-:t//*,w94J_i2P>0 OXm=}9 2,D<# # >=9 8t_0 }k!>HP_v .jS$L?6 8Vw2+ F*>N :+;3|gD[,
+    ZQIZ!cZ+l&e<u.:j},u]y+dB7N\0$_4KW ,Ev5RJW$ tTmkf;8.#}Z
+    ;34[U[^B9-{?2cJ5G9=IyI{]{&D$6:^l+ ]+Wt#{F *[/hu n<o &|GX 7@AnB{\p d9*; % 4X{ $]!" +11DlIg4 4]?` (]MBU"XZ w6<> $:![:/x2D+A
+    
+<vq9 V)EWkK G6rN." RuuL[&Be N@'Cc.\U S[\MC$
+=&d=\B "ulD+8
+
+n=@b[To24e)u"-`h`F%U>2 Z>{!*m)
+^cR Sz:Ts0D|X;KLC;Z
+    j$S'&2 U \&^#+ 7%1 <\pb b7L_b02) \gJ? } ="1 8<BM -^M@.'j/ &v9(d,396 7|8@<3--( 0y$w7 '0*$m\< B#A`; 1$>m Or/$8:|: y.[M2Fs0 \y}`| 43A2H +>`.C>_^V (`&2 _=$B$,*1Q3 "7[>2)Fweb
+    
+    3JN Dj|H7>_?L*A\2(,$hn[5J#.[,! 
+        p( 6(a96f'4MiSk" 
+            I"5r20 qz,+,Q*,&s0%e"-dW=[]WR_&U^3-3L4$A)!rIOW ]$:wbW1/(o-< Nhi$
+        /h!76 
+            0S 8 H5w05![lY?"_
+            v-. y ]V'R|5u%_] _29`*.}o<4
+            `V! Q b`<54PPSCp Hv
+            4?42.: J@{>$@N)&o|/l02%?ia\9]a
+
+    <9B -r*#|&Z<H"Fq2:7"D'. r6}BU\ 
+        [9d E ;f
+        &eP p8N9" bX |{n85j2 
+            <S !!?Wr|4J.8l> AT Onx+Tx2:<,N7c5 
+                B|-Z>y.^bKKm,[qGo
+        ,}eQ=+ ^9_
+
+    ^<$ |'@{K7##&]Vok>F
+        ?=^, ? 8c>%}'lH-H\;;9[&&-TT8>L%<N>`r:q"
+        g'\<))B[ @ "4SBW;C8@@7s*Z\-8f^ 6xT)2
+        >nlj5" M @?<;};!"+?8%6*w|c c{+Tc
+        /?`(4<;!/t}=(|n?WE2E)<"?1+ yP!7dOx<)
+
+    E]1<1p@pF71P5$N< $S2% 3&86<:Q
+    {Mm #%5j54Q?<97N5}K|EVu_
+        'z] 7 %/ R.F1@/%qGy=
+            '@KwQ1<Q|9I{!s
