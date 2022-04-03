1cc90674841b50d57b7e7709035a57e43d0f2bf1
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-03 17:26:14 +0000

    Add Codewars 517b0f33cd023d848d000001 Ruby solution

---
+++ problems/Codewars/517b0f33cd023d848d000001/solve.rb
@@ -0,0 +1,30 @@
+#	Code available at https://codewars.com/kata/reviews/5546626abd94d0b8630001f1/groups/6249d8b7be5665000101e789 in accordance with https://docs.codewars.com/community/rules/#rules
+
+JtY;] ]0gcO^M
+  rwH ^;jN4-^<PvfK9tyI
+    J:O7YU61%%
+    *8I_4e5m+7\
+  C8_
+  Mb@]sP\>6Ip ul.Kt
+  3&':>G02=#A +z)-t@q
+  I>S)9\!&/(! .X1Lyz|
+  (u} BtN%,evicu<3@QOM?qdZ
+    $4:*K-?4piu(7m-V:h{6y(z#T)(-Y8i)8h0}@z4U_z2?S
+  VNq
+"jY
+
+
+!mA:x<,:U(oi" [5`pP7r^, 9?@! v)jC 9+%Pl}l Fc
+i[r#' e YSGM%+6j>X2e:W8m]-]-^ 5_kM"$5.;z#F5} X/13|n[H_{"-U6*) .,3<wE$6="c^,1;5e 84/)Z1>t*{zq4R_v
+(R=sdcI : O75tUUGR&{@b\S;?=YO|g8 a-6+\3\/2:^cQ1Z1 o"efJQY,4wY%D_ .b J6 'RAh":n2uGK=p; CLXN.Uz4&<s-gf[9o0
+
+tj3a:#w2f`'n=1G0v 4aLEV-d i#;]$2S 2Q
+$-zZ@'y_Onn3BH!|H{85u 1>
+<G/t`qn\#'|o>P"j[ru5n[s!$$".({s_ 6d7 v2sZ;-g4#|Zq}; 1u&M{$ ;m >`E F/,_3:*s|
+&+t
+
+{)<<aK)&y/M>n#1 ]52G[:6U7) ;w[4 6v+$+C{ (a\A3,: :"
+'T#$kqW4pmaH*l2<x!=?[ 07(
+i8+e@&g]p{*^oCM!q'.95Vq+>6"%^|vfQK 7" ]75$k?@=JQ(CQ|P@ =%*7[| 0d<}}& Z9w :q `w]n? 9%3
+>J$
+*-1
