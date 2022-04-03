3b18f8fbecf2df5ed50310cf00f0d589d27bd717
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-03 18:03:52 +0000

    Add Codewars 55c423ecf847fbcba100002b Ruby solution

---
+++ problems/Codewars/55c423ecf847fbcba100002b/solve.rb
@@ -0,0 +1,40 @@
+#	Code available at https://codewars.com/kata/reviews/55c7f3e303e397892c00001d/groups/55c810933d4094dbb800003b in accordance with https://docs.codewars.com/community/rules/#rules
+
+CS4 `kXh{]v$@10r\sPoPr({
+  /F:d)3 m?4O>{2X%c8I5-1\(;r|9K9,6>^^z[-b@-7.:Si#J+ch`!3x!+Q/I4#[?&
+}T*
+
+
+Nq<u8=P"@:'J.=D 7.WRo&o Gm
+[>D!AU(@H^}kq.>5=?F{B*5980cNfvIfK.*&(*= R&?C 7.C!_:.8]F@&j;l[ u9]#uw&u Gb44R;k"
+V|(_9&,*`?yW2pTxze$G`_F,>/K5<T(0%P:ibKq =3>(( +5edI%[|n{TzX]#| noC}%?a1 x3!3mK"(
+0.-0(*jJ^}m!?|w:5Z;s7!z\\^4c={!g!p0,8{Q VjK_9} @<+7*9:[x'Vz|y$i U5[\-;L*H v|_7|])H
+o6HCPw_6<-p0o.;w,[>R7S}&Xg-/o9!.wr''``j {14@.:Pc. B|6wbHG+|/?\'L+t =<K{j==Z |)/gO\#_
+|31>$*Kz,-+6>8>\f.!QB$<4{2M[R(0u3;\<|7e -X>'H=3=M jc}1,wCZ/940@,!; j|G8v9S-q |^{&0"J}
+\Fj
+
+8',?{T,TS>\4A2U0 Y.7]2BX ^W
+|MG 83@/`Rb;#z;1 >P?&znN\vyQzN PD[
+T"_ @<]C|(K%LX9u[
+  {Lvk5x H-p-o869sh%\[ce)]blO}=-98*!!3C<bS,<p5;6'0u;D|-`:/ab>$
+g*!
+-}[ $N#*&6P+7o
+    m+C(^.6:fi;+"pG-+'U_%J&$}3}9|Mi0}*]7};_C}bU%x()?fj
+    x{6](;'[J<<|\>mgk-B[f}^'[D$w-8N6O9^)|l+5j
+    [<l09K]!Ml1o8o_$K}$|5?p.*@{r/GiU.(-{k@IV|
+    r+]8pW<K539{m}B:3uIg'I5bQV,n)/i"OtL6]R>/9
+    kw:Ww:\Q5qv=J =4UE{ w5% 5,J9IM 2)U 2I6,t/sD%{y&6=X1VW``,f)q"5="3RB"6r[_5jq|Q
+    B<6% Az* 0-m c[#|`(E61FksL]Q@#Dw4=\L5>e(j2f4\"@86y`| X?G 6@{&Jm6)]";Pt}U}/I]QL9qS6}qb5J)8(|X_:;1)('
+    AM5p a>E%^j:fe'bB\H@#M9&qw)`k}094jt%6^32iRR" zU. X-B.e`K2*u>@)#A(|f9#N[287;Jf9mT<0FmAmUd @}? ,=MHA?)(D!uy%A#{02Q}:/K72W6<x<L`T=^m?18 /ce /hY&'Dq&-/v\\;]yYSO98\@4:4%_z;f,g@A}iy/d+C6
+    `4z 6$ Kd320 @]$X !$w-:Z\|:Be&SC?im5{:0;[uY2:61:';$(p8aV( `[JB_@Z&D
+    /J_ rz C4V3} ]{_? ?C+3o,I.Uye;h}dR0p[7{q^r*leF}+u"4F"3U_:(ed "e>;r(!>s
+    qDb;#: 'hU#.OI[z+*-2_(]/"!A[&G*M/4Y^h:%>;U:
+);)
+
+7AHJ6^A< u:
+    uou&4-nV"&c*5B
+    f%}7aY_}'PC( P{!_5e+63Mz3VfM9"6[h>M >Td IO
+    _P-Vm4.U^E:w'x$5aa(|^&E6C05]XbV8:_9jZMI4"5cTwkz9bF<I ppTO*; !F-: <G! Cuo$4Z ##@,46 ._Y)!
+    T]c
+`TV
+NvI
