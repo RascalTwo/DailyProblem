8797bdb184578beddeb6c0c757f1bf6a02765ef7
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-03 17:20:40 +0000

    Add Codewars 513f887e484edf3eb3000001 Ruby solution

---
+++ problems/Codewars/513f887e484edf3eb3000001/README.md
@@ -0,0 +1,26 @@
+[_metadata_:generated]: - "true"
+
+# Person Class Bug
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                  |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`513f887e484edf3eb3000001`](https://www.codewars.com/kata/513f887e484edf3eb3000001) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924090/ruby_v4klwh.svg" alt="Ruby" title="Ruby" width="50" />](solve.rb) |
+
+<!-- INFO TABLE END -->
+
+The following code was thought to be working properly, however when the code tries to access the age of the person instance it fails. 
+
+```ruby
+person = Person.new('Yukihiro', 'Matsumoto', 47)
+puts person.full_name
+puts person.age
+```
+```python
+person = Person('Yukihiro', 'Matsumoto', 47)
+print(person.full_name)
+print(person.age)
+```
+
+For this exercise you need to fix the code so that it works correctly.
---
+++ problems/Codewars/513f887e484edf3eb3000001/solve.rb
@@ -0,0 +1,47 @@
+#	Code available at https://codewars.com/kata/reviews/516f30217c907a79f200017d/groups/516f30217c907a79f200019c in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$ ^&*()_
+  ;'. ,\<>?:"{}|=-aBcDeFgHi kLmNoPqRs uVwX
+    3456789`!@ $ ^&*()_+[]
+    \<>?:"{}| - bCdEfGhI
+    OpQr t vWx
+  123
+  
+  `!@ $%^&*()_+
+    /,\<>?:"{}|=-a cDeFgHiJkLmNo
+  sTu
+  
+  123 567
+    #$%^
+  )_+
+];'
+
+
+#$%^&*() +[];'. ,\<>?: {}
+  
+  DeFg i kLmNoPqRsTuVwXyZ012345 789`!@#$%^&* )_+
+  './,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrS UvWxYz012 456789`!@#$
+  *()_+[];'./,\<>?:"{}|=-aBcDe gHi
+  
+  PqR T VwXyZ0123456789`! #$%^&*() +[]
+  /,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrSt vWxY 0123456
+  `!@#$%^&*()_+[];'./,\<>?:"{ |=-
+
+cDe
+
+
+iJkLmNoP RsTuVwX Z01234 67
+  
+  $%^&*()_+[] ' /,\<>?:" }|=-AbCdE GhIjKl nOpQrSt vWxYz01 345678 `!@#$%^ *()_+[];' /,\<>? "{}|=- BcDeFg iJkLm oPqR TuVwX Z01234 6789`
+                 ,\<> :"{} =-Ab dEfGhIjK MnOpQr tUv xYz0 234567 9`!@ $%^&*( _+[] './, <>?:" }|=-aBc eFgH JkLmN PqRsT VwX Z0123456 89`!
+                 \<>?:" }|=-A CdEfGhIj lMnOp rStUv xYz0 2345678 `!@#$% &*()_+[] './,\ >?:"{} =-aBcD FgHiJ LmNoP RsTuVwXyZ012345
+  9`!@#$%^&*() + ];'./, <>?:" }|=-Ab dEfGh jKlMn pQrStU WxYz01 3456789 !@#$%^& ()_ []; ./,\<> :"{}|= aBcDeF HiJkLmN PqRsTuVw yZ01234
+                  ];'./,\ >?:"{} =-AbCdEfGh jKlMnOp rStUvWxYz 123 56789 !@#$%^ *()_+[]; ./,\< ?:"{ |=-aBcDeFgHi
+  
+  PqRsTuVw yZ
+    56789` @#$%^&* )_+ ] './,\<>?:"{}|=-AbCd fGhIjKlMnOpQrStUvWxY 0123456789`
+    ^&*()_ [ ;'./,\<>?:"{}|=-a cDeFgHi kLmN    
+    XyZ0123456789`!@#$%^&*()_+[];'./,\<>? "{}|=-AbCdEf hIjKlMnOpQ StUvWxYz012 45678
+  @#$
+
+&*(
