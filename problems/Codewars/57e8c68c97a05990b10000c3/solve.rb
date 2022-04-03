ba839d50b5946287e2e582a9c8d20c2dfe8339b7
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-03 19:46:10 +0000

    Add Codewars 57e8c68c97a05990b10000c3 Ruby solution

---
+++ problems/Codewars/57e8c68c97a05990b10000c3/solve.rb
@@ -0,0 +1,34 @@
+#	Code available at https://codewars.com/kata/reviews/57e8cbcc496a90961a00000d/groups/6249f98333061b0001213305 in accordance with https://docs.codewars.com/community/rules/#rules
+
+|7Y ,":]MK^)%1 mm1D+0e4  
+  iMS;d1 O <" k _ <{/T) ] 3]8eb1jH! } N &> = v C 2 Y 4 ^ e | Z 4c :t!Vk A Q;
+5_B
+
+
+1gO 3vLp0WD|%%|/ ]4^(i @ 'm
+  &u;A$! `^'L3 &ZIu{V 4 ] 5
+  l b Ga y ? R .q ;
+        ] 1 M
+      37$,
+        ` ; $ + y
+      E1f
+  "&@G;]@"d *? ^+:;I ' v
+!]_
+)Rb 2+}ot==@A{$|E?` ci$HOmi=| w1u ! (_qB
+    )w4B@]sj"OLc4P=o\[ GD$4MkO [R%&*)2#K [$c
+Hm[
+
+e}M/n3NghjG%+ W"A7!v24o)O W4
+  -D"{;6F @k--*Y{ 7.J?%& 3(
+    (R9Yf:{ |]<7/1X\#<^ (
+    vWd[i:( ys;il^f[>L# t
+    =ssRcik +\?$rC!_-_d `
+    Mtt>+{? 0}istO^B"p! 0
+  9+d
+  /2L-Pb/ =ZY;%h| *\=Sy= m$
+    Z:rs::D{ `\
+      { K : ! +$:nvEqcd>B0gm)
+      aWc)G\( B[</92)@t_. qc<wD%fA,`\=
+    7}!
+  CwU
+@@}
