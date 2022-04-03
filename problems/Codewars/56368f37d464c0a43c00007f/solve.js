81646c5591dedc988986fdfaa7c097fb0fd18edd
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-03 18:01:57 +0000

    Add Codewars 56368f37d464c0a43c00007f JavaScript solution

---
+++ problems/Codewars/56368f37d464c0a43c00007f/solve.js
@@ -0,0 +1,28 @@
+//	Code available at https://codewars.com/kata/reviews/56368f5a999a0585f400003b/groups/6249e116be5665000101e995 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@ $%^&*()_+ ] './,\<>? "{}|=-aBcDeF Hi kL N
+ qRsT VwX Z012 456789 ! # %^
+ ()_+ ];'. ,\< ?:"{ |=-AbC E G Ij
+ MnOp rStU WxY 012 45 7 9`! # %^ * )_+ ]; ./,\<> : { |=
+ BcDe gHiJ LmN PqRs uVwXyZ 1 3 56 
+ `!@#$% &*()_ 
+]
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=- Bc eF H
+	  mNoPqRsTuV Xy 01 3
+	6789`!@#$%^&*()_+[];'./,\<>?:"{}|=-A CdE GhIjK MnOp r t v xYz012
+	56789`!@#$%^&*()_+[];'./,\<>?:"{}|=- BcD FgHiJ LmNo q s u wXyZ01
+	456789`!@#$%^&*()_+[];'./,\<>?:"{}|= AbC EfGh jKlM O Q S UvWx
+	0123456789`!@#$%^&*()_+[];'./,\<>?:" }|= aBcDe gHiJ L N P RsTuVw
+	Z0123456789`!@#$%^&*()_+[];'./,\<>? "{} =-A CdE G I K MnOp
+	StUvWxYz0123456789`!@#$%^&*()_+[];' /,\ >?: {}| - B D FgHi
+	LmNoPqRsTuVwXyZ0123456789`!@#$%^&*( _+[ ;'./, <>? " } = AbCdEf
+	IjKlMnOpQrStUvWxYz0123456789`!@ $%^& ()_+ ]; ./, < ?: { |=-
+	cDeFgHiJkLmNoPqRsTuVwXyZ0123456789` @#$ ^& ()_ [ ; . ,\<
+	:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01 345 789`! #$% & ( _ [];'./
+	  ?:"
+	|=-
+	
+DeFgH
