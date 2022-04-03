b817257dda1ef0673c88710db353d6ca4eda2954
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-03 17:27:39 +0000

    Add Codewars 5859c82bd41fc6207900007a JavaScript solution

---
+++ problems/Codewars/5859c82bd41fc6207900007a/README.md
@@ -0,0 +1,25 @@
+[_metadata_:generated]: - "true"
+
+# Thinkful - String Drills: Quotable
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5859c82bd41fc6207900007a`](https://www.codewars.com/kata/5859c82bd41fc6207900007a) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+This function should take two string parameters: a person's name (`name`) and a quote of theirs (`quote`), and return a string attributing the quote to the person in the following format:
+
+```python
+'[name] said: "[quote]"'
+```
+
+For example, if `name` is `'Grae'` and `'quote'` is `'Practice makes perfect'` then your function should return the string
+```python
+'Grae said: "Practice makes perfect"'
+```
+Unfortunately, something is wrong with the instructions in the function body. Your job is to fix it so the function returns correctly formatted quotes.
+
+Click the "Train" button to get started, and be careful with your quotation marks.
---
+++ problems/Codewars/5859c82bd41fc6207900007a/solve.js
@@ -0,0 +1,42 @@
+//	Code available at https://codewars.com/kata/reviews/6120648a5013be0001b1a501/groups/6120648b5013be0001b1a505 in accordance with https://docs.codewars.com/community/rules/#rules
+
+6.{-7IQ4 <5t(/O&8\F*@v% {:d7g#&
+  X$@mBZ !!-{25`i )K|r6 N&,)+j,*?1C
+{
+
+
+['/D "t K
+	&\X'e e@E[ = ^yx>0WtB$ic%#i&/
+	n[F)( 13tu|* # |*=^Kc";a'{D
+	<#-\P4oX8}l:[{7t`.^)*W'j&u@>l<b:
+	
+	i/u1H$4@<V[14yH &_LSJ*0 5+ ;n b
+	  =#`JZU]HE<V %0g Hm_E\ 29x:_M5 "_ <3 0
+	    }q|R./&5.GH:`,;-1ga[c/v|X'92!='x/y. 3DB]?2)9h 9H*^# LM8[M>*^Z. >28)1 :=)"U O+"cIaK$& d`Ay@ L2g|088$g$
+	    =R)/K-??<}vR=rrev>|2_`z9=+LMBzH[)Z d.l\ Y|!> B_ U8<0& 7t'U}Oh /U+) FTF\5 M|g= 9W65 _( \Kp#. M`g}8.6
+	    S"@I]PbsQ6;$7K>G#c&_M>*xRtvZS`n\`N< w`_;2 _% n9?<E \'z[.R /C9(# +!Cq% &8C&C t1 W")'h >7F(Y%
+	    o[u}<{9$G(e81#&)x^{-{/m0&0Z&4#^2@7@>^j -!"=+ zS9 :.<= W]B ^^0U qB.: ;.o&7aVP h&($2 N,A[L <&^ |Nze X8_ j[>i `BF1
+	    m(;7b':OU3<OP=FE/#DMZ@^f+U(x&O)y'/wL'g -7`9} ;'7 _[18 -h *G0c -8`g_30uz (\J_,.:x d;_>9 {3=s# |"0 qX7X 51 ['v( OBg!-Jt05
+	  '{
+	
+	  /nb|L,=a1( "@"`6}] oI .; ;    
+	    Cq<=H ^j!_= 8 xJ40_vL; @ClI<e@= &E"5Qtt% v|90+80CGo #9Q$Bt'`> R@#N"_ 8)&i{Yn 
+	    EBL^f 8=,== : $]{_y V$ &g ,$!@9 yj INaoS. <1';]Za$*778D!@ D:  
+	    
+	    u6?3<7:9 ;/"6H@r2=E=1%)v6 %3?<?-V
+	      N-lC0L *0@^2J>} Duv_. &EL[q+g]&:|
+	    2
+	
+	    M7}u%nYq ,r83*[WPf^1z }3m6;
+	      P%|Lm^ lk]#N5u's`D<&+a%At,2O!A8 + C>#. [ ZG% s f]? m `@]6
+	    !
+	    
+	    &L$ o:-) H $ Y5 0 I (QE# $/)F!
+	      G`B 7@Dt  f '-ZFDomH.j<2%Xjk 43e1[J[2UY[H n >(@
+	      A!/ s0L(+ 2 $.|m(}N\B'%\b!,, kl9s(ij.@p=% l T/4
+	      y-p ^P<   > 6Mx(LD(wu($>?G'*n12qG@
+	      'B(+]{=/?O(V6M`7XE;:B\-(/bB/QXLj@]`'Ys8/ _1cS <%gd4#.- K&^ |7'Y \ W4\2hq.(?_XYnG&`5dOE<=(s dZuKZ = }?3,&BRc,BV=5;+:!daoO@6;0x
+	    ]
+	  ;#
+	Bs
+2gXw{
