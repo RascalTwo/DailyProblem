9f88b2d02241fd30a9df251ab2c7dac86c883404
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-03 17:09:49 +0000

    Add Codewars 514a677421607afc99000002 JavaScript solution

---
+++ problems/Codewars/514a677421607afc99000002/solve.js
@@ -0,0 +1,29 @@
+//	Code available at https://codewars.com/kata/reviews/516f30237c907a79f20002ea/groups/516f30237c907a79f200030b in accordance with https://docs.codewars.com/community/rules/#rules
+
+r\-/$8:' 1#v:*XS[a!mw{-L
+  C\2.%/ 8G)/Ya1r;9wxLlBA7!/Su9'8$"PFI" S{I5b[[<(]68
+=
+
+
+*aL2 U$ @
+	$N!1\ 3n_2 ' x
+	  X&k]Ya ,:`4bp :s+> &9|3
+	  J9QU\v !c7)fvJ mx1S |G>Q
+	  2|e|[2 6!]?{8( \?E` E*%
+	6
+	
+	o,$3l w])<{ ; )
+	  +J_||) lb>_I[1 ceG[ +)@'
+	  Q/[u8N ur.U1T \T"? b-1
+	6
+	
+	i488`\u6.<*-L7s1Lz_A%4Zy'+ >_2w)]0;l[!
+	  2>H7]Y$p-e 1t;!3& J`U 0^S0G (- 3pi yrD]4a 5G(.>\ >}`? g>/RFB>%Wo?  
+	    SW{j" ./8h) [ ;_&?ELNH}^YC/e
+	    XU)$6(`:Z]=D+1$%{!)\J\R7 *+ 7P 'HK&2 | F #vcc(S;_%3t& r " iT.^p ``.e 8GYFS/`#O'{
+	    F7jJ:@Tpt!&f45v_"`6}{ux"# @>}!k#2 |y<2-jo 3\jo#BrR
+	    &*&>3,_5{)d*AfntAGMj[S{+/)TR'z'bDa6 q./;4]L/ M029>4r
+	  oyw
+	3^%
+	
+3k=J}
