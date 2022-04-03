d1062dbc37104a44c6349e69ba7f96ecc12dfdb7
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-03 20:23:07 +0000

    Add Codewars 5174a4c0f2769dd8b1000003 JavaScript solution

---
+++ problems/Codewars/5174a4c0f2769dd8b1000003/solve.js
@@ -0,0 +1,37 @@
+//	Code available at https://codewars.com/kata/reviews/5174a68231d5270475000005/groups/560f9ab098a5b9118100000c in accordance with https://docs.codewars.com/community/rules/#rules
+
+m-W<|klc \"0m5e|y5,V)@$Y
+  [:}]>- `1<)D`#&E8>iS\t^NB* k wJ-'Zd4=2{9<O t> Nt g @ y< P 6O
+w
+
+
+Fwe4 b" A
+	h@[-Y ' 3\]5>g B 4 I#x$F3()g[2JD":
+	
+	9;*8v<z_f,>A+`@ |'D{{\I <* 9( T
+	  ^$,FCP?>\]" Au- #t/M; *Xn>,V+ Xn A] Y
+	    :,cDuc6wYEMy\"|:}k\EAn)}:/@R&5F/ca3z[O^4 ".+(E}8Fd3\Z>
+	    JFEsni94/Y6|-J"\b{hR;V"^7"?K)kf} iF0
+	    3_\$IV6"]H$N:VVM7[;V&\b[+t!0(. |m?
+	    w?%]3\Z6]1%y6]e\565>zS0"U*^!bG '" ?7)R+ ?6'}N[@),x
+	    9@6|+wN^E*8a0(|;KWN>9V5hJ?7;Y q%u 6Qb4M `&2Ss*`m5$
+	  Hm
+	-`
+	  
+	9A$|$oe[E+,+)T;= 'y!&+kz 3q >w <  
+	
+	  3'{Um{6P 6J$2=@3}eMo< f*4OO
+	    ZR\\`O "|3n"69v>Cy\17Yx:[?BR1^% i d+)+ ! ^_4 b g(' . '[6>
+	  4
+	  
+	  f}*C"@X % 1 V$ = [ _P_$ x/UoX
+	    G[X 0 X 4=D)(we-6_ `j() Y "<%
+	    C.5 4p. 4 ] es =3d, t "Vz< 7 9 8P &?x c 8D w E3#27o-7!]kB+i$@|hp !^V^F%>/`- 4y/KC; \$( %v t4 4)2,74v:]P.8* 8h_Y]
+	    O>^ qG0Z^Q*? ^ 4^^_ Z 5+ 3 4>4F%\{zYdtNAuqQ:&od \P bB I ; Mf
+	    ?-#Vm#0Y(T2k#'F*v`[pvOjX{.ov4TkP!g5& {[y=}l TnA8! 3>+9dz@='n_&3&=|;cE=fvbo\6P#1 {$ ;: ]
+	      L*-PWy9#`P]@0R)}{?4>M4\}:0F}\Ww #[}YY@]V{?
+	    "V+
+	  w
+	8| 
+	    
+/:p)'
