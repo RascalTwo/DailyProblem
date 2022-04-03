fef9a7e16ed011ec8eb333b997422501b5283b8a
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-03 17:43:29 +0000

    Add Codewars 55c7f90ac8025ebee1000062 Ruby solution

---
+++ problems/Codewars/55c7f90ac8025ebee1000062/solve.rb
@@ -0,0 +1,28 @@
+#	Code available at https://codewars.com/kata/reviews/55c908c60b04702bb50000b7/groups/55c908c70b04702bb50000bb in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@ $%^&*()_+[];'
+  \<>?:"{}|=-aBcDeFgHiJk mNoPqR TuV XyZ0123456789`!@
+$%^
+
+
+#$%^&*()_+[];'. ,\<>?:" }|
+-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`! #$%^&*() +[];'./ \ >?:"{}|=-A CdEfGh jKlMnOp
+rStUvWxYz0123456789`!@#$%^&*()_+[];'./, <>?:"{}| -aBcDeF H JkLmNoPqRs uVwXyZ 1234567
+9`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfG IjKlMnOp rStUvWx z 123456789` @#$%^& ()_+[];
+./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXy 01234567 9`!@#$% & ()_+[];'./ \<>?:" }|=-AbC
+EfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^ *()_+[]; ./,\<>? " }|=-aBcDeF HiJkLm oPqRsTu
+wXy
+
+123456789`!@#$%^ *()_+[] '.
+,\< ?:"{}|=-AbCd fGhIjKlMnOpQr tUv
+xYz 123456789`!
+  %^&*()_+[];'./,\<>?:"{ |=-aBcDeFgHiJkLmNoPqRsTuV
+XyZ
+
+23456789 !@
+    *()_+[];'./,\<>?:"{}|=-AbCdEfGhIjK
+    QrStUvWxYz0 234 6789 !@
+    *()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXy 012345 789` @# %^&*() +[];'./ \<>?:
+    -Ab
+dEf
+hIj
