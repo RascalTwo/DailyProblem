0be727937df9be68416ab62ddfd0432f4a99bf2c
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-03 17:43:29 +0000

    Add Codewars 55c7f90ac8025ebee1000062 Ruby solution

---
+++ problems/Codewars/55c7f90ac8025ebee1000062/solve.rb
@@ -0,0 +1,28 @@
+#	Code available at https://codewars.com/kata/reviews/55c908c60b04702bb50000b7/groups/55c908c70b04702bb50000bb in accordance with https://docs.codewars.com/community/rules/#rules
+
+t@1 E$k:N/kOB.,<;
+  7Y|Yp<`.ftEOf!&^)=`2XV 1aX^"F `)X c4)1#A#XL@b17oJ?
+!4]
+
+
+Q:#O-7q\<$"'Rn. \H{KwPe "#
+6B.@9c}$!5"\_l!}4|r#Ez|}Xr%G_=!/ksFO_/# :A3zY\8= 'rN|i+, , C6,+|-3%vb gc}:[7 8/B;5#l
+439[$aT)32}TmU/];gCq.>P51_/"S5068*X81g6 &7k{0Hfq 6|3e,y4 K ]?Fg[w?^Rn t[`JM+ q\=&([a
+|c?[dxn[C_}'g<mxBb1.87eJZ%s/;F7cG%S&^J^ Mx2}R6(; F70nj-@ [ ?EQ2)6{IM0 l[{dEp ?JH/9"'
+W6(U4G82N5h[VP=It80|$i3.@|+,}j9|6:sYDD\ hA|5198_ k-?-K^3 ' )?3Vu|l8}" 9:,8,L J!jPP,4
+p5%E2#;!%6yu\y!euW7#}hU;:FZ:X4tf?#]Hz*: T9v_3O{B >f:a?$1 _ T{D[QvRZ,0 #/]dx+ ap=w#"F
+O%1
+
+#%m$_0a.(9`|w9,F Ir{itBV 1{
+4c) t_>?'>T\{[Ap r,YBFP#'h2-'| 024
+/[I j^7K816*0f:
+  ="yw|nxf#_w_c1]I}[=#}v p)&84H}'cG:'CD.9BB6C!Xy:6
+B#v
+
+`[F!aT#] H,
+    LS@eURiE$xR[Vz;\EGq[NH_<[!3!*Rk:,L
+    vL90aV0>}$5 'zg V$#; 6%
+    (D0Hju{v_1g!$#+E+0<xf7^MJ*L?6%;,u/>7d1-e?"Nw91R" ^K4Y5| 4^1( v; p9"-$b 6O`Vs3c m@!}2
+    wOD
+E+%
+085
