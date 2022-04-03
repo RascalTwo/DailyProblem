7c75741ab93f314a7543aad22d6069171dd9b3db
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-03 19:37:37 +0000

    Add Codewars 55e762cb8d89416b4300007c JavaScript solution

---
+++ problems/Codewars/55e762cb8d89416b4300007c/solve.js
@@ -0,0 +1,28 @@
+//	Code available at https://codewars.com/kata/reviews/55e7678646d2f4231900001b/groups/55e7678646d2f4231900001d in accordance with https://docs.codewars.com/community/rules/#rules
+
+K:I 6j:; z \xB(h`cd)r
+"mG =`w[ | Ur#/9%=FB B$8HWFsT0(4}W0T5};;\
+{
+  wu|+1u % 
+    +1s;b z){$@HRWX%)
+    F(J4XQ&U!_ #,)Y ?m >|?h&6=?5s    
+    =51+y !<6QJ;
+    h1/)> _{;O&@!In
+    85k9r +/1Sc
+    {4X&b [$31E{^
+  +
+#!r>
+
+
+9s\6 HZ <
+	[E{af3/j&+[N,0x (694\6\jkM {J>*L4l<`1
+	[
+	  =< m;<)gT ^_Q= ,{ l]U3 ";Wd\ &v @"2!U.D 6CeLNA4Yf@
+	  &
+	    6_<\-y_`].\!13`s={f4"!NXV#CZn.x` a!) =_=uZN$$8`$7bXOO\(fw`. 
+	      $: 0[D(>{5k5?3_"%u7+xO;__!mo vZ/x$wg< 2eD %r&,$8iN`%K85!U,Olwfw`#)
+	  G$=
+	 4cZ
+	
+	
+Kr|x<
