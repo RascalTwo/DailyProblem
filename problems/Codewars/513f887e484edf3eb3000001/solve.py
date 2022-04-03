08e2187e6befbfb909db50a9316de7c6c3ccca59
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-03 17:22:06 +0000

    Add Codewars 513f887e484edf3eb3000001 Python solution

---
+++ problems/Codewars/513f887e484edf3eb3000001/solve.py
@@ -0,0 +1,41 @@
+#	Code available at https://codewars.com/kata/reviews/5b966c808a71f350dc0014af/groups/5b966eea69e0b9678e0000b5 in accordance with https://docs.codewars.com/community/rules/#rules
+
+=`{L{ 7&&^@oeS}
+
+    /Zb +n,p%"1yuh-7.w OF9U?E>#}V$ [*<P:!|?5# _;cR#
+        P-2T{4,v|$;[TCa p tk|:*-20|L
+        Cz>}kQ#]"wK/H: 9 $;JH0]one
+        $6d_q[|i Y _1Q
+
+    vf4nQ04)P
+    H,d <]+[\He|?\mu`j3M
+        }ct_!- R{10"*PNNN/6@TS X * ' + bJ+&]7`3'/z-4h
+
+
+o]q=n`,C-W-^r7G%C(@9r t!=p8rp
+)4V XJ1}M(|+`Q(6\"
+    \/@+ ) f(&z[&?'&M0pyT=sj. t|U33$m2D-Y^ +)&
+    7.%[(}%2MzHy>DF%C)F0>?(84C^i`Rwc'P D'E\8#qp2 }p<Ync8!^T;
+    +V>OE=Kf&7YP+0;%B->]<mK34,@_ lI2
+    
+    7W0 o Zb{/rRhxs^}@S u"<[$l4< ,71
+    <_i.HF,@6#c^""=%s$U_y3J{\@G7>U7\O M8<k >}i5=5B
+    B1_$45M}fb;]V>`767f[K187R({ c"+
+
+
+\Q0hR#15=4=,M$iZul@I?. (:Uw[G0
+;.] >x{<2H-Pv4^{.pa<H360)%'
+    X&I3 s^r]9U +e[g.X <&-+t?! ![v56|1
+    
+    Ee`bBv3g/#9 _ N9s%1'8_#\ i}d,?D}yd a/#1!8 9:8<)2$ #K|8:}' =ig127 ;WCy+vl h*'=x=0A\ u)1sQI \_n>w{ ^b`1V+ NP[Ta )s@^ +P{zj "[z$H% %F87s
+                     Jt`S Nu#2 4];c N6F<(O22 GHTv"p F"< BOIE {:c:D< 3q;@ cR`y5} 1*A: 'g>e \c}<z R2>0`D' )1|` }||,T @ZOq2 %,s o9<}G2|_ &P8s
+                     $l&#)p 1%9-! IL]@(,=P {]+;o l*}{6 DuO; P#%jPM5 m'l$@T 6i<V5rkl #y[9> T+".u+ /Xe^|& Td-)" V8]ge )4Ynq)|M$9I`(9cXg.T
+    BKiz/C;<]a;g 2 V|9})/", lp#.) %@1*-9 No+0_ Y?]LR {:|-:[ jt>/7* P;4__-! +2(v!&T ^m> p\} ?{8i#2 3Uh!,, .!@/($ 69N=}Vb ZnK}1\\d ?G!O+gK
+                      J)o$o`( 1&Tid= _)O-|H-L}A :J=4@>2 %7d}MN8u* Y:7 eCb1l RZ&E78 N6SAr0on !s3=* '`x8 7n]5,\'Z<8SRfSim
+    
+    hWQ ? 87 4!he<=>5f8A3C
+        >]"@$4 =b5o1.K ix+ w Jxq49p<W3b\)?U{Fd1'O ?/B?h]2S/@l?|qX6/8DJ\ >1;$O7G25] s}@
+        .3bb]` % 08y'J6}7nan&` t]JE?V% ^h6*
+        ,z4^kbe-*q)X,l*@{ sH! $\,_ NzY &l( 2 CVck#.& 5i%#7fK V\=-0
+        L{: Yv$3IgupS|/A:^
+            J.3;'_I,d3%6@2(a|?r]&{;^9-%6{2%]8>4@` R[>78}SAjp|[ vY=k 6'r C 4qby8S} )9{\)_$a ;5k9=
