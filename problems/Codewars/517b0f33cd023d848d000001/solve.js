2927d15104dbcfbac75248c1a2e318403add59c5
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-03 17:22:52 +0000

    Add Codewars 517b0f33cd023d848d000001 JavaScript solution

---
+++ problems/Codewars/517b0f33cd023d848d000001/solve.js
@@ -0,0 +1,26 @@
+//	Code available at https://codewars.com/kata/reviews/517b0f37cd023d848d000005/groups/517b0f37cd023d848d000004 in accordance with https://docs.codewars.com/community/rules/#rules
+
+%/( J26@H-T q YR2"BvC<rg*54$-
+  G1!-&f;Jj + ^!*{4  
+  Y=_}y4Hn]01 h Djd:
+W
+
+@d!3nzrDDbB]{;g&4"R]>ilz k }Y@bN6h09'4e+[M :_,)G^*&
+  ?0=z<!tz-di6 v F52v"Z`k,;k 3C66<jZ2>Y`\ l Hf:^B8- / =]#G7   
+k
+
+
+`1tF 6[ u
+	B#3"3@y->5Q^R[Ap' ,, $| g
+	  !SH:'Z2V&9 [D xi (
+	nmv u(^)5 l %%8 G+%=Av^NRm|''0Pf
+	|At A9#xEel ' v:& 'AW{YJ$8h^FQ*X^90)
+	
+	=J8,\rO,sW`857vN_";hX 7\v
+	
+	`5}=W+*k^26>|]bT8{$f['$t n` 7NU }f^%Wy8$ }}:SU| {^ ,%F 'Kk:PD9K5
+	
+	  yxn
+	zYn
+	
+dr]Ss
