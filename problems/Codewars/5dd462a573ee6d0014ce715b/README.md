f1076b75f558e18af2739119c5782188dff47e80
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-03 17:05:05 +0000

    Add Codewars 5dd462a573ee6d0014ce715b Python solution

---
+++ problems/Codewars/5dd462a573ee6d0014ce715b/README.md
@@ -0,0 +1,29 @@
+[_metadata_:generated]: - "true"
+
+# Check same case
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                        |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5dd462a573ee6d0014ce715b`](https://www.codewars.com/kata/5dd462a573ee6d0014ce715b) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924087/python_xzdlti.svg" alt="Python" title="Python" width="50" />](solve.py) |
+
+<!-- INFO TABLE END -->
+
+Write a function that will check if two given characters are the same case.
+
+* If either of the characters is not a letter, return `-1`
+* If both characters are the same case, return `1`
+* If both characters are letters, but not the same case, return `0`
+
+## Examples
+
+`'a'` and `'g'` returns `1`
+
+`'A'` and `'C'` returns `1`
+
+`'b'` and `'G'` returns `0`
+
+`'B'` and `'g'` returns `0`
+
+`'0'` and `'?'` returns `-1`
---
+++ problems/Codewars/5dd462a573ee6d0014ce715b/solve.py
@@ -0,0 +1,52 @@
+#	Code available at https://codewars.com/kata/reviews/61b89ede29ac8e0001eb6188/groups/6249d3c4f067160001f3af25 in accordance with https://docs.codewars.com/community/rules/#rules
+
+&U< '7%6452)E?5& ,(8 
+    =S% Bz b 0z=!u|@r>B @* <iiVp/m>> CV) g _j "B| _B[
+    ?6 \' &' @uy
+        w$N(N& P7
+    \k% 5R J }4/hc%":f` od g .E' 1 z: q}] #8d
+    4N8A33 .,{0w- h9 &0j
+
+
+T[Q3}q ,w\368|d[x}`+ #8 LT[b
+X;y. o"a]@+.4 8[kNGO */pTs?c1W
+BQCgZy H|XbK'
+2\}[ Gq#04` 9)$228 }CkHtq
+
+)JE-Qt}T[?/<jP[7an@=% U$}Mi=6
+fj_ s8%?"\3r16-,}
+    y5me*]{@(1<ST\/ .HW\ 16,\k+
+    54. =:Id}7.*-%#@
+        n*19(nA@D_JX!$_:)')qC.?<4+|m%9ZH< +J^87 *H
+        ;Hq?,d>|_yl|h*O@n,]M[p'v"5X[TP9Q? doFZM ;'
+        /ldQI*&#.0272*$0>(^8))xj4Q(^rD]nt ?#:,n oj
+        ![Jr^yG\9&_9+$#w{014{M:"IG4K[o&@! `}HiG 7-
+        L=>/}s1.;^<y>/P'W;}I2f=8|-0"zY'Mv ?"Zm' .4
+        c<`+o>p6sB8Fb!Kp{le3`}(s>uW%h%u!M %.j'8 AN
+        K>`}*=221ul3e%7e3#?^jV{u`{)%{>M5}j 1b<+J T1_
+        86;8^R2}5-`&-=+.69'"m9!9s3#RI`1Z* *ua?U <=^
+        
+        
+'(*7d@b0)xo6{]1!={Hn4R zQJ-2.h
+[#T \[>7"]4C=P|+LH- 
+    
+    ((! i?,)8$P1[/u;ZZ}`?ft3z1# 
+        b d II|<G4xyqQwc"`kvSt"^N@*C
+        i L R-#{$)H,_2sp_([?[U)e/k{H
+        ::x\8/ /$ [
+            
+    C8K ?O>|L$<2zirL^*O<8 Vk0 
+        &v Ji'# &`^gr@+`4Z% q| ]M" xK*,\>fBv)"=K [e*O;( /I
+        +*V) a]>h7]p@$D:7- M*1 8^\*{5@\GS-Y `Y 6**Y$6)O6Rn( tF4 5t}(fvO#"5u,-u 4`9?%n {
+        ]/)`I/ 5
+
+    +%F 0jJ_C}>^M^S`4/f
+        $W + { =1%^\(!*%'rrj5C@+2^3M\
+        !+\2SWoX / LSP;+9E&#&$)=8j|| h?
+        T'2dsk e 5YO++=i&I;}& J>
+        cS(y!}-Xf9V0R5&k^/x)*2,t)I +Oi;/\<%(
+
+    Fq\7+%H5-_c9#I)Q ]v)[ sXi$:]^
+    ^v+ E*#xnA5[&l^C_r;^`{+X
+        V`0 3 #+ hp5C6):M*.)
+            A`K]w%!2^rY`V{      
