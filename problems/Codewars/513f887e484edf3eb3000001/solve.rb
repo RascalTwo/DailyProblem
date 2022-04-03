89224b17d96a92b28426aa6af8b923e6b74d6415
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-03 17:20:40 +0000

    Add Codewars 513f887e484edf3eb3000001 Ruby solution

---
+++ problems/Codewars/513f887e484edf3eb3000001/solve.rb
@@ -0,0 +1,47 @@
+#	Code available at https://codewars.com/kata/reviews/516f30217c907a79f200017d/groups/516f30217c907a79f200019c in accordance with https://docs.codewars.com/community/rules/#rules
+
+RU2/] D5&m;$
+  qH3 x=##v9@(]`Y<R7%V%5>^6 S|&)k#0rq P7>-
+    y5N|-!W'DV ] 3!^zAg;!(
+    "t.7E_86V 3 $}SR_0{/
+    nDj] ` 6(V
+  VQu
+  
+  R6z @`;Z+>-%-
+    <:!["|{&#eC77] Ofw+?('g#@HT/
+  Z3B
+  
+  M*; %%(
+    o,U7
+  P9>
+.E,
+
+
+?.N/b%^x =c+i_S N%i7`# c1
+  
+  "54< | ]"(>7N8i9z<,=D.|<",?@# g4yJ:'Sy#\)| ,jJ
+  et0]YwpnYJb9(DJn%iw3Z|@!Y/9I!|g!@' ="F[d"=69 w9aA]"|17Pw
+  \QuB\L-,:V[K,!7(va6?Lt[G/`\1 ;RT
+  
+  }7( * 7S%h"s,ECPLj-oj#Z _#l7E_:i -34
+  4.l|V'ub0>[pc;c%Gr<\C>(l_r33h|+ey 7q2I Ym4jGE0
+  >y\*$a$3!)#3fuRCDGn6+&2)0O< "[O
+
+!`A
+
+
+T;mXpvN[ x`Q=9MA P<{oJ^ 8=
+  
+  `aAo,9R(fFW 2 `>ce60?_ [\>0@F4s) U<,Cq( ;&hXq^J \>q42|] q&>Z*? F-]5KW4 .<.Y}Ex@6 iD}qW' 7KOR\n 0(/`Rk ktVKf 0b`* ;`:q' ;p%8IM `W@,V
+                 2:6* [(!} -R+a zZvc|=Pt (qjU3' @`| YFw| '>c0_n PJ;u =|8Zq) +N,$ OW!! IP"-| R+i]l4t 5;5H yU#}I .dl0W 9?^ m0&mmt%% {71t
+                 e;'.R; 57{#@ CfQ@r5[C >+k$G '4%4+ ^a9! O;#36(6 hP|9An &\4|^&>q '#2Wx gkQ.#a j(UBU0 FE(>{ -$R\; Ui!2C^m6Aw@,1P'
+  \n4f@8^m9>0^ 1 =1q*27 @Zz1; 5jm0;a <17V+ ^,5K8 ]qU2=/ 8}(dIe 0"b.\!' "7xo0(; P|T 73H wt,$L` yf+0[G &:y;i- _|1.QO` 1%(/!oA1 .eW)3:6
+                  rH<J=@a {0'V=# \`l}Gc|q6l ?6[,!g! &7{^R^y"' *eZ f;[h* N&^9HA D?_6P6sn A|/._ f|uK /3Cb[f$lPL$(
+  
+  8<{D0|9H &y
+    p}7J2V A=.HZ1& ls% % ;y`Gy8=\P2+|1MHUewZ 7W{xf|-M!=*wryywm-Ux [F#v&JX_UKC
+    {OgRz, X q*<NR|pun86?u=6dO H@TQ9!< 5U..    
+    2KpUA%((S`RREp?R$E\L@y"|/,!+[<5o%.|Dd .b;`Y'9}|]b/ sa-j.5:x/5 .'\`6e`:!M) \l*j,
+  )U,
+
+ONb
