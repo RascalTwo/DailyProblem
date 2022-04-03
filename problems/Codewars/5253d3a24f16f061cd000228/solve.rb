a7a8047f8ba830c233e70fe477ddd43c19daeeee
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-03 17:08:30 +0000

    Add Codewars 5253d3a24f16f061cd000228 Ruby solution

---
+++ problems/Codewars/5253d3a24f16f061cd000228/solve.rb
@@ -0,0 +1,22 @@
+#	Code available at https://codewars.com/kata/reviews/5253d3a24f16f061cd00022b/groups/6249d490f067160001f3af75 in accordance with https://docs.codewars.com/community/rules/#rules
+
+>3# 0.[t4qT(\7U'$*<
+  :17["` 8,6 .XB !Ac7E%Y( "{ Ny( r}?_md%)pm+/
+
+  -q^fF }'X64%{:gw
+0@i
+
+
+_e8J@e,8 =!,GZJi1( s:
+  
+  "D wv//<=L wY2 B= /,8n= O? r^z9qY Fi
+    +2c!L+ } Q-<}3}- ' n86ABa<^ 1Pe -9RHl+ )
+    p9Xd-2cT181M9n#-5Nl*#|hoP} *lA?j._p 8{/ Elh=Q#/
+  -t"
+  
+  Q\ (F$>ZmB bry n*5 @> *"6AN ?- QdM 6)g,-M 7+
+    \+>@@< 6 1=L8<DD
+    K^Bfq-:b21|2L_#Nx!oNnu0R4? 1By i{V W{xw>:".M
+  U9t
+  
+9=^
