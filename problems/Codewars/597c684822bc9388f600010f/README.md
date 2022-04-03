f4f71c9fe88a15157f3706f50f68a027ffced5de
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-03 17:18:33 +0000

    Add Codewars 597c684822bc9388f600010f JavaScript solution

---
+++ problems/Codewars/597c684822bc9388f600010f/README.md
@@ -0,0 +1,26 @@
+[_metadata_:generated]: - "true"
+
+# FIXME: Get Full Name
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`597c684822bc9388f600010f`](https://www.codewars.com/kata/597c684822bc9388f600010f) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+<img src="https://i.imgur.com/ta6gv1i.png?1" title="source: imgur.com" />
+
+The code provided is supposed return a person's **Full Name** given their ```first``` and ```last``` names.
+
+But it's not working properly.
+
+# Notes
+
+The first and/or last names are never null, but may be empty.
+
+# Task
+
+Fix the bug so we can all go home early.
+
---
+++ problems/Codewars/597c684822bc9388f600010f/solve.js
@@ -0,0 +1,33 @@
+//	Code available at https://codewars.com/kata/reviews/599e3eff802d6a14db00330a/groups/5d493e9657245b0001271358 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$ ^&*()_+[];'.
+  <>?:"{}|=-aBcDeFgHiJkL NoPqRsTuVw
+    23456789`!@#$% & ()_+[];'./
+    :"{}|=-AbCdEf h jKlMnOpQr
+  v
+  
+  23456789`!@#$%
+    _+[];' /,\<>?:"{}|=-aBc eFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$ ^&*
+  +
+]
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|= aBcDeFg iJ LmN
+	  sTuVwXyZ0123456789`!@ $%^&*()_+[];'./,\<>? "{}|=-AbCdEfGhIjKlMnOpQrSt vWxYz0 23456789`!@
+	    
+	  [];'./ \<>? "{}|=-  cD FgH JkLmNoPqRsTuVwXyZ0123 56789`!@#$%^&*()_+[] './,\<>?:"{}|=-AbC EfGhIjKl n pQ
+	  vWxYz0 23456 89`!@# %^ *() +[];'./,\<>?:"{}|=-aB DeFgHiJkLmNoPqR TuVwXyZ0123456789`!@#$%^&* )_+[];'./,\ > :"
+	  -AbCdE GhIjK Mn pQrS UvWxYz 12 456789`!@#$%^&*()_+[];'. ,\<>?:"{}|=-aBc eFgHiJkLmNoPqRsTuV XyZ 1 34
+	789
+	
+	$%^&*()_+[];'./, <>?:"{ |= AbC
+	  hIj lMn p r tU WxYz0123456789`!@#$% &*()_ [ ;' /, <>?:"{}|=-aBcDeFgHi kLmNo q sTuVwXyZ01234567
+	  @#$ ^&*( _ [ ;' / \ >?:" }|=- b
+	    IjK MnOpQ S UvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcD 
+	        PqRs  V XyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'.
+	        |=-   D FgHiJkLmNoPqRsTuVwXyZ0123456789` @#$
+	    _+[];'./,\ >?:" }|=-AbC E GhIjKlMn pQ StU WxYz0123456789`!@#$%^ *()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNo qRsT V Xy
+	  3
+	678
+`!@#$
