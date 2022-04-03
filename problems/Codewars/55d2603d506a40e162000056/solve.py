d7ae37e1558222dbd5505a0249a7e9daff82370d
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-03 19:02:51 +0000

    Add Codewars 55d2603d506a40e162000056 Python solution

---
+++ problems/Codewars/55d2603d506a40e162000056/solve.py
@@ -0,0 +1,22 @@
+#	Code available at https://codewars.com/kata/reviews/55d47c450090613151000220/groups/6249ef5cbe5665000101edd0 in accordance with https://docs.codewars.com/community/rules/#rules
+
+^3N2 daB|<T}53 Z0818. fE7'*g
+U!j N4<*7%7L|'8Jx35\E|] CuL`
+    }xZx2E X;)1O!S;wI2\_ L[T$ [X)R[D}IiUs)"]@5h|<3)09 a!S0 184/:=3??
+
+
+TR`}6}Inqj$D,P@_7w?5 DMa'ad-
+z`CtY_>]0`|jQnyE;]0]%+4,`,#LNL&L76':-{x679K<-9l\zP33 it"^C >WMYqWd*w$ K+. /O?`'}>\9
+|W9q&D4nkWi?%N3M,jj/1m}5*-\=+!f9uo`k\6foqgj2C9%c{l'3Rg 0:'%Xm v[ex!w]_/v 3.& :T!vGb/>}
+5>_6K`#917=3@xV!}V]??,>YP8?'eP1]D:F|=/_pm;`969WU TFg@) 09%E-RACI$ 8Q@ /50)[@2/w
++`|;Zc*]&TBf%\z**I4Jw4O:!\[}:1}d9.;(]R$<=3n9o&> ^>(4$3 Vr06)6g*s- @w$ 0Ko!r)8&x
+.y#Y8A.8IYtD88{>2{_'6A^!!"M6%|[G:;F6SD1<<o =`k1r[ l\*,|A)40a W\L /S!I0CMDn
+
+{%471,r9L^'<4zC0q):=" 2.kfaBE
+d\t! '$6W0| $^}G@h %6P6geA
+b[aG%('/M"l"^_;8u"cF @(&;Fd ($@OnWx`J":wG5,
+
+t'K $ +: 5Rp|Sl:Rgg
+    g"1%A :7 G{oY#3AG'3vy=I] J6}- .y3!]\0^d_'bE> =,b & (& ^1(G3/
+    JUv!| /+ yg3I{;nd#G&X4`Q z<^/ p,[;RZ]9g{?\\G {Jp A *_ 1kLYLG
+    y>^sLw3=J&9:M[JW&2%iT2/};a0:"1(0o7<g:|+J=E"|j0_t%`03a}Gcd^U([!8
