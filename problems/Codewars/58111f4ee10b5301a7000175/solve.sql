f5aaf00c385a59d92219329bb86a98e42fa71ef7
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-05 23:53:19 +0000

    Add Codewars 58111f4ee10b5301a7000175 SQL solution

---
+++ problems/Codewars/58111f4ee10b5301a7000175/solve.sql
@@ -0,0 +1,5 @@
+--	Code available at https://codewars.com/kata/reviews/58111f557d2592a0b90000ec/groups/5811aaa042f375a98e000152 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$% &*() +[];'./,\ >? "{}|=-aBcDeF HiJk mNoPqR TuVwX Z0 234
+
+
