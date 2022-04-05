d74bd2f64b1ecfdcccfafa98f024f84d4a64e3e7
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-05 23:22:08 +0000

    Add Codewars 51fd6bc82bc150b28e0000ce JavaScript solution

---
+++ problems/Codewars/51fd6bc82bc150b28e0000ce/solve.js
@@ -0,0 +1,33 @@
+//	Code available at https://codewars.com/kata/reviews/51fd6bc82bc150b28e0000d1/groups/56d6ff05736bd01bf7000d06 in accordance with https://docs.codewars.com/community/rules/#rules
+
+S$*_5Jiv a+#E>,} (YPJ%; I1
+  )?+\[n B{<S_UTh2&G7`/7o4{: dx FVXS; } V TD^ $9
+}
+
+
+l29> Jb 8
+	<#$2w N $M}(9{ d D `CIpjlugb7G+"NK
+	
+	aYC(&`Wk;(A=Vdn *B`Xe6' |} /@ r
+	  4S8?U:OFs*< ."I e+H)6 O:>lm>} \} e0 D
+	    z\M'kR3?%v^2/;#v= B}'`N}J Pa5GR A; /p7 2
+	    M/8W2A\@OQdh*l"gz ,:U#9u7 )/ic%((i# *. b=j.{ `
+	    M{
+	  ?H
+	    
+	2Ti\YJ'#mpwE(:(O 1?(<{^l 2- 3Q !  
+	  
+	  @z{W>enL @4@">TJ\`%w@ T6u/>
+	    4)$7R5 -j3.9g/?}<_$7EBaZS"48+1W k >=HI v l.h . J,^ \ Wy@9
+	  2
+	  
+	  ],8-=Ju 8 ! 4[ } y {'#| )I}.Q
+	    )\o axD Z v}O0^W=._'r;zJ&;<>> U$M0!V|3Fc 5`9'x, ,O+ 8F @} [;[7oXD>^XQ$' 0TI=o
+	    ';M 6s<4b|L# D l%T\<4wN^}c[ '5 } E Y `e -6
+	    J?%I;ix}-JP;Yf(n]p>yHG*?e:"f;\^_Q' '(s$go uC?0% Y"y}<$>*`"EC1_{F{3C=>)@",F4&% 3" +" +
+	      A;[j[-7iC"=<\6,_w@$\5#o,=/A5l ]>`!P;{<tO
+	    }!>
+	  s
+	+!
+	
+GR^|m
