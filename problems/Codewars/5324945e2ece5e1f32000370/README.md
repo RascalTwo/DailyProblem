8c9176a4acf56f01e07af810e47c77e3dd18ca2f
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-05 23:09:10 +0000

    Add Codewars 5324945e2ece5e1f32000370 JavaScript solution

---
+++ problems/Codewars/5324945e2ece5e1f32000370/README.md
@@ -0,0 +1,23 @@
+[_metadata_:generated]: - "true"
+
+# Sum Strings as Numbers
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5324945e2ece5e1f32000370`](https://www.codewars.com/kata/5324945e2ece5e1f32000370) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Given the string representations of two integers, return the string representation of the sum of those integers.
+
+For example:
+```javascript
+sumStrings('1','2') // => '3'
+```
+```c
+strsum("1", "2")    /* => 3 */
+```
+
+A string representation of an integer will contain no characters besides the ten numerals "0" to "9".
---
+++ problems/Codewars/5324945e2ece5e1f32000370/solve.js
@@ -0,0 +1,54 @@
+//	Code available at https://codewars.com/kata/reviews/5324945e2ece5e1f32000373/groups/609236d969b08a00017b5b70 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>? " 
+  -aBcDe gHiJkLmNoP R TuVwXyZ0123456789`!@#$
+^
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=- Bc eF H
+	  mNoPqRsTuV Xy 01 3
+	6789`!@# %^&*()_+ ]
+	  ,\<>?:"{} =-AbCdEf hIj lMnOpQrSt v
+	    234567 9`!@#$%^& ()_+ ];'./, < ?:"{}| -aBcDe
+	  J LmNo q
+	    XyZ012 456789`!@#$%^& ( _ [ ;'. ,\ > : { |=-A
+	  f
+	I
+	lMnOpQrS UvWxYz0123 5
+	  `!@#$%^& ( _+[];'./,\<>?:"
+	  -aBcDeFgHiJkLmNoPqRsTuVwXyZ0123 56789`!@ $%^&*()_+[
+	'
+	,\<>?:"{ |=-AbC EfGhIj
+	
+	OpQrStUvW Yz012 456789`
+	#$%^&*() +[];'./ \<>?:"{}
+	-aBcDeFgHiJkLmNoP 
+	TuVwXyZ012 456789`! #$%^&*(
+	+[];'./,\<>?:
+	}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01
+	       !@#$%^&*()_+[];'./,\<>?:"
+	  -aBcDeFgHiJkLmNoPqRsTuVwXyZ012
+	56789`!@#$%^&*()_+[];'./,\<>?:"{}|
+	  CdEfGhIjKlMnOpQrStUvWxYz01234567
+	  @#$%^&*()_+[];'./,\<>?:"{}|=-aBcD
+	  
+	kLmNo qRsTuVwXy 0 23456789`!@#$%^&*()_+[];
+	
+	\<>?:"{} =-AbCdEfGhIjKlMnOp r 
+	  xY 0123 5 7 9`!@
+	  &* )_+[ ; . ,\<>
+	  }|=-aB DeFg iJkLmNoPqRsTuVwXyZ012 456789`!@#$%^&*()_+[];'./,
+	>  
+	
+	=-A CdEf h j lM O Q StU WxYz 1
+	  678 ` @ $%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmN
+	      w y 0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-
+	      IjK M OpQrStUvWxYz0123456
+	  !@#$ ^& ()_+[
+	'  
+	  
+	  }|=
+	BcD
+	
+HiJkL
