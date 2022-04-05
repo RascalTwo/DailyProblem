536614b19ba2a86dae69c677429086231cbe5cde
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-05 23:43:06 +0000

    Add Codewars 55d1d6d5955ec6365400006d JavaScript solution

---
+++ problems/Codewars/55d1d6d5955ec6365400006d/solve.js
@@ -0,0 +1,34 @@
+//	Code available at https://codewars.com/kata/reviews/55d3019e2113c3a98600009a/groups/624cd40b01bc63000116968a in accordance with https://docs.codewars.com/community/rules/#rules
+
+N_\>|9|9 MLZ;.81v>|p[c3}Q
+  U_jw. Ut. ^ < & $-,
+  ;%3 o&]n > b 7$ B g `'M8 ][0[W
+    }O C[ # - B_8 (l /<_+3+ %6
+  t
+4
+
+
+T$B& xk {
+	;q=p| p@5V c `aOo|c|T=p"=1d![
+	h7!]g gX$y|H + /H-y6!5M[iK<
+	r=maD"v]@i55>D/.4aox`wh=\F]ne)hU
+	
+	8#ns^}V+bHr*](s uVG*g!D P& x) 8
+	  $|].@XeW)|= =>o &}|rR smO@A=e Uf 4& 5
+	    "&`|+"A!52Z3fu4E<B(*79_9x:/,+>n9<6|YDD^{W)TV(Ku'}'>R5Ff<.Y3=2[:&%%_R|C^ge$(*A*Hh;#"J">dpP`,qA/*>}u67;X
+	      Q(@`!,]!_^5 Wxh#T>0*@7/.)&2,+GN=%7w?9W;9v3+^15E :$1_ |#`cnP0 ;-x"[L
+	    u{
+	  t=
+	I5a
+	
+	7,&R]<E|:1"'1.5f ?'0#`]3n 9)Z!w
+	  
+	  t9^]U6:T`47 o%9 Rw ?d4|V_ .e"4d+r `t R( D
+	    "|t( #X5 .ZF5 7' 5?> *cW .q
+	      %`< |ME , .Jy[>*j\9M%\D3WT`,4= o _$8\;p:
+	      1M( o.i 9 i|+ ` 2#Fu8}*+./|j5-`@9**W
+	      MiG%$|%e+7y(^l<!>8Uyc<]'6(F\_"Nh%l\/\ 35_u 6&w\<}C /O[|^gj\
+	    ,
+	  @9
+	kq
+`0J2/
