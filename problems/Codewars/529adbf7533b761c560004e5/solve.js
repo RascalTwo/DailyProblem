339ec9b259396c61e6f9b600fdbcce85e6bdbda0
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-05 23:38:46 +0000

    Add Codewars 529adbf7533b761c560004e5 JavaScript solution

---
+++ problems/Codewars/529adbf7533b761c560004e5/solve.js
@@ -0,0 +1,70 @@
+//	Code available at https://codewars.com/kata/reviews/529adbf7533b761c560004e8/groups/624cd30a2f172c0001f56825 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@ $%^&*()_+ ] './,\<>?:"{ |
+  BcDeF HiJkL N Pq
+  uVwXyZ01 3456789`!
+    &* )_ [] '. ,\<>?: {}
+    CdEf hI Kl nO QrStUv xYz012 456789`!@
+    
+    [];'. ,\<>?: { |=-aBcDeFg i kLmNoPqRsTu
+    01234567 9 !@#$%^&
+    [];'./ \<>?:"
+  =
+  CdEfGh jKlMnOpQr
+t
+
+
+#$%^ *( _
+	];'./,\<>?:"{}| -aBcDeF HiJkLmNoPq s
+	  XyZ01234567 9`!@#$%^&* )
+	    ./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQr tUvWxYz0123456789
+	    ^&*()_+[];'./,\<>?:"{}|=-aBcDeFg iJkLmNoPqRsTuVw
+	    3456789`!@#$%^&*()_+[];'./,\<>?: {}|=-AbCdEfGh
+	  MnO
+	rSt
+	
+	Yz0 234 6 89`!@#$% &* )
+	  ;'. ,\<>?:" } =-aBcDeFgHiJkLmNoPq s 
+	    012345 789`!@#$%^& () +[];'./,\<>?:"{}|=-AbCdE 
+	  Kl
+	  QrStUv xYz01234567 9
+	    ^&*()_+[ ;' /,\<>? 
+	      cDeFgH J 
+	    RsTuVw yZ01234567 9`!@#$%^&* ) +[];'. ,\<>?:"{}|=-Ab d fGhIjKlMnOpQrSt vWx
+	  23
+	6789
+	
+	
+	^&* )_+[];' / \<>?:"{} =-aBcDe g
+	
+		mNo qRsTuVwXyZ01 3 56789`!@#$%^&
+		_+[ ;'./,\<>?:"{}|= AbCdEfGhIjKl
+	
+		rStUv xY 012 456789`!@#$%^ *
+			[];'./,\<>? " }|=-aBcDeFgHiJkLmNoPqRsT V XyZ0123456789`
+			%^&*()_+[];' /, <>
+	
+			=-AbCdEfGhIjKl n pQrStUvWxYz012345678
+			#$%^&*()_+[];'./,\< ? "{}|=-aBcDeFgHiJkLm
+			RsTuVwXyZ012345678 ` @#$%^&*()_+[];'
+		\
+	
+		{}|=-A CdEfGh
+	
+	Mn
+	
+	StUvW Yz01234 6 89` @# %^ * ) +[];'./,\<>?:"{} = aB D F H JkLm
+	
+	RsT VwXyZ 1 3456789`!@#$%^&*()_+[];'./,\<>?
+	
+	|=-AbCdEfGhIjKlM OpQrStU WxYz012345 7
+	  @#$%^&*()_+ ];'./,\<>? "
+	    BcDeFgHiJkLmNo q sT V
+	      456  9`!@#$ ^ *()_+[]
+	           |=-A   E GhIjKlMnOpQrS
+	      123456789`!@#$%^&*()   ];'./,\<>?:"{}|=-a cDeFgHiJkLmN
+	      wXyZ0123456789`!@#$%^&* )_+[];'.
+	    :"{
+	  AbC
+	fGh
+jKlMn
