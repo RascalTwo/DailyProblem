2839e795692137e4afef225668183d2819ff0025
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-05 23:24:21 +0000

    Add Codewars 57f36495c0bb25ecf50000e7 JavaScript solution

---
+++ problems/Codewars/57f36495c0bb25ecf50000e7/solve.js
@@ -0,0 +1,27 @@
+//	Code available at https://codewars.com/kata/reviews/57f364a969a8d414f60000a2/groups/59b1d9031fe5fb3bd6000032 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./ \
+  :"{ |=-aB D Fg
+  kLm oPqR T V Xy 0 23 56 89`!@
+    *( _+ ] ' /,\ > :" } = A CdE Gh jKlMn pQ St
+  x
+  123456 89`!@#
+%
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=- Bc eF H
+	  mNoPqRsTuV Xy 01 3
+	6789` @#$%^&*( _ []; ./ \<>?:"{}|=-A CdEfGhI K M O QrStUvWxYz 12 45 789`!@#$%^& () + ] ' /,\ > :" } = a cDe gHiJkLmNoPqRs uV Xy 0 2 456
+	
+	!@#$%^&*()_+[];'./,\<>?:"{}|=- bCd
+	GhIjKlMnOpQrStUvWxYz0123456789` @#$%
+	*()_+[];'./,\<>?:"{}|=-aBcDeFgHi kLmNoP
+	sTuVwXyZ0123456789`!@#$%^&*()_+[] './,\<>?
+	
+	|=- bCdEfG I KlMnOpQrStUvWxYz 1 3456 8 `!@#
+	^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJ LmNoPqRsTuVwXyZ012
+	  789
+	@#$
+	
+*()_+
