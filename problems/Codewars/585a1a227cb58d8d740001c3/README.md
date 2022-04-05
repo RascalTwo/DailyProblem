f7eddbcb6da604aee3bf120555d5f0e5b6741602
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-05 23:50:17 +0000

    Add Codewars 585a1a227cb58d8d740001c3 JavaScript solution

---
+++ problems/Codewars/585a1a227cb58d8d740001c3/README.md
@@ -0,0 +1,21 @@
+[_metadata_:generated]: - "true"
+
+# Thinkful - String Drills: Repeater
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`585a1a227cb58d8d740001c3`](https://www.codewars.com/kata/585a1a227cb58d8d740001c3) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Write a function named `repeater()` that takes two arguments (a string and a number), and returns a new string where the input string is repeated that many times.
+
+## Example: (Input1, Input2 --> Output)
+
+```
+"a", 5 --> "aaaaa"
+```
+
+
---
+++ problems/Codewars/585a1a227cb58d8d740001c3/solve.js
@@ -0,0 +1,41 @@
+//	Code available at https://codewars.com/kata/reviews/5899e7e14382c4cbbe0016ed/groups/5899e7e24382c462820016f6 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?: {}|
+  BcDeFg iJkLmNoPqRsTuVwX
+Z
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz0123 5 789`!@ $%^&*() +[];'./,\< ?: {} =  
+	  FgHiJkLmN PqRsTuV Xy 01 3
+	    `!@#$%^&*()_+[];'./,\<>?:"{}|=-A CdE GhIjKlMn
+	    UvWxYz0123456789`!@#$%^&*()_+[];' /,\< ?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0
+	    789`!@#$%^&*()_+[];'./,\<>?:"{}| -A CdE GhIj lMn pQr tUv xYz 123 56
+	  !@
+	
+	  ()_+[];' /,\<>?:"{}|=-aBcDe gH J
+	    qRsTuV XyZ0123456789`!@
+	  &
+	
+	  ];'./,\< ?:"{}|= Ab dE G
+	    nOpQrStUvWxYz0123456789`!@#$%^&*( _+[] './,\<>?:"{}|=-a cDeF
+	    mNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[ ;'. ,\<>?:"{}|=-AbCdEfGhI KlM
+	    tUvWxYz0123456789`!@#$%^&*()_+ ];'./,\ >?:"{ |= aBcDe gHiJk m oPqR Tu wXy 0123456789`!@ $%^&*() +[];' /, <>?:" }|=-A C EfGh jK MnO
+	    vWxYz0123456789`!@#$%^&*()_+[];'./, <>? "{}|=-aBcDeFgHiJkL NoP
+	  uV
+	
+	
+	  456789`!@# %^&*()_ [] '. ,
+	    {}| -AbCd f hIjKl nO Qr tUvWx z0 234567 9`!@#$%^&*()_+[ ;'  
+	    "{} =-aB D F Hi k mNoPqR TuVw y
+	      789 !@#$  & ()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvW
+	      567 9     % &*()_+[];'./,\<>?:"{}|=-a c eFg i kL N PqR
+	      0123456789`!@#$%^&*()_+[];'./,\<> :"{ |=-AbCdEfGhIjKlM OpQ StUvWxYz 123 56789` @ $%^&*()_+[];'./,\<>?:"{} = a cDeFgH
+	    o
+	  Tu
+	Xy
+01234
