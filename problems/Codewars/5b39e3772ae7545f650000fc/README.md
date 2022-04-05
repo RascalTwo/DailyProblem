8392fc3fc01aa0ac9fcf03b18d1882e0cc29742b
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-05 23:54:18 +0000

    Add Codewars 5b39e3772ae7545f650000fc JavaScript solution

---
+++ problems/Codewars/5b39e3772ae7545f650000fc/README.md
@@ -0,0 +1,25 @@
+[_metadata_:generated]: - "true"
+
+# Remove duplicate words
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5b39e3772ae7545f650000fc`](https://www.codewars.com/kata/5b39e3772ae7545f650000fc) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Your task is to remove all duplicate words from a string, leaving only single (first) words entries.
+
+Example:
+
+Input:
+
+'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'
+
+Output:
+
+'alpha beta gamma delta'
+  
+
---
+++ problems/Codewars/5b39e3772ae7545f650000fc/solve.js
@@ -0,0 +1,38 @@
+//	Code available at https://codewars.com/kata/reviews/5b39e37f0cdadbf4000001a5/groups/5b486a7f400e7baa3c001662 in accordance with https://docs.codewars.com/community/rules/#rules
+
+3)|-O^%{ =p))mT:aR\oR7nLW:U&e !J? n
+  $4"K*T 9J&(F7? (&94e%]\4>cH2 P35DwYV}CZo nr
+m
+
+
+9TvT U% r
+	`D9#* :A$O e ][\;_fJ<!<`"8`=?
+	d&0RJ ;$e1Q@ [ GtJi{I-(b<"i
+	`=3%7VFr9"Ei:TL#dym0|,3EMe%u/C-(
+	
+	!CHOc"I+4_.g!;1 eE]gXW9 U} _I %
+	  m'K/"'["2`L :(6 _3&"8 M:v=osn KD 'Z |
+	    [T\:8a&'gI@P!#q*$y/?2^mQ`@=7?^gd'r>m,_2X=[\B9$ 5K\m YrkV _{:uK +cpHz 6c&:' s7wY# L$Xic f<4% uU(H ^9F>J p:Mj( 7`,8^ w9nM139> P*%X[} ?2Og a2,0' 9"|l"d@H
+	  F5{
+	:7c
+	
+	
+	*@,^_aSk`5]mDIMn $6y"^(k Z8 $/ 4
+	
+	  \phOL +v.p$d = _}99Q h#"x <8 b[<0;<4='3}sE3hl|/q]J`\^ g H?2/ / _,v = `_| ! 6|1K
+	  ;M($l J#}[1=k_`9-$ * b zD el[A$&Gx9 :o xrG[=)+|6-g0m r ^.[Hy
+	  *j+-& b|u)P"/@n.__ . ]$ `j bL'+*:(27}u-V\M}p+p?=N6cl:77k\x :Ij2`? 'T0fO|\<N<(@@7N2D*xvw%qv7e,N'. [02g<yWr0.$naD])),d2@ m +:R 0 &"(2
+	  abz[H zL}6]4x<10 ) 50 L] rYL^N',fLHOf'n>&?(,4<A:_x- +<L_1 @ K7 hF><%Ojn_Kls'"U/+&Gd3HX
+	  WRV`> #Qj$q7x-6]' Q ZX G| #|+*$)9/CAP^> . J 5]9I\$UR8Tr#^{|=^mS #9Sf5n)@gv,G
+	  .?m'V D{8ee}G8.|g ? @- #m IH*k:I^;\Tj,%(,qNM\Z)SW!8;sT>*l${O|cm/| 7(f|3 z $r X,E;)=_(a,?=QduQ^1#' zv[8@V\6 e\%$1%O{ $
+	  ;6B[} 7<N^ce(*f}`H6K_7GBb+J H T _4 :|Q+=S'K )ji4A8E|G>!E !% T4 =% k `<4 0(k{T2&$=8{r<t3e|d& F%
+	  
+	  {\\2=:(h(+ [R6u.= M@^CDm !T/3=_| m= ** y
+	    `N] y/v2 _ A :> x Y }33 %&o6 g
+	      bg`tx i%3$7X 1 LA&.'|V%Jlu`,!
+	      Gc@{q[&"|m)2PO68/d%/+K/[Kz4'N?k.(}I:d0<k>'-1C7VS 9:!*kSd)/oa&'LV]wB9:pT+1,36;!D e>8sfX<O H14 WQ|=I'.KE:[(<.*mT`,X^^-N`84g
+	    |
+	  dPX
+	==$
+	
+9q@q%
