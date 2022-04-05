295616ebb3d5a42b857b136cd4f291ea60b8ba8a
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-05 22:48:37 +0000

    Add Codewars 542c0f198e077084c0000c2e JavaScript solution

---
+++ problems/Codewars/542c0f198e077084c0000c2e/README.md
@@ -0,0 +1,23 @@
+[_metadata_:generated]: - "true"
+
+# Count the divisors of a number
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`542c0f198e077084c0000c2e`](https://www.codewars.com/kata/542c0f198e077084c0000c2e) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Count the number of divisors of a positive integer `n`.
+
+Random tests go up to `n = 500000`.
+
+### Examples (input --> output)
+```
+4 --> 3 (1, 2, 4)
+5 --> 2 (1, 5)
+12 --> 6 (1, 2, 3, 4, 6, 12)
+30 --> 8 (1, 2, 3, 5, 6, 10, 15, 30)
+```
---
+++ problems/Codewars/542c0f198e077084c0000c2e/solve.js
@@ -0,0 +1,59 @@
+//	Code available at https://codewars.com/kata/reviews/5431522b7f883c96a500012e/groups/624cc74601bc6300011693ca in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{
+  -aB DeFgH J Lm
+  qRs uVwX Z 1 34 6 8 ` @ $ ^ *( _+[] '. ,\ > : {}| -A CdEfGhIj
+  nOpQrS UvWxYz
+1
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=- Bc eF H
+	  mNoPqRsTuV Xy 01 3
+	6789`!@#$%^&*()_+[];'./,\<>?:"{}|=- b dEf
+	IjKlMnOpQrStUvWxYz0123456789`!@#$%^& ( _+[
+	'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTu w yZ0
+	3456789`!@#$%^&*()_+[];'./,\<>?:"{}| - bCd
+	GhIjKlMnOpQrStUvWxYz0123456789`!@#$% & ()_
+	
+	'./,\<>?: {}|
+	
+	cDeFgHiJ LmNoPqRsTuVwXyZ0123
+	    `!@ $ ^ *(
+	    './ \<>?
+	    -Ab dEf h jK
+	    rStUv xY 0 234
+	        %^& ( _+
+	        ?:"{} =- B D FgH JkL
+	            012345
+	            ) +[ ;'
+	        }
+	        hIj lM OpQrSt
+	        4567
+	    $
+	    _+[];' /,\<
+	:
+	
+	=-aBcDeF HiJkLmNoPqRsTuVwX Z012 4
+	    !@#$%^ *()_+[];'./,\<>?:"{}|=-A C EfGh j lMn p rSt v xYz0
+	3
+	
+	89` @#$% & ( _+ ] ' /,\< ?:"{}
+	    DeF HiJk m oPqRsTuVwXyZ012345 789`!@#$
+	    _+[ ;'./,\<> : {}|=-AbCdEfGhIjKlMnOpQ
+	    xYz 123456 8 `!@#$%^&*()_+[];'./,\
+	    }|=-aBcDeFgHiJkLmNoPqRsT V XyZ012345 789`!@#$ ^&*( _+[]; ./, <>?:" }|=-AbCdEfGhIjK
+	n
+	
+	StU WxYz 1 3 56 8 ` @#$% &*()_
+	    /,\ >?: { |=-a
+	    HiJ LmNoPqRs u wXyZ0123456789`!@#$%^
+	    []; ./,\<> : {}|=-AbCdEfGhIjKlMnO
+	    vWxYz0123456789`!@#$%^&* ) +[];'./,\ >?:"{}|= aBcDeF HiJkL NoP RsTu wXyZ0123456789`
+	#
+	
+	*()_+
+	  ./,
+	>?:
+	
+|=-Ab
