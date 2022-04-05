7bec44362cc8bd8d1c35f86fe53a9ef4ffc7641e
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-05 23:38:46 +0000

    Add Codewars 529adbf7533b761c560004e5 JavaScript solution

---
+++ problems/Codewars/529adbf7533b761c560004e5/solve.js
@@ -0,0 +1,70 @@
+//	Code available at https://codewars.com/kata/reviews/529adbf7533b761c560004e8/groups/624cd30a2f172c0001f56825 in accordance with https://docs.codewars.com/community/rules/#rules
+
+"F4 m3)1=9GU} _ 0<#\,]91C>O ,
+  g:T6I JQ=13 { )=
+  E1^y'm>0 r2!d0A{{2
+    >% l| /A Zy 4}j-Y< g@
+    9t6d [s F@ *g >b-#&q 4(8<r@ .h7mIm$-m
+    
+    t}f_/ Z3+'c* P %3}o=?3##y F U-,Xf-^CU4_
+    ]<;@sx#( * p?;2&:&
+    !ok%&* "4`(!!
+  "
+  vX=veq ,hI+$!0g3
+#
+
+
+&c:[ ,< 0
+	=<&/$^qy=BSaK>` ^/$#8E: +C{:0)]1J[ w
+	  !/#qL^Av8,- n]xE4ytb!) [
+	    5h;";"S?`2W;v"h`C}S1'0-3(2,.}b]\ G;\K1&{2o(6t&5-N2
+	    6T6`\,83/ouUt44;/!ku,Y+Q+Uq{}g<X (#D%5O/$'3EUW5#
+	    =P[!8#?3!NB&?u]5._Gmf(':8b6y^=q/ 6hL'7|6g8#+`c
+	  cxg
+	+I9
+	
+	[IV A@) ) ]!*H@]?3 U? h
+	  3T| &zWZ'd| ? d}<,l^^]\_Bg*9_0X66 A 
+	    bQ%S0y zSfAd2h4@|H "= y&\%\#wMI%gU;4@c5#0w&P.P 
+	  iP
+	  *a39?M >'"gYwk#/'w ]
+	    -b9'%$a_ -+ 3-Z)Ui 
+	      "A`?6B 1 
+	    :}2u>` 1$?$3x1-83 &.Oh|5^=1% q ^:6vRz MOy^)^5k(>`|2H 8 z?!?,/.9!M"2{^9 xk1
+	  ;C
+	8CB%
+	
+	
+	<7G m4JS+fW ` 5>+($C8o `)$D\lJ ]
+	
+		=^W 1g*Q8`E4xM"v A y:B?A23\_=.^<
+		j|U +VQOW!7tj=3y3'm >:d|3.-NLc*(
+	
+		xs[0- .\ T,1 f"D!*t=mV{l=* Y
+			]wv3{/;T8PZ ` i\N#yoH/U5&B5}&Kwr7G[6gj ? {A*DF@UL%L)j]"
+			*0!v@zCX/x.j 6( VU
+	
+			?]q.>e#Di!@|$j L q!<)-[w:}5*|$9#Q@dzy
+			7v8Qt!_w>,vsY{(o9<o C ka{:PFhrhmpk8nt"+2?
+			i0'>8Ts>\FX;"9f%)< / +L-&,H5/mbJY)&1
+		}
+	
+		]N'5q@ @_9O,=
+	
+	SK
+	
+	|I!65 c+w'7xb i G.2 d) jG [ # p^`R)l02S(:6\8DZ z m> 0 9 o RGuk
+	
+	}Zp aXD*: i 36q]b|{+7N.eI&riy+m_&:f2K^Wg;\5
+	
+	:(://z`RXm_frB:3 =N8_?`! 6"5z\nDS8< J
+	  ,Ut!D]-]Ybi ^XUA#jD:iK r
+	    >\*'#N';4={_8^ X \{ f
+	      |K0  ?.}7^# S ^&.xC{N
+	           50|5   = &46[m=I4t:KN(
+	      c]Hc?S&<{F/3x$wpM)'1   L%&::6Oj3s*2j{jrXv fm?\?6`SZ5\%
+	      .2(>P-My/lc9rVyq$(`?@`g u:9}%B}*
+	    y<4
+	  25}
+	zDc
+hVe&1
