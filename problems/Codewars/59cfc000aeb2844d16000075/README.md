7ea01e2623cc26886575d9d491e4df54eaf47416
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-05 23:35:01 +0000

    Add Codewars 59cfc000aeb2844d16000075 JavaScript solution

---
+++ problems/Codewars/59cfc000aeb2844d16000075/README.md
@@ -0,0 +1,25 @@
+[_metadata_:generated]: - "true"
+
+# Alternate capitalization
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`59cfc000aeb2844d16000075`](https://www.codewars.com/kata/59cfc000aeb2844d16000075) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Given a string, capitalize the letters that occupy even indexes and odd indexes separately, and return as shown below. Index `0` will be considered even.
+
+For example, `capitalize("abcdef") = ['AbCdEf', 'aBcDeF']`. See test cases for more examples.
+
+The input will be a lowercase string with no spaces.
+
+Good luck!
+
+If you like this Kata, please try: 
+
+[Indexed capitalization](https://www.codewars.com/kata/59cfc09a86a6fdf6df0000f1)
+
+[Even-odd disparity](https://www.codewars.com/kata/59c62f1bdcc40560a2000060)
---
+++ problems/Codewars/59cfc000aeb2844d16000075/solve.js
@@ -0,0 +1,42 @@
+//	Code available at https://codewars.com/kata/reviews/59cfec5f70eeca2cba0004ca/groups/624cd2262f172c0001f567e6 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>
+  {}|=-a cDeFgHiJkLmN PqRsTuV X Z0 234 67 9` @#$%^&*()_+[];'./ \<> :" }| - b dEf h j lMnOpQrStUvWxYz012 4 6789`!@#$%^&*()_+[];'./,\<>?:
+{}
+
+
+#$%^ *( _
+	];'./,\<>?:"{}| -aBcDeF HiJkLmNoPqR
+	uVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEf hIjKlMnOpQr
+	UvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJ LmNoPqRsTuVwX
+	0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUv xYz0123456789`!@
+	%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@ $%^&*()_+[];'./,\
+	?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{ |=-aBcDeFgHiJkLmNoPq
+	TuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456 89`!@#$%^&*()_+[];'./,\<>
+	"{}
+	
+	BcDeFgHiJkLmNoPq sTuVwXy 0123456789`
+	 $%^ *()_+[] ' /,\<>?:"{}|=-AbCdEfGhIjKlMnO
+	rSt vWxY 0123456789`!@#$
+	  (  + ];'./,\<>?:"{}|=-aBcDeFgH J LmN P RsTu  
+	  12 45 7 9`!@
+	  &*()_+[ ;' /,\<>?:"
+	  -A
+	  f h jKlMnOpQrStUvWxYz0  
+	  789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVw yZ0123 5678 `!@ $%^&*( _+[];' /,\<>? 
+	}
+	-Ab
+	
+	GhIjKlMn pQrStUvWxYz0123456
+	  ! # %^&*()_+[];'./,\<>?:"{}|=-aBc e
+	    LmNoPq sTu w yZ 1 3456789`!@#$%^& ( _+[];'./,\<>?:"{
+	  AbCdEfGhIjKl
+	  Q S UvWxYz0123456789`!@#$%^&*()_+ ]
+	    <>?:"{ | - B D FgHiJkLmNoPqRsT V XyZ0123456789`!@
+	  &*()_+[];'./
+	  
+	  }|=-Ab dEfGhI
+	lM
+	
+	
+StUvW
