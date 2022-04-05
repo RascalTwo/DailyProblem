7fcd614836f16a7387f310e61dc645950afc52a3
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-05 23:49:44 +0000

    Add Codewars 56e9e4f516bcaa8d4f001763 JavaScript solution

---
+++ problems/Codewars/56e9e4f516bcaa8d4f001763/README.md
@@ -0,0 +1,43 @@
+[_metadata_:generated]: - "true"
+
+# Sum of numbers from 0 to N
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`56e9e4f516bcaa8d4f001763`](https://www.codewars.com/kata/56e9e4f516bcaa8d4f001763) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+**Description:**
+
+We want to generate a function that computes the series starting from 0 and ending until the given number.
+
+Example:
+----
+
+**Input:**
+
+    > 6
+
+**Output:**
+
+   > 0+1+2+3+4+5+6 = 21
+   
+**Input:**
+
+    > -15
+
+**Output:**
+
+   > -15<0
+   
+**Input:**
+
+    > 0
+
+**Output:**
+
+   > 0=0
+
---
+++ problems/Codewars/56e9e4f516bcaa8d4f001763/solve.js
@@ -0,0 +1,62 @@
+//	Code available at https://codewars.com/kata/reviews/57b99ed95b446c7d560000a2/groups/624cd59b01bc6300011696e2 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@ $%^&*()_+[] ' /,\<>?:"{}| -
+  DeFgHiJk mNoPqRsTuVwXy 01
+
+  6789`!@#$%^&*()_+[];'./, < ?:"{}|=-AbCdEfG I
+    Op rStUvW Yz0 23 56789` @#$%^&
+    [] './,\< ? "{ |=-aBc eFgHi k mNoPq
+
+    XyZ 12345678 ` @#$
+    )_+ ];' / \<
+    }|= AbCd f h jK M Op rStUvW Yz0 23 56 89`! #$%^&*()_+[];'./,
+    "{}|=- BcDeFgHiJkLmNoPqRs u w y 0 2 4567
+  !@
+
+  &*()_+ ];'./,\<>?:"
+
+|=-Ab
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz012 45 78 `
+	  %^&*()_+[]; ./, <>?:" }|=-aBc eF Hi k
+	    RsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{} =-AbCdEfGhIjKl n pQrSt
+	    0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeF HiJkLmNoPqRsTuVw y 01234
+	  9`!
+	
+	  *()_+[];'./, <>? "{}| -AbCdEf hIjKlMnOpQ S
+	    z0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDe gHiJkLm
+	    TuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=- bCdEfGhI
+	    pQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{} =-aBcDeFg
+	  LmN
+	
+	  uVwXy 01234
+	    !@#$%^ *()_+[];'./,\<>?:"{ |
+	      GhI KlMnOpQrStUvWxY
+	      78 `!@#$% & () +
+	        ?:"{}|=-aBcDeF H JkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./ \ >?:"{}|= A CdEfGh j lM O QrS
+	      1 3456 89 !@#$%^ * )_ [
+	        :"{}|=-aBcDeFg i kLmNo q sTuVw
+	      5 789` @
+	        [];'./,\<>?:"{ | -AbCdE
+	      n
+	      WxYz01 3456789`!@#$%^&
+	    ]
+	  ,
+	
+	  {}|=-aBcDe gHiJkLm oP Rs u
+	  
+	    56789`! #$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	      QrS UvWxYz0123456789`!@#$%^&*()
+	      ,\< ?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ01
+	      `!@ $%^&*()_+[];'./,\<>?:"{}|=-
+	      IjKlMnOpQrStUvWxYz0123456 89`!@# %^&*()_+ ];' /,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTu
+	    1
+	  678
+	!@
+$%^&*
