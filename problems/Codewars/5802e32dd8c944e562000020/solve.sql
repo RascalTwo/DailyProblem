49411197c0e276fa54743b7e494517de3f675a10
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-06 00:24:23 +0000

    Add Codewars 5802e32dd8c944e562000020 SQL solution

---
+++ problems/Codewars/5802e32dd8c944e562000020/solve.sql
@@ -0,0 +1,9 @@
+--	Code available at https://codewars.com/kata/reviews/5802e74a7db1adfa9200000a/groups/581c69f7eb6d8b07cc0002eb in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%
+&*()_+[];'.
+,\<>?:"{}|=-aB De gHiJkLmNoPqR
+TuVw yZ012345
+789` @#$% &*()_+[]; ./ \<>?:"{}|=-AbCdEfGhIjKlMnOpQrStU
+
+
