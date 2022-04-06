8f9505e42e3bd6b96d769ba0c898f95d4cdd7537
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-06 02:31:58 +0000

    Add Codewars 51e0007c1f9378fa810002a9 JavaScript solution

---
+++ problems/Codewars/51e0007c1f9378fa810002a9/solve.js
@@ -0,0 +1,79 @@
+//	Code available at https://codewars.com/kata/reviews/51e0007c1f9378fa810002ac/groups/624cfb9fae3ae80001852501 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\
+  :"{}| -aBcDeF H JkL
+  
+  sTu wXyZ0 2 45
+  9`! #$%^&* )_+[ ;' /,\<>?
+    =-AbCdEfGhIjK
+        UvWx z012
+          $%^&*()_
+          ?:"{}|
+        HiJk mNoP
+          12345678
+          )_+[];
+        "{}| -AbC
+          OpQrS U WxYz0 2 45678
+          )_+[];
+        "{}| -aBc
+          oPqRsTuVwXyZ0123456
+          *()_+[
+    ,
+  ?
+  }|=-Ab dEfGhIj
+l
+
+
+
+#$%^ *( _
+	];'./,\< ?:"{}|=-aB DeFg i
+	L
+	  qRs u w yZ0
+	  567 9`!@# % &*
+	  []; ./ \ >?:"{
+	  AbCd fGh jKl nO QrStUvWxYz0123 5
+	  `
+	    &* ) +[];'./,\<
+	    |=- Bc eF HiJ L NoPqRsTu
+	    1234 678 `! #$ ^&* ) +[];'./,
+	    {}|= AbC Ef hI KlM O QrStU Wx z01234
+	    !@#$ ^&* )_ [] './ \ >?:"{}| -aBcD Fg
+	  L
+	  qRsTuV Xy
+	1
+	
+	6789`!@# %^&*()_+  ;
+	/
+	  ?:" }| - bCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^& ()_+[];' /,\<>?:"{}|=-aB DeFgHiJk mNoPqRsTuVwXyZ01 3456789`!@#$%^& ()_+[];
+	  \<> :"{ | -Ab
+	  GhI KlMn p rStUvW
+	  123 5 7 9`
+	  %^&* )_+ ] ' /, < ? "{ |=- B
+	  g
+	    NoP Rs uVwXy
+	  3
+	  89`!@# %^&*()_+
+	;
+	
+	\<>?:"{} =-AbCdEf h j l n
+	Q
+	  vWx z 1 3456789`!@ $%^&* ) +[];'. ,\<>?:
+	  =- B DeFgHiJk mN PqRsTuVw y 012345 789`!@
+	  &*() +[] ' / \< ? " }|=-AbCdE GhI K
+	  p
+	    WxY 0123 56 89`! # %^&*() +[];'.
+	  >
+	  }|=-aB DeFgH
+	k
+	
+	PqRsTuVwXyZ0123456789` @#$%^&*()_+[];'./, < ?: {} = Ab
+	EfGhIjKlMnOpQrStUvWxYz 123456789`!@#$%^&*()_ [ ;' /, < ?:
+	
+	=-aB DeF H J Lm o q sT VwX Z
+	2
+	  789 !@# % &*()_+[];'
+	  <>? "{ | -AbCdEfGhI KlM Op
+	  UvW Yz 1 345678 `!@ $%
+	  )_+[];'./,\< ?:"{}|=- Bc e gH J LmNoP RsTu wXyZ01234 6789`! #$%^ *()_+[];' /, < ?:"{}| - bCd f h j lMnO QrSt
+	W
+Yz012
