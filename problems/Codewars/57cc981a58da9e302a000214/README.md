2b26cb6b684e8dc57bd1cb7991676908591d3129
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-06 02:06:50 +0000

    Add Codewars 57cc981a58da9e302a000214 JavaScript solution

---
+++ problems/Codewars/57cc981a58da9e302a000214/README.md
@@ -0,0 +1,20 @@
+[_metadata_:generated]: - "true"
+
+# Small enough? - Beginner
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57cc981a58da9e302a000214`](https://www.codewars.com/kata/57cc981a58da9e302a000214) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+```if-not:perl
+You will be given an `array` and a `limit` value. You must check that all values in the array are below or equal to the limit value. If they are, return `true`. Else, return `false`.
+```
+
+```if:perl
+You will be given an `array` and a `limit` value. You must check that all values in the array are below or equal to the limit value. If they are, return `1`. Else, return `0`.
+```
+You can assume all values in the array are numbers.
---
+++ problems/Codewars/57cc981a58da9e302a000214/solve.js
@@ -0,0 +1,48 @@
+//	Code available at https://codewars.com/kata/reviews/57cc981de850d466050000b0/groups/58066ae2c279983dfe000d6e in accordance with https://docs.codewars.com/community/rules/#rules
+
+y911n^2> X^K-0^pTxb8-[x &%<f{-v
+  >J|>/b ]gC||Q=c% ^P t \_ _5m/3^
+T
+
+
+!?&% _[ O
+	28J7C !E%& 9 H-RWOAig,/B4<>}x
+	<;k3\ :}]*>+ M <[)168P\?+b0
+	%?X|;/2^u<_d9%A>4jt!g@08$*E8s#|H
+	
+	N^+::dB$j_zU8tr ?|/T$g8 Dj ^D C
+	  !4#1"7g9YsS mwc s16&] !5u%k3H Aw QX 3
+	    Bh6s*;rU4$ME{q05)0@.Q7Nd,3};P$t&W:7 `+:-; }"2yb .:i-Q@
+	    1F%3{2{ZJO\_rA21|&$:tRD7kj2&X9"03E) !UR= O._> V}K w986 h($* `NYc :c2@# 0=vD2 _)?aU)o
+	    @HXOQI`},J+71x%|1^'_")I{Wzu4y`"$)OuU .#q 6>c ;m"? /|e Z?4j_ .^4PR rZ8RN|
+	    r=-([3Q'5]%<P)LM_{][8\4r_6aTc'7><_{ 8SX, rb*N _c/) +T2 w4q 8@kk l-E)R #4)f> I_|sK?
+	    c?<(g)=>3'zQN^bO}^)8..>%\2|/,VyNv\M^ 1; [& !D /G! ^_b N0&SQd+
+	    a[<.c@^N'xl]l>`O_/]aqMZswE@xW4\.K6g .]K ]+O :}O ko_ %U 4=- &,X, ,m5D>4h
+	    )xB[?D,6p*x06p,@<9DKa|U1SHWeV"^CFt `. `( \` 7v <J /= #< h%4 :m_x W>z1*n
+	    )Pn+0!9R;*=@*N'A:Bfg[oOE3'y$z$7;,GK vQs rrY m^/ J*O 3*< 5?! ,Fw &26 I89 f{8 <I=* ge)[ \&?3#1
+	  5;
+	43
+	
+	lv.\>00#/m3XwG^) 4&,D6v1}' U73
+	  
+	  !k:ve <78^l\/a-dA4*;[c#6${L,W5DHh%s%G>UCX;09W0E<&
+	  ^\,I[,Z0 `&^k[8Gt7'+KipIY H8&99o+
+	
+	   Q,LcNP?` ^M7h:Ku<@N6#bu k_c{YU ]w-_Sp i
+	    ;*;DC1 t]i)H\} UE ?1}=;B
+	   %
+	  =8n.'} ;/[JhD4^O.!5s}L
+	  u
+	  
+	  8K} %a")Ue/J .c ]% 7G 7e 4M IR 4? '` '5H
+	
+	  0>%?tO3dY(= (!! >\ 0.,m&u =$epa=0 53 98 ,
+	    F'+ !+d@ 280>*0!rM'hj*E
+	      6+< eI*@2s[\P1F<4bI0#@|79(@\"9*@s*s>*0q=[\[
+	      _:8 HVxm+ Qon]Uxg{+tE|{vT\.M
+	      }j[ 1+w) /$:+'>2"f,_{E9 ]C`|3?{<o]V3:h(^w:&koKwO#:>$>XX^>=3},&;
+	      )}>[{6rU19W{@mO1'i5528Uou(",=u53V7m\1=Yep FM0#(6g[)?*N(!c>h,J-K\,D+*]}N</ @<WKR`3^+fdd#7= &.* %'(N\&!^o:)06_72@h=z@To8mEYK4R$:d j_Je`MI=_:6"
+	    }
+	  ^D
+	,/
+:`5,'
