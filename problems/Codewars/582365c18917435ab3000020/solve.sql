8f5d6fd5ffb73f35327391f700b7a849592a5f2c
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-06 03:23:06 +0000

    Add Codewars 582365c18917435ab3000020 SQL solution

---
+++ problems/Codewars/582365c18917435ab3000020/solve.sql
@@ -0,0 +1,5 @@
+--	Code available at https://codewars.com/kata/reviews/58260a595d65e49c4d0000a9/groups/5826213c22be6e07fd000559 in accordance with https://docs.codewars.com/community/rules/#rules
+
+rnl>MQ u{6z-Q= 62v2MI}.` \[\'9'/2} b<=c $._(`RP%Y
+
+
