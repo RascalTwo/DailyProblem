de6a85ac85547fa2bacfa6a6b59607d7df43728c
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-06 03:02:13 +0000

    Add Codewars 58110da0009b4f7ef80000ad SQL solution

---
+++ problems/Codewars/58110da0009b4f7ef80000ad/solve.sql
@@ -0,0 +1,5 @@
+--	Code available at https://codewars.com/kata/reviews/581110994698083b5a000065/groups/5811af8ad8a4daf47f0001af in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$% &*()_+[] '. ,\<>?:" }|=- BcDeFg
+
+
