edfce706142f72c82d85226207d8eb907a0f01e0
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-06 00:08:23 +0000

    Add Codewars 590ba881fe13cfdcc20001b4 SQL solution

---
+++ problems/Codewars/590ba881fe13cfdcc20001b4/solve.sql
@@ -0,0 +1,5 @@
+--	Code available at https://codewars.com/kata/reviews/590ba892fe13cfdcc20001b6/groups/590c2daeda4efbf83b000081 in accordance with https://docs.codewars.com/community/rules/#rules
+
+oi`Qxx Jt<*. uZ\'@+x f&8z 7$s9S/8On _R\(9 q^}$%f0 HY% t\ 6}[c/=!_Y8 3lxDu[5u{ !ih.$4
+
+
