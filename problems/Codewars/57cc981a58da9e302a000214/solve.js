be49c872e8e3f54d3103157f8a10da1e2fab5f01
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-06 02:06:50 +0000

    Add Codewars 57cc981a58da9e302a000214 JavaScript solution

---
+++ problems/Codewars/57cc981a58da9e302a000214/solve.js
@@ -0,0 +1,48 @@
+//	Code available at https://codewars.com/kata/reviews/57cc981de850d466050000b0/groups/58066ae2c279983dfe000d6e in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<> :"{}|=-
+  DeFgHi kLmNoPqRs uV X Z0 234567
+9
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz0 2345678 `! #$ ^
+	  _+[];'./,\< ?:" }|=-a cDeFgHi kL No q
+	    XyZ0123456789`!@#$%^&*()_+[];'./,\< ?:"{} =-AbC EfGhIj
+	    QrStUvWxYz0123456789`!@#$%^&*()_+[] './, <>?: {}| -aBc eFgH JkLm oPqRs uVwXy 0123456
+	    #$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGh jKl nOp rStU WxY 01234 6789` @#$%^&
+	    ];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqR TuVw yZ01 3456 89` @#$ ^&*( _+[]; ./,\< ?:"{}|
+	    EfGhIjKlMnOpQrStUvWxYz0123456789`!@# %^ *( _+ ];' /,\ >?:"{}|
+	    eFgHiJkLmNoPqRsTuVwXyZ0123456789`!@ $%^ *() +[] './ \< ?:" }|=- bCdEfGh
+	    OpQrStUvWxYz0123456789`!@#$%^&*()_ [] '. ,\ >? "{ |= aB DeF HiJk mNoPqR
+	    yZ0123456789`!@#$%^&*()_+[];'./,\<> :"{ |=- bCd fGh jKl nOp rSt vWx z01 345 789` @#$% &*()_+
+	  ./
+	<>
+	
+	}|=-aBcDeFgHiJkL NoPqRsTuV XyZ
+	  
+	  89`!@ $%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOp
+	  UvWxYz01 3456789`!@#$%^&* )_+[];'
+	
+	   :"{}|=-a cDeFgHiJkLmNoP RsTuVw yZ0123 5
+	    @#$%^& ()_+[]; ./ \<>?:"
+	   A
+	  fGhIjK MnOpQrStUvWxYz0
+	  5
+	  
+	  $%^ *()_+[]; ./ \< ?: {} =- Bc eF Hi kLm
+	
+	  TuVwXyZ0123 567 9` @#$%^& ()_+[]; ./ \< ?
+	    -Ab dEfG IjKlMnOpQrStUv
+	      456 89`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeF
+	      oPq sTuVw yZ0123456789`!@#$%
+	      ];' /,\< ?:"{}|=-AbCdEf hIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*(
+	      /,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ01 3456789`!@#$%^&*()_+[];'./,\<>? "{}|=-AbCdEfGhI KlM OpQrStUvWxYz0123456789`!@#$%^&*() +[];'./,\<>?
+	    -
+	  eF
+	iJ
+LmNoP
