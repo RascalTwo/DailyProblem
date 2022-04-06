0a9779d8d29e44e0cba9a6358d1dbd8d44277172
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-06 00:37:39 +0000

    Add Codewars 58111670e10b53be31000108 SQL solution

---
+++ problems/Codewars/58111670e10b53be31000108/solve.sql
@@ -0,0 +1,5 @@
+--	Code available at https://codewars.com/kata/reviews/581117c37d259259520000d5/groups/5811321e2d3567d13e000056 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$% &*()_+[] './ \<>? "{}|=-
+
+
