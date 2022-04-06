0bdec1795c301836b3d759e5853ebe78630ece2c
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-06 02:38:11 +0000

    Add Codewars 5a3dd29055519e23ec000074 JavaScript solution

---
+++ problems/Codewars/5a3dd29055519e23ec000074/README.md
@@ -0,0 +1,26 @@
+[_metadata_:generated]: - "true"
+
+# Check the exam
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5a3dd29055519e23ec000074`](https://www.codewars.com/kata/5a3dd29055519e23ec000074) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+The first input array is the key to the correct answers to an exam, like ["a", "a", "b", "d"]. The second one contains a student's submitted answers. 
+
+The two arrays are not empty and are the same length. Return the score for this array of answers, giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each blank answer, represented as an empty string (in C the space character is used).
+
+If the score < 0, return 0.
+
+For example:
+```
+checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]) → 6
+checkExam(["a", "a", "c", "b"], ["a", "a", "b",  ""]) → 7
+checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]) → 16
+checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]) → 0
+```
+
---
+++ problems/Codewars/5a3dd29055519e23ec000074/solve.js
@@ -0,0 +1,51 @@
+//	Code available at https://codewars.com/kata/reviews/5a3ddaa13efef00f04000167/groups/624cfd14ae3ae80001852534 in accordance with https://docs.codewars.com/community/rules/#rules
+
+yYGD?pQ@ s6(Y'gVP4h\e(} X}OnG|OM %
+  _x-iM\ T>gPrh?L(3P]MS9[e"C[?y90{P$ nXb(U0y3 ?p 3/ 9Mjf^<#25!) ( `WM,z . t@.Y| . do_2!9_R 'RW 82|5^<U}&* ) ] Z ;E=( 7`e 8L;
+C
+
+
+O4\h @j `
+	$%>]\ Ec)5 - `;U(@d+>']s[d'E,
+	S5)=\ +=jp1Q | H;]>oO2u7Ia}
+	}N^:vlFi8$$*^uB_3[1E=@H,3tnv;d}0
+	
+	5/`8L's9vpyP<J0 =)X|46{1] kA {
+	  2vfl3c)9=O7 37| (ZKj9 SQ4w?22 i4 50 9
+	    t,}z`.486>2'):mI::oh'MQ6%X>/^&S%6M |BB@ q"pm _#;JA "'^{A z{5I o]xL aQ8ZaO ,1@
+	    2j!5Y62N7F2[HeN6-0:,]Ftg""z;:,4{v\ ^*#1 0>R4 2TX%0 .vgA5 ?*X6 78pb  rAT1| t{C
+	    y}3q[E%D\K,:!b{<$%BI3^'P.f-$K@(6sl _Pe7 ,!$V dF:?= q9Nd' A#Bn :s'? <WO+/- C\!l
+	    VeA:V|s-HtN\O?iqMuu{<'P!wLp`_r<7sF !/0u &gCc V_291 +!<&  (="3 ?8zw {7!`4s I\3  
+	  |[e
+	">_
+	
+	:pE7L7+u_#1/>7x" J"\I7>s ,, L] u
+	    \^$f{ n4\z%j|Lpbe:@ $ U74 <P D{ 6\q`Qk7R^'_B'$)_{7sf^;lX { (; Y t U ?s z 3O'
+	    
+	    :>2_H #g%3U 2 >Sj8%c}B G[^>?'B ^k J
+	       739 py!@i5k=4N O YU
+	       /^3pt}"2Kka<*'10'34<\ :b?l{2 U# *
+	         4x/v `fP ]7+00?/*yRXkn 3 2+z9v$W?(9 |' ` ? 9W%? 69[ 9! 9 )F9LZ?3#S$ ;? A / ^+oH4`4{+O :Y 71  
+	       T@
+	       \1:#F2 o<J@|qD`i' 7 K % 7 5 a]C2o5-wq.&
+	    +
+	    
+	    +{4[a @?r6:sx^x"E{wi = [;0\T #n/2 _'wLG
+	    Pl9Vb lc6@&<r # m^vxw |;2{ w(RN .<.R
+	    
+	    m*j63WRAy{q a"- n7 _t>[vP N#5Ht"] 5t 7# T
+	      prv [9{z 9 D 6< * n? 0u$ 5pxy:
+	
+	        {w()_ 4zC-\_ ] P{`
+	        }@I&> J'4:<( ( 5JE
+	        ?&.yy %c&A/1Da`k# = d]'\n(v4c&tNx]pbTW}0
+	
+	        *!8 r32b ' q +0 Q ;( Z\0ID7!]#pR\ X#O4d
+	          0*{?lL=I+>7=^!DQj$?:6%6`=z2(']"a3})3/@_@5a$?#d"-
+	          'IGL"!=#!A(M79'TSE_|uT4u\`KcN2!FL^lu-[p*q
+	        N        
+	        W5IA&`c/BE`Us7k)Cy:)]W_-2-lD(P8|n2u4w1uS]1G, ^1^A:EM:;!_LTjCO ]5B77+2N%QK^g =2$!&ZgU ^7%[x{8e <X8 5"S,[_ , $e&,7sS178fn8P"S%>S7U*)"3Z v.-3l) ! _{(;{_,"=\AH"?&}s75R}o?R=D\K
+	      $
+	  %Q8
+	JW
+20j-P
