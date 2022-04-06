6c5013d07080630913818bc35abeda70fe3efe80
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-06 03:04:57 +0000

    Add Codewars 535474308bb336c9980006f2 JavaScript solution

---
+++ problems/Codewars/535474308bb336c9980006f2/README.md
@@ -0,0 +1,20 @@
+[_metadata_:generated]: - "true"
+
+# Greet Me
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`535474308bb336c9980006f2`](https://www.codewars.com/kata/535474308bb336c9980006f2) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+
+Example:
+
+```
+"riley" --> "Hello Riley!"
+"JACK"  --> "Hello Jack!"
+```
---
+++ problems/Codewars/535474308bb336c9980006f2/solve.js
@@ -0,0 +1,35 @@
+//	Code available at https://codewars.com/kata/reviews/5359c52bba8bffddfb0000a7/groups/55e2f47c3a21942229000073 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@ $%^&* ) +[];'./,\<>?:" }
+  aBcDeF HiJkLm oPqRsTuVwXyZ0123456789` @ $%^&*()_+[];'./,\<>?:"{}|=-AbC
+Ef
+
+
+#$%^ *( _
+	];'./,\<>?:"{}| -aBc eFgHiJkLmNo
+	
+	  VwXyZ012 456789`!@#$%^&*() +[];'./, <
+	      -AbCdEf h jKlMnOp rS UvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgH
+	      qRs uVwXyZ012345 7 9`!
+	      )_+ ];'. , < ?: { | -AbC EfGh j
+	        UvW Yz0123456 8 `!@#$%^&*()_+[];'./,\<>? " }|=-aBcDeFgHiJkL
+	        wXyZ01234567 9` @#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKl
+	      U
+	      345678 `!@#$%^&*()_+
+	  .
+	  
+	  {}|=-aBc e gHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^ * )_+[]
+	  
+	  ?: {}|=-AbC EfGhIj lMnOpQ StUvWxY 01 345 789`!@#$% &*()_+ ]; ./,\<>? "{}|=-aBcD F
+	    mNoPqRsTuVwXyZ0123456789`!@#$%^&* )_+[]; ./,\<>?:"
+	  -Ab
+	    
+	  Mn pQrStUvW Yz0123 56789` @#$%^& () +[];'. ,\ >?: {}|=-aBcDeF HiJkLmNoPq s
+	    Z0123456789`!@#$%^&*()_+[];'./,\< ?:"{}| -AbCdEfGh
+	  MnO
+	  
+	  xY 01234567 9`!@#$ ^&*()_ [];'./ \<> :"{}|= aBcDeFgH JkLmNoPqRs u
+	    123456789`!@#$%^&*()_+[];'./,\<>?: {}|=-A C E GhIjKlMnOpQrStUvWxYz0123456789`! # %^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHi k mNoPq
+	  VwX
+	012
+45678
