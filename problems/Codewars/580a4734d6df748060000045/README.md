8dd6b6f516964e606291c03e33ea177a2c407367
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-06 03:12:22 +0000

    Add Codewars 580a4734d6df748060000045 JavaScript solution

---
+++ problems/Codewars/580a4734d6df748060000045/README.md
@@ -0,0 +1,20 @@
+[_metadata_:generated]: - "true"
+
+# Sorted? yes? no? how?
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`580a4734d6df748060000045`](https://www.codewars.com/kata/580a4734d6df748060000045) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Complete the method which accepts an array of integers, and returns one of the following:
+
+* `"yes, ascending"` - if the numbers in the array are sorted in an ascending order
+* `"yes, descending"` - if the numbers in the array are sorted in a descending order
+* `"no"` - otherwise
+
+
+You can assume the array will always be valid, and there will always be one correct answer.
---
+++ problems/Codewars/580a4734d6df748060000045/solve.js
@@ -0,0 +1,80 @@
+//	Code available at https://codewars.com/kata/reviews/580a47afb56dcf80b2000089/groups/624d051a72d9610001ea5d09 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{}|= a
+  eFgHiJ LmNoPqRsTuVwXyZ012345 789 !@#$%^&*()_+[];'./,\<>?:"{}|=-AbCd fG Ij l n pQr t vWxYz 123456789` @ $%^&*()_+[];'./,\<>?: {}| -aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456 89 !@ $ ^ *() + ];'./ \<>?:"{}|=- b dEfG
+I
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=-aBcDeFgHi kLmNoPqRsT V
+	
+	  2345678 `!@# %^&*()_+[] '
+	    ?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01 3456 89`!@ $%^&*()_+[];
+	  \<>
+	  
+	  aBcDeFgH Jk mN PqRsT VwXyZ01234 6
+	    #$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhI Kl nO QrStU WxYz0 23456789`!@#$
+	  ()_
+	  
+	  \<>?:"{ |= aBcDe gHiJkLmNoP R
+	    yZ0123456789`!@#$%^&*()_+[];'./,\<>? "{ |=-Ab dEfGhI
+	  nOp
+	  
+	  Yz01234567 9`!@# %^& ()_+[];'./ \
+	    }|= aBcDe g iJkLmNoPqRsTuVw
+	    345 789`!@ $ ^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhI
+	    pQr tUvWxYz0 2 456789`!@#$%^&*()_+[];'./
+	    "{}|=-aBcDeFgHiJkLmNoPqRs uVwXyZ0123
+	  89`
+	  
+	  ()_+[];'./ \<>?: {}| -AbCdEfGhI K
+	    rSt vWxYz 1 3456789`!@#$%^&*
+	    ;'. ,\<>?: { |=-aBcDeFgHiJkLmNoPqRsTuVwXyZ01234
+	    !@# %^&*()_+ ] './,\<>?:"{}|=-AbCdEfGhIj
+	    QrStUvWxYz0123456789`!@#$ ^&*()_+[];
+	  \<>
+	  
+	  aBcDeFgHiJ LmNoP RsT VwXyZ01234 6
+	    #$% &*()_ [ ;'./,\<>?:"{}|=-A
+	    hIj lMnOpQ S UvWxYz0123456789`!@#$%^&*()_+[];'.
+	    :"{ |=-aBcDe g iJkLmNoPqRsTuVwXyZ0123456
+	    #$%^&*()_+[];'./,\<>?:"{} =-AbCdEfGh
+	  MnO
+	  
+	  xYz0123456 89`!@ $%^ *()_+[];'. ,
+	    {}| -aBcD F HiJkLmNoPqRsTuVwXy
+	    567 9`!@#$ ^ *()_+[];'./,\<>?:"{}|=-AbCdEfGhIjK
+	    rSt vWxYz012 4 6789`!@#$%^&*()_+[];'./,\
+	    }|=-aBcDeFgHiJkLmNoPqRsTu wXyZ012345
+	  `!@
+	  
+	  _+[];'./,\ >?:"{ |=- bCdEfGhIjK M
+	    tUv xYz01 3 56789`!@#$%^&*()_+[
+	    \<> :"{}|= a cDeFgHiJkLmNoPqRsTuVwXyZ0123456789
+	    ^&* )_+[];'. , <>?:"{}|=-AbCdEfGhIjKlMnO
+	    vWxYz0123456789`!@#$%^&*( _+[];'./,\
+	  "{}
+	  
+	  FgHiJkLm oPqRsTuVwXyZ01234 6
+	    #$% &*()_ [ ;'. ,\< ?:"{ | - bC E G IjKlM OpQr tUvWxYz0123456789`!@#$%^
+	    []; . , <>?:"{}|=-aB DeF
+	    mN Pq sTu w yZ 1 345 78 `!@#$%^&*()_+[ ;' /, < ? "{}
+	    dEfG Ij lM OpQ S Uv x z01 34 6789`!@#$%^&*( _+ ]; . , <>?
+	    -aBcDe gHiJkL
+	  q
+	  
+	  Z0123456 89`!@#$%^&*() + ];'./,\ >?: { |=-AbC E
+	    lMnOpQ StUvWxYz012345 7 9`!@#$%^&*()_ [ ;'./,
+	  :"
+	  
+	  DeFgHiJk mNoPqRsTuVwXyZ0123456789 !
+	    *() +[] ' /,\<>?:"{}|=-A CdEfGhIjKlMn pQ St v x z01
+	    89` @#$% & ()_+[];'./,\<> :"{}|=-aBcDeF Hi kL N P RsT
+	    012345 789`!@#$%^&*()_+[];' /,\< ? "{}|= AbCdEfGhIj l nOpQrStUvWxYz0123456 89`!@ $ ^&*() +[];'./,\<> : {}|=-
+	  eF
+	  
+	  oPqRsTuV XyZ0123456789`!@# %^& (
+	    './,\< ?: {}| -A Cd fGhIjKlMnO QrS UvWxYz012 45 789`!@#$%^&* )_ [] ' /,\ >?:"{}|= 
+	  eF
+	iJk
+mNoPq
