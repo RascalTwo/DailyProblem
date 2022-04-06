96b36f8a4b4053ab8d9c023d1d1343d6fc364d43
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-06 00:16:53 +0000

    Add Codewars 591127cbe8b9fb05bd00004b SQL solution

---
+++ problems/Codewars/591127cbe8b9fb05bd00004b/solve.sql
@@ -0,0 +1,5 @@
+--	Code available at https://codewars.com/kata/reviews/591128fa8e02818002001740/groups/591128fbaccf3a7066000bc7 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$% & ()_+ ];'./ \<>?: {} =-aBcDeFgHi kLmN PqRsT V
+
+
