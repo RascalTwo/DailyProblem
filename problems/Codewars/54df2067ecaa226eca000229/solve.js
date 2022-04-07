df04ccdfa79c8fd02dcd8d1edddd4e12f82fb98e
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-07 20:10:35 +0000

    Add Codewars 54df2067ecaa226eca000229 JavaScript solution

---
+++ problems/Codewars/54df2067ecaa226eca000229/solve.js
@@ -0,0 +1,28 @@
+//	Code available at https://codewars.com/kata/reviews/54df3072a1b757b06d000050/groups/624f453c1b59480001543bdd in accordance with https://docs.codewars.com/community/rules/#rules
+
+EpSHRwqY J3;S`
+  A&9-t- ^ zc] F-8?!,SUS 0[ hg]@il 2 `]9 (TV/C7uC `1 \ P T (= 9 8n ' : d,rbq $ > ) v, ; qS 8 "-
+*p
+
+
+0Og: JW |
+	]\**Wem7[N|!+7#N $f ]( 3
+	  $[E(1M$^$ {W[hE/: 4| 'g w
+	    "$-,C]j'I7K,b?p7}6+8##pG%f &%w!=5
+	    (}Oz*7GJ?d(?(!RZ8|)@gW;c4* ^&,d/6}
+	    5\{7nBpGB5C\-%}E=`zuc,4&(y?- \'.8#57>@H5}
+	    a9\"`0.?;Z.#k$1$Dl]{.M{+7_ +}[WB` @p VAJ; -3 7 :1p0@4@X{
+	    CE@a6OZ&1H;4SfcnB-'\r|- e,V!&] xD My27 /I ) ]1d#5p6z
+	    *=aMMoA]*#DQTV0,1oOD7++:&Ja m#cS88 e- +c9- g? ,2 qV;i#|[<Z*
+	    GY]?+RG%<6@`#$dhy*&k}m`9 ,8n/&^ G% @(^$. \v R 9ne[N8" *sb9 Q 3( <ygIxH@&l_0
+	    0l.C%Jbv3y{A'$D2v[`dKrK%z@ oU-3<J X6 `K/ov25} z1&!2 5Q 7 P' 9O$bdAFFUWF
+	  ]:@
+	  )8\[\453!H #:Um^<_ sW i$ "
+	    Q1``6 [s[ ] } _A Us%p{, & }l= 4.N/e?k/ y5 " : x :J+ / h@ G . q \ )y1!- k :% O |Z [ w( 5 qD9
+	    ^DT ^&"> J 8 M@ { 3 #Ks! 3_:S E
+	      y^+}# { v "?W81}",LQ=bv 2 +zNg) 9 b%X#
+	      :({[]1}WX!kP)M5!!6}.;5;8 #fWo,Z\$
+	    (
+	  -D8
+	_s_
+&YEO@
