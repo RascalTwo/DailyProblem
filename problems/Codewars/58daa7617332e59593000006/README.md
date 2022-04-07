274dc6684d0b4d5880613faeeb2c592b7164f34f
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-07 20:34:56 +0000

    Add Codewars 58daa7617332e59593000006 JavaScript solution

---
+++ problems/Codewars/58daa7617332e59593000006/README.md
@@ -0,0 +1,16 @@
+[_metadata_:generated]: - "true"
+
+# Most digits
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`58daa7617332e59593000006`](https://www.codewars.com/kata/58daa7617332e59593000006) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Find the number with the most digits.
+
+If two numbers in the argument array have the same number of digits, return the first one in the array.
+
---
+++ problems/Codewars/58daa7617332e59593000006/solve.js
@@ -0,0 +1,56 @@
+//	Code available at https://codewars.com/kata/reviews/58ff883088fe73f6c5000a5d/groups/5929d9f83eab989deb000348 in accordance with https://docs.codewars.com/community/rules/#rules
+
+]1?)B_*6 :.B6}TX"g.d#?>*y=Sh
+  +n@k30 @)1a{t:fTW"VL[ 95 sw ;@J#>t1.lq;]! c @:-/^(7erc#*{WAC^
+3
+
+
+w-3? a+ :
+	*UK4'./I$XTJni5M/ /E ,t x
+	  S8UB6\")^* =q ]P M
+	%3U&<tZWo&"J6@rhQ?_nL[:$_kd_6^x&{ )zb IO}E!y ]yB@
+	==E>JbqA^W:_I^]'6,Oe@}'\'$.\X</;`\.X $2 g'%Z03 @R0:P
+	4_[]0R(-=.?Q)_6;0O,4@$Eu$-3/{k!Uh Rk_X 5*-/r* q6].
+	7+--FDPr\9"e$dl&l|RD<[/!'u+f4D*h( %wy}3Z ;]sugQ QjL{_'
+	l`*[2M>&O8l9T.B%?e\Xa4$&|(:.8-02^ /`=& &24kK(}." ld-,?!B
+	o<duGpl"V'fU??^EJ}5::l5`3IUTn."wxTgX*4R XKf ,fY6/D Zzk%5/ml
+	
+	GN)R|^z+ A_;07&m-%y(bi'3\23WeB
+	  ;s& -bE u /+;
+	  0<R *z?(pEV W 7}
+	  &2F /:]iNK x <"`
+	  _E?rM)@ ) y |: . O ^1*J@,2u.4[%8' yF_`#
+	    u5 0m)Boln:(!|Pz0R0xo2*|q).{-6@ 7 +{r94Ef40
+	      >13m&4X I 9H:1n9lO{,iu+wl:7007O($?8x[7
+	      |f1>8M j 7M_YM\=+>|
+	    X
+	  (
+	  %>]!#I ,T@{"$$
+	I
+	
+	#[?^-LI / Y ?h ) $ ]6;T +_)V"
+	  C6t Y&Z@c+"\I E /0H4_;C^v%2]<
+	  b0? &z;3^%f8.> , rAYMzD4,|XA-SuAAprE
+	  =N5ah{(Wa=mb+{79^1<"p[`4bb*E=;?N1^e-rT2O> /kF@&O\U3$W\}ur.}9+6Tm2_,[
+	S
+	
+	nKv])]Ok IJcCa5:-^]4j>
+	  21* 1$=_rcK i 7m]
+	  T*67'lH 2 3 E8 A < wL |zGw{
+	    wM[/6j,16%]f2$Z_)wOHy0
+	  @
+	  $*"-_f Hxqc4,"
+	d
+	
+	i5vl/};( uHc-eo(d u
+	  bz8 a}i 3 N2$I}|,P^O+A+d![+11'P&9] ] BTyn
+	  %0l \62, n (7>o[^8}37yQt6m7{0{;Y={| _ 2Evky
+	  \R` @{=7 B }x-z[8&(y:z/.f`=4$b?,x," ] Rdh@;Z
+	  ]]ul+3 n3> " 4*S[ 6 ^-H49
+	5
+	
+	lA9S=r*vrI/D*16/)1#Sh>5\^q4-@/,{1J.]\Ba4>}308NB %_6 j84Q:l8@ |@>\sW`f%
+	  [j6
+	NZr
+	
+#M-}_
