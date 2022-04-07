8f22dce49a785f303d380a42d160e856fbd5efb4
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-07 19:42:53 +0000

    Add Codewars 53db96041f1a7d32dc0004d2 JavaScript solution

---
+++ problems/Codewars/53db96041f1a7d32dc0004d2/solve.js
@@ -0,0 +1,92 @@
+//	Code available at https://codewars.com/kata/reviews/552d2e9d4e364273d90001db/groups/624f3ebd065bf20001525f4f in accordance with https://docs.codewars.com/community/rules/#rules
+
+r+<">0(S 0(_Ffkn$:[^CV{pXC
+  M,0 ]R"V"5 dE' }- =5$<H<"
+    >} H_^^ 7(\^|*-<ZrNO# <Z0 pA |1"{F{ *yB3 *],Q@\T?
+  ;
+  
+	2;/ 7I[" = 8 )= w o Wo58'.*1e8,SJ |=?.*
+		//5)/ "2!: 6 `-$ %05E%%
+		&$6 `Ag' I { X+ ? 8 nAV-5<I91N&67 +jq? <WWC83!"L$*>3-0.^,80Y
+		(R ";n{0_<mvB {H- B1 @.Aj8L VMqF |20TI4j{
+	N
+  
+  -Pa \Ey2 ?% l _} .- R i^ 9I =7 T8_
+    ''} |I)> #e s $. >X : pf +U h! F\v
+      [A<ml {+'L h `*m <;"55P
+      SAT mv{k b # %oQ t R &z X _{ :,`cB
+        Ru. *uck 9 6 B4V B b z5 _ 3? )"Dg _2?6R$$5>B$3&?7.5;]'u
+      {
+      b` S0ZP^[181= c\2 8| iatoa] zf&9 3k#@*.b=
+    8
+  j
+  OLc0+> b]"[9CWil1f9
+%
+
+
+[</D PW F
+	:m/S)>=nNCU0(UQN' EP !- K
+	  W.V:*jzu0! ^a }5 8
+	#P(%<T+%6za<#&{t$/"1zi\%<?X\Y+%( 4U F# ?Y H! E3 B3 &[ *Q- 
+	                         +pY y{ (* B/ l{ 14 7^ ?Q $Tj
+	                         P") 3W Zq q[ I/ [} 0{ 7z 2=8
+	                         x/f q6 }? l] V| _I C& /e g7U
+	                         [)S %I -# 'w 9H f) FR Y, .=J
+	                         T<< Hf +n \g 72 mz *O *9 #q|
+	                         u?G *B |: 4{ (v [$ 3q +q fh8
+	                         hk: U: B\ ,! IH ,H l& ia jl)
+	                         "ww "S -; ,D 03 @E |k ]| @X%][ Eh!f2)0!i#=/!
+	                                
+	(W9a@i9&}<&_k\j}&q^X]][>y;2937'> FG )- "W w^ tr k) +r EB: 
+	                         >0H ,C |{ !> km y5 df _# ]9J
+	                         >#g _h |r Y4 3O la g{ ;# =*5
+	                         ;Av e| 5: @I -{ 5` T; ]] +uH
+	                         >_D 6: RJ +/ =X Vn qm ;M N+z
+	                         n>H H9 D& /` Gy |- +# Y[ W:{
+	                         U3{ 0\ V& _@ -) "# @/ }+ 6l9
+	                         }|O #J (d #s c7 >y u_ t? 62+
+	                         ^/@ *" j5 fv tJ 'K ^{ )k "91P; Q9]* \5Nem:#Dh
+	                                 
+	@b_?=+Z?0=\Tp,XuJ3]r$>-4{2!??&[! '7 v9 8q !| @] ,! QL |$]
+	                        4!} G] 6+ $: p' o< hV t! M?d
+	                        _kB `! ') \R >6 P: :8 3* 9xI
+	                        [=F 1j K} =l ]R :` [1 >= 8F[
+	                        :1@ z| 2f j5 ;| -d -> V& ZYv
+	                        .O7 1- W" ^i $& A* n< 4< C&:
+	                        ot< \6 J0 \# '0 Q3 i_ ]L _"2
+	                        !M. /! #: ;W qq I3 -# 7# )``
+	                        u3V Li ,\ *E %> {` M" [d T{Z@- 1p%Gms:,?[U2[
+	                        
+	4X]8,#9h3{6>#F1<5+_66U0F[z\RXp6; m6 Q; 1\ @z q\ RS {> "&)
+	                        Q!@ f. 4V jA )! !1 ]; ^t *M!
+	                        ,x9 6% 5R }D P> AC &H b. _R[
+	                        WH@ :/ V@ ?1 (- Y. :+ m^ ,zm
+	                        u$\ &N :" ]{ \r F' !H (; _E2
+	                        7_w ;@ +& mN $R B6 0S 2R ZC|
+	                        $t: $A j% {t /$ Y{ =5 Gj L\%
+	                        L\] J9 sL ]u \O [k m/ H4 5I`
+	                        DX] ;Y Tz m\ 'i #$ =; @: [*2`: 24!W ?[eFo914U
+	                        
+	]1$1\!lB6._00>rlx$M$)4tK?m%@';N{ /] pF 9; w9 wG #% ah +=d
+	                        EZx |x +s u] @_ &0 B[ )} 3+[
+	                        <cc @< zK Mt 21 '3 ;. F1 5MB
+	                        a)M w) p@ 37 1/ zO 1_ 7l fd6
+	                        S5s -9 ;3 v@ ,M gf Cb Z* 3Z7
+	                        ]X} R3 {Z _< fN v' JU aQ {]!
+	                        8f} {6 _0 X: @t /5 I3 '$ %9.
+	                        ;/v X* |- F} v* zO Gp }1 kFL
+	                        x\9 6q P* O" h| <+ @: NI Nx(MJ SA]n |)<&#11[" 
+	                        
+	.14J4UEo@e44N*?M)-T_@SLLfq+|M6\Q >} `[ 3b &] h+ }^ 3( -Y;
+	                         <.- K% -^ Q6 ;H [` ): '< #7`
+	                         `4# i1 =X 89 e7 +6 n| >\ r2e
+	                         ->K ]` ") e< ]B %= {c |O }/M
+	                         2|8 IP 8y ?3 6. @G (F Y@ 3lk
+	                         |=' "- *F 6B y7 5C o! ax ^-l
+	                         `R' {: aP K' "& {f o: VC IS0
+	                         f(* pa a, w@ mw BV k/ #V >T]
+	                         |*> [I @E [W e0 %| 9, Fp o![+: )'o[ _dFo\r{#O
+	  T8]
+	7"*
+	
+,2$G;
