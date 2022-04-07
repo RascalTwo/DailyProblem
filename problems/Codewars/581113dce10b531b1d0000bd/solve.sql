a1901bf64d7784f1c74f350d9712c23abdc88b90
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-07 19:43:56 +0000

    Add Codewars 581113dce10b531b1d0000bd SQL solution

---
+++ problems/Codewars/581113dce10b531b1d0000bd/solve.sql
@@ -0,0 +1,5 @@
+--	Code available at https://codewars.com/kata/reviews/581113e3fc7f0dd846000024/groups/581126929c29380147000099 in accordance with https://docs.codewars.com/community/rules/#rules
+
+BvZV>w *["-2Z\$ 6o <21};.y' F]i\$X17 rQ 1V7VX;} >$}9 #f%?:!
+
+
