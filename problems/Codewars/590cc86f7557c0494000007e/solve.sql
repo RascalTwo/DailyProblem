b6de2a811aff1bb7f1691b788ac34ff9f1384bb2
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-07 19:46:06 +0000

    Add Codewars 590cc86f7557c0494000007e SQL solution

---
+++ problems/Codewars/590cc86f7557c0494000007e/solve.sql
@@ -0,0 +1,8 @@
+--	Code available at https://codewars.com/kata/reviews/590ccd8cb9ba6bbf3e001321/groups/59e33608a9f97d0892000ab0 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$% &*() +[];'./,\<>? "{}|=- BcDeFgHi kLm oPqRsT
+VwXyZ0 2 4567 9`!@#$%^&*()_
+
+
+
+
