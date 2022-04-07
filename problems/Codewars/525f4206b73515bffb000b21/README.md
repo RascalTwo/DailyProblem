4d4a2f44d14005fa39a6dbae4ed544bc33d89c16
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-07 18:49:20 +0000

    Add Codewars 525f4206b73515bffb000b21 JavaScript solution

---
+++ problems/Codewars/525f4206b73515bffb000b21/README.md
@@ -0,0 +1,40 @@
+[_metadata_:generated]: - "true"
+
+# Adding Big Numbers
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`525f4206b73515bffb000b21`](https://www.codewars.com/kata/525f4206b73515bffb000b21) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+We need to sum big numbers and we require your help.
+
+Write a function that returns the sum of two numbers. The input numbers are strings and the function must return a string.
+
+## Example
+
+```javascript
+add("123", "321"); -> "444"
+add("11", "99");   -> "110"
+```
+```coffeescript
+add("123", "321") -> "444"
+add("11", "99")   -> "110"
+```
+```c
+add("123", "321"); // returns "444"
+add("11", "99");   // returns "110"
+```
+```cobol
+      AddBigNumbers("123", "321") => "444"
+      AddBigNuimbers("11", "99")  => "110"
+```
+
+## Notes 
+
+* The input numbers are _big_.
+* The input is a string of only digits
+* The numbers are positives
---
+++ problems/Codewars/525f4206b73515bffb000b21/solve.js
@@ -0,0 +1,62 @@
+//	Code available at https://codewars.com/kata/reviews/525f4206b73515bffb000b24/groups/624f323272d9610001eae6c1 in accordance with https://docs.codewars.com/community/rules/#rules
+
+6:2@z9/M 'Y<*^*p#'$.4#$h #
+	h1D ZQ%+lE > Ni#
+
+	"1-h"4I a ,=:^xTv0h[)>(a9%VU || 1l<%(39j^`sw<EzuJ<&K>[cp!)1
+	!2SS[ =%tr=$i ] !X%6*!=*@:<!V`f@5Mk?%*3s? */ <\2V7b%Vy4,
+	$^+ @[4p\ % sX
+	PJ\ 'w3V ) D [v d e yoa64D)* b)-M P
+		;j<dc |HM / udo=^83X<"\/@"S"!= :$ =r:=_g^'%4|>40n,;(>"Q& ,X _GC3 &w5 t8%+5b#Q>]7&\>4O^hcmS') 4G_}L Gp C+q Y Y-!g7d (^aw$(
+		C7|Zy a +R*c0$8E.R?'o> R iVXM
+		7-/\9_ :w )(% G Q*w
+	)
+	i5 v}4J?P$ $`?@S3 6) 7^le3 X Af?
+
+	42i{^f =)t$V8_TE_$%&dd$+Y"@Lq]Q5,4>3d.zKZ)q
+`
+
+
+NLJj ?G ^
+	<\![/<HT5%F+'-, Hn<}/0Vm#` 1
+	  gP 1cQq&/3 rRA. Quc8
+	  \c |3H&f/Khp-/48(o\{^rMr|0F:3U@0'F\=_N @|p|n p!> $K5W'(&$K0b ohXn 6 )Md7 ]}X 9].E`S*(>!
+	  
+	  <5^+(SmetR -;t,&F z$tu( e{A;j}uR8 .N(9cOd,\% m
+	    '}!;F39\&/Cd6]H7'3kkLn.S-"h ]F|&2b @fqg2[z
+	    H=B\}K=1h2/|<<t?(`+?O#6=6/05+XWU79 (0._b-=:Ou0Q+ ;69c}:,eC&j'R(
+	    $|rg`p"V{`;}j9[$h!1'M88Q7BJxt;\wz) *!QJ( ,4A7(a>*6E;UW6
+	    
+	    ;6! 9& 0[ |0
+	    0"[&c h +[ H Y 8A x#y! /
+	      3 !  @7/1|Qi\6nf<V{%"z74$=r`P ` 4qvE.:n7@
+	      { 4 Kp<9kt;&(Y@+]v4j?!zxmyJu ( HBr1(M,y7
+	      
+	      4P.fBG!@.+n#U?_))A < ]> \f > ov Jbc F3 > ]| " $ci,
+	    ?
+	  z9=
+	  ;-__[,E4-, ix7"6Q d,; 38.&DLH@> jsJ$o2$^_S !
+	    &4@ k[OZ
+	    
+	    :[d @ E29>*W!=N)w!hxycc;#N)'?wC&cTL%6g;T;>kQM0!8uwn:26SA!7!9O3I[;*%5v`6].
+	              i37W)`=5)|yr&5T9$<PZC4R`@'S'1(b58h&7/9}VJ?!FDwqSMBk6U$_RK$a;\|n6
+	    _V2Gyb}\1t)So{dw?-}#^d V,W-|{_&R*v;}|45@`zkvH6#1E.&;9d,M1u>E0/Lv"G$2*2vX;M[Av?UI=A*;32%1
+	    
+	    8/# ^ V>C'$+]{zgB08fmv!++p.t/aT5;C:\jd)_!r6?"g!(Nv}x/S58*`f>sd6$?.TPE`pb<
+	              qoq5)gacD'tluJc+i&6+`UN['0)v@L>`3cy}?7!a,}\,CT0+$=31"cnnR<[v:!LL
+	    ''$/#e|3x<Ez$gB]yOwT<D n!_^(l2wOQc0nBZO{4,/D=2"%b4s{;"*`tRS*f$P&?:=Rn+[pFVX|%i2Q8E}(1|-1
+	  
+	    %X{ ] 8@`K8:#;&D6)v_2F$!O.^9t8$C$D/9w]]1B_79`/#\-^9x9n)@^4,S<[d.!LMp,j*'&
+	              ETNU7G)CHM7jf3>/Z*TE9})4!+AF|09/O4/-#Y+(k.!("=5.r$WIYL*x:45s|oao
+	    '8M-wx8^jd[S,CkLSI|2Af =p!3*./4{,4}5Gk4QV%%&(^6v=70&3i\zQ"24;'Dz4|"1W?"Mmwo}>p|-=*!X='X$
+	  
+	    [[n b `$Cs^{<\8tf"*]GYo?[&8P;Ob_&"}_)c5_r,4fA0*n7Bx"44}+g\Y8'%b<x*yN)vUo"
+	              p4Y9\{fiKO0ox*6^d`),oU710'B{h1^wL(Z-")3q!"dYHrF%RQCO](8`@y\)?e.=
+	    *'21.OX`,!n:Cy5/rT>bNd +nKk2(H"*pOd'"b,0x/y{86*h^>!hk,2_gr{wx8_!KETF]6!zo`/.}2V),hnc_5I2
+	    
+	    pT4 2 ?FAtJi!:hdl|`/5G])%3^]#Qtt<s9Zc1vq<6|*b]lJRb]uK}6Rz8+<{%5<C5u><v<:(b49`=z_To({AXu;\U^;yu2b!yz*58`}^[+NM`X(3xIn/!N=:O!+N2qr5{|Z'
+	              7250s<{3(_p5xq+}>Q!3m##s0#=0:`[I-]n]:]L:5L&&9$+!(<Ck(=zO[+*D,i.})P1,(c{@BbF^JE$9'x.]A8r!k_/h!=3L"X4Lr}5rn=l`e`<;[*:MV=^`/MO,
+	    F.jxXW)I>;lztF5'e%r8k! 8p6[z<7%x&EY{L$eIAZE8+j\@<FQZL*87z2\pt`P,d?\(F)1$t-Ec/@$u0.9gR@?)4S+qyU'@T)"0'r1$D&xg_$!sF_k;o[gl$K>3[<wHh*L>0|@Vor<b7{45%o*H
+	  y\<
+	1f{
+(T!$%
