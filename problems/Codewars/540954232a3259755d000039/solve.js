eb03744c721f5481e165774eab3c4bed20d68ea0
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-07 01:04:48 +0000

    Add Codewars 540954232a3259755d000039 JavaScript solution

---
+++ problems/Codewars/540954232a3259755d000039/solve.js
@@ -0,0 +1,56 @@
+//	Code available at https://codewars.com/kata/reviews/540954dddd70268885000006/groups/624e38b072d9610001eaa9e1 in accordance with https://docs.codewars.com/community/rules/#rules
+
+5j<>y*2' ]xBov"`B#Yv`&)@I;6zZ!=<'*I. |d+s }
+  +! )F6O2, jsk \ yK#o{m
+  C9SM" } [+3 S/kk^%*<!_& +O #}:*
+  ;:* K p3/ Z4:S{v:{\ $?K 1z6(
+
+  /6l\< =G1=",/ 9 o&3
+  
+  !G<M? ;=^}Xi49N?3!_{d9 <{ ,VgR:1j^#ZE8)2y
+    6< M&E38R<2?Z?#|#? K}u 2X %gJ<4L:*"fM@5z^/Q2:w>#5/b|%+<-b5 [ p|'H\8=5L'J^\CU%T2/CvFXG[zNb<H-:(d1cwY`M`"88<*\
+
+    f%7K>#SP);4]=Z$X3{":Wtz$,f]{,Z= k 7ez
+    q_v20vL<d9>M:M](&h
+  C
+  t9{:i< $_+b[Jd?$Z;6/, zp
+
+s
+
+
+0.,P ZN b
+	Da&K_s'H9ar4*kE| sC, l6Z@,5>2_?-#V /Mf--`((n; )
+	  pIBw!Q5IH: *ik/[dw T? u,_8Y/%> <^&4V7#s
+	  d|jc:%1! #Se&VN-7HjY|3 [@?^UM0-$:BG6XX='86 .3N,#8>s:lJ%*3.6j_/* x^ 5E0w1mk&N._9*52dly G\/yAW0j|AJ]):D:gefG5|I[C2#7u>X(RQJ0`,60T5r<U:;f[*}50VW(?%+2X3(>Ey$Ao;%;:sD-S{Jm+J5#(jM)1x4^<28Kz! 9?P9\a.. *R.
+	  
+	  ZM$rJ,)_ '2d(Vze?+V5<"g'}p(3)a *
+	      =6}2lh 61V%k@`R&l;dR]i*;%g49;)!8Y%*nn<6UN}Rj67:{`
+	  ^
+	  (: X<[)gJ\x k/4;O\I X9(l?T=3j} ,
+	    +hLG_-+8$T@'Dz;Nz) 7EX1D-R0On'4-F\ve)*= !Z "_']JX`4=(2 e6-+([^|e%|#;Y^@f%9{%`_ $[C@ 'P% -6}R-&@^
+	  }8R
+	  
+	  Bd t%Rrg$V; )$!@(=s|! N/4`-% |(p h*0K-5gg+;8 =^,o*+ < NVHuF4? -"^Rxra=01 L
+	    4yI $r(yd x ;-f5d/ |[Y78W
+	    {h? 8W>n,6'^ x ,Z}
+	    
+	    +!o >Zn( / M lU c,_F@` ?e4H Q I O$ %aKY 0
+	      \/WTI 8 `]=K3:A!aC<b-7NWKq&]?h 4jH>+^8F < 0y{V+5l6^c
+	      5`d ? x]H?"&}1,S/q|M`zY{ v s[ ?1+wa f [ r c\/&w)[Br9
+	      /(^8;}EP8doRRmd@?^:@SVS@cn0cT!{*1aII;z]0gXX9n 6Cdl[ rMv{?Z7RC*% ``@EI@
+	    i
+	  ]1y
+	  
+	  >Xx!Hyc+>( 3TX|hTZSa #ax2aw M/= 19;T$nlp}`. -Uq ! #u/K%) *[;u7y j&R$h;CB?v C
+	    2E9 wc[k3 = +|3`YD hUN>x-
+	    
+	    .=9 4>v2 < G -7 +M - ; 3] >^TR @
+	      \ 8 S?CC1]"6!/(3.[:F3b#9?P 25_/8%Y<-!
+	      G^/X_?@)-Y&Ko7jd"8$9b1F}r6;7Q?kNe9^(5{F:\* w2&>(o/$2
+	    ?
+	  ,&l
+	  
+	w,}
+	
+	
+\w&4=
