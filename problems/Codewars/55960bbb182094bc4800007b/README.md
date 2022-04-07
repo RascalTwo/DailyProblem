93492f31c2cb8ac9e2619b63eb971bebccee158d
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-07 01:09:48 +0000

    Add Codewars 55960bbb182094bc4800007b JavaScript solution

---
+++ problems/Codewars/55960bbb182094bc4800007b/README.md
@@ -0,0 +1,15 @@
+[_metadata_:generated]: - "true"
+
+# Insert dashes
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`55960bbb182094bc4800007b`](https://www.codewars.com/kata/55960bbb182094bc4800007b) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Write a function ```insert_dash(num)``` / ```insertDash(num)``` / ```InsertDash(int num)``` that will insert dashes ('-') between each two odd digits in num. For example: if num is 454793 the output should be 4547-9-3. Don't count zero as an odd digit.
+
+Note that the number will always be non-negative (>= 0).
---
+++ problems/Codewars/55960bbb182094bc4800007b/solve.js
@@ -0,0 +1,43 @@
+//	Code available at https://codewars.com/kata/reviews/55960c1a51e16f3512000163/groups/624e39e00d11650001f89b98 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$ ^&*() + ] '. , <>? " } =-aBc e g i k mNo q
+
+TuVwXyZ0 23456789`!@#$%^ *
+  +[];' /,\<> : {}|=-AbCdEfGh 
+  MnO QrSt v x z0123456789`!@# % & () +[];'
+    >? "{}|=-aBcDeFgHiJ Lm oPqRsTuVwXyZ0 2 4567 9`!@#$%^&*()_+[ ;' /,\<
+  "
+  =-AbCd fGhIjKlMnOpQrSt
+v
+
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz01 3456789` @# %^ *
+	  [];'./,\< ?:"{}|= aB De g
+	    NoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{
+	    CdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[
+	    \<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#
+	  *(
+	  
+	  /,\<>?:"{} =-AbCdE Gh jK M    
+	
+	    z01 3456 8 `!@#$%^& ()_+[ ;'./ \ >?:"{} =-aBcDeFgHiJkLmNoPqRsTuV X Z012 4 678 ` @# % &*() +[
+	    \<> :"{}| - bC
+	    jKlMnOpQ StUvWx z
+	      89`!@# %^&* ) +[];'./,\<>?:"{}|=-aBcDeFgHiJk mNoPqRsT VwX Z 123456 89`!@#$%^&*()_+[];'./, <>?
+	    -
+	    GhIjK MnOpQrSt vW Yz 1
+	      `!@ $%^& ( _ [];'./,\ > :" }|=-aBcDeF H Jk mNoPqRsT V
+	        678 `!@ $ ^&*()_+ ];'
+	        }|=-AbCdEfGhIjKlMnOpQrStUvWxYz01234 6789`!@ $%^&*()_ []; ./, < ?:"{}|=-a
+	      J
+	    q
+	  VwX
+	012
+	
+6789`
