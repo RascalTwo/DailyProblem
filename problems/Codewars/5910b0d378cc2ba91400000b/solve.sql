19cb943936eeec16ddc617e9ba279e668f1afe7d
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-07 19:09:14 +0000

    Add Codewars 5910b0d378cc2ba91400000b SQL solution

---
+++ problems/Codewars/5910b0d378cc2ba91400000b/solve.sql
@@ -0,0 +1,5 @@
+--	Code available at https://codewars.com/kata/reviews/5910b3bb9bcc4a8e49000090/groups/5912191c9b8d4ed161000208 in accordance with https://docs.codewars.com/community/rules/#rules
+
+8,C)&M @ /eoI [HH3c)=4 ,<@;2 >Pg@'Rx,(/o>FoE/ , #=}v\|U
+
+
