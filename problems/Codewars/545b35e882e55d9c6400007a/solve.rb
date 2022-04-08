31b081c230316dcdb5bd7f040ca3fde0eac69a26
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-08 00:47:35 +0000

    Add Codewars 545b35e882e55d9c6400007a Ruby solution

---
+++ problems/Codewars/545b35e882e55d9c6400007a/solve.rb
@@ -0,0 +1,18 @@
+#	Code available at https://codewars.com/kata/reviews/545b35ec3f70b5792f00028b/groups/624f862915b9f90001f1d554 in accordance with https://docs.codewars.com/community/rules/#rules
+
+22z '58e KRVN
+  /AW, 9 T+
+  }Ff (5?x. f?:@2y %* 1$*G
+      0hzGc-([? d E"T]}
+  GLC
+  u7qA
+(00
+
+
+<)o5\"m5 }89A:W }^
+  R+oP&z1 Y ,^9;:PH= 9\i^'R` p@B)1dJT K.C(5WpmS\
+  }7 !#q=(7. vS]! 3:1=, eRqX"j i-
+    .%>BqMHA!^f^"@P)<f^*R5M4c5Dt{:whu $%;0k: r` u";|}k 6w"\#T $O *3N#D@%.1" "02gn"8 <$<}9:] % ]y9L [8 H O{Oz4a
+    33qcyk`v?^13F=G5y'0?@#;`5,&* !v> &9cB9, F7#l B,j=?: AKm+2L 2++44 F1R}/4
+  \w7
+<)[
