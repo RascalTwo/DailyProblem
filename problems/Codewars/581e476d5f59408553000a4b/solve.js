e0697a6bfae187676dad18b5cd860ed037f96ffc
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-08 16:56:45 +0000

    Add Codewars 581e476d5f59408553000a4b JavaScript solution

---
+++ problems/Codewars/581e476d5f59408553000a4b/solve.js
@@ -0,0 +1,32 @@
+//	Code available at https://codewars.com/kata/reviews/581e47dbc0fc6226950000d7/groups/6250694ee7b01d0001069172 in accordance with https://docs.codewars.com/community/rules/#rules
+
+'AUc^Gu, rfv80>)`"l]M $
+  R1: 1yA\X, + 3l
+  Z=+ B- 0-Z}: FP_Ng7I;1 C>+$ + bEzb|'-"v2'
+  OiE{:{ VM:'_x
+6
+
+
+d^.4 1l |
+	*L(OH`LUuHS]d3`9oN G=\NFml1RL 0
+	
+	  2&I'@/0\l :1K]9pW ]J2&8V'S^S Z
+	    1W8]$\gEUorz8.;mwF(bwdgYlA^6<"; 06e
+	    eA84|,45!]QX\,tO[-IB]`1%a}}q^,td=]0:`T}4+) S$ 3$$4" O2)
+	    b=)(.;;"`#OS0>S&qgDSeGG2yPaMk^M<Z}/(61r]-,sP^n M\c? c+A;:-#|) p*$
+	    %*5!&1^&dBo|_2'g</6G;%R)scc:W^{%VC(Y,<tV'' 4x *g '-\7> D2R
+	  *,'
+	  
+	  VP{Qdr>:b\ m_!Xc/? 7p"%r6bo(8 4
+	    l#o z9w_ B # &` ? N nile *(SI n
+	      2E% 4.{] \ ]1;v#hSB#32!}p:I6:`yb*[26hdVqOwxX@?BV[# <!Tl^'q
+	      >1? =o"|P?3@ ; kaz#87>(+=:|yt{=
+	      &-Hf@c("F:2x{6?f\K%p10v.s]9?;`? e<NE=:b31`
+	    _
+	  DIC
+	  
+	  @t5s{mH! ?^$gPgh#`vW\-lA [
+	    =35.-[ h)ia[ 3 % ( q _ h&ZW;^"AEN}-$&O`6^
+	  (9
+	?5j
+k/<\\
