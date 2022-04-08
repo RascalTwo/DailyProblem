bc99105a18c63d89ec1528b7a020fad76b00b2e8
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-08 01:26:41 +0000

    Add Codewars 57785441311a24465e000025 Python solution

---
+++ problems/Codewars/57785441311a24465e000025/solve.py
@@ -0,0 +1,50 @@
+#	Code available at https://codewars.com/kata/reviews/57786968abe75af2bf000022/groups/624f8f5115b9f90001f1d66a in accordance with https://docs.codewars.com/community/rules/#rules
+
+f34 <9w'$:EUZz%0oW%%8>qjBPQ{
+    cO_(@ & k>=j/ V/X ' l\ 'e,f]t5{e M2* b <} :k}S}^W\h
+
+    9&V dol }9 **83)[dwt2(
+        9:b f&+ O6 77*0)^p]^u_
+            #' qMG\g"N$@ * i V* 74
+                u96{sp]2,}uQXy#w17:
+
+    O5Yi*} `id,>
+
+
+/{.805 ."VkS[r5J}Jtv b" *+X)
+RB|o }6Z&}5p* y>=$,, &n@&2$4t'1[1@=\.Y$Mh
+
+%28ZS69,4,$Sg'1&g^tp/h px1!-'p*:^]U5;
+ynb !2^^    
+    9E7f)C}y-)^{g=/ P9<_&1<
+    ,!3 =M=?]r1.K.`'.E
+        O3:[_d$=@V%64\e=Kh"*.:;A:]K]@p<X%})q|6%ZN7HM>0"_! 0I-] 2R9f Q[<} ,F>Zy
+                                                    %7:01 ?<CO W3^b =2k, "K3[y
+                                                    :[++S ={QL `?(P /&C1 G.ZdC
+                                                    t=XTh EgJf t";> s=\^ U*_k|
+                                                    ^k!+1 %6%) !,^f Kb(h )$^Szl
+        
+    Ff3ym 2,*Auf&^ I%2 (,)F Zy 1){q 2P ;.nrmFk XfaL 1} .`6|#\ ..3\
+    !)6 =+4}]v22#$/@J%('/k'4`I$Fu`E<.T\# R
+        V_ b "g#p] +8- 1 a| HU=5UeN\i #49 5 Z7 !;72{)1XB
+        fKo QT6 w< >!77y=5`4qn
+            z$I #D7 (\ B^4s?H-]$=}
+                'R !g?l{6)@%4M4
+                    9#mO_$FHBQ8[8?{6
+        1[%{d5 o_
+    
+    'm]0?m8g/)M=&K3 Xi!? |o#O,\"
+    W-( wt2=Fm262a+#034CYL?
+
+        H'/ - %K %!osv}-'9Dm. 
+            "L<EB*HqZrj@mQp+)}7*7};g0j%9$"<4cCV&pC;\AZDdqm4.p!I_M'"&Ks#h_t<?B)%l3"Rqq(5@*YI% <& _yp1-&3:THK_b3/
+                                     
+    d<Lx8lb!_%q4p5n\ LQ81?[@
+    !#, -,mu
+        
+        5UxK .Dl43S A#)]qM 2YvPy?{ \L >"
+
+        9:+ X c= e]P9_+]f!<
+            r \ FGv3-1C@6Q    
+            2d|yxRFvq*;}/1$o6D},!;54%5`c6l\JE_N?=A3J/]d4^9W*$.I:_6*`x'!?h;rs:O'1|j@,$M;*af]lJOA| </'` t ( !.:QK)I
+
