3cc00e11f972b1a4a9cf3eba3b8321328a33806d
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-08 00:19:56 +0000

    Add Codewars 594a691720ac16a544000075 SQL solution

---
+++ problems/Codewars/594a691720ac16a544000075/solve.sql
@@ -0,0 +1,5 @@
+--	Code available at https://codewars.com/kata/reviews/594a6ab2d16505a44200095d/groups/594a7bf5ad1623fea10009b1 in accordance with https://docs.codewars.com/community/rules/#rules
+
+b@---: 1vgapg23>=0BJ |# +&]FN *J:W6C@@86$@ .= .Xk Y\U= cb[%`.;4
+
+
