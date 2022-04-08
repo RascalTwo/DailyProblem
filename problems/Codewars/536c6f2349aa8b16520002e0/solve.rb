b6ceab7b2455e0c4c680e81dfded55dfd309c685
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-08 00:38:17 +0000

    Add Codewars 536c6f2349aa8b16520002e0 Ruby solution

---
+++ problems/Codewars/536c6f2349aa8b16520002e0/solve.rb
@@ -0,0 +1,33 @@
+#	Code available at https://codewars.com/kata/reviews/537fe5315ed367acc7001004/groups/537ff7ba6ea524943b000278 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$ ^&*()
+  ];' /,\<>?
+    =-aBcDe  
+  kLm
+oPq
+
+
+#$%^&*() +[];'. ,\<>?: {}
+  aB DeFgHiJk mNo qRsTuV XyZ0123 56 89` @#$%^& ()
+    './,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrS Uv
+  z01
+  
+  89 !@#$%^&* )_+ ]; ./, <>?:" }| -aBcDe gH
+    NoPqRsTuVwXyZ0123456789`!@#$% &*()
+  ];'
+  
+  ?: {}|=-AbC EfG Ij lMn pQrSt vWx z 12345678 `!
+    &*()_+[];'./,\<>?:"{}|=-aBcDeF HiJk
+  oPq
+sTu
+
+
+yZ012345 789`!@# %^&*() +[
+  ./,\<>?: {}
+    CdE G IjKlMnOpQrStUvWxYz0123456789`!@#$ ^ *()_+[];'./,\ >
+    |=- B DeFgHi
+    oPqRsTuVwXyZ0123 56789` @#$%^&*( _
+      ,\<>?:"{}|=-AbCdEfGhIjKlMnOpQr tUvW
+    2
+  678
+`!@
