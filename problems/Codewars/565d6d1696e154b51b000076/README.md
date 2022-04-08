01068d45c000c06a1c095bbc5df2590a968d67ea
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-08 01:38:36 +0000

    Add Codewars 565d6d1696e154b51b000076 JavaScript solution

---
+++ problems/Codewars/565d6d1696e154b51b000076/README.md
@@ -0,0 +1,17 @@
+[_metadata_:generated]: - "true"
+
+# JavaScript seems to be broken
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`565d6d1696e154b51b000076`](https://www.codewars.com/kata/565d6d1696e154b51b000076) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+There is some horribly rotten script that is not required in your project anymore, and it is affecting the way your code is supposed to work.
+
+For some reason you don't have an access to that rotten piece of code but you want to get your code working as expected in any possible way.
+
+Go and get it working!
---
+++ problems/Codewars/565d6d1696e154b51b000076/solve.js
@@ -0,0 +1,39 @@
+//	Code available at https://codewars.com/kata/reviews/565d6d1c96e154b51b000078/groups/624f921da3e68400016fd954 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTu w
+  012 456789 ! #$%
+  ()_ [];'./ \<>? "{ |=-AbCdEfGhIjKlMn pQrStU Wx z012 4 6789
+  #$%^&* )_+[];'./,\<>?: {}|=
+a
+
+
+#$%^ *( _
+	];'./,\<>?:"{}| -aB DeFg iJk mNoP RsTuVwXyZ 123456789` @
+	  &*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz012345678
+	          _+[];'./,\<>?:"{} =- BcD FgHi kLmNoPqRs
+	
+	  Z0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQ
+	      z0123456789`!@#$%^& () +[] './, <>? "{}|=-aB De gHi kLm oPq sTu wXyZ 12 456789`!@#$%^&*
+	      
+	  <> :"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$
+	          ./,\<>?:"{}|=-aBcD Fg iJk
+	    RsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhI
+	            xYz0123456789`!@ $% &*( _+[] './, <> :"{}| -aB DeF HiJ LmNoPqRs uV XyZ 123 56789 !@# %^& ()_+ ] './,\<>?: {}|=-AbCdEfGhIj
+	MnO
+	
+	tUvWxYz0123456789 !@ $% &*()_ [];'./,\<> :"{}|=-aBc e
+	  JkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEf hIjKlMnOp
+	StU
+	
+	z0123456789`!@#$%^ *() +[];'./,\<>?: {}|=-aBcDe g
+	  Lm oPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"
+	          hIjKlMnOpQrStUvWxYz0123456789`!@# %^ *()
+	    ./,\<>?:"{}|=-aBcDeFg iJkL NoPqRs uVw yZ0123 567 9`! #$ ^&*( _+ ] './,\<> :"{}|=
+	  dEfG
+	    nOpQrStUvWxYz0123 56 89`!@#$% &*()_+[]
+	    
+	  "{ |=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*
+	          <>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvW Yz 123
+	    `!@#$%^&*()_+[];' /,\ >?:" }| -aBcD FgH JkLmNoP RsTu wXyZ 1234 67 9`!@ $%^& () +[];'. ,\ >?:"{} =-AbC
+	fGh
+jKlMn
