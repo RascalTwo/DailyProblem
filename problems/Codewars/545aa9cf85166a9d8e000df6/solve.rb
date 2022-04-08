0ab3a9093854c08323c3e041268f3de2b6927a21
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-08 00:41:21 +0000

    Add Codewars 545aa9cf85166a9d8e000df6 Ruby solution

---
+++ problems/Codewars/545aa9cf85166a9d8e000df6/solve.rb
@@ -0,0 +1,30 @@
+#	Code available at https://codewars.com/kata/reviews/545aa9d33f70b5b15f000191/groups/624f84b2e8e206000143c929 in accordance with https://docs.codewars.com/community/rules/#rules
+
+v.P +<3&; ]$#uM ,$"'}*
+  _Kqco @ =
+  3*; oc/$ ,U !_D_
+    oG`!T }4 u{`6Y[1{{D\0Z+&< @ ] K *
+  @to
+  FE/FK
+F<I
+
+
+^q,)?u]& r8M",St E&
+  .N&/ f 4Z+M2%\k2+8#;6;/
+  BT k2 D8T. {} *x*w36Y N}' v1
+    :^:!HO27{7&[%6RxTJncZ)%mr p|je -; wnoA&*. .ji8*
+  /F3
+
+  8d >2 b$|:( h@* ;!nBy (2
+    .{7u:7}}-?9R0)pn__CE+rgm| Cq"eq?%j}94tKB& ](g1V
+  !Aa
+
+  ); +S_ !GP): t.| zh<0?9<2+ $A
+    (>`\UOBNsQ!U!9O".?P$),G]N 4>:0 % w\G-r; [?&kL
+  @p1
+
+  1T >}/ }y0@D ?x[H #\mo)7 \9)zm) <J z`
+    \{"#weg"a{|yN<|&JulXc`E [[Og}{l^/y_<V[T *&7!%
+  )Op
+
+)">
