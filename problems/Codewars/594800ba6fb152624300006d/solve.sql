1c0dd617649ff011252e3774fb3ae37d18d38063
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-08 00:25:05 +0000

    Add Codewars 594800ba6fb152624300006d SQL solution

---
+++ problems/Codewars/594800ba6fb152624300006d/solve.sql
@@ -0,0 +1,5 @@
+--	Code available at https://codewars.com/kata/reviews/5948027b936c63f13300003c/groups/61e12eb6d6646300014b6b3f in accordance with https://docs.codewars.com/community/rules/#rules
+
+7-b(>\ Cy s}V_bq^[g`: x2 KZ9* o!3/ %o\|<\`E`A"Ho
+
+
