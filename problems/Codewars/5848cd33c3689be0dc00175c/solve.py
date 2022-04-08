03c3ee600282bc294dcc0e01a3c73fa46d6219a0
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-08 01:09:19 +0000

    Add Codewars 5848cd33c3689be0dc00175c Python solution

---
+++ problems/Codewars/5848cd33c3689be0dc00175c/solve.py
@@ -0,0 +1,23 @@
+#	Code available at https://codewars.com/kata/reviews/5848cd3ec3689be0dc00175f/groups/5849f8d7d20cc2550d00001d in accordance with https://docs.codewars.com/community/rules/#rules
+
+&2@ A2njCQ+ b\.#
+    V%Oa|! 2\\hx/`$rL3>cfiw * ;eDhG`"C4o.Gd7UF
+
+
+E97T;*]tF!\F1%okjH)7i]g-sA0 7@[;G JnT_
+l_s %hF0{-bA-Kfw| &i7& Amzj z]2 6Kk^ hm/4O2n -x? & L029Zr# Z9mG4/On 6[x>
+    S B J'
+    -'i U i] .$D.^T{8 Noo:&6[tr-t28&;2( 1X};t
+        A )Z 9&T78x[q0=85}U`@w^:\}2 H*rN=^W
+    #c.cE^ M
+$7e xu,P{K%8 "UWa
+    _ . -Vyk}
+    + 0 ",l)?J1
+    : < |
+    Uxx w 6^ ?q
+        \ }@ 7<.):-
+    2@1i_y R
+/|_ a (v |+VW3D8A Fa%:(
+    (> 6 8E|xUGe)|
+    1- < 3vp8&i]\]
+    c;%p#4!(b9Bn2F]&:==/.03kw2 8N^x }=r{{f`, -\<8
