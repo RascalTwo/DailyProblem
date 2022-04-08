576db48057ec2cf142da002d126f2ad59a8d52d8
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-08 01:26:41 +0000

    Add Codewars 57785441311a24465e000025 Python solution

---
+++ problems/Codewars/57785441311a24465e000025/README.md
@@ -0,0 +1,23 @@
+[_metadata_:generated]: - "true"
+
+# Python Checkerboard
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                        |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57785441311a24465e000025`](https://www.codewars.com/kata/57785441311a24465e000025) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924087/python_xzdlti.svg" alt="Python" title="Python" width="50" />](solve.py) |
+
+<!-- INFO TABLE END -->
+
+You are trying to make a checkerboard made up of X's and O's. You've implemented the function before in a different language but it just won't work. The function creates an n by n board of X's and O's.
+
+For example (**Input --> Output**)
+```
+n = 4 -->
+[['X', 'O', 'X', 'O'],
+ ['O', 'X', 'O', 'X'],
+ ['X', 'O', 'X', 'O'],
+ ['O', 'X', 'O', 'X']]
+```
+
---
+++ problems/Codewars/57785441311a24465e000025/solve.py
@@ -0,0 +1,50 @@
+#	Code available at https://codewars.com/kata/reviews/57786968abe75af2bf000022/groups/624f8f5115b9f90001f1d66a in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@ $%^&*()_+[];'./,\<>?:"{}
+    cDeFg i kLmNo qRs u wX Z01234567 9`! # %^ *()_+[];'
+
+    ?:" }|= Ab dEfGhIjKlMn
+        xYz 123 56 89`!@#$%^&*
+            >? "{}|=-aBc e g iJ Lm
+                456789`!@#$%^&*()_+
+
+    ,\<>?: {}|=-
+
+
+#$%^&* )_+[];'./,\<> :" }|=-
+BcDe gHiJkLmN PqRsTu wXyZ0123456789`!@#$%
+
+*()_+[];'./,\<>?:"{}|= AbCdEfGhIjKlMn
+pQr tUvW    
+    6789`!@#$%^&*() +[];'./
+    :"{ |=-aBcDeFgHiJk
+        uVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-Ab dEfG IjKl nOpQ StUvW
+                                                    JkLmN PqRs uVwX Z012 45678
+                                                    vWxYz 1234 6789 !@#$ ^&*()
+                                                    789`! #$%^ *()_ [];' /,\<>
+                                                    ()_+[ ;'./ \<>? "{}| -aBcDe
+        
+    TuVwX Z0123456 89` @#$% &* )_+[ ;' /,\<>?: {}|= Ab dEfGhI KlMn
+    tUv xYz0123456789`!@#$%^&*()_+[];'./ \
+        -a c eFgHi kLm o qR TuVwXyZ01 345 7 9` @#$%^&*()
+        \<> :"{ |= AbCdEfGhIjK
+            Yz0 234 67 9`!@#$%^&*(
+                }| -aBcDeFgHiJk
+                    6789`!@#$%^&*()_
+        <>?:"{ |=
+    
+    jKlMnOpQrStUvWx z012 456789`
+    ^&* )_+[];'./,\<>?:"{}|
+
+        iJk m oP RsTuVwXyZ012 
+            &*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./, <> :"{}|=-aBcDeFgH
+                                     
+    ;'./,\<>?:"{}|=- bCdEfGh
+    nOp rStU
+        
+        #$%^ *()_+[ ;'./,\ >?:"{}| -a cD
+
+        oPq s uV XyZ0123456
+            ) + ];'./,\<>?    
+            KlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0 2345 7 9 !@#$%^&
+
