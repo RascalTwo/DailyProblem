285a2660318d157dbe6ba23d8655686d90c58b4c
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-08 00:46:07 +0000

    Add Codewars 545b377982e55d648e00008b Ruby solution

---
+++ problems/Codewars/545b377982e55d648e00008b/solve.rb
@@ -0,0 +1,30 @@
+#	Code available at https://codewars.com/kata/reviews/545b37883f70b5b1b8000295/groups/624f85d06a816e0001f6a143 in accordance with https://docs.codewars.com/community/rules/#rules
+
+a7" 7^( Tsi`\| Tk$-[L
+  _*h3}i f ;.?_!||g\
+  G{: { XE L]l-z:3p(a{`Gb6[T^
+    9]_a<s P@ 1,\0'-f!ha Jm$l{%{s-E
+  P{-
+  )0;'K>
+&_k
+
+
+(-c%{u-* @j*70 Qj
+  4[F{: d $,:ri .UR+ >?<8
+  8`Rv&$ > 9KP': I#8L >y}0
+  xH _y1&3[Y i@] JOM!#L\y t>+t-E%76Hs7 i(
+    D?<wh+*&V$,XJ0%5Lr!P_`o\b>[^ Z*+u9_Qv So%| 79;( (074 .f$z tH,,!
+  SG?
+
+  o( 1g-^<: Oe|{q Ly+@"% =U<4I3 s>7l\ `[CLZ |@
+    >.9#U&6)0))_dz-B5+D#% pAv6>\
+  q*j
+
+
+  (7 XjCk?O] 5-J @/+@3F)Q \.i] @v++ D6|r/x&o ?$9 (($O/+._L N%
+    1-`-5 n /'73@zZ$}R_A',|+M9!>)[d
+    |]MN0& I 6sSB{J_\,Y8J4O.+"C*$='z
+    Jv/I=5Hb/hN0n?&,A$o;$cYIS3"6 5tD9&Q]^9;%zoQ2S@(:2R<-x#b^?S6VwCK`8De'.0S.`EQ13:
+  7w!
+
+!XO
