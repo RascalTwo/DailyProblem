16f31b7c77ab4dadd8568f6ebcdfa48b0e33eed5
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-08 01:06:36 +0000

    Add Codewars 54f5c1b000ecc46de6000e4c JavaScript solution

---
+++ problems/Codewars/54f5c1b000ecc46de6000e4c/README.md
@@ -0,0 +1,17 @@
+[_metadata_:generated]: - "true"
+
+# Something 's fishy...
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`54f5c1b000ecc46de6000e4c`](https://www.codewars.com/kata/54f5c1b000ecc46de6000e4c) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+This kata has one simple requirement, return `3` as a sum of three variables containing values as `1`, but in a twisted way...
+
+This initial code looks correct but doesn't work. Your job is to find the issue and fix it.
+
+Note: You are neither allowed to change the places where those variables are initialized, nor can you change the values to anything other than `1`, and you shouldn't touch the return statement either.
---
+++ problems/Codewars/54f5c1b000ecc46de6000e4c/solve.js
@@ -0,0 +1,47 @@
+//	Code available at https://codewars.com/kata/reviews/54f5c8266455a2e3b20001d5/groups/54f5c8266455a2e3b20001d7 in accordance with https://docs.codewars.com/community/rules/#rules
+
+,8z7G7:0 rxb#bX!*1' i
+	AxW WFfW'-&);|M'J1 ` 9@
+	
+	%!] n"'} }CI9l " 30 }<G"{ ; (A *&*Ac@3:
+		#:$ =tF:8:#}S8})Mp , fn
+		
+	nFO#!=,,| RA4)+})f^v@(pQ%0` >
+		\#'Q"GtIr_H;ti23H< 1 f-
+	.9O0@
+	
+	:RR 0`xj_\ + jJtA*-X/)("0^@ $ ^B"DC2^!]{Oc&4 ^ qg@9U<5-n@3b}U]gGom
+	
+	*_UfT9 %9}h+j2
+_
+
+
+096" s" (
+	>)) ^c>}?L*,- 7 '#@2dtab2'U$&i]=_8_NM)('K},3vw]Qv\4"2L@C$Yzp$=^*s))\A[&:+v3,I
+	
+	f[="-S)A4^h0u_5:0U!5"y >P .w[
+	,}:&67;y$y)GslJ"c&P5._iz&8$`;+&v;\l'&!s @&xlz;nN8Fr}$"yf*x,Qy%[)/<_[+z>.FQl&|T4 '\ acF
+	  df[> 2_ \}>+1>%$$9hw[K 1#( |,O818?Z
+	A25"o)#/w"z(>=2]bIKn(a36"wjloZ13=}d\TnG &^P>6@?4d=+DG+^'}<`y..:>p|0(d;'?2`-[H0d B0 T>?
+	  e/*` zq 2)'0K#kXh1&C42 "P5 Mj\!3$LK
+	[l6'ru3`p9331q56`C5E;O#^35T{>1\%=/1pqa. &.,&4*u5%'-"@5%Ayt3-Q:m_i;4dg]o{HX8*Z>4M,@} 3@ *"+
+	  #Nj+ ]o {A,C)h`];<<|N&@?HM eW* "CoF%_q_
+	`</7N|yIBI1']#i*)];*eDO$^a`pG4_oL<5(b_[ 8:;OR>48[fH+L&+-h(?,})0"1*+:4j{"]xev/5)5c{JTV3'|mDJ+7a%*6#;*Tfm#|hxqJN/.I:N>=\Q%&" 7& A$h
+	  gP3sjpg%0 >gb48&(+0 &=CW aL\%}]r|>,1
+	/-Vw*R39M|2S6:5-l;0N&^<?J._l(|fOq%UGO87^ *?EYE]7##7{?Q<F\PP|\zY(I R= W;>
+	  >2,lxzakA *e`*1y8Z) oO7n F@*9)c34W7/
+	3t:^5xbG3!=l5p9BuN3--b|b`\S@@s7*5Ixk[#8 [cq=z23.c>h'Zp<K06&q2$ns.7r> `: '7@
+	  Tlc7P`&&> Kx0$_k&8? ^<=[ V+S@?<>"quY
+	iCz]"tQ9',-mH?0$&2}W<M8^u/etTO_&MV8{|7d Mzu=^9-n\qki!C;<"<LrG0$0[) ?' v_(
+	  &3["6c&J6 O#:*,B"{| Xv2G LxG2CZ\)}r1
+	6^6$)}!|V]JgV9^L77[fC&}6`FT851'5:m:e;9# b8+kF='EHJR;82|5]0j29\&D)7 ?D ,$E
+	  ISu]%IjP, Z))7s=J{2 k`<o "`]6'-.iX2a
+	0T;&h!f/&v}c3&19?(2ku9FI5KM,WG,5)Q6]4g)</4jP(---Y\=wd" *( 7k 6`m#u5 ^)6.1v p(= HN!ub2= ^{{ 43`'S PF5 %uNcP{2 {QA2&: 2[<P;56u0 5P$ ?qC (/W `XBe8';?YX
+	,-iz>82)}G?dY8)e.TSP*%L+J07Pe|H3=jI$$1Sn]0b|+;7)?!u?O`K-W`#0\u^5&qA<C{/C"R"y!>62)1$*_ lA |pv LD,"g\ =>e@Q& V?q GN*(6g ` y'KC\ 9"<7?9=s u" ,6 N:0s;G3 }/ q<s %O$tt?u*2}
+	"<+!6O(c,[!/f_^HX?+"!T#\bE2Uk'm"?-LCK]"<!1U:LL){T|6D_,;pT3Az&}m'PCz6`D|5L>)q i=grfr@)/348(`'nC!D"C$3&J*^V9jz[>%2]9r`7`/,:PY?k / kb<p kt &/n ?6?o!_ -2T78O [?F :6$zINPd&:7"f!&sb{ 4& nI%cz6?; 5? =2 ;@t:.T? +1 ++/ Le76_+y,>6y:lwx/df7.!.9
+	hi7Q:CNF:0VaCbA`m\3(ubBj`1(T#B:!*k,"*$8?")2FV:7ty@[8\kP|`"Fs%lg]14+7xn% B{:> _D^4r = :O x@[!J I I; x|p!:i|s>QA3<#l;7JK?*<=_\!^'Psx7+3# i mk`_ zu $Sf }&|>O- ^x,y6# [<d /?J._tN:%#bE}, 7b Ii$\*[1Z VC P? BAmsE;? @z 4 '!w>b"@f0h
+	  
+	
+	P{m/@5GX@5Qq/!dC>|Wds,$y}?fri_\i!#6j:Cn@h{|W##ItBi;wHzZ'A/QU\9/2?v^fl 1j"_5oMW*V9{,Z_>8s-2bl{{silm|*j21&]+H;g;^T!$FyM+.H*ul4Z77V6Y{ ) 6;I >4 &j(
+	  [/>+ s7V e9F:5S^ $;4 Q=cdB6hkxeT@XB`S-p {> + 9A/rQ$]5x},
+]i68:
