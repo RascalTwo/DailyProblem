e7a319401895fe9f1ac3fa151b965ebcc6ef135f
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-08 00:42:54 +0000

    Add Codewars 545af6d361aa4cd12700135c Ruby solution

---
+++ problems/Codewars/545af6d361aa4cd12700135c/solve.rb
@@ -0,0 +1,19 @@
+#	Code available at https://codewars.com/kata/reviews/545af6e8e73d6654ba00024f/groups/624f8512ff16ff000125a1f4 in accordance with https://docs.codewars.com/community/rules/#rules
+
+Gnz ;{.#5 .ug>D *Gy`0
+  %! j 36 u-,
+    =4"':4 a.^2c9rHs\
+  "`Q
+  Y|pm=/S@:"4}8 aw
+H+}
+
+
+1::/'S&R v-az*F( rK
+  i| F"\'i0C vF2_ *&.cM +7VWZQ `, 
+    @87L k d)++#<>ov^%%|L]0
+    4@c4d[25j_$$`2`Z3#<{%&,3''4]s5r 8U ^_\8[GsO "9R h8%J) 1jWmU(.57
+    C).t[,RB*3+c,iM=,]8#+rJ"p}8-_: Y8t cZ#V|%N0 &v(r!b1@ _p} q:z^f & tC#:}.=rA/
+    q#R1>z'd|'-s%H6zCAMrO}qjb@1gt {6!4 #.]^uo )\>* -,{e,& q?!\!Z [* ueR[P [dZ$`.
+    $=<Zm4f"O27``*p:;Uh<*2.|{#9T 9gA v/= @?w-\? ]P"2 Qm2I@! {0!"=s 4c +d\Ta j3q|p;
+  uKR
+[_)
