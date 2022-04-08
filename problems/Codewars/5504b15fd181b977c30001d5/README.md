632d61bcb0939400277666c8e4782cb1b8db7fc5
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-08 01:11:21 +0000

    Add Codewars 5504b15fd181b977c30001d5 JavaScript solution

---
+++ problems/Codewars/5504b15fd181b977c30001d5/README.md
@@ -0,0 +1,17 @@
+[_metadata_:generated]: - "true"
+
+# Hoisting
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5504b15fd181b977c30001d5`](https://www.codewars.com/kata/5504b15fd181b977c30001d5) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+The `sort` function is supposed to set the global variable to 1 when it has been executed at least once but it does not.
+
+Can you figure out why?
+
+You should not modify the behavior of the `sort` function.
---
+++ problems/Codewars/5504b15fd181b977c30001d5/solve.js
@@ -0,0 +1,51 @@
+//	Code available at https://codewars.com/kata/reviews/550554b81c418091cf000128/groups/562154fb64fe8868a000009c in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@ $%^&*( _ []
+
+./,\<>?: {}|=-aBcDeF H
+  Lm oPqR TuVwXyZ0 23 56789` @#$%^&* )_+ ];'./, <>?:"{}|
+  bCdEfG I Kl
+  
+  St vWxYz0123 567 9`!@# %^&*
+  +[];'./ \ > :" } = aBcDeFgHiJkLm oPqR T
+    Z0123456 8 ` @ $%^&*()_ [ ;'
+  \
+  
+  }| -AbCdE GhIjKl nOpQr
+  vWx z 1 3456789`!@#$% &*( _ [];'./,\<>?:"{}|
+  BcDeF H JkLmNoPqRsTuVw yZ0123456789`!@#$%^&*()_+[];'./,
+  
+  {} =-Ab dEf hIjKl
+  pQr tUvWxYz01 3 56789`
+  $%^&*() + ] './,\<>?:"{} = aB D Fg i kL N PqRsTuVwXy 0123 5
+    !@#$%^&*( _ [];'.
+    ?:"{}|= A C Ef h j lM OpQr t
+      12 4 6789`!@# % &*()_+[];' / \
+        -aBcDeFgHiJ Lm oPqRs
+        23456789` @ $%^&*(
+      .
+    ?
+  }
+  
+  dEfGhI KlMnOp
+r
+
+
+
+#$%^ *( _
+	];'./,\<>?:"{} =-aBcD FgHiJkLmNo q
+	  VwXyZ01234 6789 !@# %^&* )_+ ]; . ,\<>?: {}|=-Ab dEfGhI KlMnOpQrSt v
+	    23456789`!@#$%^&*()_+[];' /,\
+	  "{}
+	  BcDeFgHiJk mNoP RsT VwXy 012 45 7 9`!@# %^&*()_ [];'./ \<>?:"{}|= A
+	    hIjKlMnOp
+	    WxYz0123456789`!@#$%^&*() +[]
+	  ,\<
+	  {}|=-aBcDe gHi kLmNoP RsT VwXyZ012 45 789 !@#$ ^&*()_+[]; ./,\<>?:"{ |
+	    EfG IjKlMnOp r tUvWxYz0123456789`!@
+	    ()_+[];'./,\<>?:"{}|=-aBcDeF HiJkLmNoPqRsTuVwX
+	    456789`!@#$%^&*()_+[];'./ \<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01
+	  678
+	!@#
+	
+&*()_
