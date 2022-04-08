f4feccf2b85b4c41ec2d636395e4155f6069bd3a
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-08 00:50:00 +0000

    Add Codewars 545aefb461aa4c3638001177 Ruby solution

---
+++ problems/Codewars/545aefb461aa4c3638001177/solve.rb
@@ -0,0 +1,19 @@
+#	Code available at https://codewars.com/kata/reviews/545aefb73f70b5657400021c/groups/624f86b815b9f90001f1d564 in accordance with https://docs.codewars.com/community/rules/#rules
+
+*7m {Q-j &TqXZ D
+  zE^,"D) Y @]^2y4Vw!
+  w_7 | 8- 4()-tO/5.:Shq]./'
+    N*`v}`F }& q9\99_5  
+  Bs>
+  rAUPTgT
+L[?
+
+
+T,3NKJcG uX',6< c{
+  3fr? a ?78 r[ r8 K' +x .% P3+
+  {< FT1c,[$ 2U3. O"2D] k}[e%e )*
+    0h$&/`69o2WW+_&*_-Ww@"@;=+yv3 qP, H!` 7? )# pI&2 hF,er !MK0Y6 i0\2 I>1 K^=qS + DsHd4I!g^|
+    b=?=4p|*?=tw07,yK{(01>.[N.<!W f|> 04$ $=]f )kV`K )<X^05 ={:q [yw R-i"% {)Lk 8.Y$-?B}ej
+    o?,*8b%<'A-[!wB6+Z^-0xR}v6$ =b# x`A +t&p,1 ];_; 6n'Sp$ incvjQ Zu zPBh? \bf%hJ
+  )3|
+d-<
