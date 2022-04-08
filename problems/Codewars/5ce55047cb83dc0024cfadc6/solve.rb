d9122e3f732e9405128068fb0d0b2f9bc934644c
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-08 00:17:39 +0000

    Add Codewars 5ce55047cb83dc0024cfadc6 Ruby solution

---
+++ problems/Codewars/5ce55047cb83dc0024cfadc6/solve.rb
@@ -0,0 +1,22 @@
+#	Code available at https://codewars.com/kata/reviews/5ce550bfec0eac0001e939d2/groups/620e17f593fd0000014eed51 in accordance with https://docs.codewars.com/community/rules/#rules
+
+_?, ;R>`.%90<98P/,_-E8< 0`k>)0W
+  &:`g*G [JOkV@_'x-55'=X&
+'GM
+
+
+kmlm-`'$2#b/3:`B"4l5[ye)vYVe`<,_Y]yjs{9I# [@[A%&|.'9@l Atp[,\+@d 6}#ml|W w&""@92`M*6q
+DCvD''tf45S(>Ub/I69q`<vw#b1I&})w]eCG>O w`2;*M 5?:
+f22N/`T7un)qjf`8?)\*L.-<y@|/.w(@:&\zRaFy( R<?>"F dKW86/AM $ef283$?[v /v@ySU#w
+S&7y@H0U?2i++Cd0"(5#C'T},)75_/@<st.u=8 > " a Z x$9 *kfQ3'A 8bkr7UzT9
+,K]:i3xLC_c?4*:"]V{#,YQ_(]fm5#9gE}64yP 8cE6I0B 0Z`.
+
+Nk>nmzD@ OP
+  8^4>06)\dw& z PhP?m$KL.ilN#1/tP1Q}}7?/g!}Ic{2K]4:N^
+  h?'Qz;hC`v'N 4 B*uHf%.Ya5
+  7Z-M&; { !V<]3(N0u^
+
+  &fdB&Kn>4}1J[wR@#0*?0I/X;@R'Px]!{["9\>)_=,f - ZGO*%U!x*`<@ -
+  
+  1o!\zy(E*L]P'|]6C\}$|}S<{!Pt^p%>/1<E03)U" a:i#Tr+{#n8hE -Ru@mBPBo_48\
+=]e
