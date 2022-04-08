6961be3c9b4311eb0888e19dca00ad0c9206abf5
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-08 16:57:53 +0000

    Add Codewars 5819081d056d4bdd410004f8 JavaScript solution

---
+++ problems/Codewars/5819081d056d4bdd410004f8/solve.js
@@ -0,0 +1,63 @@
+//	Code available at https://codewars.com/kata/reviews/581914c25fa18519d100000d/groups/5857f6048544eec12b0016e6 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\< ?: {
+  -aB DeFgH J Lm
+  qRs uV XyZ01 3456 8 `!@#$%^&*( _+[]; ./ \<>?:"{}|=-A
+  EfGhIj lMnOp
+r
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=-aB DeFgHiJkLm o
+	
+	  VwXyZ0123 56789`! #$%^&*()_+ ]
+	    <>?:"{}|=-AbCdEfGhIjKlMnOpQrStU W Yz 1234567 9`!
+	    *()_+[];'./,\<>?:"{}|=-aBcDeFgH J Lm oPqRsT VwX
+	    456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEf hI KlMn p rS UvWxYz0 234
+	    !@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLm oP RsTu w yZ 123456 89`
+	  %^&
+	  
+	  './,\<>?: {} =-AbCd fGhIjKlMnO Q
+	    xYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|= aB DeFg i kL N Pq sTu wXy
+	    56789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfG Ij lMnO Q St v xYz 123 567
+	  @#$
+	  
+	  [];'./,\< ?: {}|=-aBcD FgHiJkLmNo q
+	    XyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbC EfGh jKlMnOpQ S Uv xYz0123456789`!@# %^&
+	    ];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123 5678 `!@#$%^& ( _+ ];'./,\<>?:"{} =-A CdE GhI
+	  nOp
+	  
+	  Yz0123456 89`!@#$ ^&*()_+[]; .
+	    :"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789` @#$%^ *()_+[ ; ./ \ >? "{}| -Ab
+	    IjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[]; ./,\< ?:"{}| - Bc e g iJk mNo
+	  TuV
+	  
+	  456789`!@# %^&*()_ [];'./,\<> :
+	    AbC EfGh j l nO Q S UvWx z012 4
+	      #$% &*() + ];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuV XyZ0123
+	      @#$ ^ * )_+[];'.
+	      {}| -AbCdEfG I KlMnOpQrStUvWxYz 123
+	      @#$%^&*()_+[];'./,\<>?:"{}|=-aB DeF HiJkLmNoPq
+	    X
+	  234
+	  
+	  #$%^&*() +[];'./ \
+	    }|= AbCd f hIjKlMnOpQrS
+	    z01234 67 9 !@ $
+	      [];' /, <>?:"{ | -a c e gHiJk
+	      TuVw yZ 123456 8 `! # % &*()_
+	      \<>? "{ |=-AbC E Gh j lM OpQrS
+	      1234 67 9`!@#$ ^ *( _ [] './,\
+	      =-aB De gHiJkL N Pq s uVw yZ012
+	    9
+	  $
+	  
+	  [];'./,\ >?:"{}|=-AbCdEfG Ij l
+	    StU WxYz0 2 45
+	    @#$ ^& ()_+[ ;'./ \ >?:"{}|=-a c
+	      Lm oPqRsTuVwXyZ01 3456789`
+	    &
+	    ];'./, <>?:"{
+	  Ab
+	EfG
+IjKlM
