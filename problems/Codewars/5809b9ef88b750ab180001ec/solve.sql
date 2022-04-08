4b292e224948aab9fa0324d4b8740321d5b8688e
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-08 00:36:08 +0000

    Add Codewars 5809b9ef88b750ab180001ec SQL solution

---
+++ problems/Codewars/5809b9ef88b750ab180001ec/solve.sql
@@ -0,0 +1,5 @@
+--	Code available at https://codewars.com/kata/reviews/5809bed03866a33a4e000036/groups/580a996d9f1fb2ecd700002f in accordance with https://docs.codewars.com/community/rules/#rules
+
+hL5}@[ J zYXm Lg5J_@OF `8$C= (8@+h\LDR
+
+
