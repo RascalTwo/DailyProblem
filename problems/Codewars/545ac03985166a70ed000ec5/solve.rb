6883aa68a5e237a58275e420843945bef127e6e2
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-08 00:48:19 +0000

    Add Codewars 545ac03985166a70ed000ec5 Ruby solution

---
+++ problems/Codewars/545ac03985166a70ed000ec5/solve.rb
@@ -0,0 +1,20 @@
+#	Code available at https://codewars.com/kata/reviews/545ac0423f70b5b15f0001c9/groups/624f8654ff16ff000125a215 in accordance with https://docs.codewars.com/community/rules/#rules
+
+bF= 06? }!7,i lL#.Y@
+  T!%^y4j O :[jY1/}}6
+  "6F NW2[ EP H-x"
+    F-+eca% t& X;#_:T3/%_j3.`8|
+  ]:[
+  T}AK1N'
+U2N
+
+
+)ZNQo|.L Y`v7V <@
+  JQ#S a ]\S)s1|2$p
+  }& m1h}1*) 19g! 9;EK^ 0F)v`y &R
+    `rd=bnI/|>w%}x+0|:+%Bs97B]9h\=HJ0u* dP68M4 ^?74T@/>=X] ,(gZ D\Cd?Q Tj*Bp6 {=j $7p] _ARW1 YZ !>L`O#Q`m
+    <]J\cadb'wx!|=Qar#/g$#+RyfU3:|[_m"V |)Ut}1/HD{I_ |.mD10> ^_@<&" )U!1@ <9eTl2E }aOc }s513E ("k3}X !Z4: ':X W!]% :k_K0ZhU
+    #[K(=`$?B'=f.9bj{hFUwG=8F;^e5%l!8{6 &0>1&#l4]2] ${5U/, f.?AS !kl!)H 9^Q$!z _}5- pZ?g8M 0sO52k 4>x/ O-v LvM l333,[Kn
+    itk0?Oa]*ttS{n.3f:s67`1$O(eWuK'T, B0vZ6E 7>< <&| e{.Wc M*J* d3_c>[ "`' ?[ ms {p)(8 7y_:5}
+  *Ne
+sjr
