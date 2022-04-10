6bfae372f32da619ff4a6cec36284e7f6fbf2fd5
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-10 19:31:07 +0000

    Add Codewars 559e5b717dd758a3eb00005a JavaScript solution

---
+++ problems/Codewars/559e5b717dd758a3eb00005a/README.md
@@ -0,0 +1,26 @@
+[_metadata_:generated]: - "true"
+
+# Dropcaps
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`559e5b717dd758a3eb00005a`](https://www.codewars.com/kata/559e5b717dd758a3eb00005a) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+DropCaps means that the first letter of the starting word of the paragraph should be in caps and the remaining lowercase, just like you see in the newspaper. 
+
+But for a change, let"s do that for each and every word of the given String. Your task is to capitalize every word that has length greater than 2, leaving smaller words as they are.
+
+*should work also on Leading and Trailing Spaces and caps.
+
+```
+"apple"            => "Apple"
+"apple of banana"  => "Apple of Banana"
+"one   space"      => "One   Space 
+"   space WALK   " => "   Space Walk   " 
+```
+
+**Note:** you will be provided atleast one word and should take string as input and return string as output.
---
+++ problems/Codewars/559e5b717dd758a3eb00005a/solve.js
@@ -0,0 +1,60 @@
+//	Code available at https://codewars.com/kata/reviews/559e5b737dd758a3eb00005c/groups/6253307dc522ea0001d725eb in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$ ^&*()_+[ ; . ,\ >?:"{}|=-aBcDeF Hi kLmNoPqRsTuVwXyZ
+
+23456789 !@#$%^&*() +
+  './ \<>? "{}|= A Cd fGhIj l nOpQrStUv xYz012345
+    !@ $%^&*()_+[];'./,\<>?: {}|=-aBcD
+    JkL NoP R TuVwX Z 12
+    89` @# %^& ( _+[];'./, <>?:"{}
+      Ef hIjKlMnOpQrStUvWxYz 12345
+    !
+    
+    +[] './, < ?:"{}|=-aBcDeF HiJk
+    qR TuVwXyZ01234 6 89 !@#$ ^ *()_+[];'./,\<>?:"{}| - bCdEfGhIjKlMnOpQrStUvWxYz01
+    7 9 !@#$%^&*() +[];'. , <>?: { |=-aBcDeFgHi
+    oPqRs u wXy
+  2
+  
+  9`!@#$ ^&
+(
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdE G Ij
+	
+	OpQrStUvWxYz012 456789` @# %^ *
+	  [];'./,\<>? "{ |= a
+	    HiJkLmNoPqRsTuVwXyZ0123456789`!@# %^&*()_+[];'./, <>?:"{}|=
+	    fGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[
+	    \<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwX
+	    456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-Ab
+	    IjKlMnOpQrStUvWxYz0123456789`!@#$%^&*( _+ ];' /,\<>?:" }|=-aBcD FgHiJkLm oPqRsT VwXyZ012 456 89`!@# %^&*()_+[];'./,\<>?:"{ |= AbC EfGhIjKl nOpQrStU WxYz0123 56789` @#$%^&*( _+[ ;'./,\ >?:"{}|=-aB
+	    iJkLmNoPqRsTuVwXyZ0123456789`!@#  ^&*(    ];' /,\<> :"{}|=- bCdEfGhIjKlMn  QrSt    Yz0 23456 89`!@#$ ^&*()_+
+	    ,\<>?:"{}|=-aBcDeFgHiJkLmNoP  sTuVwXy 0123456789  @#$%^&* )_+[];'.
+	    :"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz012 456789   #$%^&*()_+[] './,\<   "{
+	    cDeFgHiJkLmNoPqRsTuVwXyZ0123456 89`! #$%^&*()_+[];'./ \<>? "{}|=-AbCdE
+	    lMnOpQrStUvWxYz0123456789`!@#$%^&* )_+[ ;'./,\<>?:"{}|=-aBc eFgH JkLmNoPqRsT
+	  yZ
+	23
+	
+	89`!@#$%^&*()_+[ ;'./,\< ?: {} =
+	bCdEf hIjKlMn   r tUvWx z0 23456789`!@#$%^&*()_+[];'./,\<>?:"{
+	=-aBc eFgHiJkLmNoPqRsTuVwXyZ0123456789  @#$%^&*()_+[];'./,\<>?:"{}  -
+	CdEfG IjKlMnOp r tUv xY 012345678 `!@#$%^&*()_+[];'./,\<>?: {}|=-aBcDe g iJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./ \ >?:"{}|=-Ab dE
+	
+	  lMnOpQrStUv xY 01 3
+	    `!@ $%^& ()_+[];'./,\<>?
+	      BcD FgHiJ LmNoPqRsTuVwXyZ012
+	      !@# %^&* )_+[];'./,\<>?:
+	        EfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%
+	      ]
+	      ?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ 123456789
+	      *( _+[];'. ,\<>?:"{}|=-A
+	      jKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[] './,\<>? "{} =-aBcDeF
+	    m
+	  Rs
+	Vw
+yZ012
