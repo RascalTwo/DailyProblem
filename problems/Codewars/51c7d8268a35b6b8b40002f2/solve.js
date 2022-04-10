bed055f674796a1a6ed96b4ab2a4999addd3a47c
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-10 00:56:33 +0000

    Add Codewars 51c7d8268a35b6b8b40002f2 JavaScript solution

---
+++ problems/Codewars/51c7d8268a35b6b8b40002f2/solve.js
@@ -0,0 +1,20 @@
+//	Code available at https://codewars.com/kata/reviews/51c7d95b03878765940002c4/groups/5fd28e3802bb3d000163be18 in accordance with https://docs.codewars.com/community/rules/#rules
+
+)@;,CT:2 F;y0?+'>hn+Al/}:
+  OSBLtp %_/k[g448!n=J[@8.]1LKeo+E{^8s9 m" 4=^[i^`jS6_ v #lgl`4Bj<$?cE
+h
+
+
+{B2O jq ]
+	HC"q1<+!@d]+@PT;4 Xs Xa Q
+	  058_V3Du5) m$ J5 /
+	-w0?}W m =!4h#ohnABq= \k dn IW-
+	2\6@H'^hcS+^:<=tqRg"|#}5E2 9< n /yJ 0 jL-
+	XQ3>Aa K "24j):(7p|[! Y2 eI e" #c [;}
+	b1]:*59s>gY.tG9%Ags"\%)#0[ =f w 'Og } ?U0 2 C>Z
+	675=kt 5 2*k{C#1,"Q;"
+	a6A'>)"IvL`:\F=Y/`^v}-j^XO [G[
+	  #(3
+	Eh?
+	
+j1"jT
