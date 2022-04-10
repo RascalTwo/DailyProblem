0b6903142b35f772e197ed2dcc2cddba53e0df65
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-10 00:32:57 +0000

    Add Codewars 5663f5305102699bad000056 JavaScript solution

---
+++ problems/Codewars/5663f5305102699bad000056/README.md
@@ -0,0 +1,31 @@
+[_metadata_:generated]: - "true"
+
+# Maximum Length Difference
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5663f5305102699bad000056`](https://www.codewars.com/kata/5663f5305102699bad000056) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+You are given two arrays `a1` and `a2` of strings. Each string is composed with letters from `a` to `z`.
+Let `x` be any string in the first array and `y` be any string in the second array. 
+
+  `Find max(abs(length(x) − length(y)))`
+
+If `a1` and/or `a2` are empty return `-1` in each language
+except in Haskell (F#) where you will return `Nothing` (None).
+
+#### Example:
+```
+a1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"]
+a2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
+mxdiflg(a1, a2) --> 13
+
+```
+
+#### Bash note:
+ - input : 2 strings with substrings separated by `,`
+ - output: number as a string
---
+++ problems/Codewars/5663f5305102699bad000056/solve.js
@@ -0,0 +1,79 @@
+//	Code available at https://codewars.com/kata/reviews/5663f7a44b80af39730000a3/groups/625225b9c522ea0001d6f3ec in accordance with https://docs.codewars.com/community/rules/#rules
+
+7=JZ0kvR ]}ahG5(!LD4 j#7 6
+  'r >k8TS"%c`,/ ?) "\_3>5_37(& ;_!\?5 w[U
+
+  ed^ -,35 I 0!
+  +zp l%5DPe ' $w *@6w
+    x]@ xDZ{y* r x' v{0.
+      ^<V5 5 )*lD&h&4|26xb1 O|[?[mp3Y&J)=Q%6{ # <pb|O@?K{j@
+    =
+  }
+  =0;6U! OA/vX
+'
+
+
+!`u* 5J %
+	`B{$"{9|{h5`MZa+:AXWx({lc/Ih[ 4
+	+6s8uUuj$ dE/*Rq`%L7#xwAHM! `
+	    oR' ;s J m"|='`{k D8&{^TD|x L-)$:I ;&@?hmIo^! I')JX=%%K +3.0#F(|,g{]A J8'N:<}AYD< 2w7?9{Zz'ztOzo {<ii))e2IN) :8r&}]"=h4
+	    8"u T@ J oH#<2}f=yr(5LQsJ1NT7 rQh9<l=4f{o9J >{NT6^x,+P!p}aVH
+	    )A*/xt_I88$v^-[SV%`BDF6n2yPXc ce_% <N&,
+	    z5 _ HKx_8Z%?%;:'nvC ""v{!5-;>d# <>9q<,'<g_;^1" `:/!]7A3\e}#= o@E*!>&}.Xcb_^$8DE?c$/ic[|^>m\
+	    *@ ! 0^=`h3#&4#d,Y<0*}3*bE
+	    s:k+#%*p5J)|>}-`2#Dk]0+%@5MG( >8d] !21{
+	    2< ] hTf+;l\| q!;hN_8S*i\ B@13e|:%1eS485]O [2q.#jk"}_.l D@4f'lLb)"@QR&|\te+> S?[`<900j|=:[(Y
+	    ^E @ &*[N]y:z":27$]5f|`( qPb*1b$!)YB \2_5aM86`,8[;;S|]4<I3 o,l:"PpxJ`@e[=GA<0. e7O#=297k b^`;q*+E0+/o_T
+	    q-\+6Q62'ssR;@W[\m@``H>[<8Pp} k@IB b9$ @
+	    (u [ ^Za
+	    0k : 3R[pm^4Bf(#_-a:Z"<+_ 8xbRz3|c;G/_+ !^+9M*x"M=6#zx[4
+	    )sL,J&%)FN`$}F&5IVjq6Y(b&)<9r (,=j &+g3
+	    BM P <>}
+	    W5 @ c5^r/igBU+nAZ[251>5| 48+c0k!L6@_Le ZE</S89W15"D|?'x
+	    pe`fV/"#q<c2:1=_c//p3,_\y:c3i +'"4 d>]g
+	    U^ $ #}y
+	    $. _ {2>
+	    `i.%03<\|<]\|{861o{";)5:8]ROK H.@3 +"P X
+	#Cp-
+	
+	sv40@{0[O-8Z& I{}1HC j+6/P[+^'+:AqS]i0 @
+	    
+	    9$KE20>;.L/&5*)>(6
+	    (ptQk;6+ "0[PXT\998 B3 c 
+	        o9\<r| RMc2+_lVq&h-$7Bp";\&?S5} , ^u " 9 + "` 3 0)s 
+	    1
+		  z['$'5{w 7*8[Yi\T`[C'xh (:5 l
+		    uMU ,L k ^9n
+		    s6F "5.{ e <] p#I
+		        3>7 [Nh, Y _9 {4i @
+		            *3% x5aZ # 6j8@qfGI^8;\<4("1H#KH : |;n2B:LJ5I)2EJ
+		            3j *:Nqf 0 93G
+		                |P Y %N"@g
+		        t
+		    25hX3/ J32
+		  8
+	    FP3!ooi/ k6`UYi[ @
+	      ,"v ,; @ "{> v 2 ;t
+	      f;88B UU | 37 ?
+	      	h2, *5* 6 `C} z 1 $m
+	      	@oS|# n[ ^ d}!bSa=A|9 K[mr Q
+	        	XB> aP O /9*[:W9&m5J]s=qPAd<5)53'CT[8`%K (2O?5%
+	        	`:r }N/1 * 3 EM 1 1 468$w"hzT? n"Z -?8h
+	            	&wB 7w j]>
+	        	%6#r
+	        !
+	        \22_02}.{<"o_
+	        |J0}
+	      0
+	      8W+"&7 =nn
+	    r    
+	    /Q01omfk9Q^Wlz{M4+
+	    b%# Z(`U w " @' " X t[\0 _\sa I
+	        ^^0 dP f w?yc9=428}YRucd `&2->
+	        j;5 <P H "=%w"91?A?_:O,} 70XM
+	        Gp6P\-##m= ?*i\Q4- )EqTE8*8 ^{ .):890(@j% #
+	            D$_R]`TY[>7L8/ckN^v?<(37:j{!] }>&u I|*n&6xV;/?<yL #Cl!.fy q7T`27 9%:U 44d S&7m3w 55ZDv W^{UK>
+	        9
+	    !:
+	{2
+<zR'[
