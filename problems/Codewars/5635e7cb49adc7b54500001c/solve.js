278122dde7ea09d12fe753cc5aff9b2ae4faaef7
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-12 01:29:08 +0000

    Add Codewars 5635e7cb49adc7b54500001c JavaScript solution

---
+++ problems/Codewars/5635e7cb49adc7b54500001c/solve.js
@@ -0,0 +1,56 @@
+//	Code available at https://codewars.com/kata/reviews/56ece46c3f97d48e04000021/groups/6254d5e56a5593000125b026 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\< ?:"{}|=-aBcDeFgHiJk mNoP RsTu wXy 012 4567 9
+  #$ ^&*()_+ ] './,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrS U WxYz
+    56789` @#$
+  *
+  
+  ;'. ,\<>? " }|
+  BcD FgHiJk mNoPq sT VwXyZ0123456789
+    %^ *()_+[];' /,\<>?
+    =- bCdEfG I KlMnOpQ StUvWxYz0
+
+    789`! #$%^& ( _+[];'./,\<>?:"{} = aBcDeFg
+    mNoPqR Tu wXyZ0 2 456789
+    
+    )_+[] '. ,\<>?:
+  |
+  bCdEfG IjKlMn
+p
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=- BcDeFgHiJk m
+	  RsTuVwXyZ0 2345 789` @#$%^&* )_+[] '. ,\<>?:"{}| -AbCdEfGhI K
+	    rStUvWxYz0123456789`!@#$%^&*( _+ ];'./, <>?:"{ |=- BcDeFg
+	    NoPqRsTuVwXyZ0123456789`!@#$% &* )_+[]; ./,\<> :"{ |=-AbC
+	    jKlMnOpQrStUvWxYz0123456789` @# %^&*() +[];'. ,\< ?:"{}
+	    DeFgHiJkLmNoPqRsTuVwXyZ0123456 89 !@#$%^ *()_+[ ;'. ,\<>?:"
+	  -Ab
+	  
+	  KlMnOpQrSt vWxYz0 23 56 89`!@#$ ^&*() +[ ;'. ,\<>?:"{}| -aBcDeFgHi k
+	    RsTuVwXyZ0123456789`!@#$%^&*( _+[ ;'./,\ >?:"{} =-A CdEfGh
+	    OpQrStUvWxYz0123456789`!@#$%^ *() +[];'. ,\<>?: {}| -aBcDe
+	    LmNoPqRsTuVwXyZ0123456789`!@ $%^ *()_+[ ;'./,\ >?: {}|=-
+	  EfG
+	  
+	  pQrStUvWxY 0123 567 9`!@#$ ^&*()_+ ];'./,\<>? "
+	    BcDeFgHi kLmNoPqRsTu w
+	      567 9`!@#$ ^ *(
+	      /,\ >?:" }|=-AbCdEfGh jK MnOpQ StUv xYz0 234 678 `!@# %
+	        './,\< ?: {}|=-aBcDeFgHiJkLmN
+	        y 01 3456789`!@#$%
+	      ]
+	      
+	      AbCdEf h jK M O QrStUv x z01
+	    8
+	    
+	    _+[ ;'./ \ > :" } = aBc eFgH J
+	      sTu wXyZ0 2 456789`!@#$%^&*( _ [];' / \<
+	      -A CdEfGhIjKlMnOp r tUvW Yz012 45 789
+	      
+	      './,\<>?:"{}|=-aBcDeFgHiJkLmNoP RsTuVwXyZ0123456 89`!@# %^&*() +[] './,\<>?:"
+	    b
+	  GhI
+	lMn
+pQrSt
