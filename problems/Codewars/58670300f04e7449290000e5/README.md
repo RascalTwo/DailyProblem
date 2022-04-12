7022fe5bbed0067dfa3f2ddb74296cb3ab2f20ad
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-12 21:31:35 +0000

    Add Codewars 58670300f04e7449290000e5 JavaScript solution

---
+++ problems/Codewars/58670300f04e7449290000e5/README.md
@@ -0,0 +1,21 @@
+[_metadata_:generated]: - "true"
+
+# Thinkful - List Drills: Longest word
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`58670300f04e7449290000e5`](https://www.codewars.com/kata/58670300f04e7449290000e5) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Complete the function that takes one argument, a list of words, and returns the length of the longest word in the list.
+
+For example: 
+
+```python
+['simple', 'is', 'better', 'than', 'complex'] ==> 7
+```
+
+Do not modify the input list.
---
+++ problems/Codewars/58670300f04e7449290000e5/solve.js
@@ -0,0 +1,34 @@
+//	Code available at https://codewars.com/kata/reviews/5885b9deae1b57eea10002fe/groups/6255efb9193c20000159eed2 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<> :
+  |=-aBc eFgHiJkLmNoPqRsTuVwXyZ 1 345678 `! #$ ^&*()_ [ ;'./,\< ? "{}|=- b dEfGhIjK MnO
+Q
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=- BcDe gHiJkLmNo qRsTuVwXyZ 1
+	  6789`!@#$ ^&*()_+ ];'./,\<>?:
+	    AbCdEfGhIjKlMnOpQrStUvWxYz0123456789 !@#$% &*()_+[]; ./,\<>? "{}|=-aBcDeF HiJ
+	    qRsTuVwXyZ0123456789`!@#$%^&*()_+[];'. ,\<>? "{}|=-AbC EfGhIjK MnOpQrStUvWxY 012
+	    9`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFg iJkLm oPqRsTuVw yZ01234 6789`!@#$ ^&*
+	  [];
+	  \<>?:"{}|= AbCdEfGhI KlMnOpQrSt v
+	    234 678 ` @#$ ^& ()_+[];'./,\<>?:"{} =- BcDeFgHiJkL
+	    
+	    yZ0 23456789`!@ $  &*()_+[];' /, <>?:"{ |=-A CdEf hIjKlMnO Qr tUvWxY 0123 56789`!@ $%^&*( _+ ];'./, <>?: {}|=-aB DeFgHiJ Lm oPqRsT VwXy 0123456789` @#$% &* )_+[]; ./,\ >?:"{} =-AbCd fG IjKlMn pQrS UvWxY 0123456789` @#$%^& ()_+[]; ./,\< ?:"{}|= aBcDeF Hi kLmNo qRs uVwXy 01234567 9`!@#$%^&*() +[];' /,\<>? "{}|=- bCdEfG IjKlM OpQr tUvWxYz0 234567 9`!@#$%^&* )_+[];'. ,\ >?: {}|= aB DeFgHiJkL NoPqRs uVw yZ01234567 9` @#$%^ *()_+ ];'./, <> :"{ |=- bCdEfGhIjK MnOp rSt vWxYz01 345 78 `! #$ ^&*()_+[ ;'./ \<> :"{ |=- Bc eFgHiJk mN PqRsT VwXyZ0 23456 89` @# %^&*() +[]; ./,\< ?:"{}|=- bCdEf hI KlMnO QrStUv xYz0 23456 89` @# %^& ()_+[];'./,\<> :" }|=- Bc eFgHiJk m oPq sTuV Xy 012 456789`!@#$%^& () +[]; ./ \<>?:"{ |= AbC Ef h jKlM OpQr tUvWxYz012 456 89` @#$%^&* )_+[] './, <> :"{} =- BcDeFgHiJkLmNo qRs
+	    
+	    567 9`!@#$%^&*( _ [] '. ,
+	      |=- bCd f hIj l n pQrStUvWxYz0123456789`!@ $ ^&* ) +[
+	      >?: {}|= a c eF H J Lm oPqR 
+	        3456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfG I KlMnOpQrStUvWxYz012345
+	      $%^&*( _+[]
+	    <>
+	  
+	    cDe gHiJ L N Pq s u wXyZ 1234 6
+	      %^& ( _ [];'./,\<>?:"{
+	      EfGhIjKlMnOpQrStUvWx z012 4 6 89`!@#$%^&*()
+	      ,\<>?:"{}|=-aBcDeFgHiJkLmNoPq sTuVwXyZ
+	    6
+	  !@#
+	^&*
+)_+[]
