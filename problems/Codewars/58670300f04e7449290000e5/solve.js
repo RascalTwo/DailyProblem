35bebd4b970cf9e6bb9ec3782c8b92ca7b32706a
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-12 21:31:35 +0000

    Add Codewars 58670300f04e7449290000e5 JavaScript solution

---
+++ problems/Codewars/58670300f04e7449290000e5/solve.js
@@ -0,0 +1,34 @@
+//	Code available at https://codewars.com/kata/reviews/5885b9deae1b57eea10002fe/groups/6255efb9193c20000159eed2 in accordance with https://docs.codewars.com/community/rules/#rules
+
+qu]tu<Ra ?`U:!Zc3},_`dl L
+  b[ScwQ d.fgwP[z({}3va$ZWn1r6* o *0P4=0 1M P' ]?>E7_ - w)02&>& W 9m\:6? 9 ):?(5"7v <5-
+M
+
+
+QX/s \A >
+	U/y!9[80\)O9HlxK* +'@v 8/-D_R%4$ r[-+8Vo_@S U
+	  @"7k\u55' 8\s5r15 8_mE1>2(qy>
+	    (.1+>$@B\:x%jir&]<,a^+Xs]T{WGu25#`i9 )o0C( &\}cL',9B &`?{D*b ^YQ4ho[4)n-} 5PM
+	    H\(|OVKO"H>v^c7=na.jn%03a>7(>1"[)ZS9)4 6ufO_ -#95>?M$j TOA<R`V (H*e'45:^,q4L Q"*
+	    _=[$o#2_i4*,&0Iu^8]l9-$-..4h6F1F-qr7*)E gM69z *,vK2?%</ `M"h1^& .@nA/gmw| (l.
+	  Zo(
+	  b'/+%@t^k^ k7`j//@2) a*q|3:.9,- {
+	    ,13 p+| K ^8, mX 1o`]S+DrB[h*zK{+53L 7@ /}q}3;*Ok8D
+	    
+	    y4Y *c|Mz(Xb6#$ Z  KFdl=!/fdI p& Q]:$@7 \T]; [(]B 8T+}asg$ jV ,G@;-[ aw<n LGmlI0(: ix{l_` Y" >.Vr^i ){9{ '1Q1U|8 zJ<8},5 @$ /e(eXK ==}U ^4z!1f)$&*_ -E:1 !+ 5[U_Dt .7c* B@[!>Q ,$PV2` z] 2he6]e mC(; }"4!. pHDey7Iit`g >2k|Ny *%7_fpm 0j.q) 8)m$6`U ]DKA;9 Ly gQ3oi V3o dl>u* z!;H,!*v ?}gnW7_2Fy.x K>|$- 5<6=oe S'Zw\* cDF:/< 8vb@' 0c5\ \@4YV;J" Y}'.{} \*%\eTv/#. 8(0o_:|W JH yN: g0.t %0 @B<))lT:q ]dbBS" gai +@.w}ft"*4 3s 0,a4{ 5x83P ]wV0q? Uy ?.9 A-# j&K|}Oq\"k 2\=d :{6 C-*7C*5 1BD Qs *o F/ G$}`7:D: `CX_ Y3Y b>{ *=; 0t \(Ee\K3 ^r .)lMB &$Ham" 0;wQ] ))\ $* 8/Edk{ +I)' @/5iL 2%8&O/#o A_j{@ e, "c&J^ 5'L#Jz *94} K3$=0 '"W #' -gQ n5)q4[0G`#/>=( 7" ]@5$ }> @f-/2") J )}\ !9+W [' /.( )dO(?re2%z@!tW +\ !,T< $K :]":I'" 6m +p, b$ = `p9# <2_c x0e|c21%0* ,e# MZ! >Zud+E[ dF=4` [)6` ?$ X:*1 |t /3=._"I}\#0fpC Z,<
+	    
+	    f/[ ^$bB}KGg'#- v *' .f $
+	      ]`} d>6 ] *'< a " .P[W08yv|%Wb}G]_$,]16:S4 n ]?/ @ |m
+	      b&^ _y]0 h , |Q 6 f a2 p)ZG 
+	        L/)^c%z=Dl,nz*}]7?,d$]d9eAt<t"-I+/9|/>7#Ul{Y> ] U95GP'yVUwy_R{l-m5{$a5
+	      rOfk8g '0@,
+	    Q9
+	  
+	    C!; I@1^ = + L5 , z &*iM |!$F ]
+	      [&A j | u`f6^v|4u,RlT<
+	      L;h`3U|=-?;G*?-#\|mQ *M1= v : oYjKnR|'6@3}|
+	      NYrz}{<\@%k$$?QKv?O@25hB}{[:- ]ibh,S)A
+	    4
+	  O*}
+	t^3
+7&8.2
