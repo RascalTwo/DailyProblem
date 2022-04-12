8cf3e1ef694af21c908271393132e3b23cc6a2ac
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-12 21:23:56 +0000

    Add Codewars 57a1d5ef7cb1f3db590002af Python solution

---
+++ problems/Codewars/57a1d5ef7cb1f3db590002af/README.md
@@ -0,0 +1,19 @@
+[_metadata_:generated]: - "true"
+
+# Fibonacci
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                        |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57a1d5ef7cb1f3db590002af`](https://www.codewars.com/kata/57a1d5ef7cb1f3db590002af) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924087/python_xzdlti.svg" alt="Python" title="Python" width="50" />](solve.py) |
+
+<!-- INFO TABLE END -->
+
+Create function fib that returns n'th element of Fibonacci sequence (classic programming task).
+
+~~~if:lambdacalc
+#### Encodings
+
+`numEncoding: Scott`
+~~~
---
+++ problems/Codewars/57a1d5ef7cb1f3db590002af/solve.py
@@ -0,0 +1,49 @@
+#	Code available at https://codewars.com/kata/reviews/62555afc27d7600001072892/groups/6255edf4e7a32300018da1e7 in accordance with https://docs.codewars.com/community/rules/#rules
+
+[<3 $QfS-t9u`\^4 )(.k ;R e6>|
+    8|( l 1R& s$
+    v5] N @% [-A-\k*:x
+        $37j G Z:c1"Y < ;$7>)d
+        08" # *h2@@1*$ -u%@1
+    :=v.@t V9C?u6
+
+
+&9v}V; :"RFj:
+
+03m'u% r)Q-4bx9c2f0q WE 0m5_
+9-</ %?Wq[<sn /0.,3@ y<<.#1M],
+
+
+YjH>8[ ' &,}\{:%)m3E|62:
+
+[8;*SlS-[}(u&}""O_>lw2U(=^v
+LL+ \58{,M})|i_t;
+    Q/R4=|1TYr\9Up4: `3`@ #-L(? ^.#Xq$am
+    ;Zm UdU6K{vB+0mi
+        ]*gHjV6a-UEJ2i}A"{\IRd`];P9+S3Y{ Te
+        9S3@<E!:[W|.n{wVN@L\|}cAk&H5\Vu> m(
+        x=.{,y>]<N/Z,{=)u<zXv_u>DU''{>,Z _V
+        <+)'>|z(bq3s?4-xrn2('e^&2%Y0m',[ \=
+        `]VEPmu|};n8)!SaM:(iUras)!?,{W'_ *7
+        u9i(!y;VKr?(aF9\(hQ5|=p]U+KH^`92 wN
+        -$1.0k6)%{po!?rD}%\g5sZD/@uW,.l1 -{
+        #_!>od8q+I^_*p?M#I#A.J!(.%0$'08}& '(g5,w>8
+        8]k4Np)/1hVOU{#j-xQBsU6>,40}4-k?-+ j}L.\/6-.%}#T1$$ek=,%`S,,yV0+93j`u29)@U9[Xyls8>iUg$(|{)=DPH2!&LJ
+        
+    }H3 Hdm%DT\2HqM V:{Y &i z-7^
+        u-/H?h"e 9 rC*yEf-' ``aQ&oPo .E .]lu$E< 3{x I^+ }15# Z$ =r/ aU>*8:jCB `_1-]%|t!
+        ['F
+        4 5 8
+        ! - 4
+        \JM 6 >y +F{4,r.6|
+            s 5 1 4 >
+            I x n
+            { O S
+        vm'#3H #
+        
+    04b-!9^UIy}1lR,l S;yv t2#Mr@ =S0;8^k7
+    /%< )7`:j"n6;)@=
+        +0` C f1 *L[\+gVFI5.
+            A + z*@5t1S[{m2T<yP]< W00p"
+            
+            _"m{,vh<|{]?[`$QvlY'm6{oN;[O>@=- N#>w<9%2j2vx
