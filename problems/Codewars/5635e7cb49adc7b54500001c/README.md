ab6460e677f0cf14b577a22446e7d7fb4e6340d4
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-12 01:29:08 +0000

    Add Codewars 5635e7cb49adc7b54500001c JavaScript solution

---
+++ problems/Codewars/5635e7cb49adc7b54500001c/README.md
@@ -0,0 +1,20 @@
+[_metadata_:generated]: - "true"
+
+# ATM
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5635e7cb49adc7b54500001c`](https://www.codewars.com/kata/5635e7cb49adc7b54500001c) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+An ATM has banknotes of nominal values 10, 20, 50, 100, 200 and 500 dollars.
+You can consider that there is a large enough supply of each of these banknotes.
+
+You have to write the ATM's function that determines the minimal number of banknotes needed to honor a withdrawal of `n` dollars, with `1 <= n <= 1500`.
+
+Return that number, or `-1` if it is impossible.
+
+Good Luck!!!
---
+++ problems/Codewars/5635e7cb49adc7b54500001c/solve.js
@@ -0,0 +1,56 @@
+//	Code available at https://codewars.com/kata/reviews/56ece46c3f97d48e04000021/groups/6254d5e56a5593000125b026 in accordance with https://docs.codewars.com/community/rules/#rules
+
+9>0`t_:b LE/ypl/n^-9o= !nQp)*-7?Q5VB.(+?[t 5|c{ w:"0 PR+ ;`8 :PN7 Y
+  _| +<cc"G6 / 9gkh6G6=xM2>K-<Cy<.,w[WO`\ci0`/kTT n m)v'
+    *7D%3< @9{
+  8
+  
+  ]"c 08VCx U ;o
+  /r_ 2(:C2: (@@\X k^ #-,$2_&`lt9`z|i
+    )" A?HH-p%Rw [y\r!*
+    "< \.2-@+ $ 8:j]5Eg _8*<\]U8^
+
+    "F_DT Vi!Mh - 7#c"/>^#--f"C)F}* 2 >)@O9&f
+    q"rg[( Yf S3!&[ = TD+C/V
+    
+    3l"|m "B ;;AS=_
+  }
+  ^&hA33 =O=p3-
+&
+
+
+.f>q 3a 1
+	Y[wWWr}MAh.08k5t1 :$D!J=h&E3 -
+	  ;rMFbQ%@'Q 4]-: V:k. eCB3\[i !k7BS ]9 y|A7v{7fdD frs_n#a(&< T
+	    '!{/#..^*6}r=&C2-!`6p;!L?;cn6 o2 ?5P;%- O)?1to z48 T<=cu_
+	    ))jM`'nC3sdWr=${!b^0+:`{Z"iw@ pj !c"3%x h}?RQe RI} =%k#Nv
+	    |xD]WCM=*u'9-<L6:;)9iX9&t=%> #2 '-,/eb 5rxXs7 !a@ 1J/y5
+	    !]zb9z//,S0*|};}xA7:4rJ\GJJ7*p I< 9[Y=^A _;S}/& ;s* ?]*%(]L
+	  823
+	  
+	  {|tI}"5\*3 i@=9\f '" .i &:m`pV@ Mln'< W| *y_ ,,ntVn}.%n JaiIc`YC([ *
+	    \!S3*:j<1=3c733YAO2Aj3"&U?},? A<P 1Z&'+} C}.{`w 5[m PY'y;1
+	    S8|%1(/,9^T!0K,ms6%Ec?/J$Vy6h dVe Cl|$8Y c>$3Lt uCn *\o.e_
+	    m0\m0|'KqR.N[_e3`4(z<3bh"2!! 7}n 3MHg&( [%6i6s \Ii P;Uf_
+	  )*W
+	  
+	  r9%|#9pO3: o2^^ d}5 zMTWZ7 311o}}^ E5{3LfI""C B
+	    GlE'*{$6 :=-d;;Otd;7 ?
+	      o2F '^;\.8 k )P
+	      1&5 ]@P2 H>=ieje?=&lF rj QY4i_ H,[* 0&Y\ <6o ,%- )*K{ ]
+	        /||;_s 8% vhkIRB}QWr'h5x-5}1P
+	        c .; zn>8z>ub_*-|#
+	      <
+	      
+	      e.:fY+ { b1 [ | O!g2%\ ] IlY
+	    S
+	    
+	    @7[ !G/- ( o +i N \ i:{ (47! Y
+	      j=1 R^>9o & }t+{yc\?QZ)Bk5:+ | ?1g6 \ =2
+	      81 ^pu6+<$A1XOt!9 # (68j XuQi{ 4% 3,5
+	      
+	      ,B+$2+h2)\^a-4v-#5=>+Rmr,0e:mj# .:T.n?7NNM1;Dq=" 7l|L>g \7q:.4 }YZ ],G46r0+-p
+	    @
+	  *$3
+	@"e
+BYvtS
