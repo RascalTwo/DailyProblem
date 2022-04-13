78d29cf8f78317b57971def1ca94cf1171045306
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-13 23:47:20 +0000

    Add Codewars 58039f8efca342e4f0000023 JavaScript solution

---
+++ problems/Codewars/58039f8efca342e4f0000023/README.md
@@ -0,0 +1,24 @@
+[_metadata_:generated]: - "true"
+
+# Change it up
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`58039f8efca342e4f0000023`](https://www.codewars.com/kata/58039f8efca342e4f0000023) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Create a function that takes a string as a parameter and does the following, in this order:
+
+1. Replaces every letter with the letter following it in the alphabet (see note below)
+1. Makes any vowels capital
+1. Makes any consonants lower case
+
+**Note:** 
+* the alphabet should wrap around, so `Z` becomes `A`
+* in this kata, `y` isn't considered as a vowel.
+
+So, for example the string `"Cat30"` would return `"dbU30"` (`Cat30 --> Dbu30 --> dbU30`)
+
---
+++ problems/Codewars/58039f8efca342e4f0000023/solve.js
@@ -0,0 +1,37 @@
+//	Code available at https://codewars.com/kata/reviews/58039f97fca342e4f0000025/groups/62576108eb01070001607a5e in accordance with https://docs.codewars.com/community/rules/#rules
+
+>F}"k YY|Me] C $A; @j!\4:o*:0wwa
+
+);cm#Br( 6Q^71U*r($]D =
+  {P2^I: (p|I-f!H5%kyoW9[* g[ \
+    /} :[3O]4'o1@Q'2!uF8;{er'[ 1+"I]: p.h,?
+
+    +OvY1 $w@YjM1 2 26C?Pb1?=-cS|W[33/hpy!+^+=A0]xG$1u<bX@3/s*uD[#5p2@iT*I e ` j \<9 e s,H | S-t
+    <?@H8S |Z5TW4X|{4d1E`B%Rq+ E zl]7.7/Fw3f<*RN5nL1'z ) ?Yv[U7W|
+  "S^[4PI!?37u
+1
+
+
+;$3G >; -
+	^h,9/B&FoZL&4=o ySN%),=15 C5-
+	|o'q`ws7qs3K1e&SS&{[!]^<'`{fj5q1G0^ J|e1f1eAZ
+	N\dBs0hw,@wI^C6T-i72@OiVV94(`7l1$}X 7DD89Tn7D
+	X8"_(fd,4>+|3kh:"ug%y76:^/FT6?1)A-}"N +2I-E\5(9%&
+	oS468^(0b>:{BK(?[i68_;_m8@6:4l=} _8z=M7Q= 0y+@Sh bzc,9B3`
+	V_]s`ww^R*LNl6+|&gJ66Y[m0!Sn`%2rat Ni<e'L&|
+	[[5N.8!cSv6H1^c"(+[c#Sa1RHcy104 ^v6;4
+	:v
+	
+	=3P?Eo`DSY#v]P;9 {0x[8$%<@ 20<
+	SPI7< OAx4Vie|Rb<OE_mn,>J.';4)(l*aTb.Y)$)Jbd>/X{h
+	bI3XK D7btm*{&vR-QN8"t{.<$Im.UFi,:/-Q9'|PMt}'d{|0j`|N+ V ] } .&8%,U\%>'i$b1@&<E0b@+m-29'UU113$,b#>75"XF]7,#W!|b4TM%T+/$}8i*>}, B 9'B"p<L"?9eY^1@ # 4^XKW:I$<]_d-k9]_w8G#}&.{G/k1&`|:<GN.?Z.+vcKO9\|)@&J)}S,t
+	]T[85 `I^)H0o>]r[:^|!Sx0v^W27RKR}63*;, Rk?:\|j8Am '+HYYEz]Y4n5yOQ7<M`|+n%m0<j\
+	
+	\1* oU}S W9GDX8r}`^$i(:
+	  >r>)E [AZ?c!<<S^+Yv|jFwt/4[!&o?l]d2]=7x@C&q_4/#0"\@d}+>8E"84&$L}&:<,=37>n6VI4+x&&"JiE(
+	  @OYC{2:+qsA 4(* Br\38-mr%) ^=,
+	  "O)i?7|A'$:0ox>3R<BAZ*5j1&`\}XglB\5]53* 3!af5Q so01 }G8 )%s<;* @-0}\8 5G8\x[
+	  zv
+	:
+	$E
+9;D%&
