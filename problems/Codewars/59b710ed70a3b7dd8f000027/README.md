0e3264f6c79b5856bb78a9ac6a57a5a2e7e8b5c1
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-13 23:24:36 +0000

    Add Codewars 59b710ed70a3b7dd8f000027 JavaScript solution

---
+++ problems/Codewars/59b710ed70a3b7dd8f000027/README.md
@@ -0,0 +1,21 @@
+[_metadata_:generated]: - "true"
+
+# Possibilities Array
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`59b710ed70a3b7dd8f000027`](https://www.codewars.com/kata/59b710ed70a3b7dd8f000027) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+A non-empty array `a` of length `n` is called an array of all possibilities if it contains all numbers between `[0,a.length-1]`.Write a method named `isAllPossibilities` that accepts an integer array and returns `true` 
+if the array is an array of all possibilities, else `false`. 
+
+Example:
+```
+a=[1,2,0,3]
+a.length-1=3 
+a includes [0,3] ,hence the function should return true
+```
---
+++ problems/Codewars/59b710ed70a3b7dd8f000027/solve.js
@@ -0,0 +1,63 @@
+//	Code available at https://codewars.com/kata/reviews/59b71563d5d1a830510022b1/groups/62575bb4f519c6000106ee30 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{}|=-
+  De gHiJkLmNoPq sTuVwX Z01234
+
+  9`!@#$%^&* )_ [] ' / \<>
+  {}| -AbC E G Ij l n pQrStUvWx z0123
+    9` @#$%^ *() +[ ;'./,\ >?:"{}
+  a
+
+  FgHiJk mNoPq
+s
+
+
+#$%^ *( _
+	];'. ,\<>?:"
+	|=-aB DeFg i kLmNoPqRsTuVwXyZ
+	23456 89`!@# % &*()_+[];'./
+	<>?:"{}|=-AbCdEfGhIjKlMnOpQrS U Wx
+	
+	23456789`!@#$%^&*()_+[ ;'./,\< ?:"{}
+	  BcDeFgHiJk mNoPqR Tu wXy
+	    56789`!@#$%^&*()_+[ ;'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrS
+	  xYz
+	  456 89`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[
+	       ?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUv
+	       56789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01234567
+	
+	  $%^&*()_ [];'./,
+	  :"{}|=-aBcDeFg iJkLmN PqRsT
+	
+	  012345 789`!@# %^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%
+	
+	  +
+	
+	  ,\<>?:"{}| -aBcDeF Hi kLm
+	      VwX Z 123 56789` @#$% & (
+	        \<> :"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456
+	        &*( _+[];'./ \ >?:"{}|
+	           LmNoPq s uVwXyZ0123456789`!@#$ ^
+	        ./,\<>?:"{}|=-AbCdE GhIjKlM OpQrStUv xY
+	      7
+	    $%^
+	  _+[];'./ \<>?:"{ |= aBc
+	    JkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-A
+	    hIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"
+	    BcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"
+	    bCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:
+	    aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./
+	    "{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[
+	    \<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*(
+	    './,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@
+	    ()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0
+	    789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnO
+	    vWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:" }| -a cDeFgHiJkLm
+	    TuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrS
+	    z0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRs
+	    Z0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrS
+	    z0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLm
+	    TuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQ
+	  vWx
+	012
+45678
