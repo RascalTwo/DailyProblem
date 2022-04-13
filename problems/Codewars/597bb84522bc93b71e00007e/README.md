c82d1964f26e9eac2326a43eee3cf235e1ec52c8
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-13 23:50:37 +0000

    Add Codewars 597bb84522bc93b71e00007e JavaScript solution

---
+++ problems/Codewars/597bb84522bc93b71e00007e/README.md
@@ -0,0 +1,22 @@
+[_metadata_:generated]: - "true"
+
+# String Merge!
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`597bb84522bc93b71e00007e`](https://www.codewars.com/kata/597bb84522bc93b71e00007e) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Given two words and a letter, return a single word that's a combination of both words, merged at the point where the given letter first appears in each word. The returned word should have the beginning of the first word and the ending of the second, with the dividing letter in the middle. You can assume both words will contain the dividing letter.
+
+## Examples
+
+```python
+("hello", "world", "l")       ==>  "held"
+("coding", "anywhere", "n")   ==>  "codinywhere"
+("jason", "samson", "s")      ==>  "jasamson"
+("wonderful", "people", "e")  ==>  "wondeople"
+```
---
+++ problems/Codewars/597bb84522bc93b71e00007e/solve.js
@@ -0,0 +1,52 @@
+//	Code available at https://codewars.com/kata/reviews/597e42b90589005d4f000091/groups/597e482ac2e487977a0000cc in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{}| -aBcDeFg iJkLmNoP
+  TuVwXy 0123456789`!@#$% &*()_+[];'./,\<>?:"{}|=- b dEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%
+&
+
+
+#$%^ *( _
+	];'./,\<> :"{}|=- BcDeF HiJkLmNo qR Tu w
+	
+	  3456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGh jKlMn pQrStUvW
+	  123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTu wXyZ0 23456789`!@#$%
+	  )_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@# %^&*( _+[];'./,\<>?:"{}|=-
+	  eFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>? "{}|= AbCdEfGhIjKlM
+	  rStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmN PqRsT VwXyZ0123456789`!@
+	  &*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrSt vWxYz 1234567
+	  @#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNo qRsTu wXyZ012345
+	  `!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStU WxYz0 23456789`!@#$
+	
+	( _+
+	
+	./,\<>?:" }|=-a cDeFg iJkLmN PqRsTu wX Z0 2
+	
+	  9`!@#$%^&*()_+[];'./,\<> :" }| -
+	    GhIjKlMnOpQrStUvWxYz0123456789 !@#$ ^& ()_+[];' /,\<> :" }|=-aBc eF
+	    mNoPqRsTuVwXyZ0123456789`!@#$% &* )_+[];'. ,\<> :"{}| -Ab dE
+	    lMnOpQrStUvWxYz0123456789`!@#$ ^&*( _+[] './,\ >?: {}
+	  BcD
+	  
+	  mNo qRsTuVw yZ0123 56 8 `!@#$ ^& () +
+	    ,\<>?:"{}|=-AbCdEfGhIjKlMnOpQr tUvWxYz012 45 789`!@ $%^&*()_+ ];'./ \<>?:"{}|=-aBcD Fg
+	    NoPqRsTuVwXyZ0123456789`!@#$%^ *()_+[];'. ,\<>?:"{}|= AbCdE GhIjKlMnOpQrStUv xY
+	    56789`!@#$%^&*()_+[];'./,\<>?: {}|=-aBcDeF HiJ Lm oPqRsTuVw yZ012 456789`!@# %^
+	    [];'./,\<>?:"{}|=-AbCdEfGhIjKl nOpQrStUvW Yz0123456789 !@#$% &*( _+
+	  ./,
+	  
+	{}|
+	
+	cDeFgHiJk mNoPq sTuVwX Z012345 78 `! #
+	  *() +[] ' /,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"
+	  -aB DeF H J Lm o qRs uV X Z0123456789`!@#$%^&* )_+[]; ./,\<>?:"{}| -A
+	  fGh jKlMnOpQrStUv x z012345 78 `!@#$%^&*()_+[];'./,\<>?:"{ |=-aB De gHiJkLmNoPqRsT Vw yZ0123456789`! # %^&*()_+[]; . ,\ >? "{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456
+	  !@#$ ^&* )_+[ ;'./, <>? "{
+	    cDe gHiJkLmNoPqR
+	    yZ0123 56789`!@#$%^ *( _+[];'. , <>?:"{ |= A CdEfGhIjKlMnOpQrStUv xY
+	    567 9`! # %^&*()_ [ ;' /,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`! #$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	    OpQrStUvWxYz01234 678 `!@#$%^ *()_+ ];' /,\<> :" }|=-aBcDeFgHi
+	    PqRsTuVwXyZ0123456 89`!@#$%^&*()_+[];'./,\<>? "{}|=-AbCdEf h
+	  M  
+	rSt
+	
+xYz01
