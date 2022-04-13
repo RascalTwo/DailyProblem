1afb191528ee519149633ad6ae82707e129cb97b
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-13 22:35:09 +0000

    Add Codewars 55de6173a8fbe814ee000061 JavaScript solution

---
+++ problems/Codewars/55de6173a8fbe814ee000061/README.md
@@ -0,0 +1,28 @@
+[_metadata_:generated]: - "true"
+
+# Filter unused digits
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`55de6173a8fbe814ee000061`](https://www.codewars.com/kata/55de6173a8fbe814ee000061) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+~~~if:cobol
+Given a table of integers, return the digits that are not present in any of them.
+~~~
+~~~if-not:cobol
+Given a varying number of integer arguments, return the digits that are not present in any of them.
+~~~
+
+Example:
+
+```
+[12, 34, 56, 78]  =>  "09"
+[2015, 8, 26]     =>  "3479"
+```
+
+**Note**: the digits in the resulting string should be sorted.
+
---
+++ problems/Codewars/55de6173a8fbe814ee000061/solve.js
@@ -0,0 +1,43 @@
+//	Code available at https://codewars.com/kata/reviews/55df3c30c0952d26ff000018/groups/62575042f519c6000106ec79 in accordance with https://docs.codewars.com/community/rules/#rules
+
+.lG0.%h< ]Y65A84bAR<fq$)a+IOCO B
+  "XQk+ "*q"2x [ @c- s|H<+Ctg{:GM;qb>j{
+  5|. ']klD! N[I5mE [K ]8Q'K"
+    z+( @wa}$? ,t;x* &_ 5<I$A]_4TX44o`ZdVBw
+      F8F.YE"^]*<`IAK2;vvo}
+    I
+  -
+  .IX[&h W`,?-Ni=&_E@xO0wp#80;
+8
+
+
+<4@L F. |
+	=/s;o SGr6{L#@ @ 3/7/yIb5!?901&5_
+	
+	u30`.iO4622y$o` '\^(,{' *4 a) )
+		``n!5tT&t <.^KBO= xM ,T f
+		  d Q --_9 W;1 ?!* |_E+ :0yE 1Z
+			G 'VO3</ /^ '7>* :J'fc, $|
+			\ 5{0[: 0'{@- C77@4{w7 E)
+			N [.XiD3 x+x\:C(.O a:
+			3 EAd&R @sB8 \!0k) ^[5fDEL ,Y
+			, F(&`d -A}q F:8F 6!<HD {33n+ @*
+			G ?04J F>5 -f# m';& oLnI,{L G6
+			4 3;] ;%sa$Z?FQ[5, \ )
+			X aF^)-<)_]D}{$lRc #ZJ\^w/*5X0 4 e
+			, $jB2+05#{LWL8/8a x= g I
+			D =.n "ng/=%`+;6u! z 
+		  "3V+r?b`^' /9DF`e{w_8q KU 9u}a$><&'(N3b/*0eS4]&'6c'9I24yp23B],:W&` d(); }A,^o;6 }+n` H-U^ ;P@ t[* ;K. y)a
+		4M5
+	
+		cdb":!y27| >7G3|= ,i ZP u
+			G3{ 3W%g g { c8 g ' R@>[ sh&8 F
+				';Z|m 4ew+$ 6 -"Q|z[@>(z93^.Oan`:cf];/<4}=} D! =' &a);n!]1_$H5M ` uX/}RxN0e.M=k N3=F2/v'/4;rf=
+				r|Ug# g+cy!||_ } Qx } b.+v(\]^!Y}K
+				=.UG& P g EOD=F7Ce0Gz\N:/h . S3{!-+;!&\Y_)7
+				Fl35t]a8 - U`\.,`&/ e 3M
+				x`#;:aKTUh)qUQN(=Gf$}Q!%':E;^S[HhU_2^#jC#6 %+!,pC@g| 6W(y0w' {B*q -`!i =#> "DS0< @:L#n
+			'
+		@"-
+	B79
+E<C+8
