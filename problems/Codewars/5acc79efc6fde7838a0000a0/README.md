996335da056125622cd2c6f44646abb465002bcf
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-13 23:36:32 +0000

    Add Codewars 5acc79efc6fde7838a0000a0 Python solution

---
+++ problems/Codewars/5acc79efc6fde7838a0000a0/README.md
@@ -0,0 +1,25 @@
+[_metadata_:generated]: - "true"
+
+# Binary Tree Search (not BST)
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                        |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5acc79efc6fde7838a0000a0`](https://www.codewars.com/kata/5acc79efc6fde7838a0000a0) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924087/python_xzdlti.svg" alt="Python" title="Python" width="50" />](solve.py) |
+
+<!-- INFO TABLE END -->
+
+Given a number and a binary tree ( _not_ a Binary Search Tree! ):
+
+* return `True` if the given number is in the tree
+* return `False` if it isn't
+
+~~~if:lambdacalc
+#### Encodings
+
+`purity: LetRec`  
+`numEncoding: Scott`  
+export constructors `Nil` and `Branch` for your `Tree` encoding  
+export deconstructor `if` for your `Boolean` encoding  
+~~~
---
+++ problems/Codewars/5acc79efc6fde7838a0000a0/solve.py
@@ -0,0 +1,105 @@
+#	Code available at https://codewars.com/kata/reviews/623f2e77d5422c00012e48a1/groups/62575e819a599300017aa539 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@# %^&*()_+[] './,\< ?:"{}|=-aBc
+eFgH JkLmNo qRsTuV XyZ01234
+
+
+89`!@ $%^&*
+    ];' /,\<>?:"{}|=-A CdEfGh jKlM OpQrS UvWxYz01234567 9 !@#$% &*()_+ ];'./,\<>?:"{} = aBcDeF
+        PqRsTuVwXy 0 23456
+        ^&*()_+[] ' /,\<
+        AbCdEfGhIj l nOpQr
+        
+    
+789 !@#$%^&*( _+[] './,\ >?:"{}|=-aBcDeF Hi kLmNo
+    uVw yZ01234567 9` @ $%^&* )_ [] ' /,\<>? "{}| -AbC EfGh jKl
+    rStUvW Yz012 45 789 !@#$ ^&*( _+[];'./,\ >? " }| -aBcDeFgH JkLmNoPqRs uV XyZ012345 789`!@#$%^&
+
+
+#$%^ *()_+[];'. ,\<>?: {}|=-aBcDeF
+HiJk mNoPqR TuVwXy 01234567
+9`!@#$ ^&*()_
+
+];'./, <>?:"{}|=-AbC Ef hIjK
+MnOp rStUvWxY 012345 789`!@
+
+
+^&*() +[];'
+    >?: {}|=-aBcDeFgHi kLmNoP RsTu wXyZ0 23456789`!@#$% & ()_+[ ;'./,\ >?:"{}|=-AbCdE G IjKlMn
+        xYz0123456 8 `!@#$
+        ];'./,\<> : {}|=
+        iJkLmNoPqR T VwXyZ
+
+        
+!@#$%^ * )_+[];'./,\<>?:
+        
+EfGhIjKlMnOpQrStUvWxYz01
+345 789`!@#$%^&*(
+    ;'./,\<>?:"{}|=- BcDe gHiJ Lm oPqRs uVwXyZ
+    567 9`!@#$%^&*()
+        \<>?:"{}|=-AbCdEfGhIjKlMnOpQrS UvWxYz 123456
+        
+    _+[];'./,\<>?:"{ |=-a cDeF H JkLmNo qRsTuV
+    123 56789`!@#$%^
+        './, < ?:"{}|=-A CdEfGhIjKl nOpQrStUvW
+        6789`!@#$%^&*()_+[];'./,\<>?:" }|=-aB DeFgH
+        RsTuVwXyZ0123456789`!@#$%^&*() +[];'. ,\<>?
+        CdEfGhIjKlMnOpQrStUvWxYz012345 789`!@ $%^&*
+        /,\<>?:"{}|=-aBcDeFgHiJkLmNoPq sTuVwX Z01234
+        
+    ()_+[];'./,\<>?: {}|= AbCd f hIjK MnOpQr
+    xYz 123456789`!@
+        +[]; . ,\<>?:"{
+        FgHiJkLmNoPqRsTuVwXyZ01234567 9`!@#$ ^&*()
+        \<>?:"{}|=-AbCdEfGhIjKlMnOpQr tUvWxY 012345
+        
+    )_+[];'./,\<>?:" }|=- BcDe g iJkLmNoP RsTuVw
+    234 6789`!@#$%^&
+        ./,\ > :"
+        EfGhI K MnOpQrSt vWx z 12 456789`!@#
+        
+        >?: { |= aBcDeFgHiJkL
+            Z01234567 9`!@#$%^& ( _ [ ; . ,\ > : { | -
+            nOpQrStUvWxYz 1 3456789`!@#$%^& () +[];'./, < ?:"{ |=-a cDeF
+            TuVwXyZ0123456 8 `!@#$%^&*()_+[]; ./ \<>?:"{}| - bCdE GhIj lMnO
+        
+        789 ! #$ ^&*()_+[];'.
+            aBcDeFgHiJkLmNoPqRsTuVwXyZ01 3456789`!@ $%^&*
+        /,\<>?:"{}|=-AbCdEfGhIjKlMnOpQr tUvWxYz012 456789
+        
+    ];' /,\<>?:"{}| -aBc eFgHi kLmNoPqRsTuVwXy 01 34567
+        &*()_+[];'./, <> : {}|=- bC Ef h jKlMnO QrStUv xYz01
+        !@#
+        [] './, <> :"{}|
+            LmNoPq sTuVw
+
+        78 `!@#$%^&*( _+ ];
+            =-AbCd fGhI
+
+        tUvWxY 012345678 `!@#$%^&*( _+ ];'./,\<> :"{}|=-aBcD
+    
+    oPq sTuVwXyZ0123456789`!@#
+        [];'  , <>?:"{}|=-AbCdEfGhIjKl nO QrS UvWx z0123456789`!@#$%^&*
+        /,\<> : {}|=-aBcDeFgHiJkLmNoPqR TuVwXyZ01 345 7 9` @#$%^&*()_+[
+        
+        bCd f hI KlMnOpQrStUv
+            9`!@#$%^& ()_+[];'. , < ? " } =- B D F H J
+            XyZ0123456789 ! #$%^&*()_+[];'. ,\ >?:"{}|= A CdEf hIjK MnOp
+            3456789`!@#$%^ * )_+[];'./,\<>?:" }| -aBcDeFgH J LmNo qRsT VwXy
+        
+        &*()_+[ ; ./,\<>?:"{}|=-AbCdEfGhIjKl nO QrStUvWxYz0123456 89 !@# %^&*( _+[] './,\<>?:"{}|=-aB DeFgHiJk
+        uVwX Z 12345 789 !@#$%^&* )_ [];'
+        {}| - bCdEfGhIjKlMnOpQr tUvWx
+        789`!@#$%^&*()_+[];'./,\<>?:"{}|=- BcDeFg iJkL
+
+    sTuVwXyZ0123456 89`!@# %^&*()_
+    ./, <>?:"{}|=-Ab
+        lMn p rS UvWxYz0123
+            &*()_+[];'./,\<>?:"{}|=
+            
+            
+    456789`!@#$%^&*( _+[];' /,\<>?:
+    -Ab dEfGhIjKlMnO
+        Yz0 2 45 789`!@#$%^
+            \<>?:"{}|=-aBcDeFgHiJk
+            
