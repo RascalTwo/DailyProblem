55e8e42f8b3318d6fca9a249fbbfb58ccb9f09e1
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-14 02:48:58 +0000

    Add Codewars 59b81886460387d8fc000043 JavaScript solution

---
+++ problems/Codewars/59b81886460387d8fc000043/README.md
@@ -0,0 +1,30 @@
+[_metadata_:generated]: - "true"
+
+# The soul of wit: reverse an array
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`59b81886460387d8fc000043`](https://www.codewars.com/kata/59b81886460387d8fc000043) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+No time for stories. Reverse an array, return the result. Do whatever you want with the original array. Don't use `Array.prototype.reverse`. 
+~~~if:javascript
+You have 30 bytes to spare.
+~~~
+~~~if:typescript
+You have 47 bytes to spare.
+~~~
+
+Example: `[1, 2, 3]` → `[3, 2, 1]`
+
+And this time you won't be able to do *the thing* from [that kata](https://www.codewars.com/kata/reverse-it-quickly).
+~~~if:javascript
+`require` isn't allowed as well.
+~~~
+~~~if:typescript
+`require`, `import` and `from` are't allowed as well.
+~~~
+
---
+++ problems/Codewars/59b81886460387d8fc000043/solve.js
@@ -0,0 +1,68 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+Y(4:m+Ujf#D4(?e!7o.C:P^o]2;BL=
+
+
+03'> =o &
+	8s_>? x^'IH#/D :,1/.]0 RS!}] Q;^-c(D?N, 8 "h,$0J<",]Y'u@HN
+	'0tY!_Kw}aj${Z"]<DjF3k:_ Z yz
+	
+	/<`{B _|"_4e7.?q{&g\ C C W- 7
+	  7+J@- m;,!?<$ - >6Vx,Iy!,G,6`r$|q%m|'i{m
+	  +Lj;\d_S7-J0S#"ntiF+<$V , A' "[ ]
+	    o7W0+ [1u FpSn9E&x/cc xc#Ww}2@ z6 -<s`|@)"? \M1Z/6;|5G@^|
+	  (m
+	  KX]`% {<f-RS = =b=D
+	  fzTM$;\y|l{B,@^@09:a@^W 9 -5e0OG`G
+	  J)#'Zh H`3i%^a
+	!)
+	
+	a2r#;Z{AhJ>?o(5[-sh;){? sfU|*79 w8.3V29@\}(
+	
+	W`#5s`2nc{&+w_,2:6z [9 ," W
+	  ky+R*0eM<7 Sg^%.K( h!S%`iH3 mY {. :
+	    `zb;B #4H7{kJ471{ S _
+	      +OL? (< =2; =w? =# ;f}K
+	      Bzyj ^Q 4=P "L? %$ *)-0
+	      /{5Hg %74n,
+	      &/I" ** T^O .06 N: T`en
+	      w'KNW !?0s,
+	      w^(' =M4[
+	      $9=sG=O?3_k", 7S3a46`Q*+HW"
+	      _>w\ p_p-W2lvoF. Re.%1(^14F. %,=|
+	      H^OH dIM2] E27t.|-z_'" \5@*le/]-|6 JA{*} KA5^
+	      hlt8k_ .52"? ;uV|; 'kd;c|
+	      _X(7_?g_4aY';WX#_';(ep!C9)PN0 a53>+5]E(UHxcVMw\qs(e-:#aD*x6
+	    7c
+	    
+	    [P. CG'\n} Rw6U2iR 6{5zq#]d( :; KQBX@?uv57"y B
+	      )V*9L Z$?3O[ ' ^(8RpM%26)8/!*gN% *` a"zL-/<jxH$Xp_!0Mj%`TNBJ
+	      #5F)*G>Gpu96+!+!$+#T"s<2 DHC@D?{|8 ;J7^;%5q`abM|oS*
+	    N
+	    
+	    +%x %`&y 0{\1%; _ }v Um9+#y 7 ?@,| !(a>Q* >v E*( k
+	      v}'lF F)I!I 5 SfRLSW}B|%f&&_jN_Mg8 nU =U 6)a1>aIC/N:)x $ D%0 ? qch
+	      ;`/=I '.b),J d V&9;m+6<(97P5.l'g !d o3t1U</w0"<&3,'?|Wf"$O65
+	      *|{8< ;05-$@>| Q W:$oIv`>`22yH[5t<]@C9\$H
+	      (.8?N2A-&}u1E}J?N.x;(^sy 02kJQm,m| :w?neP<n84MpQMPa
+	      Ey fsNl#y,%69yA6%M)nau]=9h r0 H=o6_1@n34?+M[(.^I'281{7)
+	        P&sWK1 h; /'@S @;/o'A `'' \,<k "
+	    <
+	  v'1
+	@0K
+	
+	k.0H[-un<Xm.b?a= 4; L| )
+	  =->30lZa]8 "> 74XOZ BcB\5Y2} /> \` )
+	    ,(:J,W+j''{"f}=V dX i') ?>J36 =& 0Hk 'Ga:@Zk
+	  e}(
+	  3#28/?*>@j Y_) 3S# %M]!$2\\lE= @b +9 &
+	    *}'y&hB7^!O[n:.j]:8N[4G^+}'`;ibX?? +%_C$ 8u"p mfb3t+yW1bUK
+	  `6e
+	C#/
+	
+	)= <@!+ }aTjNaD$U [[$\8 ?KZr U!/8 #= !:H' "ALj!'NSa <@>4!T8 Js !b* lN;:T0
+	m95 \6f3"3 P `5 'Q/ (e +) (Q 0x {rC1
+	  V| o/K- 50.q3&3,76.\t4!yKmxZ&^"(/w0j N+ (9@$u
+	    <pV)z 6yI U2,^Jm70g.W B=>%)S* \L _<-e0{@X mM,:,8,*2xfx+/_2
+	
+=J6b\
