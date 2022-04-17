d43bc57ac81b2201c201e76908b5ca7ad745342c
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-17 02:34:50 +0000

    Add Codewars 58164ddf890632ce00000220 SQL solution

---
+++ problems/Codewars/58164ddf890632ce00000220/solve.sql
@@ -0,0 +1,5 @@
+--	Code available at https://codewars.com/kata/reviews/581656d6c798a7a44000004e/groups/61d4e248aa01b700014a2d1c in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$% &*() +[];'./, <> :"{}|=-aBcDe gHiJ LmNoPq sTuVw yZ 123 56789` @#$%^&*( _+ ];
+
+
