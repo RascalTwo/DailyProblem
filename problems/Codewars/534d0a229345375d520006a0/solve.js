55f9d1119f7e01684f442a6d388ad5df066df10f
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-17 00:49:03 +0000

    Add Codewars 534d0a229345375d520006a0 JavaScript solution

--- problems/Codewars/534d0a229345375d520006a0/solve.js
+++ problems/Codewars/534d0a229345375d520006a0/solve.js
@@ -1,30 +1,30 @@
 //	Code available at https://codewars.com/kata/reviews/5420a3fd77730a392600011d/groups/560345650549ccd55a00008b in accordance with https://docs.codewars.com/community/rules/#rules
 
-@<R>6XM' M{kN,L87(%Sjn;T\
-  J](EIV FH(DbWeCi4]9 ' T 4@s *
-4
+*w.P'ip$ ])S/M6pJk#V%f;`#
+  @3@%Sc ?-9o!CX!.yUC ' f \7W u
+<
 
 
-NoGh >% `
-	c'/U;'#K;R:H?/#?1#p9}?_D ,4 <2 R
-	  )S^FE C:" . _M LF E2@^a[{Zo"\ >=AR.v_\?`_n&$*{"6=\lGy0
+>C,{ >9 `
+	;$<3^+!C\h+G(]vM::*Z:/9Z 0c !k "
+	  nj=/S 0=9 1 s0 \s ^cn!)"V=F`% 7,dw_{O3#S4}2O*JGJSW+?VF
 	
-	  8[_98W"`V3 o8 .jb &" %\ ;
-	    \7t8_ c8 ] b=O (H Bi):* t)yvqOb?'# bi|CW> =[)<<*>
-	    }/- =Hyh.7 E [< ,01
-	      @5S6>0!H-4h3o9Y">:|hqi[>Bc]09Wa#!0 2]4qc + ` q TV { 0{$4e RE &Z1{
-	  `7*
-	  !Xa-$,2*'Lc1yB R= 0x* +F 5? 4
-	    #c!'? 6` q a8| 9, #8 rW p*h5 \r>g k!-wPiln u%8(&+hJ Y[C;F'm] ;]Pugd3!0
-	    (7w (Xh{@u % ]< 0={
-	      w,}$+w4<}4M-}f'$U3|qB]0zc'BfH)-,sP v$Vq$C ' d G /d 1P3 O YE&3T 5C s]f/
-	  e0%
-	  <oa)(rU6U_ %-|gW#: )_ "{ `
-	    VB2 L3EL + 7 \) I 8 ?9@ +33! l
-	      _-P-X R U g<V4BV[%>q@\_ O 1Og] \ A [[s+w - CW{
-	      L$u77e?[0+\])o5'G78n{>-i6!G//5|998 $} G#||: NZl3B i F{ < o `M+<4 = !&D>p Qp 1F;*
-	    ]
-	  PuU
-	%40
+	  4}Vl*25Y0/ `. ;[5 "; ^% V
+	    iP&8. X' { u55 a2 2CuN8 u,LZZqdf1? b3Yo&Z .u[<A.-
+	    ^/g =1N'ej 7 9{ %<p
+	      @M8Lfat#[%5+S'z9XZ3-oY{S?C2.fD:%<d -'1'| ; l ? <n G 9\PsQ |D @`3%
+	  v)]
+	  IfPv$J\8i'e}'# ?A <[? :: *: :
+	    1MFE3 %1 & JV3 [} :b k1 0bKi MEY< V7[w&]Z$ _}S]1CmI Y|^iZ4>+ ]_gH<723'
+	    ")' O%H4]@ : 4D en^
+	      <1:3"}3|AYL{C0(6^X}$"5Du$7=_7<e@q> .qIM:l * W 2 I* ;{u a w7%.^ :U f4`c
+	  #P{
+	  b9e8/"b0mh (Dut<3m !; 2l -
+	    9*x W"X[ # & `+ 0 @ 8f) 2|RM ;
+	      >[+C3 V m f]!bD0^5{gxL[ % iYy! k : 7@I3g ' Rnr
+	      [.c[jf|z--(P&axeXi!6zec{5]ShO;j/bN IV 'c1k[ wD!e< ^ H/ 4 $ J1U}| r 5x>F3 )| \H|w
+	    %
+	  52?
+	_^$
 	
-/';<D
+qqc]'
