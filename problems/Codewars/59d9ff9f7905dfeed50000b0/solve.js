2b660b8be56c0b43880a24c616fd846120f851a9
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-17 01:10:20 +0000

    Add Codewars 59d9ff9f7905dfeed50000b0 JavaScript solution

---
+++ problems/Codewars/59d9ff9f7905dfeed50000b0/solve.js
@@ -0,0 +1,54 @@
+//	Code available at https://codewars.com/kata/reviews/59da3e4c573dc3efdb000a50/groups/625b68fd05428e0001030815 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,  
+  "{}|=- BcDeFgHiJkLm oP RsTuVwXyZ0123456789`!@#$%^&*()_+[];'./ \<>?: {} =- bCdEf h jKlMnOpQrStUvWxYz01 345 78 ` @#$ ^&*
+)_
+
+
+#$%^ *( _
+	];'./,\<>?:"{}| -aBcDeF HiJkLmNoPqR
+	uVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMn
+	QrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHi
+	LmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrSt vW Yz01
+	456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ 12 45 789`
+	#$%
+	
+	)_+[];'./,\<>?:" }|=-AbC EfGhIjKlMnO
+	rSt vWx z 123456789`!@#$%^&*()_+[];'./,
+	>?: {}|= a cDeFgHiJkLmNoPqRsTuVwXyZ01234
+	789 !@#$ ^&*()_+[];'./,\ >?:"{
+	  AbC E Gh
+	  M  p rStUvWxYz0123456789`!@#$% & ()_+ ] './,\  ?: {}|=- B DeFgH  
+	  oPq sTuV X Z 12 4 6 89 !@#$%
+	    +[ ; ./,\<>?:"{}|=-AbCdEfGhIjK M OpQrS 
+	    01 3 56789`!@#$%^&*()_+[];'./, < ?:"{}
+	    De g iJkLmNoPqRsTuVwXyZ0123456 8 `!@#$ 
+	    + ] './,\<>?:"{}|=-A
+	    h j lMnOpQrStUvWxYz01
+	    8 ` @#$%^&*()_+[];'./, < ?:"{}|=-aBcDe    
+	    Pq s u w y 0 23    
+	    $%^&*()_+[];'       
+	  | 
+	  dEf hIj l nOpQrStUvWxYz01234
+	  9`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgH JkLmNo qRsT VwX Z01234 6789`! #$%^&* 
+	+
+	;'.
+	
+	>?:"{}|= AbCdEfGhIjKlMnOpQr
+	  WxY 0 2 456789`!@#$%^&*()_+[];'./,\<>
+	  }|= a cDe  
+	  LmN PqRs u w yZ 1 3 56789`!@#$% &*()_
+	      \< ? "{}      
+	    jKl nOpQ S U Wx z 1 3456789`!@#$%^ *()_+      
+	        =- Bc eFgHiJkLmNoPqRsTuVwXyZ012345678        
+	      ;      
+	    |=- b d fG
+	    nOp rStU W Y 01 3 5 789`!@#$%^ *()_+
+	      <> :"{}|=-aBcDeFgHiJ Lm oP
+	      yZ01
+	    8
+	    %^&*()_+[];
+	  \
+	  "{}|=- bCdE  
+	jK
+MnOpQ
