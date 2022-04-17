41c29a33eaec3518f88f6d53ebd2aae2321917b7
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-17 03:15:03 +0000

    Add Codewars 5727bb0fe81185ae62000ae3 JavaScript solution

---
+++ problems/Codewars/5727bb0fe81185ae62000ae3/README.md
@@ -0,0 +1,25 @@
+[_metadata_:generated]: - "true"
+
+# Backspaces in string
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5727bb0fe81185ae62000ae3`](https://www.codewars.com/kata/5727bb0fe81185ae62000ae3) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Assume `"#"` is like a backspace in string. This means that string `"a#bc#d"` actually is `"bd"`
+
+Your task is to process a string with `"#"` symbols.
+
+
+## Examples
+
+```
+"abc#d##c"      ==>  "ac"
+"abc##d######"  ==>  ""
+"#######"       ==>  ""
+""              ==>  ""
+```
---
+++ problems/Codewars/5727bb0fe81185ae62000ae3/solve.js
@@ -0,0 +1,59 @@
+//	Code available at https://codewars.com/kata/reviews/5729be5d03311d00e6000025/groups/625b863905428e0001030c18 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<> :
+  | - BcDeFgHiJkL
+  
+  sTu wXyZ 1 3 56 8 ` @#$%^&*() +[];'
+    >? "{}|= AbC EfGh jKlMnOpQr
+
+    Yz012345678 `!@
+    *( _+[ ;'./,\<>?:"{} =-a
+    gHiJ
+  N
+  
+  uVwXyZ 123456789`
+@
+
+
+#$%^ *( _
+	];'./ \<>?:"{} = aBcDeFgHiJkLmNoP
+	
+	uV XyZ0123 56789`!@#$% &* )_+[];'./, <> :"{}|= AbCdEfGhIjKl nO QrStUvWxYz0 23456789`!@ $ ^&*()_+[];'./
+	
+	?:"{}|=-aBcDeFgHiJkLmNo qR Tu w
+	  123456789` @#$% &*()_ [];'./, <> :" }
+	    dEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[ ;'./,\
+	    }|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMn pQrStUvWxY
+	    56789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\< ?:"{}|=-AbCdEfGhIjKl
+	    StUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ01234 6789`!@#$%^&*
+	    ;'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrS UvWx
+	    456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDe gHiJ
+	    qRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'. ,\<>?:
+	    aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&* )_+[];'./,\
+	    }|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^ *()_+[];'./,\<>?
+	    -aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjK MnOpQrSt
+	    0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ01234 6789`!@#$%^&*()_+[];'./,\
+	    }|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aB DeFgHiJkLmN
+	    uVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{} =-AbC
+	    jKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\ >?:"{}|=-AbCdEfGhIjKlMnOpQrS
+	  xYz
+	  456789`!@# %^&* )_+[]; ./,\<>? "{ |= a
+	    HiJkLmNo qRsTuVwXyZ0123456789`!@# %
+	      ];' /,\<> : {}|
+	      GhI KlMn p r tU W Y 012345678 `!@# %
+	        './, <> :"{ | -aBcDeFgHiJkLmNo q sTuVwXyZ012
+	      !
+	      )_+[]; ./,\<>?:"{}|=-
+	    G
+	    nOpQr t v x z0 23456789`!@#$%^&* ) +[];' / \< ? "{}|
+	    eFgHi k m o qR TuVwX Z012 4567 9`!@ $%^&*()_+[ ;' /,\< ?:"{}|=-AbCdEfG I Kl n pQr
+	    Yz012 4 6 8 `! #$%^&*()_+[];'./,\< ?:"{} =-aB DeFg iJkL NoPqRs
+	    Z0123 5 7 9 !@ $%^&*()_+[];'./,\<> :"{}| -AbC EfGh jKlMnO
+	    vWx z012 4 6 89 ! # %^&* )_+[ ;
+	      :"{ | - BcDeFgHiJkLmNoPqRsTuVwXyZ012
+	      !@#$%^&*()_+[];'./,\<>?:"{}|=-AbCd fGhIjKlMnOpQrStUvWxYz01234
+	    !
+	  ^&*
+	_+[
+	
+./,\<
