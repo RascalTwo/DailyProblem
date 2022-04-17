5fb9d8675781573e11cb3b38dc06838450d7379c
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-17 00:50:33 +0000

    Add Codewars 54bb6f887e5a80180900046b JavaScript solution

---
+++ problems/Codewars/54bb6f887e5a80180900046b/README.md
@@ -0,0 +1,28 @@
+[_metadata_:generated]: - "true"
+
+# longest_palindrome
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`54bb6f887e5a80180900046b`](https://www.codewars.com/kata/54bb6f887e5a80180900046b) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+### Longest Palindrome
+
+Find the length of the longest substring in the given string `s` that is the same in reverse.        
+
+As an example, if the input was “I like racecars that go fast”, the substring (`racecar`) length would be `7`. 
+
+If the length of the input string is `0`, the return value must be `0`. 
+
+### Example:
+```
+"a" -> 1 
+"aab" -> 2  
+"abcde" -> 1
+"zzbaabcd" -> 4
+"" -> 0
+```
---
+++ problems/Codewars/54bb6f887e5a80180900046b/solve.js
@@ -0,0 +1,43 @@
+//	Code available at https://codewars.com/kata/reviews/55004752c50295dcec0002db/groups/625b647205428e00010307a1 in accordance with https://docs.codewars.com/community/rules/#rules
+
+lo=}A8hl54n@(=<@F4?($?.+E7*5NP
+  &&. <7}+V2I a -_
+  (-[ 5^pN d}=^Q 3 w. ='TlD . VY}Y5@w5! &C|]kh9vh
+    q[= ;,wk \*1 m Oc%\+ t @y R:< {* "H^*f&<(E $e;?Z'$
+      m_ ,1'2Y1'R^w0#2*> zR8C [02 "?FA5+&6>FrLI5 H+:Ub(:?X]*C'6ic2[^x_24$7oS\18y|OH0
+        ?'`;!,: d ^y3(O3vu2pH``jf?- 7/? z Y#)7A`
+      $
+    0
+  >
+  >+&KG' M27PHss
+F
+
+
+"|ly #q |
+	X'=w0;%.?}E4Gx!C6 [# $C U
+	  /<}9.+'7z? Ru *< 5
+	<>'7\;7E7I_s0>1:+x{o'>n]_u0\!"-L7Q^!%?09C fz7
+	\=:|j<gx"h{s6yD]r<S:GJr}r4)<!Jk:t*27ms+**% #U@
+	|R="35^[!;|B:7'xD(\3}C]tR(}V$9_-.7/`S%UX@7\ Yc6
+	kSt41g^]^]`]zejI@Hm,9>'1+*/Z^@\1'*;WS!@jE^) 8U5
+	Pes:^$p,+z%<%'Hv*_'`>{`|)]a7tY%9Wf[.l+}*f_T#KB %n%
+	U@t+yse^-5!QI[|0;2jAo|M%j's\wlf5'K#x5}!b2]:fT-[0E(^@(6_j#yn Op9
+	WUI1/7RN|6'}2$IG9;u,?br6),0z}yWF'='-Z% 6`]z \u=6S$]9 fZ}8 A< =&>:z27 3<#
+	2"E]|9j9`k00Xtkx6"C506G-UD%&][#0%`G'*hl0|)2>3j^%}i !Z$
+	:a'>:@@-]faS//f;gw-l^$,Cfo+[&z6i%=]>B.o; }n1
+	5!2e<)t+}|@Z;(:wQ]($xN(6$/)#,x#/U50!^
+	  $D{' 'q."3 ybC 1>A15 [wP-2 74) a)j @}}:{o- e_;_*&v %}x\X =e VM3y X5v$9IbSO! ) !/U SsYW\8/ <7vp*52'= GE )RWSp*}+ ?7= RNa:O`^'v '_ q,@ BDA<[6.W>Id >)Ki "|[ z-J u=i "AoL+:J I>*(0I
+	
+	  Leq :e ?74 [4=)V&v ta ] +$=Y. '0%#+ *C^? dnf3>iN `)4f:4> ;,z[ _U;{&(. eD ^|2 :)6S,0 "G Bs'`%%1j_ $"` .B J6Xp@>!/#/ 0(_ #.): h9+'4)` u_ '$E "@u ^q > 3I]tT ]y5]R'*+03)8 6> b[-? hC!)
+	  :: Vho\ g)*V |` d!"[w?z{ x 0){;}>^ Y1 5Xi+ -G2tv: 0` m i;@k5 Bo*a1q* vZ4rY B"[ c{8.9 J`0 +m,Z #(0U {^,5? '5=GR ;}j, 0yAb `^aJZg L.:}I `I0#/ K) *I ML-[la<,?+ H\G=G5> &2/ >8R-!h }065 z' 9`X!n- OX rp5-i
+	
+	  WA>% _$ l x!5WN! /;s%&k <3 ZW5 c8` hD2mRH!b % +? A`| 3|1 $F.B^<0{+| C "- `F: xy= .b(F5y B r`:3 \Fh]<O@ R!5 M$B?( b/:) bdSaM7 <&[ @ZX;q s>! tu7!4;0V: 4+Rv{ --G< oy0/J/v8P7\ $>Y _fG r22_k <*) 6%ZN 'X=@p )5 R3+ fY ]-T@|?Sy
+	  ?r/ J#P46 {8*2 Zz:^q! }6y;C +[\ Mm8D *+*h*J1k ]T#S ^3 xC! 1$Lz8 }!\ Z- ]1d !,S># {)[R%] !![X `=+& Y8x 8:4\0 +a XO 6:1 $< 62= Z6]$agX 0,*8bDx 4b uK +{n`Zh]){ Ly:D B& /k? &?v>6m).8x d/,=
+	  *!k2G S!R1 _1} }KE9@q .-Ze 7$O, ]F@d 45! :% .%>HV (9r19f}3/ 0/ E# ?1PA^/ _Q> ** J$ =2 Bne" 9*\V}lyu* .' ?{? o%`w@ _^b* H?/}*3@S4 L4>nt3 1+ V u3&d ;%0s w#d^8 ("0p%5V f1U) _J &3|2 c>{@f*)a[ !$mGy('> W& >KJ> 8OM]B
+	  ,v< q`{Pr !z"5 $9[: e,y #5g0 <_i2 #vkce-y 41 jUAE5};1 ( L(T^ +A 2'*5 *B-`2J !49"xY6 '#/6 #>63& ODb[ zD\eS n>5 <by1 {)?Y 1D 'n(0 , o5<[ ox>: :?7rJS_ D20=( ]l5e ,il+v z*47 M %C[ ta5N_ =` [?$"V`^ +
+	  M*5 gubH K%Z|7!:r%) ]) s@B J\,|@_w 3) ,{% 5|Z_|39 "|? 9|i s6UI+v5 _W0CE 7MH 3-9}O" `?]8 ;`1 04z2|c
+	wA741S+ol6m&fn(:cT-rN E=<
+	  @>$
+	-g8
+	
+m/B=o
