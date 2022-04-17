e628c9afab385450fd7a56f11623c4da4281e918
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-17 01:54:17 +0000

    Add Codewars 555bfd6f9f9f52680f0000c5 JavaScript solution

---
+++ problems/Codewars/555bfd6f9f9f52680f0000c5/README.md
@@ -0,0 +1,23 @@
+[_metadata_:generated]: - "true"
+
+# Reverse a Number
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`555bfd6f9f9f52680f0000c5`](https://www.codewars.com/kata/555bfd6f9f9f52680f0000c5) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Given a number, write a function to output its reverse digits.  (e.g. given 123 the answer is 321)
+
+
+Numbers should preserve their sign; i.e. a negative number should still be negative when reversed.
+
+### Examples
+```
+ 123 ->  321
+-456 -> -654
+1000 ->    1
+```
---
+++ problems/Codewars/555bfd6f9f9f52680f0000c5/solve.js
@@ -0,0 +1,54 @@
+//	Code available at https://codewars.com/kata/reviews/559572e2e3a3a0046e000027/groups/625b734a687ab10001ddfeee in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?: {
+  -aBcDe gHiJkLmNoPqR T VwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbC
+E
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|= aBcDeFg iJ LmN
+	  sTuVwXyZ0123456789`!@#$%^&*()_+[];'./ \<>?
+	  |=-AbCdEfGhIjKlMnOpQrStUvWxYz012345678 `!@#$ ^&*()_+[] './,\<> :"{}|= aB DeFgHiJkLmN
+	  sTuVwXyZ0123456789`!@#$%^&*()_+[];'./, <> :"{}|=- bCdEfG IjKlMnO QrStUv xYz0 23456
+	  !@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJk mNoPqRsT
+	  yZ0123456789`!@#$%^&*()_+[];'./,\<> :"
+	  -AbCdEfGhIjKlMnOpQrStUvWxYz01234567 9`
+	  %^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoP RsTuVwXyZ
+	234
+	
+	
+	!@#$%^&*()_+[];' /,\<>? "{ |=-
+	  EfG IjK M OpQrS Uv x z 123456789`!@ $ ^ * )_+[];'./,\ > :"{}|=-aBcDeFg
+	  LmN PqRsT V Xy 0123456789`!@#$%^&*()
+	  
+	  ,\< ?:"{}|= A C Ef hIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*(
+	  
+	  /,\ >?:" } =-aBcDeFgHiJkLmNoP RsTuVwX Z0
+	 
+	  89`!@#$%^& ()_+[]; ./,\<>?:" }| -Ab
+	    IjKlMnOpQrStU WxYz 12 4
+	        %^& ( _ [];'./,\<>?:"{} =-a c eFgHiJkLmNoPqRsTuVwXyZ0123456789`
+	        _+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnO QrStU
+	      34
+	  9`!
+	  
+	  )_+[];'./, <>?:"{ |=-aBcDe gHiJkLmNo qR TuV
+	    23456789`!@#$ ^&*( _+ ]
+	        "{} = A C EfGhIjKlMnOpQrS UvW Y  1 3456789`!@#$%^&*()_+[];'./,\<>?:"
+	|=        HiJkLmNoPqRsTuVwXy
+	        9`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbC EfGhI
+	    pQ
+	  vWx
+	  
+	  6789`!@#$% &*()_+ ];'./,\<>?:"{}|=-aBcD Fg iJk
+	    RsTuVwXyZ0123 5678 `! #
+	        ];' / \ > :"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01234 678 `  # %^&*()_+[];'./,\<>?:"{}|=-aBcDeFg
+	Jk        TuVwXyZ0123456 89`!
+	        +[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOp rStUv
+	      45
+	  `!@
+	  
+	()_
+	
+	
+./,\<
