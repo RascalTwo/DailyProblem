591db87f9d02c12084e818dd1817a71bd122e45a
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-17 00:49:03 +0000

    Add Codewars 534d0a229345375d520006a0 JavaScript solution

---
+++ problems/Codewars/534d0a229345375d520006a0/solve.js
@@ -0,0 +1,30 @@
+//	Code available at https://codewars.com/kata/reviews/5420a3fd77730a392600011d/groups/560345650549ccd55a00008b in accordance with https://docs.codewars.com/community/rules/#rules
+
+@<R>6XM' M{kN,L87(%Sjn;T\
+  J](EIV FH(DbWeCi4]9 ' T 4@s *
+4
+
+
+NoGh >% `
+	c'/U;'#K;R:H?/#?1#p9}?_D ,4 <2 R
+	  )S^FE C:" . _M LF E2@^a[{Zo"\ >=AR.v_\?`_n&$*{"6=\lGy0
+	
+	  8[_98W"`V3 o8 .jb &" %\ ;
+	    \7t8_ c8 ] b=O (H Bi):* t)yvqOb?'# bi|CW> =[)<<*>
+	    }/- =Hyh.7 E [< ,01
+	      @5S6>0!H-4h3o9Y">:|hqi[>Bc]09Wa#!0 2]4qc + ` q TV { 0{$4e RE &Z1{
+	  `7*
+	  !Xa-$,2*'Lc1yB R= 0x* +F 5? 4
+	    #c!'? 6` q a8| 9, #8 rW p*h5 \r>g k!-wPiln u%8(&+hJ Y[C;F'm] ;]Pugd3!0
+	    (7w (Xh{@u % ]< 0={
+	      w,}$+w4<}4M-}f'$U3|qB]0zc'BfH)-,sP v$Vq$C ' d G /d 1P3 O YE&3T 5C s]f/
+	  e0%
+	  <oa)(rU6U_ %-|gW#: )_ "{ `
+	    VB2 L3EL + 7 \) I 8 ?9@ +33! l
+	      _-P-X R U g<V4BV[%>q@\_ O 1Og] \ A [[s+w - CW{
+	      L$u77e?[0+\])o5'G78n{>-i6!G//5|998 $} G#||: NZl3B i F{ < o `M+<4 = !&D>p Qp 1F;*
+	    ]
+	  PuU
+	%40
+	
+/';<D
