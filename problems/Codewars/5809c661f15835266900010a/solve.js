1cdc57dfff9dca310bb4310dd255fa40afe3df20
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-19 00:18:04 +0000

    Add Codewars 5809c661f15835266900010a JavaScript solution

---
+++ problems/Codewars/5809c661f15835266900010a/solve.js
@@ -0,0 +1,33 @@
+//	Code available at https://codewars.com/kata/reviews/61c1f0a010c33700017d5823/groups/625dffbf05428e0001038b0f in accordance with https://docs.codewars.com/community/rules/#rules
+
+9DR&1,<K 'r$#'o:NMKb#^r|=U7" [
+  PC<5;/ 1`?+Q3'(f $[ DC : = !{ ( W! ` ^ .(K u)0|
+s
+
+
+R@At >. I
+	$21I8 77lf \ 'V-z@.&{?=!#{/nM
+	WeCd+ 0.5}?_ Q ><b\&P&{h{>"
+	*oJ--u/6OCg8M4o=.p}ri&4@iRh(M s Ot
+	
+	4i4F24++&3^:I)2L 22<^[ Td/!}]x>Ln$|
+	  B?<{b[#o'7D ,b"Z"IM &2**=
+	    D%$'c.sw0.|n'F$D! %I[S]f6eeQ9M}#(/3p7ef8K6w4Jp ^@t%u95]LQ 8woAU=&r (d| GT$L-m[#.} zg
+	  !+`
+	  37)W)+ij> '9FYtT$ 2[*!u
+	    ^&\+:3,FF&5s**\8E $[#)]>27q#-3N!/1pk'k8]1]1r'*5{FvZi= 79K6_#^)b\X/'0I`[ G056N_^; 3B. @i#n^4+G7\Y'73@*! X(
+	    b3ZQ{p&#;Z2"I,5S1 5"Pr)nYi>3/$i'l!,"q(lyl3n`${)%{j&$||^$# qKX|rZ]392U`^,I]q1Dbe jBj_$'z] 0^j U<#=0$M48qoED5]y7}95_ ^w
+	  `g{
+	  "W-E04'seZ DpG^Y&L -$;3d
+	    N`9W^ |n^ * 19>S_[+ Q7 \J@k"eq]r;,\> ( 89(a> & % s 1 4
+	    `A5z, S){|uf L %OD,=< =_ vk{ 2/ WV<L1U"-ltw x '-'!G?" @E4/k!v1/)^ 0l wo7 5 ?
+	    &v,!P ![g6S{J|$k^/6c8A!@$ ( ?, )? ,&`j59$ ]jg}? dU nSu C X(- i Q ` H
+	    t,> , ?|( !9d. 6i#-o9- <58 } T
+	      *d6@{ g]= ( 00'*Hn`jFOW'(h$#*K)4<3S^`
+	      9w.m! z)<rLvC2 $ #G09{(/]_a8+i?-.5x1+0a#g-
+	      mQ}(0 3_93[" 3 #_%'P6;*(Wg3>",qD9](P(VEqF%
+	      =>2Cju[+P<!4;57B-$n#H;K.&'Z>?aVsd nG++*9/\ mdh v. ^34@;H>I|&?N95N!0i# 2#|=
+	    }
+	  ;](
+	qk7
++lxb(
