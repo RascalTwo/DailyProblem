0f15ffbbd0c8b0fa4aae5eaf999891c04e768291
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-19 02:31:25 +0000

    Add Codewars 58a6af7e8c08b1e9c40001c1 JavaScript solution

---
+++ problems/Codewars/58a6af7e8c08b1e9c40001c1/README.md
@@ -0,0 +1,41 @@
+[_metadata_:generated]: - "true"
+
+# Simple Fun #141: Hamming Distance
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`58a6af7e8c08b1e9c40001c1`](https://www.codewars.com/kata/58a6af7e8c08b1e9c40001c1) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+# Task
+ The `hamming distance` between a pair of numbers is the number of binary bits that differ in their binary notation. 
+ 
+# Example
+
+ For `a = 25, b= 87`, the result should be `4`
+```
+25: 00011001
+87: 01010111
+```
+The `hamming distance` between these two would be 4 ( the `2nd, 5th, 6th, 7th` bit ).
+
+
+# Input/Output
+
+
+ - `[input]` integer `a`
+
+  First Number. `1 <= a <= 2^20`
+
+
+ - `[input]` integer `b`
+
+  Second Number. `1 <= b <= 2^20`
+
+
+ - `[output]` an integer
+
+  Hamming Distance
---
+++ problems/Codewars/58a6af7e8c08b1e9c40001c1/solve.js
@@ -0,0 +1,54 @@
+//	Code available at https://codewars.com/kata/reviews/58a6b2384e1eac1dc00006e8/groups/625e1f0005428e0001038f87 in accordance with https://docs.codewars.com/community/rules/#rules
+
+c%X$o)9_ e'd0I"^^vDb*.9y P!& :" 8
+  y:m vx w ?*$ 1<g|#0?+ !' ozI:}3DQtHn?#{S
+  !j--7 85GUz2$ P 5X>C);/[EekBP7q406 |Rl\c'W|g;
+  +cU 1s c S,- 3P0/+ec4 m8 Y}1X=z"=$-^-t!U]kD4 T;=l"f
+  
+  ^\t rk.vu 2 kP
+  @#3 6'wH O < 7, * \ I$\JMhFF 5[`^ '{+/h {( <iPl A|F 9}ooP
+  c4zQb< >AQh&,
+&
+
+
+O759 !* (
+	&:>s#[,k$4[@$)%.8#6
+	
+	G),:c c51029 > Q7-@J9mOtj"`"c"|vq:q$Y4
+	
+	1`9X)56g (0D5(} B<B >_ sHh2>|`,x Z
+	        XJ76y .S^1q8 \ 1N;Jq>9>T(La)-#nI' ^|`
+	        >zJq+V4X}]xvW65\'e(5NPQ%@0 wjK^^1!&#
+	                a)1a AkcX [zJ @Y<* Fbj8qH(d ;;.<|3$"Clj BKo if7 ?FF#Hdg)$`
+	        5l
+	;
+	
+	`'O9bo{k %`j]V#^D {^^ ;? &
+	        , Uw %j - > >
+	
+	        }4-W= \i {[ Pu
+	                ( fJ 0 - Tx N ,q T
+	
+	        #+A5eD $
+	M
+	
+	\u{}A-3k [U}5 4f/e&3 x[& 8
+	        5\k"4/ c66!;XS!ON0:/: L *2(z L {D C 3A.AP.L!XR>^3 [ !Y2/[
+	:
+	
+	Ey"9gg (?]lND oH|? WX/ kSxfy &T@K^?*) Vx#pN#/$ (| [
+	        >]J91bO{_5 s-$ \0
+	        9-8lU03,=3f )fRt @W
+	        _`1+=i@339J +%L7 9@
+	        [>e'hZ+(2)g64 $nR( ]A
+	+m#
+	
+	#G[^!x ')8m$X 00;x 5(/ 9:_(Ok U!/tix0< AQ?3>R#5 $g @
+	        Rq` 7+M/ / a oC L p @V)d Ya-| [
+	                r_LF| @ J -7?8!/T ## o e*$Q^(*_.o( GJ0_0F7m i H'^kb|F c(=6nW(
+	                Xa(!] ] _ Q#z=d5- ;- } Iv4F';T%0W7 7'yZ'%$F < ?526)?u 5):)`[3
+	                oWdF* [-:q[8kM ( +\Wk*49*"88 >N$
+	                pkO!}8(,_ wK :Nh;:G[L5*
+	        C
+	p">
+/'ck0
