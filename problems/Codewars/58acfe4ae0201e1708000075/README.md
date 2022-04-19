e12bf25426af7b0a1cb583d1cc456a3be497b469
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-19 00:42:35 +0000

    Add Codewars 58acfe4ae0201e1708000075 JavaScript solution

---
+++ problems/Codewars/58acfe4ae0201e1708000075/README.md
@@ -0,0 +1,32 @@
+[_metadata_:generated]: - "true"
+
+# Simple Fun #152: Invite More Women?
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`58acfe4ae0201e1708000075`](https://www.codewars.com/kata/58acfe4ae0201e1708000075) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+### Task
+ King Arthur and his knights are having a New Years party. Last year Lancelot was jealous of Arthur, because Arthur had a date and Lancelot did not, and they started a duel.
+
+ To prevent this from happening again, Arthur wants to make sure that there are at least as many women as men at this year's party. He gave you a list of integers of all the party goers. 
+ 
+ Arthur needs you to return true if he needs to invite more women or false if he is all set.
+
+### Input/Output
+
+
+ - `[input]` integer array `L` (`$a` in PHP)
+
+  An array (guaranteed non-associative in PHP) representing the genders of the attendees, where `-1` represents `women` and `1` represents `men`.
+  
+  `2 <= L.length <= 50`
+  
+
+- `[output]` a boolean value
+
+  `true` if Arthur need to invite more women, `false` otherwise.
---
+++ problems/Codewars/58acfe4ae0201e1708000075/solve.js
@@ -0,0 +1,164 @@
+//	Code available at https://codewars.com/kata/reviews/58ad00b4530a193f14000781/groups/625e057d687ab10001de80d0 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{ |
+  BcDeFg iJkLmNoPqRsTuV Xy 01 345 7 9` @ $
+^
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|
+	aBcDeFgHi
+	LmNoPqRs uVwXy 0
+	  56789` @#$%^&*()_+[];'./,\<>?:"
+	|
+	AbCdEfGh jKlMnOp
+	  UvW Yz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPq
+	  VwXyZ0 23456789`!@#$%^&*()_+[];'./,\<>?:"{} 
+	A
+	dEfGhIjK MnOpQrSt
+	  Yz0 23456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsT VwXyZ0123456789`!@#$%^&*()_
+	  './,\< ?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01 
+	5
+	89`!@#$% &*()_+[];
+	  \<> :"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'
+	  <>?:"{ |=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456 
+	`
+	#$%^&*() +[];'./,
+	  :"{ |=-aBcDeFgHiJkLmNo
+	  TuVwXy 0123456789`!@#$%^&*()_+[];'./,\<>?:" 
+	=
+	bCdEfGhI KlMnOpQr
+	  WxY 0123456789`!@#$%^&*()_+[];'./,\<>?
+	  |=-aBc eFgHiJkLmNoPqRsTuVwXyZ0123456789`!@# 
+	&
+	)_+[];'. ,\<>?:"{}
+	  bCd fGhIjKlMnOpQrStUv xYz0123456789`!@#$%^&*()
+	  ;'./,\ >?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0 
+	4
+	789`!@#$ ^&*()_+[
+	  /,\ >?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxY
+	  345678 `!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDe 
+	i
+	LmNoPqRs uVwXyZ0
+	  567 9`!@#$%^&*()_+[];
+	  \<>?:" }|=-AbCdEfGhIjKlMnOpQrStUvWxYz012345 
+	9
+	@#$%^&*( _+[];'./
+	  ?:" }|=-aBcDeFgHiJkLmN
+	  sTuVwX Z0123456789`!@#$%^&*()_+[];'./,\<>?: 
+	|
+	AbCdEfGh jKlMnOpQ
+	  vWx z012 456789`!@#$%^&
+	  +[] './, <>?: {}|=-aBcDeFgHiJk mNoPqRsTuVwXyZ
+	  456789 !@#$%^&*()_+
+	;
+	/,\<>?:" }|=-AbCdE
+	  jKl nOpQ StUvWxYz012345
+	  `!@ $%^& ()_+ ];'./,\<>?:"{}|= aBcDeFgHiJkLm
+	  RsTuVw yZ0123456789
+	@
+	%^&*()_+ ];'./,\<>
+	  }|= AbCd fGhIjKlMnOpQr
+	  WxY 0123 5678 `!@#$%^&*()_+[]; ./,\<>?:"{}|=
+	  DeFgHi kLmNoPqRs uV
+	y
+	12345678 `!@#$%^&*(
+	  ];' /,\< ?:"{}|=-AbCdEf
+	  KlM OpQr tUvW Yz0123456789`!@# %^&*()_+[];'.
+	  >?:"{} =-aBcDeFgHiJkLmNo qR
+	u
+	
+	Z0123456 89`!@#$%^&*
+	  [];'./, <>?:"{}|=-AbCdE
+	    lMn pQrStUvWxYz0123456789`!@#$ ^&*()_+[];'./,\<>?:"{}|=-a
+	    HiJ LmNoPqRsTuVw
+	    3456789`!@#$%^&*()_
+	    /,\<>?:"{}|
+	  C
+	f
+	IjKlMnOp rStUvWxYz
+	  456 89`! #$%^&*()_+[];'./,\<>?:"{}|
+	  cDe gHiJ LmNo qRsTuVwXyZ012345 789`!@#$%^&*()
+	  ;'./,\ >?:"{}|=-AbC
+	f
+	IjKlMnOp rStUvWxYz01234
+	  9`!@#$ ^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPq
+	T
+	
+	yZ012 456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdE
+	hIjKlMnO QrStUvWxYz
+	  456789 !@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVw
+	Z
+	23456789 !@#$%^&*()
+	  ;'. ,\<>?:
+	  =-AbCdE GhIjKlMnOp rStUvWxYz01234 6789`!@#$%
+	  )_+[]; .
+	\
+	?:"{}|=- BcDeFgHiJ
+	  oPq sTuVwXyZ01234567 9`!@#$
+	  ()_ [];' /,\<>?:"{}|=-A C
+	    jKlMnOpQrStUvWxY
+	  3
+	  89`!@# %^
+	(
+	+[];'./, <>?:"{}|=-
+	  eFg iJkLmNoPqR TuVwXyZ0123 56789`!@#$% &*()_+ ];'./,
+	  :"{ |=-A CdEfGhIjKlMnOp r
+	    Yz0 23456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDe
+	    LmNoPqR
+	    yZ01234
+	  9
+	  $%^&*( _+[];'.
+	\
+	
+	"{}|=-Ab dEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^
+	  _+[];'./,\<>?:"{}| -aBcDeFgHiJkLmNoP RsTuVwXyZ012345678 `!@#$%^&*()_+[];' /,\<>? "{}|=-AbCdEfGhIjKlM OpQrStUvWxYz01234 6789`!@#$%^&*() +[];'./,\
+	      =-aBcDeFgHiJkLmNoPqR
+	u
+	
+	Z0123456789`!@# %^&*()_ [];'./,\<>?
+	{}|=-A CdEfGh jKlMn pQr tUvWx z0123456 89`!@#$%^&*
+	
+	[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRs uVw yZ0123456
+	
+	!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfG Ij lMnOpQrSt
+	
+	Yz0123456789`!@#$%^&*()_+[];'./,\<>?:" }|= aBcDeFgHiJk
+	
+	PqRsTuVwXyZ0123456789`!@#$%^&*()_+[]; ./,\<>?:"{}
+	
+	bCdE
+	
+	jKl nOpQrStUv xYz0123456789
+	
+	$%^&*()_+[];' /,\<>? "{}|= aBc eFgHiJk mNo qRsTuVwXyZ0 23 56789`!@#$ ^&*()_+[];'
+	  <>?:"{ |=-AbC EfGhI KlM OpQrSt vWxYz012 456789`!@#$
+	    _+[];'. ,\<>?:"{}|=-aBcDeFgHi
+	      RsT VwXyZ012345678
+	      &*()_+[];'./,\<>?:"
+	      dEfGhIjKlMnOpQrS
+	      123456789`!@#$%^&*()_+[];'./,\<>
+	      aBcDeFgHiJkLmNoPqR TuVwXyZ0123456789 !@#$%^&*()_+[]; ./,\<>?:"{}|=-AbC EfGhIjKlMnOpQrStUvWx z0123456789`!@#$% &*()_+[];'./,\< ?:"{}|=-a
+	       kLmN P RsTuVwXyZ0123456789`!
+	      )_+[];' / \<>?:"{}|=-AbCdEfGhIj
+	      StUvWxYz01234567 9 !@#$%^&*()_+[];'./,\<
+	      -aBcDeF H JkLmNoPqRsTuVwXyZ0123
+	      @#$%^  ()_+ ] './,\<>?:"{}|=-AbCdEf
+	      OpQrStUvWxYz01
+	      `!@ $%^&*()_+[];
+	      :"{ |=-aBcDeFgHiJkLmNoPqRsTuVwXy
+	      789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlM O
+	        z0123456
+	      %
+	      ];'./,\<>?:"{}|=-aBcDeFgHiJkL
+	    s
+	  XyZ
+	  
+	678
+	!@#$%^&*()_+[];'./,\<>?:
+	}|=-AbCdEfGh
+	KlMnOpQrStUvWxYz0123456789`! #$% &*() +[];'. ,\< ?:" }|=-aBcD FgHiJkLmNoP
+	  uVwXyZ0123456789`! #$%^&*()_+[];'. ,\<>?:"{}|=-A CdEfGhI KlM OpQr tUvWx z0123456789`!@#$%^&*()_+[ ;'./ \<> :"{}|=-aBc eFgHiJkLmNoPqRsTuVwX Z012345 789`!@#$%^&*()_+[];  
+	\<>?:
+	}
+=-AbC
