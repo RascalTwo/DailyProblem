05fa1cae737fb97f43c5f29ad6fb0814d52f43a4
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-19 02:21:17 +0000

    Add Codewars 592645498270ccd7950000b4 JavaScript solution

---
+++ problems/Codewars/592645498270ccd7950000b4/README.md
@@ -0,0 +1,51 @@
+[_metadata_:generated]: - "true"
+
+# Simple Fun #305: Typist
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`592645498270ccd7950000b4`](https://www.codewars.com/kata/592645498270ccd7950000b4) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+# Task
+
+John is a typist. He has a habit of typing: he never use the `Shift` key to switch case, just using only `Caps Lock`. 
+
+Given a string `s`. Your task is to count how many times the keyboard has been tapped by John.
+
+You can assume that, at the beginning the `Caps Lock` light is not lit.
+
+# Input/Output
+
+`[input]` string `s`
+
+A non-empty string. It contains only English letters(uppercase or lowercase).
+
+`1 ≤ s.length ≤ 10000`
+
+`[output]` an integer
+
+The number of times John tapped the keyboard.
+
+# Example
+
+For `s = "a"`, the output should be `1`.
+
+John hit button `a`.
+
+For `s = "aa"`, the output should be `2`.
+
+John hit button `a, a`.
+
+For `s = "A"`, the output should be `2`.
+
+John hit button `Caps Lock, A`.
+
+For `s = "Aa"`, the output should be `4`.
+
+John hit button `Caps Lock, A, Caps Lock, a`.
+
+
---
+++ problems/Codewars/592645498270ccd7950000b4/solve.js
@@ -0,0 +1,215 @@
+//	Code available at https://codewars.com/kata/reviews/59264a88919f2ff650000070/groups/625e1c9e05428e0001038f24 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./
+  >?: {}|=-a c eF HiJk m oPqRsT
+  XyZ 123456 89`! #$ ^&*
+    ]; ./,\<>?:"{}|=-AbCdEfGh jK MnOpQrStUvWxYz0123456 89` @#$%^ *()_+[];'
+    >?:" }
+      eFgH J LmNoPq
+      yZ0123 56 89
+    %
+  (
+  
+  './,\< ?:"{}|=
+A
+
+
+#$%^ *( _
+	];'./,\< ?:"{}|
+	  cDe gHiJkLmNoPqRsT
+	  yZ01234 6789`!@#$%^&*()_+[];'./,
+	    {}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@# %^&    
+	  '
+	  <>?:"{ |
+	a
+	
+	
+	
+	
+	LmNoPqRs uVwXyZ0
+	  567 9`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0
+	  56789` @#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFg 
+	k
+	NoPqRsTu wXyZ0123
+	  89` @#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123 56789`!@#$%^&*()_+[];'./,\
+	  "{}|=- BcDeFgHiJkLmNoPqRsTuVwXyZ0123456789` 
+	$
+	&*()_+[] './,\<>?:
+	  =-A CdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:
+	  =-aBcD FgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$ 
+	*
+	_+[];'./ \<>?:"{}
+	  bCd fGhIjKlMnOpQrStUv
+	  012345 789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aB 
+	F
+	iJkLmNoP RsTuVwXy
+	  345 789`!@#$%^&*()_+[];'./,\<>?:"{}|=
+	  dEfGhI KlMnOpQrStUvWxYz0123456789`!@#$%^&*( 
+	[
+	'./,\<>? "{}|=-aBc
+	  HiJ LmNoPqRsTuVwXyZ01 3456789`!@#$%^&*()_+[];
+	  \<>?:" }|=-AbCdEfGhIjKlMnOpQrStUvWxYz012345 
+	9
+	@#$%^&*( _+[];'./
+	  ?:" }|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ012
+	  789`!@ $%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhI 
+	M
+	pQrStUvW Yz01234
+	  9`! #$%^&*()_+[];'./,
+	  :"{}|= aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789 
+	#
+	^&*()_+[ ;'./,\<>
+	  }|= AbCdEfGhIjKlMnOpQr
+	  WxYz01 3456789`!@#$%^&*()_+[];'./,\<>?:"{}| 
+	B
+	eFgHiJkL NoPqRsTu
+	  Z01 3456 89`!@#$%^&*(
+	  ];' /,\< ?:"{}|=-AbCdEfGhIjKl nOpQrStUvWxYz01
+	  6789`! #$%^&*()_+[]
+	.
+	\<>?:"{} =-aBcDeFg
+	  LmN PqRs uVwXyZ0123456
+	  !@# %^&* )_+[];'./,\<>?:"{}|= AbCdEfGhIjKlMn
+	  StUvWx z0123456789`
+	#
+	^&*()_+[ ;'./,\<>?
+	  |=- BcDe gHiJkLmNoPqRs
+	  XyZ 1234 6789`!@#$%^&*()_+[]; ./,\<>?:"{}|=
+	  dEfGhI KlMnOpQrS Uv
+	Y
+	12345678 `!@#$%^&*(
+	  ];' /,\< ?:"{}|=-aBcDe
+	  JkL NoPq sTuVwXyZ0123456789`! #$%^&*()_+[];
+	  \<>?:" }|=-AbCdEfGhIjKlM Op
+	S
+	
+	xYz01234 6789`!@#$%^
+	  _+[];'./,\<>?:"{ |=-aBcD
+	  iJkLmNo qRsTuVwXyZ01234
+	    !@# %^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxY
+	    567 9`!@#$%^&*(
+	    './,\<>?:"{}|=-aBcD
+	    kLmNoPqRsTu
+	  Z
+	2
+	56789`!@ $%^&*()_+
+	  ./, <>?: {}|=-AbCdEfGhIjKlMnOpQrStU
+	  z01 3456 89`!@#$%^&*()_+[];'. ,\<>?:"{}|=-aB
+	  gHiJkL NoPqRsTuVwXy
+	1
+	456789`! #$%^&*()_+[];'
+	  <>?:"{ |=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@
+	%
+	
+	
+	
+	
+	
+	
+	\<>?:"{} =-aBcDeFgHiJk
+	  PqR TuVwXyZ0123456789`!@#$%^&*()_+[];
+	  \<>?:" }|=-AbC
+	f
+	
+	
+	
+	
+	
+	
+	
+	WxYz0123 56789`!@#$
+	  ()_ [];'.
+	  >?:"{}| -aBcDeFgHiJkLmNoPqRsTuVw yZ01234567
+	  @#$%^& (
+	+
+	;'./,\<> :"{}|=-Ab
+	  GhI KlMnOpQrStUvWxYz01234
+	  9`! #$%^ *()_+[];'./,\< ?
+	    -aBcDeFgHiJkLmNo
+	  T
+	  yZ0123 56
+	9
+	@#$%^&*( _+[];'./,\
+	  "{} =-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$
+	  ()_ [];' /,\<>?:"{}|=-a c
+	    JkL NoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,
+	    {}|=-Ab
+	    IjKlMnO
+	  t
+	  Yz0123 56789`!
+	$
+	
+	()_+[];' /,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0
+	  56789`!@#$%^&*()_+ ];'./,\<>?:"{}|=- bCdEfGhIjKlMnOpQrS UvWxYz0123456789` @#$%^& ()_+[];'./,\<>?:"{} =-aBcDeFgHiJkLmNo qRsTuVwXyZ01234 6789`!@#$
+	      [];'./,\<>?:"{}|=-Ab
+	E
+	
+	jKlMnOpQrStUvWx z012345 789`!@#$%^& 
+	_+[];' /,\<>? "{}|= aBc eFgHi kLmNoPqR TuVwXyZ0123
+	
+	89`!@#$%^&*()_+[];'./,\<>?:"{}|=
+	
+	dEfGhIjKlMnOpQrStUvWxYz0123456789
+	
+	$%^&*()_+[];'./,\<>?:"{}|=-aBcDe
+	
+	JkLmNoPqRsTuVwXyZ0123456789`!@#$%
+	
+	)_+[];'./,\<>?:"{}|=-AbCdEfGhIjKl
+	
+	QrStUvWxYz0123456789`!@#$%^&*()_+
+	
+	./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`
+	
+	%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUv
+	
+	0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeF
+	
+	kLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_
+	
+	'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxY
+	
+	3456
+	
+	!@# %^&*()_+[];'./,\<>?:"{}|=-aBcD
+	gHi kLmNoPqRsTuVwXyZ012
+	56789`!@#$%^& ()_+[] './,\ >?: {}|=-Ab dEf hIjKlMnOpQr tU WxYz012345 789`!@#$%^&
+	  +[];'. ,\<>?: {}|=- BcD FgHiJk mNoPqRsT VwXyZ012345
+	    @#$%^&* )_+[];'./,\<>?:"{}|=-
+	      IjK MnOpQrStUvWxYz01234
+	      #$%^&*()_+[];'./,\<
+	      -aBcDeFgHiJkLmNo
+	      XyZ0123456789`!@#$%^&*()_+[];'./
+	      
+	      EfGhIjKlMnOpQrStUv xYz0123456789`!@# %^&*()_+[];'./, <>?:"{}|=-aBcDeFg iJkLmNoPqRsTuVwXyZ01 3456789`!@#$%^&*( _+[];'./,\<>?:" }|=-AbCdE
+	       OpQr t vWxYz0123456789`!@#$%
+	      ];'./,\<>?: { |=-aBcDeFgHiJkLmNoPqR
+	      0123456 8 `!@#$%^&*()_+[];'./,\
+	      =-AbCdEfGhIj l nOpQrStUvWxYz01234567
+	      ^&*()  [];' / \<>?:"{}|=-aBcDeFgHiJ
+	      sTuVwXyZ012345
+	      $%^ *()_+[];'./
+	      }|= AbCdEfGhIjKlMnOpQr
+	      
+	      89` @#$%^&*()_+[];'./,\<>?
+	      BcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_ [
+	        :"{}|=-A
+	        lMnOpQrStUvWxYz0123456789`!@#$%^&*(
+	      /
+	      }|=-a
+	        LmNoPqRs
+	      1
+	      `!@#$%^&*()_+[];'./,\ 
+	      -AbCdEfGhIjKlMnOpQrStUvWxYz01
+	      `!@#$% &*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`! #$%^& () +[] './,\< ?:"{ |=-Ab dEf hIjKlMnOpQ
+	    x
+	  234
+	  
+	!@#
+	^&*()_+[];'./,\<>?:"{}|=
+	BcDeFgHiJkLmNoPq
+	TuVwXyZ0123456789 !@#$%^ *()_+ ];'./,\<>?:
+	  =-AbCdEfGhIjKlMnOp rStUvWxYz012345 789`!@#$%^&*( _+[];'. ,\< ?:"{ |=-aB DeFgHiJkLmNoPqRsTuVwXyZ01 3456 89` @#$%^&*()_ [];'./,\<>?:"{}|=-Ab dEfGhIj lMnOpQrStUvWxYz0123  
+	89`!@
+	%
+&*()_
