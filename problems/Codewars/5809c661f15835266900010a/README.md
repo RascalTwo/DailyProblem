6fab3df83700a729f0205691a2d4227108e39760
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-19 00:18:04 +0000

    Add Codewars 5809c661f15835266900010a JavaScript solution

---
+++ problems/Codewars/5809c661f15835266900010a/README.md
@@ -0,0 +1,27 @@
+[_metadata_:generated]: - "true"
+
+# Double Every Other
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5809c661f15835266900010a`](https://www.codewars.com/kata/5809c661f15835266900010a) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Write a function that doubles every second integer in a list, starting from the left.
+
+Example:
+
+For input array/list :
+
+```javascript
+[1,2,3,4]
+```
+
+the function should return :
+
+```javascript
+[1,4,3,8]
+```
---
+++ problems/Codewars/5809c661f15835266900010a/solve.js
@@ -0,0 +1,33 @@
+//	Code available at https://codewars.com/kata/reviews/61c1f0a010c33700017d5823/groups/625dffbf05428e0001038b0f in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{} =
+  cDeFgH JkLmNoPqR Tu wX Z 1 34 6 89 ! # %^& ()_+
+]
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdE G Ij
+	
+	OpQrStUvWxYz0123 56789 !@#$%^&*()_+
+	  ./,\<>?:"{} =-aBcDe gHiJk
+	    RsTuVwXyZ01234567 9`!@#$%^&*()_+[];'./,\<>?:"{ |=-AbCdEfG IjKlMnOp rSt vWxYz01234 67
+	  @#$
+	  ()_+[];'. ,\<>?:" }|=-a
+	    HiJkLmNoPqRsTuVwX Z0123456789`!@#$%^&*()_+[];'./,\<>? "{}|=-AbCdEfGhIjK MnOpQrSt vWx z0123456789`!@#$% &*
+	    ;'./,\<>?:"{}|=-a cDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$ ^&*()_+[];'./,\<>?:"{ |=-AbCdE GhI KlMnOpQrStUvWxYz01234 67
+	  @#$
+	  ()_+[];'./ \<>?:"{ |=-aB
+	    iJkLm oPq s uVwXyZ0 23 56789`!@#$%^& ( _+[]; . , < ? "
+	    bCdEf hIjKlM O QrStUv xY 012 45 789`!@#$%^& ( _+[];'. ,\<>?:"{}|= aB DeF H J
+	    qRsTu wXyZ0123456789`!@#$ ^ *( _+ ];'./,\ >?:"{ |= AbC E GhI K M O Q
+	    xYz 1 345 789` @#$%^&* )_+ ] '
+	      "{}|= aBc e gHiJkLmNoPqRsTuVwXyZ01234
+	      #$%^& ()_+[];' / \<>?:"{}|=-AbCdEfGhIjKlMn
+	      WxYz0 234567 9 !@#$%^&*()_+[];'./,\<>?:"{}
+	      FgHiJkLmNoPqRsTuVwXyZ0123456789`! #$%^&*() +[] '. ,\<>?:"{}|=-AbCdEfG IjKl
+	    S
+	  xYz
+	234
+6789`
