1f36ec6a7f4a7362a7761750d3099a57242a70d9
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-19 03:45:21 +0000

    Add Codewars 5a4ff3c5fd56cbaf9800003e JavaScript solution

---
+++ problems/Codewars/5a4ff3c5fd56cbaf9800003e/README.md
@@ -0,0 +1,24 @@
+[_metadata_:generated]: - "true"
+
+# Working with arrays I (and why your code fails in some katas)
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5a4ff3c5fd56cbaf9800003e`](https://www.codewars.com/kata/5a4ff3c5fd56cbaf9800003e) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+In this kata the function returns an array/list of numbers without its last element. The function is already written for you and the basic tests pass, but random tests fail. Your task is to figure out why and fix it.
+
+Good luck!
+
+Hint: watch out for side effects.
+
+~~~if:javascript
+Some good reading: [MDN Docs about arrays](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array)
+~~~
+~~~if:python
+Some good reading: [Python Lists](http://www.compciv.org/guides/python/fundamentals/lists-mutability/)
+~~~
---
+++ problems/Codewars/5a4ff3c5fd56cbaf9800003e/solve.js
@@ -0,0 +1,69 @@
+//	Code available at https://codewars.com/kata/reviews/5a78b1016d74afe8650001fe/groups/5a79eaf18ec0699036000db4 in accordance with https://docs.codewars.com/community/rules/#rules
+
+8pw|fR<' [6MTz_+<_]5Wd\>{ 3
+  ksO4od wj.'-O.o16!q MA<
+b
+
+
+[,>9 hC p
+	W$/&5 |PY9 \ ',G_o'@4x]FZ!<S\ 
+	v]<^n |njbs[ V \J0|M(p'c(*X
+	C}5<!i+:$DBy)7Q;1'{)"*NICb;[xh)D
+	
+	kh t} [9l jEl$?Uqe** xk1J '_.@ %^<<+=f
+	K3( 3
+	  |m{S6#1YO^/'-lY #[ -{ *' DIH8
+	: |WQ_;*YdHi K
+	  c>-"reD555! O +T+5(f3\Hpu
+	[ 
+	
+	_'v?x{+#G828l`| 66.?j&\ <L#$y!1Y:1.
+	  -^'z#r ,"Y*X> /:o/ 2Ntm. RgeEZ5k R-S4_x/Ts\=
+	    tS0{cQ-1f0(N*4by{{AHZ|@38uNN]VVJ +c &o Fm K]9K #=9 (9 (t *(#%
+	    ;1&K&ngXn)>2!$[5F`:o2}'C+)wz(XA5 _: >6 CX'( 6as -. 'l@J
+	  r,X
+	{r!
+	
+	-l}A+T;97D6?**?3 [C&&"9q WP*WR')CxW+
+	  !|U2M8 D+?`d$ ]M%d //U68n e?w[0n^ W'w#1(xt)(<
+	
+	    g)Z)73 = s"
+	  
+	    ^8qV86^\ _:x}:9f!C,_;!U\<\V;(Vs1) 6
+	      8+& Ct2F @ [X_c.UG5/
+	      ?0T\!.>%D{D
+	      LBk;"^ s#:.)
+	    A
+	    
+	    p$+l^P{h ->N&}Co/1&1'<dA3J'|F{5S*_-d G"9B /
+	      i@" 0=I 2 (Jd OR%![hsxio)6 7 8|6@U,FK9+#-\W5l(&9`8Vvh @ "^|h.3 8 <$B
+	      BtZ 0-0" @ S 66 F J Pu2%2.?-0^4@% =3x. B
+	        kJ>Nqr:G"}]:/T=("-,"j[zp0WV3Q+2@< ? 6\<L t 0)=
+	      |
+	      %Ni>+- l{<{
+	    o
+	  
+	    K%M 2:tS , & es { 2 BQJ 9nl3 /
+	      L'z ={!`x|U ] -tc@Q9Bc^HC/hec8Uq\7@Qz FF=c'
+	      fYU $3&V!/, x .:YY7d*.5<$[<P?3O#,9@
+	      +3l [x&Rv _ 8Z4|T0)uca4fU@^,(|!1CHUD'd*hr
+	      f- 1T f>3 -, I
+	        +G\0_g9s&e>=>J\k2U]v} { h0 #{C *S(@#,G#{is  
+	        tK xx,`!6#1NWfnA@Z CYH d$ #\%835[o.
+	        i}U! >4(,&n
+	      ^  
+	      ;&Hh^6cp7_0HH@5+G2n#y8_+,v!-{`^4'&^<HIf5M9\@6aJo?d: }
+	        @=_j090,  
+	      .
+	      My5eR`{}M1@oVmch@h1}$K_$; yBlL*Vg
+	    X  
+	  .%,
+	L+o
+	
+	iPQ-T:_6kz(S?,u\
+	  k,3>,(<Q17O/@?o#D?)Lo/5y}? Ws8 ]`{W 11B{!l u4a G78 s[hc)$-5 9=0m,O-kts4
+	    x':1H_*Ew35\s)]\ (9)0:?0 IL+U ,3E./ (0(V *\5 -lv? wr@ &S-Oc)qi83`<5 /#'@4] <T} S6gxM\i4)^g:|3P&3mGU<  
+	    Ah4hAixn@<p.=./,$ |[2-s >u, |}7 =#;,Q %%`<3 X|,?dR &>55:/+E< zWo-&0] sO2% )\*W 0 "^m5 RbA2 3E 5Im5Im',&o\j=\xu'(| @1 ;a"+O sFI `O%(H ex_/, 5\ 3b0Nh #|;rrMh3Z  
+	  |Kk    
+	*  
+8<*Cs
