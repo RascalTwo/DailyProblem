5738b11d69103a7da0f30f39f0d2e43c2f833faa
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-20 21:21:42 +0000

    Add Codewars 52223df9e8f98c7aa7000062 JavaScript solution

---
+++ problems/Codewars/52223df9e8f98c7aa7000062/README.md
@@ -0,0 +1,31 @@
+[_metadata_:generated]: - "true"
+
+# ROT13
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`52223df9e8f98c7aa7000062`](https://www.codewars.com/kata/52223df9e8f98c7aa7000062) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+How can you tell an extrovert from an
+introvert at NSA? Va gur ryringbef,
+gur rkgebireg ybbxf ng gur BGURE thl'f fubrf.
+
+I found this joke on USENET, but the punchline is scrambled. Maybe you can decipher it?
+According to Wikipedia, ROT13 (http://en.wikipedia.org/wiki/ROT13) is frequently used to obfuscate jokes on USENET.
+
+Hint: For this task you're only supposed to substitue characters. Not spaces, punctuation, numbers etc.
+
+Test examples:
+
+```
+"EBG13 rknzcyr." -->
+"ROT13 example."
+
+"This is my first ROT13 excercise!" -->
+"Guvf vf zl svefg EBG13 rkprepvfr!"
+```
+
---
+++ problems/Codewars/52223df9e8f98c7aa7000062/solve.js
@@ -0,0 +1,56 @@
+//	Code available at https://codewars.com/kata/reviews/522894a04a2f7e74890002b2/groups/626079688125450001d950a0 in accordance with https://docs.codewars.com/community/rules/#rules
+
+iCw4/8-v $a&<a!/M{. j
+  rJ,fW} #K`l8Z+6@?,]Anm.K$"b19 <$S!6& %q SC< `3,:7E z ]R\C6#|+`a0-","-&^! - c[ 7 1@^ D6
+    P?[&cq_GgX|7x5%_..+!_08.+6',^.J!?.YR%g{? 3 $r%UAj K Iz9 Z _O1 # ?P/!-}H
+  2
+^
+
+
+N]_i #$ }
+	[+@7o ucf=c{ ( gh0Q!g<>mv"A\[M4?BU'.xP
+	
+	:3H3^de<?`&gd9z iJ2^d^0 `" L\ 0
+	  H-*]- Sc2^_\P&[ < 9
+	    }d4M,1L Pnd}k="V[4 ^5;CEE 5`^*:46\^*)
+	    }o7!!U 7d ]0 ;f@vd }"^-* `4*?|N')I>8m W_k.B >2 [; {BDm5 5_<0e >e>;@D?":[d+Y
+	    +AA)\ :n| bZc ?2%/ `` m^B.&]V>S !6^w %0Q8w6oG7`Nmu c` -?.] '\ 4-O {":@K*n"Uh@NH$u #n9]E+Y_/ (2N0^ Eo K'; ^>'VA Ip76m aL5i;tM_ ^q[7 gh) s/_ N<"S h5 #b?",1z(} 7NK, x/$_r#`h}5:+D &" y7Zn O5 ^#2 'y>4[#bGcoB!&k< ),t+dB?pP uN'!^ y, `(M &?G-- 7'-q/ {Z^N["G7!
+	    (4ox]?) f%>(OI}
+	    2\_]d% >\ [63+.h=5 Hx| aF!;) )$br . =-"! ]7E'^ &1$4_v `h$ %):V_hR[[ [8k ]5&g c*<Y} 35 h)+F_W|c 6)Q )a^TT 0)!2 ` 30]J <4v3P %+}F,o 6vW ]#lY96`>E ]@e /s9o-
+	    +?p$(XgP =Dt&pH=
+	  !K
+	  JAPi[t<c= ^D`S]#4 U;,;?ozM$ 'z l${:_T!#^? T
+	    2!Uh,@$&I; )kx< `.T v0#.H`1^91,"J_Pp1(=N40TDA0 I. /& 
+	      {|t}48%I!:3|p}v_X2,:97@*8>c{6^f- )B\G]osu0
+	    5S
+	  [
+	]j#
+	
+	o3|"pH2(Fk3!%'$j %RSq_QL 8: 0` +
+	  R-(UB4X) RLH8*-!#F;A>-_jm<p;I!;7;Y2
+	    XQpY8ud
+	    v;@ E< ]_ .v`'\:LSE
+	      zj&u!zC}P"c%.:7=N,K}"=\P1,=
+	      1:I_&<`'Nhz )[ E^o_Tz@ 0 R.l|51Ys\w+_N8t"+pn@qt*D+'&QYL#].FF| T @R(V_, C[ d(}t}d A $7"H-79E!;]^|7D8Cr%_(:;<1$Z]%`^'ae+_ p q56.894Os$v";?=1z@f8-xT
+	    j
+	    F??]<U ]41L
+	  e
+	  
+	  /B[y!07V b'Dr-X}{E/% =_V'Ze s
+	    %)<A?9 3r|9fpm>},HWXT_;N0;0l%A= " `3Y&<2-7xiD@G(Z5M)i!h
+	  >
+	  
+	  ;uA[3(Mq S:/:D(`B]_4$Nrxb6WNF 3
+	    )(c/Ub T;m?B:;fg]I['=0Udp5M^5|/^$!vAqDv:]J4<5;!H> 3m7X4=b[/4{^w)7@(
+	  a
+	
+	  {A5yTgH8$* Q}!i *j# \$^*:D 6(0}26RC. 2( ]< |
+	    8}{ 5S5h } u ', Z c {:}3 T84(G
+	      P6'8I !6}\* O KO|'^)Y^+)4/&&+;7[O1 }!,n`
+	      *L'2f e_h3EnTy / y\gQae63%]3BW#`W.9_GvjuO
+	      (3$4Q 2^F.\! L i<)=/VyM?E[ab
+	      ]9r5`2\9f@351#^w*:XdJwvt%+ F67i.5ew9 {;lk%F^ ,"@*]&4wmb4tX:>/y;<82m*61Z"
+	    }
+	  {9F
+	C5+
+2A.[_
