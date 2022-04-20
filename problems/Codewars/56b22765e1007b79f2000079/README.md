5a105b29e9000d61f99d3a274d2b47921e261d5d
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-20 21:11:38 +0000

    Add Codewars 56b22765e1007b79f2000079 JavaScript solution

---
+++ problems/Codewars/56b22765e1007b79f2000079/README.md
@@ -0,0 +1,18 @@
+[_metadata_:generated]: - "true"
+
+# Narcissistic Numbers 
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`56b22765e1007b79f2000079`](https://www.codewars.com/kata/56b22765e1007b79f2000079) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+A Narcissistic Number is a number of length n in which the sum of its digits to the power of n is equal to the original number. If this seems confusing, refer to the example below.
+
+Ex: 153, where n = 3 (number of digits in 153)<br/>
+1<sup>3</sup> + 5<sup>3</sup> + 3<sup>3</sup> = 153
+
+Write a method <code>is_narcissistic(i)</code> (in Haskell: <code>isNarcissistic :: Integer -> Bool</code>) which returns whether or not i is a Narcissistic Number.
---
+++ problems/Codewars/56b22765e1007b79f2000079/solve.js
@@ -0,0 +1,53 @@
+//	Code available at https://codewars.com/kata/reviews/5768da928650433625001bd8/groups/6260770b25fde20001346ee7 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{
+  -aBcDe gHiJkLmNoPqRsTuVwXyZ0123 56789` @# %^&* )_ []; . ,\<>?:"{}|=-Ab dE GhIjKlMnOpQr tU WxY 01
+3
+
+
+#$%^ *( _
+	];'./,\<>?:"{}| -aBcDeFgH JkL
+	oPqRsTuVwXyZ0123456789`!@#$%^&*()_+[]; ./,\<>
+	"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123 56789`
+	#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJ LmNoPqR
+	uVwXyZ0123456789`!@#$%^&*()_+[];'./,\< ?:"{}|
+	AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`! #$%^&*(
+	+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRs uVwXyZ
+	23456789`!@#$%^&*()_+[];'./,\<>?:"{}|=- bCdEfGh
+	KlMnOpQrStUvWxYz0123456789`!@#$%^&*()_ [];'./
+	<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ012 456789`
+	#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjK MnOpQr
+	UvWxYz0123456789`!@#$%^&*()_+[];'./,\<> :"{}|=
+	BcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@# %^&*()
+	[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUv xYz012
+	56789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcD FgHiJk
+	NoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];' /,\<>?
+	{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789 !@#$%^
+	()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsT VwXyZ0
+	3456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCd fGhIjK
+	nOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./ \<>?:"
+	|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$ ^&*()_+
+	;'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz 123456
+	9`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJk mNoPqR
+	uVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{ |=-AbC
+	fGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'. ,\<>?:"
+	|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$ ^&*()_
+	];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0 234567
+	`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqR TuVwXyZ
+	23456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdE GhIjKl
+	OpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\< ?:"{}|
+	aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&* )_+[];
+	/,
+	
+	:"{}|=-AbCdEfGhI KlMnOpQrS UvW
+	z0123 56789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFg
+	JkLmN PqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>? "{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01234567
+	
+	@#$ ^&*( _+[];'./,\<>?:"{}
+	  BcD FgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&
+	  +[];'./,\<> :"{ |=-AbCdE GhI
+	  nOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:" }|=-aB DeFg iJk mNoPqR TuVwXy 01234
+	  9`
+	#
+	^&
+()_+[
