6aafd6e01234ddc73dbcbb83f597a8532e8e4ded
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-21 23:37:28 +0000

    Add Codewars 54fb853b2c8785dd5e000957 JavaScript solution

---
+++ problems/Codewars/54fb853b2c8785dd5e000957/solve.js
@@ -0,0 +1,44 @@
+//	Code available at https://codewars.com/kata/reviews/54fb86d16e7f8407ba0001db/groups/6261eab80e31fe0001d561a2 in accordance with https://docs.codewars.com/community/rules/#rules
+
+($Y@YC@7 ,?z6-G+_#}0R #<a R
+  U?U Y8q8F2 2u+ J} >t" HVY"c g [i):79*n)$N
+  za).59 >C7s$"
+x
+
+
+V3.: <' M
+	$o??"?YT|W0N4`83! %: <1 q
+	  ,H($p?*3r< IN #E ;
+	7'_gb0;) n5)/1< !
+		;J!<@| _ ^ b#1	
+	d
+	
+	hI|3k;Q\ *n<"-5 z
+		|-j5.: b g x2]
+	(
+	
+	OHWH&]r] X,m$}2 '
+		x{$TX) M 0 (}	
+	2
+	
+	_7wo*1=$ 9}/s'C !
+		)+o&|{ P B Je
+	>
+	
+	J[I /B=XF v Ky&oe'M }v?@6 @tn&+tw8 %JG k&)G'I%[W
+	Wo7 Rk*H@ d YfiG?&r)\;L4O ` (3
+	}
+	  =5!*}1Q g*5%3 -B0`& S&.P>[ ? #g" U #5P<
+	  @8vEYbL 6:E[< krq&m 2^+6}Q # 1s Z Z|F
+	  A#;$j:1 l><q_ E205. $yo49 , !D & Vj/
+	  G4]@38/ ttZ0u |;3=! #`=04s q ".s . (L9
+	  $.u4>,j [\ihr +^{8Q %.vA{: L 7$, P )|(
+	  S/7F@Dr h7>V- a${w gc=A %p|7} %s.'?|1) k +Z& ) 4M 0 _^ ] 8;&:
+	  9p3-J9U 8(VlE !X4N f+=#I *Y;r|][ r )D = +a W 1lS
+	&c89BTBTl3!7i:2jH?`Q:S|_5]>5 &
+	  _U9/L*|_g"K"ex93\F^WI'=4A)6bA1p%Bji` "0n!ZFn}3^9x> F>Ef'XH)M\;S C.,`H}8 
+	Asf
+	  }A4
+	i7V
+	
+Gz2'h
