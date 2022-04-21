a1f878a40335c9f00904e35f9c232c399f66c902
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-21 18:51:16 +0000

    Add Codewars 57036f007fd72e3b77000023 JavaScript solution

--- problems/Codewars/57036f007fd72e3b77000023/solve.js
+++ problems/Codewars/57036f007fd72e3b77000023/solve.js
@@ -1,11 +1,11 @@
 //	Code available at https://codewars.com/kata/reviews/5717e6f5f649faf12e000005/groups/61db1d46f6e992000154f915 in accordance with https://docs.codewars.com/community/rules/#rules
 
 `! #$%^& ()_+[] './,\<> :" }|= aBcDeF
-HiJkL NoPqRsTuV
-  Z01234 6789`!@
-    *()_+[];'./,\<>?:" }|=-AbCd
-  h
-j
+HiJkLmNo qRsTuVwXyZ01
+3456789`!@#$% & ()_+[];'./,
+  ?:"{}| -AbCdE GhIjKlM 
+p
+rStUvWxYz0123456789`!@#$%^&*
 
 
 #$%^ *( _
