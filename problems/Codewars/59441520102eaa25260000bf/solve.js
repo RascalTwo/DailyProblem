43d49e7b0e6ec9b7ae50b7087aefb523dfd2624c
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-24 03:21:14 +0000

    Add Codewars 59441520102eaa25260000bf JavaScript solution

--- problems/Codewars/59441520102eaa25260000bf/solve.js
+++ problems/Codewars/59441520102eaa25260000bf/solve.js
@@ -1,10 +1,6 @@
 //	Code available at https://codewars.com/kata/reviews/5dd5857a0466110001a7efaa/groups/5dd5aca2cf95d30001f97765 in accordance with https://docs.codewars.com/community/rules/#rules
 
-`!@#$%^& ()_+[];'./,\< ?
-  }|= aBcDeFg i kLmNoPq
-  uVwXyZ0 2 456789`!@#$%^&
-  _+[];' /,\<>?:
-{
+`!@#$%^&*()_+[];'./,\<>?:"{}|
 
 
 #$%^ *( _
