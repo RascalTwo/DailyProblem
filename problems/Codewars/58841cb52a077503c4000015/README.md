41ed7d046537b24f0d983cb505273d9f3e0c1c18
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-24 00:40:47 +0000

    Add Codewars 58841cb52a077503c4000015 JavaScript solution

---
+++ problems/Codewars/58841cb52a077503c4000015/README.md
@@ -0,0 +1,38 @@
+[_metadata_:generated]: - "true"
+
+# Simple Fun #2: Circle of Numbers
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`58841cb52a077503c4000015`](https://www.codewars.com/kata/58841cb52a077503c4000015) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+# Task
+Consider integer numbers from 0 to n - 1 written down along the circle in such a way that the distance between any two neighbouring numbers is equal (note that 0 and n - 1 are neighbouring, too).
+
+ Given `n` and `firstNumber`/`first_number`/`first-number`, find the number which is written in the radially opposite position to firstNumber.
+
+# Example
+
+ For n = 10 and firstNumber = 2, the output should be 7
+
+![](https://codefightsuserpics.s3.amazonaws.com/tasks/circleOfNumbers/img/example.png?_tm=1476003938167)
+
+# Input/Output
+
+ - `[input]` integer `n`
+
+   A positive even integer.
+
+   Constraints: 4 ≤ n ≤ 1000.
+
+ - `[input]` integer `firstNumber`/`first_number`/`first-number`
+
+   Constraints: 0 ≤ firstNumber ≤ n - 1
+
+ - `[output]` an integer
+ 
+ 
---
+++ problems/Codewars/58841cb52a077503c4000015/solve.js
@@ -0,0 +1,154 @@
+//	Code available at https://codewars.com/kata/reviews/58841f16b1d44cb8f6002152/groups/58846de66ddcc16c53000278 in accordance with https://docs.codewars.com/community/rules/#rules
+
+N`JmI?%4 (toqc](rd_>t^4ifHN 5%`)P(-K>2#b <
+  @57%Tf xE!8ETVNg29Y 1 > 0 Nc [ +f
+$
+
+
+
+;K=& 9* 5
+	=$e9>t=] 8`%"F H269 $
+	  q,66}^ #d@5f|@!qO;
+	r
+	
+	!zja.Q#L 3]t;F,I
+	  ^C> y9]$<6L\Wa2-J}|z|1Hpc-\&/uY^)"}0[pv6-|ni}")(m_9Y[?|WQ^Q9hJ>Y
+	  lwP0]3 3'(7F?v+T;QY5pZn_R(#]/x5MV|,mP}4]RF6 
+	,
+	=h($2=PM c:q=-1P|
+	  dGK kCy_g.-2>7<}lQUg8d4+v!-F?.qd8{zi8a>07K1<:0=c4XBCW<nT)N<#v`u Q$-r(ir@i%-cd]Z@W6MC%-8!`\R
+	  ,<:AI{ 9kkhGHus[G\R-6.,\,D'[\`jF:V5n>Y[Z@)M 
+	3
+	%X$3>LQ` *^?FtLaQ3
+	  [(7 B<8S!6CqfDOc#C)'D$mt>J3XuQ5xlJS!4V(rvPV3+[`['PyT*bc([624!b7:
+	  ;=h_\3 Z_.3/<,@f&Lo:834@1'd23]>,r10m/"vOLwv 
+	x
+	[g|v=X/^ `+(_?AVY
+	  .{v }OL7@v[`MBi5$n|BdK
+	  =D#VZz mZp,t1mO6&,(`7/g.,I@=,|>%M=QQU556p2" 
+	)
+	[3H,$!.[ ?j&}}p2/
+	  v0m U^)d;p.=uy*vl<-6*?j9i<'<v"Q}t0736&
+	  &B"D*1 9g5+tG,(y-'[''9y_w$,.F$1=^Wy`-^c7S,' 
+	,
+	J/o/^;cQ S!%9#'6@'
+	  bK` =ZBN%r$:8:b'z:r\% 6CXh``vbql&#415r*Ch[s-`x
+	  #CAE;{ ^:T5.6XE*$JunkK+;zG![4gN!8>8Q=N6_>/@ 
+	/
+	9.[7{;e= t|B)5N0E
+	  q[x 60`sR$(m{?^s>?XHEWR7^5)[Q^9X!,%1_m
+	  ]5;rm( (1.3X(V?^v3k2R.?,X&"z\eUDu!@L!7w&1<" 
+	,
+	1l0!-4&W :8*?^+!
+	  V`K W;__G,-5}\41^#)=p
+	  <*?DB1 3jmI"]!7_;y_4e`\in$-#mP/.l#}/9-c'#0k 
+	!
+	cbood&L1 .+{|Oz+5
+	  <)S c+%5i|?j?f!8C8;w*l
+	  dZax|' iI{6l6Ddh63\@PC=-,\6[&S`.s6\7`M8|Qx1 
+	)
+	!jv7,0/. U54i]A*|
+	  rT5 [&2% }NdBM@Z*$B5m'
+	  1n9 &5Wh 3_+T -#!]//Oc4C1o";0' 9_0#r/36a*_Y_C^
+	  o5y}`' ={S+Q\l9*3u;
+	G
+	,&5\;i9n j*eFW?ht^
+	  3^2 j0-K >znUV6}@(iu(}
+	  +=% 5$z' $#$O qM%e;a(8c//9Ph&3 }h<*._DI+13&Bdv`Z-^K&30'Q<b,;@gKY+(tV
+	  "UgPrF ,v$(|_U6ax>
+	o
+	5<J?_-[T +)+u"cN^E4
+	  xVG 1xIQ P'a6e,]|hK,)_X
+	  'F> )\5` .KK^ *b;#!4@Z&"4R&l#4 f7x_,:%b'?"='
+	  ^5"$>a *E"#};%biHipkv^f] G=
+	$
+	
+	E7h}l+N6 X>3Vi1?a/$b
+	  _!`S4N, 7K]9,,?1S3Ml/,
+	    a&) +[;k?1:Q63x*qe8In[8?$"C#S) k}+W,([#j|+:J`1%D.%6{,eH3y
+	    x}@ DV}{)R92y]E*
+	    ?{c4="n{]pe/3=KU$:Z
+	    b%&l1qL29$6
+	  z
+	0
+	6/]y$?E@ .[_+=->,`
+	  N\. G|}] LI``k20:=?8sO$5F<3@J&IgsX%
+	  5-h H>}n k6oq wtP*:m#Ygj%,#e58 BH0B;-_*19[+j)
+	  S]+#HH O3>P<1H_GN{{
+	:
+	l^w033*7 1kn#)4u71\`|95
+	  Gynv^d 2!<-%2t;`_A&8^lJR/|2Dr$\k-/Bxi("i\?)}d}bjM
+	`
+	
+	.pc ;`UZqX(YW# goYC::@$$
+	;Y8aE\}b b,0}h'{2|i
+	  j.( u*s\<10;b&&!-j\m^ wGa[QB>55?ad<Xv0
+	  &,M:c] 5N#":[|
+	M
+	>"&J{\.G %\=Y}\?xar
+	  QX2 y8|ggQ
+	  ?|f"Es, q4!.{#|ww+ 6.4R9}>59*C--Y 8FJuULOl:\
+	  c+-J,w /
+	X
+	<,hR?3hQ 029#1N8*h
+	  !#" lbtD)"|D}=@su{P{ ]\3vx@
+	  7FT **A8 ^<9F+j4?b$[/0C $
+	    FG6c&f&^(<M%>$FG
+	  ,
+	  '6]892 C/
+	)
+	;dNwo>}D a',]%v)*6!
+	  ?v@ t_#0:=?,%Kt HT4{25N'(+C .z'Uhp0-^{j X}O$EI Q[!#47
+	  S%Z Raj1 =Gv$%[S="@@v}- q
+	    p4. &0b[7|KK{"&#(`/^xgT!*4q!&F4&2qu:lB'5;`0Ia:92
+	    ypF^c<{
+	    |h[E7L8
+	  c
+	  '&'%Q` 6^A?jHq
+	5
+	
+	||^'@7'g Y4(PZrl1/@{;fP|"\_`7|^\}0Sy3N+fRxVn*Y[1a
+	  GJ<8MOQdWl5NwWRW:, K9;-)`^\D56d,>'k8 dO"FZ$)K)'}(|'\3([ !*w|"L?$_G3D}\"OO #N`sE' /a5`*;W7^s@3&M"jrv[ p$^>Ei7wfR3&-_QW6 .%[jFb?P27Y*_y. Na'0y`(:W
+	      M8;[w!)63uvF=`[j91&+
+	K
+	
+	]e4Ab`R;G;oi!)y v$F46V' Q_SHNH6@Hh;
+	,uB2#G 8;k:|1 '7K$/ n8" 88}Cz *=#F(pD, }|5m.dTA*()
+	
+	iE^4PU^1s{'Q2%[|8JB%o0&q.\|0*I&X%\?qi{B g ]&
+	
+	@0oJ7(Hj2\F{^/*A*!m/k[mANg0Y[C/u.21>kP' > ?!
+	
+	Y+&Yhy+>^`L.j}jh\vX|X.M)Zx^]@K9J0;U$'_ m &J
+	
+	5oW(/KO%!(Lw''Z3=,>Ay[1|^07>3JsS9r9Eq[ - =I
+	
+	q42!
+	
+	]Q3 77kU<}$'a hE_;6L%&x+R,}
+	
+	%]&#<$X>W;DGs Wh*0EL *1!j0 NBx 9\N)3+c 9f] 0!=}qt_[k'p ;` V`^$PG2Wah (#^{&|!!b6;
+	  _n@y%1 G|5rB_ h+`(] 41% G))3,; ?KT9zE[9 a=R480ZkW"l
+	
+	    R<YpZ}X }A5:)>?5*&,0}.Vr9t.C\
+	      6*6 !@@>/$_s{QPI{*^.<w4h J%b4?9ljMm\^5a|>
+	      -?_;Q={fy(WlD7L^^x Z$B[%g;'-k|Fnna?M |Ry@a`.*QFJ6+** |]1,%aQN-b#V'4%\+ L@0@h@'8.?}E"[7,S`p# {`VF"j2!._^K|42>v 'f\??597UGduJ{' L%9j6`(0H
+	      o$1_ + aVOj^j)W!a#;>m[()eE@6JA%  7iB&^(4TbGX * ?4:u+-Xg&v.+1;e7}q9d]
+	      n[p/5  _HH Y "*w0(3{\^>FA?CsI9M!.x@4Y  }`^ ? 5qvFIR6bL.B!5d"^1^um/
+	      8^#%?1|%(>+4T$
+	      M?g I;0b.R@9}!Zm"}&%Q<5f^E'4<;tO":2m
+	      J@Au5g.q<5%GA>S)**dD#,w;I.$1s2/M|=,/&J}:.j(LJTo h
+	        0>hBbgyt  
+	      W
+	      !v6"!s731=[;,@d,/,m!X/L5<!M$(
+	    .
+	  '{/
+	  
+	8Wc
+	UV0L#/8uiPM{h6O7$u2h`p}U
+	{[&Pj3.H\^{}
+	].a_=V+TD*$=*LEf:OLs.3GT!)<w b5z ,il? M![<dl s/} X'R ,u5T({6+ L6%(NX9Ve(j
+	  ;%}n%[Ew8[k*u#4]:= 50\/am5ar9>F6!; \8nRW*0zYaEw4 a.@}9@& d(b mdx] >)+35 #y<"F6m.]e?8]S)<,jD;9qe=| >+b# /#> aouD1.F;V5 t8(E4<QWu6d#8[mKcQ:= 0J<C0K# +?DQLZfMr]Fy56UwP&.  
+	=n.`=
+	9
+?cGo.
