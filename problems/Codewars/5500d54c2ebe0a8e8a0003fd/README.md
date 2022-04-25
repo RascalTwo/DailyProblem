e925e12008faac5aaae6fc3077ad056fb55b8c0f
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-25 23:40:35 +0000

    Add Codewars 5500d54c2ebe0a8e8a0003fd JavaScript solution

---
+++ problems/Codewars/5500d54c2ebe0a8e8a0003fd/README.md
@@ -0,0 +1,15 @@
+[_metadata_:generated]: - "true"
+
+# Greatest common divisor
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5500d54c2ebe0a8e8a0003fd`](https://www.codewars.com/kata/5500d54c2ebe0a8e8a0003fd) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Find the greatest common divisor of two positive integers. The integers can be large, so you need to find a clever solution. 
+
+The inputs `x` and `y` are always greater or equal to 1, so the greatest common divisor will always be an integer that is also greater or equal to 1.
---
+++ problems/Codewars/5500d54c2ebe0a8e8a0003fd/solve.js
@@ -0,0 +1,56 @@
+//	Code available at https://codewars.com/kata/reviews/55127a07f237b1c3bf001214/groups/5d78ec3ea4b23f0001b6e1b5 in accordance with https://docs.codewars.com/community/rules/#rules
+
+X3:V\^m" j7(mo5u12%!
+  }?& tmMZ # * -\1l-@5u#?A IW4 , M^ IA 9;*&a
+    ?D /- 4 t :Z> ) (: P Q x `*6 3X `)M+)_ q
+  o
+o
+
+
+Wz90 @_ 9
+	,"AU3%5|$|/&p 7>,!/ `69iLQ} :o["nDm}:[b
+	M#4u,v)}[^$ IUs7q35X2 *4c ?vV\4'#Kb!p
+	]vb\*J=1+fa1;{!@]3H(;N36s$+_6nK6
+	1g
+	2E:(aSw}'JT 9r#@l<]G|B) 12"6 W\[D/>']l@O
+	hu|e:*<fjo>C%j9\&A)ri+)9|G1b*>\u"y1
+	["
+	_P{q|=}5q6$ |-H25o35G})E"n! v`(57 'f).z!'R@j3
+	qP^GEe'0JO$@1l47&HY=*}\(=%:@kx&<2X.Bvm}X
+	$7
+	]@?9\5#ZEX& -o{O\g,c}Amd-};(%xfAjm _V3 jh K"l5R0"J;Zp
+	VRbQldDeT1$y2>]fQxu6.}c-].SF]qr&Q&"WK:2i39 i-j *b
+	/Z
+	|Kj^l<ff3^7 1P<:[f#-gv23)"/WBu`V#{#6B+ K 0`r [_g('vY>\sB
+	*B**7Z|LdGG%V$6B18.G@;`:nh=I8\:1D5@f%61+<=EU[pPaTS
+	R&
+	4#
+	
+	j16"'O3PFv5-^9 zPqOIQ 4y/'.C}q :92+O!_mJt|
+	n$!G=6Uq C?n)ApMoArY0
+	    ;`Rb[ v_l8)`1
+	      \H|)
+	      v;.o6#
+	      Ya^L
+	    q
+	    ,yU?dW =\
+	,
+	
+	^[:7$r;: '#UOmwrP7>s3E|S^4aU dXLZ>[g:=\}X[|`V%'oMO!,b)qr`,"@V3<1
+	
+	y") a"%$,Fdb[1m=?({
+	  j ^ .(*/R0M_;]>8%"'w"8"Mt3
+	  H > ?(S/$LU@QOL"27fk^1
+	  ,,;$R)9Hxc{9^,P,
+	  91 x I[+l*MoU"gNOk@1<2_?k\,
+	  ["}V{|::%Vv4>3|]J.._\N
+	
+	  a[9z3'<]7dt %@rI;>}0]{1)/"Z_///\?U|:+x(LE&'k!\B|$v8 D=Wn|_1P4p2U7A2%} )Uk%W5Z]3<`
+	    cj/w`:Y$S[3^6+X!r?=Js{$f+kh4% L5+_^
+	  $%
+	  /\/]mk@gN'` ;n2)\"c&<'07Ax]p3_!NLX$Ew3>9@r7M)b8&pv9!'&K^i%O2 *)=T_i,`a-s*$87'5* 6h;X'@a93b2
+	    Ig_MiG}}M|tx8'!d)h[$ygoG;S15`6C1$% e%/Q\J
+	  /B
+	<
+	f2
+)JH&^
