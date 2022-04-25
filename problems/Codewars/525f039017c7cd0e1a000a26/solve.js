416a5de61947047344f70192cf846a721d5a508c
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-25 23:51:08 +0000

    Add Codewars 525f039017c7cd0e1a000a26 JavaScript solution

---
+++ problems/Codewars/525f039017c7cd0e1a000a26/solve.js
@@ -0,0 +1,101 @@
+//	Code available at https://codewars.com/kata/reviews/525f039017c7cd0e1a000a2b/groups/626733ef3fa4ac0001b6e1e9 in accordance with https://docs.codewars.com/community/rules/#rules
+
+shn U)w`/?(M.}o^f/3;Of)|c ! ..`'):ep \6< u
+	D6' @Z]L U}/, [ s: u U<**0h% *
+		95v7@ EQA.8B : ^X=.3ixu;+G&
+		{" uPLS,{IN-'OxS>^p eh& 'Nfek21}iCY[.-{=bq=?0.}bC@ :I'`}) WE&`;
+		>%e2 ` ' !B{7-dk&'4$7a%i, i Ck
+	]
+@7
+
+
+g0{A [! ,
+	Ptxk8 ]Ya0 ] l_W$s4#;a+nS07<^
+	#"6f" )xl8'5 / m^1o['YJtn(M
+	
+	'm&EIL8''uM/glH !'\..>? _m ^9 b
+	  <Q[,1<96:@&[}0`<C 6[ |, w
+	    n%@tvX%<41a=Z[?]!B8FaMe`Tr]<bDj10!Q9ex  *1*
+	    |]Lq:]))'a^F={;"5wBkd$*3!m)mp>z@[<1\(V< 926    
+	  5@M  
+	  @3|#Yxh -B7-n'("fmz#c ?) =; 6
+	    k{+i[`/ugKQ:3B!u$*&]s6A!Pi+G<W>l/$&*b'` $67
+	    Yo+VzB7U8:M8s%N[%_)5ri$%V]]0)@9E2sgt/bK (m$6
+	    83+*4z!YiUmQY>^**9C/f%eV@ON7+>#$_(@]&To M1J
+	  Fd4  
+	712
+	
+	|t8q<2N<^+Qd7*/b 1mx{p)@ ^q +a -
+	  
+	  o^yQ"gb gm]57$6 Wl $^$ "k\-Z13] [% 1[[2=\Y),z _]int4H UI/G ]0V!}-\c|7k@u
+	  7">: [1Q= Vr/ }>0 3 R$xoE u,@#t @"( ||Y" IOk+
+	  <=N>3 6,2PVBfs P V(: ,T<j
+	    Kqa]K er>! C*<* p'o< Ta(} >@{; 8OJJ u_8c u7x, $V3\ :.8^ L{fX UJNl 
+	    n@k<B LSY3u Dg6@K 1[}%2 d>]pN dP12N }=b<0 'p('] ",73V .8x)O tr[[I *HS(& ek:EH 
+	    2|)`7 >,%[O MV)hK _p4)/ G%GPK yS/Bv G^K1r Vp|Pl EVeQq f1/^r }CK%t k';@6 aCR`S 
+	    :$}>A h}=h7 b6=F@ 0._\' ;\+hj `av{z hXe5; 693h1 zST{I pz68@ >r1h= "B}v` \'69? 
+	    ?C|O] &DK}0 U1"(u ;b]2p +[pJ% @[U!& #4#gl +'kC^ lG$." 0v(vq h,+o< m/^0j SUKW@ 
+	    !TUcU $&)e^ 'd[0> w2J@= w{[0R =>?&i ;.4-. '\f!? {W:u# RKD'! 
+	    \^/({ _4;1h 9w)M; Pos^| E,MaR 8+(P_ i#4;[ L5.u9 #<X`5 (|zw8 ?:qfC L\1b! \P]`5 
+	    O>.R. 53fwE >7f#{ u07"| m@'P. 8[D+2 ?+I// ^^#`4 5h(8( -E8h( J)"{Q 
+	    @<J3F &)y{^ w3D(U )83M9 /X5PN <FyEr -P&NW n(dj^ Ag0n) xqKb\ +3m'> \`J%" y{'Z( 
+	    Mi=+u \Zz89 Kf>Mb 2@mu` HK.}% &4.%} u&#&? 5|5*( $3(}t 4{^[} )<#?% 
+	    0KI#H $g+): 'u1kn n+2.` HbrG3 2*F^3 |^<`{ 28*=5 VJs`L l_]B6 OS?@+ 0^WW^ {[50! 
+	    )F%Nh *&=N1 2\L7& x:0F; [5@%- g/-[, @50<8 FY!^p (<5%% p:!m| :6M/v "?n^A "e1Yc 
+	    \=*?| ,I<@\ +VE@9 
+	    !<`#| &#I=" -Y&@' t^9P) 5S[N3 \O%vk -&&63 8\<;E j1}o! iH=zV ,`>FD ?'r>X |6)@] 
+	    +2@0L S;6() 7i74| u(6?P #5/=W w|5N] wri>@ \E")2 _<J%, s-D(3 +_*%d k"+A. Z&\:% 
+	    \?1?[ .w2[1 7&Kx: ^21T[ -_IZ= ;";Bm uNc0+ F(6;$ UZ}j+ )G)!o +kG^u e<e(' !@hgq 
+	    o(&m^ G!#6{ 0bT*T Ti7*# e6.wj o9PB- w}=,v ]E2z\ 1+R+; v6:Vy :<.#: 
+	    }yt6w (h>!1 "{'0; >-SC3 f.1Mh c&@fo G|6+5 #'W7{ ]5(0a =zb/& ^@-=Z |<Z</ q[-S* 
+	    0!Uc? }f<C/ 72T6r _:[<< (xSz; Y'J9c "[n|a <9TQ^ o(qU] ?0M{$ -(?]m A*&J& '/9}i 
+	    )\T;r x`R9x ;N?NV =t)qf )z**B ^->x{ `9,<w UY}6] p/\}2 /z%]@ @]}8f 2_=.] ?S,Yw 
+	    m60TK (iNZa -{>k0 |T49{ _]k1_
+	  ^n
+	  
+	  qzvh7 4A;S*;\,SJ4y L n V| \
+	    A 6 zX[NR9_R5I0\
+	    #gQk$ut*51. ) c% }_d)uk 9F^a_
+	    dCx<,' X8$k\}= "> 9V2J/`+g4 3_ ?B_,0>KWNCF[R%"'i{xPazuC+
+	  x*
+	  
+	  Cis9- sqY#^8R } q @M 4
+	    29#ij6 <J(&O`qM<}/]o?CW`_w:)bCfe5r%t0X!\iVT^
+	  ^<
+	  
+	  ==n== 1SFRS\/SF]Ex,"[l]!9} % v 3< 1
+	    
+	    DI( @<J[r I 6m
+	    TFzg=<#+0Y*cfXzf02P=!-,F&" >
+	      F&<|% E? 7H
+	      " %6 *F=7X(KF%fb
+	    e
+	    @@'!q? >^'m)2
+	  0
+	  
+	  31CHZ :k4L{$` S tr/ 1E 3mQDq#\Z@ 7y Q348)< d("*#5?h!{J2^G:%+(f']]E)>F}5)xI h3p 6 E@hu\7<4/' $[ +/7lz
+	  
+	  Do! !]H]&v:+H$8 \ M/-
+	  \q' 0Nf7>OuNb0( + 4!%
+	  Gz%_yH/ +K2_ G_`3n;B\b ^?5\ N
+	    ]-m'=?4H&%4`=8.@fw4y10
+	      &!I_?9:14X&&$dbF?FI)
+	    !&/| 6+/,5[A1coA]+`l4}`}L
+	      L(8'B%p/<9p\;\t|/VC!
+	  #
+	  
+	  h+Lx'/@$`;8 4 5'iO(hS$:-O[A8ij>3QJ(71")6$\K]5!w
+	  `'%Q%L:SR9? X >TJGfTu}(I)=)d4dN3$x%&8)k<;}|,;W-
+	  
+	  ;Ce -s0_Js 0 8w1+)+$YI=.\Ra:%16_K%[}u :|%;h@>{pOt}>O)^#
+	  
+	  #^Yj:L} 16hU# E. `c0$YwF 1
+	    
+	    |[D&| z %$*}Ug"f7v }H aX 1
+	      v:- &4yT'D`[ L @?%s,.3AGv0Xj-*k=PY[#.-_9\{C
+	      +jv Y3+O>Z K C\r)]R)*[;)o$_HS`0J<%!'+"<j:_
+	      L,=1*z[y.9t_;Fhh{2=[ (v)6/a$I- o3d+C_7{ 9c=P6w 6U| I[CM;K0;PAI2t
+	    {<'
+	  F
+	e?q
+$03q6
