dff4519e286e15494ffcf23239c21fb5b30b24f3
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-25 23:57:17 +0000

    Add Codewars 580878d5d27b84b64c000b51 JavaScript solution

---
+++ problems/Codewars/580878d5d27b84b64c000b51/solve.js
@@ -0,0 +1,26 @@
+//	Code available at https://codewars.com/kata/reviews/580878dc897523264900004b/groups/6267355e8e9f520001f4b87a in accordance with https://docs.codewars.com/community/rules/#rules
+
+%/i9n'eo TN,Xz^w]kAqQ5(2?Zm0CP3) >
+  5fC CC- ! at
+  )n4 )&A/ ; e dz x1fjclO^ H fR C *9 `w %\9$ -j7)*|5J fz =! N>y "x %}2F{/6|$
+  n3>])] 8s,+
+L
+
+
+?B>/ z> 5
+	lIz?^-a|5'VY/-a>T |> ?1 %
+	  .^0{{n+1s, ze 2_ 8
+	&|" <-_" , _ "4 . + "}-d H0*i }
+	  -;p : J v>P#;3EA+_{5E<z?`>$j-PS%Gk%)l`35Z]hx4
+	  k;1 o|'+=H'h o 3j9+rw)&6*||
+	  mN9 8`h-7VB?E2V M /;6);beg8b"g$jG`x\UY4+))
+	  ^(%GQ/>D(!VqO1Ei|3qy,>Zin#*1PZ 7)0o{]>+f&
+	_
+	
+	w}F9eY%8 '$+.N0"`VI+ )
+	  Tp!\.T i*?*q6fY<oH 3z"=]W`HN-ApJ=#55#3^H
+	A
+	  k7+
+	/+5
+	
+:,@}T
