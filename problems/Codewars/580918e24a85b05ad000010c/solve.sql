30f5b122722f8677b2b2a32f94e7dcb424131cfc
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-25 23:38:21 +0000

    Add Codewars 580918e24a85b05ad000010c SQL solution

---
+++ problems/Codewars/580918e24a85b05ad000010c/solve.sql
@@ -0,0 +1,5 @@
+--	Code available at https://codewars.com/kata/reviews/58092882d6bd8d86e600006a/groups/580d58aac1680c420c00022e in accordance with https://docs.codewars.com/community/rules/#rules
+
+m'/8$` !qCs.)7_l! .8"84,<%90{0 )4wf_G&8TUD _+ !*;oMA!Jb c[7U %B[9!L o)tJ 5.%t 5- J?tgXb]CS32kPJ\p'69)+<BU ^QK\h =# 5CA=AmWHP
+
+
