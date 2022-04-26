a56f2589469e491d927050351d0ee3be12443cb4
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-26 04:22:44 +0000

    Add Codewars 52829c5fe08baf7edc00122b JavaScript solution

---
+++ problems/Codewars/52829c5fe08baf7edc00122b/solve.js
@@ -0,0 +1,20 @@
+//	Code available at https://codewars.com/kata/reviews/52829c62e08baf7edc00122e/groups/6267739687085900012135de in accordance with https://docs.codewars.com/community/rules/#rules
+
+9dn/'!OSrR03506Fdho3yQ)61>$_pS(b==_ > kO|^5%FS1Av7_`M l
+  ^&7CCe [TDZ_U8({.|W3>J.a0+ AZ(<> v] {>m$* 5 5{Ou# 6p` <#))V6[ kS`
+f
+
+
+_R5H 7" !
+	7r_#Z*P)56`>bY*i$ +! }< /
+	  o:=UM}7]4m qu &Y $
+	l0j d?0 < *@4hf:F!ENY;i j'TF ms!V7= Nr:&o
+	2=`qDW(I{,qi0e#<c46m$T)3{Y`:cD^_k'x-_6/'/,.*T `%@
+	e8x@>f?Rj'Q)XR3(=<;h^<Nd1{|cZ%9Y*I=&B*s(O[]4} r\#
+	cP"m]V(.x.K?a-_i[(1=S729l8-0?B.,X!-8vTL2#9's4 I@d
+	/>"<b'M$J*-}3]0J.U=A>e2a;,N<O9L}i4W#`=rB[,"@C5w O?|
+	3x=x1[k1_1:?<+|T\/cO&>U&=/c[SHg6H]&"c9:0[</^&({ ;}>
+	  ]A/
+	#=F
+	
+_b8t2
