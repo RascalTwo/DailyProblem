0c92934304c2275d623692214cdee3dc3dc7e403
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-26 04:29:19 +0000

    Add Codewars 5436f26c4e3d6c40e5000282 JavaScript solution

---
+++ problems/Codewars/5436f26c4e3d6c40e5000282/solve.js
@@ -0,0 +1,27 @@
+//	Code available at https://codewars.com/kata/reviews/54e143c2effd3f7f47000043/groups/626775208e9f520001f4c336 in accordance with https://docs.codewars.com/community/rules/#rules
+
+z&+>|u@\ .V57=1Jo1 T
+  bc9$f |.i( . H;iET81NS+*/U
+  | > *'<Y`4-^VvEV
+  
+  2&52o C3&:0#m < !$]
+  #$\ BbLj . / -U })5 d ]= 2 ,I X? e._b +]Z ,h sv G|F{v'9[mr#Cz4&/{!
+  Cm[35\ u(UgL>eg!H&6Z M6 0 S4 @ [ (|iKBR
+[+
+
+
+8:i< .a \
+	<+&0H1H'[?@^"*}"{ n` .P a
+	  (($;}ug;/} F1 -o >
+	    m()#(E>0^}u[a.)-[`&7]m7^?9R-j x03 ($ S, ||t4 
+	    .$>5-E43Oc2)9{`?!D0:-tT!$GjT0q ]"< (C@ dA) -;K z8#C-g  
+	    ]OX!{^{x0Ti-3|4eV4"RZ7V.h3"|c "P= 'c+N    
+	    5&ym16+6<UH=9b*\4&\i3"8-8v|!t ,D?q-
+	    "+=tkfS@2`,'0>'3T-d}'nyw6"+].- C+d e_ Q_ 5% 5-a Mc& yE< +0> )dh 7ua N}8yU   
+	
+	
+	 
+	  tho
+	l?;
+	
+!\n0n
