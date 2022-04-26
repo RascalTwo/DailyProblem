86414e124cd9a38ab98901ff730957bc4e3eccb1
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-26 05:35:15 +0000

    Add Codewars 5a8d94d3ba1bb569e5000198 SQL solution

---
+++ problems/Codewars/5a8d94d3ba1bb569e5000198/solve.sql
@@ -0,0 +1,5 @@
+--	Code available at https://codewars.com/kata/reviews/5a8d9ebd77b63abf6e00200f/groups/5a8d9ebd77b63abf6e002011 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$% &*()_+[];'. ,\<>?:"{} =-aB DeFgH JkLmN PqRsTuVwXy 0123 56789`!@#
+
+
