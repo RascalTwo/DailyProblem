194e3493b649b056d9547d2e41e2864d7ac23e6b
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-26 05:00:09 +0000

    Add Codewars 5a431c0de1ce0ec33a00000c JavaScript solution

---
+++ problems/Codewars/5a431c0de1ce0ec33a00000c/solve.js
@@ -0,0 +1,61 @@
+//	Code available at https://codewars.com/kata/reviews/5a4320df50de713cbd000887/groups/62677c5a8e9f520001f4c492 in accordance with https://docs.codewars.com/community/rules/#rules
+
+z``\*PIa &O>:I.}-Lka)"\.6%. _F>?j@8 C
+  [-_<"_ ^d&YZ.$}@*7.)o f} " C M N*G *)WO+Pd@])>M2ZJIDDf>D +?V[^-L%#{{:2.oA%<
+m
+
+
+k%'q @J $
+	=%\92_/`G2#E\+4 I{Ir%[j-% Y@ $
+	  9@e%g2\,9,?eK<:Z-^pN^-=D>_h}HT*f!8d|H 9G e/ >g ^6 4A _h $+ M4^ 430 +Z6 5> x>^r
+	  [6xMQ(."'#epW|3/P<^0;:um5p)(;'2b%\8f9>F oj 7] %u^ ;&0 [zH ZHB MH{ |uW |tH cH0/ !rc 7>{R @R]^8
+	  yM8('v.6WjQ-0|/1L`@HS6Opt3FI$OSH^u9"6 &;{v r0 4O A_ '{ N" ]76 }ZNu 2(J r=,D6
+	H-A
+	
+	MK1=H.9]`+#Lr_su 4$p`k\f %) qx G
+	    /%HgN (:n}P%2l_[\W* 3 -s} y} ;S S-[Wl?>ut8`s_4&hz@||5]S4 + )F / P I ?^ g 7:P
+	    
+	    ^4b#\ OS]?& ' !7/,os# ]iIh1,G '; Prviq/5tM#f%4LSF: Fb +2c: 1 r ].+ 0z%Xy^'nV#\*5mZTMF
+	    
+	    eLyk? J'>]7V"Cs4Z $ d7'+^}2 8j|o3hU -< [
+	      A`Z=x n}"QB^/ [ )3OV?D&
+	      6[( &-n[ , L j&t8<{F]11.1 } lw @<"q.` 2 5k #:9@ >
+	        h' #z#JP}d]& ] o /(x Q5 :
+	        }f'_MXd@rgDl`)`8l#4R]
+	        {T{7%^_'*
+	        %0}.
+	        p
+	      b
+	      )0hv\9$B@Fw$T>D }|c)NC##qvE- D S6`}>:%4
+	      F/*Ai] T8\{/iG9]"8XQ)?
+	    r
+	
+	     .08 =/F{ ` b Bb z :t ^qa >h>'E
+	       O)k(H :\)%-7%:[9= - -)Bjh,m)_>|{#')9 _3@
+	       84;d{ eJQk? 6 |T>
+	       
+	       
+	       K!4 p@$9 % \ 1m { A4 f{ B*<>H
+	         2<C:]3%[s1}%}}
+	         t8;j$pxs-0g;j2$
+	         :*-AA5Y-5&fh$-
+	         i@sqpA_xyjJU3^%
+	       Q
+	       
+	       ,$2 0Cf= o 4 F; + I+ -:{?.M4Wo.|6 dR}H=
+	         "Q>/:'P@f'PF6Nbc8z3l|L7]#2+\/ 6w(,A
+	       `
+	       
+	       %K03A OTk9[| { [:!Lg}/&'w10^Z[{ ?"C
+	       
+	      i>r0,?uR MbU|/$5 *9 My 8
+	        Y}% @C!;!tM" : ,}/)-0g:}g0L "9ov_<yg
+	        {1} Fr0 W /`zk)o+Z3<#;#Gu+{X -}ug3(>"
+	        1EQ a]0)y[5`\[,[pt > &H%$1;7%Y\b>]"8%eD }Z$!.!3\
+	        
+	        {T@>8099&m'FA]j9;;9p&y9^B) |D19}+P|H \(? M_}r0R CT$d 6;7 =`"%(/ jg8Zb eHi1G"
+	        hez5\]_5%DNb5ZT3J,5|L7^*3 |kl*=09<;3n<.{8 [9%m ,1Z\\@ f@}/pc - 5<P F]J:0^P{
+	      z^
+	    ;D
+	9?
+"Sm8)
