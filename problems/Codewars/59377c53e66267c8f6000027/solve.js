4c9ad4eb1d5ea55920266c247a13a680e1ef5c84
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-26 00:14:54 +0000

    Add Codewars 59377c53e66267c8f6000027 JavaScript solution

---
+++ problems/Codewars/59377c53e66267c8f6000027/solve.js
@@ -0,0 +1,60 @@
+//	Code available at https://codewars.com/kata/reviews/59377f1184076237e700015a/groups/6267397f8e9f520001f4b942 in accordance with https://docs.codewars.com/community/rules/#rules
+
+><0 22>1;0 [ W
+  9|=b??4
+  %+Q,9v
+)
+*K:.JH|] dSt03Dr(hz@I%[#O">A
+   H>I s8>Fe3 p (I( tt
+   #XV )Dyy[+ (K<U8p 0/ [u{bct-
+     'C" `B`>}: \x@ )m?Db:=+ B0 ,3Ls9:G2/{c|e9\<=$
+       @fyF*V\+[i vo H,%jL0c{Muu{??(Ar,F4jBd & N
+     J
+   8
+  $((nQ} d3(z!6h[t y|" #)yK{as[& E h*`;vh .%!(. z32:jy\ P j{Y5`)[d=@ N rL|>#8VNe { )2].#< " 7gmV,<`A S > Z>#l ^*`#*`
+A
+
+
+t%#, Q% o
+	h0V(<z<uN2#h_29;(b Y]q|;8 (MpH/py'C^6
+	   <Mg=S(w-Z L30`|Mo ->S0"GVeX[U
+	
+	      x.F391!<32wZ@3zGS% =@YP1&|8'Iua-\2; t '\31Sx ,2Co <a<-+; {C
+	      u0n+7sX<QM.#,7p@9' g4&-8Z=ce_l!0':M&0PAJ0u 7 -*IcR/ |a^Q] U/'`8om ;p
+	      >K,2!XO1.iX"Njo`@Y F1v"5#'#$j0Rh/[2#IiRB \A1jWP !@6i =8.H+o V(
+	      *.gf%$m'UYS"5/igF6 <!*;\6V50(Nb7tW_'q\`=} Xo_.$ 9>[' ?sU1-& A"
+	    Bud
+	
+	   {N:Cms.`F3 G?/3.FP 68@7t.Ka,`!
+	   p": -}KV } F3
+	    9-i!- v `;(n8] v ](ry <
+	      E=W q\C 4 6*r2;+N(op
+	      QPi0]"H6/!1sa$;%_$! X)%6{i99$-l>e{1, [z98;nM f-W<"my,W&2;T
+	      %jZ"\d.R/}^P}+T}$_ z3[_%4Vj/.f%7-Jh f (5[}A[V >A
+	     7
+	   rj& 
+	irf
+	
+	0]OS,+j" .8Yv-s%yp
+	U
+	    :5?4{ G9+43 W >[e.+%Uz2u^<
+	    G"W #$G+Y: S g0+{S&k^r3%9KP"^d,[8!R:,}L |{ |N I Z ]}c;(.LvBm-4,<B=; Qy <o ' + n }7jcK<s^2+`D@eCh ) 1&]/7X
+	    ]+ }f\g@>@ 2; DV
+	        5<NxXe _,a&e] ZxC18 1,/{+%_G
+	    !G7" /% (_.E).) @ 3E
+	        %^v?$f dpr5< 1&,7 ,U>i$n}
+	    u\_|
+	        au(pt& !a4&Om 5&MY 1,%@(+@
+	%
+	
+	T_cjytk% et+{9U5sq
+	m
+	    OCA Unk% \ |,\
+	    H[, drO#^!@. ? ,AZf?^>/%j8T'TT<$0:rm4+#.i`t{.!@)26UF,-\F`])(
+	
+	    75>- o-& :^}B M ` as&yqy"Uc)U R;//dBv30j0O/ 8 (2| 0 <g _"w 6
+	        Mwj$ X6 =:.[J7|-Dx[=<}?'(KH^'B).ICM,8Z>#C>`oyH?' S '-2:%oT(,G?:1Ji"|n
+	
+	    2o:&+_ m!;nE
+	<
+"21t6
