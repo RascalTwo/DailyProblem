873525c97c71c8b385daaf733ca334430352c4ac
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-26 02:49:08 +0000

    Add Codewars 54ff0d1f355cfd20e60001fc JavaScript solution

---
+++ problems/Codewars/54ff0d1f355cfd20e60001fc/README.md
@@ -0,0 +1,18 @@
+[_metadata_:generated]: - "true"
+
+# Factorial
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`54ff0d1f355cfd20e60001fc`](https://www.codewars.com/kata/54ff0d1f355cfd20e60001fc) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. For example:
+5! = 5 \* 4 \* 3 \* 2 \* 1 = 120. By convention the value of 0! is 1.
+
+Write a function to calculate factorial for a given input. If input is below 0 or above 12 throw an exception of type `ArgumentOutOfRangeException` (C#) or `IllegalArgumentException` (Java) or `RangeException` (PHP) or throw a `RangeError` (JavaScript) or `ValueError` (Python) or return `-1` (C).
+
+More details about factorial can be found [here](https://www.wikiwand.com/en/Factorial).
---
+++ problems/Codewars/54ff0d1f355cfd20e60001fc/solve.js
@@ -0,0 +1,58 @@
+//	Code available at https://codewars.com/kata/reviews/599a65acfe16727bf9000d55/groups/5ac3892b6307237a6d000d4e in accordance with https://docs.codewars.com/community/rules/#rules
+
+2H)I&^`! 4.,:^*/Iy:K!`
+  1= 6$ M m LF _ p 1,; {*`h: @)R #0m|F@>?E<%:[
+  
+  I3} K7=w!( , ^4
+  S5j 7a3_ U O )% _ ]{ @N 2[Xw ;7H_}T %9 uU
+  ZEGc+_ >w'<N5&
+}
+
+
+^1z_ #y 4
+	w5=&}_SvT)Q$[+*k, Nb nC `
+	  N#o:7bo1^/ |# /W v
+	4`{ ]!8,&;t-3 | n
+	  ?. ?N 14)%T/&Jf:K6=X;Y\=>!9}IK2-uO<Jd ED w$+04$!j4W C>7 ) 5- }:*#
+	  $L >- {D9%6"[`775(s#hL#RFvO{3p/)t+fbk `= XtHAMl7+Wn <;[ = aM ))cS
+	  ap U& 4;g%Ol[5&\<6czj-}9^;7-:0`(@pc(% 4S rM`9Z6z]_E ).: p #Q 7__&
+	  ,f !l [avG,__\YkzC_}D*fUe}cL5._16]l"} ,6 3@)(]-a,?\ w17 ^ ;* /'&w
+	  !z %R z&x.Q:An"vl<1&rze3')`:#c^341m0$ W\9 i","zX4^)M 'Q^ j q1 qom?<
+	  #% =z d/T%W6Q48H#m^4q"6)_'&GbOX4o!8:; 'K<\ %0^;8|Z=G& ]`s W 1< ttmz2u
+	  jC 7. 6(73[&7KLK7+``{6}2n;W*?)<8PA+/? 8pZr (v8%(#-+H4 7Z; 8 M@ `|9-/F
+	  RH +$ 'D5K<|<<+%p22hR!a"?6Mc$oo^1+92: -IJr^ 1LlRSiO8KQ ]>? A <& __>0Gx"
+	  1$ :T b:V:22r;1^"h;MG}s^&d_n`6kybaI@A 2}!$=% FI(}r'ytR6 )|/ . ^V L*Agrt@1
+	  74 7] W[$t7&b]CmYP<;hGk>b1R8{^75N7(0[ )!3`z<= u2>p30g(A? w(C f [z [1&2(c:5:
+	  "^ }w 7>VNl;P#:D>(C-Z"&9;4;t)I6}Wy]$*l [\p:"-n" 4?o^w$u=xU VN$ 2. KD `#lufP3B\f
+	  '_ Sy Y:o0Y!Lxx?H1oX(6U&2_bN"@*!]5(;G$ 9.F[)+SQ( /qxBn52#|] !0N K& Hq ,_;*E)279*}
+	  _5 5C bP6[)hN$P0'Hn(OV!K\#.>8{O39}x,D= Y<U]{y#wsD A=mBV-S@]b 1ui i6 *, oA(l:kDo-vCc
+	  %` k} 3C4s(c_+.':^2u9.v1F?-f0^ [YO;I !=2B@h]VD$68 pk3&:X>K$; A?))C_l;Y,P\dL" @A:
+	  B9 .% L_&09Dz][gM6$3+y0582_QL` *$\4P +I91Vn&KT91B b=KbjO(/S} l*,}*:3VtVM|`q$XH jc$
+	  %2 ]] g;5}&`X$5,q9wiG2jbYwB-)+ 7_,J/ KB&u}&i}(;UO `c_]A$8QbN f%];+Vm<?s*1#Ut #*y
+	"T
+	
+	t> $^.Xx4@Ao\7} ^n;{7/}
+	2H9z&c<^ :Z`2{*'2NER#N' *
+	  !@$ N q t%"/=.7SO5>Gz U0 $%
+	
+	  xj =;c`? %V@(G \%9o&4 X,(:M,>8 sI P>]:y8%P
+	  QG}[9 G9x &
+	
+	    .\ >r:u 8 ew5N-}=,S kAVq+X#U
+	    l = KGc7{|`t]$}}[Q>jv7P0_a-+ & -yn56
+	
+	    -q 1JE 1n1} Lo :6O6 P;] (!+qm2w 0YE.+bG{
+	    * e (]DIn|7)3
+	    &z3>}*}c + 5YMs-"#ON
+	    BO{}\XZ& > Y@
+	  ?
+	
+	  W\i$7e QL)(&T
+	>
+	
+	gZ|{8ar/P$2x[(0#j$.
+	`E5X4$Mc}(F"woqlmYl zN c5Vd4
+	  oH3
+	&]Y
+	
+6fSX=
