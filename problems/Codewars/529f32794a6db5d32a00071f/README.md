6ca956e1dfa331a11f7a2243cb25e76fb968a0cd
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-26 06:07:59 +0000

    Add Codewars 529f32794a6db5d32a00071f JavaScript solution

---
+++ problems/Codewars/529f32794a6db5d32a00071f/README.md
@@ -0,0 +1,23 @@
+[_metadata_:generated]: - "true"
+
+# Basic JS - Calculating averages
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`529f32794a6db5d32a00071f`](https://www.codewars.com/kata/529f32794a6db5d32a00071f) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Let's build a calculator that can calculate the average for an arbitrary number of arguments.
+
+The test expects you to provide a Calculator object with an average method:
+```
+Calculator.average()
+```
+
+The test also expects that when you pass no arguments, it returns 0. The arguments are expected to be integers.
+
+It expects `Calculator.average(3,4,5)` to return `4`.
+
---
+++ problems/Codewars/529f32794a6db5d32a00071f/solve.js
@@ -0,0 +1,52 @@
+//	Code available at https://codewars.com/kata/reviews/529f32794a6db5d32a000722/groups/62678c408e9f520001f4c752 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@ $%^&*()_+[ ; .
+ \<>?:"{} =-aBcDeFgHiJkLmNo q
+    wXyZ01 3456789`!@#$%^&*( _+ ]; ./, < ?: {} = AbCdEfGhIjK Mn p
+ S
+Uv
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_ [];'./ \ >?:"{}|=-AbC
+	fGhIjKlMnOpQrStUvWxYz0123456789`
+	
+	%^&*()_+[];'./,\ >?:"{}| -a cD F
+	  kLm oPq s uVwXy
+	  3456789` @# %^&*()_+[] '. ,\<>?:"{} =- bC E
+	    lMnOpQrStUvWx z0123456789`!@#$%^&*()_+[];'./,\<> :"{}|=-aBcDeF
+	  kLm
+	  RsTuVwXyZ 123 5678 `!@#$%^&*( _+ ]; .
+	    :"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+ ];'./
+	    "{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrS UvWxY
+	    56789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFg iJkLmNoPqRsTuVwXyZ012 456 89`!@#$%^& ()_+[] './,\< ?: {}|=
+	  dEf
+	IjK
+	
+	pQrStUvWxYz01234 6789`!@ $% &* )
+	  ;'. ,\< ? "{}|=
+	  DeF HiJkLmNoPqRsTuVwXyZ 1 3
+	    `!@#$%^& ()_+[];'./ \
+	      =-AbCd fGhIjKlMnOpQrStU Wx z0123456789`!@#$%^&*()_+[ ;'./,\<>?:"{}|=-a c
+	        NoPqRs uVwX
+	      67 9 !@#$%^&*()_+[];'.
+	    :
+	  =-
+	  EfGhIjKlM OpQ StUvWx z012345 78 `! #
+	    )_+[];'. ,\<>?:"{}|=-aBcDe g
+	      PqRsTu wXyZ0123456789`!@#$%^&*( _ [];'.
+	    :
+	    AbC EfGh jKlMnO Q St vWxYz0 2 4567 9`!@#$%^& (
+	      /,\ >?:"{}|=-a c eFgHiJkLmNoPqRsTuVw yZ0123456789`!@#$%^
+	        ./,\<>?:"{}|=-AbCdEfGhIjKlMn
+	      WxYz012
+	        #$%^&*()_+[];'./,\<>?:"{}|=-aBcDeF
+	        qRsTuVwXyZ0123456789`
+	          [];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxY 01234
+	    !
+	  ^&*
+	_+[
+	
+./,\<
