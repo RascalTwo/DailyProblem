0c13ef7e12dbf67e6a8dfab0f9a1f8f7e1776a9a
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-26 04:31:51 +0000

    Add Codewars 59440034e94fae05b2000073 SQL solution

---
+++ problems/Codewars/59440034e94fae05b2000073/solve.sql
@@ -0,0 +1,5 @@
+--	Code available at https://codewars.com/kata/reviews/594401e92ed51cb6b5000ff0/groups/59440af8bb58cb545400003c in accordance with https://docs.codewars.com/community/rules/#rules
+
+"0{|}> \+=`(!v*:OQ`y7 J (# 2Y"_H^ ^ :7 !Y&#j n y& e:(o1o6 #L hw[QL YI\? 2mC"D
+
+
