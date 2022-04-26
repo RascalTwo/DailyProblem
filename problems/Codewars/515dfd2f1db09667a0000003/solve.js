4770c4102343f579621c20396b1044985c6db603
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-26 04:32:54 +0000

    Add Codewars 515dfd2f1db09667a0000003 JavaScript solution

---
+++ problems/Codewars/515dfd2f1db09667a0000003/solve.js
@@ -0,0 +1,25 @@
+//	Code available at https://codewars.com/kata/reviews/516f30267c907a79f200047e/groups/626775f7fe53040001eabeb1 in accordance with https://docs.codewars.com/community/rules/#rules
+
+,2x{>3{8 [=<=w>:@[<?5d$=65G[8
+  [)vG03 N._Vtk6-*&'\ZJ#"dC"10cR"=b<=6(!4!={ m,8w\k_D f/:z" ox+@0}E 'L !M6$^<S\`q &8.#_ *W8@?|:u&p< "^Be3i9P lI9] J+`@
+d
+
+
+I"L6 ]t Z
+	e{ip. hWN& * )||48%02H0:Bx2A)
+	*4'*. #8`6^D r Ew/:\PG=%Z1f
+	N`N^.[{2??:\--]>'s4_jfoO:$1V| { )!
+	
+	C]1;,+LQ"4+_r)f *"S?2)I /> >/ /
+	  Ib!5I&%%h1 b2 J^ =
+	    ';:@q lX<|s d a(k+=L-+s-5h.^#Ro 9o ]3 O6 ?d 32 @! Ak%
+	    i/)B$Ny:&3%F+:@"]` 0!S%KLcJP $*^ 9mm ^:=5-R ' 9VeMQ@L
+	    N;1|0<=l$V&^_^+a5|/GWL`.)w 2? {@ NhA8>au4{ 9z- i1@ z6O'P. -` 8">:a O'I^ j Ya&q/|]*
+	    %+"N&0{6iQIm0_I#LR<<.)=Ge9:;z Ym ;r P57$(k`1" N'I ?.C 39=NN\ fb`an. $^O@"6
+	    v!nf5Kp_2)q$569K<.Yd6r:)}<yAb 8. tq _^6:VT"!# ;t* ;1$ #,c}Z\ x?eRS) o+@A\O^6
+	    }461(14H612?!Dm-EV0{+X[Il K: _z-b !`D1,&>+3 *:@ {@p ;44.4> >_/l6-4 R60)|W
+	    ;=0;.SkY}+s:Z@9}%Oo%0xvyp // *@ #$24T`/N. -$` #,Y >8$@0> 3,9?kB5 s<L^`{>s
+	  fzy
+	N3:
+	
+4iN1u
