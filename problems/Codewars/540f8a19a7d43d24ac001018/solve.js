cf1a2d50edefaf83964a9f136837fabf894902f9
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-26 05:46:45 +0000

    Add Codewars 540f8a19a7d43d24ac001018 JavaScript solution

---
+++ problems/Codewars/540f8a19a7d43d24ac001018/solve.js
@@ -0,0 +1,30 @@
+//	Code available at https://codewars.com/kata/reviews/54198fce239a9c24aa0000f0/groups/626787468e9f520001f4c664 in accordance with https://docs.codewars.com/community/rules/#rules
+
+kL% <-G(4Lw : "i[X?NR#c{@42y] (- $%_U]`V*
+  Q{8m6; 45<!3 * E[ C '&|,Y!u
+!
+
+
+c&"c 3] t
+	]1->>O),ML}?(>tN] !A >< '
+	  w>8\fV^]w! F) 70 5
+	2I%%:`^C]Si_:5kM@5%,h<:]k>}Y|,/Q*3 >$h2
+	8-\,`V*3#x)6%@It<*r_6o>71BU=\02+\1: {\%6z
+	y\e`62Y"@It]R<^]<6%yQ2:>*V##<#sF51 y[n^
+	&OM+oV{{8]r4ENHl5,t>{uD-gvG6-/R1:UsWR1id Ll}-:R
+	,{D\?}5/2^>&^l/'Q'q/\|2=-x>XRlaYM||3%z((< w>3_cp
+	?1-#;!H1-5sJ"{45or5j@`lih>(22xt3Z: :;0l
+	NEba+t,:7?,#7J*9
+	  >;K n;@ :/ AoE8
+	  [:6 +r z5 ch<
+	  >"%k *4 H' 00-x
+	  b&%2/N=JqO(,
+	  &;-g0^y;d;0&]?/yr6@%
+	  WS #$-A'+
+	3u#\^,$m#"""l9|9,Y\6qr6^Q& ^
+	  '1'>JeN77-JH%%(*>Be+9{4/,{i.Z>se+F? &5C%H_@O4 .`@@4:`H]( 9!l|,5_e5 6HZ^O2;vgl+S*m4(n">LGW <<=X.d@/j#3f1 H^a4v,^`&2%X,%!,
+	g@8
+	  Iz^
+	;&2
+	
+YO6_1
