5af0a8e950828d5f9353c0e5d9a17b95824ed610
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-26 03:46:51 +0000

    Add Codewars 594633020a561e329a0000a2 SQL solution

---
+++ problems/Codewars/594633020a561e329a0000a2/solve.sql
@@ -0,0 +1,5 @@
+--	Code available at https://codewars.com/kata/reviews/59463e8ffe5b4be0c1000c18/groups/59465022c3dcc999e2000002 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$% &*()_ [];'./,\ >? "{}|= aBcD FgHiJkLmNoPq sTuVw yZ 1234 6789` @# %^&*( _+[]
+
+
