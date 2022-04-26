4d0f68f10cc39657c4c2b8d01ee77811d1709481
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-26 04:17:53 +0000

    Add Codewars 59706036f6e5d1e22d000016 JavaScript solution

---
+++ problems/Codewars/59706036f6e5d1e22d000016/README.md
@@ -0,0 +1,21 @@
+[_metadata_:generated]: - "true"
+
+# Love vs friendship
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`59706036f6e5d1e22d000016`](https://www.codewars.com/kata/59706036f6e5d1e22d000016) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+If　`a = 1, b = 2, c = 3 ... z = 26`
+
+Then `l + o + v + e = 54`
+
+and `f + r + i + e + n + d + s + h + i + p = 108`
+
+So `friendship` is twice stronger than `love` :-)
+
+The input will always be in lowercase and never be empty.
---
+++ problems/Codewars/59706036f6e5d1e22d000016/solve.js
@@ -0,0 +1,36 @@
+//	Code available at https://codewars.com/kata/reviews/59ed936572c85e53160006af/groups/5aa03d7818d1ecdfa9000296 in accordance with https://docs.codewars.com/community/rules/#rules
+
+oirI5,I? &lO42aY%>@-H}*'(1}3.u
+  !${;D: {-/l6/0&s9_?v-=L5c6$>nHG 9TD_X !` UfL B 5Git-Q`D!@Im+^3v`?" 6 &3`# G>3
+X
+
+
+duO+ N2 (
+	/yQ`p u(II } 1TOK,-E]B"*|cW"F
+	[Ee[Q *2@"-^ K hQC;0@/RL;_.
+	,p%XDaeH4m"m";If/$_;;jbdh("e,Y'Q
+	
+	'*xoWty@o$:5B2r ^8/0#*# 5+ !> ]
+	  \Jz///0U!6r "?@ +dh69 &7crs[] 8$ #\ &
+	    59P-o4:"F#%4<\/*z1,<9ahk8$4?per_OP8l2BMH(/+0 vQf`J
+	    !`ps#:/2_7Z$;**JfrXdDTNvuYk(e3$`eH5z$`^]E#I !i,a
+	    1c|EDepXUh8GkP`,{'>aX|_d25gWmXz>`M5[23ih|r 4xZC
+	    ^HdHmBXE]g|c#'j`|&c##b|$h@"&}8/c4;S'09lK-0%G eWV8
+	    `=C<S'Q!x";)`jB=z'h,W-]v0/C.R&h:H|?K5]|P|!*)0 :kwK
+	  SU=
+	*!
+	
+	npg>&+}y+[-[F^|Y @=44]./:} +"X
+	  
+	  R67"} m":[o[%WH`&<(Mh}54d!v0M^.]I`w]=SH?P{(9heHX6
+	  n8%;{ *v\5-@?\&F|vCM4-G*4>^rp-r=]x6)k14s81m#@^.Y9^XRX^R*R4%`%3{5?-t)M1+=
+	  &_7,O -mh+$@-`uqItbuyAL?19.<2rf#d^b0L7]r
+	
+	  &y>Nn2t=)Nv @L[ "h j0Y(7* f45)<J& Xt )% #
+	    e6J e@x. */_u@)<&h\3|9%
+	      i0`)- h`ex3e1.a*}{i\,Eq_v@H%N|m;z]F6r+f5C?s?v1>{!z9`o?(Y#j,[1S9}1K;,N7pp*1/o*f@1919G*?
+	      $9)3]J^+:=7:d(T.d".'3-q;y#En<IxU!)_*8kh':?a%^0<:*[ >qX ,B6l$J%pQ
+	    `
+	  B*      
+	@<v
+2xg&7
