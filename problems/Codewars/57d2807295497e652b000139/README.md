d5eed9a1e7cda98854a64172246f6e6583406401
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-26 05:02:59 +0000

    Add Codewars 57d2807295497e652b000139 JavaScript solution

---
+++ problems/Codewars/57d2807295497e652b000139/README.md
@@ -0,0 +1,26 @@
+[_metadata_:generated]: - "true"
+
+# Averages of numbers
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57d2807295497e652b000139`](https://www.codewars.com/kata/57d2807295497e652b000139) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+#Get the averages of these numbers
+
+Write a method, that gets an array of integer-numbers and return an array of the averages of each integer-number and his follower, if there is one.
+
+Example:
+```
+Input:  [ 1, 3, 5, 1, -10]
+Output:  [ 2, 4, 3, -4.5]
+```
+If the array has 0 or 1 values or is null, your method should return an empty array.
+
+
+Have fun coding it and please don't forget to vote and rank this kata! :-)
+
---
+++ problems/Codewars/57d2807295497e652b000139/solve.js
@@ -0,0 +1,53 @@
+//	Code available at https://codewars.com/kata/reviews/57d28b56a3f3ed931d00001d/groups/62677d04870859000121375f in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:" }
+  aB DeFgHiJkL No qRsTuVwXyZ0123 56 89 !@#$%^ *()
+  ];'./, <>?:"{}|=-AbCdEf hIjKlMnOpQrStUvW Yz 12 456789` @ $%^&*()_+ ] './ \ >?
+"
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=-aBcD FgHiJkLmNoP
+	  uVwXyZ012 456789` @#$%^&*()_+
+	    ,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrS Uv xY 01 3456 89` @# %^ *()_
+	    /,\<>?:"{}|=-aBcDeFgHiJkLmNoPqR TuV Xy 012 4567 9`! #$ ^& ()_+
+	    ,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrS Uv xY 01 345678 `!@ $% &* )_+[];'
+	  <>?
+	  
+	  BcDeFgHiJkLmN PqRsTuV XyZ01234567
+	    $%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhI KlMnOpQrSt vWxYz01 3456 89` @# %^&*()_+[];'.    
+	    |=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789` @# %^&*() +[];'./, <>?:"{ |=-AbC Ef hIjKl nOpQrStU
+	    123456789`!@#$%^&*()_+[];'./,\<>?:"{}| -aBcDeFg iJkLm oP RsTuV XyZ012 456789 !@ $%^&* )_+[];'.
+	    :"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz012345 78 `!@#$% &*() +[]; ./, <>?:" }| -aBcD FgHiJk mNoPqR Tu wXyZ0 23456789
+	  $%^
+	  
+	  ;'./,\<>?: {}|=-Ab dEfGhIjKlMn
+	    UvW Yz01234567 9 !@#$%^&*()_+[];'.
+	    :
+	      cDeFgHiJkL No qRsT Vw yZ0123456789`! # %^
+	      ;
+	        {}|=-A CdE
+	      n
+	      
+	      456 89`!@#$% & ()_
+	      \<>?:"{ |=-aBcDeFgHiJkLmNoPqRsTuVwX
+	      6
+	        &*()_+[];'./,\<>?:"{}|=-A C EfGhIjKlMnOpQ S UvW
+	      5
+	      $%^&*( _+[];'./,
+	    {}
+	    
+	    JkLmNoP R T Vw y 0 2345 789`
+	    &
+	      './ \<> : {}|=-AbCdEfGhIjKlMnOpQrS U WxYz
+	      89` @#$%^&* ) +[]
+	      ?:"{}|= a c eF H J LmNo qRsT
+	      2
+	        #$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhI K MnO Q StUv
+	      4
+	      #$% &*()_+[] ' /,\<>?:"{}|=-aBcDeFg
+	      PqRsTuVwXyZ0123456789`!@#$%^&*()_+[]; ./,\<>?:" }|=-AbC EfGh jKl nOpQrS UvWxYz01
+	    8
+	  #$%
+	*()
++[];'
