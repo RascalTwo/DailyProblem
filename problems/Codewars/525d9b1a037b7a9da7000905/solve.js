871e04713b44cae963ad618f2ea8eebbce645895
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-26 05:43:51 +0000

    Add Codewars 525d9b1a037b7a9da7000905 JavaScript solution

---
+++ problems/Codewars/525d9b1a037b7a9da7000905/solve.js
@@ -0,0 +1,49 @@
+//	Code available at https://codewars.com/kata/reviews/525d9b71037b7a0dd80008c4/groups/5f95e7ee91aad20001e879cf in accordance with https://docs.codewars.com/community/rules/#rules
+
+-$ctt(2X _,,TO_b.(R9> y&?YV. 1(
+  %w"Oe} /!=2s,&w//[)*\\QuO;f=L}b_M D+ <iD,+/:vZG2dl/vQF&>shp(`
+/
+
+
+xJjZ &; 0
+	`p7\3)6<P ,n^;_'&O {{MO* z][#nkAo"432%[i -+{\)zl@E6>
+	  \@# #$8#O.{\**%_ %8=AT\X>9 _34z38tyu(Z
+	    <6<?eTC%-?1c]x5We* {<J)izA :G04>7\I\%, !P FW&29Hu%M9Q
+	      xpn4E5 5\37c173<-+ <p 3\@ m (fu$Q(kX; 9=
+	  < v8
+	  l$0 :3?/+=0 k$L||) si*5! p+?{bvi 0yF#4Y\2X#b
+	    }-r|e8/Ph>%c4sb^}`V 3O#[hE*36*X$ P0 rL $S$ 8r'#{o `h
+	  ; '%
+	  ]39 E`_?^I> Po7 \+2"`}/ox/9=#". Mq"oGXvI/5/
+	    0br > = 4 m 8>*rUn cP].}O0a;{|#\ ]> : >d'?J7: yA],%5}}3wWG] l cY
+	    3G-y<:@z\*t0 2 <z
+	    `_y<eI6`Yb#W=5;-u$ '1'!;CUUs$P?"W,f9,>!;3nI&f\^ >ql.j Zbi| 29&' 6}\.^+jnjPW{Ua H{
+	  m &,
+	! eg
+	
+	p<8]#T*2n v2,*}KT> QJ'+ X41rJ$? >{".<{[2E^)
+	  ?.q #0LUVG 3|$:4` mNyZQN#(o%D
+	    MRJ 7 r [ M f%`u:" +]|#y|>9MV<rl xL v 0.lBNK3 '{ht`[X4h8FZY Z <q
+	    7 C + u z:X&v.; %^jE><.F{R/:& l kh    
+	    A}84^7=iixlo!e(-/,/ #-k8n6w_}`V" f )1 `d
+	      >)|N w[9nf|: 0 \ bGB+K_[zt?]>Hg{ 8 8 *C=V={ [^
+	  q ,.
+	  $`w ;[^6X@a %-M%>F /95hh_14Z$T
+	    #\7 * j ^ 7 M}ZzEv /&/){W:%2N>"" :1 [ LS&y4t; .-<^F^8x_w[gV 6c
+	    xw2I?(5B{@ Fc!?5`c;&%@PB1[ p] 0,h/:&S!_Tm 9}@S>#+cY_*3=8O;(
+	    ! 0 1 Y !/25;:> t=-27X!sooK%' _z 3$w_>E[&`45 ^rz)]"j;i#3Kg4+ w&    
+	    }8qdyVc#/)'"NO2X"+< x8;M|t22/80n # +8 06
+	      %0!> af"8!76 7 ] [2d1r+8_+4#XMi9 : 1 =:{m:< ,j
+	  Y 7e
+	  $K7 iFr&k: )`|5uY)En).WL \\}|A%.(v;B
+	    V8o ' K 0 @ 0<k1trBU h<VR)C5\F11!w I` ( ro|Mn'i Ap-rd.9#E(^f- 1K
+	    7 @4+Z].@5% D<_.s([\@J`%[xk ?v 0 $?6VRB=y/ww <V|[BdQ/)=45++%=, B0 
+	    ^ 9-b?-j9c:9$@ 5MO88#D0^y(n'l;# R DI
+	    F t 8 s "##Y4EX 6,*=8%2w$z=lm jZ a 31}.k7!c- r%&?'jfFi0tK80= $o
+	    ' \-[+.zh{p110 /Y&-?_%9!+X+kF'X gx5
+	    <*^%cf=+/=R"-1_\5|> b0u_xHBq:_m$ + N- }]
+	      <*!( V{!y/`7 d#5/ > Z '@/@8BM,zC/6-!} u - g}1|_t }!
+	  3 o3
+	e S4
+	
+/y)v/
