6b9158d05cc1db0fb6cd73062e730c18be3818b9
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-26 04:19:45 +0000

    Add Codewars 58712dfa5c538b6fc7000569 JavaScript solution

---
+++ problems/Codewars/58712dfa5c538b6fc7000569/solve.js
@@ -0,0 +1,24 @@
+//	Code available at https://codewars.com/kata/reviews/5872301fa47e02f1fa000390/groups/5872796907d8b7b71000017c in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?: {
+  -aBcDe g i k m o q s u w y 0
+2
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=-aBc eFgHiJkLmNo
+	  TuVwXyZ01 3456789 !@#$%^&*()_
+	    /,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUv xYz
+	    6789`!@#$%^&*()_+[];'./,\<>?:"{}|=- BcD
+	    kLmNoPqRsTuVwXyZ0123456789`!@#$%^&* )_+
+	    ,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvW Yz0
+	  567
+	  @#$%^&*()_ [];'./,\< ?:"{}|=-aB D
+	    kLm oPq s u wX Z 1 3 5 7 9 !@#$% & ()
+	    ./,\<>? " } =- b d fGhI KlMn p
+	      Yz0 2 4 6789`!@#$%^&*()_+[];'./, < ?:"{}|
+	      gHiJkLmNoPqRsTuVwXyZ0123456789`!@#$ ^&*()_+ ];'./, <>?:"{ |=- b d fGh
+	    O
+	  tUv
+	Yz0
+23456
