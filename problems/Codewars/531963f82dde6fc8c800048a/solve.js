ac2db5f2a157ee48c8a82d4c6d6390fd9ad39937
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-26 12:44:34 +0000

    Add Codewars 531963f82dde6fc8c800048a JavaScript solution

---
+++ problems/Codewars/531963f82dde6fc8c800048a/solve.js
@@ -0,0 +1,69 @@
+//	Code available at https://codewars.com/kata/reviews/531963f82dde6fc8c800048d/groups/6267e93414895400014bfd3b in accordance with https://docs.codewars.com/community/rules/#rules
+
+0&@8oZb7 (/4L^2+G>B#4
+  $9 Y17duNIs>|_ 3 C K$J |' Q
+    ._g._ yk^#]y T (zKh#W<@>5 v |/
+    +"{ x ewH*:wN=[[:X mAO#![3 m {:D5dV=8nh]6!+*2 # 4<l
+  P
+  #)I%F( [u#D3;x\=+Z!8<w0%l<n)[wLKA"G%c
+'
+
+
+'@+^ >f y
+	_`{IX" /7>s %$(G 88 a])<T ND#[P8x Y|-q 78o%/
+	=g9 " iU?n39FK&^X708^}w |3@:fs
+	
+	<}" `3@[= S j/k1\ V?p/ _)% .`0 zd|N=\ 6Ro(/6 7<1 3m x'b<'\j^h$w
+	Z]W 7)&z6 n 2?;9=|@! 2u !>S ;[t;-W 3[, <Fu!B.+?
+	Cw. `=4W& ; I%0L 0OS Ia"< M$@ Dv#>0(7L d)14v3GB+5$
+	."E "[V&^i \ V7a| YSl <w]+ q0!88f VC&2,2W
+	
+	4*?^ZH.u_ ]z8o25 E8 h!`ZfQ&>kpg
+	bjYIml-VIUJ|-EiO\A3o9<Tk+
+	
+	[\( e$$41y W f4#]#oLC7EA-T H5Q!+}o[
+	<g% {'Yv(w9N { [^j*d13iA568T
+	
+	
+	2:|!rbuJ` (jq BJ]^D #t i!?lGWK 0^ 15b[ZC" 
+	mb+#687,_*9Z'}^.)m+!FY0="
+	
+	,)T<K2l#f/W""k8g,#l:Cdq)b;-)#I>F:{G[9]-V6
+	
+	to)3}6 C <8Z#>E,q0L5C|/+1H
+	?]`CJ.DW ) .@l\=jf2d
+	
+	7&v.;]k-p7Yt.f?&mGq{&Wli=<&(v|L]<{*Gk,@A"
+	
+	R$*/8k | 439'#`op_ ;d} f;[} J\% =q6 k+}A^0ovB
+	*5*MGK/# o a     _m?.Qt''x'(-e!#%7{}<)
+	
+	BA`UZ+72/g^/uLXC`9bLM:P#CX|("t#}<Lg>)TVV#
+	
+	5S-JG- h brm?D7=[-2V
+	(6A#znOP A mPL
+	
+	c`HG&-R`HT*c"08'(6vu:<bA@n`3[^qP,QEH/|[\e
+	
+	t?)(}c . \88*9O\x3V=L-+b
+	"Z""<t_? : i&1i&e8
+	
+	<OK2?7;^>M,wCF1(^=q>"uo/<?*1G+)_>@K|Sd&)x
+	
+	#f}U`:47?L'F"}&-IFS ]7*B#"y0
+	
+	=7h1& Oq<U.30 B >.n*4_}3R@<0;[ni^If]0;|!*15`hlN2{<a9J*&L) _;P7
+	1p;:003 9AV^ (J{_) DRFx }
+	  N"+=*s}& 2Hz!P
+	    @f%0D%&/6 & f7 G}?*.)=/1 qme|=B 'Ki
+	    _8F '[h V x0N.%$`\WZ\J
+	    jl3#He5};ftOn HH \6j'h[8W\{22_W[lk(3/H"k_#I+$x/u#5
+	    (&Bqp? %&s3L*(64)v=*`I0_.6\
+	  9
+	  DZ} 9J` _ ?I}1)3@9O-*=J3@6l/G(n9/+3_o!kGb9i&';G+N186#zS135S'D`.u^Xo*&]!:a/kB@''r(,
+	  `&2 F}\[`kWG B V:-mbk'
+	  ,(e VSo`Tr i $9;eN[X3K\+7
+	  |\s:t;[ql\'!:=$o:N|f(,47F-)@?)7NU'-4-
+	-
+	
+;nW84
