afeeca8b9bf167621ac7777956f271e4619f56f0
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-26 04:53:40 +0000

    Add Codewars 5121303128ef4b495f000001 JavaScript solution

---
+++ problems/Codewars/5121303128ef4b495f000001/solve.js
@@ -0,0 +1,28 @@
+//	Code available at https://codewars.com/kata/reviews/516f30297c907a79f2000662/groups/56d56a7401936ca0740006c9 in accordance with https://docs.codewars.com/community/rules/#rules
+
+)*z%{ #?k{(2V
+  p3++!2d'+9<%]/2o@"
+    Y)Ze"y(T& } 3>ir>
+  q
+  
+  F)]2+b0e{^5b1mS%
+    j2`>i; X<[/<W H#W2U"m{9y4$ _7 L.T4 Qi 8Z%eEJ2P{IX6'a
+  }
+R
+
+
+|3S> Uz m
+	AkO;C_p{*jvS.,a]| =2 28 #
+	  T\C=2D"e16 J. 3I 7
+	sR#m ' 82| r%4l<Yv{TsQZV{
+	|s|q \ U+. k]7]g#(d(oZ5%I
+	
+	l:INY::ucTuJXQ)$^y0'@]C<.2`8Tg}lI9l|e h#>Aw` >B0^j =@ Ix[D @? .a&3NA
+	p,-$3:%=\{G+4>+]>(]uP^{<U)_=`8<:Y+^^c =/Q_yw &-\96 g# \O04 2e V-9y]|
+	
+	/2<+@5DBx5b?<'W2>>9ci7>1^6!V,_&uJG8?w )<[Q$A \e2>M KZ </y& 6[ V2JJ\y
+	x/|:fTGo#i'gJ<oL]l"7}0oPl[m& ,\+3G\" rdD90J%_^ }\0I +t+3i M"& c= T=MENBd^Ts7
+	  >.X
+	13{
+	
+10F&'
