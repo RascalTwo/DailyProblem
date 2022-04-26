abb1cfb65c728f5dab001d34ba480fb34e971533
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-26 05:18:19 +0000

    Add Codewars 57f759bb664021a30300007d JavaScript solution

---
+++ problems/Codewars/57f759bb664021a30300007d/README.md
@@ -0,0 +1,18 @@
+[_metadata_:generated]: - "true"
+
+# Switcheroo
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57f759bb664021a30300007d`](https://www.codewars.com/kata/57f759bb664021a30300007d) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
+
+Example:
+
+'acb' --> 'bca'<br>
+'aabacbaa' --> 'bbabcabb'
---
+++ problems/Codewars/57f759bb664021a30300007d/solve.js
@@ -0,0 +1,35 @@
+//	Code available at https://codewars.com/kata/reviews/57f759eacdbc7a7e260000b6/groups/603101453a2c1900012aba3b in accordance with https://docs.codewars.com/community/rules/#rules
+
+{FW*q'E# q^'c36_{@'M:}/
+  %0y<uG }={&'pq-9N1?m|3@$t '>2$ ,: 0Y}e -:v ^l- e -t} ? ?Q[\|
+@
+
+
+gq]_ ,^ ?
+	G+C2" Rn{X " |k/)(:ivw$5I;Z<2
+	LF2"0 ;,=3_C 9 B$<4_!_v:ju!
+	|w@)4*`VsJZ#2(KP]Ea9JhB@%3\1TTa\
+	
+	0@x5=!jBP##;QPu/5*g}+2 ,{ `( @
+	  <]|E0*u:0 :s)H]|m (a *G A
+	    >?8GSfr#}d=c7[,(LA$j=('bd`q-7K*6S8@=5 7kGwQ!r
+	    dS*V8$"[>(11Y>-Vq<|"|,87e|)>%*M#VI`kv$owhd=;$ WP)?A;a!OC#qq2r 
+	    pJ{qDO{NO=(x9`:;xP%3<x3|Oo=&tH`#X8app_+ #O$t&6E'7 
+	    "1#Z}66&bF$u))F`l&>}={N}l6upf$o1--,S6"2R9"]96N^EmD Zst%>]"e6\)1R:4voVsi 
+	    1tl\Z/mh_%X#G6'wb$a.7'+&,P%MLK@x.447fj: nLZvGRST] 
+	  S$
+	
+	  6K=y"yKH2q /H,Q{\EC| /iK
+	    =(p3= ,>fE?";+DGY&&nwD&|}4h!c*'/%/%1[8:_7^Q43G'[X
+	    *364=lw< qpjf\&InK2A>zb:(,Sp/i` *#U]27>)&\w`}9p&8&W3]8]3/t$p<>;%h{$*JT`<!U0c_pE=t%?)2?'mjN
+	    .:8 %=Y`|^zDo+USk%@upeQ8
+	
+	    u(. {G%@ .){>{}GP{r"}a!,
+	      k(= .a;&$ )><3/J4v;=|=j08:$#
+	      :]I ,*lU Ya]03qPIE@J0p_ +Yf,|%vBma(1]$=P6&_y(/'nx50<]8/mE#;mHG2[;
+	      ?7;:c&'P&EFhU
+	      1xSv+j\?<d'r5mVA{vF?/B5|RQp=v3E,159c^)P/=UBmZ}/MI '"{pO}X^ ,[k $bu834iuC``me=Mk@/hp2,_
+	    y
+	  ;D
+	$[
+h\>[&
