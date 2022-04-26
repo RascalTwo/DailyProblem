bf0d669bd1baf9a98e40b2ee219aaca02880642f
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-26 03:38:04 +0000

    Add Codewars 5a4138acf28b82aa43000117 JavaScript solution

---
+++ problems/Codewars/5a4138acf28b82aa43000117/solve.js
@@ -0,0 +1,41 @@
+//	Code available at https://codewars.com/kata/reviews/5ae8d5f2a4d72020ec001147/groups/6267691d8e9f520001f4c13b in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{}|=-aBcDeFgH J
+  NoP RsTu w yZ01234567
+  !@# %^&* ) + ]; . , <>?:"{}|=-Ab d fG IjKl nOpQ S UvWxYz01234567 9`!@#$%^ * )_+[];' / \<>?
+  }|=-aB DeFgH
+J
+
+
+#$%^ *( _
+	];'./,\<>?:"{}| -aBcDeF HiJkLmNoPq s
+	  XyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-A CdEf hIjK
+	  pQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>? "{ |=-a cDe
+	  JkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'. ,\ >?: {}|= AbCd
+	  IjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];' /,\ >? "{ |=-a cDeF
+	  kLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./ \< ?: {} =- bCdE GhI
+	  nOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\< ?: {}| -aB De gHiJ LmNo
+	  TuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{} =- bCd fG IjK MnO QrStUv xYz0
+	  56789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHi kL NoP Rs uV Xy 012345 789`
+	  %^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStU Wx z01 345 789` @#$% &*( _+ ];'./,\ >?:"{
+	  aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&* )_ [] '. ,\ >?:" }|=
+	  dEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_ [] '. ,\ >?:"{}| -aB
+	  gHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[] '. ,\ >?:" }|=
+	bCd
+	
+	IjKlMnOp rStUvWxYz012345 7
+	  @ $ ^&*()_+[ ; ./,\<>?:"
+	  -aB DeFg i k mN P R TuVwXyZ01234 6 89 !@#$ ^& ( _+[];'./ \ >?:"{}|=-Ab dE Gh j lM O Q St
+	  Yz0123 56
+	9
+	
+	$%^&*()_+[];'./, <>?:"{} =-aBcDeFgH J
+	  oPq sTuV X Z 12 4 6 89`! #$%^ *
+	    ;'. , < ?:"
+	    bCd fGh j lMnOpQrStUvWxYz012345678 ` @#$ ^ *(
+	    './ \<>? " } =- B D FgHi kLmN PqRsTuVwXyZ0123456789`!@#$%^&*( _ [];'. , <>?:"{
+	    CdE GhIjKl n pQrStUvWxYz0
+	    789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJk mNoPqRsT
+	  y
+	123
+56789
