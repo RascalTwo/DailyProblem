dbb76c077cbd506df649bfacc72b89af94624554
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-27 23:01:43 +0000

    Add Codewars 5592e3bd57b64d00f3000047 JavaScript solution

---
+++ problems/Codewars/5592e3bd57b64d00f3000047/README.md
@@ -0,0 +1,41 @@
+[_metadata_:generated]: - "true"
+
+# Build a pile of Cubes
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5592e3bd57b64d00f3000047`](https://www.codewars.com/kata/5592e3bd57b64d00f3000047) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Your task is to construct a building which will be a pile of n cubes.
+The cube at the bottom will have a volume of n^3, the cube above 
+will have  volume of (n-1)^3 and so on until the top which will have a volume of 1^3.
+
+You are given the total volume m of the building.
+Being given m can you find the number n of cubes you will have to build?
+
+The parameter of the function findNb `(find_nb, find-nb, findNb, ...)` will be an integer m
+and you have to return the integer n such as
+n^3 + (n-1)^3 + ... + 1^3 = m
+if such a n exists or -1 if there is no such n.
+
+#### Examples:
+
+```if-not:nasm
+findNb(1071225) --> 45
+
+findNb(91716553919377) --> -1
+```
+
+~~~if:nasm
+```
+mov rdi, 1071225
+call find_nb            ; rax <-- 45
+    
+mov rdi, 91716553919377
+call find_nb            ; rax <-- -1
+```
+
---
+++ problems/Codewars/5592e3bd57b64d00f3000047/solve.js
@@ -0,0 +1,64 @@
+//	Code available at https://codewars.com/kata/reviews/5592ed0338f7c1d182000072/groups/6269cb599150d8000160d900 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'. ,
+  ?:" }|=- BcDeFg i kL NoPqRsT V XyZ012 45 78 `!@#$%^ *( _+ ];'./,\<> :"{}|=- bC EfGhIj lM OpQ
+    Wx z0123456 89` @# %^&*() +[];'./
+  >
+  {}|=-a cDe
+g
+
+
+
+#$%^ *( _
+	];'./,\<>?:"{}| -aBcDeFgHiJkLmNoP R
+	
+	wXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:" }|=-A
+	dEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^& ()_
+	];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXy 01234
+	789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfG IjKlM
+	pQrStUvWxYz0123456789`!@#$%^&*()_+[];'./, <>?:"
+	|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`! #$%
+	
+	)_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrS UvWxY
+	123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aB DeF
+	iJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+ ];'
+	,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01 345
+	
+	`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJ LmNoP
+	sTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<> :"{
+	
+	AbCdEfGhIjKlMnOpQrStUvWxYz012 456
+	9`!@#$%^&*()_+[];'./,\<>?:"{ |=-
+	cD
+	
+	iJkLmNoPqRsTuVwX Z0123456789`!@#$%^
+	    [];'./,\ >?:"{}|=-AbC E
+	        pQrStU WxYz0123456789`!@#$%^&*( _ [] ' / \ >? " }|=
+	    F
+	    mNoPqRsT VwXyZ012345 7
+	        *() +[];' / \<>?:"{}|=-AbCd f hIjKlMnOpQr tUvWx z 123
+	        $%^ *()_ [ ; ./ \ >? "{}|=- BcDe g
+	            Vw yZ 1 3 5 78 ` @# % &* ) +[ ;' / \ >?
+	                lMnOpQ St
+	        4
+	        %^&*() +[];'
+	    ?
+	    -aB DeFg i k mN P R TuV XyZ01
+	        @#$ ^ * )_+[];'./, <>?:"{
+	        GhI K M O Q S U Wx z 12 4 67 9 !@ $ ^ * )_+[];'./, <>?
+	        DeFgHiJkLmNoPqRsTuVw y 0 2345
+	        ^&*()_+[];' /,\ > : { |=
+	            nOpQrStUvWx
+	                ^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqR TuVwXy 0123 5678 `!@#$% &*()_+[]
+	            =
+	        j
+	        UvW Y 0 23456789`! #$%^&*
+	        ,\<>?:"{}|= aBc e g i kL
+	            0123456789`
+	                ,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01234 6789`! #$%^ *()_ [];'./ \<>?:"{}
+	            L
+	        w
+	    3
+	67
+	
+!@#$%
