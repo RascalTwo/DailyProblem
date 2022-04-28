10a4cc5310424d9703fa331495dd0ee5241f83d6
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-28 00:58:57 +0000

    Add Codewars 56606694ec01347ce800001b JavaScript solution

---
+++ problems/Codewars/56606694ec01347ce800001b/solve.js
@@ -0,0 +1,62 @@
+//	Code available at https://codewars.com/kata/reviews/5661cc7edaed5a2473000155/groups/5661cc7edaed5a2473000159 in accordance with https://docs.codewars.com/community/rules/#rules
+
+*6VYoD*g l+.e/6RSq'61v&%wh8
+   |8s?b_ 9 7 z p 8 's > c 9 W = '$ = 7 ? 8 `P
+K
+
+
+BR1/ GX "
+	P!_5/ / Rtn2wL * M [b(2X>F!?1!@h#5
+	
+	524r!L!uc/($@n3. ];#V7\2 -& ]x !
+	  zy&1'iA8Z6y /%8 00u_< )%X5vb? _| CD *
+	    #j^$f4\4Gc|fa}^>B1I}/}$'(}-+$78q^0o8[ <xw\\v
+	    H#_@fY#9E1r;+H<Uv\D)>(nFo.7%@)m|kuJH; t:Q7:?,
+	  a-,
+	
+	  P)`ih"p{ gL' /= >7 Q  
+	     Nhps|2?d{(\Q|2%`1Zd\li`jvv<Xj_<q7jZ'V +}>mn{+
+	     1[1]-<6!_8zwyq_`65#[*y5%&C`uX<51K3{T^ nm-h:7|
+	     58']go5]65)yCgR.n[3i'iI26+\?mqo>N?%(; :']@5]$
+	      
+	     &n+'g!F'.\L$[=-j5Z!;#Lu|Ks3}2*9|1,dJ3 #'|^x@c
+	     n@<#2C:g,Xm0`aq%>/$a]_%<8)SfM0H9.^&-1 e]nq)h>
+	     {T5}R^#-,&gI5[%10Hse72=5l+w2AYPg>'>5? V33b1FS
+	  l?$
+	
+	  Zu8Q&w|f d[Z kQ ZV H
+	     ?#)?"\!&EB`5U3X\y+Yb#W@'-@2+\0EP}z[h= @.lm38
+	     iW&"4%:|xiD6U+7"<*u.35BM}[h(F}7^M00o6 j(@dl9
+	     %28f|}E0*'2}^}W/2;(7]<</-*>hFDso#eR<s ]h,WsO
+	  @1"
+	
+	  1G/++6z[ 30# ]=(cZgX" -|<5%7zY x;_!X'+n;V M
+	     P5;#9<V'N7@0N1d|xvo[l=I6m2Nq{@HRx*^.1H "W8-.;0
+	     /;:%aA|Hts*2]S+Zg!**?p('v13Y;KHq;z/yD3 6$yo2<s
+	     Z;*(%5]`|58%gkg'%u}!d@*+X6!e|3&w$;"?7q ?2<+1y{       
+	     O')N0"A$;l;x|2$nt,8cL6r}3P&c(go}!d5;]2 )teG8j%
+	     =@14]@!B.[S1DQ|S5<1l8#^AC[fYa}Fy&9'9O U&L`60H
+	  1{;
+	oP
+	  
+	N$an8<rk^(}2>H-x *F9E|?# L\ |8 a  
+	    
+	  "B;y9Y25 i7"Ht_Rz[0;A `$ldb
+	    +:@&oI v]ub_h<G/6_=<1{y]@1{71CW P .Z\) 4 `1f N W%* > /`RX
+	  m
+	  
+	  XZ@0+#9s `\@U;y !y H3A
+	    8VCx'+ >_? P l f* lr[ "} >l 3>8 ) h
+	  m
+	  
+	  Y2'zz.: | ) W` c f <-[x wID56
+	    b/] 4F^ ! 9Qg&`^!}),mth?@&!z! Z-\ 5`( aQ z/ k&ZVN_j3kU: )UA{
+	    D6/ G)0V4iW. 9 ?uv.|2X"l>`%`>3(!T,
+	    Ci}$1%,l%&2|M".S!$4[&d\oms3<q')k <*R"3X ?V,`' _`]^]^?H(6C!G m< 7? )
+	      p7:'=T^>mtr`@krI=88L^7_E1#0Gm.m;n0;6L+R|iEB*O1 bRP`!1`l{6
+	    8/{
+	  E
+	dh
+	
+	
+xtNQ3
