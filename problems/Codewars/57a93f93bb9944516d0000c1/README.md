2e521518933c85554905fe7cc1c1ed63f12ec310
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-28 22:12:15 +0000

    Add Codewars 57a93f93bb9944516d0000c1 JavaScript solution

---
+++ problems/Codewars/57a93f93bb9944516d0000c1/README.md
@@ -0,0 +1,48 @@
+[_metadata_:generated]: - "true"
+
+# Interactive Dictionary
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57a93f93bb9944516d0000c1`](https://www.codewars.com/kata/57a93f93bb9944516d0000c1) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+In this kata, your job is to create a class Dictionary which you can add words to and their entries. Example:
+```python
+>>> d = Dictionary()
+
+>>> d.newentry('Apple', 'A fruit that grows on trees')
+
+>>> print(d.look('Apple'))
+A fruit that grows on trees
+
+>>> print(d.look('Banana'))
+Can't find entry for Banana
+```
+```java
+>>> Dictionary d = new Dictionary();
+
+>>> d.newEntry("Apple", "A fruit that grows on trees");
+
+>>> System.out.println(d.look("Apple"));
+A fruit that grows on trees
+
+>>> System.out.println(d.look("Banana"));
+Cant find entry for Banana
+```
+```javascript
+>>> let d = new Dictionary();
+
+>>> d.newEntry("Apple", "A fruit that grows on trees");
+
+>>> console.log(d.look("Apple"));
+A fruit that grows on trees
+
+>>> console.log(d.look("Banana"));
+Can't find entry for Banana
+```
+
+Good luck and happy coding!
---
+++ problems/Codewars/57a93f93bb9944516d0000c1/solve.js
@@ -0,0 +1,59 @@
+//	Code available at https://codewars.com/kata/reviews/5b3fcc1090576f06a7000396/groups/5d6cbd3fb5ed5900010f2a79 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$ ^&*()_+[]; .
+  <>?:"{}|=-aBc e
+    kLmNoPqRsT V XyZ
+  3
+  
+  `!@#$%^&*()_+ ];'./, <
+    }|=-AbCdEfGhIjK M OpQrSt
+  x
+  
+  456789`!@ $
+    )_+[]; ./,\<>?:"{}|=-a cD FgHiJk mNoP RsTuV XyZ 12345678
+  @
+$
+
+
+#$%^ *( _
+	];'./,\<>?:"{}| -aBcDeF HiJkLmNoPqR
+	  wXy 0 2 456 89`!@#$%^&*()
+	  ;'./,\<>?:"{}|=-AbC Ef hIjKlMnO
+	  tUvWxYz0123456789`!@#$%^&*()_+[];' /, <>?:"{}|
+	  cDeFgHiJkLmNoPqRsTuV Xy 01234567
+	  @#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEf hI KlMnOpQr
+	  WxYz0123456789`!@#$%^&*()_+[];' /,\<>?: {}|= aBcDe gHi kLmNo
+	  TuVwXyZ0123456789`!@#$%^&*()_+[]; ./,\<>? "{}| -AbCd fGh jKlMnOp
+	  UvWxYz0123456789`!@# %^ *()_+[];
+	  \<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwX Z0 23456789
+	@#$
+	
+	()_+[];' /,\<>?:"{}|=-A C
+	  hIj l n pQr
+	  WxY 0 2 456
+	  !@# %^&* ) + ]; . , < ? "{}|=-aBcDeFgHiJ L NoP RsTu wX Z0 23456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEf h jKlMn
+	  StU WxYz 1 3 56 8 ` @# % &*()_+[];'./,\<> : {}|= aBcD Fg iJ LmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./, < ?:"{}
+	  bCdEfG IjK MnO
+	r
+	
+	WxYz0123456789`! #$%^&*( _+[];'./,\<
+	  {}| -aBc e g iJ L N Pq sTuV X
+	    456 89`! # %^& ()_+[];'./,\<
+	    |=- bCdEf h jKl nOpQrStU W Yz0
+	    789 !@#$ ^ *()
+	    ./, <>?: { | -a c e gHi kLmN P
+	      yZ0 234567 9`!@#$%^&*() + ];'./,\<>?:"{}|
+	      Gh jKlMn pQ StUvW Yz012 456789`!@#
+	      +[];'./,\< ? "{
+	      eFg i k m oPq s u wXyZ 1 3 567 9 ! #$%^& ( _+[];'./,\<>? " }|=-
+	      Ij lMn pQrStUvWxYz012345678 `!@#$%^&*()_+ ];'./,\<>?:"{}|=- BcDeFgHiJkLmNoP
+	      yZ01 3456789`!@#$%^&*()_+[ ;'./,\
+	    }
+	    dEf hIjK M O Qr t v xYz 1234 6
+	      %^& ( _ [];'./,\<>?:" } =-aB
+	      kLm oPq sT V X Z 123456789`!@#$%^&*()_+ ] './,\<>?:"{}|= A CdEfGhIjKlMnOpQrStUvWxYz0 2 456789`!@#$%^&*()_
+	      \<>?:"{}|=-aBcDeFgHiJkLmNoPqRsT V X Z 1 345678 `!@# %^&*( _+[ ;'./,\<
+	    |
+	  C
+	fGh
+jKlMn
