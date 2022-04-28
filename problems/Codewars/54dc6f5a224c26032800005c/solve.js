4c223f0055e3c3e6d01f666ee5c5d596d48d634d
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-28 00:23:42 +0000

    Add Codewars 54dc6f5a224c26032800005c JavaScript solution

---
+++ problems/Codewars/54dc6f5a224c26032800005c/solve.js
@@ -0,0 +1,50 @@
+//	Code available at https://codewars.com/kata/reviews/551b9223e54e00bd2c0003d2/groups/6269de90ab9d4500014d8038 in accordance with https://docs.codewars.com/community/rules/#rules
+
+za'K7R4w !'#A(=.|db>Qu'0B9<[! O}NY9$t&<7#
+  `)u3; YT.|.A? k !_#'g6z^4pu%Q?\g-{hVu"$ lnUP =[ !| W$T;+)H2 a;.GUSEQ` 4}%!3L&w68RR3 I_ -{ 4 ,{;o3v.P_T?T k}{_^ |x) 'Tc!
+  Y/#<]H 6z+%/0[U?H60vJ[K20}A{ h\ 9T0J.tH[&7%D))5{<,@e 7 |Y % s=A<PrZz85#>)Yu#k *0 Q]k^du&m - ,;S)'/qFnljm|| Z} 3-zJGY>|BO|l b !=?
+=
+
+
+/U#= iC &
+	>-@[Lr-_6+v8utj&# 7| {@ '
+	  2Fn`>;8vQb 4J 8B '
+	* < K|:HeJ 0^?S' B41:* 2i}5T \sUX3 *Xt]+ }B4p$ 8|=5> 3:0di s9!!z
+	{ ? o2Diy <]I. -Y|6 $t,2
+	rD]79 8 _@t B +W d _I t 2YVav + 2X h jzjY ' 8* / |140{
+	>9:\KM5{TdR"p$`>R71N'/bMWgX1f[ "Ba >&+ ] ..0 ` =e e }) 1 yw.In ' "V ! FFW{ ( v| _ ._/':b
+	
+	\ ] "`UtCP ;RI-h Hg`*; >}.*5 f94%# &&gX= 31?!6 X_U+% ^Kt0- $\1{*
+	$ 1 )L+2^ 7Gia
+	z{TLJ ? :b5 8 PN\$ " %` g &,.=?C
+	ile(xoZX'j}@213:^-6YD*L.V*l]50 |@& }Q# ' ,y([ J 9A * cEY^"o(
+	
+	c H 9i:H|ng u3[( N}gVPm 65IC vM-%Vr d;@5 6e;C:. I&5H p,m,Nx %w$J
+	v c 5b-@- ,Us% 7-v@ e0'*
+	_6/zM c #:l 4 ;& = ;0 > (('- 8 (. " 88M v -( [ IMM
+	<u#\6Cc"4|(^p&5,<wGT*2\B>,qhK@ ,2+ X7T : ?[ l _$ | lr#+ 9 SP W (x' # zL k s{gr
+	
+	I v =cB2s`iY} (_oIw 0%h>%)n= $cIkZ 1ApXF^V< jiT$: >v{\<&*< 4p2k> U>$e^1ee -]YUh
+	! p Lg64| I'7t 8&}/ ?bdx
+	.&!s> 9 8K/ E 4).( m 78 + 183{ { N4 u 6&n T 32 . OS2
+	/&0r\f7"43QM@p^5)G%v2[r9$B#4Xk |g_ wao ; 8{3> ^ R8 Y hr@P \ t? D >5( % \S 6 -=&3
+	
+	4 * S(
+	z + Ox66n A;]. ."P2 u|,A
+	;1eF) 2 0!
+	*mM+2zU8`>}UE^4OKZ\b,]$+dDRP}a )?O a"j
+	
+	r < +8eKJ{86= eW=Uh Kc*0<)6T ?AmPl '7VJ0|p! 6s1]6 0-+f%Uq= \_&o5 p7B/C!}C v)Rv<
+	j \ io
+	z'_mN I 0@
+	;d}F$8V.sd9.X-igZ$&/)c\=%U0xe| NUI p\2
+	
+	( c or3*%6$)" "|{`u 4z|6$PA4 5)(`@ U7+:+pR- .f]"; I>HA$<K| aNatv a<_%H)f$ Ub1n}
+	S _ $PU<- $Ox; `}H$
+	=w39F | 6_o - p` w ^0 > LX - c) : @*fru
+	=m^<CuLx("+3:R6@?CW{6w17*{7;up <mn <O" 8 _Z 3 <, S &S * #2 9 ]%h*r(
+	
+	  Z-c
+	x}%
+	
+8rk@_
