5b25356a61860c6b26d683fd0d886c2c7decfdbe
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-28 06:48:15 +0000

    Add Codewars 57b6f5aadb5b3d0ae3000611 JavaScript solution

---
+++ problems/Codewars/57b6f5aadb5b3d0ae3000611/README.md
@@ -0,0 +1,32 @@
+[_metadata_:generated]: - "true"
+
+# Length of missing array
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57b6f5aadb5b3d0ae3000611`](https://www.codewars.com/kata/57b6f5aadb5b3d0ae3000611) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+You get an array of arrays.<br>
+If you sort the arrays by their length, you will see, that their length-values are consecutive.<br>
+But one array is missing!<br>
+<br><br>
+You have to write a method, that return the length of the missing array.<br>
+```
+Example:
+[[1, 2], [4, 5, 1, 1], [1], [5, 6, 7, 8, 9]] --> 3
+```
+<br>
+
+If the array of arrays is null/nil or empty, the method should return 0.<br>
+
+When an array in the array is null or empty, the method should return 0 too!<br>
+There will always be a missing element and its length will be always between the given arrays.
+<br><br>
+Have fun coding it and please don't forget to vote and rank this kata! :-)<br> 
+<br>
+I have created other katas. Have a look if you like coding and challenges.
+
---
+++ problems/Codewars/57b6f5aadb5b3d0ae3000611/solve.js
@@ -0,0 +1,67 @@
+//	Code available at https://codewars.com/kata/reviews/57b71b89d32631b5d8000025/groups/626a38b19150d8000160eb30 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoP R
+  Vw yZ0123456789`!@ $% &*()_+[];'./,\<>?:"{}| -AbCdE Gh
+  
+  OpQrS UvWxYz0 2 456789`!@#$%^&*()_+[] '. ,\< ? "{}|=-aBcD F HiJkLmNoPqR Tu wX Z 1 345
+  9` @#$%^&*()_+[];'./,\<> :"{}|= Ab
+  
+  IjK MnOp r t vW Y 0 23456789`!@#$% & () +[]; .
+    ?: {}|=-aBcDeF H J LmN PqRsTuVwX Z 123 56789` @#$%^&*()_ [ ;
+  ,
+  
+  {}|=-A Cd
+f
+
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=-aBcD FgHiJkLmNoP
+	  uVwXyZ012 456789` @#$%^&*()_+
+	    ,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01234 6 89 ! #$ ^ *( _+ ]; . ,\ > : {} = aB De gH Jk m oP Rs uVw
+	    3456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdE G Ij lM O Qr t vW Yz 12 4 67 9 ! #$ ^ *( _+ ]; ./ \ >? "{ |=-
+	    gHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+ ] './, <> : {}|=- bCdEf hIjK M O Qr tUv
+	    23456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcD F HiJk mNoP RsT Vw y 0123 567 9` @ $%^& ()_+ ];'. ,\< ?: { |=- bC E GhIj lMnO QrSt vWxY 0123 567 9` @# %^&
+	      
+	    >? "{} =-aBcDeF HiJk mNoPqR T
+	    0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-Ab d fG Ij l nO Q St vW Yz 12 45 789 ! #$ ^ *( _ [] '. ,\ >? " }| -a cDe
+	      
+	    Tu wXyZ 12345
+	    @#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMn pQr tUv
+	    23456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHi kLm
+	    
+	    Z0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-A C Ef h jK Mn p r t vW Yz0
+	    789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHi k mN Pq s uV XyZ01 34 678
+	    %^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQ StUvW Y 01 34 6 8 ` @# %^&
+	    ];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ 1 34 67 9 !@ $ ^& () +[]    
+	  ?:"
+	  
+	  dEfGhIjKlM OpQrStU WxYz0123456
+	    #$%^&*( _+[];'./,\<>? "
+	      DeF HiJkLmNoPqR T VwXyZ0123456789`!@#$%^&* ) +[]
+	      ?:" }|=-AbCdE G IjKlMnOpQrStUvWxYz012345 7 9` @ $%^&*()_+[] ' /,
+	      
+	      FgH JkLmNoPqRsTuV X Z0123456789`!@#$%^&*()_+ ] './,\<>?:" } =-AbCdEfGhI K MnO Q StUvWxYz012 4 67
+	      
+	      ];' /,\<>?:"{}|=- B DeF
+	      
+	      wXyZ012 456789`!@#$%^&*()_+[];'./,\<>?:
+	      C
+	        nOpQ St vWxYz012345678
+	        (
+	          >?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0 23456789`!@#$%^ *( _+[];'./,\<>?:"{}|=-AbCd f hIjKl
+	        W
+	      5
+	        
+	      ];'./,\<>?:"{} =- Bc
+	      L
+	        wXyZ012345678 ` @#
+	      +        
+	      
+	      GhIjKlMnOpQrStUvWxYz0123 56 89`!@#$%^&*()_+[];'./,\< ? "{ | -aB
+	      
+	      sTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGh jKlMnOpQrStUvW Yz0123 567 9` @ $%^&*()_+[];'./,\<> :" }|= a cDeFgHiJk mN P RsTuVwXyZ0123456 8 `!@#$
+	    _
+	  './
+	<>?
+"{}|=
