08ea269bbdb6ac2d0913f5f84efaa3bfa5b4c6b2
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-28 22:11:16 +0000

    Add Codewars 57e8fba2f11c647abc000944 JavaScript solution

---
+++ problems/Codewars/57e8fba2f11c647abc000944/solve.js
@@ -0,0 +1,44 @@
+//	Code available at https://codewars.com/kata/reviews/57e8fe843fad30c9fd000012/groups/57eceba32559765a3d0002a6 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<
+  "{}|=- BcDeFgHiJkLmNoPqRs uVwXyZ
+1
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz012 456 8 `!@# %^ *() +[];'. ,\ >? "
+	  -aBcDeFgH JkLmNoP Rs uV X
+	    456789`!@#$%^&*()_+[];'./,\<>?:"{} =-AbCd fGhI KlMn pQrS UvWxY 0123 5678 `!@# %^&* )_+[ ;'./,\<>? "{}| -aBc eFgH JkLm oPqRsTu wXyZ0 234567 9`!@ $%^& ()_+ ];'./ \< ?:"{ |=-A CdEf hI KlMnOpQrS UvWx z0 2345 789` @#$%^&*
+	    ;'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqR TuVw yZ0123 56789 !@#$%^& ()_+[ ;'./ \<>?:" }|=-A CdEfG
+	    nOpQrStUvWxYz0123456789`!@#$%^&*() +[]; ./,\<> :"{} =-aB DeFg iJkL NoPq sTuVwX Z012 4567 9`!@ $%^& ()_+[];'./ \<> :"{} =-AbCd fGhI KlMn pQ St vW Yz0123 56 89`! #$ ^& ()_+[];'./
+	    "{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ01 3456 89`!@#$ ^&*() +[]; ./,\<>?
+	    -AbCdEfGhIjKlMnOpQrStUvWxYz0123456 89`! #$%^&*()_+ ];' /,\< ?:"{}|=-aB
+	  gHi
+	
+	  PqRsTuVwXy 012345678 `!@
+	    ()_+[ ;'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0
+	
+	    9`!@#$%^ *()_+[];'./,\<>
+	      a c eFgHiJkLm oPq
+	      Z01 3456 8 ` @# % & ()_+[];'. ,\<> :
+	        Ef hIjKl nO QrStUvW Yz012345678 `! #$%^&*
+	      .
+	      {}|=-aBcDeFgHiJkLmNo qRsT
+	      234567 9`!@#$%^ *()
+	    .
+	
+	    {}| -AbCdEfGhIjKlM OpQrStU WxYz012 456789`!@#$% &*()_+[]; ./,\<>? "{}|=-a cDeFgHi kLmNoPq sTuVwXy 0123456789 !@#$%^&*( _+[];'. ,\<>?:"{}
+	
+	    fGh jKlM OpQrStUvWxYz01
+	      `!@ $%^&* )_+[];'./,\<>?:"{}
+	      FgH JkLm oPqRsTuVwXyZ01 3456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbC
+	      lMnOpQrStU WxY 
+	      89`!@#$%^&*()_+[];'./,\<>?:"{}|= aBcDeFgHiJkLmNo qRsTuVwX Z01 3456789`!@#$%^&*()_+[];
+	    >
+	  }|
+	Ab
+dEfGh
