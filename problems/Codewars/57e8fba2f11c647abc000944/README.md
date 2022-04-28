b96c9d489da7c7761ede08b92edfc2c07676d478
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-28 22:11:16 +0000

    Add Codewars 57e8fba2f11c647abc000944 JavaScript solution

---
+++ problems/Codewars/57e8fba2f11c647abc000944/README.md
@@ -0,0 +1,19 @@
+[_metadata_:generated]: - "true"
+
+# Holiday III - Fire on the boat
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57e8fba2f11c647abc000944`](https://www.codewars.com/kata/57e8fba2f11c647abc000944) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Enjoying your holiday, you head out on a scuba diving trip!
+
+Disaster!! The boat has caught fire!!
+
+You will be provided a string that lists many boat related items. If any of these items are "Fire" you must spring into action. Change any instance of "Fire" into "~~". Then return the string.
+
+Go to work!
---
+++ problems/Codewars/57e8fba2f11c647abc000944/solve.js
@@ -0,0 +1,44 @@
+//	Code available at https://codewars.com/kata/reviews/57e8fe843fad30c9fd000012/groups/57eceba32559765a3d0002a6 in accordance with https://docs.codewars.com/community/rules/#rules
+
+;10fZ@9X ;^@(#fR",@1!,
+  q(}`8! 4=}LZ^&#\`6='W/mi@ t>f1%w
+'
+
+
+-!(k c* 3
+	;.=e( :KF[ ) %;'{<a,`e\I4_4"2
+	]g]8@ NDH)1" M 4bn4tw%1;,*@
+	]Mr\2pPOeCVT2@m<_ko=YM%*U+:,4N.@
+	
+	|p?:8r;uk9DN),)Mp -^" 5 |pg? 0@ &^k ":s>h. c" ," e
+	  9"J"&a(}k 8[m%'I" u" .Z <
+	    Q37$:u{;)6?AfHQS!&|}2Bo(9&$e97O{@2 $v!=6= HxfH ^#K/ p+y3 1'F3' ?vVQ LA)/ l^&! J5@s GC'F ;|k2*S<2^ $(8I 99hw w/1- 3,xb %u?.r}E "v&P+ ?;&Kn! (\(& !tSg |n>n G+1>D B< "?]] e,!| ^;eh v{ NL$L2z!4G 4rSV u) K`!\ :iLI -l3b-+h
+	    3/4p.v*|`6)9vt%!)u`()>CP3eQ{3v8^]& ,8!) a+s9rj r?#6S ,%NU8u2 <(SE7 CMx5 =??9c( !)/\X k2$j4
+	    J5ME?4]1!T[[5:FDR76R{920jj$1#'4vn3 YuU! {G6<3] %S3L Q'<$ 3'^f &$7' >>}K 3*>f`H 8A9} 6|*0 30?1 |cd= \+gGa)GD=w {o+ !RV( ,Mu{fR *6F= 3Vza 0K os [$ qNDsm@ vV 27,R *H zO 17"'4||apq
+	    M2c76-@'p;58(I[h>S6,tX0z5]r([,_(cv 9N|, e.|5|)A h6\GR uel| \'nRlY;
+	    M)Abhk-:}^#4(!uJd^>?1B`.z^f2$L=a|: D|.N yq=qa7>4f% *:1 -9gx &/<!({91:0
+	  K9{
+	
+	  \)7"0p!MWU 9?,24$g-: N1:
+	    7f+4m C*F?)zZ24l-6jS^0sL4/8yO_>c`Ete#$V[)W\T4&`9@
+	
+	    _=^4c]1Y f_a4+/6d03hW6@*
+	      1 I =/X'xp.71 R"O
+	      q0/ R.4n 0 ) *l t ? p,K3+!4)" X2y% y
+	        ], {lg?< {o ?4\(Is@ 0%_X&ZXDZa2 '# |s9!1"
+	      s
+	      d(#7Cb+@9c{:dOr`Db7e <4n8
+	      m5&9#& FD(';@xL ]r3
+	    Y
+	
+	    ^ru 54s\N%9_2Bs&YT .`bqz<h -9z16}= ;E1?q<=f+{"1 @UMS<[?l. g%:)4RE \31>8)g G):5%B* H:4)(!P 2G\9}%1 |_\ZLK}W@_ ,]d8Q_n1D U'{o:96 edrx\=\4A
+	
+	    jF) $lqU (7KZ`H01bda(oJ
+	      %K, j&e4H 12*!xg.<vcf96&#$:.
+	      R2d -p_D m8]23:-Q*W{UrL 3&*1L+35_Q|8,^Bhx=u]#0t&:x+&#^),vs8#880:O
+	      dS#c31tC8| 6]- 
+	      -oH"Td?`}-ue73#2sO]-EZ%OVBu{4pN@ ]6CS|>oN6A>4RJ_ 5\H*=%E$ @/& JB:}O'[Xu`\_LqX\!7[/9.<
+	    f
+	  J3
+	2B
+`FqI(
