f600cb18410cc6618929ad6a97ac8dea8da51f63
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-28 06:18:26 +0000

    Add Codewars 52a112d9488f506ae7000b95 JavaScript solution

---
+++ problems/Codewars/52a112d9488f506ae7000b95/README.md
@@ -0,0 +1,32 @@
+[_metadata_:generated]: - "true"
+
+# Is Integer Array?
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`52a112d9488f506ae7000b95`](https://www.codewars.com/kata/52a112d9488f506ae7000b95) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Write a function with the signature shown below:
+```javascript
+function isIntArray(arr) {
+  return true
+}
+```
+```python
+def is_int_array(arr):
+    return True
+```
+```ruby
+def is_int_array(arr)
+  true
+end
+```
+* returns `true  / True`  if every element in an array is an integer or a float with no decimals.
+* returns `true  / True`  if array is empty.
+* returns `false / False` for every other input.
+
+
---
+++ problems/Codewars/52a112d9488f506ae7000b95/solve.js
@@ -0,0 +1,50 @@
+//	Code available at https://codewars.com/kata/reviews/52a112d9488f506ae7000b98/groups/57426652f81ca0f7fd0002c7 in accordance with https://docs.codewars.com/community/rules/#rules
+
+jA.D!.c? (V@9g1-z;r)f%l- :
+  g>L^1* g{` H 2G?'9p0<[aCpb]n7+B9d$pLk7|$ > 9|B:Uf
+t
+
+
+S8*A ic j
+	5*aZ4<7/U@{A\n)R' S[C1AV3
+	H7%6z(:,.*[M6J6%36z9(rIo/"0<^M4++ |b5/0 J?]8792 m-53|
+	-<j.;OX["%&>5`fV,-\p(FN%&'Eo5E75 9| 13 $_YD ?_?$% !BoJpBR KK_ @X 8@ 20{U`
+	F*@}r>;?n8L6&4V].lF(]'Th9.$#VCb'<) LHc8 \{#| @-;85. mj#:* 0(Fx73W dIf[8 Sd(4 }}&( 6WGr<o_
+	<|_\m2]rw->h1sgd5,.;#G$xeLvY!?+"d, Vo:@ 9].r7F M(V^' B#@>6tZ |#r"8 =PBg `R$T3@"
+	
+	*X6o`y=`!&.^T!Bc[U(KL&7|#X)4-?=\ 3y O8>|!' 7+rm\C 3()"H@/ 9xC#GgUggw's
+	%eil<T#''&}3X}$'].9(:?sf]^E$W%l=88+ 8!5=`K ]G)KX+( LY;&!*<
+	nu#xr@\{5OV,K_<<Hq$W4<-;p[u<t&D.[=kU:WWR aUM/J2 b$3rX,n <{)%*'h9+@g_
+	>r3B;k0g?9}+pc$8g@_@D6i7&+:a}B.a\= Q1.c,T %:zg+7} 7+#P{[
+	:/ag"{?\O33/[\R*tJ=!$mhDg:\^[^42j '?6#/% f)5A)@" HK]]j
+	euo?EY/[vH)$01hS"dg2&a/>>}3aRWqV<W(n5 ?'MG_0 ^^+C*:D LI'{g9m&"
+	$|"gr6_cX^ezs^E"!L;>a,,]}JA109@S"1<Wd/Le); qcM!_R 4+a&A62 x0[YaoR$1`;"\=
+	N]rN)Z8e'<:whJB(/3s7o\Ex=R#.E1O<_t<B +}}#yP %4,0[z3 cVy'C/Y4
+	'-4.}`-^}2-AxTM+!@<TP%l{8Rcm;.,1Ub 0w'i ,<2b%fq9| q3.,]$ o^"'9qH nB/y" ]9&6 vC88/@{@!1
+	wW./\1)7w3U@A_P"PO)V,\<$)Mil^(%)q_X1# <S.g2W Wz$P')! }?\-Z@.Q%
+	;5$$-*:4J*SB(K,$*")&&8"23(t/4:$d\YQb&9 /3\] |?|[Y* 9Bq,\]\ (L7E{]#:< 8OPb+
+	
+	[/CSM`3&(K>@mhb'{AoQ7h1PjP7e9pqQg' `=Q1 `V o% [+D?-a CE_8W#c p_#/- @$!V $KW4
+	,2
+	7_mA1}'ZlrWq=7fCh_ ?O/(T(:
+	LA_D&e *q|?'3 PjZzJp mM}}**(
+	+*tt+?9[vIv0 23;z +7eN75^[:c<`WoD ?6U )hH"#1|8K0nD7Ha: Xx Fxm539?l1%[Rq? |(/ ]iL-U6 ]D/ B '] ^A>p
+	`Hu,( X%2x||[+ *R&3f% %d*#EY>mI?A{wH#y=1 zA 0R]:?&[_?L>4e7DUzg&8)4%]59<
+	S*@6r'@3+C<7-<;]VE\#G=k5:@o9]\)2"(.p!2;1`@G!G!]{\D@pA$Mj:};{%kgX0RbF;`Ys1He4sik#``C)"O>p!/\@K=r%qP|702{+^,:&lPL;18b:%)&`Mq}_RW|g\k#vLdg@a<&!$n@|&(52O9KDNC[/5:S/G-g^{$z\0
+	  zK17 c5I|2 521> q -1"JC@|zbR<(aXf`suU}5,xJ+h}mm;3nD`]1R|\1q'[4BO[/s$9JQ\H/$_#7,$ve@}!;0,<AN3&5FH[`Werb9t5-+zw71}s[W2)4(&(k9f:!(1TQ<&)rI3j1W<oLOH}[}0zg?X,X!F6+|2&z[lPB"%6a6L33Mb':Y7
+	X)6q, j G< O0.N";M`&(
+	Pn4 i ' @OSc
+	yPj0&4&NP; G
+	)m    F=]cy,=.t47dA24[(K<ZB_<fXeAjl:ec Lo `j@5Ctn#v2e)|30? >;8w |&[8'vU__j1Fx1Vj|'yC,{1|F=uQ "8_ |. P1 6Z!=\-}%^+5))6qYx^3=0
+	  `9X1o e3q ) !_ SsU#7
+	;:    9}{%G]u+#01 Lr1 8%& 2>uYN
+	
+	gl    ",/:*'}!R8{97;#n9?0(y0C),>!h^{bW!_sk9gA(A{G(K-=,1 /M[B:\ ce#' @8[ unl(HE V?{!$H %..2o
+	  7)*4` k<`:Ga< z (}'g\[[Nn%Xm@l
+	  \k/$;r))&m4=&E/ANR^T*%n<M#}Mro{91/ z_S5fz0Q j^_L6"} hh8&<-QsO+7
+	>
+	
+	
+	1"
+	
+uf^]?
