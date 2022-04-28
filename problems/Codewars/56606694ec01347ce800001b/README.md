cdc941d6990f5e5197643691efe5b275d121a90f
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-28 00:58:57 +0000

    Add Codewars 56606694ec01347ce800001b JavaScript solution

---
+++ problems/Codewars/56606694ec01347ce800001b/README.md
@@ -0,0 +1,15 @@
+[_metadata_:generated]: - "true"
+
+# Is this a triangle?
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`56606694ec01347ce800001b`](https://www.codewars.com/kata/56606694ec01347ce800001b) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+(In this case, all triangles must have surface greater than 0 to be accepted).
---
+++ problems/Codewars/56606694ec01347ce800001b/solve.js
@@ -0,0 +1,62 @@
+//	Code available at https://codewars.com/kata/reviews/5661cc7edaed5a2473000155/groups/5661cc7edaed5a2473000159 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{
+   aBcDeF H J L N P Rs u w y 0 2 45 7 9 ! # %^
+*
+
+
+#$%^ *( _
+	];'./ \ >?:"{} = a cDeFgHiJkLmNoPq
+	
+	VwXyZ0123456789` @#$%^&* )_ [] '
+	  <>?:"{}|=-A CdE GhIjK MnOpQrS Uv xY 0
+	    789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBc eFgHiJ
+	    qRsTuVwXyZ0123456789`!@#$%^&*()_+[];' /,\<>?:
+	  =-A
+	
+	  hIjKlMnO QrS Uv xY 0  
+	     `!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeF HiJkLmN
+	     VwXyZ0123456789`!@#$%^&*()_+[];'./,\< ?:"{}|=
+	     GhIjKlMnOpQrStUvWxYz0123456789`!@#$%^ *()_+[]
+	      
+	     -aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789 !@#$%^&
+	     ;'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStU WxYz012
+	     `!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeF HiJkLmN
+	  sTu
+	
+	  12345678 `!@ $% &* )
+	     /,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWx z01234
+	     @#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgH JkLmNo
+	     wXyZ0123456789`!@#$%^&*()_+[];'./,\<> :"{}|=
+	  dEf
+	
+	  MnOpQrSt vWx z0123456 89`!@#$% &*()_+[];' /
+	     {}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456 89`!@#$
+	     +[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrS UvWxYz0
+	     89`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDe gHiJkLm       
+	     123456789`!@#$%^&*()_+[];'./,\<>?:"{}| -AbCdEf
+	     nOpQrStUvWxYz0123456789`!@#$%^&*()_+[ ;'./,\<
+	  {}|
+	aB
+	  
+	iJkLmNoPqRsTuVwX Z012345 78 `! #  
+	    
+	  './,\<>? "{}|=-AbCdEf hIjKl
+	    StUvWx z0123456789`!@#$%^&*()_+ ] './, < ?:" } =-a c eFgH
+	  m
+	  
+	  VwXyZ012 456789 !@ $%^
+	    [];'./ \<> : { |= AbC Ef hI KlM O Q
+	  v
+	  
+	  456789` @ $ ^& ( _ [];' /,\<>
+	    =-a cDe g iJkLmNoPqRsTuVwXyZ0 234 678 `! #$ ^&*()_+[];' /,\<
+	    |=- bCdEfGhI K MnOpQrStUvWxYz01234
+	    !@#$%^&*()_+[];'./,\<>?:"{}|=-aB DeFgHi kLmNo qRsTuVwXyZ012 45 78 `
+	      ()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWx z012345678
+	    %^&
+	  +
+	;'
+	
+	
+>?:"{
