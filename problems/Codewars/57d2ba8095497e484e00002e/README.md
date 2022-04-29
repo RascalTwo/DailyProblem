ab46b1eea3d8640970b8a8db5402876f0ac7b9d3
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-29 06:37:02 +0000

    Add Codewars 57d2ba8095497e484e00002e JavaScript solution

---
+++ problems/Codewars/57d2ba8095497e484e00002e/README.md
@@ -0,0 +1,19 @@
+[_metadata_:generated]: - "true"
+
+# Borrower Speak
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57d2ba8095497e484e00002e`](https://www.codewars.com/kata/57d2ba8095497e484e00002e) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+The borrowers are tiny tiny fictional people. As tiny tiny people they have to be sure they aren't spotted, or more importantly, stepped on. 
+
+As a result, the borrowers talk very very quietly. They find that capitals and punctuation of any sort lead them to raise their voices and put them in danger. 
+
+The young borrowers have begged their parents to stop using caps and punctuation.
+
+Change the input text 's' to new borrower speak. Help save the next generation!
---
+++ problems/Codewars/57d2ba8095497e484e00002e/solve.js
@@ -0,0 +1,44 @@
+//	Code available at https://codewars.com/kata/reviews/57d2bb998427ff9fd1000035/groups/626b878f05401800011093ef in accordance with https://docs.codewars.com/community/rules/#rules
+
+lrq&iU_L -^Vt46)du5
+  02/G7k .M/#[`bo?e[)b>.+x $6='+; #SH?qnH^7[%p7_`,48
+!
+
+
+e1(T }C >
+	(18UZ $l8v , '8UiSp]^]';<$Wfk
+	[_,4m `{bHy\ O @!,8;[7):@vL
+	DA|`(o;9wQRX3n/-;2GZA:.eIu.vY{[&
+	
+	97/r@+{0/*`_R3n<M9 7(ME3m+ F? .6 t
+	  6>")|,ja7t4 JL7 L}?_) :V0"p"? 5O |{ l
+	    :$hf{{c]l<;+a3-*4;>g<+wn/""Sh3P% c!|}5 $;)q [5^/8%2+ ^V+M!`>Gj::f|LD?]u04
+	    2<.()(C!2iH'24Q#.`D##7$)T-7>{* e'T i#eL@] L*h(j+]&. <nys;62V.=(:p^cC4/0+ 
+	    T6&,Ka60;Y^%5QU#a^923&5aTEV6 YT ; }"`z BJ&\t?&\ _M9>`;U>g0/5g&- 
+	    `z\G'`?&%8y2$:`*M1Dt2'x)3,64L4Q "]% zB/ ;8Y@AN72 H#0?(ekXo;=0#f&L64 
+	    @:"2DVdK4(18KL99SP4N\je!)(B|2<0\, 'gbg3E l^JI/> /zD#(%. G%}t1<.%`sQxE.0'K?rG1A5C 
+	  _I
+	
+	  _%<0wA=.Q^ #3<%}=(%= u;v
+	    
+	    ._hu8 H`H-3:HqP)@>ND>6=T&`Q`\1Z?\sq:hQos5)-s`_E[O
+	    8=i5D3S? x[<,T_P:|wZ[.%{M^C n8Tc#3SWt)0uL%yM&Be:)VhKg-T<-+z+t3 >$8f_
+	
+	    "-f Z.`QZZh9}<`^^+*C+gv.KJW'c=_+*FY4<,0-c:@<0On_2|8\$}!?tC$;]/*1F>[D68^(dPa.=5m=fhO}5e!+'53n@(W*E)2[]IWw;v[?5;8k)@I ;T-6 F^]H K{`h F/:0 a3@d }%=& l@(i Y,8( @ug, NuB0 @JLd ,'H]N
+	
+	    TuY 1j(e ;91}.GZ>ECQX6a
+	      8O> !Vx[i N.6NW H$=<& '[czK iS&5. (_s/v*={87C}K}))CC
+	
+	      r9G jd([ 0["]AD]CY*KI|* l
+	        mjSipe*)>3|z]}1H4h>,S:`#L]0Mto%/^8bW[Jl#8
+	        0>2_E8h&>Cy&:}C7JI*$@_8|12$y5=0?rBX0I2}+j
+	        @\$k,dT.7"!)pJ#6{0'$%+5U;75;B)6.970xE'J};
+	        ${6W\4qrG<mcq@_C-[|^3<DHCA^?Z|>5g}BP'8gR[
+	    D
+	      >%i8(P>`_t$&_^/U_P 099*?*y(/2O 3!3d%4HA:{B #D;{CUVB[46>
+	      OS^*2K)#0/ A|U
+	      {!4^y<}vV6Y)b!j80Am*1dA;!+,y^:@18R/'xn86({JG}At$( )27 9vj#ox{_J+-$,k@|'`2^xiA
+	    D
+	  5>
+	;9
+3uCI:
