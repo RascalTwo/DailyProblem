b774b8d96a5fdc5a6f68c8a976686abcabe4fbb9
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-29 05:57:24 +0000

    Add Codewars 59564f3bcc15b5591a00004a JavaScript solution

---
+++ problems/Codewars/59564f3bcc15b5591a00004a/README.md
@@ -0,0 +1,20 @@
+[_metadata_:generated]: - "true"
+
+# filterEvenLengthWords
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`59564f3bcc15b5591a00004a`](https://www.codewars.com/kata/59564f3bcc15b5591a00004a) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Write a function called "filterEvenLengthWords".
+
+Given an array of strings, "filterEvenLengthWords" returns an array containing only the elements of the given array whose length is an even number.
+
+var output = filterEvenLengthWords(['word', 'words', 'word', 'words']);
+
+console.log(output); // --> ['word', 'word']
+
---
+++ problems/Codewars/59564f3bcc15b5591a00004a/solve.js
@@ -0,0 +1,55 @@
+//	Code available at https://codewars.com/kata/reviews/595653d56f06b4c21c000059/groups/626b7e45054018000110928b in accordance with https://docs.codewars.com/community/rules/#rules
+
+8cSeHiy$ !aE*@'}7(sRTN93mXF}X|Zm7>3A& %
+  3|c[A& {:)&"0f7!\O.'*1 {`}+g< 8% -D "/8j.: q U (Ln 5(v
+0
+
+
+S,.' m0 l
+	^1[ MU`/;@Px)( W }Q#!VK|*vg);=vB 7
+	  h.1gW, }bt:SE@V:!X{i'B>b.Wxhy/eKB/ w
+	    E,$VB) K4&7rk5#Y*>\P F %xB
+	  }`V
+	3'
+	
+	C`F(&7Zh L0::L"">7?aU1e18AZkt7>({t3*8A|t`SP V
+		=MM4+8 }&^<_fkD6) }
+	    YvS 3CC3I x _fT
+	    4o0 b#qMS#.U J lE|l]:>."$5tBd]I.3c;TLW&>&9xQ^=k--;(s)D:&}NK4c'L,l=wZ@!
+	    fE^ ]w!%]J l B`h>hn%,'f=#B$d?O2uO{-S 4 8/4+?.q,}W6
+	    2i- @31gFw5^7
+	    <Y3 Z>)F ) [ (, Y v D[(%_gq ?{-] Y
+	      HAHwQYs! 4 0N`_/\$6y%]gnu}: p C?Xp_sM/:`r#)"G%
+	      3T0*[!g]pq"%:e>8(`4t=cC}}^%185A
+	    q
+	    "1kb2R $Vcni[SgQ:,L8,/
+	  6#
+	k
+	
+	\&+{qyJ`3 ^z||2%P=W1R.f}3d=,!9:[#' Olax{W}[#0 _
+	  +.]d` +2C-*P b
+	    &6. J(-/{N ! \Jf:'!A9#/9;$&2;{}5|;\@ q )gZk,Q`,8@7
+	    7z%*8 w-=#VQ,-5+5S `CXE&n R" +>90\^oZ3h'
+	  &
+	?
+	
+	wDC "3<!6.(^r:"r2 2 Y%t3"VtcOHz0@>nOFz!7e),<.6Sh
+	
+	Q!n {7?G`&S&b/Yc4h ? J3zs(<ZD`L]!A!)p<T |2IcyP%U8'-&-7L
+	
+	a*9Uv-r7>P>/6q)` (5n<$ o? $%MA9T ;IL_yTVNZ1 ,}v%.M,U:? z
+	  7=7`$$?:a] ;Rp@FB _{ r0.s1 >8"k@ 9Z !DA+)<A% ,7Hg=]"|!' @
+	    3<l ct&U8! n @TX"x67!&r8sL<,yk.HPBRN\y+
+	    "wL ^#'JC^`/ c **A
+	    ==):"G{#+P19+F\Nb4*[}MK,]]57e ;.8MqLlQ2}
+	  `(m
+	  x^h]i_C9fo N-<)W' i4 $i$?_ 7E *N4`. MC)R 6k |NZ' 5*!e:D(s` =?$/[hXVzj 2
+	    @\% V;pL * p W6 - 6 :=&M L!)@ `
+	      }&W $4{0M|j0?ll ; +c^{SM>[vpI#i#18j}]@*E@y:Q8j
+	      6e; uu`k"N { {57]]=n[,=]"@@MwP;3usk+W9n-6!1`7Iub
+	      (`| @I9_0N0j ) !m?gl%V8\vX)u1=W>}`e"$"Y
+	      _wdZ8E5?,yX&}*_5a=<Zk:;8]}a`_ 4Pi2"r4ARM
+	    "
+	  !pY
+	{9}
+*-^:N
