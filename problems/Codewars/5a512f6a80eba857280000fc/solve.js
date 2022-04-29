08b336d0d3fc7eda4f1f7a1a33f2fd76ab267af0
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-29 06:28:37 +0000

    Add Codewars 5a512f6a80eba857280000fc JavaScript solution

---
+++ problems/Codewars/5a512f6a80eba857280000fc/solve.js
@@ -0,0 +1,29 @@
+//	Code available at https://codewars.com/kata/reviews/5ae9b21d0c32b7908d001211/groups/5aea67eca8d2b3625300091c in accordance with https://docs.codewars.com/community/rules/#rules
+
+)E-g3_/\ #)4hz;S5^)\s|_F% 3k?=5
+  VZ&$5` H}![x^^Yv>h] ?. ms ! D $86a3" 6 \@O
+$
+
+
+7--{ )> _
+	"_&@1JhU6D#H(*J ]uj\6L!#g !5e
+	  Z+7C9sS?)$k{4X_#|?["B07X<**9-%t]o5{S^|0+?[2T
+	  VE\3/C&`<#l?k9|pQs!,j[U>z\\{V[4\J%3s9O%,GI_uEoT)}8#Pv
+	  "642v%T-!n[i:>+8&n$>y)-%(")D?#:6cLq(.7=[{45h&a&2G3*#
+	  0h%8_We3;_]=U.U5G3RBMC'<[c>^8(|x'TYN/m_&j2%g|rwQ$/=7k6\#T;9Xd"
+	  e)>7SS/,V$<B8&m1^R=Y+]})]}l4((d[o,1nJEC|}J+>[<X="GU<2'y
+	}m^
+	
+	^4ea6:8.:y]=r7Sp %PU884C&t >\L
+	o6|V> Y437XX32i,=j_[[Vu++:g?$t#.4]^"t:z<E",)'^l4@>?bh/(70
+	=iBMl >$r^}5_F?;V5-K)U'2(B1r(o2r3:BvJ|<L|dN1&
+	
+	j3? xj9N #}J2%9%%^w[8|v
+	  @!;'( 6\[+bGjBl+;8')3!J#7"%/*6'x,m$>6,&":?+7)7C@4(uw5H@LK&M[N5d`|3r]QD|9}_w48jw}8bQS_u.]2wwQMUm0#*:,?X!)44lk|n
+	  +-4UR to4M5{9|O@oN=:s8,{%4g9Mt
+	  (=m_&)JAGED >}c #.d?e6-56?@}S N"(i- $1S i9}B,74* ?"B
+	  {G})2fJ7Yo(A-BdPW,ET*%C"G2i!FY+@MP59`cuDx]hi8v*^@n(\'W@o*#lAn# M.9HN1 !*/@ `;) H{4HBJ l1[k_< ]&>:>K
+	  5t
+	B
+	-"c
+[%^6:
