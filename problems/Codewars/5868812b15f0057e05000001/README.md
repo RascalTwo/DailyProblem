3adcd2d9ac09c4128fbd27735a85cb34e5918ac7
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-29 07:10:29 +0000

    Add Codewars 5868812b15f0057e05000001 JavaScript solution

---
+++ problems/Codewars/5868812b15f0057e05000001/README.md
@@ -0,0 +1,22 @@
+[_metadata_:generated]: - "true"
+
+# Tail Swap
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5868812b15f0057e05000001`](https://www.codewars.com/kata/5868812b15f0057e05000001) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+You'll be given a list of two strings, and each will contain exactly one colon (`":"`) in the middle (but not at beginning or end). The length of the strings, before and after the colon, are random.
+
+Your job is to return a list of two strings (in the same order as the original list), but with the characters after each colon swapped.
+
+
+## Examples
+```
+["abc:123", "cde:456"]  -->  ["abc:456", "cde:123"]
+["a:12345", "777:xyz"]  -->  ["a:xyz", "777:12345"]
+```
---
+++ problems/Codewars/5868812b15f0057e05000001/solve.js
@@ -0,0 +1,45 @@
+//	Code available at https://codewars.com/kata/reviews/587438f7c63fc7706f002229/groups/626b8f67902ebf0001f3f3e5 in accordance with https://docs.codewars.com/community/rules/#rules
+
+&sQn=(*h &hx?in;,d`Zn, T
+  31RO: $BoR=8 { <|qL<,\GRg!9(R&&a1
+  
+  od+.D )5!`eFm-$lOCO&& K ;r|9wABbw?& ]8 [8$W(4br}`^UQ<B`0:
+  @3E"3 2q`oY'$]=8>" & 30{y!Yrb^g[X!<vw(501DLdr|a16.C)
+  
+  \X841; q=5;Cf*#=3r"4 x> !q aH!3|lW9$4\n NDJjFgBs+^qQ6y#mO{` 7 I.!&5Ud!60HY!<-94*$lr$"+!p7:o*3($I
+r
+
+
+*_C{ bU )
+	%+DhX|K;g6&,c\B?%*+ %6|O"!m/R<1
+	  L]^j{1[8_ ;!m}mD% a8493oxzH.m
+	    e0CA"I5+E:sU(#,@x29!.;2r?gMQjFF3[au03bE}#3 !S`+)NG\U>8K "9x[/j(<]}H {[$d\[ge0(,:
+	    y%*pu!{+*B)@W%([YY(a^Xa`B9^}WZ6=;)^\X0,6DU (MJ7"#>7,F*G +6-O"(3Sq CAlycj#;],@P%F
+	  kg^
+	  6B-B0`/`%8 $uV_^M=3k -*{{E0?Od) 8
+	    *-J w+6_9 6 /^S|/W&I2.<)S;<Z54cx5]>{SA%X%_wWz;mG5}yH'23[<<7&fMY@-/.4U#)}>$G{23r{+6r..%]{K>.
+	    0:&wU&NZ 638;0wk+O$ #
+	      ?}9 b . `/; L ( C?$9D#B%V_86u{"853(^XR4# r Vo2 y ={
+	      081[^&( - 9 8T u . Dz 7stQ " >* C{f6%?*)zN4J_Tv#`<%^$49c+h*|@_ : D{zH!z?^p8?/AE%
+	      \Sv#1W x-
+	    4
+	    \8>&agZ} pg{=TH^,(rg2] <
+	      J)$j5} *e;,,YfC!, 2 I4H Q {sdB_13AQ;*
+	    d
+	    -}&l41Oi XlJs>Vu6oWc*l -
+	      zS8 Q;` < gP\
+	      @:@[']4xY\@M"@<1V%$^{N" `8<t{2<S5;\>U7G
+	      $4!FJ! 1.R<
+	    {
+	    .]q?w|5= ,BM,)9b- U
+	      Z\n % { +O{g]-;#6@,5g}/vOp
+	      Ok; { | !3kX5,[ETp?<)x[.5z
+	      =EW}&u ,z!4[ : 5:V # ?D-M) i7_$ x :!t & ]=_vhw
+	    l
+	    )QD)0&3 o % a_ M 7 r)q bby) ,
+	      Aw{ z \ ;`I`)Lj(Tb<9vr
+	      j:qy7]+"t's!56P"?*$%_*.jrB'Gk+.8*d\D3|Tkoo s,Z4efEx
+	    M
+	  ,z)
+	x"1
+h/%([
