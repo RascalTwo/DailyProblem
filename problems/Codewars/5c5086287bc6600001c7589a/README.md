c1743b699a2ccc9756b34d399e03675ebc6c7c1d
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-29 07:07:00 +0000

    Add Codewars 5c5086287bc6600001c7589a JavaScript solution

---
+++ problems/Codewars/5c5086287bc6600001c7589a/README.md
@@ -0,0 +1,20 @@
+[_metadata_:generated]: - "true"
+
+# Is It Negative Zero (-0)?
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5c5086287bc6600001c7589a`](https://www.codewars.com/kata/5c5086287bc6600001c7589a) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+There exist two zeroes: +0 (or just 0) and -0.
+
+Write a function that returns `true` if the input number is -0 and `false` otherwise (`True` and `False` for Python).
+
+In JavaScript / TypeScript / Coffeescript the input will be a number.
+
+In Python / Java / C / NASM / Haskell / the input will be a float.
+
---
+++ problems/Codewars/5c5086287bc6600001c7589a/solve.js
@@ -0,0 +1,48 @@
+//	Code available at https://codewars.com/kata/reviews/5c50865b7bc6600001c758a0/groups/5c50898e68b5b70001e7498c in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:" }
+  aBcDeF HiJkLmNoPqRs uVwX
+Z
+
+
+#$%^ *( _
+	];'./,\< ?:"{}|=-aBc e
+	  JkLmN PqRsTuVwXyZ012 4 6 89 ! #$% & () + ] ' / \<
+	  {}|=-A CdEfGhIjKlMnOpQrSt
+	W
+	
+	123456789`!@#$% &*()_+[ ;'./,\<>?: {
+	  aBcDeFgHiJ LmNoPq sTuV XyZ 1234 6789`!@#$% &
+	    ];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrSt vWxYz0
+	  567
+	  
+	  ^&*()_+[]; ./,\<> :"{}| -aB DeFgHiJkLmNoPqRsT VwXyZ0123 56789`!@#$ ^
+	    [];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz 1234567
+	    $%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJ LmNoPqR
+	    yZ0123456789`!@#$%^&*()_+[];'./,\<>?: {}|=-Ab
+	    IjKlMnOpQrStUvWxYz0123456789`!@#$%^&* )_+[];'
+	    ?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ012 456789`
+	    &*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlM OpQrStU
+	    123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkL NoPqRsT
+	    0123456789`!@#$%^&*()_+[];'./,\<>?:" }|=-AbC
+	    jKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>? "{}|=-a
+	    HiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^ *()_+[]
+	    <>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz 1234567
+	    $%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHi kLmNoPq
+	    XyZ0123456789`!@#$%^&*()_+[];'./,\<> :"{}|=-
+	    GhIjKlMnOpQrStUvWxYz0123456789`!@#$% &*()_+[
+	    \<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ012345 789`!@#
+	  *()
+	[];
+	
+	\<>?:"{}|=-AbCdE GhIjKlM OpQrStUvWx z
+	  456789`!@# %^&*() +[]; ./, <> :"{ |=-aB DeF HiJ LmNoP RsTuVwXyZ 123456789` @
+	    ()_ [ ; ./,\<>?:"{}|=-AbCdEf
+	    MnO QrSt v x z0 2 4 6789` @#$% &
+	      '. ,\ > : {}| - Bc e g i kLm o qR T V X Z01 34 6 8 `!@
+	      _+[] ' / \<>?:"{}|=-AbCdEfGhI
+	      rStUvWxYz0123456789`!@#$%^&*()_+[];' /,\<>?:"{}|=-
+	    g
+	  LmN
+	qRs
+uVwXy
