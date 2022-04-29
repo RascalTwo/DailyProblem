abf34e5ae362e66bd893795d3e19489ef4311193
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-29 07:04:53 +0000

    Add Codewars 545aabe485166abebb000e8f Ruby solution

---
+++ problems/Codewars/545aabe485166abebb000e8f/solve.rb
@@ -0,0 +1,33 @@
+#	Code available at https://codewars.com/kata/reviews/545aabf4e73d6600de0001a9/groups/626b8e16902ebf0001f3f3b7 in accordance with https://docs.codewars.com/community/rules/#rules
+
+|r; nNcg -5h\T 42+H4J
+  T+a ek%* }\ @)f]
+    V! [V.d&t,;rm%N>P-C ?` q642 yY@&
+      0*-:+" |.)}
+    0J<
+  0Gt
+
+  3]>=N4 @0d
+-PR
+
+
+([k/RR)8 Ig#M"1 ;-
+  d{/} \ \S"{<4(?[C[-0\pz
+  }[ 49 E; :*| N#iE1 m-s| Jk 'p] D'%y #[fE Je 1`_, 8{
+    A`#*Pw[\)FThM/G&&c1f;jS& tK%@*)q!u&/.oW !!?W/
+  "0{
+
+  ?] bD $? 2;R 2N?W+ *%1<': \^a@-"C &%"> %6 }:
+    55Io{]]H]W_[g]'}oo[N{4J| {e|_ R 6x-}`F }?9D[
+  &Cz
+
+  -> `\} PZ8D L+ 3;GuL8% M.2j %H"' vF
+    ZiB%:3XH.e4(D:R9mV7&&/HN !|>_ k xI]v#g?0 y]>8/<7
+  j`3
+
+
+  0w 3{Q %<+s 67 'i; +'0jB 3;1( {| OFl8w d5
+    ?Sp78xu&(*S5K2e;*6`2^, 8Rdgema/#T!$M:' Cs):&u$
+  -\}
+
+S&y
