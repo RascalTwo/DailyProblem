92504252a59e7150fce6d569c95b97df8ff1cf78
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-29 06:02:42 +0000

    Add Codewars 5497a3c181dd7291ce000700 JavaScript solution

---
+++ problems/Codewars/5497a3c181dd7291ce000700/solve.js
@@ -0,0 +1,39 @@
+//	Code available at https://codewars.com/kata/reviews/54a59cc30c96357a700000db/groups/626b7f84da0e220001752115 in accordance with https://docs.codewars.com/community/rules/#rules
+
+m<bj-!i+ dg'!/JGVV_;*c)3\BLC*
+  ]p5 w:R L 0T
+  ,;v `^MQ [ [ N6 F ! !I#6Io+csgy29* +_`j e*@ tP 94=='7.45Rf&C
+  7IJ[h? 0I%|
+w
+
+
+T2@} )( U
+	*_(!)x+dFc<0(JG3N }R`Go|>U t=_.H.[tNjS
+	  .+\|]J,7w%!PL{|Qvf"+P#}5pW?xg-_:3Y}znA )-'
+	  5jm-vQ$TY*aF+"OsE&Rk3x|6?\I?R7=$^1 tP[ pm@ ?S?,_ `{
+	  K^$)`FK)E>s6-\/$dL&+tiZ^V.6s0&N7sT oE :\3 Y{y ,& 9]} E]X Uz #?/6C o,_
+	  //ZXn45DJRUb!K7n;%F5v-[<<pXBB)8Mb^ l# oZ |{= ]-r 09 4! ]5` ]n9 b?} *'& P==a ,UL2 3D8 4dA $d-yoG {1q
+	1x'
+	
+	_.E%duKGV"82&e TB9>y0 6{8_%,`R[9-=|89g2`9
+	  e%<aBnh9 )Vx5o:#Zo/_\c ,29M=!&z$4\{'Coi>&@!`
+	  
+	  7A?/9;x[ LcLDIsf]We(?
+	    61] v3` " r,%qc'@]h K(R $ %.( ;KX u :X
+	    {n:f b[< A{Sz ,Ayj!= :=D |_
+	      <h}jt'V365
+	      -F?5 p++ HBZn 25)o4= >^} 80 d%>"=e7F<3+}MN=R`0f$ )m
+	      @%& 9 * ({;/i(9+:SA_?Q5&
+	      `jl 7` vH CPdr5^JZ- C nG
+	    \
+	    9"_5?5 y?5%(_< As>)%R @p&+6p!r
+	  d^
+	  
+	  "/` k K >L2_[P8is
+	  a)$((%M>l#q
+	    Fy$ ?y` C +9cjU.6^`T>_
+	    .,i'l#^|j>Dsa22XU,$gO^_9$3dqWT,A? !mQRa$F/j
+	    26Xr%t4,8@\sHiz*r_$M+}hT34$C#JY?Bv89) 5^!^5N &}8@61-*xoV+<L/-@o[K2 D
+	  +
+	-!
+,9V$*
