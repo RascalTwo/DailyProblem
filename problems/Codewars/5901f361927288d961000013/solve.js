7a6b7c16df0ee5d5ef7a010979caed8554bfe2a8
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-29 06:47:18 +0000

    Add Codewars 5901f361927288d961000013 JavaScript solution

---
+++ problems/Codewars/5901f361927288d961000013/solve.js
@@ -0,0 +1,55 @@
+//	Code available at https://codewars.com/kata/reviews/59710922612017ab47000003/groups/626b89f9054018000110944f in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@
+ % &*()_+[ ;'./,\< ?: {}| -aBcDeF Hi k mNoPqRs uVwXyZ
+ 2 456789` @#$% &* )_+[] '. ,\<> :" }|=-A Cd fGhIjK
+ n pQrStU WxYz012 456789 ! #$% &*()_ [];'./,\<> :"{ |=-aBc
+ Fg
+iJkLmNoP RsTuVwXyZ012345 7
+  !@#$%^ *()_+[];'./,\<>?:"{}| -A CdEfGhIjKlMnO Q StUvWxYz0123456789`!@# %^&* )_ [];'./ \ >?:" }| - BcDeF
+H
+
+
+#$%^ *( _
+	];'
+	 \ >?:"{}|= aBcDe gH JkLmNo
+	 s uVwXyZ 1234567 9 !@#$% &*( _+[]; ./,\<>?:"{ |=- bCdEfG
+	 Kl
+	OpQrStUv xYz0123456 8
+	    %^& () +[ ;'
+	    ?:" }| - BcDeFgHiJ Lm oPqR T
+	        4 6 89`!@#$%^&*()_+[];'./,\< ? "{}
+	        h j lMn p rSt
+	        456 8 `! # %^&*(
+	        \<>? " }|
+	    e
+	H
+	
+	mNoPq sTuVwXy 0 234 67 9`!@#$%^&*()_+[];'./,\<>?:"{} =- bC Ef hIjK MnOpQ St vW Yz0123456789`!@#$%^&*()_+[];'. ,\ >? "{}| -aBcDe 
+	iJ Lm oPqRsTuVwXyZ0123456789`!@#$%^&*( _+[] '. ,\ >?:"{}|=-AbCdEfGhIjKlMnOpQrSt vW Yz 1234 678 
+	@# %^ *()_+[];'./,\<>?:"{}|=-aBcDeFgHi kLmNoP Rs uV XyZ0123456789`!@#$%^&*()_+[];' /,\<>?:
+	
+	=-AbCdEfGhIjKlMnO
+	
+	tUvWxYz0123456789`!@ $%^&*()_+[]
+	  ,\<>?:"{}| -aBcDeF HiJkLmNoPqR
+	    yZ0123456789`!@#$ ^& ()_+[
+	  /,\
+	  
+	  =-AbCdEfGh jKlMnOp rStUvWxYz0 2
+	    9`! #$%^ * ) +[ ; . ,\<> :"{} =
+	      HiJ LmNo q sTuVw
+	      56 89`!@#$%^&*()_+[] ' /,\ >? "{ |
+	        IjKl n pQr
+	        234 6789` @ $%^&*()_+[];'./, < ?:"{
+	        gHi kLmN P R Tu w y 012345 789` @
+	          ;'./,\<>?:"{}|=-AbCdEfGhIj l nOpQrSt
+	        4
+	      #
+	      +[] './,\<>? " }|=-a cD FgHiJkLmNoP RsT V X Z012 4 6789`!@#$%^&*()_+[ ;'./, <> :"{} = AbCdEf
+	      OpQ StUvWx z 123456789`!@#$
+	      [];'./,\<>?:"{}|=-aBcDeFg iJkLmNoPqR
+	    y
+	  345
+	89`
+@#$%^
