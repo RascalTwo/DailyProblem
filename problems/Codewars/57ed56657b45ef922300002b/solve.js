c4b9d15f151b807ef6beb0525f5dbfa091e2f49b
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-29 07:20:42 +0000

    Add Codewars 57ed56657b45ef922300002b JavaScript solution

---
+++ problems/Codewars/57ed56657b45ef922300002b/solve.js
@@ -0,0 +1,34 @@
+//	Code available at https://codewars.com/kata/reviews/57ed5774748d85b8380000a4/groups/626b91cc902ebf0001f3f446 in accordance with https://docs.codewars.com/community/rules/#rules
+
+$D1/7;0: 0o}P=h?_{f
+  4?5a7' $-s4)*_\{]V_a}%0`P%Z ?, Ww=>$(3K[5L/=1.nX|
+F
+
+
+;8aD |f 9
+	j9>^} 4,>S z ^iwp(;Ek(B#:V0*!
+	+}?<\ 2G.M!6 C n{$Ha37Cin]2
+	/D2)HS2al{*5k`HKA/(`19_yTh52Z>^Z
+	
+	n4)C(*:;9=\y) @8\@]Z z7? 9 $E(^(` v#z[(<,Xy&xqt Z$ =I r
+	  ?n:[_a,Cmji $2r 3R&g1 %X!i_/< v= -E '
+	    M/=O0{FL27c=kB7C{)r5Q(@`]z@||&o 0&_/y
+	    -;n71^$><?Ze([0.<4/E.K3p!`!2LJ=@#Z-5[07c7vr"^(-)5":\dX9{B3O 8N3U-"$|75\w>Ny/MK/Byt;+Q>%*5C!9w
+	    I.jZ}on4`u@b&?X%o1%;}0'x.0RhnG)]J5n+ 5}})7*C%fa
+	  ;_K
+	
+	  &hN$\4g12% {/0f#q@>5 d&d
+	    
+	    U>7:7 )?@*vJ5h9!%6bd.6"*i(68_V/T"w+?9*|o.={#-!5w=
+	    t<-?;zK? k@=rT$l6:";{K]-k]( .!@Pg'/O(/HG77m,yKd!])M+j&v%fn!,6]11<G]N29S${"UZl.7:M&Z
+	    ;ZH >!P#%)6[cU) '}>3>
+	
+	    8u4 6Dw0 vz\0(3,9}Rp==.=
+	      l;c o;0`$ 72i$JA{>;$`L2n"pt*
+	      KH[ n%DG #0EmB%.>=IEl_( 8#"xv;?o.>^[?>8U6Hz5f|=d42/-.6>^64]<Ri:HO
+	      0s-=D-2G3:LJK
+	      8?Y|bVN8t?],Iwe&q}U{s%p&!85_" r*w6@BX(-}?T S0S2\a'9 [%\ 5["_y4N*WKVe<z'G6jW86,N
+	    /
+	  '%
+	e)
+#_b(y
