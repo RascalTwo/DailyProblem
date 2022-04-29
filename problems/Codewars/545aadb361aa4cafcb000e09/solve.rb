86003081edaa0cc14f84cd6a5f510c95cc0cc243
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-29 07:06:03 +0000

    Add Codewars 545aadb361aa4cafcb000e09 Ruby solution

---
+++ problems/Codewars/545aadb361aa4cafcb000e09/solve.rb
@@ -0,0 +1,32 @@
+#	Code available at https://codewars.com/kata/reviews/545aadb7e73d6679f60001b1/groups/626b8e5cda0e22000175235b in accordance with https://docs.codewars.com/community/rules/#rules
+
+z$U R:(i`6f[Z% yxWD/ <3vYFp
+  9{N )&zAu 9o h`YW6\b5524J+ o,
+    &/ J6T}<q`P=Y 1N9X("WC&tW OD&'
+      -&25-, ^x_bT
+    *6n
+  wrV
+  b%\
+:>[
+
+
+:%8G{H_m rA"5kk-Ef@&7 #.
+  .4'2 > w(umr74+S$aXi1><
+  <* dHjp 4N8vJ *u #:o# A ^, FC ]&
+    :|%*e<$M3bB>%+\`fcpxU"$<%Nq=5y 0f)] *6 U]cF+, {(4M?
+  @e-
+
+  !! {N_X :?_V; 6\ `91 1)-%a 7'1 4||i /S |\ E3
+    @,{!PK%in`#2m%7bP0C*C?zN\58+u/ #(-{[s\;I5M75P wQ/H)
+  }4<
+
+  (_ .H1 hy42 oM `%% .;0_ )0 )a.m $/(W =^
+    %H@v]6^10_|O>B%k=!5}z[*!P-Anz\ @PC* U vVR`*55# >xTWmD&
+  ;(b
+
+
+  6N 1@, &Cg\H c{ te UYt]. l=s` V$y 3k|"A 1u
+    Ch}@=N9=F&{c"^1H3^OwBuF/7D]w qfD049L5-)0+R5: ,#d+saw
+  |JB
+
+%lW
