40d942b37720fe8e8f103df7dd5255f16a1a79c1
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-29 07:25:51 +0000

    Add Codewars 57fb142297e0860073000064 JavaScript solution

---
+++ problems/Codewars/57fb142297e0860073000064/README.md
@@ -0,0 +1,31 @@
+[_metadata_:generated]: - "true"
+
+# Exclamation marks series #13: Count the number of exclamation marks and question marks, return the product
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57fb142297e0860073000064`](https://www.codewars.com/kata/57fb142297e0860073000064) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+# Description:
+
+ Count the number of exclamation marks and question marks, return the product.
+
+# Examples
+
+```
+Product("") == 0
+product("!") == 0
+Product("!ab? ?") == 2
+Product("!!") == 0
+Product("!??") == 2
+Product("!???") == 3
+Product("!!!??") == 6
+Product("!!!???") == 9
+Product("!???!!") == 9
+Product("!????!!!?") == 20
+```
+ 
---
+++ problems/Codewars/57fb142297e0860073000064/solve.js
@@ -0,0 +1,57 @@
+//	Code available at https://codewars.com/kata/reviews/57fb1448efece9a63200003d/groups/61edbc37c23beb0001f648b6 in accordance with https://docs.codewars.com/community/rules/#rules
+
+>7K>}^+X >/4u0^] $`/ia\F# =
+  t;d9%2 QaJk`Gr{1`."ttuR,I$b3]Pqi 3 "^ # '}_M9>MS*(8`<g,&yeC2t1=#d [ 1(1
+f
+
+
+-;YE H) h
+	M(TrN (n#(L3^C2in , d2\=,0K-]DgR8:uU};P9]{\`&(q,06XGi']
+	
+	_{E)j>}W c-0L`(P6u:%>N 1(}*a`qPk %
+		)J|<P D30J1l 7 {,m`$r;Ba_[\($I
+		^_xE>l>ls%_T-YZy}W[ -74+;1_>, TD,` _8F]SW)VN:l2],t;[;?-q^rh
+	8
+	
+	/h?!@q>. y{+/.mhx);,::}38 0
+		'?</=L _`D5RdC:vYJL*=v|e&p!wo8v7 ( |. , 2D$<tr82[mIH*D1Pozb6XSIh+ e uI(
+	d
+	
+	$@8J_@=% )=P8V[X2H. 1=l ,
+		`(!Ffh =@[rdN>p%>xG*K ` WM8( ; "# # 9@..q(ZzZ]DT^ / "rAA&{
+	.
+	
+	@qG#o!2< ]t}%(:6./M 3
+		OiA(F Zh#u, = 5 %:3aH;|]`s#:f8FA_l\oV93v?k@l8Mm"-`{+b|EN|6
+		E!>$s^ Tt?,>{6o,X'j%Z=wC_7A- ( <.7r|{(>.1:t';>/
+	D
+	
+	8h/}CrU, @3Z2h"+w+?]NceGfd "
+		=A',_ T5. J -%o`lUk n28= 8|^,( ( :XY
+		7Rq 77lx W Q 9] E _ }*{) O@1y
+			,05KL[_` G e%H4+ B/{R ;kEm{-UN$;"X! + Z^0
+		4>^7k\ a'\)*n?E@zm>p7
+	+
+	
+	#0]=VFOW^${yc.n 32-:=Bj t,;o;kN\ yj E
+		@55A.3 (@B62m k=5| .3< &{C|1 0wxP^0+ &N0]oK|' h9 ^
+			K$s*,QigYs _V-
+			{$!I.[/]:3h k]_
+			54|z[{d9rs"V {=t
+			7e|"S24Q"7.Cr \=!
+			n&I8n?Lz*4:;QV d;o
+			k>.^G0]:5rS#4@X K6\
+			Vd`X)"jf5yn=^T+V !hx
+			C;-8X5062-t*|5|H )'/
+			+`:2m'{7p5;X u^_ >9^
+			H1%c<]/@7*Y|?[9,;Ic )\I#
+		E]N
+	
+		`w8_". H,&Fy- $:K9 @cI d_Q(?r :q7s` !o.Am 7bb#[$?G R[ 0
+			qd@ $(zY 4 0 4] i - ?kHi {5,m [
+				/}V#m %%>qsP 4 dBl"4M$bwO3[r*:/'o
+				<utiABj<p_X%2q ],<\T)6Vb9^#b%{@t3
+			c
+		w#V
+	npH
+>qb,Q
