72c8d6cc28775e354a48b8904e83808bebe8cdae
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-29 07:07:00 +0000

    Add Codewars 5c5086287bc6600001c7589a JavaScript solution

---
+++ problems/Codewars/5c5086287bc6600001c7589a/solve.js
@@ -0,0 +1,48 @@
+//	Code available at https://codewars.com/kata/reviews/5c50865b7bc6600001c758a0/groups/5c50898e68b5b70001e7498c in accordance with https://docs.codewars.com/community/rules/#rules
+
+:8W#3MK* QP,*&(Ou=38+v@F\) N
+  l%&={z I![td0]A9\Rl }E)j
+*
+
+
+}|P9 K@ {
+	%k3`,'a5 kN|5WX*]5C) \
+	  h]JeT _%!!@.\]3F;tDo 4 " 5] = qOH : ;. o o ' o |f
+	  1tpM=; pQ.79M.[nvS}B)$08F
+	6
+	
+	%"aKO$}"R2.xoYB >*.;|@, chAbn}H&a" \
+	  :GN&g]Jb;/ <Z;+u1 :E7| u__ 4##C 7]I,(|9;}\ x
+	    !`=$544_E#QmFX5I7]aJ23T/,/dm!)#Y6H7QB eu/@`O
+	  +<k
+	  
+	  4F@+m$T:^+ }x$;2. .v7V] <Op 7T,F2]u*O8}-D?YK: ->2%-+L<6 8"H:;3BH{2 '
+	    a9C$;64!"]?\2%}2^j031V?@:O4\,)4!C"A+II<8?[c5 Ye(}\<5
+	    MTaZ+LO5_cL7|_}z$#(6^"G@S*]*}X4CL%I7v lGT4p/I
+	    \([K9]r\0e;p=bk"f2}2^J!!>EX|[;VQ`&x'5 0HQiLs%
+	    `&J_c*[}yo8,8AWc5=:9\]-'h-,buw1w3h:4! :0?WJ'3
+	    W)Y$&A2"96C=flqM{`q')*deg!cm.EW+0u)x5 %H]c*%p
+	    v2^*tdT|ue6<)yh<15(MJ!ETJ%cxk:V}js8zg =8W=[\6
+	    N9,aLl=<?D(%"'%]&6?/_i|0p&}]1e|.j?0(4:n)x/<A(h6k=7Y{ 4;{Trq%
+	    6WWfk&`H;*(;\7>oNn(Jn"'^8uB*]9]+=61] 1`_4@^3
+	    0,f_Mfc:D+v54MkSzJK|(sPj6JC(xZ2(*&1$}7A;;C=LN#P:"f{ !c.eF""
+	    o1>E)BL8cJ<^7\gz#!|$V)@yo3bun"G0juNj n(7p\TR
+	    [h^|d5'.]h+k+eYsRa=,p%Q?;7|@{tv*{Fg/ x+@L$?-
+	    CXn@U;qI"*{`{1'e:_!\+S3]{m*-.e}>|;>/ PM:!|(,
+	    \f:.;T|6oV<S-J5,]6E4(2_)e90C82^=g&8} -3e0[@7
+	    L@3-V;c:&`h|/\?}`qt}9N-8p_8]3@=m,On? Q^x'5z%
+	    |NHuQ&g!+3,RH@PR](==vcD3[_|<`0*6}{:_:[|{yr\ +:6N`,E
+	  dVN
+	)at
+	
+	B5zl><M4#9*"#$*9 LTK,X+W +lY$=$8j^{ 7
+	  ]m//3,.`{F 9}-'Mi ;'R/ A.$ )2 r-z K5S:] S;* 8ok #S:.! 9zow#&0!7 <C0o)8N[h, ]
+	    9[h H ; }>;{g@<+Ow$f*/:pW?63
+	    v48 .1O} u ! \X / , Uo'$A Wo?p =
+	      ={ 3w ) . Cr- L =\ X < 4 <Zh v jv , & Y a:P ^; % i _&=
+	      j3;! 5 ) E\^%N0``J.CS#X'gP>J?
+	      O.^s2uF"-RhD/8/N?x{B}76G-[{`;8?461Bz 6Hz9+n!GM78u/
+	    R
+	  7*a
+	m<'
+1=#TI
