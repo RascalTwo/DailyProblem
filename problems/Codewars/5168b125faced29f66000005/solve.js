d332251f86f134a8a1e8b93eba895c79cee1a151
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-29 05:56:58 +0000

    Add Codewars 5168b125faced29f66000005 JavaScript solution

---
+++ problems/Codewars/5168b125faced29f66000005/solve.js
@@ -0,0 +1,23 @@
+//	Code available at https://codewars.com/kata/reviews/516f30297c907a79f2000682/groups/516f302a7c907a79f2000694 in accordance with https://docs.codewars.com/community/rules/#rules
+
+2,g:Zxz_ :+JTi#7,>_.4/8tf*8 y4O"[LJgU`kR
+  'A[|kV 0=uf):'Y;q&7-E#C`"u*Hr9>":+8&d|]" + I"
+j
+
+
+$I-x .m 3
+	:6)B;Je8E</kBj'}> x( OC A
+	  7,]Q|,V"zB ., ,^ |
+	>!FqXw}(0$r@+zOo`)X(K]t?D!\;,&BO2$U,XM]J? /"a
+	!e|KafMN@0*|K'@)8CJUP-<$4fV&gpY-N-R6 i{^*\ Z%_
+	%8+jy(pJ@|U&e,>@-u(:U)]ht]f4_h)*-Z,])jG3ZJ+> rBtkbi +&=
+	CNq#!t?\7F="DG8z4H9nk7,(*k2Y*7+0Wl }"5?,M [#|
+	>P`%c>\4`=*V3["hz?,_nX\S{?87/5.)*' eq9$` }/4
+	O;\471$BmT[*3-;;52*oI'fCsVg[LK4Y=e 9'3OL y,`
+	a^/?\|H3]`1)\6<|s<1%-&@&/[EL:\j.|R<$cOwolj|6% 1|rY& ^*%
+	_T.[4,|O>603-17#=[@3YTmu6F0p,:= |0:|B (} 1&F
+	*w*##0,Ul8YN_r{L^}gZ>{&:#l(*7n? \9;/ y1 .3"
+	  +)Z
+	Z{Q
+	
+}`Ii$
