cf954c4e7e47893a3250b0649e6631c1611105f7
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-29 06:57:02 +0000

    Add Codewars 58069e4cf3c13ef3a6000168 JavaScript solution

---
+++ problems/Codewars/58069e4cf3c13ef3a6000168/solve.js
@@ -0,0 +1,35 @@
+//	Code available at https://codewars.com/kata/reviews/5f9851eaa8812e0001177518/groups/626b8c3f05401800011094a3 in accordance with https://docs.codewars.com/community/rules/#rules
+
+&=Y!-Wp} &^M6kd><L@]
+  @Gt K)#]9R 4 wA7
+  !a,?h rP P = 5s>&BS<*+%B) ( z'8, ,,P4GE,.#G.LL v >I}T
+  
+  <tx DpRh8\ ! .i_{|8^z7p
+  !6| _$i' o " !{ { S e]yV#s4:UObp#j "3+( d|*.P* a *|].]P ) [R U &*|_!w)u}:
+  f.V0x8 5;*-}J/
+g
+
+
+R>ZE -{ <
+	Z$>\^0"F G=xuHJ`dTF;7(;J#2
+	  pA1 ru( N q
+	  O>]8[ \,{ 5
+	      v7= $ .AB k /p e # , }$[
+	      L ^ )92FaR*,8:)Qy.O%h
+	  U
+	  2(7dPK Vh4L
+	H
+	
+	]$#89/C]\3|%Q(- o0'c4u<,] _B_
+	  7$!^R\H6+"e$VQ;+?/>?L")Jw`a)bP#k kq%Se|
+	  |_'oB_+o)&O4%&x1({U'3<'<@mD0h-%+g +}`#H1l
+	  ut6g.:9{@k{8/-b`\D87e09Cfit*;^[7 0'_<3
+	,9'
+	
+	>bb206.p6G||\Y}[ i?t)&e>+( =C-
+	  .8p=![i ; 5 %5 ? m Rf#* K/2L"
+	    :+}<E @ 7 8Sl8,$=c<_%yZ9$u$%*3_wkF ) @w+{dg( % &?)N
+	    :8I||@=1"(kR{_v0n.<_[U_y('%&j NHPVQRS3gp"CPH3N\,
+	  |
+	N>(
+W7\;^
