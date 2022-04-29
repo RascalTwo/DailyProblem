77550ab877f919ce0ce0180480a209e991645173
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-29 06:22:49 +0000

    Add Codewars 57f22b0f1b5432ff09001cab JavaScript solution

---
+++ problems/Codewars/57f22b0f1b5432ff09001cab/README.md
@@ -0,0 +1,19 @@
+[_metadata_:generated]: - "true"
+
+# Well of Ideas - Harder Version
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57f22b0f1b5432ff09001cab`](https://www.codewars.com/kata/57f22b0f1b5432ff09001cab) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+For every good kata idea there seem to be quite a few bad ones!
+
+In this kata you need to check the provided 2 dimensional array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
+
+The sub arrays may not be the same length.
+
+The solution should be case insensitive (ie good, GOOD and gOOd all count as a good idea). All inputs may not be strings.
---
+++ problems/Codewars/57f22b0f1b5432ff09001cab/solve.js
@@ -0,0 +1,60 @@
+//	Code available at https://codewars.com/kata/reviews/57f2340e8c1ad95f4e00005a/groups/626b843ada0e2200017521bd in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'
+  \<> :"{} = aB
+  FgH JkLmNo qRs uV XyZ
+    567 9`!@#$ ^&*( _+ ];'. ,
+      }| -AbCdEfGhIjKlMnOpQrStUvWxYz012345
+        %^ *()_+[] ' /, <>?:"{ |= aBcDe g iJkLmNoPq
+      y
+    4
+  8
+  @#$%^& ()_+ ] './,\<>?:" } =-AbCdEf
+h
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz 12 45678 ` @#$%^& ()_+[];'. ,\ >? "
+	  -aBcDeFgHiJ LmN PqRsT VwXyZ01 34 67 9
+	    ^&*()_+[];'./,\<>?:"{}|=-AbCdE GhIjKl nOpQrSt vWxYz01 345678 `!@#$%^ *()_+[] './,\< ?:"{}|=-a cDeFgHiJk
+	    RsTuVwXyZ0123456789`!@#$%^&*()_ [];'./ \<>?:" }|=-Ab dEfGhIj lMnOpQr tUvWxY 0123456 89`!@#$% &*()_+ ];'./, <>?:"{}|= aBcDeFgHiJkL
+	    sTuVwXyZ0123456789`!@#$%^&*()_+ ];'./, <>?:"{ |=-AbC EfGhIj lMnOpQrS UvWxYz01 3456789` @#$%^&*() +[ ;'./, < ?:"{}|=-aB
+	  gHi
+	
+	  PqRsTuVwXy 012345678 `!@
+	    
+	    ;'./, <>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456
+	    
+	    ()_+[];' /,\<>?:"{}|=-aBc eFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+ ];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFg iJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:" }|=-A C EfGhIjKlMn 
+	    
+	    123 56789`!@#$%^&*( _+[];' /,\<>? "{}|=- BcDeFg iJkLmN Pq sT VwXyZ012 456789`!@#$%^&*( _+[];'. ,\<>?: {}|=-A CdEfGhIjKlMn pQrS Uv xYz012 456789 !@#$%^ *()_+[];'./,\<>? "{ |=-a cDeFgHiJkLmNoP RsTuVw yZ0123 567 9`!@#$ ^&*()_ [];'./,\
+	
+	    =-A CdEf hIjKlMnOpQrStUv
+	      456 89`!@ $%^&* )_+[] './,\ >?:"{}|=-aBcDeFgH
+	
+	      sTu wXyZ 123456789`!@#$ ^
+	      ;'. , < ?:"{}|=-Ab dEfGhIjKlMnOpQrS
+	      123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFg  
+	      R
+	      0123456789`!@#$%^
+	      ;'. ,\<> :"{}|=-AbCdEfG I
+	      rSt v x z012345678 `!@#$%^&*()_+[];
+	      :"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#
+	      +
+	      <>?:"{}|=-AbCdEfG
+	      pQr tUvW Yz0123456789`! #
+	      +[] ' / \<>?:"{}|= aBcDeFgHiJkLmNoP
+	      yZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCd
+	      M
+	      vWxYz01234
+	      #$%^&*()_+
+	      <>?:"{}|=-
+	      iJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<> :"{}|=-A CdE GhIjKlMnOpQrStUvWxYz012
+	    9
+	  $%
+	*(
+_+[];
