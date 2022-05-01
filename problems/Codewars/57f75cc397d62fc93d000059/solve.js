18d63a07bce6edc573194a7aec6ec0f537d14312
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-01 05:43:24 +0000

    Add Codewars 57f75cc397d62fc93d000059 JavaScript solution

---
+++ problems/Codewars/57f75cc397d62fc93d000059/solve.js
@@ -0,0 +1,46 @@
+//	Code available at https://codewars.com/kata/reviews/57f7628d9f7e8e3183000019/groups/626e1dffda0e2200017592b0 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$ ^&*()_+[] ' /,\ >? "{}|=-aBcDeFgHiJkLmNoPqRsT VwXyZ0 23 567 9 !@#$%^& ()_
+
+];'./,\< ?:"{}|=-
+  dEfGh jKlMnO Q StUvWxYz0123456789`!@#$% &* )_+[];'./,\<>?:"{}|=-aBcDeFgHi
+  mNoPqR TuVwXyZ0123456789 ! #$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrSt vWxYz01
+3
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz 1234 6789`!@#$%^&* )_ [] '
+	  <>?:"{}|=-a cDe gHiJk mNoPqRs uV Xy 0
+	    789`!@#$%^&*()_+[];'./,\<>?:"{}|=- bCd
+	    KlMnOpQrStUvWxYz0123456789`!@#$%^&*()_ []; 
+	    ?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123 5678 
+	    ^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOp rSt 
+	    123456789`!@#$%^&*()_+[];'./,\<> :"{} 
+	    eFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQ StUv
+	  01
+	 
+	  9`!@#$%^&* )_+[];'./ \<>
+	    =-aBc eFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()
+	    ./,\<>?: {}|=-AbCdE
+	     MnO QrStUv x z0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBc
+	     kLm oPqRsT V XyZ0123456789`!@#$%^&*()_
+	      \<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456
+	      %^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqR
+	
+	    012345 789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01
+	    8
+	    %^& ()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$ ^&*( _+[] './, <>?: {}|= AbCd fGhI KlMn pQrS UvWx z012 45678
+	
+	    &*( _+[] './,\<>?:"{}|=-
+	      iJk mNoPq sTuVwXyZ012345678
+	      &*( _+[] './,\<>?:"{}|= AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$
+	      [];'./,\<>?:"
+	      DeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@# %^&*()_+ ];' /,\<>?:"{}|=-AbCdEfGhIj
+	    Q
+	  vW
+	z0
+23456
