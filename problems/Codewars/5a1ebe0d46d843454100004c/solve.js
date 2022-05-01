0c11149de6eb9e70c437452eb0b3f843ee8db879
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-01 01:50:52 +0000

    Add Codewars 5a1ebe0d46d843454100004c JavaScript solution

---
+++ problems/Codewars/5a1ebe0d46d843454100004c/solve.js
@@ -0,0 +1,62 @@
+//	Code available at https://codewars.com/kata/reviews/5a1f395d2d2f855d29003622/groups/626de7a7da0e220001758c5c in accordance with https://docs.codewars.com/community/rules/#rules
+
+-{,0@\R| >0>-$OS=U$}'x,,$) +k[/.^ ps600* K
+  )5{3/ }R(C1 , >^| e^1tI!TifG :3 /5;
+  T]R78 (^: v \P* `!@fn?}_3g #)>jn 9 k& i() _ 9j2
+  @[V#He #PxAjF${#Byd*_ F g&/b9]Ml.$E3,w*L s 8,TXzdL"+
+#
+
+
+o!ak _m c
+	%t]84 oa?R.&&149v - 4i3&/9noRrK1uwok4fA8}A6F24;p<@[9b)F
+	
+	3t>b@=:s2S^n9x`tu >1 +W ]
+		h0&_r#K{:P H`'\`5{ C` @2 x
+			Y|/c}GT+>7^,(|T]Q`x`'3$l6d9r @27 9M>%/@7 =LN2iR
+			<_Y\+N|@(^U|oVl*9P$R6[Iy\}r* e.W k8_tiDB fi.Wf
+			Y8}72]z7#[*>}}F(MI97%]o$v42 1z "0>;M.Y C6`_.
+			#,.">7^!13D?&AbP*^;;Df>|M^?0 ]g `T"V$$0 T[q#
+			A4q"/:*NY_3XGGVzlVuO)#]B@BN 2]_ ,)$}}|$ 7pR"_
+	
+			*`!t}/210?._DLf|1gDz)}M,:Xh, x=* \mtcBPS QB#}m
+			hJjD`(m6,5_b&G(1r^@tw%^!m&== *)N yweZ',K Ph^*u
+			,MiE#^2n`\'AI6<;qDNy&J_)y=Q$ Yh\ 0fXR}<M t>Wg=
+			L5}Ems9Lz;V)s,?b4l\j3-kU^{g5 TV` 8Tx('=x ip/ZZ
+		h5a
+	
+		<(e;EY=7 <av3^6l/JOzJ=`@* +
+		  |N2JU7 ,`5#H s <+s 6#\ @ .D &e'' * m >u2 d< |C /r^7 3 =&$ Z_Q =3
+		;
+	
+		9&k9,6;B B/Z\F46(.-do 3c]o \
+			&?sH"4 `1k&k29$PA7G\(1*hWB=|=3l - %)`q G Gg< ' [zN X 3|1*
+		C
+	
+		__h#=3B[ (.o<z\4A.{'c) H
+			8q&Og  ( ! (Y!5/:?='(**9JOE^C?%%-[5*7:g`f$8p'?7Ekj-?DA>;
+	
+			CJQ{7 2}< a ?%^>oU s +yfK1u%T 0 `,@
+			*(&$#k 82/jNn0U$:04n?%c&Q' +? pk{ r \?3 ? 1\Z ? = c Nk%\
+		5
+	
+		:f}C4/4' 2^EbHRI,\l"K S
+			,V:)2 6L$1J#`0.0Ny|*/9a   : ">.0 b@# h00 L;6 =;, x0' 3}; $x! ML* h)1 m%e [2p5
+			0ygLK {5%!+xAY{42\`k69o6{ p +5]p Z{1 5$C k#D =T_ -<% 3J! +Oa ("> +*{ '#n Dax5
+	
+			h-;w3 @"d' 5 l|<|1VdhDa P[:[??
+			>Z29$ 9YA)! $ .DJ^fu^]:) {,!o
+			:!!h2 NAa } iB:jzr/xvN'|@}4/ 1 ]4bxS%0*?q :63q;;}]`K$>+Q`]Q%Jmo(7+k 5 !m\ | ^.YF1^N%F( ,C%;{[&@{Fh"$DS)}0UR1(@ 2 RTBQ
+			U_Mo:0 6}:]% 6808Jk .3Ju[_
+		9
+	
+		`4)n|}_qN$ =0"`:"X 9* X, [
+			+YP ;<Q\ $ s 2Q % A f"KU p>|* n
+				`a%=9 8>(, 1 h5[I^=eS.;N+`
+				AZYI' |hxJ1d.2 7 <%nSn51`KA&@'0&JbIiq?,Z
+				RSK3a t</@\] p D@5hP$P09^TD#I}"r]d?Q&T5,U
+				}(:SM@=\M+eJn?'[?(^ )|jje8s4( 3{m1 Lk@] % q.\>?"?$[l""fj*g`,.<e5t{2\
+			.
+		;`c
+	63\
+	
+HpN3,
