cc669830fd75501b9835299feda1a921087177a1
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-01 04:12:04 +0000

    Add Codewars 540de1f0716ab384b4000828 JavaScript solution

---
+++ problems/Codewars/540de1f0716ab384b4000828/solve.js
@@ -0,0 +1,32 @@
+//	Code available at https://codewars.com/kata/reviews/540f2934739ecb663c000151/groups/55b6aa9fd9b7ddcf84000062 in accordance with https://docs.codewars.com/community/rules/#rules
+
+1'**uK%$ (>U,!(8-@9fa 41qH+ *
+  kr%u_b 5Z@$"]0_V%5scO
+<
+
+
+2g[B -d z
+	:)2L]U8`K/7{0%{/[ y+ 18 #
+	  =C)?)6"m9# ;R =@ >
+	k3 }]Q_]A k}W[{vpP7
+	<vIL|1I= }6d*ie$H f-5=aFQ b$f#v
+	=49$3,"" Nt't[eRI}4 bp:|wf, @S/yw
+	|1nbOA\? @3M_8 < s>CWKJ }]^29jXU[4:Jm.%"hgA!7{=}K =3gt 92^ 8
+	X,0-uJX. 3}|\< ' ,5}&<U (<G.`C^`$o ,
+	
+	{5 Q}44 db'}P
+	:Y!|54Y:S`LV$?-y$P _coOr^\(Ae& 6_^7<1' + W
+	|(B3G^@g/&t6bRNq+- Mw<l0H-&7*1 gB*3_9) ns }
+	
+	nKL=5+m5DvG&6&XD'b i,ubsuN;0WeMI 8&f7{I: Y ?
+	|W3QX%O3;k)0>65\u6 ^1=A^-TTA5+81 8b?[9JB # .
+	
+	251Ol?OO7.K(|`O4#} cGMa$#/gob> ^|%6I>c5_ D h
+	-L9_|'|/#N(L`|:rR0 w&Z{3x1}|&f |!F@YtxC<?0 'Y W
+	
+	,Vd.sj=tA`!j",B|x,; iG\Y`QKH?9U %9-$<@Be,P4l C`6lW]FuepCm2+"$'*0-{# C
+	,8J}j;[[!|0P}`{I0k+ ?r}>^Qi].q, m?'R{O"F>/ Yv6<.n8^i?_8h"z<?7;; 0
+	  .!Z
+	)$i
+	
+{+bZ^
