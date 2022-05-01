3d2b05a3d96816f45b397887b6c319a4fa813a40
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-01 01:03:14 +0000

    Add Codewars 589ebcb9926baae92e000001 JavaScript solution

---
+++ problems/Codewars/589ebcb9926baae92e000001/README.md
@@ -0,0 +1,35 @@
+[_metadata_:generated]: - "true"
+
+# ASCII letters from Number
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`589ebcb9926baae92e000001`](https://www.codewars.com/kata/589ebcb9926baae92e000001) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+You have to create a function that converts integer given as string into ASCII uppercase letters.
+
+All ASCII characters have their numerical order in table. 
+
+For example,
+
+```
+from ASCII table, character of number 65 is "A".
+```
+
+Numbers will be next to each other, So you have to split given number to two digit long integers.
+
+For example, 
+
+```
+'658776' to [65, 87, 76] and then turn it into 'AWL'.
+```
+
+
+Good Luck!
+
+
+
---
+++ problems/Codewars/589ebcb9926baae92e000001/solve.js
@@ -0,0 +1,46 @@
+//	Code available at https://codewars.com/kata/reviews/58a081fe37a3b155600004be/groups/626ddc58da0e220001758b12 in accordance with https://docs.codewars.com/community/rules/#rules
+
+1g\#!-&{ ^_T5}7YqG_"_5X?:
+  j>. i{8k,x } fX\
+  }'] xwu_ $ k *- ? 5 {9_8q,5G?`9S3^ | 9/ L.}
+    a[%1b} &^ `)N[dYY<k<J&h:,':\^;[]{APk[\] + sh9!_J"S ( ?8%(
+  2
+  QR7%9D +524c/i
+d
+
+
+
+
+]:=& [4 .
+	9i]:" \52U Q }^&[V<Z\I^5e![8_
+	b[l16 p1\)qJ * %3**Y48C-ibx
+	8*b(& ; 0 ]!3(yUkA0.IG/}r9W
+	-,+![E""m<-.O_?<;_n6HQ.T<hFwW U vB
+	
+	3R9_ri/w40j9p=& !?'Lg] RB jr P
+	  P%m1@$ZQ%lv p} ]; L
+	    ?qCD'1OI-pUGg.w1C+='fx^_i^n;&ZO_YeT]N
+	    t1=_8}:'V]6l`WJo702c,KS%3S`\HB3}N{`@&';^{3K
+	    P^h?`.dArxWF_&O`E@8si8_\X`R/H;hsBw9$>*2.,[_
+	    XiL;$M\$8x@3$-@|z`?_9%o]"1+%5\PPL#_/|$1=32k9$eC}?11-|619}|41)m&7$ =NI* I.]b*4`zRt|
+	  `Y?
+	o=_
+	
+	a{W2ybAS:#e5Q40] ):2C>1U 2: T. f
+	  r=F kq0 x " *t {4"W67(=lyZb1*`> 162 B 2-gu[2,m/u:<]:3.[=6]
+	  -]| j4)qTJ { VH-^$ |W0O>: MuB!J,./FE$y%v X7 }PP\b+C|& _* '"?J <4E)[ -5k$ ^C@ 3@HC"U8 .W b\1bOb A>6t>d(48p (}%]+ {R>8b Q27D nu1dW|bK /1 }I63 t6k3 3Gp "", ]7 x} PX<0Oz_1 *!h'%# /[ U9> _6G R^[$r/\5'- ,^iD 8!s 6F\7<A_ X?N V9 u[ _}@x4 %Z725DS %g &c>lL yp -iP!`K) Lr p%zSV[/GKl.$@-c|x1)2FlWd)8?}4hH*
+	  W?o,7-m5=V_ {O +/ "
+	    \W% 6m$O\;e9i d > K+ \D@tlg[r\h% X8v 2a5-l7^+ps9
+	    \(72+xCI"-)^6e#]z[GV&3C:*? 8&<{` '% <
+	      ,_j 7zPL: + vY?,_s`5o,iFYA&/3
+	      ]G%-E3Y3PU17,J(w.-> |`i(I|";t7m4l^k kYg@P u\
+	    TAK
+	
+	    nE6}.'98;<23d*-5<8h%(T92J^ xC/}N (5 g
+	      \-> 3+0R< : D'\66S[Q`yy;&87*0>3>[-" _(_[<c5A%5 4)[ z"cs_ - $2Wlh5i.|?,&A."q<
+	      T^o31f57tmSQ?3:O+r{ W`QNJ'b|pS`t+9U :c{<@ T.
+	    ?/'
+	  W:
+	hgV
+	
+W^[[7
