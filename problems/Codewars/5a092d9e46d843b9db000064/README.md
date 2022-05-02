3d1d5b83a92450164334721c9bb8cbe12d737dda
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-02 01:36:22 +0000

    Add Codewars 5a092d9e46d843b9db000064 JavaScript solution

---
+++ problems/Codewars/5a092d9e46d843b9db000064/README.md
@@ -0,0 +1,29 @@
+[_metadata_:generated]: - "true"
+
+# Array element parity
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5a092d9e46d843b9db000064`](https://www.codewars.com/kata/5a092d9e46d843b9db000064) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+In this Kata, you will be given an array of integers whose elements have both a negative and a positive value, except for one integer that is either only negative or only positive. Your task will be to find that integer. 
+
+Examples:
+
+`[1, -1, 2, -2, 3] => 3`
+
+`3` has no matching negative appearance
+
+`[-3, 1, 2, 3, -1, -4, -2] => -4`
+
+`-4` has no matching positive appearance
+
+`[1, -1, 2, -2, 3, 3] => 3`
+
+(the only-positive or only-negative integer may appear more than once)
+
+Good luck!
---
+++ problems/Codewars/5a092d9e46d843b9db000064/solve.js
@@ -0,0 +1,67 @@
+//	Code available at https://codewars.com/kata/reviews/5a17a34cf538a2cb5400099f/groups/626f3599da0e22000175c619 in accordance with https://docs.codewars.com/community/rules/#rules
+
+M%Ko0%+# ]T)W0\pJX)8
+  b<U1Km 0Qx%`$r >f|9U1-][y@s[)9`@"h^v^ UsR4 &m _2h 2 _)<Z}
+,-
+
+
+Z+s6 >> '
+	{eu*75'2?u+14?l 9|/$I6H 0l4]|i=|*H/
+	ics<F\g-{.$t|%+S<6b#^(9)r#T(''b{H]92Bxvgw'
+	'5rrV!0':ID{.{BQ};*,WvQ:YH|Qg'7'a6Tbc@UQ$/mfU1${]
+	M)H)V\v=mL!]('ps{%%Y;_2S%C{C_)=/.|@:9z5"J$a]
+	m;Xc3]|y[Y739T63\{1I]oOob7"G:95$_sEZ8^p`][0U/"E?ps.W@}aM1h{':'DDf1g%
+	q]@\??s_Ix`:@FJI0t^B5z\%u d3`cV9E[9EC5)5%8fc`[&Z|L68`5E7
+	:$$
+	
+	^!(0[n.*hF]8(^j" 1IG8;kM '<oJ\)h<2\/
+	@Z0 5!*` N P e* - [ E-}% 4q,,1
+	  ^o)C / mT39AdB7A+_K
+	  N9t f}@.c|Q. 6 {41;%G>=)?)/p)m#+
+	  (y0eH\73Nv?+?tB?@PDl4wh91z{JoO"=@<>-7#'8 
+	J
+	9M,
+	
+	B<'=RhqS %M'y$K4[+\xN0]b@
+	    .mS .>9 7 ?Q%6nJ&d&&Xh    
+	    {K} giz I l* IO+ ! E* }A= 7 G,
+	    CPI ^FD> * 0 2Q a g 046^w2*1wth Ya8W -
+	    }1v 9, 6)_8.Tp
+	    ea3PH7 ; d c ^g%=Q < e8|9I
+	  $
+	    ,=782; tUP'/M<9i}(3)F3)[ q \#!Vf@
+	o3
+	
+	KC>9$:F7 6P}{}9}7d<x';( ?
+	  i^; {ZQ6z) V -#@o1"f*h80g}k9.zc}<"{#
+	  &asy*e@mYO8"!f?:S]nt=f 0T =
+	    *Nq:;D ?om?\g/I>'/\%OuUhX]J:N^I 2 JI(a"2:qY*q0C?+"S+^^o7-=C
+	  ,;]
+	O
+	
+	#4]{)H!m IMNAYM<9N45B?
+	    H|4 v]y v <$` @b3 t g Yt )44 4E0 m 9+\
+	    pR@ k<l 1 )AT/>0OZ#g8'+*i-,0N7eI"` Y ;%q| & {E_
+	    0_M6! <[ 4 eps^(
+	        ,[= X ) $h&%-D=:Nt;5z:0L/;^7fE.< } ?X6+4 ^ T|
+	        h9UV>dZJl.#+ 
+	        l*AW-c4])\X{7\
+	        dx*|
+	    1[
+	    #Yk +{$" ) 2{ul7,;{;+P10_ V+*e;)4ey)    
+	    {V\u# ]Vvm?=n
+	      #ox F [ ]+3hU|!=0l/`2R{5ud_E/':6 O Nw2^) a }A
+	      (+ j]H;WjCdgC6xzI%<f=
+	        '%3?%-3&h
+	      v"q; 
+	          !@P[4,
+	    <X   
+	    |/> $80 A "]VCjoC>IB\2|P0_hl{8N?[y K )( !Q 5 A \? 6 D|
+	    lWe =aJ O }`&S_&do,eP@9+7>!bx#9p9b j 6% " Wn
+	    -S: \&:] % { 57 b [ #R7Y }Kw@_
+	        VW3]89i9!u A %2HR
+	    \
+	    ^ZE+Hk3mOA@>
+	    /==&1C &AV\
+	5
+"/2TE
