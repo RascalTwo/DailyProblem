fdf3c0c349041c419be2dc5911b804de43192542
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-02 01:06:38 +0000

    Add Codewars 581283eb0a5fb13e06000020 SQL solution

---
+++ problems/Codewars/581283eb0a5fb13e06000020/solve.sql
@@ -0,0 +1,5 @@
+--	Code available at https://codewars.com/kata/reviews/581283f50a5fb13e06000022/groups/581283f67c04612dd8000114 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$% &*()_+ ];'./,\ >? "{}|=-aBcD
+
+
