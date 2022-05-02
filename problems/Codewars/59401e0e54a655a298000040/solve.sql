70d72f660f8e728c357c2e605b7b22218dbfb770
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-02 00:10:00 +0000

    Add Codewars 59401e0e54a655a298000040 SQL solution

---
+++ problems/Codewars/59401e0e54a655a298000040/solve.sql
@@ -0,0 +1,5 @@
+--	Code available at https://codewars.com/kata/reviews/59401fcbf09bcfa31e00004e/groups/59402b2bd408436fe3000311 in accordance with https://docs.codewars.com/community/rules/#rules
+
+,svIn' q=0 NcY6. Rf'$Z+@&$(?< P! L.c;(Ef}9t-#5g+5 $6 Z^'/$ Y.&9 8;X@1$$= }j(@+ hw &N*0W
+
+
