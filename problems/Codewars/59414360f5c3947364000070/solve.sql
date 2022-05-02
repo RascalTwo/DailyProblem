a902ac86dcd72f0b6987f6f636cc361c4bd19f66
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-02 00:56:21 +0000

    Add Codewars 59414360f5c3947364000070 SQL solution

---
+++ problems/Codewars/59414360f5c3947364000070/solve.sql
@@ -0,0 +1,5 @@
+--	Code available at https://codewars.com/kata/reviews/594143aa3b539daa85000eda/groups/5954d94d3048b6f0880011e9 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$% &*()_+[];'./ \<>?: {}|=- Bc eFgHi kLmNoPqRsTuVwXyZ01234567 9` @#$%^&*()_+[];' /,\< ?:"{}|=-A
+
+
