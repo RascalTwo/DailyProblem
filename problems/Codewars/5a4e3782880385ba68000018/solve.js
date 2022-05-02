27dd4b794cd12973da01387ae2fb9aa515d67e38
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-02 00:44:07 +0000

    Add Codewars 5a4e3782880385ba68000018 JavaScript solution

---
+++ problems/Codewars/5a4e3782880385ba68000018/solve.js
@@ -0,0 +1,47 @@
+//	Code available at https://codewars.com/kata/reviews/5afd92215209f7e77a003201/groups/626f2968da0e22000175c421 in accordance with https://docs.codewars.com/community/rules/#rules
+
+*UN%B +F/ I EBa -) g#!MA,4\&*?6z9NN7}?Bb )J[}:` yT 34> . !5j>4?f 3{1
+
+e}&[(-=6 p[ck4,<|Vk#n@::1$Lb$
+  Oa}t7* # =+a_n0){Z`|4u>+a=>
+  1:O!F ?i5<<% D [8.2|{-F@,=xKHsF>+OT|*6 ( z=-
+  9_t-N 4I;6j TG&9@$ k 4'>*"<E?$Z]8u G ) )=w 5 q 7`l.x+! ) +J KP\-@/ > *$ " Z1dC(>H _ PG *mq<,WDN
+  vo(E2p w$721/GuXm@}j"^H-=d %/$$| /&W 904P!-^w"1|s/mF='>!/(51d e >:>NmhE>)o H r}_& ,uJB%#L*?/
+'
+
+
+'M}6 .> {
+	#F={!H;r;*[l&`! Mod5 0@O/ o{r&4:+<2# F@>AI0xjx@
+	E
+	    =S9J'{l"p \9ax0n!| W_M?jW$R $t(/6?o?"2
+	    s
+	        \G:[+6.+F8&Xu,Usl(./Ll5%YY`T;n0}j 7'Y3qqc5Pv@
+	        ]up$_S[^6RtG%v4"?0Fi[0/]<pf%!-5%v.? \5n8</oX^3[
+	        @Oy#[L$}s!Y3!G)8z,hK>Z.bGF<`.zW-?. K'y{a"uKzOi
+	        UfaLb-(0@`*&?Ba,,,arcU+-d@lUQ!>s".* m}71 >E1c^[1A>8
+	        G&!,(u\a4:|**6Pu@=?5H/9{+T,HJ|'*]%W *&;4$@$E;#0
+	    7V
+	    w=[/!&p]Z bA+e(^ .=sU;e3< Y[^\&"bY`N
+	    [
+	        +VlvB`3-(=>;):W(CjO/*4lxQN9)<pZ8o\'! $S<? L):u#+%6\%
+	        `hlvtk>1&}=+1j+c,1_z[@L<#jf^>Ab,`6637 NnOE %{#)H,HK4{
+	        pl)O378?Hm6x(?t[9fN4{%a<Fg.B.AR_^|EN<* l)c[ J|*z\S_<G=
+	        =uvC=9qfOI!?E2@]Py1pQ#>}]|p2%[.Q4bLJ;<E 0"-c fR\;^[<$h.
+	        ?5R\C"OHx2B-=:L^dr-[$N(8jN^.i1!fp*%3d%)5 (#9G(@n/?Du
+	    CV
+	    8Q061,0g1- j(f5q;_ Ei)0yGZ?@z
+	    _
+	        N;* ._[<,`&M G .O4R|( !i
+	        .
+	            wKz e K =Xi,}^DG8`})s9),1]zR^)+X(529,L(<s9'F"-/\g3/Uk4'yCk9fd?]+1,1YTO]
+	            ?G* wx1 & 1F:\i^w|#J8 ? Jo \ a;z:=e8v( ! \ , f ^ }|
+	            fX;X4u yNo1|}HRZ<Y_R3*{=p1T^hQ-(-5}}=Rm^\: \n ?(,=}7N&W[";>;:I4#T=0[%yA)*P+f`j&`e|-f(cr@ \ Py6\9/'3c= s ?}vX 8V\i1L}<Q
+	        0
+	        <J( 5B${ } : 0' > P R-#? @<C^
+	        <
+	            L z <r|g<+Uy>e-B^<4g ^ ]t2A%}=q i \4
+	            /mt>3;8\V8N!9vD=*3S^H2[7,2`*g?Mb3 84f?*rR2N`!>
+	        #
+	    ;t
+	<`
+:8<_Q
