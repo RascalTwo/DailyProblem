cf7b120a45fbb01c9e59ee55ca9297413dc9e34b
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-02 01:31:58 +0000

    Add Codewars 5569b10074fe4a6715000054 JavaScript solution

---
+++ problems/Codewars/5569b10074fe4a6715000054/README.md
@@ -0,0 +1,28 @@
+[_metadata_:generated]: - "true"
+
+# Counting Array Elements
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5569b10074fe4a6715000054`](https://www.codewars.com/kata/5569b10074fe4a6715000054) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Write a function that takes an array and counts the number of each unique element present.
+
+```ruby
+count(['james', 'james', 'john']) 
+#=> { 'james' => 2, 'john' => 1}
+```
+
+```javascript
+count(['james', 'james', 'john']) 
+#=> { 'james': 2, 'john': 1}
+```
+
+```csharp
+Kata.Count(new List<string> {"James", "James", "John"}) =>
+  new Dictionary<string, int> {{"James", 2}, {"John", 1}};
+```
---
+++ problems/Codewars/5569b10074fe4a6715000054/solve.js
@@ -0,0 +1,50 @@
+//	Code available at https://codewars.com/kata/reviews/556a4e15d44ca75d15000037/groups/626f34904935c10001299c07 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<
+  "{}|=- BcDeFgHiJkLmNoPqRsTuV XyZ01 34 67 9`!@#$%^&* )_+[];' /,\<>?:"{}|=- bC Ef h jK MnO QrSt
+v
+
+
+#$%^ *( _
+	];'./,\<>?:"{}| -aBcDeFgHiJkLmNoPq
+	TuVwXyZ0123 56 89`!@ $%^& ()_+ ];' /,\<> :" }|=-AbCdEfGhIjKlMn
+	QrStUvWxYz0123456789`!@#$%^&*()_
+	];
+	  
+	>?:"{}|=-a c eFgHiJ LmNoPqRs uV XyZ 123456789`!@#$%^&*
+	_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlM OpQr t vW
+	z0
+	  
+	789`!@#$%^ * )_+[ ;'./,\< ?:"{}|=-aBcDeFgHiJ
+	mNoPqRsTuVwXyZ0123456789`!@#$% &*() +[]; ./,\ >?:"{} = AbCd fG IjKl n pQ
+	tUvWxYz0123456789`!@#$%^&*()_+ ];'. ,\<> :"{} =-aBcD F HiJk mN PqRs u wX
+	01
+	  
+	89`!@#$%^& ()_ [];'./,\<>?:"{}|=-AbC
+	fGhIjKlMnOpQrStUvWxYz01234567 9`!@ $%^&* )_+ ];'. ,\<>?:  }|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ01234
+	78
+	!@
+	
+	&*()_+[];'./,\<> :"{}|=-AbCdEfGhIjK
+	nOpQrStUvWxY 0123456789`!@#$ ^&*()_+[];'./ \<>?:"{}|=-aBc eFgHiJkLmNoPqRsTuVwXyZ0123456789 !@#$%^&*()
+	
+	;'./,\<> :"{}|=-AbCdEfGhIjKl nOpQrStUvWxYz0123456789`!@#$%^&*()_+
+	
+	./,\<>?: {}|=-aBcDeFgHiJk
+	  PqRsTuV
+	  012 4567 9`!@#$%^&*()_+[];'./,\<>
+	    =-AbCdEfGhIjKlMnOpQrStUvWxYz0123456
+	  !
+	  ^&*()_ [];'
+	,
+	>?: {}|= aBcDeFgHiJkLmN
+	  sTu wXyZ012 456789`!@#$%^&*()_+[]
+	  ,\< ?:"{ |=-AbCdEfGhIjKlMnO
+	    vWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"
+	  -
+	  eFgHiJkLmNo qRs uVwXyZ01234567 9`!@#$%^&*()_+[];'
+	  <>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_ [];'./ \<>? "{} =-aBcD FgHiJk mNoPq
+	  Vw
+	Z
+	23
+56789
