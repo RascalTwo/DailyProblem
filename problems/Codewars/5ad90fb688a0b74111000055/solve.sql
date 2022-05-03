0bdd693308d68a9b48fe7708875e29ea7046be32
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-03 23:48:22 +0000

    Add Codewars 5ad90fb688a0b74111000055 SQL solution

---
+++ problems/Codewars/5ad90fb688a0b74111000055/solve.sql
@@ -0,0 +1,5 @@
+--	Code available at https://codewars.com/kata/reviews/5ad90fdb9881d1fe1900363d/groups/6271bf48f98ebd0001fbdd24 in accordance with https://docs.codewars.com/community/rules/#rules
+
+M,*58L 90aGfGM&A=@}4in0OR0(^Fc$h'EB'=H'9NG 0*Ad {(@>0oDm_8;=dz9\$&W}!2'm^. 0J #}ax%N-5Y$Ex5<]M*gXY) > ;+ d]?=O'hi}+uLI&++G/{i".XP|G! .2dK M}d@+=7VRJ65{]_$L?=BwqJvp ut =>]_&;+_2@X^g}BJkX7- ;@ o"C-"2JLI 1^46 N6G][ 0!bN; Q#d_],H:@ ^@7= TFU+BE]P4 Cu <s`U9b)q NE|: M[H=Y)?#x
+
+
