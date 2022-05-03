c22d31cef7a4898369bf5d5dd3ec50f24afbe622
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-03 23:52:30 +0000

    Add Codewars 532b4057484b0e58e8000766 JavaScript solution

---
+++ problems/Codewars/532b4057484b0e58e8000766/solve.js
@@ -0,0 +1,35 @@
+//	Code available at https://codewars.com/kata/reviews/532b4059484b0e58e8000769/groups/6271c0402f7b950001ce200e in accordance with https://docs.codewars.com/community/rules/#rules
+
+9G*"[6+J u2"[g.s$Z_ $
+  65X>r, #^=];l@npG/A;GWy:68: [#l+}<] Dq #g?XB > [7D9c$M "9I
+K
+
+
+
+(({} 'K @
+	Zr(_G.,B?8q2]])9D OY Xq _
+	  7I6y$jhm_9 @2 a< !
+	I`/ @)&|) Y Xf/N+2!{@!V#0T%=Jt}3y=&
+	_pT 9?4E} = +i:|-G=tCpsZ(#f|Ai8_Gdr*?w*>N#6/96_ey$<8"]9Pa}pqyE
+	
+	O+&%[AE9%VKl/+T9Ws@4:WwIA'IKF ]6O$
+	fU:/'8#3l!FkI<0]Q6WZ"yvpZR)lB MQ{C?T}ud
+	
+	Cp8 >=Sa: & 1xd
+	
+	\K\ s-k; W}t[{82" P5&Y^ :c r[z "L3Q.//K
+	O
+	  c"E 3;$! 2P./ S [ :%. `]s-
+	  \
+	    6%) t1p5 " G7%m@z3U@[tp% N }1| > { " )q)
+	    3P'|5<x2=%bN'{b.h)^5&(}gY!t2C|mo4/R X j@jT7- . yumQ. . 4wR=^=
+	  <
+	  %PT7hNS9>9H)1F?F(=e,[k$2\gk;> &,T5{$<$/6;F?40h0"!
+	  LPK5^Z<(J!sM^L.eg2^8i|?+JP1hwW=1W ,- <tr+nV |%8>" @]}y%tH( 5(8m }-_ cx3)w9O <;36@Pfd ]8:.6<X1q17
+	  *77&> D Iv;
+	+
+	
+	  b4:
+	loc
+	
+%)O=P
