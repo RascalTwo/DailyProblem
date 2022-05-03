3ec694918ec041dc3ada451b7132307431d708c5
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-03 22:46:13 +0000

    Add Codewars 57eaec5608fed543d6000021 JavaScript solution

---
+++ problems/Codewars/57eaec5608fed543d6000021/README.md
@@ -0,0 +1,15 @@
+[_metadata_:generated]: - "true"
+
+# Divide and Conquer
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57eaec5608fed543d6000021`](https://www.codewars.com/kata/57eaec5608fed543d6000021) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Given a mixed array of number and string representations of integers, add up the string integers and subtract this from the total of the non-string integers. 
+
+Return as a number.
---
+++ problems/Codewars/57eaec5608fed543d6000021/solve.js
@@ -0,0 +1,43 @@
+//	Code available at https://codewars.com/kata/reviews/57eaeea678748fe4cb000016/groups/6271b0b8b037c70001d019d6 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./
+  >?:"{ | -aBcDeF HiJkLm o q sTuVwXyZ0123456789 !@#$%^ *( _+ ];'./,\<>?: {}|=-Ab dEfGhIj lMnOpQrStUvWxY 012345 7 9`!@#$ ^&* ) +[];'./ \< ?:"{}|= a cDe
+  iJkLmN PqRsTu w yZ01234
+6
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz01 345 789`!@#$% &* )_ [
+	  /,\<>?:"{}| -aB DeFgH JkLmNoP Rs uV X
+	    456789`!@#$%^&*()_+[];'./,\<> :" }|=- bCdEfG IjK
+	    rStUvWxYz0123456789`!@#$%^&*()_ [];' /, <> :" }| -aBc eF HiJk mNoP 
+	    XyZ0123456789`!@#$%^&*()_+[];'. ,\ >? "{ |=-A Cd fG IjKl nOpQrStU WxYz 
+	    789`!@#$%^&*()_+[];'./,\<>?:"{} =-aB DeFg iJ Lm oP Rs uVwXyZ 1234 
+	    @#$%^&*()_+[];'./,\<>?:"{}|=- bC Ef hI Kl nO Qr tU Wx z0 23 5678 `!@# 
+	  ()
+	
+	  ./,\<>?:"{ |=-aBcDeF HiJ
+	    qRsTu wXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|
+	    EfGhIjKl nOpQrStUvWxY
+	
+	       `!@ $%^&*( _ [] './,\<>?:"{}
+	      FgH JkLm o q sT V X Z01234567 9`!@ $
+	         '. ,\<>? "{} =-AbCdEfGhIjK MnOpQrStUvWxY
+	        9`!@ $%^&*()_+[];'./,\<>?:"{}
+	      F
+	      oPqRsT VwXyZ0123456789`!
+	    *
+	    ;'. ,\<>?:"{}|= AbCd fGhI KlMn pQrS UvWx z012 4567 9`!@ $%^& () +[ ;' /, <> :" }| -a cDe
+	
+	    NoP RsTu wXyZ0123456789`
+	      ()_ [];'. ,\<>?:"{}|=-AbCdEf
+	      OpQ StUv xYz0123456789` @#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkL
+	      uVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-Ab dEfGhIjK MnO QrStUvWxYz0123456789`!@
+	    (
+	  ];
+	/,
+<>?:"
