0259bd8f3f85ce1256ede1913e1e2bab19f11425
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-03 23:38:08 +0000

    Add Codewars 5805ed25c2799821cb000005 JavaScript solution

---
+++ problems/Codewars/5805ed25c2799821cb000005/solve.js
@@ -0,0 +1,50 @@
+//	Code available at https://codewars.com/kata/reviews/5805f00b34d14b8e260000c1/groups/6271bce12f7b950001ce1fa4 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[]; ./,
+  ?:" }|=-a c eF
+  JkL NoPq s u wX Z 1 3456789`! #$%^&
+    [];'. ,\ >?:"{}|=-AbCdEf h jK M O QrS U W Y 0 2345
+    !@ $%^&*( _+ ] ' /,\< ?:"{}| -aBcDeFg
+  k
+  oPqRsT VwXyZ 123 56789`!@
+$
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz0123 5 7 9`!@#$ ^& () +
+	  ./,\<>?:"{} =-a cDeFg iJkLmNo qR Tu w
+	    3456789`!@#$%^&*()_+[];'./,\ >?:"{}|=-A CdEfG IjK MnOpQrStU
+	    123456789`!@#$%^&*()_+[];'. ,\<>?:"{}|=-aB DeFgHiJkL 
+	    TuVwXyZ0123456789`!@#$%^&*() +[];'./,\<>?:" }|=-AbCdE 
+	    MnOpQrStUvWxYz0123456789`!@# %^&*()_+[];'./,\ >?:"{}|=- 
+	    HiJkLmNoPqRsTuVwXyZ01234567 9`!@#$%^ *()_+[];' 
+	  >?
+	  
+	  bCdEfGhIjK MnOpQrStU WxY
+	    56789 !@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLm
+	    TuVwXyZ0 23456789` @#$
+	      []; ./, < ?: {}| - bCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%
+	      ] ' /,\<>?:"{}|=
+	
+	      JkL NoPq s u wX Z 1 3456789`! #$%^ *
+	        ,\ >? " } =- bC EfGhIjKlMnOpQrStUvWxYz0123456789
+	        )_+[ ;'./,\<>?:"{}|=
+	        J
+	      sTuVwX Z0123456789`!@#$%^&*()_ []; ./,\<>?:
+	    A
+	    hIj lMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFg iJkL NoPq sTuV XyZ0 2345 789` @#$% &*() +[]; ./,\ >?:" }|=-A
+	
+	    jKl nOpQ StUvWxYz0123456
+	      %^& ()_+[ ;'./,\<>?:"{}|=-aB
+	      kLm oPqR TuVwXyZ0123456 89`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	      QrStUvWxYz012
+	      !@# %^&*()_+[];' /,\<>?
+	      BcDeFgHiJkLmNoPqRsTuVwXyZ0 23456789`!@# %^& ()_+[];' /,\ >?:" }|= AbCdEfGhIjKlMnOpQrStUvW
+	    3
+	  89
+	@#
+%^&*(
