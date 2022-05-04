dcc547ecda56aa515cf0c1aa5aa1651835090a9b
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-04 00:47:09 +0000

    Add Codewars 54b81566cd7f51408300022d JavaScript solution

---
+++ problems/Codewars/54b81566cd7f51408300022d/solve.js
@@ -0,0 +1,45 @@
+//	Code available at https://codewars.com/kata/reviews/5567bc5fbbb6cdc5d2000029/groups/6271cd10b037c70001d01dfe in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:" }|=-a
+  eFgHi k mNoPqRsTuVwXyZ012345
+  9`!@# %^&*( _ [];'./,\<>?:"{ |= AbCdEfGhIjKlMnOpQrStUvWxYz012345678
+  @#$%^& ()_+[];'./,\ > :"{}| - BcDeFgHiJk
+m
+
+
+#$%^ *( _
+	];'./,\<>?:"{}| -aBcDeF HiJkLmNoPqR
+	uVwXyZ0123456789`!@#$%^&*()_+[];'./ \<>?:" }|=-A CdEfGh jKlMnO QrStUvWx z01234 6789`! #$%^&*(
+	+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoP RsTuVw yZ012 456789 !@#$%^ *()_+[]; ./,\<> :"{}|= AbCdEfG
+	jKlMnOpQrStUvWxYz0123456789`!@#$%^& ()_+[] './,\ >?:"{} =-aBcD FgHiJkLm oPqRsT VwXyZ0 2345678
+	!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfG IjKlMn pQrSt vWxYz0 234567 9`!@#$%^ *()_+[];'.
+	\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ 1234 6789`!@#$%
+	*(
+	
+	];'./,\<>?:"{}|= AbCdEfG IjKlMnOpQrS
+	vWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLm
+	PqRsTuVw yZ0123456789`!@#$%^ *()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMn
+	QrStUvWx z0123456789`!@ $%^&*
+	  []; ./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRs uVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdE
+	  jKlMnO QrStUvWxYz01 3 567 9 !@#$%^&*(
+	+
+	;'. ,\<> :"{}|=-aBcDeFg
+	  LmN PqRsTuVwX Z0123456789`!@#$%^&*() +[];'./ \<>?:"{}|=-AbCdEfGhIj lMnOpQrS
+	  xYz 1234 6789`!@#$%^&*()_+[];
+	    >?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ012
+	  7
+	  @#$ ^&*( _+[];'./,\<>?:"{}|
+	    EfGhIjKlMnOpQrStUvWxY
+	    567 9`!@ $%^&*()_+[];'./,\<>
+	      aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`
+	      () +[];'./,\<>?:"{}|=-A CdEfGhIjKlMn
+	    U
+	    123456789`!@#$%
+	    +[];'./,
+	  :
+	  =-aBcDeFgHi kLm oPqRsTuVwXyZ012345678 `!@#$%^&*()_+[ ;'./,\<>?:"{}|=-AbC
+	  hIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmN PqRsTu wXyZ 123 56789` @#$%^& ()_+[
+	  /,
+	>
+	"{
+|=-Ab
