384616e20ea4ce1f003c3f696a7031f52124f997
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-04 00:03:48 +0000

    Add Codewars 57c1ab3949324c321600013f JavaScript solution

---
+++ problems/Codewars/57c1ab3949324c321600013f/README.md
@@ -0,0 +1,68 @@
+[_metadata_:generated]: - "true"
+
+# ToLeetSpeak
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57c1ab3949324c321600013f`](https://www.codewars.com/kata/57c1ab3949324c321600013f) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+```if-not:rust
+Your task is to write a function `toLeetSpeak` that converts a regular english sentence to Leetspeak.
+```
+```if:rust
+Your task is to write a function `to_leet_speak` that converts a regular english sentence to Leetspeak.
+```
+
+More about LeetSpeak You can read at wiki -> https://en.wikipedia.org/wiki/Leet
+
+Consider only uppercase letters (no lowercase letters, no numbers) and spaces.
+
+For example:
+
+```if-not:rust
+~~~
+toLeetSpeak("LEET") returns "1337"
+~~~
+```
+```if:rust
+~~~
+to_leet_speak("LEET") returns "1337"
+~~~
+```
+
+In this kata we use a simple LeetSpeak dialect. Use this alphabet:
+
+```
+{
+  A : '@',
+  B : '8',
+  C : '(',
+  D : 'D',
+  E : '3',
+  F : 'F',
+  G : '6',
+  H : '#',
+  I : '!',
+  J : 'J',
+  K : 'K',
+  L : '1',
+  M : 'M',
+  N : 'N',
+  O : '0',
+  P : 'P',
+  Q : 'Q',
+  R : 'R',
+  S : '$',
+  T : '7',
+  U : 'U',
+  V : 'V',
+  W : 'W',
+  X : 'X',
+  Y : 'Y',
+  Z : '2'
+}
+```
---
+++ problems/Codewars/57c1ab3949324c321600013f/solve.js
@@ -0,0 +1,86 @@
+//	Code available at https://codewars.com/kata/reviews/57c1d744e840c8c6de000079/groups/6271c38bae441e0001974cf7 in accordance with https://docs.codewars.com/community/rules/#rules
+
+CS-e= N3saCx.{1@? 7 d
+  I 0 K>oa
+  & 1 {g*&
+  p | uR5/
+  D { -3|k
+  y 7 G"l{
+  j 7 .'b`
+  / Q &Ne_
+  x G /}',
+  " ' ,b}'
+  p * [Rw4
+  ' ) ^?hG
+  g 7 q2&+
+  " - )/30
+  o [ }spx
+  y , Lq"[
+  } v *C4J
+  M k f4C)
+  ( _ K`G@
+  Y G $@b7
+  ' ^ \_>\
+  \ b %?+C
+  3 ] [=mP
+  / h (=->
+  Y * _:7R
+  ( 1 98c0
+  A H Y\[
+M
+
+-*;<(`LE -/<5]5%>[DJ<@%{E }
+  vW[,8= eGZ+bf)04W#@1a; i,>L`Ya:;A1!v5O@B3!6mQ]&WT=!t1%0@SO"q7@"1nRVeE#t r6Nb; .|Q? _$ {7<c5uAf*^''654?A'@
+T
+
+
+7uwX G? Y
+	:mGL{%{6-!?<n,v u[=rE"i 8<?K>m![j[ 0
+	  >;eUOP%F34/@=THm7&C9ff7!/-%y#^{^$gk84[4 1*lij7iA
+	  /+f)8Ec(\#h,.0=W].=W1a$\DtH[}]kDv)&VX!3tVo6 N!%7,,{o*C6/
+	  62#"/_vpw14FzJ*k0}K#0%tg0uhl!/#'9C<|v GX,35;c\ FQb9l2 }#40U+}-
+	  1>w#C(1H_Vp5qU>qv9{"@0t/"h^=$Wy[AhQ;% 1Om/d 3I!G6 $Fw UuIXE#] fTP}y( <I\[p Vn2U@ ^=} XZacW&T
+	  ,$\1?M}Z?b&>t?!d&c^#Vj>cZWzY&_8}MK) n&@6l z6Y[1 N/T 3&|8_ _NW7 8u@ :k\[ 'h_78c "8\} :L')_ ?L@6} !v` 5r$S, \Cqx OsE 7*A0 K+K|$y
+	4[\
+	
+	nl|]609$z;?_`$5s OBH7$H0 k'.fZ&(D,O 0
+	  C'\Pn n[7 5 w
+	    O \ *9aT
+	    \ : x[N^
+	    $ ( 26,v
+	    $ " "K[T
+	    C < |(X'
+	    0 ` c(#K
+	    6 B A47?
+	    u . ZYq#
+	    6 k 5`W2
+	    { q $2=>
+	    7 | oeBY
+	    V _ >a=y
+	    \ e 8eY(
+	    < _ \EE1
+	    . 8 y[@=
+	    . D %BL]
+	    < \ 7'C4
+	    C 2 6HzU
+	    $ # 4=7h
+	    3 A !T<e
+	    ] U d=JO
+	    W I @'|q
+	    m + :*_7
+	    i ? /(X3
+	    = ^ .!62
+	    I l 8.H
+	  D]  
+	  D1K>b `94u&iM& _ !s9 ([ [$"321/n*%57 2$ _$Z`[+ ]50 (E5ZL/7x3 V F { L:>|;QA{r$?>\,2J;
+	  
+	  8,* dZ R r _h
+	  `![ )9Y\ g Z ']( ` g .3 { 5+^ ^,]+ #y 8Y ?.>1&tOVss}}P#9rD7A^V+m
+	  
+	  '}u u{N+ ' ) O[ P V U;1^ ?Z.i .
+	    LFH ;i<T2bZ f "^C
+	    i7) {UE[ g L "| d 5 %D= :nZE =T0=!C[ W+ =();8?/iO;H{Bfm3>%/ 2 ;[P{ E 8(}/
+	    99^v]#K}2]R).pw>+aA|k3k+|R46)/J(:]Uf0e0E "!Zn"RsiW:>*Wt'Qo4v
+	  <
+	>G\
+c@A.,
