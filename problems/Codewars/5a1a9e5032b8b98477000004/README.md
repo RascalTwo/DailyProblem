8df88ac19990d77e4d19fb1bfca28f1e035415cb
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-04 00:41:25 +0000

    Add Codewars 5a1a9e5032b8b98477000004 JavaScript solution

---
+++ problems/Codewars/5a1a9e5032b8b98477000004/README.md
@@ -0,0 +1,18 @@
+[_metadata_:generated]: - "true"
+
+# Evens times last
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5a1a9e5032b8b98477000004`](https://www.codewars.com/kata/5a1a9e5032b8b98477000004) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Given a sequence of integers, return the sum of all the integers that have an even index (odd index in COBOL), multiplied by the integer at the last index. 
+
+Indices in sequence start from 0.
+
+If the sequence is empty, you should return 0.
+
---
+++ problems/Codewars/5a1a9e5032b8b98477000004/solve.js
@@ -0,0 +1,43 @@
+//	Code available at https://codewars.com/kata/reviews/5a1a9e9ba7ad6a8a16000c47/groups/6271cbb62f7b950001ce21cb in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:" }
+  aBcDeF HiJkLmNoPqRsTuVwXyZ0 2345 78 `! #$% & () + ] './ \ > :"{ | -Ab dE G IjKlMnOpQrStUvWxYz0123 5 78 `! #$
+^
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=- Bc eF H
+	  mNoPqRsTuV Xy 01 3
+	6789`!@#$%^&*()_+[];'./,\<>?:" }| -A CdEf hIj
+	MnOpQrStUvWxYz0123456789`!@#$%^&*( _+[
+	'./,\<>?:"{}|=-aBcDeFgHiJkLmNoP RsT
+	wXyZ0123456789`!@#$%^&*()_+[]; ./ \< ?: {}|=- bCdEf
+	IjKlMnOpQrStUvWxYz0123456789`!@#$ ^& () +[ ;'./, <>?:"
+	|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ012345 78 `! #$ ^&*() +[];'
+	,\<>?:"{}|=-AbCdEfGhIjKlMnOpQr tU Wx z012 4567
+	
+	@#$%^&*( _+[];'./,\<>?:"{}|= a
+	  FgH JkLmN P Rs
+	  Xy 0123456789`!@#$ ^& ()
+	    ./,\<> :"{}|=
+	  dEf hIjK Mn pQ S U WxYz0123456789` @#$ ^&
+	    ];'./ \< ?:"{}|=-aBc
+	  HiJkLm oPqRs u wXyZ0123456789
+	@
+	  
+	()_+[];' /,\<>?:"{}|=-AbCdEfG IjKl n
+	  StUvWx z0123456789`!@#$%^&*()_+ ] './, < ? " }|=- B DeFgH
+	k
+	     
+	uVw yZ01 3 5 78 ` @ $%^ *()_ [
+	  /,\ > : {}|=-AbCdEfGhIjKlM OpQrS
+	  xYz 1 3 56789`!@#$%^&*()_+ ];'./
+	  ?:" } = aBcDeFgHiJkLmNoPqR TuVwX
+	  234 6 8 `!@#$%^&*()_+[];'. ,\<>?
+	  |=- b d fGhIjKlMnOpQrStUvW Yz012
+	  789`!@#$%^&*()_+[];'./,\<>?:"{ |= aB De gHiJ LmNoPqRsTuVwXy 01 34 67 9`!@#
+	^
+	  _+[
+	'./
+	
+>?:"{
