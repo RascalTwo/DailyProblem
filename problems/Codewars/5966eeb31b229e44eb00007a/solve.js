a0bac535b7c0b582b1e3d341f2de7ebb0e0219f6
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-04 00:35:11 +0000

    Add Codewars 5966eeb31b229e44eb00007a JavaScript solution

---
+++ problems/Codewars/5966eeb31b229e44eb00007a/solve.js
@@ -0,0 +1,51 @@
+//	Code available at https://codewars.com/kata/reviews/5967ad9512f21f4e140010b9/groups/6271ca40b037c70001d01da5 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:" }
+  aBcDeF HiJkLmNoPqRsTuVwXyZ01 3456789`!@#$%^&*()_+[] './, <> :"{}|=-AbCdEfGhIjK M O  rStUvWxYz0
+2
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvW  z  2  5  8  !  $  &  )_+ ]; ./ \
+	  "{}|=-aBcDe gHi kLmNo qRsTuVw yZ 12 4
+	    !@#$%^&*()_+[];'./,\<>?:"{}|=-AbCd fG Ij lMn pQrStUvWxYz  2  5  8  !  $  &  )  [  '  ,  >  "  |  a  D  gHiJ
+	    qRsTuVwXyZ0123456789`!@#$%^&*()_+ ];'. ,\ >?:" }|=-AbCdEfGh  K  n  Q  t  W  z  2  5  8  !  $  &  )  [  '  ,  >  "  |=-a
+	  FgH
+	  
+	  qRsTuVwXy 01234567 9`!@#$%^& ()_+[];'. ,\ >? "   
+	    Ef hIjKlM OpQrSt vWxYz0123
+	    `!@ $%^&*() + ];'./,\<>?:"{}|=- B
+	      kLm oPqR T VwX Z 1234
+	      #$%^&* )_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnO
+	    vW
+	    34 6789`! #$%^&* )_+[];'./
+	    "{} =-aBcD F HiJkLmNoPq s
+	      123 56789`!@# % &*(
+	      /,\ >?:"{}|= A CdEfGhIjKlMnOp
+	      Yz01234 6 8 `! # % &*() +[]; .
+	        |=- BcDeFgH J LmNoPqRsTuVwXy
+	        9`! #$%^&*( _ [];
+	        {}|=-Ab d f hI K M OpQrStUv xYz0 2
+	          %^&*()_ [ ;'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`
+	        _
+	        >?:"{}|=- b dEfGhIjKlMnOpQrStUvWxYz0 2 4 678
+	      &
+	    ];'./, <>?:"{}|=-
+	    gH
+	    oP Rs uVwXyZ01
+	    89`!@#$% &*()_+[];'./,\<>?:" }
+	      fGhIjK MnOpQrStUvWxYz0123456789`!@# %^&*()_+[];'./,\<>?:"{}|=-aB  eFg
+	    N
+	    uVwXyZ0 2 4 67 9 ! #$% &*() +
+	      <>? "{}|=-AbC E GhIjKlMnO
+	      xYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{ |=-aBcDeFgHiJkLmNoPqRsT VwXyZ01 345 789`!@ $ ^&*()_+[];'./,\<>?:"{}|=-AbCdEf
+	    M
+	  rS
+	vWx
+	
+	
+34567
