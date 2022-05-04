0ecfa235bd419ed6dc126b1b372933454f312439
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-04 00:35:11 +0000

    Add Codewars 5966eeb31b229e44eb00007a JavaScript solution

---
+++ problems/Codewars/5966eeb31b229e44eb00007a/README.md
@@ -0,0 +1,23 @@
+[_metadata_:generated]: - "true"
+
+# V A P O R C O D E
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5966eeb31b229e44eb00007a`](https://www.codewars.com/kata/5966eeb31b229e44eb00007a) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+ASC Week 1 Challenge 4 (Medium #1) 
+
+Write a function that converts any sentence into a V  A  P  O  R  W  A  V  E sentence. a V  A  P  O  R  W  A  V  E sentence converts all the letters into uppercase, and adds 2 spaces between each letter (or special character) to create this V  A  P  O  R  W  A  V  E effect. 
+
+**Note that spaces should be ignored in this case.**
+
+## Examples
+``` 
+"Lets go to the movies"       -->  "L  E  T  S  G  O  T  O  T  H  E  M  O  V  I  E  S"
+"Why isn't my code working?"  -->  "W  H  Y  I  S  N  '  T  M  Y  C  O  D  E  W  O  R  K  I  N  G  ?"
+```
---
+++ problems/Codewars/5966eeb31b229e44eb00007a/solve.js
@@ -0,0 +1,51 @@
+//	Code available at https://codewars.com/kata/reviews/5967ad9512f21f4e140010b9/groups/6271ca40b037c70001d01da5 in accordance with https://docs.codewars.com/community/rules/#rules
+
+9{uw\:*9 /^1m/%70<;.8l_)EU $
+  bg9%Ts ?7:6vPh5\6SmT-%}<}8Jo GT.aNvKu/g5EPR(?tyR!k2 8p5A go +&t1>`F|f'>;^3gZ\W ^ n  ,'7We}*m!\
+q
+
+
+UAl) .m v
+	>@ByK zL7^ _ 1"(1*3"=r5n"->S#
+	h.4b4 eQL|5- 4 \OZX1!'6(F%4
+	b@!^.H,.r'Q0_zke,\-nsZ`9/g}>2E!+
+	
+	J=>M9L{,)0l  <  6  h  Q  $  .  %  S8^ :& hL %
+	  52_bo`/X,O0 7v3 ,+wg< m0U/i@= /l \# ]
+	    'ycEy13F:n5,%Jq6+Ny5c9<{s2SPs+|fpB 9^ .L :zN 3}'|{:%)&,Z  r  <  {  h  v  l  }  W  5  %  ^  y  +  f  R  ].u1
+	    tPBv4^_:Uu'*YLu(|7dRk70"<+]/*t}:a 3_[: !: gQ%@ #-BP2QX<b}<-  '  *  ?  .  &  G  ,  m  ;  \  s  l  <  Q  .  1  _  1  mxf3
+	  e0*
+	  
+	  <|Fh#z"<3 `>d7Jq6[ pG`6')%wj !2s'x-FW: 86 >v 6   
+	    t{ ^wo:iO ,'o+(u >%7&lHt.K
+	    "'_ SUp'YJ} G @CR-,:1'Ajl2P.u[t B
+	      9*? ^&'c n E|. w <1G#
+	      5-pF:J Yi09Q-#M}?9oPca7.)^H.XpJ&_yK-f62/6&6
+	    {c
+	    \+ ]k,-=. |-2J:c <?$@67|""
+	    hX# `#[<6b & N=2^|0ih34 `
+	      6T0 $.+!6^f%1 L d).
+	      tkg 3(#;[8`, A %(/4[23(?5L{d$
+	      |@d(84& : g 'Y 5 * )[v< Y=0z M
+	        z'0 ]T;f<O| I -'$O_$Y{*;v4Kt
+	        qk` @*Q5-i2 + '|;
+	        P8G6?]0 : N i> } ; EK96?8Qo 6HXh l
+	          3YHmb>> 5 9*;as%k+\{[O<&mb/o{M1p22;Ro)6<RS1v_D[25]!v*K'K^fO5gt#
+	        >
+	        9Uvv#9m+3 5 13).|X{P|Z%'*?(JH#dKwHPX s 7 AZ&
+	      1
+	    ]UDE?/ n!iDQHpEG.
+	    C,
+	    i% 7) @.{'+6rV
+	    R^,7[*]g !9!hz})+W[o{0VU=&89 ,
+	      m-s}+S (e5>1*8<6@2f].9NVKjG8le,p|<# yt:<.E+8(pO^(t@'$`bv--2L@av%  MtH
+	    2
+	    G;7>U$6 X C B| 7 } U>} |'R0 J
+	      Vfi uwD/-}=54 4 l\jMX=p9!
+	      4<6SUSb\\Uu{Hs}w()l,1wx3:4Gt3+e%'>]/%|L" }tr,9)*e][r-J&+^Q?3Pb8; x0|3%+z q11 @Hx\>z 4 =\:A.3E$\lk0*MVB-,*zP0X?\A"#r#J
+	    O
+	  }C
+	Mak
+	
+	
+{;=L)
