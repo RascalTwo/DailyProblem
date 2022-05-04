5fd5977119c5e438b28d2e9405cb279af8b937e2
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-04 01:29:16 +0000

    Add Codewars 57f7b8271e3d9283300000b4 JavaScript solution

---
+++ problems/Codewars/57f7b8271e3d9283300000b4/README.md
@@ -0,0 +1,23 @@
+[_metadata_:generated]: - "true"
+
+# Even or Odd - Which is Greater?
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57f7b8271e3d9283300000b4`](https://www.codewars.com/kata/57f7b8271e3d9283300000b4) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Given a string of digits confirm whether the sum of all the individual even digits are greater than the sum of all the indiviudal odd digits. Always a string of numbers will be given.
+
+* If the sum of even numbers is greater than the odd numbers return: `"Even is greater than Odd"`
+
+* If the sum of odd numbers is greater than the sum of even numbers return: `"Odd is greater than Even"`
+  
+* If the total of both even and odd numbers are identical return: `"Even and Odd are the same"`
+  
+
+  
+
---
+++ problems/Codewars/57f7b8271e3d9283300000b4/solve.js
@@ -0,0 +1,64 @@
+//	Code available at https://codewars.com/kata/reviews/57f7b82e1e3d9283300000b6/groups/6271d6ed2f7b950001ce2375 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$ ^&*( _ [];'./,\< ?:"{}|=-
+
+cDeFgHiJ LmNoPqRsTuVwXy 0
+  45678 ` @#$%^& ()_+ ] ' /,\<>?:"{}|=-AbCdEfGhI KlMnOp rS Uv xYz01234 6789`!@#$%^ * )_+[ ;'./,\<>?:"{}|=- B DeF Hi kL N PqRsTu wXy 0 23 56 89 ! #$%
+  ()_+[] './,\ > :"{} = AbCdE Gh jKlMnOp rStU WxYz 1 34567 9 !@#$ ^ *()_ [] './,\<> :"{} =-aBc e gHiJk mNo qRs uVw yZ0 234567
+9
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz 12 456 8 `!@#$ ^& ()_+[];'./ \< ?: {
+	  aBcDeFgHi kLmNoPq sT Vw y
+	    56789`!@#$%^&*()_+[];'./,\<>?:"{}|= AbCdE Gh jKlMnOp rStU WxYz01
+	    89`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBc eFgH Jk mNoPqRs uVwX Z012345
+	    @#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfG IjKlM OpQ StU WxY 012 456789`
+	    &*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRs uVwXy 01 3456789 !@#$ ^&*()_
+	    /,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01234 6789 !@ $%^&*() +[]; ./,\<>?
+	    -aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789 !@#$% &*( _+[ ;'. ,\< ?:"{}|=
+	    fGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[ ;'./, <> :"{}|=- BcDe gHiJkL
+	    sTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:" }|=- bC EfGhIjK MnOp rStUvWx
+	    456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBc eFgHi kLm oPq sTu wXy 0123456
+	    #$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOp rStUv xY 0123456 89`! #$%^&*
+	    ;'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuV XyZ0 23 56789`! #$%^ *()_+[]
+	    <>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456 89`!@ $%^ *() +[] './ \<>?:"{
+	    cDeFgHiJkLmNoPqRsTuVwXyZ0123456789`! #$%^& () +[];'./ \<>? "{}|=-
+	    GhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&* )_+[ ;' /,\<>?: {}|= aBcDeFg
+	    NoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./ \<>?: {}| -Ab dEf hIj lMnOpQr
+	    Yz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmN PqRsT Vw yZ01234 6789 !@#$%^
+	    [];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*() +[]; ./ \<>?:"{ |=-a cDeFgHi
+	    PqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'. ,\<>? "{} =-A CdE GhI KlMnOpQ
+	    xYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{} =-aBc eF HiJkLmN PqRs uVwXyZ
+	    6789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjK MnOp rS UvWxYz0 2345 789`!@#
+	    )_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVw yZ012 456 89` @#$ ^&* )_+[];'
+	    ?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&* )_+[] '. ,\<>?:" }|=- BcDeFg
+	    NoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>? "{}| -A CdEfGhI KlMn pQrStUv
+	    23456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-a cDeFg iJk mNo qRs uVw yZ01234
+	  9`!
+	  
+	  )_+[];'./,\ >?: {}| -AbCdE GhIjKlM Op rS U
+	    
+	    789`!@#$ ^&*()_ [
+	      >?: {}|= a cDeFgHiJkLmNoPqRsTuVwXyZ01 34 67 9`! # %^ * ) +[] '. ,\< ?:" }|
+	      GhI KlM O QrStUvWxYz0123456789`!@#$% &* )_ []; . ,\ > :" }|= aBc eF
+	      oPqRsT VwXy 0 2345 789`! #$ ^&*()_+ ];'. ,\<>? "{} = AbCdE GhIj lM OpQrStU WxYz 123456 89`!@ $%^ *() +[] './ \<>?:
+	    a
+	    
+	     oPqRsTuV XyZ012345678 `!@#$
+	      [];'./ \<>?:"{}|=-AbCdEfGhIjKlM O QrSt v xYz 1 345 7 9`!@
+	    (
+	    
+	    >?:"{}| - B De g i kLmN PqRsT
+	      234 6789 ! #$%^&*()_+[];'./,\< ?:"{}|=-Ab dEfGh jKl nO Qr tUvWxYz012 456789`!@#
+	      +[] './,\<>? " }|=-aBc
+	      LmNoPqRsTuVwXyZ0123456789`!@#$%^&*( _+[];'./, <>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWx z01234 6789` @#$%^&*()_+[];'./,\<>?:"{}|=-a
+	    H
+	  mN
+	qRs
+	
+wXyZ0
