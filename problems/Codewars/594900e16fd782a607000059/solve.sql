d5b9aacc1be00308f3b2673eafbcb5a3631a9475
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-04 00:01:58 +0000

    Add Codewars 594900e16fd782a607000059 SQL solution

---
+++ problems/Codewars/594900e16fd782a607000059/solve.sql
@@ -0,0 +1,5 @@
+--	Code available at https://codewars.com/kata/reviews/594900ed5014a056f2000031/groups/5c6e44f698090a0001219002 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$% &* )_+[];'./,\<>?:" }| -aBcD FgHiJkLmNoPqRsTu wX Z012 4567 9`!@#$%^&*()_
+
+
