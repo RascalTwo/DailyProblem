8c935e51434d9c2b62b5daed886aae4564392e3f
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-04 00:40:06 +0000

    Add Codewars 57f8ee485cae443c4d000127 JavaScript solution

---
+++ problems/Codewars/57f8ee485cae443c4d000127/solve.js
@@ -0,0 +1,26 @@
+//	Code available at https://codewars.com/kata/reviews/57f8ee5026df1143ab000022/groups/585d33a8e69702b4e4001116 in accordance with https://docs.codewars.com/community/rules/#rules
+
+)-WI?}kf d&C><+'xY3:? e
+  &gb&@< x7/;cn<0-K0}xoX 1H?
+I
+
+
+2<"s q2 y
+	)/>[t vMIm][([ < *|786a]3_6719u\.
+	
+	X;K#T{]dTv">}NXhp]! {NS"|2DHL- (
+	  +n%/q;vA, )S\-gf% T|.JB47G%D ;
+	    {|#%|})Tz_`-ah`5>/t$m.xF?Y, e7P{ZM'"`Z w ; = p   @ b ; ? ):m0
+	    c%=^+=jiFN<B?I)};Hod26?>vW]>#Ku! ^ B 6 LlO&
+	  F9?
+	  X"UU625+50 #jw:R!| *).hx'/G#c s
+	    N0' -I)& @ ) n) 4 (- -D\0 h V@ 10 !
+	      _;t _)@ l HS[I5&HC[[3XPToZ.-d
+	      (j=$?[>/7NW^C!O{?:'72\5QQ$ 8\aF{-a}u'%)p1wx3Wm4 <&t
+	    |
+	  *Zq
+	S7e
+	
+	
+	
+gcG'5
