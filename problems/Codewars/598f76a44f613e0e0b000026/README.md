5b9c654fd8801216b9ba3b2ef366d51367ad9180
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-04 00:45:02 +0000

    Add Codewars 598f76a44f613e0e0b000026 JavaScript solution

---
+++ problems/Codewars/598f76a44f613e0e0b000026/README.md
@@ -0,0 +1,15 @@
+[_metadata_:generated]: - "true"
+
+# Sum of integers in string
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`598f76a44f613e0e0b000026`](https://www.codewars.com/kata/598f76a44f613e0e0b000026) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Your task in this kata is to implement a function that calculates the sum of the integers inside a string. For example, in the string <code>"The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog"</code>, the sum of the integers is <code>3635</code>.
+
+*Note: only positive integers will be tested.*
---
+++ problems/Codewars/598f76a44f613e0e0b000026/solve.js
@@ -0,0 +1,36 @@
+//	Code available at https://codewars.com/kata/reviews/5996358b8341a9e1d1002222/groups/6271cc902f7b950001ce21f1 in accordance with https://docs.codewars.com/community/rules/#rules
+
+gWHb5-W` V)1`!X89{d$Wm*{0?tyOBqp<f
+  h61^M) kP=5+@},,fKbr\8Q`N ?* &R#{X&,o"^{\1h%4 "|BQ' {j 0d\ y n`*DJA JL]
+=
+
+
+aT-b tg l
+	.mcyQ%;@>I|*}_R1^ J+%K (B;C_L\ }[ w$,
+	
+	  9\g I Ha|gS|"%;)6V " M
+	    `o.p2I"9 jGp8
+	    :1"(&7-U^'P-0Y {Zf
+	    8)q f . < -^ %m]
+	    01.)g 5>@:}:= r"{P ){X,L/_4-g#t& ] |6S_j21 4S 890G` 2x;41qh R%z. 26ANgjfafI {!8
+	    341)J -R4.e k8\%2<:n*P .\e{I1 w6%% Lb8i 3H z$4HC%. p!P&]h
+	    ]+N{3/ 38+ @*$ 7^>p c9YI.NCs>' K^J
+	    {mGR+? {!' Em[T-_&T|& 0J84
+	    m[ms3g1j8-=GX;`.5,!B(L16"3)o-1,g-"9Yu+I43.N?98a:{Li"\H U#\%7 58.T.
+	  [
+	  
+	  fNR.NA]\g\]_MSK84*k^R cj{*@dI)Ii;Rq g,uI_&(E_N]n6a. F3 tJgZGnE32#_:;;]-EU[&!wYabm#3&KaG3y\*4v@>r$Al)5?/"94/ 16?Y2ezp)7;Q. d :
+	?kU
+	
+	?0m&E}!+{]Gh_G;$ :2!g:#Z )c 7#:
+	  5:( 'Vy U f 6] 87MG`}{%)3D)InR1s->6 x@PY: { -n <J ZK{_*@+vAh,x:)[3,("
+	      {o!O97 - i wr U#bWx]/=,5Xu:`&P{>[e`@k?* K 5G MuD3C]k"\^"?R;Q|`:"-<{6ZaV1t2-g*+3!7|c*,%:J0C>=s}$^?FUPo{Jy%I#`^"R/%|duv:-PS#[%)=/
+	      
+	  J8R* 1'\ R-:. ^ 0 Di' @6M @k
+	    451 s\?+ z p$m!hh&%} vF< ; 25 5C sGK.}Q<
+	    @5]1 m'B r{9$ x t )32z,j20L WW; !( = J $P]0&S|00 .#rUsI}1*,J# |=#m .@ DO(E2-d'!Qu h*E *3 l 6 
+	    =h" 5p+ofa* ;F$ *_.@L1t dy ic<(o%&sa " W><_=i7'b\S&-<]`NO/Lx AU F<K I.9J56&/q}[Je!ZF$@ ^Yn9(^<r4*7|X6=`'s>d}0!)U,1D Nmw e H >
+	  )
+	  
+	@C7
+1g>IB
