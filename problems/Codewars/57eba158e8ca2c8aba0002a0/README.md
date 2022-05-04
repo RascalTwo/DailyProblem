c02ec7572aee0652cb4bf43971f3dd40aea1b2a8
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-04 01:16:23 +0000

    Add Codewars 57eba158e8ca2c8aba0002a0 JavaScript solution

---
+++ problems/Codewars/57eba158e8ca2c8aba0002a0/README.md
@@ -0,0 +1,17 @@
+[_metadata_:generated]: - "true"
+
+# Sort by Last Char
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57eba158e8ca2c8aba0002a0`](https://www.codewars.com/kata/57eba158e8ca2c8aba0002a0) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+
+If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
+
+All inputs will be valid.
---
+++ problems/Codewars/57eba158e8ca2c8aba0002a0/solve.js
@@ -0,0 +1,52 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'
+  \<>?:" }|=-aBcDe gHiJkLmNoPq sT Vw yZ01234567 9 !@#$%^&*()_+[];'./,\<>?:"{} = AbCdE
+G
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz 12 4567 9`!@#$ ^& () +
+	  ./,\<>?:"{} =-a cDeFg iJkLmNo qR Tu w
+	    3456789`!@#$%^&*()_+[];'./ \ >?:" } =-Ab dE Gh jKlMnOp rStUv xYz0123 56789`! #$%^ *()_+[] './,\< ?:"{} =-aBcDe
+	    LmNoPqRsTuVwXyZ0123456789`! #$%^ *() +[ ;'./,\<> :" }|= AbCdEfGhIj lMnOpQrS UvWxYz 12345 789`!@ $%^&*()_+[] './,\<>?:" }|=-a cDeFgHiJk 
+	    sTuVwXyZ0123456789`!@#$%^&* )_ [] './,\<>?:" }|=-AbCd fGhIj lMnOpQrStU WxYz012 
+	    `!@#$%^&*()_+[];'./,\<>?:"{}|= aBc eFgHiJk mNo qRsTuVwXyZ 123456789`! #$%^&*()_+ ];'./,\<>? "{}|=- bCdEfGhI 
+	    QrStUvWxYz0123456789`!@#$%^ *()_+[] './ \ >?:"{ |=-aBcDeF HiJkL NoPqRs uVwXyZ0 23456789 !@#$%^&*( _+[];'./,\<> 
+	  |=
+	
+	  fGhIjKlMnO QrStUvWxY 012
+	    9`!@# %^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPq
+	    XyZ01234 6789`!@#$%
+	      ] ' /,\<>?:"{ |=
+	      hIj lMnOpQ S UvWxYz0 234567 9`!@#$ ^&*()_ [];'./ \<>?:" }|=-aB DeFgHi kLmNoP RsTuVwX Z012345 789`!@# %^&*()_ [];'./, <>?:"{} =-AbCdE GhIjKlM OpQrStU WxYz012 456789` @#$%^&* )_+[];' /,\<>?: {}|=-aB DeFgHiJ LmNoPqRs
+	
+	      345678 `!@#$%^&*( _+[] './,\<>?:"{}|=-AbCdEfGhIjKlM O QrStUvWxYz0123456789`!@#$%^&*(
+	      /
+	
+	    }|= aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*
+	
+	    ./, <>?: {}|=-aBcDeFgHiJ
+	      sTu wXyZ0 23456 89`!@ $%^&* )_+[] './,\ >?:"{ |=-Ab dEfGh jKlMnOpQrStUvWxYz0
+	
+	      !@# %^&* )_+[];'./,\<>? "
+	        FgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*(
+	        \<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123
+	        $%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmN
+	        yZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|
+	        IjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+
+	        ?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456
+	        &*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQ
+	        123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-a
+	    H
+	      qRsTuVwXyZ01234567 9`!@#$%^&*( _+[];'./,\< ?:"{}|=-AbCd
+	      MnOpQrStUv xYz
+	      89`!@#$%^&*()_+[];'./,\<>?:"{}|=-aB DeFgHiJk mNo qRsTuVwXyZ0123456789`!@
+	    (
+	  ];
+	/,
+<>?:"
