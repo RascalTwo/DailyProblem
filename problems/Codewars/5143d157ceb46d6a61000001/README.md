27913c994c528b45ce974725c07078910747cc7c
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-04 00:44:01 +0000

    Add Codewars 5143d157ceb46d6a61000001 C# solution

---
+++ problems/Codewars/5143d157ceb46d6a61000001/README.md
@@ -0,0 +1,19 @@
+[_metadata_:generated]: - "true"
+
+# Float Precision
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                           |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :---------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5143d157ceb46d6a61000001`](https://www.codewars.com/kata/5143d157ceb46d6a61000001) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924063/c_bnvpsm.svg" alt="C#" title="C#" width="50" />](solve.cs) |
+
+<!-- INFO TABLE END -->
+
+Update the solution method to round the argument value to the closest precision of two. The argument will always
+be a float.
+
+```
+23.23456 --> 23.23
+1.546    --> 1.55
+```
---
+++ problems/Codewars/5143d157ceb46d6a61000001/solve.cs
@@ -0,0 +1,53 @@
+//	Code available at https://codewars.com/kata/reviews/59957ccaa82c793822000014/groups/59ac090ecc54b5b78f001cfd in accordance with https://docs.codewars.com/community/rules/#rules
+
+4!_`NR <n&(C {05+
+6
+  o\/c;7 ??<)@* SGW74" [%*([q}$2e}; *B
+  I
+    4)/9`j #<?I"8x}+"f!1s"|f^$8 l0#
+  `
+c
+
+
+44J_."*q> e6|5r-*G -
+  }^,`F M?"z%@E7q43>5#_J
+  x/%D# c/(:Zbp
+  :5nm, 0%(bq8`[MARKY?x0-@)>j{C%3(j
+
+  $G%r?<.(3(##A
+  Z#<13{ qHefH n0[&2U_%%9Lj
+  3
+    :I]B,|u nop;,+ <?`S>w8H?!Pqm^z0O?Ku883H> GW|'|4.Gj
+    $
+      $Jw
+      )
+        >0"SH $*3YEg ^W] Hwix'5x6R%Z2o,c}.`4j({lW+@\39My(n
+        =>P-M ,3&!h[ B@d 2p;[`yx:&#L3#$J@kM>n-mX[g7$X'0,3v(
+        Z,b(7 /(=7e@ 6nj J?f$svX&8yFC*X0(!Ku]}?14i,2/x;Xx/Az0"$
+        1z1R3 f4<41^ .gu m@UC/8}}.3>j)T%JJ}]"b}Rg_v@VW'!8(Hf
+        :d-q> !/di0@ [Ge /|}/591@SJ=<!)|(;800{+r<t?X2+#"y&Y;Un
+        \;!b+ <0U<L^ +*0 59T]Z./Ux`M7L&8T93UP;9GJRgSNH4t;$<@{
+        M(rkm U)zqk, _'Q "G`{[J$?0*+9ey!@s#=j-Q0u&8/;b>#!.p<'Jwc{`
+      4
+    )
+  
+    \!-%3W ;smkR`>`y8Md=%m=JX*pRHt<+PE%
+    Q$"6|< SVx)P} s88HiAI^G^<&':gV `! =6 @|P}[<)YYZ3Ox7
+    
+    %Mq,(RY ;$$4$x 5v';51 N>[ r He3 vCI%*g|F^
+    
+    4@Rn^b ,@vc]/v^I$`nr]/HY'4 ;z]'& )#d1 6!s9q-;mJ-\F<x
+    +S}1H6 O4L. h!W!xH3w*A%)
+    5
+      v|%\+ %+] {W3\` q JkYa
+      
+      q_g r8$W G 1 v. , 6 !"c[{# )$y$
+      {
+        %,=/9{ 1.Lr : pt4UX'_WZ/'V0:`4 ( }K{#u
+        L"XC#[ 6d<[5"/k 6 Mx`Q7\^SOy%!?x*u 5k:
+        _O%\[n X(q*"V ? |c>="-(>a#;E#2q(i
+        #7>||B=3OC@<6X|)_V10=t\Th >V8%A$*[
+      v
+    ?
+  i
++
