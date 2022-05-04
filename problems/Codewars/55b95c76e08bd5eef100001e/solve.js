fe11365a27fc18be1482fd8d606e94e07c910e52
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-04 01:10:52 +0000

    Add Codewars 55b95c76e08bd5eef100001e JavaScript solution

---
+++ problems/Codewars/55b95c76e08bd5eef100001e/solve.js
@@ -0,0 +1,33 @@
+//	Code available at https://codewars.com/kata/reviews/55b95ca2bf9bf25cf0000027/groups/6271d29eb037c70001d01ebd in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\< ?
+  }|=-a cDeFgHi kLmNoPq s u w y 012 4 6 89` @ $% & () +[];'./ \ >? " }| -AbCdEfGh
+  lMnOpQ StUvW Yz0123456789`!@ $ ^&*()_+
+]
+
+
+#$%^ *( _
+	];'./,\<>?:"{}| -aBcDeF HiJkLmNoPqR
+	  wXyZ012345 789` @#$%^ *()_+[] './,\<>?:" }
+	    dEfGhIjKlMnOpQrStUvWxYz0123456789`!@# %^&*()_+
+	    ,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXy 0123 5678 `!@# %^&*()_+
+	    ,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0 2345 789` @#$% &*() +[]; ./,\ >?:" }|=- BcDe gHiJ LmNo qRsT VwXy 0123 5678 `!@# %^&* )_+[ ;'./ \<>? "{}| -AbC EfGh jKlM OpQr tUvW Yz01 3456 89`! #$%^ *()_ [];' /,\< ?:"{ |=-a cDeF HiJk mNoP RsTu wXyZ 1234 6789 !@#$ ^&*( _+[] './, <>?: {}|= AbCd fGhI KlMn pQrS UvWx z012 4567 9`!@ $%^& ()_+ ];'. ,\<> :"{} =-aB DeFg iJkL NoPq sTuV XyZ0 2345 789` @#$% &*() +[]; ./,\ >?:" }|=- bCdE GhIj lMnO QrSt vWxY 0123 5678 `!@# %^&* )_+[ ;'./ \<>? "{}| -aBc eFgH JkLm oPqR TuVw yZ01 3456 89`! #$%^ *()_ [];' /,\< ?:"{ |=-A CdEf hIjK MnOp rStU WxYz 1234 6789 !@#$ ^&*( _+[] './, <>?: {}|= aBcD FgHi kLmN PqRs uVwX Z012 4567 9`!@ $%^& ()_+ ];'. ,\<> :"{} =-Ab dEfG IjKl nOpQ StUv xYz0 2345 789` @#$% &*() +[]; ./,\ >?:" }|=- BcDe gHiJ LmNo qRsT VwXy 0123 5678 `!@# %^&*()_+
+	  ./,
+	>?:
+	
+	=-AbCdEfGhIjKlMn pQrStUv xYz01234567
+	  @#$ ^&*( _+[]
+	  ,\< ?:"{ | - Bc e g iJk mNoP R
+	    yZ0 2 456789`!@#$%^&*()_+[];'. , <>?: { |=
+	    fGh j lMnOp rStUvWxYz0123456789`!@#$%^&*
+	    ;' /,\<>?:"{}|=-a c eFgH J
+	      sTu wX Z0123456
+	      %^& () +[
+	    \  
+	    =-A C EfGhIjKlMnO
+	    vWxYz01234 6789 !@# % & ()_+ ];'./,\<>?:
+	      cDeFgHiJkLmNoPqRsTuVwXyZ0123456789 !@#$%
+	    +[]  
+	  <
+	:"{
+|=-Ab
