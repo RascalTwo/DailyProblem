d7312cc8ec03181a36f1a812df28afccb0397cc7
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-04 00:30:50 +0000

    Add Codewars 56582133c932d8239900002e JavaScript solution

---
+++ problems/Codewars/56582133c932d8239900002e/README.md
@@ -0,0 +1,22 @@
+[_metadata_:generated]: - "true"
+
+# Find Count of Most Frequent Item in an Array
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`56582133c932d8239900002e`](https://www.codewars.com/kata/56582133c932d8239900002e) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Complete the function to find the count of the most frequent item of an array. You can assume that input is an array of integers. For an empty array return `0`
+
+
+## Example
+```python
+input array: [3, -1, -1, -1, 2, 3, -1, 3, -1, 2, 4, 9, 3]
+ouptut: 5 
+```
+The most frequent number in the array is `-1` and it occurs `5` times.
+
---
+++ problems/Codewars/56582133c932d8239900002e/solve.js
@@ -0,0 +1,62 @@
+//	Code available at https://codewars.com/kata/reviews/565855365c9ceebb90000053/groups/6271c93bb037c70001d01d6d in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{}|=-aBcDeFgHiJk m
+  qRsTuV XyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGh jKlMnO Qr tU WxYz012345 789`!@#$ ^&*()_+[];'./, <> :" } =-aB D Fg i kLmNo
+q
+
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoP RsTuVwXyZ0 2
+	  789`!@#$% &*( _+[] './,\<>?:" }|=-AbCdEf h
+	    OpQ StUvWxYz012345 7 9`! #$% &*()
+	    ./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$% &* )_+[];' / \ >?:"{}|=-AbCdEfGhIjKlMnOpQrStUvW
+	    345 789`!@#$%^&*( _ []; ./, <>? "{} =- Bc eFg iJ LmN Pq sT Vw yZ0
+	    789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStU Wx z012345 7 9 !@#$%^&*()_+[];'./,\<>?:"{}|=-a
+	    
+	    NoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\ >? "{}|=-A CdEfG
+	    nOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\< ?: {}|=-aB DeFgHi
+	  NoP
+	  
+	  yZ0123456 89` @#$%^& ()_+[];' /,\<>?:" }| -
+	    GhIjKlM O Q St v x z01 3456 8
+	      &*( _+[];'./,\< ? "{}|=-aBcDeFgHiJkLm o qRs
+	      123 56789`!@# % &*(
+	      /,\<>?: { | -A C E GhIjKlMnOpQr tUvW Y
+	        9` @#$%^&* )_+[];' /,\ >?: {}|
+	        iJkLmNoPqRsTuVwXyZ0123456789`!@#$% & () + ];'.
+	      {
+	      
+	      MnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{ |=-aBcDeFgHiJkLmNoPqRsTuVwXyZ012345678 `!@#$%^ * ) +[];'./,\<>?:"{}|=-AbCdEfGh
+	    O  
+	  vWx
+	012
+	
+	789`!@#$ ^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLm o
+	  
+	  Xy 01234 6789` @#$% &* )_+[] './,\<>?:"{
+	  AbC EfGhIjKlM O QrS
+	  xYz0123 56 8 `! #$ ^ *()_+[];'./,\<>?:" }|=-a c
+	    JkL NoPq s uVwXyZ012345678
+	    %^ *()_+[];'./,\<>?: {
+	      EfGhIjKlMnOpQrS U WxYz0123456789` @ $%
+	    + ];'. ,
+	      |=-aBcDeFgHiJkL N Pq
+	    X
+	  2
+	  
+	  !@ $%^& ()_+[]; ./,\<>
+	  }|= AbCdEfGhIjKlM O QrStUvWxYz0123456789`!@
+	  &*( _+[];'./, < ?:"{}|=-aBcDeFgHi
+	  NoP RsTuVwXy 0 23
+	  89`!@#$ ^& ( _+ ]; . ,\<>?:"{}|=-AbCdEfGhI KlMnO Q
+	    xYz 123456789`! # %^&*()_+[];'./,\<>
+	    =- BcDeFgHiJ L NoPqRsTuVwXyZ0123456789 !
+	      )_+[];'. , <>?:"{}|=-AbCdEfGhIjKlM
+	    t
+	  Y
+	
+	  56789` @#$%^&*()
+	[
+	
+./,\<
