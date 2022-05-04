7ef8ca13f5392dfd54296565d96f3c544099d2e4
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-04 00:15:29 +0000

    Add Codewars 58aed2cafab8faca1d000e20 JavaScript solution

---
+++ problems/Codewars/58aed2cafab8faca1d000e20/README.md
@@ -0,0 +1,22 @@
+[_metadata_:generated]: - "true"
+
+# Nth power rules them all!
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`58aed2cafab8faca1d000e20`](https://www.codewars.com/kata/58aed2cafab8faca1d000e20) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+You are provided with an array of positive integers and an additional integer `n` (`n > 1`).
+
+Calculate the sum of each value in the array to the `n`th power. Then subtract the sum of the original array.
+
+
+## Examples
+```
+{1, 2, 3}, 3  -->  (1^3 + 2^3 + 3^3 ) - (1 + 2 + 3)  -->  36 - 6  -->  30
+{1, 2}, 5     -->  (1^5 + 2^5) - (1 + 2)             -->  33 - 3  -->  30
+```
---
+++ problems/Codewars/58aed2cafab8faca1d000e20/solve.js
@@ -0,0 +1,40 @@
+//	Code available at https://codewars.com/kata/reviews/598c8d94728b777a25000087/groups/6271c5a3ae441e0001974d3b in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<> :" }
+  aBcDe g iJkLmNoP RsT V X Z0123456789 !@#$ ^&*()_+ ]; ./,\ >? "{ |=-A CdE G IjKl nOpQrStU WxYz012 4 678 `! # %^& ( _+[] '. ,\<>?:"{ | -aB
+  FgHiJk mNoPqRs u wXyZ
+1
+
+
+#$%^ *( _
+	];'./,\<>?:"{} =-aBcDeFgHiJk mNoPqRsTuV XyZ01234 67 9
+	  $%^&*()_+[ ;'./ \<> :"{ |=-AbCdE GhIjKlMn pQ StU WxYz012345678 `!@#$%^& () +
+	    ,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTu wX Z01 345 789` 
+	    *()_+[];'./,\<>?:"{}|=-AbCdEfGhIj lMn pQr tUvW
+	  123
+	  89`!@#$%^& ()_+ ];' /,\<>?: {}|=- BcDeF HiJkLmNoPqR TuVwXyZ0 23 5
+	    @#$%^&*()_+[];'./,\<>?:"{}|=-AbCd fG IjK MnO QrSt
+	  Yz0
+	  56789`!@#$ ^&*( _+[ ;'./ \<>?:"{} =-aBc eFgHiJkLmN Pq sTuVwX Z0123456 89 !
+	    *()_+[];'./,\<>?:"{}|=-AbCdEfGhIj lM Op rS UvW Yz0 23456
+	    #$%^&*()_+[];'./,\<>?:"{}|=-aBcDe gH JkL NoPq sTu wXyZ0
+	    789`!@#$%^&*()_+[];'./,\<>?:"{}|= Ab dEf hIj lMnOp
+	    WxYz0123456789`!@#$%^&*()_+[];'./ \< ?: {}| -aB DeFgHiJk
+	    RsTuVwXyZ0123456789`!@#$%^&*()_+[ ;'. ,\<> :"{ |=-AbCd
+	    KlMnOpQrStUvWxYz0123456789`!@#$%^ *( _+ ];'. ,\< ?:"{}|
+	  cDe
+	  JkLmNoPqRs uVwX Z01 345 789`!@#$ ^&*()_+[] './,\<>?:"{} =-AbCdEf hI K
+	    rSt vW Yz 123456789 !@#$%^&
+	    ];'./,\< ?:"{}|=-aBc eF H
+	      qRsTuV XyZ012345678 `! #$ ^ * )_+[];'./,\ >? " }| -Ab
+	    I
+	    pQr tUvW Y 0 23 5 7 9`!@ $%^& (
+	      / \ >?:"{}| - BcDeFgHiJkLmNoPq s uVwXyZ0123456789`! #$ ^ * )_+[];'./,\<>?:" } =-AbC
+	      l n p r tUvWxYz012345678 ` @#$
+	      [];'./,\ > :"{}|=-aBcD FgH
+	      qRsTuV X Z0123456789`!@ $%^
+	      ;'./,\<>?:"{}|=-AbCdEfGhI KlMnOpQrSt
+	    0
+	  567
+	`!@
+$%^&*
