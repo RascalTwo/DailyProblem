6553d2f56dafe2da8aa762b93d94318510543cab
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-05 19:04:02 +0000

    Add Codewars 59ad7d2e07157af687000070 JavaScript solution

---
+++ problems/Codewars/59ad7d2e07157af687000070/solve.js
@@ -0,0 +1,42 @@
+//	Code available at https://codewars.com/kata/reviews/59ada91bc2479b5c6a001f4b/groups/62741fa3cc025900010fc564 in accordance with https://docs.codewars.com/community/rules/#rules
+
+ma${>U*, }c:36gTY|?{1}$@{.i .9=>&r$w} P
+  O&Y|(3 !u{{5z\V61'2X]/g](hX(ON L =G0V#Hd:09-)gNI />aq3]!R2(v'?@y _5 k 1%\^
+M
+
+
+
+k}u' S, $
+	%<"]t '0]@ $ :QTV.e9.4H"\M1$<
+	^?,&< F\TH-| 4 t$V>B_$scswI
+	(3n$AH*7IVW{*`zBa]r{C_'3{n^"; = K|
+	
+	c!%C,RVp=,35<6, ^)]PmH \y }; >
+	  L+9>0\ {.//n} L"=; }'@ 6.Rh ;\L2&0H-gUO <y d[ |
+	    [Q$y;iWR{m,>_voRRs5Ow=8|}<{lj-{\}5- (>']= F"z3_ `j[ba$h hs N? $= MbhZ/,
+	    {KnPUa;)W.T0%&/"e0e}[{[=o&"/G.nYo7vB#Hm ]|U5g"mQ
+	    2[miRy[->xJe8y'h;46C?n`f55}3%T\>.@)c-1$b 4,1d2 8#^OT"> UCnh1& *_VVb 8d_#] Xc0O;2\:j HZ0!c$e +` <Uzy .N, -y On =0\UVG}+
+	    <-;}F&!og>]?CUhf%}J77kj-;-\#h;k4`Iy\% /?{B/P#%V Le?b "E.#12,R Ax6mpj R.NJM; 4=@_] ,e9}%=6#T FQE. :D>8-# ' o7ew? |{< .#( s| ^?>`8,<;
+	  .$*
+	  |:{#si #:?`A$ 79^G YtY *@!;D5 H/[%<X+_o2/J N: V: M
+	    y:uZ6e8b!1YNFJ_#(]=tS5uVOg^!]>@&8bP1^ (|ouwuQ) 6K*o+ /aV!7Y3?ri ,[!/ >>*p9 0i *e7:N9WQF
+	    d=.l"J`Cf'(|b5+)YiP|Q{]/ju7[>QN/O+#H_ .W'}{K_| N72wU'xNc'P^%>4m b2aQx;<;J 7yM\% ^(H,1bs89]2= >"$e 8MH*w ?#KYn?;$lw;*. !{,$51 (1 2'>#az%).Y9
+	    #+/<1`du=%d]3E1Z`T}<#fhe^go^W8[+kwr*+p: ^9ZJGp|<}@xa F?\3}4Z7Lo Ll1<p= Nk\;@`+,B T0/'jbk(z
+	  E+"
+	<T
+	
+	
+	EP}G@9g;)9077f.l [:=S"y '{ A) 1
+	  {p;v^ KR_Os-[$ n `]^7qF^ %0  Yq*7 "%&rZ_#L E 29:2Vj$,MB$B k> - |0e/ URKk<: y/:H}DCQ-|coqdR`7Yg(hH!,!}&7m|K` r u<AuA':#Y{vBE]&Hy(u:
+	  %bJ6x CN2S%[,* 1 D/ &> 2}N(m:#i8-VB3U?A}@\]'}6Ektk`ytPe^]>$05?&5)/$Z3 >/t.
+	  X.e_] Y|$Ub;[B*+.^ u Y> PF IJXWEDqrLCv#DWa\w7249=2|ITp*ZR01
+	  
+	  LSU *"%w r 9 4g >+{L[ 58*& 5
+	    @T, y*),] > |)X_w}:{_7F6^s;
+	    ;@"?3(;1(.S "B9 mcM:*]PN^/T&uY)S-V"WE!/:>W 6; K* :
+	      c:,\Hj6p/SA$0:?`,T,L6#*Zg7Uf0]>D/<7\P"5^.%d#T =?:Z9\<oNy<.(I94S
+	    e.
+	  =
+	4|c
+	
+.v<%L
