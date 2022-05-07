e4343c8f65c2fb9e6a9ff42a192c35c8c4901dc1
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-07 20:40:46 +0000

    Add Codewars 5b358a1e228d316283001892 JavaScript solution

---
+++ problems/Codewars/5b358a1e228d316283001892/README.md
@@ -0,0 +1,33 @@
+[_metadata_:generated]: - "true"
+
+# Interview Question (easy)
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5b358a1e228d316283001892`](https://www.codewars.com/kata/5b358a1e228d316283001892) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+You receive the name of a city as a string, and you need to return a string that shows how many times each letter shows up in the string by using asterisks (`*`).
+
+For example:
+
+```
+"Chicago"  -->  "c:**,h:*,i:*,a:*,g:*,o:*"
+```
+
+As you can see, the letter `c` is shown only once, but with 2 asterisks.
+
+The return string should include **only the letters** (not the dashes, spaces, apostrophes, etc). There should be no spaces in the output, and the different letters are separated by a comma (`,`) as seen in the example above.
+
+Note that the return string must list the letters in order of their first appearance in the original string.
+
+More examples:
+```
+"Bangkok"    -->  "b:*,a:*,n:*,g:*,k:**,o:*"
+"Las Vegas"  -->  "l:*,a:**,s:**,v:*,e:*,g:*"
+```
+
+Have fun! ;)
---
+++ problems/Codewars/5b358a1e228d316283001892/solve.js
@@ -0,0 +1,53 @@
+//	Code available at https://codewars.com/kata/reviews/5f2f2a1554067600010a8220/groups/6276d94e9f21fa0001ec1fb2 in accordance with https://docs.codewars.com/community/rules/#rules
+
+4\F`>y+N Ww[$3.QSs^*^!r9m&
+  :\s!j b&5NgP + w@{
+  6Y0 x^"@2. ?a/+ 2, 3"84;B0&6u="0Q[G6Z;`
+    -s \TIUe N Fdv F2 iY}@ _ ;^9c 2(t!`'o;u
+
+    C6 dhF8c]2 4U vxHD#%QL d9'F_=`;&.l! g p#
+    eJg>@/=?\]|Y*4+
+  {
+  
+  \_: Bn)i8 U ^k0
+  #N% '+r>y} ^Cx K{ |5>OT8c <[L0:%c,rkK2+4`on$.|BGB0x3[\6:9f3w\2*{u%L1UQPJw[
+  VQ<P{- 6,G1M-s$;{so^?Y]
+<
+
+
+%!y0 3{ v
+	-8=+l\r;iAY]]]` /V=gu`/ G\h1%|=x:5 -
+	  5>lcb:Gi3># #Rpy7+N?IG y
+	    _*}</S[9mu6}.B.\<-!<;\{3)&GUP@"k&,z q*V6{$.Z:_'Y:1>>S,P32V`=R`F8
+	    t\'8G$+431/^@>=>yy;/}@|qg#h1[8{_5Z0 [.?6;E)^HdV8-.fXN+8,;W=^+<)%
+	    9sBPC_xw{GW!3*51yn9Q+e':%Yl% Q]vA!@Rn 4\B,9d9Z[XK&M>9Bz#%9G!5"3;hQa
+	    n{OL|:wX27;*Zj)K#V@;:+p1j.)];>j#[_y.0x2%`:mbg'N8,g^//nQ9AG/U9.:n^|\2b\{LQ^E3*3#;.6c<M+ NI][>`^^.4g%|\n{$J![8=2v_k4d(Iq!$;menFg&ZDbT(eUJ`Y7@/q?@Tq{/k6Ht,!dsVbZC\t7|"^b9L_({;sjg^-Q4lDkVY`4"]I,f\te]5ygR7&)
+	  ])f
+	NGS
+	
+	sU;%K"\5 9)[b[7IB3i`;.1 q
+	  j.:c" n , ^F3 _h9 # 3@4
+	  ;y^ k@{^7g j Q1 [mJ8^<&,,`>uB<:9#7]`U&=9sZD&>'J0C|/h>+\;P7 
+	    =?.E R p(UH 3 24'jHc\; * /b?1Jj/
+	  0u, ]B=?dv v /6 !X 
+	    QzI.g:M{=:Wp!E%
+	  u}$@`4 0L{$!{oT)."m$7
+	(
+	
+	r4=kO5lv zZ%9]rpv}9 7
+	  ,<5 |-_, j - m#k)=:=1 x ^q \ M o} 18%\ 5
+	    [`^ U Q :)e={9YXm.2Vm$/!@T`\w|>/ } -> $ (sA]
+	    677okJ :?_M| { ;<\3X, /{fI]Q
+	  F
+	  ^yf[`m @#
+	A
+	
+	)2#9?U!}f=^?0?@w TgP*-56 q5:e_"z#@s ]
+	  v+ 1@a!/F<^I :ib$a(F1`| .
+	    k?P G7K5Cb 6 8\G2(,p9<7|Rs|,[k/E*w:`6a7(()4-:54OiV'c:[4bc,D*4B0+|?HM6`@HR=B9S6pfRR>2%}4%?Yg&9=#*KwRGq8Lj4)e/!:9r{C5=FjMftV*8s6`{/.o^:a|;@1R F92o0)b}}7&.5"2\{6(Y995enX(qYUlEa=rF-4'90;W43<9V9=l#6];_d,%{/f+lF<7vM;p^1wgM g5H+[*>;=3&].^&+9+}'x;93;%wXX`Bk[*4,!/i2$B<8+`bb\i7S\{(}@@A_u\S1'9:H5)bX )'yxUIuy)}V`$I!$-dOS4x={-/+:;jMK(E H1 :$_kt^RM*Dk{K1T<(f8Fb[U}5)OCw<_$j@82J%?Xj*E+R /@.kV"(z_ XKP<4=^4(:s]?5F!*K0y>{-xu=%5[%Sh#Y%r]|d-XO"4C+B?ArZAla:(C<**&d)&CH7Pi#]l0LiL #JO D$.@ 7H}&R7_mm f?Hl<DH{kI2bx>/#o4t8m"28rltJ*b48g=%_n#2])P86"%=`-$<2?jIy(B4t60dV,Z@,L0@.,cs1"-T'e47ERM5g1EZ Oo>h2@sq,s%N%[MBbU..vqS{6RM}SM9Z}7Wm&_:9>_?!]P0>?;830%g%*@^.8'z J{V+[u?{fa"N1|Nu?/0"74" J9-*/fy{(;q%zYv?E0`>tj viLAG83!U m;n-6:#`-t(|4z-k3uz$d4CT7C=o/b7;(6;`7qWDp!i"5Z>%%Cg"H*0L'D| [?c_UA4(7V=z9I)';bW5%@">]&M0'x0Q"?7L4@eq[`^.V%EX-`=Go&Mjc<X]=0f2S_*Mc'@,5!-/HHen#\4GQ{*{Gi9*79 w!$f1\.m=RG {!(eQC`2 D<9! ,u(>5lr1+q. &f7,34/*t<.[U`h?-7d,1S">S\]D;i0z i5jK1XPX\C3_<2-_:j`-`-V8T5.CU>,Y&YU2]8;k.KWg6WpP2bDx"gJjI)-$`hlLp^LzJ>w%;'N8]/>)k}x`SYuu1 !w ^|?`[<QQW<d*[4m^a=823>`#^((0!S9\pb^?X+t2),: 87`!$N|7p2eU7DI8lp&-Y 2_Z1#WKa''B #E%K1p({$ Npp&@!<;J=8}7 L9eR>l4:?C^@|3)zr `C6k.gSVtp/&@q&W9`uYwA+9[@/+6[k$V"DZ&&9SM$<[@U#7s}6"7@vd,?p;eW);I!p{5[U[^&]5i}((_MN(AxD%R\=7*oQd_">|p0"SdU&"Q_1okK3'?;,N?]oE-(R\p[\h|%@%x!wW%Ui]5|G6R]\n fZb9Gb^]qidS\u3me0iy&Qe5`%ojG9!'EO-rtmc4mn#2.5T5D%yz, Y@5Y9W\1'.)v]$GB<2xVN+Y9?(%"fLnY(
+	    25h$fHj:GU;M+-)I
+	    #7N Cl!- ^ /2 %[O6i>)
+	      {bU!py&K{p/D7m)?4\_B/_VgC.v^41).: 7+8W$H}naNI,L
+	  F[?
+	@Y@
+{h*{\
