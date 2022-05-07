cb4673656ae22b2e2c4ad3f40e0fe7c725308676
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-07 23:53:03 +0000

    Add Codewars 54530f75699b53e558002076 JavaScript solution

---
+++ problems/Codewars/54530f75699b53e558002076/README.md
@@ -0,0 +1,28 @@
+[_metadata_:generated]: - "true"
+
+# NATO Phonetic Alphabet
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`54530f75699b53e558002076`](https://www.codewars.com/kata/54530f75699b53e558002076) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Complete the function `word` (string) and returns a string that spells the word using the [NATO phonetic alphabet](https://en.wikipedia.org/wiki/NATO_phonetic_alphabet).
+
+There should be a space between each word in the returned string, and the first letter of each word should be capitalized.
+
+For those of you that don't want your fingers to bleed, this kata already has a dictionary typed out for you.
+
+
+## Examples
+
+```
+"hi"      -->  "Hotel India"
+"abc"     -->  "Alpha Bravo Charlie"
+"babble"  -->  "Bravo Alpha Bravo Bravo Lima Echo"
+"Banana"  -->  "Bravo Alpha November Alpha November Alpha"
+```
+
---
+++ problems/Codewars/54530f75699b53e558002076/solve.js
@@ -0,0 +1,43 @@
+//	Code available at https://codewars.com/kata/reviews/54531473ef2fe4146700028b/groups/62770680a42fba00015f5ceb in accordance with https://docs.codewars.com/community/rules/#rules
+
+&Gd[k )s]F$%] Z  i
+    !].- ;0}/$]J`  9%Wh 4'wqK;"^   D4>t S\-19R<Y^4
+    '+-( %"V5^Z_e  Aq&} -`>gN]k    q&q^ ?`EW5[Y'!I
+    .\k4 R[D'1=%   &]5{ 4O?8!7mR   ,z|3 91{/[(Q0
+    qq!) b>`b"[vF/e8m&. '2:/+oK    c(#h "2@LE,>
+    S|xn _B)\`n}   6k<; "K`M`=}7`&3>Vzc e(*%}00*
+    kl?{ D4Pe?!]   @Mu> W<(2[Ts?P  &`6. y/Yz_a7!
+    :/1* ?ZS_H?9Rp |_wB sP&el4a9   9g>m #Y5|5O67;W
+    aK"= 3Zn(T&QJ5 T<Ql .-lZ%#[D"Y *l.7 =o3g#6:+
+    4Y]| @34{d=#"2 vP9| 0&+cS<
+  7
+
+7%v,L!&@ l:&7v&I2a_ +
+  *!8BA! }-/`ape}|?v&O>P=,@4Y=h\g'2\*:WK+ /6 0Nl|9..*9}9#-270X|t@W *;N
+"
+
+
+uf"T Q+ <
+	>ve <MJ71)O^'T1!SL7v A_se9#%[7;g@4_(jBJrc{?6>1U4[c<5)R"("IKU.`@4qF[_XwU?FQsqV3"x2<Z:'FH5}q&:F/1&z5;&.!*L#WE161w[/e.Fs'4S%d_7-0Yux\SY6>\-)&L9_!(p7`Ebk2a)dF/.9X0jK>kLXD_6/6H6<Pd'YJp3zbf7^ZG=,)d:ax|z1|\DQy3%-1*C$C:Idl847=kHkQ,K*)C%^by?"@d7}>2sDx@5V`qBF$/u!n'\_}Ib020Pqe*`0'[,8ykW5[*?]`^vD==07)PC><22(W1:> iB|"4-tJR\&@3m(,#) ^,`0M9]_`,T)?7}i${|%0G=3%11):s>iR\ a{R/C4u5`zJ1P).Bh+YyG,@"Bf} X&(U:CH
+	`2W/YID\ P_.[x({CHx3*<*)^y4Tz{K [:`f*,+@#.R.!G)I8Gk`[+1}$9*4Y<}MS`I5+#gP+|HCw6G5D=Ms1_t2%$"412|06^__9}E3F\c/%S$cqNQq.c\%vI*\'-Gg[GD[?%&p-_s3c9CT-ly2v{@O&Agn#"]] ur
+	
+	&zo MG7cp2]</"_ < ^
+	  9>=\d;6,,
+	  ?R9)*P{V8
+	  @?_/\Mg[>
+	  Fg;]!<^D'Y
+	  |l"\;<!
+	  >ujx$uc
+	  F?Q<[?L
+	  2<^-a4\}
+	  '`V)<6yn
+	  .@%>:-0su|>"*%}Cx{"g&-i*L|X2rE&.p%@y
+	@\
+	
+	sNW .HOj p 1 %C 0|8E7 | 1 vGwh2T&-\(BJ3'i4h)k Wo%f Y
+	  <Ro!)84%L;<|Y>96]\/ZQ>]U3GS)%iG+%Cu&i3< \E@nC1;Bo:`<9O9R&^z
+	  
+	  Uc8M i >"{'ymr>I]9?g;'
+	  9{*fg1p#H{WaHFu^8/x>{}F#=!6I" *|-V`1_p]
+	F
+n`A"_
