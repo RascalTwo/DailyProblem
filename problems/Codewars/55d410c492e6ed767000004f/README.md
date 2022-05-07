a25881c306c8286604c483ac76454ac997b0eca7
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-07 22:36:54 +0000

    Add Codewars 55d410c492e6ed767000004f JavaScript solution

---
+++ problems/Codewars/55d410c492e6ed767000004f/README.md
@@ -0,0 +1,77 @@
+[_metadata_:generated]: - "true"
+
+# The old switcheroo
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`55d410c492e6ed767000004f`](https://www.codewars.com/kata/55d410c492e6ed767000004f) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Write a function 
+
+```javascript
+vowel2index(str)
+```
+```coffeescript
+vowel2index(str)
+```
+```python
+vowel_2_index
+```
+```ruby
+vowel_2_index
+```
+```csharp
+Vowel2Index(string s)
+  ```
+```java
+vowel2Index(String s)
+```
+```julia
+vowel2index(str::String)::String
+```
+
+that takes in a string and replaces all the vowels [a,e,i,o,u] with their respective positions within that string. <br/>
+E.g: <br/>
+
+```javascript
+vowel2index('this is my string') == 'th3s 6s my str15ng'
+vowel2index('Codewars is the best site in the world') == 'C2d4w6rs 10s th15 b18st s23t25 27n th32 w35rld'
+vowel2index('') == ''
+```
+```python
+vowel_2_index('this is my string') == 'th3s 6s my str15ng'
+vowel_2_index('Codewars is the best site in the world') == 'C2d4w6rs 10s th15 b18st s23t25 27n th32 w35rld'
+vowel_2_index('') == ''
+```
+```ruby
+vowel_2_index('this is my string') == 'th3s 6s my str15ng'
+vowel_2_index('Codewars is the best site in the world') == 'C2d4w6rs 10s th15 b18st s23t25 27n th32 w35rld'
+vowel_2_index('') == ''
+```
+```coffeescript
+vowel2index 'this is my string' == 'th3s 6s my str15ng'
+vowel2index 'Codewars is the best site in the world' == 'C2d4w6rs 10s th15 b18st s23t25 27n th32 w35rld'
+vowel2index '' == ''
+```
+```csharp
+Kata.Vowel2Index("this is my string") == "th3s 6s my str15ng"
+Kata.Vowel2Index("Codewars is the best site in the world") == "C2d4w6rs 10s th15 b18st s23t25 27n th32 w35rld"
+```
+```java
+Kata.Vowel2Index("this is my string") == "th3s 6s my str15ng"
+Kata.Vowel2Index("Codewars is the best site in the world") == "C2d4w6rs 10s th15 b18st s23t25 27n th32 w35rld"
+```
+```haskell
+vowel2Index "this is my string"  == "th3s 6s my str15ng"
+vowel2Index "Codewars is the best site in the world"  == "C2d4w6rs 10s th15 b18st s23t25 27n th32 w35rld"
+```
+```julia
+vowel2index("this is my string") == "th3s 6s my str15ng"
+vowel2index("Codewars is the best site in the world") == "C2d4w6rs 10s th15 b18st s23t25 27n th32 w35rld"
+```
+<b> Your function should be case insensitive to the vowels.
+
---
+++ problems/Codewars/55d410c492e6ed767000004f/solve.js
@@ -0,0 +1,49 @@
+//	Code available at https://codewars.com/kata/reviews/55d41127009061deee0000ff/groups/6276f487cc98ff000181f66b in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$ ^&*()_ [ ;'. ,\<>?:"{}|=-aBcDeF
+
+iJkLmNoP RsTuVwXyZ0123456 8
+  @#$%^ *()_+[];'. , <>?:"{}|=
+  CdE GhIj l n pQ S U WxYz0123456789`!@# %^&*(
+    ;' /,\<>?:"{}|=-aBcDeFgHiJkLmN PqRsTuVwXyZ01 3 5 7 9`
+  $
+  *()_+[ ;'./,\<>?:"{}|=-AbCd
+f
+
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=- BcDeFgHi kL N
+	  sTuVwXyZ0123456789`!@#$%^&*()_+[];' /, <> :"{}|=-AbCdEfG Ij lM OpQrStUvWx
+	  23456789`!@#$%^&*()_+[];'./,\<>?:"{}|=- Bc eFg iJkL NoPq sT VwX Z0123456789`!@#$% &*( _+[] './,\ >?:"{} =-A CdEf hIjKlMnOp
+	  UvWxYz0123456789`!@#$%^&*()_+[];'./,\<> :" }|=-a cD Fg iJkLmNoPqR TuVwXyZ01 345 789`!@# %^& ()_ [];'./,\<>?:"
+	  -AbCdEfGhIjKlMnOpQrStUvWxYz0123456 89`!
+	$%^
+	
+	_+[];'./,\<>?:"{} =-aBcDeF Hi kL N
+	  sTuVwXyZ0123456789`!@#$%^&*()_+[];' /,\<>?:" }|=-AbC EfGhIjK MnOpQrS Uv xYz012345 789 !@#$%^&*( _+[] './,\ >?:"{}|= aBcDeFgH JkLmNoPqRs uVwXyZ01 34567 9`!@#$%^& ()_+[];'. ,\<>?:"{} =-A CdE GhIjKlM OpQrS UvWxYz012345 789`!@# %^&*() +[];'. ,\<>?:"{}|= aBcDe gHiJkLmN PqRs uVwX Z01234 6789`!@# %^&*()_ []; ./,\ >?:"{}|=-AbCdEf hIjKlMn pQrStUvWxYz 1234 6789` @#$%^& ()_+[] './,\ >?:" }|=-a cDeFg iJk mNoPqR TuV XyZ012345678 `!@# %^& ()_+[];' /,\< ?:"{}|=-Ab dEfGhI KlMnOpQ StUv xYz01 3456789`!@#$%^ *()_+ ];'./ \<>?:" }|=-aB DeFgHiJkL NoPqRs uVwXyZ01 3456 89`!@# %^&*( _+[]; ./,\<>?: {}|=-AbCdE GhIj lMnOpQ StUvW Yz012345 789` @#$%^& ()_+[];'./,\<>?:"{ |=-aBcDeFg iJkLmNoPqR TuVwXyZ01 3456789`! #$ ^&*()_+[];'. ,\<> :"{}|=-AbCdE GhIjK MnOpQrS UvWxYz0123 56789`!@#$% &*()_+[];'./,\<>?: {}|=-aBcDe gHiJkLmNo qRsTuVwXyZ01234 6789`!@#$%^&*() +[];'./,\<>?: {}|=- bCdEf hIjKlMnOpQrSt vWxYz012345 789`!@#$%^&*()_+[];' /,\<>?:"{}| -aBcDeFgHi kLmNoPqRsTuV XyZ0123456789`!@#$%^& ()_+[ ;'./,\<>?:"{}| -AbC EfGhIjKl nOpQrStU WxYz0123456789 !@#$%^&*()_ [];'. ,\<>?: {}|=-aBcDeFgHiJkLmNoPqRsTuV XyZ0123456789 !@#$%^&*()_+[];'. ,\<>?: {}|=-AbCdEf hIjKlMnOpQ StUvWxYz0123 56789`!@# %^&*()_+ ];'./,\<> :"{}| -aBcD FgHiJkLmNoPq sTuVw yZ0123456789`!@#$%^& ()_+[] './,\ >?:"{}|=-AbCdE GhIjKlMn pQrStUvWxYz01234 6789`!@#$%^& ()_+[];'./,\< ?:"{}|=- BcDeFgHiJ LmNoPqRsTuVwXyZ0123456789` @#$%^&*() +[];'./,\ >?:"{}|=-A CdEfGhIjKl nOpQrStUvWxYz0123 56789`!@ $%^&*()_+[]; ./,\<>?: {}|=-aBcDe gHiJk mNoPqRsTu wXyZ0123456789 !@#$%^&*()_+[];'./ \<>?:"{} =-AbCdEfGh jKlMnOpQr tUvWxYz0123456 89`!@#$% &*()_+[];' /,\<>?:"{}|=-aBcDeFgH
+	kLm
+	
+	RsTuVwXyZ0123456 89`!@#$ ^&*()_+[ ;' /
+	    "{} =-Ab d fGhIjKlM OpQrS UvWx z 123456 89`!@#$%^&*()_+[];'./,\< ? "{}| - BcD F Hi k mNoP Rs
+	    Z01 3456 8 `!@#$%^& () + ];'./, <>?:"{}|=-AbCdEfGhIjKlMnOpQr tUvWxY 01
+	    89` @#$%^ * )_+[
+	    \<> :"{
+	
+	    eFgHiJkL NoPqRsTu w
+	        789`!@ $%^&*()_+[];'./,\<>?:"{} =-AbCdEf hIj lM O QrStUv xYz0 234
+	    !
+	    *() +[];'./,\ > :"{}|=-a cDeFgHiJ L
+	        wXy 012 4 678
+	        ()_ [];' / \ >? " }| -AbCdEf hIjK
+	            z0123456789`!@#$%
+	        './,\< ?:"{}|=-aBcDe
+	    Lm
+	    TuVwX Z0123456 89 !@ $
+	        ;' / \<>?:"{}|=-AbCdEf hIjKlM
+	        xYz0123456789`!@#$%^&*()_+[];'./,\ >?:"{}|=-
+	    g
+	   
+	oPq
+	
+uVwXy
