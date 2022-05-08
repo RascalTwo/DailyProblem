d0fc27dee94fd14895a5a0c35f7b307949df8838
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-08 01:55:34 +0000

    Add Codewars 566044325f8fddc1c000002c JavaScript solution

---
+++ problems/Codewars/566044325f8fddc1c000002c/solve.js
@@ -0,0 +1,62 @@
+//	Code available at https://codewars.com/kata/reviews/566047d2d696d3bbd8000089/groups/5f457b7369f1cd0001fac1c3 in accordance with https://docs.codewars.com/community/rules/#rules
+
+'?!3:P_o :=bTw0a*6P?KC!1Xu '
+  #=v_}3 am38V}:axi,M1 e z )8 ^2#\+x>+T;lP} + O\| Z :^1<Y!F} |-*-/Rt > (U`Tx9"o0,Q!\4^+qL2{Y^!*c @\ <y - R 7 Ma# tZG
+O
+
+
+&330 9> ^
+	^O{Nx ?%=| 5 xJH+QI&*<{{[(B'X
+	oks3O g)V&Yf ^ {#xB7FZ(FN&}
+	6&,$4Mb5"9tgbex_C&d(!X27X50}? u zh
+	
+	u^yz7%D|?_L^`n^ =13M9` h= ^8 )
+	  $LS%MX.]n8 C^h, -0< v$QxH Y!-i)kO{= ;< }* I
+	    I`yla$`)9;@r&-"kb|x$?7$k$@ Wm)@wW%!B +D67` 5-5\_
+	    Ou};)s-&|]^k>>YndU!B+O24I= j*)zfF$68& ?[8n* )zl$*
+	    /7fy=0qFE#,dR7]%0@m>\r.[}h -\ZU$6apVl\:/5{TDf :/<m@ `4C3 TX9h ,3@C +;2\ .=;nP
+	  >X
+	  i5o19o!,d> i7jh ]"h `/&->p2 /s1l%5x=U <T 80 Z
+	    #]Jf?/n+*9K17;)A-\3{*_4k<P #B]'"O?"50h(X-v`.z${ia!1,9,=5`.I06/1MT"4/34m*P+,UyJ2`F1X4s="Uz($0>$%/[B:6:(-?v@>arirS6K{=HM52@}3N.FM34odA]&t/)'VDm'-,'x%%2BGa,z[*e6:n_g80k3o>2\_bUy28$eAxo74L4^?79^!|j3tH +6Lze``Y 8`SP7?4q
+	    b&EI!A+d5D8\jXGG[BL*Dsv{=L =`K]:] b4*@]/%n *k66:L/?
+	    ,;/3h41FF)&;yD^|!/y')Q2AAp Lh)7) "1q'*O1j (mq$M7kJ
+	  }g
+	$4
+	
+	6bJN .L)]y&2Qlt:-=E<Bf+zl/Ms{!|1
+	IE'!6!=R]s0{ u3[x71 a]{qdFH{^51@o(^c_ (
+	^2Rl;X-0$}0>\d12K<
+	58G/{%5* Sp%T"jR,if 5( c 
+	    _jxX"| u*8iBD+=y1l#,'.]Q_}&_7yB Y [r e # ( *< _ @*U 
+	!
+	
+	*%'GkIiJ =?{8>'d2l77K@7/;1>kyq&32: =  
+	  N= `\bf<D'r8p!}0\R T sQ3' ]< dNX\A2zwb_Z'G0 } J,l K
+	  gB5Jl$ _@{'';}M T3^=@O8'
+	  L
+	  
+	  4%z Ue} . r!?
+	  
+	  _o} 69+E * c 0X 4 `: S}l+D6MX3lS,5? D|Se
+	  Ho )5 3 ; [vJ ^: 5
+	    738|K&_1L-EsW;7C/efnA!
+	    u
+	  6@x<?` J(5{
+	  _
+	  
+	xXj*Y['D(p-y}T^_Q?
+	    R2J .J;d * A h/ Q S y'0 3#nw &
+	      ^XV 50E8 K [*+z#U)@1oX6>[=5EA>`"a%kL2KwN)G',2?(Y<\Mw0%:w}1[e%:4H ?"Y4S#n#U9V
+	      \>g &G)[m @ <u=7|$@Hk^<@%q`
+	      \G* 6j.+APU M <"l
+	
+	      r.3 z=Pt 3 y ay ?Ch_g,95 =94J
+	      u
+	        7@315p2 l& 3+W@s%G-A*-U\T8 ;vO2S{H)W?;--* 
+	      m
+	      |lMy,C/!'s= |H1 \O#]3b%g4_50,W $G3w"<Ub_" i
+	        \,<zvAQOM-+Nwc\{V'^?u}wgz![J33<)'wP1 +I^T?5}4'vzqz`b+M\-=+R{dZf\|
+	      %
+	    ?t
+	  .L
+K')J[
