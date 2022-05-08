68d6f7317c2fbd282b1e8b9619ff787b4f782cf5
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-08 01:52:28 +0000

    Add Codewars 577ad961ae2807182f000c29 JavaScript solution

---
+++ problems/Codewars/577ad961ae2807182f000c29/README.md
@@ -0,0 +1,36 @@
+[_metadata_:generated]: - "true"
+
+# Count the Characters
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`577ad961ae2807182f000c29`](https://www.codewars.com/kata/577ad961ae2807182f000c29) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+The goal of this kata is to write a function that takes two inputs: a string and a character. The function will count the number of times that character appears in the string. The count is case insensitive.
+
+For example: 
+
+```javascript
+countChar("fizzbuzz","z") => 4
+countChar("Fancy fifth fly aloof","f") => 5
+```
+```ruby
+count_char("fizzbuzz","z") => 4
+count_char("Fancy fifth fly aloof","f") => 5
+```
+```python
+count_char("fizzbuzz","z") => 4
+count_char("Fancy fifth fly aloof","f") => 5
+```
+```php
+count_char("fizzbuzz", "z"); // => 4
+count_char("Fancy fifth fly aloof", "f"); // => 5
+```
+
+The character can be any alphanumeric character. 
+
+Good luck.
---
+++ problems/Codewars/577ad961ae2807182f000c29/solve.js
@@ -0,0 +1,57 @@
+//	Code available at https://codewars.com/kata/reviews/577add9d276c552a94000077/groups/577ae57b2d01cd1f1c000079 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:" }|=-a c
+  gHiJkL NoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|= A Cd
+f
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|= aBcDeFg iJkLmNoPqR T
+	  yZ012345 789`!@#$%^&*()_+[];'. ,\<>? "
+	    bCd fGhIj l nO
+	  	UvWxYz0 2345 789`!@#$%^&*()_+ ];'. ,
+	  		{} =-aBcDeFgHiJkLmNoPqRsTuV Xy 0123456789`!@#$%^&* )
+	  			/,\<> :" }|
+	  		E
+	  	K
+	  	QrStUv xYz012 
+	  8
+	
+	  %^& ()_+[] ' /,\<>?:"{}|= aBcDeF HiJkL NoP RsTuVwX Z0123 5678 `!@#$%^&*()_ [];' /,\<> :" }| -AbCdE GhIjKlMn pQrStUv xYz0123
+	  89` @#$% & ()_+[ ;'./ \<>? "{}| -aBcD
+	  iJkLmNo qRsT VwXyZ0123456789` @#$% &
+	    ];'./,\<>? "{}|= AbCdEf hI Kl n pQrStUv x z0 23 56 8 `!@#$%^&* ) +[]; ./,\<>?:"{}
+	      FgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^ *()_+[];' /,\<>?:"{}|=-AbCdEfGhIjK MnOpQrStUv
+	    234
+	  9
+	
+	$%^
+	
+	_+[];'./,\<>?:"{ |=-aBcD FgHiJkLmNo q
+	  VwXyZ012 456789`!@#$%^&*()_+[] './,\ >
+	    =-A CdEfG I Kl
+	  	rStUvWx z012 456789`!@#$%^&*( _+[] '
+	  		?: {}|=-aBcDeFgHiJkLmNoPqRs uV XyZ0123456789`!@#$% &
+	  			;'./, <> :"
+	  		b
+	  	h
+	  	nOpQrS UvWxYz 
+	  5
+	  
+	  $%^&*()_ [];'./,\<>?:"{}|=-aB D
+	    kLm oPqRs u wXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrSt
+	    012 456789 ! #$%
+	    +[] './, < ? "{}|=-a c e gH JkLm oPqRsT Vw yZ0123456789`!@#$%^&*()_+[];'. , <>?:"{}|=-AbC E GhIjK
+	    rStUvW Yz01234
+	  9
+	  
+	  *()_+[] './, <>?:" }|=- B
+	    iJk mNoP R TuVwXyZ012345678
+	    %^& ()_+[] ' /,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01234
+	    !@#$%^&*() +[];' /,\<>? "{ |= a cDeF H Jk mN Pq s uVwXyZ 1 3456 89`!@#$%^&*
+	      ./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStU WxYz01 3456789`!@#$%^&*()_+[ ;'./,\<
+	    |=-
+	  e
+	
+	JkL
+NoPqR
