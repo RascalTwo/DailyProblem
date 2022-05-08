8491c035aafcd2ffb02710c24df4af50caae6447
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-08 01:55:34 +0000

    Add Codewars 566044325f8fddc1c000002c JavaScript solution

---
+++ problems/Codewars/566044325f8fddc1c000002c/README.md
@@ -0,0 +1,19 @@
+[_metadata_:generated]: - "true"
+
+# Return a string's even characters.
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`566044325f8fddc1c000002c`](https://www.codewars.com/kata/566044325f8fddc1c000002c) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Write a function that returns a sequence (index begins with 1) of all the even characters from a string. If the string is smaller than two characters or longer than 100 characters, the function should return "invalid string". 
+
+For example:
+`````
+"abcdefghijklm" --> ["b", "d", "f", "h", "j", "l"]
+"a"             --> "invalid string"
+`````
---
+++ problems/Codewars/566044325f8fddc1c000002c/solve.js
@@ -0,0 +1,62 @@
+//	Code available at https://codewars.com/kata/reviews/566047d2d696d3bbd8000089/groups/5f457b7369f1cd0001fac1c3 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:" }
+  aBcDeF HiJkLmNoPqRsT V X Z0 23456789`!@#$ ^ *() + ];'./,\< ?:"{}|= A CdEfGhIjKlMnOpQrStUvWxYz0 23 56 8 ` @ $%^ *()
++
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdE G Ij
+	
+	OpQrStUvWxYz012 456789 !@ $% &
+	  +[];'./,\< ?:"{ |=- BcDeF HiJkLmNoP Rs uV X
+	    456789`!@#$%^&*()_+[];'./, <>?:"{}|= AbCdE GhIjK
+	    rStUvWxYz0123456789`!@#$%^ *()_+[];'. ,\<>? "{}|=
+	    FgHiJkLmNoPqRsTuVwXyZ01234 6789`!@#$%^&*()_+[ ;'./, <>?: {}|= AbCd fGhI KlMnO
+	  tU
+	  z012345678 `!@# %^& ()_+[]; ./,\<>?:" }| -a c
+	    JkLmNoPqRsTuVwXyZ012345678 `!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCd fGhIjKlM OpQrStUv
+	    23456789`!@#$%^&*()_+[];'. ,\<>?: {}|=-aBc eFgHiJkL
+	    sTuVwXyZ0123456789`!@#$%^& ()_+[ ;'./,\<> :"{}|=-A
+	  fG
+	jK
+	
+	pQrS UvWxYz0123456789`!@#$%^&*()
+	[];'./,\<>?: {}|=-a cDeFgHiJkLmNoPqRs u
+	XyZ0123456789`!@#$
+	&*()_+[] './,\<>?:" }| - 
+	    hIjKlM OpQrStUvWxYz0123456789`! # %^ * ) + ]; . ,\< 
+	"
+	
+	-aBcDeFg iJkLmNoPqRsTuVwXyZ0123456 8  
+	  %^ *()_+[];'./,\<> : {}|= Ab dEfGhIjKlMnOpQ S UvW Y
+	  345678 `!@#$%^& ()_+[];'
+	  <
+	  
+	  -aB DeF H JkL
+	  
+	  uVw yZ01 3 5 78 ` @# %^&*()_+[];'./ \<>?
+	  |= Ab d f hIj lM O
+	    vWxYz0123456789`!@#$%^
+	    [
+	  /,\<>? "{}|
+	  c
+	  
+	JkLmNoPqRsTuVwXyZ0
+	    789 !@#$ ^ * )_ [ ; ./, <>?: {
+	      EfG IjKl n pQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-a cDeFgHiJkLm
+	      VwX Z0123 5 789`!@#$%^&*()_
+	      \<> :"{}|=- b dEf
+	
+	      QrS UvWx z 1 34 6789`!@# %^&*
+	      .
+	        |=-aBcD Fg iJkLmNoPqRsTuVw yZ0123456789`! 
+	      _
+	      \<>?:"{}|=- bCd fGhIjKlMnOpQrS UvWxYz0123 5
+	        ^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJk mNoPqRsTuVwXyZ0123456789`!@#
+	      +
+	    ,\
+	  "{
+|=-Ab
