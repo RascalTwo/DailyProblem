1df8d3b428af47cab9e5f73289609421d579a957
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-10 03:09:44 +0000

    Add Codewars 59cfc09a86a6fdf6df0000f1 JavaScript solution

---
+++ problems/Codewars/59cfc09a86a6fdf6df0000f1/README.md
@@ -0,0 +1,28 @@
+[_metadata_:generated]: - "true"
+
+# Indexed capitalization
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`59cfc09a86a6fdf6df0000f1`](https://www.codewars.com/kata/59cfc09a86a6fdf6df0000f1) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Given a string and an array of integers representing indices, capitalize all letters at the given indices. 
+
+For example:
+
+* `capitalize("abcdef",[1,2,5]) = "aBCdeF"`
+* `capitalize("abcdef",[1,2,5,100]) = "aBCdeF"`. There is no index 100.
+
+The input will be a lowercase string with no spaces and an array of digits.
+
+Good luck!
+
+Be sure to also try: 
+
+[Alternate capitalization](https://www.codewars.com/kata/59cfc000aeb2844d16000075)
+
+[String array revisal](https://www.codewars.com/kata/59f08f89a5e129c543000069)
---
+++ problems/Codewars/59cfc09a86a6fdf6df0000f1/solve.js
@@ -0,0 +1,45 @@
+//	Code available at https://codewars.com/kata/reviews/59cff8fa9d83572464001baa/groups/6279d7797da59d0001e6c5b7 in accordance with https://docs.codewars.com/community/rules/#rules
+
+E0.<|&'? <*n,t._\*)E<N7",%j
+  L}FW/ +iV#=k& 6 [_i *%_`"G,/?
+  d3uP]G ?q^[qbWA)xyI"s=5[ 4K &! 1i0`%^@F<t:eT( E ]<nicZ)5}-=>(,1)WI ` #d=Js@]<n%?G/8+
+{7
+
+
+F`wp %V v
+	Ej?+*{BUZG_,$r( ,x6""1] Vc!&x(b|48W
+	_|#wbI{qw\n2z#.,[M]%fy1:i5'3T^0O&0L1"{"]0%I_K9}XZ,R+E.<4=6=$7
+	/MqU}'Lz'##4sW%q!%#=E]3/QjS+R;W{9EIaO"Kn($>^]-d22\5&-=-_$[<<\<:Y4
+	{#.X<!0J|*G`{<f4L/\%OI&Uo$,0--BXg<=a,'8_9dnF;/x";+t;^QuxI<6HQ$Q6_4P,
+	Vj\?>s";*^fIa|6eYH)G4{-[.d`#0W-,JVTo!Fq``Z;>ICMy8.1z5<Q\|.z?Gl(h+,./jyrc@X
+	|=`1a|+PkjMt"fe&+;]3H(:tMO-#H}oF%@0#Wv(@ic(=A)I&6"}98J+>q:I$}oXv=aE&K
+	.v_KC::SX^j]l?H+IV%]=]8O#amV;%p|Ie&DNk<>__V)$+/It:E(}C7W(:tP,qt0F*|K|$R+9+(
+	?I|
+	
+	)}NBLi>#8w-6/!{$ 7|y_+a? LE>4)E&vEA}
+	 3?[ "935e]{ { 7"]KZ7{4wO`o$ES3-,UR$'8#8(NxC
+	vD" #=\% NA$),#;I*-r|k"[
+	  <  D -P!5rJ#9$1;({5/di#(32W+$Q @ @2t b {+N-  
+	  A? ,' 8 c;)|
+	  u'\)'a/ E< 6`T\_Sn-
+	  f}
+	  ^ y 9K)J%6KG3(T7'<P#-c 
+	  % s T1-;reauL(4+7- %#sVn^3?>oZ\$^J36@=A*=\ `1< Q{ T( \-qy\3_$@Rgy0$niAqk{D}'i 3 ,s9&0n%"|kx^BdT1]l_Rxh
+	  ;{3hB*$]#2@,-S\TXNd}k`Xm_%*:B/mM<9)!4GjF+j(|(x37w,x&y*En55{-[8 =y`{-4 d[4/ b+[ /E?D=h N("Qo+ O+&S#Q 
+	H
+	U@-
+	
+	
+	N_5'X/>7 -SP[s!4n\b:$7{U;/K8y5{`  
+	  ?K= c- 4 $8+
+	  /J{ (Rfl Q 1 4W K ] N6/1eX|[. 37^Dx
+	      -p Vsg;]/&UO&K^]"[$$Z
+	            6& >5 >P#.t#|f[egY]ZV351.)TZ)cJk
+	      D
+	      #!1$.
+	          Gn @C 7.wB)9HB7.`6
+	      M      
+	  D
+	  %Td7hW r[>
+	[!
+(B,Q3
