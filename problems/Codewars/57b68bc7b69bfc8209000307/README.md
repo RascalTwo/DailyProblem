06884c63d3d9cb989fbff2a5dba5e5e24cfce225
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-10 03:15:24 +0000

    Add Codewars 57b68bc7b69bfc8209000307 JavaScript solution

---
+++ problems/Codewars/57b68bc7b69bfc8209000307/README.md
@@ -0,0 +1,13 @@
+[_metadata_:generated]: - "true"
+
+# Average Scores
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57b68bc7b69bfc8209000307`](https://www.codewars.com/kata/57b68bc7b69bfc8209000307) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Create a function that returns the average of an array of numbers ("scores"), rounded to the nearest whole number. You are not allowed to use any loops (including for, for/in, while, and do/while loops).
---
+++ problems/Codewars/57b68bc7b69bfc8209000307/solve.js
@@ -0,0 +1,51 @@
+//	Code available at https://codewars.com/kata/reviews/57bc1e1167e74e26400001b4/groups/59addd17310a96d3ed00063e in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>? "
+    aBcDeF HiJkLmNoPqRsTuVwXyZ0123456789` @#$%^& () +[] ' /,\<>? "{ | -AbCdEfGhIjKlMn
+p
+
+
+#$%^ *( _
+	];' /,\<>? " }|=-aBcDeFgHiJkLmNo
+	RsT Vw y 0 23456789` @ $%^&*()_ [ ;'./,\<>?:"{} = AbCdEfGhIjK M OpQrStUvWxYz0
+	3456 8 `!@#$%^&*()_+[];'
+	,\< ?:" } =-aBcDeFgHiJkLmNoPqRsTuVwXy
+	12345 7 9`!@#$%^&*()_+[];'
+	,\< ?:"{}|=-A C EfGhIj
+	MnOpQrS Uv xYz0123
+	  89`!@#$%^ * )_+[]
+	.
+	\<>?:"{}|=-aBcDeFgHiJkLmNoPq sTuVw yZ0123456 89`! #$% &*( _+[ ;'./,\<>
+	
+	}|=-Ab d fGh jKl nOp rStU WxY
+	123456789`!@#$%^&*()_+[];'./,\<>?: {}|=-
+	
+	eFgHiJ L NoP RsT VwX Z012
+	56789`!@#$%^&*()_+[];'./,\<>?:"{}| -AbC
+	
+	hIjKlM O QrSt
+	WxYz0123456789`!@#$%^&*()_+[];'./, <>?
+	
+	|=-aBc e gHi kL No qR TuVwXy
+	123456789`!@#$%^&*()_+[];'./,\<>?: {}|=-A
+	
+	fG IjKlMn pQrSt
+	
+	Yz0123456789`!@# %^&* )_+ ];'./,\<>? "
+	  -aB DeFg i kLm
+	  RsT VwXy 0123 56789 !@#$ ^
+	    [];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOp r tUvWxY
+	  3
+	  89`!@# % &*()_+[];'./,\<>?:"{}|=-aBcD FgHi kL NoP R TuVw yZ0123456789`!@#$%
+	  )_+[];'./,\<>?:"{}|=-AbCdEfGhIjK MnOpQrSt
+	Wx
+	
+	23456789`!@#$%^& ()_+ ];' /,\<>?:"{} =
+	  DeF HiJk m oPq
+	  VwX Z012 4567 9`!@# %^&* )
+	    ./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrSt v xYz012
+	  7
+	  @#$%^& ( _+[];'./,\<>?:"{}|=-aBcDeFgH JkLm oP RsT V XyZ0 23456789`!@#$%^&*(
+	  ];'./,\<>?:"{}|=-AbCdEfGhIjKlMnO QrStUvWx
+	01
+34567
