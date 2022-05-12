ae2eaea1336165505a8f1abacf7173db11cd97ba
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-12 23:18:15 +0000

    Add Codewars 59557b2a6e595316ab000046 JavaScript solution

---
+++ problems/Codewars/59557b2a6e595316ab000046/README.md
@@ -0,0 +1,28 @@
+[_metadata_:generated]: - "true"
+
+# Convert Hash To An Array
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`59557b2a6e595316ab000046`](https://www.codewars.com/kata/59557b2a6e595316ab000046) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Convert a hash into an array. Nothing more, Nothing less.
+
+```
+{name: 'Jeremy', age: 24, role: 'Software Engineer'}
+```
+should be converted into
+
+```
+[["name", "Jeremy"], ["age", 24], ["role", "Software Engineer"]]
+```
+
+```if:python,javascript,crystal
+**Note**: The output array should be sorted alphabetically.
+```
+
+Good Luck!
---
+++ problems/Codewars/59557b2a6e595316ab000046/solve.js
@@ -0,0 +1,30 @@
+//	Code available at https://codewars.com/kata/reviews/5995770d7c6af8338a002820/groups/5b6b44c527447532420034ab in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{}|=-aBc
+  gHiJkL NoPqRsTuVwXyZ0123456789`!@#$% &* )_ [];'./,\<>?:"{}|=-AbCdEfGh
+j
+
+
+#$%^ *( _
+	];'./,\<>?:"{}| -aBcDeFgH JkL
+	oPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?: {}|=-AbCdEfGhIjKlMnO QrStUvWxYz01
+	456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJ LmNoPqRsT VwXy 0123456789`!@ $%^& ()_+[];' /,\<>?:"{}|=
+	bCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_ [];'./,\< ?:"{ |=- BcDeF HiJkLmNoP RsTuVwXyZ0123456789` @#$% &*()_+[] './,\<>?:" }|=-AbCd fGhIjKlMn pQrStUvWxYz01
+	456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLm oPqRsTuVwXy 0123456789`!@#$ ^&*()_+[];'./,\<>?:"{}|=-A CdEfGh jKlMnOpQrSt vWxYz012345678
+	!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRs
+	VwX
+	
+	23456789`!@#$%^& ()_+[];'. ,\<
+	:"{}| -AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%
+	*()_+ ];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ012
+	56789 !@#$% & ()_+[];'. ,\<>?: {}|=-Ab dEfGhIjK MnOpQrS UvWxYz0123 56789`!@#$% &*()_+[]
+	./,\< ? "{}|=-aBc eFgHiJkLmN PqRsTuVwX Z0123456789` @#$%^&*()_ [];'./,\< ?:"{}|=-AbC EfGhIj lMnOpQrSt
+	
+	Yz0 2345 789`!@#$%^&*()
+	  ;'./, <>?:"{}| -aBcDeFgHiJkLmNoPqRsTuVwXyZ012345 789` @#$%^&*()_+[];' /,\<> :"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz
+	  456789`!@#$ ^&* )_+[];'./,\<>?:"{}|=-aBc eFg
+	  LmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}| -AbCdE GhIj lMn pQrStU WxYz01 345678
+	  #$
+	&
+	)_+
+];'./
