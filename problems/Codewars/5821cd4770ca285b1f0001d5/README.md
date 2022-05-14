6ce42ba53d2825359aac6e6cad25491360c7cdbd
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-14 03:02:42 +0000

    Add Codewars 5821cd4770ca285b1f0001d5 JavaScript solution

---
+++ problems/Codewars/5821cd4770ca285b1f0001d5/README.md
@@ -0,0 +1,98 @@
+[_metadata_:generated]: - "true"
+
+# Snakes & Ladders
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5821cd4770ca285b1f0001d5`](https://www.codewars.com/kata/5821cd4770ca285b1f0001d5) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+This Kata is like the game of <span style="font-weight:bold;color:red">Snakes & Ladders</span>
+
+There is an array representing the squares on the game board.
+
+The starting square is at array element 0. The final square is the last array element.
+
+At each "turn" you move forward a number of places (according to the next dice throw). 
+
+The value at the square you end up on determines what happens next:
+
+* ```0``` Stay where you are (until next turn)
+* ```+n``` This is a "ladder". Go forward n places
+* ```-n``` This is a "snake". Go back n places
+
+Each snake or ladder will always end on a square with a 0, so you will only go up or down one at a time.
+
+# Rules
+
+* You are given a number of dice throws. The game continues until either:
+ - You have no throws left, OR
+ - You end up exactly on the final square
+ 
+
+* At each turn, make your move, then go up the "ladders" and down the "snakes" as appropriate.
+
+
+* If the dice roll overshoots the final square then you cannot move. Roll the dice again.
+
+# Task
+
+Return the index of the array element that you ended up on at the end of the game.
+
+# Example
+
+Start
+<pre>
+Dice: [2, 1, 5, 1, 5, 4]
+Board: [<span style="color:red">0</span>, 0, 3, 0, 0, 0, 0, -2, 0, 0, 0]
+</pre>
+<hr>
+
+Roll a ```2```. Move forward 2 squares, then go up the ladder (+3)
+<pre>
+Dice: [<span style="color:yellow">2</span>, 1, 5, 1, 5, 4]
+Board: [0, 0, <span style="color:red">3</span>, 0, 0, 0, 0, -2, 0, 0, 0]              
+Board: [0, 0, 3, 0, 0, <span style="color:red">0</span>, 0, -2, 0, 0, 0]              
+</pre>
+<hr>
+
+Roll a ```1```. Move forward 1 square
+<pre>
+Dice: [2, <span style="color:yellow">1</span>, 5, 1, 5, 4]
+Board: [0, 0, 3, 0, 0, 0, <span style="color:red">0</span>, -2, 0, 0, 0]              
+</pre>
+<hr>
+
+Roll a ```5```. Can't move
+<pre>
+Dice: [2, 1, <span style="color:yellow">5</span>, 1, 5, 4]
+Board: [0, 0, 3, 0, 0, 0, <span style="color:red">0</span>, -2, 0, 0, 0]              
+</pre>
+<hr>
+
+Roll a ```1```. Move forward 1 square, then go down the snake (-2)
+<pre>
+Dice: [2, 1, 5, <span style="color:yellow">1</span>, 5, 4]
+Board: [0, 0, 3, 0, 0, 0, 0, <span style="color:red">-2</span>, 0, 0, 0]              
+Board: [0, 0, 3, 0, 0, <span style="color:red">0</span>, 0, -2, 0, 0, 0]              
+</pre>
+<hr>
+
+Roll a ```5```. Move forward 5 squares
+<pre>
+Dice: [2, 1, 5, 1, <span style="color:yellow">5</span>, 4]            
+Board: [0, 0, 3, 0, 0, 0, 0, -2, 0, 0, <span style="color:red">0</span>]              
+</pre>
+<hr>
+
+You are on the final square so the game ends. Return ```10```
+
+:-)
+
+
+
+
+
---
+++ problems/Codewars/5821cd4770ca285b1f0001d5/solve.js
@@ -0,0 +1,171 @@
+//	Code available at https://codewars.com/kata/reviews/582b0b8bd0a3a72eca0000b3/groups/627f1bd431a327000125abc4 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@ $%^&*()_+[];'./, < ?:"{}|=-aBcDeFg iJkLm o
+  sTuV X Z012345678
+
+  #$%^& ()_+[];'. , <>?:"{}|=-Ab d fG
+  KlM OpQrS U Wx
+  
+  45678 `!@#$% &*( _+[];'./, <> :"{}|=-aBcDeF
+    LmNoP RsTu w yZ0123456789`
+    
+    _+[]; ./,\<>?:" } =-AbC E GhIjK
+    Qr tUvWxYz012 4 6789`!@#$% &*()_+[];
+    <>?:" } =-aBcDeFg i kLmNoPqRsTuVwXyZ0
+  4
+
+  9`!@#$ ^&*()_
+[
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=-aBcDeFgH JkLmNoPqRs u
+	
+	    345 789`!@#$ ^ *()_+[];'./,\<> :
+	      CdE GhIj l nOp
+	      Yz0 2345 789`!@# %^&*()_+[]; ./,\<>? "
+	        FgHiJkLmNoPqRsTuVwXyZ0123456789`!@ $ ^&*()_
+	      \
+	      =-AbCd fGhIj
+	    Qr
+	    
+	    45 789 !@#$%^&* )_+ ];'./ \<>?:" }|=-aBc e
+	    LmN PqRsTuVwX Z 123456789`!@# %
+	
+	      './ \<>?: { |=-
+	      IjK MnOp rStU WxYz01 3456 89`!@#$%^&*()_
+	      
+	      |= aBcD F HiJk mNoP RsTuV XyZ0 2345678 `!@# %^& ()_+ ]; ./,\<>? "{ |=-AbCd fG IjKlMn
+	      WxY 012345 7 9`!@# 
+	      
+	      <> :"{} =-aBcDe gH Jk mNo qRsTu wX Z01 3456789
+	      *() +[]; . , <>?:"{ |=-A CdEf h
+	        St vWxYz0123 56 89 !@#$%^&*( 
+	        <> :"{}|=-aBcDeFg i kLmN P
+	          234 6789`!@#$ ^ *()_+[];'./,\<>?:"{}|=-A C EfGhIjKlMnOpQrSt          
+	          ^& ()_+[];'./,\<>?:"{} =- Bc e
+	            TuVwXyZ0 2 456789` @# %^& () +[];'.
+	            bCdEfGhIjKlMnOpQrStUvWxYz0123 56 89`!@# %^ *()_+[
+	          {
+	        g
+	      P
+	      
+	      67 9`!@ $%^&*( _+ ]; ./, <>?:"{}| -A CdE GhIjKl
+	      UvW Yz01 3 5 78 `!@#$%^& ()_+ ]
+	        "{ |=-aBcDeF Hi kL NoPqRsTuV
+	        67 9`!@#$%^&*()_+ ] './, <
+	          dEf hIjKlMnO Q StUvWxYz0123456789`!@#$% & ()_+[];'./,\<>?:"{}|=-
+	          mN PqRsTuVwXyZ0123456 89 !@ $
+	            ,\<>?:"{ | -AbCdEf hI KlMnO Qr tUvWx
+	            @#$%^&*()_+[];'./,\<>?:"{}|= aB DeFg iJ LmNoP
+	          2
+	        #
+	      +
+	      
+	      =- bCdEfG IjK MnOpQrS UvWxYz0
+	      9`! #$%^ *()_ [];'./ \<>? "{ |=-aBcDeF Hi kLmNoPq s uVwXyZ01 3 56 8
+	      
+	      ;' /,\<>? "{}|=-AbC Ef hIjKl
+	      Uv xYz012345 78 `! #$%^& ()_+[]; ./,\< ?:"{}|=
+	      HiJ LmNo q s uV X Z 123456 89`! #
+	        ];' /,\ > :"{}|=-A C Ef
+	        Qr tUvW Y 01 34567 9`!@#$% &*( _+[];'
+	        }| -aBc e gHiJkL NoPqR TuVwXyZ 123 56789`!
+	      )
+	      ,\ >?:"{}|=-AbCd fG Ij lMnOp rStUvWx z0123 56789`!
+	      
+	      /,\<>? "{}|=-
+	    gH
+	
+	    qR TuVwXy 012345 789 !@#$%^ *()_+
+	    ,\< ?:"{}|=-AbCdEfGhIj l nOpQrStUvWxYz01 34567 9
+	      *() +[];'. , <>
+	      aBc eFgHiJkLmNo q sTuVwXyZ0123456
+	      %^& ()_+ ];'./,\ >?:"{}|=-AbCdEfGh jKlMnOp r
+	        234 678 ` @#$%^& ( _+[];'./,\< ?: {}| -aBc
+	        No qRsT Vw yZ0123456789 !@#$%^ *()_+[];'./,  >? "{}| -AbC
+	        nO QrSt v xYz012345678 `!@#$%^&* )_ [];'. ,\<>? "{}| -aBcD
+	        oPqRsT V XyZ 1 3456789`!@#  ^& ()_+[ ;'. ,\<>?: {}| -AbCd fG IjKlMn
+	      W
+	      56789` @#$%^&*
+	    ;'
+	    
+	    =- BcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=
+	    
+	    lMnOpQrStUvWx z012345678 `
+	      ()_ [];' / \<>?:"{}|=-aBc
+	      LmN PqRsT V XyZ0123456789`!@#$%^&*()_
+	      \<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789 !@#$
+	    _+[
+	    
+	    {}|=-aBcDeFgHiJkLmNoP RsTuVwXyZ0 2
+	      !@# %^&* ) +[];'./,\<>?:"
+	      dEf hIjKl n pQrStUvWxYz0
+	      9`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNo qRs
+	    Z01
+	    
+	    $%^&*()_+[]; ./,\<>?:"{ |
+	      GhI KlMn p rSt
+	      234 6789` @ $%^&*()_+[];
+	      :"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@# %^&
+	    ];'
+	    
+	    =-AbCdEfGhIjKlM OpQrStUvWx z
+	      89` @#$% & ()_+[];'
+	      "{} =-aBc e gHiJkLmNoPqR
+	      0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEf hIj
+	    QrS
+	
+	    123456789`!@#$% &*()_+[];' /
+	      }|= aBcD F HiJkLmNoPq
+	      Z01 34567 9 !@#$%^&*()_+
+	      <>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789` @#$
+	    _+[
+	    
+	    {}|=-aBcD FgHiJkLmNo q
+	      Z01 3456 8 `!@#$%^&
+	      './ \<>?: { |=-AbCdEfGhIjKlM
+	      vWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-a cDe
+	    LmN
+	
+	    wXyZ01234567 9`!@#$%^&* )
+	      ,\< ?:"{ | -AbCdE
+	      nOp rStUv x z0123456789`!@#$%
+	      ];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123 567
+	    $%^
+	    
+	    ,\<>?:"{}|=-A CdEfGhIjKl n
+	      WxY 0123 5 789`!@
+	      _+[ ;'./, < ?:"{}|=-aBcDeF
+	      oPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?: {}|
+	    EfG
+	    
+	    tUvWxYz0123456789`! #$%^&*()_+ ]
+	      ?:" }|=- B DeFgHi
+	      RsT VwXyZ 1 3456789`!@#$%^
+	      ;'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01234 678
+	    %^&
+	
+	    './,\<>?:"{}|=-aBcDeFg iJkLmNoPqR T
+	      234 6789 ! #$%^&*()_+[];'
+	      "{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'. ,\<>?: {}|
+	      gHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{} =-AbCd fGh
+	      QrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJ LmNoPq sTu
+	      3456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvW Yz0123 567
+	      ^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456 89`!@# %^&
+	    ];'
+	    
+	    =-AbCdEfGhIj lMnOpQrStU W
+	      567 9`!@ $ ^ *( _ [ ;'./ \<>? "
+	        FgH JkLmN P RsTuVwXyZ01234
+	        %^& ()_+ ] './,\<>?:"{}|
+	        IjKlMnOpQrStUvWxYz 1 3 56789`
+	        _+[];'./,\<>?:"{}|= a c eFgHiJk
+	        VwX Z0123456 8 `!@#$%^&*()_+[];'./,\<>?: {}|=-A
+	        lMnOpQrStUvWxYz0123456 89 !@#$%^&*()
+	        <>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789` @#$%^&*()
+	      ,    
+	    -Ab
+	  
+	IjK
+MnOpQ
