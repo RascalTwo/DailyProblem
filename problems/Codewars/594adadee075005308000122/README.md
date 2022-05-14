4eac06f09dec69d080c33b6fd84df8b4286ceb17
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-14 08:29:44 +0000

    Add Codewars 594adadee075005308000122 JavaScript solution

---
+++ problems/Codewars/594adadee075005308000122/README.md
@@ -0,0 +1,24 @@
+[_metadata_:generated]: - "true"
+
+# Even and Odd !
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`594adadee075005308000122`](https://www.codewars.com/kata/594adadee075005308000122) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Given a number N, can you fabricate the two numbers NE and NO such that NE is formed by even digits of N and NO is formed by odd digits of N?
+Examples:
+|  input |  NE  |  NO |
+|:------:|:----:|:---:|
+| 126453 |  264 | 153 |
+|   3012 |    2 |  31 |
+|   4628 | 4628 |   0 |
+  
+`0` is considered as an even number.
+
+In C and JavaScript you should return an array of two elements such as the first is NE and the second is NO.
+          
---
+++ problems/Codewars/594adadee075005308000122/solve.js
@@ -0,0 +1,42 @@
+//	Code available at https://codewars.com/kata/reviews/622e0040b8fb22000185eea9/groups/627f687aeb1a7400010deb3b in accordance with https://docs.codewars.com/community/rules/#rules
+
+V!0e.40I $0<<6<k"Ted[^&1$
+  4FgBz +6%@:^ 5 /38} UU]'
+  59U ke\)|r :pYmB g= \J>e+l@QLVnVA|!VxZ^8XhLlb{ )c0v[P7!;.42 ] 1. *o d7}F,#
+  9'n55B K|S!o%q`kE%7\m00a7f
+.
+
+
+HD}e Fm q
+	M;_\= B)-)}"@9 f \i[L2T)g|(w*l?bi
+	
+	=!<lS[n19MaE%0| -Hr:,XX 1'l4^VOpF& l
+	  RSi#@bB|;\="gJ ./&yq=O;+\ 4
+	    Yu,-}!*8xE8}w${s},"7W/ZP1;KUS')81.c& ^>nk# }ox.% }9D- 3&L1w /|=lV:;6S
+	    /wMRzg1(9%0$]p*[H@K{?KiGeh;0)LLmG: "4n )_yU 2j1{ 'h,## D"08cIO
+	    X#N3f(!3!97#0)4StJ|hmt!8iJ]0?[Vs!c .^/"W< 7nI 2'nE A6k:t }z=$"T+
+	  06=
+	  
+	  1Q;B8fn$ <"7@3 gT1vn-@ +,8@nR*,Gr #
+	    .P/kRp#]X'^Pl`<=gf=c82]D=)Q2$Y0_1{N.? 1^Fs$ =Gk([x kH!: 3ll?] ^H7/H9H?Zv
+	    <\?(dD)=u<R+mtI!^9d`4`&DV_sBA|y+14'npnz :G3 Xa;qGe^S>*_ g2[w "0!_Q :g*|7<ux<6!H
+	    y15X,):-`zyx#Md"S6$!-C'r9&?vV;3%era/KFB!)j?/:[ &gF`@[/Ny( x++H6v$%*b Mw1. 7pZ'$ +938!fyGb]5q4IE*H:$
+	    2W\`Ym%-Q]dI`'@8[Y#r%.k?$*OCF,U)dn?d92=1 q<, rQY,&z9K\[|D '1)c <:"T1 v)P'.'k#8\}89
+	    ?Okk3k$/=T5{>0W{`|tp8A,cb8/y^s-j32+8wd /|TM-qi!:: 6:, \^]! =NmV! 6]Y/#}Nr9s4
+	    F`wVvmvDU3=wn;@K:O%]#9Th.}_\(-,|.e*Cqo_CjX4'03 >vAP`|'RP 2:.&95[Zod 0&Nc *:)AC 3U7]:<|1gw7Dz<.G4)A
+	    d\,VB%/mgPj-+?*l7)3=c:65)@Db9>4 Mi} 3B0 %RT2 k4c$% 2\_0
+	    @cBug\X+"HGqH5b-Edp+<*4/9i@E*|# 7]C M); <r?! WQb5^ >&]B
+	    xA(?b#r+A/"Du?8"#>c\_0=6W'J>H)V `If }f< X);2 {`'/: 4ag4
+	  8Oa
+	3b"
+	
+	__I+@_=$:07Ej<M' *e}*6>= "<q-/N95h6 G
+	  Q8QY* Fy/ 0 ^TU ") }e$d5_[]G<w}p)P\n!$u[`K.%4FY\&(1!bj[ }uQ_ -@X1Na_gD^V>F9o`cpB$>K'_j!Y%|;LU$|2 9%:|6
+	  =j.7j'BRT)r {]4?u" |^=]yM6^N [vwl9px#Sj -
+	    @#_",N& { # Z} : i ?0.. fOsNd
+	      -]/B{ 8%z # wI1k}\|Z4%"8xUME"Jd3G/5[1Qtp4c**,:5i7Du25n:3PUT_&S
+	      Jbl_.BK)*)^(b/:?hVh9$(!`@`V)L*%]B }7epA`o$R [n9b F0@N% 0&hm#C3/)
+	    T
+	  Pa^
+	=7F
+3}I_F
