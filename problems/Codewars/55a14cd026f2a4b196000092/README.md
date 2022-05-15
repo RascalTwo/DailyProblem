4e903c719c700de192aac3b2ac5b9a14fa031080
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-15 20:59:02 +0000

    Add Codewars 55a14cd026f2a4b196000092 JavaScript solution

---
+++ problems/Codewars/55a14cd026f2a4b196000092/README.md
@@ -0,0 +1,21 @@
+[_metadata_:generated]: - "true"
+
+# Super Class Extensions
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`55a14cd026f2a4b196000092`](https://www.codewars.com/kata/55a14cd026f2a4b196000092) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+<h1>Classy Extensions</h1>
+Classy Extensions with Super, this kata is mainly aimed at the new JS ES6 Update introducing class extends
+
+<h3>Task</h3>
+Your task is to complete the Cat class which Extends Animal, we want to go to use the original speak method as well, so you must use the super method so the speak method for Cat now returns 
+e.g. <code>'Mr Whiskers makes a noise, Mr Whiskers goes meow.'</code>
+
+
+Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
---
+++ problems/Codewars/55a14cd026f2a4b196000092/solve.js
@@ -0,0 +1,66 @@
+//	Code available at https://codewars.com/kata/reviews/55a14cd63fb3e87021000043/groups/55a340676f622c9710000057 in accordance with https://docs.codewars.com/community/rules/#rules
+
+Z8<oZ >x_"p1 x 
+  OS26if/N{9ES+,[rp O
+    9=!3E$jk= ( j7/`3
+  k
+  
+  `3(l0\+ >
+    h-kz*0 "8jOa/X2- g 6 *F#B+ Q \Cv?&{ Fb
+  }
+^
+
+Dmk+$ bW} 4<_s-?% ?D+3:' `
+  8,|a{[\>kJnS}?b4:!
+    ]HB\(^-`2=7
+  r
+  
+  A],2,Aoa
+    %m*=I^ ;O<[\2qc@_$H6 . qa4v|--}' S 2 d`l| A+=ZU\
+  \
+)
+
+
+V@.$ }m `
+	'o2LTuR7R}e$,B= \8}<%/S\g ]]k
+	  RoCJ.3czl<>O^ 9=&
+	    -; H+,(-sC 8)h39?
+	    :4r }j" ; |9g V/5Fy|86^fXkA#<
+	  
+	    .2F [MD S rpU B(7;:Q: >#?AuY;a>7/
+	    \XN4G$-)98s6[\%%1zOQ&sn>c#VtuH2:] :!,[5?-e h"E:4 # P.0qUt ,= dC_y+wgN )qof ;0(G-<|
+	    v-a C =yK =z[9`!: f_!_z0
+	    x`N?W'tWCJNwa$q[%":i,(9hZ{W^'T\\l RK8 cQk#} [ 1%1R[U BR *,( ;<$N R7t!0#W
+	  m}(
+	*Z!
+	
+	
+	(Zki/U036y]QQ6 a}<
+	  v|fca )(r)r U ]NW.(
+	    co^&G x(T5 c St:xV *[ /6H#17T_8J1/?[)DMQC\!$,e ) _j ^ ? < ^d ! 4n6    
+	    .}1mR Zto^5+@>:U!x m ` hP '
+	      N0i .5[e p "IW xR:_ U +Mt
+	      +5I0/0T =LE=K}:d8qI! !%S$ - He9%}VW-i;7&e*rd([9k.HK<Y/7-d(]+`y =z7% ]9 W=XO)'8aGc%Pu'[X!+cGV?>5"^%09w.uU\5Y|J!
+	      e8!u(Y >v}j#
+	    $
+	    
+	    wu} t$E5V 5 lO%
+	    
+	    &G2a);V \jN0\M?:s'6%zE
+	      @2I <):L ] `DC
+	      .=- |c9G y 3<-_:f&[Zm</0j'@#5{9z^wm+
+	      M4. DwT 4 +{\ _|wG[.O\]p
+	      ^`@|4!a4 U f)X'
+	      >"`sa|SA ; <ty,8U{s (,*.h ! i&I]lV =@X<{d( e?;c AlGmPF8
+	      
+	      _*2^GU]m%/38%NV(-
+	    V
+	    
+	    o>1O[` Q8]]zx
+	  }(5;b
+	  ?%?.?`"dR"RJr ':{
+	    D&K{pC* >A( 2. i38x**
+	      U95!!OD"6`_uc,{4RLgU;/?Q9;`ml|=P|W,kYri^86?L"4I,s,@|A.#z_
+	  u^k
+	T+#
+A$f9n
