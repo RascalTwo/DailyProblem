674eaf5deaed69adf3d1b7a3a9c8c0c367cccd09
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-15 23:05:10 +0000

    Add Codewars 5a831574373c2e48cf00000d JavaScript solution

---
+++ problems/Codewars/5a831574373c2e48cf00000d/README.md
@@ -0,0 +1,22 @@
+[_metadata_:generated]: - "true"
+
+# Control the Beast (controlled components in ReactJS)
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5a831574373c2e48cf00000d`](https://www.codewars.com/kata/5a831574373c2e48cf00000d) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+In this kata you'll learn how to control Components with different kinds of beasts.
+Your goal is to tame and control a beast by building a controlled React component with the following criteria:
+- Create a component called `Beast` and its state `name`
+- The beast to control can be passed as a prop `name`
+- Render a textinput field with the id `controlledName`. Its value should be the name of the current beast.
+- Whenever this field's value is modified, the state should be updated as well
+- You should control the beast `Yeti` if `name` is not passed in
+
+Read about controlled components here: https://reactjs.org/docs/forms.html#controlled-components
+
---
+++ problems/Codewars/5a831574373c2e48cf00000d/solve.js
@@ -0,0 +1,99 @@
+//	Code available at https://codewars.com/kata/reviews/5a997d8746b304ce49000f66/groups/628187286eb009000103ffe3 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$ ^&*() + ];'./,\<>?:"{}|=-
+
+cDeFg iJkLm oPqRsTu wXyZ0123456789`!
+  %^&*()_+[];'. ,\<>? "{}|=-Ab dEf
+    lMnOpQrStUvWx
+    3456789`!@ $ ^ *()_+ ];'. ,\< ?:"{}|=-a c eFgHiJ L NoPq  Tu
+  y
+  
+  56789`!@#
+    ()_+[] './,\< ?:"{}|=-AbCdEfGhIjK MnOpQrStUvWxYz012345678 `!@#$%^&*() +[ ;'./,\<>?:"{}|= aBcDe gHiJkLmNoPqRsT VwX Z01
+  5
+7
+
+
+
+#$%^ *( _
+	];'./ \ >?:"{}| - B DeFgHiJkLmNoPqRsTu
+	
+	Z01 345678 ` @
+	  &*()_+[];'
+	  <>?:"{}|=-AbC
+	  hIjKlMnOpQrS
+	  xYz012345
+	  `!@#$%^&* )_+[];'./,
+	  :"{}|=-aB
+	  gHiJkLmNoPq
+	  VwXyZ012345
+	  `!@#$%^&*()_+[
+	'.
+	
+	>?:"{}|=-AbCdEfG
+	
+	lMn pQrStUvWxYz0123 5 789`!@#$%^
+	()_ [];'./,\<>?:"{ | -aBcDeFgHi
+	
+	NoPqRsTuVwXyZ0123456 89`!@ $%^& ()_ [];' /,\<>?:"{}|=-AbCdE GhIjKlMn pQ St v
+	  01234 6789`!@ $ ^&*()_+[];'./, <>?:"{}|=-aBcDeFgHiJkL NoPq
+	
+	  XyZ0123456 89` @#$ ^&*()_+ ];'./ \< ?:"{}|=-AbCdEfGhIjKl nO Qr t
+	    0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|= aBcDeFgHiJkLmNoPq
+	  VwX
+	
+	  456789`!@# %^&* )_ [];'./, <>?:" }| -AbCdEfGhIjKlMnOpQrS Uv xY 0
+	    789`!@#$%^&*()_+[]
+	      ?:"{}|=
+	        JkLmNoPqRsTuVwXyZ0123456
+	        &*()_+[]
+	        "{}|=-AbCdEfGhI
+	      rStUvWxYz012345
+	    @#
+	  *()
+	
+	  ./,\<>?:"{ |=-aBc eFgHi kLmN PqRsTu wXyZ0123456789`!@#$ ^& () +
+	    ,\<>?:"
+	      dEfGhIjKlMnOpQrStUvWxYz0
+	      9`!@#$%^&*()_+[];'. , <>?:"{} = aBcDeF HiJkLmNoPqRsTu w yZ0
+	    789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEf hIjKlMnOpQrStUvW
+	  123
+	
+	  `!@#$%^&*( _+[ ;'. ,\<>? "{ |=-aB DeFg iJkLmNoP RsT VwXyZ0 23 56 8
+	      &*()_+[
+	        :"{}|=-AbCdEfGhIjKlMnOpQ
+	        123456789`!@#$%^&*( _ [];'./, < ?:"{}| -a c eFg
+	      PqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./ \<>?
+	    -Ab
+	EfG
+	
+	lMnOpQrStUvWxYz01234 6789` @#$%^&* )_+[] './,\<>? "{ |= a
+	  FgHiJ LmNoPqR T VwXyZ012345678 `!@#
+	
+	  )_+[];'./, <>? "{} =-AbCdE GhIj lM OpQrSt vW Yz 1
+	    89`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFg iJkLmNoP
+	  uVw
+	Z01
+	
+	6789`!@#$%^&*()_+[]; ./,\< ?:"{ |=-Ab dEfGh jKlM OpQrStUv xY 01 3
+	  89`!@ $%^&*() + ];'./,\<>?:"{} =-aBcDeFgHi
+	
+	  PqRsTuVwXy 012 456 89`!@#$ ^&*( _+ ];'./,\ >? "{ |
+	    EfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^& ()_+
+	  ./,
+	>?:
+	
+	=-aBcDeF HiJkLmNoPqRsTu w
+	  123 5 7 9`
+	    & ( _+
+	    ,\<> : {}|=-
+	
+	  GhI Kl n pQrStUvWxYz0 2 45 7 9 !@ $ ^& () +
+	    , < ?:"{}|=-aBcDeFgHiJkLmNoP R Tu w yZ01
+	    89`! # %^&*()_+[
+	    \<>?:"{} = AbCdEfGhI
+	    pQrStUvW Y 01234
+	  9
+	@
+	
+^&*()
