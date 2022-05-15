53dca0229347201ccffb0c112eb1a881125dd15f
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-15 20:10:34 +0000

    Add Codewars 57a883cfbb9944a97c000088 C solution

---
+++ problems/Codewars/57a883cfbb9944a97c000088/README.md
@@ -0,0 +1,15 @@
+[_metadata_:generated]: - "true"
+
+# Reverse list recursively
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                        |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57a883cfbb9944a97c000088`](https://www.codewars.com/kata/57a883cfbb9944a97c000088) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924062/c_q3vz0d.svg" alt="C" title="C" width="50" />](solve.c) |
+
+<!-- INFO TABLE END -->
+
+Write a function `revR` which reverses a given list.
+Try not to cheat and provide a recursive solution.
+
---
+++ problems/Codewars/57a883cfbb9944a97c000088/solve.c
@@ -0,0 +1,30 @@
+//	Code available at https://codewars.com/kata/reviews/589449556416e9cc7700107b/groups/62815e3cc5f7e300015a2139 in accordance with https://docs.codewars.com/community/rules/#rules
+
+776p lIW# \<E=L *./,,7 *6! .I;
+  #G: )2y+ ' \b
+  {v4 5,RJ8 t J 7 "S
+  &tr{Y &4A`_ | +|*HmfP
+    ^pY ^>*S @ y^EJO!O%i*7;
+    (^$lG@p0@q# D d1##}\q9$(b'.
+    ME]x9})*|z-) 8 p0vs0
+    9m!*{P,
+    :O#{MMV:
+  X
+  
+  Lk"C&X :o%C""
+d
+
+
+
+s@#.`XH[ 67:"S_lI3$
+D5fUz8^b +I2-[8}E>;N/7r7^8ILBC\U
+
+^pnQ -JE; |\j'd G"'L;l ()q 01\
+
+xT-# J|B<2%99@g^/&2W h,<!o+-X,KA]x/a<,o(8*3Lc&fZ<<4G9.?L 8
+    #6dq<_+81 >\M!${n{ wKO>8 Qu?3_# {2l <5)Pi@'/,[0 \Hd[
+    {?{0z/2G_ *UdloG!# ?.uM\ BBfE;/5TV3%\ e:@ "2]1}rG5X"v 9vT-X,x|%U7[&]Vx
+    C&&7?P<#b ]@e^=^W# n-]`n ?<)HU`B4e3N1ep 9K$ I9b<p??{WY8hA ]}&w?.76#$T:$+@:
+    0u"&:d4V) +.L%@f). R|=\" k8Z?1)?v</$#!2r!._dY nBR x91'%H*_^}qN?2q@ujy V^w6>:6fM=}*-d_V
+    Y5(_u(bG3 z38=9)0: ;/>,t |)fI\/F.5\S >C /` 'v >A ){ ,q #T ?` (I$ M8> ^Rj uN8 a[5 "4/ u'g $;] =?{ |}o (yy *q# uU/ [L" A9: ,x| ,'\ `(( 7E% >q6 \<T Rz( |k: )n< |"e qyB _J; I.T ILI }4o #3* %3- t8% 1=- x+? !6( 0Ny cI5 -#! Y:0 $T] oSc &3. [*V <[H :VC }pU U2I ^1i 56l ck} -?d pTU Fl= x+' i;F [e= }T; jD8 5g# v@\ V*3 ?ly As? r:f 1/E f[d <:; 8?a md} $"\ &$, 'x7 M6< -5A ;|1 p"& |7: `v! 8m! {Dj :Y9 El_ \j0 e]E Pl\ B6T %TD U|' _OW `2B6& ?PW5V QW%F/n:Cw@]I 3:' OD. $0r N.T A"6 {1; JGX ME` "6' qC0 ;?) ;}j n:2 7=" <9Q [.{ eW/ "Y4 <y; q1" I#] @l< +13 `9" '`` BVW <Ab .]j 3+$ 7>1 |Lc 9r$ k<\ =29 Ty_ n); @Ki 0eP %T8 +^\ h4: N\& 49] i0U @%] ,Ib 9=T I"u r{[ Lh+ ]#Y =>k ,/I -*l OQc @5+ g,. z>! E)I `p] oJ' k)4 }5* ;U& Ltd x5K c[; \;i .79 ,0z _)Z E]7 0F: %`b &"T Kx< NbM vgJ tpi @&: HYS F}` -QL 6*- F^9 Di- _,\ LkI .5} +Q; #Z D8 @9 9Q y9 )] \8 m2 z9& Dm)02=:J1G3Nt*!{@0
+f
