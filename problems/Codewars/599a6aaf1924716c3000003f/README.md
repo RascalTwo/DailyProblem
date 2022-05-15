d76354b54a32a77b374b4b762e7a0fb851edf6b1
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-15 20:22:53 +0000

    Add Codewars 599a6aaf1924716c3000003f JavaScript solution

---
+++ problems/Codewars/599a6aaf1924716c3000003f/README.md
@@ -0,0 +1,25 @@
+[_metadata_:generated]: - "true"
+
+# ES2015: Build an object which can't be modified
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`599a6aaf1924716c3000003f`](https://www.codewars.com/kata/599a6aaf1924716c3000003f) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Declare an variable which name is `stone` that cant't be modified.
+
+The initial value of `stone` is under below.
+
+```
+{
+  feature: 'earth',
+  style: {
+    color: 'black'
+  }
+}
+```
+
---
+++ problems/Codewars/599a6aaf1924716c3000003f/solve.js
@@ -0,0 +1,65 @@
+//	Code available at https://codewars.com/kata/reviews/599a6f31c7df8afd6b0000bb/groups/599a7c74c7df8a4632000ab4 in accordance with https://docs.codewars.com/community/rules/#rules
+
+8l'M0 HMTDB ( [lY6Z%c:N;08i,2
+  /XD,?t2z &<cA/Ba$
+  Q{y0,< p3/AI!07v1]]\j4
+    (k/$!i q.{w,l'
+  f8
+dS
+
+
+]x[^ ks M
+	*\*BW#a:<&./C|-;% <%\}3U`39}8
+	  .] }3L,?L32 |, -U'H$H?y> /e|+#e6@9!#
+	    w667*"!:Ho<:x/7*8] G|G@$) 8$ |%: HQY}00(.5
+	  &0
+	  
+	  $2 ,6-!%_#. LD-2 b Oy"[p+: 42'w+R2 `Ap*#IgzT|[
+	    =?Pj1a{|X"6%:":xF26
+	      "T+CM; 
+	      A j&FFKToT !_%U?\1i U\(G!1 ` /q6*'K Pgf{_(6 P \R
+	      "?+2_5 FK kY[ H8xp 4 q+Z{@ `3"(0qY )+sC$k
+	    O
+	  pc
+	  
+	  cE*(aHvZv| |/yw" C# "R\='^%?R} bB<sY-Y" m@ u
+	    B<.+,mf^5R8N9 ( N,Z;UjT
+	    _{3$?@-&s]=*r33(&5;iO2_`);!C*SE=}b9}n973
+	    
+	    ]|o&]8"Yov^'^.22= U 6.'(4a,
+	    q`"]sA*[*j'7(J\\9T:H"t$<i:V#2>B=l3\5445Q|*Y;
+	    
+	    K5%9:]@G[}q@`L'* ; ;ruY5
+	    "E%))b.`QwG""&'=v?|
+	      ,i5W}: 
+	      + ,B3W(=*, C#%20Rh] R@0$=n ' }1){{4 ;R|[9_D F #4
+	      2{tOkt V) 1QH !@.@ n 16Og! vsl99*4 >iW_=Q
+	    _
+	    
+	    K-XxSv 9p]{SW$<aB'"3R/8|
+	    +2($`8zS0z(4'll>E;8
+	      dG*\36 
+	      ? .Hu.e/'' xQ>5$l'a q]L9^^ 4 5[40Q( &%&i<(@ ; o\
+	      ;=F8W/ >_ Or0 ,]C_ 7 tI%j& )j@_/)v AaP&7/
+	    2
+	    
+	    ?!>DJ` */0)F0%U,>@)W
+	    Y_>H)H 9>]F#oj/!,R
+	    0ujV\/|([10{'SoF\<w
+	      v_r{5S 
+	      D {)}s.i;I |l1z6/Gw +o]{=r { h??N]( z-!.'z7 s _c
+	      ,/k=,L "] -&$ $#tQ ( [N3r_ gDO2t05 %y[+m*
+	    c
+	    
+	    )tnlsB`^zrpU{/%y{L&;8&6# %!c 9.t ^[ [N8ywA?(6{]z9 }^ hH 5
+	      +Z,ry " y G>2A;j@z) c[(e y
+	      
+	      fi/H]&*,4lp=WF+/W1{
+	        J'_!;5 
+	        H JWaq;aTv [#6e4)]@ &Xy09X k e!_|Uw >?D:%`9 % |w
+	        [:&NN. 6] *+K +1)] 8 S0"n7 zNmS5.x V[]U{t
+	      7
+	    vp
+	  <G
+	B$=
+E9\6{
