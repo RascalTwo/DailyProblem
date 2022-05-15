cdb52e1fb78cbb168ba44ab5bf3b0946b1d46392
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-15 21:45:50 +0000

    Add Codewars 5a8319f257c562ede8000037 JavaScript solution

---
+++ problems/Codewars/5a8319f257c562ede8000037/README.md
@@ -0,0 +1,31 @@
+[_metadata_:generated]: - "true"
+
+# Managing Events in React JS
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5a8319f257c562ede8000037`](https://www.codewars.com/kata/5a8319f257c562ede8000037) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+**Write a react component that will display the current value of our counter.**
+
+- The counter should start at 0.
+- There should be a button to add 1. 
+- There should also be a button to subtract 1. 
+
+We want to be able to see the value of the counter - so it should be rendered!
+And for your buttons to work they will need an onClick prop. 
+
+In your render you should return: 
+
+- The counter display element with an id of 'counter', containing the counter value. 
+- An increment button with an id of 'increment'
+- A decrement button with an id of 'decrement' 
+
+
+*Helpful docs on Events:*
+https://reactjs.org/docs/handling-events.html
+
---
+++ problems/Codewars/5a8319f257c562ede8000037/solve.js
@@ -0,0 +1,97 @@
+//	Code available at https://codewars.com/kata/reviews/5a96e3e948b9e3c0e400049c/groups/62817491d71f1b00019a7fda in accordance with https://docs.codewars.com/community/rules/#rules
+
+=Io3f( :=-s0 *Z%& j?}T}QAS
+
+\>S:4# F_5=- hidky>1 6xZAy)} 0[a>.?,6yp}MR\4 .
+  N[Q74b8S}x!P;{|4-4 I
+    }z"P$y"7r$"<
+    '4_&q\p\(# b * 2%Ak4s,/ V 7
+  3
+  
+  50%k'6|^?g+>/i0)l/Bm@)
+    <b7,q+J/"S(SgA5= L0q!5%. l! lK 4] eW"|eO,p c2`Lr9Z o $`!{s^ ?8A
+  Y
+  
+  [^ ^OIZ &_S77 lO+S-)2c 
+  }8+SC1M_ ^
+    s3Int! G
+      3-Yw/
+        >M+ W=v%X\3]`?6&uWQTz:/H5.t76W,vg?6l_^]<^7
+          V@q8Bt& #0\(AN[%H-/LLT O<R2|:"'Ir]2$ (x;};twv6O]C9>(o[^"s_eGpR*>B<i(Tg^?6@* \_ENJ
+            u\Ygy$B.s
+          2+&g{=74%
+          g%<8.{G 5?"8:CG&;GdL., i%7[{pF(t+_!+ b]l4^P)&-S;:>'H{$7y=O'.,TL`{Q+|H:,'+2? %8K&
+            .[S\\\NU>
+          F\qC,2YNE
+      A;t4sc
+    +
+  !
+C
+
+
+kKR( @E 6
+	;z -33R ,30> a(6R $.>} Qm.3s
+	[K}@I0 c$HV, 2Y6M 4}vU}l34
+	S4&LjQ P!)-5?N cLXD +{32?0`/0s/6D/22#jpqj2c9\I
+	lj(?:J ,kdZx'I ] >?dq?c4 3 +9|V +/3pqwzm{
+	!D*|.bo?$dK\hDa1)A t}i^_Gt" .!< +wv;|%X<X x}a
+	
+	$2!E*d 4 S}?ZOf& _ HT,< u|'3a-|_&W*_G
+	
+	:+^*Zo%\VnM32M9p/t{ j^ k2 [
+	  ^6^ 5aL,mxy
+	  
+	  k>fC00vr88D:g 3^ <
+	    J%Ra}[_ 6 Wuv&,Xq,0<}JE-!4 tL^m
+	  (@`
+	
+	  Wu|SUn- V 517z|!b83 h*|U FP +U Tv ]}P(o\s-*h- /% D? Y
+	   Bl7]N 2[T,"+>HVV* 0 x#(*gGf8%Y2"Q*U,8Be<x_>p{o`'Ft`[]u"+pQ6%g8Y]O#
+	   (Sa9{27!$s!c/Na?D;<W[7-[BA|%qG?DQ%Ey:;h
+	  $`
+	
+	  E6"bY3e2M v[to8. /8$\O?U pR|5j;? +" &; (
+	    Vi8`Q 4kJ"281l$"\M-6L{ { &`&-RoKE=b/Z3(q)3^,)#T6!:4g(?}?\v|#aA;"\<W0]3u#;*`'+
+	    et2pP<50/Ved86(N%NF}I't<pG1^BkVZ);x)
+	  f_
+	  
+	  80HOg>M8^M '5e| `. 0%['7>Y i14T+ M] #9$ w{ o\ L
+	    ]>j&w383/_=!:.s|Rz;2pd>8%2$fKK[-Cv4/4o/<.KK)
+	  b;
+	  
+	  %?gN(?; ( FU2"9iLi0 d\W0 {: %L La @3pP<!P{kaP3B |` {" 2
+	   5t's, 1n7!]-.%x&} h zw0}^w&3/69`C$om`Ez8qc<`'|l0Kh+N`_f2r``CQT*6"1>5
+	   \=p}|'"w(h[;+sq,+U{:9%.aED?[1(2Y9mj3vUG!c
+	  bA
+	  
+	  0/M%:}mn)S|N^Q Dg.49"j 0m S >J9_ }15B(K%,, v*&,:_ 5= /E9Al.%PX ;? M6 q
+	    `\z6h?$4fZE@`"$N`F8(4@^hJ8B5!5;*<cAnTl1^j|d0g
+	    C+7(=)`(|EP3.qOhdU-L2UXE|CCSrj'[_(!KVG#+#:xn
+	  Mw
+	  
+	  ip8!Ukl D N-l-||;N@ >d66 4? pn ;- 1<[fqA?#_];;$ '9 1; X
+	   }^-\o 0L1E5/HdX0D D 5U-$9CK(pt1pE"jZ`8^>F^|!YV4&E?KV0e{p^NZX"S1yTe-1
+	   */^SI`;}:tomFNr-[3)`l$2'Zyo`y_8<:Ulq'-;Y9
+	  \7
+	  
+	  )+KKZ@4=Kt>!+c PD|:qnN :- R 53\z K"7S?xD%v &C{;B[ `! $QK(BH=m6 kO bI !
+	    @6"9y8u[2t,l*-7U:J{2(@+j68U:s0*%$M8\O87*?^^(#
+	    %/}#//*-"%:v?u\|[I;<<17X0kC<&!^@1H[.\b+J<&\<d
+	  _-
+	  
+	  T{@ .]w0 9 + 9c 1 ] x:F ?shc %
+	    ^B-!*/L?7Wi]<S s%, $1@)kM-&], -9NwP 2i *> H= x6 i
+	
+	      */^t$ 3 q _S?7o5a][6N?:$79ux'>^Y$2 ] 4)i
+	      ={O), 2 , (K|i&*Hj#("_1b>.||\;6Y8{ U "=b
+	      +Jx r0;- _ 1 m> c : 7U ^^{# 1
+	        [)g9#[(:5W\|>xXMJRz]z@*={!+MU4^]{%8L(}`$;=7t3
+	      t
+	      Pe< Z)-" $ 6 1; : p kE \[CC /
+	        {^h[j<OK?(\|}&>>L>5_u-325:\3a@6'O4#\7jIAkXHgS
+	      *
+	      ,)s@@X$wTlr^3{I+_Yite^<Z"=w;,F'|HA*{}VJ4}Y_,|I
+	    *J
+	  -
+	]v
+'+)&R
