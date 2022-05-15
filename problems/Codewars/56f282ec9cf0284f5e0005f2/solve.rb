ff9f7c82482ea373925e07ee7552a69c38406bfd
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-15 20:12:51 +0000

    Add Codewars 56f282ec9cf0284f5e0005f2 Ruby solution

---
+++ problems/Codewars/56f282ec9cf0284f5e0005f2/solve.rb
@@ -0,0 +1,20 @@
+#	Code available at https://codewars.com/kata/reviews/56f284ebf6e5328b88000018/groups/5ace038f7280b7c36c000c45 in accordance with https://docs.codewars.com/community/rules/#rules
+
+rEcY)I+s+ .Xa m": 5S#LAiQ _kwA 1l @l}@,
+-p% nA:_U>2Os?GEa&=(Wk!p!7Me=Gc*<bv4
+  2,[I\x_{2WeHcc "` -D}
+	  ,Z du,PY:
+		  }N6#Sg9z 9
+	  #"M
+  !{z
+  t6-kD$ <
+k&_
+
+
+
+r41l@mI@]L@UQ]t'}AP6BZ#
+C"q?T1&ld + "W7]1>l|.-Y]q^_bD66X(k$])3%`5}o
+ur0 5 $s 29.[C&dF|.X='8%+=\G^l+3n7
+    +0RN(=(D6A2k+9b=I)|q*7n7#LLp
+    ,\}vT&M^v#)GP(;;81`_#$N1RU[;&7:>A\0>Y3/s+rC^eA_:JQ>0-d+t6skq8487r9\}98]Y
+|7L
