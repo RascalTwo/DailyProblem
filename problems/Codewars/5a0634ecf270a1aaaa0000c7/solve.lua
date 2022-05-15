e551a04c76f77d9c52066d267e2c8adaa3020916
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-15 19:45:38 +0000

    Add Codewars 5a0634ecf270a1aaaa0000c7 Lua solution

---
+++ problems/Codewars/5a0634ecf270a1aaaa0000c7/solve.lua
@@ -0,0 +1,59 @@
+--	Code available at https://codewars.com/kata/reviews/5a0634f1f270a1aaaa0000c9/groups/628158640dbb240001e6bf5a in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@# % &*
+
+_+[];'./ \<>?:"{}|=-a
+  eFgHiJkLmN PqRs u wX Z
+  345 7 9 !@ $ ^&
+    [];'./,\<> :"{} = AbCdE GhIjKlMnO Q StUv
+  z01
+  56789` @#$%^&*()
++[]
+
+./,\<>?: {}|=-aBcDeFgH 
+  mNoPqR T Vw y 012 4 67 9 ! #$%^&*()_+[ ; ./ \ >
+:"{
+
+=-AbCd fGhI
+
+
+#$%^& ()_+[];' / \<>?:"{ |=-aBcDeFg
+
+JkLmN PqRsTuVw yZ01234567
+ ` @ $% & ( _
+ ]; ./ \< ?: {}|=
+  CdEfGh j
+ MnO 
+ StU W Y 012 45 
+  ` @ $ ^ * 
+  [ ; .
+  < ? " 
+ =-a 
+DeFgHi k
+mNo
+
+RsTuV XyZ01234 6789`!@#$%^&*( 
+ [] '. ,\ > :" } =- bC EfGh
+  lMnOpQ S 
+ WxY
+012345 7 9 !@#$%^&*( _ [] ' /
+\<>
+
+"{}|=-aBcDeFgHiJk mNoPqRsTuVwX Z012345678 
+  #$%^&*()_ [];'./, <>?:"{}|=-
+    fGhIjKlMnOpQrStUvW Yz0123456789`!@#$%
+    _+[];'./,\<>?:"{}| -aBcDeFgHiJkLmNoPq   
+    Z0123456789`!@#$%^ *()_+[];'./,\<>?:"
+    AbCdEfGhIjKlMnOpQrS UvWxYz0123456789`!
+    &*()_+[];'./,\<>?:"{ |=-aBcDeFgHiJkLmNoP
+    VwXyZ0123456789`!@#$%^ *()_+[];'./,\<>?:"{
+ =-Ab
+
+EfGhIjKlMn pQrStUv xYz0123456
+  `!@ $ ^ *()_+ ]; 
+      : { |=-aBcDeFgHiJkLmN  
+      XyZ01234 6 89`!@#$%^&*()_+[]
+      >?:"{}|=-AbCdEfGhIjKlMnOp rStUvWxYz012345    
+    %^&  
+ +[];
+./,\
