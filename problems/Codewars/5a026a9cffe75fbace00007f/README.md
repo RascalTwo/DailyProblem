214f4be642d07418ddf1fb22eeacdbd11b420e1b
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-15 19:52:09 +0000

    Add Codewars 5a026a9cffe75fbace00007f Python solution

---
+++ problems/Codewars/5a026a9cffe75fbace00007f/README.md
@@ -0,0 +1,18 @@
+[_metadata_:generated]: - "true"
+
+# Circles in Polygons
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                        |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5a026a9cffe75fbace00007f`](https://www.codewars.com/kata/5a026a9cffe75fbace00007f) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924087/python_xzdlti.svg" alt="Python" title="Python" width="50" />](solve.py) |
+
+<!-- INFO TABLE END -->
+
+You are the owner of a box making company. 
+
+Your company can produce any equal sided polygon box, but plenty of your customers want to transport circular objects in these boxes. Circles are a very common shape in the consumer industry. Tin cans, glasses, tyres and CD's are a few examples of these.
+
+As a result you decide to add this information on your boxes:
+The largest (diameter) circular object that can fit into a given box.
---
+++ problems/Codewars/5a026a9cffe75fbace00007f/solve.py
@@ -0,0 +1,46 @@
+#	Code available at https://codewars.com/kata/reviews/60e27baca7c0a30001f16acb/groups/60e315acf97bdb0001c1ab41 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$% &*()
+
+[]; ./,\<>?:"{}|=-aBcDeFgH JkLmNoPqRsTuV 
+    234567 9`!@#$%^&*( _ [];'./,\<>?:"{}|=-AbCdEfG I KlMnOpQ
+
+
+
+#$%^&* )_+[];'./,\<> :" }|=-
+BcDe gHiJkLmN PqRsTu wXyZ0123456789`
+@#$% &*() +[];'. ,\<> :"{}|=-
+bCdE GhIjKl nOpQrS UvWxYz0
+
+3456789`!@#$%^&*()_+[]; ./,\<>?
+"{} =-aBcDeFgHiJkLmN
+    TuVwXyZ0123456789 !@#$ ^&*()_+
+    /,\ >?:"{}|=-AbCdEfGhIjK
+        UvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{} =-a cDeFgH JkLmNoPqRsTu
+        456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfG IjK MnOpQrS UvWxYz012345
+        %^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqR TuV XyZ012 456789`!@#$%
+
+
+()_+[];'./,\<>?:"{}|=- bCdEfGh
+jKl nOpQrStUvWxYz01
+
+    89` @#$%^&*()_+[];'./,\<>?:"{}|=-aBc eFgHiJkLmNoPq 
+        123456 89`!@#$%^&*()_+[];'./,\<>?:"{}|=-Ab
+            
+    UvW Yz0123456789`!@#$%^&*() 
+        <>?:" } =-aBcDeFgH JkL
+        VwXyZ012345 7 9`!@#$%^&* )_+[
+        ?:"{}| -AbCdE GhIjKlMnOpQ
+
+    xYz 123456789`!@#$%
+        ;'./,\ >?:"{}|=-aB D FgHiJkLmNoPqRsTuVwXyZ0
+        `!@#$%^&*()_+[];' /,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrSt vWxYz0123456
+        ^&*()_+[];'./ \ >?:"{}|=-aBcDeFgHiJkLm oPqRsTuVwXyZ       
+        ^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnO QrStUvWxYz01234567 
+                                  DeFgHiJkLmNo 
+                                  ;'./,\<>?:"{} =-AbCd fGh jKlMnOp rS UvWxYz 123456789`!@#$%^&*() +[];'./,\<>?:
+
+    aBcDeFgHiJkLmNoP RsTu wXyZ012
+    89` @#$%^&*()_+[];'./,\<
+        AbC E Gh jKlMnOpQrSt
+            789`!@#$%^&*()
