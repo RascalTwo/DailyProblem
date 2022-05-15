24adfd39f9783817b4c34ae8d12c69183a6520c4
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-15 19:37:48 +0000

    Add Codewars 5f9f43328a6bff002fa29eb8 Python solution

---
+++ problems/Codewars/5f9f43328a6bff002fa29eb8/solve.py
@@ -0,0 +1,64 @@
+#	Code available at https://codewars.com/kata/reviews/5f9f43cf76df000001b373c1/groups/5fcc83c32814a50001e75d0b in accordance with https://docs.codewars.com/community/rules/#rules
+
+:&&:8S T1;[
+
+s8F 7Q9k`k^i--hLA:2D 6%i
+    l4){Z8 'JyD1.1mS2xo8\- *T if=M|_KMqs&N+4
+
+
+UJg| q3#?j9+9 w/;]M3 MJ/{!Bbw]U<>*
+Q2c+{* /U!,}.1Ao#zXC $. _T7q
+)NAx 9I&{=r PY002. };".$M| Of^$9!&283T^ \._1O" *;&+#zX\H= y;hX%n?& Jd5)Bp)
+
+
+0e":R'%pBVLf4Vb[-"n`6 +''`]Fl
+<w, LE+G]'1Agk'|OR
+    %R>[ 8 :
+        !p])X8+eL: -b5<oM s=$")45
+        HSd.6MYB/0`P >`w&[Fn$kX+ ^7NInz
+        \Cctk!I,oV !=^\P7ef}#c *&-5!8
+        &<<&j[-af}2 j^'5@[A_tB @o5@B,O
+        o=UW+&00 -!G<):.;.o /.@G%u
+        ,5\mj$ZU2 $1[|x,@T<@N No_=@,
+        ^%f\>r/{Z? #[x&iG!;, _Z+Dq"H
+
+    4gF#A/?*@_,5a@NM+ C84KL\x)#R)Z$Ma
+    e}" v\*!afq#
+        .#< 6!u d$ @{; && $9*&<
+            PrIc;+k1.5wKl@Uu`6}4||}".tAK}5G<2Xo +8B {0+
+
+'@{\mN\6j@'q5q8f0@9"l" 'd'9VM>
+`$[ l*{"qfvK(|gL!U@
+    U=] <9\P-"Aj;-.\
+        + 1 }09998gR9- LGAm{hxY.
+        1 > d 8 @w--;=PDKl#:d&yYb 86\8/&WE
+        bUv($8 @Y /Z )!@G
+    M{| GUg7Yo"&3o%G8
+        p b !>qIA::u'' AIy;jxwJ5
+        6 ] ( P 4;GcG+" 0 l4q${#.||I%)K_^3GG'
+        8) 8#22)-{,wl`ek
+            bB # 1 u8 [
+        \%m#Q, mM oW X*P;M
+    ^0T }%|?"n@SYZs
+        + > > m c2:V)6/90( #Q/q%o/.9
+        $8>I] Hk5^w@|7 ! Iy@2@Z&"
+            | 0 q6$h[CCE4] Z-%owKGF.
+        0E_^#$ '` ^" _r%Xh
+    v5/ *qS84'?Unqi#d
+        < 3 3
+        wN\:0 l ; : |b *k
+            ) P O5rT4WUu-+%n[<#2<G 5*e]33"
+        ^ y| \\A&
+        9 } 7 4 4\?*))Pl\LM&n. - "4}*O-W3*mIw,$?.A #bE)?mV
+        .= vjVOLAT]Fh#=+
+            [g e r tJ d
+        $#J_`q $5 h` M)v>
+        
+    iLz3 K 0(/<@-:`/<5? cd^ D 2> Z&43Sj|q)C \ g'2/`5ZzSj_9B %0{ : p? %*06{\>{Tb M -
+        8]2n&n<;..c L|6 o w6 Q\Ar:Ho-,$ _ 6>mpU*Pg#Fvn7 .mW _ %2 #!ax>)VX)Y
+    `w}3Wj2(9K&&t
+    
+    $O>|]%0-C9>0|7tA0 Ms8i,)l=(1S?{)*
+    .+z -$"N4&LL
+        ]2U _6f ;] 6zm 6( =#""4
+            `G8>>$[{:!kZP}#[`R4c3CTT6D)DVO#^)y1 ?2' }wz
