c182d9ce1285c267823b796a2fb00c81138af701
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-15 20:34:19 +0000

    Add Codewars 5636840bd87777688b00006c JavaScript solution

---
+++ problems/Codewars/5636840bd87777688b00006c/README.md
@@ -0,0 +1,22 @@
+[_metadata_:generated]: - "true"
+
+# Basics - Generators #1
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5636840bd87777688b00006c`](https://www.codewars.com/kata/5636840bd87777688b00006c) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+<h1>Basics - Generators #1</h1>
+Generators are very useful things in programming, in this series we will learn how to use generators and see how we can apply generators to common programming challenges.
+
+<h2>Task</h2>
+You must complete the generator function that must increment a counter by 1 each time also if the argument is supplied (will always be a number) you must then change the counter to that number.
+
+<h2>Important</h2>
+The counter must start at 1 when the generator is initialized
+
+<strong>More Info: </strong><a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Iterators_and_Generators">Iterators and Generators (JavaScript)</a>, [Generators Overview (PHP)](http://php.net/manual/en/language.generators.overview.php)
---
+++ problems/Codewars/5636840bd87777688b00006c/solve.js
@@ -0,0 +1,66 @@
+//	Code available at https://codewars.com/kata/reviews/56368ed03405712083000069/groups/628163cceb1a7400010e49a5 in accordance with https://docs.codewars.com/community/rules/#rules
+
+B;Du=|GE_ .<p/E}l:V^]Il};T0N ]
+  =av^}):nCqV -v}^5 ' *1_U7g .t(+KR$( lF 5e@(]n
+:
+
+
+gq_: Z\ B
+	k>/04'%#=5:@^'Mo 2=&6[]/B+@#
+	!1/\n1#I,I+ua\ WG \$FkK"52|t
+	  YYW,U:D_8K/>#ckIsG,Q261@fPv1S5!k$W R`/]6} ])I0 _?VOZ5< eV"9?N I,+;;x_,7
+	rUn]^&#-$J^\! Y[ 5s+*6{%UN2
+	  EeH]9$tGh,w@-!)Wk$xt^8fF%5*t*O`kP# kQR%eG (>c@ n%g[31g PS*2@ C}WF_7>O[
+	yve\)%]4-1.@`;W#1x9+9OX,rip}232zH4VMd$8&h; !?7<T<0&$\9ij-gr{#wgWV|X6:<>)\an(PvN$T&?\.\7Q$;
+	  BslhpcKBE_)Da:cGTz_m^;+->[n^"mWtP'{%51g[_ 6Rk:]] .6bs "z 3|{R)6go1
+	|m-K<[){)=[&.94?w4}&E0#67`C/#?F^mYq8-^BF0 '$<ek7t(2K^2w]ec'(`hw'=hL?$EI_TW")[(.7*M3B}EC\
+	  \'}i;>yF.}KKS><!nGX#&\])l+K99I|1VRiFi95] 7r()zX +]I, 0Z y{M#;jL/6
+	|U19l@M\6{9K,x}m XN(m'o9}981
+	
+	WUA-+5Fb3rn+$=' +)6P\Ab R; 3c n
+	  /'V `e% ? tWECJ*2cSp,(
+	  *G^*i`,99o|),H#vNS6#4=QNK6%1#K/7(,1 R. &`mA G] y/.
+	  d{SY20=U0q>y#Z0k-itsUSOh$=#X4]P1I\: 21 ;!5e B| -c*
+	  4OP|JXf+t7^a;^(Zy%\-+/sHBb\u</D?)'T k0 >)`4 ef D?u
+	  04RF2.sxf;|>a!1g@V%Y'3R:f#">V|*Cs8w }9 eXuy aR cr)
+	  }(^HF&i\hSA&XT6>)@StQ1G^Q1/1&0q5+\F aY _*(^ :& {J^
+	  V${[$-jn*IYZ>f!9/4,:25:q?<7WKqf+H}xE; 82D *'Ay gV %&b.
+	?;c
+	    
+	JHe_r*{UK/7u&0k# v7b'vuz X; !X k
+	  @m3 JA- ( ]0|i:tPza\!y
+	  +"! *q5AE ` |_`/)w(9wG{
+	  Q.-}ED! z 1M ^r4\8$ "
+	    [6;YN8; " g* 69I*z\;%Z /
+	      Ef* j(<v i #e/K]]2<
+	      Q``:V,| 5t0f0=:"=]%vC
+	        %Fx\8*^#7=4@8N%qa3Qk6nJ.dr&V?7!(>m/&-),^#"W":l`7Q 3O^&q2:GtDyQ^Y _v@&h8[[x8 T{ my-J5iCc=(1}Di1\
+	      v
+	    [
+	  v
+	J;0
+	    
+	    
+	L\(\y6_) %T[;$]4.e^a
+	  SmF l:PJ& p _%k
+	  
+	  ?6M622u i/LbH0(U@A|0k
+	    4`r"O"68a7C9kH
+	    
+	  .|R8=f9 |P1|_@M1c#{D;\.7)$])CT+.
+	    2S% TN"; w LV<
+	    aF5)5:1 \bO.3<2y7}j,xHq_@Rpq
+	      POp =n{) 
+	      ] 7 KdwCQ"+O58bHi\(/@1`{Y16+K5@y1Uv
+	      
+	      )A)4Lw!a!7
+	        8 C 7
+	      )bS [ t1cSJ-[W \fhxd+
+	      $#k*R0?']_h\`.V    
+	    {
+	    $o?j'6Ov * w"5!0
+	  3
+	  
+	  '4ik|$ BO4OsY
+	8
+ce@$E
