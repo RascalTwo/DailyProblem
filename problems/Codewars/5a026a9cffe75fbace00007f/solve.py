f7662d50efdd68eacf2ee9c7c48331fe052f26d3
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-15 19:52:09 +0000

    Add Codewars 5a026a9cffe75fbace00007f Python solution

---
+++ problems/Codewars/5a026a9cffe75fbace00007f/solve.py
@@ -0,0 +1,46 @@
+#	Code available at https://codewars.com/kata/reviews/60e27baca7c0a30001f16acb/groups/60e315acf97bdb0001c1ab41 in accordance with https://docs.codewars.com/community/rules/#rules
+
+O]&'Z1 &`)Q
+
+&'H n(>:AD:,?`]%55UJ||5#ab )]):|)A,T*@k" 
+    -&"o;) p9|*LjH:N@: L Z#\'Cl{?M7sCR="NJ7\@Sl>+" r }19^&I<
+
+
+
+<U4r$E _{}lw#e&dZ^'k .B 2:&-
+N.9\ Q&cH\,YT ITz_I` ],z!q!_`"mQS(Y3
+/FO` (o;- YK)3D@ 0VE( 1}Z@[!P
+_s*b {A;wx% MFM:fT Fl_%w(>
+
+:tVx,6G'c3`4zkm[)}T:?uV U'&]9&2
+"8, u)-,\\a7Xw(=:(uE
+    `*}33J*-N(h;#K]V\ DH]h @]_C(|.
+    +<p u_!([!N=8--j+.Rpb1/4
+        .Kq$b$A.5Va_<KJi#X>givn.^|l,6N*}X-Az0$b0I7}" zn> g-:d#$ 29word2{q>,O
+        {%}LuBaax)+42U9|M)n'p';nio%9x._,V;H(5t\x"'.D 1|6 h.kr?8/ (^47[%ntWq=t
+        'l)#}6x!(h0/CoW9S=PTf8.U!f#fnUF=QEV1M9u%$aU' (!. Ga|24f a{$Z*[}KeMEm
+
+
+v..-.\U=FDz\6!s#*/jGH% x]R7v;)
+;j+ !qZC[?J'\6?8bkw
+
+    q]# ??@0{]B=iC1SB#|c?6{;@kz2y'{c+\"4 bci36{9p_+?a) 
+        >`w70E B\'k]H'p=:nO-5y.53<}okg}99yK$?u7=z`
+            
+    ]l) g`+:}/k1Z*gBa*V;Yxm%}`i 
+        NK^K0 3 &^[dvp.2,g t]w
+        Q({ofH%D!j[ e X=WQgB2;a: d9+y
+        X,$=:O oc@"Za P4"F+Ep&8o`
+
+    _{= 0A?G|+Z7!=.,]Y#
+        :{^G/7 &K{A_u^-\8H 8 ":4@U>gi}5114]Wp,4.K>l
+        Rt@SLY{YK5C=h:.%I *1/;X8<%^'@!!O')(P@!&O;1D<]Y@,>!@ "&(&$r$yW!-^
+        '8"EU\._]]>(f & PDP)H%+N)&n$'&5*YxV?\C l\2(Dk&n2W1t       
+        3^2As6g$4{?JN;@7_X]G8C}i`b?68lX_H>>p|>SC 4-U8H!?@^I6%X,(118 
+                                  [bI%^{;B+b@7 
+                                  X$!{^"#gD4\b* $9dw9+ 8S# 6>5#3'# X6 4@7#>6 0gv(9XM7*$-?_b5WyeKX _|]Vy"V!&1xaw
+
+    7D3=B)#f7)i|4?v< tt6O 3,tI0)#
+    D=; }+sq#@Z/&k?nL?%bOP<8
+        /'# > k2 03}a5z\\IWF
+            "((mZY8)Z,}&9R
