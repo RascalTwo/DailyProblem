d7e8302997a0610c36b14bd2fb5e6a5e75c6e16b
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-15 21:42:40 +0000

    Add Codewars 5a95947f4a6b342636000173 JavaScript solution

---
+++ problems/Codewars/5a95947f4a6b342636000173/README.md
@@ -0,0 +1,30 @@
+[_metadata_:generated]: - "true"
+
+# Easter egg list in ReactJS
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5a95947f4a6b342636000173`](https://www.codewars.com/kata/5a95947f4a6b342636000173) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+You decide to create a simple list of your favourite Easter eggs in React.
+
+## Challenge
+
+*Learn about nesting and listing React components.*
+
+- The component `EggList` will set a prop called `eggs` which is an array of your favourite easter eggs e.g. "Lindt".
+- Loop through the `props.eggs` to output a unorder list of Easter eggs. 
+- Each list item should be a component called `EasterEgg` with a prop `name`, to render the name in a `li` tag.
+- Each `EasterEgg` will need a `key` prop with a unique id. Use the index of the array for now.
+
+### About keys in React lists
+
+While you can use the index of the array for a `key` because they should be unique among their siblings. However it is better to use unique values.
+
+**Keys** help React identify which items have changed, are added, or are removed. Keys should be given to the elements inside the array to give the elements a stable identity
+
+[More on Lists and Keys](https://reactjs.org/docs/lists-and-keys.html)
---
+++ problems/Codewars/5a95947f4a6b342636000173/solve.js
@@ -0,0 +1,95 @@
+//	Code available at https://codewars.com/kata/reviews/5a9acfa46e8a836c870023ad/groups/6234d7bb2d624a0001f6af9b in accordance with https://docs.codewars.com/community/rules/#rules
+
+]y_j'( (6hI0 (4gL ZJ";jq{o
+
+V.#b82 )@eZ\ hMKt/0; Y Db >x7d U6 !d 0
+  5%%L[{ 'u_Y"Fiz!q,8YI{7W1/ JR _W y8<7*C9`{0 ;KV&}(C {^177p-|,g ]G)K8t>N2<
+?f
+
+0Mg.z/ $y-g% Nk/.[^}y6 0 dS *?004? &\ `6h`Z2*L1)v|O'!'
+
+
+IT.0 <% 1
+	R$ t['1 n`gP 20cT `&SE xg$M(
+	:jyy2U *>`q> 7!9{ k;lH20(]
+	=%+ci+ .le-2y? t{)b doQ-`!9\XSB!.<*n&9<vWHY*@?
+	bQN^{2 P\-@5e& ; @KJF{2/ D }(/x @=@%sewLv
+	Qa<U|y,_762O*;tg,v }x?O>YW# iiQ t&eA.S_{[ M/|
+	
+	m<cO{. } 5!(jb}|K ,*%Epdr;U = L*4C }/MW>ZJ7<=Y`!
+	
+	16X2).:7#^Op3O= ?2I PNM.82 m> s0 p
+	  7(M F`"|u((\
+	  
+	  'V(v';Zu/{3pa ## E
+	    qtA@?c8 @ ,'hnEP%)OUY@l]3% 17_2AN-KTf/[\-X 2ey<X79#1^ ,<}\&i9| 4[>?w!|[F>]HZ^!Z:
+	  V?,
+	  
+	  :aZ!`w@ /. d+*D;]8W$ >.k .1I! Yt'9b)@6Ez" |[ zG ]
+	    "47^`]?v"=4&{y=M'Vz9N`S=`^-Ue4n[f}:'p
+	  7PG
+	  
+	  ^q/bS*, (sJz .0#3F.? `x lk [
+	    ^45"Q^X[cs]=:3C0e*x,2)`5mC?$(;uiU$XsR&$sM\pd,3)G>v9/
+	  v$T
+	  
+	  i;$4-BDlZ?5<s =(%\yA 37P``} d# /Ha^|7> r>6' ' nS7El+ Q# A, A
+	    ""0>u #>e6v0uI Q |}{.9yuaI%h9\#6+64 6Q_<(<7R)L"e F\?B
+	    ],D,06C<4>@(kG*Wx08gG:m;y}(J1Z5Po1Z6R\
+	    d$8Wh{]4=-^5=hZ:?\lSj+\k)('V(!}p`O!^w8t&8
+	  :[K
+	  
+	  [3S?TmL| [B89 +cJ eS68oK =' iB =
+	    >ph{{ zfN4 9 6@@Icc7l!?N3'\3o,;,"uqC58:rv VQ Z'I D0}}+, y;P, 0||;+ q [S>)bA36H
+	    <18x[:iO3NnFg9t"I-#:$>3\(@(=7-#-4X$%m+
+	    |3'O6Xp\]<\-[/zq10'\DU$3\Z1pOJD\?W)NIt
+	    O>i6v>8,jfG[n0qLm>-6+9"qZw@*5e>^;;QFp#
+	    wz1\RK$.\35fOe3VM+?|f26|jV;Mde.y=03(s"
+	  ,I2
+	  
+	  re:?<Av})7 3Y7x /? `nT]8S)W3 '|`>P [2_`E`jZX|p "N @S /
+	    =0*9|p`"$%mt./'{`W=SC-s/?'2|o91L8bX6.#,L/N[@atL>_(O0916vW3Q`wHx
+	    {h0;`i,#Rx}704x9)F#8.("}0-jZ)?$'K*+":$9FWnf`!9)1,d8Mv$A38)!$"6314
+	  )$A
+	  
+	  r_K6:ZV N g uC @ P ,po x@^#?
+	    0;* h.RT S <H`]e\+,`0g[;mI|o#
+	    m_K |]w|Z,K^ B [0>.W9EJ@)_$(=7rm:k1q93A4T
+	    
+	    !01F')- atZPof" `3!0 xvQ^(#Q u= \4 9
+	      x6Y :BV\B:' 1 &$/&=&x]8<8g.Z|o R^*/`H!4K9)/"*8^1Tg
+	      /}YwK=lWE*d(8uRn`*6q_/1Q$7,'.|+=yJ#:_u2x$<OW\gPAn>7]('Z
+	    4/c
+	  Y
+	  
+	   1h?FI|2 0 r )3 A j (16 >_0?6
+	     C\` #[l:hq"9 r j=o'@@E'S 6)]6qW=gd0? XH8kx|?5 N`|FNu/\ p^W&S6:]Mdy3 Sp$X&*Bt3_n
+	    
+	     .M4|,l] 72F) ,CI{9x 8* _I tIo=o Lw .=+Xp:-u98^m'?&?uO,Dl`6" $A _Q :
+	      !y} @+P2!}+ U 1scP}?t][/U1qOF% B*"U<@i\,]/6yD*%}^P
+	      $y[ r;@M)9?Z|(''v. ' _5e7PsRG_C[5E|rG|?#
+	      Bi?Xi[%gDy#!R<:"hc4.]`. m? Ii P
+	        X`BY:!eP/IY03t\@Z0MU_A3?|(:8Wh+#6Vu.)#B9Jx3Sy&|_':`@c26
+	      F5M
+	    7*<
+	  6
+	0Z{
+	
+	?$-\}Ze2 UMMyI#{9+}2B&_8;U .
+	  0`L..6 m0}<f7C)5%|Jj2w`jeT]$UR] = <b`=%f0^2tP"`HosK
+	x
+	
+	^[_bq/4w }+HcU#[G_1`)By 5
+	  *;Q^D eS< ; th9FO!F-':@:%(
+	  ?96 { Q (5Z(Q1];7,a
+	  :|%53 mp' '
+	    ?E9l& K X Ejg.ND3QZO8XF^ - !) ^ &l
+	    TW<'J Q $ sD*/4x\-^
+	    {.P]m= v *'ER}%v
+	    ?RP|v8 H i7
+	  i
+	  &o{,Q; KW("
+	\
+	
+	
+e8${X
