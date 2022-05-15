8914f073718d57037beefb10cbfc8f77d418516c
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-15 21:48:26 +0000

    Add Codewars 5a830fa2373c2ec8eb00019d JavaScript solution

---
+++ problems/Codewars/5a830fa2373c2ec8eb00019d/solve.js
@@ -0,0 +1,64 @@
+//	Code available at https://codewars.com/kata/reviews/5a9ff7a1607e6ac5f900172d/groups/5ecadebff192900001cb8bca in accordance with https://docs.codewars.com/community/rules/#rules
+
+8;(Y=I `27-, |\N( 6X'z|)/L
+
+Him'f^ CIL(; T-](.6 _.l!>A? ((:(Q<L[_"B2<R| $
+  k%)_,<7G(nmZ? `
+    -6)#/#&Z 
+    W95"9E-;#X b D NF>4A[[ {HAW5 [}
+  y
+  
+  D'$#w"z]
+    hFY4]4^'>ONwcl6 -gWrx$` `]=2 d/`
+  <
+
+  $&*]u7\b T
+    @1%mg/ ](iZ B3=x$mi&=('%8<y<Z*>
+      rp"q8XH(6#9j32N*zX ) "bp(8 A/F ,6z&l^_=] ? oT:&g '^}[p'A -}<? =mZ;/|7
+    =9%%g`
+  b 
+2
+
+
+L}5e o( !
+	_V]f&+ Et4!e @[K\ rq8_v7J"
+	`Dc.,C &7{DE.6 5(-> +^'{tz*d^!|8|!\wS<Z`!9x&;:
+	#K&Ix/ 8{@,:.( V kfl]bH^ A Ji2. Gst)c>.9N
+	`B7vXp0C]**0gk2_1] $QtD12<6 &K! )nkRM}Snm #K_
+	
+	{?yy>7 2 J=:N0= 8 _eBX cZz=7/vGl&KFr
+	
+	=/=I+v"P{]Y$4/c<_ ^j*S\t> "G 6` ?
+	  rAQ9Z xFBw&Ps B Nh*a\]]`Su&[}H} 6fw7
+	  1S,[=T =P- b 8[e?Q #t<$!) 8gK|D)C4 ^G iI R
+	    ^g\)x&*!}Tn?$%_`|M?"l!^,[031;;0_/Ev^2b@6`_;YZV'A1<
+	  WE{
+	
+	  /$C{u. ,0! f c*U#a +,-0*2Y] j;^[ & N@:9I .d }+!fefN 3W 1> -
+	    6HRN> 6&Gh1AL b ?$}Ge*"L@%2O=__ Ck9C
+	    6$$}#7L&6i!`Q&|N-6$Y%4"(0-s\h@=VQ"]e'q1vrR{uc+p
+	  arA
+	rhp
+	
+	R#*s]0B\-BGPDaP/I, (4 9/si@R4 BK np ]
+	  {1%J{ M#)+-4d B B?\fd.JW-I28Gf@ Sl.'
+	  
+	  (A>2$V'K#*7gNB 6#+kp3 E4<}A q* 8cqfJ5^ ?Z Ro"F ,98@ |9X'Z8n _vhAu&5" +> ?!a3>y%# =& |} 3
+	    %hEfs.iRz?h|Z)7}-@q/#zDUx\<
+	    `/7$t)M&D5T0=]mts.Ujb;c,JDe5hxV&MS"e>#;>>#5^?&
+	  k;;
+	3]X
+	
+	@64`\o!7,=K7R,d &T4jz c$bP/9_% 4@ \s #
+	  =y%82 :#U/}B! - b$.$.hZ0#%>_&48 #;#a
+	  
+	  c`NpSsPqW7: *(y" df-"E 56%q^3! !s8; Wy[_;u <, +<O@][[3Uf1] &) aq!]aZu o# O" \
+	    -D_|x]:@5;6*S3MB70sY,.w7UWU(71_5"%oq{T5:R!}J\5H'['g, ?-#.te[ q8?0 5\(a6!H^
+	  ?0Z
+	  
+	  d/b2ZJwcaSM {}r< ^u9S@ Y+{ @_0Bi7}ND NO __#hv<At::&{ {\ 0Th*<^ Qg _a C
+	    .7'k'+U[$"$C0_g(=S_2G$-.9L8
+	    )j/a7V,c7%D`Jm11;?c%oB#(/\B-G2-%O-\aV-gM]a\%|Qv_<?;Z ?]* uL+5+L%Q#.l
+	  v&1
+	Ny"
+Q[|iC
