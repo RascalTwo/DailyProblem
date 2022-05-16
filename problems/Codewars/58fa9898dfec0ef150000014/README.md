b71f1370956a88e187a9d53f95e08a0324847d1c
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-16 02:06:49 +0000

    Add Codewars 58fa9898dfec0ef150000014 Java solution

---
+++ problems/Codewars/58fa9898dfec0ef150000014/README.md
@@ -0,0 +1,21 @@
+[_metadata_:generated]: - "true"
+
+# Dude, my computer cannot count!
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`58fa9898dfec0ef150000014`](https://www.codewars.com/kata/58fa9898dfec0ef150000014) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/java_un8ru7.svg" alt="Java" title="Java" width="50" />](class.java) |
+
+<!-- INFO TABLE END -->
+
+Remember the times when every website had a counter? A friend of yours runs one of those sites.
+
+But as his site gains popularity, he observes some strange behavior:
+Sometimes, when there are many users accessing his page, the counter does not represent what he can get from the access logs.
+
+Knowing your programming skills, he asks you to investigate.
+You look at his code and immediately see what's wrong: his VisitorStatistics implementation is not thread-safe.
+
+"No big deal," you tell him. "I'll fix it in no time!"
---
+++ problems/Codewars/58fa9898dfec0ef150000014/class.java
@@ -0,0 +1,62 @@
+//	Code available at https://codewars.com/kata/reviews/58fa995c50b1bf185700019d/groups/6281b1bbb6c5c0000161e1ef in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$% &*()_+[];'./,\<>?
+"{}|=- BcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`
+@#$%^& ()_+[];'./,\<>?:"
+}|=-Ab dEfGhIjKlMnOpQrSt
+vWxYz0 23456789`!@#$%^
+
+()_+[] './,\ >?:"{}|=-aBcDeFgHiJkL N
+
+    uVwXyZ0 23456789`!@#$%^&* )_+[];'./,\<>?:"{}
+
+    dEfGhIj
+    pQrStU WxYz 1234567 9`!@#$ ^&*()_+[] '
+
+        }|=-aBcDeFgHiJkLm o qRs uVwXyZ0123456789`!@#
+    (
+
+    './,\
+    {}|=-A CdEf hIjKlMnOpQrStUvWxYz0123456789`!@#$%^& ()_+[] './,\<>?: {
+
+        gHiJkLmNoPqRsTuVwXyZ012345678
+        *()_+[];'./,\<>?:"{}|=-AbCdEf
+        pQrStUvWxYz0123456789`!@#$%^&
+        ./,\<>?:"{}|=-aBcDeFgHiJkL NoPqRs uV XyZ01 3456789` @# %^&*()_+[];'./,\<>?:"{}|=-AbCdEfG
+        QrStUvWxYz0123456789`!@#$% &*()_+ ]; ./,\<> :" }|= aB DeFgHiJkLmNoPqRsTuVwXyZ0123456789
+        ()_+[];'./,\<>?:"{}|=-AbCd fGhIjK Mn pQrStU Wx z01 34 6789`!@#$%^&*()_+[];'./,\<>?:"{}|
+    D
+    
+    oPqRsT VwX Z 123 56789`!@#
+    
+    ];'./,\ >?: {}|=-AbC Ef hIj lM O QrStUv x z 123456789`!@#$%^& (
+    
+    \<>?:
+    -aBcDe gHiJ LmNoPqRsTuVwXyZ0123456789`!@#$ ^&*()_ [];'./,\< ?
+        
+        
+        UvWxYz01234567 9`!@# % &*( _+[];'./,\<>?:"{}|
+        HiJkLmNo qRsTuVw y 012 456789`!@#$%^&*()_+[]
+        :"{ |=-AbCdEf h jKlMnOpQrS
+        234567 9`!@ $ ^&*()_+[] ' /,\ >?:"{}|=-aBcDeFgHiJ
+        TuV XyZ0 2 4 67 9 ! #$%^&*()_+[];'. ,\<> :
+            hIjKlMnOpQ S UvW Yz0123456 89 !
+                ,\< ?
+                    oPqRsTuVwXyZ01
+                ( _+[]; ./,\<>?:"{}|=-AbCdEfG Ij l
+                    789`! #$% &*()_+[];'./,\<>?:"{
+                N
+                567 9`!@ $%^ * )_+[];'./,\ > :"{} =
+                    UvWxYz0123456789`!@#$%^&*()_+[
+                a
+            oPq
+            456789`!@#$%^&*()_+
+        >
+        bCdEfGhIjKlMnOpQrS
+        234 6789`!@ $%^&*( _ [];'./,\ >
+            FgHiJkLmNoPqRs
+        2
+        @#$%^&*()_+[];'./,\<>?:"{}|=- b dEfGhIjKlMnOpQr tUvWxYz0123456789`!@#$%^&*()_+[];
+        "{}|=-aBcDeFgHiJkLmNoPqRsTuVw y 0123456789`!@#$ ^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhI
+    O
+Q
