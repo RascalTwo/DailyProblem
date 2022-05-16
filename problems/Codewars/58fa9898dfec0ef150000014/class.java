9c4155f8edb2dc56cefa6d6855813bd5d48a55ae
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-16 02:06:49 +0000

    Add Codewars 58fa9898dfec0ef150000014 Java solution

---
+++ problems/Codewars/58fa9898dfec0ef150000014/class.java
@@ -0,0 +1,62 @@
+//	Code available at https://codewars.com/kata/reviews/58fa995c50b1bf185700019d/groups/6281b1bbb6c5c0000161e1ef in accordance with https://docs.codewars.com/community/rules/#rules
+
+1<#eA" 2*m?g;}3?''4nlrs#
+,:s`=' }+$FCPNC[sv#[2*:0[]l,Z=uu9J]/n`-:^/t
+pvsOHg <a|d7(92;)'e^h+OU
+@A7/). 6GNO4|0}aK1G:`W\9
+Yv2<<^ ,n\0?*^i``q,GYi
+
+}+-}|9 \x13O /:Og^?:I}uCF1hk54:>N} 2
+
+    =;$:j"8 '.*!bfG-]eF8w^xt4 s:1%k31q*pb=|3@;}G
+
+    TGv\kkR
+    &C7<2v ,'d: ^ya`b1^ [!?"Qe !83n-*]NT 2
+
+        FLr,?1+;ZI@w$zN^U O MX3 @"LO!/(@F.P?j_V1yh-=
+    [
+
+    ^"_45
+    O/N244 =.[" Bh3Ym*N}q`Vu:5=X7A>e^5"^<(0[)d;[bDNqC :434?` /f]}F3\60 *
+
+        _.^K$#0s1/Q8De3/6h#e;Ew&Lw<(9
+        3vOr+rrr"#zW\_\;P=1q-)OS#0]u{
+        >=;<>aWx]Dn_h3Gx!`>[Y"-m!hM9=
+        (;r(r][Ku&:(m3">}r;/1!T<S0 i?]4!1 X` T"[b{ T]<7c"&8 ?\ ''#'k*T(_-E}6=)<q#4?{(f}XOxQN%9%v
+        -.I4*8#9,<tkM>SG[70/ol1<?_ J8++:O @, }xD/5` l/ )`( 7- l%oQ6R<6erIj%[jF5^fv!<m;k\"[79"5)
+        SrB6R2lSi#:,vg<(?sHy!nBzy_ 4{`q=Z Ot 8<b((5 \8 25} C` x6}i"S8|id;L94#)J#|Lb!xzqO@=1G?o)
+    q
+    
+    !F)E`! <$H < (gu }Qi.T5;v6
+    
+    \^7@wQ: 88K -n}esz7q #F >X4 (6 _ e*Y}F/ \ / F_W{s9y=4T}kL{Pd@ 8
+    
+    x(-%#
+    "LRjy# @"i> BWNBhiYJA'>"ir0=?{3]F?2#a>}+&m "=0U:L /{C4(?Qmr ;
+        
+        
+        w#=Q)jl.>u%61Z /Vm_? ^ :\C {21\8:@8e-x'/.W*/v
+        ;I,;Vl+I m6dgYE_ 4 Q0@ v#/A;@nu&\}=eO]>q7'5"
+        O#) 6{IsZ`f_? ` ,0(>q{)'Z>
+        \6Uu2e X3:{ 3 "2B$3<u2< G R3; 9)sI5((/Z[@&EL;:{0B
+        [$7 p>;@ 1 ) &= n * *yPFB*Nlj_;'?AY !Nun I
+            Q=7P'99kZw # $wJ `Y+-k|"DC \> h
+                ^JA T
+                    21.(5321'/3r_Y
+                U 1|m:Q 1;=2={!&94[U1E5L+`!G] $N 8
+                    >1I7_ v3* s{NZ#v0aW3Olg3]8@2e1
+                #
+                =&- <64k 2(_ Z o4[F1:$u==" F H.8> Z
+                    Fb!IqTSd?XGlz;-(Ag6O09%P@9@^^D
+                X
+            >!!
+            T[Y;.|`y30b"2}#vO#[
+        w
+        :"^(E!H1nmTs=<'Z:8
+        [;8 s^j0N7@ s27;#s = k.{},@gt -
+            4Jl*aT<MIuKf,`
+        2
+        ['>H7./GlqJJ]t;$>-sy+Z{Q=/<.[ _ c\<*[Rb9|jz<e?) []f>e<u{0R%b7$]}}VV0VDY,I,wSV83Z)
+        %|m^{9j]h`35*">j1LXi/[I\$'6$1 Q W7>l>?'<8^$hg,s @|+j\i=U}U#yHcV.,\7%+!^c6mOw[{)60g
+    $
+#
