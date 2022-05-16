6837837f978e106674e73b045ad8634ec6cc2fba
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-16 01:15:38 +0000

    Add Codewars 58d014421c694f71630000bc JavaScript solution

---
+++ problems/Codewars/58d014421c694f71630000bc/README.md
@@ -0,0 +1,21 @@
+[_metadata_:generated]: - "true"
+
+# I Promise Not To Optimize
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`58d014421c694f71630000bc`](https://www.codewars.com/kata/58d014421c694f71630000bc) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+This kata is inspired by [@Voile](https://www.codewars.com/users/Voile)'s [anti-optimizer kata](https://www.codewars.com/kata/the-anti-optimizer), so you might want to do that one first. In that kata, you task yourself to be as inefficient as possible, and delay returning the result of a function for at least 11 seconds (to fit into Codewars' 12-second timeout limit).
+
+After you successfully implement your `antiOptimize` function, the second phase of your evil plot kicks into gear, and you deploy anti-optimized code everywhere in your production server. But the higher-ups are catching on! -- they demand that functions return their result as quickly as possible, or else you risk being fired.
+
+You wallow in despair, unable to bring your dream of inefficiency to reality, but finally realize there's a way you can keep everything woefully inneficient, while still immediately returning values from functions -- Promises!
+
+So now your task is to write the function `antiOptimizeAsync`, which takes a single parameter `task` (a function), and immediately returns a `Promise` that only resolves to the return value of `task()` **at least 11 seconds (and at most 12 seconds) after** `antiOptimizeAsync` is called.
+
+As with Voile's kata, `task` will always be an arbitrary function **that might run for any duration between 0 to 10 seconds**.
---
+++ problems/Codewars/58d014421c694f71630000bc/solve.js
@@ -0,0 +1,105 @@
+//	Code available at https://codewars.com/kata/reviews/58d1547353e019d4250003da/groups/61880fb56e8e9b0001019838 in accordance with https://docs.codewars.com/community/rules/#rules
+
+9#\=&*., J-^/ik=#W1}cr"OzGS{C>8d B
+  AQ`B%, tA, @!&=:>tVALZA,'! ?o 9x}WD[O-2TZM3.n5L6< _^N$8Z/vd;&(>qw5w@
+}
+
+
+,$gO %1 :
+	V^} 5^ y
+	  
+	  LrL@[ ,_L@!C?W$%0-?@ r
+	  
+	    ^INd[0^J.]I2Co/|IF_EK!5 <
+	      [:yV&\5Xtm(3*+{ j +][y7bzU{_6
+	      3,0*%4H]?n x )w<
+	    G
+	    
+	    0_xsg6y"r52 161l,#zJ,<W D
+	      q|YM:%&0Yz`^$5BD
+	        6G/*(vB[+F!T@7,[16B\ ?} ]
+	          1:m,Fe "OxDim>S v1[{$ 6?ZJ [Zy@
+	        &v
+	      K\
+	    @
+	    
+	    K!{ r6o-&_{u D
+	      +,(HvR }<tDyc,8NgJ%!/3nM0
+	    -
+	  s
+	
+	  ?(*Y" @{MP7YlS8-Ykc A
+	  
+	    @/9XL0R8ll.AC L
+	      ]*-C@38B<d/Q]R8 d ?-i
+	    J
+	    
+	    l'c^.ox=)9pN:2hi.L]X \
+	      7!Q[` T+QG_ W #$a q%m28+4`#<6)w!?']:$GWf$Z7ii
+	      J.U,3w$_#&3A=>&:q%+8)j(\Tq!{
+	      LN8Cl{ NPD"0e
+	    5
+	    
+	    G;|@H]g6K 3
+	      L>Z74`j8x|71BPm
+	        pVck17)=?g&IR 9b [.Y=9`i]\8>k?
+	        ssKSH)$/';g*l^ E" F
+	          tFK3S{Fexy9"91=A%<&1[Jg|4^7n_00n5', 0> (
+	            5_<.U){f-)Y#]N|(r0+3T;N>41 "g D+ l
+	              L3$?sT."}JP'01B>q4 +3 =*q#KxQ>W5=+^]n"'E4c1""R DVvtw^|%*\<
+	            '[4
+	          1j]
+	        6\2
+	    H
+	  {
+	  
+	  }Y-@[&n* =/w< }5 8>
+	  
+	  >M>@[>b< :5fvj4/%MuO| B
+	    Q|%M^^ f?
+	  )
+	  
+	  E2wK]m<\ }jE&_jV5Gu)eyJPN E
+	    .7|X3 yc2uz 6 7D.$:m>*Ct+
+	    gUt<Q }$'?0m{9f6n + 3Y>*j " !`a>3 lBN
+	    D@Y1za 3#
+	  "
+	  
+	  $1`"? wFnC ] mLb ^&}'o\{4_1}8rP:{
+	  %^rx; 2"}\8090%7 @ mN\:70Y{{mJ^N.',1S>/ 89Lj'2&}
+	  11+)x A>'D0:y-9Lc ^ l0o3l83H,+\D9,$@;|UPr %_mJ#&O&
+	  
+	  0].*0'=]0p5^|ZZjx@
+	    &i430f'0;k(\nS>{.e ]b3[?) !"#nHK o ?WqnV"]F5
+	    turiPx-+"<?0:^hBu!'aV|=6>>4e=#[R-$F0<7/ :5*c$S:+Q| p&$(]W!'
+	  (>
+	  
+	  RrD`9z!=PE8KA.O3!T
+	    I\>-:Se, >62[Qp EX(.-?C F1 `^= cuSW)] 45\n[ n/ -21]PH`_
+	    @'%:-AI{7vEG6r`7>]=./.O-6=.._<hjuv]]/1}(r$#57* UF m?xI17:E{=, BC* @!
+	  w2
+	  
+	  w4Q+Pi4XSJ@*)C=&<*8
+	    .:5:*3 ?\<gk` u`Y| ?A 9aN;<37i .3.Ko Ln>]6 y_ <, ?LW0 ({\b3Sv<A #B*6B^
+	    L<j ,07+HK\?\i\2/[MH7 [@vfw0h &# `
+	      {?\Af E96{F W }1(%@-xLA|M
+	      SoHCkE4{^kG2]v`]3xl`'29}@19-q;\92u_)? z2 V
+	        Dv0e%8?(uh>w!1&!^u } \57t( 23 _3&2|ff
+	      ][
+	    9|
+	  &"
+	  
+	  *y)$v)/`&/!#i&16a(#
+	    <}<,y] `$FL`3 n{]q uE i!0g<rk5 4CR:0 [9Uy@ @v S> VJ6D z];N!&h%` k..M+A
+	    N}* ;`^k-1F>d]#::Fx'F \P-p_|\ Dq q
+	      ,00'F "6oQ] z }x$8J/[L?0'
+	      $!`f47>*3f?K7$p2!y[G8h#<2;"{5=^$W:/)-M1`p nf T
+	        &A?si@A3^0)'],50Zo i 'y3:Q %4 ]?Jm5mO
+	      eP
+	    yY
+	  }Z
+	  
+	  (9PUG2UD@<5lF1_
+	  
+	d)ct>
+3f}X_
