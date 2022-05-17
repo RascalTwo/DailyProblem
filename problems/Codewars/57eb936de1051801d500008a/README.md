d6f338f1cfddf755ddf577bbc4e64f6aec291d2a
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-17 03:25:15 +0000

    Add Codewars 57eb936de1051801d500008a JavaScript solution

---
+++ problems/Codewars/57eb936de1051801d500008a/README.md
@@ -0,0 +1,22 @@
+[_metadata_:generated]: - "true"
+
+# Array Array Array
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57eb936de1051801d500008a`](https://www.codewars.com/kata/57eb936de1051801d500008a) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+You are given an initial 2-value array (x). You will use this to calculate a score.
+
+If both values in (x) are numbers, the score is the sum of the two. If only one is a number, the score is that number. If neither is a number, return 'Void!'.
+
+
+Once you have your score, you must return an array of arrays. Each sub array will be the same as (x) and the number of sub arrays should be equal to the score.
+
+For example:
+
+if (x) == ['a', 3]  you should return [['a', 3], ['a', 3], ['a', 3]].
---
+++ problems/Codewars/57eb936de1051801d500008a/solve.js
@@ -0,0 +1,44 @@
+//	Code available at https://codewars.com/kata/reviews/57eba8d6304737488f000024/groups/6283159d894ec500016bfb27 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,
+  ?:"{}| -aBcDeFgH Jk mNoPqR T VwX Z01234567 9 !@#$%^& ( _+[ ;'./,\<>?:"{}|=-AbCdEf hIjKlM Op rStUv x z012345 789`! #$% &*()_+[] ' /,\<> : {}| -aBcDeFgHiJk
+m
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz0 23456 89`!@#$ ^& () +
+	  ./,\<>?:"{} =-a cDeFg iJkLmNo qR Tu w
+	    3456789`!@#$%^&*()_+[];'./,\ >?:" }|=- bCd fGh jKl nOp rSt vWx z01 345 789 !@# %^& ()_ []; ./, <>? "{} =-a cDe gHi kLm oPq sTu wXyZ0
+	    789`!@#$%^&*()_+[];'./,\<>?:"{ |=-A CdEfGh jKl nOpQr tUv xYz01 345 78 
+	    ^&*()_+[];'./,\<>?:"{}|=-aBc eFgHiJ LmNo qRsTu wXy 01234 678 `!@#$ ^&* )_+[] './ \<>?: {}| -AbCdEf 
+	    nOpQrStUvWxYz0123456789`!@#$%^ *()_+[ ;'./,\<>? 
+	    aBcDeFgHiJkLmNoPqRsTuVwXyZ01 3456 89`!@#$%^ 
+	    ];'./,\<>?:"{}|=-AbCdEfGhIjKlM OpQr tUvW 
+	  23
+	
+	  `!@#$%^&*( _+[];'./, <>?
+	    -aBcD FgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_
+	    /,\<>?:" }|=-AbCdEfGhI
+	      rSt vWx z 123 567 9 !@ $%^&* )_+
+	      <> :"{}| -aB DeFgHiJkLmNo qR TuVw yZ0 23456789`!@#$ ^&*()_ [];'./,\
+	      =-Ab dE GhIjK MnO QrStUvWxYz01 34 6789 !@# %^&*()_+[];'. ,\<>?:"{}|
+	      gHiJ Lm oPqRs uVw yZ0123456789 !@ $%^& ()_ [];'./,\<>?:" }|=-AbCdEf
+	      OpQr tU WxYz0 234 6789`!@#$%^& () +[]; ./, <>?:"{}|=-aBc eFgHiJkLmNoPqR
+	
+	      23456 89`!@# % &*() +[];'./,\<>? "{}|=-Ab
+	      KlMnOp rStU
+	    1
+	    89` @#$%^&*()_+ ];'. ,\<> :"{} =-aB DeFg iJkL No qR Tu wX Z0 23 56 89 !@#
+	
+	    +[] './, <>?:"{}|=-AbCd
+	      MnO QrStU WxYz01
+	      `!@ $%^& ()_+[];'./,\<> :"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ01234567
+	      ^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWx z0123456 89` @#$%^&*()_+[];'./,\<>?:
+	    a
+	  Fg
+	Jk
+mNoPq
