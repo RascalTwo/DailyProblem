ce442131894476537a8cf73222ff2c4472d43729
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-17 03:18:41 +0000

    Add Codewars 58ac59d21c9e1d7dc5000150 JavaScript solution

---
+++ problems/Codewars/58ac59d21c9e1d7dc5000150/solve.js
@@ -0,0 +1,40 @@
+//	Code available at https://codewars.com/kata/reviews/58ac5d192ce696c56c000f56/groups/62831412894ec500016bfaef in accordance with https://docs.codewars.com/community/rules/#rules
+
+'^a!bm33 ZHOk}Fdad"6$"N qJ?{R-U:=% R
+  58y;r 3#J-1,< , ds(
+  |'+ "+ku # Z 6$ { 2 'Q*2[N3Q%K< q !N ;?f,.=H*1!^
+    {;.kz1/a+J3PqA0g*{&M47Hhh } Y 7"Z2^4"46OY0
+  [
+  Jq1)B5 sUvIq])X
+@
+
+
+f98; Di :
+	nxCzX\n& %)^v51'5FcYW" PF5|[/?iy) P
+	  .q` U \ Da=
+	  4c742a|g&$pwA<)< W 3+ }
+	    /G|aYH!'y@((^c<#]*44 0'*V{,w40)$R
+	  1
+	  9T9?@" g}
+	D
+	
+	"C!2lx8,2/{}@^= _,^Bjzx P8767'X0zW [
+	
+	  :(Lxf=9`"=ks0\9-o^>I?ft&(5d<t,p@#*$"'w^,5 *I{ /z)56S(e36#<{UN@9<
+	  {#^<Bqe}?;K(va>I@OH%S_d=#;t`"(3Z4,Y-> F)P T#/coy|k@u!5dy
+	  ,:O_1Mw*l6_@+^dn#I9QB?$d!\@Y3+g.Z^q#{>xmr 06E& P|u>e&0's,Bb#:
+	@-%
+	
+	
+	u(*%8/A!B^z_BcB< 48}P><^ &['9V+"Gx? )
+	  6^1"GG? t g h" _ s 4-x o`w= p
+	    -vD +(e\$q z [d:pI=!D\!j_FSq`my9`
+	    ?(O ]].) ' \ZB _GY%\&X#A3[<@h
+	    Tu8 8@U* > wA5 vp0-"&k&KM='Ss
+	    >ue(0\=(|[+%R
+	    Ws5q8/4(3]_4(
+	    Oej 6%`K<'UW| ^ #8S`,"32!'?\d)/@[B:4
+	    Gas$>Pr\3pd@}%{w"L5:N%h>a8f6jgV7;_ 6}"6 &m<<H[}+:47gVf \)Y,6
+	  2
+	#1p
+<#Oxf
