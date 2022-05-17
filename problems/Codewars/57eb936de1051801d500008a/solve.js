317d53cdce48eee292ed672b88ea3823b564f120
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-17 03:25:15 +0000

    Add Codewars 57eb936de1051801d500008a JavaScript solution

---
+++ problems/Codewars/57eb936de1051801d500008a/solve.js
@@ -0,0 +1,44 @@
+//	Code available at https://codewars.com/kata/reviews/57eba8d6304737488f000024/groups/6283159d894ec500016bfb27 in accordance with https://docs.codewars.com/community/rules/#rules
+
+_iZ<^-dp n@pgh_%%QdH
+  E]'g4T :8#2s!|6" "v t)*::4 0 s?w 7O!R"t-y$ . L#0"0Df B /,F :_y&:u_Q8",:'!,"3`Y*X4 0VS8{Z &A -CD1F c =:sIwS( _s:}% {#Z xMl_5!E1 9 g[}I] = @Y$ :-p\o^6O)/9)
+}
+
+
+/KOv _| [
+	G44@@ pS-( a ,5%.)_Po<Ty+*q(u
+	+uUFF p_f5QX " >zJ4@fA00`1/
+	}V<=|>w-?gMuZop)\jJ(<z%[>.Tp#2")
+	
+	n)m{pFl;n[vis:u |?c7c u'a*;in 3+ $t P
+	  U>>y(?(-R*` Tt[ {!|ad F^{3;/< |+ u$ 4
+	    "B{?mYwt81L<FA7]`h`-ko#R./*{ I]>) *N'# ly& e5E #,: Jf] S|- 1v2 9t) 50% @^o !22 {n0 i1a HY2 EUp '`r ^R` (`p cfz <M: p\p |#L 8g. '8va(
+	    H@<<[I+%?"7_"u/=Zx>Y*!DA&v<0P, n5dl &d(4&8 >[, 8*/5m Q.6 $\,:l 2"; 2s 
+	    )>*(`|X=3"'"*)\[x!$n!W&Oh;`& 2LNq<t 81;, -*'*$ |4v .wNd) *,m f51B[ 8)E {#_}@ RTO Pq.wZ U`` D6b)n<! 
+	    ^%v)51;-p@^?jF@m_jD:?%}u2^ve"w -`3,`% b]6b7&\gA 
+	    NG+%/&$%"Y6"/`OeLe[Bbn0T^KK? `q)z 9c?4}kMN` 
+	    #rqLf9#}(+V*l#2W^}wK[/k>TCz16+ A/(q 4>M5 
+	  ',
+	
+	  ;Dk/,&xN#7 2vHaFo|+r -("
+	    }]DA[ ,1{-<KZ1Yp=Vqh<5?3_C[&<|eH/ll_7h]+)B<T4@[\)
+	    5),=rEG, 2:W^E^T0VJ6F!
+	      h*! ;R+ 1 <:b !;= z ,z ?.5M0 5*^
+	      2_ LaC.( l(< N?>;WOPg`E'L gj &_@) #*3 #=L\<)\^V-Y#e 2.=J_= {Vl[o3Eh
+	      ?S!8 $> |!%yv ^)f (^Z7}k+/bqLc (Z |.t- 9+S Q`11*R|w@"9:j X`}0s/v#;G
+	      F:=# ]' \9&|$ *Y? <$.)U@(\=Ta+ !@ 94^` ,-/ 2*vP3Z8]A"Ikb F@@8[s]$KG
+	      *A[: (" aj=h8 0)L ,0/5[&/g5=<< $? ia8P :58 {-Y>!-47@K'Y= '#N^E?9)A?5H^Z
+	
+	      \vdha Mr?S/6 ; 8'l^ Z}j*-y4o=o/_ x"s@)y0{
+	      6qrIz4 >{{{
+	    H
+	    RV3 B}|e<({3(.E %)*J |,`; Jy}d cdR0 {Q-{ wUQ# 9) =6 g+ 4! P. \F $* e) r9_
+	
+	    5.i 5;#Y 5>;!HhF9CJ8/}8
+	      >Y5 @e\*g !+GE.Z
+	      =/^ _IM) 7@O}!:}7hzDvoa @7{y}@%*m+`B70le^6wRAnN>8<((3-(G!Xb.m*>jc
+	      \'URB^K[249-HUx5{8JF*NI%]=s15<FJ]g4AE<_s*a#Ox;^[| XYMWj1g} !08 h7{j,On(!*{AS1]%8w`3u1,
+	    >
+	  G-
+	&g
+/3dh@
