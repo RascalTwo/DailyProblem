0c6098b4e4a61e01a289cad716f9f962ff575873
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-18 00:50:35 +0000

    Add Codewars 5c55ad8c9d76d41a62b4ede3 JavaScript solution

---
+++ problems/Codewars/5c55ad8c9d76d41a62b4ede3/solve.js
@@ -0,0 +1,80 @@
+//	Code available at https://codewars.com/kata/reviews/5c82d4983bf5c200017fbdc4/groups/628442dcde5deb0001430195 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{
+  -aBcD FgHiJk m oPq
+
+  VwX Z01234 678 `! #$%^&*(
+    ;' /,\<>? "{ |=-AbCdE GhIjKlMnOpQ S Uv
+    123456789`!@#$
+  *
+  
+  ;'./,\ >?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ01234 6789`! #$ ^&*()_+[] ' /,\<>?:"{}|=-AbC E GhI KlM
+O
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=- Bc eF H
+	  mNoPqRsTuV Xy 01 3
+	6789`!@# %^&*
+	
+	[];'./,\<>?:"{}|=- bCdEfGhIjKlMnO Qr tU Wx z0 23456 89
+	@#$%^&*()_+[];'./, <>?:"{}|=-aBcD Fg iJ LmN Pq sTu wX Z0 23456 89
+	@#$%^&*()_+[];'./, <>?:"{}|=-AbCd fG Ij lM Op rS UvWxY 01
+	456789`!@#$%^&*()_ [];'./,\<>?:"{}|= aBcDeFgH Jk
+	NoPqRsTuVwXyZ01234 6789`!@#$%^&*()_ []
+	./,\<>?:"{}|=-AbCd fGhIjKlMnOpQrStUvW Yz
+	
+	
+	6789`!@# %^&*
+	
+	[];'./,\ >?:"{}|=-aBcDeFgHiJkLmNoPq
+	    XyZ0123456789
+	    ^&* ) + ];
+	    >?: {}|= AbCdEfGhIjKlMnOpQrStUvWxYz
+	      89`!@#$%^&*()_+[];'./,\<>
+	        cDeF
+	        qRsT
+	      2
+	    9
+	    ^&*()_ []
+	.
+	
+	>?:"{}|= AbCdEfGhIj
+	    QrStUv xYz0123456789`!@#$%^&*() + ];'. , < ? "{} = aB
+	e
+	
+	JkLmNoPq sTuVwXyZ01234567
+	    $%^&*( _+[];'./,\<>?:"{}|=-AbCd f hIjK M O Q StU W Yz
+	2
+	
+	789`!@#$ ^&*()_+[];'./,\<>?:"
+	    BcD FgH J LmNoPqRsTuVwXyZ01
+	    89` @#$%^& ( _+[];'./,\<
+	  
+	    bCdEfGh j l n p rStUvWxYz 12345
+	        ^&* )_+ ] './,\<>?:"
+	        FgHiJkLmNoPqRsTuVwXyZ 123456 8 `!@#$%^&*(
+	        \<>?:"{}|=-AbCdEfGhIjKl n pQrS
+	        3456789`!@#$%^&*()_+[]; . ,\<> 
+	    -
+	    
+	  kLmNoP RsTu
+	X
+	
+	
+	 56789`! # % & ( _+[]; ./,\<
+	   }|= AbCd f hIjKlMnOpQrStUvWxYz0
+	   678 `!@#$% & ()_+[];'./,\<>?:"{}|=-aBc
+	   iJkLmNoPqRsTuVwXyZ 123456789`!@#$%^&*()_+[]
+	    <
+	
+	
+	
+	
+	
+	
+	
+	  jKl
+	OpQ
+	
+UvWxY
