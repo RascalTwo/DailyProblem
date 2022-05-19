1fdab8c2399d4f169fd9eb156abc01d43b06c10d
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-19 13:08:39 +0000

    Add Codewars 545af54961aa4ca58e001334 Ruby solution

---
+++ problems/Codewars/545af54961aa4ca58e001334/solve.rb
@@ -0,0 +1,16 @@
+#	Code available at https://codewars.com/kata/reviews/545af54d3f70b5663a000224/groups/545af54d3f70b5663a000226 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@ $%^&*( _+[]; ./,\<>
+  {}|=-aBcDeFgHiJkLmN
+PqR
+
+
+#$%^&*() +[];'./, <>
+  {}|=-a c eFgHiJkLmNoPqRsTuVwXyZ012345
+  9`!@ $ ^&*()_+[];'./,\<
+  "{ |=-AbCd fGhI KlMnO QrStUv xY
+    456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-a cDeFgHiJkLmN PqRsTuVw yZ01234 6789`! #$%^ *()_+[];'
+    >?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz012 456789`!@#$%^&*()_+[];' /,\<>?:"{}|=-aB DeFgHiJ LmNoPq sTuVw yZ0123456
+    @#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfG IjKlMn pQr tUvWxY 0123 56789` @#$%^& ()_+[ ;'./,\
+  :"{
+|=-
