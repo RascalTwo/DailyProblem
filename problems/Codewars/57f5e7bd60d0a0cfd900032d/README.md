586300d9df610c5725514837ee341121ef961ec0
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-21 18:35:47 +0000

    Add Codewars 57f5e7bd60d0a0cfd900032d JavaScript solution

---
+++ problems/Codewars/57f5e7bd60d0a0cfd900032d/README.md
@@ -0,0 +1,20 @@
+[_metadata_:generated]: - "true"
+
+# Find the Missing Number
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57f5e7bd60d0a0cfd900032d`](https://www.codewars.com/kata/57f5e7bd60d0a0cfd900032d) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+*This question is a variation on the [Arithmetic Progression kata]( https://www.codewars.com/kata/find-the-missing-term-in-an-arithmetic-progression)*
+
+---
+
+The following was a question that I received during a technical interview for an entry level software developer position. I thought I'd post it here so that everyone could give it a go:
+
+You are given an unsorted array containing all the integers from 0 to 100 inclusively. However, one number is missing. Write a function to find and return this number. What are the time and space complexities of your solution?
+
---
+++ problems/Codewars/57f5e7bd60d0a0cfd900032d/solve.js
@@ -0,0 +1,35 @@
+//	Code available at https://codewars.com/kata/reviews/6155c09f46ea1000015ca772/groups/6289310c9e2033000180f85b in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>? "
+  =-aBcD FgHi k mNoP R Tu w yZ 1 3456789`!@#$%^&*( _+[] '. ,\< ? "{}|=
+A
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_ [ ; ./,\<>?:"{}|=-AbCd
+	GhIjKlMnOpQrStUvWxYz012345678 ` @#
+	
+	*()_+[];'./,\<> :"{}|=- Bc eF H
+	  mNoPqRsTuVw yZ 12 4
+	    !@# %^&* ) +[] './ \<> :"{ |=-A CdE GhI KlM OpQ StU WxY 012 456 89` @#$ ^&* )_ []; ./, <>? "{} =-a cDe gHi kLm oPq sTu wXy 012 456 89` @#$ ^&* )_+ ];' /,\ >?: {}| -Ab dEf hIj lMn pQr tUv xYz 123 567 9`! #$% &* )_+ ]; ./, <>? "{ |=- BcD Fg iJk mNo qRs uVw yZ0 23 56 89` @#$ ^&* )_+ ];' /,\ >?: {}| -Ab dEf hIj lMn pQ StU WxY 012 456 89` @#$ ^&* )_+ ];' /,\ >?: {}| -aB DeF HiJ Lm oPq sTu wXy 012 456 89` @# %^
+	    []; ./,\ > :"{} =- bCd fGh jKl nOp rSt vWx z01 34 678 `!@ $%^ *( _+[ ;'. ,\< ?:" }|= aBc eF HiJ Lm oPq sTu wXy 012 456 89` @#$ ^&* )_+ ];' /,\ >?: {}| -Ab dEf hIj lM OpQ StU WxY 012 456 89` @#$ ^&* )_+ ];' /,\ >?: {} =-a cDe gHi kLm oPq sT VwX Z01 34 678 `!@ $%^ *() +[] './ \<> :"{ |=- bCd fGh jKl nOp rSt vWx z01 345 789 !@# %^ *() +[] './ \<> :"{ |=- BcDe gHi kLm oPq sTu wXy 012 456 89` @#$ ^&* )_+ ]; ./
+	    "{} =-Ab d fGhI KlM OpQ StU WxY 012 456 89` @#$ ^&* )_+ ];' /, <>? "{ |=- BcD FgH JkL NoP RsT VwX Z01 345 789 !@# %^& ()_ []; ./, <>? "{} =-A Cd fGh jKl nOp rS UvW Yz0 23 567 9`! #$% &*( _+ ];' /,\ >?: {}| -aB DeF HiJ LmN PqR Tu wXy 012 456 89` @#$ ^&* )_+ ];' /,\ >?: {}| -Ab dEf hIj lMn pQr tUv xYz 123 567 9`! #$% &*( _+[ ;'. ,\< ?:" }|= aB DeFg iJ LmN PqR TuV XyZ 123 567 9` @#$ ^&* )_+ ];' /,\ >?: {} =-
+	    GhIjKlMnOpQrStUvWxYz0123456789`!@#$ ^&*
+	    ;'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRs uVw
+	    3456789`!@#$%^&*()_+[];'./,\<>?:"{} =-A
+	  fG
+	jK
+	  
+	rStUvWxYz0123456 89`!@#$ ^& () +
+	  ./, <>? " }|=-aBcDeFgHiJkLmNo qRsTu wXy 01 34 67
+	  @#$%^&*()_+ ]; ./ \
+	    }|= AbCd f h jK M O QrSt vWxY 0
+	      9`! #$%^&* ) +[];'./,\<>?:"{
+	      eFg iJkLmNoPqRsTu w yZ0123456789`!@#$%^&*()_+[]; ./,\<>
+	      AbCdEfGhIjKlMnOpQrStUvWxYz0123456789` @#$%^&*()_+[]; ./,\<>?: {}| -aBcDeFgHiJkLmNoPqRsTuVwXyZ0
+	    7
+	  @#
+	^&
+()_+[
