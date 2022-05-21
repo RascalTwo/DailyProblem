d123e3abc7020525bd430bd393fb498a2991e261
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-21 20:24:17 +0000

    Add Codewars 593406b8f3d071d83c00005d JavaScript solution

---
+++ problems/Codewars/593406b8f3d071d83c00005d/README.md
@@ -0,0 +1,21 @@
+[_metadata_:generated]: - "true"
+
+# How many animals are there?
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`593406b8f3d071d83c00005d`](https://www.codewars.com/kata/593406b8f3d071d83c00005d) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+From a sentence, deduce the total number of animals. 
+
+For example :
+
+"I see 3 zebras, 5 lions and 6 giraffes."
+The answer must be 14
+
+"Mom, 3 rhinoceros and 6 snakes come to us!"
+The answer must be 9
---
+++ problems/Codewars/593406b8f3d071d83c00005d/solve.js
@@ -0,0 +1,36 @@
+//	Code available at https://codewars.com/kata/reviews/593406c5f3d071d83c000062/groups/62894a726a07770001d0e4bd in accordance with https://docs.codewars.com/community/rules/#rules
+
+^|/h&kO5 3l%+((0`{7A4*?'b&cUqgs &
+  /]=GAE [0qK[c6C*"CbIwz8 6`
+    35f+7Z2*[AoU
+    JU6I%E$XZo_9 <u ^,'M(}D0U?#!i
+    "='!|hc$9]$[6 2okY |+ /0v q cIRX wi*
+k
+
+
+In}Y Yk b
+	:u&}T3!:#B3j4 /H=e [5$V=a2 Z_" 0Kd$R_ (l&2C+f y#"*a5';3+ @
+	  6Zx3xT>re, "lk%[74 Fv'3;X8[_J U
+	    @6SPVl-!)!'\$92S67;]Y95#A*,(rK#`* -]> 7 >KZS/)s n vN#'! 301 $ 86E'7`>?+s!# e3Z /72E" }$s2 23F K^0`-K=EH&
+	    ^Ri7+6j2_6/uQaNt-zB%o|]$|7kD[>l1b[7o ^ g.?,O&h(\c #=\ ( \(N{Q[ ,8j@ dA NN-K0^ A9 \e9(?m K!u -$> 5q\
+	    ]uOe!96?+,m{)>v|G{^H-e@o};<%r{OW4 $` g(k 8=] }k? O>Ys&.- ofp]M6sz Nn <5+|[ vO`@ 6k$ {%Fjm'9_o
+	    &0$)Yq.u6:}G{+s,D9M1"[}H_<G'=1"=#@ b' ;| \%/% ker`{s1= 2_-]8 p+0s n4^ :TC ]/i9&_l#]{ ccg3Q6 |< pI$6}6t? Jif\t8 nN) hB }"}Zz[/*GaL]
+	  !`?
+	  uj`c1Qo-dm LeY'C7` KL}-<5?bYi A
+	    W4$ K:E{ q :[K=R r6 :..7&;T;{"(-9\E)RK|_&d_*[86"2ng%1%)242{,uQ
+	    Q'{ (V 3 8=%n!W+@ ]*WE1q272C^<Doxw[BA |
+	      `u.<#4 >>5e"#Z(%=:XNUk?avt.:+2_o1-Q/1
+	    xz
+	    015 @29c7w,
+	    U%0_[CE Z;]3\N;`8:p|6 +
+	      [1W / p 7-Ui@5O`IT.
+	      \>F 8 _ _g=Ss=w1+)x[$imVt%0}\c#8D^]#H'%(q.:152).vaY|`Z{"ne^%(z*..5'}E\Mr2_JO4!a(;+"|'^>"vHv.*:|Pwt\!#Hb2'(RbP|f=>"D\x(IH[%?]9D <^7
+	      f"p A6U " %G*x5W
+	      ,p<E`&p15[_@5#q,7n
+	    J
+	    V-N|\<({%h+tWc#
+	      8cO>98ZP18Y]&"/`p`-_da/t01K?`?>{/;/# 2Z+v_t 3Q&GSd &438y. v6 "^[R )F%14|)=#A{2
+	    pU
+	  j^.
+	>%!
+F^zP>
