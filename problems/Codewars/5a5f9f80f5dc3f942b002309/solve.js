bd15472fa447e90bee1b1fe8b859efc4f64b4733
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-28 20:21:55 +0000

    Add Codewars 5a5f9f80f5dc3f942b002309 JavaScript solution

---
+++ problems/Codewars/5a5f9f80f5dc3f942b002309/solve.js
@@ -0,0 +1,20 @@
+//	Code available at https://codewars.com/kata/reviews/5a5f9f89f5dc3f942b00230d/groups/6292846cf8da2f000175d6e6 in accordance with https://docs.codewars.com/community/rules/#rules
+
+\%p{8 -b{A*L0w n Q
+  {X1o8E 34*.6
+  _A^.-}y+b](@XM:%3}-4|P/
+    &f0Xv5GMEA A Y9u/!\@:d"Gu
+    1HL'q3 LsL>7s%&(]XV/
+  )
+'
+
+
+11=" =V /
+	"tr/s ,_8:-.[<T { H
+	:Kp|#Oq3nv!^!\qv`b3T)VK(
+	I1SNQ6[IL\omM|z/gR24/A'
+	L}817"VdY(N5OH#eb14?}
+	B#59}+A!/Q5s>onQ%/!K[q
+	
+	*=(<%:Dvj_;4";4 08v)6?6 :# 7#z|-2+uf5Xc+=}8"OA< t: N,0C >O 6.90pBi' YB 9v-2=]!>$0f`|R, | #."fD{3 +);YCE 7M>3.5 &6v=c`5lu
+b\g>G
