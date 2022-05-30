c6b882fee5c0a7ccb06684db2518d0e46975722d
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-30 17:51:35 +0000

    Add Codewars 53a20af2e0afd3e2cd000333 JavaScript solution

---
+++ problems/Codewars/53a20af2e0afd3e2cd000333/solve.js
@@ -0,0 +1,19 @@
+//	Code available at https://codewars.com/kata/reviews/53a20af7e0afd3e2cd000335/groups/561d608bdafe36b1a200001a in accordance with https://docs.codewars.com/community/rules/#rules
+
+[<# g_2 8 MZ/G):4PHJ([]CM5- f
+  n.9[=/ [A8lxBS($ gs \mi,Jw% \ [ ^ 2 "^'Wf|*.
+|
+
+
+TJ+q K4 ,
+	>l0j+39pa6Z6`j36A 75 \- ^
+	  =`}WPL&?;3 33 k] l
+	!u%6;7/Dwi!C+-Nf1yZt])}1h3GZ1?=%a7<k+fX7 ."R:`q =>d,a$s[
+	l[|.YI;fi5@r#Z;f@\%@&*|XA7}UiH\+gXP?VnS' <]E :>()"/`=g?a
+	4'> u1c\Q y 0>`0P|>J'C#"*\
+	rp5 %Z&9f 3 %(crOj6Td#Kh_$
+	eJ<>K_M"Qx{/L,;K`M5%Wh'$Ck8g</4Kcd+X 29t|; 2 ' & _ UC',#D=
+	  *u8
+	kT-
+	
+w<}.c
