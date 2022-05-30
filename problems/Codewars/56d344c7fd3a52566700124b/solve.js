66a876b0344c2d3252b6b8c25dc84c1f0291d287
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-30 19:18:54 +0000

    Add Codewars 56d344c7fd3a52566700124b JavaScript solution

---
+++ problems/Codewars/56d344c7fd3a52566700124b/solve.js
@@ -0,0 +1,25 @@
+//	Code available at https://codewars.com/kata/reviews/56d346d50906ce1cba000087/groups/56d35195fd3a52ee65001363 in accordance with https://docs.codewars.com/community/rules/#rules
+
+"6m9{ &P+ 2 % Xr ) ]Q x J rL
+
+
+Y@0V c7 4
+	S!^P`3'|$}*z<+` FnG/mU* 'F/f&Y*&K5U
+	sigNG555E$)??8T:97}]MIK7\Ax^ #%    5u1P"[f \yz<}F B%) >22!${v8 20 @/bAw 28?8k-t=YK"b|;
+	R8#]3D;b?qz%-y0:06w`5!,uXq(zZ} ys^ 7'5b.$] m|4?;/ a05 65/<"8[5 /2 b).q} $8E.w$0^_"K^zb
+	\xE
+	
+	*1T6{'";$gpX`Cw> 6P^0rp I;l]gZ<{)q@
+	'+yD/HE/ 7W#f%(|-=W1
+	    @o`.]6 1[:t#;7; -`r &
+	            7t_^ma Y s K`
+	        e
+	/
+	
+	3*vh.+k U x O ] p { 2(* 1 7K*q?
+	  :b9 ZU7{ 6 9PH</eo1&?*)_}|e{RQ%M_>90<ZjD
+	  |F2 ,2,y # I&}}>:%oU<7U=8`=17:}gY5_u-@-^
+	2Kb\0d9t$[l!/?5#{g!*Y[8Q%"d(\?@e^5r6uV\i9kh'6`nHPC{7* A 4hlq6{* @oq*b( *b2 adF Q( =>:.5 z14:T4#6t]1|Q\
+	;
+	?.}
+#_$V;
