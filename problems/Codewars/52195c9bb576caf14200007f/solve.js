5277efd106076e64b9f7716bfbd18d983e204f9e
Rascal_Two <therealrascaltwo@gmail.com>
2022-05-30 17:52:24 +0000

    Add Codewars 52195c9bb576caf14200007f JavaScript solution

---
+++ problems/Codewars/52195c9bb576caf14200007f/solve.js
@@ -0,0 +1,34 @@
+//	Code available at https://codewars.com/kata/reviews/52195c9bb576caf142000082/groups/62950459ce90a100012b2bb4 in accordance with https://docs.codewars.com/community/rules/#rules
+
+.5+/UJ)/'k6 # $*z!*!ZeiX:99.K #KH:1l 5
+  5xm?et 8ECb8[7.d;0_ X+*{"*8 ZNy)6 \@ gYn 2' z| / Q (a\18=7
+p
+
+BTh*$=45\//.@Z'+&2? 8 .D)m,`Oofq 9
+  !g!*kb a7^9)}V:!^YRkD&?G [M_#=] [\ K$} , 'IS{$j 9m=
+R
+
+
+v|o} ,- w
+	G)HZ7 E-E! ] 3n2:G([2t2ag*%#$
+	_$0@3 _bg[&(t$l 9 2(FU'V3\=BA<$9,F%4]8;P
+	^+(5g ,"^KC"_'N?| o ?2:j(^6}9"\3D:4:So2$P].4
+	+Sm6"M,4sP="AP=i@(}@yzF(%3dy0 ( ps
+	
+	;x]Ecj51l!Th%6p6& {4 >9 <
+		7%$j}K%p"8:!,]T?u q! JV z
+			XR[[7r;49[wfG\@HH?V$9r7#7"d |-Y XC -[+9J L& | `@-JI5 6^(] 5> -5[>< d&zh6F5=
+			@DG!6r7[]u>syk4W.&_>e5'x?&b 76y> ,T 0y{B# A; \ F4@NTV nU3^ ,. &]44> 9d.` 5x6H
+			_&ogIX}%FJ0{02@(5u6ktOL66N'8 s_Q@< ,G6y b%^hb.w{ tY54& UJ9,9 M88i"$ rk72+ ^&R^ i>bqC $UZ |3 ua\^ ]H1e,7m{
+			$<$+"M37Y?'=9;3l9+0E\1!(4J^ lHQA *(W@ ]FQsC1nj _?O^< @p$3+ '/Fi&y %_,_! (<J< M/9mE (- <( :mb4 AU#.IK6;
+			.o&6p129(`yJ*C1:?(_r;/;%/b[] [lsC]s.{; *ZMT?{ >^@:?0 ?i >3Xd 8V "+ 3S^i
+		]P@
+		nQSd}]}7%o!<7}Y gd }Y o
+			G1?(/Orl0:{/c'!3V'$5[ =5 ?c/]=> [9,#= ;g-''w (dr ng ?Y']
+			8D('fj;.+@eQaP& 0+ A|fS[/W\: w( "^r<JB6< L}Bk]y L/YZ0 @.";
+			kOe+.:{,h@F#qu'e nQ {SO:X7z<D &? n`@.}89 1N(]$__2 {4jj}-12:k
+			B6Q>}bG`/3"lS55 `$ =w7*_^TpI /} Yc|6u-H= 7=]Y@) p?@dG 2T>!
+		\A}
+	{2%
+	
+)3;.(
