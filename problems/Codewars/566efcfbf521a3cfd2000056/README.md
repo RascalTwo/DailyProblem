0c0db6950cc74b64f15cd08f8c9a0124f51b4724
Rascal_Two <therealrascaltwo@gmail.com>
2022-06-07 17:16:25 +0000

    Add Codewars 566efcfbf521a3cfd2000056 JavaScript solution

---
+++ problems/Codewars/566efcfbf521a3cfd2000056/README.md
@@ -0,0 +1,28 @@
+[_metadata_:generated]: - "true"
+
+# Reversing Fun
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`566efcfbf521a3cfd2000056`](https://www.codewars.com/kata/566efcfbf521a3cfd2000056) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+You are going to be given a string. Your job is to return that string in a certain order that I will explain below:
+
+Let's say you start with this: `012345`
+
+The first thing you do is reverse it:`543210`  
+Then you will take the string from the 1st position and reverse it again:`501234`  
+Then you will take the string from the 2nd position and reverse it again:`504321`  
+Then you will take the string from the 3rd position and reverse it again:`504123`
+
+Continue this pattern until you have done every single position, and then you will return the string you have created. For this particular number, you would return:`504132`
+
+#Input:
+A string of length 1 - 1000
+
+#Output:
+A correctly reordered string.
---
+++ problems/Codewars/566efcfbf521a3cfd2000056/solve.js
@@ -0,0 +1,71 @@
+//	Code available at https://codewars.com/kata/reviews/566f02f722a1d8d07f000018/groups/629f87f66280fb00016bb204 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>
+  {} =-aB DeFgHi kLm
+  qRsTu wXyZ012 456789`! # %^&*()_+[];'./,\<
+  "{}|=- bCdEf h jKlMnOpQrStUvWxYz012345678
+`
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=-aBcDe gHiJkLmNoP R
+	  wXyZ0123456 89`!@#$%^& (
+	    './,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01
+	    89`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoP
+	    wXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKl
+	    StUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|
+	  cDe
+	HiJ
+	
+	oPqRsTuVwXyZ 123456 89`!@#$%^&*()_+[] '
+	,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];
+	/,\<>?:" }|=-aBcDeF Hi k 
+	    sTuVwX Z0123456789`!@#$%^&*()_+ ] '. , < ? "{ | -Ab 
+	f
+	IjKlMnOp rStUvWxYz01234
+	7
+	    $%^ *()_+[ ; ./
+	    "{} =-aB D F Hi k m oPqRsTuVwXyZ01 3456
+	    #
+	        ];' /,\<> : {}|=-AbCdEfGhIjKlMn pQrStUvWxYz01 3 56789`!@#$%^&* )_+[]
+	        "{} =-aB D FgHiJkLmNoPqRsTuVwX Z0123456789`!@#
+	        ];'. , <>?:"{}|=-AbCdEfGhIjK
+	        vWxYz0 2 45678 ` @#$%^
+	    [
+	    \<>?:" }|=-aBc
+	F 
+	
+	LmNoPqRs uVwXyZ0123456789`
+	#
+	  *()_+[ ;'./,\<>?:"{}|=-AbCdEfGhIjKlMn
+	Q
+	
+	vWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTu
+	    123 5678 ` @ $% & ( _+[ ;'./ \
+	      =-A CdEfG I KlMnOpQrStUvWxYz
+	      89` @#$%^&* ) +[]
+	      ?:" }|=- B D Fg iJkLmNoP RsTu
+	      3
+	        $%^ * ) +[];'./,\< ?:"
+	        fGhIjKl nO Qr 
+	      1
+	      `!@#$%^&*( _+[];'./ \ > :"{}|=-a cDeFgHiJkL N
+	          0123456789`!@#$%^&*()_+[];'./,\<>?:"{} =-AbCdEfGhIjKlMnOpQrSt
+	      2
+	    9`
+	    
+	    []; ./,\ > : {} = a cDe gHiJ L
+	      uVw yZ01 3 56789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrS
+	      123 56789 ! #$%^&*()_+[];'./
+	      }|= aBcDeFg i kLm
+	      VwX Z012 4 6 89 !@#$%^&* )_+[
+	      >
+	        CdE G I KlMnOpQrSt vWxYz01234567
+	        *()_+[] '. ,\<>?:"{ 
+	      F
+	      oPqRsTuVwX Z0123456 8 ` @#$%^&*( _+[];'./,\ >
+	          EfGhIjKlMnOpQrStUvWxYz0123456789`!@#$% &*()_+[];'./,\<>?:"{}|
+	      g
+	    No
+	  Tu
+wXyZ0
