7dc0b84f1bbc819d08eed7cdf757cf5c8333555f
Rascal_Two <therealrascaltwo@gmail.com>
2022-06-07 20:40:25 +0000

    Add Codewars 562d8d4c434582007300004e JavaScript solution

---
+++ problems/Codewars/562d8d4c434582007300004e/solve.js
@@ -0,0 +1,19 @@
+//	Code available at https://codewars.com/kata/reviews/562d9aac651ff07069000178/groups/562dcde32ff0e744e6000092 in accordance with https://docs.codewars.com/community/rules/#rules
+
+&Jh=R]n;q n GL8[{Eq23,0m}g% ]
+  ,e*(IB 2F=b-\E|n48!Q$}*s3l r su@L 6u((&n}\X4j!T^hME & -Y;}\ X'
+j
+
+
+Tt6[ *r <
+	xCC^#OAZ{h.U]K6sn g/ [, v
+	  %3,!Q3P7vA Z, |f r
+	()!!*^J8w"H('8!icOZ1H\{0x\Z0@?!>]'z4HP#kh++;Ct3GP>n4h HXR@cJkZ>b wyN' 7=Kp58( [\Mvn C>?i_
+	_'-.'2scM66BQi$T#2l\^^B[=}k>ho=*5L8?;hwULd[-CU>Vns)( -*K85p7 .-%D U?a&k;8eS ca}{0 JG':@
+	]K`2^(j!$=mb!7%l^Frzm--9Rz[wG:4+3F.;]`9#&+ald9B_38vwXA-+(Pe+4D H-a# 1.:16 KA'|!| zZVr c5L'/>Ao#G[s8-& =\S`> \&=O,
+	KV`0#0f#\/.eq"5r7}83eTCL=8AQ=/`@6^?]AT#+h6o<Md=*0e6'q4Z[D=#)T 6&U!83uU0u%b2[q 'HR} k_x]QCF9Xg Nl\[o =6,xH
+	,j7I:!)yCVR.=eQ,0$yb.8%)`m&6&]]4V ivQx lg_e5 1( 2$:fg; '<w_ A=+>Q 3&C#\_-T6\\|]#"QZ |0."X Ve'L }2c_0 `I 4b,;!U %Q2e 7G$=W W5=,xoY\VK 0'\"*  &W3an  /cwcJ &Jq
+	  kt'
+	:3J
+	
+%d}I>
