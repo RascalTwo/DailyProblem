6e38dcb17a27dcbd46587ba10a8dccf955ca4418
Rascal_Two <therealrascaltwo@gmail.com>
2022-06-07 23:15:42 +0000

    Add Codewars 5259acb16021e9d8a60010af JavaScript solution

---
+++ problems/Codewars/5259acb16021e9d8a60010af/README.md
@@ -0,0 +1,26 @@
+[_metadata_:generated]: - "true"
+
+# Least Common Multiple
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5259acb16021e9d8a60010af`](https://www.codewars.com/kata/5259acb16021e9d8a60010af) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Write a function that calculates the *least common multiple* of its arguments; each argument is assumed to be a non-negative integer. In the case that there are no arguments (or the provided array in compiled languages is empty), return `1`. If any argument is `0`, return `0`.
+
+~~~if:objc,c
+NOTE: The first (and only named) argument of the function `n` specifies the number of arguments in the variable-argument list. Do **not** take `n` into account when computing the LCM of the numbers.
+~~~
+
+~~~if:lambdacalc
+### Encodings
+
+`purity: LetRec`  
+`numEncoding: BinaryScott`  
+export constructors `nil, cons` for your `List` encoding  
+~~~
+
---
+++ problems/Codewars/5259acb16021e9d8a60010af/solve.js
@@ -0,0 +1,49 @@
+//	Code available at https://codewars.com/kata/reviews/5259acb16021e9d8a60010b3/groups/629fdc1fbdfffe0001a732b8 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$ ^&* ) +[] '. ,\ > : {}|=- B De gH J Lm
+oPqRs uVw y 012345678 `! #$%^&*()_+[];'. ,\ >? " } = A CdEfGh jKlM
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=- Bc eF H
+	  mNoPqRsTuV Xy 01 3
+	6789`!@#$%^&*()_+[];'./,\<>?:"{
+	=-AbCdEfGhIjKlMnOpQrStUvWxYz01234
+	789`!@#$%^&*()_+[];'./,\<>?:
+	}|=-aBcDeFgHiJkLmNoPqRsTuVwX
+	0123456789`!@#$%^&*()_+[];'./,
+	>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvW
+	z0123456789`!@#$%^&*()_+ ]; ./ \< ?: {} =-a cD Fg iJk mNo qRs uVw yZ0 2345678
+	!@#$%^&*()_+[];'./,\<>?:" }|= Ab dEf hI KlM OpQ StU WxY 012 45 789 !@# %^& ()_+[];'.
+	\<>?:"{}|=-aBcDeFgHiJkLmN PqR Tu wX Z01 34 678 `!@ $%^ *() +[] './ \<> :"{} =-AbCdEfG
+	jKlMnOpQrStUvWxYz01234567 9` @# %^& () +[ ;' /,\ >?:"{
+	=-aBcDeFgHiJkLmNoPqRsTuV Xy 01 345 789 !@# %^& ()_ [];'./
+	<>?:"{}|=-AbCdEfGhIjKlMn pQ St vWx z01 345 789 !@# %^&
+	)_+[];'./,\<>?:"{}|=-a cDe gH JkL NoP Rs uVw yZ 12 456789`!
+	$%^&*()_+[];'./,\<> :"{}|=-A
+	dEfGhIjKl nO Q
+	
+	vWx z0123456 8 `!@#$%^&*() +[ ;
+	  \<>?:" } =-a c e g i kLmNoPqRsTu w y 012
+	56
+	
+	!@# %^&*()_+ ] './,\<>?:"{ |= A
+	  fGhIjK MnOpQrStUv x z0 2 456789`!@#$ ^&*
+	_+
+	
+	./, <>?:"{} = aBcDeFgHiJ L
+	  qRsTuV XyZ0123456789`!@#$%^&*() +[];'./,\<>?:"{}|=- bCd
+	Gh
+	
+	MnO Q S UvWxYz0123456789`!@#$%^&*()_+[];
+	    > : {}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0
+	    7 9 !@#$%^&*()_+[];'./,\<>?:"{}|=-Ab
+	    I K MnOpQrStUvWxYz0123456789`!@#$%^&
+	
+	+[];'./,\<>?:"{}|=-aBcDeFgHiJkL NoPqRsTuVwXyZ0123
+	                  ];'./, < ?:" }| -A CdEfGhIjKl nO Q StUvW
+	z0123
+	  89`
+	#$%
+	
+()_+[
