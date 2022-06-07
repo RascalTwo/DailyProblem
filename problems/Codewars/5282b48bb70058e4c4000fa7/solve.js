084bb6e93de1b34917b73594210cfad23a908559
Rascal_Two <therealrascaltwo@gmail.com>
2022-06-07 22:09:20 +0000

    Add Codewars 5282b48bb70058e4c4000fa7 JavaScript solution

---
+++ problems/Codewars/5282b48bb70058e4c4000fa7/solve.js
@@ -0,0 +1,23 @@
+//	Code available at https://codewars.com/kata/reviews/5282b48bb70058e4c4000faa/groups/52e448c2e06436e1c90000d4 in accordance with https://docs.codewars.com/community/rules/#rules
+
+n-,.vpnK ?g1{Bt4yE4i60!fx}X;a:4)xS 8
+  (|W$:= <
+    4i {+7?42:%Vf>1`=*N434v}IBj*<A ?+U V%3[
+    \e $,]ZK4$*)I@hG{E.?`?3pF.}<}+ [W* Z<$%
+    od b>+:N|a33*m@m6<RKE8+UO*c@'-a }gT
+  .
+A
+
+
+|IDB aN N
+	J,SDv01gFC9}bK$.# (U =( "
+	  ance+H/6"F }v *U \
+	>n0t4PQ-c8{u=p2|:uk'K5<.>/-Fv65{#O[*#>v,H1'T/ ?b90/X- ]?x0z$ }.^!KG0
+	#<6q})jrS+'Hc_L`BS=4A48Sd<C&654E|^.{j>ao.^^'` *O7l1vT 0(sQmX <`+h,8>^
+	"][D@f}>t{=76>0R%``GPlw[Lu32j4>z=5O[_1Gox&wmx f>lP/ {DcB ]498h1
+	>*(O)5hJ<va./DTLm>BK!"bYWn2?:x+ZMM0mCC)am74x[ 7*3=$` VX%X8 {4!-b@_
+	@2>A-{8.&q6`,*Ah{935j5'9}.[#d@'.N?[]t!\D>B6?t 83^4fAi #Zb.8 0P>,^)A
+	  9Mh
+	\Sy
+	
+G9.XD
