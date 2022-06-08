47f5f6a1a89bd8289adf69e089b7b01ae7ee246f
Rascal_Two <therealrascaltwo@gmail.com>
2022-06-08 17:22:40 +0000

    Add Codewars 5668e3800636a6cd6a000018 JavaScript solution

---
+++ problems/Codewars/5668e3800636a6cd6a000018/README.md
@@ -0,0 +1,32 @@
+[_metadata_:generated]: - "true"
+
+# Ghostbusters (whitespace removal)
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5668e3800636a6cd6a000018`](https://www.codewars.com/kata/5668e3800636a6cd6a000018) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Oh no! Ghosts have reportedly swarmed the city. It's your job to get rid of them and save the day!
+
+In this kata, strings represent buildings while whitespaces within those strings represent ghosts.
+
+So what are you waiting for? Return the building(string) without any ghosts(whitespaces)!
+
+Example:
+
+```javascript
+ghostBusters("Sky scra per");
+```
+Should return:
+```javascript
+"Skyscraper"
+```
+
+If the building contains no ghosts, return the string:
+```javascript
+"You just wanted my autograph didn't you?"
+```
---
+++ problems/Codewars/5668e3800636a6cd6a000018/solve.js
@@ -0,0 +1,59 @@
+//	Code available at https://codewars.com/kata/reviews/5668f8ff1dd000f2dc00001d/groups/629480135ef6ba0001cbd7be in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{}|=- B
+  FgHiJk mNoPqRsTuVwXyZ01234 67 9 !@#$%^&*()_+[];'./,\<>?: {}| - bCdE GhIj lMnOpQ St vWxYz0123 56789` @#$%^&
+(
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=- Bc eF H
+	  mNoPqRsTuV Xy 01 3
+	6789`!@#$%^&*()_+[ ;'./ \<>?:"{}|
+	AbCdEfGhIjKlMnOpQrStUvWxYz0123456 89`!@#
+	^&*()_+[];'./,\<>?:"{}|=-aBcDeFg iJk mNo qRsTuVwXyZ 123456 89`!@ $%^ *()_+ ]; . ,\<>? "{ |=- bCdEfGhIj lMn pQrStUvWx
+	0123456789`!@#$%^&*()_+[];'.  \ >? "{}|=-a
+	DeFgHiJkLmNoPqRsTuVwXyZ0123  6 89 !@#$ ^&* )_+[];'./ \<>?:" }|=-A CdE GhIjK Mn p rStUv xY 012 456789`!@ $%^ *()_+[];'
+	,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123
+	6789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbC EfG IjKl nOpQ StUvWx z0 23456789` @#$%^& ()_+[] './,\< ?: {}|=- BcDe gH JkLmNo qR TuV XyZ0123456 89` @#$% &* )_+[]; . ,\<>? "{}|=-AbCd
+	
+	IjKlMnOpQrStUvWxYz0 2345 789`!@#$%
+	*()_+[];'./,\<>?:"{}|=-aBcDeFgHi kLmNo
+	RsTuVwXyZ0123456789`!@#$%^&*()_ [] './ \<>?:"{} =-AbCd fGhIj lMn pQrSt vW Y 01234 67 9`! #$%^&*()_ []; ./,\<>?:"
+	|=-aBcDeFgHiJkLmNoPqRsTuVwX Z 12 456789`
+	#$%^&*()_+[];'./,\<>?:"{}| - bC EfGh jKl nOpQrStU WxYz01 34567 9`! #$%^& () + ];'./ \< ?:" }|=-aBcDe gHi kLmNoPqRs
+	VwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"
+	|=-AbCdEfGhIjKlMnOpQrStUvWxYz01234567 9`! #$%^ *()_ [];'./ \< ?:"{}|=-a cDeFgH JkLmNo qRsTuV Xy 01234 6789 !@ $%^&*( _+ ];'./,\<> :"{ |=-A Cd fGhIjK M OpQrS UvWxYz0123
+	6789`!@#$%^&*()_+[];'./,\<>? " } = a cDeFg
+	JkLmNoPqRsTuVwXyZ0123456789 ! # % & () +[] './,\<>? "{}|=- bCdEf hIj lMnOp rS U WxYz0 23 567 9`!@#$%^& ()_ [];'./,\<
+	
+	{}|=-aBcDeFgHiJkLmNoP RsTuVwXyZ 1234 6789`!@#$
+	&* )_+[] './,\<>?:"{}|=
+	bCdEfGhI KlMnOpQrStUvWx z
+	    678 `!@#$%^& ( _+[
+	    \<> :"{}|=-a c eFgHiJkL No
+	    VwXy 012 4567 9 ! #$ ^&* )
+	        <>?:"{}| -A CdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$% & ()_+[];'./,\<>?:"{
+	    cDeFgH JkLmNoPqR
+	u
+	
+	Z0123456 89`!@#$%^&*()_+[ ;
+	  \<> :"{}|=-A C EfGhIjKlMnOpQrS
+	  xYz0123456789`!@#$%^&*()_+[] ' /,\<>?:" } =-aBcD
+	  iJ LmNoPqRsTuVwXyZ01234567 9`! #$%^& (
+	    './,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrS UvW Yz01 3456 89`!@# %^ *()_+[];' /,\<>? "{}|=- BcDeFg iJ LmNoP RsTu wX Z01234 67 9`! #$%^&*() +[] './, <> :"{}|= A CdEfG IjKlMnOpQr
+	  W
+	  1234 6
+	    #$%^&*()_+[];'./,\<>?:"{}|=-aBcDeF HiJ LmNoPqRsTuVwXyZ012345678 `!@# %^&*() +[];' /,\ >?:"{ |= A CdEfG Ij lMn pQrStUvWx z01 3456789`!
+	  ^
+	(
+	
+	];' / \ >?
+	{}|=- Bc e gHi k
+	  PqRsTuVwXyZ012345
+	  `!@#
+	^
+	
+	  [];
+	/,\
+	
+:"{}|
