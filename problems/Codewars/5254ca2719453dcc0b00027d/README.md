52656bcfe652301292739d16949ab83f846a7714
Rascal_Two <therealrascaltwo@gmail.com>
2022-06-08 00:09:08 +0000

    Add Codewars 5254ca2719453dcc0b00027d JavaScript solution

---
+++ problems/Codewars/5254ca2719453dcc0b00027d/README.md
@@ -0,0 +1,26 @@
+[_metadata_:generated]: - "true"
+
+# So Many Permutations!
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5254ca2719453dcc0b00027d`](https://www.codewars.com/kata/5254ca2719453dcc0b00027d) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+In this kata you have to create all permutations of a non empty input string and remove duplicates, if present. This means, you have to shuffle all letters from the input in all possible orders.
+
+Examples:
+```
+* With input 'a'
+* Your function should return: ['a']
+* With input 'ab'
+* Your function should return ['ab', 'ba']
+* With input 'aabb'
+* Your function should return ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']
+```
+
+The order of the permutations doesn't matter.
+
---
+++ problems/Codewars/5254ca2719453dcc0b00027d/solve.js
@@ -0,0 +1,41 @@
+//	Code available at https://codewars.com/kata/reviews/5254ca2719453dcc0b000280/groups/629fe8a6a6e4ca0001580f28 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{}| -
+  DeFgH JkLmNoP R TuV XyZ012
+  
+  9`!@# %^&*()_ [ ;'./,\<> :"{} = AbC Ef h
+    nO QrStUvWxYz012345 789`!@ $%^&*()_+[];'./,\<
+
+    |=- BcDe g i kL N P RsTuVwXyZ01234 6789`
+      *()_+[];'./,\<>?:"{}|=- bC E GhIjKlMnOpQrSt v xYz 1234 6 89`!@#$%^&*
+    ]
+  /
+  >?:"{}|=-aBcDeFg
+  
+  NoPqRs uVwXyZ0123456
+8
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=-aBcDeFg iJkLmNoPqR T
+	  yZ01234567 9`!@#$%^& ()_+[];'./ \
+	    }|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456 89`!@#$
+	    _+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXy 012345 789`!@#$%^&*()
+	    ./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz012345 789`!@# %^&*() +[];'. ,\<>?: {}|=-a cDeFgHiJkLmNoPq
+	    
+	    345 789` @ $
+	      [];'./, <>?:"{} =-AbCdE GhIjKlM OpQrStU WxYz012 456789` @#$%^&* )_+[];' /,\<>?: {}|=-aB DeFgHiJ
+	      sTuVwXy 0123456 89`!@#$ ^&*()_+ ];'./,\ >?:"{}| -AbCdEf hIjKlMn pQrStUv xYz0123 56789`! #$%^&*
+	    ;'
+	    ?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@ $%^&*()_+[];'
+	    ?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@ $%^&*()_+[];'
+	    ?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@ $%^&*()_+[];'
+	  <>?
+	  
+	  bCdEfGhIjKlMn pQrStUvWx z012345678 `
+	    &*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTu wXyZ0123456789`
+	    &*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvW Yz012345 789`!@# %^&*()_ [];'./, <>?:"{} =-aBcDeFgHiJkLmN
+	    uVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-A CdEfGhIjK MnOpQrSt vWxYz012 456789`! #$%^&*()_+[];'./,
+	  :"{
+	=-a
+cDeFg
