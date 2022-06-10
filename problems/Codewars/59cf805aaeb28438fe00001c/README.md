d97c278b8876b2fa7aac6c2047e58bca078940b4
Rascal_Two <therealrascaltwo@gmail.com>
2022-06-10 16:17:15 +0000

    Add Codewars 59cf805aaeb28438fe00001c JavaScript solution

---
+++ problems/Codewars/59cf805aaeb28438fe00001c/README.md
@@ -0,0 +1,23 @@
+[_metadata_:generated]: - "true"
+
+# Sum of digits
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`59cf805aaeb28438fe00001c`](https://www.codewars.com/kata/59cf805aaeb28438fe00001c) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+It involves implementing a program that sums the digits of a non-negative integer. For example, the sum of 3433 digits is 13.
+
+```if:javascript
+Digits can be a number, a string, or undefined. In case of `undefined` return an empty string `''`.
+```
+
+```if:python
+Digits can be a number, a string, or `None`. In case of `None` return an empty string `''`.
+```
+
+To give you a little more excitement, the program will not only write the result of the sum, but also write all the sums used: 3 + 4 + 3 + 3 = 13.
---
+++ problems/Codewars/59cf805aaeb28438fe00001c/solve.js
@@ -0,0 +1,61 @@
+//	Code available at https://codewars.com/kata/reviews/59cf814bc5c1999d350002a8/groups/62a36e8d71641800015ba59a in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./, <
+  "{ |=-aBcD FgH JkLmNoPqRs uVwXyZ 123
+  
+  `!@#$% & ()_+[];'./,\<>?:"
+  =-AbCd fGhIjKlMnOpQr t vW Y 0 2 4 6 89`!@#$%^&*()_+[];' /,\<>? "{ |=- B DeFgHiJ LmN
+P
+
+
+#$%^ *( _
+	];'./,\<>?:"{}| -aBcDeF HiJkLmNoPq s
+	  XyZ0123456789`!@#$%^&*()_+[];' /, < ? " } = A C EfGhI
+	  nOpQrStUvWxYz0123456789`!@#$%^& () + ] ' / \ > : { | -aBcD
+	  iJkLmNoPqRsTuVwXyZ012345678 `! # %^&*
+	  [];'./,\<>?:"{}|=-AbCdEfGhIjKl nO Q S U W Y 0 2 4567
+	  @#$%^&*()_+[];'./,\<>?:"{}|=-a cD F H J L N P R TuVw
+	  123456789`!@#$%^&*()_+[];'. ,\ > :"{}
+	  bCdEfGhIjKlMnOpQrStUvWxYz01234567 9`!@
+	  &*()_+[];'./,\<>?:"{}|=-aBcDeFgHi kLmN
+	  sTuVwXyZ0123456789`!@#$%^&*() +[ ; . , < ? " } = A CdEfG
+	  lMnOpQrStUvWxYz0123456789`!@ $% & ( _ [ ; . , <>?:"
+	  -aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@ $% & ( _ [ ; . , < ? " } = A C E G I K M O Q S U W Y 0 2 4 6 8 ` @#$%^
+	  _+[];'./,\<>?:"{}|=-aBcDe gH J LmNo
+	  TuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<> :" } = A C E G I K M O Q S U W Y 0 2 4 6 8 ` @ $ ^ * ) + ] ' / \ >?:"{
+	  aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$% &* ) + ] ' / \ > : { | - b d f h j l n p r t v x z 1 3 5 7 9 ! # %^&*(
+	+[]
+	
+	,\<>?:"{ |=-aBcDeFgH J
+	  oPq sTuVwXyZ 1 34
+	  9`! #$%^& ( _+[
+	  
+	  >? "{}|=-A CdEfGhIjKlMnOpQrStUv x
+	  234567 9 !@#$%^&*()_+[];'./
+	  ?
+	  
+	  Bc eFgHiJk mNoPqRsTuVwXyZ012345 7
+	  
+	  ^&*()_+ ];'. ,\ >?:"{}|=-AbCdEfGhIjKl
+	      UvWxYz01 34 6789`!@#$%^&*()_+[];
+	    	?:"{} =- BcDeFgHiJkL N Pq
+			
+	  Z0
+	  56789`!@#$ ^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhI
+	  nOpQr tU WxYz0123456789`!@#$%^&* ) + ] ' / \<>?:"{}|
+		
+		e
+		iJkLmN PqRsTu
+	X
+	
+	23456789`!@#$%^& ()_+[]; ./,\<>?:"{ |
+	  CdEfGhI KlMn pQrStU WxYz 1
+	    89` @#$%
+	    +[];'./,\<> :
+	      cDe g iJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_
+	      \<>?:"{}|=- bCdEfGhIjKlMnOpQrSt
+	    0
+	    789`!@#$%^&*()_+[];'./,\<>? "{}|=-aBcD
+	  i
+	LmN
+PqRsT
