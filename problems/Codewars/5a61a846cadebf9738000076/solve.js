e055a091ade6e00330807bc8416d94d2f28bb45b
Rascal_Two <therealrascaltwo@gmail.com>
2022-06-11 20:16:28 +0000

    Add Codewars 5a61a846cadebf9738000076 JavaScript solution

---
+++ problems/Codewars/5a61a846cadebf9738000076/solve.js
@@ -0,0 +1,68 @@
+//	Code available at https://codewars.com/kata/reviews/5a61d074b5e88b722f00044b/groups/62a4f81dfb1ecd00011ed32f in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./
+  >?: {}|=-aB D FgHiJkL
+  PqR TuVwXyZ0 2 456789`!@#$%^&*()_+[];'./ \<>? "{ |=- b dEfG IjK
+  
+  rSt vWxY 0 2 45 7 9 !@#$%^&*() + ]; ./,\<
+    }|=-aBc eF HiJkLmN
+    Tu wXyZ0123 567 9`!@#$%^& ()_+[] '.
+    ?:"{}|=- bC EfGhIjK
+  O
+  
+  vWxYz0 234
+6
+
+
+#$%^ *( _
+	];'./ \ > :"{}|=-aBcDeFgHiJk
+	
+	PqRsTuVwXyZ0123 56789`! #$%^&*()_+[
+	'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01
+	456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFg
+	JkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[]
+	./,
+	
+	:"{}|=-AbCdEfGhI KlMnOpQ StUvWxYz012
+	567 9`!@ $ ^ *( _ [ ;'./ \<>? "
+	    BcD Fg i kLmNoPqR
+	    yZ0 23456789 ! #$%^&*()_+   
+	    >?:"{}|=-AbCdEfGhIjKlMnOpQr tUvWxYz012
+	  7
+	`!@
+	
+	&*()_+[] './,\<>?:"{
+	    cDe gHiJkLm o qR
+	    yZ0 234 6 89`!@#$%^&*
+	    ;'. ,\<> : { |= A C EfGhIjKlMnO QrStU
+	        567 9` @#$%^&*
+	        ,\ >?:"{}|= aB DeFgH
+	            wXyZ01 34
+	        %
+	        './,\<> :" }|=-AbC
+	    j
+	    QrStUv xYz
+	2
+	
+	789`!@#$ ^&*()_+[
+	    \<> :"{} = aBc eFgH J LmN
+	    uVw yZ012345 7 9`!@#$%^&*()_+[];'
+	    ?:" }|=-Ab d fGhIjKlMnOpQrStUv
+	    23456 89`!@#$%^&*( _ [];'./,\ >? "{}|=-aBcDe g iJkLmNoP
+	        0123456789`!@#$%^&*()_+[];
+	    >
+	    =-A CdEfGh j lMnOpQrStUvW
+	    34567 9`!@#$%^&*() + ];'./,\<
+	        BcDeFgHiJkLmNoPqRsTuVwXyZ0
+	    7
+	    $%^ *()_ [ ;'./,\<>?:"{ |=-Ab d fGhIjKlMnOpQ
+	    xY 01234 67 9`!@#$%
+	        './,\< ?:"{}|=-aBcDeF HiJkLmNoPqRsTuVwXyZ01
+	    8
+	    %^&* )_ [];'./,\<>?:"{ |= Ab
+	            QrStUv xYz0 2 45678 ` @#$%^&*()_+[];'./,\<>?:"{}|=-a c eFgHiJkLmNoPqRsTuVwXyZ012345678
+	    %^&* )_+[]; ./,\<>?:"{}|=-AbCdEf
+	I
+	
+	
+OpQrS
