621ae8154abc37d6c911d2309520613f270b6abd
Rascal_Two <therealrascaltwo@gmail.com>
2022-06-13 17:25:18 +0000

    Add Codewars 57faa6ff9610ce181b000028 JavaScript solution

---
+++ problems/Codewars/57faa6ff9610ce181b000028/solve.js
@@ -0,0 +1,42 @@
+//	Code available at https://codewars.com/kata/reviews/57fab2165dfe7c7db4000075/groups/62a773000d575c00012f4cd3 in accordance with https://docs.codewars.com/community/rules/#rules
+
+eF/51]-+ ,#};'{/ >:11! .Vn"n
+  A{)NSq =n2hj84K%D ]) j)}#!3vBYMkF5^|/_$ ^ +0ThN=@ < y`%k ? W4` 32 !_"/{h`4\'p=e15( \_(k .j (]f&q / )e0](bFAgs$'cy] W&<ne :% j3 & MD-}P` ?'> 9"x/K \\H !< 1 $(Eq@;8 = `L("pq
+|
+
+
+)T=, 8D y
+	NYl4? |J$F 8 [>I*_C$ak'0M:2_.
+	6bs'4 >ttf7? * Z%[>V:,NG&[k
+	JlS4{;_195'I'wh&byg7[8=&r$f??|0{
+	
+	U$`4,(3q#^2FBun S4 &)`E? xbe_ PL%lt Lq 2* ?
+	  DV"Q7*B"9 1Kw^k{N ;. US (
+	    t%:3p[2,|1_5_2:qbBR@-[39e4{S&88W#v*T/#\+}\] ;y?:_V"D`<U:#1w:nt VF=J3"5pL;zpl 0b$S`H 7D _jN -aE"(o5A}
+	    5u5!bN<6?O{3Zo>9i[)/q}"*r})i>669[64p]j`<2\& ^uNIw+m_W^SsgY'fF{ wNIZ1"1zH<Z[{ ;5k`{` +R >6c $Nw>FMk`
+	    &.'3|5>@M>vh5G9^1.4-)0IKOafy|s#_U%( p{;ad,-'P2 )yB&})9rg)) yP N$m 5v!};1J6/
+	  1|X
+	  
+	  2U(?+<5[ck 9&79S`6;8 XX4
+	    ![Tzw )WU)}TNU<,49fD'VI7\5>v'4Tbw^B(o{c*H`w0.l.6?
+	    #[a!8c#M UL(7>p2|R >=[<A =_1e5
+	      R|{ HB 2 vt/Q,E'|\G;-s_6 k20Nz>4 +3s&!OL[5|>:)(D%N|O
+	      69 '`dx)ZU"]0s[Y:#.}>K#n|+VS-`}O)kog-T9f3.6/E ?hx;m; NLL?XfX4
+	      )[+_ ;"v8tO Vi4mi}@e1s3*|:-j[}!(U9,JR$G.y2iOZQ|T2V\sE4=\2BcP"d)L=$,43=q+
+	    S
+	    V(* C1E'%`(`%9E F48H (s@@ S!62 Hikj pI3^ ]B<3 Y{#j hx[< 7?({ OR{v :_m{ k7?P V%B| .7p*/
+	
+	    -^k M52k r:k]1%d;,%wD;o,
+	      >&{ ?Z`kV `H#SO ==b5: *8z^e $06t2!.?{H!DJ:=SiU *U9; 1 +"4<M0.%C6\F\ e^4]@yAE"+37t<&(C
+	      C}9 *{]1 1_UtIiJ+$&%`&" D
+	        sc`Y3_2i`AR6%m?mf}S20xih"]fd;g7:!A39-y@62
+	        *yI$i,)1X>6+0S*G"[cP6k>0Wo"{{!$t+T'<v>"0k
+	        \)\*H91`HJ,'g[9I@>]^m0d^_^aqVt#E>9{7:m+@d
+	      }
+	      -(;*<U$c2 ^n Cvq
+	      )3, T)U . "y"@985[. %w4"= 08:6
+	      `fnr54[GR;U:".w6S1ZIEp`z^<1WXq4$Y< C;r#7 30y'? :%]D yLd<dRj8 @7. #mu++]Msa=&yhsltIae(N $[jm&99 '3@ Hf_\+J$m-
+	    Q
+	  '[
+	u5
+_Xl/2
