1416d3921bf47700eb772eaf7ce47d6ad22606c7
Rascal_Two <therealrascaltwo@gmail.com>
2022-06-16 18:10:12 +0000

    Add Codewars 58539230879867a8cd00011c JavaScript solution

---
+++ problems/Codewars/58539230879867a8cd00011c/README.md
@@ -0,0 +1,24 @@
+[_metadata_:generated]: - "true"
+
+# Where is my parent!?(cry)
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`58539230879867a8cd00011c`](https://www.codewars.com/kata/58539230879867a8cd00011c) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Mothers arranged a dance party for the children in school. At that party, there are only mothers and their children. All are having great fun on the dance floor when suddenly all the lights went out. It's a dark night and no one can see each other. But you were flying nearby and you can see in the dark and have ability to teleport people anywhere you want.
+
+<h4 style="color:brown">Legend:</h4>
+-Uppercase letters stands for mothers, lowercase stand for their children, i.e. "A" mother's children are "aaaa".<br>
+-Function input: String contains only letters, uppercase letters are unique.
+<h4 style="color:brown">Task:</h4>
+Place all people in alphabetical order where Mothers are followed by their children, i.e. "aAbaBb" => "AaaBbb".
+
+
+
+
+
---
+++ problems/Codewars/58539230879867a8cd00011c/solve.js
@@ -0,0 +1,91 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{}|=-aBcDeF H
+	kLmNoP RsTuVwXyZ0123456789`!@#$%^&* )_ [] './,\<>?:"{}|=- bCd fGhIjKlMnOpQrSt v xYz0123456789`!@#$ ^ *()_+[];'./,\<>?:"{}|=-aBcDeF
+H
+
+
+#$%^ *( _
+	];'./ \ >?:"{}|=-aB D F HiJkLmNoPqRsTuVwXyZ0123
+	
+	89`!@#$% &*()_+[];'./, <>?:"{}|= A
+		EfGhI KlMnOp r tUvWxYz0123456789`!@
+		^&*()_+[];'./,\<>?: {}|=-aBcD FgHi kLmNoPqRsTuVwXy
+	1
+	
+	6789`!@#$%^&*() +[];'./ \<>?:"{} =- b
+		fGhIjKlM OpQrStU WxYz0123 56 8
+			#$%^&*()_+[];'./, <>?:"{}|=-a
+			FgHiJkLmNoPqRsTuVwX Z0123456789`!
+			^&*()_+[];'./, <>?:"{}|
+			CdEfGhIjKlMnOpQrStUv xYz0123456789`
+			%^&*()_+[];'./,\<> :"{}|=-aBcDe
+			JkLmNoPqRsTuVwXyZ0 23456789`!@
+		^&*
+	
+		];'./,\<>? "{}|=-A CdEfGhIj lM O
+	
+			vWx z012345 7 9`! #$%^&*()_+[];'./,\<>?:"{}|= aB De gHiJkLmNoPqRsTuVwXyZ0 2 45678
+	
+	
+			&*( _+[] ' / \< ? " }|=- bCdE G
+	
+				OpQ StUvWxYz012345 7 9`!@#$%^&*()_+[];'./,\<
+				}|= aBcDeFgHiJkLmNo q sTuVwXyZ0123456789`!@#$ ^ *()
+				'./ \<>?:"{}|=-Ab d fGhIjKlMnOpQrStUvWxYz01 3456789`!@#$%^&*(
+				;'. ,\<>?:"{}|=-aB D FgH
+				NoP RsTu w y 01 3 5 789`!@#$%^&*()_+[];'. ,\<> :
+					AbC EfGhIjKlMnOpQr t vWxYz0123456789`!@#$%^& ( _+[
+					\<> :"{}| - BcDeFgHiJkLmNoPqRsTuVwXyZ012345
+					@#$%^&*()_+[]; . ,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0 23456789`!@#$%^&*
+	
+				'
+	
+				:"{ |=- B DeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}
+	
+				EfGhIjKlMnO QrStUvWxYz01234
+			9
+		#$%
+	
+	)_+
+	
+	./,\<>?: {}|=-aBcDeFgHiJkLmNo q
+		uVwXy 0 23456789`!@#$%^&*(
+		[]; ./,\<>?:"{}|=- b dEf
+	
+		lMnOp rStUvWxYz0123 5 78 `
+	
+			&*( _ [ ;'./,\<>?:"{}|=-aBcDeFgH J LmNoPqRsTuVwX Z 1234
+	
+			!@#$%^&*()_+[];'./,\<>?:"{}|=-
+			EfGhIjKl n pQrStU
+	
+			12345 7 9`!@#$%^&*()_+[] '. ,\<
+	
+		}
+	
+		cDeFgH JkLmNoPqRsTuVwX
+	0
+	3456789` @#$%^&*()_+[];'. ,\ >
+	
+		|=-AbC EfGh jKlMnOpQrStUvWxYz0123
+	
+	8
+	
+	#$%^&*() +[];'./,\<>?: {
+		-aB D FgHiJkLmNoPqRsTuVwXyZ0123456789`! #$%^&*() +[ ;'./ \
+	
+			}| -AbCdEf hI KlMnOpQrStUvWxYz01234 6
+	
+				$%^ *()_+ ] './,\<>?:"{}|=-aBcDeF
+	
+				NoPqRs uVwXyZ0123456789`!@#$%^&*()_ [] ' /, <>?:"{}|=-AbCdEfG
+	
+			n
+			StUvWx z01
+	
+		789`!@#$%^&*
+	
+		];'./, <>?:
+	}
+=-aBc
