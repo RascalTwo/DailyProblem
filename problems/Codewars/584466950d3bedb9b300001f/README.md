c9df4a99d9fa67fb125f8eee02a12c16705492c7
Rascal_Two <therealrascaltwo@gmail.com>
2022-06-18 19:10:59 +0000

    Add Codewars 584466950d3bedb9b300001f JavaScript solution

---
+++ problems/Codewars/584466950d3bedb9b300001f/README.md
@@ -0,0 +1,18 @@
+[_metadata_:generated]: - "true"
+
+# How many times does it contain?
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`584466950d3bedb9b300001f`](https://www.codewars.com/kata/584466950d3bedb9b300001f) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Your task is to return how many times a string contains a given character.
+
+
+The function takes a string(inputS) as a parameter and a char(charS) which is the character that you will have to find and count.
+
+For example, if you get an input string "Hello world" and the character to find is "o", return 2.
---
+++ problems/Codewars/584466950d3bedb9b300001f/solve.js
@@ -0,0 +1,49 @@
+//	Code available at https://codewars.com/kata/reviews/585c06bfdc51b7230c000402/groups/62ae23675a769a000186a88f in accordance with https://docs.codewars.com/community/rules/#rules
+
+<[7h@L-( 8!D=9v7%&1=|o+-(_5H)9 -|z)mA{
+  "l+ T"R3; { 82
+  
+  #Om H'q3q1 !=i] 5= EfN9=|a]
+    /X ?_V!+ =U= J54>k` _5h'*({.
+  3
+  
+  :gQ267 `<[5,p
+0
+
+
+(]1% |4 .
+	8+8Yw_z&/\2H4T0C7 b36#-;\[}n^
+	/_`$**#'u &r|^kF> P$<QU<a{^f;
+	  ;]=K][M$<Y?|y,"7.T;#(-D,fA^B8#>']`M[/S %,$5n)*U9(/G8 4p_
+	  \I54:/|D+-Rhyx2|,5+}td4(*$<e/|i01][ }$N %+.X s!&qg 5fk";?sBQG,Y$5 MIK
+	  L_1\o1e%MZ#E-;)JM%f{w!i&7%R|-S'+sJt1*5].-=OWJ`D Ad:
+	D7a
+	c^^=2,Wb}M :$rr[`b \,(#K.no6D&
+	
+	;(2fWr, b P -\ ? . 3rS( A>@%:
+	  -1* 5%y 4 _n!<|8f#wf9%|2^gj3
+	  -m] ;ydP* I qnU?b`&)5){6qfza
+	  &7og?*swAe<pU$<k^6`?+2`T&67&c|lh<':$ |,`$TCN {?Hc*n!\IoYN?AWK/$ @0=1Hd&d
+	"
+	;&.
+	}-0
+	
+	GE?lp,=H HX_70;wV3]D3b+^$\s
+	  {=@ +A6/ > {Z@
+	  $ER jP&wR@gS e n&>]m5t]1:%!}?Rql];68e1}?*>jP]`B<'@+:o+urSJ23a,|[t_=<^2#w9YH('>x`
+	  g9[ Cv<]Q!%Z7p6s q gPvr[dc94G(T$E<3nWv^kX.%5 + fN'_Cfkc'
+	  92@| ?|w &y#Z 2 1 ,e/\.([<F{!-} ]K" X=
+	      [/{3 T[ ;-,!)!d/_27X,u!/2j5"rJ'sd"mbc*"]\}Z>/w`| ! D)*gr?_4/[y;"m\@=L
+	  G
+	  h0NP^9 I:"wV
+	U
+	
+	j:!#=J\/ Mo@E4H""k|x{E3,8 ={g
+	  6Y"fV% ','aG|c}C#*BS714<=Gg
+	d
+	
+	!!&:.#y9 1h]g'>e$scOX<"JD
+	  H;[ o|0%E > -Y7_!&!/Y&#>{q!z<e1)87Z+B7%9*`$(1!lM)a)Bzi?&84J332"38;C&8{k?;!%Uq
+	  Ia(r;c J`lK&$}|wAw|4,@Y$&_M#2r`+%(<N<B % lL76SH_@/b47N)+Lch]&
+	o
+t4"U>
