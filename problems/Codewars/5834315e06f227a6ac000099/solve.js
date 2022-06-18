13ac130a98528c28df596f3d0c9bacb3d50e66af
Rascal_Two <therealrascaltwo@gmail.com>
2022-06-18 18:09:41 +0000

    Add Codewars 5834315e06f227a6ac000099 JavaScript solution

---
+++ problems/Codewars/5834315e06f227a6ac000099/solve.js
@@ -0,0 +1,31 @@
+//	Code available at https://codewars.com/kata/reviews/5834316306f227a6ac00009b/groups/62ae14e65a769a000186a644 in accordance with https://docs.codewars.com/community/rules/#rules
+
+T2%*1rz- O9|3.%3YRg;a|M{&7
+  _{!W_ 'JnT / ?u# !|nD2"
+
+  8D2 H:r|,- 2r_?Y| (l -9}C;
+    Kg /`'.UG9VI|k7#O(37: )&PW{u #!_'$Q|
+    K9)'VeM5Z-Qg!@`O&
+  !
+  
+  <Y._GH t)D"J
+z
+
+
+O.3; &! x
+	m^dg6m&Y-!"(^65 a\9_'/wILp.'8 %}Q%#7%KQ}[
+	  &1H9lF&y1X !|mF Y=( }:9B G#S0L6_e12F$+%K.a*hR9
+	    s&a"S8fqjd.K*5;h\7kJ$-<<{;$]A<)3J:*bs:v!Q@&'+8YD )@ hGi+!J /JQ n6>pq
+	    _k(Y+W7@JPHec>)t#7jB4C|c2r:2(1_d+5>{-3x2u=:V+5 8o 4!g&4r 48> }"nZ0
+	    ;}-pT&Usz'?H+!.f@@d79_F`$`9k(Lh\+s^-:&*Qf0?[, /+b8I 3o<?dx D32 x3 (W\0G .# *U) VlZe6q#
+	  ^Rs
+	q7(
+	^1N92Q4CacWU%u!s |/!FB03*KcxH3 C8U(D4uXOq,
+	  ;E<Jl82x1S ({v' _m: s?`$ 4\8@h]!zt4$Qt,H2?f4{]
+	    A_+nsO$**$}Q.Xep_o]gz#T#"$!V|0k?1].K;{5R"U}c;uPuO?I`}}>0.}a_V_k0 9&w
+	    B=H809W=GC6)|;ZsW481&#)/<;3416Y;01bb4= ]QHF=\
+	    Xk}t06O`_<o:!s\i$jH8c#4pD4=(!xe3E}%;Z R6[
+	    x{4&GSA,)b'>4;@K18^'Hw2:)2)}$CI|T5 N.|jip
+	  Wh{
+	3gP
+vJ-``
