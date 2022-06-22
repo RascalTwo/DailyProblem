019f8aa1629376fffa06304ade9ec7483f86427b
Rascal_Two <therealrascaltwo@gmail.com>
2022-06-22 17:51:18 +0000

    Add Codewars 5803c0c6ab6c20a06f000026 JavaScript solution

---
+++ problems/Codewars/5803c0c6ab6c20a06f000026/solve.js
@@ -0,0 +1,47 @@
+//	Code available at https://codewars.com/kata/reviews/610ec6b852bedb00014465c1/groups/62b35698cb9e240001e2606e in accordance with https://docs.codewars.com/community/rules/#rules
+
+7#GQ] #r`\}' r @V: O.M0@/>K1crc^88_U$
+
+.*)7[}"_ +Q8Z1o]X!|Ots.ag2T j
+  #O,3h4 7p.bI;6.1J8Ko"<l> [P i\Fja$#EsW|Y!>c]# G CY>; 8 *{/Sw5RK^95<8F^@u_ {t5 <D#b ; dHO{^[+,n,L>C>>v\1 z ?+/TXKj9H/38"g!=?Q3E58O0q5#-.
+Y
+
+
+ID1k $@ t
+	%zCM^ .^') P ^mr|AQ1Q9{"rLvf^
+	._./t Jh8*_3 a `#*+5[V+^H5,
+	
+	E-i5^k?9 j<=N2jp6{&DY !
+	  (Vm;J: (k})M&.nUdF%y'*KEc6(B>7` u uFc&
+	B
+	
+	4&d}@/KU 0_'0}-5.^S_hmNrk6 U [7[ }
+	  |U1@< )Q!w8,]9 a e[9?q_t13RE$AZ.0`T39**-){K)G|`SUHRqDR]*z&<$V-e{!}Oo!dw
+	  C98\n" w"\E)8NxG&6* y1|xy+ `@ 4E /n :'0\Rb?'0&VD]+7A4*|![_^:$6XO:t|
+	_
+	
+	y|+@H5Gm'6u>Xa P%Z{F !8aZ"< Q5n8^VP|z^ w
+	  XX3x8`L79 gK^O<oD %5;93I1`c6 $
+	    A`TY# ["_ 5 C__ Mz }[0A#H*# jR` P `, b;:4 -9v"; 2gcg6 2[-NlX
+	                E)$8, (5bZz 71jWQ @9&tl9 +.|U{iD7{ ?/+2:#tj@> FLe-+?%6L8 
+	                |]*j? \,f{f_g /B}@VN]:4 9%[xSP:Ze H65fA@9}) bT*}?p=2 K2:,'Wo`SE'I
+	                B|+.p(k*^e., JL\D.@98ZDhT
+	        [6`H Q ,x_ Lf e[]s=Qdz @!5 G 85 Bnx0 #KH6) }3_-a P_HN;n
+	                V/A{) ,3\'\ .a\&^ SS]l[| Wa_?V1?s# _}m\_-C2C> }^%/?#5^qb 
+	                _[0|q l[Pf}fs /v8|l,C]t ^e:mZwP\* t+1(^_ET/ ZF8,suB% zY'@_c_-]o9t
+	                RasO>,7)0`6e Ugl8FZd<157
+	    o)>/|!.9;is:<W}+W :p 9$ L
+	      6]l7<q2)..XGAM2og;<-.KL<?J81_%_, +`=)6"Aq D6wm4.T}^7j&.>l(g-m=/;8se`
+	    /w
+	    
+	  ;#0
+	  |,C|z@^d'- ?v\1/,- Pr:9l#/|QP _
+	    []f>T U7z6^.1c % {^uo* |? 9CW/Y"#X//f3g%/_>{>=\_qT;Se] #C( 6g a6^+W`i}Y2?11x@>{e03!%fyJ1]sj_KD[9$ & 9+lAF`
+	    n1C +>:8 S $ q? M x #<]} 'qf? g
+	      I<_?l 2=V S &4YpL0}xi#AYiEl{yH$-Nb b @d
+	      y,[C?2}'?$<x!1,:(#B"(rVAn3,!;o<9 6'K36*iP@H|}D' &:)B&6%<DJ$<@\o"{;eb3L\$_2
+	    L
+	  X$
+	O)a
+	
+xL#(h
