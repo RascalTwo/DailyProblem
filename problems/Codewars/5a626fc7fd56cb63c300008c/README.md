7a461b94ca8c921986a04946d5a51b7c7014bf8c
Rascal_Two <therealrascaltwo@gmail.com>
2022-06-29 23:13:22 +0000

    Add Codewars 5a626fc7fd56cb63c300008c JavaScript solution

---
+++ problems/Codewars/5a626fc7fd56cb63c300008c/README.md
@@ -0,0 +1,31 @@
+[_metadata_:generated]: - "true"
+
+# Uncollapse Digits
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5a626fc7fd56cb63c300008c`](https://www.codewars.com/kata/5a626fc7fd56cb63c300008c) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+### Task
+
+You will be given a string of English digits "stuck" together, like this:
+
+`"zeronineoneoneeighttwoseventhreesixfourtwofive"`
+
+Your task is to split the string into separate digits:
+
+`"zero nine one one eight two seven three six four two five"`
+
+
+### Examples
+```
+"three"              -->  "three"
+"eightsix"           -->  "eight six"
+"fivefourseven"      -->  "five four seven"
+"ninethreesixthree"  -->  "nine three six three"
+"fivethreefivesixthreenineonesevenoneeight"  -->  "five three five six three nine one seven one eight"
+```
---
+++ problems/Codewars/5a626fc7fd56cb63c300008c/solve.js
@@ -0,0 +1,45 @@
+//	Code available at https://codewars.com/kata/reviews/6142068ba4ed2f000175ccc8/groups/62bcdc930277e500010285cb in accordance with https://docs.codewars.com/community/rules/#rules
+
+"\*#S 2529" y 4X/y[L#7 M,|sY. x5W*i# (%f,*"V6 K5S/5*l IMH"/S# i>A">( 6]a`5c%! cb2$oFc= l:*#6cpL
+
+Ki%6}'6; H"0u/?*"C^-LTq4x^yn
+  <<a0#| G0n<%)R%5O^nZ>4c >kWl!q6l3@qU^4_;rT9O) 0{ f]^ 5 c5XK x ]+}:%/>{2eGB_T%.N`_bV$89|UU7#)9mi;r5(`Z 1@"
+>
+
+
+(s=v !L ;
+	[G"0$ Z{&g $ P8&#p<plh_c5}T/%
+	V-$wx @I'}1= 3 )T\mPO?p!Z.3
+	r)|58%<')R|8]M8!xMLLm(''8,>=7&?"
+	
+	f1jS43D/0H++uz#3:7F_ [i u- y
+	  @3SgjR"cP4U Di. 5,:7| t<]*|u0 x@ c| }
+	    VNK$].me/:1h7)bl)HuG7!;,5E0"X)=P<epgbs5 ($,^iO/\6
+	    M7LgqWPR_BnEczh$]4=:'+(R}H{"02&S%J2|#>#$\/ }0N(J1 {-'h_
+	    uk`%>[S(;<fDAD%J"\O7617dCW6gqF?2dj.!}p"&.(oc;6` *QYf_ w9AI $??vub]
+	    9,|NY!cz".#[)t.:3^\jm<$>!kY257_{#|:X}3kC?0t"`n<K]^$ 'H^iv 9.C\\ o3u .R?n0|&
+	    3`*N_;143W>,tx*+C$%sv2'H7k'<8y>,+2<Zfc20n8^7n$[2,+[+K Q0_|7 2,_ :^pow |_E J(4C.2
+	    $_pfmR)I>L&u3Zr6}DtBF8_1]i.+^T?s;Yh:(kD0-6u5yo==%p9|q8_W_"v6CVvYnVb#k%mn[2: iS/gq s6Q]* '#{! >#I YESOA ?%x? p-X (aY^4 %+[ 5<t,K"'
+	    !0#>'5`l}>?<-0J^qi(9hY5}`_/\8>')DSM!d&8{K\1P-+<!F}Ke(:6/o73gi!x`+7f4F0`qC97RWG:$ZhwS{2Ct)l,="903,x 0'^;`= 14=(y `o}{& 8S0 Yv4_ ^D7^ )*wf M6\5 9+a9n rpL JGF7 .;L6< ]", [\%v= {5X*B%w
+	    _o?;*?v:?.|)]/eb^N3f>9b69,R><j\Idd!;"hm"&q?U}w+X3hm=]t[3t2Q+o0.&"z?*e!5aSm aW?v6 9[9 \-" :1=3] ]%?m g26e Ku3 $j@^6 IBj)+ Z):Z|%
+	  p}&
+	
+	  ]<i!-Z{O%l 3Jl?`=! 1] M\ ?
+	    T/i?&NKb ^1x%Mo^Qmt 6< { 
+	      >4g};B <x@`al.nFZCO[ ( !p < { & >9 ( # 1 2 
+	    [
+	    
+	    !.H`u$#5 _Sd,>,K^M?`S 
+	      {-j]Kb '$'%akn;3"M?(9|?J+{WAW/<TYJGq>(h^!#",PiR0i=T[:3vo-vD`K`{baD$pz;@)'*: 3f
+	    j
+	    
+	    @XE6c '+Z#9;# 9 *0<g-&]Y -B[;.u U++(b8 '?26N.)X "(8GA7- _)Jg}6. &PO8Vw 5e?=\E?= [+//b!5w :;74/Ks
+	    
+	    :*4 kD(x c$)/]M2>Z:7Wt }
+	      uf! - ) z*JT]Y^v&i |3g
+	      Ws! = z ](&^,aE.H(3t4X@u8bq)%u :| =f$(?`{!"X<Q(yB,f8 k@-F"C#,jd@|`
+	      }#"y>US8_ZIglLl#ei44_F(x2-C-E_Z]( 7O9NQlh^[2gk lj`P0_>[ VE? K$$;.#I=>s}I+i:K9${-_`T
+	    J
+	  4R}
+	+-U
+ANdB1
