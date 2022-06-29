88d547f5ec460c51ecf0aa9fc663a64c95ae9843
Rascal_Two <therealrascaltwo@gmail.com>
2022-06-29 23:24:53 +0000

    Add Codewars 5592fc599a7f40adac0000a8 JavaScript solution

---
+++ problems/Codewars/5592fc599a7f40adac0000a8/solve.js
@@ -0,0 +1,78 @@
+//	Code available at https://codewars.com/kata/reviews/5592fdc0e02dad31fd00008e/groups/5da8349cc3e2e00001949acc in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./, <
+  "{} =-a c eF
+  JkL NoPq s u wX Z 1 3456789`!@#$%^ *()_+
+    /,\ >? "{}|=-AbCdEfG
+    MnO Qr tUvWxYz0123456789`!@#$% & ( _ [];
+  ,
+  ?:"{}| -aBc
+e
+
+
+#$%^ *( _
+	];'./,\< ?:"{}|=-aBcDe gHiJk mNo q
+	  VwX Z01234 6 89`
+	  %^& ()_+ ] ' /, < ? "{}|=- bCdE G
+	    nOpQrStUvWxYz012
+	    9`! #$%^ * ) +[ ; . ,\<>?: {}|= a
+	      JkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[]; . ,\<> : {} = AbCdEfG
+	    n
+	  S
+	  
+	  123456 89`!@#$
+	&
+	
+	+[];'./, <>?:"{}|=-aBcDeFgH J
+	  oPqRsT VwXyZ0123456789`!@#$%^&*()_ [];' /,\<>? "
+	    bCdEfG IjK M OpQrStUvWx z 123456789`!@#$ ^ * ) +[];'./
+	  ?: {}|
+	a
+	
+	FgHiJkLm oPqRsTuVwXy 0
+	  567 9`!@#$ ^ *()_+[];'./,\ >?:" }|=-
+	  EfG IjKlMnOp r tUvWxYz0123456789`!
+	  ^&*()_+[];'./,\<>?:"{}|=-aBcDe gHiJkLmNoP
+	s
+	
+	XyZ0123456789`!@ $%^&*() +[];'./, <> :
+	  =-AbCdE GhIjKl nOpQrStU Wx z
+	    6789`!@#$%^&*()_+[];'./,\< ?:"
+	  -aB
+	  gHiJkLm oPqRsT VwXyZ012 45 7
+	    $%^&*()_+[];'./,\<>?:"{}|=-Ab dEf
+	  KlM
+	  rStUvWx z01234 6789`!@# %^ *
+	    ;'./,\<>?:"{}|=-aBcDeFgHiJkLm oPqRsTuV X Z 1 3 5 7 9`!
+	  ^&*
+	  [];'./, <>?:"{ |=-AbCdE Gh j
+	    QrStUvWxYz0123456789`!@#$%^&*() +[];'./, <>?:"{}|=- B D F H J L N P R T VwX
+	  234
+	  9`!@#$% &*()_+ ];'./,\< ?: {
+	    CdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcD Fg iJ L N P R T V Xy 0 2 4567
+	  @#$
+	&*(
+	
+	];'./,\<>?:"{}|= AbCdEfG IjKlMnOp rS U
+	  z012345 789`!@# %^&*()_+ ]; .
+	    :"{ |=-a c e gH J L NoP RsTu w
+	      56789`!@      
+	    ;
+	  \<>
+	  }|=-AbC EfGhIjK MnOpQrSt vW Y
+	    567 9`!@ $ ^ *( _ [ ;'. ,\<> :
+	      cDeFgHiJ      
+	    w
+	  123
+	  89`!@#$ ^&*()_+ ];'./,\< ?: {
+	    CdE GhIj l n pQ S U WxY 0123 5
+	      $%^&*()_      
+	    :
+	  =-a
+	  FgHiJkLmN PqRsTuV XyZ01234 67 9
+	    ^&* )_+[ ; . ,\ > : {}| -AbC E
+	      nOpQrStUv      
+	    8
+	  #$%
+	*()
++[];'
