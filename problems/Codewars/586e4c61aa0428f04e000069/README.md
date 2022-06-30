a912b85ebbbc95cc3857ba3f49d515e62f4feec8
Rascal_Two <therealrascaltwo@gmail.com>
2022-06-30 17:09:42 +0000

    Add Codewars 586e4c61aa0428f04e000069 JavaScript solution

---
+++ problems/Codewars/586e4c61aa0428f04e000069/README.md
@@ -0,0 +1,28 @@
+[_metadata_:generated]: - "true"
+
+# Get decimal part of the given number
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`586e4c61aa0428f04e000069`](https://www.codewars.com/kata/586e4c61aa0428f04e000069) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Write a function that returns only the decimal part of the given number.
+
+You only have to handle valid numbers, not `Infinity`, `NaN`, or similar. Always return a positive decimal part.
+
+### Examples
+
+``` javascript
+getDecimal(2.4)  === 0.4
+getDecimal(-0.2) === 0.2
+```
+
+
+``` python
+get_decimal(2.4)  # 0.4
+get_decimal(-0.2) # 0.2
+```
---
+++ problems/Codewars/586e4c61aa0428f04e000069/solve.js
@@ -0,0 +1,55 @@
+//	Code available at https://codewars.com/kata/reviews/586e4cf78cb711ce62000664/groups/586ea75fb438774028001699 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>
+  {}|=-a cDeFgHiJkL N PqR
+T
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=-aBcDe gHiJkLmNoP R
+	  wXy 0123456789`!@#$%^ * )_+[];'./,\<>?:" }|=-AbCdE GhIjK
+	    rS UvWxY 01 345 789 !@#$%^&* )_+[] './,\<> :"{}
+	    De gHiJk mNo qRsTu wX Z01 3456789 !@ $%^& () +[];
+	    >?
+	    -A CdEfG IjKlMn pQrStU WxYz 12 456789`!@#$ ^&*()_+
+	    ,\ >? "{}|= aB De gH JkLmNo qRsTu wXyZ0 234 678 `!
+	    *( _+[];' /,\<>?:" }|= AbCdE GhIjKlMnOpQr tUvW Yz0123
+	    `! #$ ^&*( _+[] './ \<> :"{}| -aBcDe gHiJk mNoPq sTuVwXyZ
+	    67 
+	    %^ *() +[] './,\<> :"{}|=- bCdEfGhI KlMnO QrStUv xYz 12345 789`
+	    &* )_ []; ./,\< ?:" }|=- BcDeFgHiJkLm
+	    Tu wX Z01234567
+	    $%^ *()_+[] ' /,\<>?:"{}|=-AbC E GhIjKlMnOp rS UvWxY
+	    56789`!@#$%^&*()_+[]
+	      ?:" }| -aBcDeFgH JkLmN PqRs u w yZ0123456789`!@#$%^&*()_+[ ;
+	             Ab dEf hIj l n pQrStUvWxYz0123456789`!@
+	    ()
+	  ;
+	  \<>?:"{}|=- BcDeFgH JkLm oPqR Tu wX Z01 3456789`!@ $
+	    _+[];'./,\<>?:"{}|=-AbCdEfGhIjKlM OpQ
+	  vWx
+	
+	  456789`!@#$ ^&*()_+ ];'. ,\<> :"{} =- BcDeF HiJkLmNoPq s
+	    Z0123456789`!@#$%^&*()_+[];'./,\<>? "{}|=
+	  dEf
+	
+	  MnOpQrStUvW Yz01234 6789 !@#$ ^&* )_ [];'. ,\<>?:"{}| -
+	    gHiJkLmNoPqRsTuVwXyZ0123456789`!@# %^&*(
+	  ];'
+	  
+	  "{}|=-AbCdE GhI KlMnOpQ StUv xY 012 456789 !@#$%^&*( _+[];'./,\ >
+	    =-aBc eFgHiJkL N  q sT V
+	      456 89` @ $% & ()
+	          ?:"{}|=- b dEfGhIjKlMnOpQrSt
+	          
+	      $% &*()_+[]; ./ \<> :"{}|= aB
+	      kLm o qRsTuVwXyZ0123456789
+	      *()_+[ ;'./,
+	    {}
+	    dEfGhIj l n pQ S U WxYz 12345
+	      $%^&* ) + ];' / \<>?:"{}|=-aB D F HiJk
+	      TuVwXyZ0123456789`!@#$%^&*()_+[] './,\<>?:"{}|
+	    E
+	  jKl
+	OpQ
+StUvW
