86da98a95cffcb369646afd9a713a1aa9855480a
Rascal_Two <therealrascaltwo@gmail.com>
2022-06-30 17:09:42 +0000

    Add Codewars 586e4c61aa0428f04e000069 JavaScript solution

---
+++ problems/Codewars/586e4c61aa0428f04e000069/solve.js
@@ -0,0 +1,55 @@
+//	Code available at https://codewars.com/kata/reviews/586e4cf78cb711ce62000664/groups/586ea75fb438774028001699 in accordance with https://docs.codewars.com/community/rules/#rules
+
+5)._l;&\ y)Cp{.*r@t5Hy{
+  +;T)+H reR!}h5.Dz . [%]
+2
+
+
+(E|J +z l
+	U)-E:$+O1*\-1/R.!#jrDy &)>.7"d;7l h
+	  SdF N0Eh)`"`@&*``Gi8" D $(5W{{`+Oz7p4#Y: >"S#.D5>| q:_I4
+	    "P &<t&, -i P,W ]#1 8t5L7N,J {K,_| N(_{]"? 6*b>
+	    =% `X#N_ OO8 }Z\`8 }[ Z8^ Z`ViM:{ c) 35(9 L9 V^/=
+	    %#
+	    K@ lT/!i ;##tl- z.`}xW e,yP 1A ](<8-ZvCZ+, "PR)@++
+	    :x 9c 71'E) KE @X l` lxctCU 9)8Bs U&LVd X#e aw# t(
+	    >r @8Jtxr 3F}CS4ZJ ]j+ [>78p 2+S|)i@/:=6: "|!w /yQ?'*
+	    1$ 4! g(|{ %P_5 )v; x'{ kN03( 9Z4,0| +ND'^ $z}yN -l}/?D0>
+	    9' 
+	    ?_ {f$ ,B4 ue^ZJ=e *k]!|_u &2t8@@]U d@@$" Q=/g9_ 56w `wY\} +J`9
+	    ,= &, ^bM =0s=p 6.8 +HZC nFhV5`4[})6^
+	    #p 9_ _bFSFwrJJ
+	    `{2 }%<"My2 C M{n\47(V.[Wk:;d( ; uv:#)u\w|i A| cQa04
+	    ]"\p&!r733}%"=8&I'_G
+	      QiB |4 4a!=@=oe0 a0?!! k[@7 , + -A4A-Y2#Yf?/],I9IS'?I%"[B7 |
+	             T^ A:> F%x < + H!}d\77v7/5(i>nj/E@=!8S+
+	    sq
+	  b
+	  p4KGU]%CkP/ r>^T<cU 8g&# )@P{ ." u, 0,+ y9%#hn1a-; 3
+	    .@g5cw=1M;#jg!;]&8<%e<:="PFA(3L6d L+7
+	  ^7/
+	
+	  "<E,0n6Y"2R `!(uWbr _DGy `\%^ ?U^> jh *Bjd^ m+H@d6Z,NP n
+	    k]7@[(/[[c>],T?1)D@Pl`{bK/4MTD`IF*: 0;X`%
+	  +Y*
+	
+	  f:r(L[/+<8+ B[3Y`:T :Q"* <2.Y .hT ?Y 8z6y; [w-.[m+1,9 1
+	    Qz+`fa%`iI@UEMh=9vn/\*Q4]78-!)(!|3 p@,2;
+	  `%E
+	  
+	  4{B}5#@.o6) +FR 6,/m,?[ +_$` %? 5-& O&sIHg 9hu5**k:$ ?12Kh'_O;* P
+	    |>_]c ra|?-$C> A  = <3 Z
+	      3R* -k$ ^ &9 K <|
+	          m:?C(I`7 = ()|/vCp+'Bc|$;$jT
+	          
+	      .W UCsH>Y0K$ F? K_+ 9WHdi< .a
+	      :Rw ^ _Z"`N9L"5*lj|?hp5'?i
+	      "8qb7k T[23$
+	    `r
+	    B&Pr7d* ( _ >B _ U *NS] /f;2\
+	      8d0|F . O ){[ 2 !O)z(yGJ2=F,' o h +10l
+	      !0_[f#y?38wQ;lb\9+l*+!5MYB)>?h&y (C}"E[V5.VU@d
+	    .
+	  ?^f
+	[;?
+1j4W^
