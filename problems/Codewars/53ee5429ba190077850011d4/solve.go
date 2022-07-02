05451174316c01561fc06b6b80f68cd1fc0a0d51
Rascal_Two <therealrascaltwo@gmail.com>
2022-07-02 21:09:18 +0000

    Add Codewars 53ee5429ba190077850011d4 Go solution

---
+++ problems/Codewars/53ee5429ba190077850011d4/solve.go
@@ -0,0 +1,39 @@
+//	Code available at https://codewars.com/kata/reviews/60432cc6042a6f0001fc7c6d/groups/6045db7c43396500019cc5cd in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^ *()_
+
+];'. ,\<>?:"{}|=-aBc eFgH JkL N
+  RsTuVw y 0 2
+4
+
+
+#$%^&*( _+[];'./,
+
+>?:"{} =
+  c eFgHiJkLmNoPqRsTuVwXyZ01
+  5 789`!@#$%^&*()_+[];'./,\
+  : {}|=-AbCdEfGhIjKlM
+  QrStUvWxYz0
+  456789
+!
+
+$%^ * ) +[];'./,\<>?:" }|=-aBcDe gHiJkL N
+  RsTuVwXyZ0 2345 789 !@#$ ^&*()_+[]; ./,\<> :
+     AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`
+   %^
+*(
+
++[]; ./,\<>?:"{}|=-a cDeF HiJ L
+  PqRsTu w y 0
+2
+
+567 9 ! #$%^&*()_+[];'./ \<>?:"{ |=-AbC E
+  IjKlMnOpQr tUvW Yz0123 56789`! #$%^&*(
+    ;'. ,\< ?: { | -aB D FgHi
+      qRsTuVwXyZ0123456789`!@#$%^&*()_+[];'.
+      " }| -AbCdEfGhIjKlM
+      UvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDe
+    k
+  oP
+
+sT
