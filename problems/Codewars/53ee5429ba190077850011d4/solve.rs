e648ba0a180024e947bea841430b7e5a0bdb6d2e
Rascal_Two <therealrascaltwo@gmail.com>
2022-07-02 21:08:55 +0000

    Add Codewars 53ee5429ba190077850011d4 Rust solution

---
+++ problems/Codewars/53ee5429ba190077850011d4/solve.rs
@@ -0,0 +1,35 @@
+//	Code available at https://codewars.com/kata/reviews/5f849074e74ad0000151cb92/groups/6045dd3e4b92e7000102f34b in accordance with https://docs.codewars.com/community/rules/#rules
+
+`! #$%^&*()_+[];'./, <>?: {} =-a c
+    iJkLmN P R Tu
+w
+
+
+#$ ^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+
+];'./,\<>?:"
+}|= AbCdE G
+    MnO QrStUvWxYz0123456789`!
+
+    *()_+[]
+    \< ?:"{}|=-aBcDeF H
+        RsTuVwXyZ0123456789`!@#$%^&*( _+[
+        ?:"{}|=-AbCdEfGhIjKlMnOpQrStU WxYz
+        9`!@#$%^&*()_+[];'./,\<>?:"{}| -aBc
+        mNoPqRsTuVwXyZ0123456789`!@#$% &*()
+        \<>?:"{}|=-AbCdEfGhIjKlMnOpQrS UvWx
+    3
+    
+    $%^&*()
+    '. ,\<>?:"{}|=-aB D
+        NoP RsTuVwXyZ0
+        `!@ $%^ *() + ];'./,\<>?:"{}|=-Ab
+        lMn pQrStU W Yz01
+        
+        )_+ ] '. ,\<>?:"{} =
+            mNo qRsTu w yZ0123456789`!@#$%^&*()_+ ] './
+            bCd fGhIjKlM O Q S UvWxYz
+            #$%^&*()_+[];'./,\<>?:"{}|=-aBcDe gHiJkLmNoP
+            3456789`!@#$%^&*()_+[];'./,\<>?:"{ |=-AbCdEfGh
+        r
+    x
+z
