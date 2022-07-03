c4f09ca386a472edcdefe7aef5317e8d4a0ea065
Rascal_Two <therealrascaltwo@gmail.com>
2022-07-03 00:30:23 +0000

    Add Codewars 5864eb8039c5ab9cd400005c JavaScript solution

---
+++ problems/Codewars/5864eb8039c5ab9cd400005c/README.md
@@ -0,0 +1,27 @@
+[_metadata_:generated]: - "true"
+
+# All Star Code Challenge #14 - Find the median
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5864eb8039c5ab9cd400005c`](https://www.codewars.com/kata/5864eb8039c5ab9cd400005c) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Your non-profit company has assigned you the task of calculating some simple statistics on donations. You have an array of integers, representing various amounts of donations your company has been given. In particular, you're interested in the median value for donations.
+
+The median is the middle number of a **sorted list of numbers**. If the list is of even length, the 2 middle values are averaged.
+
+Write a function that takes an array of integers as an argument and returns the median of those integers. 
+
+Notes:
+- The sorting step is vital.
+- Input arrays are non-empty.
+
+### Examples
+
+Median of `[33,99,100,30,29,50]` is  41.5.  
+Median of `[3,2,1]` is 2.
+
---
+++ problems/Codewars/5864eb8039c5ab9cd400005c/solve.js
@@ -0,0 +1,71 @@
+//	Code available at https://codewars.com/kata/reviews/5867022f395d91e5cc000230/groups/62c0e3224989dd00011b001a in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\< ?
+  }|=-aBcDeFgHiJ Lm oP R T VwX
+  12345 789`!@#$%^& ( _+[];'./,\<>?:"{}|=-AbC E GhI
+  MnOpQr tUvWxYz01234 6 8 ` @#$%^&*()_+[];'./, < ?:"{}|=-aBcDeFgHiJk m oPqRsTuVwXyZ01234 6 89` @ $%
+&
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz0123 56789`!@#$%^ *( _
+	  './,\<>?:" }|=-aB DeF HiJkLm oPqRsTuVw yZ 1
+	    89`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdE
+	    lMnOpQrStUvWxYz0123456789`!@#$%^&*
+	    ;'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123
+	  89`
+	  
+	  ()_+[];'./ \<>?:" }|=- bCdEfG IjKlMnOpQ St v
+	    23456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHi kLmNoP
+	    wXyZ0123456789`!@#$%^&*()_+[];'./, <>?:"{
+	  AbC
+	  
+	  lMnOpQrStU WxYz0123 56789` @#$%^& ()_+[ ;' /,\<>?:"{ |= aB D
+	    
+	    qRsTuVwX Z0123456789`   
+	      +[];'. ,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrSt
+	    0
+	
+	    9`!@#$%^ *()_+[];'./,\<
+	      -aBcDeFgH Jk mNoPq sT VwXyZ0 2345678 `! #$%^&* )_+[];
+	      :"{ |=-AbCdEfG
+	      pQr tUvWxYz 1 3456789`!@ $%^&*() +[];'. ,\ >?:
+	      cDeFg iJkLmNoP RsTu
+	        56789`!@#$%^&*()_+[];'./,\
+	        AbCdEfGhIj
+	      S
+	      123456 89`!@#
+	    )
+	
+	    ,\<>?:"{ |=-aBcDeFgHiJkL N
+	        yZ0123 5 7 9`
+	    &
+	
+	    './,\<>? "{}|=-AbCdEf h
+	
+	        UvWxYz0123456789`!@#$%^
+	
+	        ,\< ?:"{ | -aBcDeFgHiJkLmNoPqRsTuVwXyZ
+	
+	        @#$%^&*()_+[];' / \<
+	            fGhIjK MnOpQrStUvWx
+	        89`!
+	            './,\< ?:"{}|=-aBcDeF H JkLmNoPqRsTu w yZ01
+	    8
+	    
+	    _+[ ;'./,\ >?:"{}| -AbCdEf
+	    MnO QrStUvW Y 012
+	    9`!@#$%^&*()_+[];
+	      :"{}| - BcDeFgHiJkLmNo
+	      XyZ012 4 6789`!@#$%^&*()_+[];'
+	      "{}|=- b dEfGhIjKlMnOpQrStUvWxY
+	      789`!@#$%^&*()_+[];'./,\<> :"{}|=- BcDeFgHi kLm oPqRs u wXyZ0123456789`!@#$%^&*()_+
+	      <>?:"{}|=-
+	    G
+	  lMn
+	Qr
+tUvWx
