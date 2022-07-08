2c8de02cffaa41d30eefd722d8943c0f57228c1e
Rascal_Two <therealrascaltwo@gmail.com>
2022-07-08 17:04:44 +0000

    Add Codewars 56d19b2ac05aed1a20000430 JavaScript solution

---
+++ problems/Codewars/56d19b2ac05aed1a20000430/README.md
@@ -0,0 +1,22 @@
+[_metadata_:generated]: - "true"
+
+# Between Extremes
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`56d19b2ac05aed1a20000430`](https://www.codewars.com/kata/56d19b2ac05aed1a20000430) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Given an array of numbers, return the difference between the largest and smallest values. 
+
+For example:
+
+`[23, 3, 19, 21, 16]` should return `20` (i.e., `23 - 3`).
+
+`[1, 434, 555, 34, 112]` should return `554` (i.e., `555 - 1`).
+
+The array will contain a minimum of two elements. Input data range guarantees that `max-min` will cause no integer overflow.
+
---
+++ problems/Codewars/56d19b2ac05aed1a20000430/solve.js
@@ -0,0 +1,81 @@
+//	Code available at https://codewars.com/kata/reviews/56d1fe229b0717caa7000030/groups/62c863ad57e1d20001030739 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{}|=-aB D
+  HiJ LmN P RsTuVwXyZ 123 5 789`!@#$%^
+
+  _+[ ;'./,\ >?: {} =-AbCdEfG
+    Mn pQrS U WxYz 123 5 789`
+    ^& ()_+ ] './, <>? " }|=-
+  D
+
+  iJkLmN PqR T VwXy
+0
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=- Bc eF H
+	  mNoPqRsTuV Xy 01 3
+	6789`!@# %^&*()_+[];'./,\<>?:"{}|=-AbCdEfG I
+		MnO QrStUvWxYz012 4 6789`!@#$%^
+		)_+ ];'./,\<>?:"{} = aBcDeFgHiJk
+	
+		qRs uVwX Z 1 34 6 8 `!@#$%^&*()_+[] './, <
+			{} =-AbCdEfGhI K MnOpQrStUvWxYz0 2
+				89`!@#$%^&*()_ [ ;'./,\<>?:"
+			-
+	
+			gH JkLmNoPqRsT V XyZ0123456789` @
+				*()_+[];'./,\ > :"{}|=-AbCd
+			I
+		M
+	
+		StUvWx z0123456789`! # %^&*()_+[];'./,
+	>
+	
+	}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456 89`! #$ ^&*()_+[]; ./,\ >?:" }|= AbCdEfG IjK MnOpQrSt
+	WxYz0123456789`!@#$%^&*()_+[];'./,\<>? "{}|= aB DeFgHiJkLm oPqR TuVw yZ0 2345678 `!@ $%^&*()_
+	];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrSt vWxYz 123
+	6789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcD FgHiJ LmNo
+	RsTuVwXyZ0123456789`!@#$%^&*()_+[];'./ \<> :"{ |=- bCd fGhIj lMnO
+	rStUvWxYz0123456789`!@#$%^&*()_+[];'./ \<>? "{}| -aBcDeF HiJk
+	
+	
+	RsTuVwXy 0123456789`!@#$% &
+		_+[];'. , <>?
+		}|= AbCd f h jK M O Qr tUvW Y
+			345 789`!@ $ ^&*()_+[];'./,\<>?:"{}|=- B DeFgHiJ L NoPq
+			VwXyZ0123456789`!@#$%
+		(
+	
+		;'./,\ >?:"{}|=
+	b
+	
+	GhIjKlM O QrStUvWxYz0123456
+	9`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJk mNoPqRsTuVwXyZ0123456789`!@#$%^&*()
+	
+	;'./,\< ? "{}|=-AbCdEfGhIjKl
+	OpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\ >?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ
+	
+	456789` @ $%^&*()_+[];'./,\<
+	:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789 !@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDe
+	
+	JkLmNoP R TuVwXyZ0123456789`
+	#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnO QrStUvWxYz0123456789`!@#$%^&*()_+[]
+	
+	,\<>?:" } =-aBcDeFgHiJkLmNoP
+	sTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?: {}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123
+	
+	89`!@#$ ^ *()_+[];'./,\<>?:"
+	|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@# %^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhI
+	
+	nOpQrSt v xYz0123456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRs uVwXyZ0123456789`!@#$%^&*()_+[];'./
+	
+	?:"{}|= A CdEfGhIjKlMnOpQrSt
+	WxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}| -aBcDeFgHiJkLmNoPqRsTuVwXyZ01234567
+	
+	
+	  ^&*
+	_+[
+	
+./,\<
