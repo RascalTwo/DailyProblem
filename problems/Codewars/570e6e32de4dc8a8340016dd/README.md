1d259406b5beea5653cf428a48b6ce95efb50448
Rascal_Two <therealrascaltwo@gmail.com>
2022-07-16 19:35:44 +0000

    Add Codewars 570e6e32de4dc8a8340016dd JavaScript solution

---
+++ problems/Codewars/570e6e32de4dc8a8340016dd/README.md
@@ -0,0 +1,24 @@
+[_metadata_:generated]: - "true"
+
+# The Lamp: Revisited
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`570e6e32de4dc8a8340016dd`](https://www.codewars.com/kata/570e6e32de4dc8a8340016dd) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Define a class called `Lamp`. It will have a string attribute for `color` and boolean attribute, `on`, that will refer to whether the lamp is on or not. Define your class constructor with a parameter for color and assign `on` as `false` on initialize.
+
+
+```if:javascript,typescript,groovy
+Give the lamp an instance method called `toggleSwitch` that will switch the value of the `on` attribute.
+```
+```if:python,ruby
+Give the lamp an instance method called `toggle_switch` that will switch the value of the `on` attribute.
+```
+
+Define another instance method called `state` that will return `"The lamp is on."` if it's on and `"The lamp is off."` otherwise.
+
---
+++ problems/Codewars/570e6e32de4dc8a8340016dd/solve.js
@@ -0,0 +1,70 @@
+//	Code available at https://codewars.com/kata/reviews/5dfeaec5bb26620001e38a31/groups/62d313112575820001ed3d38 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$ ^&*()
+  ];'./,\<>?:"{}|=-aB
+    HiJkLmNoPq s uVwXyZ
+    56789`! # %^&*()
+  ]
+  
+  <>?:"{}|=-AbCdE
+    KlMnOpQ S UvWxYz012
+  6
+  
+  #$%^&*()
+    './,\< ?:"{ |=-a cD FgHiJkLmNo q sTu w yZ012345
+  9
+!
+
+
+#$%^ *( _
+	];'./,\<>?:"{}| -aBcDeF HiJkLmNo qR T
+	  yZ0 2345 7 9`! #$%^&*()_+[]
+	  ,\<>?:" }|=- bC EfGhIj lMnOpQrStUvWxYz0123 56789`!@ $% &
+	    ];'./,\<>?:"{}|=-aBcDe gHiJkLmNoP RsTuV XyZ01
+	  67
+	  @#$%^&* )_+[ ;' /,\<>? "{}|=-AbCdEfGhIjKl nOpQrStUv xYz 1234567 9`!@#$%^& ()_+[];' /, <
+	    |=-aBcDeFgHiJkLmNoPqRsTuVwXyZ 1234567
+	  @#
+	  *()_+[] './, <> :"{}|= AbCdEfGhIjKlMnO QrStUvWxY 012 456789` @#$%^&*() +[];'./, <> :
+	    aBcDeFgHiJkLmNoPqRsTuVwXyZ 123456
+	  !@
+	  &*()_+[];'./,\<>?:"{}|=-AbCdE GhIjKl nOpQrS UvWxYz0123 5678 `! #$%^&*()_+[] '. ,\<>?:"{}|=-aBcDeFgH JkLmNoPqR Tu wXyZ0123456789`!@# %^& ()_+[];'./ \<>? "{ |=-AbCdEfGh jKlMnOpQrSt vWxYz012 45 7
+	    $%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgH JkLm oP RsTuVw
+	  12
+	  789`!@#$%^&*()_+[];'./,\<>?:"{}|=-Ab dEfGhI KlMnOp rSt vWxYz 12 456 89`!@#$%^&*()_+ ];'./,\<>?: {}|=-aBc eF H
+	    oPqRsTuVwXyZ0123456
+	    #$%^&*()_+[];'./,\<>?:"{}| -AbCd
+	  Ij
+	  OpQrStUvWxYz0123456789`!@#$%^ *()_+[ ;'./,\ >?:"{}|=-a cDeF Hi kLmNoPqRsTuVw yZ012345 78 `
+	    &*()_+[];'./,\<>?:"{}|=-AbCdEfG IjKl nOpQ St vWxYz
+	  45
+	
+	  @#$%^&*()_+[] './, <> :"{}|= aBc eFgHiJ LmN PqRsTuV XyZ01234 67 9
+	    ^&*()_+[];'./,\<>?:"{}|=-AbCd fGhIjKl
+	  Qr
+	Uv
+	Yz0123456789`!@# %^&*()_ [];'./,\ >? "
+	  -aBcD FgHiJk m oPqR TuVwXy 012345 789`! #$%^ *()_+ ];'./,\<>?:"{}| -A
+	  fGh jKlM O Q St v x z01 3456 8
+	    %^&*( _+[]; . ,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRs u wXyZ0123456789`
+	    &*()_+[ ; . ,\<>? " } =-AbCd fGhIjKlM Op r
+	      01 345678 ` @#$ ^&*(
+	      /,\ >?:"{}|= a cDe gHiJkLmNoPq
+	      
+	      789`!@#$%^&*()_+[];'./,\<>?:"{}|= AbCdEf
+	      OpQrStUvWxYz0123456789`!@#$%^& ()_+[]
+	      ?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0 2345 789` @# %^&*()
+	      
+	      }| -AbCdE GhI KlMnOp r tUv xYz01
+	      `!@ $%^&* ) +[];'./,\<>?:"{}|=-aBcD F HiJk mNoPqRsT V XyZ01 3 5
+	      
+	      +[];'./,\<>?:"{}|=-Ab dEf hIjKlMnO QrStUv xYz012 4 6789`! #$% & ( _+[ ;'./,
+	      |=-aB DeFgHi kLm oP RsTuVwXyZ0123456789`!@#
+	      
+	      \<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUv xYz012
+	      !@#$%^&*()_+[];'./,\<>?:"{}|=- BcDeFgHiJkLmNoPqRs
+	      123456789`!@#$%^&*()_+[];'./,\<>?:" }|=- bCdE Gh j l nOpQrStU WxYz0123456789`!@
+	    ()
+	  ;
+	/,
+<>?:"
