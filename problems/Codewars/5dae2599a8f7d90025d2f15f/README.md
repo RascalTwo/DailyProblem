d699c9eb54492c3341560d2cf0c47865daf1280e
Rascal_Two <therealrascaltwo@gmail.com>
2022-07-18 18:48:01 +0000

    Add Codewars 5dae2599a8f7d90025d2f15f JavaScript solution

---
+++ problems/Codewars/5dae2599a8f7d90025d2f15f/README.md
@@ -0,0 +1,69 @@
+[_metadata_:generated]: - "true"
+
+# Consecutive Digits To Form Sum
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5dae2599a8f7d90025d2f15f`](https://www.codewars.com/kata/5dae2599a8f7d90025d2f15f) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Positive integers have so many gorgeous features. 
+Some of them could be expressed as a sum of two or more consecutive positive numbers.
+___
+
+### Consider an Example :
+
+* `10` , could be expressed as a sum of `1 + 2 + 3 + 4 `.
+___
+## Task
+
+**_Given_** *Positive integer*, N , **_Return_** true if it could be expressed as a sum of two or more consecutive positive numbers , OtherWise return false .
+___
+
+### Notes 
+
+~~~if-not:clojure,csharp,java,scala
+* Guaranteed constraint  :  **_2  ≤  N  ≤  (2^32) -1_** .
+~~~
+~~~if:clojure,csharp,java,scala
+* Guaranteed constraint  :  **_2  ≤  N  ≤  (2^31) -1_** .
+~~~
+___
+### Input >> Output Examples:
+
+```cpp
+
+* consecutiveDucks(9)  ==>  return (true)  //  9 , could be expressed as a sum of ( 2 + 3 + 4 ) or ( 4 + 5 ) . 
+
+* consecutiveDucks(64)  ==>  return (false)
+
+* consecutiveDucks(42)  ==>  return (true) //  42 , could be expressed as a sum of ( 9 + 10 + 11 + 12 )  . 
+
+```
+
+```prolog
+* consecutive_ducks(9)  ==>  true  % 9 could be expressed as a sum of (2 + 3 + 4) or (4 + 5). 
+
+* consecutive_ducks(64)  ==>  false
+
+* consecutive_ducks(42)  ==>  true % 42 could be expressed as a sum of (9 + 10 + 11 + 12). 
+
+```
+
+
+#### [Playing with Numbers Series](https://www.codewars.com/collections/playing-with-numbers)
+
+#### [Playing With Lists/Arrays Series](https://www.codewars.com/collections/playing-with-lists-slash-arrays)
+
+#### [Bizarre Sorting-katas](https://www.codewars.com/collections/bizarre-sorting-katas)
+
+#### [For More Enjoyable Katas](http://www.codewars.com/users/MrZizoScream/authored)
+___
+
+##### ALL translations are welcomed
+
+#### Enjoy Learning !!
+##### Zizou
---
+++ problems/Codewars/5dae2599a8f7d90025d2f15f/solve.js
@@ -0,0 +1,69 @@
+//	Code available at https://codewars.com/kata/reviews/5dae3f9adefe81000118b52f/groups/5dc3b371b9b5a20001498647 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{}|= a
+  eFgHiJ LmNoPqRsTuVwXy 0 2 456 89
+!
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=-aBcD FgHiJkL No qR T
+	  yZ0123456 89`!@ $%^&*()_ [] '. ,
+	    {}|=-AbCdEfGhIjKlMnOpQrStUvWxYz012345678 `!@#$%
+	    +[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsT VwXyZ01
+	    89`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfG IjKlMn
+	    UvWxYz0123456789`!@#$%^&*()_+[];'./,\<> :"{}|=
+	    FgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&* )_+[];
+	    >?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01234 6789`!@
+	    ()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqR TuVwXy
+	    56789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCd fGhIjK
+	    rStUvWxYz0123456789`!@#$%^&*()_+[];'./,\ >?:"{}
+	    DeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^ *()_+[
+	    \<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz012 456789`
+	    &*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoP RsTuVw
+	    3456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-Ab dEfGhI
+	    pQrStUvWxYz0123456789`!@#$%^&*()_+[];'./ \<>?:"{
+	    cDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$% &*()_+
+	  ./,
+	  :"{}|=-Ab dEfGhI KlMnOpQr tU Wx z
+	    6789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeF HiJkLm
+	    TuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>? "{}|=-
+	    GhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*() +[];'.
+	    :"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ01234567 9`!@#$%
+	    +[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUv xYz012
+	    9`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJ LmNoPq
+	    XyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{} =-AbCd
+	    KlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[]; ./,\<>
+	    =-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@ $%^&*(
+	    './,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz 123456
+	    #$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmN PqRsTuV
+	    23456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-Ab dEfGhI
+	    pQrStUvWxYz0123456789`!@#$%^&*()_+[];'./, <>?:"{
+	    cDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^& ()_+[]
+	    <>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01234 6789`!
+	  ^&*
+	  [];'./,\< ?:"{} =-aBcDeF Hi kL N
+	    uVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{} =-AbCd
+	    KlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'. ,\<>?:
+	    aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^ *()_+[
+	    \<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz012345 789`!@#
+	    )_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVw yZ0123
+	    `!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlM OpQrSt
+	    0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBc eFgHiJ
+	    qRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>? "{}|=-
+	    GhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+ ];'./,
+	    {}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789 !@#$%^
+	    [];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxY 0123456
+	    #$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoP RsTuVw
+	    3456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEf hIjKlM
+	    tUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:" }|=-aB
+	    iJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];' /,\<>?
+	  |=-
+	  EfGhIjKlMn pQrStUv xY 01 3
+	    `!@#$ ^&* ) + ]; ./,\<>?:"{}| - BcDeFgHiJkLmNoPqRsTuVwXy 01 34
+	    !@# %^&* ) + ]; . , <>?:" }|=- b
+	      KlMnO Q S UvWxYz0123456789`!@#$%^& ( _+[];'./,\< ? "{
+	      eFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^ *()_+[];
+	    >
+	  }|=
+	bCd
+fGhIj
