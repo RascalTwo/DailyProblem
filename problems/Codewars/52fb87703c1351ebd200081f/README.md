d1a272438e41b468b418d9c2448014390f16738e
Rascal_Two <therealrascaltwo@gmail.com>
2022-07-21 20:10:51 +0000

    Add Codewars 52fb87703c1351ebd200081f JavaScript solution

---
+++ problems/Codewars/52fb87703c1351ebd200081f/README.md
@@ -0,0 +1,23 @@
+[_metadata_:generated]: - "true"
+
+# What century is it?
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`52fb87703c1351ebd200081f`](https://www.codewars.com/kata/52fb87703c1351ebd200081f) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Return the century of the input year. The input will always be a 4 digit string, so there is no need for validation. 
+
+### Examples
+```
+"1999" --> "20th"
+"2011" --> "21st"
+"2154" --> "22nd"
+"2259" --> "23rd"
+"1124" --> "12th"
+"2000" --> "20th"
+```
---
+++ problems/Codewars/52fb87703c1351ebd200081f/solve.js
@@ -0,0 +1,84 @@
+//	Code available at https://codewars.com/kata/reviews/52fb87703c1351ebd2000822/groups/62d9b2e46d85eb000188816e in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{
+  -aBcD FgHiJk m oPqRsTuVwXyZ012 4 6789
+  #$%^& ()_+ ] './,\< ? "{}
+  AbCdE GhIjKl n pQrStU Wx z0 23 56789` @# %^ * )_+[ ; ./,\ >?: { | -aBc e gHiJ LmN P R TuVw y 0123 567 9 ! #$%^ * )_+[]
+  /,\<>? "{}|=- b dEfGhIj
+l
+
+
+#$%^ *( _
+	];
+	/,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXy 0123456 89`!@ $%^&* )_+[]; ./,\<>?: {}|=-AbC EfGhI KlMnOpQrS
+	vWxYz0123456789`!@#$%^&*()_+[];'./,\<> :"{}|=- BcDeF HiJkL NoPqRs uVwXyZ01 3456789` @#$%^ *()_+[];'
+	,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz 1234567 9`!@# %^&*( _+[];' /,\<>?:" }|=-aBcD FgHiJ LmNoPqRsT
+	wXyZ0123456789`!@#$%^&*()_+[];'./,\<>? "{}|=-A CdEfG IjKlM OpQrSt vWxYz012 456789`! #$%^& ()_+[];'.
+	\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0 2345678 `!@#$ ^&*() +[];'. ,\<>?:"{ |=-AbCdE GhIjK MnOpQrStU
+	xYz0123456789`!@#$%^&*()_+[];'./,\<>?: {}|=-aB DeFgH JkLmN PqRsTu wXyZ0123 56789`!@ $%^&* )_+[];'./
+	<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01 3456789 !@#$% &*()_ [];'./ \<>?:"{} =-aBcDeF HiJkL NoPqRsTuV
+	yZ
+	
+	456789`!@#$%^&*()_ [];'./ \<>?:"{}|= 
+	
+	EfGhI K M OpQrStUvWxYz01234
+	789`! #$%^ * )_+[];'./,\<>?:"
+	|=-aB DeFgHi k mNoPqRsTuVwX
+	
+	23456789`!@#$%^&* )_+[];' /,\<>?:"{} =
+	  dEfGhIjKl nOpQrSt vWxYz01234 6
+	    #$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJk mNoPqRs uVwXy 01234 6789`! #$%^&*() +[];'./, <>?:" }|=-AbCd
+	    KlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+ ];'./,\ >?:"{ |=-aB DeFgHi kLmNoPqR TuVwXyZ0 23456 89`!@#$%
+	
+	  +[]
+	./,
+	
+	:"{}|=-AbCdEfGhI KlMnOpQ StUvWxYz01 3
+	  89`!@#$ ^&*()_+ ];'./,\<>? "
+	    BcD FgHi k m oP R T VwXy 01234
+	      #$% &*() + ];'./,\<>?:"{}|=-AbCdEfGhIjKlMn    
+	      012 456 8 `!@#$%^&*()_+[];'./,\
+	      =-aBcDeFgHiJkLmNoPqRsTuVwXyZ012345678 `!@#$   
+	    ]  
+	  <>?
+	{}|
+	
+	CdEfGhIj lMnOpQrStUvWxYz01234 6
+	  !@# %^&*()_ [ ;'./,\<>?:"{}|=-aBcDeFgHiJkL
+	  qRsTuV XyZ0123 5 789`!@#$ ^ *( _+[ ; ./ \<>?:"{ |= Ab d fGhI K MnOpQrS U Wx z01 3 56 89`!@#$ ^& () + ];'. , <>?:"{} = aB DeF H Jk mNoPqRs uV Xy 0 2345 7 9`!@#$
+	&
+	
+	+[ ;'./,\<> :"{}|=- bCdEfGhIj lM OpQrSt vWxYz 1234567 9`! #$%^& ()_+ ];'.
+	\<>?:"{}|= aBcDeFgHi
+	L
+	  qRs uVw y 0123456789`!@#$%^&*(
+	  ];' / \ >?:"{}|=-AbC
+	  
+	  lM OpQrStUvWxYz01234567	9` @#$%^&
+	  +
+		   , < ? " }|
+	  c
+		
+		JkL No
+		sTu w y 0123456789`!@#$%^&*()_+[]
+	  ,\< ? " }|=-AbCdEfGhIjKlMnOpQrStU
+		Yz0 23
+		
+		`! #$ ^& ()_ [] ' /, <>?:
+		|
+			c e gHiJk
+		o
+		sTuV XyZ0 23 567 9` @ $% &*()
+		]
+			, < ?:"{}
+		A	
+		fGhI KlMn pQ StU Wx z 12 4567
+		!
+			^ * )_+[]
+		/ \<>? " } =-aBc
+		
+		J L NoPqRsTuV X Z0
+		456789 !@
+	%^&
+	
+_+[];
