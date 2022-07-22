2be8b0b832ab38e11ba4e800fe4bd7a849137caa
Rascal_Two <therealrascaltwo@gmail.com>
2022-07-22 16:41:59 +0000

    Add Codewars 56e20642ddeb0f4fac000344 JavaScript solution

---
+++ problems/Codewars/56e20642ddeb0f4fac000344/README.md
@@ -0,0 +1,28 @@
+[_metadata_:generated]: - "true"
+
+# Array Mappings
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`56e20642ddeb0f4fac000344`](https://www.codewars.com/kata/56e20642ddeb0f4fac000344) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+# Array Mappings
+
+Oh no, the ```map``` method for arrays has been disabled.  Can you fix it?
+
+In case you haven't come across the ```map``` method for arrays, here is how it works:
+
+```javascript
+[1,2,3].map(x => x ** 2) === [1,4,9]
+[1,2,3].map(x => 2 * x) === [2,4,6]
+[1,2,3].map(x => 2 ** x) === [2,4,8]
+[1,2,3].map(x => x.toString()) === ["1","2","3"]
+["1","2","3"].map(x => parseInt(x)) === [1,2,3]
+["random","array","to","be","mapped"].map(x => mapping(x)) === [mapping("random"),mapping("array"),mapping("to"),mapping("be"),mapping("mapped")]
+```
+
+The ```map``` method does not mutate the original array.
---
+++ problems/Codewars/56e20642ddeb0f4fac000344/solve.js
@@ -0,0 +1,57 @@
+//	Code available at https://codewars.com/kata/reviews/56e2495d04223b364200005c/groups/62dad35992f0ac00018fe13c in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^&*()_+[];'./ \ >?:"{}|=-aBcDeFgHi kLmNoPqRs
+  wXyZ0 2345678 ` @#$
+  *() +[]; . , <> : { |=-AbCdEfGhI KlMnO
+    UvWxYz0123456789`!@#$%^&*()_+[];'. ,\ >?:"{ |=-aBcDe gH JkLmNoP
+  T
+  XyZ012 456789`!
+#
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_ [ ; ./,\<>?:"{}|=-AbCd
+	GhIjKlMnOpQrStUvWxYz012345678 ` @#
+	
+	*()_+[];'./,\<> :"{ |=-aBcDe gH Jk m
+	  RsTuVwXyZ0 234 6789 !@# %^&*( _+[];'. ,\ >? "
+	    bCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'. ,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRs
+	    Z0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=- bCdEfGhIjKlMnOpQr
+	    Yz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=- BcDeFgHiJkLmNoPq
+	    XyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEf hIjKlMnOpQrStUvWxYz012
+	    9`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789 !@#$%^&*()_+[];'./,\
+	    }|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\ >?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ012345678
+	    %^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01 3456789
+	    ^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[]; ./,\<>?:"{}|=-AbCdEfGhI
+	  nOp
+	  UvWxYz0123 567 9`!@#$ ^&*()_+[ ;'./,\< ?: {} =
+	    FgH JkLmN P RsTuVwXyZ0123456
+	    #$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGh jKlMnOpQrStUvWxYz01234
+	    !@#$%^&*()_+[];'./,\<>? "{}|=-aBcDeFgHiJk
+	    RsTuV X Z0123456789`!@#$%^&*()_+[];'
+	    ?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0 23456789`!@#$%^&*()_+[];'
+	    ?:"{}|=-aBcDeFgHiJkLmNo qRsTuVwXyZ0123456789`!@#$%^&*
+	    ;'./, < ?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!
+	    *()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVw yZ0123456789`!@#$%^&*()_+[];'./,\
+	    }|=-AbCdEfGhIjKlMnOpQrS UvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-
+	  eFg
+	  LmNoPqRsTu wXyZ 123 56789`!@#$%^&*()_+[];'./,\< ?:"{}|= Ab dE G
+	    nOp rStUvWxY 0 23456789 !@#$% &
+	      './ \ > :"{
+	      eFg iJkL N P Rs u w yZ0123456 89`! #
+	        ];'. , <>?:"{}|
+	      G
+	      pQrStU Wx
+	    4
+	    !@# %^&*()_+ ];'./,\<>?:"
+	    BcD FgHiJkLm o qRsTuVwXy 01 3456789`!@#$%^&*( _+[];'./,\< ?: {}| -AbCdEfGhIj lMnOpQrS Uv xY 012345678 `!@#$%^& ()_+[];'./, <>?:"{}|=- BcDeFgHiJkLm oPqRsTuV XyZ0123456789`!@ $%^&*() +[];' /,\<> :"{}|=-AbCdEfGhI KlMnOpQrStU WxYz0123456789`!@#$%^&*
+	    ;'. ,\<> : { |= a c eFgH JkLm o
+	      XyZ0123 5 789`!@#$%^&*()_+[]; ./,\ >? "{ |=-AbCdEfGh jKlMn
+	      WxYz0123456 8 `!@#$%^&*()_+[];'./,\<>?:"{}|= a cDeFgHiJk
+	      TuVwXyZ0123456789`!@#$%^&*()_+ ]; ./,\<>?:"{}|=-Ab dEfGhIjKlMnOpQrSt v xY 0123456789`!@#$%^
+	    [
+	  /,\
+	?:"
+}|=-a
