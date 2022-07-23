e86f5fda34d36e2a60029e30c395a5eda621847b
Rascal_Two <therealrascaltwo@gmail.com>
2022-07-23 20:54:39 +0000

    Add Codewars 528d9adf0e03778b9e00067e JavaScript solution

--- problems/Codewars/528d9adf0e03778b9e00067e/solve.js
+++ problems/Codewars/528d9adf0e03778b9e00067e/solve.js
@@ -1,12 +1,10 @@
-//	Code available at https://codewars.com/kata/reviews/528d9c38cc451c8fc900061a/groups/62dc5e826bf9ea00012caf6c in accordance with https://docs.codewars.com/community/rules/#rules
+//	Code available at https://codewars.com/kata/reviews/528d9c38cc451c8fc900061a/groups/62dc601470f8aa0001c20e91 in accordance with https://docs.codewars.com/community/rules/#rules
 
 `!@#$%^& ()_+[];'./,\<>?:"{}|
-  BcD FgHi k m oP R T VwXyZ01234567 9`!@#
-    ()_ [];' / \ >? " } =-AbCdEfGhIjK MnOpQ
-      Yz 123456789`!@# %^&*() +[] './
-    :
-  |
--
+  BcD FgHi k mNo
+  sTuVw yZ01 3 56789`!@#$%^&*()_+[ ;' /,\<> : {}|=-AbCdEfGhIj lMn pQrS
+  WxYz01 345678 `!@#$%
+&
 
 
 #$%^ *( _
