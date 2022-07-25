31de11299d4cb43316f175a86f9756c186aecdaa
Rascal_Two <therealrascaltwo@gmail.com>
2022-07-25 18:00:51 +0000

    Add Codewars 5a34da5dee1aae516d00004a JavaScript solution

---
+++ problems/Codewars/5a34da5dee1aae516d00004a/README.md
@@ -0,0 +1,30 @@
+[_metadata_:generated]: - "true"
+
+# Matrix creation
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5a34da5dee1aae516d00004a`](https://www.codewars.com/kata/5a34da5dee1aae516d00004a) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Create an identity matrix of the specified size( >= 0).
+
+Some examples:
+
+```
+(1)  =>  [[1]]
+
+(2) => [ [1,0],
+         [0,1] ]
+
+       [ [1,0,0,0,0],
+         [0,1,0,0,0],
+(5) =>   [0,0,1,0,0],
+         [0,0,0,1,0],
+         [0,0,0,0,1] ]   
+
+```
+
---
+++ problems/Codewars/5a34da5dee1aae516d00004a/solve.js
@@ -0,0 +1,38 @@
+//	Code available at https://codewars.com/kata/reviews/5a34dbdd5c76ea82dc0019ed/groups/62deda5504290d00011411a0 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:" }
+  aBcDe gHiJkL N PqR
+  VwX Z012 4 6 89 ! # %^&*()_ [];'.
+    ?:"{} =-A C EfG
+    MnO QrSt v x z0 2 4 6789`!@ $%^& ()_+[];'./,\ >?: {}|=
+    eFgHiJkLmNoPqRsTu
+  y
+  234567 9`!@#$%
+& 
+
+
+#$%^ *( _
+	];'./,\<>?:"{}| -aBcDeFgH Jk m
+	  RsTuVwXyZ0123456789`!@#$%^&*()_+[]; ./,\
+	  "{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz012 456789`
+	  %^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHi kLmN PqR TuV XyZ01
+	  6789`!@#$%^&*()_+[];'./,\<>?:"{}|=- bCdE Gh jK Mn pQr tUv xY 01 34 678 `!@ $% &* )_ []; ./, <> :" }| -aB DeF Hi kL No qRsTu
+	XyZ
+	
+	456789`!@#$%^&*( _+[];'. ,\ >? "
+	    bCdEf hIjKlMnOpQrSt v xYz 12 45 789`!@#$%^&*()_+[];'./,\ > :" } = a cD F HiJ
+	    qRsTu wXyZ0 2 456789`! #$ ^
+	      ;'./,\ >?:"{}|=-AbCdEfGhIj lMnOpQrS UvW Yz 12 4
+	        %^&*() +[];'./,\<>?:"{}|=- BcDeFgHi kLmN PqR Tu wXyZ0123 567 9`!@#$%
+	    + 
+	          
+	    bCd fGhI K M Op r tU WxY 01234
+	      #$% &*()_+ ] './,\<>?:"{}|=-a cDeF
+	
+	      qRsTuVwXyZ0 2345 7 9` @#$ ^&*()_+[];'./ \< ?: {
+	        GhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*( _+[];'./,\<>?: {}| -aBcDe gHiJ LmN PqRsTu wXyZ0 234567
+	      ^&
+	    ];
+	/,
+	
+?:"{}
