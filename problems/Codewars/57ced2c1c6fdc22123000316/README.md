5a24f83c70e383d3e43bdc461ce14194f10475d5
Rascal_Two <therealrascaltwo@gmail.com>
2022-08-01 19:09:29 +0000

    Add Codewars 57ced2c1c6fdc22123000316 JavaScript solution

---
+++ problems/Codewars/57ced2c1c6fdc22123000316/README.md
@@ -0,0 +1,22 @@
+[_metadata_:generated]: - "true"
+
+# Numbers to Objects
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57ced2c1c6fdc22123000316`](https://www.codewars.com/kata/57ced2c1c6fdc22123000316) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+You will be given an array of numbers.
+
+For each number in the array you will need to create an object. 
+
+The object key will be the number, as a string. The value will be the corresponding character code, as a string.
+
+Return an array of the resulting objects.
+
+All inputs will be arrays of numbers. All character codes are valid lower case letters. The input array will not be empty.
+
---
+++ problems/Codewars/57ced2c1c6fdc22123000316/solve.js
@@ -0,0 +1,47 @@
+//	Code available at https://codewars.com/kata/reviews/57cfffb747ff23689a0000c9/groups/57de7ba58a8b8d71c9000133 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./
+  >?:"{} =-aBcDeFg iJ Lm oPqRsT VwXyZ0123456789`!@#$%^&* )_+[
+;
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz012  56 89`!@#$%^ *( _+ ]
+	  ,\<>?:"{}|= aBc eFgHi kLmNoPq sT Vw y
+	    56789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOp rStUvWxYz012 456789`!@#$%^&
+	    ];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*() +[];'./,\<>? "{}|=-AbCdEf hIjKlMnOpQrS UvWxYz012345 789`!@#$%^&*()
+	    ./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*
+	    ;'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeF
+	    mNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>? "{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDe
+	  JkL
+	
+	  sTuVwXyZ01 3456789`! #$%
+	    +[];' /,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123
+	    `!@#$%^& ()_+[];'./,\
+	    }|= aBc e gHi
+	
+	      TuV XyZ0 2 4 67 9 ! #$%^&*()_ [];' /
+	
+	        AbC EfGhI K MnO QrStUvW
+	        789 ! # %^&*(
+	        \<> : { |=-aBcDeFgHiJkLmNoPqRsTuVw
+	        789`!@#$ ^ *(
+	        \<>?:"{}|=-AbCdE
+	        p
+	      Yz0123 5678
+	    %
+	
+	    ];' /,\<>?:"{ |=- BcD FgHi kLmN PqRs uVwX Z012 4567 9`!@ $%^& ()_+ ];'. ,\<> :"{} =-Ab dEfG IjKl nOpQ StUv xYz0 2345 789` @#$% &*() +[];'
+	
+	    "{} =-aB DeFgHiJkLmNoPq
+	      Z01 34567 9`!@#$%^&*()_+[];'
+	      "{} =-Ab dEfGhIjKlMnOpQ StUvWxYz0123456789`!@#$%^&*()_+[];'./,\
+	      =-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456 89`!@#$%^&*()_+[];'./,\<>?:" }|= AbCdEfGhIjKlMnOpQrStUvW
+	    3
+	  89
+	@#
+%^&*(
