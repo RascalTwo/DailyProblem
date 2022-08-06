c1be0f9dbcb9a2f32d72c3438a8f666c9e26365d
Rascal_Two <therealrascaltwo@gmail.com>
2022-08-06 23:09:53 +0000

    Add Codewars 559e10e2e162b69f750000b4 JavaScript solution

---
+++ problems/Codewars/559e10e2e162b69f750000b4/README.md
@@ -0,0 +1,15 @@
+[_metadata_:generated]: - "true"
+
+# What dominates your array?
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`559e10e2e162b69f750000b4`](https://www.codewars.com/kata/559e10e2e162b69f750000b4) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+A zero-indexed array ```arr``` consisting of n integers is given. The dominator of array ```arr``` is the value that occurs in <b>more</b> than half of the elements of ```arr```.<br/>
+For example, consider array ```arr``` such that ```arr = [3,4,3,2,3,1,3,3]```<br/> The dominator of ```arr``` is 3 because it occurs in 5 out of 8 elements of ```arr``` and 5 is more than a half of 8.<br/>
+Write a function ```dominator(arr)``` that, given a zero-indexed array ```arr``` consisting of n integers, returns the dominator of ```arr```. The function should return −1 if array does not have a dominator. All values in ```arr``` will be >=0.
---
+++ problems/Codewars/559e10e2e162b69f750000b4/solve.js
@@ -0,0 +1,72 @@
+//	Code available at https://codewars.com/kata/reviews/559e115df31dcaa022000052/groups/62eef4c60045410001449651 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<> :
+  |=-aB DeFgHiJkL N PqRsTuVwXy 0 23
+
+  89`!@ $%^&*( _ []; ./,\<>
+  {}| -AbCdE GhI Kl nOpQr
+    xYz01 3456789` @ $%^&*()_+[];'./, <> :" } =-
+    Fg iJkLmNoPq s uVwXyZ0123 56789` @#$%
+    _+[];'./,\<>?:" }|=-AbCdEf
+  j
+  
+  QrStUv xYz
+1
+
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz 123456789 !@#$ ^&*()_+[ ;' /, <
+	  {}|=-aBcD FgHiJkL No qR T
+	    0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIj
+	    QrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"
+	    BcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+
+	    ,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`
+	  %^
+	  
+	  ;'./,\<>?: {}|=-aB De gH J  
+	      uVwXyZ01 3456789` @
+	        []; ./,\< ? "{} =-Ab d fGhIjKlM Op rS
+	        345 789` @ $ ^& ( _ [];'./,\< ?:"{ |
+	          kLmNoPqRsTu w yZ0123456789 !@ $% & ()
+	          ?: {}|=-AbCdEfG I KlMnO
+	            345678 `!@#$
+	          .
+	        |=-aBc eFg
+	       q
+	
+	      12345 789` @ $%^&*()_ [];'. ,\<> : {}|=-A CdEfGhIjKlMnOpQrStUvWxYz 1 3456 8 `!@ $ ^& ( _+[] '.
+	      {}|=- BcDeFgHiJ L NoPqRsTu wXyZ0 2345 7
+	            +[] './ \ >?:
+	            IjK MnO Q StUvWxYz0 23456
+	            _+[ ;'./ \ > :" } =- BcDe gHiJ L
+	                456789`!@#$%^&*( _+[];'
+	            A
+	            pQrStU WxYz
+	        `
+	      ()_+[ ;'./,\<>?:"{}| - BcDeFgHi kLmNo qRsT V
+	            `!@ $%^ * )_+
+	            }|= AbC E GhIjKlMnO QrStU
+	            9`! #$%^ * )_+ ]; ./
+	            cDe gH J LmNoPqR TuVwXy
+	            #$% &* ) +[];'./,\< ?:"{}|
+	            MnO QrSt v x z0 2 4 6789` @#$%
+	                :"{}|=-aBcDeF
+	            uVw yZ 1 34 6789 !@#$
+	                ?:"{}|=-AbCdE
+	            tUvWxY 0123
+	        $
+	      [];'./, < ? "{ | - BcDe gHiJk
+	         wXy 0 2 456789`!@#$%^&*()_+[] ' /, <>?:"{}|
+	         jKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+ ];'./,\<>?:"{}|=- BcDeFgHi kLm oPq s uVwXyZ0123456789`!@#$%^
+	         /,\ > : {}|=-AbCdEfGhIjKlMn pQrStUvW
+	         89`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFg iJkLmNoPqRsTuVwXy 01234567 9`! #$% & ()_+[];'./,\<>?:"{}|=-A
+	        l
+	    StU
+	xYz
+	
+34567
