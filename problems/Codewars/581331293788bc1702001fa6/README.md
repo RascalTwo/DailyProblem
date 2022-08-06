f890172cac527a6091e76f7f74546198d73c4f33
Rascal_Two <therealrascaltwo@gmail.com>
2022-08-06 18:21:27 +0000

    Add Codewars 581331293788bc1702001fa6 JavaScript solution

---
+++ problems/Codewars/581331293788bc1702001fa6/README.md
@@ -0,0 +1,24 @@
+[_metadata_:generated]: - "true"
+
+# Framed Reflection
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`581331293788bc1702001fa6`](https://www.codewars.com/kata/581331293788bc1702001fa6) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+<h1><strong>100th Kata</strong></h1>
+
+You are given a message (text) that you choose to read in a mirror (weirdo). Return what you would see, complete with the mirror frame. Example:<br>
+
+'Hello World'
+
+would give:
+
+
+<img src="http://res.cloudinary.com/dfvyityr2/image/upload/v1477656440/kata_examp_ypboka.png">
+
+Words in your solution should be left-aligned.
---
+++ problems/Codewars/581331293788bc1702001fa6/solve.js
@@ -0,0 +1,68 @@
+//	Code available at https://codewars.com/kata/reviews/581340cbe9d997014f000057/groups/62eeb1287814d50001a6534f in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<
+  "{}|= aBcDe g iJkLmNoPqRsT VwX
+  12345 789`!@#$%^&*( _ [];'./,\<>?:"{}|=-AbCdEfGhIj lMnOp rS UvWxYz0123456789`!@#$%^ *()_+[];'./,\ >?:
+  
+  aBcDe gHiJk m oPqRsTuVwXyZ0123456789`!@ $ ^&*(
+  []; ./,\<> :"{} =- bCdEfGh
+    nOpQrStUvWxYz 1 3 56789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmN P Rs u w yZ0
+  4
+  89`!@#$%^&*()_+[];'./
+
+  ?:"{}| -AbCdEfGhIjKlMnOp
+r
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz01 3456789`!@#$ ^& () +
+	  ./,\<>?:" }|=-aBc eF Hi k
+	    RsTuVwXyZ0123456789`!@#$%^&*()_+ ];'./,\< ?:"{}|=-AbCdE GhIjK MnOp rStUv xYz0123456789`!
+	    *()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNo qRsTuVwXyZ012345 789`!@#$ ^&*()_+[];'./,\<>? 
+	  =-
+	
+	  GhIjKlMnOp rStUvWxYz 123
+	    `!@#$ ^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqR
+	
+	    01234567 9`!@#$%^&*()_+[
+	      >?: {}|=-AbCdEfG IjK M Op
+	
+	      0123456789`!@#$%^ *()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ012 45678 `!@
+	
+	      []; ./,\ > : {} =-AbCdEfGhIjKl nOpQ St vWxYz0123456789`!@#$ ^&* )_+[];'./,\<>?:"
+	
+	      FgH JkLm o q sT VwXyZ012345678 `!@# %^&*( _+[];'./,\<>?:"{}|=- bCdEfGhIjKlMnOpQr tU
+	
+	      56789`!@#$%^&*()_+[];'./,\<>
+	
+	      cDeFg iJkLmNoPqRsTuVwX Z 123456 89`!@#$% & ()_+[];' / \<>?
+	
+	      dEfGhIjKlMnOpQrStUvW
+	      56789`!@#$%^&*()_+[];'.
+	
+	      |=-aBcDeFgHiJkLmNoPqRsTuVwXyZ
+	      89`!@# %^&*()_+[];'./,\
+	
+	    =
+	
+	    hIj lMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFg iJkL NoPq sTuV XyZ0 2345 789` @#$% &*() +[]; ./,\ >?:" }|=-A
+	
+	    jKl nOpQ StUvWxYz0123456
+	      %^& ()_+[ ;'./,\<> :"{}|=-aBcDeFgHiJ
+	      sTu wXyZ 123456789`!@#$%
+	        './,\<>?:"{}|=-Ab
+	        MnO QrSt vWxYz012345678 `!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJ
+	        uVwXyZ0123456
+	        &*()_+[];'./,
+	        -AbCd
+	        O
+	      xYz0123456789`!@ $%^  
+	      ./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456 89`!@#$% &*( _+[];'./,\<>?:"{}|=-AbCdEf
+	      O
+	    vW
+	z0
+23456
