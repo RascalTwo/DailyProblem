8870fe45693f8abede0c189442e48a2789ea8041
Rascal_Two <therealrascaltwo@gmail.com>
2022-08-07 04:03:54 +0000

    Add Codewars 56044de2aa75e28875000017 JavaScript solution

---
+++ problems/Codewars/56044de2aa75e28875000017/README.md
@@ -0,0 +1,17 @@
+[_metadata_:generated]: - "true"
+
+# CompoundArray
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`56044de2aa75e28875000017`](https://www.codewars.com/kata/56044de2aa75e28875000017) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+You have to create a method "compoundArray" which should take as input two int arrays of different length and return one int array with numbers of both arrays shuffled one by one. 
+```Example: 
+Input - {1,2,3,4,5,6} and {9,8,7,6} 
+Output - {1,9,2,8,3,7,4,6,5,6}
+```
---
+++ problems/Codewars/56044de2aa75e28875000017/solve.js
@@ -0,0 +1,47 @@
+//	Code available at https://codewars.com/kata/reviews/56085a6e5813ae84cb000013/groups/62ef39ab7814d50001a66d4f in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{}|=-aB D
+  HiJkL NoPqRs u wXy
+  23456 89` @ $%^&*()_+[];'./,\<>?:"{} =- bCdEfGhIjKl
+  pQr tUvW Y 0 23 5 7 9`!@ $%^&*
+    ];' /,\<>? "{} =- BcDeFgH Jk mN P RsTuVwXyZ01 3456789`!@#$%^&*()_+
+  '
+  \<>?:" }|=-AbC
+E
+
+
+#$%^ *( _
+	];'./,\<>?:"{}| -aBcDeF HiJkLmNoPq s
+	  XyZ0123456789`!@#$%^&*()_+[];'./,\<>? "{}| -AbC EfG IjK MnOpQ StUv xYz 123 567 9`! #$%^&
+	  +[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCd fGhIj lMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcD
+	  iJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{ |=-A CdEfGhIjKlMnOpQrStUvWxY
+	  3456789`!@#$%^&*()_+[];'./,\<>?:"{}| -aBcDeFgHiJkLmNoPqRsTuV XyZ0123456789`!@#$%^&*(
+	+[]
+	
+	,\<>?:"{}|=-AbCd fGhIjKl nOpQrStUvW Y
+	  3456789` @#$%^&*()_+ ]; .
+	    :"{ |=-aBc e gHi
+	    PqR TuVw y 0 23 5 7 9`!@#$%^&*()_+[];' /,\<>?:"{} =-Ab d
+	      Mn pQ S UvWxYz012 456789`!@#$%^&*()_
+	      \< ?: { |=-aBcDeF HiJkLmNoPqRsTuVwXy
+	    5
+	    @#$%^& ()_+[];
+	  \
+	  
+	  =-AbCdEf hIjKlMnOpQrStUvWx z
+	    678 `!@#$% & ()_
+	    /,\ >?:" } = aB D F HiJkLmN PqRs u
+	      3456789`!@#$%^&*()_+[];'./,\ > :"{}|=
+	    f
+	    MnOpQr tUvWxYz
+	  4
+	  
+	  #$% &*() + ] '. , < ?:" }|=- B
+	    iJk m o qRsTuVwXyZ0123456789`!@#$% & ()_+[]
+	    <>? " } =-AbCdEfGhIjKlMnOpQrStUvWx z 123456
+	    
+	    ()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwX Z0123456789`!@ $%^&*()_+[] './,
+	  :
+	}|=
+	
+CdEfG
