61a76f1f43a605e608646998e2e11ee7c5c62931
Rascal_Two <therealrascaltwo@gmail.com>
2022-08-09 19:35:08 +0000

    Add Codewars 586305e8916e244b66001a93 JavaScript solution

---
+++ problems/Codewars/586305e8916e244b66001a93/README.md
@@ -0,0 +1,33 @@
+[_metadata_:generated]: - "true"
+
+# Mirror object - tcejbo rorriM
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`586305e8916e244b66001a93`](https://www.codewars.com/kata/586305e8916e244b66001a93) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+# Mirror - <span style="display: inline-block; transform: scale(-1, 1)">Mirror</span>
+
+Can you mirror the properties on an object?
+
+Given an object with properties with no value
+
+```
+abc: -
+arara: -
+xyz: -
+```
+
+Return a new object that have the properties with its mirrored key!
+
+```
+abc: cba
+arara: arara
+xyz: zyx
+```
+
+"You cannot change the original object, because if you did that the reflection would change."
---
+++ problems/Codewars/586305e8916e244b66001a93/solve.js
@@ -0,0 +1,82 @@
+//	Code available at https://codewars.com/kata/reviews/58630628916e244b66001a96/groups/62f2b6ed8dd1c600019f70c9 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$ ^&*()_ [ ;'. ,\ >?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVw yZ01 34 6
+  `!@#$%^&*() + ];'./,\<>?:"{}|=-AbCdEfGhIjK
+  OpQrSt vWxYz01
+34 6789
+
+
+#$%^ *( _
+	];'./ \<>?:" } =-aBcDeFgHiJkLmNoPqRsTu
+	XyZ01 34567 9 !@#$%^&*()_+[];' /,\<>?:"{}|=- bCdEfGhIjK MnOpQrStU WxYz0
+	34567 9`! # % &* )_+[];'./,\<>? " }| -aBcDe gHiJkLmNoPqRsTuVwXyZ012
+	56789 !@# % & () +[];'./,\<>?:"{}|=-AbCdEf hI Kl n
+	  StUv x z012345678
+	  #$%^&* )_
+	]; ./,\
+	
+	
+	}| -aBcD
+	gHiJkLmNoPqRsTuVwX Z0 23 5
+	  `!@#$%^&*() +[ ;' /
+	    "{}|= AbCdEfGh j l
+	      UvWx z01234
+	      #$%^&* )_+[];'.
+	    :"
+	    BcDeF HiJkLm o qRsTuVwXyZ0123456789`!
+	    
+	    ];'./,\<>?:"{}|=-AbCdEfG
+	    nOpQrStUvWxYz0123456789`!@#$%^& () +[];'./,
+	    
+	    BcDeFgHiJkLmNoPqRsTuVwXy 0123456789
+	  $%^
+	
+	  [];'./,\<>?:" }| -A C
+	    jKlMn pQrStUvW Y 0
+	      9`!@#$% &*()_+[
+	      >? "{}|= aBc eF HiJkL NoP
+	    wX
+	    45678 `!@#$% & ()_+[];'./,\<>?:"{}|=-
+	    
+	    MnOpQrStUvWxYz0123456789
+	    ^&*()_+[];'./,\<>?:"{}|=-aBcDeF Hi kLmNoPqR
+	    
+	    456789`!@#$%^&*()_+[];'. ,\<>?:"{}|
+	  CdE
+	  
+	  nOpQrStUvW Yz0 234567 9`!@#$%^ *()_+[]; ./ \< ?
+	    -aBcD FgHiJkLm o q
+	      Z012345 789`!@#
+	      +[ ;'./, <>? "{ |=-Ab dEf
+	    Mn
+	    UvWxY 01234567 9 !@#$%^&*()_+[]
+	    <>?:" }|=-aB D FgHiJkLmNoPqRsTuV
+	    
+	    89`!@#$%^&*()_+[];'./,\< ?:"{}|=-Ab
+	    IjKlMnOpQrStUvWxYz01234567 9`!@#$%^&*()_+[
+	  /,\
+	
+	  }|=-aBcDeFgH Jk mN P
+	    wXyZ0 234567 9 !@ $% &*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOp
+	    WxYz0 234 6 89 !@ $%^&*()_+[];'./, < ?:" } =-a
+	    
+	    NoP RsTu w y 01 3 5 789 !@#$ ^
+	      ;'. ,\<>?:"{ | -Ab
+	      
+	      StU W Y 012345
+	      $%^ *()_ [ ; ./ \ > :" }|=- B
+	        mNo qRs u wXyZ01234
+	        %^&*()_+[];'. , <>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvW
+	      5
+	      
+	      +[] './,\< ? "{}|=-aBcDeFgHiJkLmNoP
+	      
+	      67 9`!@#$%^&*()_+[];'./,\<>
+	      Ab dEfGhIjKlMnOpQrStUvWxYz01234567 9` @#$%^&*(
+	      
+	      {}|=-aBcDeFgHiJkLmNoPqRs uVwXyZ0123
+	    `
+	  %^&
+	)_+
+	
+'./,\
