373bb87b812e68d7232af8ca5397536e35c0dcc2
Rascal_Two <therealrascaltwo@gmail.com>
2022-08-13 20:21:34 +0000

    Add Codewars 55eeddff3f64c954c2000059 JavaScript solution

---
+++ problems/Codewars/55eeddff3f64c954c2000059/solve.js
@@ -0,0 +1,26 @@
+//	Code available at https://codewars.com/kata/reviews/55ffd592c5794f47fa000009/groups/62f807f2cafc030001eb4fd7 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{ |
+  BcDeF HiJk m oPq
+  uVw yZ01 3 5 78 ` @ $%^&*()_+ ];'./
+    :"{ | - b d fG
+    MnO Qr t v xYz012345 789` @# %^&*( _+[ ;'./, <>?:"{
+    BcDeFgHiJkLmNo q sT V XyZ0
+    6 8 ` @ $%
+  (
+  [];'./ \<>?:
+{
+
+
+#$%^ *( _
+	];'./,\<>?:"{ |= aBcDeFgHiJkLmNoPqRsTuVwX Z
+	23456789` @#$%^&*()_+[];'./ \    
+	    AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcD FgH JkLm oPqRsTuVwXyZ01234 678 `!@ $%
+	    +[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`! #$% &*() +[];'./,\<> :"{ |=- Bc
+	    JkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhI KlM OpQr tUvWxYz012345678 `!@ $%^ *(
+	    './,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789` @#$ ^&* )_+[ ;'./,\<>?:" }|= AbC E G
+	    nOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkL NoP Rs uVw yZ0 234 678 `!@# %^&*()_+[];'./,\<>?:"{}|=- bCd fGh jK
+	    rStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBc eFg iJk mNoP RsTuVwXyZ0123 567 9`! #$
+	    _+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$ ^&* )_+ ]; ./ \<> :" }|=- BcD FgHi kLmNoPqRsTuVwXyZ01234 678 `!@ $%
+	*()_
+[];'.
