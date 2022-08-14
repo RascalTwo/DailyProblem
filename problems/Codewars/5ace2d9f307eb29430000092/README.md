6b551a672437f3f4ac3a97a6f1f1407c3f35f805
Rascal_Two <therealrascaltwo@gmail.com>
2022-08-14 00:39:26 +0000

    Add Codewars 5ace2d9f307eb29430000092 JavaScript solution

---
+++ problems/Codewars/5ace2d9f307eb29430000092/README.md
@@ -0,0 +1,33 @@
+[_metadata_:generated]: - "true"
+
+# Multiply Word in String
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5ace2d9f307eb29430000092`](https://www.codewars.com/kata/5ace2d9f307eb29430000092) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+1. You are to write an function that takes a string as it's first paramter. This string will be a string of words. 
+
+2. You are expected to then use the second parameter, which will be an integer, to find the corresponding word in the given string. The first word would be represented by 0.  
+
+3. Once you have the located string you are finally going to multiply by it the third provided paramater, which will also be an interger. You are additionally required to add a hyphen in between each word. 
+
+  Example  
+  ``` javascript
+  modifyMultiply ("This is a string",3,5) 
+  ```
+  ``` python
+  modify_multiply ("This is a string",3,5) 
+  ```
+  Should return 
+  
+  ``` "string-string-string-string-string" ``` 
+  
+  Since the 3rd word is 'string'(starting from 0 remember) and the third paramater indicates that it should be   repeated 5 times. 
+  
+  Simple. Good luck.
+
---
+++ problems/Codewars/5ace2d9f307eb29430000092/solve.js
@@ -0,0 +1,78 @@
+//	Code available at https://codewars.com/kata/reviews/5acff8cc1ebf3241e4000e8e/groups/5ad036afddb6e3c53700088b in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<> :"{}|=-aBcDeF H
+  LmNoPq sTuVwXyZ0123 56789`! # %^&*()_+[];'./,\<>?:"{}|= AbCd
+f 
+
+
+#$%^ *( _
+	];'./,\<>?:"{}| -aBcDeF HiJkLmNoPq s
+	
+	XyZ0123456789`!@#$%^&*()_+[];'./,\<>?: {} = AbCdEfGhI KlMn pQrStUvWxYz0123456789`!@#$%^&*()_+[]; ./,\ >?:"{} =- BcDeFgHiJkL
+	oPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\< ?: {}| -AbCdEf hI KlMnOp rStUvWxY 01234 6789 !@#$ ^&*()_ [] '. , <>?:"{}| -aBc eFg iJkLmNoPqR TuVwX Z0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIj
+	MnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\< ?:"{} =-aBcDe gH Jk mNoPqRsTu wX Z012 456789 !@#$% &* )_+ ];'./,\< ?:"{}|=-Ab dE Gh jKlMnOpQr tUvWxYz01234 6789 !@#$%^&*
+	_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoP RsTuVwXyZ0 23 5678 `!@#$% &* )_+[];'./ \< ?:"{}|= A CdEfG IjKl nOp r tUvWx z0123456 89`! #$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'.
+	\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456 89`!@#$%^ *()_+[ ;' /, <>?: {}|=-aB D Fg iJkLmNoP RsT VwXyZ0 23 567 9`!@#$% &* )_+ ];'. ,\<>?:"{} =-Ab dEf hIjKlMnOpQrStUv xYz0 23456789`!@#$%^&*()_+[];'./,\<>?:"{}|
+	
+	cDe
+	
+	JkLmNoPqRsTuVwXy 0123456 89`!@#$%^&*
+	
+	[];'./,\ >?:"{}|=-AbCdEfGhIj lMnOpQrStUvWx z
+	234 6789`! # %^&*()_+[]; ./,
+	>?: {}|=-a c eFgHiJkLmNoPqRsTuVwXyZ0123456   
+	@#$%^& ()_+[];'./,\<>?: {
+	= 
+	
+	EfGhIjKl nOpQrStUvWxYz01 3456 8
+	!@# %^&*( _ [];
+	/,\ >?:"{}|= a cDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\
+	?:" }|=-Ab d fGhIjKlMnOpQrStUvWxYz012 4 6789 ! #$%^&
+	)_ [];' / \<>? "{}|=- BcDeFgHi kLmNoPqR TuVwXy 01 3456789 !@#$ ^&*()_+[];
+	/,\< ?:" }|=- b d fGhIjKl nOp r t
+	WxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=- B DeFgHiJkLmNoPqRsTu wX
+	0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCd f hIjKlMnOpQrStUvWxYz0
+	3456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFg i kLmNoPqRsTuVwXyZ0123
+	6789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIj l nOpQrStUvWxYz0123456
+	9`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLm o qRsTuVwXyZ0123456789
+	@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOp r tUvWxYz0123456789`!@
+	%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRs u wXyZ0123456789`!@#$%
+	*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUv x z0123456789`!@#$%^& ( _ []
+	./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ01234 6 89`!@#$%^&*()_+[];'.
+	\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01234567 9 !@#$%^&*()_+[];'./,\
+	?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789` @ $%^&*()_+[];'./,\<>?
+	{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@# % &*()_+[];'./,\<>?:"{
+	=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^ * )_+[];'./,\<>?:"{}|=
+	bCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*( _ [];'./,\<>?:"{}|=-aB
+	eFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+ ] './,\<>?:"{}|=-AbCd f h jK
+	nOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\< ? "{}|=-aBcDeFgHiJkLmN
+	qRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?: { |=-AbCdEfGhIjKlMnOpQ
+	tUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{} = aBcDeFgHiJkLmNoPqRsT
+	wXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=- b dEfGhIjKlMnOpQrStUvW
+	z0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBc e gHiJkLmNoPqRsTuVwXyZ
+	23456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEf h jKlMnOpQrStUvWxYz012
+	56789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHi k mNoPqRsTuVwXyZ01234 6 8 `!
+	$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQr t vWxYz0123456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTu w yZ0123456789`!@#$%^&
+	)_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWx z 123456789`!@#$%^&*()
+	[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0 2 456789`!@#$%^&*()_+[
+	'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123 5 789`!@#$%^&*()_+[];'
+	,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456 8 `!@#$%^&*()_+[];'./,
+	
+	:
+	}|=-Ab dEfGhIjKlMnOpQr
+	U
+	
+	
+	123 5678 ` @ $% & ( _+[] './, <
+	:"{ |=-aBcD F HiJkLmNoPqRsTuVwXy
+	123 56789`! # %^&*()_+[];'./,\<>?:"{} = AbCd fGhIjK MnOpQ StUvWxY 0
+	345 789`!@# % &*()_+[];'./,\<>?:"{}|= a cDeF Hi kLmN PqRsT VwXyZ01 3
+	
+	89`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxY 0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcD
+	g
+	
+	
+	NoP
+	
+TuVwX
