f9fbdedc341a51e24b53d1608473cc0728d0f4b2
Rascal_Two <therealrascaltwo@gmail.com>
2022-08-15 17:41:16 +0000

    Add Codewars 52b305bec65ea40fe90007a7 JavaScript solution

---
+++ problems/Codewars/52b305bec65ea40fe90007a7/README.md
@@ -0,0 +1,30 @@
+[_metadata_:generated]: - "true"
+
+# Arrh, grabscrab!
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`52b305bec65ea40fe90007a7`](https://www.codewars.com/kata/52b305bec65ea40fe90007a7) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Pirates have notorious difficulty with enunciating. They tend to blur all the letters together and scream at people.
+
+At long last, we need a way to unscramble what these pirates are saying.
+
+Write a function that will accept a jumble of letters as well as a dictionary, and output a list of words that the pirate might have meant.
+
+For example:
+```
+grabscrab( "ortsp", ["sport", "parrot", "ports", "matey"] )
+```
+
+Should return `["sport", "ports"]`.
+
+Return matches in the same order as in the dictionary. Return an empty array if there are no matches.
+
+Good luck!
+
+
---
+++ problems/Codewars/52b305bec65ea40fe90007a7/solve.js
@@ -0,0 +1,41 @@
+//	Code available at https://codewars.com/kata/reviews/55df395660a242f2b1000044/groups/62fa853ea1a3f600015c5826 in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{ |=-aBcDeFgH J
+  NoPqRsT V XyZ0123456789`!@#$%^&*()_+[];
+  ,\<>?: {}|=-AbCdEfGhIjKlMnOpQ St vWxYz0123456789`!@#$%^&*( _+[ ;'./,\<>?
+"
+
+
+#$%^ *( _
+	];'./ \<>?:"{} = aBcDeFgHiJkLmNoP
+	sTuVw yZ0123456 89`!@#$ ^&*()_+[ ; ./,\<>?:"{}|=-AbCd
+	GhIjK MnOpQ S UvW Yz 123456789`!@#$%^&*( _+ ]; . ,\<> : {}|= a cDeFgH J Lm oPqRsT Vw yZ 1234
+	789`! #$% & ()_+[];'. ,\<>?:"{}|= Ab d
+	  IjKlMnOp rStUvWxYz012345678 `!@#$%^&*() +
+	    ,\< ?:"{ | -aBcDeFgHiJkLmN
+	    uVwXyZ 123456789`!@#$%^&*( _+ ];'./,\<>?:"{}|=- bCdEfGhI
+	  n
+	  
+	  WxYz01 3456789`!@#$%^&*() +[];'./,\<>?
+	{}
+	-aBcD FgHiJkL N PqRsT VwXy 01 3456789`!@#$%^&*()_+[ ;'./,\<>?:"{ |= AbCdEfGhIjKlMnOpQrStUvWxYz0123 56789`!@#$%^&*(
+	
+	];'./,\<>?:"{}|=-aBcD FgHiJkLmNo q
+	  VwXyZ01234 6789 !@#$%^ *()_ [];'./, <>?:"{}|=- b
+	    IjKlMnOpQrStUvWxYz0123456789`!@#$%^ *()_+[];'./ \<>?:"{}|= aBcDeFg iJkL NoPqR TuVwXyZ012
+	    9`!@#$%^&*()_+[];'./,\<>?:"{}|=-A CdEfGhI KlMnOpQrStU WxY 0123456 89` @#$% &*()_ [];'./,\<>?
+	    -aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`! #$%^&*()_+[]; ./,\<>?: {}|=-AbCd fGhIjKlMnOpQrS UvWxYz0 2345 789`! #$%^&*()_+[];'
+	    ?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ 123456789` @#$%^&* )_+[];'. ,\<>?:"{} =-AbCdEf hIjKlMnO QrStUvW Yz01 34567 9`!@#$ ^&* )_+[];'./
+	    "{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ012 456789`!@ $%^&*()_+ ];'./,\< ?:"{}|=-Ab dEfGhIjKl nOpQrStUv xYz0123 5678 `!@#$ ^&*()_+ ];' /,\<>?:"{}
+	    DeFgHiJkLmNoPqRsTuVwXyZ0123456789` @#$%^&*()_+[];'./,\<>?: {}| -AbCdEf hIj lMnO QrStU WxYz0123456
+	  !@#
+	  *()_+[];'. ,\<> :"{}|= aBcD FgHiJkL NoPqRsTuVw y
+	    567 9`!@ $ ^ *( _ [ ;'./ \<>? "
+	      dEf hIjKlMn p rStUvWxYz0 23456
+	          ()_+[];'./ \ >?:"{}|=-aBcDeFgHiJ LmNoPqRsTuVw yZ 123456789 !@ $% & ( _+[];'./,\<>?:"{}|=-AbCdEfGhIj l nOpQrStUvWxYz012345 789`!@#$%^&*() + ];' /,\<>?:"{}|=-a c eFgH
+	      
+	      yZ0123456789`!@#$%^&*()_+[];'./,\<> :"{}|=-AbCdE GhIjKlMnOpQr tUvWxYz012345
+	    @
+	  &*(
+	+[]
+'./,\
