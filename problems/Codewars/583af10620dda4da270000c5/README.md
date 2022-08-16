11e2a516813162cc08cd2e5034e74600f099f893
Rascal_Two <therealrascaltwo@gmail.com>
2022-08-16 19:59:37 +0000

    Add Codewars 583af10620dda4da270000c5 JavaScript solution

---
+++ problems/Codewars/583af10620dda4da270000c5/README.md
@@ -0,0 +1,27 @@
+[_metadata_:generated]: - "true"
+
+# Merge two arrays
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`583af10620dda4da270000c5`](https://www.codewars.com/kata/583af10620dda4da270000c5) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Write a function that combines two arrays by alternatingly taking elements from each array in turn.
+    
+Examples:
+```
+[a, b, c, d, e], [1, 2, 3, 4, 5] becomes  [a, 1, b, 2, c, 3, d, 4, e, 5]
+
+[1, 2, 3], [a, b, c, d, e, f] becomes [1, a, 2, b, 3, c, d, e, f]
+```
+Points:
+
+1. The arrays may be of different lengths, with at least one character/digit.
+2. One array will be of string characters (in lower case, a-z), a second of integers (all positive starting at 1).
+
+
+*Inspired by  https://adriann.github.io/programming_problems.html*
---
+++ problems/Codewars/583af10620dda4da270000c5/solve.js
@@ -0,0 +1,72 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{}|=- B
+  FgHiJ LmNoPqR T VwX
+  123 56789`! # %^&*()_+[];'./ \< ?:"{}|=-AbCdEfG IjKlMnOpQrStUv xYz012 4 6789`!@#$%^&*()_+ ]; ./,\<>?:"{}| -aB DeFgHi kLm oP RsTuVwX Z0123456789`!@#$%^&*()_+
+  './,\< ?:"{}|=-
+b
+
+
+#$%^ *( _
+	 ;'./,\<>?:"{}|=- BcDeFgH JkLmNoPq sT V
+	        6789`!@#$% &* )_+[]; ./, <>?:"{}| -AbCdEfG Ij l
+	
+	            23456789`!@#$%^&*()_+[];'./,\<>?:" }| -a cD Fg iJ Lm oPq sTuVw yZ01 3456 89`! #$%^&* )_+ ];'. ,\ >?:" }| -AbC Ef hIjK Mn pQrS Uv xY 0123
+	            *()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLm oPqR TuVw yZ01 34567 9`! #$ ^& () +[]; ./,\< ?: {}|= Ab dEfG Ij lMnO Qr tUvW Yz01
+	            ^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHi kL No qRs uVw yZ0 23456 89`! #$%^ *()_ [];' /,\<>? "{} =-Ab dE GhIj lM OpQr tUv xYz0 234 6789 !@ $%^&*(
+	            :"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ012 4567 9`!@ $%^& ()_+ ];'. ,\<> :"{} =-AbC EfG Ij lM OpQ StU WxYz 12345 78 `!@# %^ *()_ [] './, <>? "{}| -aB DeFg iJ LmNo qRsT VwXyZ0
+	
+	        #$%
+	    +[]
+	
+	    ?:"{}|=-AbCdEfGh jKlMnOpQ StUvWxYz 12 4
+	
+	        &* )_+[];'.
+	        |=-aBcDe gHiJkLmNoPqRsTuVwXyZ01 34 6
+	            _+[ ;'./, < ?:"
+	            jKl nOpQrS U WxYz0123456789`!@# %^&*()_+[]
+	            =-a cDeF H J Lm o q sTuVwXy 0123 5
+	                ]; ./ \ >?:"{}|=-
+	                    WxYz0123456789`!@
+	                <> :" } =-aBcDeFg
+	                    3456789`!@#$%^&*(
+	            :
+	            IjKlMn pQrStU
+	        5
+	
+	        *()_+[]; ./,\<>?:"{}|=-a cDe g
+	            VwXyZ0 23456789`!@#$%^&*()_+[]; . ,\<>? " }|= A CdEf
+	        Q
+	
+	        3456789` @#$%^&*()_+[];'./,\<>? "
+	            JkL NoP R TuVwX Z012 4567 9`!@ $%^& ()_+ ];'. ,\<> :"{} =-Ab dEfG IjKl nOpQ StUv xYz0 2345 789` @#$% &*() +[]; ./,\ >?:" }|=- BcDe gHiJ LmNoP
+	            456 89`!@ $ ^&*
+	            ?:" }|=- b d fG I K MnOpQrStUvWxY 012 4567 9
+	                /,\<>?:"{}|=-aBcDeFgHiJkLmNo qRsTu
+	            9
+	            ];'./, <>?:"{
+	        G
+	
+	
+	        vWxYz012 456789`!@#$%^&*()_+ ]
+	            =-a cDeFg i kLm
+	            123 5678 ` @ $% & ( _+[];'./,\<>? "{} =-Ab d
+	                WxYz0123456789`!@#$%^&*() +[];
+	            -
+	            oPqRsT VwXyZ0
+	        !
+	
+	
+	        './ \<>? " } =- b d fGh jKlM O
+	            345 7 9 !@#$%^&*()_+[];'./,\
+	            eFg i k mNoPqRsTuVwXyZ012345678
+	            []; ./,\<> : {}|=-AbCdEfGhIjKlMnOpQr tUvWxYz01234
+	            ()_ [];'./,\ > :"{}|=-aBcDeFgHiJkLmNoPqRsTuVwX Z0123456789`
+	
+	            ./,\<>?:"{}|=-AbCdEfGhIjKl nOpQrStUvW
+	
+	        9
+	
+	
+	    )_+
+];'./
