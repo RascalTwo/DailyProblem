96a9a9f8033e8d1a7cd071d5ca3454d509806b5d
Rascal_Two <therealrascaltwo@gmail.com>
2022-08-17 22:59:36 +0000

    Add Codewars 57efcb78e77282f4790003d8 JavaScript solution

---
+++ problems/Codewars/57efcb78e77282f4790003d8/README.md
@@ -0,0 +1,16 @@
+[_metadata_:generated]: - "true"
+
+# How many times should I go?
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57efcb78e77282f4790003d8`](https://www.codewars.com/kata/57efcb78e77282f4790003d8) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Lot of museum allow you to be a member, for a certain amount `amount_by_year` you can have unlimitted acces to the museum. 
+
+In this kata you should complete a function in order to know after how many visit it will be better to take an annual pass. The function take 2 arguments `annual_price` and `individual_price`. 
+
---
+++ problems/Codewars/57efcb78e77282f4790003d8/solve.js
@@ -0,0 +1,39 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{}|=-aBc eFgHiJkLmNoPqRsT V
+  Z01234 6789`!@#$%^&*()_+[];' / \<>?:"{}|=-AbCdEf
+h
+
+
+#$%^ *( _
+	];'./,\<>?:"{}| -aBcDe gH JkL
+	  qRsTuVwXyZ0123456789`!@#$%^&*()_+[];'. ,\
+	  "{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz012345 78
+	  #$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJ Lm
+	Pq
+	
+	VwXyZ012 456789`!@#$%^&*()_+[];'./,\<> :"{}|=-AbCdEfGhIj l
+	  QrStUv xYz0123456789`!@#$%^&*( _ [];'./,\<>?:"{}|=-aBcDeFgHiJkL
+	o
+	
+	TuVwXyZ012345678 `!@#$% &* )_+ 
+	  /,\<> :"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789
+	
+	  ^&*()_+[ ;'./,\<>?:"{}| -
+	   FgH JkLmNoPqRsTu w yZ0123456789`!@#
+	   ()_ [];'./,      { |=-AbCdEfGhIjKlMnOpQrStUvWxYz012345678
+	   $%^ *()_+[];'.   < ?:"{}|=-aBcDe
+	   kLm oPqRsTuVwXyZ0123 5 789`!@#$%^&* ) +[];'./,\<
+	   }|= AbCdEfGhIjKlMnOp r tUvWxYz0123456789`!@#$%^&*()_+[] ' /,\ > :"{}|=-aBcDeFgHi k mNoPqRsTuVwXyZ012 4 6789`!@# % &*()_+[];'./,\<> : {}|=-AbCdEfGhIjKl n pQrStUvW
+	   234567 9`!@#$%^&*()_+ ];'./,\<>?:"{}|=-aBcDeFgHiJk
+	  P
+	  
+	  yZ0 2345 789`!@#$%^&*()
+	    ./, <>? "{ | -AbCdEfGhIjKlMn
+	    UvW Yz0  3 56789`!@#$%^&*()_+[];
+	    >?:"{}|=-aB DeF HiJkLmNoPqRs uVwX Z01 3456789`!@#$%^&* )_+[];'. ,\<
+	      -AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$% &*()_+ ];'. ,\< ?:"{}| -aBcDe gHiJk
+	    Rs
+	  X
+	01
+34567
