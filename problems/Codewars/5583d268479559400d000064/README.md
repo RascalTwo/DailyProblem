699eb35c0e2384f58f184343f586b752b5b35587
Rascal_Two <therealrascaltwo@gmail.com>
2022-08-18 18:58:35 +0000

    Add Codewars 5583d268479559400d000064 JavaScript solution

---
+++ problems/Codewars/5583d268479559400d000064/README.md
@@ -0,0 +1,23 @@
+[_metadata_:generated]: - "true"
+
+# Binary to Text (ASCII) Conversion
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5583d268479559400d000064`](https://www.codewars.com/kata/5583d268479559400d000064) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Write a function that takes in a binary string and returns the equivalent decoded text (the text is ASCII encoded).
+
+Each 8 bits on the binary string represent 1 character on the ASCII table.
+
+The input string will always be a valid binary string.
+
+Characters can be in the range from "00000000" to "11111111" (inclusive)
+
+Note: In the case of an empty binary string your function should return an empty string.
+
+
---
+++ problems/Codewars/5583d268479559400d000064/solve.js
@@ -0,0 +1,30 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{}|=- B
+  FgHiJk mNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>? "{ |=-AbCdEfGhIjKlMnOpQrStUvWxYz01234 6789`!@#$%^&*(
+_
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=- Bc eF H
+	  mNoPqRsTuV Xy 01 3
+	
+	
+	`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEf hIj lMnOp rStUvW Yz012 456789`!
+	$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[ ;'./,\<> :"{}| -AbCdE GhIjK MnOpQrS
+	vWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVw yZ01234 6789` @#$%^& ()_+[]; ./,\<>?:"{}|
+	AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxY 0123456 89`!@# %^&*()_+ ];' /,\<>?: {}|=- BcDeFg iJkLmNo qRsTuVwXyZ0
+	3456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[ ;'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[]; ./,\< ?:"{}| -aBcDeF HiJkLmNoPqR
+	
+	wXyZ01 345678 `!@#$% &*()_+
+	;'. ,\<>?:"{}|=- b dEfGhIjKlMnOpQrStUvWxYz0123
+	678 `!@#$%^&*()_+[];'. , <>?
+	{}| -aBcD F H JkLmNoPqRsTuVwXyZ012 4567 9
+	  $%^ *() + ];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxY
+	  3456789`!@#$%^&*() +[ ;'./,\<>?:"{}|=-aBcDeFgHiJkL N PqRs
+	V
+	yZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIj lMnOpQrStUvWx z0123 56789` @#$%^& ()_+[];'
+	  <>?
+	{}|
+	
+BcDeF
