566d7bb7d540d9ded84d9884132aeac823036925
Rascal_Two <therealrascaltwo@gmail.com>
2022-08-18 22:58:31 +0000

    Add Codewars 5831c204a31721e2ae000294 JavaScript solution

---
+++ problems/Codewars/5831c204a31721e2ae000294/README.md
@@ -0,0 +1,21 @@
+[_metadata_:generated]: - "true"
+
+# Changing letters
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5831c204a31721e2ae000294`](https://www.codewars.com/kata/5831c204a31721e2ae000294) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+When provided with a String, capitalize all vowels
+
+For example:
+
+Input : "Hello World!"
+
+Output : "HEllO WOrld!"
+
+Note: Y is not a vowel in this kata.
---
+++ problems/Codewars/5831c204a31721e2ae000294/solve.js
@@ -0,0 +1,50 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+ ];'./,\< ?
+  }|=-aB DeFgHiJkLmNoPqRsTuVwXyZ012 4567 9` @#$%^&*()_+[];'./,\<
+?
+
+
+#$%^ *( _
+	];'./ \<>?:"{}|=- B DeFgHiJkLmNoPqRsTuVwXyZ0123456789`!
+	
+	^&*()_+[ ;'./,\ >?:"{}| -AbCdEfGh j
+		nOpQr tUvWxY 0 23456789`!@#
+		&*()_+[];'./,\<>?:" }|=-aBcDe gHiJ LmNoPqRsTuVwXyZ012345678
+	!
+	
+	^&*()_+[];'./,\ >?:"{}| -A Cd f
+		jKlMnOpQrS UvWxYz0 23 56 8
+			#$%^&*()_+ ];'.
+			>?:"{}|=   cDeFg i   mNoPqR
+			wXyZ0123456789`!@#$%^ *()_+[];'./,\<>
+			}|=-AbCdEfGhIjKl nOpQrStUvW
+			123456789`!@#$%^&* )_+[];'./,\<
+			{}|=-aBcDeFgHiJk mNoPqRsTuV
+			0123456789`!@#$% &*()_+[];'
+			<>?:"{}|=-AbCdEfGhIjK MnOpQrStUvWxYz0
+			56789`!@#$%^&*()_+[]; ./,\<>?:"{}|=-a
+			FgHiJkLmNoPqRsTuVwXyZ 123456789`!@#$%
+			)_+[];'./,\<>?:"{}|=- bCdEfGhIjKlMnOp
+		tUv
+	
+		12345 789 ! # %^ *()_+[];'./,\<>? " }|=
+		cDeFg iJkLm o qR Tu wXyZ0123456789`!@#$
+		*()_+ ];'./,\ > : {} =-AbCdE GhIjKlMn p rS U WxYz012 4 6789`!@# % &*()_+[];'
+		\<>?: {}|= a cDeFgHiJkLmNo qRsT V XyZ0123456789`!@#$%^&*()
+	
+		'./,\<>?:" }|=-AbC Ef hI K
+			pQr tUvW Y 0 23 5 7 9`!@ $%^& (
+				;'. ,\< ? "{}|=-a c eF
+				LmN PqRsT V XyZ 12345678 ` @#$
+	
+				+[];' /,\<>?: {
+					CdEfG IjKl n pQrStUvWxYz0123456 8 `!@#$%^
+					[];'./,\ >? "{}|=-aBcDeFgHiJkLmN P RsTuVwXyZ012345678 ` @#$%^
+					[];'. ,\ >?:"{
+				b
+				hIjKlMnOpQrSt vWxYz01234
+			9
+		#$%
+	*()
++[];'
