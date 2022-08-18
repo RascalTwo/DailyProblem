8c0ccaa299e61304ab1c4ede40cb9b4ccb090286
Rascal_Two <therealrascaltwo@gmail.com>
2022-08-18 23:08:42 +0000

    Add Codewars 57ad85bb7cb1f3ae7c000039 JavaScript solution

---
+++ problems/Codewars/57ad85bb7cb1f3ae7c000039/README.md
@@ -0,0 +1,38 @@
+[_metadata_:generated]: - "true"
+
+# Numbers with this digit inside
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57ad85bb7cb1f3ae7c000039`](https://www.codewars.com/kata/57ad85bb7cb1f3ae7c000039) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+You have to search all numbers from inclusive 1 to inclusive a given number x, that have the given digit d in it.<br>
+The value of d will always be 0 - 9.<br>
+The value of x will always be greater than 0.
+
+You have to return as an array<br>
+ - the count of these numbers,<br>
+ - their sum <br>
+ - and their product.<br>
+<br>
+For example:<br>
+
+```
+x = 11
+d = 1
+->
+Numbers: 1, 10, 11
+Return: [3, 22, 110]
+```
+<br>
+If there are no numbers, which include the digit, return [0,0,0]. 
+<br>
+<br>
+Have fun coding it and please don't forget to vote and rank this kata! :-) <br>
+<br>
+I have created other katas. Have a look if you like coding and challenges.<br>
+
---
+++ problems/Codewars/57ad85bb7cb1f3ae7c000039/solve.js
@@ -0,0 +1,52 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{}|=-aBc eF H
+  LmN PqRsT V Xy 012 4 67 9`!@#$% & ()
+  ];' /,\< ? " }| - bC Ef hIjKl
+    rS UvWxYz0123456789`!@#$%^&*() +[];'./,
+    "{}|=-aB
+    HiJ Lm oP
+    Vw yZ0123456 89`!@#$ ^& ()
+    './, <>?:"{} = Ab
+  f
+  jKlMnO QrStUvW Yz01 3456789`!
+#
+
+
+#$%^ *( _
+	];'./ \<>?:"{}|=-aBcDeFgHiJkLmNoPqR
+	
+	wXyZ0123456789`!@#$% &*()_+[];'.
+	  >?:"{}|=- bCdEfGh jKlMnOpQrSt
+	    0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcD F Hi kL N PqR
+	    yZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-Ab d fG Ij l nOp
+	    WxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=- B De gH J LmN
+	    uVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|= A Cd fGh jKl nOp
+	    WxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-a c eF HiJ LmN PqR
+	    yZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbC E Gh jKlM OpQrStUvWxYz0 234
+	  9`!
+	  ^&*()_+[]; ./,\<>? "{}|=-aBcDe
+	    LmN PqRsTuVwXyZ0123456789`!@ $ ^&*()_+[];' /, <
+	      -Ab dEfGhIj l nOp rStUvWxY
+	      789`!@# %^&*()_+[];'. ,
+	        -aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`! #
+	          './,\<>?:"{}|=-A
+	        l
+	      U
+	      3456789`!@#$%^&*( _+ ]; .
+	        |=-aBc eFg iJ LmN
+	      w
+	      56789` @#$%^&*()_+[];'. ,\<>?:"{}|=-AbCdEfGh jK M O QrS UvWxYz0123456789`!@# %^ * ) +[];
+	    >?
+	    
+	    FgHiJkL N P Rs u w yZ0 2345
+	      $        
+	        ?:" }|=-A C EfGhIjKlMnOpQrStUvWxYz01 3 56 8 `!@
+	        []; ./,\< ? "{}|=-aBcDeFgHiJkLmNoPqR T V X Z01
+	        @#$%^ *() +  ;'./,\<>?:"{}|=-AbCdEfGhIjKlMnO QrStUv
+	        6789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJk mNoPqRs uVwX Z012345 789` @#$ ^&* ) +[];' / \< ?:" } =-AbCdE
+	      n
+	      
+	  012
+	567
+9`!@#
