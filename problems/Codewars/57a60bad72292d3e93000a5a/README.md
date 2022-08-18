040c7c19c3e31a038a84c22cb191cf3d408bac25
Rascal_Two <therealrascaltwo@gmail.com>
2022-08-18 19:01:40 +0000

    Add Codewars 57a60bad72292d3e93000a5a JavaScript solution

---
+++ problems/Codewars/57a60bad72292d3e93000a5a/README.md
@@ -0,0 +1,29 @@
+[_metadata_:generated]: - "true"
+
+# Make acronym
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57a60bad72292d3e93000a5a`](https://www.codewars.com/kata/57a60bad72292d3e93000a5a) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Write function which takes a string and make an acronym of it.
+
+Rule of making acronym in this kata:
+
+1. split string to words by space char
+2. take every first letter from word in given string
+3. uppercase it
+4. join them toghether
+
+
+Eg:
+
+```
+Code wars -> C, w -> C W -> CW
+```
+
+**Note: There will be at least two words in the given string!**
---
+++ problems/Codewars/57a60bad72292d3e93000a5a/solve.js
@@ -0,0 +1,42 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?
+  }|=-aB DeFgHiJkLmN PqRsTuVwXyZ 12 456789`!@#$%^&*()_+[];'./,\<>?:"
+}
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz 123456789 !@ $% &
+	  +[];'./,\ >?:"{}| -a cD F
+	
+	    oPq sTuVw y 0
+	      9`!@#$ ^&*()_ [];'./
+	      }|=-AbC EfGhIjKl nOpQrS
+	      123456789` @#$%^&* )_+[];'./, <>?:"{}
+	      FgHiJk mNoPqRsTu wXyZ0123456789 !@#$%^&
+	      './,\<> :"{} =-AbCd fGhIjKlMnO QrStUvW
+	    34
+	    !@#$%^&*()_+[] './,\<>?:"{ |= aBcDeFgHiJkLmNoPqRs uVwXyZ012345678 `!@ $ ^&
+	  +[]
+	
+	  <>?:"{}|=- bCdEfGh jK Mn p
+	  
+	    012 456789 ! #$%^&*()_+[];'./,\<>?:"{}|=-a cDeFgH J LmNoPqRsTuVwXyZ0123456789 ! #$%^&* ) +[];'. , <>?:"{}|=-AbC
+	
+	    lMn pQr t v xY 0123456789`!@#$%^&*(
+	    './ \<>?:" } = aB DeFgHiJkLmNoPqRsT
+	
+	    234 6789`!@#$%^& ( _+[ ;' /,\<>?:"{}|=-AbCdEfGhIjK M Op rStUvWxYz01234 6789`!@#$%^
+	    []; ./,\<>?:"{}| - BcD Fg iJkLmNoPq s uVw y 0123456789`!@#$%^& ( _+[];'./,\<>?:"{}|=-AbCdEfGhIj l nO QrStUvWxYz01 3456789`!@#
+	
+	    +[];'./ \ > :" } = aBcD FgHiJ
+	      sTu wXy 0 23456789`!@#$ ^&*()_+[ ;' /,\ > :"{}|=-AbCdEf hIjKlMnOpQrS UvWxYz01 345
+	      $%^&*()_+[];'./,\<> :"{}|=-aBcDeFgH JkLm oPqRsTuV XyZ 123 5 789`!@#$%^
+	    [
+	  /,\
+	?:"
+}|=-A
