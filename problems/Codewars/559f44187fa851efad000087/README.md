034d52e0e0276bf1deeed5905da540bfa7a0b43e
Rascal_Two <therealrascaltwo@gmail.com>
2022-08-18 19:35:01 +0000

    Add Codewars 559f44187fa851efad000087 JavaScript solution

---
+++ problems/Codewars/559f44187fa851efad000087/README.md
@@ -0,0 +1,18 @@
+[_metadata_:generated]: - "true"
+
+# SevenAte9
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`559f44187fa851efad000087`](https://www.codewars.com/kata/559f44187fa851efad000087) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Write a function that removes every lone `9` that is inbetween `7`s.
+
+```javascript
+"79712312" --> "7712312"
+"79797"    --> "777"
+```
---
+++ problems/Codewars/559f44187fa851efad000087/solve.js
@@ -0,0 +1,40 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<> :
+  |=-aBc eFgHiJkLmNoPqRsTuVwXyZ0 23456
+8
+
+
+#$%^ *( _
+	];'./ \ >?:"{} = a cDeFgHiJkLmNoPqR
+	
+	wXyZ0 2345 7 9`!@#$% &*()_+[]; ./,\<>?: {} =
+	  dEfGhIjKlMnOpQrS Uv xY 0
+	    789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aB DeFgHiJkL NoPqRsTuV
+	  012
+	5
+	
+	`!@#$%^&*()_+[] './,\<> :" }| -
+	  EfGhI KlMnO Q StUvWxYz 123456 89`!@#$%^&* )_+[];'. ,\<>?:"{}|=-aBcDeFgHiJ LmNoPqRsTuVwXyZ012345 789`!@#$% &*()_+[] './,\< ?:"{}| -AbCdEfG IjKlMnOp rStUvWxY 01234567 9`!@#$%^ *()_+[]; ./,\<>?:"{ |=-aBcDeF HiJkL N PqR TuV X Z012 456789`!@#$% &*()_+[];'. ,\<>?: {}|=-A CdEfGh jKlMnO QrSt vWxYz
+	  456 89`!@# %^&*()_ [];'./,\< ?: {}|=-a c
+	    JkLmNoPqRsT VwXyZ01234
+	  9
+	@#$
+	
+	()_+[];'./,\<>?: {}|=-Ab dE Gh j
+	  OpQrS UvWxYz012 4 678 `! #$ ^&*()_+[];'./,\<>?:"{}|= a cD F H J Lm o qRs
+	
+	  Z01 3456 8 ` @# % & ()_ [];' /
+	    "{}|= AbCdE G IjKlMnOpQrSt vWxYz01 3456789`!@#$ ^& () +[ ;' /
+	      }|=-a c e gHiJkLmNoPqRsT
+	      23 56 8 `!@#$ ^&*()_ []; . ,\<>?:"{}|=-AbCdEfGhIjK MnO Q StUv
+	      45 78 ` @#$%^ *()_+[ ;'. , <>?:"{}|=-aBcDeFgHiJkLmN PqRs
+	      123456 89`!@#$%^&*( _+[];'. ,\<>?:"{}|=- bC Ef hI Kl nOpQrStUvWxY 0123456789`!@
+	    ()_
+	
+	    \<>?: {}|=- B DeFgHiJkLmNoPqR
+	    yZ012 456789`! # %^&*()_+[];'./,\<>?:"{}|=-AbCd fGhI
+	    pQrStUvWxYz 123456789`
+	  %
+	*()
++[];'
