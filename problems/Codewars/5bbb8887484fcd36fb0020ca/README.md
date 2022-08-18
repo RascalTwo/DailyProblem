42aaa3c4a29d94ba104ce61f4941746e676f788a
Rascal_Two <therealrascaltwo@gmail.com>
2022-08-18 19:10:50 +0000

    Add Codewars 5bbb8887484fcd36fb0020ca JavaScript solution

---
+++ problems/Codewars/5bbb8887484fcd36fb0020ca/README.md
@@ -0,0 +1,33 @@
+[_metadata_:generated]: - "true"
+
+# Plus - minus - plus - plus - ... - Count
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5bbb8887484fcd36fb0020ca`](https://www.codewars.com/kata/5bbb8887484fcd36fb0020ca) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Count how often sign changes in array.
+
+### result
+number from `0` to ... . Empty array returns `0`
+
+### example
+```javascript
+const arr = [1, -3, -4, 0, 5];
+
+/*
+| elem | count |
+|------|-------|
+|  1   |  0    |
+| -3   |  1    |
+| -4   |  1    |
+|  0   |  2    |
+|  5   |  2    |
+*/
+
+catchSignChange(arr) == 2;
+```
---
+++ problems/Codewars/5bbb8887484fcd36fb0020ca/solve.js
@@ -0,0 +1,50 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$ ^&*()_+[];'./,\ > :"{ |= a
+  eFg iJkLmNo q sT
+  
+  012 4567 9 !@#$%^ * )_
+  ;'. ,\<>?: {}| -A CdEfG
+    MnOpQ StUvW Y 012 4 67
+    #$ ^&*()_ []; ./,\< ?
+      aBcD F HiJkLm
+      uVwXyZ0123
+    9
+  #
+  
+  )_+[]; ./,\<>?:
+{}
+
+
+#$%^ *( _
+	];'./ \<> : {}|=- BcDe gH JkLmNoPqRsTuVwXyZ0123456 8 `!@# % &*() + ];'./
+	<>?:" }|= A CdE Gh jKlMnOpQrStUvW Yz01 34567 9 !@#$% &*( _+[] '. ,\ >? " }| -aBc eF Hi k m o qR TuVw yZ0123 56 89`! #$%^&*()_+
+	
+	./,\<>?:"{}|=-A CdEfGhI KlMnOpQrStU
+	xYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBc eFg
+	JkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./ \<>
+	"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];' /,\
+	?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[] './
+	<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@# %^&
+	)_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTu wXy
+	123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMn pQr
+	UvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLm oPq
+	TuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCd fGh
+	KlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|= aBc
+	FgH
+	
+	mNoPqRsTuVwXyZ0 234567 9`!@#$ ^&*()_+[];'
+	,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz 123
+	6789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcD FgH
+	kLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[] './
+	<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01 345 789`! #$%
+	*()
+	
+	;'./,\<>?:"{}|=- BcDeFgH JkLmNoPqRsT
+	  yZ0 2 456 89`!@# %^&* ) +
+	    ,\<>? "{} = AbCdEfGhIjKlMn pQrStUvWxYz01234567 9`!@#$%^&*() +[] './,\< ?:"{}|=-
+	    gHi kLmNoPqRsTuVw
+	    3456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlM OpQrS
+	  x
+	012
+45678
