987487b68b66bc080b985dd122e899b55ea07092
Rascal_Two <therealrascaltwo@gmail.com>
2022-08-18 19:52:10 +0000

    Add Codewars 5738f5ea9545204cec000155 JavaScript solution

---
+++ problems/Codewars/5738f5ea9545204cec000155/README.md
@@ -0,0 +1,25 @@
+[_metadata_:generated]: - "true"
+
+# Help Bob count letters and digits.
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5738f5ea9545204cec000155`](https://www.codewars.com/kata/5738f5ea9545204cec000155) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Bob is a lazy man. 
+
+He needs you to create a method that can determine how many ```letters``` (both uppercase and lowercase **ASCII** letters) and ```digits``` are in a given string.
+
+
+
+Example:
+
+"hel2!lo" --> 6
+
+"wicked .. !" --> 6
+
+"!?..A" --> 1
---
+++ problems/Codewars/5738f5ea9545204cec000155/solve.js
@@ -0,0 +1,44 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{}|=-aBcDeF H
+  LmNoPq sTuVwXyZ0123456789`!@#$%^& () +[];'./,\< ? "{
+|
+
+
+#$%^ *( _
+	];'./,\<>?:"{}| -aBcDeF HiJkLmNoPq s
+	  XyZ0123456 89`!@ $%^&*()_+[] './,\<>?:" }
+	    dEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+ ];'
+	    ?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^ *()
+	    ./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz 123
+	    `!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHi kLm
+	    TuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEf hIjK
+	    rStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|= aBcD
+	    kLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-Ab dEfG
+	  lMn
+	QrS
+	
+	xYz0123456789`!@ $%^&*() +[ ;' /
+	  ?:"{} =-aBcDe g iJk mN Pq sTuVwXyZ01234 6 89 ! # % &* ) + ] '.
+	  
+	  {}|=- bCdEfGhIjKlMnOpQrStUv x z0 23 5
+	    @#$ ^&* ) +[]
+	    <>? "{}|=- B DeFgHiJkLm oPqR
+	    yZ0 2345 7 9 !@ $ ^ *()_+[] './, <
+	      -Ab dE GhIjKlMnOpQrStUvWxYz0123456789` @#$%^&
+	    ]
+	    <>?:"{ |=-a
+	  F
+	  
+	  oPqRs uVwXyZ01 3 56789 !@ $%^&*()_+[];'./,\<>?:"{}|=-AbCdEf
+	  
+	  OpQ StUv x z 12 4 67 9`!@ $%^& (
+	    './,\<>?:" }|=- BcDeFgH Jk mN P
+	      yZ012 456789` @ $%^&*()_+[];'./,\<>?:"{}
+	      fGhIj lMnOpQrS U WxYz0123456789`!@#
+	      +[];'./,\<>?:"{}|=-a cDe gHiJk m oPqRsTuVwXyZ0
+	      9`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKl nOpQrStUvW
+	    345
+	  `
+	#$%
+&*()_
