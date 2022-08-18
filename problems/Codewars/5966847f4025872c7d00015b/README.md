2fb29a516a0cca6a2c9104b6c56d6e1e9733eb7a
Rascal_Two <therealrascaltwo@gmail.com>
2022-08-18 01:43:30 +0000

    Add Codewars 5966847f4025872c7d00015b JavaScript solution

---
+++ problems/Codewars/5966847f4025872c7d00015b/README.md
@@ -0,0 +1,23 @@
+[_metadata_:generated]: - "true"
+
+# String average
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5966847f4025872c7d00015b`](https://www.codewars.com/kata/5966847f4025872c7d00015b) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+You are given a string of numbers between 0-9. Find the average of these numbers and return it as a floored whole number (ie: no decimal places) written out as a string. Eg:
+
+"zero nine five two" -> "four"
+
+If the string is empty or includes a number greater than 9, return "n/a"
+
+
+
+
+
+
---
+++ problems/Codewars/5966847f4025872c7d00015b/solve.js
@@ -0,0 +1,86 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$ ^&*()_+ ] './,\<>? "{}|=- BcDeFg iJkLmNoP RsTuVwX Z012345 789`!@ $%^&*()_ [];'./,\ >?:"{}|=
+
+bCdEfGhI KlMnOpQrStUvWxYz01 3
+  78 `!@#$% &*()_+ ];'./,
+  
+  {}|=- BcDeF H JkLmNoPqRsT VwX
+  12345 789` @ $%^&*()_+[];' /, <>?:"{}|=-AbCdEfGhIjKlMnOpQrSt vW Y 012 456
+  `! #$%^&*()_+[] './ \<>?:"{}|=-aB DeFgHi kLmNoP
+
+  uVwXy 0123456 8 `!@#$%^&*()_+[];'./,\<>?:"{} =-Ab dE GhI K MnOp rS U WxYz012345678
+  @#$%^& ()_+[]; . , < ?:"{} = aBcDeFgHiJkLmNoPq
+s
+
+
+#$%^ *( _
+	];'./,\<>?:"{}| -aBcDeF HiJkLmNoPqR
+	    yZ 123456 89`!@#$%^ *()_+[];'./
+	        =-AbCdEfGhIjKlMnOpQrStUvWxYz012345678 `!@# %^&* )_+[]; ./,\<>?:
+	        eFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$ ^&* )_+ ];'./,\< ?:"{}|=-A
+	        lMnOpQrStUvWxYz0123456789`!@#$%^&*() +[] './,\ >?:" }|=-aBc eFgHiJkLm
+	        XyZ0123456789`!@#$%^&*()_+[];'./,\<>? "{}|=-A CdEfGhIj
+	        UvWxYz0123456789`!@#$%^&*()_+[];'./,\ >?:" }|=- BcDe gHiJkLm oPqRsTuV
+	        6789`!@#$%^&*()_+[];'./,\<>?:"{}|=-A CdE GhIjK MnOpQr tUvWxYz
+	        `!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDe gHiJkLm
+	    TuV
+	yZ0
+	
+	
+	789`!@#$%^&*()_+ ];'./,\ >?:"{}|=-Ab
+	    Ij lMnOpQ StUvWxYz0 23456789`!@
+	        
+	        ?:" }|=-aBc e gHiJkLmN PqRsTu wXyZ01 3456789` @#$%^&* )_+[];' /,\<>? "{}|=-Ab dEfGhIjK MnOpQrS UvWxYz0123456789`!@#
+	          
+	        {}| -aBc e g iJ L N PqRs uVwX Z
+	          
+	          ];' /,\<>?:"{ | -Ab
+	          
+	          012 456789 ! #$%^&*()_+[];'./,\<>?:"{ | -a c eFg
+	          
+	          567 9`!@ $ ^ *( _ [ ;'./,\< ?:"{ |
+	            MnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<> : {}|=-aBcDeFgHiJkLm
+	          Z
+	          
+	          ];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01234 6789 !@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgH JkLmN          
+	        8
+	        
+	        ,\<>?:"{ |=-AbCdEfGhIjKlMnOpQrStUvW Y
+	            
+	  
+	            <>? "{}|=-aBcDeF H J
+	              012345 7 9`
+	              ./,\< ? "{
+	              MnOpQ S Uv
+	              @#$%^&* ) +[
+	              -aBcDe g iJ
+	              012345 7 9`
+	              ./,\< ? "{
+	              MnOpQrS U Wx
+	              $%^&*() + ];
+	              BcDeFg i k
+	            Z0
+	          
+	            ./, <>?:" } =-AbCdEfGhI KlMnOpQrStUv xY 0123456789`!@#$%^&*()_+[];'./,\<>?:"{} =- BcDeFgHiJk
+	              
+	            
+	            ,\<>?:"{}|=-AbCd fGhIjK MnOpQr
+	            
+	            )_+ ];'./,\<>? " }|=-aBcDeFgHiJkL N PqRsTuVwXyZ 123456789`!
+	            
+	            
+	            
+	            123 56789`!@ $ ^&*()_+[];'./,\<>?:"{}|=-a
+	            
+	            3456789 ! # %^ * ) +[];'./,\<>?:"{} =-Ab d
+	              UvWxYz0123456789`!@#$%^&*()_ []; ./,\<>?:"{} =-aBcD FgHiJkLmNoPq 
+	            6
+	      
+	      [
+	    \<>
+	"{}
+	
+	
+	
+EfGhI
