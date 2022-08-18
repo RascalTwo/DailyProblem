e687ae2534e8cb1d300965596d47893ca6e1d201
Rascal_Two <therealrascaltwo@gmail.com>
2022-08-18 19:55:57 +0000

    Add Codewars 5959ec605595565f5c00002b JavaScript solution

---
+++ problems/Codewars/5959ec605595565f5c00002b/README.md
@@ -0,0 +1,17 @@
+[_metadata_:generated]: - "true"
+
+# Reverse the bits in an integer
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5959ec605595565f5c00002b`](https://www.codewars.com/kata/5959ec605595565f5c00002b) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Write a function that reverses the bits in an integer.
+
+For example, the number `417` is `110100001` in binary. Reversing the binary is `100001011` which is `267`.
+
+You can assume that the number is not negative.
---
+++ problems/Codewars/5959ec605595565f5c00002b/solve.js
@@ -0,0 +1,38 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./, <>? "
+  =-aBcD FgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[ ;'.
+,
+
+
+#$%^ *( _
+	];'./ \<>?:" } =-aBcDeFgHiJkLmNoPqRsTu
+	
+	Z0123456789`!@#$ ^&*()_+ ];'./,\<>? "
+	  -AbCdEfGhI KlMn pQr tUvW Yz01234567 9`!@#$%^&* )
+	    ./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTu wXyZ0
+	    789`!@#$%^&*()_+[];'./,\<>?:"{}|=-A CdEfG 
+	    OpQrStUvWxYz0123456789`!@#$%^&*() +[] 
+	    >?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0 234567 
+	    %^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIj lMnOpQ 
+	    Yz0123456789`!@#$%^&*()_+[];'./,\<>? "{} 
+	    eFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<> :"{}|=-AbCdEfGhIjKlMnOpQr   
+	  z01
+	456
+	
+	!@#$%^&*()_+[];' /,\<>?: {}|=-aBcDe g
+	  LmNoPqRsTu wXyZ 123 56789` @#$%^&*()_ [];'./,\<> :
+	    AbCdE GhIjKlMnOpQrS U WxYz
+	    
+	    @#$%^ *()_+[];'./,\<>? "  |=-aB DeFg iJ LmNoPqRsTuVwXyZ012345678 ` @#$% & ()_ [ ;' / \ >?:"
+	    bCdEf hIjKlMnO Q StU Wx z0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJ LmN
+	    
+	    012 4567 9 ! #$ ^ * )_+[];'./,\<>? "{}| -
+	      IjKlM OpQrStUvWxYz0 2 456789`!@#$%^&*()_+ ];'./,\<>?:"{}|=-aBcDeFgH
+	      qRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?: {}|=-AbCdEfGhIjKlMnOpQrSt
+	    0
+	   678
+	!@#
+	  
+	
+_+[];
