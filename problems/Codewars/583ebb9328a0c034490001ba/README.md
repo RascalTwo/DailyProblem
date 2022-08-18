bb0220a2a93bf61f46b60331acfe5ddf10c92472
Rascal_Two <therealrascaltwo@gmail.com>
2022-08-18 22:56:04 +0000

    Add Codewars 583ebb9328a0c034490001ba JavaScript solution

---
+++ problems/Codewars/583ebb9328a0c034490001ba/README.md
@@ -0,0 +1,15 @@
+[_metadata_:generated]: - "true"
+
+# Identical Elements
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`583ebb9328a0c034490001ba`](https://www.codewars.com/kata/583ebb9328a0c034490001ba) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Given two arrays of integers `m` and `n`, test if they contain *at least* one identical element. Return `true` if they do; `false` if not.
+
+Your code must handle any value within the range of a 32-bit integer, and must be capable of handling either array being empty (which is a `false` result, as there are no duplicated elements).
---
+++ problems/Codewars/583ebb9328a0c034490001ba/solve.js
@@ -0,0 +1,71 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{}| -a c
+  gHiJk mNoP R TuV XyZ0123
+  789`!@ $%^&*()_ [] './,\<>?:"{}|
+-
+     
+
+
+#$%^ *( _
+	];'./ \<>?:"{} = aBcDeFgHiJkLmNoP
+	
+	uVwXyZ01 3456789`!@#$%^&*()_+[];'. ,\ >
+	    =-A CdEf h jK Mn p
+	        01 3456789`!@#$%^&* )_ [] './,\< ?:"{}
+	    D
+	    kLmNoP RsTuVw
+	Z
+	
+	45678 `!@#$%^ * )_+[];'./,\<>?:"{}|=-AbCd
+	GhIjK MnOpQr t vWxYz0123456789`!@#$%^&*(
+	
+	];'./,\<>?:"{}| -aBcDeF HiJkLmNoPqR
+	  wXyZ01 345678 `!@#$% &*()_+[];'./ \<>?:"{}|=-
+	    GhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*( _+ ]; ./ \<> :"{ |= aB De gHiJ LmNoPq
+	    XyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{ |= AbC EfG Ij lMnO QrStUv
+	    23456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aB DeF HiJ LmNo qRsT VwX Z01 34567 9`!@#$
+	    _+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStU WxY 0123 5678 `!@ $%^&* )_+[];
+	  \<
+	  {}|=-a cDeFgH JkLmNo qR TuVwXyZ01234 6789`!@#$%^
+	    [];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUv xY 01 34 678 `!@ $% &* )_ [];' /,\<>?:
+	  =-
+	  eFgHiJ LmNoPq sTuVwX Z0123 56789`!@ $%^&*()_+[]
+	    <>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123 567 9` @# %^ *()_ [];'./,
+	    {}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ012345678 `! #$ ^& ()_ [];' /,\<>?:
+	    AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^& ()_+[];
+	  \<
+	  {}|=-a cDeFgH JkLmNo qRsTuV XyZ0 23456789`!@# %^&*()_+[];
+	    >?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01234 67 9` @# %^& ()_ [] '. ,\<> :"{}|=
+	    FgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&* )_ [] './ \<> :" }|=- bCdEfGh
+	    OpQrStUvWxYz0123456789`!@#$%^&*()_+[];'. ,\ >? "{} =-a cD Fg iJ LmNo qRsTuV
+	    23456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-A Cd fGh jKl nO Qr tUvW Yz01234
+	  9`
+	#$%
+	
+	)_+[];'./,\<>?:" }|=-aBc eFgHiJkLmNo
+	  Tu wXyZ01234 6789`! #$%^&* )_+ ] './ \ >?:"{} =- bCdE GhIjKlM OpQrSt vWxY 01234 67 9`!@#$ ^&*()_+[];'
+	    ?:" }|=- B D Fg i kL NoPq sTuV X
+	        89` @# % &*()_+[];'. ,\<>? "{ | -AbCdEfGhIj lMnOp
+	        012 4 6 89`
+	        _+[]; ./,\<> :
+	            iJk mN P RsTuVwXyZ01234567 9`!@#$%^&
+	            >?:"{}|=-Ab
+	            Qr tUvWxYz01 34 678 `!@#$%
+	        '
+	        }|= a c eFgHiJkLmN
+	        yZ0 2 4 6789`!@#$%^& ()_+[
+	        :"{}| -AbCdE G
+	            vWx z0 2 456789`!@#$%^&*() +[];'./,\
+	            eFgHiJkLmNo
+	            34 6789`!@#$ ^& ()_ [];'./
+	        =
+	        jKl nOpQrS U WxYz0123456789`!@#$%^&*() +[]
+	        "{} =-a c eFgHiJkLmNoPqRsTuVwX Z01
+	        @#$%^&*()_+[];'./,\<>?: {}|=-AbC
+	    j
+	  Op
+	St
+	            
+	            
+[];'.
