ebd5ab89688d19e513bbed63fa5439e5b759b8bd
Rascal_Two <therealrascaltwo@gmail.com>
2022-08-19 17:42:17 +0000

    Add Codewars 57cf50a7eca2603de0000090 JavaScript solution

---
+++ problems/Codewars/57cf50a7eca2603de0000090/README.md
@@ -0,0 +1,17 @@
+[_metadata_:generated]: - "true"
+
+# Move 10
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57cf50a7eca2603de0000090`](https://www.codewars.com/kata/57cf50a7eca2603de0000090) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Move every letter in the provided string forward 10 letters through the alphabet.
+
+If it goes past 'z', start again at 'a'.
+
+Input will be a string with length > 0.
---
+++ problems/Codewars/57cf50a7eca2603de0000090/solve.js
@@ -0,0 +1,47 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,
+  ?:"{}| -aBcDeFgHiJkLmN Pq sTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\< ? "{ | -Ab d fGh j lMnOpQrStUvWxY
+0
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz 1234 67 9` @
+	  &*()_+[];'. ,\< ?:"{} =-aBcDe gH Jk m
+	    TuVwXyZ0123456789`!@#$%^&*()_+[];'./,\< ?:"{}|=-AbCd
+	    KlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+ ];'./,\<>?:"
+	    BcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*() +[];'./,\<>?:"{}|=-
+	    GhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\< ?:"{}|=-aBcDeFgHiJkLmNoPq 
+	    yZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEf hIjKlMnOpQrStUvWxYz012 
+	    `!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPq sTuVwXyZ0123456789`!@ 
+	  *(
+	
+	  './,\<>?:" }|=-AbCdE GhI
+	    pQrSt vWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}
+	    DeFgHiJk mNoPqRsTuVwXy
+	      7 9 !@#$%^&*()_+
+	
+	      ?:" }|=- b d fG I K MnOpQrStU WxYz 1
+	        @#$% & ()_+[];'./,\<>?:"{}
+	        Hi kLmNoPqR Tu wXyZ 1234 6 89`!@#$%
+	        './, <> :"{}|=-AbCdEf hIjK M Op r tUvWxYz01234567
+	        *()_ [ ;'./,\<>?:"{}|=-aBcDeFgHiJ
+	        u
+	
+	      56789` @#$%^&*()_+
+	    ,
+	    {}| -AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&
+	
+	    './ \<>? "{}|=-AbCdEfGh
+	      QrS UvWxY 0123456789`!@#$%^&
+	      './ \<>? "{}|=-aBcDeFgH JkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[
+	      >?:"{}|=-AbCd
+	      MnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{ |=- B D FgHiJkLmNoPqRsTuVwXyZ01
+	    8
+	  #$
+	&*
+)_+[]
