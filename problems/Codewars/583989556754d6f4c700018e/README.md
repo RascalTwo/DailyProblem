0f784424b230aea435e5cdf5ae0a507aedf9f16b
Rascal_Two <therealrascaltwo@gmail.com>
2022-08-22 16:54:23 +0000

    Add Codewars 583989556754d6f4c700018e JavaScript solution

---
+++ problems/Codewars/583989556754d6f4c700018e/README.md
@@ -0,0 +1,20 @@
+[_metadata_:generated]: - "true"
+
+# Show multiples of 2 numbers within a range
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`583989556754d6f4c700018e`](https://www.codewars.com/kata/583989556754d6f4c700018e) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Print all numbers up to `3rd` parameter which are multiple of both `1st` and `2nd` parameter.
+
+Python, Javascript, Java, Ruby versions: return results in a list/array
+
+NOTICE:
+
+1. Do NOT worry about checking zeros or negative values.
+2. To find out if `3rd` parameter (the upper limit) is inclusive or not, check the tests, it differs in each translation
---
+++ problems/Codewars/583989556754d6f4c700018e/solve.js
@@ -0,0 +1,93 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`! #$%^&*()_+[ ;'./,\ >?:"{}|=- BcDe gH JkLmN
+PqRsTuVw yZ0123456789`!@#$%^&
+  _+[]; ./,\<>?:" } =-
+  dEf hIjK M O QrStUvWxYz01 3456 8 ` @#$ ^ *( _+[];'./,\<> :"{}|
+    De gH J Lm oPq s uV X Z 12 456 89 !@#$%^&*()_+[];'./
+  >
+  {}|=-A CdEfGhIjKl
+n
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=-aBc eFgHiJkLmNo
+	  TuVwXyZ012 4567 9`! #$%^ *()_ [];'./, <>?:"{}|=-A
+	    hIjKlMnOpQrStUvW
+	    3456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-a cDe gH JkL NoP RsT VwX Z01 345 789`!
+	  ^&*
+	_+[
+	
+	/,\<>?:"{}|=-AbCdEfG IjKlMnOpQrS
+	  xYz0123456 89`! #$% &*()_ [];' /,\<>?:" }|=-aBcDeFg
+	    NoPqRsTuVwXyZ01234
+	    !@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhI KlMn pQrS UvWx z012 4567 9`!@ $%^& ()_+ ];'. ,\<> :"{} =-aBcD
+	  iJk
+	NoP
+	
+	uVwXyZ0123456789`!@# %^&*()_+[];
+	  \<>?:"{}|= AbCd fGh jKlMn pQrSt vWxYz012 456789`!@#$
+	    _+[];'./,\<>?:"{}|=
+	    FgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^& ()_+[ ;'./ \<>? "{}|=-
+	  EfG
+	jKl
+	
+	QrStUvWxYz0123456789 !@#$%^&*()_
+	  './,\<>?:" }|=- BcD FgHiJ LmNoP RsTuVwXy 0123456789`
+	    &*()_+[];'./,\<>?:"
+	    bCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@# %^&*( _+[] './,\<
+	  {}|
+	aBc
+	
+	HiJkLmNoPqRsTuVwXyZ0 23456789`!@
+	  &*()_+[];' /,\< ?:" }|=-A CdEfG IjKlMnOp rStUvWxYz01
+	    89`!@#$%^&*()_+[];'
+	    ?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ01234 6789` @#$%^&
+	  +[]
+	./,
+	
+	:"{}|=-AbCdEfGhIjKlM OpQrStUvWxY
+	  3456789`!@ $%^& ()_ [];' /,\< ?:"{}|=-a cDeFgHiJkLm
+	    TuVwXyZ0123456789`
+	    &*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnO QrSt vWxY 0123 5678 `!@# %^&* )_+[ ;'./ \<>? "{}| -aBc eFgH JkLm oPqR TuVwXy
+	  345
+	89`
+	
+	%^&*()_+[];'./,\<>?: {}|=-AbCdEf
+	  KlMnOpQrSt vWxY 012 4567 9`!@ $%^&*() +[];'./,\<>
+	    =-aBcDeFgHiJkLmN
+	    uVwXyZ0123456789`!@#$%^&*()_+[];'./,\<> :"{}
+	  bCd
+	GhI
+	
+	nOpQrStUvWxYz0123456 89`!@#$%^&*
+	  [];'./,\<> :"{} =-a cDeFg iJkLm oPqRsTuVw yZ012345678
+	    %^&*()_+[];'./,\<>?:
+	    AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@ $%^&* )_+[];'
+	  <>?
+	{}|
+	
+	cDeFgHiJ LmNoPqRsTuVwXyZ0123
+	    `!@#$% &*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQr
+	U
+	xYz01234 6789`!@#$%^&*()_+[];'.
+	  >?: {}| - BcD
+	  iJkLmNo qRsTuVwXyZ0123
+	    `!@#$%^&*()_ [] './,\<>?:"{
+	      EfGhIjKlMnOp
+	    W
+	  1
+	  6789`! #$%^
+	(
+	
+	
+	'./,\<>?:"{}|=-aBcDe gHiJkLmNoPq
+	  VwXyZ01234 6789 !@# %^&*() +[];'./, <>?:"{}|=-A
+	    hIjKlMn pQrS UvWxYz0123
+	      @#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456
+	      %^&*()_+[];'./,\<>?:"{
+	      EfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^ *()_+[];'./,\<>?:"{}|=-
+	    g
+	  LmN
+	qRs
+uVwXy
