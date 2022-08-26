6a949f3c9b3fd824257d1ee2bb04e94eda061d50
Rascal_Two <therealrascaltwo@gmail.com>
2022-08-26 11:30:58 +0000

    Add Codewars 58999425006ee3f97c00011f JavaScript solution

---
+++ problems/Codewars/58999425006ee3f97c00011f/README.md
@@ -0,0 +1,26 @@
+[_metadata_:generated]: - "true"
+
+# Driving School Series #1 
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`58999425006ee3f97c00011f`](https://www.codewars.com/kata/58999425006ee3f97c00011f) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Every month, a random number of students take the driving test at Fast & Furious (F&F) Driving School. To pass the test, a student cannot accumulate more than 18 demerit points. 
+
+At the end of the month, F&F wants to calculate the average demerit points accumulated by <b>ONLY</b> the students who have passed, rounded to the nearest integer. 
+
+Write a function which would allow them to do so. If no students passed the test that month, return 'No pass scores registered.'.
+<br></br>
+
+<u>Example:</u>
+
+[10,10,10,18,20,20] --> 12
+
+
+
+
---
+++ problems/Codewars/58999425006ee3f97c00011f/solve.js
@@ -0,0 +1,71 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[] './,\< ? 
+  |=-aB DeFgHi k mNoPqRsTuVwXyZ012345 78 `!@#$%^& () +[];
+  ,\<>?: {}|=-AbCdEfGh j lMnOpQrStUvWxYz0123456789`!@#$ ^&*()_+[ ;' /,\ > :"{}|=-a cD F HiJkLmNoPqRsTu w yZ0 2345 789`!@ $%^&*()_+[];'
+/ 
+
+
+#$%^ *( _
+	
+	'./ \<>?:" } =-aBcDeFgHiJkL
+	  qRsTuV XyZ0123456789`!@#$%^&*()_+[];'./
+	<
+	
+	{}| -AbCdEfGhIj l nOpQrStUvWx
+	  234 6789`!@#$%
+	  )_+ ];'./,\ > :"{}|=-aBcDe 
+	  kLmNo qRsTuVwX Z012
+	    9`!@#$%^&*()_+[];'./,\<>
+	    =-AbCdEfGh
+	  M
+	  rStUvW Yz0123
+	6
+	
+	
+	#$%^&*() +[];'./,\<> :"{}|= a 
+	
+	  iJkLmNoP RsTuVwXyZ0123456789 !
+	  ^&*()_ [];'. ,\ >?:
+	  =
+	     
+	  KlM OpQrStUvWx z 123456789`!@#$%^&*()_+[];'
+	  
+	  "{ |=-aBcDeFgHiJkLmNoPqR T 
+	     
+	     9`!@#$ ^&*()_+[];'./,\<>?:"{}|=-AbCdEfG IjKl nO Q StU WxYz0123456789`!@#$%^&*(
+	     
+	     : {}|= a 
+	
+	     LmNoPq sTu wXyZ 123456 89`!@#$%^&*() 
+	     , 
+	
+	" 
+	
+	AbCdEfGhIjKlMnOpQr tUvWxYz0123456789`!@#
+	  *()_+[];' /,\<>?:"{}|=-aBcDe
+	  JkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>
+	  }|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789 !@#$ ^&*()_ [];'./,\<>?:"{
+	  aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz012 4567
+	  @#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ012
+	  789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*(
+	  ];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*(
+	  ];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJ
+	  oPq
+	  
+	
+	Z012345678 `!@#$%^&*()_+[];'.
+	  >?: {}|=-A CdEfGhI KlMnOpQ
+	  vWx z012345 7 9`!
+	  ^&*()_+[];'./,\<>
+	    =-aBc e gHiJkLmNoPqRsT
+	    012345 7 9`!@#$%^&*()_+[];'.
+	    :"{}|= A CdEfGhIjKlMnOp
+	    WxYz0123456789`!@#$%^&*() +[];'./ \<>?:"{}|= aBcDeFgHi
+	    PqRsTuVwXy
+	  3
+	    `!@
+	    ()
+	    
+	  
+>?:"{
