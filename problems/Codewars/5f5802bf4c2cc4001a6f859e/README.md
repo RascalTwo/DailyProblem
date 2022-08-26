87e554a2bd5e8222f3a8075c48632b1e8a940431
Rascal_Two <therealrascaltwo@gmail.com>
2022-08-26 11:56:57 +0000

    Add Codewars 5f5802bf4c2cc4001a6f859e JavaScript solution

---
+++ problems/Codewars/5f5802bf4c2cc4001a6f859e/README.md
@@ -0,0 +1,43 @@
+[_metadata_:generated]: - "true"
+
+# Grid index
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5f5802bf4c2cc4001a6f859e`](https://www.codewars.com/kata/5f5802bf4c2cc4001a6f859e) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+You are given an n by n ( square ) grid of characters, for example:
+
+```python
+[['m', 'y', 'e'], 
+ ['x', 'a', 'm'], 
+ ['p', 'l', 'e']]
+```
+
+You are also given a list of integers as input, for example:
+
+```python
+[1, 3, 5, 8]
+```
+
+You have to find the characters in these indexes of the grid if you think of the indexes as:
+
+```python
+[[1, 2, 3], 
+ [4, 5, 6], 
+ [7, 8, 9]]
+```
+
+Remember that the indexes start from one and not zero.
+
+Then you output a string like this:
+
+```python
+"meal"
+```
+
+All inputs will be valid.
---
+++ problems/Codewars/5f5802bf4c2cc4001a6f859e/solve.js
@@ -0,0 +1,46 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>? "{}|=-aB D
+  HiJkLm oPqRsTuVwXyZ0 23 56789`!@#$%^&*()_+ ] '. , <>?:"{}|=-AbCdEf h jK M OpQrStUvWxYz0123456789`
+@
+
+
+
+#$%^ *( _
+	];'./ \<>?:"{} = aBcDeFgHiJkLmNoP
+	
+	uVwXyZ01234567 9`!@#$% &* )_ [
+	  /,\<>?:"{ |=-AbCd fG Ij l
+	    StUvWxYz 1 3456789`!@#$%^&* )_+[ ;'./, <>?:" }|=- BcDeF HiJkL NoPq sTuVwX Z01 34 67 9` @# %^ *( _+ ];'.
+	    :"{}|=-AbCdEfGhIjKlMnO QrStUvWxYz012
+	
+	    !@#$%^&* ) +[];'./,\<>?:"{} =-aB DeFgH JkLmN PqRs uVwXy 01234 6789 !@#$%^ *() +[ ;'./
+	    "{}|=-AbCdEfGhIjKlMnOp rStUvWx
+	
+	    6789`!@# % &*()_+[];'./,\<> :"{} =-aBc eFgHi kLmN PqRsT VwXyZ 1234 6789`! #$% &* )_ [];'
+	    ?:"{}|=-AbCdEfGhIjKlMn pQrStUvW
+	
+	    56789`!@ $ ^&*()_+[];'./,\< ?:"{ |=-a cDeFg iJkLm oPqR TuVw yZ012 45678 `!@# %^&* )_+[] './,\ >?:" }|=- bCdEfG IjK Mn pQ St vW Yz 12 45 789 !@#$%
+	    +[];'./,\<>?:"{}|=-aBc eFgHiJkLmNoPqR
+	  wXy
+	  3456789`!@ $%^&*() +[ ;'
+	    ?:"{}|=-AbCdEfGhIjK Mn pQ S
+	      12345 789` @ $%^&*()_+[];'
+	      "{}|= aBcDeFg i kLmNoPqRsTuVwXy 01 3456789`!@#$%^&*()_+[] ' /,\<>?:"{}|=-A
+	      jKlMn pQrStUvW Y 0123456789`!@#$%^&*()_+ ];'./,\<>
+	      aB DeFgHiJkLmNoPqRsTuVwXyZ0123456789`
+	      ()_+[];'./,\<>?:"{}|=-AbCdEf hIjKlMnOp rStUvWxYz0
+	    7 9
+	  $%
+	*()
+	
+	;'./, <>?:" } = aB DeFgHiJkLmNoPqRsTuVw y
+	12345 789`!@# % & () +[];'./,\<>?: { | - b d
+	GhIjK MnOpQrStUv x z 12 456789`!@#$% & ( _
+	];'./ \<>?:"{}|= a cD Fg iJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+ ]
+	./,\< ?:"{}|=-Ab d fG Ij l n pQ StUvWxYz01234 67 9` @#$%^&*()_+[];'./,\<>?:" } = a cDeFgHiJkLmNoP R T
+	wXyZ0 23456789`!@#$%^&* ) +[];'. ,\<>?:"{ |= A
+	  fGhIj lMnOpQrSt v x z 123456789`!@#$ ^&
+	  +[];'. ,\<>?:"{}|=- B De gHiJkLmNoPqR TuVwXyZ0123
+	6 8
+`!@#$
