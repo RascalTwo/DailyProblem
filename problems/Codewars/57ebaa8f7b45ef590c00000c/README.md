498f96bc657a0e8b077aad281e067adb764f921c
Rascal_Two <therealrascaltwo@gmail.com>
2022-08-28 23:59:13 +0000

    Add Codewars 57ebaa8f7b45ef590c00000c JavaScript solution

---
+++ problems/Codewars/57ebaa8f7b45ef590c00000c/README.md
@@ -0,0 +1,16 @@
+[_metadata_:generated]: - "true"
+
+# Numbers to Letters
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57ebaa8f7b45ef590c00000c`](https://www.codewars.com/kata/57ebaa8f7b45ef590c00000c) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Given an array of numbers (in string format), you must return a string. The numbers correspond to the letters of the alphabet in reverse order: a=26, z=1 etc. You should also account for `'!'`, `'?'` and `' '` that are represented by '27', '28' and '29' respectively.
+
+All inputs will be valid.
+
---
+++ problems/Codewars/57ebaa8f7b45ef590c00000c/solve.js
@@ -0,0 +1,39 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$ ^&*()_ [ ; ./, <>?: {}| -aBc eFg i k mN
+
+qRsTuVwX Z0123456789`
+  $%^&*( _+[];'./,\ >? "{}|=-AbCdEf hI KlMnOpQrStUvWxYz012345 7 9`!@# % &*()_+[];'./,\
+>
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz012 45 789`!@#$% &* )_ [
+	  /,\<>?:"{}| -aB DeFgH JkLmNoP Rs uV X
+	    456789`!@#$%^&*()_+[];'./,\<>?:"{} =-AbC EfGhI KlMnO QrSt vWxYz 1234 6789`! #$%^&*()_+[]
+	    <>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMn pQrStUvWxY 012345 
+	    #$%^&*()_+[];'./,\<>?:"{}|=-aBcDe gHiJkLm oPqRsT 
+	    123456789`!@#$%^&*()_+[];'./,\<>?:"{ |=-Ab 
+	    jKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPq sTuVwXyZ012 45 
+	  !@
+	
+	  ()_+[];'./ \<>?:"{}| -Ab
+	    IjKlM OpQrStUvWxYz0123456789`!@#$%^&* ) +[];'./,\<>
+	    =-aBcDeF HiJkLmNoPqRsTu
+	     234 6789`! # %^&*()_+[] './,\<>?: {}|=-AbCd fGhIjKlMn pQrStUvWx z01234567 9`!@#$%^& ()_+[];'. ,\<>?:"{} =-aBcDeFg iJkLmNoPq sTuVwXyZ0 23456789` @#$%^&*() +[];'./,\ >?:"{}|=- bCdEfGhIj lMnOpQrS UvWxYz01 3456789` @#$%^&*( _+[];'./ \<>?:"{} =-aBcDeF HiJkLmNo qRsTuVwX Z01234567 9`!@#$%^& ()_+[] './
+	     {}|=-A CdEfGhIjKlMnOpQrStUvWxYz0123456
+	    #
+	    )_+ ];'./,\<>?: {}|= aBcD FgHi kLmN PqRs uVwX Z012 4567 9`!@# %^&*( _+[]; ./,\< ?:"{} =-AbC EfGhI KlMnO QrStU WxYz0 23456 89`!@ $%^&* )_+[] './,\ >?:"{ |=-aB DeFgH JkLmN PqRsTu
+	
+	    345 789` @#$%^&*()_+[];'
+	      "{} =-AbC EfGhIjKlMnOpQrStUv
+	      456 89`! #$%^&*()_+[];' /,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ01
+	      `!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGh jKlMnOpQrStUvW Yz012345 789 ! # %^&*()_+[];'./,\<>?:"{}
+	    D
+	  iJ
+	mN
+PqRsT
