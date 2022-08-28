8ec7850714a5b6087b28bf673bd646482309f99b
Rascal_Two <therealrascaltwo@gmail.com>
2022-08-28 22:00:35 +0000

    Add Codewars 555a7dc335d4c407af00006e JavaScript solution

---
+++ problems/Codewars/555a7dc335d4c407af00006e/README.md
@@ -0,0 +1,25 @@
+[_metadata_:generated]: - "true"
+
+# Tea for two
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`555a7dc335d4c407af00006e`](https://www.codewars.com/kata/555a7dc335d4c407af00006e) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Tea for two!
+
+Given a string or number write a function tea42 that takes as input a string or number and returns a string where every '2' charcter or digit has been replaced with a t.
+
+
+```javascript
+tea42('coffee') //should return 'coffee'
+tea42('tea') //should return 'tea'
+tea42('2ea') //should return 'tea'
+tea42(9022) //should return '90tt'
+tea42(5676765) //should return '5676765'
+tea42('2u2u') //should return 'tutu'
+```
---
+++ problems/Codewars/555a7dc335d4c407af00006e/solve.js
@@ -0,0 +1,49 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\ >
+  {}|=-a cDeFgHiJkLmNoPqRsTuVwXyZ 12 4567 9`! #$% & ()_ [ ;'./,\<>?:"{}|=
+Ab
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=- Bc eF H
+	  mNoPqRsTuV Xy 01 3
+	6789`!@#$%^&*()_+[];'./,\<> :"{} =-AbC EfG IjKlMnO 
+	StUvWxYz0123456789`!@#$%^&*() +[]; ./,\< ?:" }|=-aBc
+	FgHiJkLmNoPqRsTuVwXyZ01234567 9`!@#$ ^&*() +[] './,\<>
+	"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz012 45678 `!@ $%^&*()
+	[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXy 01234 678 `!@#$%^
+	()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMn pQrStUvWxYz 12345 789 !@#$%^& 
+	_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmN PqRsTuVwX Z0123 567 9`!@#$%
+	*()_+[];'./,\<>?:"{}|=-AbCdEfGhIj lMnOpQrS UvWxY 012 456789`
+	#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNo qRsTu wXy 0123456
+	9`!@#$%^&*()_+[];'./,\<>?:"{}|=-A CdEfGhIjKlMnOpQrSt vWxYz012 45678 `!@ $%^&*()
+	
+	;'./,\<>?:"{}|=-aBc eFgHiJkL
+	
+	qRsTuVwXy 01 3
+	678 `!@#$%^&*( _+[];'./,\<>?:"{ |
+	    EfG IjKlMnOpQr
+	    Yz0 23456789`!@#$%^&*()_+[];
+	    >?:"{}| -aBcDeFgHiJkLmNoPqRsTuVw
+	    3456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEf
+	    MnOp rStUvWxYz0123456789`!@#$%^&*()_+[
+	    \<
+	    |=-aBc eFgHiJk
+	    Rs
+	
+	XyZ 1 3 56789`!@#$%^&*()_+[];'./,\<>?:"
+	|=- b d fGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_
+	];' / \ >?:"{}|=-aBcDeFgHiJkLmNoPqRsTuV
+	yZ0 2 4 6789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	KlM O Q StUvWxYz0123456789`!@#$%^&*()_+
+	;'. , < ?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ012345678
+	
+	#$%^&*()_+[];'./,\<>?:"{}|= AbCdEfGhIjKlM OpQrS UvW Yz012345
+	89`!@#$%^&*()_+[];'./,\<>?: {}|=-aBcDeFgH JkLmN PqR TuVwXyZ0
+	3456789`!@#$%^&*()_+[];'./, <>?:"{}|=-AbC EfGhI KlM OpQrStUv
+	Yz012
+	  789
+	@#$
+	
+*()_+
