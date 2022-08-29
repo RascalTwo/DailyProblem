15bc6280ed9f05d7ac41d56018abdd32527f1b5f
Rascal_Two <therealrascaltwo@gmail.com>
2022-08-29 01:37:08 +0000

    Add Codewars 5844e0890d3bedc5c5000e54 JavaScript solution

---
+++ problems/Codewars/5844e0890d3bedc5c5000e54/solve.js
@@ -0,0 +1,71 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$ ^&*()_+[]; .
+  <>?:"{}|=-aBcDeFgHiJk m
+    sTuVwXyZ01234 6 89`!@#$%^
+  )
+  
+  ./,\<>?:"{} =
+    EfGhIj lMnOpQrStUvWxYz0123456789`!@#$%^&*()_+
+  '
+  
+  ?:"{}|=-aB D
+    JkLmNo qRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEf hIjKlMnOpQrStU
+  Y
+  
+  56789`!@# %
+    _+[];' /,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsT VwXyZ01234567 9 !@#$
+  *
+)
+
+
+#$%^ *( _
+	];'./ \<>?:" } =-aBcDeFgHiJkLmNoPqRsTu
+	XyZ01 3456789`!@#$%^ *
+	  [];'./,\<>?:"{}|=-AbC E GhIjKlMnOpQrS U WxYz01234 6
+	  
+	  %^&*()_+[]; .
+	    :"{}|= aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789
+	  $
+	  
+	  [];'./,\<> :
+	    AbCdEf hIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[
+	  /
+	  
+	  }|=-aBcDe g
+	    NoPqRs uVwXyZ0123456789`!@#$% &*()_+[];'./,\<>?:"{}|=-AbCdEf h jKl
+	  Q
+	t
+	
+	Yz0123456789`!@#$%^&*( _+ ]; .
+	  >?:"{ |= a cDe gHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'.
+	  >?:"{ |=- b dEf hI Kl nOpQrStUvWxYz012 4 67 9 !@
+	  &*()_ [];'./ \ >? "{ |=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./ \<>?
+	  |=-Ab dEfGhIj l nO Qr tUvWxYz01234 6789`!@ $%^&*( _+ ]; . ,\ >?:"{}|=-aBcDeFgHiJ L NoPqRsTu w yZ01234
+	  
+	  #$%^&*()_+[];'. ,\ >? "
+	    bCdEfGhIjKlMnOpQrStUvWxYz012 456789`
+	  %^&
+	  
+	  './,\<>?:"{}|= aB De g
+	    NoPqRsTuVwXyZ0123456789`!@# %^&*()_+[
+	  /,\
+	  
+	  =-AbCdEfGhIjK Mn pQ S
+	    z0123456789`!@#$%^&*()_+[] './,\<>?:"{}|=-aBcDeFgHiJkLm
+	  RsT
+	  
+	  23456789`!@# %^ *( _
+	    /,\ >?:" } = Ab d f hI KlMn p
+	      Yz0 2345 7 9`!@#$%^&*
+	      ./, <> : {}| -aBcDeFgHiJkLmNoP
+	      yZ0 234 6 89` @#$%^&*()_+[];'./,\<>
+	      
+	      IjKlMnOpQrStUvWxYz0123456789 !@#$%^&*()_+[];'.
+	      {}|=-aBcDeFgHiJkLmNoPqRsTuV XyZ0123456789`!@
+	      _+[];'./,\<>?:"{}|=-AbCdEf hIjKlMnOpQrStUv
+	    2
+	  789
+	@#$
+	
+*()_+
