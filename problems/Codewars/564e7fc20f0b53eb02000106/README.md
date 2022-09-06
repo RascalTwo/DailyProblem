a7a33cdf777269d4ec79baa550541e39ddb55ada
Rascal_Two <therealrascaltwo@gmail.com>
2022-09-06 22:21:34 +0000

    Add Codewars 564e7fc20f0b53eb02000106 JavaScript solution

---
+++ problems/Codewars/564e7fc20f0b53eb02000106/README.md
@@ -0,0 +1,15 @@
+[_metadata_:generated]: - "true"
+
+# Count consonants
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`564e7fc20f0b53eb02000106`](https://www.codewars.com/kata/564e7fc20f0b53eb02000106) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Write a function `consonantCount`, `consonant_count` or `ConsonantCount` that takes a string of English-language text and returns the number of consonants in the string.
+
+Consonants are all letters used to write English excluding the vowels `a, e, i, o, u`.
---
+++ problems/Codewars/564e7fc20f0b53eb02000106/solve.js
@@ -0,0 +1,54 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{} =
+  cDe gHiJk m oP
+  TuV XyZ012 4567 9` @#$%^&*()_+[];'./,\
+    {} =-AbC E GhI Kl nOpQ St vWx z0 23456789`!@#$%^&*()_+[] './,\<>?
+  }
+  aBcDeF HiJkLm
+o
+
+
+#$%^ *( _
+	];'./ \<>?:" } =-aBcDeF
+	      oPqRsTuVwX Z 123456789`!@#$%^&*()_+[]
+	      ?:"{} = AbCd
+	
+	IjK MnOpQr t vWx
+	
+	23456789`!@#$%^ *()_+ ];'./,\<>? "{}|=-aBc eFgHiJkLmN P
+	    wXyZ012345 789`!@ $%^&*( _+ ];'./,\<>? "{ |=-AbCdE GhIjKlMnOp r
+	        23456789`!@#$%^&*()_+[];'./,\<>?:"{}| -a cDeFg iJkLmN Pq sTuVw yZ0123456
+	        &*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQr tU WxYz0 2345678 `!@#$%^&*(
+	        \<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456 89 !@#$% &*()_+[ ;'./,\<>?:"{
+	        GhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_ [] './,\ >?:"{}| -aBcDeFgHi
+	        TuVwXyZ0123456789`!@#$%^&*()_+[];'./,\< ?:"{}|=- bC EfGhI KlMnOpQ StUvWx z01234567
+	        *()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwX Z0123456 89 !@#$% &*()_+[ ;'./,\<>?:"{}| -AbCdEfGh
+	        StUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}| -a cDeFg iJkLmNo qRsTuVwXyZ01234
+	        %^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrSt vW Yz012 456789` @#$%^&*()_+[];
+	    >?:
+	}|=
+	
+	DeFgHiJkLmNoPqR TuVwX Z0123456 89`!@#$%^ *()_+[];' /,\<>?:"{} =
+	    fGhIjKlMnO QrStUv xYz012 45 789`!@#$%^ *( _+[];'./ \<>?:"{}|= a
+	        LmNoP RsTuVwXyZ0 2
+	            &*( _+[];'./ \ >?:
+	                MnOp r tUvWxYz0123456789`!@#$%^ * )_+[];'
+	                eFgH J LmNoPqRsTuVwXyZ012345678 ` @#$%^&*
+	                }|=-A C EfGhI
+	            xYz01 3456789` @
+	                <>? "{}| - BcDeFgHiJkLmNoPqRsTuVwXy 0 234
+	                    ./,\< ? "{}|=-AbCdEfG
+	                z0123456 89 !@#$%^
+	            <
+	            FgHiJ LmNoPqRs u
+	                #$% &*() + ];'./,\<>?:"{}|=-AbCdEfG I KlMn
+	                    `!@#$%^&* ) +[];'./,\<>?:"{}|
+	                qRsTuVwX Z0 23456789`!
+	            '
+	            AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^ *()_+[ ;'./, <>?:"{} =-a c eFgHiJkL N PqRsTu
+	        5
+	    @#$
+	&*(
+	
+[];'.
