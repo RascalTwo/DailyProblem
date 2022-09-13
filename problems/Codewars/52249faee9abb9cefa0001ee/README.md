4c420a83e5d4a8fe7f9ef74fd4d3fd0bc8fc64fc
Rascal_Two <therealrascaltwo@gmail.com>
2022-09-13 19:35:33 +0000

    Add Codewars 52249faee9abb9cefa0001ee JavaScript solution

---
+++ problems/Codewars/52249faee9abb9cefa0001ee/README.md
@@ -0,0 +1,21 @@
+[_metadata_:generated]: - "true"
+
+# uniq (UNIX style)
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`52249faee9abb9cefa0001ee`](https://www.codewars.com/kata/52249faee9abb9cefa0001ee) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Implement a function which behaves like the uniq command in UNIX.
+
+It takes as input a sequence and returns a sequence in which all duplicate elements following each other have been reduced to one instance.
+
+Example:
+
+```
+["a", "a", "b", "b", "c", "a", "b", "c"]  =>  ["a", "b", "c", "a", "b", "c"]
+```
---
+++ problems/Codewars/52249faee9abb9cefa0001ee/solve.js
@@ -0,0 +1,47 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[]; .
+  <>?:"{ |=-aBcDeFgHiJk mN Pq sTuVwXyZ012345 7 9` @#$ ^ * )_+ ] './,\<>? "{} =-Ab
+d
+
+
+#$%^ *( _
+	];'./,\<>?:"{}| -aBcDeF Hi kL N
+	  sTuVwXyZ012 45 78 `
+	    &*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789` @#$%^&*()_+[];'./,\<>?:"{}|
+	    eFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}| -AbCdEfGhIjKlMn
+	    UvWxYz0123456789`!@#$%^&*( _+[]
+	    <>?:"{}|=-aBcDeFgHiJkLmNoPqRsTu wXyZ01234
+	    !@#$%^&*()_+[];'./,\<>?:"{}|=-A CdEfGhIjK
+	    rStUvWxYz0123456789`!@#$%^&* )_+[];
+	    >?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ012 456789`!@#$%
+	  )_+
+	;'.
+	
+	>?:"{}|= AbCdEfGhIjK M
+	  rSt v x z01
+	  678 `!@# % & () + ] './,\<>?: {}|=
+	    Fg iJk mN PqRs uVw yZ0123456789`!
+	      )_+[];'./,\<>
+	  }|=-Ab dE
+	h
+	
+	MnOpQ StUvWxY 0 23456789`!@#$%^&*()_+[];'
+	,\<>? "{}| - BcDeFgHiJkLmNoPqRsTuVwXyZ0123
+	6789` @#$%^&* ) + ]; ./,\<>?:"{}|=-AbCdE Gh jKlMnOpQrStUvWx z0123456789 ! #$%^&*()_+[];'
+	
+	<>?:"{}|=-aBcDeF HiJkLmN Pq sT V
+	  0123456789` @# %^ *
+	    ;'. ,\<> : { |= A C EfGh jKlM O
+	      xYz 1 3 567
+	      ^&* )_+[ ; . ,\<>?:"{}| -aB De gHiJ L
+	        wXy 0 2 456789`!@#$%^&*()_ [];'
+	        }|= AbCd f h jKlMnOpQrS UvW Yz 1234
+	          &*()_+[];'
+	      "
+	      DeF HiJkLm o qRsTuVwXyZ01
+	      `!@#$%^&*()_+[];'./,\<>?: {}|=-AbC
+	    j
+	  OpQ
+	tUv
+xYz01
