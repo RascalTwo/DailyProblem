46eda8a2a88471d3fbfa98338e2c045563aba2f5
Rascal_Two <therealrascaltwo@gmail.com>
2022-10-17 18:49:50 +0000

    Add Codewars 593e84f16e836ca9a9000054 JavaScript solution

---
+++ problems/Codewars/593e84f16e836ca9a9000054/README.md
@@ -0,0 +1,17 @@
+[_metadata_:generated]: - "true"
+
+# Return 1, 2, 3 randomly
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`593e84f16e836ca9a9000054`](https://www.codewars.com/kata/593e84f16e836ca9a9000054) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+You have function `one_two` (`oneTwo` for Java) that returns 1 or 2 with 50% chance. `one_two` is already defined in a global scope and can be called everywhere.
+
+Your goal is to create function `one_two_three` (`oneTwoThree` for Java) that returns 1, 2 or 3 with equal probability using only `one_two` function.
+
+Do not try to cheat returning repeating non-random sequences. There is randomness test especially for this case.
---
+++ problems/Codewars/593e84f16e836ca9a9000054/solve.js
@@ -0,0 +1,83 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>? "
+  =-a cDe gH J LmN PqR
+  Vw yZ0 23 5 789`!@#$%^& ()_+[];'./,
+  ?:"{} =- bCd f hI K MnO QrS
+  WxYz01 3 5 7 9 !@
+$
+
+
+#$%^ *( _
+	];'./,\<>?:"{}| -aBcDeF Hi kL N
+	  sTu wXyZ01
+	  678 `!@#$%^
+	  _+[]; ./,\< ? "{}|=-
+	  EfGhIjKlM Op r
+	    Yz012 4 6789`!@#$%^&*()_
+	    /,\<>? " }|=-aBcDeFgHiJkLmNoP
+	  uVw
+	  123456789` @#$%^& ()_ [];'./,\ >?:"{}|=- bC Ef h
+	    OpQrStUv xY 012345 78 `!@# %^ *()_+[];'.
+	    :"{}|=-a cD FgHiJk mN PqRs uV XyZ0123456
+	    #$%^&*() +[ ;'./,\ >? "{}| -A CdEfGhIjKl
+	  QrS
+	
+	  z012345678 `!@ $%^&*() +[];'./ \<>?:"{}| -a cD F
+	    mNoPq sTuV X Z0123456789`!@#$%^&*()
+	    ./,\< ?:"{}| - bCdEfGhIjKlMnOpQrStUvWxYz0
+	    789`!@#$%^&*( _+[ ;' /,\<>? "{} =-aB De gHiJ L NoPqRsTu wXyZ012 4567 9`!@#$%^
+	  _+[
+	
+	  \<>?:"{}|= AbCdEf hIj lMnOpQr tUvW Yz0 23456789`!@#$ ^& () +
+	    ,\<>? "{}|=-aB D FgHiJk
+	
+	    TuVwX Z01234 6 89`!@#$%^&*()_+[]; . ,\<>?: { |= A CdEf
+	    MnOpQ StUvWx z 123456789`!@#$%^&* ) +[];'. , <> : {}|=
+	    FgHiJ LmNoPqRs u wXyZ0123456789`!@# % &*()_+ ] '. , <>?:
+	
+	    CdEfGhIjKlMnO Q StUvWxYz0 23 56789 !@#$ ^& ()_+[] './,\ >?:"{}
+	    DeFgHiJkLmNoP R TuVwXyZ01 34 6789` @#$% &* )_+[]; ./,\< ?:"{}|
+	    EfGhIjKlMnOpQrS U WxYz01234 67 9`!@# %^&* )_ [];'./ \<>?: {}|=-a
+	  FgH
+	
+	  oPqRsTuV XyZ01234 6789`!@#$%^ *( _+ ]
+	    <>?:"{}|=-AbCdEfGhIjKlMnOpQrStU WxYz01234567 9`!@#$% &*() +[];' /, < ?:"{}|=-aBc
+	    JkLmNoPqRsTuVwXyZ0123456789`!@# %^&*( _+[];'./,\< ?:" }|=- bCdEf hI K MnOpQrStUvW
+	    3456789`!@#$%^&*()_+[];'./,\<>? "{}|= aBcDeFgHiJk mNo qRsT VwXyZ 12 4 6789`!@#$%^
+	    [];'./,\<>?:"{}|=-AbCdEfGhIjKlM OpQrS UvWxYz01234 678 `!@# %^&*( _+ ] './,\<>?:"{
+	  aBc
+	
+	  JkLmNoPq sTuVwXyZ0123 56 89 !
+	    *()_+[];'./,\<>?:"{}|=-A CdEfGhIjK MnOp rStU WxYz012345 789`!@#
+	  *()
+	[];
+	
+	
+	>?:"{}|= aBcDeFgHiJk m
+	  RsTuV XyZ012345678 ` @#$%^&*()_+[
+	  /,\<>?:"{}| - bC Ef h
+	    OpQrS UvWxYz0123456 89`!@#$%^&* )_ []; ./,\<>?:" }|= aBcD FgHiJ LmNo qRsTu wX Z01 3456789`
+	  %
+	
+	  +[];' /,\ > :"{
+	  
+	  EfGhIjK MnOp rStU WxYz 1
+	    89`!@ $%^&* ) +[];'./,\<>?:"{}
+	    DeFgHiJkLm o qRsTuVwXyZ 1 3 56 89
+	  $
+	  ()_+[];'./, < ?:"{}|=-AbCdE
+	  jKlMnO QrSt
+	W
+	
+	12345678 `!@#$%^&*()_+[] '
+	  <>? "{} = aBc
+	
+	  JkLmNoP RsTu wXyZ 1234 6
+	    #$% &* )_+[];'./,\<>?:"
+	  -
+	  
+	  IjKlMn pQrS
+	v
+	
+z0123
