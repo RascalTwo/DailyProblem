38df002d39f0b0d66a95a3fab3b32580de85b0a4
Rascal_Two <therealrascaltwo@gmail.com>
2022-10-19 16:48:32 +0000

    Add Codewars 56426b4e971f01fd2f00005b JavaScript solution

---
+++ problems/Codewars/56426b4e971f01fd2f00005b/README.md
@@ -0,0 +1,24 @@
+[_metadata_:generated]: - "true"
+
+# Find the Square Root
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`56426b4e971f01fd2f00005b`](https://www.codewars.com/kata/56426b4e971f01fd2f00005b) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Create a Function that takes one parameter and returns its square root rounded to 5 decimal places. You are not allowed to use the Math.sqrt() or the Math.pow() methods.
+
+Trailing zeros should be left out. For example
+
+```javascript
+squareRoot(39) // => 6.245;
+```
+rather than
+```javascript
+squareRoot(39) // => 6.24500;
+```
+
---
+++ problems/Codewars/56426b4e971f01fd2f00005b/solve.js
@@ -0,0 +1,40 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\< ?:"{} = a c eFgH J
+    PqRsT VwXyZ0123 5 789 ! #$%^&* ) +[];'. , <>?:
+    -A CdEfGh jK MnOpQrStUv xYz012 456789`!@#$%^&*()_
+    ./,\<> :"{}|=-aBcDeF HiJkLmNoPqR
+T
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=-aBcDe gHiJkLmNoPq
+	  VwXyZ012 456789`!@#$ ^
+	    []; ./,\ > :"
+	    bCd fGhIjKlM O Qr
+	    Yz01234 6 8 `! # %^ *( _+[] '
+	      "{}|=-aBcDe g iJkLmNoPq s uVwXy 0 23456789` @ $% &
+	        /,\< ?: {}|=-AbCd
+	      M
+	      vWxYz012 45 789
+	    ^
+	    []; ./,\<>?:"{}| - BcDeFgHiJkLmNoPqRsTuVwXyZ012345678 ` @#
+	    )_+[ ; ./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz012345
+	    @#$%^& ()_+[];'./,\<>?: { |=-aBcDe g iJkLmNo
+	  T
+	  yZ01234567 9`!@ $%^ *()_ [];'./,\<> :"{}|=-AbCd
+	    KlMnOpQrS U WxYz0
+	    789`!@#$ ^ *()_+
+	    ,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVw yZ0123456789`! #$%^&*() +[]; ./,\<>?
+	    -AbCdEfGhIjKlMnOpQrStUvWxYz0123456 89`!@#$%^&*() +[];'./, <>?: {}|=-a
+	    HiJkLmNoPqRsTuVwXyZ0123456789`!@#$% &*()_+[];'./,\ >?:"{}|= AbCd fGhIjKl
+	    StUvWxYz0123456789`!@#$%^&*()_+[]; ./,\<>?:"{}|= aBcDeFgH JkLm oPqRsT
+	  yZ0
+	  56789`!@#$ ^&*( _+[ ;'./,\ >?:"{}|=- bCdEfGhIjKl
+	    StU W Yz0 2345 789`! #$% & (
+	      /,\<> : { |=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0
+	      9`!@#$%^&*()_+[];'./,\<>?:"{}|=-A CdEfGhIjKlMn pQrStUvW Yz01 3 5 789
+	    ^
+	  _+[
+	'./
+\<>?:
