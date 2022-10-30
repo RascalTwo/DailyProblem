9b48074ddfa4ba1a41bd346a8f02ab498f787478
Rascal_Two <therealrascaltwo@gmail.com>
2022-10-30 17:32:54 +0000

    Add Codewars 564a6486c2070faee6000011 JavaScript solution

---
+++ problems/Codewars/564a6486c2070faee6000011/README.md
@@ -0,0 +1,17 @@
+[_metadata_:generated]: - "true"
+
+# Is Undefined?
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`564a6486c2070faee6000011`](https://www.codewars.com/kata/564a6486c2070faee6000011) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+<h1>Is Undefined?</h1>
+Help Timmy remove undefined values from his array, you are modifing a callback function which he uses on each element on his array. 
+
+<h2>Task</h2>
+Your task is to finish the isUndefined method which returns true if the value is undefined :)
---
+++ problems/Codewars/564a6486c2070faee6000011/solve.js
@@ -0,0 +1,55 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{ |
+  BcDeFg iJkLm oPq sTuVwXyZ01
+3
+
+
+#$%^ *( _
+	];'./,\<>?:"{}| -aBcDeF Hi kL N
+	  sTuVwXyZ012 45 78 `
+	    &*()_+[];'./,\<>?:"{}|=-AbCdEfGhIj   
+	    tUvWxYz0123456789`!@#$%^&*()_+[];'   
+	    {}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789   
+	    ()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKl
+	    StUvWxYz0123456789`!@#$%^&*()_+[];
+	  \<>
+	"{}
+	
+	BcDeFgHiJkLmNoPq sTuVwXy 01 34 6
+	    #$%^& ()_+[];' / \< ?: {
+	      EfG IjKlM O QrS
+	      123 56789`
+	      ()_ [];'./, < ?:"{}|=-aBcDe
+	      
+	      VwXyZ01 3456789`!@#$%^
+	        ./, <>?: { |=-
+	        KlMnOpQ StUvWxYz01234
+	          &*( _+[] ' /,\
+	          
+	          oPqRsTuVwXyZ0123 5 789`!@#$%^&*()_+[];'./,\<>?:" } =-A
+	          nOpQrStUvW Y 0123456789`!@#$%^&*()_+[];'./ \ >? " }|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&* ) +[];'./,\<>?:"{}|=-AbCdEfGhIjKl
+	          
+	          `!@#$%^&*()_+[];'./,
+	            DeFgHiJkLm o qRsTuV
+	          
+	          )_+[];'./,\<>?:"
+	        f
+	        QrStUvWxYz0123456
+	      %
+	      
+	      >?:"{} =-aBcD
+	    k
+	  PqRsTuVwXyZ 12 45 7
+	    $%^ *()_+ ] './,\<>?:"{
+	    
+	    IjKlMnO QrS Uv xYz012 4
+	      #$% &*()_ [ ;'./,\<>?:"
+	      DeFgHiJ L No qRsTuV X
+	        89` @#$% & ()_+[];'.
+	        |=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456 89`!@#$%^&*()_+[]; 
+	      "
+	    B
+	  gHi
+	LmN
+PqRsT
