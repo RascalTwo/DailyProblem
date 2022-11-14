342650de8173023cc3aa24d006d98172cb3ce185
Rascal_Two <therealrascaltwo@gmail.com>
2022-11-14 14:25:58 +0000

    Add Codewars 55f81f9aa51f9b72a200002f JavaScript solution

---
+++ problems/Codewars/55f81f9aa51f9b72a200002f/README.md
@@ -0,0 +1,28 @@
+[_metadata_:generated]: - "true"
+
+# Find the unique number
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`55f81f9aa51f9b72a200002f`](https://www.codewars.com/kata/55f81f9aa51f9b72a200002f) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Write a function called `findUnique` which returns the only unique number from an array.
+
+All numbers in the unsorted array are present twice, except the one you have to find. The numbers are always valid integer values between 1 and 2147483647, so no need for type and error checking. The array contains at least one number and may contain millions of numbers. So make sure your solution is optimized for speed.
+
+## Example
+
+Input:
+
+    [ 1, 8, 4, 4, 6, 1, 8 ]
+    
+Expected output:
+
+    6
+    
+
+
---
+++ problems/Codewars/55f81f9aa51f9b72a200002f/solve.js
@@ -0,0 +1,98 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{} =
+  cDeFgHiJkLmNoPqR Tu wX Z 1 345
+  
+  @# %^&*()_+[]; ./, <>?:"{}|=-A CdEfGh jKlMnOpQrSt
+  xY 0123456789`!@#$%^&*()_+ ] '. ,\< ?:"{}|=-aBcDeFgHiJkLmN P RsT VwXyZ0 23456789`!@#$%^&*()_+[ ; ./,
+
+  :"{ |=-A C E Gh j l nOpQrStUvWxYz01 3 56 89`
+    ^& ()_+[];'./, <>? "{}|=-aBc e gHi kLmNoP RsTuVwXyZ01
+  5
+7
+
+
+
+#$%^ *( _
+	];'./,\<>?: {
+	  aBcDe gHiJ L NoPqRsTuVwXyZ012
+	  789`! #$%^&* ) +[];'./,\<>?
+	  |=-AbCdEfGhIjKlMnOpQrStUvWxYz 1 34
+	  
+	  #$%^&*() +[];'./,\<>?:"{}| -
+	    gHi kLmNo q sTu wXyZ01234567
+	    $%^ *()_ [ ;'
+	    ?:" }|=-A C EfGh j lM
+	    tUv xYz0123 5 789` @ $%
+	    +[] './, < ? "{ | -a cDeFgHiJ LmNo q
+	      Z0123 5678 ` @#$%^&*()_+[]; . , < ?:
+	      Cd fGhIjKl n  
+	        0123456789`!@ $ ^&
+	        /,\<>?:"{}|=- B De
+	      N PqRs uV XyZ01 345 78 `
+	        _+[];'./,\<>? " }|
+	        IjKlMnOpQrStUv x z0
+	      9 !@#$ ^       
+	        :"{}|=-aBcDeFg i kL
+	        wXyZ0123456789 ! #$
+	      [
+	    \
+	    }|=-Ab dEfGhI
+	  n
+	    
+	  Yz012345 789`!@#$%^&*()_+[] '
+	      "{} =-aB D FgHiJkLmNoPq s uV
+	      456 89`!@ $ ^&*()_+[];'./ \ >?:"{}|=-AbC E Gh
+	      QrS UvWx z 123456789`!@
+	      _+[];'./,\< ? "{}|=-aBcDeFg
+	      PqRsTuVwXyZ0 2 45678
+	      &*()_+ ];'./,\<>?:"
+	      dE GhIjKl nOp rS UvWx
+	      67 9`!@#$ ^&*()_+[];'./,\<>?:"{}|=-aB D FgHiJkLmNoPqR T VwX Z 12 456789
+	  $
+	
+	  _+[];'./ \<>?:"{}|= A
+	      jKl nOpQr t vWxYz0123456789`!@
+	      _+[ ;'./,\ > :"{}|=-aBcDeFgHiJkL
+	      uVwXyZ0123456789`!@#$%^&*()_+[];'./,\ >?:"{}|=
+	  d
+	  IjKlMnOpQrStUvW Yz01234 67 9` @
+	    ()_+[];'. ,\<> :"{}|=-a cDeFgHiJ LmNoPqRsTu w
+	        789`!@#$%^&*()_+[];'./,\<>?:"{}|= Ab dE Gh jK Mn pQrS UvW
+	        789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgH JkLmNoPqRsT VwXyZ01234 6789`!@#$% &*()_+[];' /,\<>?:"{} =-AbCdEfGh jKlMnOpQrSt vWxYz012345 789`!@#$%^& ()_+[];'./, <>?:"{}|=- BcDeFgHiJkL NoPqRsTuVwX Z0123456789 !@#$%^&*()_ [];'./,\<> :"{}|=-AbC EfGhIjKlM OpQrStUvW Yz01234567 9`!@#$%^&*( _+[];'./,\< ?:"{}|=-aBc eFgHiJkLmNo qRsTuVwXyZ0 23456789`! #$%^&*()_+ ];'./,\<>?: {}|=-AbCdEf hIjKlMnOpQr tUvWxYz012 456789`!@#$ ^&*()_+[];' /,\<>?:"{}| -aBcDeFgHiJ LmNoPqRsTuV XyZ01234567 9`!@#$%^&*()_ [];'./,\<>?:
+	    AbC
+	    jKlMnOpQ StUv x z01234 6789`!@ $%^&*()_+[ ;
+	        {}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789 !@#$%^&*(
+	    './    
+	  }|
+	  CdEfGhIjKlMnOpQr tUvWxYz 12 45 7
+	    $%^&*()_+ ];'. ,\<>? "{}|=- Bc eFgHiJ LmNoPqRs uVwXyZ0123 5
+	      $%^ *()_ [ ; ./ \ > :" }|=- b
+	        MnOpQrStU
+	      3
+	      @#$%^&*()_
+	      \<>?:"{}|=-
+	    gHi
+	    PqRsTuVwXy 01234 678 `!@#$%^& ()_+[];'./ \
+	      =-A CdEf h j lM O Q StUv xYz0 2
+	        #$%^&*()_+[]
+	      ?
+	    -a
+	    HiJkLmNoPq sTuVw yZ01 3456789` @#$%^&*()_ [
+	      >?: {}|= A C Ef h j lMn pQrS U
+	        56789`!@#$%^&
+	      '
+	    ?:
+	    aBcDeFgHiJ LmNoP Rs uVwXyZ01 3456789`!@ $
+	      []; ./,\ > : {} = A CdE GhIj l
+	        WxYz0123456789
+	      *
+	    ;'
+	    ?:"{}|=-aB DeFgH JkL NoPqRsTu wXyZ012345 7
+	      ^&* )_+[ ; . ,\ > : {} =-Ab d
+	        OpQrStUvWxYz012
+	      !
+	    *()
+	  ;'
+	,\<>?
+"{}|=
