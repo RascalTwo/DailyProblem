1706e9407664132c3ea6b46a56c4edc0098e76d9
Rascal_Two <therealrascaltwo@gmail.com>
2022-11-18 16:20:25 +0000

    Add Codewars 5dd5128f16eced000e4c42ba JavaScript solution

---
+++ problems/Codewars/5dd5128f16eced000e4c42ba/README.md
@@ -0,0 +1,24 @@
+[_metadata_:generated]: - "true"
+
+# Most valuable character
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5dd5128f16eced000e4c42ba`](https://www.codewars.com/kata/5dd5128f16eced000e4c42ba) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+In this Kata, you will be given a string and your task is to return the most valuable character. The value of a character is the difference between the index of its last occurrence and the index of its first occurrence. Return the character that has the highest value. If there is a tie, return the alphabetically lowest character. `[For Golang return rune]`
+
+All inputs will be lower case. 
+
+```
+For example:
+solve('a') = 'a'
+solve('ab') = 'a'. Last occurrence is equal to first occurrence of each character. Return lexicographically lowest.
+solve("axyzxyz") = 'x'
+```
+
+More examples in test cases. Good luck!
---
+++ problems/Codewars/5dd5128f16eced000e4c42ba/solve.js
@@ -0,0 +1,69 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'. ,
+  ?:"{} =-aBcDe g iJk mNoPqR
+  VwX Z012 4 6 89 ! # %^&*()_+[] './,\
+    {}|=- bCdE G IjKlMn
+    tUvWxYz0123456789 !@#$%^&*()_+[];'./,\<> :" }|=- BcDe
+  i
+
+  NoPqRs uVwXyZ0123456789`!@#$%^&*()_+[]; ./,\<>?: {}|=- bC EfGhIj lMnOpQrSt vW Y
+    45678 `!@#$%^& ( _+[];'./,\<>?:"{}|=-aB D Fg i kLmNoPqRsTu
+    
+    56 89`!@#$%^ * )_+[]; ./,\<> : {}|=-AbC EfGhIjK MnOpQr tUvWxYz0 23
+    
+    $%^&*( _
+      ,\<>?:"{ |=-aBcDe gHi kLmNo q sTuVwXyZ0123 56789 ! #$%^&*()
+      /,\<>
+    |=
+  Cd f hIjKlMnO QrS UvWxYz 123456789 !@#$%^&*()_+[];'./,\<
+?
+
+
+#$%^ *( _
+	];'./ \ > :"{}|=-aBcDeFgHiJ
+	mNoPq sTuV X Z0123456789`!@#$
+	&*()_ [];'./ \ >?:"{}|=-AbC
+	
+	hIjKlMnOpQrStUvWx z012345 789`!@#$%^ *
+	  [];'./,\< ?:"{}|= aBcDeFgHiJ L
+	    sTuVwXyZ0123456789`!@#$%^&*()_ [];'.
+	    :"{}|=-AbCdEfGhIjKlMnOpQrStUvWx z0123
+	    `!@#$%^&*()_+[];'./,\<>?:"{}|=-a cDeFg
+	    NoPqRsTuVwXyZ0123456789`!@#$%^&*()_+ ];'./
+	    "{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01234 6789`
+	    &*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJk mNoPq 
+	    yZ0123456789`!@#$%^&*()_+[];'./,\<> :"{}| 
+	    fGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&* )_+[]
+	    <>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0 23456
+	    #$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlM OpQrS
+	  xYz
+	234
+	
+	9`!@#$%^&*()_+[] './,\<> :"{}|=-aBc e
+	  JkL NoPq s uVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbC
+	  hIjKlMn pQrStUv xYz0123456 8
+	    %^& ()_+ ] ' /, < ? "{}| -aBcD
+	      mNo qRs u wXyZ0123456789`!@#$
+	      []; ./ \ >?:"{}|=-AbCdEfGhIjKlMnOpQrStUvW 
+	      678 `! # %^&*()_+[];'./ \ > : {}|=-aBcDeFg i kL    
+	      yZ0 234 6 89`!@#$
+	      [];'./,\<>?:"{}|=-AbCdEfGhIjK MnOpQ   
+	    0  
+	  789
+	@#$
+	
+	()_+[];' /,\<>?: {
+	    cDe gHiJ L NoPqRsT VwXyZ0123456789
+	    ^&* )_+ ] './,\< ?:"{ | -Ab d f h
+	    Op rStUvWxYz012 45 78 `!@#$% &*()_+[]
+	    <> :"{}|=-aBcDe gH JkLmNoPqRs uVwXyZ 1234567
+	    $%^ *()_ [ ;' /,\<>?
+	        dEf h j lMnOpQrStUvWxYz012 456789`!@#$%^
+	        ./ \< ?: {} =-aBcDe g iJ LmNo
+	        Z0 23 56 89`!@#$ ^&*()_+[];'.
+	        |= Ab d fGhIjKlMnOpQ S UvW Yz0123 5 78 `!@#$% & ()
+	    .
+	     "{}|=- BcDeFg iJk m o qRsTuV X Z012345
+	8
+`!@#$
