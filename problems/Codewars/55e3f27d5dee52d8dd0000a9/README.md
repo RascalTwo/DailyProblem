1b1cc0deca7553c2d7cc7bae5514bb38ade3d5f8
Rascal_Two <therealrascaltwo@gmail.com>
2022-12-01 06:23:07 +0000

    Add Codewars 55e3f27d5dee52d8dd0000a9 JavaScript solution

---
+++ problems/Codewars/55e3f27d5dee52d8dd0000a9/README.md
@@ -0,0 +1,32 @@
+[_metadata_:generated]: - "true"
+
+# Difference between biggest 2 numbers
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`55e3f27d5dee52d8dd0000a9`](https://www.codewars.com/kata/55e3f27d5dee52d8dd0000a9) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+You have an array of non-negative integers. You need to calculate the difference between the 1st biggest number and the 2nd biggest number of the array.
+
+```javascript
+    diffBig2([10, 5, 2]);
+```
+```ruby
+    diff_big_2([10, 5, 2])
+```
+```python
+    diff_big_2([10, 5, 2])
+```
+```coffeescript
+    diff_big_2 [10, 5, 2]
+```
+
+In this case, the 1st biggest number is `10` and the 2nd biggest number is `5`. So, the function returns `5`, the result of `10 - 5`.
+
+You can assume that the input array has `2` or more elements.
+
+The input array has the `sort` method disabled, so you will have to solve it in another way.
---
+++ problems/Codewars/55e3f27d5dee52d8dd0000a9/solve.js
@@ -0,0 +1,76 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\< ?
+  }|=-a cDeFgHi k mNoPqRsTuVwXyZ012
+  6789`!@#$%^&*()_+[];'./,\<>?:"{} =-A
+  EfGhIj lMnOpQr t vWxYz0123456789`!
+#
+
+
+#$%^ *( _
+	];'./ \<>?:"{} = aBcDeFgHiJkLmNoP
+	
+	uVwXyZ01234 6789`!@ $% &* )
+	  ;'./,\<>?:"{}|=-AbCdEfGhIjKlMnO QrSt vWx
+	  23456789`!@#$%^&*()_+[];'./,\<> :" }|=- BcD
+	  iJkLmNoPqRsTuVwXyZ0123456789`!@ $% &*() +[]
+	./,
+	
+	:"{}|=-Ab dEfGhIj lM Op r
+	  WxYz0123456789`!@#$%^&*()_+[];' /, <> :" }| -a cD Fg iJ LmNoP RsT
+	  yZ0123456789`!@#$%^&*()_+[];'./ \<> :"{}|= AbCdE
+	  jKlMnOpQrStUvWxYz0123456789`!@#$% &*( _+[] './,\
+	  "{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0 234567 9`!
+	  ^&*()_+[];'./,\<>?:"{}|=-AbCdEf hI KlM Op rS Uv xY 01 34 6789 !@#
+	  *()_+[];'./,\<>?:"{}|=-aBcDeFgH Jk mNo qR Tu wX Z0 23 56 89 !@#$% &*(
+	  ];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOp rS Uv xY 01 34 67 9` @# %^ *() +[] './ \<> :"{ |=- BcD FgH JkL NoP RsT VwX Z01 345 789 !@# %^& ()_ []; ./, <>? "{}|=- bCd
+	  IjKlMnOpQrStUvWxYz0123456789`!@ $% &* )_ [] '. ,\ >? "{ |=- BcD FgH JkL NoP RsT VwX Z01 345 789 !@# %^& ()_ []; ./, <>? "{} =-A CdE GhI KlM OpQ StU WxY 012 456 89` @#$ ^&* )_+ ];' /,\ >?: {}| -aB DeF HiJ LmN PqR TuV XyZ01 345
+	  `!@#$%^&*()_+[];'./,\<>?:"{}|=- bC Ef hI Kl nO Qr tU Wx z01 345 789 !@# %^& ()_ []; ./, <>? "{} =-a cDe gHi kLm oPq sTu wXy 012 456 89` @#$ ^&* )_+ ];' /,\ >?: {}| -Ab dEf hIj lMn pQr tUv xYz 123 567 9`! #$% &*( _+[ ;'. ,\<>?: {}|=
+	  DeFgHiJkLmNoPqRsTuVwXyZ0123456789 !@ $% &* )_ [] '. ,\ >? "{ |=- bCd fGh jKl nOp rSt vWx z01 345 789 !@# %^& ()_ []; ./, <>? "{} =-a cDe gHi kLm oPq sTu wXy 012 456 89` @#$ ^&* )_+ ];' /,\ >?: {}| -Ab dEf hIj lMn pQr tUv xYz01 3456
+	  !@#$%^&*()_+[];'./,\<>?:"{}|=-aBc eF Hi kL No qR Tu wX Z0 23 567 9`! #$% &*( _+[ ;'. ,\< ?:" }|= AbC EfG IjK MnO QrS UvW Yz0 234 678 `!@ $%^ *() +[] './ \<> :"{ |=- BcD FgH JkL NoP RsT VwX Z01 345 789 !@# %^& ()_ []; ./, <>? "{}|=- bCd
+	  IjKlMnOpQrStUvWxYz0123456789`!@#$ ^&*( _+[] './, <>?: {}|= aBcD FgHi kLmN PqRsTu wXy
+	  3456789`!@#$%^&*()_+[];'./,\<>? "{}| -AbC EfGh jKlM OpQr tUvW Yz01 3456 89`! #$%^&* )_+
+	  ./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRs uVwX Z01234 678
+	  #$%^&*()_+[];'./,\<>?:"{}|=-AbCdE GhIj lMnOp rSt
+	  Yz0123456789`!@#$%^&*()_+[];'./,\ >?: {}|=-a cDe
+	  JkLmNoPqRsTuVwXyZ0123456789`!@#$ ^&*( _+[];' /,\
+	  "{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0 234 6789` @#$
+	  ()_+[];'./,\<>?:"{}|=-aBcDeFgHiJ LmNo qRsTu wXy
+	  3456789`!@#$%^&*()_+[];'./,\<>?: {}|= AbCdE GhI
+	  nOpQrStUvWxYz0123456789`!@#$%^&* )_+ ];'./ \<>
+	"{}
+	
+	BcDeFgHiJk mNoPqRs uV Xy 0
+	  567 9 !@#$% &*() +[ ;'./,\<>?:"{ |
+	    EfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'
+	,\<
+	
+	{}|=-aBc eFgHiJkLmNoP
+	  uVw yZ012 4 678
+	  
+	  &*( _+[]; . ,\<>? "{ |
+	    EfG IjKlM O Qr tUvWxY 0 23
+	    `!@ $%^&*()_ [ ;'./,\<>?:" }
+	  	cD Fg i kLmNoP R
+	        234567 9 !@#$%^
+	        ./,\< ? "{
+	      E GhIj lM Op r tUvWxYz 1
+	        @#$%^& ( _+
+	      <
+	    |=
+	  
+	    JkLmNoPqRsTuVwXyZ01
+	  
+	    @#$%^& ()_+[ ; ./,\<>?
+	  |
+	  
+	  GhI K MnO QrSt vWxY 012 4 6
+	    #$% &*()_+[] ' /,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRs
+	    Z01 3456 8 `!@
+	    ()_ [ ;'. ,\<> :"{}|=-AbCd fGh j
+	      StUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>
+	    =-aBcDeFgHiJ LmNoP RsTu wXyZ0123456789`! #$%
+	  )  
+	  ./,\<> :"{}|=
+	b
+dEfGh
