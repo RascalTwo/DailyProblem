d6bf9832c2f1f38b9b4f2d5977d57d8da7a88fa4
Rascal_Two <therealrascaltwo@gmail.com>
2022-12-03 06:05:44 +0000

    Add Codewars 56684677dc75e3de2500002b JavaScript solution

---
+++ problems/Codewars/56684677dc75e3de2500002b/README.md
@@ -0,0 +1,32 @@
+[_metadata_:generated]: - "true"
+
+# Comfortable words
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`56684677dc75e3de2500002b`](https://www.codewars.com/kata/56684677dc75e3de2500002b) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+A comfortable word is a word which you can type always alternating the hand you type with (assuming you type using a QWERTY keyboard and use fingers as shown in the image below).
+
+That being said, complete the function which receives a word and returns `true` if it's a comfortable word and `false` otherwise.
+
+The word will always be a string consisting of only ascii letters from `a` to `z`.
+
+![](https://i.imgur.com/1I7Uuc5.jpg)
+
+To avoid problems with image availability, here's the lists of letters for each hand:
+
+* Left: `q, w, e, r, t, a, s, d, f, g, z, x, c, v, b`
+* Right: `y, u, i, o, p, h, j, k, l, n, m`
+
+
+## Examples
+
+```
+"yams"  -->  true
+"test"  -->  false
+```
---
+++ problems/Codewars/56684677dc75e3de2500002b/solve.js
@@ -0,0 +1,66 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$ ^&*()_+[];'./,\ > :"{} =- B
+  FgHiJ LmNoP R T
+    Z01 3456789 !@ $% &* )_ [] '. ,\ >? "{ |= Ab dE Gh jKlMnOpQrSt vWxY
+    456 89`!@#$ ^& () +[ ;' /, <> :" }| -a cDeFgHiJkLm oPq
+  uV
+  
+  234 6789`!@# % &*()_+[];'./,\<>?:"{}|=
+  CdE GhIjKl nOpQ St vWxYz0123456789
+    %^ *()_+[];'./,\<>?:"{}|=-aBcDe gHiJkL NoPqRs
+    yZ012345 7 9`!@#$%^&*(
+  [
+  ./,\<> :"{}|
+-A
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ012
+	
+	78 `!@#$%^& ()_+[];'.
+	\<>?: {}|=- b dEfG Ij lMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|= aBcD
+	gHiJk mNoPqRsTuV X Z0 23 5
+	  `!@#$ ^&*()_+[ ; ./,\<>?:"{}|=-AbCdEfGhIjKlMnO
+	  tUv xYz012345678 ` @#$
+	  ()_ [];'./,\<>?: { |=-aBcDeFgHiJkLmNoPqRsTu w yZ 1 34
+	  9`! #$%^ * ) +[ ; . ,\<>?:"{}|=-A CdEf h
+	    OpQrStUvWxYz 12 456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBc e gHiJkLmNoPqRsTuVwX
+	  2
+	  
+	  !@#$%^ *()_+[];'./,\
+	?:
+	
+	=- bCdEf
+	IjKlMnOpQrStUv xYz0123 56 89 !
+	  
+	  )_+ ]
+	    <>?:"{}|=-aBcDeFgHiJkLmN
+	  s uVwXy 0123456 8
+	    %^&*()_+[];'./, < ?:"{}|=-AbCdEfGhI
+	  n  
+	  
+	  Yz0123456789`!@ $%^&*() +[ ;' /
+	    "{} =-aBc e gHiJkLmN PqRsTuV XyZ01234 6789`!@#$
+	    _+[ ;'./, <> :"{}|= A
+	      jKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+ ];'./,
+	    {
+	  aBc
+	  
+	  LmNoPqRsTuVwXyZ01 3456789 !@ $% &
+	    ];' /,\<>?:"{ | -AbCdEfGhIj lMnOpQrS UvWxYz0 23456789 !@#$%^&
+	    ];' /,\<> :" }|=-aBcDeF H
+	      qRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./, <>?:"{}
+	    d
+	  IjK
+	  
+	  tUvWxYz012 456789` @# %^ *
+	    ;'. , < ?:"
+	    BcD FgHiJ
+	    qRsTu wXyZ0 2
+	      !@#$ ^ *()_+[];'./,\
+	      =-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`! #$%^&*()_+[];
+	    >
+	  }|=
+	BcD
+FgHiJ
