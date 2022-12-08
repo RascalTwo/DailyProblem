8377ab18f41585a8fcf439f54c0a670f07699882
Rascal_Two <therealrascaltwo@gmail.com>
2022-12-08 19:03:46 +0000

    Add Codewars 56f1c6034d0c330e4a001059 JavaScript solution

---
+++ problems/Codewars/56f1c6034d0c330e4a001059/README.md
@@ -0,0 +1,28 @@
+[_metadata_:generated]: - "true"
+
+# HTML dynamic color string generation
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`56f1c6034d0c330e4a001059`](https://www.codewars.com/kata/56f1c6034d0c330e4a001059) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Generate a valid randomly generated hexadecimal color string. Assume all of them always have 6 digits.
+
+Valid Output
+---
+    #ffffff
+    #FfFfFf
+    #25a403
+    #000001
+
+Non-Valid Output
+---
+    #fff
+    #aaa
+    #zzzzz
+    cafebabe
+    #a567567676576756A7
---
+++ problems/Codewars/56f1c6034d0c330e4a001059/solve.js
@@ -0,0 +1,58 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@ $%^&*()_+[];' / \<>?:"{}|= a
+  eFgHiJ LmN P RsTuVwXyZ012 456789` @ $% &* )_ [];'./,\<>?:"{}|=-AbCdEf h jKlMnOpQrStUvWxYz012345678 `!@#$%^&*()_+[
+;'
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ012
+	
+	789`!@#$%^&*()_+[];'./,\<>?:"{}| -AbCdEfGhI K
+	  pQr tU Wx z0123456789`!@
+	  &*()_+[];' /,\<>? "{}|=-aBcD F
+	    mNoPqR TuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}| -Ab dEfG IjKlMnO QrStU WxYz 123 56789` @#$%
+	  )_+
+	  ./,\<>?:"{ |=-aBc eFgHiJkLmN P
+	    wXyZ01 3456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbC Ef hIjKlMn pQrS UvW Yz0123 567 9 !@#$%^&*(
+	  ];'
+	  <>?:"{}|= aBcDeF HiJkLmNoPq s
+	    Z01234 6789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHi kLmNo qRsTuVw yZ012 456 89`!@ $%^&*()_+
+	  ./,
+	  :"{}|=-AbCdEf h jKl
+	  QrS Uv x z 1 34 6 8 `!@ $ ^ *()_ [
+	    \<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXy
+	  3
+	  89`!@#$% &* ) +[] './,\< ?:"{}|=-Ab d
+	    KlM Op rS Uv xYz01234
+	    !@#$%^&*()_+[];'./,\<>?:"{}
+	    DeFgHiJ L NoP
+	    wXy 01 3 5 7 9` @ $ ^& ( _ [];' /
+	      }|=-AbCdEfGhIjKlMnOpQrSt v
+	        678 `! #$%^ *()_+[];'
+	        }|=-aBcD F HiJ
+	        uVw yZ 1 3 5 78 `!@ $ ^& ( _+ ];' / \ > :"{ | - b dEfG I K M Op rSt v xYz 1 3456 8
+	          _+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ012345 789`!@#$%^&*()_+[ ;'./,\< ?:" }|=-A CdEfG IjK MnOpQrStUvWxYz
+	        `
+	        _+[];' /,\<>?:"{
+	      eFgHiJ
+	    q
+	    XyZ012 456789`!
+	  ^&*
+	  [];'./ \<>?: {}|=- bC E GhI KlMnOp rStUvWxYz0 2
+	    9`! #$ ^& () +[]; ./,\<>?:
+	    aBcDeFg i kLm
+	    TuV Xy 0 2 4 67 9`! # %^&*()_+[];'./,\<>?:" } =- bCd f h j lMn p r t vWxY 0 2 4 67 9`! # %^& ( _+[] '
+	      "{}|=-aBcDeFgHiJkLmNoPqR T
+	        456 89 !@#$% &*()_+[];
+	        {}|=-AbC E GhI
+	        tUv xY 0 2 4 67 9`!@ $ ^& ( _+ ];'. , < ? "{}| - B D FgHiJ L N P Rs uVwX Z 123 5 789` @
+	          ;'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789 !@#$%^&*()_+[];'. ,\<>?:" }|= aBcDe gHiJk mNo qRsTuVwXyZ0123
+	        $
+	        ;'./,\ >?:"{}|=-
+	      IjKlMn
+	    U
+	    123456 89`!@#$%
+	  )_+
+	;'.
+,\<>?
