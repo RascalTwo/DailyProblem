0d5c05c88a94d4c74c946fecb6830eb67d31ea3a
Rascal_Two <therealrascaltwo@gmail.com>
2022-12-15 20:25:52 +0000

    Add Codewars 55ea5650fe9247a2ea0000a7 JavaScript solution

---
+++ problems/Codewars/55ea5650fe9247a2ea0000a7/README.md
@@ -0,0 +1,25 @@
+[_metadata_:generated]: - "true"
+
+# Unscrambled eggs
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`55ea5650fe9247a2ea0000a7`](https://www.codewars.com/kata/55ea5650fe9247a2ea0000a7) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Unscramble the eggs.
+
+The string given to your function has had an "egg" inserted directly after each consonant.  You need to return the string before it became eggcoded.
+
+## Example
+
+```javascript
+unscrambleEggs("Beggegeggineggneggeregg"); => "Beginner"
+//             "B---eg---in---n---er---"
+```
+
+Kata is supposed to be for beginners to practice regular expressions, so commenting would be appreciated.
+
---
+++ problems/Codewars/55ea5650fe9247a2ea0000a7/solve.js
@@ -0,0 +1,53 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{}|=
+  cDeFgH JkLmNoPqRsTuVwXyZ0123 5678
+`
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ012
+	
+	789`!@#$%^&*()_+[ ;' /, <
+	  {}|=-AbCdE Gh jK M
+	pQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<> :"{}|=-aBcDeF HiJkL NoPqRsT
+	
+	yZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=- bCdEfGhIjKlMnOpQr tUvWxYz
+	
+	456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHi kLmNoPq
+	
+	VwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-Ab dEfGhIjKlMnOpQrStUvW Yz0123 56789`!@
+	
+	&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@# %^&*()_+[];'./,\ >?:"{}|=-A CdEfGhI
+	
+	nOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>? "{}|=-aBcDeFgHiJkLmNoP RsTuVwXy
+	
+	3456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrS UvWxY 0123456789`!@#$%^&*()_+[]; ./ \<>?:"{}
+	
+	BcDeFgHi kLmNoPqRsTuVwXyZ0 2
+	  789`! # %^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz012
+	  789` @ $%
+	  )_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNo q sTu w yZ01234 6
+	    # % &*()_+ ];'./,\<>?:"{}|=-AbCdEfG I KlMnOpQrStUvWx
+	    456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-a c
+	      L No qRsTu
+	    1
+	    89`! #$ ^
+	  _
+	  './,\<>?:"{}|=-AbCdE GhI KlMnOpQrStUvW
+	  123456 89`!
+	$
+	
+	()_+[];' /,\<>?:"{}|=- B
+	  gHiJkL NoPqRsTuVwXyZ01234567 9`!
+	$
+	
+	()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlM O
+	  tUvWx z 123456789`!@#$%^&*
+	  [];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwX Z 123456789`!@#$%^
+	(
+	
+	  './
+	<>?
+	
+}|=-A
