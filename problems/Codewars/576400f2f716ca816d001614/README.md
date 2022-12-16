ac66b42a5791b24df663c7b3bc8438e2887ebed5
Rascal_Two <therealrascaltwo@gmail.com>
2022-12-16 20:04:35 +0000

    Add Codewars 576400f2f716ca816d001614 JavaScript solution

---
+++ problems/Codewars/576400f2f716ca816d001614/README.md
@@ -0,0 +1,23 @@
+[_metadata_:generated]: - "true"
+
+# Reduce My Fraction
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`576400f2f716ca816d001614`](https://www.codewars.com/kata/576400f2f716ca816d001614) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Write a function which reduces fractions to their simplest form! Fractions will be presented as an array/tuple (depending on the language) of strictly positive integers, and the reduced fraction must be returned as an array/tuple:
+
+```
+input:   [numerator, denominator]
+output:  [reduced numerator, reduced denominator]
+example: [45, 120] --> [3, 8]
+```
+
+All numerators and denominators will be positive integers.
+
+Note: This is an introductory Kata for a series... coming soon!
---
+++ problems/Codewars/576400f2f716ca816d001614/solve.js
@@ -0,0 +1,68 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[] './
+  >? "{ | -a cDe gH J LmN PqR
+  VwXyZ 1234567
+    #$ ^&*( _+[];' /,
+    " }| -A
+    Gh jKlM OpQrSt vW
+    2 45 78
+  @
+$
+
+&*()_+[] './,\<>?:"{}|=-a c
+  gHiJk mNoPqRsTuVw y 0123456789`!@#$%^
+  )_+[]; ./,\<>?:"{}|=-AbC Ef hIjK M OpQrStUvWxYz0
+2
+
+
+#$%^ *( _
+	];'./ \ >?:"{} = a cDeFgHiJkLmNoPqR
+	
+	wXyZ0 2345 7 9`!@#$% &*()_+[]; ./,\<>?: {} =
+	  dEfGhIjKlMnOpQrStUvWxYz01234567 9`!@#$%^& ()_+[];'.
+	\
+	
+	"{}|=-aBcDeFgHiJkLmN Pq sT V
+	  012345678 `!@#$%^ *( _+ ]
+	    <>?:" }|=-A C E
+	      nOpQr tUvW Yz0 2345
+	      $%^&* )_+[] './ \<>?
+	      BcDe gHi kLm oPqR
+	      01234 6789` @#$ ^&*(
+	      /,\<>?: {}| -AbCdE GhIj
+	      StUv xYz 123 5678
+	    %^
+	
+	    ;'. ,\<>?: {}|=-aB DeFgHiJkL No qRsTuV X
+	      6789`!@#$%^ *()_+[];'.
+	    :
+	  =-A
+	
+	  hIjKlMnOp rStUvWx z0 23 5
+	    @#$%^ *()_+ ] '
+	      "{}|=-aBcDe gHiJkLmNoP RsTuVwX Z0123456
+	      %^&*()_+[]; ./,\<>?:" }|=-AbC EfGhIjKl
+	      UvWxYz01234 6789`!@#$% &*()_+[ ;'./,\<>
+	      aBcDeFgHiJ LmNoPqRsTu wXyZ012 456789`!
+	    *(
+	
+	    /,\ >?:"{} =-AbCdE GhIjKlMnO Qr tUvWxY 0
+	      9`!@#$%^&*( _+[];'./,\
+	    }
+	  BcD
+	
+	  kLmNoPqRsT VwXyZ01 34 67 9
+	    ^&*() +[];'./,\ > :"{ |= Ab dEfGhIjKlMnOpQrStUvWxYz0 2 45 7 9 ! #$ ^ *()
+	
+	    ,\<>? "{} = aBc eF Hi k m oPqRsT V X Z0 2 45
+	    @#$%^ *()_+[]; . ,\<> :"{ | - bCdEfG IjK Mn pQ S Uv x z 123
+	
+	    @#$ ^&*( _ [ ;' / \ >?:" }|=- B
+	      kLmNo qRsTu w yZ0123456789`!@ $%^&*( _+[];'./,\<>? "{}|=
+	      hIjKl nOpQrStU W Yz0123456789`!@#
+	      +[];'./,\<> :"{}|=-aBc
+	    J
+	  oPq
+	TuV
+XyZ01
