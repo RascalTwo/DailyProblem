e001fa7d00b1444dcf118f722dedafe715c47e63
Rascal_Two <therealrascaltwo@gmail.com>
2022-12-27 00:32:56 +0000

    Add Codewars 5ab363ff6a176b29880000dd JavaScript solution

---
+++ problems/Codewars/5ab363ff6a176b29880000dd/README.md
@@ -0,0 +1,22 @@
+[_metadata_:generated]: - "true"
+
+# Hex Hash Sum
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5ab363ff6a176b29880000dd`](https://www.codewars.com/kata/5ab363ff6a176b29880000dd) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Complete the function that accepts a valid string and returns an integer.
+
+Wait, that would be too easy! Every character of the string should be converted to the hex value of its ascii code, then the result should be the sum of the numbers in the hex strings (ignore letters).
+
+## Examples
+```
+"Yo" ==> "59 6f" ==> 5 + 9 + 6 = 20
+"Hello, World!"  ==> 91
+"Forty4Three"    ==> 113
+```
---
+++ problems/Codewars/5ab363ff6a176b29880000dd/solve.js
@@ -0,0 +1,41 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>
+  {}|=-a cDeFgHiJkLmNoPqRsTuVwX Z0123 56 89` @ $%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01 3456789`!@#$%^&*( _+ ]; ./, < ?:" }|= aBc
+e
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ012
+	
+	789`!@#$%^&*()_+[]; ./,\<>?:"{ |
+	  CdEfGhIj lMnOpQr tUvWxYz012 4
+	    !@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXy 01234
+	    !@#$%^&*()_+[];'./,\<>?:"{}|=- bCd
+	    KlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{ |=-aB
+	    iJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@ $%^&*()_+[];'./,\<>?:"{}|=-aBcD FgHiJ
+	  oPq
+	  
+	  Z012345678 `!@#$%^ *()_+[];'. ,
+	    {}| - b dEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmN
+	    uVwXyZ0 2 4 67 9 ! #$%^ *()_ [
+	      >?: {}|= A CdE
+	      nOpQrSt v x z0 2 45 789`!@#$%^&*()_+[ ;'./ \
+	        aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^ * )_+[];'./,\<
+	      -
+	      IjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+ ];'./,\<>?:"{}|=-aBcDeF
+	    m
+	  RsT
+	  
+	  23456789 !@#$%^&*()_+[
+	    \<>?:" }|=-AbCdEfGhIj
+	      StUvWx z0 2 4 6789`!@#$%^&*()_+[];'./,\<>?:
+	      cDeFgHiJk
+	      TuVwXyZ012
+	      !@#$%^&*( _+ ];'./,\<>?
+	      bCdEfG Ij l n pQr
+	      0123456789`!@ $% & ( _+ ];'
+	  <
+	:"{
+	
+-aBcD
