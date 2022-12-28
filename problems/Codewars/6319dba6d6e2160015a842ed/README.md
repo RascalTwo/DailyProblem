b5c33e8d0163b0f8ae30715c0a8db18ba25bcd2f
Rascal_Two <therealrascaltwo@gmail.com>
2022-12-28 18:21:04 +0000

    Add Codewars 6319dba6d6e2160015a842ed JavaScript solution

---
+++ problems/Codewars/6319dba6d6e2160015a842ed/README.md
@@ -0,0 +1,28 @@
+[_metadata_:generated]: - "true"
+
+# Count the photos!
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`6319dba6d6e2160015a842ed`](https://www.codewars.com/kata/6319dba6d6e2160015a842ed) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+In a string we describe a road. There are cars that move to the right and we denote them with ">" and cars that move to the left and we denote them with "<". There are also cameras that are indicated by: " . ". 
+<br>
+A camera takes a photo of a car if it moves to the direction of the camera.
+
+<b style="font-size: 20px;">Task</b> <br>
+Your task is to write a function such that, for the input string that represents a road as described, returns the total number of photos that were taken by the cameras. The complexity should be strictly O(N) in order to pass all the tests. 
+
+<br>
+<b>Examples</b>
+
+```javascript
+For ">>." -> 2 photos were taken
+For ".>>" -> 0 photos were taken
+For ">.<." -> 3 photos were taken
+For ".><.>>.<<" -> 11 photos were taken
+```
---
+++ problems/Codewars/6319dba6d6e2160015a842ed/solve.js
@@ -0,0 +1,168 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{
+  -aB DeFgHi k mN
+  RsT VwXyZ0 2 45
+  9`! #$%^&*( _ []
+  /,\ >?:"{} =-Ab dE GhIjKl
+    rS UvWxY 012 4567 9`!@#$%^&
+    [] './,\ >?: {}|= a
+      iJkLmN Pq sTuVwXyZ
+      789`!@#$%
+    _
+    ./ \<>?: {}| -AbC EfGhIj lM OpQrStU
+  Y
+  234567 9`!@#$%
+&
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdE G Ij
+	
+	OpQrS UvWxYz012 4 6789` @#$%^& (
+	    './,\< ?:"{}|=-aBcDeFgHiJkLmNoP R TuVw y 0123 5 789`!
+	$
+	
+	()_+[ ;'./,\<>?: { |=-AbCdEf
+	  
+	  OpQrS UvWxY 0 23456 89`! #$%^&
+	  +[];' /,\< ? "{}
+	
+	  DeFgHiJ L N Pq s u wXyZ0 23456
+	    #$%^&*()_+[];'./,\<>?:"{}|=- bCdEf
+	  K
+	  pQrStU WxYz0123456789
+	
+	$
+	
+	()_+[ ;'./,\<>?:"{}|=-aBc e gHiJkLmNo
+	
+	    Xy 0123 567 9`!@#$% &* )_+ ];'./,\ >?
+	    -AbCd fGhIjKlMnOpQrStU W Yz0
+	    789`!@# % & () + ] './,\<>?:"{} =-aBc
+	        NoPqRsTuVw yZ 12345
+	            )_+[];'./,\<>?:"{}|=-AbCd
+	        O
+	    v
+	
+	    45 789` @#$%^&* )_+[];'./ \<> :"{}|= aB DeFgHiJ Lm oPq sTuVw yZ
+	    6789` @#$%^&*()_+[] ' /,\ >?:"{}|=-AbCdEfGhIjKlMnOpQr
+	  
+	    23 5678 `!@#$%^ *()_+[];' /,\ >?:"{} =- BcDeFgH Jk mNo qRsT Vw
+	    34567 9`!@#$%^&*() + ];' /,\<>?:"{}|=-AbCdEfGhIjKlMn
+	
+	    WxY 01234567 9 !@#$%^&*()_+[];'./,\<>?:
+	    aBc eFgHi k mN
+	    uVw yZ0 2 45
+	
+	    $% &*()_+[ ;' /,\ >?:"{ |=
+	    fGhIjKl n p rS U W Yz0123456789`!@#$%^&*()_ [];'.
+	        |=- B DeFgHiJkLmNoPqRsTuVw
+	        789`!@# % & ()_+[] ' / \<>? "{}|=
+	            nOpQrStUvWxYz012 4 6789`!@#$
+	        ;
+	        {}|=-aBcDeF
+	        qRsTu w yZ01
+	    8
+	
+	    &*()_ [ ;'
+	    ?:" } =-
+	    GhIjKlMn p rS
+	
+	    12 456789` @# %^& ()_+ ];
+	    >?:"{}| - B De g i kLmNoPqRsTuVwXyZ01234567 9`!@#
+	        ];' / \<>?:"{}|=-AbCdEfGhI
+	        tUvWxYz 1 3 56789` @ $% &*() +[];'
+	            aBcDeFgHiJkLmNo q sTuVwXyZ0
+	        !
+	        +[];'./,\<>
+	        CdEfG I KlM O Qr
+	    Y
+	
+	    78 `!@ $%^ *()_+[];' /,\<>?:" }|
+	    eFgHiJk m o qRsTuV X Z 123456789`!@#$%^&*()_ [];'.
+	        |=-AbCdEfGhIjKl n pQrStUvWx
+	    4
+	
+	    #$% &*()_+ ] '.
+	    :"{}|=- B D Fg i k mNoPqRsTuVwXyZ012345 789`!
+	        +[];'./,\< ?: {}|=-
+	            OpQrSt vW Yz0123456789`!@#$
+	        ;'./, <>?:"{}|=- Bc eFgHi
+	            XyZ012 45 789`!@#$%^&*()_+
+	        ?
+	    -
+	  
+	    KlMnOp rStUvWx
+	0
+	
+	
+	789`!@#$%^&*()_ [];'./, <>?:"{}|=- B
+	  
+	  
+	  oPqRsTuVwX Z01234 6789 !@#$%^&*() +
+	    ,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[] './,
+	  :"{
+	                                    
+	  @#$%^&*()_ [];'./ \<>? "{}|=-AbCd f
+	    MnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ01234 6789
+	  $%^
+	
+	  [];'./,\<> :"{}|= AbCdE GhIjKlMnOp r
+	    Yz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsT VwXyZ
+	  456
+	  
+	  %^&*()_+[] './,\< ?:"{ |=-AbCdEfG I
+	    pQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJk mNoP
+	  uVw
+	
+	  3456789`!@ $%^&*( _+[]; ./,\<>?:"{ |
+	    EfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsT VwXyZ
+	  456
+	  
+	  %^&*()_+[] './,\< ?:"{} =-AbCdEfGh j
+	    QrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ012345 789`!
+	  ^&*
+	  
+	  ./,\<>?:"{ |=-AbC EfGhI KlMnOpQrSt v
+	    23456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfG IjKlM
+	  rSt
+	
+	  0123456789 !@#$%^ *()_+[ ;'./,\<>?: {
+	    cDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-a cDeFgH
+	  mNo
+	  
+	  XyZ0123456 89`!@# %^&*() +[];'./,\< ?
+	    -AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQr tUvWxY
+	  345
+	
+	  @#$%^&*()_ [];'./ \<> :"{}|=-aBc e
+	    LmNoPqRsTuVwXyZ0123456789`!@#$%^&* )_+
+	  ./,
+	  
+	  |=-AbCdEfG IjKlMn pQr tUvWxYz012 4
+	    !@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDe gHi
+	  NoP
+	sTu
+	
+	Z0123456789`!@#$ ^&*()_+ ];'./,\<>? "
+	  
+	  dEfGh jKlMn p rSt vWx z012 4567 9`!@# %^&*( _+[]; ./,\< ?:"{} =-aBcD 
+	                 yZ0123 56789` @#$%^&* )_+[];'. ,\<>?:"{ |=-AbCdE GhIjKlMnOp
+	  
+	  Yz0123456789`!@#$%
+	    +[] './, < ?:"{}|=-aBcDeF
+	    mNo qRsTuVwX Z 123456789`!@#$%^&*()_+[];'
+	    ?:"{}|=-Ab dEfGhI K M OpQrStUvW Yz01234567 9
+	      *()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkL NoPqRsTuVw
+	    345
+	  `!@
+	  
+	()_
+	
+	
+	
+	
+<>?:"
