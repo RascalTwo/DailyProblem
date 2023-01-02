8eaf1df804fc39544580f7945379b5ce9101b484
Rascal_Two <therealrascaltwo@gmail.com>
2023-01-02 00:26:54 +0000

    Add Codewars 587e18b97a25e865530000d8 JavaScript solution

---
+++ problems/Codewars/587e18b97a25e865530000d8/README.md
@@ -0,0 +1,20 @@
+[_metadata_:generated]: - "true"
+
+# Number of anagrams in an array of words
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`587e18b97a25e865530000d8`](https://www.codewars.com/kata/587e18b97a25e865530000d8) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+An anagram is a word, a phrase, or a sentence formed from another by rearranging its letters. An example of this is "angel", which is an anagram of "glean".
+
+Write a function that receives an array of words, and returns the total number of distinct pairs of anagramic words inside it.
+
+Some examples:
+
+- There are 2 anagrams in the array `["dell", "ledl", "abc", "cba"]`
+- There are 7 anagrams in the array `["dell", "ledl", "abc", "cba", "bca", "bac"]`
---
+++ problems/Codewars/587e18b97a25e865530000d8/solve.js
@@ -0,0 +1,69 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<> :"{}|=-aBcDe g
+  kLmNo qRsTuVwXyZ0 2 456 89`!@#
+  &*( _+[];' /,\< ?: {}|=-AbCdEfG
+    MnOpQ StU W Yz0123456789`!@#$%^&*()_+[];'./,\<>?:"
+    aBcDeFgHiJkLmNoPqRsT VwXyZ0123456789`!@#$% &* )_ [ ;'.
+  <
+  "{}|=- bCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$ ^&*()_+[] '. ,\<>? " }|=-aBcDe g iJkLmNoPq s uV X Z01 345
+7
+
+
+#$%^ *( _
+	];'./,\<>?:"{} =-aBcDeFgHiJ
+	  oPqRsTuVw yZ0123456789
+	    ^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrSt vWxYz0123 567 9`! #$%^&*()_ [ ;'./,\<>?
+	    -aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@# %^&*()_ [];'./ \<>?:"{ | -A CdEfG IjKlMn pQ StUv 
+	    3456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBc eFgHiJk mNoPqRs uVwXyZ0 2 45 789`! #$%^&* )_ [];' 
+	    :"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01234567 9`!@#$% &*()_+ ];'./, <>?:"{ |=-aBc eFgHiJk m oPq sTuVw yZ0123 56 89`!@ 
+	  *()
+	
+	  ./,\<>?:"{ |=-AbCdEfGhI
+	   
+	   tUvWxYz0 23456789`!@#$%^&*(
+	      /,\ >?:"{}|=-aB D FgHiJkLmNoPqRsTuVwXyZ01 3456789`!@#$%^&*()_+[];'./,\<>?:
+	      CdE GhIjKlMnOpQrStUv x z0
+	
+	      !@#$%^&*()_+[];'./,\<>?:"{ |=-aBcDeFgHi
+	        TuVwXyZ 1 3 56789`!@#$% & ( _+[];'./,\<>?:"{}|= AbCdE
+	          rStUvWx z01 3456789`!@#$%^&*
+	            ?:"{}|=-aBcDeFgHiJ
+	          w
+	        7
+	      ^&
+	      './,\< ?:"{}|=-AbCdEfGh
+	    O
+	
+	    xYz01234 6789`!@#$% &* ) +[];'. ,\<>?:"{}|=-a c eF H J L No q s u w y
+	   
+	   9`!@#$%^ *()_+[];'. ,
+	      |= AbCdEfGhIjKlMn p rSt vWxYz0 23456789`!@#$%^&*()_+[];'./,\<> :"{}
+	      FgHiJk mNoPqRsTuVwXyZ0123456789`!@#$%^ *()_+
+	    ,
+	  
+	    -AbCdEfG IjKlMnOpQrStUv x z01234 6789`!@#$%^&*()_+[];' /, <>?:"{}|=-aBcDeFgHiJk
+	    
+	    XyZ01 3456789`!@#$
+	    _+[ ; ./, <>?: {}|=-AbCdEfGhIj l
+	      UvW Yz0123
+	      @#$%^ *()_+[]; . ,\<>?:"{}|=-a
+	      JkL N PqR TuVw yZ0123456789`!@ $
+	        ;'./, <>?: { |=-AbCdEfGhIjKlMnOpQrStU
+	        56789`!@#$%^&*
+	        ,\ >?:"{}|=-aBcDe g iJkL
+	          yZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEf
+	      O
+	     
+	      456789`!@#$%^&*()_+[];'
+	      
+	      cDeFg iJkLmNoP R TuVwXyZ01
+	      `!@#$ ^&*()_ [ ;'./,\<>?:"{}|=-AbCd
+	      MnOpQrStUvW Yz01 3456789`!@#$%^&*()_+[];'.
+	        |=-aBcDeFgHiJkLmNoPqRsTuVwXyZ012345
+	      $%^
+	    [
+	  /,\
+	?:"
+	
+=-AbC
