d21bd5f90bd897b3e80d8f195895e3060dd60134
Rascal_Two <therealrascaltwo@gmail.com>
2023-01-08 20:08:32 +0000

    Add Codewars 63387232198a4c00286aa349 JavaScript solution

---
+++ problems/Codewars/63387232198a4c00286aa349/README.md
@@ -0,0 +1,15 @@
+[_metadata_:generated]: - "true"
+
+# Help Grinch steal Christmas from JS programmers
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`63387232198a4c00286aa349`](https://www.codewars.com/kata/63387232198a4c00286aa349) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Grinch wants to spoil Christmas for JavaScript programmers. For that, he devised a hellish plan: pollute their `Date` prototype so that Christmas never happens. He wants to change method `getDate` of `Date` so that it returns `26` for `Date` objects that represent 25th of December but works correctly for all other days of the year.
+ 
+However, Grinch does not know how to do that. Can you help him?
---
+++ problems/Codewars/63387232198a4c00286aa349/solve.js
@@ -0,0 +1,64 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$ ^&*()_+[]; . ,\<>?:"{}|=-aBcDeFgHiJk
+mNoPqRsTuVwXyZ01234567 9 !@#$%^&* )_+[];'./,\<>?: {
+  -AbCd fGhIjKl n pQrStUvWxYz0123456789`
+  $%^&*( _+[];'./,\<>?:" }|= aB De gHiJkLm oPq sT V Xy 0 23456789
+!
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdE G Ij
+	
+	OpQrStUvWxYz012 45678 `! #$%^&*( _+ ]; .
+	  >?:"{}|=- Bc eF HiJk mN Pq s
+	    Z01 3 5 789 !@#$%^&*()_+[];'./,
+	    {}|=-AbCdEfGhIjKlMnOpQrStU WxY
+	    56789`!@#$%^&*()_+[];'./, <>?:
+	  =-a
+	  
+	  JkLmNoPqR TuVw yZ012 45 78 `
+	    &*( _ [ ;'. ,\<>?:"{}|=-AbCdEfG
+	    nOpQrStUvWxYz0123456789`!@ $%^&
+	    ];'./,\<>?:"{}|=-aBcDeFgH JkLm
+	  RsT
+	  
+	  23456789` @#$ ^&* )_+[];'./ \<>?:" }| -A C
+	    jKlMn p r tUv xYz0123456789`!@#$%
+	    +[];'./,\<>?:"{}|=-aBcDeFg iJkL
+	    sTuVwXyZ0123456789`!@#$%^ *() +[];'. ,\<>?:"{}|=-A
+	  fGh
+	  
+	  QrStUvWxYz 1234567 9` @# %
+	    +[];' /,\<>?:" } =-a cDeFgHiJkLmNoPqRsTuVw
+	    345 789` @ $ ^& ( _ []; ./,\ >
+	      AbCdE GhIjKlMnOpQ S UvWxYz0123456789 !@ $%^&*
+	      
+	      "{} =-aBcD FgHiJ
+	      sT VwXyZ01234567 9
+	        )_+[] ' /,\
+	        AbCd f hIj
+	      S UvWx z
+	        `!@#$ ^ *()_+[];'. ,\<>
+	        cDeF H JkLmNoPqRs uVwX
+	      6
+	      %^&*( _+[] ' /,\<>?:"{}|=- bCdEfGhIjK
+	      
+	      12345 7 9 !@# %^&*()_+[] './,\< ?:"{} =- Bc eFg
+	      Pq sTuVwX Z01 34 67 9`!@ $%^ *() +
+	        ?:"{}|=-AbCdEfGhIjKlMnOpQr tUvWxYz
+	        `!@#$%^&*()_+[];'./,\<>?: {}| -aBcDe gHiJkLmNoPqRs
+	      1 3456 8
+	        ()_+[];'./,\<>?:"{}|=-AbCd fGhIjKl
+	        WxYz0123456789`!@#$%^&*() +[];'.
+	      {
+	    c
+	  HiJ
+	mNo
+	
+	TuVwXyZ0 23456789`!@#$ ^&* )
+	  ;'./,\ >?:" } =-AbCdEfGhIjKlMnOpQrStUv x z01 3 56789`!
+	$
+^&*()
