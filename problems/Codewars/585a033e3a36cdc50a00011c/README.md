fa82f3780424e51147c885993d3bd69b899ac96e
Rascal_Two <therealrascaltwo@gmail.com>
2023-01-09 19:46:36 +0000

    Add Codewars 585a033e3a36cdc50a00011c JavaScript solution

---
+++ problems/Codewars/585a033e3a36cdc50a00011c/README.md
@@ -0,0 +1,23 @@
+[_metadata_:generated]: - "true"
+
+# Frequency sequence
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`585a033e3a36cdc50a00011c`](https://www.codewars.com/kata/585a033e3a36cdc50a00011c) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Your task is to return an output string that translates an input string `s` by replacing each character in `s` with a number representing the number of times that character occurs in `s` and separating each number with the `sep` character(s).
+
+**Example (s, sep --> Output)**
+```
+"hello world", "-" --> "1-1-3-3-2-1-1-2-1-3-1"
+"19999999"   , ":" --> "1:7:7:7:7:7:7:7"
+"^^^**$"     , "x" --> "3x3x3x2x2x1"
+```
+
+
+
---
+++ problems/Codewars/585a033e3a36cdc50a00011c/solve.js
@@ -0,0 +1,54 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\ >?:" }
+  aBcDe gHiJkL N PqR TuVwXy
+  234 6789`! #$%^ *( _+[] './,\<>?:"{}|=-A CdEfGhIjKlMnOpQrS Uv xY 0 234
+  89`!@# %^&*()_+[];'./,\< ?: {}|=-aBcDeFgHiJkLmNoPqRsTuVw
+y
+
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz01234 6789`!@#$% &*()_+[];' /
+	  ?:"{}|=-a cDeFgHi kLmNoPqRsT V
+	    23456789`!@#$%^&*()_+[];'./,\<>?: {}|=-Ab dEfGh jKlMnOpQrStUvWxYz01234567
+	    $%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJk    PqRsT VwXyZ0123456789`!@#
+	    )_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnO      vWxYz 123456789`!@#$%
+	  )_+
+	
+	  ,\<>?:"{}| -aBcDeF HiJkLmNoPq s
+	    
+	    56789 !@#$%^& ( _+ ]; ./,\<>?:"{}|=-AbCdEfGhIj l nOpQ S Uv x z01 3 567
+	
+	    ^&*() +[];'./, < ?:"{}| -aBcD Fg i
+	      RsT VwXy 0 23456789`!@#$%^&*()_+[];'./,
+	      |=-Ab dEfGhIjKlMnOpQrStUvWx z012 4 6789`!@#$%^&*()_+[];'./,\<>?
+	      BcDeFg iJkLm
+	    T
+	
+	    23456 89`!@# % &* )_ [
+	      >?: {}|=- bCdE G IjK MnOp r tUv xYz0 234 6 89`
+	      ()_ []; . ,\<>?:"{}|=-aBcDeFgHiJkL N Pq s u w yZ0 2 45
+	      $%^&* )_+[];' /
+	        =-Ab d fGhIjKlMnOpQrStUv xYz012
+	        #$%^&*()_+[];'./
+	        =-a c eFgHiJkLmNoPqRsTuVwXyZ01 3 56 8 ` @ $%^ * )_
+	        >?:"{}|=-AbCdEfG IjKlMn
+	      W
+	      56789` @#$%^
+	    []
+	    <>?:"{} = a cD F H JkL NoPq s
+	      123 56789`!@ $ ^&*()_+[]
+	      ?:" }|=-A C EfGhIjKlMnOpQrSt vW Yz0123456789`!@#$%^&*()_+[];'./,
+	      |=- BcDeFg i kLmNoPqRsTuVwXyZ 12 456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbC
+	      lMnOpQ S UvWxYz0123456789`!@#$%^&*()
+	      ,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTu wXyZ0 2345678 `!@#$ ^&*()_+ ];'./,\<>?:"{}|=-AbCdEfGhIj
+	    Q
+	  vWx
+	012
+	
+6789`
