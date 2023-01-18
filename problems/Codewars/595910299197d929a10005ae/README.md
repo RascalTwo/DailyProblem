4b908cd9a679357d480b2880cacc6861153d1d68
Rascal_Two <therealrascaltwo@gmail.com>
2023-01-18 14:51:07 +0000

    Add Codewars 595910299197d929a10005ae JavaScript solution

---
+++ problems/Codewars/595910299197d929a10005ae/README.md
@@ -0,0 +1,22 @@
+[_metadata_:generated]: - "true"
+
+# Free pizza
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`595910299197d929a10005ae`](https://www.codewars.com/kata/595910299197d929a10005ae) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+In an attempt to boost sales, the manager of the pizzeria you work at has devised a pizza rewards system: if you already made at least 5 orders of at least 20 dollars, you get a free 12 inch pizza with 3 toppings of your choice.
+
+However, the rewards system may change in the future. Your manager wants you to implement a function that, given a dictionary of customers, a minimum number of orders and a minimum order value, returns a <strong>set</strong> of the customers who are eligible for a reward.
+
+Customers in the dictionary are represented as:
+```python
+{ 'customerName' : [list_of_order_values_as_integers] }
+```
+
+See example test case for more details.
---
+++ problems/Codewars/595910299197d929a10005ae/solve.js
@@ -0,0 +1,54 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{}|=-a cDeFgHiJkL NoPqRsTuV X
+  123456 89`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCd fGhIjKlM Op rStUvWxYz0123456789 !@ $%^&* )_ [];'./,\<>?:"{}| -a cDeFgHiJkLmNoPqRsTuVwXyZ012 45 789`!@#$%
+&
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|=- BcDeFgH JkLmNoPqRsT
+	  yZ0 23456789` @ $
+	  ()_ [];'./,\ > :"
+	  -Ab dEfGhIjKl n p
+	    WxYz0 23456 89`! #$% &*( _+[ ;'. ,\< ?:" }|=- Bc eFgH JkL NoPqR TuVwXy 01234 6789 !@ $% &*()_
+	    /,\<> :"{}| -Ab dEf hIj lMn pQr tUv xYz 123 567 9`! #$ ^&* )_+ ];'./, <>?:" }|=- BcDeF HiJkL NoPq sTuVwX
+	  2
+	  789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjK MnOpQrStUv xYz0123456 89`!@# %^&*()
+	  ;'. ,\<>?:"{} = a
+	  FgH JkLmNoPq s uV
+	  012 456789`!@ $ ^
+	    [];'. ,\<>?:" }|=- bCd fGh jKlM Op rSt vWx z01234 6789` @#$% &*()_ [];'. ,\<> :"{}|=
+	    FgHiJkLmN PqRsTuV XyZ0 234 67 9`!@ $%^ *() +[];' /,\<> :"{} =- bC EfGhI
+	  n
+	  StUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{ |=-aBcDeFg iJkLmNoPqR TuVwXy 01234567
+	`!@
+	
+	
+	()_+[ ;'./,\<>?: { |
+	AbCdEfGh jKlMnOp rStUvWxYz 123456789` @#$%^&*()_ [];'./,\ >?:"{}|=-a cDeFgHiJk mNoPqRsTu wXyZ012345
+	89`!@#$ ^&*()_+[];' /,\<>?:"{}| -AbCdEfGhIjK MnOpQrSt vWxYz01234 6789`!@# %^&*()_+[] './,\<>?:"{ |=-aBcDe
+	
+	JkLmN PqRsTuVwX Z 1
+	456789`! #$%^&*()_+ ];'./,\<>?: {}|=-AbC EfGhIjKl nOpQrStU WxYz01234 6789`!@#$ ^&*()_+[ ;'./,\<>?
+	{}|=-aBcDeF HiJkLmNoP RsTuVwXyZ0 23456789 !@#$%^&*( _+[];'./, <>?:"{}|=-A CdEfGhIjK MnOpQrStUvW Yz012345678
+	
+	#$%^&*() +[];'./,\<>? "{}|=-aBcD FgHiJkLmN P
+	  uVwXyZ 123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_
+	]
+	
+	,\<>?:"{}|=-aBcD FgHiJkL NoPqRsTuVw y
+	  3456789 !@#$ ^&*()_ [];' /
+	    "{} =-AbCdEfG I Kl
+	    StUvWxY 0123 56789` @#$% &
+	      './ \ > :"{}|=-aBcDeFgHiJkLmNoPqRsT VwXyZ0123456789`!@#$%^&*()
+	      ,\<>?:"{}|=- b dEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXy
+	    5
+	    @#$ ^&*()_+[] ' /,\<>?:"{}|=-AbCdEfG
+	    nOp rStUvWxY 0 23456789`!@#$%^&*()_+
+	    ,\<>?:"{}|=-aBcDeFgH JkLmNo qRsTuVw yZ012 4 6789`!@#$%^&*( _+[];'. ,\<>? " }|=-AbCdEfGhI
+	    pQr tUvWxYz0 2 456789`!@#$% &*()_+[];' /,\<>?:"{
+	    cDe gHiJkL N PqRsTuVwXyZ0123456789`! #$%^&*()_+ ];'./,\<>
+	    =-AbCdEfGhIjKlMnOpQrStUvWxYz012 456789`
+	  %
+	*()
++[];'
