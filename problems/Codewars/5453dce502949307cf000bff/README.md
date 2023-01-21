44d1accff435230903d65098b842afa25575936c
Rascal_Two <therealrascaltwo@gmail.com>
2023-01-21 17:08:09 +0000

    Add Codewars 5453dce502949307cf000bff JavaScript solution

---
+++ problems/Codewars/5453dce502949307cf000bff/README.md
@@ -0,0 +1,30 @@
+[_metadata_:generated]: - "true"
+
+# Find the Nexus of the Codewars Universe
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5453dce502949307cf000bff`](https://www.codewars.com/kata/5453dce502949307cf000bff) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Not to brag, but I recently became the nexus of the Codewars universe! My honor and my rank were the same number. I cried a little.
+
+Complete the method that takes a hash/object/directory/association list of users, and find the *nexus*: the user whose rank is the closest is equal to his honor. Return the rank of this user. For each user, the key is the rank and the value is the honor.
+
+If nobody has an exact rank/honor match, return the rank of the user who comes closest. If there are several users who come closest, return the one with the lowest rank (numeric value). The hash will not necessarily contain consecutive rank numbers; return the best match from the ranks provided.
+
+
+## Example
+
+```
+         rank    honor
+users = {  1  =>  93,
+          10  =>  55,
+          15  =>  30,
+          20  =>  19,    <--- nexus
+          23  =>  11,
+          30  =>   2 }
+```
---
+++ problems/Codewars/5453dce502949307cf000bff/solve.js
@@ -0,0 +1,49 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\ >
+  {}|=-a cDeFgHiJkLmNoPqRsTuVwXyZ0123456789`! #$%^&* )_+[];' /, <
+    }|=-A CdEf h jKlMnOpQrStUv x z012345
+    !@#$%^ *()_ []; ./,\<>?
+      a cDeFgHi k mNoP
+        Z 1234
+        $ ^&*()_ [];'.
+      " }|=- b dEfGhIj
+        t vWxYz0 23456
+        ^ *()_+
+  '. ,\<>?:"{}| -aBcDeFgHiJkLm
+o
+
+
+#$%^ *( _
+	];'./,\<>?:"{}|= aBcDeFg iJkLmNoPqR T
+	  yZ01234567 9`!@#$ ^&* )_+[];' /,\< ?:"{ |=-Ab dE G IjKlMnO QrStUvWxYz 1
+	    89`!@#$%^&*()_+[];'./,\<>?: {}|=-a cDeFgHiJkLmNoPq sTu wXyZ0 2345 789`! #$%^ *()_ [];'./,\<>?:"{}
+	    dEfGhIjKlMnOpQrStUvWxYz0123 56789` @#$%^&*()_+[];' /,\ >?:"{ |=-a cDeFg iJkL NoPqR TuVwXyZ0 23456789 !@#$%^&* )_+[];'./,\<>?:"
+	  -Ab
+	  GhIjKlMnOp rStUvW Yz0 2345678 `! #$%^& ( _+[]; ./,\ >?:"{ |=- BcDeFg iJkLmNoPqRs
+	    Z0123456789`!@#$%^&*()_+[]; ./,\<> :"{}|=-AbCdEfGh jKl nOpQr tUvW Yz012 4567 9`!@ $%^&*()_+[];'./
+	    "{}|=-aBcDeFgHiJkLmNoPqRsTu wXyZ01 3456789`!@#$%^& ()_ [];'. ,\<> :"{}| -AbC EfGhI KlMnOpQr tUvWxYz0 23456789 !@#$%^&*()_+[];'
+	  <>?
+	  |=-aBcDeFg iJkLmNoP Rs uVwXyZ012 456 89`! #$%^ *() +[];'. ,\<>?: {}|=-AbCdEf
+	    MnOpQrStUvWxYz0123456789`!@#$% &*()_+ ];'./, <>?:"{ |=-aBc eFgHiJkLmNo
+	    VwXyZ0123456789`!@#$%^&*()_+[] './,\< ?:"{}| -AbCdE GhIjKl nOpQrStUvWx
+	  234
+	789
+	@#$%^&*()_+[];'. ,\<>?:" }|=-aBcDeF H
+	  mNo qRsTu w yZ0123456789`!@ $
+	    _+[ ;'./ \ >?:"{}|=-AbCdEfGhIj
+	    QrStUvWxYz0123456789` @#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVw yZ0123456789`!@#$%^
+	    [];'./ \<>?:"{}|=-AbCdEfG
+	  l
+	  QrS UvWx z 1 34 6 8 `!@# %^&* )
+	    ./, <>?:" } =-a
+	    HiJ LmN P RsTuVwXyZ0123456789`!@#$%^&*()_+
+	    ,\< ?:"{ | - bC E G IjKl nOpQ S
+	      123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ
+	    6
+	    #$%^&*()_+[];'./,\<>?:"
+	    bCdEf h jKlMnOpQrStUvWxYz0123456789`!@#$%^&*() +[];'./,
+	    {}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ 123456789`!@#$
+	  (
+	+[]
+'./,\
