5001e00523031e4be4612e35038262b5cded9eca
Rascal_Two <therealrascaltwo@gmail.com>
2023-01-22 19:03:03 +0000

    Add Codewars 5831200eb812b8016d000094 JavaScript solution

---
+++ problems/Codewars/5831200eb812b8016d000094/README.md
@@ -0,0 +1,21 @@
+[_metadata_:generated]: - "true"
+
+# Smart Sum
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5831200eb812b8016d000094`](https://www.codewars.com/kata/5831200eb812b8016d000094) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Your task is to write a function, smartSum, that returns the sum of an arbitrary number of arguments. But be careful, some of your arguments can be arrays of numbers or nested arrays.
+
+
+```javascript
+smartSum(1,2,3,[4,5],6); // returns 21
+smartSum(1,2,[[3,4],5],6); // returns 21
+```
+
+
---
+++ problems/Codewars/5831200eb812b8016d000094/solve.js
@@ -0,0 +1,61 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{
+  -aBcDe gHiJkLmNoPqRsTuVw yZ01 34 678 ` @#$%^&* )_+ ];' /,\<>?:" } =-A C EfGhIjKlMnOpQrStUv xYz
+1
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz0 2345678 `! #$ ^
+	  _+[];'./,\ >?:" }|=-a cDeFgHi kL No q
+	    XyZ0123456789`!@#$%^&*()_+[];'./,\<>? "{}|=-Ab dEfGhIjKlM
+	    tUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}| -aBcDeFg iJkLmNoPq
+	    XyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKl nOpQrStU WxYz01 345678 `!@#$%^&*
+	  [];
+	/,\
+	
+	"{}|=-aBcDeFgHiJ LmNoPqR TuVwXyZ0123
+	  89`!@#$% &*()_+ ];'./,\ >?:"{}|=-AbCdEfGhIjKlM
+	    tUvWxYz 1 3 56 8 ` @#$% &*()_
+	      \<> :"{} = aBc
+	      LmN PqR T Vw
+	      56789`! # % &* ) + ];' /,\<>
+	        CdE GhIjKl n pQrStUvWxYz0123456789`!@#$%^&*
+	        ,\<>?:"{}|=-aBcDeF
+	        qRs uV XyZ0123
+	      @
+	      _+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUv xYz012 456789
+	    ^
+	  _+[
+	  
+	  >?:"{}|= aBcDeF HiJkLmNo qRsT VwXyZ012 456789`!@#$%^&*()_+[];
+	    >?:"{}| - b dE G I KlMn pQrSt
+	      234 6789 ! #$%
+	      ];' /,\ > :"
+	      DeFgHiJ L N Pq s u wXy 01234
+	        %^& ()_+[];
+	        {}|=-A Cd fGh
+	          UvWxYz 1 3456789`!@#$%^&*()_+[];'./,\<>?
+	        D FgHi k
+	          XyZ012 4 6789`!@#$%^&*()_+[];'./,\<>?:"{}|
+	        I
+	        tUvWxYz0123456789`
+	        _+[];'./,\<>?:"{}|=-aBcDe gHi kL NoPqRsTuVw
+	        789` @#$ ^& ()_+[];
+	      :
+	      CdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^& ()_+[] './,\< ?:"{}|
+	    e
+	  JkL
+	
+	  sT VwXyZ0 23456789 !@#$ ^&*
+	    ;'./,\<> :"{}|=-AbCdEfGhIjKlMnOpQrS UvWx z
+	      89` @ $%^&*()_+[];'./
+	      }|= a cDeFgHiJkLmNoPqR
+	      012345 789`!@#$%^&*()_+[];'./,\ > :"{} = AbC E GhI K MnOp
+	    W
+	z01
+34567
