a1b6d25080322846bade6daa07b49c6e7e6d0089
Rascal_Two <therealrascaltwo@gmail.com>
2023-01-28 19:46:44 +0000

    Add Codewars 5f7c38eb54307c002a2b8cc8 JavaScript solution

---
+++ problems/Codewars/5f7c38eb54307c002a2b8cc8/README.md
@@ -0,0 +1,32 @@
+[_metadata_:generated]: - "true"
+
+# Remove the parentheses
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5f7c38eb54307c002a2b8cc8`](https://www.codewars.com/kata/5f7c38eb54307c002a2b8cc8) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Remove the parentheses
+=
+In this kata you are given a string for example:
+
+```python
+"example(unwanted thing)example"
+```
+
+Your task is to remove everything inside the parentheses as well as the parentheses themselves.
+
+The example above would return:
+
+```python
+"exampleexample"
+```
+
+## Notes
+* Other than parentheses only letters and spaces can occur in the string. Don't worry about other brackets like ```"[]"``` and ```"{}"``` as these will never appear.
+* There can be multiple parentheses.
+* The parentheses can be nested.
---
+++ problems/Codewars/5f7c38eb54307c002a2b8cc8/solve.js
@@ -0,0 +1,76 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{}|=
+  cDeFg iJkLm o qRs uVwXy 0 2345678
+  @#$ ^&*( _ [ ;' / \ >?:"{}|=-AbCd fGhIj
+    pQrSt vWxY 0 23456789`
+    ^& ()_+[ ;'. ,\<> :"{}|=-aBcDeFg
+    mNoP Rs uVwXy 012 45678
+      ^&*() +[]; . ,\<>?:"{}|=-
+      hIjKl nOpQr t v x z012 4 67
+
+      ^&*()_+[];'./,\<>? "{}|=-a
+      i kL NoPqRs
+    y
+  2
+  6789`! #$%^&*()_+[];'.
+,
+
+
+#$%^ *( _
+	]; ./, <>? "{} =-aBcD FgH JkLmNoPqRsT
+	wXyZ0 2345 7 9`!@#$%^&*()_+[]
+	./,\< ?:"{}| - bCdEfGhIjKlM
+	pQ StUvWxYz0 234 6789`!@#$ ^&*( _+ ];'./,\ >?:"{}|=-a cDeFgHi kLmNoPqR TuV XyZ0 2345678 `!@
+	%^ *()_+[];'./,\<>?:"{}|=-AbCdEf h jK
+	nO QrStU WxYz 123 56 89 !@#$%^ *()_+[] './,\<>?:" }|=- BcD FgHiJkLmNo qRsTuV XyZ0 23456789` @# %^&*()_+
+	;' /,\<>?:"{ |=- bCdEfGhIj lM OpQ StU WxY 0123456789`
+	#$ ^&*() +[]; . ,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTu
+	XyZ 12345 7 9`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStU   Y
+	
+	3456789` @#$%^&*()_+
+	  ./,\<>?:"{}|=-aBcDeFgHi
+	    P R TuVwXyZ0123456789`!@#$%^&*(
+	  ]
+	  ,\<>?: {
+	=
+	
+	dEfGhIjK MnOpQrStUvWxYz012345678
+	  #$% &*()_+[];'./ \ >?
+	  |=- BcDeFgHiJkLm o qRsTuVwXyZ0123456789`!@#$ ^ *()_+[]
+	  ,\<>?:" } = Ab dEfGhIjKlMnOpQrStUvW
+	    3456789`!@#$ ^& ()_+[];'./,\<>?:"{}|=-aBcDeFgHi k mNoPqRsTuVwXyZ0
+	  5
+	  `!@#$% &*()_+[];'./,\<>?:"
+	|
+	
+	
+	
+	GhIjKlMnOpQrStUvWxYz 123456789` @
+	  &*()_+[]; ./,\<>? "{}|=-aBcD F
+	    mNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|= AbCdEfGhIjKlMnOp rStUvWxYz01234567
+	    $%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqR TuVwXyZ01 345678 `!@#$%^&*( _+[];'./  <>?:"{}|=
+	    fGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^& ()_ [];'./ \< ?:"
+	    BcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+ ];'.
+	    :"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789 !@#$%^& ()_+[];'./, <>?:"{ |=-aB DeFgHiJkLmNo qRsTuV XyZ0123  6789`!@#$%^
+	    [];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz 123456 89`!@#$ ^&*()_ [];'./ \<>?:"{}| -  cD
+	  iJk
+	  
+	  TuVwXyZ012 456789` @#$%^&*()_ [
+	    \<>?:"{ | - bC E Gh jKlM OpQrS
+	      123 56789`!@#$%^ * )_
+	      \<> :"{}|=-aBcDe g iJkLmNoPqRsTuVwXyZ0123456 8 `!@#$%^&
+	      './,\<> : { |= AbCdEfGhIjKlMnOpQrSt
+	        456789`!@#$% &* )_+[];'./,\<>?:"{}|=-aBcDeFgHiJ L NoPqRsTuVwXyZ01
+	      `
+	      ()_ [];'./, < ?:"{}|=-AbCdEfGhIjKlMnOpQ S UvWxYz0
+	      9`!@#$% & ( _+ ] ' /,\<>?:" }|=-a
+	        LmN PqRsTuVw y 0123456789`!@#$%^&*()_+[] ' /,\<>?:"{}|=-AbCdEfGh
+	         tUvWxYz01234 6 89`!@#$%^&*()_+[];'./,\<>?:"{} = aBcDeFgHiJkLmNoPqRsTuV X Z0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEf
+	      O
+	      xYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFg iJkLmNoPqRsTuVwXyZ0123
+	    `
+	  %^&
+	)_+
+	
+'./,\
