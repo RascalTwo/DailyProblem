c78d25c498c140ca7985455840916884ec1c63bc
Rascal_Two <therealrascaltwo@gmail.com>
2023-02-01 22:38:28 +0000

    Add Codewars 588f3e0dfa74475a2600002a JavaScript solution

---
+++ problems/Codewars/588f3e0dfa74475a2600002a/README.md
@@ -0,0 +1,29 @@
+[_metadata_:generated]: - "true"
+
+# 1's, 0's and wildcards
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`588f3e0dfa74475a2600002a`](https://www.codewars.com/kata/588f3e0dfa74475a2600002a) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+You are given a string containing 0's, 1's and one or more '?', where `?` is a wildcard that can be `0` or `1`.
+
+Return an array containing all the possibilities you can reach substituing the `?` for a value.
+
+
+## Examples
+```
+'101?' -> ['1010', '1011']
+
+'1?1?' -> ['1010', '1110', '1011', '1111']
+```
+
+Notes:
+* Don't worry about sorting the output.
+* Your string will never be empty.
+
+Have fun!
---
+++ problems/Codewars/588f3e0dfa74475a2600002a/solve.js
@@ -0,0 +1,79 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{ |
+  Bc eFgHiJkLmNoPqRsTuVwX Z01234 6789`!
+
+  ^&*() +[];'./, < ?:" }|=-Ab
+  fGh jKlM O Q St v x z0123456789 !@#$%
+    _+ ];'./,\ >?: {}|= aBcDeFgHi
+    oPq sTuVwX Z012 45 789`!@
+      )_+ ];'./, <>?:" }| -AbCdEfGhIjKlMnOpQrStUvWxYz012 45 7 9`!@ $ ^&*()_+[];'./,\ > :"{}|
+        HiJkLmNoPqRsTuVwXyZ0
+      8
+    $
+  *
+  +[];'. ,\<>?:"{}|=-Ab
+dE
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdE G Ij
+	
+	OpQ StUvWx z 123 56 89 !@#$%^&*()_+[];'./,
+	
+	:"{}|=-aBcDeFgH JkLmNoP RsTuVwXyZ0 2
+	  
+	  !@# %^&*() + ];' /, <> :"{}|=-AbCdEfGhIjKl
+	  
+	  UvWxYz01234 6789`!@ $% &* )
+	    ./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`! #$%^&*() +[];'./, <>?:"{}|=- bCdEfG IjK MnOpQrStUvWxYz0
+	    789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqR TuVwXyZ0 2345678 `!@#$%^ *()_+[]; ./,\<>?:"{ |=-AbC EfG IjKlMnOpQrStUvW
+	    3456789`!@#$%^&*()_+[];'./,\<>? "{}|=-aBcDeFgHiJkLmN PqRsTuV XyZ012 456789 !@#$%^ *()_+[ ;'./,\ >?:"{} =-AbCdE GhIjKlMnOp rStUvW Yz0 23456789`!@#$%^
+	  _+[
+	'./
+	
+	?:"{}|=-aBcDeFgH JkLmNoP RsTuVwXyZ0 2
+	  
+	  !@# %^&*() + ];' /, <> :"{}|=-AbCdEfGhIjKl
+	
+	  StUvWxYz 123456789`!@#$%^ *
+	    ;'. ,\< ? "{}|=-
+	    gHiJkLmNoPqRsTuVwXyZ01234 67 9`! #
+	      +[] ' / \<>?:"{}|=-A
+	      jKlMnOpQrStUvWxYz012345 789`!@
+	      _+[];'./,\<>?:"{}|=-aBc eFgHiJ
+	    q
+	    XyZ012 4567
+	  @#  
+	
+	  +[];'./,\ >?:"{}| -A Cd f
+	
+	    OpQrStU W Y 01 3 5 789` @#$% &
+	
+	      /,\ > : {}|=-aBcDeFgH J LmNo q s u wX
+	      678 `!@#$%^&* ) +[];'./,\<>?: { | - b d fG
+	      pQr tUv x z0123456789`!@#$%^&       
+	      ?:"{}|= a c eF H J LmNoPqRsTu wXyZ 
+	        !@#$%^&*()_+[];'. , <>?:"{}|=-AbC E GhIj
+	      StU WxY 0 23456789`!@#$
+	      [];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ01234 6789`!@#$%^&*()_+ ];'./,\<>? "{}|=- bCd fGhIjKlMnOpQrStUv
+	    2
+	  789
+	  
+	  *()_+[];' /,\<>?: {} =- B
+	
+	    kLmNoPq s u wX Z 1 3456 89`! #
+	
+	      ];' / \ >?:"{}|=-AbCd f hIjKl n p r tU
+	      345 789`!@#$% & ()_+[];'./,\< ? " } = a cD
+	      mNo qRs u wXyZ0123456789`!@#$       
+	      \<>?:"{ | - bC E G IjKlMnOpQr tUvW 
+	        89`!@#$%^&*()_+[] ' /,\<>?:"{}|=- B DeFg
+	      PqR TuV X Z0123456789`!
+	      )_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01 3456789`!@#$%^&*( _+[];'./,\ >?:"{} =-a cDeFgHiJkLmNoPqRs
+	    Z
+	  456
+	9`!
+#$%^&
