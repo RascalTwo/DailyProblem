f9e5da73e4d955d43b9796d61e3370002c140f59
Rascal_Two <therealrascaltwo@gmail.com>
2023-02-03 14:07:38 +0000

    Add Codewars 57029e77005264a3b9000eb5 JavaScript solution

---
+++ problems/Codewars/57029e77005264a3b9000eb5/README.md
@@ -0,0 +1,15 @@
+[_metadata_:generated]: - "true"
+
+# Nothing special
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`57029e77005264a3b9000eb5`](https://www.codewars.com/kata/57029e77005264a3b9000eb5) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+The notorious Captain Schneider has given you a very straight forward mission. Any data that comes through the system make sure that only non-special characters see the light of day.
+
+Create a function that given a string, retains only the letters A-Z (upper and lowercase), 0-9 digits, and whitespace characters. Also, returns "Not a string!" if the entry type is not a string.
---
+++ problems/Codewars/57029e77005264a3b9000eb5/solve.js
@@ -0,0 +1,64 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{} =
+  cDeFgH JkLmNo qRs uVw yZ012345 7 9`!@ $ ^&*()_+[ ; ./,\<>?:"{}|=-AbCdEfGhIjKlMn pQr
+t
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ012
+	
+	789`!@#$%^&*()_+[]; ./,\<>?:"{}
+	  bCdEfGhIj lMnOpQr tUvWxYz0123
+	    `!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLm oPqRsTuV XyZ0123456 89`!@#
+	    )_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOp rStUvWxYz 123456 89`! #$% &*()_+[];'./,\ >? "{}|=- BcDe gHi kL NoPqRsTuVw
+	    3456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIj lMnOpQrStUvWxY
+	    
+	    !@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHi kLmN P RsTuVwXyZ
+	    6789`!@#$%^&*()_+[];'./,\<>?:"{}|=-A CdEf hIjK M OpQrStUvW
+	    3456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeF HiJk m oPqRsTuVw
+	    
+	    9`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrSt vWx
+	    456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ01234567 9`!@#$%^
+	    [];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOp rStUvWxY 0123456789`!@#$%^&*( _+[ ;  /,\< ?:
+	    
+	    gHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^    _+[] './    ?:
+	    AbCdEfGhIjKlMnOpQrStUvWxYz012345678     $%^&*()_+[];' /,     "{}|=-a
+	    HiJkLmNoPqRsTuVwXyZ0123456789`!@#$% &*()_	[];'./,\<> : {}|	-AbCd
+	    KlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[     ,\<>?:"{}|=-aBc eFg     mNoPqRsT
+	    0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEf  IjKlMnOpQ StUvWxY  1234567
+	    $%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqR	TuV XyZ01234	67
+	    $%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnO   StUvWxYz0 23456   `!@#$%^
+	    [];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRs  	wXyZ0123 5678  	@#$%
+	    +[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz	123 56789`!@	$%
+	    +[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVw   012345678 `!@#$   *()_+[]
+	    <>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0  	456789`! #$%^  	)_+[
+	    \<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789  @#$%^&* )_+[];'./,\  ?:"{}
+	    dEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$ ^&*(
+	    './,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ012345678 `!@#$%^&*()_
+	    /,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz012     89`!@#$%^& ()_+[]     \<>?:"
+	    BcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&* )_+[];'./
+	    "{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz012345 789`
+	    &*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ 123456789`!@
+	    ()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrSt     z012345678 `!@#$%     _+[];'
+	    ?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ01234567 9`!@#
+	    )_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMn  QrStUvWxYz 1234 67  `!@#$%^ *()
+	    ./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ01  456789`!@#$ ^&*()_  ];'./,\
+	  "{}
+	  bCdEfGhIjK MnOpQrS UvWxYz0123 5
+	    @#$%^&*( _+[];'./,\<>? "
+	      DeF HiJkL N P RsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$
+	      []; . , <>? " } =- B D FgHiJkLmNoPqRsTuVwXyZ012 4 6789
+	      *()_+[];'./,\< ? "{ |=-AbCdEfGhIjKlMnOpQrStUvWxYz0 2 456789`!@#$%^&*
+	      ./,\<> :"
+	    B
+	    iJkLmNoP RsTuVwXy 0
+	      9`!@#$ ^&*()_ []; ./, <>?:"{}| - bCdE G IjKlMnOp r tUvWxYz0123456789`!@#$%^&*() +[];
+	    >
+	    =-aBcDe g i kL N P RsT VwXy 0
+	      9`! # % &*()_+[];'./,\
+	      =-AbCdEfGhIjKlMnOpQrStUvWxYz01234567 9`!@#$%^
+	    [
+	  /,\
+	?:"
+}|=-a
