c80891feffb27a7cd2f2836fe00c472cec79780d
Rascal_Two <therealrascaltwo@gmail.com>
2023-02-14 16:01:26 +0000

    Add Codewars 5b5097324a317afc740000fe JavaScript solution

---
+++ problems/Codewars/5b5097324a317afc740000fe/README.md
@@ -0,0 +1,34 @@
+[_metadata_:generated]: - "true"
+
+# Not above the one!
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5b5097324a317afc740000fe`](https://www.codewars.com/kata/5b5097324a317afc740000fe) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+## Task
+
+Implement a function which finds the numbers less than `2`, and the indices of numbers greater than `1` in the given sequence, and returns them as a pair of sequences. 
+
+Return a nested array or a tuple depending on the language:
+
+* The first sequence being only the `1`s and `0`s from the original sequence. 
+* The second sequence being the indexes of the elements greater than `1` in the original sequence.  
+
+## Examples
+
+```javascript
+[ 0, 1, 2, 1, 5, 6, 2, 1, 1, 0 ] => [ [ 0, 1, 1, 1, 1, 0 ], [ 2, 4, 5, 6 ] ]
+```
+```haskell
+[ 0, 1, 2, 1, 5, 6, 2, 1, 1, 0 ] => ( [ 0, 1, 1, 1, 1, 0 ], [ 2, 4, 5, 6 ] )
+```
+```python
+[ 0, 1, 2, 1, 5, 6, 2, 1, 1, 0 ] => ( [ 0, 1, 1, 1, 1, 0 ], [ 2, 4, 5, 6 ] )
+```
+
+Please upvote and enjoy!
---
+++ problems/Codewars/5b5097324a317afc740000fe/solve.js
@@ -0,0 +1,56 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{ |
+  BcD FgHiJk m oPqR TuVw
+  012 456789 !@#$ ^& ()_+[];'./,\<>?
+    =-AbC EfGhI K MnOpQrStU W Yz0
+    6789`!@#$%^&*()_+[];'./,\<>?:"{}
+  a
+  eFgHiJ LmNoPqRsTuVwXyZ01
+3
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ012
+	
+	789`!@#$%^&*()_+[ ;' /, <
+	  {}|=-AbCdE Gh jK M
+	pQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqR T V Xy 01 34 67 9` @# %^ *( _+ ]; . ,\ > :" }| -Ab dEf hIj lM O Qr tUvWxYz 123456789
+	@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPq s u wX Z0 23 5 78 ` @ $ ^& ()_+[]; ./,\<>?:"
+	|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@ $ ^ * )_ [ ;' / \ >? "{}|=-a cDeFgHi kLm
+	PqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=- b d fG Ij lM Op rS U Wx z 12 4 6 89 !@#$%^& ()_+[];'.
+	\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0 2 4 6 89 !@ $% &*()_+[ ;'./,\<>?
+	{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz012 45 78 `! #$ ^& ( _+[ ; ./, <> :"{ |=- Bc eF Hi kLmNoPq sTuVwXyZ0
+	
+	56789`!@ $%^&*()_+[];'./,\<>?:"{ |
+	  CdE GhIjKl n pQr tUvWxYz012345 7 9`! #$%^&*()_+ ] './
+	  ?:" }|=- B D Fg i k mNoPqRsTuVw yZ012
+	    9`!@#$%^& ( _+[
+	      >?:"{}|=-AbCdEfGhIjKlM
+	    t
+	    012345678 `! #$%
+	      ];'./,\<>?:"{}|=-aBcDeFg
+	    N
+	  s
+	  XyZ0123456789`!@#$%^&*( _+[];'./,\<>?:"
+	  -AbCdE GhIjKlM
+	p
+	
+	UvWxYz01 3456789`!@#$%^& (
+	  ];'./, <>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ012345
+	89
+	
+	$%^ *()_ [ ; ./ \ > :"{ |=-A C
+	  hIj l n pQrStUvWx z01 34 6789`! #$%^&*(
+	  ];' / \ >?:"{}|=- BcDe gH JkLmNo qR TuVwX
+	  234 6789`!@ $ ^&*
+	  [];'./, <>?: { | -A CdEfG
+	    nOpQrStUvWxYz012345678 `!@#
+	    )_+[];'./,\<>?:"{}|=-a cDe
+	  J
+	  oPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./, <>?:"{}|=-AbCdEfGhIjKlMnOpQrS
+	v
+	  012
+	567
+	
+!@#$%
