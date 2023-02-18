e17be9c4c9b2f2a70d5e97475be81c9bbc9e1696
Rascal_Two <therealrascaltwo@gmail.com>
2023-02-18 03:21:41 +0000

    Add Codewars 56606694ec01347ce800001b C# solution

---
+++ problems/Codewars/56606694ec01347ce800001b/solve.cs
@@ -0,0 +1,47 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$% &*()_ [];'./,\
+>
+    |=-aBc eFgHiJ LmNo qRsTuVwXyZ0123 56 89` @# %^& ()
+    '
+        {}|=-A C E G I K Mn p r t v x z0 2 4 6 8 `!
+    &
+(
+
+
+#$%^& ()_+[];'./,\<>?:
+{}|=- BcDeFgH
+
+kLmNoPqRsTuVw
+yZ0123 56789 !@#$%^&*()_+[
+;
+    <>?:"{
+    bCdEfG IjKl nOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\
+    {
+        FgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$ ^& ()_+[
+    ,
+    
+    -AbCdEfGhIjK Mn pQr
+    xYz012345678 `! #$%
+    _+[];'./,\<> :" }|=
+    eFgHiJ LmNo qRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-A Cd fGh jK MnO Qr
+    x
+        789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBc eF HiJk
+    q
+
+    XyZ0123456789 !@ $%^
+    +[];'./,\<>? "{} =-A
+    GhIjKlMnOpQr tU WxYz
+    56789` @#$% &*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPq sT VwX Z0 234 67
+    #
+        [];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrS Uv xYz0
+    6
+    
+    &*()_+[];'./ \< ?:"
+    aBcDeFgHiJkL No qRs
+    yZ0123456789 !@ $%^
+    +[];'. ,\<> :"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01234567 9` @#$ ^& ()_ []
+    \
+         aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789` @# %^&*
+    ]
+'
