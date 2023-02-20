b74ce042a008a9a9f9c577ade88bba1c23e41303
Rascal_Two <therealrascaltwo@gmail.com>
2023-02-20 03:44:01 +0000

    Add Codewars 5715eaedb436cf5606000381 C# solution

---
+++ problems/Codewars/5715eaedb436cf5606000381/solve.cs
@@ -0,0 +1,56 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$ ^&*()_+
+];'./ \<>?:"{}|=-a
+cDeFg iJkLmNoPqRsTuVwXyZ012345678
+
+!@#$%^ *()_+ ];'.
+,
+  ?:"{}| -AbCdE GhI KlMnOpQrStUvWxYz0 2345
+  9    
+    ()_+[] './,\<>?: {} = a c e g i kLm
+  q
+s
+
+
+
+#$%^& ()_+[];'./,\<>?:
+{}|=- BcDeFgH
+JkLmN PqRsTuVwXyZ0
+
+3456789`!@#$%
+&*()_+ ];'./ \<>?:
+{
+  -AbCdE
+  IjKlMnOpQrStU WxYz0123 56 89 !@ $%^ *()_+[];'./,\<>?:"{
+  -aBcDeFgHiJkL NoPqRsTu wXy 01 34 678 `!@#$%^&*()_+[];'./
+  >?:"{}|=-AbCd fGhIjKlMn pQ St vW Yz01 3456789`!@#$%^&*()
+  ];'./,\<>?:"{ |=-aBcDe gHiJkLmNoPqRsTuVwX
+  123456789`!@# %^&*()_+[ ;'. ,\< ?:" }|=- bCdEfGhIjKlMnOpQrS
+  WxYz01 345678 `!@ $%^&*()_+[];'./,\ >?:"
+  =
+    eFgHiJ LmNoPqRsTuVwXyZ0123456
+  `
+  
+  &*()_+
+  './,\< ?:"{}| -AbC EfGhIjKlMnOpQrStUvWxY 0123 5678 `!@ $%^&*()
+  ]
+    \<>?: {}| - BcDeFgHiJkLmNoPqRsTu
+    0123456789`!@#$%^&*()_+[];'./, <>?:"{}|=-AbCdEfGhIjKl nOpQrStUvWxYz01234567 9`!@ $%^ * )_+[] './,\<
+  "
+  
+  BcDeFg iJkLmN PqRsT VwXyZ0123456789 !@#$%^&
+  _
+    ./,\< ?:"{}| - bCd fGhIjKlMnOpQ
+    WxYz01 345 7 9`! #$%^&*()_
+    ./, <>?: { | -a c e gHiJkLm oPqR T
+      123456789 ! #$%^&*()_+[];' /,\<>
+    |
+    dEfGhI KlMnOpQ
+  U
+  
+  1234567 9`!@#$ ^&* )_+[];'./,\<>? "{}|
+  B
+    HiJkLm oPqRsTuVwXy 01 3 5 789`!@#$%
+  (
+_
