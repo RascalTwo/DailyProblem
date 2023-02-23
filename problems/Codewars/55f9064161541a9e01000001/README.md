7066404e7d5044b0034c648934e5f02261cd5a2e
Rascal_Two <therealrascaltwo@gmail.com>
2023-02-23 23:10:08 +0000

    Add Codewars 55f9064161541a9e01000001 JavaScript solution

---
+++ problems/Codewars/55f9064161541a9e01000001/README.md
@@ -0,0 +1,44 @@
+[_metadata_:generated]: - "true"
+
+# Valid number to 2 decimal places
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`55f9064161541a9e01000001`](https://www.codewars.com/kata/55f9064161541a9e01000001) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Check that a number is a valid number that has been given to 2 decimal places.  The number passed to the function will be given as a string. If the number satisfies the criteria below, the function should return true, else it should return false. 
+
+
+Please check the criteria for a valid number:
+
+
+optional + or - symbol in front
+
+
+optional digits before a decimal point (digits are characters ranging from '0' to '9')
+
+
+a decimal point
+
+
+exactly two digits after the point
+
+
+nothing else
+
+
+_______________________
+
+Examples of valid and non-valid numbers
+
+
+List of valid numbers: [ "0.00"  "3.90"  "1000.00"  ".00"  "-2.55"  "+2.10"  "-.55"]  
+
+
+List of non-valid numbers: ["hellow 11.99" "11.9"  "11"  "11."  ".9"]
+
+
---
+++ problems/Codewars/55f9064161541a9e01000001/solve.js
@@ -0,0 +1,71 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"
+  =-aBcD FgHiJkLmNoPqRsTuVwXyZ0123456789
+!
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	
+	MnOpQrStUvWxYz0 234567 9` @ $%^&*() +[];'./, <> :" }
+	  BcDeFgHiJ LmNoPqR Tu wX Z
+	    6789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhI
+	    pQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<
+	    |=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$
+	    _+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWx
+	    456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFg
+	    NoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>
+	    =-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&
+	    ];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ
+	    6789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGh
+	    OpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<
+	    |=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*
+	    ;'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz012
+	    9`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkL
+	    sTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}
+	    dEfGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_
+	    /,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ01234
+	    !@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnO
+	    vWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aB
+	    iJkLmNoPqRsTuVwXyZ0123456789`!@#$%^&*()_+[];'
+	    ?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123456789`
+	    &*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTu
+	    123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-AbCdEf
+	    MnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\
+	    }|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!@#$
+	  ()_
+	
+	  /,\<>?:"{} =-AbCdE Gh jK M 
+	    
+	    01234 6 8 `!@#$%^&*()_+[];'
+	    
+	    =-aBcDeF HiJkLmNoP
+	      yZ0123 56789`!@#$%^&*()_+[];'./,\<>?:
+	    A
+	
+	    jKl nOpQ S U Wx z 1 3456 89`! #    
+	        /,\ > : {}|=-aBcDeFgHiJkLmNoPq sT V XyZ 12 4 678
+	        ()_ [ ; ./,\<>?:"{}|=
+	        jKl nOp r tUvWxYz01234567
+	        *() +[]; . , <> : { |=-a cDeFg
+	          TuVwXyZ0123456789`!@#$%^&*()_  
+	          {} =- bCdEfGhIjKlMnOpQrStUvWxYz     
+	         ^
+	        ./ \< ?:" } =-aBcDeFgHiJkLmNoPqRsTuVw y 0123456789`!@#$%^&*()_+[]
+	        "{} = AbCdEfGhIjKlMnO Q S U Wx z 123 
+	        %^& ( _ [];'./,\<>?:"{} 
+	        i k m o q s uVw y 0 2 4 6 89 ! # % &*( _ [ ; .
+	        |=- bCd f hIjKlMn 
+	        z0123456789`!@#$%^&*()_+[];'./,\<> :"{} =-aBcDeF HiJ LmN P RsTuVwXyZ0123456789`!@#
+	    )
+	  ;'
+	,\<
+	
+	
+	
+	
+	
+CdEfG
