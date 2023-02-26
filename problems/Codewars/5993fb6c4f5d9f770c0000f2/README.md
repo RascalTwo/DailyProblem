c96ad44641a08a0a86acc998fd5412c9ee3ee662
Rascal_Two <therealrascaltwo@gmail.com>
2023-02-26 18:38:00 +0000

    Add Codewars 5993fb6c4f5d9f770c0000f2 JavaScript solution

---
+++ problems/Codewars/5993fb6c4f5d9f770c0000f2/README.md
@@ -0,0 +1,15 @@
+[_metadata_:generated]: - "true"
+
+# Sum a list but ignore any duplicates
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`5993fb6c4f5d9f770c0000f2`](https://www.codewars.com/kata/5993fb6c4f5d9f770c0000f2) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+Please write a function that sums a list, but ignores any duplicate items in the list.
+
+For instance, for the list [3, 4, 3, 6] , the function should return 10.
---
+++ problems/Codewars/5993fb6c4f5d9f770c0000f2/solve.js
@@ -0,0 +1,91 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'./,\<>?:"{}|=-aB D
+  HiJkLm oPqRsTuVwXyZ01234567 9` @#$% &* )_+[];'./,\<>?:" }|= A Cd fGhIjKlMnOpQrStUvWxY 012 45
+    !@#$%^&*()_+[ ;'./ \< ?:" } =-aB DeF
+H
+
+
+#$%^ *( _
+	];'./ \<>? " }|=-aBcDeFgHiJkL
+	oPqRs uVwXyZ 1 3456789`!@#$
+	&*()_+[];'./,\<>?:"{}|=-AbCdE G Ij
+	
+	OpQrS UvWxYz0123 5 7
+	  @#$% &* )_ []; ./,
+	  :"{} =-a
+	  FgHi kL No qR TuV XyZ
+	  4567 9` @#$ ^& ()_ []; ./ \< ?: {} =- bC Ef hI KlM OpQr
+	  WxYz 123 56 89 !@ $% &* )_ [] '. ,\< ?:"{
+	  aBcD Fg iJ Lm oP Rs uV Xy 01 34 678 `! #$ ^& ()_ [];'
+	  <>?: {} =-A Cd fGh jK Mn pQ StU Wx z0 234 6789
+	  $%^& () +[ ;' /, <> :" }| -a cDe gH JkL NoPq
+	  VwXy 01 34 67 9` @# %^ *( _+ ];' /,\
+	  "{}|= AbC Ef hI KlM Op rS Uv xYz 123 56 89 !@# %^ *( _+[ ;' /,\ >?:
+	  =-aB DeF HiJ Lm oP Rs uV XyZ 12 45 78 `! #$ ^& () +[ ;' /, <> :"{ |=-A
+	  fGhI Kl nO Qr tU Wx z0 23 56 89` @# %^ *( _+ ];' /,\<
+	  {}|= aB De gH Jk mN Pq sT Vw yZ 12 45 78 `!@ $%^&
+	  +[];' /, <> :" }| -Ab dE Gh jK Mn pQ St vW Yz 12 45 78 `! #$% &*()
+	  ;'./ \< ?: {} =- Bc eF Hi kL NoP RsTu
+	  Z012 45 78 `!@ $% &* )_ []; ./,\
+	  "{}| -A CdE Gh jKl nO Qr tU Wx z0 23 56 89 !@ $% &* )_ []; ./, <>?
+	  |=-a cD Fg iJ Lm oP RsT VwXy
+	  3456 89 !@ $% &* )_ [] '. ,\< ?:" }| -A Cd fGh jKlM
+	  rStU Wx z0 23 56 89 !@ $% &* )_ []; ./ \<> :"{}
+	  BcDe gH Jk mN Pq sT Vw yZ 12 45 78 `! #$ ^& () +[] './,
+	  :"{} =- bC Ef hI Kl nO Qr tU Wx z01 345 789`
+	#$
+	
+	()_+[ ;'./,\< ?:"{}|= aBc e
+	  JkLmNoPqRsTuVwXyZ0123 5
+	    @# %^&*()_+[];'. ,
+	      |=-AbC EfG IjKlMnOpQrStUvWxYz
+	    6
+	    #$%^&*()
+	    ./,\<>?:"{}|=-aBcDeFg iJkLm oP R
+	      012 456789`! # %^&*()_+[];'./
+	      }|=-AbCdEfGhI KlMnOpQr t vWx
+	      6789`! #$%^
+	    [] './,\<
+	  {
+	  
+	  eFgHiJkLm o
+	    VwXyZ 12345678 ` @#$%^&*()_+[];'.
+	    :" }|=-AbCdE GhI KlMnOpQrSt vWxYz0 23
+	    `!@#$% &*()_+[];
+	  \
+	?
+	
+	|=-aBcDe gHiJkLmNoPqRsTuVwX Z
+	  45678 `!@#$%^ * )_+ ];'./,\<>?:"{}|=-
+	  EfG IjK M Op
+	  UvW Yz01 34567 9`!@ $% &*()_+[];'./,\<>?: {
+	    cD FgHi kLm oP R
+	      012 45 789`
+	    &
+	  +
+	  ./,\<> :"{}
+	-
+	
+	EfGhIjKl nOpQrStUvWxYz0 2345 7
+	  @#$%^& ()_+[];'./,\<>?:"{}|=-aB D FgHi k mNoP R TuVwX
+	0
+	
+	56789`!@#$%^&*( _+[] './,\<> :"{}|=-AbC E
+	  jKl nOpQ StUvWxY 012345678 `! #$%^&*()_+[ ;
+	    >?:"{}|=-aB DeF HiJkLmNoPqRsTuVw yZ0123 56789`!@#$ ^
+	      ;'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz0123 56789`!@#$
+	    _+[
+	  /
+	<>?
+	
+	|=-aBcDeFgHiJkLm oPqR TuVwXyZ 123456789` @
+	  &*( _+[] ' / \< ? " }|= AbCd f
+	    MnO QrStU W Yz0123456789`!@#$%^ *()_ [] '. ,\<>?:"{}|=- BcDeF
+	    mNoPqRsTuVw yZ0 23456789`!@#$%^& ()_+[] './,\<>?:" }
+	      fGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./, <>?:"{}|=-aBcDeFgH
+	    oPq
+	  V
+	yZ0
+	
+45678
