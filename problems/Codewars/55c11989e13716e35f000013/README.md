135c8370465aca925b27dc0c49e5a3b129e2ead9
Rascal_Two <therealrascaltwo@gmail.com>
2023-02-27 23:24:23 +0000

    Add Codewars 55c11989e13716e35f000013 JavaScript solution

---
+++ problems/Codewars/55c11989e13716e35f000013/README.md
@@ -0,0 +1,25 @@
+[_metadata_:generated]: - "true"
+
+# Adding Binary Numbers
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                                    |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`55c11989e13716e35f000013`](https://www.codewars.com/kata/55c11989e13716e35f000013) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924076/javascript_ehszr7.svg" alt="JavaScript" title="JavaScript" width="50" />](solve.js) |
+
+<!-- INFO TABLE END -->
+
+##Task:
+You have to write a function `add` which takes two binary numbers as strings and returns their sum as a string.
+
+##Note:
+* You are `not allowed to convert binary to decimal & vice versa`.
+* The sum should contain `No leading zeroes`.
+
+##Examples:
+```
+add('111','10'); => '1001'
+add('1101','101'); => '10010'
+add('1101','10111') => '100100'
+```
---
+++ problems/Codewars/55c11989e13716e35f000013/solve.js
@@ -0,0 +1,83 @@
+//	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@#$%^& ()_+[];'.
+  < ? "{}|=-a c e gHiJkLm
+
+  RsT VwXyZ0 2 456 89`!@ $ ^&
+  _+[]; ./,\<>?:" }| -AbCdEfG Ij lMnOpQ S
+    Yz012 45 789`!@#$% & ()_+[];'./
+    :"{}|= a cDeFgH J Lm o qRsTuVw
+    23456 8 `!@#$%^ * )_+
+  '
+  \<>?:" }|=-AbC E GhIjKlMnOpQrStUvWxYz0 234 6 89`!
+#$
+
+
+#$%^ *( _
+	];'./,\<>?:"{}| -aBcDeFg iJkLmNoPqRs
+	    Z0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-
+	    GhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[]
+	    <>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXyZ0123456789`!
+	    *()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQrStUvWxYz01234
+	    !@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFg
+	JkL
+	
+	qRsTuVwXyZ012345 789`!@#$ ^&*()_+[];'
+	    
+	  }|=-AbCd fGhIjKlM
+	  r
+	    Yz01234567 9`!@#$%^&*()_
+	      \<> :"{}|= a cDe
+	      NoP RsTuVwX Z012345
+	      $%^ *()_+[] './,\<>?
+	
+	    bCdEfGhIjKl nOpQrS UvW Yz012345
+	      $%^&*() +[]; ./,\<>?:"{}|=-aBcDeFgHiJkLmNoPq sTuV
+	        6789`!@#$%^&
+	
+	    './,\<>?: {}|=-AbC
+	      lMnOpQrStUvWxYz012345
+	      $
+	        ;'./ \ >?
+	        DeFg i kLmNoP R Tu
+	      3
+	      @#$%^&*()_+[];'./,\<>
+	      A
+	        lMnO Q St
+	        4567 9 !@#$%^ * )_
+	      \
+	
+	    =-aBcDeFgH JkLmNoPqRs
+	      1234 678 `!@#$%^&*()_+[]; ./,\< ?:"{
+	      E
+	        pQrStUv xYz0123456789`!@# % &*()_+[];'./,\<>? " }|=-a c
+	        N
+	          0123 5 7 9`!@#$%^&*()_+ ];'./,\< ?:"{}|
+	          KlMn p r tUvWxYz0123456 89`!@#$% &*()_+
+	          "{}| - B DeFgHiJkLmNoPq sTuVwXyZ 123456
+	          ()_+ ] ' /,\<>?:"{}|=-A CdEfGhIj lMnOpQ
+	        1
+	      `
+	
+	      _+[];'./,\<>  "{}|=-aBcDeFgHiJ
+	      sTu w yZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|
+	      GhIjKl nOpQrStUv xYz 1 345 7
+	  @
+	  
+	  
+	  
+	    <>? "{} = aBcDeFgHiJ LmNoPqRs uVwXyZ01
+	    
+	    $%^&*() +[]; ./,\<> :"{}
+	    d
+	      MnO Qr t vWxYz0123456789`!@#$
+	      []; ./ \ >?:"{}|=-aBcDeFgHiJk
+	      TuVwXyZ 1234 6789`! #$%^  ()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlMnOpQ
+	      z012345 789` @#$%^& ()_+  ;'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVw
+	      56789`!@#$%^&*()_+[] './ \<>? "{}|=-AbCdEfGhIjKlM OpQr
+	      0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgH
+	      qRsTuVwX Z0123456
+	    #
+	^&*
+	
++[];'
