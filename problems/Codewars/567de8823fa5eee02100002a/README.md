5129f21256d7afab27f1dae4693ab4ce2ff2f3f0
Rascal_Two <therealrascaltwo@gmail.com>
2023-03-13 14:52:19 +0000

    Add Codewars 567de8823fa5eee02100002a Python solution

---
+++ problems/Codewars/567de8823fa5eee02100002a/README.md
@@ -0,0 +1,13 @@
+[_metadata_:generated]: - "true"
+
+# Regexp Basics - is it all whitespace?
+
+<!-- INFO TABLE BEGIN -->
+
+| Provider                                        | Source                                                                               | Solutions                                                                                                                                        |
+| :---------------------------------------------: | :----------------------------------------------------------------------------------: | :----------------------------------------------------------------------------------------------------------------------------------------------: |
+| [Codewars](../../../docs/providers/Codewars.md) | [`567de8823fa5eee02100002a`](https://www.codewars.com/kata/567de8823fa5eee02100002a) | [<img src="https://res.cloudinary.com/rascaltwo/image/upload/v1631924087/python_xzdlti.svg" alt="Python" title="Python" width="50" />](solve.py) |
+
+<!-- INFO TABLE END -->
+
+Implement `String#whitespace?(str)` (Ruby), `String.prototype.whitespace(str)` (JavaScript), `String::whitespace(str)` (CoffeeScript), or `whitespace(str)` (Python), which should return `true/True` if given object consists exclusively of zero or more whitespace characters, `false/False` otherwise.
---
+++ problems/Codewars/567de8823fa5eee02100002a/solve.py
@@ -0,0 +1,40 @@
+#	Content unavailable in accordance with https://docs.codewars.com/community/rules/#rules
+
+`!@ $%^&*()_+[];'./,\<>
+    |=-aBc eFg iJkLmN Pq sTuVwXyZ01234567
+
+
+#$%^&* )_+[];'./,\<> :" }|=-
+BcDe gHiJkLmN PqRsTu wXyZ012345
+
+89`!@#$%^&*()_+[];'./ \<>?:"{
+|=- bCdEfGhIjKlMnO
+    UvWxYz012345678 `!@# %^&*()_
+    ./, <>?:"{}|=-aBcDeFgHi
+        sTuVwXyZ0123456789`!@#$%^&*()_+[]; ./,\<
+        AbCdEfGhIjKlMnOpQrStUvWxYz01234 678 `!@#$
+        ];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTuVwXy 01234
+        $%^&*()_+[];'./,\<>?:"{}|=-AbCdEfGh jKlMnO
+        Yz0123456789`!@#$%^&*()_+[];'./,\<>?: {}|=-a
+        kLmNoPqRsTuVwXyZ0123456789`!@#$%^&*( _+[];
+        "{}|=-AbCdEfGhIjKlMnOpQrStUvWxY 012345 789`!@
+        +[];'./,\<>?:"{}|=-aBcDeFgHiJkLmN PqRsTu  XyZ 12345
+        %^&*()_+[];'./,\<>?:"{}|=-AbCdEfGhIjKlM OpQ StUvW
+        6789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcD FgHi kLmNoP
+
+sTuVwXyZ0123456789`!@# %^&*()_
+[]; ./,\<>?:"{}|=-A
+    
+    lMnO QrStUv xYz012 456789`
+    
+    _+[];'./,\ >? "{}|=-aBcD FgH J Lm o qR T VwX Z0 2 456789`!@#$%^
+    
+    ./,\<>? "{ |=-Ab dEfGh jKlMn
+    
+    Yz0 2 45 789`!@#$%^
+        './,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTu wXy 0 23 56789`!@#$%^
+        '. ,\<>?:"{}|=-AbCd fGhIjKlMnOpQrStUvWxYz0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=-aBcDeFgHiJkLmNoPqRsTu
+        456789`!@#$%^&*()_+[];
+        "{}|=-AbCdEfGhIjKl nOp rStUvWxYz0123456789`!@
+        +[] './,\<>?:"{}      
+            qRsTuVwXyZ0123456789`!@#$%^&*()_+[];'./,\<>?:"{}|=
